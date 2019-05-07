b8047107bc72fa2712381faa075f79d231a2b433
brandon <26451183+ahkuma@users.noreply.github.com>
2019-05-07 14:00:37 +0800

    refactor

--- src/javascript/app_2/App/Components/Elements/Popover/popover.jsx
+++ src/javascript/app_2/App/Components/Elements/Popover/popover.jsx
@@ -5,11 +5,21 @@ import PopoverBubble from './popover-bubble.jsx';
 class Popover extends React.PureComponent {
     constructor(props) {
         super(props);
-        this.state = { is_open: false };
+        this.state = {
+            is_open         : false,
+            target_rectangle: null,
+        };
         this.target_reference = React.createRef();
     }
 
-    toggleIsOpen = () => this.setState({ is_open: !this.state.is_open });
+    componentDidMount() {
+        this.setState({ target_rectangle: this.target_reference.current.getBoundingClientRect() });
+    }
+
+    toggleIsOpen = () => {
+        this.setState({ is_open: !this.state.is_open });
+        this.setState({ target_rectangle: this.target_reference.current.getBoundingClientRect() });
+    }
 
     render() {
         const {
@@ -36,8 +46,8 @@ class Popover extends React.PureComponent {
                     className={className}
                     has_error={has_error}
                     icon={icon}
-                    is_open={(has_error || this.state.is_open) && this.target_reference.current}
-                    target_reference={this.target_reference}
+                    is_open={(has_error || this.state.is_open) && this.state.target_rectangle}
+                    target_rectangle={this.state.target_rectangle}
                     message={message}
                 />
             </div>

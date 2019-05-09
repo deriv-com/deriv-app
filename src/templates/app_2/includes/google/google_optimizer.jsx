3e790db0ac04fd13996715e927f5dc886871dbfd
ashkan <ashkan.nemati@gmail.com>
2019-05-09 14:54:00 +0200

    remove spaces

--- src/templates/app_2/includes/google/google_optimizer.jsx
+++ src/templates/app_2/includes/google/google_optimizer.jsx
@@ -7,8 +7,7 @@ const GoogleOptimizer = () => (
         <script dangerouslySetInnerHTML={{__html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;
         h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
         (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);
-        })(window,document.documentElement,'async-hide','dataLayer',4000,{'
-GTM-NF7884S':true});`}}/>
+        })(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-NF7884S':true});`}}/>
     </React.Fragment>
 );
 export default GoogleOptimizer;

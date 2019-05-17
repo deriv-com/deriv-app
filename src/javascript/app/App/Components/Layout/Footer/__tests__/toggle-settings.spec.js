882e9ea1469f5179816d32a4fe856f77120212e6
Rin <26451183+ahkuma@users.noreply.github.com>
2019-05-17 13:01:43 +0800

    Update src/javascript/app/App/Components/Layout/Footer/__tests__/toggle-settings.spec.js
    Co-Authored-By: cakasuma <amammustofa@gmail.com>

--- src/javascript/app/App/Components/Layout/Footer/__tests__/toggle-settings.spec.js
+++ src/javascript/app/App/Components/Layout/Footer/__tests__/toggle-settings.spec.js
@@ -3,7 +3,7 @@ import { expect }             from 'chai';
 import { configure, shallow } from 'enzyme';
 import Adapter                from 'enzyme-adapter-react-16';
 import { ToggleSettings }     from '../toggle-settings.jsx';
-import Icon                   from 'Assets/Common/icon.jsx';
+import Icon                   from 'Assets/Common;
 import { IconSettings }       from 'Assets/Footer';
 import { SettingsDialog }     from '../../../Elements/SettingsDialog/settings-dialog.jsx';
 import { CSSTransition }      from 'react-transition-group';

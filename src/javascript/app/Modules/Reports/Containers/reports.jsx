83c4035e7c76b0a0a777e7b82b34f64ccb19bda5
zakibinary <zaki@binary.com>
2019-05-17 17:40:35 +0800

    add blur to header and footer for reports page as per zeplin

--- src/javascript/app/Modules/Reports/Containers/reports.jsx
+++ src/javascript/app/Modules/Reports/Containers/reports.jsx
@@ -1,56 +1,73 @@
 import PropTypes      from 'prop-types';
 import React          from 'react';
 import { withRouter } from 'react-router-dom';
+import { connect }    from 'Stores/connect';
 import VerticalTab    from 'App/Components/Elements/VerticalTabs/vertical-tab.jsx';
 import { IconClose }  from 'Assets/Settings';
 import AppRoutes      from 'Constants/routes';
 import { localize }   from '_common/localize';
 
-const Reports = ({ routes, location, history }) => {
-    const menu_options = () => {
-        const options = [];
+class Reports extends React.Component {
+    componentDidMount() {
+        this.props.showBlur();
+    }
 
-        routes.forEach(route => {
-            options.push({
-                default: route.default,
-                icon   : route.icon_component,
-                label  : route.title,
-                value  : route.component,
-                path   : route.path,
+    componentWillUnmount() {
+        this.props.hideBlur();
+    }
+
+    render () {
+        const menu_options = () => {
+            const options = [];
+
+            this.props.routes.forEach(route => {
+                options.push({
+                    default: route.default,
+                    icon   : route.icon_component,
+                    label  : route.title,
+                    value  : route.component,
+                    path   : route.path,
+                });
             });
-        });
 
-        return options;
-    };
+            return options;
+        };
 
-    const action_bar_items = [
-        {
-            onClick: () => { history.push(AppRoutes.trade); },
-            icon   : IconClose,
-            title  : localize('Close'),
-        },
-    ];
-
-    return (
-        <div className='reports'>
-            <VerticalTab
-                header_title={localize('Reports')}
-                action_bar={action_bar_items}
-                alignment='center'
-                classNameHeader='reports__tab-header'
-                current_path={location.pathname}
-                is_routed={true}
-                is_full_width={true}
-                list={menu_options()}
-            />
-        </div>
-    );
-};
+        const action_bar_items = [
+            {
+                onClick: () => { this.props.history.push(AppRoutes.trade); },
+                icon   : IconClose,
+                title  : localize('Close'),
+            },
+        ];
+        return (
+            <div className='reports'>
+                <VerticalTab
+                    header_title={localize('Reports')}
+                    action_bar={action_bar_items}
+                    alignment='center'
+                    classNameHeader='reports__tab-header'
+                    current_path={this.props.location.pathname}
+                    is_routed={true}
+                    is_full_width={true}
+                    list={menu_options()}
+                />
+            </div>
+        );
+    }
+}
 
 Reports.propTypes = {
+    hideBlur: PropTypes.func,
     history : PropTypes.object,
     location: PropTypes.object,
     routes  : PropTypes.arrayOf(PropTypes.object),
+    showBlur: PropTypes.func,
 };
 
-export default withRouter(Reports);
+export default connect(
+    ({ ui }) => ({
+        hideBlur: ui.hideRouteBlur,
+        showBlur: ui.showRouteBlur,
+    })
+)(withRouter(Reports));

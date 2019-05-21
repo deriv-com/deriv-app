7d9e3c742ed2b41fe9f18b3c454840d476c4cc3b
Amam Mustofa <mustofa@binary.com>
2019-05-21 17:41:38 +0800

    add todo

--- src/javascript/app/Stores/Modules/Trading/trade-store.js
+++ src/javascript/app/Stores/Modules/Trading/trade-store.js
@@ -552,6 +552,7 @@ export default class TradeStore extends BaseStore {
 
     @action.bound
     onLoadingMount() {
+        // TODO: find better way to remove initial loader
         setTimeout(() => {
             this.root_store.ui.setAppLoading(false);
         }, 2200);

7da108f1c3eba864a3e487325d3590f55d6eb0cd
Amam Mustofa <mustofa@binary.com>
2019-05-21 16:23:20 +0800

    remove console log

--- src/javascript/app/Stores/Modules/SmartChart/smart-chart-store.js
+++ src/javascript/app/Stores/Modules/SmartChart/smart-chart-store.js
@@ -229,7 +229,6 @@ export default class SmartChartStore extends BaseStore {
 
     @computed
     get markers_array() {
-        console.log(...this.markers)
         return barriersObjectToArray(this.markers);
     }
 

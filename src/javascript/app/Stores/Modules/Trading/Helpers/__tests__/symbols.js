366242bd6b4d83d49893ec514c368efdf1033980
arash <arash@firstsource.io>
2019-05-22 13:31:50 +0430

    change detecting favorite market

--- src/javascript/app/Stores/Modules/Trading/Helpers/__tests__/symbols.js
+++ src/javascript/app/Stores/Modules/Trading/Helpers/__tests__/symbols.js
@@ -32,8 +32,21 @@ const active_symbols = [
     },
     {
         "allow_forward_starting": 1,
+        "display_name": "AUD/USD",
+        "exchange_is_open": 0,
+        "is_trading_suspended": 0,
+        "market": "forex",
+        "market_display_name": "Forex",
+        "pip": "0.00001",
+        "submarket": "major_pairs",
+        "submarket_display_name": "Major Pairs",
+        "symbol": "frxAUDUSD",
+        "symbol_type": "forex"
+      },
+    {
+        "allow_forward_starting": 1,
         "display_name": "Volatility 10 Index",
-        "exchange_is_open": 1,
+        "exchange_is_open": 0,
         "is_trading_suspended": 0,
         "market": "volidx",
         "market_display_name": "Volatility Indices",
@@ -121,29 +134,42 @@ const active_symbols = [
         "symbol": "OTC_HSI",
         "symbol_type": "stockindex"
     },
-];    
+];
 
 describe('pickDefaultSymbol', () => {
-    it('It Returns first open symbol if favorite list was empty', () => {
+    it('It Returns first open major_pair or random_index symbol if favorite list was empty', () => {
         const cq_favorites = {"indicators":[],"chartTitle&Comparison":[]};
         LocalStore.set('cq-favorites', JSON.stringify(cq_favorites));
-        expect(pickDefaultSymbol(active_symbols)).to.eql('RDBEAR');
+        expect(pickDefaultSymbol(active_symbols)).to.eql('R_25');
     });
 
-    it('It Returns first open symbol if all symbols were close in favorite list', () => {
+    it('It Returns first open major_pair or random_index symbol if all symbols were close in favorite list', () => {
         const cq_favorites = {"indicators":[],"chartTitle&Comparison":["OTC_N225","OTC_HSI"]};
         LocalStore.set('cq-favorites', JSON.stringify(cq_favorites));
-        expect(pickDefaultSymbol(active_symbols)).to.eql('RDBEAR');
+        expect(pickDefaultSymbol(active_symbols)).to.eql('R_25');
     });
 
-    it('It Returns the first open symbol in favorite list if an open symbol exist in favorite list', () => {
-        const cq_favorites = {"indicators":[],"chartTitle&Comparison":["R_10","OTC_HSI"]};
+    it('It Returns the first open symbol in favorite list if an open symbol exist in the favorite list', () => {
+        const cq_favorites = {"indicators":[],"chartTitle&Comparison":["OTC_HSI","R_50"]};
         LocalStore.set('cq-favorites', JSON.stringify(cq_favorites));
-        expect(pickDefaultSymbol(active_symbols)).to.eql('R_10');
+        expect(pickDefaultSymbol(active_symbols)).to.eql('R_50');
     });
 
-    it('It Returns a symbol which symbol.submarket is major_pairs|random_index if all active symbols were close', ()=> {
+    it('It Returns a major_pairs symbol if all active symbols were close', ()=> {
         const active_symbols = [
+            {
+                "allow_forward_starting": 1,
+                "display_name": "Bear Market Index",
+                "exchange_is_open": 0,
+                "is_trading_suspended": 0,
+                "market": "volidx",
+                "market_display_name": "Volatility Indices",
+                "pip": "0.0001",
+                "submarket": "random_daily",
+                "submarket_display_name": "Daily Reset Indices",
+                "symbol": "RDBEAR",
+                "symbol_type": "stockindex"
+            },
             {
                 "allow_forward_starting": 1,
                 "display_name": "AUD/USD",
@@ -156,34 +182,21 @@ describe('pickDefaultSymbol', () => {
                 "submarket_display_name": "Major Pairs",
                 "symbol": "frxAUDUSD",
                 "symbol_type": "forex"
-              },
+            },
             {
-                "allow_forward_starting": 1,
-                "display_name": "Japanese Index",
-                "exchange_is_open": 0,
-                "is_trading_suspended": 0,
-                "market": "indices",
-                "market_display_name": "Indices",
-                "pip": "0.01",
-                "submarket": "asia_oceania_OTC",
-                "submarket_display_name": "Asia/Oceania",
-                "symbol": "OTC_N225",
-                "symbol_type": "stockindex"
-              },
-              {
-                "allow_forward_starting": 1,
-                "display_name": "Hong Kong Index",
-                "exchange_is_open": 0,
-                "is_trading_suspended": 0,
-                "market": "indices",
-                "market_display_name": "Indices",
-                "pip": "0.01",
-                "submarket": "asia_oceania_OTC",
-                "submarket_display_name": "Asia/Oceania",
-                "symbol": "OTC_HSI",
-                "symbol_type": "stockindex"
-              },
-        ]
+              "allow_forward_starting": 1,
+              "display_name": "Volatility 10 Index",
+              "exchange_is_open": 0,
+              "is_trading_suspended": 0,
+              "market": "volidx",
+              "market_display_name": "Volatility Indices",
+              "pip": "0.001",
+              "submarket": "random_index",
+              "submarket_display_name": "Continuous Indices",
+              "symbol": "R_10",
+              "symbol_type": "stockindex"
+          }
+      ]
         expect(pickDefaultSymbol(active_symbols)).to.eql('frxAUDUSD');
     });
 

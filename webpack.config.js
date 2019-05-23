161ef053806581c44b7229481122087f9b12741f
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-05-23 13:26:48 +0800

    Add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,22 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry : path.join(__dirname, 'src', 'botPage', 'bot', 'cli.js'),
+    entry : path.join(__dirname, './src/app.js'),
     output: {
-        path    : path.resolve(__dirname, 'lib'),
-        filename: 'index.js',
+        path    : path.resolve(__dirname, 'dist'),
+        filename: 'bot.js',
     },
+    devtool: 'source-map',
     target: 'node',
     module: {
         rules: [
+            {
+                enforce: "pre",
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/,/\\srcatch/],
+                loader: "eslint-loader",
+                options: { fix: true },
+            },
             {
                 test   : /\.(js|jsx)$/,
                 exclude: /node_modules/,

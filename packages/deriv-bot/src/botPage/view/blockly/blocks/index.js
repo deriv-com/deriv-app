38b3b18320bc1427ed12d5b955f195a3110020bf
Ashkan <ashkanx@users.noreply.github.com>
2019-05-23 09:48:43 +0200

    Merge pull request #1 from MahboobehMohammadi/cleanup
    Refactoring

--- packages/deriv-bot/src/botPage/view/blockly/blocks/index.js
+++
@@ -1,8 +0,0 @@
-import './tools';
-import './ticks';
-import './before_purchase';
-import './during_purchase';
-import './after_purchase';
-import './indicators';
-
-export { default } from './trade';
--- packages/deriv-bot/src/botPage/view/react-components/ExportButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { exportButton as exportButtonStyle } from '../style';
-
-const ExportButton = ({ onClick, customStyle }) => (
-    <button onClick={onClick} style={{ ...exportButtonStyle, ...customStyle }} className="icon-save" />
-);
-
-export default ExportButton;

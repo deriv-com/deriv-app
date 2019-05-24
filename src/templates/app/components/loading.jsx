be8574479d09d44972c9bb7cf1b61e6dc8c17815
Amam Mustofa <mustofa@binary.com>
2019-05-24 16:37:28 +0800

    remove button component from loading component

--- src/templates/app/components/loading.jsx
+++ src/templates/app/components/loading.jsx
@@ -1,8 +1,7 @@
 import React      from 'react';
 import PropTypes  from 'prop-types';
-import ButtonLink from '../../../javascript/app/App/Components/Routes/button-link.jsx';
 
-const Loading = ({ btn_text, status, is_slow_loading, theme, id }) => (
+const Loading = ({ status, is_slow_loading, theme, id }) => (
     <div className='initial-loader'>
         <div id={id} className={`initial-loader__barspinner barspinner barspinner-${ theme || 'light'}`}>
             { Array.from(new Array(5)).map((x, inx) => (
@@ -12,13 +11,6 @@ const Loading = ({ btn_text, status, is_slow_loading, theme, id }) => (
         { is_slow_loading &&
             <React.Fragment>
                 <h3 className='initial-loader__text'>{status}</h3>
-                <ButtonLink
-                    className='btn--primary btn--primary--orange'
-                    to={'/trade'}
-                    onClick={(() => location.reload())}
-                >
-                    <span className='btn__text'>{btn_text}</span>
-                </ButtonLink>
             </React.Fragment>
         }
     </div>

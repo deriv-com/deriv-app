cd38ce66109694d5c11311d64d920dca13231f10
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-05-28 11:09:42 +0800

    Add mobx store injection

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,19 @@
 
 import React from 'react';
+import { Provider } from 'mobx-react';
+import RootStore from './stores';
+import Bot from './components/bot.jsx';
 
 class App extends React.Component {
+    
+    rootStore = new RootStore();
+
     render () {
-        return <div>Deriv bot</div>;
+        return (
+            <Provider {...this.rootStore}>
+                <Bot>{this.rootStore.bot.title}</Bot>
+            </Provider>
+        );
     }
 }
 

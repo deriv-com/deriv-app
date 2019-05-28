359ed77e01dcd1717a287da0d56080aee08a14d5
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-05-28 13:25:56 +0800

    Remove console.log

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
 
-console.log('hey there');
-
 ReactDOM.render(
     <App />,
     document.getElementById('root'),

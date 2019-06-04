2d0d354b401e4663b173ca757672db16daf5181d
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-06-04 12:30:31 +0800

    Add webpackplugin to create svg sprite and create icon component

--- src/components/bot.jsx
+++ src/components/bot.jsx
@@ -1,10 +1,12 @@
 import React            from 'react';
+import Header           from './header/header.jsx';
 import Workspace        from './workspace.jsx';
 import { connect }      from '../stores/connect';
 import                       '../assets/sass/_bot.scss';
 
 const Bot = () => (
     <React.Fragment>
+        <Header />
         <Workspace />
     </React.Fragment>
 );
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LogoIcon,TradeActive } from '../Icons.jsx';
+
+const Header = () => (
+    <div className='header-items'>
+        <LogoIcon />
+        <TradeActive />
+    </div>
+);
+
+export default Header;

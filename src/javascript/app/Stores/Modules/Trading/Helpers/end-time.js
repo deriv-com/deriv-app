6196001c9a4b3a70bbe1c25f0db2784a5d999acd
brandon <26451183+ahkuma@users.noreply.github.com>
2019-06-05 16:49:49 +0800

    timepicker : add return statements that got forgotten :skull:

--- src/javascript/app/Stores/Modules/Trading/Helpers/end-time.js
+++ src/javascript/app/Stores/Modules/Trading/Helpers/end-time.js
@@ -9,10 +9,10 @@ export const getSelectedTime = (
     market_open_time,
 ) => {
     if (selected_time.isAfter(market_open_time)) {
-        getClosestTime(selected_time, 5).format('HH:mm');
+        return getClosestTime(selected_time, 5).format('HH:mm');
     }
     if (market_open_time.isAfter(server_time)) {
-        getClosestTime(market_open_time, 5).format('HH:mm');
+        return getClosestTime(market_open_time, 5).format('HH:mm');
     }
 
     return getClosestTime(server_time, 5).format('HH:mm');

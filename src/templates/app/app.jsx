6305344c3d225b688120d796f8f225d298ada78f
OskarAhl <oskar@binary.com>
2019-06-12 16:06:24 +0800

    add initial loader before react mounts

--- src/templates/app/app.jsx
+++ src/templates/app/app.jsx
@@ -9,7 +9,17 @@ const BinaryApp = () => (
         <body className={classNames('body', it.language, 'theme')}>
             <GTMNoScript />
 
-            <div id='binary_app' className='binary-app' />
+            <div id='binary_app' className='binary-app'>
+                {/* Initial loader before react mounts */}
+                <div className='initial-loader'>
+                    {/* TODO add theme */}
+                    <div className={'initial-loader__barspinner barspinner barspinner-light'}>
+                        { Array.from(new Array(5)).map((x, inx) => (
+                            <div key={inx} className={`initial-loader__barspinner--rect barspinner__rect barspinner__rect--${inx + 1} rect${inx + 1}`} />
+                        ))}
+                    </div>
+                </div>
+            </div>
             <div id='modal_root' className='modal-root' />
         </body>
     </html>

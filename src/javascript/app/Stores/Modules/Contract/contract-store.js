e50a40af368dc07f4cafa40132b8b5e92567ed21
zakibinary <zaki@binary.com>
2019-06-17 15:47:43 +0800

    add cb func to forget poc request

--- src/javascript/app/Stores/Modules/Contract/contract-store.js
+++ src/javascript/app/Stores/Modules/Contract/contract-store.js
@@ -167,7 +167,7 @@ export default class ContractStore extends BaseStore {
 
     @action.bound
     onUnmountReplay() {
-        this.forgetProposalOpenContract(this.replay_contract_id);
+        this.forgetProposalOpenContract(this.replay_contract_id, this.populateConfig);
         this.replay_contract_id       = null;
         this.digits_info              = {};
         this.is_ongoing_contract      = false;
@@ -190,7 +190,7 @@ export default class ContractStore extends BaseStore {
 
     @action.bound
     onCloseContract() {
-        this.forgetProposalOpenContract(this.contract_id);
+        this.forgetProposalOpenContract(this.contract_id, this.updateProposal);
         this.chart_type          = 'mountain';
         this.contract_id         = null;
         this.contract_info       = {};
@@ -355,9 +355,12 @@ export default class ContractStore extends BaseStore {
         SmartChartStore.updateGranularity(granularity);
     }
 
-    forgetProposalOpenContract(contract_id) {
-        WS.forget('proposal_open_contract', this.updateProposal, { contract_id });
+    // TODO: Remove disabling eslint below for class-methods-use-this once contract-store is refactored
+    /* eslint-disable */
+    forgetProposalOpenContract(contract_id, cb) {
+        WS.forget('proposal_open_contract', cb, { contract_id });
     }
+    /* eslint-enable */
 
     @action.bound
     removeErrorMessage() {

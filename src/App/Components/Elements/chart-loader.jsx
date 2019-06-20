0a52ace3b557ffa31d97adfdc7c2597a6706fda6
Mahdi <m.pourziaei@gmail.com>
2019-06-20 10:52:04 +0800

    + Folder restructuring [WIP] + Added missing packages + Added smartcharts stylings [WIP] + Fixed references to folder paths to use Webpack aliases

---
+++ src/App/Components/Elements/chart-loader.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import React     from 'react';
+import Loading   from '../../../templates/_common/components/loading.jsx';
+
+const ChartLoader = ({ is_visible }) => (
+    is_visible &&
+    <div className='chart-container__loader'>
+        <Loading theme='chart-loader' />
+    </div>
+);
+
+ChartLoader.propTypes = {
+    is_visible: PropTypes.bool,
+};
+
+export default ChartLoader;

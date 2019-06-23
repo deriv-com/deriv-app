97465a214f0740cb6de431d628ffb23401e52427
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-06-23 13:34:34 +0430

    Add Fast-forward button to two-month date picker

--- src/javascript/app/App/Components/Form/CompositeCalendar/two-month-picker.jsx
+++ src/javascript/app/App/Components/Form/CompositeCalendar/two-month-picker.jsx
@@ -1,13 +1,18 @@
-import PropTypes            from 'prop-types';
-import moment               from 'moment';
-import React, { Component } from 'react';
-import CalendarBody         from 'App/Components/Elements/Calendar/calendar-body.jsx';
-import CalendarHeader       from 'App/Components/Elements/Calendar/calendar-header.jsx';
+import PropTypes                from 'prop-types';
+import moment                   from 'moment';
+import React                    from 'react';
+import CalendarBody             from 'App/Components/Elements/Calendar/calendar-body.jsx';
+import CalendarHeader           from 'App/Components/Elements/Calendar/calendar-header.jsx';
+import CalendarFooter           from 'App/Components/Elements/Calendar/calendar-footer.jsx';
 import {
-    addMonths, diffInMonths, epochToMoment, subMonths, toMoment,
-} from 'Utils/Date';
+    addMonths,
+    diffInMonths,
+    epochToMoment,
+    subMonths,
+    toMoment,
+}                               from 'Utils/Date';
 
-class TwoMonthPicker extends Component {
+class TwoMonthPicker extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -54,6 +59,14 @@ class TwoMonthPicker extends Component {
         const r_date = epochToMoment(this.state.right_pane_date).startOf('month');
         if (diffInMonths(toMoment().startOf('month'), r_date) === 0) return true; // future months are disallowed
         return diffInMonths(r_date, date) !== 1;
+    }
+
+    jumpToCurrentMonth () {
+        const current_month = toMoment().endOf('month').unix();
+        this.setState({
+            left_pane_date : epochToMoment(current_month).endOf('month').subtract(1, 'month').unix(),
+            right_pane_date: current_month,
+        });
     }
 
     navigateTo(e) {
@@ -112,6 +125,7 @@ class TwoMonthPicker extends Component {
                         hide_others={true}
                         updateSelected={this.updateSelectedDate.bind(this)}
                     />
+                    <CalendarFooter use_icon='IconCalendarForwardToday' has_today_btn={true} onClick={this.jumpToCurrentMonth.bind(this)} />
                 </div>
             </React.Fragment>
         );

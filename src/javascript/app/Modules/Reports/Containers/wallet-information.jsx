a1492cdac7bc33277bb00b3e4d57a39f3f0ec89b
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-06-24 14:01:37 +0430

    Put a fallback loader for currency config

--- src/javascript/app/Modules/Reports/Containers/wallet-information.jsx
+++ src/javascript/app/Modules/Reports/Containers/wallet-information.jsx
@@ -1,28 +1,35 @@
-import React        from 'react';
-import Icon         from 'Assets/icon.jsx';
-import Label        from 'App/Components/Elements/Label/label.jsx';
-import Localize     from 'App/Components/Elements/localize.jsx';
-import Money        from 'App/Components/Elements/money.jsx';
-import { connect }  from 'Stores/connect';
+import React             from 'react';
+import Icon              from 'Assets/icon.jsx';
+import { getCurrencies } from '_common/base/currency_base';
+import Label             from 'App/Components/Elements/Label/label.jsx';
+import Localize          from 'App/Components/Elements/localize.jsx';
+import Money             from 'App/Components/Elements/money.jsx';
+import { connect }       from 'Stores/connect';
 
 const WalletInformation = ({
     currency,
     balance,
     is_virtual,
     loginid,
-}) => (
-    <div className='account-wallet'>
-        {!is_virtual && <Icon icon='IconAccountsCurrency' type={currency.toLowerCase()} />}
-        {is_virtual && <Icon icon='IconDemo' />}
-        <span className='description'>
-            <Localize str={`${is_virtual ? 'Practice' : currency.toUpperCase()} wallet`} />
-        </span>
-        <span className='current-loginid'>{loginid}</span>
-        <Label mode={`${is_virtual ? 'warn-invert' : 'success-invert'}`} size='large'>
-            <Money amount={balance} currency={currency} />
-        </Label>
-    </div>
-);
+}) => {
+    const is_currency_ready = Object.keys(getCurrencies()).length > 0;
+    return (
+        <div className='account-wallet'>
+            {!is_virtual && <Icon icon='IconAccountsCurrency' type={currency.toLowerCase()} />}
+            {is_virtual && <Icon icon='IconDemo' />}
+            <span className='description'>
+                <Localize str={`${is_virtual ? 'Practice' : currency.toUpperCase()} wallet`} />
+            </span>
+            <span className='current-loginid'>{loginid}</span>
+            {is_currency_ready &&
+            <Label mode={`${is_virtual ? 'warn-invert' : 'success-invert'}`} size='large'>
+                <Money amount={balance} currency={currency} />
+            </Label>
+            }
+            {!is_currency_ready && <div />}
+        </div>
+    );
+};
 
 export default connect(({ client }) => ({
     balance   : client.balance,

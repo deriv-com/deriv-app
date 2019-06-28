77cd1fe9a6f6d863d425293294a344955b32e693
brandon <26451183+ahkuma@users.noreply.github.com>
2019-06-28 12:33:25 +0800

    fix for regex not detecting ticks

--- src/javascript/app/Modules/Reports/Helpers/market-underlying.js
+++ src/javascript/app/Modules/Reports/Helpers/market-underlying.js
@@ -5,12 +5,13 @@ export const getMarketInformation = (payload) => {
         atm       : '',
     };
 
-    const pattern = new RegExp('^([A-Z]+)_((OTC_[A-Z0-9]+)|R_[\\d]{2,3}|[A-Z]+)_([\\d.\\d]+)_(\\d+)_(\\d+)_(S[\\d.\\d]+P|(?:\\d+))_(\\d+)'); // Used to get market name from shortcode
+    const pattern = new RegExp('^([A-Z]+)_((OTC_[A-Z0-9]+)|R_[\\d]{2,3}|[A-Z]+)_([\\d.\\d]+)_(\\d+\\w)_(\\d+\\w)_(S[\\d.\\d]+P|(?:\\d+))_(\\d+)'); // Used to get market name from shortcode
     const extracted = pattern.exec(payload.shortcode);
     if (extracted !== null) {
         market_info.category   = extracted[1].toLowerCase();
         market_info.underlying = extracted[2];
         market_info.atm        = extracted[7];
     }
+
     return market_info;
 };

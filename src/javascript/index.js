242c33e1392ed34a7d172a90926ea67adf8f03b9
Mahdi <m.pourziaei@gmail.com>
2019-07-01 18:05:28 +0800

    Moved items from `javascript` folder after merge

--- src/javascript/index.js
+++
@@ -1,7 +0,0 @@
-require('babel-polyfill');
-require('promise-polyfill');
-require('./_common/lib/polyfills/nodelist.foreach');
-require('./_common/lib/polyfills/element.closest');
-
-require('event-source-polyfill');
-require('./_common/lib/plugins');

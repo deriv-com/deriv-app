6cc6651fb66019c297ab57257d7281f3c4fdad2b
brandon <26451183+ahkuma@users.noreply.github.com>
2019-07-02 13:10:51 +0800

    fix toUpperCase of undefined

--- src/javascript/app/Modules/Reports/Helpers/shortcode.js
+++ src/javascript/app/Modules/Reports/Helpers/shortcode.js
@@ -1,11 +1,17 @@
 const Shortcode = (() => {
-    let info_from_shortcode = {};
+    let info_from_shortcode = {
+        category  : '',
+        underlying: '',
+        atm       : '',
+    };
 
     const extractInfoFromShortcode = (shortcode) => {
         const pattern = new RegExp('^([A-Z]+)_((OTC_[A-Z0-9]+)|R_[\\d]{2,3}|[A-Z]+)_([\\d.\\d]+)_(\\d+\\w)_(\\d+\\w)_(S[\\d.\\d]+P|(?:\\d+))_(\\d+)'); // Used to get market name from shortcode
         const extracted = pattern.exec(shortcode);
         if (extracted !== null) {
+            info_from_shortcode.shortcode  = shortcode;
             info_from_shortcode.category   = extracted[1].toLowerCase();
+            info_from_shortcode.buy_price  = extracted[4];
             info_from_shortcode.underlying = extracted[2];
             info_from_shortcode.atm        = extracted[7];
         }

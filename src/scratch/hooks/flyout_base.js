174d4d2dc008bf55492f65ea9917eb64f9c64638
Aaron Imming <aaron@binary.com>
2019-07-04 21:03:43 +0800

    Set correct x-coordinate so scrollbar renders correctly

--- src/scratch/hooks/flyout_base.js
+++ src/scratch/hooks/flyout_base.js
@@ -1,10 +1,26 @@
 /* eslint-disable func-names, no-underscore-dangle */
+
+/**
+ * Corner radius of the flyout background.
+ * @type {number}
+ * @const
+ */
+Blockly.Flyout.prototype.CORNER_RADIUS = 10;
+
 /**
  * Margin around the edges of the blocks in the flyout.
  * @type {number}
  * @const
  */
-Blockly.Flyout.prototype.MARGIN = 24;
+Blockly.Flyout.prototype.MARGIN = 30;
+
+/**
+ * Top/bottom padding between scrollbar and edge of flyout background.
+ * deriv-bot: Should be equal to CORNER_RADIUS
+ * @type {number}
+ * @const
+ */
+Blockly.Flyout.prototype.SCROLLBAR_PADDING = 20;
 
 /**
  * Update the view based on coordinates calculated in position().
@@ -13,26 +29,31 @@ Blockly.Flyout.prototype.MARGIN = 24;
  * @param {number} x The computed x origin of the flyout's SVG group.
  * @param {number} y The computed y origin of the flyout's SVG group.
  * @protected
- * deriv-bot: Imported from Blockly, used in flyout_vertical.js
  */
 Blockly.Flyout.prototype.positionAt_ = function(width, height, x, y) {
     this.svgGroup_.setAttribute('width', width);
     this.svgGroup_.setAttribute('height', height);
+
     if (this.svgGroup_.tagName === 'svg') {
         const transform = `translate(${x}px,${y}px)`;
+
         Blockly.utils.setCssTransform(this.svgGroup_, transform);
     } else {
         // IE and Edge don't support CSS transforms on SVG elements so
         // it's important to set the transform on the SVG element itself
         const transform = `translate(${x},${y})`;
+
         this.svgGroup_.setAttribute('transform', transform);
     }
 
     // Update the scrollbar (if one exists).
     if (this.scrollbar_) {
+        const newX = x - Blockly.VerticalFlyout.ARROW_SIZE;
+
         // Set the scrollbars origin to be the top left of the flyout.
-        this.scrollbar_.setOrigin(x, y);
+        this.scrollbar_.setOrigin(newX, y);
         this.scrollbar_.resize();
+
         // Set the position again so that if the metrics were the same (and the
         // resize failed) our position is still updated.
         this.scrollbar_.setPosition_(this.scrollbar_.position_.x, this.scrollbar_.position_.y);

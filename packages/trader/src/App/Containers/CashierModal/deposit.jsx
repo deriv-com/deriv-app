4fc747481ba0acf651354bfb93a42a1ab109445c
negar <negar@binary.com>
2019-07-10 16:32:31 +0800

    fix deposit load

--- packages/trader/src/App/Containers/CashierModal/deposit.jsx
+++ packages/trader/src/App/Containers/CashierModal/deposit.jsx
@@ -1,38 +1,48 @@
 import PropTypes   from 'prop-types';
 import React       from 'react';
 import { connect } from 'Stores/connect';
+import Loading     from '../../../templates/_common/components/loading.jsx';
 
 class Deposit extends React.Component {
     componentDidMount() {
         this.props.onMount();
     }
 
-    componentWillUnmount() {
-        this.props.onUnmount();
-    }
-
     render() {
         return (
-            <iframe
-                className='cashier__iframe'
-                src={this.props.deposit_url}
-                frameBorder='0'
-                scrolling='auto'
-            />
+            <React.Fragment>
+                {this.props.is_loading && <Loading />}
+                {this.props.deposit_url &&
+                <iframe
+                    className='deposit__content'
+                    src={this.props.deposit_url}
+                    frameBorder='0'
+                    scrolling='auto'
+                />
+                }
+                {this.props.error_message && <p>{this.props.error_message}</p>}
+                {/* TODO: uncomment this if cross origin issue is fixed */}
+                {/* <div */}
+                {/*     className='deposit__content' */}
+                {/*     dangerouslySetInnerHTML={{ __html: this.props.deposit_url }} */}
+                {/* /> */}
+            </React.Fragment>
         );
     }
 }
 
 Deposit.propTypes = {
     deposit_url  : PropTypes.string,
-    iframe_height: PropTypes.number,
+    error_message: PropTypes.string,
+    is_loading   : PropTypes.bool,
+    onMount      : PropTypes.func,
 };
 
 export default connect(
     ({ modules }) => ({
         deposit_url  : modules.cashier.deposit_url,
-        iframe_height: modules.cashier.iframe_height,
+        error_message: modules.cashier.error_message,
+        is_loading   : modules.cashier.is_loading,
         onMount      : modules.cashier.onMount,
-        onUnmount    : modules.cashier.onUnmount,
     })
 )(Deposit);

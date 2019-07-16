1608107bbd98c01a388502708b2c11bee55a4e0e
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-07-16 17:25:18 +0430

    Refactor Modals to be more performant

--- src/App/Containers/ServicesErrorModal/services-error-modal.jsx
+++ src/App/Containers/ServicesErrorModal/services-error-modal.jsx
@@ -2,14 +2,12 @@ import React         from 'react';
 import PropTypes     from 'prop-types';
 import { localize }  from 'App/i18n';
 import FullPageModal from 'App/Components/Elements/FullPageModal/full-page-modal.jsx';
-import { connect }   from 'Stores/connect';
 import { title }     from './constants';
 
 const ServicesErrorModal = ({
-    is_services_error_visible,
-    resetPurchase,
+    is_visible,
+    onConfirm,
     services_error,
-    toggleServicesErrorModal,
 }) => {
     const { code, message } = services_error;
 
@@ -19,16 +17,11 @@ const ServicesErrorModal = ({
         <FullPageModal
             title={title[services_error.type]}
             confirm_button_text={localize('OK')}
-            onConfirm={() => {
-                toggleServicesErrorModal(false);
-                if (services_error.type === 'buy') {
-                    resetPurchase();
-                }
-            }}
+            onConfirm={onConfirm}
             // TODO: handle onCancel
             // cancel_button_text={cancel_button_text}
             // onCancel={onCancel}
-            is_visible={is_services_error_visible}
+            is_visible={is_visible}
         >
             {message}
         </FullPageModal>
@@ -36,17 +29,9 @@ const ServicesErrorModal = ({
 };
 
 ServicesErrorModal.propTypes = {
-    is_services_error_visible: PropTypes.bool,
-    resetPurchase            : PropTypes.func,
-    services_error           : PropTypes.object,
-    toggleServicesErrorModal : PropTypes.func,
+    is_visible    : PropTypes.bool,
+    onConfirm     : PropTypes.func,
+    services_error: PropTypes.object,
 };
 
-export default connect (
-    ({ common, modules, ui }) => ({
-        services_error           : common.services_error,
-        is_services_error_visible: ui.is_services_error_visible,
-        toggleServicesErrorModal : ui.toggleServicesErrorModal,
-        resetPurchase            : modules.trade.requestProposal,
-    }),
-)(ServicesErrorModal);
+export default ServicesErrorModal;

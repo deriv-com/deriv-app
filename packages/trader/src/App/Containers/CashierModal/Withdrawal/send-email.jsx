fa944e4403c346e391bd4a7fa222e42d0e0b8142
negar <negar@binary.com>
2019-07-19 13:34:25 +0800

    scroll the container not the iframe

--- packages/trader/src/App/Containers/CashierModal/Withdrawal/send-email.jsx
+++ packages/trader/src/App/Containers/CashierModal/Withdrawal/send-email.jsx
@@ -1,6 +1,5 @@
 import PropTypes    from 'prop-types';
 import React        from 'react';
-import classNames   from 'classnames';
 import Localize     from 'App/Components/Elements/localize.jsx';
 import Button       from 'App/Components/Form/button.jsx';
 import { localize } from 'App/i18n';
@@ -10,33 +9,38 @@ import { connect }  from 'Stores/connect';
 class SendEmail extends React.Component {
     render() {
         return (
-            <div className='withdraw__verify-wrapper'>
-                <Icon icon='IconAuthenticateWithdrawals' className='withdraw__icon-authenticate' />
-                <p className='withdraw__text'>
-                    <Localize i18n_default_text='To protect your account, we need to authenticate withdrawals.' />
-                </p>
-                <Button
-                    className={classNames('btn--primary btn--primary--orange withdraw__verify-button', { 'btn--disabled': this.props.is_button_clicked })}
-                    is_disabled={this.props.is_button_clicked}
-                    classNameSpan='withdraw__verify-button-text'
-                    has_effect
-                    text={localize('Get authentication email')}
-                    onClick={() => { this.props.sendVerificationEmail(this.props.client_email); }}
-                />
+            <React.Fragment>
+                <div className='withdraw__verify-container'>
+                    <div className='withdraw__verify-wrapper'>
+                        <Icon icon='IconAuthenticateWithdrawals' className='withdraw__icon-authenticate' />
+                        <p className='withdraw__text'>
+                            <Localize i18n_default_text='To protect your account, we need to authenticate withdrawals.' />
+                        </p>
+                        <Button
+                            className='btn--primary btn--primary--orange withdraw__verify-button'
+                            classNameSpan='withdraw__verify-button-text'
+                            has_effect
+                            text={localize('Get authentication email')}
+                            onClick={() => { this.props.sendVerificationEmail(this.props.client_email); }}
+                        />
+                    </div>
+                </div>
                 {this.props.is_email_sent &&
-                <div className='withdraw__email-sent'>
-                    <h1><Localize i18n_default_text="We've sent you an email." /></h1>
-                    <p><Localize i18n_default_text='Please click on the authentication link in the email to continue.' /></p>
+                <div className='withdraw__verify-container'>
+                    <div className='withdraw__email-sent'>
+                        <Icon icon='IconEmailSent' className='withdraw__icon-authenticate' />
+                        <h1><Localize i18n_default_text="We've sent you an email." /></h1>
+                        <p><Localize i18n_default_text='Please click on the authentication link in the email to continue.' /></p>
+                    </div>
                 </div>
                 }
-            </div>
+            </React.Fragment>
         );
     }
 }
 
 SendEmail.propTypes = {
     client_email         : PropTypes.string,
-    is_button_clicked    : PropTypes.bool,
     is_email_sent        : PropTypes.bool,
     sendVerificationEmail: PropTypes.func,
 };
@@ -44,7 +48,6 @@ SendEmail.propTypes = {
 export default connect(
     ({ client, modules }) => ({
         client_email         : client.email,
-        is_button_clicked    : modules.cashier.is_verification_button_clicked,
         is_email_sent        : modules.cashier.is_verification_email_sent,
         sendVerificationEmail: modules.cashier.sendVerificationEmail,
     })

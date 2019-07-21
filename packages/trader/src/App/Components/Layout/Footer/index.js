c58caf5ef6934fe2bd0341bd7b4013b366abbbe0
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-07-21 17:40:30 +0430

    Undo removing toggle-settings from 'footer'

--- packages/trader/src/App/Components/Layout/Footer/index.js
+++ packages/trader/src/App/Components/Layout/Footer/index.js
@@ -2,3 +2,4 @@
 export NetworkStatus from './network-status.jsx';
 export *             from './toggle-fullscreen.jsx';
 export *             from './toggle-positions.jsx';
+export *             from './toggle-settings.jsx';

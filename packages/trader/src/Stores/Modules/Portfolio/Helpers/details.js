99ec2680b7c7b09fb6641eb5a0dd731a32777cec
zakibinary <zaki@binary.com>
2019-07-22 10:46:37 +0800

    Handle end_time display via calculation and formatting with momentjs.

--- packages/trader/src/Stores/Modules/Portfolio/Helpers/details.js
+++ packages/trader/src/Stores/Modules/Portfolio/Helpers/details.js
@@ -2,6 +2,7 @@ import { localize }        from 'App/i18n';
 import { unique }          from '_common/utility';
 import {
     epochToMoment,
+    formatMiliseconds,
     getDiffDuration }      from 'Utils/Date';
 import { isDigitContract } from '../../Contract/Helpers/digits';
 
@@ -13,18 +14,34 @@ export const getCurrentTick = (contract_info) => {
 
 export const getDurationUnitValue = (obj_duration) => {
     const duration_ms = obj_duration.asMilliseconds() / 1000;
+    // Check with isEndTime to find out if value of duration has decimals
+    // for days we do not require precision for End Time value since users cannot select with timepicker if not in same day
+    // Seconds is the smallest End Time duration displayed thus we can keep the same formatting as normal Duration
 
     if (duration_ms >= 86400000) {
-        return Math.floor(duration_ms / (1000 * 60 * 60 * 24));
+        const duration = duration_ms / (1000 * 60 * 60 * 24);
+        return Math.floor(duration);
     } else if (duration_ms >= 3600000 && duration_ms < 86400000) {
-        return Math.floor(duration_ms / (1000 * 60 * 60));
+        const duration = duration_ms / (1000 * 60 * 60);
+        const is_end_time = isEndTime(duration);
+        return is_end_time ?
+            formatMiliseconds(duration_ms, 'HH[h] mm[m] ss[s]')
+            :
+            Math.floor(duration_ms / (1000 * 60 * 60));
     } else if (duration_ms >= 60000 && duration_ms < 3600000) {
-        return Math.floor(duration_ms / (1000 * 60));
+        const duration = duration_ms / (1000 * 60);
+        const is_end_time = isEndTime(duration);
+        return is_end_time ?
+            formatMiliseconds(duration_ms, 'mm[m] ss[s]')
+            :
+            Math.floor(duration_ms / (1000 * 60));
     } else if (duration_ms >= 1000 && duration_ms < 60000) {
-        return Math.floor(duration_ms / (1000));
+        return Math.floor(duration_ms / 1000);
     }
-    return Math.floor(duration_ms / (1000));
+    return Math.floor(duration_ms / 1000);
 };
+
+export const isEndTime = (duration) => (duration % 1) !== 0;
 
 export const getDurationUnitText = (obj_duration) => {
     const unit_map = {
@@ -34,13 +51,17 @@ export const getDurationUnitText = (obj_duration) => {
         s: { name: localize('seconds') },
     };
     const duration_ms = obj_duration.asMilliseconds() / 1000;
+    // return empty suffix string if duration is End Time set except for days and seconds, refer to L18 and L19
+
     if (duration_ms) {
         if (duration_ms >= 86400000) {
             const days_value = duration_ms / 86400000;
             return (days_value <= 2) ? unit_map.d.name_singular : unit_map.d.name_plural;
         } else if (duration_ms >= 3600000 && duration_ms < 86400000) {
+            if (isEndTime(duration_ms / (1000 * 60 * 60))) return '';
             return (duration_ms === 3600000) ? unit_map.h.name_singular : unit_map.h.name_plural;
         } else if (duration_ms >= 60000 && duration_ms < 3600000) {
+            if (isEndTime(duration_ms / (1000 * 60))) return '';
             return (duration_ms === 60000) ? unit_map.m.name_singular : unit_map.m.name_plural;
         } else if (duration_ms >= 1000 && duration_ms < 60000) {
             return unit_map.s.name;

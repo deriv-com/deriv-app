ce580201af08375aada6abcba8752847fbb2f0ca
Kelly Anyi <kelly@binary.com>
2019-07-23 02:49:19 +0800

    update tick slider animation

--- packages/trader/src/App/Components/Form/RangeSlider/range-slider.jsx
+++ packages/trader/src/App/Components/Form/RangeSlider/range-slider.jsx
@@ -6,82 +6,128 @@ import { localize } from 'App/i18n';
 import { connect }  from 'Stores/connect';
 import TickSteps    from './tick-steps.jsx';
 
-const RangeSlider = ({
-    className,
-    max_value,
-    min_value,
-    name,
-    value,
-    onChange,
-}) => {
-    const handleChange = (e) => {
-        if (e.target.value !== value) {
-            onChange({ target: { name, value: e.target.value } });
-        }
-    };
-
-    const handleClick = (e, index) => {
-        if (index !== value) {
-            onChange({ target: { name, value: index } });
-        }
-    };
-
-    if (+value < min_value || +value > max_value) {
-        onChange({
-            target: {
-                name,
-                value: min_value,
-            },
-        });
+class RangeSlider extends React.Component {
+    state = {
+        hover_value: 0,
     }
 
-    return (
-        <div className={classNames('range-slider', className, { 'range-slider__error': ((value < +min_value) || (value > +max_value)) })}>
-            <label className='range-slider__label' htmlFor='range'>
-                <input
-                    id='range'
-                    className='input trade-container__input range-slider__track'
-                    type='range'
-                    min={min_value}
-                    max={max_value}
-                    min_value={min_value}
-                    max_value={max_value}
-                    name={name}
-                    steps={max_value - min_value}
-                    onChange={handleChange}
-                    tabIndex='0'
-                    value={value}
-                />
-                <div className='range-slider__ticks'>
-                    <TickSteps
+    handleChange = (e) => {
+        if (e.target.value !== this.props.value) {
+            this.resetOnHover();
+            this.props.onChange({ target: { name: this.props.name, value: e.target.value } });
+        }
+    };
+
+    handleClick = (e, index) => {
+        if (index !== this.props.value) {
+            this.resetOnHover();
+            this.props.onChange({ target: { name: this.props.name, value: index } });
+        }
+    };
+
+    onMouseEnter = (e, index) => {
+        if (index) {
+            this.setState({ hover_value: index });
+            this.rangeSliderTrack.style.width = this.getRangeSliderTrackWidth(index, true);
+        }
+    };
+
+    onMouseLeave = (e) => {
+        const { offsetX, offsetY } = e.nativeEvent;
+        if (offsetY <= -3 || offsetY >= 3 || offsetX < -3 || offsetX > 3) {
+            this.resetOnHover();
+        }
+    };
+
+    getRangeSliderTrackWidth = (value, is_hover) => {
+        const width = (value - this.props.min_value) * (10 / (this.props.max_value - this.props.min_value));
+        return `${(width * 2) + (is_hover ? 0.8 : 1.4)}em`;
+    };
+
+    resetOnHover = () => {
+        if (this.state.hover_value) {
+            this.setState({ hover_value: 0 });
+            this.rangeSliderTrack.style.width = 0;
+        }
+    };
+
+    render() {
+        const {
+            className,
+            max_value,
+            min_value,
+            name,
+            value,
+            onChange,
+        } = this.props;
+
+        if (+value < min_value || +value > max_value) {
+            onChange({
+                target: {
+                    name,
+                    value: min_value,
+                },
+            });
+        }
+
+        const display_value = this.state.hover_value || value;
+        return (
+            <div className={classNames('range-slider', className, { 'range-slider__error': ((value < +min_value) || (value > +max_value)) })}>
+                <label className='range-slider__label' htmlFor='range'>
+                    <input
+                        id='range'
+                        className='input trade-container__input range-slider__track'
+                        type='range'
+                        min={min_value}
+                        max={max_value}
+                        min_value={min_value}
+                        max_value={max_value}
+                        name={name}
+                        steps={max_value - min_value}
+                        onChange={this.handleChange}
+                        tabIndex='0'
                         value={value}
-                        onClick={handleClick}
                     />
-                </div>
-                {/* Calculate line width based on active value and size of range thumb */}
-                <div
-                    className='range-slider__line'
-                    style={{ width: `calc(${(value - min_value) * (100 / (max_value - min_value))}% ` }}
-                />
-            </label>
-            <div className='range-slider__caption'>
-                <span className='range-slider__caption--first'>
-                    {min_value}
-                </span>
-                {
-                    !!value &&
-                    <span className='range-slider__caption-title'>
-                        {+value === 1 && localize('{{value}} Tick', { value })}
-                        {+value > 1 && localize('{{value}} Ticks', { value })}
+                    {/* this element to be placed before <TickSteps /> to prevent overlapping colors */}
+                    <span
+                        ref={node => this.rangeSliderTrack = node}
+                        className='range-slider__line range-slider__line--hover'
+                        onMouseLeave={this.onMouseLeave}
+                    />
+                    <div className='range-slider__ticks'>
+                        <TickSteps
+                            value={value}
+                            onClick={this.handleClick}
+                            onMouseLeave={this.onMouseLeave}
+                            onMouseEnter={this.onMouseEnter}
+                            hover_value={this.state.hover_value}
+                        />
+                    </div>
+                    {/* Calculate line width based on active value and size of range thumb */}
+                    <div
+                        className='range-slider__line'
+                        style={{ width: `${this.getRangeSliderTrackWidth(value)}` }}
+                    />
+                </label>
+                <div className='range-slider__caption'>
+                    <span className='range-slider__caption--first'>
+                        {min_value}
                     </span>
-                }
-                <span className='range-slider__caption--last'>
-                    {max_value}
-                </span>
+                    {
+                        !!display_value &&
+                        <span className='range-slider__caption-title'>
+                            {+display_value === 1 && localize('{{display_value}} Tick', { display_value })}
+                            {+display_value > 1 && localize('{{display_value}} Ticks', { display_value })}
+                        </span>
+                    }
+                    <span className='range-slider__caption--last'>
+                        {max_value}
+                    </span>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 // Keypress events do not trigger on Safari due to the way it handles input type='range' elements, using focus on the input element also doesn't work for Safari.
 
 RangeSlider.propTypes = {
--- packages/trader/src/App/Components/Form/RangeSlider/tick-steps.jsx
+++ packages/trader/src/App/Components/Form/RangeSlider/tick-steps.jsx
@@ -3,18 +3,26 @@ import PropTypes    from 'prop-types';
 import React        from 'react';
 import { connect }  from 'Stores/connect';
 
-const TickSteps = ({ max_value, min_value, value, onClick }) => {
+const TickSteps = ({ max_value, min_value, hover_value, value, onClick, onMouseEnter, onMouseLeave }) => {
     const arr_ticks = [...Array((max_value - min_value) + 1).keys()];
+
+    const isActive = (idx) => (idx + parseInt(min_value)) === parseInt(value);
+    const isMarked = (idx) => (idx + parseInt(min_value)) < parseInt(value);
+    const isMarkedOnHover = (idx) => (isMarked(idx)) ? false : (idx + parseInt(min_value)) <= parseInt(hover_value);
+
     return (
         <React.Fragment>
             {arr_ticks.map(idx =>
                 <span
                     key={idx}
                     className={classNames('range-slider__ticks-step', {
-                        'range-slider__ticks-step--active': (idx + parseInt(min_value)) === parseInt(value),
-                        'range-slider__ticks-step--marked': (idx + parseInt(min_value)) < parseInt(value),
+                        'range-slider__ticks-step--active'      : isActive(idx),
+                        'range-slider__ticks-step--marked'      : isMarked(idx),
+                        'range-slider__ticks-step--marked-hover': isMarkedOnHover(idx),
                     })}
                     onClick={(e) => onClick(e, idx + parseInt(min_value))}
+                    onMouseEnter={(e) => onMouseEnter(e, idx + parseInt(min_value))}
+                    onMouseLeave={onMouseLeave}
                 />
             )}
         </React.Fragment>
@@ -22,6 +30,10 @@ const TickSteps = ({ max_value, min_value, value, onClick }) => {
 };
 
 TickSteps.propTypes = {
+    hover_value: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]),
     max_value: PropTypes.string,
     min_value: PropTypes.string,
     onClick  : PropTypes.func,

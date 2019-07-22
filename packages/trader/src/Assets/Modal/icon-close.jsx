5db63071aaffd9bee9c90e6eec1813cfb5ac63eb
zakibinary <zaki@binary.com>
2019-07-22 18:12:53 +0800

    update icons with zeplin icons

--- packages/trader/src/Assets/Modal/icon-close.jsx
+++ packages/trader/src/Assets/Modal/icon-close.jsx
@@ -12,11 +12,10 @@ const IconClose = ({ className, onClick }) => (
     >
         <path
             className='color1-fill'
-            fillOpacity='0.8'
+            fillOpacity='.8'
             d='M8 6.587l4.293-4.294a1 1 0 0 1 1.414 1.414L9.414 8.002l4.293 4.294a1 1 0 0 1-1.414 1.414L8 9.416 3.707 13.71a1 1 0 1 1-1.414-1.414l4.293-4.294-4.293-4.295a1 1 0 1 1 1.414-1.414L8 6.587z'
         />
     </svg>
-
 );
 
 IconClose.propTypes = {

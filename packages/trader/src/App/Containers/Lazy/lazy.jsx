141c811a3d86aa2ea40c15828a2ca29d79981746
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-07-23 15:24:03 +0430

    Refactor Lazy Loading Component

--- packages/trader/src/App/Containers/Lazy/lazy.jsx
+++ packages/trader/src/App/Containers/Lazy/lazy.jsx
@@ -2,17 +2,24 @@ import PropTypes from 'prop-types';
 import React     from 'react';
 import UILoader  from 'App/Components/Elements/ui-loader.jsx';
 
-const Lazy = (props) => {
-    if (!props.should_load) {
+const Lazy = ({ should_load = true, ctor, is, has_progress, ...component_props }) => {
+    if (!should_load) {
         return null;
     }
-    const LazyLoadedComponent       = React.lazy(props.ctor);
-    LazyLoadedComponent.displayName = props.is || 'LazyLoadedComponent';
+    const LazyLoadedComponent       = React.lazy(ctor);
+    LazyLoadedComponent.displayName = is;
     return (
-        <React.Suspense fallback={props.has_progress ? <UILoader /> : <div />}>
-            <LazyLoadedComponent {...props} />
+        <React.Suspense fallback={has_progress ? <UILoader /> : <div />}>
+            <LazyLoadedComponent {...component_props} />
         </React.Suspense>
     );
+};
+
+// Lazy Load the component by default.
+Lazy.defaultProps = {
+    should_load : true,
+    has_progress: false,
+    is          : 'UnknownLazyLoadedComponent',
 };
 
 Lazy.propTypes = {

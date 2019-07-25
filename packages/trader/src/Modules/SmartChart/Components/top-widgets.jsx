525a4ff646e8beea487b3d52d313e75a18676ef2
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-07-25 15:12:35 +0430

    Add ShouldComponentUpdate for TopWidgets to avoid unnecessary rerenders

--- packages/trader/src/Modules/SmartChart/Components/top-widgets.jsx
+++ packages/trader/src/Modules/SmartChart/Components/top-widgets.jsx
@@ -1,20 +1,35 @@
 import {
     AssetInformation,
-    ChartTitle }      from 'smartcharts-beta';
-import PropTypes      from 'prop-types';
-import React          from 'react';
+    ChartTitle }            from 'smartcharts-beta';
+import PropTypes            from 'prop-types';
+import React, { Component } from 'react';
 
-const TopWidgets = ({
-    InfoBox,
-    is_title_enabled = true,
-    onSymbolChange,
-}) => (
-    <React.Fragment>
-        {InfoBox}
-        <ChartTitle enabled={is_title_enabled} onChange={onSymbolChange} searchInputClassName='data-hj-whitelist' />
-        <AssetInformation />
-    </React.Fragment>
-);
+class TopWidgets extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.is_title_enabled !== this.props.is_title_enabled;
+    }
+
+    render() {
+        const {
+            InfoBox,
+            is_title_enabled,
+            onSymbolChange,
+        } = this.props;
+        return (
+            <React.Fragment>
+                {InfoBox}
+                <ChartTitle
+                    enabled={is_title_enabled}
+                    onChange={onSymbolChange}
+                    searchInputClassName='data-hj-whitelist'
+                />
+                <AssetInformation />
+            </React.Fragment>
+        );
+    }
+}
+
+TopWidgets.defaultProps = { is_title_enabled: true };
 
 TopWidgets.propTypes = {
     InfoBox         : PropTypes.node,

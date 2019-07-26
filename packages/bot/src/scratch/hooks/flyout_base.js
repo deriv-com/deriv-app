dfd721c39bdf3eb09cddb3c150e4a1398cf49a3f
McSam <samyong@Sams-Mac.local>
2019-07-26 10:09:14 +0800

    drag-upward-bug

--- packages/bot/src/scratch/hooks/flyout_base.js
+++ packages/bot/src/scratch/hooks/flyout_base.js
@@ -84,7 +84,7 @@ Blockly.Flyout.prototype.placeNewBlock_ = function(event, oldBlock) {
   
     // The position of the old block in pixels relative to the flyout
     // workspace's origin.
-    const oldBlockPosPixels = new goog.math.Coordinate(event.clientX, event.clientY);
+    const oldBlockPosPixels = new goog.math.Coordinate(event.clientX - 50 , event.clientY - 20);
   
     // The position of the old block in pixels relative to the upper left corner
     // of the injection div.

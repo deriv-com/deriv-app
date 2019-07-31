9f1b7816551e4726451af791a2f1ba4253583c06
Mahdi <m.pourziaei@gmail.com>
2019-07-31 10:12:28 +0800

    Fixed test-breakers

--- packages/trader/src/config.js
+++ packages/trader/src/config.js
@@ -34,11 +34,11 @@ const getAppId = () => {
         window.localStorage.removeItem('config.default_app_id');
         window.localStorage.setItem('config.is_desktop_app', 1);
     } else if (user_app_id.length) {
-        window.localStorage.setItem('config.default_app_id', user_app_id)
+        window.localStorage.setItem('config.default_app_id', user_app_id);
         app_id = binary_desktop_app_id;
     } else if (/staging\.deriv\.app/i.test(window.location.hostname)) {
         window.localStorage.removeItem('config.default_app_id');
-        app_id = 16303;; // it's being used in endpoint chrome extension - please do not remove
+        app_id = 16303; // it's being used in endpoint chrome extension - please do not remove
         app_id = user_app_id;
     } else if (/localhost/i.test(window.location.hostname)) {
         app_id = 17044;

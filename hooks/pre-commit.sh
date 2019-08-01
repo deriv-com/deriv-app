9fb30f430d47cfb675e58674526012889aed613e
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-08-01 18:09:11 +0800

    saving work

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-# To do
-# 1. run based on project
+RUN_STYLELINT=false
+RUN_ESLINT=false
 
-for file in `git diff --name-only`:
+for file_name in `git diff --name-only`:
 do
-    if [[ $file =~ \.css|s(c|a)ss ]]; then
-        npm run test:stylelint
+    if [[ $file_name =~ \.css|s(c|a)ss ]]; then
+        RUN_STYLELINT=true
     fi
 
-    if [[ $file =~ \.js|jsx ]]; then
-        npm run test:eslint
+    if [[ $file_name =~ \.js|jsx ]]; then
+        RUN_ESLINT=true
     fi
+done
+
+for file_name in `git diff --name-only`:
+do
+    echo | grep -P (?<=packages\/)(\w+) << $file_name
 done

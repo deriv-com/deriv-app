1ac2f99c7374c878aad9d0f49e49626989a3752a
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-08-02 10:00:07 +0800

    Add test to start command in components

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -3,87 +3,92 @@ const StyleLintPlugin           = require('stylelint-webpack-plugin');
 const SpriteLoaderPlugin        = require('svg-sprite-loader/plugin');
 const path                      = require('path');
 
-module.exports = (env, argv) => ({
+module.exports = (env, argv) => {
+    var isDevMode = ( argv.mode === "development" );
+    const is_serve = isDevMode;
+    
     // entry: path.join(__dirname, 'src', 'index.js'),
-    entry: {
-        // index: path.join(__dirname, 'src', 'index.js'),
-        button: path.resolve(__dirname, 'src' ,'components/button/index.js'),
-        label: path.resolve(__dirname, 'src' , 'components/label/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: "[name].js",
-        libraryExport: 'default',
-        library: ["deriv-component", "[name]"],
-        libraryTarget: 'umd',
-    },
-    optimization : {
-        minimize: true,
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    devServer: {
-        publicPath: '/dist/',
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(s*)css$/,
-                use: [
-                    'css-hot-loader',
-                     MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: { sourceMap: true },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: { sourceMap: true },
-                    }
-               ]
-            },  
-            {  
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: 'svg-sprite-loader',
-                        options: {
-                            extract: true,
-                            spriteFilename: 'bot-sprite.svg',
+    return ({
+        entry: {
+            // index: path.join(__dirname, 'src', 'index.js'),
+            button: path.resolve(__dirname, 'src' ,'components/button/index.js'),
+            label: path.resolve(__dirname, 'src' , 'components/label/index.js'),
+        },
+        output: {
+            path: path.resolve(__dirname, 'lib'),
+            filename: "[name].js",
+            libraryExport: 'default',
+            library: ["deriv-component", "[name]"],
+            libraryTarget: 'umd',
+        },
+        optimization : {
+            minimize: true,
+            splitChunks: {
+                chunks: 'all'
+            }
+        },
+        devServer: {
+            publicPath: '/dist/',
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.(s*)css$/,
+                    use: [
+                        'css-hot-loader',
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: { sourceMap: true },
                         },
-                    },
-                    {
-                        loader: 'svgo-loader',
-                        options: {
-                            plugins: [
-                                { removeUselessStrokeAndFill: false },
-                                { removeUnknownsAndDefaults: false },
-                            ],
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: true },
+                        }
+                ]
+                },  
+                {  
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: 'svg-sprite-loader',
+                            options: {
+                                extract: true,
+                                spriteFilename: 'bot-sprite.svg',
+                            },
                         },
-                    },
-                ],
-            },
-           (argv.mode === 'production' ? {
-                    enforce: "pre",
-                    test: /\.(js|jsx)$/,
-                    exclude: [/node_modules/],
-                    loader: "eslint-loader",
-                    options: {
-                        fix: true
-                    },
-                  } :{}),                
-            {
-                  test   : /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader : 'babel-loader',
-            },
+                        {
+                            loader: 'svgo-loader',
+                            options: {
+                                plugins: [
+                                    { removeUselessStrokeAndFill: false },
+                                    { removeUnknownsAndDefaults: false },
+                                ],
+                            },
+                        },
+                    ],
+                },
+            ( !is_serve ? {
+                        enforce: "pre",
+                        test: /\.(js|jsx)$/,
+                        exclude: [/node_modules/],
+                        loader: "eslint-loader",
+                        options: {
+                            fix: true
+                        },
+                    } :{}),                
+                {
+                    test   : /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader : 'babel-loader',
+                },
+            ],
+        },
+        plugins: [
+            // new MiniCssExtractPlugin({ filename: 'component.css' }),
+            new StyleLintPlugin( { fix: true }),
+            new SpriteLoaderPlugin(),
         ],
-    },
-    plugins: [
-        // new MiniCssExtractPlugin({ filename: 'component.css' }),
-        new StyleLintPlugin( { fix: true }),
-        new SpriteLoaderPlugin(),
-    ],
-});
+    });
+}

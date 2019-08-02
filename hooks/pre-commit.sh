91ee7af734caaf4bf0b2b4e7637f888f68038015
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-08-02 16:46:39 +0800

    make sure linters run based on package affected

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,20 +1,42 @@
 #!/bin/sh
 
-RUN_STYLELINT=false
-RUN_ESLINT=false
+## Main Routine
+SHOULD_RUN_STYLELINT=false
+SHOULD_RUN_ESLINT=false
+MODIFIED_PACKAGES=()
 
-for file_name in `git diff --name-only`:
+# Checks if we should run eslint or stylelint.
+files=`git diff --name-only`
+for file in $files;
 do
-    if [[ $file_name =~ \.css|s(c|a)ss ]]; then
-        RUN_STYLELINT=true
+    # checks for stylelint
+    if [[ $file =~ \.css|s(c|a)ss ]]; then
+        SHOULD_RUN_STYLELINT=true
     fi
 
-    if [[ $file_name =~ \.js|jsx ]]; then
-        RUN_ESLINT=true
+    # checks for eslint
+    if [[ $file =~ \.js|jsx ]]; then
+        SHOULD_RUN_ESLINT=true
     fi
+
+    # get which packages are affected
+    package_to_test=('trader' 'bot')
+    for package in ${package_to_test[@]};
+    do
+        if [[ $package =~ ${package} ]]; then
+            echo $package
+            MODIFIED_PACKAGES+=($package)
+        fi
+    done
 done
 
-for file_name in `git diff --name-only`:
+# Run linters based on packages
+for package in ${MODIFIED_PACKAGES[@]};
 do
-    echo | grep -P (?<=packages\/)(\w+) << $file_name
+    if [ $SHOULD_RUN_STYLELINT = true ]; then
+        npm run test:stylelint $package
+    fi
+    if [ $SHOULD_RUN_ESLINT = true ]; then
+        npm run test:eslint $package
+    fi
 done

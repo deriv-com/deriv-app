6b539e8a3139189f2d6b60453f9d4c68c5001090
Aaron Imming <aaron@binary.com>
2019-08-06 11:50:52 +0430

    Fetch url base for production urls

--- packages/bot/src/app.js
+++ packages/bot/src/app.js
@@ -1,9 +1,17 @@
 import Bot from './app.jsx';
 
+const getUrlBase = (path = '') => {
+    const l = window.location;
+
+    if (!/^\/br_/.test(l.pathname)) return path;
+
+    return `/${l.pathname.split('/')[1]}${/^\//.test(path) ? path : `/${path}`}`;
+};
+
 export function setBotPublicPath(path) {
     __webpack_public_path__ = path; // eslint-disable-line
 }
 
-setBotPublicPath('/js/bot/');
+setBotPublicPath(getUrlBase('/js/bot/'));
 
 export default Bot;

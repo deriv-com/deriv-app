655cf7ea4188035614ead8284a89d7b946e14751
Mahdi <m.pourziaei@gmail.com>
2019-08-07 14:01:50 +0800

    + Moved `pushTransactionData()` and `eventHandler()` to GTM Store (&ref) + Added `getClientAccountType` and `getMT5AccountType` helpers [WIP] + Added `account_type` property to Client Store + Mirrored changes to Core

--- packages/trader/src/Stores/gtm-store.js
+++ packages/trader/src/Stores/gtm-store.js
@@ -1,10 +1,16 @@
+import * as Cookies           from 'js-cookie';
 import {
     action,
-    computed }              from 'mobx';
+    computed }                from 'mobx';
 import BinarySocket           from '_common/base/socket_base';
 import { isLoginPages }       from '_common/base/login';
 import { get as getLanguage } from '_common/language';
+import { getPropertyValue }   from '_common/utility';
+import {
+    toMoment,
+    epochToMoment }           from 'Utils/Date';
 import BaseStore              from './base-store';
+import { getMT5AccountType }  from './Helpers/client';
 import { getAppId }           from '../config';
 
 export default class GTMStore extends BaseStore {
@@ -21,11 +27,6 @@ export default class GTMStore extends BaseStore {
         return this.root_store.client.loginid;
     }
 
-    @computed
-    get currency() {
-        return this.root_store.client.currency;
-    }
-
     /**
      * Contains common data that will be passed to GTM on each datalayer push
      *
@@ -37,7 +38,7 @@ export default class GTMStore extends BaseStore {
             language: getLanguage(),
             ...this.root_store.client.is_logged_in && {
                 visitorId: this.visitorId,
-                currency : this.currency,
+                currency : this.root_store.client.currency,
                 userId   : this.root_store.client.user_id,
             },
             ...this.root_store.ui.is_dark_mode_on && {
@@ -101,6 +102,119 @@ export default class GTMStore extends BaseStore {
             },
         };
         this.pushDataLayer(data);
+    }
+
+    /**
+     * Pushes deposit & withdrawal data from transaction-stream to GTM
+     *
+     * @param {object} response
+     * @param {object} extra_data
+     */
+    @action.bound
+    pushTransactionData(response, extra_data = {}) {
+        if (!this.is_gtm_applicable || this.root_store.client.is_virtual) return;
+        if (!response.transaction || !response.transaction.action) return;
+        if (!['deposit', 'withdrawal'].includes(response.transaction.action)) return;
+
+        const moment_now  = toMoment();
+        const storage_key = 'GTM_transactions';
+
+        // Remove values from prev days so localStorage doesn't grow to infinity
+        let gtm_transactions = JSON.parse(localStorage.getItem(storage_key)) || {};
+        if (Object.prototype.hasOwnProperty.call(gtm_transactions, 'timestamp')) {
+            if (moment_now.isAfter(epochToMoment(gtm_transactions.timestamp), 'day')) {
+                localStorage.removeItem(storage_key);
+                gtm_transactions = { timestamp: moment_now.unix() };
+            }
+        }
+        const transactions_arr = gtm_transactions.transactions || [];
+        if (!transactions_arr.includes(response.transaction.transaction_id)) {
+            const data = {
+                event           : 'transaction',
+                bom_account_type: this.root_store.client.account_type,
+                bom_today       : moment_now.unix(),
+                transaction     : {
+                    id     : response.transaction.transaction_id,
+                    type   : response.transaction.action,
+                    time   : response.transaction.transaction_time,
+                    amount : response.transaction.amount,
+                    balance: response.transaction.balance,
+                },
+            };
+            Object.assign(data, extra_data);
+            this.pushDataLayer(data);
+
+            transactions_arr.push(response.transaction.transaction_id);
+            gtm_transactions.transactions = transactions_arr;
+            gtm_transactions.timestamp    = gtm_transactions.timestamp || moment_now.unix();
+
+            localStorage.setItem(storage_key, JSON.stringify(gtm_transactions));
+        }
+    }
+
+    @action.bound
+    eventHandler(get_settings) {
+        if (!this.is_gtm_applicable) return;
+
+        const login_event       = localStorage.getItem('GTM_login');
+        const is_new_account    = localStorage.getItem('GTM_new_account') === '1';
+
+        localStorage.removeItem('GTM_login');
+        localStorage.removeItem('GTM_new_account');
+
+        const affiliate_token = Cookies.getJSON('affiliate_tracking');
+        if (affiliate_token) {
+            this.pushDataLayer({ affiliate_token: affiliate_token.t });
+        }
+
+        // Get current time (moment, set by server), else fallback to client time
+        const moment_now = toMoment();
+        const data = {
+            visitorId     : this.visitorId,
+            account_type  : this.root_store.client.account_type,
+            currency      : this.root_store.client.currency,
+            country       : get_settings.country,
+            country_abbrev: get_settings.country_code,
+            email         : get_settings.email,
+            url           : window.location.href,
+            today         : moment_now.unix(),
+        };
+
+        if (is_new_account) {
+            data.event = 'new_account';
+            data.bom_date_joined = data.bom_today;
+        }
+
+        if (!this.root_store.client.is_virtual) {
+            data.bom_age       = moment_now.diff(epochToMoment(get_settings.date_of_birth), 'year');
+            data.bom_firstname = get_settings.first_name;
+            data.bom_lastname  = get_settings.last_name;
+            data.bom_phone     = get_settings.phone;
+        }
+
+        if (login_event) {
+            data.event = login_event;
+            BinarySocket.wait('mt5_login_list').then((response) => {
+                (response.mt5_login_list || []).forEach((obj) => {
+                    const acc_type = (getMT5AccountType(obj.group) || '')
+                        .replace('real_vanuatu', 'financial').replace('vanuatu_', '').replace(/svg/, 'gaming'); // i.e. financial_cent, demo_cent, demo_gaming, real_gaming
+                    if (acc_type) {
+                        data[`mt5_${acc_type}_id`] = obj.login;
+                    }
+                });
+                this.pushDataLayer(data);
+            });
+        } else {
+            this.pushDataLayer(data);
+        }
+
+        // check if there are any transactions in the last 30 days for UX interview selection
+        BinarySocket.send({ statement: 1, limit: 1 }).then((response) => {
+            const last_transaction_timestamp = getPropertyValue(response, ['statement', 'transactions', '0', 'transaction_time']);
+            this.pushDataLayer({
+                bom_transaction_in_last_30d: !!last_transaction_timestamp && toMoment(last_transaction_timestamp * 1000).isAfter(toMoment().subtract(30, 'days')),
+            });
+        });
     }
 
     @action.bound

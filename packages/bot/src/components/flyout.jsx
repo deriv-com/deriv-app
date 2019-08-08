bb3ca11811950f7676f4f6e244248106392d1ca7
Aaron Imming <aaron@binary.com>
2019-08-08 19:09:34 +0430

    Generate list of components in flyout

--- packages/bot/src/components/flyout.jsx
+++ packages/bot/src/components/flyout.jsx
@@ -1,23 +1,63 @@
 import React            from 'react';
 import PropTypes        from 'prop-types';
+import FlyoutBlock      from './flyout-block.jsx';
 import { connect }      from '../stores/connect';
-import                       '../assets/sass/scratch/_flyout.scss';
+import                       '../assets/sass/scratch/flyout.scss';
 
-class Flyout extends React.PureComponent {
-    render() {
-        const { is_visible, flyout_width, flyout_content } = this.props;
+const Flyout = ({ is_visible, flyout_width, flyout_content }) => {
+    return (
+        <div
+            className={`flyout${!is_visible ? ' hidden' : ''}`}
+            style={{ width: `${flyout_width}px` }}
+        >
+            { flyout_content.map((node, index) => {
+                const tag = node.tagName.toUpperCase();
 
-        return (
-            <div
-                ref={node => this.node = node}
-                className={`flyout${!is_visible ? ' flyout--hidden' : ''}`}
-                style={{ width: `${flyout_width}px` }}
-            >
-                { flyout_content }
-            </div>
-        );
-    }
-}
+                if (tag === Blockly.Xml.NODE_BLOCK) {
+                    return (
+                        <FlyoutBlock
+                            key={node.getAttribute('type') + index}
+                            id={`flyout__item-workspace--${index}`}
+                            block_node={node}
+                        />
+                    );
+                } else if (tag === Blockly.Xml.NODE_LABEL) {
+                    return (
+                        <div
+                            key={node.getAttribute('text') + index}
+                            className='flyout__item-label'
+                        >
+                            { node.getAttribute('text') }
+                        </div>
+                    );
+                } else if (tag === Blockly.Xml.NODE_BUTTON) {
+                    const callback_key = node.getAttribute('callbackKey');
+                    const callback = Blockly.derivWorkspace.getButtonCallback(callback_key) || (() => {});
+
+                    return (
+                        <button
+                            key={callback_key + index}
+                            className='flyout__button'
+                            onClick={(button) => {
+                                const flyout_button = button;
+    
+                                // Workaround for not having a flyout workspace.
+                                flyout_button.targetWorkspace_ = Blockly.derivWorkspace;
+                                flyout_button.getTargetWorkspace = () => flyout_button.targetWorkspace_;
+    
+                                callback(flyout_button);
+                            }}
+                        >
+                            { node.getAttribute('text') }
+                        </button>
+                    );
+                }
+
+                return null;
+            }) }
+        </div>
+    );
+};
 
 Flyout.propTypes = {
     flyout_content: PropTypes.array,

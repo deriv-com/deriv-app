ed04ed0a1bcf322061213f6405854793a806b5f4
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-08-09 16:50:58 +0800

    Fix font mixin issue

--- packages/shared/src/styles/index.js
+++ packages/shared/src/styles/index.js
@@ -1,5 +1,5 @@
 const path = require("path");
 
-const resources = ["constants.scss", "fonts.scss" , "devices.scss", "reset.scss" , "themes.scss"];
+const resources = ["constants.scss", "mixins.scss", "fonts.scss", "themes.scss", "devices.scss", "reset.scss"];
 
 module.exports = resources.map(file => path.resolve(__dirname , 'styles' , file));

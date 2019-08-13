de958175e69384ddc3d6d2b33300dd8b2c7d85d9
Aaron Imming <aaron@binary.com>
2019-08-13 16:48:17 +0430

    Fix formatting, change if statement into switch statement

--- packages/bot/src/components/flyout.jsx
+++ packages/bot/src/components/flyout.jsx
@@ -4,7 +4,11 @@ import FlyoutBlock      from './flyout-block.jsx';
 import { connect }      from '../stores/connect';
 import                       '../assets/sass/scratch/flyout.scss';
 
-const Flyout = ({ is_visible, flyout_width, flyout_content }) => {
+const Flyout = ({
+    flyout_content,
+    flyout_width,
+    is_visible,
+}) => {
     return (
         <div
             className={`flyout${!is_visible ? ' hidden' : ''}`}
@@ -13,47 +17,48 @@ const Flyout = ({ is_visible, flyout_width, flyout_content }) => {
             { flyout_content.map((node, index) => {
                 const tag_name = node.tagName.toUpperCase();
 
-                if (tag_name === Blockly.Xml.NODE_BLOCK) {
-                    return (
-                        <FlyoutBlock
-                            key={node.getAttribute('type') + index}
-                            id={`flyout__item-workspace--${index}`}
-                            block_node={node}
-                        />
-                    );
-                } else if (tag_name === Blockly.Xml.NODE_LABEL) {
-                    return (
-                        <div
-                            key={node.getAttribute('text') + index}
-                            className='flyout__item-label'
-                        >
-                            { node.getAttribute('text') }
-                        </div>
-                    );
-                } else if (tag_name === Blockly.Xml.NODE_BUTTON) {
-                    const callback_key = node.getAttribute('callbackKey');
-                    const callback = Blockly.derivWorkspace.getButtonCallback(callback_key) || (() => {});
-
-                    return (
-                        <button
-                            key={callback_key + index}
-                            className='flyout__button'
-                            onClick={(button) => {
-                                const flyout_button = button;
-    
-                                // Workaround for not having a flyout workspace.
-                                flyout_button.targetWorkspace_ = Blockly.derivWorkspace;
-                                flyout_button.getTargetWorkspace = () => flyout_button.targetWorkspace_;
-    
-                                callback(flyout_button);
-                            }}
-                        >
-                            { node.getAttribute('text') }
-                        </button>
-                    );
+                switch (tag_name) {
+                    case Blockly.Xml.NODE_BLOCK:
+                        return (
+                            <FlyoutBlock
+                                key={node.getAttribute('type') + index}
+                                id={`flyout__item-workspace--${index}`}
+                                block_node={node}
+                            />
+                        );
+                    case Blockly.Xml.NODE_LABEL:
+                        return (
+                            <div
+                                key={node.getAttribute('text') + index}
+                                className='flyout__item-label'
+                            >
+                                { node.getAttribute('text') }
+                            </div>
+                        );
+                    case Blockly.Xml.NODE_BUTTON:
+                        const callback_key = node.getAttribute('callbackKey');
+                        const callback = Blockly.derivWorkspace.getButtonCallback(callback_key) || (() => {});
+                
+                        return (
+                            <button
+                                key={callback_key + index}
+                                className='flyout__button'
+                                onClick={(button) => {
+                                    const flyout_button = button;
+        
+                                    // Workaround for not having a flyout workspace.
+                                    flyout_button.targetWorkspace_ = Blockly.derivWorkspace;
+                                    flyout_button.getTargetWorkspace = () => flyout_button.targetWorkspace_;
+        
+                                    callback(flyout_button);
+                                }}
+                            >
+                                { node.getAttribute('text') }
+                            </button>
+                        );
+                    default:
+                        return null;
                 }
-
-                return null;
             }) }
         </div>
     );

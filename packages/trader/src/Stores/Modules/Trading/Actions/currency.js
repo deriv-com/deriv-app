39f4823cdfdf6d75c54cb747139eba454843b802
mustofa-binary <mustofa@regentmarkets.com>
2019-08-15 13:29:13 +0800

    remove console currency

--- packages/trader/src/Stores/Modules/Trading/Actions/currency.js
+++ packages/trader/src/Stores/Modules/Trading/Actions/currency.js
@@ -5,8 +5,6 @@ import {
 
 export const getCurrenciesAsync = async(currency) => {
     const response = await WS.payoutCurrencies();
-    console.log(currency)
-    console.log('hi')
 
     const currencies_list  = buildCurrenciesList(response.payout_currencies);
     const default_currency = getDefaultCurrency(currencies_list, currency);

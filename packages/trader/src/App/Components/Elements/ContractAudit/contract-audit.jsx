e96d45250db0fec031b63e01e20567c0384190b4
mustofa-binary <mustofa@regentmarkets.com>
2019-08-15 18:18:13 +0800

    remove contract

--- packages/trader/src/App/Components/Elements/ContractAudit/contract-audit.jsx
+++ packages/trader/src/App/Components/Elements/ContractAudit/contract-audit.jsx
@@ -39,7 +39,7 @@ class ContractAudit extends React.PureComponent {
                         style={{ width: '100%', height: '100%' }}
                         autoHide
                     >
-                        <div id={`dt_id_label`} className='contract-audit__grid'>
+                        <div id={'dt_id_label'} className='contract-audit__grid'>
                             <ContractAuditItem
                                 icon={getThemedIcon('id', is_dark_theme)}
                                 label={localize('Reference ID')}
@@ -47,7 +47,7 @@ class ContractAudit extends React.PureComponent {
                                 value2={localize('{{sell_value}} (Sell)', { sell_value: contract_info.transaction_ids.sell })}
                             />
                         </div>
-                        <div id={`dt_duration_label`} className='contract-audit__grid'>
+                        <div id={'dt_duration_label'} className='contract-audit__grid'>
                             <ContractAuditItem
                                 icon={getThemedIcon('duration', is_dark_theme)}
                                 label={localize('Duration')}
@@ -57,7 +57,7 @@ class ContractAudit extends React.PureComponent {
                                     `${duration} ${duration_unit}`}
                             />
                         </div>
-                        <div id={`dt_bt_label`} className='contract-audit__grid'>
+                        <div id={'dt_bt_label'} className='contract-audit__grid'>
                             <ContractAuditItem
                                 icon={
                                     isDigitType(contract_info.contract_type)
@@ -68,7 +68,7 @@ class ContractAudit extends React.PureComponent {
                                 value={getBarrierValue(contract_info) || ' - '}
                             />
                         </div>
-                        <div id={`dt_start_time_label`} className='contract-audit__grid'>
+                        <div id={'dt_start_time_label'} className='contract-audit__grid'>
                             <ContractAuditItem
                                 icon={getThemedIcon('start_time', is_dark_theme)}
                                 label={localize('Start time')}
@@ -76,7 +76,7 @@ class ContractAudit extends React.PureComponent {
                             />
                         </div>
                         {!isDigitType(contract_info.contract_type) &&
-                        <div id={`dt_entry_spot_label`} className='contract-audit__grid'>
+                        <div id={'dt_entry_spot_label'} className='contract-audit__grid'>
                             <ContractAuditItem
                                 icon={getThemedIcon('entry_spot', is_dark_theme)}
                                 label={localize('Entry spot')}
@@ -87,7 +87,7 @@ class ContractAudit extends React.PureComponent {
                         }
                         {
                             !isNaN(exit_spot) &&
-                            <div id={`dt_exit_spot_label`} className='contract-audit__grid'>
+                            <div id={'dt_exit_spot_label'} className='contract-audit__grid'>
                                 <ContractAuditItem
                                     icon={getThemedIcon('exit_spot', is_dark_theme)}
                                     label={localize('Exit spot')}
@@ -96,7 +96,7 @@ class ContractAudit extends React.PureComponent {
                                 />
                             </div>
                         }
-                        <div id={`dt_exit_time_label`} className='contract-audit__grid'>
+                        <div id={'dt_exit_time_label'} className='contract-audit__grid'>
                             <ContractAuditItem
                                 icon={IconExitTime}
                                 label={localize('Exit Time')}

75e1794a154df568715d489313a0d532a1878550
Mahdi <m.pourziaei@gmail.com>
2019-08-16 16:18:50 +0800

    + Fixed error message position + Updated input, label, and placeholder styles to match styleguide

--- packages/components/src/components/forms/input/input.jsx
+++ packages/components/src/components/forms/input/input.jsx
@@ -6,20 +6,22 @@ const Input = (props) => (
     <Field { ...props }>
         {
             ({ field }) => (
-                <div className='dc-input'>
-                    <input { ...field } { ...props } className='dc-input__field' />
-                    <span className='dc-input__bar' />
-                    <label className='dc-input__label' htmlFor={ field.id }>{ props.label || props.placeholder }</label>
+                <React.Fragment>
+                    <div className='dc-input'>
+                        <input { ...field } { ...props } className='dc-input__field' />
+                        <span className='dc-input__bar' />
+                        <label className='dc-input__label' htmlFor={ field.id }>{ props.label || props.placeholder }</label>
+                    </div>
                     <ErrorMessage name={ field.name }>
                         {
                             (msg) => (
-                                <span className='dc-input__error'>
+                                <p className='dc-input__error'>
                                     { msg }
-                                </span>
+                                </p>
                             )
                         }
                     </ErrorMessage>
-                </div>
+                </React.Fragment>
             )
         }
     </Field>

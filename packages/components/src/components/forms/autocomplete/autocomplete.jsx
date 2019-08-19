6fda3698e68f58b757383590ad0bc0feb2878a8b
Mahdi <m.pourziaei@gmail.com>
2019-08-19 16:38:30 +0800

    Added dropdown view adjust for errors

--- packages/components/src/components/forms/autocomplete/autocomplete.jsx
+++ packages/components/src/components/forms/autocomplete/autocomplete.jsx
@@ -7,9 +7,9 @@ import                   './autocomplete.scss';
 
 // TODO: use-from-shared - Use this icon from icons' shared package
 const IconArrow = ({ className, classNamePath }) => (
-    <svg className={classNames('inline-icon', className)} width='16' height='16'>
+    <svg className={ classNames('inline-icon', className) } width='16' height='16'>
         <path
-            className={classNames(classNamePath, 'color1-fill')}
+            className={ classNames(classNamePath, 'color1-fill') }
             fill='rgba(0, 0, 0, 0.8)'
             fillRule='nonzero'
             d='M13.164 5.13a.5.5 0 1 1 .672.74l-5.5 5a.5.5 0 0 1-.672 0l-5.5-5a.5.5 0 0 1 .672-.74L8 9.824l5.164-4.694z'
@@ -61,16 +61,19 @@ class Autocomplete extends React.PureComponent {
                                     onBlur={ (e) => { this.hideDropdownList(); field.onBlur(e); } }
                                     trailing_icon={
                                         <IconArrow
-                                            className={{
+                                            className={ {
                                                 'dc-autocomplete__trailing-icon'        : true,
                                                 'dc-autocomplete__trailing-icon--opened': this.state.should_show_list,
-                                            }}
+                                            } }
                                         />
                                     }
                                 />
                             </div>
                             <DropdownList
-                                style={ { width: this.input_wrapper_ref ? `${ this.input_wrapper_ref.offsetWidth }px` : '100%' } }
+                                style={ {
+                                    width    : this.input_wrapper_ref ? `${ this.input_wrapper_ref.offsetWidth }px` : '100%',
+                                    marginTop: form.errors[field.name] ? 'calc(4px - 18px)' : '4px', // 4px is the standard margin. In case of error, the list should overlap the error
+                                } }
                                 is_visible={ this.state.should_show_list }
                                 list_items={ this.state.filtered_items || this.props.list_items }
                                 onItemSelection={ (item) => form.setFieldValue(field.name, item, true) }

e75d5a82d59e118e6684c8e5e28916bf230a5e89
Aaron Imming <aaron@binary.com>
2019-08-21 16:18:32 +0430

    Refactor updateOptions function

--- packages/bot/src/scratch/hooks/field_dropdown.js
+++ packages/bot/src/scratch/hooks/field_dropdown.js
@@ -1,14 +1,14 @@
 /* eslint-disable func-names, no-underscore-dangle */
-Blockly.FieldDropdown.prototype.updateOptions = function(options, optDefault = null, triggerEvent = true) {
-    const previousValue = this.getValue();
-
+Blockly.FieldDropdown.prototype.updateOptions = function(options, opt_default = null, should_trigger_event = true) {
     Blockly.Events.disable();
+
+    const previous_value = this.getValue();
 
     this.menuGenerator_ = options;
 
-    if (optDefault && this.menuGenerator_.findIndex(item => item[1] === optDefault) !== -1) {
+    if (opt_default && this.menuGenerator_.findIndex(item => item[1] === opt_default) !== -1) {
         this.setValue('');
-        this.setValue(optDefault);
+        this.setValue(opt_default);
     } else if (this.menuGenerator_.length > 0) {
         this.setValue('');
         this.setValue(this.menuGenerator_[0][1]);
@@ -16,14 +16,8 @@ Blockly.FieldDropdown.prototype.updateOptions = function(options, optDefault = n
 
     Blockly.Events.enable();
 
-    if (triggerEvent) {
-        const event = new Blockly.Events.BlockChange(
-            this.sourceBlock_,
-            'field',
-            this.name,
-            previousValue,
-            this.getValue()
-        );
+    if (should_trigger_event) {
+        const event = new Blockly.Events.BlockChange(this.sourceBlock_, 'field', this.name, previous_value, this.getValue());
         Blockly.Events.fire(event);
     }
 };

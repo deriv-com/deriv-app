9058dc7ee81a1e1712d659763b9dcf5785870e4a
Aaron Imming <aaron@binary.com>
2019-08-22 14:26:56 +0430

    Refactor updateOptions function on Blockly.FieldDropdown

--- packages/bot/src/scratch/hooks/field_dropdown.js
+++ packages/bot/src/scratch/hooks/field_dropdown.js
@@ -2,14 +2,17 @@
 Blockly.FieldDropdown.prototype.updateOptions = function(options, opt_default = null, should_trigger_event = true) {
     Blockly.Events.disable();
 
-    const previous_value = this.getValue();
+    const previous_value    = this.getValue();
+    const has_default_value = opt_default && options.findIndex(item => item[1] === opt_default) !== -1;
 
     this.menuGenerator_ = options;
 
-    if (opt_default && this.menuGenerator_.findIndex(item => item[1] === opt_default) !== -1) {
+    if (has_default_value) {
+        // Set default value if available in new options.
         this.setValue('');
         this.setValue(opt_default);
-    } else if (this.menuGenerator_.length > 0) {
+    } else if (options.length > 0) {
+        // Default to first if option isn't available.
         this.setValue('');
         this.setValue(this.menuGenerator_[0][1]);
     }
@@ -18,6 +21,7 @@ Blockly.FieldDropdown.prototype.updateOptions = function(options, opt_default = 
 
     if (should_trigger_event) {
         const event = new Blockly.Events.BlockChange(this.sourceBlock_, 'field', this.name, previous_value, this.getValue());
+
         Blockly.Events.fire(event);
     }
 };

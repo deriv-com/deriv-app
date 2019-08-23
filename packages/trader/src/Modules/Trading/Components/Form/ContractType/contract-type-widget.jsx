271e1b4e6a0c65a705bf5b4d5831c5c122c784a7
nazaninreihani <nazanin.reyh@gmail.com>
2019-08-23 10:16:27 -0400

    refactor

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/contract-type-widget.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/contract-type-widget.jsx
@@ -134,9 +134,9 @@ class ContractTypeWidget extends React.PureComponent {
     render() {
         const { is_dark_theme, is_equal, is_mobile, list, name, value } = this.props;
         const { is_dialog_open, is_info_dialog_open, item }             = this.state;
-        const contractTypes       = this.getItemList();
-        const item_index          = this.getItemIndex(item, contractTypes);
-        const contractTypesLength = contractTypes ? contractTypes.length : 0;
+        const item_list        = this.getItemList();
+        const item_index       = this.getItemIndex(item, item_list);
+        const item_list_length = item_list ? item_list.length : 0;
         return (
             <div
                 id='dt_contract_dropdown'
@@ -194,8 +194,8 @@ class ContractTypeWidget extends React.PureComponent {
                         is_mobile={is_mobile}
                         item={item}
                         item_index={item_index < 0 ? undefined : item_index}
-                        itemList={contractTypes}
-                        itemListLength={contractTypesLength}
+                        itemList={item_list}
+                        itemListLength={item_list_length}
                         onBackButtonClick={this.onBackButtonClick}
                         onSubmitButtonClick={this.onSubmitButtonClick}
                     />

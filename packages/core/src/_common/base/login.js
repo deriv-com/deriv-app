dda682e281eb82646c12ac4a39c56fdd5824c762
Mahdi <m.pourziaei@gmail.com>
2019-08-23 10:16:59 +0800

    Updated login (OAuth) URL

--- packages/core/src/_common/base/login.js
+++ packages/core/src/_common/base/login.js
@@ -23,7 +23,7 @@ const Login = (() => {
         const signup_device      = LocalStore.get('signup_device') || (isMobile() ? 'mobile' : 'desktop');
         const date_first_contact = LocalStore.get('date_first_contact');
         const marketing_queries  = `&signup_device=${signup_device}${date_first_contact ? `&date_first_contact=${date_first_contact}` : ''}`;
-        const default_login_url  = `https://oauth.deriv.com/oauth2/authorize?app_id=${getAppId()}&l=${language}${marketing_queries}&brand=${website_name.toLowerCase()}`;
+        const default_login_url  = `https://oauth.deriv.app/oauth2/authorize?app_id=${getAppId()}&l=${language}${marketing_queries}&brand=${website_name.toLowerCase()}`;
 
         if (server_url && /qa/.test(server_url)) {
             return `https://${server_url}/oauth2/authorize?app_id=${getAppId()}&l=${language}${marketing_queries}`;

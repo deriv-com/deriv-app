3f0dc3695a371950423f1982c5191510e994e3fc
Mohammadreza Ghorbani <reza@firstsource.io>
2019-08-27 14:23:24 +0430

    Merge remote-tracking branch 'upstream/dev' into storybook-dropdown

--- packages/components/src/components/dropdown/symbol-span.jsx
+++ packages/components/src/components/dropdown/symbol-span.jsx
@@ -1,6 +1,6 @@
 import PropTypes                        from 'prop-types';
 import React, { Component }             from 'react';
-import { getDisplayText, listPropType } from './helpers';
+import { getDisplayText, listPropType } from './dropdown';
 
 class SymbolSpan extends Component {
     render () {
--- packages/components/src/components/popover/index.js
+++ packages/components/src/components/popover/index.js
@@ -1,4 +1,4 @@
-export default from './popover.jsx';
-
 import './popover.scss';
+
+export default from './popover.jsx';
 

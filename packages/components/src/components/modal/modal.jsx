dc252f978bf7a8bf9a6712f369c2f7a25bd016a8
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-08-29 16:01:41 +0800

    Add close icon to modal

--- packages/components/src/components/modal/modal.jsx
+++ packages/components/src/components/modal/modal.jsx
@@ -5,6 +5,13 @@ import ReactDOM          from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
 // import Icon              from 'Assets/icon.jsx';
 
+// TODO: use-from-shared - Use this icon from icons' shared package
+const IconClose = ({ className, onClick }) => (
+    <svg onClick={onClick} className={classNames('inline-icon', className)} width='16' height='16' viewBox='0 0 16 16'>
+        <path className='color1-fill' fill='#2A3052' fillRule='nonzero' d='M8 7.293l4.146-4.147a.5.5 0 0 1 .708.708L8.707 8l4.147 4.146a.5.5 0 0 1-.708.708L8 8.707l-4.146 4.147a.5.5 0 0 1-.708-.708L7.293 8 3.146 3.854a.5.5 0 1 1 .708-.708L8 7.293z' />
+    </svg>
+);
+
 class ModalElement extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -43,7 +50,7 @@ class ModalElement extends React.PureComponent {
                             </div>
                         }
                         <div id='dt_modal_close_icon' className='modal-header__close' onClick={this.props.toggleModal}>
-                            {/* <Icon icon='ModalIconClose' /> */}
+                            <IconClose icon='ModalIconClose' />
                         </div>
                     </div>
                 </div>

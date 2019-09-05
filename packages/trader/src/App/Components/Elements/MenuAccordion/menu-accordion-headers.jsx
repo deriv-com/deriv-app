646a75affbc89e699805363955047fbbbe37cda1
Kelly Anyi <kelly@binary.com>
2019-09-05 16:14:19 +0800

    update accordion logic and css

--- packages/trader/src/App/Components/Elements/MenuAccordion/menu-accordion-headers.jsx
+++ packages/trader/src/App/Components/Elements/MenuAccordion/menu-accordion-headers.jsx
@@ -8,50 +8,47 @@ import {
 
 class MenuAccordionHeaders extends React.PureComponent {
     state = {
-        is_active_tab_open: true,
-        selected_tab      : this.props.items[0].label,
+        [this.props.items[0].label]: true,
     }
 
     handleAccordion = (selected) => {
-        const is_same_header   = this.state.selected_tab === selected.label;
-        let is_active_tab_open = true;
-
-        if (is_same_header) {
-            is_active_tab_open = !this.state.is_active_tab_open;
-        } else {
-            this.props.onChange(selected.sub_tab_list[0]);
-        }
-        this.setState({ selected_tab: selected.label, is_active_tab_open });
+        this.setState({
+            [selected.label]: !this.state[selected.label],
+        });
     }
 
     render() {
-        const { selected_tab, is_active_tab_open } = this.state;
-        const { onChange, selected, items }        = this.props;
+        const { onChange, selected, items } = this.props;
 
         return (
             <VerticalTabWrapper>
                 {items.map((item, idx) => {
-                    const show_sub_tab_list = (selected_tab === item.label && is_active_tab_open);
+                    const show_sub_tab_list = this.state[item.label] ;
 
                     return (
                         <div key={idx} className='menu-accordion'>
-                            <div>
-                                <VerticalTabHeader item={item} onChange={this.handleAccordion} >
-                                    <Icon
-                                        icon='IconChevronRight'
-                                        className={classNames('dt_accordion-chevron', {
-                                            'dt_accordion-chevron--show': show_sub_tab_list,
-                                        })}
-                                    />
-                                </VerticalTabHeader>
-                            </div>
-                            {show_sub_tab_list && (
+                            <VerticalTabHeader
+                                item={item}
+                                className='menu-accordion__header'
+                                onChange={this.handleAccordion}
+                            >
+                                <Icon
+                                    icon='IconChevronRight'
+                                    className={classNames('menu-accordion__icon', {
+                                        'menu-accordion__icon--show': show_sub_tab_list,
+                                    })}
+                                />
+                            </VerticalTabHeader>
+                            <div className={classNames('menu-accordion__subtab', {
+                                'menu-accordion__subtab--show': show_sub_tab_list,
+                            })}
+                            >
                                 <VerticalTabHeaders
                                     items={item.sub_tab_list}
                                     onChange={onChange}
                                     selected={selected}
                                 />
-                            )}
+                            </div>
                         </div>
                     );
                 }

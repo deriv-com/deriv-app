e87163d58273ec157eecfc1d91c085a8d3b6db2e
Kelly Anyi <kelly@binary.com>
2019-09-06 17:17:43 +0800

    fix account-management form layout

--- packages/trader/src/App/Containers/AccountManagementModal/Components/layout-components.jsx
+++ packages/trader/src/App/Containers/AccountManagementModal/Components/layout-components.jsx
@@ -3,3 +3,7 @@ import React            from 'react';
 export const FormFooter = ({ children }) => (
         <div className='account-management-form-footer'>{children}</div>
 );
+
+export const FormBody = ({ children }) => (
+        <div className='account-management-form-body'>{children}</div>
+);

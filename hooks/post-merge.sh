666b3ae1f53a9cd4e3e3b7a3a70cf589613f070e
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-06 14:20:32 +0800

    update post-merge.sh

--- hooks/post-merge.sh
+++ hooks/post-merge.sh
@@ -1,1 +1,9 @@
-npx lerna run build:travis --since `git branch | grep \* | cut -d ' ' -f2`
+CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+
+# Check if package json or package lock is changed.
+npm ci
+npx lerna bootstrap --since $CURRENT_BRANCH
+npx lerna run build:travis \
+    --scope deriv-shared \
+    --scope deriv-components \
+    --since $CURRENT_BRANCH

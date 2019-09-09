b3c32151c4a78c3e93cfbd66d3d8d9ecb57acb2f
Kelly Anyi <kelly@binary.com>
2019-09-09 14:08:44 +0800

    configure storybook to load svgs

--- packages/components/.storybook/webpack.config.js
+++ packages/components/.storybook/webpack.config.js
@@ -23,6 +23,21 @@ module.exports = async ({ config, mode }) => {
         ],
     });
 
+    // Remove svg from existing rule
+    config.module.rules = config.module.rules.map( data => {
+        if (/svg\|/.test( String( data.test ) )) {
+            data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+        }
+        return data;
+    });
+
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          { loader: '@svgr/webpack' }
+        ]
+    });
+
     config.module.rules.push({
         test: /\.stories\.js?$/,
         loaders: [

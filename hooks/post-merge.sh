3f6561adfc1cd5671dd7e001acf5ad0f4d6a343e
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-10 15:15:14 +0800

    update so lerna only runs build:travis since dev

--- hooks/post-merge.sh
+++ hooks/post-merge.sh
@@ -1,7 +1,5 @@
-CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-
 npx lerna bootstrap --since $CURRENT_BRANCH
 npx lerna run build:travis \
     --scope deriv-shared \
     --scope deriv-components \
-    --since $CURRENT_BRANCH
+    --since dev

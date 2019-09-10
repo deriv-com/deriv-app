3d9857f6ca0535ae67a7e815f2bd4c63b15f14c1
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-10 13:52:30 +0800

    remove npm ci from post-merge.sh

--- hooks/post-merge.sh
+++ hooks/post-merge.sh
@@ -1,7 +1,5 @@
 CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
-# Check if package json or package lock is changed.
-npm ci
 npx lerna bootstrap --since $CURRENT_BRANCH
 npx lerna run build:travis \
     --scope deriv-shared \

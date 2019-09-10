5f06e11b8eb2eb2e39f3c92064bc794a339d388c
zakibinary <zaki@binary.com>
2019-09-10 13:27:44 +0800

    position adjustments for initial loader

--- packages/trader/src/App/Containers/AccountManagementModal/Profile/financial-assessment.jsx
+++ packages/trader/src/App/Containers/AccountManagementModal/Profile/financial-assessment.jsx
@@ -53,7 +53,6 @@ class FinancialAssessment extends React.Component {
             account_turnover,
             is_loading } = this.state;
 
-        if (is_loading) return <Loading />;
         return (
             <Formik
                 initialValues={{
@@ -87,151 +86,159 @@ class FinancialAssessment extends React.Component {
                                 <i className='account-management-form-header-subheader'>
                                     ({localize('All fields are required')})
                                 </i>
-                            </h1>
+                            </h1>x
                         </div>
-                        <FormBody scroll_offset='64px'>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('source of income')}
-                                    is_align_text_left
-                                    name='income_source'
-                                    list={income_source_list}
-                                    value={values.income_source}
-                                    onChange={handleChange}
-                                />
-                                {(errors.income_source || (touched.income_source && errors.income_source)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.income_source}
-                                </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('employment status')}
-                                    is_align_text_left
-                                    name='employment_status'
-                                    list={employment_status_list}
-                                    value={values.employment_status}
-                                    onChange={handleChange}
-                                />
-                                {(errors.employment_status ||
-                                    (touched.employment_status && errors.employment_status)) &&
+                        {is_loading ?
+                            <FormBody>
+                                <Loading />
+                            </FormBody>
+                            :
+                            <FormBody scroll_offset='64px'>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('source of income')}
+                                        is_align_text_left
+                                        name='income_source'
+                                        list={income_source_list}
+                                        value={values.income_source}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.income_source || (touched.income_source && errors.income_source)) &&
                                     <span className='fa-dropdown__error-message'>
-                                        {errors.employment_status}
+                                        {errors.income_source}
                                     </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('industry of employment')}
-                                    is_align_text_left
-                                    name='employment_industry'
-                                    list={employment_industry_list}
-                                    value={values.employment_industry}
-                                    onChange={handleChange}
-                                />
-                                {(errors.employment_industry ||
-                                    (touched.employment_industry && errors.employment_industry)) &&
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('employment status')}
+                                        is_align_text_left
+                                        name='employment_status'
+                                        list={employment_status_list}
+                                        value={values.employment_status}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.employment_status ||
+                                        (touched.employment_status && errors.employment_status)) &&
+                                        <span className='fa-dropdown__error-message'>
+                                            {errors.employment_status}
+                                        </span>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('industry of employment')}
+                                        is_align_text_left
+                                        name='employment_industry'
+                                        list={employment_industry_list}
+                                        value={values.employment_industry}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.employment_industry ||
+                                        (touched.employment_industry && errors.employment_industry)) &&
+                                        <span className='fa-dropdown__error-message'>
+                                            {errors.employment_industry}
+                                        </span>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('occupation')}
+                                        is_align_text_left
+                                        name='occupation'
+                                        list={occupation_list}
+                                        value={values.occupation}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.occupation || (errors.occupation && touched.occupation)) &&
                                     <span className='fa-dropdown__error-message'>
-                                        {errors.employment_industry}
+                                        {errors.occupation}
                                     </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('occupation')}
-                                    is_align_text_left
-                                    name='occupation'
-                                    list={occupation_list}
-                                    value={values.occupation}
-                                    onChange={handleChange}
-                                />
-                                {(errors.occupation || (errors.occupation && touched.occupation)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.occupation}
-                                </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('source of wealth')}
-                                    is_align_text_left
-                                    name='source_of_wealth'
-                                    list={source_of_wealth_list}
-                                    value={values.source_of_wealth}
-                                    onChange={handleChange}
-                                />
-                                {(errors.source_of_wealth || (touched.source_of_wealth && errors.source_of_wealth)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.source_of_wealth}
-                                </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('level of education')}
-                                    is_align_text_left
-                                    name='education_level'
-                                    list={education_level_list}
-                                    value={values.education_level}
-                                    onChange={handleChange}
-                                />
-                                {(errors.education_level || (touched.education_level && errors.education_level)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.education_level}
-                                </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('net annual income')}
-                                    is_align_text_left
-                                    name='net_income'
-                                    list={net_income_list}
-                                    value={values.net_income}
-                                    onChange={handleChange}
-                                />
-                                {(errors.net_income || (touched.net_income && errors.net_income)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.net_income}
-                                </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('estimated net worth')}
-                                    is_alignment_top
-                                    is_align_text_left
-                                    name='estimated_worth'
-                                    list={estimated_worth_list}
-                                    value={values.estimated_worth}
-                                    onChange={handleChange}
-                                />
-                                {(errors.estimated_worth || (errors.estimated_worth && touched.estimated_worth)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.estimated_worth}
-                                </span>
-                                }
-                            </fieldset>
-                            <fieldset className='account-management-form-fieldset'>
-                                <Dropdown
-                                    placeholder={localize('anticipated account turnover')}
-                                    is_alignment_top
-                                    is_align_text_left
-                                    name='account_turnover'
-                                    list={account_turnover_list}
-                                    value={values.account_turnover}
-                                    onChange={handleChange}
-                                />
-                                {(errors.account_turnover || (errors.account_turnover && touched.account_turnover)) &&
-                                <span className='fa-dropdown__error-message'>
-                                    {errors.account_turnover}
-                                </span>
-                                }
-                            </fieldset>
-                        </FormBody>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('source of wealth')}
+                                        is_align_text_left
+                                        name='source_of_wealth'
+                                        list={source_of_wealth_list}
+                                        value={values.source_of_wealth}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.source_of_wealth ||
+                                        (touched.source_of_wealth && errors.source_of_wealth)) &&
+                                        <span className='fa-dropdown__error-message'>
+                                            {errors.source_of_wealth}
+                                        </span>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('level of education')}
+                                        is_align_text_left
+                                        name='education_level'
+                                        list={education_level_list}
+                                        value={values.education_level}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.education_level || (touched.education_level && errors.education_level)) &&
+                                    <span className='fa-dropdown__error-message'>
+                                        {errors.education_level}
+                                    </span>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('net annual income')}
+                                        is_align_text_left
+                                        name='net_income'
+                                        list={net_income_list}
+                                        value={values.net_income}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.net_income || (touched.net_income && errors.net_income)) &&
+                                    <span className='fa-dropdown__error-message'>
+                                        {errors.net_income}
+                                    </span>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('estimated net worth')}
+                                        is_alignment_top
+                                        is_align_text_left
+                                        name='estimated_worth'
+                                        list={estimated_worth_list}
+                                        value={values.estimated_worth}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.estimated_worth || (errors.estimated_worth && touched.estimated_worth)) &&
+                                    <span className='fa-dropdown__error-message'>
+                                        {errors.estimated_worth}
+                                    </span>
+                                    }
+                                </fieldset>
+                                <fieldset className='account-management-form-fieldset'>
+                                    <Dropdown
+                                        placeholder={localize('anticipated account turnover')}
+                                        is_alignment_top
+                                        is_align_text_left
+                                        name='account_turnover'
+                                        list={account_turnover_list}
+                                        value={values.account_turnover}
+                                        onChange={handleChange}
+                                    />
+                                    {(errors.account_turnover ||
+                                        (errors.account_turnover && touched.account_turnover)) &&
+                                        <span className='fa-dropdown__error-message'>
+                                            {errors.account_turnover}
+                                        </span>
+                                    }
+                                </fieldset>
+                            </FormBody>
+                        }
                         <FormFooter>
-                            <Button type='submit' disabled={isSubmitting}>
+                            <Button type='submit' disabled={isSubmitting || this.state.is_loading}>
                               Submit
                             </Button>
                         </FormFooter>

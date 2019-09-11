8016b0c0a1d1468865c497776d32ebc5f2982421
oskar-binary <oskar@binary.com>
2019-09-11 10:31:34 +0800

    refactor

--- packages/trader/src/App/Containers/AccountManagementModal/Components/layout-components.jsx
+++ packages/trader/src/App/Containers/AccountManagementModal/Components/layout-components.jsx
@@ -21,28 +21,6 @@ export const FormSubHeader = ({ text }) => (
         <h2 className='account-management-form-header'>{text}</h2>
 );
 
-export const Table = ({ children, className }) => (
-        <table className={classNames('account-management-table', className)}>
-                {children}
-        </table>
-);
-
-export const TableHead = ({ children, className }) => <thead classNames={className}>{children}</thead>
-
-export const TableHeader = ({ children, is_flex }) =>
-        <th
-            className={classNames('account-management-form-header', {
-                'account-management-flex-wrapper': is_flex,
-        })}>{children}</th>
-
-export const TableBody = ({ children }) => <tbody>{children}</tbody>
-
-export const Row = ({ children }) => (
-        <tr style={{ display: 'flex', justifyContent: 'space-between' }}>
-            {children}
-        </tr>
-);
-
 export const TextContainer = ({ children }) => <div className='account-management-text-container'>{children}</div>
 
 export const Text = ({ children, size, color, className }) => (
@@ -52,11 +30,4 @@ export const Text = ({ children, size, color, className }) => (
                 'account-management-text--small': size === 'small',
                 'account-management-text--grey': color === 'grey',
         })}>{children}</p>
-);
-
-export const Td = ({ children, is_flex }) => (
-        <td
-                className={classNames('account-management-text', {
-                'account-management-flex-wrapper': is_flex,
-        })}>{children}</td>
 );
--- packages/trader/src/App/Containers/AccountManagementModal/SecurityAndSafety/account-limits.jsx
+++ packages/trader/src/App/Containers/AccountManagementModal/SecurityAndSafety/account-limits.jsx
@@ -1,11 +1,14 @@
 // import PropTypes        from 'prop-types';
+import classNames       from 'classnames';
 import React            from 'react';
+import { Scrollbars }   from 'tt-react-custom-scrollbars';
 import { localize }     from 'App/i18n';
+import Localize         from 'App/Components/Elements/localize.jsx';
 import { WS }           from 'Services';
 import Loading          from '../../../../templates/app/components/loading.jsx';
 import { addCommaToNumber } from '../../../Components/Elements/PositionsDrawer/helpers/positions-helper';
 import { Popover }     from 'deriv-components';
-import { Table, TableHead, TableHeader, TableBody, Row, TextContainer, Text, Td } from '../Components/layout-components';
+import { TextContainer, Text } from '../Components/layout-components';
 
 const display_decimals = 2;
 const makeTurnoverLimitRow = arr => (
@@ -19,6 +22,25 @@ const makeTurnoverLimitRow = arr => (
             )
         }
     </>
+);
+
+const TableHeader = ({ children, is_flex }) => (
+    <th
+        className={classNames({'account-management-flex-wrapper': is_flex})}
+    >{children}</th>
+);
+
+const Row = ({ children }) => (
+    <tr style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {children}
+    </tr>
+);
+
+const Td = ({ children, is_flex }) => (
+    <td
+        className={classNames('account-management-text', {
+            'account-management-flex-wrapper': is_flex,
+    })}>{children}</td>
 );
 
 class AccountLimits extends React.Component {
@@ -41,123 +63,136 @@ class AccountLimits extends React.Component {
             remainder,
             withdrawal_since_inception_monetary } = this.state;
 
-        if (is_loading) return <Loading />
+        if (is_loading) return <Loading is_fullscreen={false} className='initial-loader--accounts-modal' />
 
         const { commodities, forex, indices, volidx } = market_specific;
 
         return (
             <section className='account-limit-container'>
-                <Text className='account-limit-container__main-text'>
-                    These are default limits that we apply to your accounts. To learn more about trading limits and how they apply, please go to the Help Centre.
-                </Text>
-                <Table>
-                    <TableHead>
-                        <Row>
-                            <TableHeader>{localize('Trading limits - item')}</TableHeader>
-                            <TableHeader>{localize('Limit')}</TableHeader>
-                        </Row>
-                        <tr>
-                            <th/>
-                        </tr>
-                    </TableHead>
-                    <TableBody>
-                        <Row>
-                            <Td is_flex>
-                                {localize('*Maximum number of open positions')}
-                                <Popover
-                                    alignment='top'
-                                    classNameTargetIcon='account-limit-popover'
-                                    classNameTarget='account-limit-popover-target'
-                                    icon='info'
-                                    message={localize('Represents the maximum number of outstanding contracts in your portfolio. Each line in your portfolio counts for one open position. Once the maximum is reached, you will not be able to open new positions without closing an existing position first.')}
-                                    portal_container='modal_root'
-                                />
-                            </Td>
-                            <Td>{ addCommaToNumber(open_positions, 0) }</Td>
-                        </Row>
-                        <Row>
-                            <Td is_flex>
-                                {localize('*Maximum account cash balance')}
-                                <Popover
-                                    alignment='top'
-                                    classNameTargetIcon='account-limit-popover'
-                                    classNameTarget='account-limit-popover-target'
-                                    icon='info'
-                                    message={localize('Represents the maximum amount of cash that you may hold in your account.  If the maximum is reached, you will be asked to withdraw funds.')}
-                                    portal_container='modal_root'
-                                />
-                            </Td>
-                            <Td>{ addCommaToNumber(account_balance, display_decimals) }</Td>
-                        </Row>
-                        <Row>
-                            <Td is_flex>
-                                {localize('Maximum aggregate payouts on open positions')}
-                                <Popover
-                                    alignment='top'
-                                    classNameTargetIcon='account-limit-popover'
-                                    classNameTarget='account-limit-popover-target'
-                                    icon='info'
-                                    message={localize('Presents the maximum aggregate payouts on outstanding contracts in your portfolio. If the maximum is attained, you may not purchase additional contracts without first closing out existing positions.')}
-                                    portal_container='modal_root'
-                                />
-                            </Td>
-                            <Td>{ addCommaToNumber(payout, display_decimals) }</Td>
-                        </Row>
-                    </TableBody>
-                </Table>
-                <TextContainer>
-                    <Text size='xsmall'>{localize('*Any limits in your Self-exclusion settings will override these default limits.')}</Text>
-                </TextContainer>
-                <Table>
-                    <TableHead>
-                        <Row>
-                            <TableHeader is_flex>
-                                {localize('Trading limits - Maximum daily turnover')}
-                                <Popover
-                                    alignment='top'
-                                    classNameTargetIcon='account-limit-popover'
-                                    classNameTarget='account-limit-popover-target'
-                                    icon='info'
-                                    message={localize('Presents the maximum aggregate payouts on outstanding contracts in your portfolio. If the maximum is attained, you may not purchase additional contracts without first closing out existing positions.')}
-                                    margin={0}
-                                    portal_container='modal_root'
-                                />
-                            </TableHeader>
-                            <TableHeader>{localize('Limit')}</TableHeader>
-                        </Row>
-                    </TableHead>
-                    <TableBody>
-                        { makeTurnoverLimitRow(commodities) }
-                        { makeTurnoverLimitRow(forex) }
-                        { makeTurnoverLimitRow(indices) }
-                        { makeTurnoverLimitRow(volidx) }
-                    </TableBody>
-                </Table>
-                <Table className='account-limit-container__withdrawal-table'>
-                    <TableHead>
-                        <Row>
-                            <TableHeader>{localize('Withdrawal limits')}</TableHeader>
-                            <TableHeader>{localize('Limit')}</TableHeader>
-                        </Row>
-                    </TableHead>
-                    <TableBody>
-                        <Row>
-                            <Td>{localize('Total withdrawal allowed')}</Td>
-                            <Td>{addCommaToNumber(num_of_days_limit, display_decimals)}</Td>
-                        </Row>
-                        <Row>
-                            <Td>{localize('Total withdrawn')}</Td>
-                            <Td>{addCommaToNumber(withdrawal_since_inception_monetary, display_decimals)}</Td>
-                        </Row>
-                        <Row>
-                            <Td>{localize('Maximum withdrawal remaining')}</Td>
-                            <Td>{addCommaToNumber(remainder, display_decimals)}</Td>
-                        </Row>
-                    </TableBody>
-                </Table>
-                <TextContainer>
-                    <Text size='small' color='grey'>{localize('Stated limits are subject to change without prior notice.')}</Text>
-                </TextContainer>
+                <Scrollbars>
+                    <Text className='account-limit-container__main-text'>
+                        <Localize
+                            i18n_default_text='These are default limits that we apply to your accounts. To learn more about trading limits and how they apply, please go to the <0>Help Centre</0>.'
+                            components={[
+                                <a key={0} className='link link--orange' rel='noopener noreferrer' target='_blank' href='https://www.deriv.com/help-centre/' />,
+                            ]}
+                        />
+                    </Text>
+                    <table className='account-management-table'>
+                        <thead>
+                            <Row>
+                                <TableHeader>{localize('Trading limits - item')}</TableHeader>
+                                <TableHeader>{localize('Limit')}</TableHeader>
+                            </Row>
+                            <tr className='account-management-table__divider'>
+                                <th />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <Row>
+                                <Td is_flex>
+                                    {localize('*Maximum number of open positions')}
+                                    <Popover
+                                        alignment='top'
+                                        classNameTargetIcon='account-limit-popover'
+                                        classNameTarget='account-limit-popover-target'
+                                        icon='info'
+                                        message={localize('Represents the maximum number of outstanding contracts in your portfolio. Each line in your portfolio counts for one open position. Once the maximum is reached, you will not be able to open new positions without closing an existing position first.')}
+                                        portal_container='modal_root'
+                                    />
+                                </Td>
+                                <Td>{ addCommaToNumber(open_positions, 0) }</Td>
+                            </Row>
+                            <Row>
+                                <Td is_flex>
+                                    {localize('*Maximum account cash balance')}
+                                    <Popover
+                                        alignment='top'
+                                        classNameTargetIcon='account-limit-popover'
+                                        classNameTarget='account-limit-popover-target'
+                                        icon='info'
+                                        message={localize('Represents the maximum amount of cash that you may hold in your account.  If the maximum is reached, you will be asked to withdraw funds.')}
+                                        portal_container='modal_root'
+                                    />
+                                </Td>
+                                <Td>{ addCommaToNumber(account_balance, display_decimals) }</Td>
+                            </Row>
+                            <Row>
+                                <Td is_flex>
+                                    {localize('Maximum aggregate payouts on open positions')}
+                                    <Popover
+                                        alignment='top'
+                                        classNameTargetIcon='account-limit-popover'
+                                        classNameTarget='account-limit-popover-target'
+                                        icon='info'
+                                        message={localize('Presents the maximum aggregate payouts on outstanding contracts in your portfolio. If the maximum is attained, you may not purchase additional contracts without first closing out existing positions.')}
+                                        portal_container='modal_root'
+                                    />
+                                </Td>
+                                <Td>{ addCommaToNumber(payout, display_decimals) }</Td>
+                            </Row>
+                        </tbody>
+                    </table>
+                    <TextContainer>
+                        <Text size='xsmall'>{localize('*Any limits in your Self-exclusion settings will override these default limits.')}</Text>
+                    </TextContainer>
+                    <table className='account-management-table'>
+                        <thead>
+                            <Row>
+                                <TableHeader is_flex>
+                                    {localize('Trading limits - Maximum daily turnover')}
+                                    <Popover
+                                        alignment='top'
+                                        classNameTargetIcon='account-limit-popover'
+                                        classNameTarget='account-limit-popover-target'
+                                        icon='info'
+                                        message={localize('Presents the maximum aggregate payouts on outstanding contracts in your portfolio. If the maximum is attained, you may not purchase additional contracts without first closing out existing positions.')}
+                                        margin={0}
+                                        portal_container='modal_root'
+                                    />
+                                </TableHeader>
+                                <TableHeader>{localize('Limit')}</TableHeader>
+                            </Row>
+                            <tr className='account-management-table__divider'>
+                                <th />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { makeTurnoverLimitRow(commodities) }
+                            { makeTurnoverLimitRow(forex) }
+                            { makeTurnoverLimitRow(indices) }
+                            { makeTurnoverLimitRow(volidx) }
+                        </tbody>
+                    </table>
+                    <table className='account-management-table account-management-table--last'>
+                        <thead>
+                            <Row>
+                                <TableHeader>{localize('Withdrawal limits')}</TableHeader>
+                                <TableHeader>{localize('Limit')}</TableHeader>
+                            </Row>
+                            <tr  className='account-management-table__divider'>
+                                <th />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <Row>
+                                <Td>{localize('Total withdrawal allowed')}</Td>
+                                <Td>{addCommaToNumber(num_of_days_limit, display_decimals)}</Td>
+                            </Row>
+                            <Row>
+                                <Td>{localize('Total withdrawn')}</Td>
+                                <Td>{addCommaToNumber(withdrawal_since_inception_monetary, display_decimals)}</Td>
+                            </Row>
+                            <Row>
+                                <Td>{localize('Maximum withdrawal remaining')}</Td>
+                                <Td>{addCommaToNumber(remainder, display_decimals)}</Td>
+                            </Row>
+                        </tbody>
+                    </table>
+                    <TextContainer>
+                        <Text size='small' color='grey'>{localize('Stated limits are subject to change without prior notice.')}</Text>
+                    </TextContainer>
+                </Scrollbars>
             </section>
         );
     }

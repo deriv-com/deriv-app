ff965df4facd9f89efdc8da5263a14f1f7761757
amin roosta <amin.roosta@outlook.com>
2019-09-12 17:34:26 +0800

    update the message

--- packages/trader/src/Stores/Helpers/client-notifications.js
+++ packages/trader/src/Stores/Helpers/client-notifications.js
@@ -156,10 +156,10 @@ export const clientNotifications = {
     },
     switch_to_tick_chart: {
         key    : 'switch_to_tick_chart',
-        header : localize('This chart duration is not ideal for tick contracts'),
+        header : localize('This chart display is not ideal for tick contracts'),
         message: (
             <Localize
-                i18n_default_text='Please change the chart duration to tick.'
+                i18n_default_text='Please change the chart duration to tick for a better trading experience.'
             />
         ),
         type: 'info',

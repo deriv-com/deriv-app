1c5f6557753b4775749255e178c09583a8e008cd
Mahdi <m.pourziaei@gmail.com>
2019-09-13 16:47:35 +0800

    + Added MT5 dashboard icons + Added download center section to MT5Dashboard [WIP] + Updated `mt5-dashboard` stylesheet [WIP]

--- packages/trader/src/Modules/MT5/Containers/mt5-dashboard.jsx
+++ packages/trader/src/Modules/MT5/Containers/mt5-dashboard.jsx
@@ -1,9 +1,34 @@
-import { Button } from 'deriv-components';
-import React      from 'react';
-import Localize   from 'App/Components/Elements/localize.jsx';
+import { Button }              from 'deriv-components';
+import React                   from 'react';
+import Localize                from 'App/Components/Elements/localize.jsx';
+import { Tabs }                from 'App/Components/Elements/Tabs';
+import { localize }            from 'App/i18n';
+import IconDeviceLaptop        from 'Assets/SvgComponents/mt5/icon-device-laptop.svg';
+import IconDeviceMac           from 'Assets/SvgComponents/mt5/icon-device-mac.svg';
+import IconDeviceMobile        from 'Assets/SvgComponents/mt5/icon-device-mobile.svg';
+import IconInstallationApple   from 'Assets/SvgComponents/mt5/icon-installation-apple.svg';
+import IconInstallationGoogle  from 'Assets/SvgComponents/mt5/icon-installation-google.svg';
+import IconInstallationLinux   from 'Assets/SvgComponents/mt5/icon-installation-linux.svg';
+import IconInstallationMac     from 'Assets/SvgComponents/mt5/icon-installation-mac.svg';
+import IconInstallationWindows from 'Assets/SvgComponents/mt5/icon-installation-windows.svg';
 import 'Sass/app/modules/mt5-dashboard.scss';
 
+const RealDisplay = () => (
+    <p>Hi I am in REAL</p>
+);
+
+const VirtualDisplay = () => (
+    <p>Hi I am in Virtual</p>
+);
+
 class MT5Dashboard extends React.Component {
+    static get dashboard_tabs() {
+        return {
+            1: { header: localize('Real account'), content: RealDisplay },
+            2: { header: localize('Virtual account'), content: VirtualDisplay },
+        };
+    }
+
     render() {
         const { location } = this.props;
 
@@ -28,6 +53,39 @@ class MT5Dashboard extends React.Component {
 
                 <div className='mt5-dashboard__accounts-display'>
                     {/* TODO: Add MT5 accounts display component */}
+                    <Tabs alignment='center' list={ MT5Dashboard.dashboard_tabs } />
+                </div>
+
+                <div className='mt5-dashboard__download-center'>
+                    <h1 className='mt5-dashboard__welcome-message--heading'>
+                        <Localize i18n_default_text='After creating your account, download MT5 for your desktop or mobile' />
+                    </h1>
+
+                    <div className='mt5-dashboard__download-center-options'>
+                        <div className='mt5-dashboard__download-center-options--desktop'>
+                            <div>
+                                <IconDeviceMac />
+                                <IconDeviceLaptop />
+                            </div>
+                            <div>
+                                <IconInstallationWindows />
+                                <IconInstallationMac />
+                                <IconInstallationLinux />
+                            </div>
+                        </div>
+                        <div className='mt5-dashboard__download-center-options--mobile'>
+                            <div>
+                                <IconDeviceMobile />
+                            </div>
+                            <div>
+                                <IconInstallationApple />
+                                <IconInstallationGoogle />
+                            </div>
+                        </div>
+                    </div>
+                    <p className='mt5-dashboard__welcome-message--hint'>
+                        <Localize i18n_default_text='The MT5 platform does not support Windows XP, Windows 2003 and Windows Vista.' />
+                    </p>
                 </div>
             </div>
         );

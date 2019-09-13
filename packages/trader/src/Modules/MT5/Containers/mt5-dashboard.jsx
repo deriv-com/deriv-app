6f7a765587a9629c25bd9895e3ae83ea1a27e511
Mahdi <m.pourziaei@gmail.com>
2019-09-13 13:35:42 +0800

    Added MT5 Dashboard's welcome message section

--- packages/trader/src/Modules/MT5/Containers/mt5-dashboard.jsx
+++ packages/trader/src/Modules/MT5/Containers/mt5-dashboard.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { Button } from 'deriv-components';
+import React      from 'react';
+import Localize   from 'App/Components/Elements/localize.jsx';
 import 'Sass/app/modules/mt5-dashboard.scss';
 
 class MT5Dashboard extends React.Component {
@@ -7,7 +9,26 @@ class MT5Dashboard extends React.Component {
 
         return (
             <div className='mt5-dashboard'>
-                Dashboard loads at { location.pathname }!
+
+                <div className='mt5-dashboard__welcome-message'>
+                    <h1 className='mt5-dashboard__welcome-message--heading'>
+                        <Localize i18n_default_text='Welcome to Deriv MetaTrader 5 (MT5)' />
+                    </h1>
+                    <div className='mt5-dashboard__welcome-message--content'>
+                        <p className='mt5-dashboard__welcome-message--paragraph'>
+                            <Localize i18n_default_text='MetaTrader 5 (MT5) is a popular online trading platform for forex and stock markets. Get prices and currency quotes, perform analysis using charts and technical indicators, and easily view your trading history.' />
+                        </p>
+                        <Button className='mt5-dashboard__welcome-message--button' type='button'>
+                            <p className='mt5-dashboard__welcome-message--paragraph'>
+                                <Localize i18n_default_text='Learn more' />
+                            </p>
+                        </Button>
+                    </div>
+                </div>
+
+                <div className='mt5-dashboard__accounts-display'>
+                    {/* TODO: Add MT5 accounts display component */}
+                </div>
             </div>
         );
     }

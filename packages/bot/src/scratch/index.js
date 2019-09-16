f0efd8593fe69eb6a05aa8868d0697a5ce1235ae
Anton <anton@binary.com>
2019-09-16 12:27:42 +0800

    reverted grid color

--- packages/bot/src/scratch/index.js
+++ packages/bot/src/scratch/index.js
@@ -34,7 +34,7 @@ export const scratchWorkspaceInit = async (scratch_area_name, scratch_div_name) 
         const main_xml = await fetch(`${__webpack_public_path__}xml/main.xml`).then(response => response.text());
 
         const workspace = Blockly.inject(el_scratch_div, {
-            grid    : { spacing: 40, length: 11, colour: '#f3f3f3' },
+            grid    : { spacing: 40, length: 11, colour: '#ebebed' },
             media   : `${__webpack_public_path__}media/`, // eslint-disable-line
             toolbox : toolbox_xml,
             trashcan: true,

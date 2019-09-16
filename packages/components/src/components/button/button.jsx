813677b7fcac7ef220357c31f86f2df9feaf1324
zakibinary <zaki@binary.com>
2019-09-16 15:26:21 +0800

    fix eslint and stylelint errors

--- packages/components/src/components/button/button.jsx
+++ packages/components/src/components/button/button.jsx
@@ -3,11 +3,11 @@ import PropTypes  from 'prop-types';
 import React      from 'react';
 
 // TODO: use Icons from components
-const IconCheckmark = (props) => (
+const IconCheckmark = () => (
     <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
         <g fill='none' fillRule='evenodd'>
-            <path d='M0 0h24v24H0z'/>
-            <path fill='#FFF' fillRule='nonzero' d='M9 16.2l-3.5-3.5a.984.984 0 0 0-1.4 0 .984.984 0 0 0 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7a.984.984 0 0 0 0-1.4.984.984 0 0 0-1.4 0L9 16.2z'/>
+            <path d='M0 0h24v24H0z' />
+            <path fill='#FFF' fillRule='nonzero' d='M9 16.2l-3.5-3.5a.984.984 0 0 0-1.4 0 .984.984 0 0 0 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7a.984.984 0 0 0 0-1.4.984.984 0 0 0-1.4 0L9 16.2z' />
         </g>
     </svg>
 );

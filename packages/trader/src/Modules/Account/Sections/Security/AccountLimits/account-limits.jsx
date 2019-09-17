b46251b559c3dab91addb71a9d11980cab1a2bc8
oskar-binary <oskar@binary.com>
2019-09-17 16:49:32 +0800

    fix popover text and open positions

--- packages/trader/src/Modules/Account/Sections/Security/AccountLimits/account-limits.jsx
+++ packages/trader/src/Modules/Account/Sections/Security/AccountLimits/account-limits.jsx
@@ -120,7 +120,7 @@ class AccountLimits extends React.Component {
                                         message={localize('Represents the maximum number of outstanding contracts in your portfolio. Each line in your portfolio counts for one open position. Once the maximum is reached, you will not be able to open new positions without closing an existing position first.')}
                                     />
                                 </Td>
-                                <Td>{ formatMoney(currency, open_positions, true) }</Td>
+                                <Td>{ open_positions }</Td>
                             </Row>
                             <Row>
                                 <Td is_flex>
@@ -163,7 +163,7 @@ class AccountLimits extends React.Component {
                                         classNameTargetIcon='account-limit-popover'
                                         classNameTarget='account-limit-popover-target'
                                         icon='info'
-                                        message={localize('Presents the maximum aggregate payouts on outstanding contracts in your portfolio. If the maximum is attained, you may not purchase additional contracts without first closing out existing positions.')}
+                                        message={localize('Represents the maximum volume of contracts that you may purchase in any given trading day.')}
                                         margin={0}
                                     />
                                 </TableHeader>

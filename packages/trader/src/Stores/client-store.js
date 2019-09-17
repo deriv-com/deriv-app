52055de7ccc16c32dbda42c65cb96c8bddbb18f9
mohammad-binary <mohammad@binary.com>
2019-09-17 13:51:27 +0800

    Remove forced

--- packages/trader/src/Stores/client-store.js
+++ packages/trader/src/Stores/client-store.js
@@ -466,7 +466,7 @@ export default class ClientStore extends BaseStore {
         this.upgrade_info = undefined;
         this.accounts = {};
         runInAction(async () => {
-            this.responsePayoutCurrencies(await WS.payoutCurrencies({ forced: true }));
+            this.responsePayoutCurrencies(await WS.payoutCurrencies());
         });
         this.root_store.modules.smart_chart.should_refresh_active_symbols = true;
         this.root_store.modules.trade.clearContracts();

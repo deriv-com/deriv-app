ca3857df39a1aaccfb84bedf8af8d1d9edca511c
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-09-17 11:56:36 +0800

    Clean up

--- packages/components/src/components/tabs/tab.jsx
+++ packages/components/src/components/tabs/tab.jsx
@@ -1,6 +1,6 @@
-import classNames from 'classnames';
+import classNames           from 'classnames';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes            from 'prop-types';
 
 class Tab extends Component {
     onClick = () => {

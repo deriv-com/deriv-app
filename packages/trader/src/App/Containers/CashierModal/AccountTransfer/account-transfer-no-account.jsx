5bccb5c890215a0089066dd9e9a37a5459f14570
negar <negar@binary.com>
2019-09-18 16:51:41 +0430

    add space before bracket

--- packages/trader/src/App/Containers/CashierModal/AccountTransfer/account-transfer-no-account.jsx
+++ packages/trader/src/App/Containers/CashierModal/AccountTransfer/account-transfer-no-account.jsx
@@ -8,7 +8,7 @@ class AccountTransferNoAccount extends React.Component {
             <div className='cashier__wrapper account-transfer__no-balance'>
                 <Icon icon='IconNoBalance' className='account-transfer__no-balance-icon' />
                 <h2 className='withdraw__header'><Localize i18n_default_text='You need at least two accounts' /></h2>
-                <p className='cashier__text'><Localize i18n_default_text='Please create a second account(Deriv or DMT5) to enable fund transfers.' /></p>
+                <p className='cashier__text'><Localize i18n_default_text='Please create a second account (Deriv or DMT5) to enable fund transfers.' /></p>
                 {/* TODO: add account opening CTA and MT5 account opening CTA once ready */}
             </div>
         );

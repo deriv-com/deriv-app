647aa955317ef92e325b8431410baacdbb19831c
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-19 13:40:50 +0800

    clean up some code, remove animations classes

--- packages/trader/src/App/Components/Layout/Header/platform-switcher.jsx
+++ packages/trader/src/App/Components/Layout/Header/platform-switcher.jsx
@@ -1,19 +1,8 @@
 import PropTypes        from 'prop-types';
 import React            from 'react';
-import posed            from 'react-pose';
 import { PlatformList } from './platform-list.jsx';
 
-const FadeInFromTop = posed.div({
-    popped: {
-        x         : -10,
-        y         : -10,
-        background: 'rgba(161, 0, 246, 1)',
-        boxShadow : '10px 10px 20px rgba(161, 0, 246, 0.2)',
-        transition: { duration: 700 },
-    },
-});
-
-class PlatformSwitcher extends React.Component {
+class PlatformSwitcher extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = { is_open: false };
@@ -21,19 +10,15 @@ class PlatformSwitcher extends React.Component {
 
     handleClick = () => this.setState({ is_open: !this.state.is_open });
 
-    render() {
-        return (
-            <React.Fragment>
-                <button onClick={this.handleClick} />
+    render = () => (
+        <React.Fragment>
+            <button onClick={this.handleClick} />
 
-                { this.state.is_open &&
-                <FadeInFromTop pose='popped'>
-                    <PlatformList platform_config={this.props.platform_config} handleClick={this.handleClick} />
-                </FadeInFromTop>
-                }
-            </React.Fragment>
-        );
-    }
+            { this.state.is_open &&
+                <PlatformList platform_config={this.props.platform_config} handleClick={this.handleClick} />
+            }
+        </React.Fragment>
+    );
 }
 
 PlatformSwitcher.propTypes = {

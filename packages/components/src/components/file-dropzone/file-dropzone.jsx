334dac53059723e2f3c1ba07e41c2953d1442b8e
zakibinary <zaki@binary.com>
2019-09-19 14:31:22 +0800

    added file dropzone component to components, added file dropzone to proof address

---
+++ packages/components/src/components/file-dropzone/file-dropzone.jsx
@@ -0,0 +1,94 @@
+import classNames        from 'classnames';
+import React             from 'react';
+import { CSSTransition } from 'react-transition-group';
+import Dropzone          from 'react-dropzone';
+import                        './file-dropzone.scss';
+
+const FadeInMessage = ({ is_visible, classes, children, key, timeout }) => (
+    <CSSTransition
+        appear
+        key={key}
+        in={is_visible}
+        timeout={timeout}
+        classNames={classes}
+        unmountOnExit
+    >
+        {children}
+    </CSSTransition>
+);
+
+// TODO: Clean up unnecessary transition class states
+const message_classnames = {
+    appear     : 'dc-file-dropzone__message--enter',
+    enter      : 'dc-file-dropzone__message--enter',
+    enterActive: 'dc-file-dropzone__message--enter-active',
+    enterDone  : 'dc-file-dropzone__message--enter-done',
+    exit       : 'dc-file-dropzone__message--exit',
+    exitActive : 'dc-file-dropzone__message--exit-active',
+};
+
+const FileDropzone = ({ className, ...props }) => (
+    <Dropzone
+        onDrop={props.onDrop}
+        multiple={props.multiple}
+        accept={props.accept}
+        minSize={props.min_size}
+        maxSize={props.max_size}
+    >
+        {({ getRootProps, getInputProps, isDragActive, isDragReject, acceptedFiles }) => {
+            return (
+                <div
+                    {...getRootProps()}
+                    className={classNames('dc-file-dropzone', className, {
+                        'dc-file-dropzone--is-active': isDragActive,
+                        'dc-file-dropzone--has-file' : (acceptedFiles > 0),
+                    })}
+                >
+                    <input {...getInputProps()} />
+                    <div className='dc-file-dropzone__content'>
+                        <FadeInMessage
+                            classes={message_classnames}
+                            // default message when not on hover or onDrag
+                            is_visible={(!isDragActive && props.message && (acceptedFiles.length < 1))}
+                            timeout={150}
+                        >
+                            <div className='dc-file-dropzone__message'>
+                                {props.message}
+                            </div>
+                        </FadeInMessage>
+                        <FadeInMessage
+                            classes={message_classnames}
+                            // message shown on hover if files are accepted onDrag
+                            is_visible={(isDragActive && !isDragReject)}
+                            timeout={150}
+                        >
+                            <div className='dc-file-dropzone__message'>
+                                {props.hover_message}
+                            </div>
+                        </FadeInMessage>
+                        <FadeInMessage
+                            classes={message_classnames}
+                            // message shown on hover if files are rejected onDrag
+                            is_visible={isDragReject}
+                            timeout={150}
+                        >
+                            <div className='dc-file-dropzone__message'>
+                                {props.error_message}
+                            </div>
+                        </FadeInMessage>
+                        {props.multiple && (acceptedFiles > 0) ?
+                            acceptedFiles.map((item, idx) =>
+                                <span key={idx} className='dc-file-dropzone__filename'>{item.name}</span>
+                            )
+                            :
+                            acceptedFiles[0] && (!isDragReject || !isDragActive) &&
+                                <span className='dc-file-dropzone__filename'>{acceptedFiles[0].name}</span>
+                        }
+                    </div>
+                </div>
+            );
+        }}
+    </Dropzone>
+);
+
+export default FileDropzone;
--- packages/trader/src/Modules/Account/Sections/Verification/ProofOfAddress/proof-of-address.jsx
+++ packages/trader/src/Modules/Account/Sections/Verification/ProofOfAddress/proof-of-address.jsx
@@ -1,14 +1,83 @@
 // import PropTypes        from 'prop-types';
-import React            from 'react';
-import { connect }      from 'Stores/connect';
-import DemoMessage      from '../../ErrorMessages/DemoMessage';
+import React                  from 'react';
+import {
+    Button,
+    FileDropzone }            from 'deriv-components';
+import { connect }            from 'Stores/connect';
+import { localize }           from 'App/i18n';
+import { WS }                 from 'Services';
+import {
+    FormFooter,
+    FormBody,
+    FormSubHeader }           from '../../../Components/layout-components.jsx';
+import Loading                from '../../../../../templates/app/components/loading.jsx';
+import FormSubmitErrorMessage from '../../ErrorMessages/FormSubmitErrorMessage';
+import LoadErrorMessage       from '../../ErrorMessages/LoadErrorMessage';
+import DemoMessage            from '../../ErrorMessages/DemoMessage';
+import { LeaveConfirm }       from '../../../Components/leave-confirm.jsx';
 
 class ProofOfAddress extends React.Component {
+    state = { is_loading: true, show_form: true }
+
+    showForm = show_form => this.setState({ show_form });
+
+    onSubmit = () => {
+    }
+
     render() {
+        if (this.state.api_initial_load_error) {
+            return <LoadErrorMessage error_message={this.state.api_initial_load_error} />;
+        }
         if (this.props.is_virtual) return <DemoMessage />;
+        if (this.state.is_loading) return <Loading is_fullscreen={false} className='account___intial-loader' />;
         return (
-            <h1>Proof of Address</h1>
+            <>
+                <LeaveConfirm onDirty={this.showForm} />
+                {this.state.show_form && (
+                    <form className='account-form' onSubmit={this.onSubmit}>
+                        <FormBody scroll_offset='80px'>
+                            <FormSubHeader title={localize('Details')} />
+                            <FormSubHeader title={localize('Please upload one of the following:')} />
+                            <FileDropzone
+                                accept='image/png, image/jpeg, image/jpg, image/gif, application/pdf'
+                                max_size={8e+6}
+                                multiple={false}
+                                message={localize('Drop file (JPEG  JPG  PNG  PDF  GIF) or click here to upload')}
+                                hover_message={localize('Drop files here..')}
+                                error_message={localize('Please choose only one supported file type')}
+                            />
+                        </FormBody>
+                        <FormFooter>
+                            {status && status.msg && <FormSubmitErrorMessage message={status.msg} />}
+                            <Button
+                                className='account-form__footer-btn btn--primary'
+                                type='submit'
+                                has_effect
+                                text={localize('Save and submit')}
+                            />
+                        </FormFooter>
+                    </form>
+                )}
+            </>
         );
+    }
+
+    componentDidMount() {
+        this.props.fetchResidenceList();
+        WS.authorized.storage.getSettings().then((data) => {
+            if (data.error) {
+                this.setState({ api_initial_load_error: data.error.message });
+                return;
+            }
+            this.setState({ ...data.get_settings, is_loading: false });
+        });
+        WS.authorized.storage.getAccountStatus().then((data) => {
+            if (data.get_account_status.status &&
+                data.get_account_status.status.some(state => state === 'authenticated')
+            ) {
+                this.setState({ is_account_authenticated: true });
+            }
+        });
     }
 }
 
@@ -16,6 +85,8 @@ class ProofOfAddress extends React.Component {
 
 export default connect(
     ({ client }) => ({
-        is_virtual: client.is_virtual,
+        is_virtual        : client.is_virtual,
+        residence_list    : client.residence_list,
+        fetchResidenceList: client.fetchResidenceList,
     }),
 )(ProofOfAddress);

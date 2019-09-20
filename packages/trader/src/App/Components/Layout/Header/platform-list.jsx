afd7619c2a6d24769dd988c0858227af0392473e
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-20 16:30:42 +0800

    add trigger styles and remove react-pose animations

--- packages/trader/src/App/Components/Layout/Header/platform-list.jsx
+++ packages/trader/src/App/Components/Layout/Header/platform-list.jsx
@@ -1,56 +1,26 @@
 import PropTypes      from 'prop-types';
 import React          from 'react';
-import ReactDOM       from 'react-dom';
-import posed, { PoseGroup }        from 'react-pose';
 import { BinaryLink } from 'App/Components/Routes';
 import Icon           from 'Assets/icon.jsx';
 import 'Sass/app/_common/components/platform-list.scss';
 
-const Container = posed.div({
-    enter: {
-        opacity   : 1,
-        transition: { duration: 200 },
-    },
-    exit: {
-        opacity   : 0,
-        transition: { duration: 200 },
-    },
-});
-
-const SlideFromTop = posed.div({
-    enter: {
-        top       : 48,
-        transition: { duration: 200 },
-    },
-    exit: {
-        top       : 0,
-        transition: { duration: 200 },
-    },
-});
-
 const PlatformList = ({
     platform_config,
     handleClick,
-    is_open,
-}) => ReactDOM.createPortal(
-    <PoseGroup>
-        { is_open &&
-            <Container key='container' className='platform_switcher__container' onClick={handleClick}>
-                <SlideFromTop key='slide_from_top' initialPose='exit' className='platform_switcher__list'>
-                    {platform_config.map((platform, idx) => (
-                        <BinaryLink to={platform.link_to} href={platform.href} key={idx} onClick={handleClick} className='platform_switcher__list__platform'>
-                            <Icon className='platform_switcher__list__platform__icon' icon={platform.icon} />
-                            <div className='platform_switcher__list__platform__details'>
-                                <p className='platform_switcher__list__platform__title'>{platform.title}</p>
-                                <p className='platform_switcher__list__platform__description'>{platform.description}</p>
-                            </div>
-                        </BinaryLink>
-                    ))}
-                </SlideFromTop>
-            </Container>
-        }
-    </PoseGroup>,
-    document.getElementById('deriv_app')
+}) => (
+    <div className='platform_switcher__container' onClick={handleClick}>
+        <div className='platform_switcher__list'>
+            {platform_config.map((platform, idx) => (
+                <BinaryLink to={platform.link_to} href={platform.href} key={idx} onClick={handleClick} className='platform_switcher__list__platform'>
+                    <Icon className='platform_switcher__list__platform__icon' icon={platform.icon} />
+                    <div className='platform_switcher__list__platform__details'>
+                        <p className='platform_switcher__list__platform__title'>{platform.title}</p>
+                        <p className='platform_switcher__list__platform__description'>{platform.description}</p>
+                    </div>
+                </BinaryLink>
+            ))}
+        </div>
+    </div>
 );
 
 PlatformList.propTypes = {
--- packages/trader/src/App/Components/Layout/Header/platform-switcher.jsx
+++ packages/trader/src/App/Components/Layout/Header/platform-switcher.jsx
@@ -1,6 +1,8 @@
 import PropTypes        from 'prop-types';
 import React            from 'react';
+import Icon             from 'Assets/icon.jsx';
 import { PlatformList } from './platform-list.jsx';
+import 'Sass/app/_common/components/platform-switcher.scss';
 
 class PlatformSwitcher extends React.PureComponent {
     constructor(props) {
@@ -12,12 +14,19 @@ class PlatformSwitcher extends React.PureComponent {
 
     render = () => (
         <React.Fragment>
-            <button onClick={this.handleClick} />
-            <PlatformList
-                platform_config={this.props.platform_config}
-                handleClick={this.handleClick}
-                is_open={this.state.is_open}
-            />
+            <div className='platform_switcher' onClick={this.handleClick}>
+                <Icon icon='IconMT5' className='platform_switcher__icon' />
+                <p className='platform_switcher__header'>DTrader</p>
+                <p className='platform_switcher__label'>BETA</p>
+                <Icon icon='IconArrowBold' />
+            </div>
+            { this.state.is_open &&
+                <PlatformList
+                    platform_config={this.props.platform_config}
+                    handleClick={this.handleClick}
+                    is_open={this.state.is_open}
+                />
+            }
         </React.Fragment>
     );
 }

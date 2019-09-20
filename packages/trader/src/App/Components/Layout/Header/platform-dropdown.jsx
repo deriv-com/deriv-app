06eab04746d5728d7ddd2d5875e487cf94d98c39
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-20 18:06:41 +0800

    update animations to use react-transition-group's CSSTransition

---
+++ packages/trader/src/App/Components/Layout/Header/platform-dropdown.jsx
@@ -0,0 +1,39 @@
+import PropTypes         from 'prop-types';
+import React             from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { BinaryLink }    from 'App/Components/Routes';
+import Icon              from 'Assets/icon.jsx';
+import 'Sass/app/_common/components/platform-dropdown.scss';
+
+const PlatformDropdown = ({
+    platform_config,
+    handleClick,
+    is_open,
+}) => (
+    <CSSTransition
+        mountOnEnter
+        in={is_open}
+        classNames='platform_dropdown'
+        unmountOnExit
+    >
+        <div className='platform_dropdown' onClick={handleClick}>
+            <div className='platform_dropdown__list'>
+                {platform_config.map((platform, idx) => (
+                    <BinaryLink to={platform.link_to} href={platform.href} key={idx} onClick={handleClick} className='platform_dropdown__list__platform'>
+                        <Icon className='platform_dropdown__list__platform__icon' icon={platform.icon} />
+                        <div className='platform_dropdown__list__platform__details'>
+                            <p className='platform_dropdown__list__platform__title'>{platform.title}</p>
+                            <p className='platform_dropdown__list__platform__description'>{platform.description}</p>
+                        </div>
+                    </BinaryLink>
+                ))}
+            </div>
+        </div>
+    </CSSTransition>
+);
+
+PlatformDropdown.propTypes = {
+    platform_configs: PropTypes.array,
+};
+
+export { PlatformDropdown };
--- packages/trader/src/App/Components/Layout/Header/platform-switcher.jsx
+++ packages/trader/src/App/Components/Layout/Header/platform-switcher.jsx
@@ -1,7 +1,7 @@
-import PropTypes        from 'prop-types';
-import React            from 'react';
-import Icon             from 'Assets/icon.jsx';
-import { PlatformList } from './platform-list.jsx';
+import PropTypes            from 'prop-types';
+import React                from 'react';
+import Icon                 from 'Assets/icon.jsx';
+import { PlatformDropdown } from './platform-dropdown.jsx';
 import 'Sass/app/_common/components/platform-switcher.scss';
 
 class PlatformSwitcher extends React.PureComponent {
@@ -18,15 +18,13 @@ class PlatformSwitcher extends React.PureComponent {
                 <Icon icon='IconMT5' className='platform_switcher__icon' />
                 <p className='platform_switcher__header'>DTrader</p>
                 <p className='platform_switcher__label'>BETA</p>
-                <Icon icon='IconArrowBold' />
+                <Icon icon='IconArrowBold' className='platform_switcher__arrow' />
             </div>
-            { this.state.is_open &&
-                <PlatformList
-                    platform_config={this.props.platform_config}
-                    handleClick={this.handleClick}
-                    is_open={this.state.is_open}
-                />
-            }
+            <PlatformDropdown
+                platform_config={this.props.platform_config}
+                handleClick={this.handleClick}
+                is_open={this.state.is_open}
+            />
         </React.Fragment>
     );
 }

046aa3dd20d1581f22e30bdd883191989e36e993
Aaron Imming <aaim@pm.me>
2019-09-20 17:54:10 +0800

    Add summary + contract_card stores

--- packages/bot/src/stores/index.js
+++ packages/bot/src/stores/index.js
@@ -1,11 +1,15 @@
-import FlyoutStore  from './flyout-store';
-import ScratchStore from './scratch-store';
+import FlyoutStore       from './flyout-store';
+import ScratchStore      from './scratch-store';
+import SummaryStore      from './summary-store';
+import ContractCardStore from './contract-card-store';
 
 export default class RootStore {
     constructor(core, ws) {
-        this.core = core;
-        this.ws = ws;
-        this.flyout = new FlyoutStore();
+        this.core          = core;
+        this.ws            = ws;
+        this.flyout        = new FlyoutStore();
+        this.summary       = new SummaryStore(this);
+        this.contract_card = new ContractCardStore(this);
 
         // Create a singleton class to share rootStore with scratch
         ScratchStore.setInstance(this);

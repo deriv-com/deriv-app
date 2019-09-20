58a6ae36299d77a5f477bf7e3933d270d391d26a
Kelly Anyi <kelly@binary.com>
2019-09-20 12:36:12 +0800

    fix layout

--- packages/trader/src/Modules/Account/Sections/Security/DerivPassword/forgot-your-password.jsx
+++ packages/trader/src/Modules/Account/Sections/Security/DerivPassword/forgot-your-password.jsx
@@ -5,30 +5,33 @@ import IconSpamEmail     from 'Assets/AccountManagement/icon-spam-email.svg';
 import IconTypo          from 'Assets/AccountManagement/icon-typo.svg';
 import IconWorkEmail     from 'Assets/AccountManagement/icon-work-email.svg';
 import SendEmailTemplate from 'App/Containers/Layout/send-email.jsx';
+import { ScrollbarsContainer } from '../../../Components/layout-components.jsx';
 
 const ForgotYourPassword = ({ onClickSendEmail }) => (
-    <SendEmailTemplate
-        className='forgot-password'
-        subtitle={'Please click on the link in the email to reset your password.'}
-        onClickSendEmail={onClickSendEmail}
-    >
-        <div className='forgot-password__content'>
-            <IconSpamEmail />
-            <p>{localize('The email is in your spam folder (Sometimes things get lost there).')}</p>
-        </div>
-        <div className='forgot-password__content'>
-            <IconTypo />
-            <p>{localize('The email address you entered had a mistake or typo (happens to the best of us).')}</p>
-        </div>
-        <div className='forgot-password__content'>
-            <IconWorkEmail />
-            <p>{localize('You accidentally gave us another email address (Usually a work or a personal one instead of the one you meant).')}</p>
-        </div>
-        <div className='forgot-password__content'>
-            <IconFirewall />
-            <p>{localize('We can’t deliver the email to this address (Usually because of firewalls or filtering).')}</p>
-        </div>
-    </SendEmailTemplate>
+    <ScrollbarsContainer>
+        <SendEmailTemplate
+            className='forgot-password'
+            subtitle={'Please click on the link in the email to reset your password.'}
+            onClickSendEmail={onClickSendEmail}
+        >
+            <div className='forgot-password__content'>
+                <IconSpamEmail />
+                <p>{localize('The email is in your spam folder (Sometimes things get lost there).')}</p>
+            </div>
+            <div className='forgot-password__content'>
+                <IconTypo />
+                <p>{localize('The email address you entered had a mistake or typo (happens to the best of us).')}</p>
+            </div>
+            <div className='forgot-password__content'>
+                <IconWorkEmail />
+                <p>{localize('You accidentally gave us another email address (Usually a work or a personal one instead of the one you meant).')}</p>
+            </div>
+            <div className='forgot-password__content'>
+                <IconFirewall />
+                <p>{localize('We can’t deliver the email to this address (Usually because of firewalls or filtering).')}</p>
+            </div>
+        </SendEmailTemplate>
+    </ScrollbarsContainer>
 );
 
 export default ForgotYourPassword;

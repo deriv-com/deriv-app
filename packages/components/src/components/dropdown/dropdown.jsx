d2ac7b21a12d9e9858b02010dd79a9196ea97d3d
negar <negar@binary.com>
2019-09-22 13:30:18 +0330

    update comment

--- packages/components/src/components/dropdown/dropdown.jsx
+++ packages/components/src/components/dropdown/dropdown.jsx
@@ -50,7 +50,7 @@ class Dropdown extends React.PureComponent {
     get is_single_option() {
         return Array.isArray(this.props.list) ?
             (this.props.list.length < 2) :
-            // object has less than one prop or inner object has less than one prop
+            // object has less than two props or inner object has less than two props
             (Object.keys(this.props.list).length < 2 && this.props.list[Object.keys(this.props.list)[0]].length < 2);
     }
 

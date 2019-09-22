ad1eb61c134c29127f18434b979c9773ecde5b55
Aaron Imming <aaim@pm.me>
2019-09-22 16:50:52 +0800

    Update loader shape

--- packages/bot/src/components/contract-card-loading.jsx
+++ packages/bot/src/components/contract-card-loading.jsx
@@ -1,5 +1,5 @@
-import React         from 'react';
-import ContentLoader from 'react-content-loader';
+import React             from 'react';
+import ContentLoader     from 'react-content-loader';
 
 const ContractCardLoader = () => (
     <ContentLoader
@@ -9,20 +9,21 @@ const ContractCardLoader = () => (
         primaryColor={'#f4f4f6'}
         secondaryColor={'#eaeaec'}
     >
-        <rect x='12' y='10' rx='0' ry='0' width='32' height='32' />
-        <rect x='52' y='22' rx='0' ry='0' width='70' height='8' />
-        <rect x='134' y='10' rx='0' ry='0' width='32' height='32' />
-        <rect x='176' y='22' rx='0' ry='0' width='70' height='8' />
+        <rect x='12' y='15' rx='0' ry='0' width='25' height='25' />
+        <rect x='45' y='24' rx='0' ry='0' width='70' height='8' />
+        <rect x='154' y='15' rx='0' ry='0' width='25' height='25' />
+        <rect x='187' y='24' rx='0' ry='0' width='70' height='8' />
         <rect x='12' y='57' rx='0' ry='0' width='308' height='1' />
-        <rect x='50' y='75' rx='0' ry='0' width='60' height='8' />
-        <rect x='50' y='90' rx='0' ry='0' width='100' height='8' />
-        <rect x='50' y='115' rx='0' ry='0' width='60' height='8' />
-        <rect x='50' y='130' rx='0' ry='0' width='100' height='8' />
-        <rect x='180' y='75' rx='0' ry='0' width='60' height='8' />
-        <rect x='180' y='90' rx='0' ry='0' width='100' height='8' />
-        <rect x='180' y='115' rx='0' ry='0' width='60' height='8' />
-        <rect x='180' y='130' rx='0' ry='0' width='100' height='8' />
+        <rect x='40' y='75' rx='0' ry='0' width='100' height='8' />
+        <rect x='40' y='90' rx='0' ry='0' width='60' height='8' />
+        <rect x='40' y='115' rx='0' ry='0' width='100' height='8' />
+        <rect x='40' y='130' rx='0' ry='0' width='60' height='8' />
+        <rect x='200' y='75' rx='0' ry='0' width='100' height='8' />
+        <rect x='200' y='90' rx='0' ry='0' width='60' height='8' />
+        <rect x='200' y='115' rx='0' ry='0' width='100' height='8' />
+        <rect x='200' y='130' rx='0' ry='0' width='60' height='8' />
     </ContentLoader>
+    
 );
 
 export default ContractCardLoader;

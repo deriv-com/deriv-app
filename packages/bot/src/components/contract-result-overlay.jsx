3c9e983fe59a23252d552597d43ac651a0b1e6c6
Aaron Imming <aaim@pm.me>
2019-09-22 16:53:46 +0800

    Create contract-card-result-overlay component

---
+++ packages/bot/src/components/contract-result-overlay.jsx
@@ -0,0 +1,40 @@
+import classNames    from 'classnames';
+import PropTypes     from 'prop-types';
+import React         from 'react';
+import {
+    CheckIcon,
+    CrossIcon }      from './icons.jsx';
+import { translate } from '../utils/lang/i18n';
+import                    '../assets/sass/contract-result-overlay.scss';
+
+const ContractResultOverlay = (props) => {
+    const has_won_contract = props.profit >= 0;
+
+    return (
+        <div className={classNames('db-contract-card__result', {
+            'db-contract-card__result--won' : has_won_contract,
+            'db-contract-card__result--lost': !has_won_contract,
+        })}
+        >
+            <span className='db-contract-card__result-caption'>
+                { has_won_contract ?
+                    <React.Fragment>
+                        {translate('Won')}
+                        <CheckIcon className='db-contract-card__result-icon' />
+                    </React.Fragment>
+                    :
+                    <React.Fragment>
+                        {translate('Lost')}
+                        <CrossIcon className='db-contract-card__result-icon' />
+                    </React.Fragment>
+                }
+            </span>
+        </div>
+    );
+};
+
+ContractResultOverlay.propTypes = {
+    profit: PropTypes.number,
+};
+
+export default ContractResultOverlay;

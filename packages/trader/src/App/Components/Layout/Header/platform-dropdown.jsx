968a264843a3bfcf4a2115ea983b1f9c501cc6da
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-23 16:17:39 +0800

    add checks to apply active class to platform

--- packages/trader/src/App/Components/Layout/Header/platform-dropdown.jsx
+++ packages/trader/src/App/Components/Layout/Header/platform-dropdown.jsx
@@ -1,3 +1,4 @@
+import classNames        from 'classnames';
 import PropTypes         from 'prop-types';
 import React             from 'react';
 import ReactDOM          from 'react-dom';
@@ -11,15 +12,29 @@ const PlatformDropdown = ({
 }) => ReactDOM.createPortal(
     <div className='platform_dropdown' onClick={handleClick}>
         <div className='platform_dropdown__list'>
-            {platform_config.map((platform, idx) => (
-                <BinaryLink to={platform.link_to} href={platform.href} key={idx} onClick={handleClick} className='platform_dropdown__list__platform'>
-                    <Icon className='platform_dropdown__list__platform__icon' icon={platform.icon} />
-                    <div className='platform_dropdown__list__platform__details'>
-                        <p className='platform_dropdown__list__platform__title'>{platform.title}</p>
-                        <p className='platform_dropdown__list__platform__description'>{platform.description}</p>
-                    </div>
-                </BinaryLink>
-            ))}
+            {platform_config.map((platform, idx) => {
+                const is_bot = /^\/bot/.test(location.pathname);
+
+                return (
+                    <BinaryLink
+                        to={platform.link_to}
+                        href={platform.href}
+                        key={idx}
+                        onClick={handleClick}
+                        className={classNames(
+                            'platform_dropdown__list__platform',
+                            { 'active': is_bot && platform.href === '/bot' }
+                        )}
+                    >
+                        <Icon className='platform_dropdown__list__platform__icon' icon={platform.icon} />
+                        <div className='platform_dropdown__list__platform__details'>
+                            <p className='platform_dropdown__list__platform__title'>{platform.title}</p>
+                            <p className='platform_dropdown__list__platform__description'>{platform.description}</p>
+                        </div>
+                    </BinaryLink>
+                );
+            }
+            )}
         </div>
     </div>,
     document.getElementById('deriv_app'),

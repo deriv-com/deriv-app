4029bdc31a54247e1bb36bed7bc0695ab44a1f59
zakibinary <zaki@binary.com>
2019-09-24 17:28:59 +0800

    ui fixes

--- packages/trader/src/Modules/Account/Sections/Verification/ProofOfAddress/proof-of-address.jsx
+++ packages/trader/src/Modules/Account/Sections/Verification/ProofOfAddress/proof-of-address.jsx
@@ -184,7 +184,6 @@ class ProofOfAddress extends React.Component {
             document_file,
             document_expired,
             document_verified,
-            // document_needs_action,
             document_under_review,
             file_error_message,
             needs_poi,
@@ -265,7 +264,6 @@ class ProofOfAddress extends React.Component {
                                                     error={touched.address_line_2 && errors.address_line_2}
                                                     onChange={handleChange}
                                                     onBlur={handleBlur}
-                                                    required
                                                 />
                                             </fieldset>
                                             <fieldset className='account-form__fieldset'>

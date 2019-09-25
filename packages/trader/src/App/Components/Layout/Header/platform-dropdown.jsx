e8fc8046df17cd059c89a42c9e189ca9b89075e8
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-25 12:52:51 +0800

    drawer now closes if clicked outside

--- packages/trader/src/App/Components/Layout/Header/platform-dropdown.jsx
+++ packages/trader/src/App/Components/Layout/Header/platform-dropdown.jsx
@@ -7,38 +7,57 @@ import Icon           from 'Assets/icon.jsx';
 import { isBot }      from 'Utils/PlatformSwitcher';
 import 'Sass/app/_common/components/platform-dropdown.scss';
 
-const PlatformDropdown = ({
-    platform_config,
-    handleClick,
-}) => {
-    const is_bot = isBot();
-    return ReactDOM.createPortal(
-        <div className='platform_dropdown' onClick={handleClick}>
-            <div className='platform_dropdown__list'>
-                {platform_config.map((platform, idx) => (
-                    <BinaryLink
-                        to={platform.link_to}
-                        href={platform.href}
-                        key={idx}
-                        onClick={handleClick}
-                        className={classNames(
-                            'platform_dropdown__list__platform',
-                            { 'active': is_bot && platform.href === '/bot' }
-                        )}
-                    >
-                        <Icon className='platform_dropdown__list__platform__icon' icon={platform.icon} />
+class PlatformDropdown extends React.PureComponent {
+    handleClickOutside = (event) => {
+        if (!event.target.closest('.platform_dropdown__list') && !event.target.closest('.platform_switcher')) {
+            this.props.handleClick();
+        }
+    }
 
-                        <div className='platform_dropdown__list__platform__details'>
-                            <p className='platform_dropdown__list__platform__title'>{platform.title}</p>
-                            <p className='platform_dropdown__list__platform__description'>{platform.description}</p>
-                        </div>
-                    </BinaryLink>
-                ))}
+    componentWillMount() {
+        document.addEventListener('click', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside);
+    }
+
+    render() {
+        const {
+            platform_config,
+            handleClick,
+        } = this.props;
+        const is_bot = isBot();
+
+        const platform_dropdown = (
+            <div className='platform_dropdown'>
+                <div className='platform_dropdown__list'>
+                    {platform_config.map((platform, idx) => (
+                        <BinaryLink
+                            to={platform.link_to}
+                            href={platform.href}
+                            key={idx}
+                            onClick={handleClick}
+                            className={classNames(
+                                'platform_dropdown__list__platform',
+                                { 'active': is_bot && platform.href === '/bot' }
+                            )}
+                        >
+                            <Icon className='platform_dropdown__list__platform__icon' icon={platform.icon} />
+
+                            <div className='platform_dropdown__list__platform__details'>
+                                <p className='platform_dropdown__list__platform__title'>{platform.title}</p>
+                                <p className='platform_dropdown__list__platform__description'>{platform.description}</p>
+                            </div>
+                        </BinaryLink>
+                    ))}
+                </div>
             </div>
-        </div>,
-        document.getElementById('deriv_app'),
-    );
-};
+        );
+
+        return ReactDOM.createPortal(platform_dropdown, document.getElementById('deriv_app'));
+    }
+}
 
 PlatformDropdown.propTypes = {
     platform_configs: PropTypes.array,

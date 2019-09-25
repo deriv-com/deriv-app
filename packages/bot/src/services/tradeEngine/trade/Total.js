994b2decb50d895b14bbb3f17e7a66d5ccfd6790
Aaron Imming <aaim@protonmail.com>
2019-09-25 14:26:00 +0800

    Sync changes in trade engine from Binary Bot

--- packages/bot/src/services/tradeEngine/trade/Total.js
+++ packages/bot/src/services/tradeEngine/trade/Total.js
@@ -90,9 +90,12 @@ export default Engine =>
             return accountStat.totalRuns;
         }
 
-        getTotalProfit() {
+        getTotalProfit(toString, currency) {
             const accountStat = this.getAccountStat();
-            return Number(accountStat.totalProfit);
+            
+            return toString && accountStat.totalProfit !== 0
+                ? roundBalance({ currency, balance: +accountStat.totalProfit })
+                : +accountStat.totalProfit;
         }
 
         /* eslint-enable */

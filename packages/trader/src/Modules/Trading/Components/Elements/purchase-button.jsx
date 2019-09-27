ec70ec9bbc6f4690b3dcc34be2e24342414a9ecb
zakibinary <zaki@binary.com>
2019-09-27 13:10:18 +0800

    refactor purchase button to use native button element and update necessary css

--- packages/trader/src/Modules/Trading/Components/Elements/purchase-button.jsx
+++ packages/trader/src/Modules/Trading/Components/Elements/purchase-button.jsx
@@ -1,7 +1,6 @@
 import classNames                 from 'classnames';
 import PropTypes                  from 'prop-types';
 import React                      from 'react';
-import Button                     from 'Modules/Trading/Components/Form/PurchaseButton';
 import { localize }               from 'App/i18n';
 import Icon                       from 'Assets/icon.jsx';
 import { getContractTypeDisplay } from 'Constants/contract';
@@ -27,7 +26,7 @@ const PurchaseButton = ({
     const is_button_disabled = (is_disabled && !is_loading) || is_proposal_empty;
 
     return (
-        <Button
+        <button
             is_disabled={is_disabled}
             id={`dt_purchase_${type.toLowerCase()}_button`}
             className={classNames(
@@ -70,7 +69,7 @@ const PurchaseButton = ({
                     </div>
                 </div>
             </React.Fragment>
-        </Button>
+        </button>
     );
 };
 

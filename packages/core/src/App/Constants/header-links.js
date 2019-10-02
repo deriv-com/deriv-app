08927e9924924a81b9f17530abe3dce223ee70c9
Aaron Imming <aaim@protonmail.com>
2019-10-02 19:29:02 +0800

    Disable dark theme + set DBot logo

--- packages/core/src/App/Constants/header-links.js
+++ packages/core/src/App/Constants/header-links.js
@@ -7,7 +7,8 @@ import { routes }   from 'Constants/index';
 const header_links = [
     {
         id     : 'dt_deriv_logo',
-        logo   : <div className='header__title'>{localize('DTrader')}<span className='header__logo'>{localize('BETA')}</span></div>,
+        // TODO: Update naming.
+        logo   : <div className='header__title'>{localize('DBot')}<span className='header__logo'>{localize('BETA')}</span></div>,
         image  : <IconDeriv className='header__icon' />,
         link_to: routes.trade,
     },

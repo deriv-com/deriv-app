c243c6fb01a648818d05247bb1b7e3c814bb3b24
McSam <samyong@Sams-Mac.local>
2019-10-03 09:41:05 +0800

    update bot status text

--- packages/bot/src/constants/contract-stage.js
+++ packages/bot/src/constants/contract-stage.js
@@ -2,7 +2,7 @@ import { translate } from '../utils/tools';
 
 export const CONTRACT_STAGES = {
     not_running      : { index: 0 , text: translate('Bot is not running') },
-    bot_starting     : { index: 1, text: translate('Bot Starting') },
+    bot_starting     : { index: 1, text: translate('Bot is starting') },
     purchase_sent    : { index: 2 , text: translate('Attempting to Buy') },
     purchase_recieved: { index: 3 , text: translate('Buy Succeeded') },
     bot_is_stopping  : { index: 4 , text: translate('Bot is stopping') },

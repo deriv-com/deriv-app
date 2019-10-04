414277cc62e0e17d168ddc4b2cf635c348ca529e
Anton <anton@binary.com>
2019-10-04 16:33:54 +0800

    more help content fixes

--- packages/bot/src/scratch/blocks/Binary/Tick Analysis/ticks.js
+++ packages/bot/src/scratch/blocks/Binary/Tick Analysis/ticks.js
@@ -6,20 +6,20 @@ Blockly.Blocks.ticks = {
     },
     definition(){
         return {
-            message0       : translate('Ticks List'),
+            message0       : translate('Ticks list'),
             output         : 'Array',
             outputShape    : Blockly.OUTPUT_SHAPE_ROUND,
             colour         : Blockly.Colours.Analysis.colour,
             colourSecondary: Blockly.Colours.Analysis.colourSecondary,
             colourTertiary : Blockly.Colours.Analysis.colourTertiary,
-            tooltip        : translate('Returns the list of 1000 recent tick values'),
+            tooltip        : translate('This block gives you a list of the last 1000 tick values.'),
             category       : Blockly.Categories.Tick_Analysis,
         };
     },
     meta() {
         return {
-            'display_name': translate('Tick List'),
-            'description' : translate('This block returns the list of 1000 recent tick values for the market selected in the Trade Parameters.'),
+            'display_name': translate('Tick list'),
+            'description' : translate('This block gives you a list of the last 1000 tick values.'),
         };
     },
 };

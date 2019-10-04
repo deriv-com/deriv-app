63d26f95c6e40e73c38b943b0df340b963a0b4d4
negar <negar@binary.com>
2019-10-04 15:41:39 +0800

    hide button if no link or text

--- packages/trader/src/App/Containers/CashierModal/error.jsx
+++ packages/trader/src/App/Containers/CashierModal/error.jsx
@@ -20,15 +20,16 @@ const ErrorComponent = ({
         <Icon icon='IconCashierError' className='cashier-error__icon' />
         {header && <h2 className='cashier-error__header'>{header}</h2>}
         <p className='cashier__paragraph'>{message}</p>
-        {button_link ?
-            <ButtonLink
-                className='btn--primary--default cashier-error__button'
-                to={button_link}
-                onClick={onClickButton}
-            >
-                <span className='btn__text'>{button_text}</span>
-            </ButtonLink>
-            :
+        {button_link &&
+        <ButtonLink
+            className='btn--primary--default cashier-error__button'
+            to={button_link}
+            onClick={onClickButton}
+        >
+            <span className='btn__text'>{button_text}</span>
+        </ButtonLink>
+        }
+        {!button_link && button_text &&
             <Button
                 className='btn--primary--default cashier-error__button'
                 onClick={onClickButton}

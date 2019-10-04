2b8691ec44088e37992dec6748b5bf7f4e22e518
Mahdi <m.pourziaei@gmail.com>
2019-10-04 14:59:09 +0800

    Enabled Reports

--- packages/core/src/App/Constants/header-links.js
+++ packages/core/src/App/Constants/header-links.js
@@ -1,16 +1,16 @@
 import React        from 'react';
-// import { localize } from 'App/i18n';
-// import Icon         from 'Assets/icon.jsx';
-// import { routes }   from 'Constants/index';
+import { localize } from 'App/i18n';
+import Icon         from 'Assets/icon.jsx';
+import { routes }   from 'Constants/index';
 
 const header_links = [
-    // {
-    //     id        : 'dt_reports_tab',
-    //     icon      : <Icon icon='IconReports' className='header__icon' />,
-    //     text      : localize('Reports'),
-    //     href      : routes.reports,
-    //     login_only: true,
-    // },
+    {
+        id        : 'dt_reports_tab',
+        icon      : <Icon icon='IconReports' className='header__icon' />,
+        text      : localize('Reports'),
+        href      : routes.reports,
+        login_only: true,
+    },
 ];
 
 export default header_links;

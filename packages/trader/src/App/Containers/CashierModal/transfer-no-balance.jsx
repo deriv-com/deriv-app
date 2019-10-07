2cd9677ab1773316201035299355c1832219cb4c
negar <negar@binary.com>
2019-10-07 11:55:21 +0800

    update no balance screen

--- packages/trader/src/App/Containers/CashierModal/transfer-no-balance.jsx
+++ packages/trader/src/App/Containers/CashierModal/transfer-no-balance.jsx
@@ -3,8 +3,8 @@ import React            from 'react';
 import { Button }       from 'deriv-components';
 import Icon             from 'Assets/icon.jsx';
 import Localize         from 'App/Components/Elements/localize.jsx';
-import { website_name } from 'App/Constants/app-config';
 import { localize }     from 'App/i18n';
+import { connect }      from 'Stores/connect';
 
 class TransferNoBalance extends React.Component {
     onClickDeposit = () => {
@@ -16,8 +16,8 @@ class TransferNoBalance extends React.Component {
         return (
             <div className='cashier__wrapper account-transfer__no-balance'>
                 <Icon icon='IconNoBalance' className='account-transfer__no-balance-icon' />
-                <h2 className='withdraw__header'><Localize i18n_default_text='You have no funds in your {{website_name}} account' values={{ website_name }} /></h2>
-                <p className='cashier__text'><Localize i18n_default_text='Please make a deposit to enable fund transfers.' /></p>
+                <h2 className='withdraw__header'><Localize i18n_default_text='You have no funds in your {{currency}} account' values={{ currency: this.props.currency.toUpperCase() }} /></h2>
+                <p className='cashier__text'><Localize i18n_default_text='Please make a deposit to use this feature.' /></p>
                 <Button
                     className='btn--primary--default account-transfer__no-balance-button'
                     has_effect
@@ -30,7 +30,13 @@ class TransferNoBalance extends React.Component {
 }
 
 TransferNoBalance.propTypes = {
+    currency     : PropTypes.string,
     setModalIndex: PropTypes.func,
 };
 
-export default TransferNoBalance;
+export default connect(
+    ({ client }) => ({
+        currency: client.currency,
+    })
+)(TransferNoBalance);
+

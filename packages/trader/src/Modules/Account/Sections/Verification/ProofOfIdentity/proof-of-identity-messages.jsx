13b7ae16d5b3ded3de61973dbfbab7054e1160ab
Kelly Anyi <kelly@binary.com>
2019-10-08 18:06:39 +0800

    hide link to Help Center for now

--- packages/trader/src/Modules/Account/Sections/Verification/ProofOfIdentity/proof-of-identity-messages.jsx
+++ packages/trader/src/Modules/Account/Sections/Verification/ProofOfIdentity/proof-of-identity-messages.jsx
@@ -106,10 +106,12 @@ export const Unverified = () => (
         message={localize('We could not verify your proof of identity')}
         text={
             <Localize
-                i18n_default_text='Please check your email for details. If you have any questions, please go to our <0>Help Centre</0>.'
-                components={[
-                    <a key={0} className='link link--orange' rel='noopener noreferrer' target='_blank' href='https://www.deriv.com/help-centre/' />,
-                ]}
+                i18n_default_text='Please check your email for details.'
+                // TODO: enable link to Help Center when POI help content is ready
+                // i18n_default_text='Please check your email for details. If you have any questions, please go to our <0>Help Centre</0>.'
+                // components={[
+                //     <a key={0} className='link link--orange' rel='noopener noreferrer' target='_blank' href='https://www.deriv.com/help-centre/' />,
+                // ]}
             />}
         icon={<IconPoiUnverified />}
     />

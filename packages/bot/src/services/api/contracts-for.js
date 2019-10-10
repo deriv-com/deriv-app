d1f4723c451d4b88dc04e2e8ed7db191b5a242b7
McSam <samyong@Sams-Mac.local>
2019-10-10 18:59:54 +0800

    disable rise equal option in metals

--- packages/bot/src/services/api/contracts-for.js
+++ packages/bot/src/services/api/contracts-for.js
@@ -16,6 +16,10 @@ export default class ContractsFor {
                 trade_type: 'higherlower',
             },
             {
+                submarket : 'metals',
+                trade_type: 'callputequal',
+            },
+            {
                 market    : 'indices',
                 trade_type: 'callputequal',
             },

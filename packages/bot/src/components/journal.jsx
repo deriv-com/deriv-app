182632ad920186d32d27bb1b722595fe29b02c04
zakibinary <zaki@binary.com>
2019-10-10 15:16:50 +0800

    remove whitespace

--- packages/bot/src/components/journal.jsx
+++ packages/bot/src/components/journal.jsx
@@ -116,7 +116,6 @@ class Journal extends React.PureComponent {
                                 const { date, time, message, message_type } = item;
                                 const date_el = DateItem({ date, time });
                                 const message_el = MessageItem({ message });
-
                                 return (
                                     <tr className='journal__table--tr' key={`${item.date}-${index}`}>
                                         <td className='journal__table--td'>{date_el}</td>

394da09091947d422704f6c67dbbae6288b0b582
aliafsah-fs <ali.afsah@regentmarkets.com>
2019-10-13 16:54:32 +0330

    sign up redirect to deriv.com

--- packages/trader/src/App/Components/Layout/Header/signup-button.jsx
+++ packages/trader/src/App/Components/Layout/Header/signup-button.jsx
@@ -1,8 +1,9 @@
-import classNames   from 'classnames';
-import PropTypes    from 'prop-types';
-import React        from 'react';
-import { Button }   from 'deriv-components';
-import { localize } from 'App/i18n';
+import classNames     from 'classnames';
+import PropTypes      from 'prop-types';
+import React          from 'react';
+import { Button }     from 'deriv-components';
+import { goToSignUp } from '_common/base/login';
+import { localize }   from 'App/i18n';
 
 const SignupButton = ({ className }) => (
     <Button
@@ -10,7 +11,7 @@ const SignupButton = ({ className }) => (
         className={classNames(className, 'btn--primary--default')}
         has_effect
         text={localize('Sign up')}
-        onClick={() => { window.open('https://deriv.com'); }}
+        onClick={goToSignUp}
     />
 );
 

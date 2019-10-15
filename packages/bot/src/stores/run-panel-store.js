45ddbac418f916d06604030b8f30fd52d71f5aa5
McSam <samyong@Sams-Mac.local>
2019-10-15 17:11:11 +0800

    stop bot and revert run button when strategy stop trade again

--- packages/bot/src/stores/run-panel-store.js
+++ packages/bot/src/stores/run-panel-store.js
@@ -20,6 +20,7 @@ export default class RunPanelStore {
         observer.register('bot.stop', this.onBotStopEvent);
         observer.register('contract.status', this.onContractStatusEvent);
         observer.register('bot.contract', this.onBotContractEvent);
+        observer.register('bot.trade_again', this.onBotTradeAgain);
 
         this.registerReactions();
     }
@@ -36,6 +37,14 @@ export default class RunPanelStore {
     // otherwise we keep opening new contracts and set the ContractStage to purchase_sent
     is_error_happened   = false;
     is_continue_trading = true;
+
+    @action.bound
+    onBotTradeAgain(is_trade_again) {
+        if (!is_trade_again) {
+            this.is_run_button_clicked = false;
+            this.onBotStopEvent();
+        }
+    }
 
     @action.bound
     onBotStopEvent() {

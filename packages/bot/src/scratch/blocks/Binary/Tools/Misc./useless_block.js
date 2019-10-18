ff58779d6fd4cc82aa50a813df4b8669b4a1e646
Aaron Imming <aaim@protonmail.com>
2019-10-18 14:55:05 +0800

    Create a useless block for reasons

---
+++ packages/bot/src/scratch/blocks/Binary/Tools/Misc./useless_block.js
@@ -0,0 +1,25 @@
+import { translate }         from '../../../../../utils/lang/i18n';
+import { setBlockTextColor } from '../../../../utils';
+
+Blockly.Blocks.useless_block = {
+    init() {
+        this.jsonInit({
+            message0         : translate('Conversion Helper Block'),
+            colour           : Blockly.Colours.RootBlock.colour,
+            colourSecondary  : Blockly.Colours.RootBlock.colourSecondary,
+            colourTertiary   : Blockly.Colours.RootBlock.colourTertiary,
+            tooltip          : translate('This block was required to correctly convert your old strategy.'),
+            category         : Blockly.Categories.Miscellaneous,
+            nextStatement    : null,
+            previousStatement: null,
+        });
+    },
+    onchange() {
+        // TODO: uncomment this when the dark mode is done
+        // if (!ScratchStore.instance.root_store.core.ui.is_dark_mode_on) {
+        setBlockTextColor(this);
+        // }
+    },
+};
+
+Blockly.JavaScript.useless_block = () => {};

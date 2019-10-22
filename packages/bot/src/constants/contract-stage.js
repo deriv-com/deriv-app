86b4e1f9aa0ec27d7c2975126b46ccacc7e3404b
mahboobeh <mahboob_mohamadi@hotmail.com>
2019-10-22 11:35:28 +0800

    Update packages/bot/src/constants/contract-stage.js
    Co-Authored-By: Aaron <aaim@protonmail.com>

--- packages/bot/src/constants/contract-stage.js
+++ packages/bot/src/constants/contract-stage.js
@@ -4,7 +4,7 @@ export const contract_stages = Object.freeze({
     NOT_RUNNING      : { index: 0 , text: translate('Bot is not running') },
     STARTING         : { index: 1 , text: translate('Bot is starting') },
     PURCHASE_SENT    : { index: 2 , text: translate('Attempting to Buy') },
-    PURCHASE_RECIEVED: { index: 3 , text: translate('Buy Succeeded') },
+    PURCHASE_RECEIVED: { index: 3 , text: translate('Buy Succeeded') },
     IS_STOPPING      : { index: 4 , text: translate('Bot is stopping') },
     CONTRACT_CLOSED  : { index: 5 , text: translate('Contract Closed') },
 });

3f6eebf5c71347fbbef070a0e96200e0f024ce45
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-10-22 14:03:36 +0800

    Update save-loadmodal string

--- packages/bot/src/constants/contract-stage.js
+++ packages/bot/src/constants/contract-stage.js
@@ -3,8 +3,8 @@ import { translate } from '../utils/tools';
 export const contract_stages = Object.freeze({
     NOT_RUNNING      : { index: 0 , text: translate('Bot is not running') },
     STARTING         : { index: 1 , text: translate('Bot is starting') },
-    PURCHASE_SENT    : { index: 2 , text: translate('Attempting to Buy') },
-    PURCHASE_RECEIVED: { index: 3 , text: translate('Buy Succeeded') },
+    PURCHASE_SENT    : { index: 2 , text: translate('Attempting to buy') },
+    PURCHASE_RECEIVED: { index: 3 , text: translate('Buy is successful') },
     IS_STOPPING      : { index: 4 , text: translate('Bot is stopping') },
-    CONTRACT_CLOSED  : { index: 5 , text: translate('Contract Closed') },
+    CONTRACT_CLOSED  : { index: 5 , text: translate('Contract closed') },
 });

b866a1484358878d3648b78d1dd4fda6880777d3
ali.afsah@regentmarkets.com <ali.afsah@regentmarkets.com>
2019-10-30 10:18:23 +0330

    format correction

--- packages/trader/src/Services/socket-general.js
+++ packages/trader/src/Services/socket-general.js
@@ -1,12 +1,12 @@
-import { action, flow }  from 'mobx';
-import ObjectUtils       from 'deriv-shared/utils/object';
+import { action, flow } from 'mobx';
+import ObjectUtils      from 'deriv-shared/utils/object';
 import Login,
-{ redirectToLogin }      from '_common/base/login';
-import ServerTime        from '_common/base/server_time';
-import BinarySocket      from '_common/base/socket_base';
-import { State }         from '_common/storage';
+{ redirectToLogin }     from '_common/base/login';
+import ServerTime       from '_common/base/server_time';
+import BinarySocket     from '_common/base/socket_base';
+import { State }        from '_common/storage';
 import { localize }      from 'App/i18n';
-import { requestLogout } from './logout';
+import { requestLogout }from './logout';
 import WS                from './ws-methods';
 
 let client_store,

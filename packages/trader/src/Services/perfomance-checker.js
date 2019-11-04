e2e93802ff88da4d8f6189f8cfe5a290808daf00
ali.afsah@regentmarkets.com <ali.afsah@regentmarkets.com>
2019-11-04 17:16:28 +0330

    measurePerformance added

--- packages/trader/src/Services/perfomance-checker.js
+++ packages/trader/src/Services/perfomance-checker.js
@@ -1,14 +1,14 @@
-class PerformanceChecker {
-    startChecking = () => {
-        performance.measure('difference', 'start', 'set_proposal_info');
-        const differences = performance.getEntriesByType('measure');
-        for (let i = 0; i < differences.length; i++) {
-            const time = Math.round(differences[i].startTime + differences[i].duration);
-            // eslint-disable-next-line no-console
-            console.log(`time: ${  JSON.stringify(time)}`);
-        }
+function measurePerformance (end, start = 'start') {
+    performance.measure(end, start, end);
+    const differences = performance.getEntriesByType('measure');
+    for (let i = 0; i < differences.length; i++) {
+        // const time = Math.round(differences[i].startTime + differences[i].duration);
+        // eslint-disable-next-line no-console
+        console.log(`${JSON.stringify(differences[i])}: ${  JSON.stringify(differences[i].duration)}`);
+    }
 
-        // performance.clearMarks();
-    };
+    // performance.clearMarks();
 }
-export default new PerformanceChecker();
+export {
+    measurePerformance,
+};

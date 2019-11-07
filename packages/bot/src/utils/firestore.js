dd1f765aa22a43c3ed05d712654f10617e27910b
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-11-07 15:45:07 +0800

    Add errors to firestore

--- packages/bot/src/utils/firestore.js
+++ packages/bot/src/utils/firestore.js
@@ -1,4 +1,5 @@
 import { reaction } from 'mobx';
+import { message_types } from '../constants/messages';
 const firebase = require("firebase");
 require("firebase/firestore");
 
@@ -19,6 +20,7 @@ const firestore = (() => {
         const run_panel = root_store.run_panel;
         const client = root_store.core.client;
         const transactions = root_store.transactions;
+        const journal = root_store.journal;
 
         db = firebase.firestore();
         users = db.collection('Users');
@@ -27,39 +29,43 @@ const firestore = (() => {
         reaction(
             () => run_panel.is_running,
             () => run_panel.is_running ?
-                onRunBotEvent(client.loginid) :
-                onStopBotEvent()
+                onRunBot(client.loginid) :
+                onStopBot()
         );
 
         reaction(
             () => transactions.contracts,
-            () => onTransactionEvent(transactions.contracts)
+            () => onTransaction(transactions.contracts)
+        )
+
+        reaction(
+            () => journal.messages,
+            () => onError(journal.messages)
         )
     }
 
-    const onRunBotEvent = (login_id) => {
+    const onRunBot = (login_id) => {
         const strategy = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(Blockly.derivWorkspace));
         runs.add({
-            start_time: Date.now(),
+            start_time: firebase.firestore.FieldValue.serverTimestamp(),
             account_id: login_id,
-            xml: strategy
-        }).then(function(docRef) {
-            doc_id= docRef.id;
+            xml: strategy,
+        }).then(function (docRef) {
+            doc_id = docRef.id;
         })
-        .catch(function(error) {
-            console.error("Error adding document to firestore", error);
-            doc_id = Math.floor(10000000 + Math.random() * 90000000)
-        });
+            .catch(function (error) {
+                console.error("Error adding document to firestore", error);
+                doc_id = Math.floor(10000000 + Math.random() * 90000000)
+            });
     }
 
-    const onStopBotEvent = () => {
-        const end_time = Date.now();
+    const onStopBot = () => {
         runs.doc(doc_id).update({
-            end_time: end_time,
+            end_time: firebase.firestore.FieldValue.serverTimestamp(),
         });
     }
 
-    const onTransactionEvent = (contracts) => {
+    const onTransaction = (contracts) => {
         const contract = contracts.length > 0 && contracts[0];
         if (contract.is_completed) {
             runs.doc(doc_id).collection('Transactions').add({
@@ -70,7 +76,21 @@ const firestore = (() => {
                 entry_spot: contract.entry_spot,
                 exit_spot: contract.exit_spot,
                 profit: contract.profit,
-                is_completed:contract.is_completed
+                is_completed: contract.is_completed,
+                time_stamp: firebase.firestore.FieldValue.serverTimestamp()
+            });
+        }
+    }
+
+
+    const onError = (messages) => {
+        const item = messages.length > 0 && messages[0];
+        if (item.message_type === message_types.ERROR) {
+            runs.doc(doc_id).collection('Errors').add({
+                date: item.date,
+                time: item.time,
+                message: item.message,
+                time_stamp: firebase.firestore.FieldValue.serverTimestamp()
             });
         }
     }

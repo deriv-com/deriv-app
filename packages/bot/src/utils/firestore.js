903137fc44a0864394ff05d7ba561649236c1829
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-11-08 15:34:36 +0800

    Clean up

--- packages/bot/src/utils/firestore.js
+++ packages/bot/src/utils/firestore.js
@@ -4,12 +4,8 @@ import { reaction } from 'mobx';
 import { message_types } from '../constants/messages';
 
 const firestore = (() => {
-<<<<<<< HEAD
 
     let db, users, runs, doc_id, server_time;
-=======
-    var db, users, runs, doc_id, server_time;
->>>>>>> a090956cad526c6e7c6db358ea96eea95e619213
 
     const init = (root_store) => {
         try {

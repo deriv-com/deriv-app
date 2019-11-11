5be9dfc975b91d763b3a2a13c9c9b5d054ff4bdd
ali.afsah@regentmarkets.com <ali.afsah@regentmarkets.com>
2019-11-11 16:33:54 +0330

    dummy GTM & GA accounts

--- packages/trader/src/templates/app/includes/google/gtm_no_script.jsx
+++ packages/trader/src/templates/app/includes/google/gtm_no_script.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const GTMNoScript = () => (
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NF7884S"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M596458"
     height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
 );
 export default GTMNoScript;

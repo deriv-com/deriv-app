d4914f469a891761fd4e1ad93a4c01fedeccd561
mustofa binary <mustofa@regentmarkets.com>
2019-11-13 13:41:08 +0800

    add from client store

--- packages/trader/src/App/Containers/Layout/app-contents.jsx
+++ packages/trader/src/App/Containers/Layout/app-contents.jsx
@@ -1,35 +1,48 @@
 import classNames           from 'classnames';
 import PropTypes            from 'prop-types';
-import React                from 'react';
+import React, { useEffect } from 'react';
 import { withRouter }       from 'react-router';
 import { ThemedScrollbars } from 'deriv-components';
 import { connect }          from 'Stores/connect';
+import { get as getLanguage } from '_common/language';
 // import InstallPWA    from './install-pwa.jsx';
 
 const AppContents = ({
     // addNotificationBar,
     children,
+    client_email,
+    client_loginid,
     is_app_disabled,
     is_logged_in,
     is_positions_drawer_on,
     is_route_modal_on,
     // setPWAPromptEvent,
 }) => {
-    if (is_logged_in) {
-        // TODO: uncomment these after the issues with showing the prompt too often and in the app are fixed
-        // window.addEventListener('beforeinstallprompt', e => {
-        //     console.log('Going to show the installation prompt'); // eslint-disable-line no-console
-        //
-        //     e.preventDefault();
-        //
-        //     setPWAPromptEvent(e);
-        //     addNotificationBar({
-        //         content : <InstallPWA />,
-        //         autoShow: 10000, // show after 10 secs
-        //         msg_type: 'pwa',
-        //     });
-        // });
-    }
+
+    useEffect(() => {
+        if (is_logged_in) {
+            // eslint-disable-next-line no-console
+            console.log(window.location.pathname);
+            window.analytics.identify(client_loginid, {
+                email   : client_email,
+                language: getLanguage(),
+            });
+            window.analytics.page();
+            // TODO: uncomment these after the issues with showing the prompt too often and in the app are fixed
+            // window.addEventListener('beforeinstallprompt', e => {
+            //     console.log('Going to show the installation prompt'); // eslint-disable-line no-console
+            //
+            //     e.preventDefault();
+            //
+            //     setPWAPromptEvent(e);
+            //     addNotificationBar({
+            //         content : <InstallPWA />,
+            //         autoShow: 10000, // show after 10 secs
+            //         msg_type: 'pwa',
+            //     });
+            // });
+        }
+    }, []);
 
     return (
         <div
@@ -54,6 +67,8 @@ const AppContents = ({
 AppContents.propTypes = {
     addNotificationBar    : PropTypes.func,
     children              : PropTypes.any,
+    client_email          : PropTypes.string,
+    client_loginid        : PropTypes.String,
     is_app_disabled       : PropTypes.bool,
     is_logged_in          : PropTypes.bool,
     is_positions_drawer_on: PropTypes.bool,
@@ -64,6 +79,8 @@ AppContents.propTypes = {
 
 export default withRouter(connect(
     ({ client, ui }) => ({
+        client_email          : client.email,
+        client_loginid        : client.loginid,
         is_logged_in          : client.is_logged_in,
         // addNotificationBar    : ui.addNotificationBar,
         is_app_disabled       : ui.is_app_disabled,

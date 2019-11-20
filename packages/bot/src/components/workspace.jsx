16d06d9c2245327a944cfa86410f2e542c4fbdb6
zakibinary <zaki@binary.com>
2019-11-20 11:47:18 +0800

    refactoring code and check for state of run panel when displaying notification messages

--- packages/bot/src/components/workspace.jsx
+++ packages/bot/src/components/workspace.jsx
@@ -1,22 +1,17 @@
-import React        from 'react';
-import Flyout       from './flyout.jsx';
-import ScratchStore from '../stores/scratch-store';
-import                   '../assets/sass/scratch/workspace.scss';
-import                   '../assets/sass/scratch/toolbox.scss';
+import React                from 'react';
+import Flyout               from './flyout.jsx';
+import NotificationMessages from './notification-messages.jsx';
+import                           '../assets/sass/scratch/workspace.scss';
+import                           '../assets/sass/scratch/toolbox.scss';
 
-const Workspace = () => {
-    const NotificationMessages = ScratchStore.instance.root_store.core.ui.notification_messages_ui || null;
-    return (
-        <React.Fragment>
-            {/* <div id='scratch_area' /> */}
-            <div id='scratch_div'>
-                <div className='notificationsContainer'>
-                    <NotificationMessages />
-                </div>
-                <Flyout />
-            </div>
-        </React.Fragment>
-    );
-};
+const Workspace = () =>  (
+    <React.Fragment>
+        {/* <div id='scratch_area' /> */}
+        <div id='scratch_div'>
+            <NotificationMessages />
+            <Flyout />
+        </div>
+    </React.Fragment>
+);
 
 export default Workspace;

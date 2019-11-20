fb83049ed0d9518f67007cda19e0110b80ffeca6
zakibinary <zaki@binary.com>
2019-11-20 16:02:23 +0800

    check if browser is firefox and disable canvas text shadow if firefox is detected

--- packages/trader/src/Modules/SmartChart/Components/all-markers.jsx
+++ packages/trader/src/Modules/SmartChart/Components/all-markers.jsx
@@ -7,6 +7,8 @@ import CurrencyUtils from 'deriv-shared/utils/currency';
 import { RawMarker } from 'Modules/SmartChart';
 import * as ICONS    from './icons';
 
+const is_firefox = (navigator.userAgent.search('Firefox')) > 0;
+
 const RawMarkerMaker = (draw_callback) => {
     const Marker = ({ epoch_array, price_array, ...rest }) => (
         <RawMarker
@@ -141,8 +143,10 @@ const shadowed_text = ({ ctx, is_dark_theme, text, left, top, scale }) => {
     ctx.textAlign = 'center';
     const size = Math.floor(scale * 12);
     ctx.font = `bold ${size}px BinarySymbols, Roboto`;
-    ctx.shadowColor = is_dark_theme ? 'rgba(16,19,31,1)' : 'rgba(255,255,255,1)';
-    ctx.shadowBlur = 12;
+    if (!is_firefox) {
+        ctx.shadowColor = is_dark_theme ? 'rgba(16,19,31,1)' : 'rgba(255,255,255,1)';
+        ctx.shadowBlur = 12;
+    }
     for (let i = 0; i < 5; ++i) {
         ctx.fillText(
             text,

8241c36a19d0896dd3432de6f6eaf39d91dce7a5
mustofa binary <mustofa@regentmarkets.com>
2019-11-26 16:39:45 +0800

    add semicolon

--- packages/core/src/App/Containers/CashierModal/p2p-cashier.jsx
+++ packages/core/src/App/Containers/CashierModal/p2p-cashier.jsx
@@ -2,6 +2,6 @@ import React  from 'react';
 import { WS } from 'Services';
 import P2P    from 'deriv-p2p';
 
-const P2PCashier = () => <P2P websocket_api={WS} />
+const P2PCashier = () => <P2P websocket_api={WS} />;
 
 export default P2PCashier;

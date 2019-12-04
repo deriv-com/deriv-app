a0b1b1a74b6236d3cbd3ef458d50ec32f4f354eb
mustofa binary <mustofa@regentmarkets.com>
2019-12-04 17:49:04 +0800

    finish tag structure

--- packages/p2p/src/components/buy-sell/popup.jsx
+++ packages/p2p/src/components/buy-sell/popup.jsx
@@ -4,8 +4,29 @@ import PropTypes from 'prop-types';
 const Popup = ({ type }) => {
     return (
         <Fragment>
-            <div className='popup'>
-
+            <div className='buy-sell__popup'>
+                <div className='buy-sell__popup-header'>
+                
+                </div>
+                <div className='buy-sell__popup-content'>
+                    <div>
+                        <input />
+                        <svg></svg>
+                        <input />
+                    </div>
+                    <div>
+                        <span></span>
+                        <p></p>
+                    </div>
+                    <div>
+                        <span></span>
+                        <p></p>
+                    </div>
+                    <div>
+                        <span></span>
+                        <p></p>
+                    </div>
+                </div>
             </div>
         </Fragment>
     );

47a2af72e928b54356a917daa80d1180007d5d08
Kelly Anyi <kelly@binary.com>
2019-12-04 16:21:04 +0800

    use icon from component

--- packages/core/src/Modules/Cashier/Components/Error/no-balance.jsx
+++ packages/core/src/Modules/Cashier/Components/Error/no-balance.jsx
@@ -1,9 +1,8 @@
 import PropTypes              from 'prop-types';
 import React                  from 'react';
 import { withRouter }         from 'react-router-dom';
-import { Button }             from 'deriv-components';
+import { Button, Icon }       from 'deriv-components';
 import { localize, Localize } from 'deriv-translations';
-import Icon                   from 'Assets/icon.jsx';
 import routes                 from 'Constants/routes';
 import { connect }            from 'Stores/connect';
 
@@ -18,7 +17,7 @@ class NoBalance extends React.Component {
         return (
             <div className='cashier__wrapper cashier__no-balance cashier__center-align-wrapper'>
                 <div className='cashier__center-align-content'>
-                    <Icon icon='IconNoBalance' className='cashier__no-balance-icon' />
+                    <Icon icon='IcCashierNoBalance' className='cashier__no-balance-icon' />
                     <h2 className='withdraw__header'><Localize i18n_default_text='You have no funds in your {{currency}} account' values={{ currency: this.props.currency.toUpperCase() }} /></h2>
                     <p className='cashier__text'><Localize i18n_default_text='Please make a deposit to use this feature.' /></p>
                     <Button

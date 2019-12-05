e4e7256d447f4d228b2e75d7e098db3f1112c312
Aaron Imming <aaim@protonmail.com>
2019-12-05 09:52:06 +0800

    Add undo/redo indication

--- packages/bot/src/components/toolbar.jsx
+++ packages/bot/src/components/toolbar.jsx
@@ -119,13 +119,14 @@ const BotNameBox = ({ onBotNameTyped, file_name }) => (
 );
 
 const ButtonGroup = ({
+    has_redo_stack,
+    has_undo_stack,
     is_stop_button_disabled,
     is_stop_button_visible,
     onResetClick,
-    onUndoClick,
-    onRedoClick,
     onRunClick,
     onSortClick,
+    onUndoClick,
     onZoomInOutClick,
     onStopClick,
     toggleSaveLoadModal,
@@ -135,13 +136,19 @@ const ButtonGroup = ({
             alignment='bottom'
             message={localize('Import')}
         >
-            <ToolbarOpenIcon className='toolbar__icon' onClick={() => toggleSaveLoadModal(false)} />
+            <ToolbarOpenIcon
+                className='toolbar__icon'
+                onClick={() => toggleSaveLoadModal(false)}
+            />
         </Popover>
         <Popover
             alignment='bottom'
             message={localize('Reset')}
         >
-            <ToolbarNewFileIcon className='toolbar__icon' onClick={onResetClick} />
+            <ToolbarNewFileIcon
+                className='toolbar__icon'
+                onClick={onResetClick}
+            />
         </Popover>
         <Popover
             alignment='bottom'
@@ -157,13 +164,25 @@ const ButtonGroup = ({
             alignment='bottom'
             message={localize('Undo')}
         >
-            <ToolbarUndoIcon className='toolbar__icon' onClick={onUndoClick} />️
+            <ToolbarUndoIcon
+                className={classNames(
+                    'toolbar__icon',
+                    'toolbar__icon-undo',
+                    { 'toolbar__icon--disabled': !has_undo_stack })}
+                onClick={() => onUndoClick(/* redo */ false)}
+            />️
         </Popover>
         <Popover
             alignment='bottom'
             message={localize('Redo')}
         >
-            <ToolbarRedoIcon className='toolbar__icon' onClick={onRedoClick} />
+            <ToolbarRedoIcon
+                className={classNames(
+                    'toolbar__icon',
+                    'toolbar__icon-redo',
+                    { 'toolbar__icon--disabled': !has_redo_stack })}
+                onClick={() => onUndoClick(/* redo */ true)}
+            />️
         </Popover>
         <div className='vertical-divider' />
         {is_stop_button_visible ?
@@ -174,7 +193,7 @@ const ButtonGroup = ({
                 <ToolbarStopIcon
                     className={classNames(
                         'toolbar__icon',
-                        'toolbar__icon--stop',
+                        'toolbar__icon-stop',
                         { 'toolbar__icon--disabled': is_stop_button_disabled })}
                     onClick={onStopClick}
                 />
@@ -184,122 +203,103 @@ const ButtonGroup = ({
                 alignment='bottom'
                 message={localize('Run')}
             >
-                <ToolbarRunIcon className='toolbar__icon' onClick={onRunClick} />
+                <ToolbarRunIcon
+                    className='toolbar__icon'
+                    onClick={onRunClick}
+                />
             </Popover>
         }
         <Popover
             alignment='bottom'
             message={localize('Sort')}
         >
-            <ToolbarReaarangeIcon className='toolbar__icon' onClick={onSortClick} />
+            <ToolbarReaarangeIcon
+                className='toolbar__icon'
+                onClick={onSortClick}
+            />
         </Popover>
         <Popover
             alignment='bottom'
             message={localize('Zoom in')}
         >
-            <ToolbarZoomInIcon className='toolbar__icon' onClick={() => onZoomInOutClick(true)} />
+            <ToolbarZoomInIcon
+                className='toolbar__icon'
+                onClick={() => onZoomInOutClick(true)}
+            />
         </Popover>
         <Popover
             alignment='bottom'
             message={localize('Zoom out')}
         >
-            <ToolbarZoomOutIcon className='toolbar__icon' onClick={() => onZoomInOutClick(false)} />
+            <ToolbarZoomOutIcon
+                className='toolbar__icon'
+                onClick={() => onZoomInOutClick(false)}
+            />
         </Popover>
     </div>
 );
 
-const Toolbar = ({
-    file_name,
-    is_dialog_open,
-    is_drawer_open,
-    is_search_loading,
-    is_stop_button_disabled,
-    is_stop_button_visible,
-    onBotNameTyped,
-    onOkButtonClick,
-    onCancelButtonClick,
-    onRedoClick,
-    onResetClick,
-    onRunClick,
-    onSearch,
-    onSearchBlur,
-    onSearchClear,
-    onSearchKeyUp,
-    onSortClick,
-    onStopClick,
-    onToolboxToggle,
-    onUndoClick,
-    onZoomInOutClick,
-    toggleSaveLoadModal,
-}) => (
-    <div className='toolbar'>
-        <div className='toolbar__section'>
-            <Popover
-                alignment='bottom'
-                classNameBubble='toolbar__bubble'
-                message={localize('Click here to start building your DBot.')}
-            >
-                <Button
-                    id='start'
-                    className='toolbar__btn--icon toolbar__btn--start'
-                    has_effect
-                    onClick={onToolboxToggle}
-                    icon={<ToolbarStartIcon />}
-                    green
-                >
-                    {localize('Get started')}
-                </Button>
-            </Popover>
-            <SearchBox
-                is_search_loading={is_search_loading}
-                onSearch={onSearch}
-                onSearchClear={onSearchClear}
-                onSearchBlur={onSearchBlur}
-                onSearchKeyUp={onSearchKeyUp}
-            />
-            <BotNameBox
-                file_name={file_name}
-                onBotNameTyped={onBotNameTyped}
-            />
-            <ButtonGroup
-                is_stop_button_disabled={is_stop_button_disabled}
-                is_stop_button_visible={is_stop_button_visible}
-                onRedoClick={onRedoClick}
-                onResetClick={onResetClick}
-                onRunClick={onRunClick}
-                onSortClick={onSortClick}
-                onStopClick={onStopClick}
-                onUndoClick={onUndoClick}
-                onZoomInOutClick={onZoomInOutClick}
-                toggleSaveLoadModal={toggleSaveLoadModal}
-            />
-        </div>
-        <div className='toolbar__section'>
-            <TradeAnimation
-                className={classNames(
-                    'toolbar__animation',
-                    { 'animation--hidden': is_drawer_open }
-                )}
-                should_show_overlay={true}
-            />
+const Toolbar = (props) => {
+    const {
+        is_dialog_open,
+        is_drawer_open,
+        onOkButtonClick,
+        onCancelButtonClick,
+        onToolboxToggle,
+    } = props;
 
+    return (
+        <div className='toolbar'>
+            <div className='toolbar__section'>
+                <Popover
+                    alignment='bottom'
+                    classNameBubble='toolbar__bubble'
+                    message={localize('Click here to start building your DBot.')}
+                >
+                    <Button
+                        id='start'
+                        className='toolbar__btn--icon toolbar__btn--start'
+                        has_effect
+                        onClick={onToolboxToggle}
+                        icon={<ToolbarStartIcon />}
+                        green
+                    >
+                        {localize('Get started')}
+                    </Button>
+                </Popover>
+                <SearchBox {...props} />
+                <BotNameBox {...props} />
+                <ButtonGroup {...props} />
+            </div>
+            <div className='toolbar__section'>
+                <TradeAnimation
+                    className={classNames(
+                        'toolbar__animation',
+                        { 'animation--hidden': is_drawer_open }
+                    )}
+                    should_show_overlay={true}
+                />
+
+            </div>
+            <SaveLoadModal />
+            {is_dialog_open &&
+            <Dialog
+                title={localize('Are you sure?')}
+                is_open={is_dialog_open}
+                onOkButtonClick={onOkButtonClick}
+                onCancelButtonClick={onCancelButtonClick}
+            >
+                {localize('Any unsaved changes will be lost.')}
+            </Dialog>
+            }
         </div>
-        <SaveLoadModal />
-        {is_dialog_open &&
-        <Dialog
-            title={localize('Are you sure?')}
-            is_open={is_dialog_open}
-            onOkButtonClick={onOkButtonClick}
-            onCancelButtonClick={onCancelButtonClick}
-        >
-            {localize('Any unsaved changes will be lost.')}
-        </Dialog>
-        }
-    </div>
-);
+    );
+};
 
 Toolbar.propTypes = {
     file_name              : PropTypes.string,
+    has_redo_stack         : PropTypes.bool,
+    has_undo_stack         : PropTypes.bool,
     is_dialog_open         : PropTypes.bool,
     is_drawer_open         : PropTypes.bool,
     is_search_loading      : PropTypes.bool,
@@ -309,7 +309,6 @@ Toolbar.propTypes = {
     onCancelButtonClick    : PropTypes.func,
     onGoogleDriveClick     : PropTypes.func,
     onOkButtonClick        : PropTypes.func,
-    onRedoClick            : PropTypes.func,
     onResetClick           : PropTypes.func,
     onRunClick             : PropTypes.func,
     onSearch               : PropTypes.func,
@@ -326,6 +325,8 @@ Toolbar.propTypes = {
 
 export default connect(({ run_panel, saveload, toolbar }) => ({
     file_name              : toolbar.file_name,
+    has_redo_stack         : toolbar.has_redo_stack,
+    has_undo_stack         : toolbar.has_undo_stack,
     is_dialog_open         : toolbar.is_dialog_open,
     is_drawer_open         : run_panel.is_drawer_open,
     is_search_loading      : toolbar.is_search_loading,
@@ -335,7 +336,6 @@ export default connect(({ run_panel, saveload, toolbar }) => ({
     onCancelButtonClick    : toolbar.onResetCancelButtonClick,
     onGoogleDriveClick     : toolbar.onGoogleDriveClick,
     onOkButtonClick        : toolbar.onResetOkButtonClick,
-    onRedoClick            : toolbar.onRedoClick,
     onResetClick           : toolbar.onResetClick,
     onRunClick             : toolbar.onRunClick,
     onSearch               : toolbar.onSearch,

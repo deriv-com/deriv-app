314b46b078df4cbe2b3fc4a4351579f689afbbec
oskar-binary <oskar@binary.com>
2019-12-05 13:55:01 +0800

    update lock file

--- packages/components/src/components/table/table-header.jsx
+++ packages/components/src/components/table/table-header.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React      from 'react';
+import classNames from 'classnames';
+import PropTypes  from 'prop-types';
 
-const Header = ({ children }) => (
-    <div role='rowgroup' className='dc-table__header'>{children}</div>
+const Header = ({ children, className }) => (
+    <div role='rowgroup' className={classNames('dc-table__header', className)}>
+        {children}
+    </div>
 );
+
+Header.propTypes = {
+    children : PropTypes.node.isRequired,
+    className: PropTypes.string,
+};
 
 export default Header;

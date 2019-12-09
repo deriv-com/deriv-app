87134c4deded119fadb761b1cfe9a53f151509d1
Kelly Anyi <kelly@binary.com>
2019-12-09 15:26:47 +0800

    replace Dialog with Modal, add InsufficientBalanceModal

--- packages/trader/src/App/Components/Elements/Modals/ServicesErrorModal/authorization-required-modal.jsx
+++ packages/trader/src/App/Components/Elements/Modals/ServicesErrorModal/authorization-required-modal.jsx
@@ -1,37 +1,45 @@
-import { Dialog }      from 'deriv-components';
-import PropTypes       from 'prop-types';
-import React           from 'react';
-import { localize }    from 'deriv-translations';
-import { ClientBase }  from '_common/base/client_base';
+import { Button, Modal } from 'deriv-components';
+import PropTypes         from 'prop-types';
+import React             from 'react';
+import { localize }      from 'deriv-translations';
+import { ClientBase }    from '_common/base/client_base';
 import {
     redirectToLogin,
-    redirectToSignUp } from '_common/base/login';
+    redirectToSignUp }   from '_common/base/login';
 
 const AuthorizationRequiredModal = ({
-    disableApp,
-    enableApp,
-    is_loading,
     is_visible,
+    toggleModal,
 }) => (
-    <Dialog
-        cancel_button_text={localize('Log in')}
-        confirm_button_text={localize('Create free account')}
-        disableApp={disableApp}
-        enableApp={enableApp}
-        is_loading={is_loading}
-        is_visible={is_visible}
-        onCancel={() => redirectToLogin(ClientBase.isLoggedIn())}
-        onConfirm={redirectToSignUp}
-        title={localize('Log in or create a free account to start trading')}
-    />
+    <Modal
+        id='dt_authorization_required_modal'
+        is_open={is_visible}
+        small
+        toggleModal={toggleModal}
+    >
+        <Modal.Body>
+            {localize('Log in or create a free account to start trading.')}
+        </Modal.Body >
+        <Modal.Footer>
+            <Button
+                has_effect
+                text={localize('Log in')}
+                onClick={() => redirectToLogin(ClientBase.isLoggedIn())}
+                secondary
+            />
+            <Button
+                has_effect
+                text={localize('Create free account')}
+                onClick={redirectToSignUp}
+                primary
+            />
+        </Modal.Footer>
+    </Modal>
 );
 
 AuthorizationRequiredModal.propTypes = {
-    disableApp    : PropTypes.func,
-    enableApp     : PropTypes.func,
-    is_loading    : PropTypes.bool,
-    is_visible    : PropTypes.bool,
-    services_error: PropTypes.object,
+    is_visible : PropTypes.bool,
+    toggleModal: PropTypes.func,
 };
 
 export default AuthorizationRequiredModal;
--- packages/trader/src/App/Components/Elements/Modals/ServicesErrorModal/services-error-modal.jsx
+++ packages/trader/src/App/Components/Elements/Modals/ServicesErrorModal/services-error-modal.jsx
@@ -1,15 +1,12 @@
-import { Dialog }    from 'deriv-components';
-import PropTypes     from 'prop-types';
-import React         from 'react';
-import { localize }  from 'deriv-translations';
-import { connect }   from 'Stores/connect';
-import { title }     from './constants';
+import { Button, Modal }          from 'deriv-components';
+import PropTypes                  from 'prop-types';
+import React                      from 'react';
+import { localize }               from 'deriv-translations';
+import { title }                  from './constants';
 import AuthorizationRequiredModal from './authorization-required-modal.jsx';
+import InsufficientBalanceModal   from './insufficient-balance-modal.jsx';
 
 const ServicesErrorModal = ({
-    disableApp,
-    enableApp,
-    is_loading,
     is_visible,
     onConfirm,
     services_error,
@@ -21,45 +18,46 @@ const ServicesErrorModal = ({
     if (code === 'AuthorizationRequired') {
         return (
             <AuthorizationRequiredModal
-                disableApp={disableApp}
-                enableApp={enableApp}
-                is_loading={is_loading}
                 is_visible={is_visible}
+                toggleModal={onConfirm}
             />
         );
     }
-
+    if (code === 'InsufficientBalance') {
+        return (
+            <InsufficientBalanceModal
+                is_visible={is_visible}
+                message={message}
+                toggleModal={onConfirm}
+            />
+        );
+    }
     return (
-        <Dialog
+        <Modal
+            is_open={is_visible}
+            small
             title={title[services_error.type]}
-            confirm_button_text={localize('OK')}
-            onConfirm={onConfirm}
-            // TODO: handle onCancel
-            // cancel_button_text={cancel_button_text}
-            // onCancel={onCancel}
-            disableApp={disableApp}
-            enableApp={enableApp}
-            is_loading={is_loading}
-            is_visible={is_visible}
+            toggleModal={onConfirm}
         >
-            {message}
-        </Dialog>
+            <Modal.Body>
+                {message}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    has_effect
+                    text={localize('OK')}
+                    onClick={onConfirm}
+                    primary
+                />
+            </Modal.Footer>
+        </Modal>
     );
 };
 
 ServicesErrorModal.propTypes = {
-    disableApp    : PropTypes.func,
-    enableApp     : PropTypes.func,
-    is_loading    : PropTypes.bool,
     is_visible    : PropTypes.bool,
     onConfirm     : PropTypes.func,
     services_error: PropTypes.object,
 };
 
-export default connect(
-    ({ ui }) => ({
-        disableApp: ui.disableApp,
-        enableApp : ui.enableApp,
-        is_loading: ui.is_loading,
-    }),
-)(ServicesErrorModal);
+export default ServicesErrorModal;

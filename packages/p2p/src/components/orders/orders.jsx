720b80cffc2c00ea05e53303464d4f5037d3a030
mustofa binary <mustofa@regentmarkets.com>
2019-12-09 14:41:40 +0800

    add popup to order flow

--- packages/p2p/src/components/orders/orders.jsx
+++ packages/p2p/src/components/orders/orders.jsx
@@ -1,9 +1,82 @@
 import React from 'react';
-import './orders.scss';
+import { Button, Dialog } from 'deriv-components';
 import { localize } from 'deriv-translations';
+import Popup from './popup.jsx';
+import './orders.scss';
 
 const Orders = () => {
-    return <div>{localize('Orders')}</div>;
+    const [show_popup, setShowPopup] = React.useState(false);
+    const [popup_options, setPopupOptions] = React.useState({});
+
+    const onCancelClick = () => {
+        setShowPopup(false);
+    }
+
+    const onConfirmation = () => {
+        // eslint-disable-next-line no-console
+        console.log('confirmed')
+    }
+
+    const popupSellConfirm = () => {
+        const options = {
+            title: localize('Have you received funds?'),
+            message: localize('Make sure that you have logged in your bank account or other e-wallet to check the receipt.'),
+            need_confirmation: true,
+            offer: {
+                currency       : 'IDR',
+                asset          : 'USD',
+                fix_price      : 12000,
+                amount         : 20,
+            },
+            onClickConfirm: onConfirmation,
+        }
+        setPopupOptions(options);
+        setShowPopup(true);
+    }
+    const popupBuyCancelUnpaid = () => {
+        const options = {
+            title: localize('Confirm this payment?'),
+            message: localize('Make sure you have successfully sent the funds to the seller’s bank account or e-wallet mentioned above.'),
+            has_cancel: true,
+            cancel_text: localize('I didn\'t pay yet'),
+            confirm_text: localize('I\'ve paid'),
+            onClickConfirm: onConfirmation,
+        }
+        setPopupOptions(options);
+        setShowPopup(true);
+    }
+    const popupBuyCancelOrder = () => {
+        const options = {
+            title: localize('Cancel this order?'),
+            message: localize('There will be no refund after canceling the order. If you have paid, please do not cancel the order.'),
+            confirm_text: localize('Cancel this order'),
+            onClickConfirm: onConfirmation,
+        }
+        setPopupOptions(options);
+        setShowPopup(true);
+    }
+    return (
+        <div className='orders'>
+            <Button className='orders__button' primary onClick={popupSellConfirm}>Show popup sell confirm</Button>
+            <Button className='orders__button' primary onClick={popupBuyCancelUnpaid}>Show popup buy cancel unpaid</Button>
+            <Button className='orders__button' primary onClick={popupBuyCancelOrder}>Show popup buy cancel order</Button>
+            {show_popup && (
+                <div className='orders__dialog'>
+                    <Dialog
+                        is_visible={show_popup}
+                        disableApp={() => {
+                            /* do nothing // disableApp is a mandatory props in dialog */
+                        }}
+                        enableApp={() => {
+                            /* do nothing // enableApp is a mandatory props in dialog */
+                        }}
+                    >
+                        <Popup {...popup_options} onCancel={onCancelClick} />
+                    </Dialog>
+                </div>
+            )}
+        </div>
+    );
 };
  
 export default Orders;
---
+++ packages/p2p/src/components/orders/popup.jsx
@@ -0,0 +1,115 @@
+import React, { Fragment, Component }         from 'react';
+import PropTypes                              from 'prop-types';
+import { Formik, Field, Form }                from 'formik';
+import { Checkbox, Button, ThemedScrollbars } from 'deriv-components';
+import { localize }                           from 'deriv-translations';
+import IconClose                              from '../../assets/icon-close.jsx';
+
+class Popup extends Component {
+
+    handleSubmit = ({ setSubmitting }) => {
+        this.props.onClickConfirm();
+        setSubmitting(false);
+    }
+
+    render() {
+        const {
+            onCancel,
+            onClickConfirm,
+            title,
+            need_confirmation,
+            offer,
+            message,
+            has_cancel,
+            cancel_text,
+            confirm_text,
+        } = this.props;
+
+        return (
+            <Fragment>
+                <div className="orders__popup">
+                    <div className="orders__popup-header">
+                        <div className="orders__popup-header_wrapper">
+                            <h2 className="orders__popup-header--title">{title}</h2>
+                            <IconClose className="orders__popup-close_icon" onClick={onCancel} />
+                        </div>
+                    </div>
+                    {need_confirmation ? (
+                        <Formik
+                            initialValues={{
+                                need_confirmation: false,
+                            }}
+                            onSubmit={this.handleSubmit}
+                        >
+                            {({ isSubmitting, setFieldValue, values }) => (
+                                <Form noValidate>
+                                    <ThemedScrollbars autoHide style={{ height: '124px' }}>
+                                        <div className="orders__popup-content">
+                                            {message}
+                                            <div className="orders__popup-field">
+                                                <Field name="need_confirmation">
+                                                    {({ field }) => (
+                                                        <Checkbox
+                                                            {...field}
+                                                            onChange={() =>
+                                                                setFieldValue(
+                                                                    'need_confirmation',
+                                                                    !values.need_confirmation,
+                                                                )
+                                                            }
+                                                            defaultChecked={
+                                                                values.need_confirmation
+                                                            }
+                                                            label={localize(
+                                                                `I have received ${offer.currency} ${offer.fix_price}`,
+                                                            )}
+                                                            classNameLabel="orders__popup-field_text"
+                                                        />
+                                                    )}
+                                                </Field>
+                                            </div>
+                                        </div>
+                                    </ThemedScrollbars>
+                                    <div className="orders__popup-footer">
+                                        <Button
+                                            is_disabled={isSubmitting || !values.need_confirmation}
+                                            primary
+                                        >
+                                            {localize(`Release ${offer.asset} ${offer.amount}`)}
+                                        </Button>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
+                    ) : (
+                        <>
+                            <ThemedScrollbars autoHide style={{ height: '88px' }}>
+                                <div className="orders__popup-content">
+                                    {message}
+                                </div>
+                            </ThemedScrollbars>
+                            <div className="orders__popup-footer">
+                                {has_cancel && <Button onClick={onCancel} secondary>{cancel_text}</Button>}
+                                <Button onClick={onClickConfirm} primary>{confirm_text}</Button>
+                            </div>
+                        </>
+                    )}
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+Popup.propTypes = {
+    offer         : PropTypes.object,
+    onCancel      : PropTypes.func,
+    onClickConfirm: PropTypes.func,
+    confirm_text: PropTypes.string,
+    cancel_text: PropTypes.string,
+    title: PropTypes.string,
+    message: PropTypes.string,
+    has_cancel: PropTypes.bool,
+    need_confirmation: PropTypes.bool,
+};
+ 
+export default Popup;

5645de5fd7261053d6f4ad9d62c61e8238cf6929
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2019-12-09 10:43:39 +0800

    Applied some fixes

--- packages/p2p/src/components/my-profile/my-profile.jsx
+++ packages/p2p/src/components/my-profile/my-profile.jsx
@@ -5,15 +5,15 @@ import                   './my-profile.scss';
 
 // TODO: This is just mockup data. it will be removed after Adding BE API.
 const user_data = {
-    firstName              : 'johny',
-    lastName               : 'bravo',
-    securityDeposit        : 0,
-    buyTrades              : 23,
-    sellTrades             : 25,
-    pastMonthTrades        : 42,
-    pastMonthCompletionRate: 99,
-    averageReleaseTime     : 5,
-    averageReleaseUnit     : 'min',
+    first_name                : 'johny',
+    last_name                 : 'bravo',
+    security_deposit          : 0,
+    buy_trades                : 23,
+    sell_trades               : 25,
+    past_month_trades         : 42,
+    past_month_completion_rate: 99,
+    average_release_time      : 5,
+    average_release_unit      : 'min',
 };
 
 function capitalize (string) {
@@ -61,31 +61,31 @@ const MyProfile = () => {
     return (
         <div className='my-profile'>
             <div className='my-profile__name'>
-                {`${capitalize(user_data.firstName)} ${capitalize(user_data.lastName)}`}
+                {`${capitalize(user_data.first_name)} ${capitalize(user_data.last_name)}`}
             </div>
             <div className='my-profile__data'>
                 <Cell
                     title={localize('Security Deposit')}
-                    value={user_data.securityDeposit}
+                    value={user_data.security_deposit}
                 />
                 <Cell
                     title={localize('Trades')}
-                    value={user_data.buyTrades + user_data.sellTrades}
-                    upperSubValue={localize('{{ buyTrades }} buy', { buyTrades: user_data.buyTrades })}
-                    lowerSubValue={localize('{{ sellTrades }} sell', { sellTrades: user_data.sellTrades })}
+                    value={user_data.buy_trades + user_data.sell_trades}
+                    upperSubValue={localize('{{buy_trades}} buy', { buy_trades: user_data.buy_trades })}
+                    lowerSubValue={localize('{{sell_trades}} sell', { sell_trades: user_data.sell_trades })}
                 />
                 <Cell
                     title={localize('Trades in the last 30 days')}
-                    value={user_data.pastMonthTrades}
+                    value={user_data.past_month_trades}
                 />
                 <Cell
                     title={localize('30 day completion rate')}
-                    value={`${user_data.pastMonthCompletionRate}%`}
+                    value={`${user_data.past_month_completion_rate}%`}
                 />
                 <Cell
                     title={localize('Average release time')}
-                    value={user_data.averageReleaseTime}
-                    dimension={user_data.averageReleaseUnit}
+                    value={user_data.average_release_time}
+                    dimension={user_data.average_release_unit}
                 />
             </div>
         </div>

576b4184f6d20da8b078b3318b59d1894852a704
Bala <bala.krishna@regentmarkets.com>
2019-12-17 15:16:02 +0800

    validation message change

--- packages/trader/src/Stores/Modules/Contract/Constants/validation-rules.js
+++ packages/trader/src/Stores/Modules/Contract/Constants/validation-rules.js
@@ -8,7 +8,7 @@ const getValidationRules = () => ({
             ['custom' , { func: (value, options, store) => {
                 const profit = getProfitLossFromStore(store.root_store.modules);
                 return !(profit < 0 && -value > profit);
-            }, message: localize('Stop loss must be lower than current stop loss.') }],
+            }, message: localize('Stop loss must be lower than current loss.') }],
         ],
     },
     contract_update_take_profit: {
@@ -17,7 +17,7 @@ const getValidationRules = () => ({
             ['custom' , { func: (value, options, store) => {
                 const profit = getProfitLossFromStore(store.root_store.modules);
                 return !(profit > 0 && +value < profit);
-            }, message: localize('Take profit must be higher than current take profit.') }],
+            }, message: localize('Take profit must be higher than current profit.') }],
         ],
     },
 });

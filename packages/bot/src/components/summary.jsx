0b8986fd27a4b290d9d7749591824c39a686d3ea
Anton <anton@binary.com>
2019-12-20 17:29:01 +0800

    tooltips for the run panel

--- packages/bot/src/components/summary.jsx
+++ packages/bot/src/components/summary.jsx
@@ -2,6 +2,7 @@ import classNames    from 'classnames';
 import {
     Money,
     ThemedScrollbars,
+    Popover,
 }                    from 'deriv-components';
 import PropTypes     from 'prop-types';
 import React         from 'react';
@@ -12,10 +13,18 @@ import                    '../assets/sass/summary.scss';
 
 const SummaryTile = (props) => (
     <div className={'summary__tile'}>
-        <div className='summary__tile-title'>{props.title}</div>
-        <div className={classNames('summary__tile-content', props.contentClassName)}>
-            { props.content }
-        </div>
+        <Popover
+            className='run-panel__info'
+            classNameBubble='run-panel__info--bubble'
+            alignment='top'
+            message={ props.tooltip }
+        >
+            {/* <Icon icon='IcInfoOutline' className='run-panel__icon-info' /> */}
+            <div className='summary__tile-title'>{props.title}</div>
+            <div className={classNames('summary__tile-content', props.contentClassName)}>
+                { props.content }
+            </div>
+        </Popover>
     </div>
 );
 
@@ -40,32 +49,39 @@ const Summary = ({
                     <SummaryTile
                         title={localize('Total stake')}
                         content={ Money({ amount: total_stake, currency }) }
+                        tooltip={localize('Your total stake. This is reset when you click on “Clear stat” or when you refresh this page.')}
                     />
                     <SummaryTile
                         title={localize('Total payout')}
                         content={ Money({ amount: total_payout, currency }) }
+                        tooltip={localize('Your total payout. This is reset when you click on “Clear stat” or when you refresh this page.')}
                     />
                     <SummaryTile
                         title={localize('No. of runs')}
                         content={number_of_runs}
+                        tooltip={localize('The number of times your bot has run. Each run includes the execution of all the root blocks. This is reset when you click on “Clear stat” or when you refresh this page.')}
                     />
                     <SummaryTile
                         title={localize('Loss contracts')}
                         content={lost_contracts}
+                        tooltip={localize('The number of contracts you have lost. This is reset when you click on “Clear stat” or when you refresh this page.')}
                     />
                     <SummaryTile
                         title={localize('Win contracts')}
                         content={won_contracts}
+                        tooltip={localize('The number of contracts you have won. This is reset when you click on “Clear stat” or when you refresh this page.')}
                     />
                     <SummaryTile
                         title={localize('Profit/Loss')}
                         content={Money({ amount: total_profit, currency, has_sign: true })}
+                        tooltip={localize('A sum of all stakes you made')}
                         contentClassName={classNames(
                             'summary__tile-content',
                             'summary__amount', {
                                 'summary__amount--positive': total_profit > 0,
                                 'summary__amount--negative': total_profit < 0,
                             })}
+                        tooltip={localize('Your total profit/loss, which is the difference between your total payout and your total stake. This is reset when you click on “Clear stat” or when you refresh this page.')}
                     />
                 </div>
             </ThemedScrollbars>

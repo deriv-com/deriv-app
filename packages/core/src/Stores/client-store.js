294d6a7dc1c5866975c2206fee9b128d5b99bb1f
mustofa binary <mustofa@regentmarkets.com>
2019-12-20 17:14:03 +0800

    remove login and logout event

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -611,7 +611,7 @@ export default class ClientStore extends BaseStore {
 
                 // Client comes back from oauth and logs in
                 await this.root_store.segment.identifyEvent();
-                this.root_store.segment.track('login');
+
                 this.root_store.gtm.pushDataLayer({ event: 'login' });
             } else { // So it will send an authorize with the accepted token, to be handled by socket-general
                 await BinarySocket.authorize(client.token);
@@ -885,6 +885,7 @@ export default class ClientStore extends BaseStore {
     cleanUp() {
         this.root_store.gtm.pushDataLayer({ event: 'log_out' });
         this.loginid      = null;
+        this.user_id      = null;
         this.upgrade_info = undefined;
         this.accounts     = {};
         runInAction(async () => {
@@ -901,7 +902,7 @@ export default class ClientStore extends BaseStore {
 
         if (response.logout === 1) {
             this.cleanUp();
-            this.root_store.segment.track('logout');
+
             this.root_store.segment.reset();
             this.setLogout(true);
         }

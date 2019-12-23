e89c66a5f6426363d3ec9172679ead126c9e3694
Bala <bala.krishna@regentmarkets.com>
2019-12-23 16:42:45 +0800

    open positions with multiplier tab

--- packages/components/src/components/tabs/tabs.jsx
+++ packages/components/src/components/tabs/tabs.jsx
@@ -24,6 +24,29 @@ class Tabs extends Component {
         }
     }
 
+    setActiveLineStyle = () => {
+        if (this.props.header_fit_content && this.active_tab_ref && this.tabs_wrapper_ref) {
+            const tabs_wrapper_bounds = this.tabs_wrapper_ref.getBoundingClientRect();
+            const active_tab_bounds = this.active_tab_ref.getBoundingClientRect();
+            this.setState({
+                active_line_style: {
+                    left : active_tab_bounds.left - tabs_wrapper_bounds.left,
+                    width: active_tab_bounds.width,
+                },
+            });
+        }
+    }
+
+    setActiveTabRef = (ref) => {
+        this.active_tab_ref = ref;
+        this.setActiveLineStyle();
+    }
+
+    setTabsWrapperRef = (ref) => {
+        this.tabs_wrapper_ref = ref;
+        this.setActiveLineStyle();
+    }
+
     render() {
         const {
             children,
@@ -31,6 +54,7 @@ class Tabs extends Component {
             top,
             bottom,
             fit_content,
+            header_fit_content,
         } = this.props;
         const { active_index } = this.state;
         const tab_width = (100 / children.length).toFixed(2);
@@ -44,12 +68,15 @@ class Tabs extends Component {
                     )}
                 style={{ '--tab-width': `${tab_width}%` }}
             >
-                <ul className={
-                    classNames('dc-tabs__list', {
-                        'dc-tabs__list--top'        : top,
-                        'dc-tabs__list--bottom'     : bottom,
-                        'dc-tabs__list--fit-content': fit_content,
-                    })}
+                <ul
+                    className={
+                        classNames('dc-tabs__list', {
+                            'dc-tabs__list--top'               : top,
+                            'dc-tabs__list--bottom'            : bottom,
+                            'dc-tabs__list--fit-content'       : fit_content,
+                            'dc-tabs__list--header-fit-content': header_fit_content,
+                        })}
+                    ref={this.setTabsWrapperRef}
                 >
                     {children.map((child, index) => {
                         const { count, label } = child.props;
@@ -62,16 +89,21 @@ class Tabs extends Component {
                                 label={label}
                                 top={top}
                                 bottom={bottom}
+                                header_fit_content={header_fit_content}
+                                active_tab_ref={index === active_index ? this.setActiveTabRef : null}
                                 onClick={() => this.onTabItemClick(index)}
                             />
                         );
                     })}
-                    <span className={
-                        classNames('dc-tabs__active-line', {
-                            'dc-tabs__active-line--top'        : top,
-                            'dc-tabs__active-line--bottom'     : bottom,
-                            'dc-tabs__active-line--fit-content': fit_content,
-                        })}
+                    <span
+                        className={
+                            classNames('dc-tabs__active-line', {
+                                'dc-tabs__active-line--top'               : top,
+                                'dc-tabs__active-line--bottom'            : bottom,
+                                'dc-tabs__active-line--fit-content'       : fit_content,
+                                'dc-tabs__active-line--header-fit-content': header_fit_content,
+                            })}
+                        style= {this.state.active_line_style}
                     />
                 </ul>
                 <div className='dc-tabs__content'>

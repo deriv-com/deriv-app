8ee8da57ca74e1f516dd64f4ad76bee6c0791b57
negar <negar@binary.com>
2019-12-26 13:20:37 +0800

    update params according to API

--- packages/p2p/src/utils/websocket.js
+++ packages/p2p/src/utils/websocket.js
@@ -39,20 +39,23 @@ const getModifiedP2POfferList = (response) => {
         modified_response[i].offer_currency          = offer_currency;
         modified_response[i].advertiser_id           = response.list[i].agent_id;
         modified_response[i].offer_amount            = +response.list[i].max_amount;
+        // TODO: [p2p-replace-with-api] use display value from API when formatting works
         modified_response[i].display_offer_amount    = formatMoney(offer_currency, response.list[i].max_amount);
-        modified_response[i].advertiser_note         = response.list[i].description;
+        modified_response[i].advertiser_note         = response.list[i].offer_description;
         modified_response[i].offer_id                = response.list[i].offer_id;
         modified_response[i].transaction_currency    = transaction_currency;
         modified_response[i].min_transaction         = +response.list[i].min_amount;
+        // TODO: [p2p-replace-with-api] use display value from API when formatting works
         modified_response[i].display_min_transaction = formatMoney(offer_currency, response.list[i].min_amount);
-        modified_response[i].price_rate              = +response.list[i].price;
-        modified_response[i].display_price_rate      = formatMoney(transaction_currency, response.list[i].price);
+        modified_response[i].price_rate              = +response.list[i].rate;
+        // TODO: [p2p-replace-with-api] use display value from API when formatting works
+        modified_response[i].display_price_rate      = formatMoney(transaction_currency, response.list[i].rate);
         modified_response[i].type                    = response.list[i].type;
         modified_response[i].advertiser              = response.list[i].agent_name;
 
         // TOOD: [p2p-api-request] API should give us the allowed decimal places of local currency
         modified_response[i].transaction_currency_decimals = 2;
-        // (((response.list[i].price.toString().split('.') || [])[1]) || []).length;
+        // (((response.list[i].rate_display.toString().split('.') || [])[1]) || []).length;
 
         modified_response[i].offer_currency_decimals =
             ObjectUtils.getPropertyValue(initial_responses, [
@@ -69,26 +72,25 @@ const getModifiedP2POfferList = (response) => {
 const getModifiedP2POrder = (response) => {
     const modified_response = {};
 
-    modified_response.type = response.type;
-
+    modified_response.type                       = response.type;
     modified_response.offer_amount               = +response.amount;
-    modified_response.display_offer_amount       = formatMoney(response.account_currency, response.amount);
+    // TODO: [p2p-replace-with-api] use display value from API when formatting works
+    modified_response.display_offer_amount       = formatMoney(response.local_currency, response.amount);
     modified_response.order_purchase_datetime    = getFormattedDateString(
         new Date(convertToMillis(response.created_time))
     );
-    modified_response.advertiser_notes           = response.description;
+    modified_response.advertiser_notes           = response.offer_description;
     modified_response.order_id                   = response.order_id;
     modified_response.offer_currency             = response.account_currency;
     modified_response.status                     = response.status;
     modified_response.advertiser_name            = response.agent_name;
-    modified_response.price_rate                 = +response.price;
-    modified_response.display_price_rate         = formatMoney(response.local_currency, response.price);
+    modified_response.price_rate                 = +response.rate;
+    // TODO: [p2p-replace-with-api] use display value from API when formatting works
+    modified_response.display_price_rate         = formatMoney(response.local_currency, response.rate);
     modified_response.transaction_currency       = response.local_currency;
-    modified_response.transaction_amount         = +response.transaction_amount;
-    modified_response.display_transaction_amount = formatMoney(response.local_currency, response.transaction_amount);
-
-    // TOOD: calculate this based on expiry time - created time
-    // const remaining_seconds = (+response.expiry_time) - (+response.created_time);
+    modified_response.transaction_amount         = +response.price;
+    // TODO: [p2p-replace-with-api] use display value from API when formatting works
+    modified_response.display_transaction_amount = formatMoney(response.local_currency, response.price);
     modified_response.order_expiry_millis         = convertToMillis(response.expiry_time);
 
     return modified_response;

4f2acaccd2831de5456bb296991847ade77ea6bd
Aaron Imming <aaim@protonmail.com>
2020-01-13 16:54:37 +0800

    Re-order imports

--- packages/core/src/Modules/Cashier/Containers/cashier.jsx
+++ packages/core/src/Modules/Cashier/Containers/cashier.jsx
@@ -1,8 +1,8 @@
-import { VerticalTab }   from '@deriv/components';
-import { localize }      from '@deriv/translations';
 import PropTypes         from 'prop-types';
 import React             from 'react';
 import { withRouter }    from 'react-router-dom';
+import { VerticalTab }   from '@deriv/components';
+import { localize }      from '@deriv/translations';
 import { FadeWrapper }   from 'App/Components/Animations';
 import routes            from 'Constants/routes';
 import { connect }       from 'Stores/connect';

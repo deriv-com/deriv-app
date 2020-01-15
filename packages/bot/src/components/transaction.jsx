65bef42ed2c312c85711701ff6d0da58e9ca4746
Aaron Imming <aaim@protonmail.com>
2020-01-15 15:53:28 +0800

    Restore original TransactionFieldLoader

--- packages/bot/src/components/transaction.jsx
+++ packages/bot/src/components/transaction.jsx
@@ -32,7 +32,7 @@ const TransactionIconWithText = ({
     </React.Fragment>
 );
 
-const TransactionFieldLoader = ({ width }) => (
+const TransactionFieldLoader = () => (
     <ContentLoader
         className='transactions__loader'
         height={10}
@@ -41,7 +41,7 @@ const TransactionFieldLoader = ({ width }) => (
         primaryColor={'var(--general-section-1)'}
         secondaryColor={'var(--general-hover)'}
     >
-        <rect x='0' y='0' rx='0' ry='0' width={width || '100'} height='12' />
+        <rect x='0' y='0' rx='0' ry='0' width='100' height='12' />
     </ContentLoader>
 );
 

d9758733effc42f282fb9a1c4e872f9bfff15442
Kelly Anyi <kelly@binary.com>
2020-01-20 16:30:22 +0800

    render remaining time once we get date_expiry is poc

--- packages/trader/src/App/Components/Elements/PositionsDrawer/PositionsDrawerCard/multiplier-close-actions.jsx
+++ packages/trader/src/App/Components/Elements/PositionsDrawer/PositionsDrawerCard/multiplier-close-actions.jsx
@@ -13,39 +13,48 @@ const MultiplierCloseActions  = ({
     contract_info,
     is_sell_requested,
 }) => {
+    const {
+        contract_id,
+        deal_cancellation: {
+            date_expiry: deal_cancellation_date_expiry,
+        } = {},
+        profit,
+    } = contract_info;
+
     const is_valid_to_cancel = isValidToCancel(contract_info);
-    
+
     return (
         <React.Fragment>
             <Button
-                id={`dt_drawer_card_${contract_info.contract_id}_button`}
+                id={`dt_drawer_card_${contract_id}_button`}
                 className={classNames(
                     className, {
                         'btn--loading': is_sell_requested,
                     })}
-                is_disabled={is_sell_requested || (+contract_info.profit < 0 && is_valid_to_cancel)}
+                is_disabled={is_sell_requested || (+profit < 0 && is_valid_to_cancel)}
                 text={localize('Close')}
-                onClick={() => onClickSell(contract_info.contract_id, true)}
+                onClick={() => onClickSell(contract_id, true)}
                 secondary
             />
             {is_valid_to_cancel &&
                 <Button
-                    id={`dt_drawer_card_${contract_info.contract_id}_cancel_button`}
+                    id={`dt_drawer_card_${contract_id}_cancel_button`}
                     className='btn--cancel'
-                    is_disabled={+contract_info.profit >= 0}
-                    onClick={() => onClickCancel(contract_info.contract_id, true)}
+                    is_disabled={+profit >= 0}
+                    onClick={() => onClickCancel(contract_id, true)}
                     secondary
                 >
                     {localize('Cancel')}
-                    <RemainingTime
-                        end_time={contract_info.deal_cancellation.date_expiry}
-                        format='mm:ss'
-                    />
+                    {deal_cancellation_date_expiry &&
+                        <RemainingTime
+                            end_time={deal_cancellation_date_expiry}
+                            format='mm:ss'
+                        />
+                    }
                 </Button>
             }
         </React.Fragment>
     );
-    
 };
 
 MultiplierCloseActions.propTypes = {

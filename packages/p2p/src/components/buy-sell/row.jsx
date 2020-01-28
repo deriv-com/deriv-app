7f9af4819f40b93b29bb5d290161da3a849bbb02
negar <negar@binary.com>
2020-01-28 16:31:20 +0800

    update buy/sell popup fields

--- packages/p2p/src/components/buy-sell/row.jsx
+++ packages/p2p/src/components/buy-sell/row.jsx
@@ -33,7 +33,7 @@ export const RowComponent = React.memo(({ data, is_buy, setSelectedAd, style }) 
         <div style={style}>
             <Table.Row>
                 <Table.Cell>{data.advertiser_name}</Table.Cell>
-                <Table.Cell>{data.display_min_transaction}-{data.display_max_transaction}{' '}{data.offer_currency}</Table.Cell>
+                <Table.Cell>{data.display_min_transaction}&ndash;{data.display_max_transaction}{' '}{data.offer_currency}</Table.Cell>
                 <Table.Cell className='buy-sell__price'>{data.display_price_rate}{' '}{data.transaction_currency}</Table.Cell>
                 <Table.Cell>{data.display_payment_method}</Table.Cell>
                 {!is_agent ? (

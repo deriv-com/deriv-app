4f86af81a2a5ccf608b828f2f7830db80f601ab4
Salar Hafezi <salar@firstsource.io>
2020-02-02 17:39:53 +0330

    add logic for crypto currencies

--- packages/components/src/components/money/money.jsx
+++ packages/components/src/components/money/money.jsx
@@ -1,7 +1,8 @@
-import classNames    from 'classnames';
-import PropTypes     from 'prop-types';
-import React         from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
 import CurrencyUtils from '@deriv/shared/utils/currency';
+import CryptoMoney from '../crypto-money';
 
 const Money = ({
     amount,
@@ -17,13 +18,21 @@ const Money = ({
 
     // if it's formatted already then don't make any changes unless we should remove extra -/+ signs
     const value = (has_sign || should_format) ? Math.abs(amount) : amount;
-    const final_amount = should_format ? CurrencyUtils.formatMoney(currency, value, true) : value;
-
+    const decimalPart = CurrencyUtils.getDecimalPart(amount);
+    const needsCryptoToggle = CurrencyUtils.isCryptocurrency(currency) && decimalPart && decimalPart.length > CurrencyUtils.getDecimalPlaces(currency);
+    let final_amount = null;
+    if (needsCryptoToggle) {
+        final_amount = CurrencyUtils.getCryptoFormat(value);
+    } else {
+        final_amount = should_format ? CurrencyUtils.formatMoney(currency, value, true) : value;
+    }
     return (
         <React.Fragment>
             {has_sign && sign}
-            <span className={classNames(className, 'symbols', `symbols--${currency.toLowerCase()}`)} />
-            {final_amount}
+            <span
+                className={classNames(className, 'symbols', `symbols--${currency.toLowerCase()}`)}
+            />
+            {needsCryptoToggle ? <CryptoMoney {...final_amount} /> : final_amount}
         </React.Fragment>
     );
 };
@@ -33,9 +42,9 @@ Money.propTypes = {
         PropTypes.number,
         PropTypes.string,
     ]),
-    className    : PropTypes.string,
-    currency     : PropTypes.string,
-    has_sign     : PropTypes.bool,
+    className: PropTypes.string,
+    currency: PropTypes.string,
+    has_sign: PropTypes.bool,
     should_format: PropTypes.bool,
 };
 

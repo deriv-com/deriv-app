5f346c4156366f9df31bf87e6a1740afd0e24b30
negar <negar@binary.com>
2020-02-03 13:24:44 +0800

    prettify

--- packages/components/src/components/vertical-tab/vertical-tab-headers.jsx
+++ packages/components/src/components/vertical-tab/vertical-tab-headers.jsx
@@ -1,14 +1,12 @@
-import React                  from 'react';
-import VerticalTabWrapper     from 'Components/vertical-tab/vertical-tab-wrapper.jsx';
-import VerticalTabHeader      from 'Components/vertical-tab/vertical-tab-header.jsx';
+import React from 'react';
+import VerticalTabWrapper from 'Components/vertical-tab/vertical-tab-wrapper.jsx';
+import VerticalTabHeader from 'Components/vertical-tab/vertical-tab-header.jsx';
 import VerticalTabHeaderTitle from 'Components/vertical-tab/vertical-tab-header-title.jsx';
 
-const VerticalTabHeaders = (props) => (
+const VerticalTabHeaders = props => (
     <VerticalTabWrapper className={props.className}>
-        { props.header_title &&
-            <VerticalTabHeaderTitle header_title={props.header_title} />
-        }
-        { props.items.map((item, idx) => (
+        {props.header_title && <VerticalTabHeaderTitle header_title={props.header_title} />}
+        {props.items.map((item, idx) => (
             <VerticalTabHeader
                 item={item}
                 onChange={props.onChange || undefined}

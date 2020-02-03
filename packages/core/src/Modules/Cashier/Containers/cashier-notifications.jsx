eb0918cf72bc04d58bc9f615f96518b82a02d637
mustofa binary <mustofa@regentmarkets.com>
2020-02-03 10:51:55 +0800

    prettify

--- packages/core/src/Modules/Cashier/Containers/cashier-notifications.jsx
+++ packages/core/src/Modules/Cashier/Containers/cashier-notifications.jsx
@@ -1,26 +1,19 @@
-import React             from 'react';
-import PropTypes         from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Icon, Counter } from '@deriv/components';
-import { connect }       from 'Stores/connect';
+import { connect } from 'Stores/connect';
 
 const CashierNotifications = ({ p2p_notifications }) => (
     <React.Fragment>
         <Icon icon='IcCashier' className='header__icon' />
-        {!!p2p_notifications &&
-            <Counter
-                className='cashier__counter'
-                count={p2p_notifications}
-            />
-        }
+        {!!p2p_notifications && <Counter className='cashier__counter' count={p2p_notifications} />}
     </React.Fragment>
 );
 
 CashierNotifications.propTypes = {
     notifications: PropTypes.number,
-}
+};
 
-export default connect(
-    ({ modules }) => ({
-        p2p_notifications: modules.cashier.p2p_notifications,
-    }),
-)(CashierNotifications);
+export default connect(({ modules }) => ({
+    p2p_notifications: modules.cashier.p2p_notifications,
+}))(CashierNotifications);

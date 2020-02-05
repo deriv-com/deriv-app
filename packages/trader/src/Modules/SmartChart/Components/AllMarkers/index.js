714e3e659017895fe463de2d4efe088eca713abe
Brandon <52795236+brandon-binary@users.noreply.github.com>
2020-02-05 17:19:48 +0800

    Re-export imports

--- packages/trader/src/Modules/SmartChart/Components/AllMarkers/index.js
+++ packages/trader/src/Modules/SmartChart/Components/AllMarkers/index.js
@@ -3,12 +3,6 @@
 // 2- Please read RawMarker.jsx in https://github.com/binary-com/SmartCharts
 // 3- Please read contract-store.js & trade.jsx carefully
 
-import TickContract from './tick-contract.jsx';
-import NonTickContract from './non-tick-contract.jsx';
-import DigitContract from './digit-contract.jsx';
-
-export default {
-    TickContract,
-    NonTickContract,
-    DigitContract,
-};
+export { default as TickContract } from './tick-contract.jsx';
+export { default as NonTickContract } from './non-tick-contract.jsx';
+export { default as DigitContract } from './digit-contract.jsx';

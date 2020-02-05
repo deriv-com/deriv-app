1e44c6515bd8665958f0834b41543b24ca9e94bf
arash <arash@firstsource.io>
2020-02-05 15:21:11 +0330

    convert native datepicker to functional component

--- packages/components/src/components/native-datepicker/native-datepicker.jsx
+++ packages/components/src/components/native-datepicker/native-datepicker.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { toMoment, daysFromTodayTo } from '@deriv/shared/src/utils/date/date-time';
 
-class NativeDatePicker extends React.Component {
-    myRef = React.createRef();
-    state = {
-        date: null,
+const NativeDatePicker = props => {
+    const myRef = useRef();
+    const clickHandler = () => {
+        myRef.current.click();
     };
-    clickHandler = () => {
-        this.myRef.current.click();
+    const onChangeHandler = e => {
+        props.onChange(e.target.value);
+        console.log('today: ' + toMoment().format('YYYY-MM-DD'));
+        console.log('diff: ' + daysFromTodayTo(e.target.value));
     };
-    onChangeHandler = e => {
-        this.setState({
-            date: e.target.value,
-        });
-        this.props.onChange(e.target.value);
-    };
-    render() {
-        return (
-            <div id='dc-native-datepicker'>
-                <span onClick={this.clickHandler}>Pick an end date</span>
-                <input type='date' id='native-calender' ref={this.myRef} onChange={this.onChangeHandler} />
-            </div>
-        );
-    }
-}
+    const min_date = props.min_date ? toMoment(props.min_date).format('YYYY-MM-DD') : toMoment().format('YYYY-MM-DD');
+    const max_date = props.max_date ? toMoment(props.max_date).format('YYYY-MM-DD') : null;
+    return (
+        <div id='dc-native-datepicker'>
+            <span onClick={clickHandler}>Pick an end date</span>
+            <input
+                type='date'
+                id='native-calender'
+                ref={myRef}
+                onChange={onChangeHandler}
+                min={min_date}
+                max={max_date}
+            />
+        </div>
+    );
+};
 export default NativeDatePicker;

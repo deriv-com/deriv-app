af22b53df52a59255633c08465b9b7c3f76ecea5
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2020-02-09 12:21:58 +0330

    Updated compoent storybook

--- packages/components/stories/circular-progress/circular-progress.stories.js
+++ packages/components/stories/circular-progress/circular-progress.stories.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { storiesOf } from '@storybook/react';
 import CircularProgress from '../../src/components/circular-progress/index';
 import Theme from '../shared/theme';
 
+function useInterval(callback, delay) {
+    const savedCallback = useRef();
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
+
+function formatTime(seconds) {
+    const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+    const remaining_seconds = String(seconds % 60).padStart(2, '0');
+    return `${hours}:${minutes}:${remaining_seconds}`;
+}
+
 const progresses = [
     {
+        icon: 'IcInfo',
         is_clockwise: true,
         is_dark: false,
         value: 75,
     },
     {
+        icon: 'IcBrandDtrader',
         is_clockwise: true,
         is_dark: false,
         value: 25,
     },
     {
+        icon: 'IcLock',
         is_clockwise: true,
         is_dark: true,
         value: 50,
     },
     {
+        icon: 'IcTrade',
         is_clockwise: true,
         is_dark: true,
         value: 100,
@@ -43,15 +71,20 @@ storiesOf('Circular Progress', module)
             {progresses.map((item, index) => {
                 return (
                     <Theme is_dark={item.is_dark} key={index}>
-                        <CircularProgress progress={item.value} />
+                        <CircularProgress progress={item.value} icon={item.icon} />
                     </Theme>
                 );
             })}
         </div>
     ))
     .add('Timer', () => {
-        const initial_time = 3600; // 3600 seconds == 1 hour
-        const [remaining_time, setRemainingTime] = React.useState(initial_time);
+        const initial_time = 120; // 3600 seconds == 1 hour
+        const [remaining_time, setRemainingTime] = useState(initial_time);
+        useInterval(() => {
+            if (remaining_time > 0) {
+                setRemainingTime(remaining_time - 1);
+            }
+        }, 1000);
         return (
             <div
                 style={{
@@ -65,13 +98,18 @@ storiesOf('Circular Progress', module)
                     paddingTop: '10px',
                 }}
             >
-                {progresses.map((item, index) => {
-                    return (
-                        <Theme is_dark={item.is_dark} key={index}>
-                            <CircularProgress progress={item.value} />
-                        </Theme>
-                    );
-                })}
+                <Theme>
+                    <CircularProgress progress={(remaining_time / initial_time) * 100} />
+                    <p
+                        style={{
+                            fontSize: '14px',
+                            paddingTop: '10px',
+                            textAlign: 'center',
+                        }}
+                    >
+                        {formatTime(remaining_time)}
+                    </p>
+                </Theme>
             </div>
         );
     });

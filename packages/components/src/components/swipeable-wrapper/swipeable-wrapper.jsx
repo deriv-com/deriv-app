b356979815e55ae188e62f25c66df9ac84edd336
Kelly Anyi <kelly@binary.com>
2020-02-12 15:33:10 +0800

    add navigation arrow

--- packages/components/src/components/swipeable-wrapper/swipeable-wrapper.jsx
+++ packages/components/src/components/swipeable-wrapper/swipeable-wrapper.jsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { Swipeable } from 'react-swipeable';
+import Icon from 'Components/icon';
 
 const SwipeableWrapper = ({ children, className, ...props }) => {
     const [active_index, setActiveIndex] = useState(0);
@@ -24,17 +25,35 @@ const SwipeableWrapper = ({ children, className, ...props }) => {
     });
 
     return (
-        <Swipeable
-            style={{
-                transform: `translateX(${active_index * -100}vw)`,
-            }}
-            className={classNames('dc-swipeable', className)}
-            onSwipedLeft={swipedLeft}
-            onSwipedRight={swipedRight}
-            {...props}
-        >
-            {childrenWithWrapperDiv}
-        </Swipeable>
+        <div className='dc-swipeable'>
+            <Swipeable
+                style={{
+                    transform: `translateX(${active_index * -100}vw)`,
+                }}
+                className={classNames('dc-swipeable__view', className)}
+                onSwipedLeft={swipedLeft}
+                onSwipedRight={swipedRight}
+                {...props}
+            >
+                {childrenWithWrapperDiv}
+            </Swipeable>
+            <nav className='dc-swipeable__nav'>
+                <Icon
+                    className='dc-swipeable__nav__item'
+                    icon='IcChevronDoubleLeft'
+                    size={24}
+                    onClick={swipedRight}
+                    color={active_index === 0 ? 'disabled' : ''}
+                />
+                <Icon
+                    className='dc-swipeable__nav__item'
+                    icon='IcChevronDoubleRight'
+                    size={24}
+                    onClick={swipedLeft}
+                    color={active_index + 1 === React.Children.toArray(children).length ? 'disabled' : ''}
+                />
+            </nav>
+        </div>
     );
 };
 

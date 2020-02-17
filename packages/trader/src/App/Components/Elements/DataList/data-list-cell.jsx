86a12628d07a7fa5593048cc848e6f79a666c7d6
Bala <bala.krishna@regentmarkets.com>
2020-02-17 11:40:56 +0800

    added page overlay mobile wrapper and other fixes

--- packages/trader/src/App/Components/Elements/DataList/data-list-cell.jsx
+++ packages/trader/src/App/Components/Elements/DataList/data-list-cell.jsx
@@ -11,9 +11,9 @@ class DataListCell extends React.PureComponent {
         return (
             <div className={classNames(className, column.col_index)}>
                 {!is_footer && (
-                    <span className={classNames(`${column.col_index}__row-title`, 'data-list__row-title')}>
-                        {title}
-                    </span>
+                    <div className={classNames(`${column.col_index}__row-title`, 'data-list__row-title')}>
+                        {column.renderHeader ? column.renderHeader({ title }) : title}
+                    </div>
                 )}
                 <div className='data-list__row-content'>
                     {column.renderCellContent

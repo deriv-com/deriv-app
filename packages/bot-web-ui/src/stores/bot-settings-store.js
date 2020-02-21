7cfa7bbe0ca04a53a1a37d85b1be8fe6ff1e3891
Aaron Imming <aaim@protonmail.com>
2020-02-21 16:47:59 +0800

    Persist settings in localStorage

--- packages/bot-web-ui/src/stores/bot-settings-store.js
+++ packages/bot-web-ui/src/stores/bot-settings-store.js
@@ -1,20 +1,23 @@
 import { observable, action } from 'mobx';
+import { getSetting, storeSetting } from '../utils/settings';
 
 export default class BotSettingsStore {
     constructor(root_store) {
         this.root_store = root_store;
     }
 
-    @observable should_restart_on_buy_sell_error = false;
-    @observable should_restart_on_last_trade_error = true;
+    @observable should_restart_on_buy_sell_error = getSetting('restart_on_buy_sell_error') || false;
+    @observable should_restart_on_last_trade_error = getSetting('restart_on_last_trade_error') || true;
 
     @action.bound
     setRestartOnBuySellError(should_restart) {
         this.should_restart_on_buy_sell_error = should_restart;
+        storeSetting('restart_on_buy_sell_error', should_restart);
     }
 
     @action.bound
     setRestartOnLastTradeError(should_restart) {
         this.should_restart_on_last_trade_error = should_restart;
+        storeSetting('restart_on_last_trade_error', should_restart);
     }
 }

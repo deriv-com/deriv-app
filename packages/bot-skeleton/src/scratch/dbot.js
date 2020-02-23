7ff9a95940de77bc966dd1bd4a067853a31df3a8
mehrzad-fs <mehrzad@firstsource.io>
2020-02-23 08:31:50 +0330

    initial workspace scale updated

--- packages/bot-skeleton/src/scratch/dbot.js
+++ packages/bot-skeleton/src/scratch/dbot.js
@@ -28,16 +28,23 @@ class DBot {
             __webpack_public_path__ = public_path; // eslint-disable-line no-global-assign
             ApiHelpers.setInstance(api_helpers_store);
             DBotStore.setInstance(store);
+            const window_width = window.innerWidth;
+            let workspaceScale = 1;
 
             const { handleFileChange, onBotNameTyped } = DBotStore.instance;
 
+            if (window_width < 1640) {
+                const scratch_div_width = document.getElementById('scratch_div').offsetWidth;
+                const zoom_scale = scratch_div_width / window_width;
+                workspaceScale = zoom_scale;
+            }
             const el_scratch_div = document.getElementById('scratch_div');
             this.workspace = Blockly.inject(el_scratch_div, {
                 grid: { spacing: 40, length: 11, colour: '#f3f3f3' },
                 media: `${__webpack_public_path__}media/`,
                 toolbox: toolbox_xml,
                 trashcan: true,
-                zoom: { wheel: true, startScale: config.workspaces.mainWorkspaceStartScale },
+                zoom: { wheel: true, startScale: workspaceScale },
             });
 
             this.workspace.cached_xml = { main: main_xml, toolbox: toolbox_xml };
@@ -66,6 +73,9 @@ class DBot {
             }
             Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(strategy_to_load), this.workspace);
             onBotNameTyped(file_name);
+            if (window_width < 1640) {
+                this.workspace.cleanUp();
+            }
             this.workspace.clearUndo();
 
             window.addEventListener('resize', () => onWorkspaceResize());

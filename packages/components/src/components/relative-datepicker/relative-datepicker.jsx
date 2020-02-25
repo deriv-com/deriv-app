057d110d3c7a5cdb8d53a2e04a6825439d907965
Mohammadreza Ghorbani <reza@firstsource.io>
2020-02-25 16:47:28 +0330

    Fixup input[date] attribute

--- packages/components/src/components/relative-datepicker/relative-datepicker.jsx
+++ packages/components/src/components/relative-datepicker/relative-datepicker.jsx
@@ -27,7 +27,7 @@ const RelativeDatepicker = ({ onChange, min_date, max_date, title }) => {
                 type='date'
                 ref={hidden_input_ref}
                 onChange={onChangeHandler}
-                min_date={min_date_moment}
+                min={min_date_moment}
                 max={max_date_moment}
                 className='dc-relative-datepicker__input'
             />

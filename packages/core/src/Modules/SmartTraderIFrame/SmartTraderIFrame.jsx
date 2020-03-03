b750fd9374c45a12eee080e7c4113331403848db
Aaron Imming <aaim@protonmail.com>
2020-03-03 10:41:25 +0800

    Synchronise w/ SmartTrader on LS changes

---
+++ packages/core/src/Modules/SmartTraderIFrame/SmartTraderIFrame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SmartTraderIFrame = () => (
+    <iframe
+        id='localstorage-sync'
+        src='https://aaron-binary3.binary.sx/br_ls-sync/localstorage-sync.html'
+        style={{ display: 'none', visibility: 'hidden' }}
+    />
+);
+
+export default SmartTraderIFrame;

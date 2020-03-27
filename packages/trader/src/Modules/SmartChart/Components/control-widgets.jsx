da0c078810bc28e463e3e787b6fa5f8c3cb8220a
shayanypn <shayanypn@gmail.com>
2020-03-27 07:25:25 +0430

    cleanup

--- packages/trader/src/Modules/SmartChart/Components/control-widgets.jsx
+++ packages/trader/src/Modules/SmartChart/Components/control-widgets.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { DesktopWrapper } from '@deriv/components';
-import { ChartMode, Comparison, DrawTools, Share, StudyLegend, Views } from 'Modules/SmartChart';
+import { ChartMode, DrawTools, Share, StudyLegend, Views } from 'Modules/SmartChart';
 
 const ControlWidgets = ({ updateChartType, updateGranularity }) => (
     <React.Fragment>

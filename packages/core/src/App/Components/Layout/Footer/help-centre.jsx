e61e3fb1a03481f398ab0cb5af5d2667ea4c742c
cakasuma <amammustofa@gmail.com>
2020-03-30 13:06:57 +0800

    change to fcc

--- packages/core/src/App/Components/Layout/Footer/help-centre.jsx
+++ packages/core/src/App/Components/Layout/Footer/help-centre.jsx
@@ -2,25 +2,17 @@ import React from 'react';
 import { Popover, Icon } from '@deriv/components';
 import { localize } from '@deriv/translations';
 
-class HelpCentre extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Popover
-                className='footer__link'
-                classNameBubble='help-centre__tooltip'
-                alignment='top'
-                message={localize('Help centre')}
-            >
-                <a href='https://deriv.com/help-centre/' id='dt_help_centre' target='_blank' rel='noopener'>
-                    <Icon icon='IcHelpCentre' className='footer__icon' />
-                </a>
-            </Popover>
-        );
-    }
-}
+const HelpCentre = () => (
+    <Popover
+        className='footer__link'
+        classNameBubble='help-centre__tooltip'
+        alignment='top'
+        message={localize('Help centre')}
+    >
+        <a href='https://deriv.com/help-centre/' id='dt_help_centre' target='_blank' rel='noopener noreferrer'>
+            <Icon icon='IcHelpCentre' className='footer__icon' />
+        </a>
+    </Popover>
+);
 
 export { HelpCentre };

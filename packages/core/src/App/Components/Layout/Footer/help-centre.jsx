86078b235e891cfa8c4afe3bd8018b5f95264d6c
cakasuma <amammustofa@gmail.com>
2020-03-30 12:17:59 +0800

    add icona nad add help centre to footer

---
+++ packages/core/src/App/Components/Layout/Footer/help-centre.jsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Popover } from '@deriv/components';
+import { Icon } from '@deriv/components';
+import { localize } from '@deriv/translations';
+
+class HelpCentre extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Popover
+                className='footer__link'
+                classNameBubble='help-centre__tooltip'
+                alignment='top'
+                message={localize('Help centre')}
+            >
+                <a href='https://deriv.com/help-centre/' id='dt_help_centre' target='_blank' rel='noopener'>
+                    <Icon icon='IcHelpCentre' className='footer__icon' />
+                </a>
+            </Popover>
+        );
+    }
+}
+
+export { HelpCentre };

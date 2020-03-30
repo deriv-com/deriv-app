136f978ce4b067a0a8d1304d5bc7c419cc42bbbe
cakasuma <amammustofa@gmail.com>
2020-03-30 15:16:08 +0800

    svgo and handle lang

--- packages/core/src/App/Components/Layout/Footer/help-centre.jsx
+++ packages/core/src/App/Components/Layout/Footer/help-centre.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Popover, Icon } from '@deriv/components';
-import { localize } from '@deriv/translations';
+import { localize, getLanguage } from '@deriv/translations';
 
-const HelpCentre = () => (
-    <Popover
-        className='footer__link'
-        classNameBubble='help-centre__tooltip'
-        alignment='top'
-        message={localize('Help centre')}
-    >
-        <a href='https://deriv.com/help-centre/' id='dt_help_centre' target='_blank' rel='noopener noreferrer'>
-            <Icon icon='IcHelpCentre' className='footer__icon' />
-        </a>
-    </Popover>
-);
+const HelpCentre = () => {
+    const lang = getLanguage().toLowerCase();
+    const link_lang = lang === 'en' ? '' : `/${lang}`;
+
+    return (
+        <Popover
+            className='footer__link'
+            classNameBubble='help-centre__tooltip'
+            alignment='top'
+            message={localize('Help centre')}
+        >
+            <a
+                href={`https://deriv.com${link_lang}/help-centre/`}
+                id='dt_help_centre'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <Icon icon='IcHelpCentre' className='footer__icon' />
+            </a>
+        </Popover>
+    );
+};
 
 export { HelpCentre };

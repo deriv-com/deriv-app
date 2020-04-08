db8c58aa1f345cb59d322d727f4940503a92ac82
zakibinary <zaki@regentmarkets.com>
2020-04-08 11:41:24 +0800

    virtual keyboard landscape blocker fix - part 2

--- packages/core/src/Modules/Cashier/Containers/cashier.jsx
+++ packages/core/src/Modules/Cashier/Containers/cashier.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { PageOverlay, VerticalTab, DesktopWrapper, MobileWrapper, Div100vhContainer } from '@deriv/components';
 import { localize } from '@deriv/translations';
-import { mobileOSDetect } from '@deriv/shared/utils/os';
+import { isMobile, isTouchDevice } from '@deriv/shared/utils/screen';
 import { FadeWrapper } from 'App/Components/Animations';
 import routes from 'Constants/routes';
 import { connect } from 'Stores/connect';
@@ -21,7 +21,7 @@ class Cashier extends React.Component {
         // TODO: Remove L21, L31, and L38 code blocks once landscape design is ready
         // doughflow iframe inconjunction with android's virtual keyboard causes issues with css screen height calculation (thus falsely triggering landscape blocker in Android)
         // this is due to the onscreen virtual keyboard resizing the innerHeight of the window and ignoring the actual height of content within the iframe
-        if (mobileOSDetect() === 'Android') {
+        if (isMobile() && isTouchDevice()) {
             window.addEventListener('resize', this.handleOnScreenKeyboard);
         }
     }
@@ -29,21 +29,22 @@ class Cashier extends React.Component {
         this.props.toggleCashier();
 
         // cleanup onscreen keyboard class suffix and eventlistener for landscape blocker upon unMount
-        if (mobileOSDetect() === 'Android') {
+        if (isMobile() && isTouchDevice()) {
             window.removeEventListener('resize', this.handleOnScreenKeyboard);
             if (el_landscape_blocker) el_landscape_blocker.classList.remove('landscape-blocker--keyboard-visible');
         }
     }
 
     handleOnScreenKeyboard = () => {
-        // android's virtual keyboard is a minimum of 205px
-        // so we are comparing the android device's height onMount and the height after the keyboard causes the resize event
-        const is_android_keyboard = this.state.device_height - 205 <= window.innerHeight;
-        if (el_landscape_blocker && is_android_keyboard) {
-            el_landscape_blocker.classList.add('landscape-blocker--keyboard-visible');
-        }
-        if (el_landscape_blocker && window.innerHeight === this.state.device_height) {
-            el_landscape_blocker.classList.remove('landscape-blocker--keyboard-visible');
+        // We are listening to resize window resize events on mobile,
+        // and comparing the android device's height onMount and the height after the keyboard causes the resize event
+        const is_android_keyboard = this.state.device_height !== window.innerHeight;
+        if (el_landscape_blocker) {
+            if (is_android_keyboard) {
+                el_landscape_blocker.classList.add('landscape-blocker--keyboard-visible');
+            } else {
+                el_landscape_blocker.classList.remove('landscape-blocker--keyboard-visible');
+            }
         }
     };
 

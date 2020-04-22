367661e90bf14cdd593e6d6480835e3bc3edb781
Kelly Anyi <kelly@binary.com>
2020-04-22 19:31:46 +0800

    remove unused function

--- packages/trader/src/App/Components/Elements/ContractDrawer/contract-drawer.jsx
+++ packages/trader/src/App/Components/Elements/ContractDrawer/contract-drawer.jsx
@@ -7,7 +7,7 @@ import { DesktopWrapper, MobileWrapper } from '@deriv/components';
 import routes from 'Constants/routes';
 import ContractAudit from 'App/Components/Elements/ContractAudit';
 import { PositionsCardLoader } from 'App/Components/Elements/ContentLoader';
-import { getDurationPeriod, getDurationTime, getDurationUnitText } from 'Stores/Modules/Portfolio/Helpers/details';
+import { getDurationPeriod, getDurationTime } from 'Stores/Modules/Portfolio/Helpers/details';
 import { getEndTime, isUserSold } from 'Stores/Modules/Contract/Helpers/logic';
 import { isMultiplierContract } from 'Stores/Modules/Contract/Helpers/multiplier';
 import ContractDrawerCard from './contract-drawer-card.jsx';

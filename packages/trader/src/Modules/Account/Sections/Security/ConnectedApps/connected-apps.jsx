d99da2fa72d6b1614c250f7b147acaa54464d890
arash <arash@firstsource.io>
2020-04-27 17:39:58 +0430

    get data

--- packages/trader/src/Modules/Account/Sections/Security/ConnectedApps/connected-apps.jsx
+++ packages/trader/src/Modules/Account/Sections/Security/ConnectedApps/connected-apps.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
+import Loading from '../../../../../templates/_common/components/loading.jsx';
+import DataTable from 'App/Components/Elements/DataTable';
 
+const ColTemplate = [{ title: 'Name', col_index: 'name' }];
 class ConnectedApps extends React.Component {
     state = {
         is_loading: true,
     };
     componentDidMount() {
-        let apps_list = this.props.fetchConnectedApps();
-        apps_list.then(
+        this.props.fetchConnectedApps().then(
             result => {
                 this.props.setConnectedApps(result);
                 this.setState({ is_loading: false });
@@ -23,7 +25,23 @@ class ConnectedApps extends React.Component {
             <section className='connected-apps'>
                 <div className='connected-apps__title'>
                     <p>{localize('Authorised applications')}</p>
-                    <div>{this.state.is_loading ? 'waiting...' : this.props.connected_apps.map(v => v.name)}</div>
+                    <div>
+                        {this.state.is_loading ? (
+                            <Loading />
+                        ) : (
+                            <DataTable
+                                className='connected-apps'
+                                data_source={this.props.connected_apps}
+                                columns={ColTemplate}
+                                custom_width={'100%'}
+                                getRowSize={() => 63}
+                                custom_height={300}
+                                is_empty={false}
+                            >
+                                {this.state.is_loading && <Loading />}
+                            </DataTable>
+                        )}
+                    </div>
                 </div>
             </section>
         );

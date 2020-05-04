9d0c4545380710e7440e38cf723feaacade42ed6
Brandon <52795236+brandon-binary@users.noreply.github.com>
2020-05-04 10:42:19 +0800

    Updated sendbird and sendbird-uikit, as well as override the username and profile picture

--- packages/p2p/src/components/orders/order-details/order-details-chatbox.jsx
+++ packages/p2p/src/components/orders/order-details/order-details-chatbox.jsx
@@ -3,6 +3,22 @@ import { Channel, SendBirdProvider } from 'sendbird-uikit';
 import 'sendbird-uikit/dist/index.css';
 
 const OrderDetailsChatbox = ({ token, app_id, user_id, channel_url }) => {
+    const nickname = 'Joseph Joestar';
+    setInterval(() => {
+        const chat_title_element = document.querySelector('.sendbird-chat-header__title');
+        if (chat_title_element && chat_title_element.innerText !== nickname) {
+            chat_title_element.setAttribute('data-content', nickname);
+        }
+
+        const chat_avatar_elements = document.getElementsByClassName('sendbird-avatar');
+        if (chat_avatar_elements) {
+            var matches = nickname.match(/\b(\w)/g);
+            var acronym = matches.join('');
+
+            Array.from(chat_avatar_elements).map(element => (element.innerText = acronym));
+        }
+    }, 1000);
+
     return (
         <SendBirdProvider appId={app_id} userId={user_id} accessToken={token}>
             <Channel channelUrl={channel_url} />

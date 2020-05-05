0a966168ce2881a7c81d1a40ce6c495fe8effe27
shayan <shayan@firstsource.tech>
2020-05-05 22:40:28 +0430

    update pakcages

--- packages/trader/src/Modules/SmartChart/Components/toolbar-widgets.jsx
+++ packages/trader/src/Modules/SmartChart/Components/toolbar-widgets.jsx
@@ -7,8 +7,8 @@ const ToolbarWidgets = ({ position, updateChartType, updateGranularity }) => (
     <ToolbarWidget position={position || isMobile() ? 'bottom' : null}>
         <ChartMode portalNodeId='modal_root' onChartType={updateChartType} onGranularity={updateGranularity} />
         {isDesktop() && <StudyLegend portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
-        {isDesktop() && <DrawTools portalNodeId='modal_root' />}
         {isDesktop() && <Views portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
+        {isDesktop() && <DrawTools portalNodeId='modal_root' />}
         {isDesktop() && <Share portalNodeId='modal_root' />}
     </ToolbarWidget>
 );

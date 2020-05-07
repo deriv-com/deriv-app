adf3bc978bc8afe90a26e3592964cc0fdddbf0f5
arash <arash@firstsource.io>
2020-05-07 11:05:05 +0430

    merge to dev

--- packages/trader/src/Modules/Account/Sections/Security/ConnectedApps/connected-apps.jsx
+++ packages/trader/src/Modules/Account/Sections/Security/ConnectedApps/connected-apps.jsx
@@ -3,13 +3,52 @@ import { localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 import Loading from '../../../../../templates/_common/components/loading.jsx';
 import DataTable from 'App/Components/Elements/DataTable';
+import { Button } from '@deriv/components';
 
-const ColTemplate = [{ title: 'Name', col_index: 'name' }];
+const ColTemplate = revokeAccess => [
+    { title: 'Name', col_index: 'name' },
+    {
+        title: 'Permission',
+        col_index: 'scopes',
+        renderCellContent: ({ cell_value }) => {
+            return PreparePermissions(cell_value);
+        },
+    },
+    { title: 'Last login', col_index: 'last_used' },
+    {
+        title: 'Action',
+        col_index: 'app_id',
+        renderCellContent: ({ cell_value }) => {
+            return (
+                <Button small secondary onClick={() => revokeAccess(cell_value)}>
+                    {localize('Revoke access')}
+                </Button>
+            );
+        },
+    },
+];
+
+const PreparePermissions = permissions_list => {
+    let list = [];
+    permissions_list.forEach((value, index) => {
+        permissions_list.length - 1 !== index
+            ? list.push(value.charAt(0).toUpperCase() + value.slice(1) + ', ')
+            : list.push(value.charAt(0).toUpperCase() + value.slice(1));
+    });
+    return <div>{list}</div>;
+};
 class ConnectedApps extends React.Component {
     state = {
         is_loading: true,
     };
     componentDidMount() {
+        this.updateApp();
+    }
+    handleRevokeAccess = app_id => {
+        this.setState({ is_loading: true });
+        this.props.revokeAccess(app_id).then(() => this.updateApp());
+    };
+    updateApp = () => {
         this.props.fetchConnectedApps().then(
             result => {
                 this.props.setConnectedApps(result);
@@ -19,7 +58,8 @@ class ConnectedApps extends React.Component {
                 this.setState({ is_loading: true });
             }
         );
-    }
+    };
+
     render() {
         return (
             <section className='connected-apps'>
@@ -32,10 +72,9 @@ class ConnectedApps extends React.Component {
                             <DataTable
                                 className='connected-apps'
                                 data_source={this.props.connected_apps}
-                                columns={ColTemplate}
+                                columns={ColTemplate(this.handleRevokeAccess)}
                                 custom_width={'100%'}
                                 getRowSize={() => 63}
-                                custom_height={300}
                                 is_empty={false}
                             >
                                 {this.state.is_loading && <Loading />}
@@ -51,5 +90,6 @@ class ConnectedApps extends React.Component {
 export default connect(({ client }) => ({
     connected_apps: client.connected_apps,
     fetchConnectedApps: client.fetchConnectedApps,
+    revokeAccess: client.revokeAccess,
     setConnectedApps: client.setConnectedApps,
 }))(ConnectedApps);
--- packages/trader/src/Modules/Account/index.js
+++ packages/trader/src/Modules/Account/index.js
@@ -15,4 +15,5 @@ export default {
     ProofOfAddress,
     DerivPassword,
     AccountLimits,
+    ConnectedApps,
 };

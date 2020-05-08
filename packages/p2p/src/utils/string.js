732dc1dd75c169a70154bc1598ee3b59c21e63f2
Aaron Imming <aaim@protonmail.com>
2020-05-08 10:55:48 +0800

    Generate colour based on nickname rather than random

--- packages/p2p/src/utils/string.js
+++ packages/p2p/src/utils/string.js
@@ -8,3 +8,12 @@ export const toSentenceCase = string => {
 export const countDecimalPlaces = value => {
     return ((value.toString().split('.') || [])[1] || []).length;
 };
+
+export const generateHexColourFromNickname = nickname => {
+    const colours = ['#f43f83', 'var(--brand-secondary)', '#9ed178', 'var(--brand-orange)'];
+    const colour_hash = nickname.split().reduce((hash, char, idx) => {
+        hash = nickname.charCodeAt(idx) + ((hash << 5) - hash);
+        return hash & hash;
+    }, 0);
+    return colours[((colour_hash % colours.length) + colours.length) % colours.length];
+};

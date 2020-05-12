aef3fa992d091e5fea1f2c49986d7dba60e5ce17
Brandon <52795236+brandon-binary@users.noreply.github.com>
2020-05-12 17:43:18 +0800

    fix chat header

--- packages/p2p/src/components/orders/order-details/order-details-chatbox.jsx
+++ packages/p2p/src/components/orders/order-details/order-details-chatbox.jsx
@@ -6,14 +6,32 @@ const OrderDetailsChatbox = ({ token, app_id, user_id, channel_url, nickname }) 
     React.useEffect(() => {
         const interval_header = setInterval(() => {
             const chat_title_element = document.querySelector('.sendbird-chat-header__title');
-            const chat_avatar = document.querySelector('.sendbird-chat-header__avatar');
-            const acronym = nickname.match(/\b(\w)/g).join('');
-            if (chat_title_element.innerText === 'No title') return;
-            if (chat_title_element.innerText === nickname && chat_avatar.innerText === acronym)
-                clearInterval(interval_header);
 
-            chat_title_element.innerText = nickname;
-            chat_avatar.innerText = acronym;
+            if (chat_title_element && chat_title_element.innerText == 'No title') return;
+
+            if (chat_title_element && chat_title_element.innerText !== nickname) {
+                chat_title_element.innerText = nickname;
+            }
+
+            const chat_avatar_elements = document.getElementsByClassName('sendbird-avatar');
+            if (chat_avatar_elements) {
+                const acronym = nickname.match(/\b(\w)/g).join('');
+
+                Array.from(chat_avatar_elements).map(element => {
+                    if (element.innerText !== acronym) {
+                        element.innerText = acronym;
+                    }
+                });
+            }
+
+            if (!document.querySelector('.sendbird-chat-header__info-container')) {
+                const chat_info_container_element = document.createElement('div');
+                chat_info_container_element.className = 'sendbird-chat-header__info-container';
+                if (chat_title_element) chat_info_container_element.appendChild(chat_title_element);
+                const chat_subtitle_element = document.querySelector('.sendbird-chat-header__subtitle');
+                if (chat_subtitle_element) chat_info_container_element.appendChild(chat_subtitle_element);
+                document.querySelector('.sendbird-chat-header__left').appendChild(chat_info_container_element);
+            }
         }, 500);
 
         return () => {

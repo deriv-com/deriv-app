8a304b654ee02929edc1d387672c1d088af665ce
Mahdi <m.pourziaei@gmail.com>
2020-05-13 16:09:27 +0800

    refactor(trader): removed unused language function

--- packages/trader/src/Utils/Language/language.js
+++ packages/trader/src/Utils/Language/language.js
@@ -1,23 +1,6 @@
-import { isProduction } from '@deriv/shared/utils/config';
-import { getLanguage, getAllLanguages } from '@deriv/translations';
+import { getLanguage } from '@deriv/translations';
 import { urlFor } from '_common/language';
 
 export const currentLanguage = getLanguage();
-
-export const getAllowedLanguages = () => {
-    const exclude_languages = ['ACH'];
-    // TODO Change language_list to const when languages are available in prod.
-    let language_list = Object.keys(getAllLanguages())
-        .filter(key => !exclude_languages.includes(key))
-        .reduce((obj, key) => {
-            obj[key] = getAllLanguages()[key];
-            return obj;
-        }, {});
-
-    // TODO Remove this one line below when languages are available in prod.
-    if (isProduction()) language_list = { EN: 'English' };
-
-    return language_list;
-};
 
 export const getURL = lang => urlFor(lang);

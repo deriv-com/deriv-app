702b4ac7b821edc8b666e831c6e6fce77589bad8
Mahdi <m.pourziaei@gmail.com>
2020-05-15 13:48:57 +0800

    refactor: deleted unused language utils

--- packages/trader/src/Utils/Language/language.js
+++
@@ -1,6 +0,0 @@
-import { getLanguage } from '@deriv/translations';
-import { urlForLanguage } from '@deriv/shared/utils/url';
-
-export const currentLanguage = getLanguage();
-
-export const getURL = lang => urlForLanguage(lang);

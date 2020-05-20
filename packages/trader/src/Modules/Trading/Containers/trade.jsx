809a9eb79557829f6d5cd49f2f25396fe9b4c7ab
Bala <bala.krishna@regentmarkets.com>
2020-05-20 15:58:41 +0800

    set active_markets on mount

--- packages/trader/src/Modules/Trading/Containers/trade.jsx
+++ packages/trader/src/Modules/Trading/Containers/trade.jsx
@@ -188,6 +188,7 @@ class ChartTradeClass extends React.Component {
 
     componentDidMount() {
         performance.mark('smart-charts-mounted');
+        this.setActiveMarkets();
     }
 
     componentDidUpdate(prevProps) {

1b6dc640d8d94c411103beee1daa7b01414bf36a
arash <arash@firstsource.io>
2020-06-01 11:40:00 +0430

    depart reasons and steps

---
+++ packages/account/src/Sections/Security/DeactivateAccount/deactivate-account-reason.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const DeactivateAccountReason = () => <div>Account Reason</div>;
+
+export default DeactivateAccountReason;
--- packages/account/src/Sections/Security/DeactivateAccount/deactivate-account.jsx
+++ packages/account/src/Sections/Security/DeactivateAccount/deactivate-account.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import ConfirmAllClose from './confirm-all-close.jsx';
-
+import { localize } from '@deriv/translations';
+import { Modal } from '@deriv/components';
+// import { WS } from 'Services/ws-methods';
+import DeactivateAccountSteps from './deactivate-account-steps';
+import DeactivateAccountReason from './deactivate-account-reason';
 class DeactivateAccount extends React.Component {
+    state = {
+        is_modal_open: false,
+        accounts_with_balance: {},
+        account_closure_message: '',
+        is_loading: false,
+        render_deactivate_account_reason: false,
+    };
+    startDeactivatingProcesses = () => {
+        this.setState;
+    };
+    redirectToReasons = () => {
+        this.setState({
+            render_deactivate_account_reason: true,
+        });
+    };
     render() {
         return (
-            <div className='deactivate-account'>
-                <ConfirmAllClose />
-            </div>
+            <React.Fragment>
+                {this.state.render_deactivate_account_reason ? (
+                    <DeactivateAccountReason />
+                ) : (
+                    <DeactivateAccountSteps redirectToReasons={this.redirectToReasons} />
+                )}
+                <Modal
+                    className='confirm-all-close'
+                    is_open={this.state.is_modal_open}
+                    toggleModal={() => this.setState({ is_modal_open: false })}
+                    title={localize('Action required')}
+                >
+                    <Modal.Body>{this.state.is_loading ? 'loading...' : 'modal content'}</Modal.Body>
+                </Modal>
+            </React.Fragment>
         );
     }
 }
 
 export default DeactivateAccount;
+
+// WS.send(
+//     {
+//         "account_closure": 1,
+//         "reason": null
+//     })
+//     .then((res) => {
+//         console.log(res.error.code);
+//         this.setState({
+//             account_closure_message: res.error.code,
+//             is_loading: false
+//         })
+//     })
+//     .catch((err) => console.log(err));
+
+// const AccountsWithBalance = () => {
+//     return (
+//         <div className='open-positions'>
+//             <p className='open-positions__title'>{localize('You have funds in these Deriv accounts:')}</p>
+//         </div>
+//     )
+// }
+// const AccountsWithOpenPositions = () => {
+//     <div>open positions</div>
+// }

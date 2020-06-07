3ed9fdcd49b5c297a1307c3ea79a32eae5643569
Salar Hafezi <salar@firstsource.io>
2020-06-07 20:49:18 +0430

    remove dark theme, add dont accept btn

--- packages/core/src/App/Components/Elements/CookieBanner/cookie-banner.jsx
+++ packages/core/src/App/Components/Elements/CookieBanner/cookie-banner.jsx
@@ -4,39 +4,43 @@ import PropTypes from 'prop-types';
 import { Button } from '@deriv/components';
 import { getDerivComLink } from '@deriv/shared/utils/url';
 import { localize, Localize } from '@deriv/translations';
-import { connect } from 'Stores/connect';
 
-const CookieBanner = ({ onAccept, is_open, is_dark_mode_on }) => (
+const CookieBanner = ({ onAccept, onDecline, is_open }) => (
     <div
         className={classNames('cookie-banner', {
-            'theme-dark': is_dark_mode_on,
             'is-open': is_open,
             'is-closed': !is_open,
         })}
     >
-        <Localize
-            i18n_default_text='Our website uses cookies to give you the best user experience. For more information, <0>view our policy</0>.'
-            components={[
-                <a
-                    key={0}
-                    className='link link--red'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    href={getDerivComLink('/terms-and-conditions')}
-                />,
-            ]}
-        />
-        <Button className='btn-accept' secondary onClick={onAccept}>
-            {localize('Accept')}
-        </Button>
+        <div className='description'>
+            <Localize i18n_default_text='Cookies help us to give you a better experience and personalised content on our site.' />
+            <br />
+            <Localize
+                i18n_default_text='If you agree to our use of cookies, click on Accept. For more information, <0>see our policy</0>.'
+                components={[
+                    <a
+                        key={0}
+                        className='link link--red'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                        href={getDerivComLink('/terms-and-conditions')}
+                    />,
+                ]}
+            />
+            <Button className='btn-dont-accept' secondary onClick={onDecline}>
+                {localize('Don’t accept')}
+            </Button>
+            <Button className='btn-accept' secondary onClick={onAccept}>
+                {localize('Accept')}
+            </Button>
+        </div>
     </div>
 );
 
 CookieBanner.prototype = {
     is_open: PropTypes.bool,
     onAccept: PropTypes.func,
+    onDecline: PropTypes.func,
 };
 
-export default connect(({ ui }) => ({
-    is_dark_mode_on: ui.is_dark_mode_on,
-}))(CookieBanner);
+export default CookieBanner;

4005188ab13131b602358e8ab8bce1bab3ae660e
Salar Hafezi <salar@firstsource.io>
2020-06-07 09:15:15 +0430

    remove unused function

--- packages/trader/src/Modules/Reports/Constants/data-table-constants.js
+++ packages/trader/src/Modules/Reports/Constants/data-table-constants.js
@@ -302,26 +302,4 @@ export const getMultiplierOpenPositionsColumnsTemplate = currency => [
         },
     },
 ];
-export const getLoginHistoryColumnsTemplate = () => [
-    {
-        title: localize('Date and time'),
-        col_index: 'date',
-    },
-    {
-        title: localize('Action'),
-        col_index: 'action',
-    },
-    {
-        title: localize('Browser'),
-        col_index: 'browser',
-    },
-    {
-        title: localize('IP'),
-        col_index: 'ip',
-    },
-    {
-        title: localize('Status'),
-        col_index: 'status',
-    },
-];
 /* eslint-enable react/display-name, react/prop-types */

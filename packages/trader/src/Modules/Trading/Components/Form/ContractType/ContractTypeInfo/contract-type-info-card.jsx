e0a8d6556abbbe6f07a6a7f3d86d47cf114db09c
zakibinary <zaki@regentmarkets.com>
2020-06-09 12:15:07 +0800

    fix: contract-type-info scrolling issue

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info-card.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info-card.jsx
@@ -6,25 +6,25 @@ import TradeCategories from 'Assets/Trading/Categories/trade-categories.jsx';
 import TradeCategoriesGIF from 'Assets/Trading/Categories/trade-categories-gif.jsx';
 
 const Card = ({ contract_type, onClick }) => (
-    <ThemedScrollbars height='calc(100vh- 262px)'>
-        <div className='contract-type-info__card'>
-            <div className='contract-type-info__gif'>
-                <TradeCategoriesGIF category={contract_type.value} />
-            </div>
-            <div className='contract-type-info__content'>
-                <TradeCategories category={contract_type.value} />
-            </div>
-            <Button
-                id={`dt_contract_info_${contract_type.value}_btn`}
-                onClick={e => onClick(contract_type, e)}
-                text={localize('Choose {{contract_type}}', {
-                    contract_type: contract_type.text,
-                    interpolation: { escapeValue: false },
-                })}
-                secondary
-            />
+    <div className='contract-type-info__card'>
+        <div className='contract-type-info__gif'>
+            <TradeCategoriesGIF category={contract_type.value} />
         </div>
-    </ThemedScrollbars>
+        <div className='contract-type-info__content'>
+            <ThemedScrollbars height='340px'>
+                <TradeCategories category={contract_type.value} />
+            </ThemedScrollbars>
+        </div>
+        <Button
+            id={`dt_contract_info_${contract_type.value}_btn`}
+            onClick={e => onClick(contract_type, e)}
+            text={localize('Choose {{contract_type}}', {
+                contract_type: contract_type.text,
+                interpolation: { escapeValue: false },
+            })}
+            secondary
+        />
+    </div>
 );
 
 Card.propTypes = {

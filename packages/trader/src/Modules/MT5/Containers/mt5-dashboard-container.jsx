476ccb6d2cd5045b942b4ccd4b6c15fe2cbc70f5
Mohammadreza Ghorbani <reza@firstsource.email>
2020-06-11 11:07:40 +0430

    Dont fill-in loginid if there is none

--- packages/trader/src/Modules/MT5/Containers/mt5-dashboard-container.jsx
+++ packages/trader/src/Modules/MT5/Containers/mt5-dashboard-container.jsx
@@ -14,11 +14,7 @@ const MT5DashboardContainer = () => (
                 <div className='mt5-dashboard__download-center-options--desktop-devices'>
                     <Icon icon='IcMt5DeviceDesktop' width={118} height={85} />
                     <Icon icon='IcMt5DeviceLaptop' width={75} height={51} />
-                    <a
-                        href={getMT5WebTerminalLink({ category: 'demo', loginid: '' })}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
+                    <a href={getMT5WebTerminalLink({ category: 'demo' })} target='_blank' rel='noopener noreferrer'>
                         <Icon icon='IcInstallationWebDemo' width={138} height={40} />
                     </a>
                 </div>
@@ -41,7 +37,7 @@ const MT5DashboardContainer = () => (
                     </a>
                     <a
                         className='mt5-dashboard__download-center-options--desktop-link--top'
-                        href={getMT5WebTerminalLink({ category: 'real', loginid: '' })}
+                        href={getMT5WebTerminalLink({ category: 'real' })}
                         target='_blank'
                         rel='noopener noreferrer'
                     >

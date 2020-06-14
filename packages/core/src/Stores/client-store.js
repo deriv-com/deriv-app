e2e52499aee96db895007fe85682ae71f62c22bc
Salar Hafezi <salar@firstsource.io>
2020-06-14 14:03:23 +0430

    remove clients_country from cookies

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1,5 +1,4 @@
 import moment from 'moment';
-import Cookies from 'js-cookie';
 import { action, computed, observable, runInAction, when, reaction, toJS } from 'mobx';
 import CurrencyUtils from '@deriv/shared/utils/currency';
 import ObjectUtils from '@deriv/shared/utils/object';
@@ -14,7 +13,6 @@ import { isEmptyObject } from '@deriv/shared/src/utils/object/object';
 import { LocalStore, State } from '_common/storage';
 import { isEuCountry } from '_common/utility';
 import BinarySocketGeneral from 'Services/socket-general';
-import { cookie_expires } from 'App/Constants/app-config';
 import { handleClientNotifications } from './Helpers/client-notifications';
 import BaseStore from './base-store';
 import { getClientAccountType } from './Helpers/client';
@@ -346,14 +344,7 @@ export default class ClientStore extends BaseStore {
 
     @computed({ keepAlive: true })
     get is_eu_country() {
-        let clients_country = Cookies.get('clients_country');
-        if (!clients_country || clients_country === 'undefined') {
-            clients_country = this.website_status.clients_country;
-            Cookies.set('clients_country', clients_country, {
-                expires: cookie_expires,
-            });
-        }
-        return isEuCountry(clients_country);
+        return isEuCountry(this.website_status.clients_country);
     }
     /**
      * Store Values relevant to the loginid to local storage.

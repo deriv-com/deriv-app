9637647e56b43ea43a1ff59001c9ef4770810aff
Aaron Imming <aaim@protonmail.com>
2020-06-15 15:33:47 +0800

    Add missing prop type

--- packages/core/src/Modules/Cashier/Components/on-ramp-provider-card.jsx
+++ packages/core/src/Modules/Cashier/Components/on-ramp-provider-card.jsx
@@ -4,42 +4,36 @@ import { Button, Icon } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 
-const OnRampProviderCard = ({ provider, setSelectedProvider }) => {
-    return (
-        <div className='on-ramp__provider'>
-            <div className='on-ramp__provider-logo'>
-                <Icon icon={provider.icon} width={118} height={28} />
-            </div>
-            <h2 className='on-ramp__provider-name'>{provider.name}</h2>
-            <div className='on-ramp__provider-description'>{provider.description}</div>
-            <a
-                href={provider.website}
-                className='on-ramp__provider-website link'
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                {provider.website}
-            </a>
-            {provider.payment_icons.length && (
-                <div className='on-ramp__provider-payment-types'>
-                    {provider.payment_icons.map((payment_type, idx) => (
-                        <Icon key={idx} size={40} icon={payment_type} />
-                    ))}
-                </div>
-            )}
-            <Button
-                className='on-ramp__provider-button'
-                type='button'
-                has_effect
-                onClick={() => setSelectedProvider(provider)}
-                text={localize('Select')}
-                primary
-            />
+const OnRampProviderCard = ({ provider, setSelectedProvider }) => (
+    <div className='on-ramp__provider'>
+        <div className='on-ramp__provider-logo'>
+            <Icon icon={provider.icon} width={118} height={28} />
         </div>
-    );
-};
+        <h2 className='on-ramp__provider-name'>{provider.name}</h2>
+        <div className='on-ramp__provider-description'>{provider.description}</div>
+        <a href={provider.website} className='on-ramp__provider-website link' target='_blank' rel='noopener noreferrer'>
+            {provider.website}
+        </a>
+        {provider.payment_icons.length && (
+            <div className='on-ramp__provider-payment-types'>
+                {provider.payment_icons.map((payment_type, idx) => (
+                    <Icon key={idx} size={40} icon={payment_type} />
+                ))}
+            </div>
+        )}
+        <Button
+            className='on-ramp__provider-button'
+            type='button'
+            has_effect
+            onClick={() => setSelectedProvider(provider)}
+            text={localize('Select')}
+            primary
+        />
+    </div>
+);
 
 OnRampProviderCard.propTypes = {
+    provider: PropTypes.object,
     setSelectedProvider: PropTypes.func,
 };
 

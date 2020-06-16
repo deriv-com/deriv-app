b91dc331915f8fb126736b0a07a78017c1007da3
Mahdi <m.pourziaei@gmail.com>
2020-06-16 15:18:30 +0800

    test(components): added theme context to Calendar and DatePicker unit tests

--- packages/components/src/components/calendar/__tests__/calender.spec.js
+++ packages/components/src/components/calendar/__tests__/calender.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import ThemeProvider from 'Components/theme-provider/theme-provider.jsx';
 import Calendar from '../calendar.jsx';
 
 // TODO: add tests
 describe('Calendar', () => {
     it('renders without crashing', () => {
-        render(<Calendar />);
+        render(
+            <ThemeProvider>
+                <Calendar />
+            </ThemeProvider>
+        );
     });
 });
--- packages/components/src/components/date-picker/__tests__/date-picker.spec.js
+++ packages/components/src/components/date-picker/__tests__/date-picker.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import ThemeProvider from 'Components/theme-provider/theme-provider.jsx';
 import DatePicker from '../date-picker.jsx';
 
 // TODO: add tests
 describe('DatePicker', () => {
     it('renders without crashing', () => {
-        render(<DatePicker />);
+        render(
+            <ThemeProvider>
+                <DatePicker />
+            </ThemeProvider>
+        );
     });
 });

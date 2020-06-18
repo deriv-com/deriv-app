4b110a5d8cd78050d5834888e219719d9ef16d26
Aaron Imming <aaim@protonmail.com>
2020-06-18 15:23:36 +0800

    Add sendbird-store

--- packages/core/src/Stores/Modules/index.js
+++ packages/core/src/Stores/Modules/index.js
@@ -1,7 +1,9 @@
 import CashierStore from './Cashier/cashier-store';
+import SendbirdStore from './SendBird/sendbird-store';
 
 export default class ModulesStore {
     constructor(root_store) {
         this.cashier = new CashierStore({ root_store });
+        this.sendbird = new SendbirdStore({ root_store });
     }
 }

54e956dcac59b0761f13b84f031c785965b3d52a
Salar Hafezi <salar@firstsource.io>
2020-06-21 18:27:35 +0430

    add MarketCountdownTimer to contract details

--- packages/trader/src/App/Components/Elements/ContractCard/contract-card.jsx
+++ packages/trader/src/App/Components/Elements/ContractCard/contract-card.jsx
@@ -1,8 +1,11 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Localize } from '@deriv/translations';
+import { connect } from 'Stores/connect';
+import MarketCountdownTimer from '../market-countdown-timer.jsx';
 
-const ContractCard = ({ children, profit_loss, is_sold }) => (
+const ContractCard = ({ children, profit_loss, is_sold, is_dark_mode_on }) => (
     <div
         className={classNames('contract-card', {
             'contract-card--green': profit_loss > 0 && !is_sold,
@@ -10,6 +13,16 @@ const ContractCard = ({ children, profit_loss, is_sold }) => (
         })}
     >
         {children}
+        <div
+            className={classNames('contract-card--market-closed', {
+                'contract-card--market-closed--dark': is_dark_mode_on,
+            })}
+        >
+            <p className='contract-card--market-closed--heading'>
+                <Localize i18n_default_text='Market is closed' />
+            </p>
+            <MarketCountdownTimer target_date='2020-07-29' />
+        </div>
     </div>
 );
 
@@ -19,4 +32,6 @@ ContractCard.propTypes = {
     profit_loss: PropTypes.number,
 };
 
-export default ContractCard;
+export default connect(({ ui }) => ({
+    is_dark_mode_on: ui.is_dark_mode_on,
+}))(ContractCard);

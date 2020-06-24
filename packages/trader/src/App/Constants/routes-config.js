910330543e27f01c6ead84c2cf1966062650ceb4
arash <arash@firstsource.io>
2020-06-24 11:40:19 +0430

    resolve routes conflict

--- packages/trader/src/App/Constants/routes-config.js
+++ packages/trader/src/App/Constants/routes-config.js
@@ -68,69 +68,6 @@ const initRoutesConfig = () => [
             },
         ],
     },
-    {
-        path: routes.account,
-        component: lazyLoadAccountComponent('Account'),
-        is_authenticated: true,
-        title: localize('Account settings'),
-        icon_component: 'IcUserOutline',
-        routes: [
-            {
-                title: localize('Profile'),
-                icon: 'IcUserOutline',
-                subroutes: [
-                    {
-                        path: routes.personal_details,
-                        component: lazyLoadAccountComponent('PersonalDetails'),
-                        title: localize('Personal details'),
-                        default: true,
-                    },
-                    {
-                        path: routes.financial_assessment,
-                        component: lazyLoadAccountComponent('FinancialAssessment'),
-                        title: localize('Financial assessment'),
-                    },
-                ],
-            },
-            {
-                title: localize('Verification'),
-                icon: 'IcVerification',
-                subroutes: [
-                    {
-                        path: routes.proof_of_identity,
-                        component: lazyLoadAccountComponent('ProofOfIdentity'),
-                        title: localize('Proof of identity'),
-                    },
-                    {
-                        path: routes.proof_of_address,
-                        component: lazyLoadAccountComponent('ProofOfAddress'),
-                        title: localize('Proof of address'),
-                    },
-                ],
-            },
-            {
-                title: localize('Security and safety'),
-                icon: 'IcSecurity',
-                subroutes: [
-                    {
-                        path: routes.deriv_password,
-                        component: lazyLoadAccountComponent('DerivPassword'),
-                        title: localize('Deriv password'),
-                    },
-                    {
-                        path: routes.account_limits,
-                        component: lazyLoadAccountComponent('AccountLimits'),
-                        title: localize('Account limits'),
-                    },
-                    {
-                        path: routes.connected_apps,
-                        component: lazyLoadAccountComponent('ConnectedApps'),
-                        title: localize('Connected apps'),
-                    },
-                ],
-            },
-        ],
-    },
     { path: routes.trade, component: Trade, title: localize('Trade'), exact: true },
 ];
 

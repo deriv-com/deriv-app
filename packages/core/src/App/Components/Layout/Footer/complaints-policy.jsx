49270012344f859a9c065ba13f5352e5aec56f7e
Mohammadreza Ghorbani <reza@firstsource.email>
2020-06-28 19:00:05 +0430

    Add Footer links
    + WIP Complaints policy
    + WIP MGA license logo

---
+++ packages/core/src/App/Components/Layout/Footer/complaints-policy.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Modal, Popover, Icon } from '@deriv/components';
+import { localize } from '@deriv/translations';
+
+export const ComplaintsPolicy = ({ standpoint }) => {
+    const [should_show_modal, showModal] = React.useState(false);
+
+    return (
+        <div className='footer__link'>
+            <a onClick={() => showModal(true)}>
+                <Popover alignment='bottom' message={localize('Regulatory Information')}>
+                    <Icon icon='IcComplaintsPolicy' className='footer__icon ic-deriv__icon' />
+                </Popover>
+            </a>
+            <Modal
+                is_open={should_show_modal}
+                small
+                has_close_icon
+                toggleModal={() => showModal(false)}
+                title={localize('Complaints policy')}
+            >
+                <div>Complaints policy</div>
+            </Modal>
+        </div>
+    );
+};

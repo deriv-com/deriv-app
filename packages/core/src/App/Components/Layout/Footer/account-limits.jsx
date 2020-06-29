fa961e92c8ae9d2e7a18945b791a5caffd25503a
Mohammadreza Ghorbani <reza@firstsource.email>
2020-06-29 11:50:51 +0430

    Refactor single element components to return directly

--- packages/core/src/App/Components/Layout/Footer/account-limits.jsx
+++ packages/core/src/App/Components/Layout/Footer/account-limits.jsx
@@ -5,14 +5,12 @@ import routes from '@deriv/shared/utils/routes';
 import { getDerivComLink } from '@deriv/shared/utils/url';
 import { localize } from '@deriv/translations';
 
-export const AccountLimits = () => {
-    return (
-        <div className='footer__link'>
-            <Link to={routes.account_limits} href={getDerivComLink('/')}>
-                <Popover alignment='bottom' message={localize('Account limits')}>
-                    <Icon icon='IcAccountLimits' className='footer__icon ic-deriv__icon' />
-                </Popover>
-            </Link>
-        </div>
-    );
-};
+export const AccountLimits = () => (
+    <div className='footer__link'>
+        <Link to={routes.account_limits} href={getDerivComLink('/')}>
+            <Popover alignment='bottom' message={localize('Account limits')}>
+                <Icon icon='IcAccountLimits' className='footer__icon ic-deriv__icon' />
+            </Popover>
+        </Link>
+    </div>
+);

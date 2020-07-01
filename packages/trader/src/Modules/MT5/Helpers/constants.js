24c083696d89e4201623af75880e4ea0c2adfaae
zakibinary <zaki@regentmarkets.com>
2020-07-01 18:44:41 +0800

    doc: add comment for future eu changes

--- packages/trader/src/Modules/MT5/Helpers/constants.js
+++ packages/trader/src/Modules/MT5/Helpers/constants.js
@@ -1,5 +1,6 @@
 import { OSDetect } from '@deriv/shared/utils/os';
 
+// TODO: [deriv-eu] for EU we should return 'Binary-Demo for demo accounts
 const getServerName = is_demo => (is_demo ? 'Deriv-Demo' : 'Deriv-Server');
 
 const getBrokerName = () => 'Deriv Limited (Deriv)';

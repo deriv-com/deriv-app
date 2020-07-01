b6120f94eff3fb09b265f3fce203636f71b7cfe2
Salar Hafezi <salar@firstsource.io>
2020-07-01 21:48:32 +0430

    add view-mode resize listener component

--- packages/core/src/App/Containers/Layout/app-contents.jsx
+++ packages/core/src/App/Containers/Layout/app-contents.jsx
@@ -2,10 +2,80 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withRouter } from 'react-router';
-import { DesktopWrapper, MobileWrapper, ThemedScrollbars } from '@deriv/components';
+import { DesktopWrapper, MobileWrapper, ThemedScrollbars, Button } from '@deriv/components';
+import { localize } from '@deriv/translations';
 import { isMobile } from '@deriv/shared/utils/screen';
 import { connect } from 'Stores/connect';
 // import InstallPWA    from './install-pwa.jsx';
+
+const relaunch_in_mobile = 'Relaunch in Mobile mode';
+const relaunch_in_desktop = 'Relaunch in Desktop mode';
+
+const Snackbar = ({ is_active, message, closeSnackBar }) => {
+    return (
+        <div
+            className={classNames('snackbar', {
+                'snackbar--fade-in': is_active,
+                'snackbar--fade-out': !is_active,
+            })}
+        >
+            <div className='description'>
+                <strong>View this site in {message} mode to get the best User Experience</strong>
+                <br />
+                <Button className='snackbar__btn-dont-accept' secondary onClick={closeSnackBar}>
+                    {localize('Dismiss')}
+                </Button>
+                <Button
+                    className='snackbar__btn-dont-accept'
+                    secondary
+                    onClick={() => {
+                        window.location.reload(false);
+                        closeSnackBar();
+                    }}
+                >
+                    {localize(message === 'Mobile' ? relaunch_in_mobile : relaunch_in_desktop)}
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+const useSnackbar = () => {
+    const [is_active, setIsActive] = React.useState(false);
+    const [message, setMessage] = React.useState();
+
+    const openSnackBar = (msg = 'Something went wrong...') => {
+        setMessage(msg);
+        setIsActive(true);
+    };
+
+    const closeSnackBar = () => {
+        setIsActive(false);
+    };
+
+    return { is_active, message, openSnackBar, closeSnackBar };
+};
+
+const getWidth = () => window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+const useCurrentWitdh = () => {
+    const [width, setWidth] = React.useState(getWidth());
+
+    React.useEffect(() => {
+        let timeoutId = null;
+        const resizeListener = () => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => setWidth(getWidth()), 150);
+        };
+        window.addEventListener('resize', resizeListener);
+
+        return () => {
+            window.removeEventListener('resize', resizeListener);
+        };
+    }, []);
+
+    return width;
+};
 
 const AppContents = ({
     // addNotificationBar,
@@ -19,6 +89,31 @@ const AppContents = ({
     pageView,
     // setPWAPromptEvent,
 }) => {
+    const { is_active, message, openSnackBar, closeSnackBar } = useSnackbar();
+    const width = useCurrentWitdh();
+    const [view_mode, setViewMode] = React.useState(width <= 767 ? 'Mobile' : 'Desktop');
+    const [is_rendered, setIsRendered] = React.useState(false);
+
+    React.useEffect(() => {
+        if (is_rendered) {
+            if (width <= 767) {
+                setViewMode('Mobile');
+            } else {
+                setViewMode('Desktop');
+            }
+        }
+    }, [width]);
+
+    React.useEffect(() => {
+        if (is_rendered) {
+            if (view_mode) {
+                openSnackBar(view_mode);
+            }
+        } else {
+            setIsRendered(true);
+        }
+    }, [view_mode]);
+
     // Segment page view trigger
     identifyEvent();
     pageView();
@@ -54,6 +149,7 @@ const AppContents = ({
                 {/* Calculate height of user screen and offset height of header and footer */}
                 <ThemedScrollbars height='calc(100vh - 84px)'>{children}</ThemedScrollbars>
             </DesktopWrapper>
+            <Snackbar is_active={is_active} message={message} closeSnackBar={closeSnackBar} />
         </div>
     );
 };

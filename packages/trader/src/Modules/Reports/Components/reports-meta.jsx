4b880183c354c29b4b7bbc1d463c291a6e1803e4
sara-fs <sara.khosropour@regentmarkets.com>
2020-07-01 08:29:52 +0430

    Replaced conditional statment with and operator

--- packages/trader/src/Modules/Reports/Components/reports-meta.jsx
+++ packages/trader/src/Modules/Reports/Components/reports-meta.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 
 const ReportsMeta = ({ i18n_heading, i18n_message, filter_component, className, optional_component }) => (
     <div className={classNames('reports__meta', className)}>
-        {i18n_heading ? (
+        {i18n_heading && (
             <div className='reports__meta-description'>
                 <h1 className='reports__meta-description--heading'>{i18n_heading}</h1>
                 <p className='reports__meta-description--paragraph'>{i18n_message}</p>
             </div>
-        ) : (
-            undefined
         )}
 
         {optional_component}

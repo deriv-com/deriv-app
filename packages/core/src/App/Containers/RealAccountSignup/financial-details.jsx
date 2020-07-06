786abccd39c0ca58e249e289acb556b66eef8c45
Mohammadreza Ghorbani <reza@firstsource.email>
2020-07-06 11:43:05 +0430

    Fix eslint errors

--- packages/core/src/App/Containers/RealAccountSignup/financial-details.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/financial-details.jsx
@@ -3,7 +3,7 @@ import { Formik } from 'formik';
 import React from 'react';
 import { FormSubHeader } from '@deriv/account';
 import { localize, Localize } from '@deriv/translations';
-import { isDesktop, isMobile } from '@deriv/shared/utils/screen';
+import { isDesktop, isMobile } from '@deriv/shared';
 import { connect } from 'Stores/connect';
 import {
     AccountTurnover,

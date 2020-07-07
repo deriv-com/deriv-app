aee88355eeb68d3b39ebbb4a1fc2bcd6f6c7f680
Carol Sachdeva <carol@binary.com>
2020-07-07 15:22:12 +0400

    disable download when bot is running

--- packages/bot-web-ui/src/components/download.jsx
+++ packages/bot-web-ui/src/components/download.jsx
@@ -2,20 +2,23 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button, Icon } from '@deriv/components';
+import { Button, Icon, DesktopWrapper } from '@deriv/components';
 import '../assets/sass/download.scss';
 import { connect } from '../stores/connect';
 
-const Download = ({ onDownloadClick }) => {
+const Download = ({ onDownloadClick, is_clear_stat_disabled }) => {
     return (
-        <div className='download__container'>
-            <Button
-                id='download-button'
-                className='download__button'
-                icon={<Icon icon='IcDownload' className='download__icon' />}
-                onClick={onDownloadClick}
-            />
-        </div>
+        <DesktopWrapper>
+            <div className='download__container'>
+                <Button
+                    id='download-button'
+                    is_disabled={is_clear_stat_disabled}
+                    className='download__button'
+                    icon={<Icon icon='IcDownload' className='download__icon' />}
+                    onClick={onDownloadClick}
+                />
+            </div>
+        </DesktopWrapper>
     );
 };
 
@@ -23,6 +26,7 @@ Download.propTypes = {
     onDownloadClick: PropTypes.func,
 };
 
-export default connect(({ download }) => ({
+export default connect(({ download, run_panel }) => ({
     onDownloadClick: download.onDownloadClick,
+    is_clear_stat_disabled: run_panel.is_clear_stat_disabled,
 }))(Download);

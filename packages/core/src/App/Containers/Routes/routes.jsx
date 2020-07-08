8bb5a8098bbf5b1b7f7de22c066dfa38e2d6c110
mehrzad-fs <mehrzad@firstsource.tech>
2020-07-08 16:21:30 +0430

    remove landscape blocker bot

--- packages/core/src/App/Containers/Routes/routes.jsx
+++ packages/core/src/App/Containers/Routes/routes.jsx
@@ -17,10 +17,21 @@ const Error = Loadable({
 });
 
 class Routes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_bot: false,
+        };
+    }
+    el_landscape_blocker = document.getElementById('landscape_blocker');
     unlisten_to_change = null;
     initial_route = null;
 
     componentDidMount() {
+        this.isBot(window.location);
+        if (this.state.is_bot) {
+            this.el_landscape_blocker.classList.add('landscape-blocker--keyboard-visible');
+        }
         if (!this.unlisten_to_change && !this.initial_route) {
             this.initial_route = this.props.location.pathname;
         }
@@ -34,7 +45,23 @@ class Routes extends React.Component {
         this.props.setAppRouterHistory(this.props.history);
     }
 
+    componentDidUpdate(pp, ps) {
+        if (pp.location.pathname !== this.props.location.pathname) {
+            this.isBot(this.props.location);
+        }
+        if (this.state.is_bot !== ps.is_bot) {
+            if (this.state.is_bot) {
+                this.removeBlockerEvents();
+                this.el_landscape_blocker.classList.add('landscape-blocker--keyboard-visible');
+            } else {
+                this.addBlockerEvents();
+                this.el_landscape_blocker.classList.remove('landscape-blocker--keyboard-visible');
+            }
+        }
+    }
+
     componentWillUnmount() {
+        this.removeBlockerEvents();
         if (typeof this.unlisten_to_change === 'function') {
             this.unlisten_to_change();
             this.unlisten_to_change = null;
@@ -42,13 +69,47 @@ class Routes extends React.Component {
         }
     }
 
+    isBot = location => {
+        const base = location.pathname.split('/')[1];
+        this.setState({
+            is_bot: base === 'bot',
+        });
+    };
+    addBlockerEvents = () => {
+        document.addEventListener('focus', this.onFocus, false);
+        document.addEventListener('focusout', this.onFocusOut, false);
+        document.addEventListener('touchstart', this.onTouchStart, true);
+    };
+    removeBlockerEvents = () => {
+        document.removeEventListener('focus', this.onFocus);
+        document.removeEventListener('focusout', this.onFocusOut);
+        document.removeEventListener('touchstart', this.onTouchStart);
+    };
+    onFocus = () => {
+        this.el_landscape_blocker.classList.add('landscape-blocker--keyboard-visible');
+        this.props.setIsNativepickerVisible(true);
+    };
+
+    onFocusOut = e => {
+        if (e.target.classList.contains('dc-dropdown__display')) {
+            return;
+        }
+        this.props.setIsNativepickerVisible(false);
+    };
+
+    onTouchStart = () => {
+        if (document.activeElement.tagName !== 'INPUT') {
+            if (!this.state.is_bot) {
+                this.el_landscape_blocker.classList.remove('landscape-blocker--keyboard-visible');
+            }
+        }
+    };
+
     render() {
         const { error, has_error, is_logged_in, passthrough } = this.props;
-
         if (has_error) {
             return <Error {...error} />;
         }
-
         return <BinaryRoutes is_logged_in={is_logged_in} passthrough={passthrough} />;
     }
 }
@@ -58,17 +119,19 @@ Routes.propTypes = {
     has_error: PropTypes.bool,
     is_logged_in: PropTypes.bool,
     is_virtual: PropTypes.bool,
+    setIsNativepickerVisible: PropTypes.func,
 };
 
 // need to wrap withRouter around connect
 // to prevent updates on <BinaryRoutes /> from being blocked
 export default withRouter(
-    connect(({ client, common }) => ({
+    connect(({ client, common, ui }) => ({
         is_logged_in: client.is_logged_in,
         error: common.error,
         has_error: common.has_error,
         setAppRouterHistory: common.setAppRouterHistory,
         addRouteHistoryItem: common.addRouteHistoryItem,
         setInitialRouteHistoryItem: common.setInitialRouteHistoryItem,
+        setIsNativepickerVisible: ui.setIsNativepickerVisible,
     }))(Routes)
 );

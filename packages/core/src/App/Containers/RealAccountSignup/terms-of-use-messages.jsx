8647c3608d071f44ca65dbb38bfe8a29b68a2ce2
Mohammadreza Ghorbani <reza@firstsource.email>
2020-07-08 16:47:06 +0430

    Fix mx terms of use message

--- packages/core/src/App/Containers/RealAccountSignup/terms-of-use-messages.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/terms-of-use-messages.jsx
@@ -45,7 +45,7 @@ export const IOMDescription = () => (
         <p>
             <Localize
                 i18n_default_text={
-                    'Your account will be opened with Deriv (IOM) Ltd, and will be subjected to the jurisdiction and laws of Isle of Man.'
+                    'Your account will be opened with Deriv (MX) Ltd, regulated by the UK Gaming Commission (UKGC) and subject to the jurisdiction and laws of Isle of Man.'
                 }
             />
         </p>

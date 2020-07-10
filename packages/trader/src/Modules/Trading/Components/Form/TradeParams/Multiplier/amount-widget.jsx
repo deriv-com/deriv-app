f3ebfa6c06dd84984d527373eca46a32a092f195
Bala <bala.krishna@regentmarkets.com>
2020-07-10 13:30:03 +0800

    stake & multiplier

--- packages/trader/src/Modules/Trading/Components/Form/TradeParams/Multiplier/amount-widget.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/TradeParams/Multiplier/amount-widget.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import { Money } from '@deriv/components';
 import { connect } from 'Stores/connect';
 import { localize } from '@deriv/translations';
+import MultiplierTradeParamsModal from 'Modules/Trading/Containers/Multiplier/multiplier-trade-params-modal.jsx';
 
-const Amount = ({ amount, currency, multiplier }) => {
+const AmountWidget = ({ amount, currency, multiplier }) => {
+    const [is_open, setIsOpen] = React.useState(false);
+
+    const toggleModal = () => {
+        setIsOpen(!is_open);
+    };
+
     return (
-        <div className='mobile-widget mobile-widget__multiplier-amount'>
-            <div className='mobile-widget__item'>
-                <span className='mobile-widget__item-label'>{localize('Stake')}</span>
-                <span className='mobile-widget__item-value'>
-                    <Money amount={amount} currency={currency} />
-                </span>
+        <React.Fragment>
+            <MultiplierTradeParamsModal is_open={is_open} toggleModal={toggleModal} />
+            <div className='mobile-widget mobile-widget__multiplier-amount' onClick={toggleModal}>
+                <div className='mobile-widget__item'>
+                    <span className='mobile-widget__item-label'>{localize('Stake')}</span>
+                    <span className='mobile-widget__item-value'>
+                        <Money amount={amount} currency={currency} />
+                    </span>
+                </div>
+                <div className='mobile-widget__item'>
+                    <span className='mobile-widget__item-label'>{localize('Multiplier')}</span>
+                    <span className='mobile-widget__item-value'>x{multiplier}</span>
+                </div>
             </div>
-            <div className='mobile-widget__item'>
-                <span className='mobile-widget__item-label'>{localize('Multiplier')}</span>
-                <span className='mobile-widget__item-value'>x{multiplier}</span>
-            </div>
-        </div>
+        </React.Fragment>
     );
 };
 
@@ -24,4 +34,4 @@ export default connect(({ modules }) => ({
     amount: modules.trade.amount,
     currency: modules.trade.currency,
     multiplier: modules.trade.multiplier,
-}))(Amount);
+}))(AmountWidget);

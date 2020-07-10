ab35842044c5ed429b100fdc4cf55014fc5fea3d
cakasuma <amammustofa@gmail.com>
2020-07-10 13:40:14 +0800

    add it to logged in pre-fill information

--- packages/core/src/App/Components/Layout/Footer/index.js
+++ packages/core/src/App/Components/Layout/Footer/index.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-named-as-default
 export NetworkStatus from './network-status.jsx';
+export LiveChat from './live-chat.jsx';
 export * from './endpoint-note.jsx';
 export * from './toggle-fullscreen.jsx';
 export * from './toggle-settings.jsx';
 export * from './help-centre.jsx';
-export * from './live-chat.jsx';
--- packages/core/src/App/Components/Layout/Footer/live-chat.jsx
+++ packages/core/src/App/Components/Layout/Footer/live-chat.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Popover, Icon } from '@deriv/components';
 import { localize } from '@deriv/translations';
+import { WS } from 'Services';
+import { connect } from 'Stores/connect';
 
 class LiveChat extends React.Component {
     state = {
@@ -8,9 +10,41 @@ class LiveChat extends React.Component {
     };
 
     componentDidMount() {
-        window.LiveChatWidget.on('ready', () => {
-            this.setState({ is_livechat_interactive: true });
-        });
+        const { email, is_logged_in, loginid, landing_company_shortcode, currency, residence } = this.props;
+        if (window.LiveChatWidget) {
+            // WS.authorized.getSettings({ get_settings: 1 }).then((response) => {
+            //     const get_settings = response.get_settings || {};
+            //     const { first_name, last_name } = get_settings;
+
+            //     if (email) window.LiveChatWidget.call('set_customer_email', email);
+            //     if (first_name && last_name) window.LiveChatWidget.call('set_customer_name', `${first_name} ${last_name}`);
+            // });
+
+            window.LiveChatWidget.on('ready', () => {
+                this.setState({ is_livechat_interactive: true });
+            });
+            window.LiveChatWidget.on('visibility_changed', ({ visibility }) => {
+                // only visible to CS
+                let session_variables = { loginid: '', landing_company_shortcode: '', currency: '', residence: '' };
+
+                if (visibility === 'maximized' && is_logged_in) {
+                    session_variables = {
+                        ...(loginid && { loginid }),
+                        ...(landing_company_shortcode && { landing_company_shortcode }),
+                        ...(currency && { currency }),
+                        ...(residence && { residence }),
+                    };
+
+                    window.LiveChatWidget.call('set_session_variables', session_variables);
+                }
+
+                if (visibility === 'maximized' && !is_logged_in) {
+                    window.LiveChatWidget.call('set_customer_email', ' ');
+                    window.LiveChatWidget.call('set_customer_name', ' ');
+                    window.LiveChatWidget.call('set_session_variables', session_variables);
+                }
+            });
+        }
     }
 
     render() {
@@ -37,4 +71,11 @@ class LiveChat extends React.Component {
     }
 }
 
-export { LiveChat };
+export default connect(({ client }) => ({
+    email: client.email,
+    is_logged_in: client.is_logged_in,
+    loginid: client.loginid,
+    landing_company_shortcode: client.landing_company_shortcode,
+    currency: client.currency,
+    residence: client.residence,
+}))(LiveChat);

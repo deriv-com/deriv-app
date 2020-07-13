080a846494c913406efc6d143ff2dcf923a64273
Carol Sachdeva <carol@binary.com>
2020-07-13 14:01:14 +0400

    fixed content height issue

--- packages/bot-web-ui/src/components/journal.jsx
+++ packages/bot-web-ui/src/components/journal.jsx
@@ -135,7 +135,7 @@ const Journal = ({ filtered_messages, contract_stage, is_stop_button_visible, ..
         <div className='journal run-panel-tab__content'>
             <Download tab='journal' />
             <Tools {...props} />
-            <ThemedScrollbars className='journal__scrollbars' height={'calc(100% - 50px)'}>
+            <ThemedScrollbars className='journal__scrollbars' height={'calc(100% - 108px)'}>
                 <div className='journal__item-list'>
                     {filtered_messages.length ? (
                         <TransitionGroup>

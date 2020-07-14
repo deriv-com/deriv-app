ccce4f6bc3072117976bccf00414a138c2b01229
sara-fs <sara.khosropour@regentmarkets.com>
2020-07-14 13:54:21 +0430

    Changed uppercase to lowercase

--- packages/trader/src/Modules/Reports/Containers/statement.jsx
+++ packages/trader/src/Modules/Reports/Containers/statement.jsx
@@ -128,19 +128,19 @@ class Statement extends React.Component {
             <React.Fragment>
                 <div className='statement__account-statistics'>
                     <div className='statement__account-statistics--is-rectangle'>
-                        <span className='statement__account-statistics-title'>{localize('Total Deposits')}</span>
+                        <span className='statement__account-statistics-title'>{localize('Total deposits')}</span>
                         <span className='statement__account-statistics-amount'>
                             <Money amount={this.state.total_deposits} currency={currency} />
                         </span>
                     </div>
                     <div className='statement__account-statistics--is-rectangle'>
-                        <span className='statement__account-statistics-title'>{localize('Total Withdrawals')}</span>
+                        <span className='statement__account-statistics-title'>{localize('Total withdrawals')}</span>
                         <span className='statement__account-statistics-amount'>
                             <Money amount={this.state.total_withdrawals} currency={currency} />
                         </span>
                     </div>
                     <div className='statement__account-statistics--is-rectangle'>
-                        <span className='statement__account-statistics-title'>{localize('Net Deposits')}</span>
+                        <span className='statement__account-statistics-title'>{localize('Net deposits')}</span>
                         <span className='statement__account-statistics-amount'>
                             <Money
                                 amount={this.state.total_deposits - this.state.total_withdrawals}

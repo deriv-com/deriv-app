754e8c98e02940daffef61fa1ede553be05418f5
sara-fs <sara.khosropour@regentmarkets.com>
2020-07-20 17:35:10 +0430

    Revert test changes

--- packages/core/src/App/Components/Layout/Header/login-button.jsx
+++ packages/core/src/App/Components/Layout/Header/login-button.jsx
@@ -2,31 +2,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Button } from '@deriv/components';
 import { redirectToLogin } from '_common/base/login';
-import { localize, Localize } from '@deriv/translations';
+import { localize } from '@deriv/translations';
 
 const LoginButton = ({ className }) => (
-    <>
-        <Localize
-            i18n_default_text='Want to exchange between e-wallet currencies? Try <0>bestchange.com</0>'
-            components={[
-                <a
-                    key={0}
-                    href='https://www.bestchange.com/?p=1095016'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    className='link'
-                />,
-            ]}
-        />{' '}
-        <Button
-            id='dt_login_button'
-            className={className}
-            has_effect
-            text={localize('Log in')}
-            onClick={redirectToLogin}
-            tertiary
-        />
-    </>
+    <Button
+        id='dt_login_button'
+        className={className}
+        has_effect
+        text={localize('Log in')}
+        onClick={redirectToLogin}
+        tertiary
+    />
 );
 
 LoginButton.propTypes = {

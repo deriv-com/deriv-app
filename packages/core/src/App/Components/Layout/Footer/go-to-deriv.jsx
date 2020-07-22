bf3e46b16182c47eae772029f97c23eb64d08d15
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2020-07-22 10:38:46 +0430

    Changed go to deriv label

--- packages/core/src/App/Components/Layout/Footer/go-to-deriv.jsx
+++ packages/core/src/App/Components/Layout/Footer/go-to-deriv.jsx
@@ -4,7 +4,7 @@ import { getDerivComLink } from '@deriv/shared';
 import { localize } from '@deriv/translations';
 
 export const GoToDeriv = () => (
-    <Popover alignment='top' message={localize('Go to deriv.com')} className='footer__link'>
+    <Popover alignment='top' message={localize('Go to Deriv.com')} className='footer__link'>
         <a href={getDerivComLink('/')} target='_blank' rel='nofollow noreferrer'>
             <Icon icon='IcDerivOutline' className='footer__icon ic-deriv__icon' />
         </a>

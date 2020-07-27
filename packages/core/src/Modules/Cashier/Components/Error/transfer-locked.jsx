9cebb4bd66692d82e6c426b305312aa835f9c2dd
cakasuma <amammustofa@gmail.com>
2020-07-27 11:42:34 +0800

    fix: error on deposit lock financial assessment

--- packages/core/src/Modules/Cashier/Components/Error/transfer-locked.jsx
+++ packages/core/src/Modules/Cashier/Components/Error/transfer-locked.jsx
@@ -13,7 +13,7 @@ const WithdrawalLocked = ({
 }) => {
     const history = useHistory();
     const items = [
-        ...(is_trading_experience_incomplete || (is_financial_account && is_financial_information_incomplete)
+        ...(is_financial_account && (is_financial_information_incomplete || is_trading_experience_incomplete)
             ? [
                   {
                       content: localize('Complete the financial assessment form'),

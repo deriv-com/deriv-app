748b9595b76ef40458972af665cb71ef96614d4b
Salar Hafezi <salar@firstsource.io>
2020-07-28 19:53:07 +0430

    remove extra dot from domain

--- packages/core/src/App/Constants/app-config.js
+++ packages/core/src/App/Constants/app-config.js
@@ -4,4 +4,4 @@ export const website_domain = 'app.deriv.com';
 export const website_name = 'Deriv';
 export const default_title = website_name;
 export const cookie_banner_expires_in_days = 7;
-export const cookie_banner_domain = isProduction() ? '.deriv.com' : window.location.hostname;
+export const cookie_banner_domain = isProduction() ? 'deriv.com' : window.location.hostname;

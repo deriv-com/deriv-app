49617a0a5362e8aad3a85d6433b476b9623edd33
Salar Hafezi <salar@firstsource.io>
2020-07-28 19:37:18 +0430

    improve cookie_banner_domain for dev and prod envs

--- packages/core/src/App/Constants/app-config.js
+++ packages/core/src/App/Constants/app-config.js
@@ -1,5 +1,7 @@
+import { isProduction } from '@deriv/shared';
+
 export const website_domain = 'app.deriv.com';
 export const website_name = 'Deriv';
 export const default_title = website_name;
 export const cookie_banner_expires_in_days = 7;
-export const cookie_banner_domain = '.deriv.com';
+export const cookie_banner_domain = isProduction() ? '.deriv.com' : window.location.hostname;

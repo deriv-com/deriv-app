62adfbe17eddaafb4dd8951d225639c8745bdff3
Salar Hafezi <salar@firstsource.io>
2020-08-02 13:38:46 +0430

    replace localize with function argument

--- packages/shared/src/utils/digital-options/digital-options.js
+++ packages/shared/src/utils/digital-options/digital-options.js
@@ -1,14 +1,9 @@
-import { localize } from '@deriv/translations';
-
-export const showDigitalOptionsUnavailableError = (showError, platform_name) => {
+export const showDigitalOptionsUnavailableError = (showError, message) => {
+    const { title, text, link } = message;
     showError(
-        localize(
-            'We’re working to have this available for you soon. If you have another account, switch to that account to continue trading. You may add a DMT5 Financial.'
-        ),
-        localize('{{platform_name}} is not available for this account', {
-            platform_name,
-        }),
-        localize('Go to DMT5 dashboard'),
+        title,
+        text,
+        link,
         () => (window.location.href = `${location.protocol}//${location.hostname}/mt5`),
         false
     );

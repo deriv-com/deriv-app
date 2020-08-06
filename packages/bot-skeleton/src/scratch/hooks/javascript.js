a2244db49d0397c2c4fc19d7a074a7f1fd6e8c6f
Carol Sachdeva <carol@binary.com>
2020-08-06 12:30:53 +0400

    Revert "review changes"
    This reverts commit baeb5d373b078b44b30663fd78296728d34fa779.

--- packages/bot-skeleton/src/scratch/hooks/javascript.js
+++
@@ -1,4 +0,0 @@
-// Prefix for highlighting blocks that are executed when Bot is running
-
-Blockly.JavaScript.STATEMENT_PREFIX = 'Bot.highlightedDuringExecution(%1);\n';
-Blockly.JavaScript.addReservedWords('Bot.highlightedDuringExecution');

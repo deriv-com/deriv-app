baeb5d373b078b44b30663fd78296728d34fa779
Carol Sachdeva <carol@binary.com>
2020-08-06 12:29:14 +0400

    review changes

---
+++ packages/bot-skeleton/src/scratch/hooks/javascript.js
@@ -0,0 +1,4 @@
+// Prefix for highlighting blocks that are executed when Bot is running
+
+Blockly.JavaScript.STATEMENT_PREFIX = 'Bot.highlightedDuringExecution(%1);\n';
+Blockly.JavaScript.addReservedWords('Bot.highlightedDuringExecution');

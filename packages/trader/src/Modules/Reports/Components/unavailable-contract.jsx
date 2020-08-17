48abd73567c5ce2afc8a07233a217858c2a535c2
Salar Hafezi <salar@firstsource.io>
2020-08-18 00:15:01 +0430

    Add UnavailableContract component

---
+++ packages/trader/src/Modules/Reports/Components/unavailable-contract.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Localize } from '@deriv/translations';
+import { urlFor } from '@deriv/shared';
+import { website_name } from 'App/Constants/app-config';
+
+const contract_type_binary_url = [
+    'user/portfoliows', // Open position - 0
+    'user/profit_tablews', // Profit table  - 1
+    'user/statementws', // Statement     - 2
+];
+
+const UnavailableContract = ({ contract_type, landing_company_shortcode }) =>
+    landing_company_shortcode === 'iom' ? (
+        <Localize
+            i18n_default_text='This trade type is no longer available. For more details on this transaction, please <0>chat with us</0>.'
+            components={[<a key={0} className='link link--orange' onClick={() => window.LC_API.open_chat_window()} />]}
+        />
+    ) : (
+        <Localize
+            i18n_default_text='This trade type is currently not supported on {{website_name}}. Please go to <0>Binary.com</0> for details.'
+            values={{
+                website_name,
+            }}
+            components={[
+                <a
+                    key={0}
+                    className='link link--orange'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                    href={urlFor(contract_type_binary_url[contract_type], { legacy: true })}
+                />,
+            ]}
+        />
+    );
+
+UnavailableContract.propTypes = {
+    contract_type: PropTypes.number,
+    landing_company_shortcode: PropTypes.string,
+};
+
+export default UnavailableContract;

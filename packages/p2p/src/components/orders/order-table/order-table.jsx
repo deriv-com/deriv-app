272c72ae204d48f548abe527e5992a3864409938
Aaron Imming <aaim@protonmail.com>
2020-08-18 09:53:20 +0800

    Remove redundant optional chaining

--- packages/p2p/src/components/orders/order-table/order-table.jsx
+++ packages/p2p/src/components/orders/order-table/order-table.jsx
@@ -12,12 +12,12 @@ const OrderTable = ({ showDetails }) => {
         {
             text: localize('Active order'),
             value: orderToggleIndex.ACTIVE,
-            count: LocalStorage?.getActiveNotificationCount?.(true) || 0,
+            count: LocalStorage.getActiveNotificationCount?.(true) || 0,
         },
         {
             text: localize('Past order'),
             value: orderToggleIndex.INACTIVE,
-            count: LocalStorage?.getActiveNotificationCount?.(false) || 0,
+            count: LocalStorage.getActiveNotificationCount?.(false) || 0,
         },
     ];
 

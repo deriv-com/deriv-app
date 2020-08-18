93cc235a3f1dfcf3f7923c7afb6f6523eedac622
zakibinary <zaki@regentmarkets.com>
2020-08-18 16:36:24 +0800

    chore: bring back proptypes

--- packages/components/src/components/select-native/select-native.jsx
+++ packages/components/src/components/select-native/select-native.jsx
@@ -102,10 +102,23 @@ class SelectNative extends React.Component {
     }
 }
 
+const list_items_shape = PropTypes.oneOfType([
+    PropTypes.arrayOf(
+        PropTypes.shape({
+            disabled: PropTypes.bool,
+            nativepicker_text: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+            text: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+            value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        })
+    ),
+    PropTypes.object,
+    PropTypes.array,
+]);
+
 SelectNative.propTypes = {
     className: PropTypes.string,
     classNameDisplay: PropTypes.string,
-    list_items: PropTypes.any,
+    list_items: list_items_shape,
     value: PropTypes.string,
     label: PropTypes.string,
     onChange: PropTypes.func,

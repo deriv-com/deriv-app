2b486d7aa969100a0acdc2a28121a24b5b3fc224
Salar Hafezi <salar@firstsource.io>
2020-08-20 19:46:22 +0430

    Add a new type of sucess message -> added account

--- packages/core/src/App/Containers/RealAccountSignup/finished-set-currency.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/finished-set-currency.jsx
@@ -1,18 +1,27 @@
+import classNames from 'classnames';
 import React from 'react';
 import { Button, Icon, Div100vhContainer } from '@deriv/components';
 import { isDesktop } from '@deriv/shared';
 import { localize, Localize } from '@deriv/translations';
 
 const FinishedSetCurrency = ({ current, onCancel, onSubmit, prev }) => {
-    const IconPrevCurrency = () => <Icon icon={`IcCurrency-${prev.toLowerCase()}`} height={120} width={90} />;
+    const IconPrevCurrency = () =>
+        prev ? <Icon icon={`IcCurrency-${prev.toLowerCase()}`} height={120} width={90} /> : null;
     const IconNextCurrency = () => <Icon icon={`IcCurrency-${current.toLowerCase()}`} height={120} width={90} />;
-    const IconArrow = () => <Icon icon='IcArrowPointerRight' color='red' width={50} height={20} />;
+    const IconArrow = () => (prev ? <Icon icon='IcArrowPointerRight' color='red' width={50} height={20} /> : null);
 
     const IconWon = ({ className }) => <Icon className={className} icon='IcCheckmarkCircle' color='green' />;
 
+    const message = prev
+        ? '<0>You have successfully changed your currency to {{currency}}.</0><0>Make a deposit now to start trading.</0>'
+        : '<0>You have added a {{currency}} account.</0><0> Make a deposit now to start trading.</0>';
     return (
         <Div100vhContainer className='status-dialog' is_disabled={isDesktop()} height_offset='40px'>
-            <div className='status-dialog__icon-area'>
+            <div
+                className={classNames('status-dialog__icon-area', {
+                    'set-currency': !prev,
+                })}
+            >
                 <IconPrevCurrency />
                 <IconArrow />
                 <IconNextCurrency />
@@ -23,7 +32,7 @@ const FinishedSetCurrency = ({ current, onCancel, onSubmit, prev }) => {
                     <Localize i18n_default_text='Success!' />
                 </h2>
                 <Localize
-                    i18n_default_text='<0>You have successfully changed your currency to {{currency}}.</0><0>Make a deposit now to start trading.</0>'
+                    i18n_default_text={message}
                     values={{
                         currency: current,
                     }}

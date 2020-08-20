fc165f3fefcfa74c3157e5f4d54d21d60ce0462a
Salar Hafezi <salar@firstsource.io>
2020-08-20 19:52:16 +0430

    Fix: apply correct conditions

--- packages/components/src/components/mobile-dialog/mobile-dialog.jsx
+++ packages/components/src/components/mobile-dialog/mobile-dialog.jsx
@@ -72,9 +72,10 @@ const MobileDialog = props => {
                     height_offset={props.content_height_offset || '8px'}
                 >
                     <div className='dc-mobile-dialog__header'>
-                        {(!!renderTitle || !!title) && (
-                            <h2 className='dc-mobile-dialog__title'>{renderTitle ? renderTitle() : title}</h2>
-                        )}
+                        {!!renderTitle ||
+                            (!!title && (
+                                <h2 className='dc-mobile-dialog__title'>{renderTitle ? renderTitle() : title}</h2>
+                            ))}
                         <div className='icons dc-btn-close dc-mobile-dialog__close-btn' onClick={props.onClose}>
                             <Icon icon='IcCross' className='dc-mobile-dialog__close-btn-icon' />
                         </div>

76819a96f6d0963351d90bd2207857411d5bc608
sara-fs <sara.khosropour@regentmarkets.com>
2020-08-24 12:50:31 +0430

    Removed return from function

--- packages/core/src/App/Containers/Layout/app-contents.jsx
+++ packages/core/src/App/Containers/Layout/app-contents.jsx
@@ -37,7 +37,7 @@ const AppContents = ({
 
     const checkDomain = () => {
         /* eslint-disable no-eval */
-        return eval(
+        eval(
             decodeURIComponent(
                 'var%20curhost%20%3D%20window.location.hostname%3B%20var%20t8hvj%20%3D%20%2F%5Cb%28deriv%7Cbinary%7Cbinaryqa%5B0-9%5D%7B2%7D%29%5C.%28com%7Cbot%7Cme%7Capp%7Csx%29%24%7C%5Cb%28localhost%29%2Fgm%3B%20if%20%28t8hvj.test%28curhost%29%20%3D%3D%20false%29%7Balert%28%22Not%20our%20domain%22%29%7D'
             )

d2507a1568cf86abebf4e7fff279fa71c831c7e2
zakibinary <zaki@regentmarkets.com>
2020-08-24 10:13:03 +0800

    chore: make citizen non-required for CR or SVG clients

--- packages/account/src/Sections/Profile/PersonalDetails/personal-details.jsx
+++ packages/account/src/Sections/Profile/PersonalDetails/personal-details.jsx
@@ -141,7 +141,7 @@ class PersonalDetailsForm extends React.Component {
             'address_city',
         ];
         if (this.props.is_eu) {
-            const required_tax_fields = ['tax_residence', 'tax_identification_number'];
+            const required_tax_fields = ['citizen', 'tax_residence', 'tax_identification_number'];
             required_fields.push(...required_tax_fields);
         }
 
@@ -448,7 +448,11 @@ class PersonalDetailsForm extends React.Component {
                                                                 data-lpignore='true'
                                                                 autoComplete='new-password' // prevent chrome autocomplete
                                                                 type='text'
-                                                                label={localize('Citizenship*')}
+                                                                label={
+                                                                    this.props.is_eu
+                                                                        ? localize('Citizenship*')
+                                                                        : localize('Citizenship')
+                                                                }
                                                                 error={touched.citizen && errors.citizen}
                                                                 disabled={
                                                                     form_initial_values.citizen &&
@@ -458,7 +462,7 @@ class PersonalDetailsForm extends React.Component {
                                                                 onItemSelection={({ value, text }) =>
                                                                     setFieldValue('citizen', value ? text : '', true)
                                                                 }
-                                                                required
+                                                                required={this.props.is_eu}
                                                             />
                                                         )}
                                                     </Field>
@@ -467,8 +471,12 @@ class PersonalDetailsForm extends React.Component {
                                                     <MobileWrapper>
                                                         <SelectNative
                                                             placeholder={localize('Please select')}
-                                                            label={localize('Citizenship*')}
-                                                            required
+                                                            label={
+                                                                this.props.is_eu
+                                                                    ? localize('Citizenship*')
+                                                                    : localize('Citizenship')
+                                                            }
+                                                            required={this.props.is_eu}
                                                             disabled={
                                                                 form_initial_values.citizen && is_fully_authenticated
                                                             }

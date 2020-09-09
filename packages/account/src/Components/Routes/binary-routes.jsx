1316f9e1aea3f704424ff8f0b12d0b3a4e609faa
Mohammadreza Ghorbani <reza@firstsource.email>
2020-09-09 16:36:12 +0430

    Hide some of the account package routes for deriv-crypto

--- packages/account/src/Components/Routes/binary-routes.jsx
+++ packages/account/src/Components/Routes/binary-routes.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import getRoutesConfig from 'Constants/routes-config';
+import { PlatformContext } from '@deriv/shared';
 import RouteWithSubRoutes from './route-with-sub-routes.jsx';
 
-const BinaryRoutes = (props) => (
-    <React.Suspense
-        fallback={() => {
-            return <div>LOADING...</div>;
-        }}
-    >
-        <Switch>
-            {getRoutesConfig().map((route, idx) => (
-                <RouteWithSubRoutes key={idx} {...route} {...props} />
-            ))}
-        </Switch>
-    </React.Suspense>
-);
+const BinaryRoutes = (props) => {
+    const platform_store = React.useContext(PlatformContext);
+
+    return (
+        <React.Suspense
+            fallback={() => {
+                return <div>LOADING...</div>;
+            }}
+        >
+            <Switch>
+                {getRoutesConfig({ is_deriv_crypto: platform_store.is_deriv_crypto }).map((route, idx) => (
+                    <RouteWithSubRoutes key={idx} {...route} {...props} />
+                ))}
+            </Switch>
+        </React.Suspense>
+    );
+};
 
 export default BinaryRoutes;

6143fc2bfc3929aff1aa7b0ea7305ee5a94a0caf
oskar-binary <oskar@binary.com>
2020-09-14 16:23:05 +0800

    fix: remove prettified bot-skeleton files

--- packages/bot-skeleton/src/services/tradeEngine/trade/OpenContract.js
+++ packages/bot-skeleton/src/services/tradeEngine/trade/OpenContract.js
@@ -5,10 +5,10 @@ import { doUntilDone } from '../utils/helpers';
 
 const AFTER_FINISH_TIMEOUT = 5;
 
-export default (Engine) =>
+export default Engine =>
     class OpenContract extends Engine {
         observeOpenContract() {
-            this.listen('proposal_open_contract', (r) => {
+            this.listen('proposal_open_contract', r => {
                 const contract = r.proposal_open_contract;
 
                 if (!this.expectedContractId(contract.contract_id)) {
@@ -47,7 +47,7 @@ export default (Engine) =>
         }
 
         waitForAfter() {
-            return new Promise((resolve) => {
+            return new Promise(resolve => {
                 this.afterPromise = resolve;
             });
         }
@@ -58,7 +58,7 @@ export default (Engine) =>
             }
             this.contractId = contractId;
 
-            doUntilDone(() => this.api.subscribeToOpenContract(contractId)).then((r) => {
+            doUntilDone(() => this.api.subscribeToOpenContract(contractId)).then(r => {
                 ({
                     proposal_open_contract: { id: this.openContractId },
                 } = r);

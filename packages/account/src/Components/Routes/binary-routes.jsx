2e2ec2a5f8e12c017a89ea2c2300ed2ff83501f7
oskar-binary <oskar@binary.com>
2020-09-15 16:32:46 +0800

    feat: filter pages for deriv-crypto for cashier and mobile

--- packages/account/src/Components/Routes/binary-routes.jsx
+++ packages/account/src/Components/Routes/binary-routes.jsx
@@ -5,7 +5,7 @@ import getRoutesConfig from 'Constants/routes-config';
 import RouteWithSubRoutes from './route-with-sub-routes.jsx';
 
 const BinaryRoutes = (props) => {
-    const platform_store = React.useContext(PlatformContext);
+    const { is_deriv_crypto } = React.useContext(PlatformContext);
 
     return (
         <React.Suspense
@@ -18,7 +18,7 @@ const BinaryRoutes = (props) => {
             }}
         >
             <Switch>
-                {getRoutesConfig({ hide_crypto_routes: platform_store.is_deriv_crypto }).map((route, idx) => (
+                {getRoutesConfig({ hide_crypto_routes: is_deriv_crypto }).map((route, idx) => (
                     <RouteWithSubRoutes key={idx} {...route} {...props} />
                 ))}
             </Switch>

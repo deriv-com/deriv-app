c69b23027028a73b298dcdc2ef0ea0c2cd99b3ef
Carol Sachdeva <carol@binary.com>
2020-09-15 16:49:06 +0400

    create useIsMounted in shared

---
+++ packages/shared/src/utils/mounted/mounted.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const useIsMounted = () => {
+    const is_mounted = React.useRef(false);
+
+    React.useEffect(() => {
+        is_mounted.current = true;
+
+        return () => (is_mounted.current = false);
+    }, []);
+    return is_mounted;
+};

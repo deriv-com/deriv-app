89d64b5b2dd07492619f1facef3a486a959ad04b
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2020-09-16 12:51:31 +0430

    Added responsivness

--- packages/core/src/App/Containers/WelcomeModal/welcome-modal.jsx
+++ packages/core/src/App/Containers/WelcomeModal/welcome-modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Modal, Icon, Button } from '@deriv/components';
+import { Modal, Icon, Button, ThemedScrollbars } from '@deriv/components';
 import { routes } from '@deriv/shared';
 import { localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
@@ -85,83 +85,85 @@ const WelcomeModal = ({ toggleAccountsDialog, toggleWelcomeModal, history }) => 
             has_close_icon={false}
             has_outer_content={true}
         >
-            <h2 className='welcome__title'>{localize('Where would you like to start?')}</h2>
-            <div
-                className={classNames('welcome__message', 'welcome__message--left', {
-                    'welcome__message--visible': hovered === 'left',
-                })}
-            >
-                <p className='welcome__message__text'>{localize('The choice of professionals')}</p>
-                <Icon icon='IcArrowRightCurly' size={43} />
-            </div>
-            <div
-                className={classNames('welcome__message', 'welcome__message--right', {
-                    'welcome__message--visible': hovered === 'right',
-                })}
-            >
-                <Icon icon='IcArrowLeftCurly' size={43} />
-                <p className='welcome__message__text'>{localize('Not sure? try this')}</p>
-            </div>
-            <div className='welcome__body'>
-                <WelcomeColumn
-                    button_text={localize('Start here')}
-                    className='welcome-column--left'
-                    description={localize(
-                        'Trade with leverage and low spreads for better returns on successful trades.'
+            <ThemedScrollbars height={600}>
+                <h2 className='welcome__title'>{localize('Where would you like to start?')}</h2>
+                <div
+                    className={classNames('welcome__message', 'welcome__message--left', {
+                        'welcome__message--visible': hovered === 'left',
+                    })}
+                >
+                    <p className='welcome__message__text'>{localize('The choice of professionals')}</p>
+                    <Icon icon='IcArrowRightCurly' size={43} />
+                </div>
+                <div
+                    className={classNames('welcome__message', 'welcome__message--right', {
+                        'welcome__message--visible': hovered === 'right',
+                    })}
+                >
+                    <Icon icon='IcArrowLeftCurly' size={43} />
+                    <p className='welcome__message__text'>{localize('Not sure? try this')}</p>
+                </div>
+                <div className='welcome__body'>
+                    <WelcomeColumn
+                        button_text={localize('Start here')}
+                        className='welcome-column--left'
+                        description={localize(
+                            'Trade with leverage and low spreads for better returns on successful trades.'
+                        )}
+                        icons={['IcPercentSolid']}
+                        is_hovered={hovered === 'left'}
+                        onButtonClick={switchToDMT5}
+                        platforms={[
+                            {
+                                icon: 'IcBrandDmt5',
+                                title: localize('Deriv MetaTrader 5 (DMT5)'),
+                                description: localize('The platform of choice for professionals.'),
+                            },
+                        ]}
+                        title={localize('Margin (MetaTrader 5)')}
+                        onMouseEnter={() => {
+                            setHovered('left');
+                        }}
+                        onMouseLeave={() => {
+                            setHovered(null);
+                        }}
+                    />
+                    <WelcomeColumn
+                        button_text={localize('Start here')}
+                        className='welcome-column--right'
+                        description={localize(
+                            'Earn fixed payouts with options, or trade multipliers to combine the upside of margin trading with the simplicity of options.'
+                        )}
+                        icons={['IcUpDownSolid', 'IcCrossSolid']}
+                        is_hovered={hovered === 'right'}
+                        onButtonClick={switchToRealAccount}
+                        platforms={[
+                            {
+                                icon: 'IcBrandDtrader',
+                                title: localize('DTrader'),
+                                description: localize('Start trading with a powerful, yet easy-to-use platform.'),
+                            },
+                            {
+                                icon: 'IcBrandDbot',
+                                title: localize('DBot'),
+                                description: localize('Automate your trading ideas without coding.'),
+                            },
+                        ]}
+                        title={localize('Options & Multipliers')}
+                        onMouseEnter={() => {
+                            setHovered('right');
+                        }}
+                        onMouseLeave={() => {
+                            setHovered(null);
+                        }}
+                    />
+                </div>
+                <p className='welcome__footer'>
+                    {localize(
+                        'Whatever you choose, you will always be able to access Margin or Options & Multipliers at any time'
                     )}
-                    icons={['IcPercentSolid']}
-                    is_hovered={hovered === 'left'}
-                    onButtonClick={switchToDMT5}
-                    platforms={[
-                        {
-                            icon: 'IcBrandDmt5',
-                            title: localize('Deriv MetaTrader 5 (DMT5)'),
-                            description: localize('The platform of choice for professionals.'),
-                        },
-                    ]}
-                    title={localize('Margin (MetaTrader 5)')}
-                    onMouseEnter={() => {
-                        setHovered('left');
-                    }}
-                    onMouseLeave={() => {
-                        setHovered(null);
-                    }}
-                />
-                <WelcomeColumn
-                    button_text={localize('Start here')}
-                    className='welcome-column--right'
-                    description={localize(
-                        'Earn fixed payouts with options, or trade multipliers to combine the upside of margin trading with the simplicity of options.'
-                    )}
-                    icons={['IcUpDownSolid', 'IcCrossSolid']}
-                    is_hovered={hovered === 'right'}
-                    onButtonClick={switchToRealAccount}
-                    platforms={[
-                        {
-                            icon: 'IcBrandDtrader',
-                            title: localize('DTrader'),
-                            description: localize('Start trading with a powerful, yet easy-to-use platform.'),
-                        },
-                        {
-                            icon: 'IcBrandDbot',
-                            title: localize('DBot'),
-                            description: localize('Automate your trading ideas without coding.'),
-                        },
-                    ]}
-                    title={localize('Options & Multipliers')}
-                    onMouseEnter={() => {
-                        setHovered('right');
-                    }}
-                    onMouseLeave={() => {
-                        setHovered(null);
-                    }}
-                />
-            </div>
-            <p className='welcome__footer'>
-                {localize(
-                    'Whatever you choose, you will always be able to access Margin or Options & Multipliers at any time'
-                )}
-            </p>
+                </p>
+            </ThemedScrollbars>
         </Modal>
     );
 };

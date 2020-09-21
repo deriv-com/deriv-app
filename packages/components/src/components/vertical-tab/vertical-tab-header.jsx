89aebaf42b651279a88c799a3e47aeb9a4707396
cakasuma <amammustofa@gmail.com>
2020-09-21 17:07:26 +0800

    fix: pass in props default text instead of direct string to shared function

--- packages/components/src/components/vertical-tab/vertical-tab-header.jsx
+++ packages/components/src/components/vertical-tab/vertical-tab-header.jsx
@@ -20,7 +20,7 @@ const VerticalTabHeader = ({ children, className, is_floating, is_routed, item, 
     const label = item.label || item.title; // item.label.charAt(0).toUpperCase() + item.label.slice(1).toLowerCase();
     const is_active = selected && selected.label === item.label;
     const handleClick = () => onChange(item);
-    const id = `dc_${getKebabCase(label)}_link`;
+    const id = `dc_${getKebabCase(label?.props?.i18n_default_text)}_link`;
     const is_disabled = !!item.is_disabled;
     const count = item.count || 0;
 

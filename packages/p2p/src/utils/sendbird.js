e4e018edc3d582dad6b1410575dfdd1315467934
Mohammadreza Ghorbani <reza@firstsource.email>
2020-09-22 13:35:23 +0330

    fix eslint errors

--- packages/p2p/src/utils/sendbird.js
+++ packages/p2p/src/utils/sendbird.js
@@ -1,13 +1,14 @@
 import { requestWS } from './websocket';
 
 const chatCreate = order_id =>
-    new Promise(async (resolve, reject) => {
-        const chat_create_response = await requestWS({ p2p_chat_create: 1, order_id });
-        if (chat_create_response.error) {
-            reject(chat_create_response.error.code);
-        }
+    new Promise((resolve, reject) => {
+        requestWS({ p2p_chat_create: 1, order_id }).then(chat_create_response => {
+            if (chat_create_response.error) {
+                reject(chat_create_response.error.code);
+            }
 
-        resolve(chat_create_response.p2p_chat_create);
+            resolve(chat_create_response.p2p_chat_create);
+        });
     });
 
 export { chatCreate };

8add834e481a1f2ef8c899e1e237b7e0f6470c76
Abdalla <abdalla@Abdallas-MacBook-Pro.local>
2020-09-23 16:51:37 +0800

    fix back-p2p button appearance

--- packages/shared/src/utils/platform/platform.js
+++ packages/shared/src/utils/platform/platform.js
@@ -12,12 +12,6 @@ export const isMT5 = () =>
     /^\/mt5/.test(window.location.pathname) ||
     (/^\/(br_)/.test(window.location.pathname) && window.location.pathname.split('/')[2] === 'mt5');
 
-export const isP2P = (routing_history, platform_route) => {
-    const routing_history_index = routing_history.length > 1 ? 1 : 0;
-    const history_item = routing_history[routing_history_index];
-    return history_item?.pathname === platform_route;
-};
-
 export const getPlatformHeader = routing_history => {
     if (isBot() || isNavigationFromPlatform(routing_history, routes.bot)) return 'DBot';
     if (isMT5() || isNavigationFromPlatform(routing_history, routes.mt5)) return 'DMT5';
@@ -35,30 +29,50 @@ export const getPlatformIcon = routing_history => {
 export const getPlatformRedirect = routing_history => {
     if (isBot() || isNavigationFromPlatform(routing_history, routes.bot)) return { name: 'DBot', route: routes.bot };
     if (isMT5() || isNavigationFromPlatform(routing_history, routes.mt5)) return { name: 'DMT5', route: routes.mt5 };
-    if (isP2P(routing_history, routes.cashier_p2p)) return { name: 'P2P', route: routes.cashier_p2p };
+    if (isNavegationFromP2P(routing_history, routes.cashier_p2p)) return { name: 'P2P', route: routes.cashier_p2p };
     if (isNavigationFromPlatform(routing_history, routes.smarttrader))
         return { name: 'SmartTrader', route: routes.smarttrader };
     return { name: 'DTrader', route: routes.trade };
 };
 
 export const isNavigationFromPlatform = (app_routing_history, platform_route, should_ignore_parent_path = false) => {
-    const app_routing_history_length = app_routing_history.length;
-
-    if (app_routing_history_length > 0) {
-        // Look for the most recent tab visited
+    if (app_routing_history.length > 0) {
         const getParentPath = pathname => (/^http/.test(pathname) ? false : pathname.split('/')[1]);
-        const recent_history_index = app_routing_history_length > 1 ? 1 : 0;
-        const history_item = app_routing_history[recent_history_index];
-        const history_item_parent_path = getParentPath(history_item.pathname);
 
-        if (
-            history_item_parent_path === getParentPath(platform_route) &&
-            should_ignore_parent_path &&
-            history_item.pathname === platform_route
-        ) {
-            return true;
+        for (let i = 0; i < app_routing_history.length; i++) {
+            const history_item = app_routing_history[i];
+            const history_item_parent_path = getParentPath(history_item.pathname);
+            const next_history_item = app_routing_history.length > i + 1 && app_routing_history[i + 1];
+
+            if (
+                history_item_parent_path === getParentPath(platform_route) ||
+                (should_ignore_parent_path && history_item.pathname === platform_route)
+            ) {
+                return true;
+            } else if (!next_history_item) {
+                return false;
+            } else if (history_item_parent_path === getParentPath(next_history_item.pathname)) {
+                // Continue walking until we see passed in platform_route.
+                continue; // eslint-disable-line no-continue
+            } else {
+                // Return false when path matches a platform parent path, but don't return anything
+                // when a non-platform path was seen. i.e. navigating between /cashier and /reports
+                // should not affect navigating back to platform when clicking cross.
+                const platform_parent_paths = [routes.mt5, routes.bot, routes.trade].map(route => getParentPath(route));
+                const is_other_platform_path = platform_parent_paths.includes(history_item_parent_path);
+
+                if (is_other_platform_path) {
+                    break;
+                }
+            }
         }
     }
 
     return false;
+};
+
+export const isNavegationFromP2P = (routing_history, platform_route) => {
+    const routing_history_index = routing_history.length > 1 ? 1 : 0;
+    const history_item = routing_history[routing_history_index];
+    return history_item?.pathname === platform_route;
 };

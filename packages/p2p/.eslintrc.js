53bf99a6add96ca4da671dd0832c8c2f19179796
Mohammadreza Ghorbani <reza@firstsource.email>
2020-09-23 09:20:10 +0330

    Remove eslint packages from p2p

--- packages/p2p/.eslintrc.js
+++ packages/p2p/.eslintrc.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
     extends: '../../.eslintrc.js',
     settings: {
-        react: {
-            version: '16',
-        },
         'import/resolver': {
             webpack: { config: path.resolve(__dirname, 'webpack.config.js') },
         },

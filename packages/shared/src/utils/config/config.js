747fc97a933682b3abd6ad0e609001e2d99f556b
Mohammadreza Ghorbani <reza@firstsource.email>
2020-09-29 18:04:29 +0330

    Fix staging-crypto default app id

--- packages/shared/src/utils/config/config.js
+++ packages/shared/src/utils/config/config.js
@@ -65,9 +65,10 @@ export const getAppId = () => {
     } else if (isStaging()) {
         window.localStorage.removeItem('config.default_app_id');
         app_id = isBot() ? 19112 : 16303; // it's being used in endpoint chrome extension - please do not remove
-    } else if (getPlatformFromUrl().is_staging_deriv_crypto) {
-        window.localStorage.removeItem('config.default_app_id');
-        app_id = isBot() ? DERIV_CRYPTO_STAGING_DBOT_APP_ID : DERIV_CRYPTO_STAGING_APP_ID;
+        if (getPlatformFromUrl().is_staging_deriv_crypto) {
+            window.localStorage.removeItem('config.default_app_id');
+            app_id = isBot() ? DERIV_CRYPTO_STAGING_DBOT_APP_ID : DERIV_CRYPTO_STAGING_APP_ID;
+        }
     } else if (/localhost/i.test(window.location.hostname)) {
         app_id = 17044;
     } else if (getPlatformFromUrl().is_deriv_crypto && isBot()) {

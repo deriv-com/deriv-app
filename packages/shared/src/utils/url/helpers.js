c783cb93d1917b42573b65db5a96774a184b4ff5
Mohammadreza Ghorbani <reza@firstsource.email>
2020-09-30 12:05:08 +0330

    refactor into variable

--- packages/shared/src/utils/url/helpers.js
+++ packages/shared/src/utils/url/helpers.js
@@ -12,11 +12,22 @@ export const getUrlSmartTrader = () => {
 };
 
 export const getPlatformFromUrl = (domain = window.location.hostname) => {
-    return {
+    const resolutions = {
         is_staging_deriv_crypto: /^staging-app\.derivcrypto\.com$/i.test(domain),
         is_staging_deriv_app: /^staging-app\.deriv\.com$/i.test(domain),
         is_deriv_crypto: /^app\.derivcrypto\.com$/i.test(domain),
         is_deriv_app: /^app\.deriv\.com$/i.test(domain),
         is_test_link: /^(.*)\.binary\.sx$/i.test(domain),
     };
+
+    return {
+        ...resolutions,
+        is_staging: resolutions.is_staging_deriv_app || resolutions.is_staging_deriv_crypto,
+    };
+};
+
+export const isStaging = (domain = window.location.hostname) => {
+    const { is_staging_deriv_crypto, is_staging_deriv_app } = getPlatformFromUrl(domain);
+
+    return is_staging_deriv_crypto || is_staging_deriv_app;
 };

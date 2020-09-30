75b1d6b1f6bb7ca3434318f494e3527f522e1634
Matin <seyad@Seyads-MacBook-Pro.local>
2020-09-30 12:06:23 +0800

    Fix: changes has been made based on code reviews - Header component added to handle Wizard header from outside of the component.

--- packages/components/src/components/wizard/wizard-header.jsx
+++ packages/components/src/components/wizard/wizard-header.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Step = ({ children, className }) => {
-    return <div className={classNames('wizard__step', className)}>{children}</div>;
-};
+const Header = ({ children, className }) => (
+    <div className={classNames('wizard__main-header', className)}>{children}</div>
+);
 
-Step.propTypes = {
+Header.propTypes = {
     children: PropTypes.node.isRequired,
     className: PropTypes.string,
 };
 
-export default Step;
+export default Header;
--- packages/components/src/components/wizard/wizard.jsx
+++ packages/components/src/components/wizard/wizard.jsx
@@ -1,94 +1,77 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
-import Step from './wizard-step.jsx';
-import Icon from '../icon';
-import FormProgress from '../form-progress';
-import DesktopWrapper from '../desktop-wrapper';
-import MobileWrapper from '../mobile-wrapper';
+import Header from './wizard-header.jsx';
+import Icon from '../icon/icon.jsx';
 
-const Wizard = React.forwardRef(
-    ({ steps, lbl_previous, has_prev, lbl_next, has_next, has_form_progress, className }, ref) => {
-        const [step, setStep] = React.useState(0);
-        const [lastStep] = React.useState(steps.length - 1);
-        const [component, setComponent] = React.useState(steps[0].component);
+const Wizard = React.forwardRef(({ children, steps, lbl_previous, has_prev, lbl_next, has_next, className }, ref) => {
+    const [step, setStep] = React.useState(0);
+    const [last_step] = React.useState(steps.length - 1);
+    const [component, setComponent] = React.useState(steps[0].component);
 
-        const nextStep = () => {
-            if (step + 1 > lastStep) return;
-            const newStep = step + 1;
-            setStep(newStep);
-            setComponent(steps[newStep].component);
-        };
+    const nextStep = () => {
+        const next_step = step + 1;
+        const is_last_step = next_step > last_step;
+        if (is_last_step) return;
+        setStep(next_step);
+        setComponent(steps[next_step].component);
+    };
 
-        const prevStep = () => {
-            if (step - 1 < 0) return;
-            const newStep = step - 1;
-            setStep(newStep);
-            setComponent(steps[newStep].component);
-        };
+    const prevStep = () => {
+        const prev_step = step - 1;
+        const is_first_step = prev_step < 0;
+        if (is_first_step) return;
+        setStep(prev_step);
+        setComponent(steps[prev_step].component);
+    };
 
-        const selectStep = selectedStep => {
-            if (selectedStep < 0 || selectedStep > lastStep) return;
-            setStep(selectedStep);
-            setComponent(steps[selectedStep].component);
-        };
+    const selectStep = selected_step => {
+        const is_prev_step_not_allowed = selected_step < 0;
+        const is_next_step_not_allowed = selected_step > last_step;
+        if (is_prev_step_not_allowed || is_next_step_not_allowed) return;
+        setStep(selected_step);
+        setComponent(steps[selected_step].component);
+    };
 
-        React.useImperativeHandle(ref, () => ({
-            nextStep,
-            selectStep,
-        }));
+    React.useImperativeHandle(ref, () => ({
+        nextStep,
+        selectStep,
+    }));
 
-        const prevButton = () =>
-            step !== 0 ? (
-                <a onClick={prevStep} className='wizard__btn wizard__btn-left'>
-                    <Icon icon='IcArrowLeftBold' className='wizard__btn-icon-left' />
-                    {lbl_previous}
-                </a>
-            ) : (
-                <div className='wizard__placeholder' />
-            );
-
-        const nextButton = () =>
-            step !== lastStep ? (
-                <a onClick={nextStep} className='wizard__btn wizard__btn-right'>
-                    {lbl_next}
-                    <Icon icon='IcArrowRightBold' className='wizard__btn-icon-right' />
-                </a>
-            ) : (
-                <div className='wizard__placeholder' />
-            );
-
-        return (
-            <div className={classNames('wizard', className)}>
-                {has_form_progress && (
-                    <React.Fragment>
-                        <DesktopWrapper>
-                            <FormProgress steps={steps} current_step={step} />
-                        </DesktopWrapper>
-                        <MobileWrapper>
-                            <div className='wizard__header-steps'>
-                                <h4 className='wizard__header-steps-title'>
-                                    {`Step ${step + 1}: ${steps[step].header.title} (${step + 1} of ${steps.length})`}
-                                </h4>
-                                {steps[step].header.active_title && (
-                                    <h4 className='wizard__header-steps-subtitle'>{steps[step].header.active_title}</h4>
-                                )}
-                            </div>
-                        </MobileWrapper>
-                    </React.Fragment>
-                )}
-                <div className='wizard__header'>
-                    {has_prev && prevButton()}
-                    {has_next && nextButton()}
-                </div>
-                <Step>{component}</Step>
-            </div>
+    const prevButton = () =>
+        step !== 0 ? (
+            <a onClick={prevStep} className='wizard__btn wizard__btn-left'>
+                <Icon icon='IcArrowLeftBold' className='wizard__btn-icon-left' />
+                {lbl_previous}
+            </a>
+        ) : (
+            <div className='wizard__placeholder' />
         );
-    }
-);
+
+    const nextButton = () =>
+        step !== last_step ? (
+            <a onClick={nextStep} className='wizard__btn wizard__btn-right'>
+                {lbl_next}
+                <Icon icon='IcArrowRightBold' className='wizard__btn-icon-right' />
+            </a>
+        ) : (
+            <div className='wizard__placeholder' />
+        );
+
+    return (
+        <div className={classNames('wizard', className)}>
+            {children}
+            <div className='wizard__header'>
+                {has_prev && prevButton()}
+                {has_next && nextButton()}
+            </div>
+            {component}
+        </div>
+    );
+});
 
 Wizard.displayName = 'Wizard';
-Wizard.Step = Step;
+Wizard.Header = Header;
 
 Wizard.propTypes = {
     className: PropTypes.string,
@@ -96,7 +79,6 @@ Wizard.propTypes = {
     has_prev: PropTypes.bool,
     lbl_next: PropTypes.string,
     has_next: PropTypes.bool,
-    has_form_progress: PropTypes.bool,
     steps: PropTypes.array,
 };
 

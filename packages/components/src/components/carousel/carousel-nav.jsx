c7e782ebf09e9f1358a3f5d4bf19fce17b86a19d
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2020-10-04 17:11:39 +0330

    Added navigation logics

--- packages/components/src/components/carousel/carousel-nav.jsx
+++ packages/components/src/components/carousel/carousel-nav.jsx
@@ -3,45 +3,39 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Icon from '../icon';
 
-const Nav = ({ active_index, handleNavigationClick, list }) => {
-    const handleNextClick = () => {
-        const next_idx = active_index + 1;
-        const is_reached_end = next_idx === list.length;
-
-        if (!is_reached_end) {
-            handleNavigationClick(next_idx);
-        } else {
-            handleNavigationClick(0);
-        }
-    };
-
-    const handlePrevClick = () => {
-        const prev_idx = active_index - 1;
-
-        if (prev_idx > -1) {
-            handleNavigationClick(prev_idx);
-        } else {
-            handleNavigationClick(list.length - 1);
-        }
-    };
-
+const Nav = ({
+    active_index,
+    className,
+    handleNextClick,
+    handlePrevClick,
+    handleNavigationClick,
+    list,
+    show_bullet,
+    show_nav,
+}) => {
     return (
-        <nav className='carousel__nav'>
-            <span id='dt_contract_info_left_nav' className='carousel__icon' onClick={handlePrevClick}>
-                <Icon icon='IcChevronLeft' />
-            </span>
-            <ul className='carousel__nav-list'>
-                <li
-                    className={classNames('carousel__nav-item', 'carousel__nav-item--active')}
-                    style={{ transform: `translate3d(${24 * active_index}px, 0, 0)` }}
-                />
-                {list.map((_, idx) => (
-                    <li key={idx} className='carousel__nav-item' onClick={() => handleNavigationClick(idx)} />
-                ))}
-            </ul>
-            <span id='dt_contract_info_right_nav' className='carousel__icon' onClick={handleNextClick}>
-                <Icon icon='IcChevronRight' />
-            </span>
+        <nav className={classNames('carousel__nav', className)}>
+            {show_nav && (
+                <span id='dt_contract_info_left_nav' className='carousel__icon' onClick={handlePrevClick}>
+                    <Icon icon='IcChevronLeft' />
+                </span>
+            )}
+            {show_bullet && (
+                <ul className='carousel__nav-list'>
+                    <li
+                        className={classNames('carousel__nav-item', 'carousel__nav-item--active')}
+                        style={{ transform: `translate3d(${24 * active_index}px, 0, 0)` }}
+                    />
+                    {list.map((_, idx) => (
+                        <li key={idx} className='carousel__nav-item' onClick={() => handleNavigationClick(idx)} />
+                    ))}
+                </ul>
+            )}
+            {show_nav && (
+                <span id='dt_contract_info_right_nav' className='carousel__icon' onClick={handleNextClick}>
+                    <Icon icon='IcChevronRight' />
+                </span>
+            )}
         </nav>
     );
 };
@@ -50,6 +44,7 @@ Nav.propTypes = {
     active_index: PropTypes.number,
     handleNavigationClick: PropTypes.func,
     list: PropTypes.array,
+    show_bullet: PropTypes.bool,
 };
 
 export default React.memo(Nav);
--- packages/components/src/components/carousel/carousel.jsx
+++ packages/components/src/components/carousel/carousel.jsx
@@ -3,27 +3,103 @@ import React from 'react';
 import classNames from 'classnames';
 import Card from './carousel-card.jsx';
 import Nav from './carousel-nav.jsx';
+import Icon from '../icon';
 
-const Carousel = ({ className, handleNavigationClick, handleSelect, initial_index, list }) => {
-    // const active_index = list.findIndex(i => i.value === item.value);
+const Carousel = ({
+    className,
+    handleSelect,
+    initial_index,
+    list,
+    bullet_position,
+    nav_position,
+    show_bullet,
+    show_nav,
+}) => {
     const [active_index, setActiveIndex] = React.useState(initial_index);
+
+    const handleNextClick = () => {
+        const next_idx = active_index + 1;
+        const is_reached_end = next_idx === list.length;
+
+        if (!is_reached_end) {
+            setActiveIndex(next_idx);
+        } else {
+            setActiveIndex(0);
+        }
+    };
+
+    const handlePrevClick = () => {
+        const prev_idx = active_index - 1;
+
+        if (prev_idx > -1) {
+            setActiveIndex(prev_idx);
+        } else {
+            setActiveIndex(list.length - 1);
+        }
+    };
 
     return (
         <div className={classNames('carousel', className)}>
-            <div className='carousel__wrapper' style={{ transform: `translate3d(-${350 * active_index}px, 0, 0)` }}>
-                {list.map((type, idx) => (
-                    <Card key={idx} onClick={handleSelect}>
-                        {list[idx]}
-                    </Card>
-                ))}
+            {list.length > 1 && (
+                <Nav
+                    active_index={active_index}
+                    className='carousel__nav--upper'
+                    handleNextClick={handleNextClick}
+                    handlePrevClick={handlePrevClick}
+                    handleNavigationClick={setActiveIndex}
+                    show_bullet={show_bullet && bullet_position === 'top'}
+                    show_nav={show_nav && nav_position === 'top'}
+                    list={list}
+                />
+            )}
+            <div className='carousel__container'>
+                {nav_position === 'middle' && (
+                    <span className='carousel__icon' onClick={handlePrevClick}>
+                        <Icon icon='IcChevronLeft' size='24' />
+                    </span>
+                )}
+
+                <div className='carousel__box'>
+                    <div
+                        className='carousel__wrapper'
+                        style={{ transform: `translate3d(-${350 * active_index}px, 0, 0)` }}
+                    >
+                        {list.map((type, idx) => (
+                            <Card key={idx} onClick={handleSelect}>
+                                {list[idx]}
+                            </Card>
+                        ))}
+                    </div>
+                </div>
+
+                {nav_position === 'middle' && (
+                    <span className='carousel__icon' onClick={handleNextClick}>
+                        <Icon icon='IcChevronRight' size='24' />
+                    </span>
+                )}
             </div>
-            {list.length > 1 && <Nav active_index={active_index} handleNavigationClick={setActiveIndex} list={list} />}
+            {list.length > 1 && (
+                <Nav
+                    active_index={active_index}
+                    className='carousel__nav--lower'
+                    handleNextClick={handleNextClick}
+                    handlePrevClick={handlePrevClick}
+                    handleNavigationClick={setActiveIndex}
+                    show_bullet={show_bullet && bullet_position === 'bottom'}
+                    show_nav={show_nav && nav_position === 'bottom'}
+                    list={list}
+                />
+            )}
         </div>
     );
 };
 
 Carousel.defaultProps = {
     initial_index: 0,
+    nav_position: 'bottom',
+    bullet_position: 'bottom',
+    show_bullet: true,
+    show_nav: true,
 };
 Carousel.propTypes = {
     className: PropTypes.string,
@@ -31,6 +107,10 @@ Carousel.propTypes = {
     handleSelect: PropTypes.func,
     item: PropTypes.object,
     list: PropTypes.array,
+    nav_position: PropTypes.string,
+    show_nav: PropTypes.bool,
+    bullet_position: PropTypes.string,
+    show_bullet: PropTypes.bool,
 };
 
 export default Carousel;

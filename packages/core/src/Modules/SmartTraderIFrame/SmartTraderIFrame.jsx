13770802f0c128d46b72c3066de0614db418ab2d
Aaron Imming <aaim@protonmail.com>
2020-10-09 17:14:14 +0800

    Fix synchronisation between Deriv.app + DSmartTrader

--- packages/core/src/Modules/SmartTraderIFrame/SmartTraderIFrame.jsx
+++ packages/core/src/Modules/SmartTraderIFrame/SmartTraderIFrame.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { getUrlSmartTrader } from '@deriv/shared';
+import { getPlatformFromUrl } from '@deriv/shared';
 
 const SmartTraderIFrame = () => {
-    const url = getUrlSmartTrader();
-    if (!url) {
-        return null;
+    const { is_deriv_crypto, is_staging_deriv_app, is_staging_deriv_crypto } = getPlatformFromUrl();
+
+    let base_link = '';
+
+    if (is_staging_deriv_app) {
+        base_link = 'https://staging-smarttrader.deriv.com';
+    } else if (is_staging_deriv_crypto) {
+        base_link = 'https://staging-smarttrader.derivcrypto.com';
+    } else if (is_deriv_crypto) {
+        base_link = 'https://smarttrader.derivcrypto.com';
+    } else {
+        base_link = 'https://smarttrader.deriv.com';
     }
 
     return (
         <iframe
             id='localstorage-sync'
-            src={`${url}/localstorage-sync.html`}
+            src={`${base_link}/localstorage-sync.html`}
             style={{ display: 'none', visibility: 'hidden' }}
             sandbox='allow-same-origin allow-scripts'
         />

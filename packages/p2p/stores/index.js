7348e090f6861a99f36a347d1fef737d24ad2c4d
Carol Sachdeva <carol@binary.com>
2020-10-11 13:14:27 +0400

    missed my_profile_store in index

--- packages/p2p/stores/index.js
+++ packages/p2p/stores/index.js
@@ -5,8 +5,8 @@ import MyProfileStore from './my-profile-store.js';
 
 class RootStore {
     constructor() {
-        this.general_store = new GeneralStore(this);
         this.advertiser_page_store = new AdvertiserPageStore(this);
+        this.general_store = new GeneralStore(this);
         this.my_profile_store = new MyProfileStore(this);
     }
 }
@@ -17,8 +17,9 @@ export const useStores = () => {
     if (!stores_context) {
         const root_store = new RootStore();
         stores_context = React.createContext({
-            general_store: root_store.general_store,
             advertiser_page_store: root_store.advertiser_page_store,
+            general_store: root_store.general_store,
+            my_profile_store: root_store.my_profile_store,
         });
     }
     return React.useContext(stores_context);

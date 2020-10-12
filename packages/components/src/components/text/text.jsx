ca2f6d9cd11a857c9b128730931e58f0634ede15
sara-fs <sara.khosropour@regentmarkets.com>
2020-10-12 13:54:24 +0330

    Added default value to style

--- packages/components/src/components/text/text.jsx
+++ packages/components/src/components/text/text.jsx
@@ -1,18 +1,18 @@
 import classNames from 'classnames';
 import React from 'react';
 
-const Text = ({ children, size, color, align, weight, lineHeight, as }) => {
+const Text = ({ children, size, color, align, weight, lineHeight, as, className }) => {
     function setStyle() {
         const style = {
-            '--text-size': `var(--text-size-${size})`,
-            '--text-color': `var(--text-${color})`,
-            '--text-lh': `var(--text-lh-${lineHeight})`,
+            '--text-size': size ? `var(--text-size-${size})` : '',
+            '--text-color': color ? `var(--text-${color})` : '',
+            '--text-lh': lineHeight ? `var(--text-lh-${lineHeight})` : '',
         };
         return style;
     }
     function setClassName() {
-        const className = ['dc-text', `dc-text--${weight} dc-text--${align}`];
-        return className;
+        const classStyle = ['dc-text', `dc-text--${weight} dc-text--${align}`, className];
+        return classStyle;
     }
     return (
         ({ as } === 'span' && (

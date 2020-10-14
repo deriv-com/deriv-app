fa53ec7e6cb3c08aabf4c5b8df63cdc6910dbd42
sara-fs <sara.khosropour@regentmarkets.com>
2020-10-14 10:08:16 +0330

    merged

--- packages/components/src/components/text/text.jsx
+++ packages/components/src/components/text/text.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Text = ({ children, size, color, align, weight, lineHeight, as, className }) => {
     function setStyle() {
         const style = {
-            '--text-size': `var(--text-size-${size ? size : 's'})`,
-            '--text-color': `var(--text-${color ? color : 'general'})`,
-            '--text-lh': `var(--text-lh-${lineHeight ? lineHeight : as === 'p' ? 'm' : 'xs'})`,
-            '--text-weight': `var(--text-weight-${weight ? weight : 'n'})`,
+            '--text-size': `var(--text-size-${size || 's'})`,
+            '--text-color': `var(--text-${color || 'general'})`,
+            '--text-lh': `var(--text-lh-${lineHeight || (as === 'p' ? 'm' : 'xs')})`,
+            '--text-weight': `var(--text-weight-${weight || 'n'})`,
         };
         return style;
     }

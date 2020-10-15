2ab852c53f2ef674f9fbdcfd47da291c37e1aa0c
sara-fs <sara.khosropour@regentmarkets.com>
2020-10-15 09:55:11 +0330

    Replaced span with text on empty-notification

--- packages/core/src/App/Components/Elements/Notifications/empty-notification.jsx
+++ packages/core/src/App/Components/Elements/Notifications/empty-notification.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Icon } from '@deriv/components';
 import { localize } from '@deriv/translations';
+import { Text } from '@deriv/components';
 
 const EmptyNotification = () => (
     <div className='notifications-empty__container'>
@@ -8,9 +9,9 @@ const EmptyNotification = () => (
             <Icon icon='IcBox' className='notifications-empty__icon' size={64} color='secondary' />
             <div className='notifications-empty__content'>
                 <h4 className='notifications-empty__header'>{localize('No notifications')}</h4>
-                <span className='notifications-empty__message'>
+                <Text size='xxs' lineHeight='m' color='less-prominent'>
                     {localize('You have yet to receive any notifications')}
-                </span>
+                </Text>
             </div>
         </div>
     </div>

2b0dd6487998801ec91e1e9f58e9518becf81caf
Mohammadreza Ghorbani <reza@firstsource.email>
2020-10-18 21:19:26 +0330

    Fix Account package tests

--- packages/account/jest.config.js
+++ packages/account/jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     moduleNameMapper: {
+        "\\.s(c|a)ss$": "<rootDir>/../../__mocks__/styleMock.js",
+        "^.+\\.svg$": "<rootDir>/../../__mocks__/styleMock.js",
         '^Components\/(.*)$': "<rootDir>/src/Components/$1",
+        '^Constants\/(.*)$': "<rootDir>/src/Constants/$1",
+        '^Duplicated\/(.*)$': "<rootDir>/src/Duplicated/$1",
         '^Services\/(.*)$': "<rootDir>/src/Services/$1",
         '^Stores\/(.*)$': "<rootDir>/src/Stores/$1",
+        '^Sections\/(.*)$': "<rootDir>/src/Sections/$1",
+        '^Sections$': "<rootDir>/src/Sections/index.js",
     },
+    testRegex: "(/__tests__/.*|(\\.)(test|spec))\\.js?$",
+    setupFiles: [
+        "<rootDir>/../../jest.setup.js",
+    ],
 };

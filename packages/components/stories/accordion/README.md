278c8b07bfc1573354d48d45a38e69e54e30ff2b
ali-fs <ali.sabzinezhad@firstsource.tech>
2020-10-21 15:34:57 +0330

    storybook for accordion

---
+++ packages/components/stories/accordion/README.md
@@ -0,0 +1,44 @@
+# Mobile Dialog Component
+
+Renders an accordions with a list of inner objects. The objects must have `header` and `content` property and both of them can be a `simple` string or a `node object`.
+
+#### Supported Gestures:
+
+-   Click
+
+## Usage
+
+```jsx
+import Accordion from 'Components/accordion';
+
+const DummyComponent = props => (
+   <Accordion
+        className='accordion__wrapper'
+        list={data}
+    />
+);
+```
+
+## Props
+
+| Name          | Type            | Default   | Description                         |
+| ------------- | --------------- | --------- | ----------------------------------- |
+| list          | {array<object>} | []        | Array contains a list of objects.   |
+| classname     | {string}        | null      | Classname for accordion wrapper.    |
+
+
+# Full example:
+
+```jsx
+import Accordion from 'Components/accordion';
+
+const DummyComponent = props => (
+    <Accordion
+        className='accordion__wrapper'
+        list={[ { header: 'header 1', content: 'content 1' },
+                { header: 'header 2', content: 'content 2' },
+                { header: 'header 3', content: 'content 3' }
+        ]}
+    />
+);
+```
---
+++ packages/components/stories/accordion/accordion.stories.js
@@ -0,0 +1,41 @@
+import { storiesOf } from '@storybook/react';
+import Accordion from 'Components/accordion';
+import React from 'react';
+import notes from './README.md';
+import './accordion.stories.scss';
+
+storiesOf('Accordion', module).add(
+    'Main function',
+    () => {
+        const data = [
+            {
+                header: 'Apple',
+                content:
+                    'An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
+            },
+            {
+                header: 'Orange',
+                content:
+                    'The orange is the fruit of various citrus species in the family Rutaceae; it primarily refers to Citrus × sinensis, which is also called sweet orange, to distinguish it from the related Citrus × aurantium, referred to as bitter orange.',
+            },
+            {
+                header: 'Banana',
+                content:
+                    'A banana is an elongated, edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called "plantains", distinguishing them from dessert bananas.',
+            },
+            {
+                header: 'Kiwifruit',
+                content: `Kiwifruit or Chinese gooseberry is the edible berry of several species of woody vines in the genus Actinidia. The most common cultivar group of kiwifruit is oval, about the size of a large hen's egg: 5–8 centimetres in length and 4.5–5.5 cm in diameter.`,
+            },
+        ];
+
+        return (
+            <React.Fragment>
+                <Accordion className='accordion__wrapper' list={data} />
+            </React.Fragment>
+        );
+    },
+    {
+        notes,
+    }
+);

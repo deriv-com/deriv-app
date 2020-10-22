e8f608b56429a057f0aa8774e43ad51c4698fabd
Mahdi Aryayi <55829032+mahdiaryayi-fs@users.noreply.github.com>
2020-10-22 05:09:03 +0330

    Fixed 2 popups bug (#2002)
    Co-authored-by: Mahdi Aryayi <mahdi.aryayi@firstsource.io>
    Co-authored-by: negar-binary <negar@binary.com>

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1051,7 +1051,12 @@ export default class ClientStore extends BaseStore {
             await WS.authorized.cache.landingCompany(this.residence).then(this.responseLandingCompany);
             if (!this.is_virtual) await this.getLimits();
 
-            if (!this.switched && !this.has_any_real_account && this.is_mt5_allowed) {
+            if (
+                !this.switched &&
+                !this.has_any_real_account &&
+                this.is_mt5_allowed &&
+                !this.root_store.ui.is_real_acc_signup_on
+            ) {
                 this.root_store.ui.toggleWelcomeModal({ is_visible: true });
             }
         } else {

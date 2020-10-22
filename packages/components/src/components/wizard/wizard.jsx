738a82e8049bd7deaf8cb6a3b76a3f50241318f5
matin-binary <matin@deriv.com>
2020-10-22 17:48:21 +0800

    Matin/Improvement: Wizard component enhancement

--- packages/components/src/components/wizard/wizard.jsx
+++ packages/components/src/components/wizard/wizard.jsx
@@ -1,86 +1,99 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
-import Header from './wizard-header.jsx';
-import Icon from '../icon/icon.jsx';
+import Step from './wizard-step.jsx';
 
-const Wizard = React.forwardRef(({ children, steps, lbl_previous, has_prev, lbl_next, has_next, className }, ref) => {
-    const [step, setStep] = React.useState(0);
-    const [component, setComponent] = React.useState(steps[0].component);
-    const last_step = steps.length - 1;
+const Wizard = ({ children, className, initial_step, onStepChange, nav }) => {
+    const [active_step, setActiveStep] = React.useState(0);
 
-    const nextStep = () => {
-        const next_step = step + 1;
-        const is_last_step = next_step > last_step;
-        if (is_last_step) return;
-        setStep(next_step);
-        setComponent(steps[next_step].component);
+    React.useEffect(() => {
+        const local_initial_step = initial_step - 1;
+        const local_children = getSteps();
+
+        if (local_initial_step && local_children[local_initial_step]) {
+            setActiveStep(initial_step);
+        }
+    }, [initial_step, getSteps]);
+
+    const onChangeStep = stats => {
+        // User callback
+        onStepChange(stats);
     };
 
-    const prevStep = () => {
-        const prev_step = step - 1;
-        const is_first_step = prev_step < 0;
-        if (is_first_step) return;
-        setStep(prev_step);
-        setComponent(steps[prev_step].component);
+    const isInvalidStep = next => next < 0 || next >= totalSteps();
+
+    const onSetActiveStep = next => {
+        if (active_step === next) return;
+        if (isInvalidStep(next)) return;
+
+        setActiveStep(next);
+        onChangeStep({
+            active_step: next + 1,
+        });
     };
 
-    const selectStep = selected_step => {
-        const is_prev_step_not_allowed = selected_step < 0;
-        const is_next_step_not_allowed = selected_step > last_step;
-        if (is_prev_step_not_allowed || is_next_step_not_allowed) return;
-        setStep(selected_step);
-        setComponent(steps[selected_step].component);
-    };
+    const getSteps = React.useCallback(() => React.Children.toArray(children), [children]);
 
-    React.useImperativeHandle(ref, () => ({
-        prevStep,
+    const currentStep = () => active_step + 1;
+
+    const totalSteps = () => getSteps().length;
+
+    const goToStep = step => onSetActiveStep(step - 1);
+
+    const firstStep = () => goToStep(1);
+
+    const lastStep = () => goToStep(totalSteps());
+
+    const nextStep = () => onSetActiveStep(active_step + 1);
+
+    const previousStep = () => onSetActiveStep(active_step - 1);
+
+    // Allows for using HTML elements as a step
+    const isReactComponent = ({ type }) => typeof type === 'function' || typeof type === 'object';
+
+    const properties = {
+        currentStep,
+        totalSteps,
         nextStep,
-        selectStep,
-    }));
+        previousStep,
+        goToStep,
+        firstStep,
+        lastStep,
+    };
 
-    const prevButton = () =>
-        step === 0 ? (
-            <div className='wizard__placeholder' />
-        ) : (
-            <div role='button' onClick={prevStep} className='wizard__btn wizard__btn-left'>
-                <Icon icon='IcArrowLeftBold' className='wizard__btn-icon-left' />
-                {lbl_previous}
-            </div>
-        );
+    const childrenWithProps = React.Children.map(getSteps(), (child, i) => {
+        if (!child) return null;
 
-    const nextButton = () =>
-        step === last_step ? (
-            <div className='wizard__placeholder' />
-        ) : (
-            <div role='button' onClick={nextStep} className='wizard__btn wizard__btn-right'>
-                {lbl_next}
-                <Icon icon='IcArrowRightBold' className='wizard__btn-icon-right' />
-            </div>
-        );
+        if (i === active_step)
+            return <Wizard.Step>{isReactComponent(child) ? React.cloneElement(child, properties) : child}</Wizard.Step>;
+
+        return null;
+    });
 
     return (
         <div className={classNames('wizard', className)}>
-            {children}
-            <div className='wizard__header'>
-                {has_prev && prevButton()}
-                {has_next && nextButton()}
-            </div>
-            {component}
+            {nav && React.cloneElement(nav, properties)}
+            {childrenWithProps}
         </div>
     );
-});
+};
 
 Wizard.displayName = 'Wizard';
-Wizard.Header = Header;
+Wizard.Step = Step;
+
+Wizard.defaultProps = {
+    children: [],
+    initial_step: 1,
+    onStepChange: () => {},
+    nav: null,
+};
 
 Wizard.propTypes = {
+    children: PropTypes.node,
     className: PropTypes.string,
-    lbl_previous: PropTypes.string,
-    has_prev: PropTypes.bool,
-    lbl_next: PropTypes.string,
-    has_next: PropTypes.bool,
-    steps: PropTypes.array,
+    initial_step: PropTypes.number,
+    onStepChange: PropTypes.func,
+    nav: PropTypes.node,
 };
 
 export default Wizard;
--- packages/components/stories/wizard/wizard.stories.js
+++ packages/components/stories/wizard/wizard.stories.js
@@ -11,98 +11,77 @@ const stories = storiesOf('Wizard', module);
 
 stories.addDecorator(withKnobs).addDecorator(withInfo);
 
-stories
-    .add('Basic Usage', () => (
+const Controller = ({ currentStep, firstStep, goToStep, lastStep, nextStep, previousStep, totalSteps, step }) => (
+    <React.Fragment>
+        <ButtonWrapper>
+            {step > 1 && (
+                <Button has_effect secondary small onClick={() => previousStep()}>
+                    Go Back
+                </Button>
+            )}
+            <Button has_effect primary small onClick={() => nextStep()}>
+                {step < totalSteps() ? 'Continue' : 'Finish'}
+            </Button>
+        </ButtonWrapper>
+        <hr />
+        <div>
+            <Text size='1.4rem'>Other Functions</Text>
+            <Text size='1.2rem'>Current Step: {currentStep()}</Text>
+            <Text size='1.2rem'>Total Steps: {totalSteps()}</Text>
+            <div>
+                <Button has_effect secondary small onClick={() => firstStep()}>
+                    First Step
+                </Button>
+                <Button has_effect secondary small onClick={() => lastStep()}>
+                    Last Step
+                </Button>
+                {step !== 2 && (
+                    <Button has_effect secondary small onClick={() => goToStep(2)}>
+                        Go to Step 2
+                    </Button>
+                )}
+            </div>
+        </div>
+    </React.Fragment>
+);
+
+const StepOne = props => {
+    return (
+        <div>
+            <Text size='1.6rem'>Customized Contents {props.currentStep()}</Text>
+            <Controller step={1} {...props} />
+        </div>
+    );
+};
+
+const StepTwo = props => {
+    return (
+        <div>
+            <Text size='1.6rem'>Customized Contents {props.currentStep()}</Text>
+            <Controller step={2} {...props} />
+        </div>
+    );
+};
+
+const StepThree = props => {
+    return (
+        <div>
+            <Text size='1.6rem'>Customized Contents {props.currentStep()}</Text>
+            <Controller step={3} {...props} />
+        </div>
+    );
+};
+
+stories.add('Basic Usage', () => {
+    return (
         <Theme is_dark={boolean('Theme', false)}>
             <FlexWrapper>
-                <Wizard
-                    steps={[
-                        {
-                            component: <Text size='1.2rem'>Customized component 1</Text>,
-                        },
-                        {
-                            component: <Text size='1.2rem'>Customized component 2</Text>,
-                        },
-                    ]}
-                    has_next
-                    has_prev
-                >
-                    <Wizard.Header>
-                        <Text size='1.6rem'>Customizable Header</Text>
-                    </Wizard.Header>
+                <Wizard>
+                    <StepOne />
+                    <StepTwo />
+                    <StepThree />
                 </Wizard>
             </FlexWrapper>
         </Theme>
-    ))
-    .add('Labeled Icons', () => (
-        <Theme is_dark={boolean('Theme', false)}>
-            <FlexWrapper>
-                <Wizard
-                    steps={[
-                        {
-                            component: <Text size='1.2rem'>Customized component 1</Text>,
-                        },
-                        {
-                            component: <Text size='1.2rem'>Customized component 2</Text>,
-                        },
-                    ]}
-                    lbl_previous='Back'
-                    lbl_next='Next'
-                    has_next
-                    has_prev
-                >
-                    <Wizard.Header>
-                        <Text size='1.6rem'>Labeled Icons Header</Text>
-                    </Wizard.Header>
-                </Wizard>
-            </FlexWrapper>
-        </Theme>
-    ))
-    .add('Custom Component', () => {
-        const childRef = React.useRef();
-        return (
-            <Theme is_dark={boolean('Theme', false)}>
-                <FlexWrapper>
-                    <Wizard
-                        ref={childRef}
-                        steps={[
-                            {
-                                component: (
-                                    <GroupWrapper>
-                                        <Text size='1.2rem'>Customized component 1</Text>
-                                        <ButtonWrapper>
-                                            <Button
-                                                onClick={() => childRef.current.nextStep()}
-                                                text='Go to Next Step'
-                                                primary
-                                                medium
-                                            />
-                                        </ButtonWrapper>
-                                    </GroupWrapper>
-                                ),
-                            },
-                            {
-                                component: (
-                                    <GroupWrapper>
-                                        <Text size='1.2rem'>Customized component 2</Text>
-                                        <ButtonWrapper>
-                                            <Button
-                                                onClick={() => childRef.current.prevStep()}
-                                                text='Go to previous Step'
-                                                primary
-                                                medium
-                                            />
-                                        </ButtonWrapper>
-                                    </GroupWrapper>
-                                ),
-                            },
-                        ]}
-                    >
-                        <Wizard.Header>
-                            <Text size='1.6rem'>Custom Component Header</Text>
-                        </Wizard.Header>
-                    </Wizard>
-                </FlexWrapper>
-            </Theme>
-        );
-    });
+    );
+});

3c5ef528547b953d780483845994ff31ba291456
sara-fs <sara.khosropour@regentmarkets.com>
2020-10-27 17:11:18 +0330

    Replaced paragraphs with text on payment agent list

--- packages/core/src/Modules/Cashier/Components/payment-agent-list.jsx
+++ packages/core/src/Modules/Cashier/Components/payment-agent-list.jsx
@@ -1,7 +1,7 @@
 import { PropTypes as MobxPropTypes } from 'mobx-react';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Accordion, Button, DesktopWrapper, Dropdown, MobileWrapper, SelectNative } from '@deriv/components';
+import { Accordion, Button, DesktopWrapper, Dropdown, MobileWrapper, SelectNative, Text } from '@deriv/components';
 import { localize, Localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 import { website_name } from '@deriv/shared';
@@ -34,20 +34,20 @@ class PaymentAgentList extends React.Component {
 
         return (
             <div className='cashier__wrapper--align-left'>
-                <p className='cashier__paragraph'>
+                <Text as='p' size='xs' lineHeight='x' className='cashier__paragraph'>
                     <Localize
                         i18n_default_text='A payment agent is authorised to process deposits and withdrawals for you if your local payment methods or currencies are not supported on {{website_name}}.'
                         values={{ website_name }}
                     />
-                </p>
+                </Text>
                 <div className='payment-agent__instructions'>
                     <div className='payment-agent__instructions-section'>
                         <h2 className='cashier__header payment-agent__header'>
                             <Localize i18n_default_text='Deposit' />
                         </h2>
-                        <p className='cashier__paragraph'>
+                        <Text as='p' size='xs' lineHeight='x' className='cashier__paragraph'>
                             <Localize i18n_default_text='Choose a payment agent and contact them for instructions.' />
-                        </p>
+                        </Text>
                     </div>
                     <div className='payment-agent__instructions-section'>
                         <h2 className='cashier__header payment-agent__header'>

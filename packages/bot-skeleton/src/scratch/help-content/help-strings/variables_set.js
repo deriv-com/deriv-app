64312db66dc826b32a38d51c5a5b0b4126535b24
bahar <bahar@firstsource.tech>
2020-11-04 18:16:58 +0330

    resolve_design_issue

--- packages/bot-skeleton/src/scratch/help-content/help-strings/variables_set.js
+++ packages/bot-skeleton/src/scratch/help-content/help-strings/variables_set.js
@@ -7,7 +7,7 @@ export default {
             'A variable is among the most important and powerful components in creating a bot. It is a way to store information, either as text or numbers. The information stored as a variable can be used and changed according to the given instructions. Variables can be given any name, but usually they are given useful, symbolic names so that it is easier to call them during the execution of instructions.'
         ),
         localize('Creating a variable'),
-        localize('1. From the block library, click Variable, and then click Create a variable.'),
+        localize('1. From the block library, enter a name for the new variable and click Create.'),
         localize('2. The new variable will appear as a block under Set variable.'),
     ],
 };

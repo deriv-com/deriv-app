6974eaf1a2adfce1bd04a7a9754b91bd9a7cabae
nijilnirmal <nijil@deriv.com>
2020-11-06 16:28:01 +0400

    Fix issue with save_workspaces

--- packages/bot-skeleton/src/utils/local-storage.js
+++ packages/bot-skeleton/src/utils/local-storage.js
@@ -49,9 +49,9 @@ export const saveWorkspaceToRecent = (xml, save_type = save_types.UNSAVED) => {
 
 export const getSavedWorkspaces = () => {
     try {
-        return JSON.parse(LZString.decompress(localStorage.getItem('saved_workspaces')));
+        return JSON.parse(LZString.decompress(localStorage.getItem('saved_workspaces'))) || [];
     } catch (e) {
-        return null;
+        return [];
     }
 };
 

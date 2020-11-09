1544e87374c8e52f231e0141b648823986508f97
sara-fs <sara.khosropour@regentmarkets.com>
2020-11-09 09:19:29 +0330

    Changed classname function

--- packages/components/src/components/text/text.jsx
+++ packages/components/src/components/text/text.jsx
@@ -11,17 +11,18 @@ const Text = ({ children, size, color, align, weight, line_height, as, className
         };
         return style;
     };
-    const setClassName = () => {
-        const classStyle = ['dc-text', `${align ? `dc-text--${align}` : ''}`, className];
-        return classStyle;
-    };
+
+    const class_names = classNames('dc-text', {
+        [`dc-text--${align}`]: !!align,
+        className,
+    });
     return (
         (as === 'p' && (
-            <p {...props} className={classNames(setClassName())} style={setStyle()}>
+            <p {...props} className={class_names} style={setStyle()}>
                 {children}
             </p>
         )) || (
-            <span {...props} title={props.title} className={classNames(setClassName())} style={setStyle()}>
+            <span {...props} title={props.title} className={class_names} style={setStyle()}>
                 {children}
             </span>
         )

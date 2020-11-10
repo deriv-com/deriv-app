4a09a2e772efa5f0dd2df53259c75c583e79ae43
sara-fs <sara.khosropour@regentmarkets.com>
2020-11-10 12:27:50 +0330

    Chnaged setStyle function

--- packages/components/src/components/text/text.jsx
+++ packages/components/src/components/text/text.jsx
@@ -3,14 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Text = ({ children, size, color, align, weight, line_height, as, className, ...props }) => {
-    const setStyle = () => {
-        const style = {
-            '--text-size': `var(--text-size-${size || 's'})`,
-            '--text-color': `var(--text-${color || 'general'})`,
-            '--text-lh': `var(--text-lh-${line_height || 'm'})`,
-            '--text-weight': `var(--text-weight-${weight || 'normal'})`,
-        };
-        return style;
+    const style = {
+        '--text-size': `var(--text-size-${size || 's'})`,
+        '--text-color': `var(--text-${color || 'general'})`,
+        '--text-lh': `var(--text-lh-${line_height || 'm'})`,
+        '--text-weight': `var(--text-weight-${weight || 'normal'})`,
     };
 
     const text_align = align || 'left';
@@ -20,11 +17,11 @@ const Text = ({ children, size, color, align, weight, line_height, as, className
     });
     return (
         (as === 'p' && (
-            <p {...props} className={class_names} style={setStyle()}>
+            <p {...props} className={class_names} style={style}>
                 {children}
             </p>
         )) || (
-            <span {...props} className={class_names} style={setStyle()}>
+            <span {...props} className={class_names} style={style}>
                 {children}
             </span>
         )

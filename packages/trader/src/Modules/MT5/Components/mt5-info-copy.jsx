69c998de584c6a1e485dbf9335cb126366764cc4
sara-fs <sara.khosropour@regentmarkets.com>
2020-11-11 20:15:52 +0330

    Replaced spans with text on mt5-info-copy

--- packages/trader/src/Modules/MT5/Components/mt5-info-copy.jsx
+++ packages/trader/src/Modules/MT5/Components/mt5-info-copy.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Clipboard } from '@deriv/components';
+import { Clipboard, Text } from '@deriv/components';
 
 const MT5InfoCopy = ({ display_name, label, success_msg, info_msg, text_copy }) => (
     <div className='mt5-dashboard__info-copy'>
         <div className='mt5-dashboard__info-display'>
-            <span className='mt5-dashboard__info-label'>{label}:</span>
+            <Text className='mt5-dashboard__info-label'>{label}:</Text>
             {display_name}
         </div>
         <Clipboard text_copy={text_copy} info_message={info_msg} success_message={success_msg} />

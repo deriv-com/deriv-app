b28104dffb3307d888b9e67d251cdce0cb1b2b20
Salar Hafezi <salar@firstsource.io>
2020-11-12 09:18:19 +0330

    salar / Invalid token session (#2031)
    * Feat: add should_skip_raise_error to common store
    
    * Feat: socket-general handles the error
    
    * Feat: show prompt if not  should_skip_raise_error
    
    * Update: improve naming
    
    * Fix: show error if revoke fails
    
    * Update: remove extra flag and use routeTo
    
    * Update: remove comment
    
    * Update: redirect to exact same platform
    
    * Fix: typo
    
    * Fix: prevent Bot showing default dialog on leaving
    
    * Fix: check auth if client is logged in
    
    * Update: handle InvalidToken in DBot
    
    * Update: use active_platform to decide where to go
    
    Co-authored-by: Negar Naghshbandi <negar254000@gmail.com>

--- packages/bot-web-ui/src/stores/run-panel-store.js
+++ packages/bot-web-ui/src/stores/run-panel-store.js
@@ -548,6 +548,7 @@ export default class RunPanelStore {
         observer.register('ui.log.error', this.showErrorMessage);
         observer.register('ui.log.notify', journal.onNotify);
         observer.register('ui.log.success', journal.onLogSuccess);
+        observer.register('client.invalid_token', this.handleInvalidToken);
     }
 
     @action.bound
@@ -562,6 +563,7 @@ export default class RunPanelStore {
         observer.unregisterAll('ui.log.error');
         observer.unregisterAll('ui.log.notify');
         observer.unregisterAll('ui.log.success');
+        observer.unregisterAll('client.invalid_token');
     }
 
     disposeListeners() {
@@ -576,5 +578,12 @@ export default class RunPanelStore {
         if (typeof this.disposeSwitchAccountListener === 'function') {
             this.disposeSwitchAccountListener();
         }
+    }
+
+    @action.bound
+    async handleInvalidToken() {
+        const { client } = this.root_store.core;
+        await client.logout();
+        this.setActiveTabIndex(run_panel.SUMMARY);
     }
 }

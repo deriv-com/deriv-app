190b4922e3a649a96ffa3c3e932da5d9f29b472c
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2020-11-13 14:02:49 +0800

    end livechat session on logout

--- packages/core/src/Services/logout.js
+++ packages/core/src/Services/logout.js
@@ -1,14 +1,34 @@
-import { removeCookies } from '@deriv/shared';
+import { init } from '@livechat/customer-sdk';
+import { removeCookies, livechat_client_id, livechat_license_id } from '@deriv/shared';
 import SocketCache from '_common/base/socket_cache';
 import WS from './ws-methods';
 
 export const requestLogout = () => WS.logout().then(doLogout);
+
+function endChat() {
+    const customerSDK = init({
+        licenseId: livechat_license_id,
+        clientId: livechat_client_id,
+    });
+
+    customerSDK.on('connected', () => {
+        if (window.LiveChatWidget.get('chat_data')) {
+            const { chatId, threadId } = window.LiveChatWidget.get('chat_data');
+            if (threadId) {
+                customerSDK.deactivateChat({ chatId });
+            }
+        }
+    });
+
+    window.LiveChatWidget.call('set_customer_email', ' ');
+    window.LiveChatWidget.call('set_customer_name', ' ');
+}
 
 const doLogout = response => {
     if (response.logout !== 1) return undefined;
     removeCookies('affiliate_token', 'affiliate_tracking', 'onfido_token');
     SocketCache.clear();
     sessionStorage.clear();
-
+    endChat();
     return response;
 };

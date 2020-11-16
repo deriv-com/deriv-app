990a684e5c2537c72b937ffb46863a61f6555da5
Akmal Djumakhodjaev <71861519+akmal-binary@users.noreply.github.com>
2020-11-16 12:06:38 +0800

    Changed variable name

--- packages/components/src/components/select-native/select-native.jsx
+++ packages/components/src/components/select-native/select-native.jsx
@@ -80,13 +80,13 @@ class SelectNative extends React.Component {
                                 </React.Fragment>
                             ) : (
                                 Object.keys(list_items).map(key => {
-                                    const list_items_enabled = list_items[key].filter(opt => !opt.disabled);
-                                    if (list_items_enabled.length > 0) {
+                                    const enabled_items = list_items[key].filter(opt => !opt.disabled);
+                                    if (enabled_items.length > 0) {
                                         return (
                                             <optgroup key={key} label={key}>
                                                 {/* Safari on ios allows to select a disabled option.
                                                 So, we should avoid showing it */}
-                                                {list_items_enabled.map((option, idx) => (
+                                                {enabled_items.map((option, idx) => (
                                                     <option key={idx} value={use_text ? option.text : option.value}>
                                                         {option.nativepicker_text || option.text}
                                                     </option>

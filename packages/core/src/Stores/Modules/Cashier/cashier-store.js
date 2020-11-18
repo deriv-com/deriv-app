5c06e0abd2165fa17cab04eb27f0d34c95ee11ab
Salar Hafezi <salar@firstsource.io>
2020-11-18 09:47:01 +0330

    Fix: allow CR to deposit without latest TnC

--- packages/core/src/Stores/Modules/Cashier/cashier-store.js
+++ packages/core/src/Stores/Modules/Cashier/cashier-store.js
@@ -349,10 +349,12 @@ export default class CashierStore extends BaseStore {
             this.config.deposit.error.is_ask_authentication || (is_authentication_needed && is_eu);
         const need_financial_assessment =
             is_financial_account && (is_financial_information_incomplete || is_trading_experience_incomplete);
+        // CR can deposit without accepting latest tnc
+        const need_tnc = is_eu && is_tnc_needed;
 
         return (
             need_authentication ||
-            is_tnc_needed ||
+            need_tnc ||
             need_financial_assessment ||
             this.config.deposit.error.is_ask_financial_risk_approval
         );

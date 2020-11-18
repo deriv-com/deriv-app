e57d5c701fe7a90445293487b03c1b2851b424f7
Salar Hafezi <salar@firstsource.io>
2020-11-18 14:58:18 +0330

    Update: add labuan as an exception

--- packages/core/src/Stores/Modules/Cashier/cashier-store.js
+++ packages/core/src/Stores/Modules/Cashier/cashier-store.js
@@ -342,6 +342,7 @@ export default class CashierStore extends BaseStore {
             is_trading_experience_incomplete,
             account_status,
             is_eu,
+            mt5_login_list,
         } = this.root_store.client;
         if (!account_status.status) return false;
 
@@ -349,8 +350,8 @@ export default class CashierStore extends BaseStore {
             this.config.deposit.error.is_ask_authentication || (is_authentication_needed && is_eu);
         const need_financial_assessment =
             is_financial_account && (is_financial_information_incomplete || is_trading_experience_incomplete);
-        // CR can deposit without accepting latest tnc
-        const need_tnc = is_eu && is_tnc_needed;
+        // CR can deposit without accepting latest tnc except those with Financial STP
+        const need_tnc = (is_eu || mt5_login_list.some(item => item.group.startsWith('real_labuan'))) && is_tnc_needed;
 
         return (
             need_authentication ||

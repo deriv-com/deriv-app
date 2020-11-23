812e085c8cb5467b7429d04db20c09c84190ca24
Mustofa Ghaleb Amami (Front End) <52783019+mustofa-binary@users.noreply.github.com>
2020-11-23 18:06:02 +0800

    amam/add onramp description (#2060)
    * feat: new side note for onramp
    
    * Update packages/core/src/Modules/Cashier/Components/side-note.jsx
    
    Co-authored-by: Aaron Imming <aaim@protonmail.com>
    
    * fix: make title optional as suggested
    
    * fix: setSideNotes to null
    
    * fix: handle reset note in parent
    
    * fix: use previous for selected item change
    
    * fix: thanks to aaron's comment
    
    * omit has_bullets in onramp side note
    
    * fix: apply suggestions
    
    Co-authored-by: cakasuma <amammustofa@gmail.com>
    Co-authored-by: Aaron Imming <aaim@protonmail.com>
    Co-authored-by: Negar Naghshbandi <negar254000@gmail.com>
    Co-authored-by: Zaki <31648832+zaki-binary@users.noreply.github.com>
    Co-authored-by: Mustofa Ghaleb Amami (Front End) <mustofa@binary.com>

--- packages/core/src/Modules/Cashier/Containers/withdrawal.jsx
+++ packages/core/src/Modules/Cashier/Containers/withdrawal.jsx
@@ -32,7 +32,7 @@ const WithdrawalSideNote = () => {
         */
     ];
 
-    return <SideNote notes={notes} />;
+    return <SideNote has_bullets notes={notes} title={<Localize i18n_default_text='Notes' />} />;
 };
 
 const Withdrawal = ({

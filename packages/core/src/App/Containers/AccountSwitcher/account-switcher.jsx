6aa6f0fcbdcf6ed331dc5d5fefab598c51768753
sara-fs <55442058+sara-fs@users.noreply.github.com>
2020-11-24 07:32:53 +0330

    Sara / Changed line height label (#2123)
    * Changed line height label
    
    * Exchanged s and xs value
    
    * Fixed issue with line height in total asset
    
    * Merged branch
    
    * Resolved issue with currency style
    
    * Fixed issue with dropdown margin
    
    * Put line height values in order
    
    * Fixed issue with input margin in ios
    
    Co-authored-by: sara-fs <sara.khosropour@regentmarkets.com>

--- packages/core/src/App/Containers/AccountSwitcher/account-switcher.jsx
+++ packages/core/src/App/Containers/AccountSwitcher/account-switcher.jsx
@@ -566,7 +566,7 @@ class AccountSwitcher extends React.Component {
                     })}
                 />
                 <div className='acc-switcher__total'>
-                    <Text size='xs' weight='bold' color='prominent'>
+                    <Text line_height='s' size='xs' weight='bold' color='prominent'>
                         <Localize i18n_default_text='Total assets' />
                     </Text>
                     <Text size='xs' color='prominent' className='acc-switcher__balance'>
@@ -582,7 +582,7 @@ class AccountSwitcher extends React.Component {
                         />
                     </Text>
                 </div>
-                <Text color='less-prominent' line_height='x' size='xxxs' className='acc-switcher__total-subtitle'>
+                <Text color='less-prominent' line_height='s' size='xxxs' className='acc-switcher__total-subtitle'>
                     {total_assets_message}
                 </Text>
                 <div className='acc-switcher__separator' />

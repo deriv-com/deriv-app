d638829560f8ca4ca207a970e6be0bb3e4a1eca4
matin-binary <matin@deriv.com>
2020-11-26 10:04:09 +0800

    Matin/Fix: Console error while logged out * Notification message font width has been removed

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1271,6 +1271,7 @@ export default class ClientStore extends BaseStore {
 
     @action.bound
     resetVirtualBalanceNotification(loginid) {
+        if (!this.is_logged_in) return;
         if (!this.accounts[loginid].is_virtual) return;
         const min_reset_limit = 1000;
         const max_reset_limit = 999000;

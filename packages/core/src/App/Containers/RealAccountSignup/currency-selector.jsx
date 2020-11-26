bbcf0b0a8cc1b7744b2cb4e366287d63d5d97bda
Akmal Djumakhodjaev <akmal@binary.com>
2020-11-26 17:55:33 +0800

    Updated UI for set currency page

--- packages/core/src/App/Containers/RealAccountSignup/currency-selector.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/currency-selector.jsx
@@ -171,7 +171,7 @@ const CurrencySelector = ({
                                         <React.Fragment>
                                             <RadioButtonGroup
                                                 id='currency'
-                                                className='currency-selector__radio-group'
+                                                className='with-margin currency-selector__radio-group'
                                                 label={localize('Fiat currencies')}
                                                 is_fiat
                                                 value={values.currency}
@@ -196,7 +196,7 @@ const CurrencySelector = ({
                                         <React.Fragment>
                                             <RadioButtonGroup
                                                 id='currency'
-                                                className='currency-selector__radio-group'
+                                                className='with-margin currency-selector__radio-group'
                                                 label={is_deriv_crypto ? '' : localize('Cryptocurrencies')}
                                                 value={values.currency}
                                                 error={errors.currency}

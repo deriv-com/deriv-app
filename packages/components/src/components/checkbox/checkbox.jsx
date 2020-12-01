279df2ccf83cb3782f8351cbbbcce6ce93f2b46a
ali fs <ali.sabzinezhad@firstsource.tech>
2020-12-01 11:36:06 +0330

    refactoring checkbox to functional component

--- packages/components/src/components/checkbox/checkbox.jsx
+++ packages/components/src/components/checkbox/checkbox.jsx
@@ -3,98 +3,78 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Icon from '../icon';
 
-class Checkbox extends React.Component {
-    constructor(props) {
-        super(props);
+const Checkbox = ({
+    className,
+    classNameLabel,
+    disabled,
+    id,
+    label,
+    defaultChecked,
+    onChange, // This needs to be here so it's not included in `otherProps`
+    value,
+    withTabIndex,
+    ...otherProps
+}) => {
+    const [checked, setChecked] = React.useState(defaultChecked || value);
 
-        this.state = {
-            checked: props.defaultChecked || props.value,
-        };
-    }
+    React.useEffect(() => {
+        setChecked(value);
+    }, [value]);
 
-    static getDerivedStateFromProps(nextProps, state) {
-        if (state.checked !== nextProps.defaultChecked || state.checked !== nextProps.value) {
-            return { checked: nextProps.defaultChecked || nextProps.value };
-        }
-        return null;
-    }
-
-    onChange = e => {
+    const onInputChange = e => {
         e.persist();
-        this.setState(
-            state => ({ checked: !state.checked }),
-            () => {
-                this.props.onChange(e);
-            }
-        );
+        setChecked(!checked);
+        onChange(e);
     };
 
-    setChecked = checked => {
-        this.setState({ checked });
-    };
-
-    handleKeyDown = e => {
+    const handleKeyDown = e => {
         // Enter or space
         if (e.key === 'Enter' || e.keyCode === 32) {
-            this.setState(
-                state => ({ checked: !state.checked }),
-                () => {
-                    this.props.onChange();
-                }
-            );
+            setChecked(!checked);
+            onChange();
         }
     };
 
-    render() {
-        const {
-            className,
-            classNameLabel,
-            id,
-            label,
-            defaultChecked,
-            onChange, // This needs to be here so it's not included in `otherProps`
-            withTabIndex,
-            ...otherProps
-        } = this.props;
-
-        return (
-            <label
-                htmlFor={id}
-                onClick={e => e.stopPropagation()}
-                className={classNames('dc-checkbox', className, {
-                    'dc-checkbox--disabled': this.props.disabled,
+    return (
+        <label
+            htmlFor={id}
+            onClick={e => e.stopPropagation()}
+            className={classNames('dc-checkbox', className, {
+                'dc-checkbox--disabled': disabled,
+            })}
+        >
+            <input
+                className='dc-checkbox__input'
+                type='checkbox'
+                id={id}
+                onChange={onInputChange}
+                defaultChecked={checked}
+                {...otherProps}
+            />
+            <span
+                className={classNames('dc-checkbox__box', {
+                    'dc-checkbox__box--active': checked,
+                    'dc-checkbox__box--disabled': disabled,
                 })}
+                {...(withTabIndex?.length > 0 ? { tabIndex: withTabIndex } : {})}
+                onKeyDown={handleKeyDown}
             >
-                <input
-                    className='dc-checkbox__input'
-                    type='checkbox'
-                    id={id}
-                    onChange={this.onChange}
-                    defaultChecked={this.state.checked}
-                    {...otherProps}
-                />
-                <span
-                    className={classNames('dc-checkbox__box', {
-                        'dc-checkbox__box--active': this.state.checked,
-                        'dc-checkbox__box--disabled': this.props.disabled,
-                    })}
-                    {...(withTabIndex?.length > 0 ? { tabIndex: withTabIndex } : {})}
-                    onKeyDown={this.handleKeyDown}
-                >
-                    {!!this.state.checked && <Icon icon='IcCheckmark' color='active' />}
-                </span>
-                <span className={classNames('dc-checkbox__label', classNameLabel)}>{label}</span>
-            </label>
-        );
-    }
-}
+                {!!checked && <Icon icon='IcCheckmark' color='active' />}
+            </span>
+            <span className={classNames('dc-checkbox__label', classNameLabel)}>{label}</span>
+        </label>
+    );
+};
 
 Checkbox.propTypes = {
     className: PropTypes.string,
     classNameLabel: PropTypes.string,
+    defaultChecked: PropTypes.bool,
     disabled: PropTypes.bool,
     id: PropTypes.string,
     label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    onChange: PropTypes.func,
+    value: PropTypes.bool,
     withTabIndex: PropTypes.string,
 };
 

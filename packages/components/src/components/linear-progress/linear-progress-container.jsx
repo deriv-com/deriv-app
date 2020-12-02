4e1a8989fddd9033bf5b8259e82883fc775c961c
Akmal Djumakhodjaev <akmal@binary.com>
2020-12-02 15:55:45 +0800

    Applied code from suggestions

--- packages/components/src/components/linear-progress/linear-progress-container.jsx
+++ packages/components/src/components/linear-progress/linear-progress-container.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { LinearProgress } from './linear-progress.jsx';
 
-const LinearProgressContainer = ({ timeout, action, render, ...other_props }) => {
+const LinearProgressContainer = ({ timeout, action, render, className }) => {
     const [timeout_state, setTimeout] = React.useState(timeout);
     const [current_tick, setCurrentTick] = React.useState(Math.round(timeout / 1000));
     const total_ticks = React.useState(Math.round(timeout / 1000));
 
-    const progress = () => {
-        return 100 - Math.round((current_tick / total_ticks) * 100);
-    };
+    const getProgress = () => 100 - Math.round((current_tick / total_ticks) * 100);
 
-    const remaining = () => {
-        return timeout >= 0 ? timeout : 0;
-    };
+    const getRemaining = () => (timeout >= 0 ? timeout : 0);
 
     const makeProgress = () => {
         setCurrentTick(current_tick - 1);
@@ -32,18 +28,17 @@ const LinearProgressContainer = ({ timeout, action, render, ...other_props }) =>
     }, []);
 
     React.useEffect(() => {
-        if (progress > 100) {
+        if (getProgress() > 100) {
             run();
         }
     });
 
-    const { className } = other_props;
     if (!timeout) return null;
 
     return (
         <div className='dc-linear-progress-container'>
-            <div className='dc-linear-progress__countdown'>{render(remaining())}</div>
-            <LinearProgress className={className} progress={progress()} height={4} />
+            <div className='dc-linear-progress__countdown'>{render(getRemaining())}</div>
+            <LinearProgress className={className} progress={getProgress()} height={4} />
         </div>
     );
 };

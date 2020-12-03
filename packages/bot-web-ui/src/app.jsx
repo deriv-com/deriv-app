56a87d43a10e81ffbb29da2c63a3a3633a6678e9
bahar <bahar@firstsource.tech>
2020-12-03 10:16:09 +0330

    refactoring_dbot_app_jsx

--- packages/bot-web-ui/src/app.jsx
+++ packages/bot-web-ui/src/app.jsx
@@ -1,6 +1,7 @@
 import './public-path'; // Leave this here (at the top)! OK boss!
 import React from 'react'; // eslint-disable-line import/first
-import { DBot, ServerTime } from '@deriv/bot-skeleton'; // eslint-disable-line import/first
+import { Loading } from '@deriv/components';
+import { DBot, ServerTime, ApiHelpers } from '@deriv/bot-skeleton'; // eslint-disable-line import/first
 import Audio from './components/audio.jsx';
 import BotFooterExtensions from './components/bot-footer-extensions.jsx';
 import MainContent from './components/main-content.jsx';
@@ -15,43 +16,54 @@ import RootStore from './stores';
 import GTM from './utils/gtm';
 import './assets/sass/app.scss';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+const App = ({ passthrough }) => {
+    const { root_store, WS } = passthrough;
+    const [is_loading, setIsLoading] = React.useState(true);
+    const root_store_instance = React.useRef(new RootStore(root_store, WS, DBot));
 
-        const { root_store, WS } = props.passthrough;
-        this.root_store = new RootStore(root_store, WS, DBot);
+    const { app, common } = root_store_instance.current;
 
-        GTM.init(this.root_store);
-        ServerTime.init(this.root_store.common);
-        this.root_store.app.setDBotEngineStores(this.root_store);
+    React.useEffect(() => {
+        GTM.init(root_store_instance.current);
+        ServerTime.init(common);
+    }, [root_store_instance.current]);
+
+    app.setDBotEngineStores(root_store_instance.current);
+
+    const { onMount, onUnmount } = app;
+    React.useEffect(() => {
+        ApiHelpers.setInstance(app.api_helpers_store);
+        const { active_symbols } = ApiHelpers.instance;
+
+        setIsLoading(true);
+        active_symbols.retrieveActiveSymbols(true).then(() => {
+            setIsLoading(false);
+            onMount();
+        });
+
+        return () => {
+            onUnmount();
+        };
+    }, [onMount, onUnmount]);
+
+    if (is_loading) {
+        return <Loading />;
     }
-
-    componentDidMount() {
-        this.root_store.app.onMount();
-    }
-
-    componentWillUnmount() {
-        this.root_store.app.onUnmount();
-    }
-
-    render() {
-        return (
-            <MobxContentProvider store={this.root_store}>
-                <div className='bot'>
-                    <BotNotificationMessages />
-                    <Toolbar />
-                    <MainContent />
-                    <RunPanel />
-                    <QuickStrategy />
-                    <BotFooterExtensions />
-                    <Audio />
-                    <RoutePromptDialog />
-                    <BlocklyLoading />
-                </div>
-            </MobxContentProvider>
-        );
-    }
-}
+    return (
+        <MobxContentProvider store={root_store_instance.current}>
+            <div className='bot'>
+                <BotNotificationMessages />
+                <Toolbar />
+                <MainContent />
+                <RunPanel />
+                <QuickStrategy />
+                <BotFooterExtensions />
+                <Audio />
+                <RoutePromptDialog />
+                <BlocklyLoading />
+            </div>
+        </MobxContentProvider>
+    );
+};
 
 export default App;

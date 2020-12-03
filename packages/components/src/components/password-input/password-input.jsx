c49335b3943f15d5d02f650d5c01a52db422accd
bahar <bahar@firstsource.tech>
2020-12-03 14:42:02 +0330

    refactor_password_input_component

--- packages/components/src/components/password-input/password-input.jsx
+++ packages/components/src/components/password-input/password-input.jsx
@@ -4,47 +4,38 @@ import PropTypes from 'prop-types';
 import Icon from '../icon';
 import Input from '../input';
 
-class PasswordInput extends React.Component {
-    state = {
-        show_password: false,
+const PasswordInput = ({
+    className, // Must not be passed to Input as the only trailing icon should be the visibility icon
+    ...otherProps
+}) => {
+    const [show_password, setShowPassword] = React.useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!show_password);
     };
 
-    togglePasswordVisibility = () => {
-        this.setState(prev_state => ({ show_password: !prev_state.show_password }));
-    };
-
-    render() {
-        const {
-            className,
-            trailing_icon, // Must not be passed to Input as the only trailing icon should be the visibility icon
-            ...otherProps
-        } = this.props;
-
-        return (
-            <div className='dc-password-input'>
-                <Input
-                    {...otherProps}
-                    type={this.state.show_password ? 'text' : 'password'}
-                    className={classNames('dc-password-input__field', className)}
-                    trailing_icon={
-                        <Icon
-                            icon={this.state.show_password ? 'IcPasswordEyeHide' : 'IcPasswordEyeVisible'}
-                            className='dc-password-input__visibility-icon'
-                            onClick={this.togglePasswordVisibility}
-                            color='secondary'
-                            width={18}
-                        />
-                    }
-                />
-            </div>
-        );
-    }
-}
+    return (
+        <div className='dc-password-input'>
+            <Input
+                {...otherProps}
+                type={show_password ? 'text' : 'password'}
+                className={classNames('dc-password-input__field', className)}
+                trailing_icon={
+                    <Icon
+                        icon={show_password ? 'IcPasswordEyeHide' : 'IcPasswordEyeVisible'}
+                        className='dc-password-input__visibility-icon'
+                        onClick={togglePasswordVisibility}
+                        color='secondary'
+                        width={18}
+                    />
+                }
+            />
+        </div>
+    );
+};
 
 PasswordInput.propTypes = {
-    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-    has_error: PropTypes.bool,
-    input: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
 };
 
 export default PasswordInput;

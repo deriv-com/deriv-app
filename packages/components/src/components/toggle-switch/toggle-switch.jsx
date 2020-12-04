5e62a452f155e9424ff05d82c1e5724a559b0b4b
sean-binary <sean@deriv.com>
2020-12-04 16:30:44 +0800

    refactor toggle switch

--- packages/components/src/components/toggle-switch/toggle-switch.jsx
+++ packages/components/src/components/toggle-switch/toggle-switch.jsx
@@ -2,27 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-class ToggleSwitch extends React.Component {
-    render() {
-        return (
-            <>
-                <input
-                    className={classNames('dc-toggle-switch', {}, this.props.className)}
-                    id={this.props.id}
-                    type='checkbox'
-                    checked={this.props.is_enabled}
-                    onChange={this.props.handleToggle}
-                />
-                <label
-                    className={classNames('dc-toggle-switch__label', {}, this.props.classNameLabel)}
-                    htmlFor={this.props.id}
-                >
-                    <span className={classNames('dc-toggle-switch__button', {}, this.props.classNameButton)} />
-                </label>
-            </>
-        );
-    }
-}
+const ToggleSwitch = ({ className, id, is_enabled, handleToggle, classNameLabel, classNameButton }) => (
+    <>
+        <input
+            className={classNames('dc-toggle-switch', {}, className)}
+            id={id}
+            type='checkbox'
+            checked={is_enabled}
+            onChange={handleToggle}
+        />
+        <label className={classNames('dc-toggle-switch__label', {}, classNameLabel)} htmlFor={id}>
+            <span className={classNames('dc-toggle-switch__button', {}, classNameButton)} />
+        </label>
+    </>
+);
 
 ToggleSwitch.propTypes = {
     className: PropTypes.string,

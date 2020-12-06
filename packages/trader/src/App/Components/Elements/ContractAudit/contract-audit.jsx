739bff811b7852e7dad438c3d5aa439189292d0c
bahar <bahar@firstsource.tech>
2020-12-06 15:37:21 +0330

    refactor_contract_audit_component

--- packages/trader/src/App/Components/Elements/ContractAudit/contract-audit.jsx
+++ packages/trader/src/App/Components/Elements/ContractAudit/contract-audit.jsx
@@ -6,70 +6,50 @@ import { WS } from 'Services/ws-methods';
 import ContractDetails from './contract-details.jsx';
 import ContractHistory from './contract-history.jsx';
 
-class ContractAudit extends React.Component {
-    state = {
-        contract_update_history: [],
-    };
+const ContractAudit = ({ has_result, toggleHistoryTab, is_multiplier, contract_update_history, ...props }) => {
+    const { contract_id, currency } = props.contract_info;
+    const [update_history, setUpdateHistory] = React.useState([]);
 
-    static getDerivedStateFromProps({ is_multiplier, contract_update_history }, state) {
-        if (!is_multiplier) return state;
-        if (!!contract_update_history.length && contract_update_history.length > state.contract_update_history.length) {
-            return {
-                contract_update_history,
-            };
-        }
-        return state;
-    }
+    React.useEffect(() => {
+        if (!!contract_update_history.length && contract_update_history.length > update_history.length)
+            setUpdateHistory(contract_update_history);
+    }, [contract_update_history]);
 
-    onTabItemClick = tab_index => {
-        this.props.toggleHistoryTab(tab_index);
+    const onTabItemClick = tab_index => {
+        toggleHistoryTab(tab_index);
         if (tab_index) {
-            WS.contractUpdateHistory(this.props.contract_info.contract_id).then(response => {
-                this.setState({
-                    contract_update_history: response.contract_update_history,
-                });
+            WS.contractUpdateHistory(contract_id).then(response => {
+                setUpdateHistory(response.contract_update_history);
             });
         }
     };
 
-    render() {
-        const { has_result, is_multiplier, contract_info } = this.props;
+    if (!has_result) return null;
 
-        if (!has_result) return null;
-
-        if (!is_multiplier) {
-            return (
-                <div className='contract-audit__wrapper'>
-                    <ContractDetails {...this.props} />
-                </div>
-            );
-        }
-
+    if (!is_multiplier) {
         return (
             <div className='contract-audit__wrapper'>
-                <Tabs top className='contract-audit__tabs' onTabItemClick={this.onTabItemClick}>
-                    <div label={localize('Details')}>
-                        <ContractDetails {...this.props} />
-                    </div>
-                    <div label={localize('History')}>
-                        <ContractHistory
-                            currency={contract_info.currency}
-                            history={this.state.contract_update_history}
-                        />
-                    </div>
-                </Tabs>
+                <ContractDetails {...props} />
             </div>
         );
     }
-}
+    return (
+        <div className='contract-audit__wrapper'>
+            <Tabs top className='contract-audit__tabs' onTabItemClick={onTabItemClick}>
+                <div label={localize('Details')}>
+                    <ContractDetails {...props} />
+                </div>
+                <div label={localize('History')}>
+                    <ContractHistory currency={currency} history={update_history} />
+                </div>
+            </Tabs>
+        </div>
+    );
+};
 
 ContractAudit.propTypes = {
-    contract_end_time: PropTypes.PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     contract_info: PropTypes.object,
     contract_update_history: PropTypes.array,
-    duration: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    duration_unit: PropTypes.string,
-    exit_spot: PropTypes.string,
     has_result: PropTypes.bool,
     is_multiplier: PropTypes.bool,
     toggleHistoryTab: PropTypes.func,

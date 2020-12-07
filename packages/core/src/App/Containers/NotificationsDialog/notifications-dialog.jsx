6f49567b584187bb381611d9836e731416a5c1b0
mehrzad-fs <mehrzad@firstsource.tech>
2020-12-07 14:16:29 +0330

    refactor notifications dialog

--- packages/core/src/App/Containers/NotificationsDialog/notifications-dialog.jsx
+++ packages/core/src/App/Containers/NotificationsDialog/notifications-dialog.jsx
@@ -2,152 +2,154 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
-import { Button, DesktopWrapper, Icon, MobileDialog, MobileWrapper, ThemedScrollbars, Text } from '@deriv/components';
+import {
+    Button,
+    DesktopWrapper,
+    Icon,
+    MobileDialog,
+    MobileWrapper,
+    ThemedScrollbars,
+    Text,
+    useOnClickOutside,
+} from '@deriv/components';
 import { BinaryLink } from 'App/Components/Routes';
 import { connect } from 'Stores/connect';
-import { localize } from '@deriv/translations';
+import { localize, Localize } from '@deriv/translations';
 import { toTitleCase, isEmptyObject, isMobile } from '@deriv/shared';
 
 import { EmptyNotification } from 'App/Components/Elements/Notifications/empty-notification.jsx';
 
-class NotificationsDialog extends React.Component {
-    setWrapperRef = node => {
-        this.wrapper_ref = node;
-    };
+const NotificationsList = ({ notifications, toggleDialog }) => (
+    <React.Fragment>
+        {notifications.map(item => (
+            <div className='notifications-item' key={item.key}>
+                <h2 className='notifications-item__title'>
+                    {item.type && (
+                        <Icon
+                            icon={
+                                ['contract_sold', 'info'].includes(item.type)
+                                    ? 'IcAlertInfo'
+                                    : `IcAlert${toTitleCase(item.type)}`
+                            }
+                            className={classNames('notifications-item__title-icon', {
+                                [`notifications-item__title-icon--${item.type}`]: item.type,
+                            })}
+                        />
+                    )}
+                    {item.header}
+                </h2>
+                <div className='notifications-item__message'>{item.message}</div>
+                <div className='notifications-item__action'>
+                    {!isEmptyObject(item.action) && (
+                        <React.Fragment>
+                            {item.action.route ? (
+                                <BinaryLink
+                                    onClick={toggleDialog}
+                                    active_class='notifications-item'
+                                    className={classNames(
+                                        'dc-btn',
+                                        'dc-btn--secondary',
+                                        'notifications-item__cta-button'
+                                    )}
+                                    to={item.action.route}
+                                >
+                                    <Text weight='bold' size='xxs'>
+                                        {item.action.text}
+                                    </Text>
+                                </BinaryLink>
+                            ) : (
+                                <Button
+                                    className={classNames('dc-btn--secondary', 'notifications-item__cta-button')}
+                                    onClick={item.action.onClick}
+                                >
+                                    <Text size='xxs'>{item.action.text}</Text>
+                                </Button>
+                            )}
+                        </React.Fragment>
+                    )}
+                </div>
+            </div>
+        ))}
+    </React.Fragment>
+);
 
-    handleClickOutside = event => {
+const NotificationListWrapper = React.forwardRef(({ notifications, toggleDialog }, ref) => {
+    const is_empty = !notifications.length;
+    return (
+        <div className='notifications-dialog' ref={ref}>
+            <div className='notifications-dialog__header'>
+                <h2 className='notifications-dialog__header-text'>
+                    <Localize i18n_default_text='Notifications' />
+                </h2>
+            </div>
+            <div
+                className={classNames('notifications-dialog__content', {
+                    'notifications-dialog__content--empty': is_empty,
+                })}
+            >
+                <ThemedScrollbars is_bypassed={isMobile() || is_empty}>
+                    {is_empty ? (
+                        <EmptyNotification />
+                    ) : (
+                        <NotificationsList notifications={notifications} toggleDialog={toggleDialog} />
+                    )}
+                </ThemedScrollbars>
+            </div>
+        </div>
+    );
+});
+NotificationListWrapper.displayName = 'NotificationListWrapper';
+
+const NotificationsDialog = ({ is_visible, notifications, toggleDialog }) => {
+    const wrapper_ref = React.useRef();
+
+    const handleClickOutside = event => {
         const notifications_toggle_btn = !event.target.classList.contains('notifications-toggle__icon-wrapper');
-        if (
-            this.wrapper_ref &&
-            !this.wrapper_ref.contains(event.target) &&
-            this.props.is_visible &&
-            notifications_toggle_btn
-        ) {
-            this.props.toggleDialog();
+        if (!wrapper_ref.current?.contains(event.target) && is_visible && notifications_toggle_btn) {
+            toggleDialog();
         }
     };
 
-    componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside, {
-            passive: true,
-        });
-    }
+    useOnClickOutside(wrapper_ref, handleClickOutside);
 
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside);
-    }
-
-    render() {
-        const notifications_list_el = (
-            <React.Fragment>
-                {this.props.notifications && this.props.notifications.length ? (
-                    this.props.notifications.map((item, idx) => (
-                        <div className='notifications-item' key={idx}>
-                            <h2 className='notifications-item__title'>
-                                {item.type && (
-                                    <Icon
-                                        icon={
-                                            item.type === 'info' || item.type === 'contract_sold'
-                                                ? 'IcAlertInfo'
-                                                : `IcAlert${toTitleCase(item.type)}`
-                                        }
-                                        className={classNames('notifications-item__title-icon', {
-                                            [`notifications-item__title-icon--${item.type}`]: item.type,
-                                        })}
-                                    />
-                                )}
-                                {item.header}
-                            </h2>
-                            <div className='notifications-item__message'>{item.message}</div>
-                            <div className='notifications-item__action'>
-                                {!isEmptyObject(item.action) && (
-                                    <>
-                                        {item.action.route ? (
-                                            <BinaryLink
-                                                onClick={this.props.toggleDialog}
-                                                active_class='notifications-item'
-                                                className={classNames(
-                                                    'dc-btn',
-                                                    'dc-btn--secondary',
-                                                    'notifications-item__cta-button'
-                                                )}
-                                                to={item.action.route}
-                                            >
-                                                <Text weight='bold' size='xxs'>
-                                                    {item.action.text}
-                                                </Text>
-                                            </BinaryLink>
-                                        ) : (
-                                            <Button
-                                                className={classNames(
-                                                    'dc-btn--secondary',
-                                                    'notifications-item__cta-button'
-                                                )}
-                                                onClick={item.action.onClick}
-                                            >
-                                                <Text weight='bold' size='xxs'>
-                                                    {item.action.text}
-                                                </Text>
-                                            </Button>
-                                        )}
-                                    </>
-                                )}
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <EmptyNotification />
-                )}
-            </React.Fragment>
-        );
-
-        const is_empty = !(this.props.notifications && this.props.notifications.length);
-        const notifications_dialog_el = (
-            <div className='notifications-dialog' ref={this.setWrapperRef}>
-                <div className='notifications-dialog__header'>
-                    <h2 className='notifications-dialog__header-text'>{localize('Notifications')}</h2>
-                </div>
-                <div
-                    className={classNames('notifications-dialog__content', {
-                        'notifications-dialog__content--empty': is_empty,
-                    })}
+    return (
+        <React.Fragment>
+            <MobileWrapper>
+                <MobileDialog
+                    portal_element_id='modal_root'
+                    title={localize('Notifications')}
+                    wrapper_classname='notifications-mobile-dialog'
+                    visible={is_visible}
+                    onClose={toggleDialog}
                 >
-                    <ThemedScrollbars is_bypassed={isMobile() || is_empty}>{notifications_list_el}</ThemedScrollbars>
-                </div>
-            </div>
-        );
-
-        return (
-            <React.Fragment>
-                <MobileWrapper>
-                    <MobileDialog
-                        portal_element_id='modal_root'
-                        title={localize('Notifications')}
-                        wrapper_classname='notifications-mobile-dialog'
-                        visible={this.props.is_visible}
-                        onClose={this.props.toggleDialog}
-                    >
-                        {notifications_dialog_el}
-                    </MobileDialog>
-                </MobileWrapper>
-                <DesktopWrapper>
-                    <CSSTransition
-                        in={this.props.is_visible}
-                        classNames={{
-                            enter: 'notifications-dialog--enter',
-                            enterDone: 'notifications-dialog--enter-done',
-                            exit: 'notifications-dialog--exit',
-                        }}
-                        timeout={150}
-                        unmountOnExit
-                    >
-                        {notifications_dialog_el}
-                    </CSSTransition>
-                </DesktopWrapper>
-            </React.Fragment>
-        );
-    }
-}
+                    <NotificationListWrapper
+                        notifications={notifications}
+                        ref={wrapper_ref}
+                        toggleDialog={toggleDialog}
+                    />
+                </MobileDialog>
+            </MobileWrapper>
+            <DesktopWrapper>
+                <CSSTransition
+                    in={is_visible}
+                    classNames={{
+                        enter: 'notifications-dialog--enter',
+                        enterDone: 'notifications-dialog--enter-done',
+                        exit: 'notifications-dialog--exit',
+                    }}
+                    timeout={150}
+                    unmountOnExit
+                >
+                    <NotificationListWrapper
+                        notifications={notifications}
+                        ref={wrapper_ref}
+                        toggleDialog={toggleDialog}
+                    />
+                </CSSTransition>
+            </DesktopWrapper>
+        </React.Fragment>
+    );
+};
 
 NotificationsDialog.propTypes = {
     is_visible: PropTypes.bool,

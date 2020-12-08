a4f204b73df49c04d75ba20be6f3cfb693af9ae2
sara-fs <sara.khosropour@regentmarkets.com>
2020-12-08 13:26:40 +0330

    Replaced span with text on progress-ticks and progress-ticks-mobile

--- packages/components/src/components/progress-slider/progress-ticks.jsx
+++ packages/components/src/components/progress-slider/progress-ticks.jsx
@@ -1,14 +1,15 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Text from '../text';
 
 const ProgressTicks = ({ current_tick, getCardLabels, ticks_count }) => {
     const arr_ticks = [...Array(ticks_count).keys()];
     return (
         <div className='dc-progress-slider__ticks'>
-            <span className='dc-progress-slider__ticks-caption'>
+            <Text styles={{ lineHeight: '18px' }} size='xxs' className='dc-progress-slider__ticks-caption'>
                 {getCardLabels().TICK} {current_tick}
-            </span>
+            </Text>
             <div className='dc-progress-slider__ticks-wrapper'>
                 {arr_ticks.map(idx => (
                     <div

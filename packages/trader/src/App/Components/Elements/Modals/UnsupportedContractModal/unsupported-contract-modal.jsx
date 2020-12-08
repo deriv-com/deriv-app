a29288bce203956525aa8915a6246da6cb73aff0
sean-binary <sean@deriv.com>
2020-12-08 12:06:22 +0800

    refactor unsupported contract modal to functional component

--- packages/trader/src/App/Components/Elements/Modals/UnsupportedContractModal/unsupported-contract-modal.jsx
+++ packages/trader/src/App/Components/Elements/Modals/UnsupportedContractModal/unsupported-contract-modal.jsx
@@ -5,27 +5,24 @@ import { localize, Localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 import { website_name } from '@deriv/shared';
 
-class UnsupportedContractModal extends React.Component {
-    render() {
-        const { disableApp, enableApp, is_loading, is_visible, onConfirm, onClose } = this.props;
-        return (
-            <Dialog
-                title={localize('We’re working on it')}
-                confirm_button_text={localize('Stay on {{website_domain}}', { website_domain: website_name })}
-                cancel_button_text={localize('Go to Binary.com')}
-                onConfirm={onConfirm}
-                onCancel={onClose}
-                disableApp={disableApp}
-                enableApp={enableApp}
-                is_loading={is_loading}
-                is_closed_on_cancel
-                is_visible={is_visible}
-            >
-                <Localize i18n_default_text='You’ve selected a trade type that is currently unsupported, but we’re working on it.' />
-            </Dialog>
-        );
-    }
-}
+const UnsupportedContractModal = ({ disableApp, enableApp, is_loading, is_visible, onConfirm, onClose }) => {
+    return (
+        <Dialog
+            title={localize('We’re working on it')}
+            confirm_button_text={localize('Stay on {{website_domain}}', { website_domain: website_name })}
+            cancel_button_text={localize('Go to Binary.com')}
+            onConfirm={onConfirm}
+            onCancel={onClose}
+            disableApp={disableApp}
+            enableApp={enableApp}
+            is_loading={is_loading}
+            is_closed_on_cancel
+            is_visible={is_visible}
+        >
+            <Localize i18n_default_text='You’ve selected a trade type that is currently unsupported, but we’re working on it.' />
+        </Dialog>
+    );
+};
 
 UnsupportedContractModal.propTypes = {
     disableApp: PropTypes.func,

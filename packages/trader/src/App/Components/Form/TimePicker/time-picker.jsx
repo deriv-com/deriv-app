b3170da992f36060801d396d2e1f585a075744f0
Mohammadreza Ghorbani <reza@firstsource.email>
2020-12-08 17:48:15 +0330

    Refactor into functional component

--- packages/trader/src/App/Components/Form/TimePicker/time-picker.jsx
+++ packages/trader/src/App/Components/Form/TimePicker/time-picker.jsx
@@ -7,117 +7,106 @@ import { Icon, InputField } from '@deriv/components';
 import { connect } from 'Stores/connect';
 import Dialog from './dialog.jsx';
 
-class TimePicker extends React.Component {
-    state = { is_open: false };
+const prefix_class = 'time-picker';
 
-    componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside);
-    }
+const TimePicker = ({
+    current_focus,
+    selected_time,
+    name,
+    is_nativepicker,
+    placeholder,
+    end_time,
+    setCurrentFocus,
+    start_time,
+    validation_errors,
+    onChange,
+    padding,
+}) => {
+    const [is_open, setOpen] = React.useState(false);
+    const [wrapper_ref, setWrapperRef] = React.useState(null);
 
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside);
-    }
+    React.useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    });
 
-    toggleDropDown = () => {
-        this.setState(state => ({ is_open: !state.is_open }));
+    const toggleDropDown = () => {
+        setOpen(!is_open);
     };
 
-    handleChange = arg => {
+    const handleChange = arg => {
         // To handle nativepicker;
         const value = typeof arg === 'object' ? arg.target.selected_time : arg;
 
-        if (value !== this.props.selected_time) {
-            this.props.onChange({ target: { name: this.props.name, value } });
+        if (value !== selected_time) {
+            onChange({ target: { name, value } });
         }
     };
 
-    saveRef = node => {
+    const saveRef = node => {
         if (!node) return;
-        if (node.nodeName === 'INPUT') {
-            this.target_element = node;
-            return;
-        }
-        this.wrapper_ref = node;
+        setWrapperRef(node);
     };
 
-    handleClickOutside = event => {
-        if (this.wrapper_ref && !this.wrapper_ref.contains(event.target)) {
-            if (this.state.is_open) {
-                this.setState({ is_open: false });
-            }
+    const handleClickOutside = event => {
+        if (!wrapper_ref?.contains(event.target) && is_open) {
+            setOpen(false);
         }
     };
 
-    render() {
-        const prefix_class = 'time-picker';
-        const {
-            current_focus,
-            selected_time,
-            name,
-            is_nativepicker,
-            placeholder,
-            end_time,
-            setCurrentFocus,
-            start_time,
-            validation_errors,
-        } = this.props;
-        return (
-            <div
-                ref={this.saveRef}
-                className={classNames(prefix_class, { [`${prefix_class}--padding`]: this.props.padding })}
-            >
-                {is_nativepicker ? (
-                    <input
-                        type='time'
+    return (
+        <div ref={saveRef} className={classNames(prefix_class, { [`${prefix_class}--padding`]: padding })}>
+            {is_nativepicker ? (
+                <input
+                    type='time'
+                    id={`dt_${name}_input`}
+                    value={selected_time}
+                    onChange={handleChange}
+                    name={name}
+                    min={start_time}
+                    max={end_time}
+                />
+            ) : (
+                <React.Fragment>
+                    <InputField
+                        error_messages={validation_errors}
+                        type='text'
+                        is_hj_whitelisted
+                        is_read_only
                         id={`dt_${name}_input`}
-                        value={selected_time}
-                        onChange={this.handleChange}
+                        className={classNames(`${prefix_class}-input`)}
+                        current_focus={current_focus}
+                        value={`${selected_time} GMT`}
+                        onClick={toggleDropDown}
                         name={name}
-                        min={start_time}
-                        max={end_time}
+                        placeholder={placeholder}
+                        setCurrentFocus={setCurrentFocus}
                     />
-                ) : (
-                    <React.Fragment>
-                        <InputField
-                            error_messages={validation_errors}
-                            type='text'
-                            is_hj_whitelisted
-                            is_read_only
-                            id={`dt_${name}_input`}
-                            className={classNames(`${prefix_class}-input`)}
-                            current_focus={current_focus}
-                            value={`${selected_time} GMT`}
-                            onClick={this.toggleDropDown}
-                            name={name}
-                            placeholder={placeholder}
-                            setCurrentFocus={setCurrentFocus}
+                    <Icon icon='IcClockOutline' className={`${prefix_class}__icon`} />
+                    <CSSTransition
+                        in={is_open}
+                        classNames={{
+                            enter: 'time-picker__dialog--enter',
+                            enterDone: 'time-picker__dialog--enter-done',
+                            exit: 'time-picker__dialog--exit',
+                        }}
+                        timeout={100}
+                        unmountOnExit
+                    >
+                        <Dialog
+                            end_time={end_time}
+                            start_time={start_time}
+                            className={'from-left'}
+                            onChange={handleChange}
+                            preClass={prefix_class}
+                            selected_time={selected_time}
                         />
-                        <Icon icon='IcClockOutline' className={`${prefix_class}__icon`} />
-                        <CSSTransition
-                            in={this.state.is_open}
-                            classNames={{
-                                enter: 'time-picker__dialog--enter',
-                                enterDone: 'time-picker__dialog--enter-done',
-                                exit: 'time-picker__dialog--exit',
-                            }}
-                            timeout={100}
-                            unmountOnExit
-                        >
-                            <Dialog
-                                end_time={end_time}
-                                start_time={start_time}
-                                className={'from-left'}
-                                onChange={this.handleChange}
-                                preClass={prefix_class}
-                                selected_time={selected_time}
-                            />
-                        </CSSTransition>
-                    </React.Fragment>
-                )}
-            </div>
-        );
-    }
-}
+                    </CSSTransition>
+                </React.Fragment>
+            )}
+        </div>
+    );
+};
 
 TimePicker.propTypes = {
     end_time: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.object]),

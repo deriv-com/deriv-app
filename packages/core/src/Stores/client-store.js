dcccb1c021f97219ad03083b871abf2faa8dbd65
AbdallaMourad <abdullah.f98m@gmail.com>
2020-12-09 15:01:45 +0800

    Add: Cookies script

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -21,7 +21,7 @@ import * as SocketCache from '_common/base/socket_cache';
 import { isEuCountry } from '_common/utility';
 import BaseStore from './base-store';
 import { getClientAccountType, getAccountTitle } from './Helpers/client';
-import { createDeviceDataObject, setDeviceDataCookie } from './Helpers/device';
+import { createDeviceDataObject, getCookieObject, setDeviceDataCookie } from './Helpers/device';
 import { handleClientNotifications, clientNotifications } from './Helpers/client-notifications';
 import { buildCurrenciesList } from './Modules/Trading/Helpers/currency';
 
@@ -1571,8 +1571,32 @@ export default class ClientStore extends BaseStore {
             this.root_store.common.server_time.format('YYYY-MM-DD')
         );
         const signup_device_cookie = setDeviceDataCookie('signup_device', isDesktopOs() ? 'desktop' : 'mobile');
-        const device_data = createDeviceDataObject(date_first_contact_cookie, signup_device_cookie);
+        const cookies_list = [
+            'affiliate_token',
+            'gclid_url',
+            'utm_source',
+            'utm_ad_id',
+            'utm_adgroup_id',
+            'utm_adrollclk_id',
+            'utm_campaign_id',
+            'utm_campaign',
+            'utm_fbcl_id',
+            'utm_gl_client_id',
+            'utm_msclk_id',
+            'utm_medium',
+            'utm_term',
+            'utm_content',
+        ];
+        const cookies = {
+            signup_device: signup_device_cookie,
+            date_first_contact: date_first_contact_cookie,
+        };
 
+        cookies_list.forEach(element => {
+            cookies[element] = getCookieObject(element);
+        });
+
+        const device_data = createDeviceDataObject(cookies);
         this.device_data = { ...this.device_data, ...device_data };
     }
 

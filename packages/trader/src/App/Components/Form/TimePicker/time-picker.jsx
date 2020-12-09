1f4d3aa6b57295e967b2ab1fd42cc1bbdd6405f5
Mohammadreza Ghorbani <reza@firstsource.email>
2020-12-09 13:03:32 +0330

    Reorder props and improve naming

--- packages/trader/src/App/Components/Form/TimePicker/time-picker.jsx
+++ packages/trader/src/App/Components/Form/TimePicker/time-picker.jsx
@@ -7,22 +7,22 @@ import { Icon, InputField } from '@deriv/components';
 import { connect } from 'Stores/connect';
 import Dialog from './dialog.jsx';
 
-const prefix_class = 'time-picker';
+const class_prefix = 'time-picker';
 
 const TimePicker = ({
     current_focus,
-    selected_time,
-    name,
-    is_nativepicker,
-    placeholder,
     end_time,
+    is_nativepicker,
+    name,
+    onChange,
+    padding,
+    placeholder,
+    selected_time,
     setCurrentFocus,
     start_time,
     validation_errors,
-    onChange,
-    padding,
 }) => {
-    const [is_open, setOpen] = React.useState(false);
+    const [is_open, setIsOpen] = React.useState(false);
     const [wrapper_ref, setWrapperRef] = React.useState(null);
 
     React.useEffect(() => {
@@ -31,7 +31,7 @@ const TimePicker = ({
     });
 
     const toggleDropDown = () => {
-        setOpen(!is_open);
+        setIsOpen(!is_open);
     };
 
     const handleChange = arg => {
@@ -50,12 +50,12 @@ const TimePicker = ({
 
     const handleClickOutside = event => {
         if (!wrapper_ref?.contains(event.target) && is_open) {
-            setOpen(false);
+            setIsOpen(false);
         }
     };
 
     return (
-        <div ref={saveRef} className={classNames(prefix_class, { [`${prefix_class}--padding`]: padding })}>
+        <div ref={saveRef} className={classNames(class_prefix, { [`${class_prefix}--padding`]: padding })}>
             {is_nativepicker ? (
                 <input
                     type='time'
@@ -74,7 +74,7 @@ const TimePicker = ({
                         is_hj_whitelisted
                         is_read_only
                         id={`dt_${name}_input`}
-                        className={classNames(`${prefix_class}-input`)}
+                        className={classNames(`${class_prefix}-input`)}
                         current_focus={current_focus}
                         value={`${selected_time} GMT`}
                         onClick={toggleDropDown}
@@ -82,7 +82,7 @@ const TimePicker = ({
                         placeholder={placeholder}
                         setCurrentFocus={setCurrentFocus}
                     />
-                    <Icon icon='IcClockOutline' className={`${prefix_class}__icon`} />
+                    <Icon icon='IcClockOutline' className={`${class_prefix}__icon`} />
                     <CSSTransition
                         in={is_open}
                         classNames={{
@@ -98,7 +98,7 @@ const TimePicker = ({
                             start_time={start_time}
                             className={'from-left'}
                             onChange={handleChange}
-                            preClass={prefix_class}
+                            preClass={class_prefix}
                             selected_time={selected_time}
                         />
                     </CSSTransition>

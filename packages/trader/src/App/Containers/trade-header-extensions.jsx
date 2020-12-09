e3525a398578066c0aa50f6840f8720e85194327
Amir Ghadimi <amir.ghadimi@firstsource.tech>
2020-12-09 15:06:12 +0330

    update: refactor components

--- packages/trader/src/App/Containers/trade-header-extensions.jsx
+++ packages/trader/src/App/Containers/trade-header-extensions.jsx
@@ -7,26 +7,29 @@ import TogglePositionsMobile from 'App/Components/Elements/TogglePositions/toggl
 import { connect, MobxContentProvider } from 'Stores/connect';
 import { WS } from 'Services/ws-methods';
 
-class TradeHeaderExtensions extends React.Component {
-    populateHeader = () => {
-        const {
-            active_positions_count,
-            disableApp,
-            enableApp,
-            is_logged_in,
-            is_positions_empty,
-            onPositionsRemove,
-            onPositionsSell,
-            onPositionsCancel,
-            positions,
-            positions_currency,
-            positions_error,
-            populateHeaderExtensions,
-        } = this.props;
-
+const TradeHeaderExtensions = ({
+    active_positions_count,
+    disableApp,
+    enableApp,
+    is_logged_in,
+    is_positions_empty,
+    onPositionsRemove,
+    onPositionsSell,
+    onPositionsCancel,
+    positions,
+    positions_currency,
+    positions_error,
+    populateHeaderExtensions,
+    store,
+    onMountPositions,
+    onMountCashier,
+    setAccountSwitchListener,
+    onUnmountPositions,
+}) => {
+    const populateHeader = () => {
         const header_items = is_logged_in && (
             <MobileWrapper>
-                <MobxContentProvider store={this.props.store}>
+                <MobxContentProvider store={store}>
                     <TogglePositionsMobile
                         active_positions_count={active_positions_count}
                         all_positions={positions}
@@ -46,36 +49,37 @@ class TradeHeaderExtensions extends React.Component {
         populateHeaderExtensions(header_items);
     };
 
-    async componentDidMount() {
-        if (isMobile()) {
-            const { client } = this.props.store;
-            // Waits for login to complete
-            await when(() => !client.is_populating_account_list);
-            if (this.props.is_logged_in) {
-                await WS.wait('authorize');
-                this.props.onMountPositions();
-                this.props.onMountCashier(true);
-                this.props.setAccountSwitchListener();
+    React.useEffect(() => {
+        const waitForLogin = async () => {
+            if (isMobile()) {
+                const { client } = store;
+                // Waits for login to complete
+                await when(() => !client.is_populating_account_list);
+                if (is_logged_in) {
+                    await WS.wait('authorize');
+                    onMountPositions();
+                    onMountCashier(true);
+                    setAccountSwitchListener();
+                }
             }
-        }
 
-        this.populateHeader();
-    }
+            populateHeader();
+        };
 
-    componentDidUpdate() {
-        this.populateHeader();
-    }
+        waitForLogin();
+    }, []);
 
-    componentWillUnmount() {
-        if (isMobile()) this.props.onUnmountPositions();
-        this.props.populateHeaderExtensions(null);
-    }
+    React.useEffect(() => {
+        populateHeader();
 
-    // eslint-disable-next-line class-methods-use-this
-    render() {
-        return null;
-    }
-}
+        return () => {
+            if (isMobile()) onUnmountPositions();
+            populateHeaderExtensions(null);
+        };
+    });
+
+    return null;
+};
 
 TradeHeaderExtensions.propTypes = {
     is_logged_in: PropTypes.bool,
--- packages/trader/src/App/Containers/trade-settings-extensions.jsx
+++ packages/trader/src/App/Containers/trade-settings-extensions.jsx
@@ -19,19 +19,19 @@ const ChartSettingContainer = Loadable({
 //     loading: UILoader,
 // });
 
-class TradeSettingsExtensions extends React.Component {
-    populateSettings = () => {
-        const { populateSettingsExtensions } = this.props;
+const TradeSettingsExtensions = ({ populateSettingsExtensions, store }) => {
+    const populateSettings = () => {
+        const value = ({ ...props }) => (
+            <MobxContentProvider store={store}>
+                <ChartSettingContainer {...props} />
+            </MobxContentProvider>
+        );
 
         const menu_items = [
             {
                 icon: 'IcChart',
                 label: localize('Charts'),
-                value: ({ ...props }) => (
-                    <MobxContentProvider store={this.props.store}>
-                        <ChartSettingContainer {...props} />
-                    </MobxContentProvider>
-                ),
+                value,
                 // uncomment below lines to bring back purchase lock and purchase confirmation}
                 // }, {
                 //     icon : IconPurchase,
@@ -43,23 +43,14 @@ class TradeSettingsExtensions extends React.Component {
         populateSettingsExtensions(menu_items);
     };
 
-    componentDidMount() {
-        this.populateSettings();
-    }
+    React.useEffect(() => {
+        populateSettings();
 
-    componentDidUpdate() {
-        this.populateSettings();
-    }
+        return () => populateSettingsExtensions(null);
+    });
 
-    componentWillUnmount() {
-        this.props.populateSettingsExtensions(null);
-    }
-
-    // eslint-disable-next-line class-methods-use-this
-    render() {
-        return null;
-    }
-}
+    return null;
+};
 
 TradeSettingsExtensions.propTypes = {
     populateSettingsExtensions: PropTypes.func,

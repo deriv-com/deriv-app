ca171e07321d5710823ff51a6ef76daa18c4d8eb
Mohammadreza Ghorbani <reza@firstsource.email>
2020-12-09 18:02:05 +0330

    Fix Create_test script to always start from the project root

--- tests/create_test.js
+++ tests/create_test.js
@@ -1,3 +1,4 @@
+const exec = require('child_process').exec;
 const path = require('path');
 const fs = require('fs');
 const render = require('./src/_utils/test_template');
@@ -8,13 +9,20 @@ const render = require('./src/_utils/test_template');
         throw new Error('Insufficient args. Please use node tests/create_test.js mobile|desktop test_name');
     }
     const test_name = `[${args[0]}]-${args[1]}`;
-    const file_name = `tests/src/${args[0]}/${args[1]}.test.js`;
-    const content = render(test_name);
-    const target_dir = path.dirname(file_name);
-    fs.mkdirSync(target_dir, { recursive: true });
-    fs.writeFile(file_name, content, function (err) {
+
+    exec("git rev-parse --show-toplevel", (err, stdout) =>  {
         if (err) throw err;
-        console.log('Saved!');
+
+        const project_root = stdout.trim();
+        const file_name = `tests/src/${args[0]}/${args[1]}.test.js`;
+        const content = render(test_name);
+        const target_dir = path.dirname(path.resolve(project_root, file_name));
+
+        fs.mkdirSync(target_dir, { recursive: true });
+        fs.writeFile(`${project_root}/${file_name}`, content, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
     });
 })();
 

a0dc446dec0d944f60fb4389ec385adbaedcaf74
AbdallaMourad <abdullah.f98m@gmail.com>
2020-12-10 10:49:18 +0800

    Fix: setState Callback

--- packages/components/src/components/send-email-template/send-email-template.jsx
+++ packages/components/src/components/send-email-template/send-email-template.jsx
@@ -3,88 +3,92 @@ import React from 'react';
 import Button from '../button/button.jsx';
 import Icon from '../icon/icon.jsx';
 
-class SendEmailTemplate extends React.Component {
-    resend_interval = null;
+const SendEmailTemplate = ({
+    children,
+    className,
+    lbl_no_receive,
+    onClickSendEmail,
+    resend_timeout,
+    subtitle,
+    title,
+    txt_resend,
+    txt_resend_in,
+}) => {
+    const [is_email_not_received_clicked, setIsEmailNotReceivedClicked] = React.useState(false);
+    const [is_resend_btn_disabled, setIsResendBtnDisabled] = React.useState(false);
+    const [resend_email_btn_text, setResendEmailBtnText] = React.useState(txt_resend);
 
-    state = {
-        is_email_not_received_clicked: false,
-        resend_timeout: this.props.resend_timeout || 60,
+    const timeout_limit = resend_timeout || 60;
+    let resend_interval = null;
+
+    React.useEffect(() => {
+        return () => {
+            clearInterval(resend_interval);
+        };
+    }, []);
+
+    const onClickEmailNotReceived = () => {
+        if (!is_email_not_received_clicked) {
+            setIsEmailNotReceivedClicked(true);
+        }
     };
 
-    componentWillUnmount() {
-        clearInterval(this.resend_interval);
-    }
-
-    onClickEmailNotReceived = () => {
-        this.setState({ is_email_not_received_clicked: true });
+    const onClickResendEmail = () => {
+        startCountdownTimer();
+        onClickSendEmail();
     };
 
-    onClickResendEmail = () => {
-        this.startCountdownTimer();
-        this.props.onClickSendEmail();
-    };
-
-    startCountdownTimer = () => {
-        this.setState(
-            prevState => ({
-                resend_timeout: prevState.resend_timeout - 1,
-            }),
-            () => {
-                this.resend_interval = setInterval(() => {
-                    if (this.state.resend_timeout === 1) {
-                        this.setState({ resend_timeout: 60 });
-                        clearInterval(this.resend_interval);
-                    } else {
-                        this.setState(prevState => ({ resend_timeout: prevState.resend_timeout - 1 }));
-                    }
-                }, 1000);
+    const startCountdownTimer = () => {
+        let timeout = timeout_limit;
+        resend_interval = setInterval(() => {
+            if (timeout-- !== 1) {
+                setIsResendBtnDisabled(timeout < timeout_limit);
+                setResendEmailBtnText(
+                    timeout < timeout_limit ? txt_resend_in.replace('{{seconds}}', timeout) : txt_resend
+                );
+            } else {
+                setIsResendBtnDisabled(false);
+                setResendEmailBtnText(txt_resend);
+                clearInterval(resend_interval);
             }
-        );
+        }, 1000);
     };
 
-    render() {
-        const is_resend_btn_disabled = this.state.resend_timeout < 60;
-        const resend_email_btn_text =
-            this.state.resend_timeout < 60
-                ? this.props.txt_resend_in.replace('{{seconds}}', this.state.resend_timeout)
-                : this.props.txt_resend;
-
-        return (
-            <div className='send-email-template'>
-                <div className='send-email-template__sent'>
-                    <Icon icon='IcEmailSent' className='send-email-template__icon' size={128} />
-                    <h1 className='send-email-template__title'>{this.props.title}</h1>
-                    <p className='send-email-template__subtitle'>{this.props.subtitle}</p>
-                    <Button
-                        className='send-email-template__btn'
-                        onClick={this.onClickEmailNotReceived}
-                        text={this.props.lbl_no_receive}
-                        tertiary
-                    />
-                </div>
-                {this.state.is_email_not_received_clicked && (
-                    <>
-                        <div className={`send-email-template__resend ${this.props.className}`}>
-                            {this.props.children}
-                        </div>
-                        <div className='send-email-template__cta'>
-                            <Button
-                                className='send-email-template__cta-btn'
-                                is_disabled={is_resend_btn_disabled}
-                                has_effect
-                                text={resend_email_btn_text}
-                                onClick={this.onClickResendEmail}
-                                primary
-                            />
-                        </div>
-                    </>
-                )}
+    return (
+        <div className='send-email-template'>
+            <div className='send-email-template__sent'>
+                <Icon icon='IcEmailSent' className='send-email-template__icon' size={128} />
+                <h1 className='send-email-template__title'>{title}</h1>
+                <p className='send-email-template__subtitle'>{subtitle}</p>
+                <Button
+                    className='send-email-template__btn'
+                    onClick={onClickEmailNotReceived}
+                    text={lbl_no_receive}
+                    tertiary
+                />
             </div>
-        );
-    }
-}
+            {is_email_not_received_clicked && (
+                <>
+                    <div className={`send-email-template__resend ${className}`}>{children}</div>
+                    <div className='send-email-template__cta'>
+                        <Button
+                            className='send-email-template__cta-btn'
+                            is_disabled={is_resend_btn_disabled}
+                            has_effect
+                            text={resend_email_btn_text}
+                            onClick={onClickResendEmail}
+                            primary
+                        />
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
 
 SendEmailTemplate.propTypes = {
+    children: PropTypes.array,
+    className: PropTypes.string,
     is_disabled: PropTypes.bool,
     lbl_no_receive: PropTypes.string,
     onClickSendEmail: PropTypes.func,

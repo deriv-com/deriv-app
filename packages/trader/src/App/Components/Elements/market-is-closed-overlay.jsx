8ea72717caa3a7682e0c547e1b60e861fa5bcb1e
ali fs <ali.sabzinezhad@firstsource.tech>
2020-12-15 12:34:38 +0330

    stop offering synthetic indices for MF if other markets are closed

--- packages/trader/src/App/Components/Elements/market-is-closed-overlay.jsx
+++ packages/trader/src/App/Components/Elements/market-is-closed-overlay.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Button } from '@deriv/components';
 import { localize, Localize } from '@deriv/translations';
+import { connect } from 'Stores/connect';
 
-const MarketIsClosedOverlay = ({ onClick }) => (
+const MarketIsClosedOverlay = ({ onClick, is_financial_account }) => (
     <div className='market-is-closed-overlay'>
         <p>
             <Localize i18n_default_text='Market is closed.' />
         </p>
-        <p>
-            <Localize i18n_default_text='Try Synthetic Indices which simulate real-world market volatility and are open 24/7.' />
-        </p>
-        <Button
-            className='market-is-closed-overlay__button'
-            onClick={onClick}
-            text={localize('Try Synthetic Indices')}
-            primary
-        />
+        {!is_financial_account && (
+            <div>
+                <p>
+                    <Localize i18n_default_text='Try Synthetic Indices which simulate real-world market volatility and are open 24/7.' />
+                </p>
+                <Button
+                    className='market-is-closed-overlay__button'
+                    onClick={onClick}
+                    text={localize('Try Synthetic Indices')}
+                    primary
+                />
+            </div>
+        )}
     </div>
 );
 
-export default MarketIsClosedOverlay;
+export default connect(({ client }) => ({
+    is_financial_account: client.is_financial_account,
+}))(MarketIsClosedOverlay);

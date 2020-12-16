1e21edb008fe0af0b2def5875d522ea8829d3629
sara-fs <sara.khosropour@regentmarkets.com>
2020-12-16 20:29:59 +0330

    Replaced paragraph with text on form-submit-error-message

--- packages/components/src/components/form-submit-error-message/form-submit-error-message.jsx
+++ packages/components/src/components/form-submit-error-message/form-submit-error-message.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Icon from '../icon/icon.jsx';
+import Text from '../text';
 
 const FormSubmitErrorMessage = ({ message }) => (
     <div className='dc-form-submit-error-message'>
         <Icon icon='IcAlertDanger' />
-        <p>{message}</p>
+        <Text as='p' size='xxs' weight='bold' color='prominent'>
+            {message}
+        </Text>
     </div>
 );
 

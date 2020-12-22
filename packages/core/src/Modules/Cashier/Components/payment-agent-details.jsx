25f73564eee88a4af75bcedf57464e20d675ac99
mehrzad-fs <mehrzad@firstsource.tech>
2020-12-22 15:43:44 +0330

    refactor payment agen details

--- packages/core/src/Modules/Cashier/Components/payment-agent-details.jsx
+++ packages/core/src/Modules/Cashier/Components/payment-agent-details.jsx
@@ -3,24 +3,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Icon } from '@deriv/components';
 
-class Detail extends React.Component {
-    render() {
-        const { action, icon, is_last_child, value, ...rest } = this.props;
-
-        return (
-            <div className='payment-agent__accordion-content-line'>
-                <Icon icon={`Ic${icon}`} className='payment-agent__accordion-content-icon' color='secondary' />
-                <a
-                    className='payment-agent__contact cashier__paragraph'
-                    href={`${action ? `${action}:` : ''}${value}`}
-                    {...rest}
-                >
-                    {value}
-                </a>
-            </div>
-        );
-    }
-}
+const Detail = ({ action, icon, is_last_child, value, ...rest }) => {
+    return (
+        <div className='payment-agent__accordion-content-line'>
+            <Icon icon={`Ic${icon}`} className='payment-agent__accordion-content-icon' color='secondary' />
+            <a
+                className='payment-agent__contact cashier__paragraph'
+                href={`${action ? `${action}:` : ''}${value}`}
+                {...rest}
+            >
+                {value}
+            </a>
+        </div>
+    );
+};
 
 Detail.propTypes = {
     action: PropTypes.string,
@@ -31,35 +27,26 @@ Detail.propTypes = {
     value: PropTypes.string,
 };
 
-class PaymentAgentDetails extends React.Component {
-    render() {
-        return (
-            <div className={classNames('payment-agent__accordion-content', this.props.className)}>
-                {this.props.payment_agent_phone && (
-                    <Detail action='tel' value={this.props.payment_agent_phone} icon='Phone' />
-                )}
-                {this.props.payment_agent_url && (
-                    <Detail
-                        value={this.props.payment_agent_url}
-                        icon='Website'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    />
-                )}
-                {this.props.payment_agent_email && (
-                    <Detail
-                        action='mailto'
-                        value={this.props.payment_agent_email}
-                        icon='EmailOutline'
-                        is_last_child
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    />
-                )}
-            </div>
-        );
-    }
-}
+const PaymentAgentDetails = ({ className, payment_agent_phone, payment_agent_url, payment_agent_email }) => {
+    return (
+        <div className={classNames('payment-agent__accordion-content', className)}>
+            {payment_agent_phone && <Detail action='tel' value={payment_agent_phone} icon='Phone' />}
+            {payment_agent_url && (
+                <Detail value={payment_agent_url} icon='Website' target='_blank' rel='noopener noreferrer' />
+            )}
+            {payment_agent_email && (
+                <Detail
+                    action='mailto'
+                    value={payment_agent_email}
+                    icon='EmailOutline'
+                    is_last_child
+                    target='_blank'
+                    rel='noopener noreferrer'
+                />
+            )}
+        </div>
+    );
+};
 
 PaymentAgentDetails.propTypes = {
     className: PropTypes.string,

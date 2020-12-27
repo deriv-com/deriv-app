f8e1897ea4356bcfe9fc9bc45cc2884b0c7b9f2f
sara-fs <sara.khosropour@regentmarkets.com>
2020-12-27 14:25:48 +0330

    Replaced span with text on contract-result-overlay

--- packages/bot-web-ui/src/components/contract-result-overlay.jsx
+++ packages/bot-web-ui/src/components/contract-result-overlay.jsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Icon } from '@deriv/components';
+import { Icon, Text } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import '../assets/sass/contract-result-overlay.scss';
 
@@ -15,7 +15,7 @@ const ContractResultOverlay = props => {
                 'db-contract-card__result--lost': !has_won_contract,
             })}
         >
-            <span className='db-contract-card__result-caption'>
+            <Text weight='bold' className='db-contract-card__result-caption'>
                 {has_won_contract ? (
                     <React.Fragment>
                         {localize('Won')}
@@ -27,7 +27,7 @@ const ContractResultOverlay = props => {
                         <Icon icon='IcCrossCircle' className='db-contract-card__result-icon' color='red' />
                     </React.Fragment>
                 )}
-            </span>
+            </Text>
         </div>
     );
 };

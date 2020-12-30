13a7d9bc24d2f432ffec0a205b3ddc32d0f79317
mehrzad-fs <mehrzad@firstsource.tech>
2020-12-30 09:22:14 +0330

    update value to children

--- packages/core/src/Modules/Cashier/Components/payment-agent-details.jsx
+++ packages/core/src/Modules/Cashier/Components/payment-agent-details.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Icon } from '@deriv/components';
 
-const Detail = ({ action, icon, is_last_child, value, ...rest }) => {
+const Detail = ({ action, icon, is_last_child, children, ...rest }) => {
     return (
         <div className='payment-agent__accordion-content-line'>
             <Icon icon={`Ic${icon}`} className='payment-agent__accordion-content-icon' color='secondary' />
             <a
                 className='payment-agent__contact cashier__paragraph'
-                href={`${action ? `${action}:` : ''}${value}`}
+                href={`${action ? `${action}:` : ''}${children}`}
                 {...rest}
             >
-                {value}
+                {children}
             </a>
         </div>
     );
@@ -35,14 +35,9 @@ const PaymentAgentDetails = ({ className, payment_agent_phone, payment_agent_url
                 <Detail value={payment_agent_url} icon='Website' target='_blank' rel='noopener noreferrer' />
             )}
             {payment_agent_email && (
-                <Detail
-                    action='mailto'
-                    value={payment_agent_email}
-                    icon='EmailOutline'
-                    is_last_child
-                    target='_blank'
-                    rel='noopener noreferrer'
-                />
+                <Detail action='mailto' icon='EmailOutline' is_last_child target='_blank' rel='noopener noreferrer'>
+                    {payment_agent_email}
+                </Detail>
             )}
         </div>
     );

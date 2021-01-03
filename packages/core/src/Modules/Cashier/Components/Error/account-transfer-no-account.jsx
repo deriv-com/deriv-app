2fed67430b502fe364f86413f9cb2598b1629781
sara-fs <sara.khosropour@regentmarkets.com>
2021-01-03 14:30:40 +0330

    Replaced header with text on no-balance and account-transfer-no-account

--- packages/core/src/Modules/Cashier/Components/Error/account-transfer-no-account.jsx
+++ packages/core/src/Modules/Cashier/Components/Error/account-transfer-no-account.jsx
@@ -6,9 +6,9 @@ import { connect } from 'Stores/connect';
 const AccountTransferNoAccount = ({ toggleAccountsDialog }) => (
     <div className='cashier__wrapper cashier__no-balance'>
         <Icon icon='IcCashierNoBalance' className='cashier__no-balance-icon' size={116} />
-        <h2 className='withdraw__header'>
+        <Text as='h2' weight='bold' className='withdraw__header'>
             <Localize i18n_default_text='You need at least two accounts' />
-        </h2>
+        </Text>
         <Text as='p' size='xs' line_height='s' className='cashier__text'>
             <Localize i18n_default_text='Please create a second account (Deriv or DMT5) to enable fund transfers.' />
         </Text>

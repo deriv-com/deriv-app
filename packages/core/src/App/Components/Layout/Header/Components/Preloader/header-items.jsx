cafd594a84b69cb2ba993d3db6820063417a6d6a
sean-binary <seanho96@yahoo.com>
2021-01-11 15:37:14 +0800

    apply code suggestions

--- packages/core/src/App/Components/Layout/Header/Components/Preloader/header-items.jsx
+++ packages/core/src/App/Components/Layout/Header/Components/Preloader/header-items.jsx
@@ -2,22 +2,16 @@ import ContentLoader from 'react-content-loader';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const HeaderItemsLoader = ({ is_logged_in, speed }) => (
+const HeaderItemsLoader = ({ speed }) => (
     <ContentLoader
         speed={speed}
         primaryColor={'var(--general-section-1)'}
         secondaryColor={'var(--general-hover)'}
         style={{ width: '150px', height: '48px' }}
     >
-        {is_logged_in ? <LoggedInPreloader /> : <></>}
-    </ContentLoader>
-);
-
-const LoggedInPreloader = () => (
-    <>
         <circle cx='250' cy='65' r='42' />
         <circle cx='355' cy='65' r='42' />
-    </>
+    </ContentLoader>
 );
 
 HeaderItemsLoader.propTypes = {
--- packages/core/src/App/Containers/Layout/header/dashboard-header.jsx
+++ packages/core/src/App/Containers/Layout/header/dashboard-header.jsx
@@ -3,85 +3,96 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { Button, Icon, Text } from '@deriv/components';
 import { PlatformContext, redirectToLogin, redirectToSignUp, routes } from '@deriv/shared';
-import { getLanguage } from '@deriv/translations';
+import { getLanguage, localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 import DerivLogo from 'Assets/SvgComponents/header/deriv-logo.svg';
 import DerivText from 'Assets/SvgComponents/header/deriv-text.svg';
 import DerivLogoLight from 'Assets/SvgComponents/header/deriv-logo-light.svg';
 import { HeaderItemsLoader } from '../../../Components/Layout/Header/Components/Preloader/header-items.jsx';
 
-const DashboardHeader = ({ is_logged_in, is_logging_in }) => {
+const LoggedInHeader = () => {
+    const history = useHistory();
+
+    return (
+        <header className='logged-in-dashboard-header'>
+            <div className='logged-in-dashboard-header__menu-items'>
+                <div className='logged-in-dashboard-header__menu-items-left'>
+                    <div onClick={() => history.push(routes.dashboard)}>
+                        <DerivLogoLight />
+                    </div>
+                </div>
+                <div className='logged-in-dashboard-header__menu-items-right'>
+                    <Icon icon={'IcProfile'} size={32} className='logged-in-dashboard-header__menu-items-right-icon' />
+                    <Icon icon={'IcNotification'} size={32} />
+                </div>
+            </div>
+        </header>
+    );
+};
+
+const LoggedOutHeader = () => {
     const history = useHistory();
     const { is_deriv_crypto } = React.useContext(PlatformContext);
 
     return (
-        <>
-            {is_logging_in && (
-                <div className={'header-items__preloader'}>
-                    <HeaderItemsLoader is_logged_in={is_logged_in} speed={3} />
+        <header className='logged-out-dashboard-header'>
+            <div className='logged-out-dashboard-header__menu-items'>
+                <div className='logged-out-dashboard-header__menu-items-left'>
+                    <div onClick={() => history.push(routes.dashboard)}>
+                        <DerivLogo style={{ marginRight: '0.684rem' }} />
+                        <DerivText />
+                    </div>
                 </div>
-            )}
-            {is_logged_in ? (
-                <header className='logged-in-dashboard-header'>
-                    <div className='logged-in-dashboard-header__menu-items'>
-                        <div className='logged-in-dashboard-header__menu-items-left'>
-                            <div onClick={() => history.push(routes.dashboard)}>
-                                <DerivLogoLight />
-                            </div>
-                        </div>
-                        <div className='logged-in-dashboard-header__menu-items-right'>
-                            <Icon
-                                icon={'IcProfile'}
-                                size={32}
-                                className='logged-in-dashboard-header__menu-items-right-icon'
-                            />
-                            <Icon icon={'IcNotification'} size={32} />
-                        </div>
-                    </div>
-                </header>
-            ) : (
-                <header className='logged-out-dashboard-header'>
-                    <div className='logged-out-dashboard-header__menu-items'>
-                        <div className='logged-out-dashboard-header__menu-items-left'>
-                            <div onClick={() => history.push(routes.dashboard)}>
-                                <DerivLogo style={{ marginRight: '0.684rem' }} />
-                                <DerivText />
-                            </div>
-                        </div>
-                        <div className='logged-out-dashboard-header__menu-items-middle'>
-                            <Text color='colored-background' size='s' onClick={() => history.push(routes.explore)}>
-                                Explore
-                            </Text>
-                            <Text color='colored-background' size='s' onClick={() => history.push(routes.about_us)}>
-                                About us
-                            </Text>
-                            <Text color='colored-background' size='s' onClick={() => history.push(routes.resources)}>
-                                Resources
-                            </Text>
-                        </div>
-                        <div className='logged-out-dashboard-header__menu-items-right'>
-                            <Button.Group>
-                                <Button
-                                    tertiary
-                                    medium
-                                    has_effect
-                                    className='logged-out-dashboard-header__buttons-secondary'
-                                    text='Log in'
-                                    onClick={() => redirectToLogin(false, getLanguage())}
-                                />
-                                <Button
-                                    primary
-                                    className='logged-out-dashboard-header__buttons-primary'
-                                    text='Create free demo account'
-                                    onClick={() => redirectToSignUp({ is_deriv_crypto })}
-                                />
-                            </Button.Group>
-                        </div>
-                    </div>
-                </header>
-            )}
-        </>
+                <div className='logged-out-dashboard-header__menu-items-middle'>
+                    <Text color='colored-background' size='s' onClick={() => history.push(routes.explore)}>
+                        {localize('Explore')}
+                    </Text>
+                    <Text color='colored-background' size='s' onClick={() => history.push(routes.about_us)}>
+                        {localize('About us')}
+                    </Text>
+                    <Text color='colored-background' size='s' onClick={() => history.push(routes.resources)}>
+                        {localize('Resources')}
+                    </Text>
+                </div>
+                <div className='logged-out-dashboard-header__menu-items-right'>
+                    <Button.Group>
+                        <Button
+                            tertiary
+                            medium
+                            has_effect
+                            className='logged-out-dashboard-header__buttons-secondary'
+                            text={localize('Log in')}
+                            onClick={() => redirectToLogin(false, getLanguage())}
+                        />
+                        <Button
+                            primary
+                            className='logged-out-dashboard-header__buttons-primary'
+                            text={localize('Create free demo account')}
+                            onClick={() => redirectToSignUp({ is_deriv_crypto })}
+                        />
+                    </Button.Group>
+                </div>
+            </div>
+        </header>
     );
+};
+
+const HeaderPreloader = () => (
+    <div className={'header-items__preloader'}>
+        <HeaderItemsLoader speed={3} />
+    </div>
+);
+
+const DashboardHeader = ({ is_logged_in, is_logging_in }) => {
+    if (is_logging_in) {
+        return <HeaderPreloader />;
+    }
+
+    if (is_logged_in) {
+        return <LoggedInHeader />;
+    }
+
+    return <LoggedOutHeader />;
 };
 
 DashboardHeader.propTypes = {

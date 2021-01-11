83a1780a30f19378a868b78e2b5efff2cc0947c0
Amir Ghadimi <amir.ghadimi@firstsource.tech>
2021-01-11 13:06:26 +0330

    wipe: add notes and fix issues

--- packages/components/stories/tabs/stories/basic.js
+++ packages/components/stories/tabs/stories/basic.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { boolean } from '@storybook/addon-knobs';
 import Tabs from 'Components/tabs';
 import Wrapper from '../../shared/wrapper';
 
 const Basic = () => (
     <Wrapper is_block is_dark={boolean('Dark Theme', false)}>
-        <Tabs>
-            <div header_content='Tab one' label='Tab label'>
-                <div className='tab-content-storybook'>Content for tab 1</div>
-            </div>
-            <div header_content='Tab two' label='Tab label'>
-                <div className='tab-content-storybook'>Content for tab 2</div>
-            </div>
-            <div header_content='Tab three' label='Tab label'>
-                <div className='tab-content-storybook'>Content for tab 3</div>
-            </div>
-        </Tabs>
+        <Router>
+            <Tabs>
+                <div header_content={<h3>Tab one</h3>} label='Tab label'>
+                    <div className='tab-content-storybook'>Content for tab 1</div>
+                </div>
+                <div header_content={<h3>Tab two</h3>} label='Tab label'>
+                    <div className='tab-content-storybook'>Content for tab 2</div>
+                </div>
+                <div header_content={<h3>Tab three</h3>} label='Tab label'>
+                    <div className='tab-content-storybook'>Content for tab 3</div>
+                </div>
+            </Tabs>
+        </Router>
     </Wrapper>
 );
 

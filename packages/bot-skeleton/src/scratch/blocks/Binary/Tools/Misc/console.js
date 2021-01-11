c53c4fcb85c4db821e39ab04252119aa564d2d96
kseniya-deriv <kseniya@deriv.com>
2021-01-11 20:13:51 +0300

    fix: error: invalid path 'packages/bot-skeleton/src/scratch/blocks/Binary/Tools/Misc.'

---
+++ packages/bot-skeleton/src/scratch/blocks/Binary/Tools/Misc/console.js
@@ -0,0 +1,63 @@
+import { localize } from '@deriv/translations';
+import { emptyTextValidator } from '../../../../utils';
+
+Blockly.Blocks.console = {
+    init() {
+        this.jsonInit(this.definition());
+    },
+    definition() {
+        return {
+            message0: localize('Console {{ message_type }} value: {{ input_message }}', {
+                message_type: '%1',
+                input_message: '%2',
+            }),
+            args0: [
+                {
+                    type: 'field_dropdown',
+                    name: 'CONSOLE_TYPE',
+                    options: [
+                        [localize('Log'), 'log'],
+                        [localize('Warn'), 'warn'],
+                        [localize('Error'), 'error'],
+                        [localize('Table'), 'table'],
+                    ],
+                },
+                {
+                    type: 'input_value',
+                    name: 'MESSAGE',
+                    check: null,
+                },
+            ],
+            colour: Blockly.Colours.Special3.colour,
+            colourSecondary: Blockly.Colours.Special3.colourSecondary,
+            colourTertiary: Blockly.Colours.Special3.colourTertiary,
+            previousStatement: null,
+            nextStatement: null,
+            tooltip: localize('Display messages in the developer’s console.'),
+            category: Blockly.Categories.Miscellaneous,
+        };
+    },
+    meta() {
+        return {
+            display_name: localize('Console'),
+            description: localize(
+                "This block displays messages in the developer's console with an input that can be either a string of text, a number, boolean, or an array of data."
+            ),
+        };
+    },
+    getRequiredValueInputs() {
+        return {
+            MESSAGE: emptyTextValidator,
+        };
+    },
+};
+
+Blockly.JavaScript.console = block => {
+    const console_type = block.getFieldValue('CONSOLE_TYPE') || 'log';
+    const message =
+        Blockly.JavaScript.valueToCode(block, 'MESSAGE', Blockly.JavaScript.ORDER_ATOMIC) ||
+        `"${localize('<empty message>')}"`;
+
+    const code = `Bot.console({ type: '${console_type}', message: ${message}});\n`;
+    return code;
+};
---
+++ packages/bot-skeleton/src/scratch/blocks/Binary/Tools/Misc/notify.js
@@ -0,0 +1,77 @@
+import { localize } from '@deriv/translations';
+import { emptyTextValidator } from '../../../../utils';
+import { config } from '../../../../../constants/config';
+
+Blockly.Blocks.notify = {
+    init() {
+        this.jsonInit(this.definition());
+    },
+    definition() {
+        return {
+            message0: localize(
+                'Notify {{ notification_type }} with sound: {{ notification_sound }} {{ input_message }}',
+                {
+                    notification_type: '%1',
+                    notification_sound: '%2',
+                    input_message: '%3',
+                }
+            ),
+            args0: [
+                {
+                    type: 'field_dropdown',
+                    name: 'NOTIFICATION_TYPE',
+                    options: config.lists.NOTIFICATION_TYPE,
+                },
+                {
+                    type: 'field_dropdown',
+                    name: 'NOTIFICATION_SOUND',
+                    options: config.lists.NOTIFICATION_SOUND,
+                },
+                {
+                    type: 'input_value',
+                    name: 'MESSAGE',
+                    check: null,
+                },
+            ],
+            colour: Blockly.Colours.Special3.colour,
+            colourSecondary: Blockly.Colours.Special3.colourSecondary,
+            colourTertiary: Blockly.Colours.Special3.colourTertiary,
+            previousStatement: null,
+            nextStatement: null,
+            tooltip: localize('Displays a notification and optionally play selected sound'),
+            category: Blockly.Categories.Miscellaneous,
+        };
+    },
+    meta() {
+        return {
+            display_name: localize('Notify'),
+            description: localize(
+                'This block displays a message. You can specify the color of the message and choose from 6 different sound options.'
+            ),
+        };
+    },
+    getRequiredValueInputs() {
+        return {
+            MESSAGE: emptyTextValidator,
+        };
+    },
+};
+
+Blockly.JavaScript.notify = block => {
+    const notificationType = block.getFieldValue('NOTIFICATION_TYPE');
+    const sound = block.getFieldValue('NOTIFICATION_SOUND');
+    const message_block = block.getInputTargetBlock('MESSAGE');
+    let variable_name = null;
+
+    if (message_block.type === 'variables_get') {
+        const variable_id = message_block.getFieldValue('VAR');
+        variable_name = Blockly.derivWorkspace.getVariableById(variable_id).name;
+    }
+
+    const message =
+        Blockly.JavaScript.valueToCode(block, 'MESSAGE', Blockly.JavaScript.ORDER_ATOMIC) ||
+        `"${localize('<empty message>')}"`;
+
+    const code = `Bot.notify({ className: 'journal__text--${notificationType}', message: ${message}, sound: '${sound}', block_id: '${block.id}', variable_name: '${variable_name}' });\n`;
+    return code;
+};

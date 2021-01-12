968965f104ea2fa74f29862e90a00fe50447805b
sara-fs <sara.khosropour@regentmarkets.com>
2021-01-12 09:51:45 +0330

    Removed extra codes related to cfd-test page

--- packages/dashboard/src/components/pages/home/home.tsx
+++ packages/dashboard/src/components/pages/home/home.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
-// import { Text } from '@deriv/components';
-// import TempButtons from 'Components/temp-buttons';
-import CFDTest from '../trade-type/cfd-test';
+import { Text } from '@deriv/components';
+import TempButtons from 'Components/temp-buttons';
 
 const Home: React.FC = () => {
     return (
         <React.Fragment>
             <div className='dw-home'>
-                {/* <Text as='p' size='l' color='less-prominent' align='center'>
+                <Text as='p' size='l' color='less-prominent' align='center'>
                     Home
                 </Text>
-                <TempButtons /> */}
-                <CFDTest />
+                <TempButtons />
             </div>
         </React.Fragment>
     );

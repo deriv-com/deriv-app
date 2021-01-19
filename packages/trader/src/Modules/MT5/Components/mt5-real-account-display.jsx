e6db974af02c9969b900a0c14897545bc413dc02
zakibinary <zaki@regentmarkets.com>
2021-01-19 11:31:45 +0800

    refactor: remove is_eu bool

--- packages/trader/src/Modules/MT5/Components/mt5-real-account-display.jsx
+++ packages/trader/src/Modules/MT5/Components/mt5-real-account-display.jsx
@@ -193,7 +193,7 @@ const MT5RealAccountDisplay = ({
                 'Trade major, minor, exotic currency pairs, and cryptocurrencies with Straight-Through Processing (STP) of your orders direct to the market.'
             )}
             specs={real_financial_stp_specs}
-            is_disabled={(!is_eu && is_real_financial_stp_disabled) || has_mt5_account_error}
+            is_disabled={is_real_financial_stp_disabled || has_mt5_account_error}
             is_virtual={is_virtual}
             has_real_account={has_real_account}
             toggleAccountsDialog={toggleAccountsDialog}
@@ -206,7 +206,7 @@ const MT5RealAccountDisplay = ({
         <MT5AccountCard
             key='real.financial'
             has_mt5_account={has_mt5_account}
-            is_disabled={(!is_eu && !has_real_account) || has_mt5_account_error}
+            is_disabled={!has_real_account || has_mt5_account_error}
             title={localize('Financial')}
             type={{
                 category: 'real',

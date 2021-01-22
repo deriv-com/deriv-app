ad23c16dedefbc671bc7984097714ca6856851dc
Aaron Imming <aaim@protonmail.com>
2021-01-22 12:05:13 +0800

    fix: only request new data when not already loading

--- packages/p2p/src/components/buy-sell/buy-sell-table.jsx
+++ packages/p2p/src/components/buy-sell/buy-sell-table.jsx
@@ -33,9 +33,11 @@ const BuySellTable = ({ onScroll }) => {
             reaction(
                 () => buy_sell_store.is_buy,
                 () => {
-                    buy_sell_store.setItems([]);
-                    buy_sell_store.setIsLoading(true);
-                    buy_sell_store.loadMoreItems({ startIndex: 0 });
+                    if (!buy_sell_store.is_loading) {
+                        buy_sell_store.setItems([]);
+                        buy_sell_store.setIsLoading(true);
+                        buy_sell_store.loadMoreItems({ startIndex: 0 });
+                    }
                 },
                 { fireImmediately: true }
             ),

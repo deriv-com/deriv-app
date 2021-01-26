f76817319068d447dacb7cb827d26eb3ebcac409
Mohammadreza Ghorbani <reza@firstsource.email>
2021-01-26 13:16:55 +0330

    update text

--- packages/account/src/Sections/Security/DerivPassword/change-password-form.jsx
+++ packages/account/src/Sections/Security/DerivPassword/change-password-form.jsx
@@ -20,7 +20,7 @@ const AlertBox = ({ is_dark_mode_on }) => (
             color={is_dark_mode_on ? 'colored_background' : 'prominent'}
             className='account__alert-box-text'
         >
-            <Localize i18n_default_text='We’ve upgraded our system to support a single, more secure password across all of Deriv/Binary.com. Once you’ve set a new password, you can use it to log into all your Deriv/Binary.com, and DMT5/MT5 accounts.' />
+            <Localize i18n_default_text='We’ve We’ve upgraded our system to support a single, more secure password across Deriv. Once you’ve set a new password, you can use it to log into all your Deriv and DMT5 accounts.' />
         </Text>
     </div>
 );

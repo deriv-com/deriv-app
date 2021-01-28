58778a3b318bf1fdb5051708ece6c5b55653c881
Salar Hafezi <salar@firstsource.tech>
2021-01-28 15:00:10 +0330

    Add close overlay loading to contract details

--- packages/trader/src/App/Components/Elements/ContractDrawer/market-closed-contract-overlay.jsx
+++ packages/trader/src/App/Components/Elements/ContractDrawer/market-closed-contract-overlay.jsx
@@ -1,11 +1,16 @@
+import classNames from 'classnames';
 import React from 'react';
 import { Text } from '@deriv/components';
 import { Localize } from '@deriv/translations';
 // eslint-disable-next-line import/no-useless-path-segments
 import MarketCountdownTimer from '../market-countdown-timer.jsx';
 
-const MarketClosedContractOverlay = () => (
-    <div className='contract-card__market-closed'>
+const MarketClosedContractOverlay = ({ is_market_close_overlay_loading }) => (
+    <div
+        className={classNames('contract-card__market-closed', {
+            'contract-card__market-closed--loading': is_market_close_overlay_loading,
+        })}
+    >
         <Text align='center' as='p' styles={{ color: 'var(--brand-orange)', marginBottom: '1rem' }} weight='bold'>
             <Localize i18n_default_text='Market is closed' />
         </Text>

cc3203a0cd1ec969aa7a23c6dc5ab49319202d98
sara-fs <55442058+sara-fs@users.noreply.github.com>
2021-02-01 12:26:09 +0330

    Sara / Replaced span with text on trader package (#2074)
    * Replaced span with text on contract-audit-item
    
    * Replaced span with text on contract-history
    
    * Replaced span with text on position-drawer
    
    * Replaced spans with text on position-modal
    
    * Replaced spans with text on toggle-position-mobile
    
    * Replaced spans with text on range-slider
    
    * Relaced spans with text on info-box-longcode
    
    * Replaced spans with text on mt5-info-copy
    
    * Replaced paragraphs with text on empty-portfolio-message
    
    * Replaced paragraph with text on empty-trade-history-message
    
    * Replaced spans with text on top-widgets
    
    * Replaced span with text on purchase-button
    
    * Replaced span with text on contract-info
    
    * Replaced span with text on contract-type-item
    
    * Replaced span with text on digits and digit-spot
    
    * Replaced span with text on mt5-compare-accounts-modal
    
    * Replaced span with text on statement
    
    * Replaced span with text on contract-type-info-header
    
    * Replaced span with text on amount-widget
    
    * Removed extra space
    
    * Merged with master
    
    * Merged with master
    
    * Fixed issue with text props on text components
    
    * Changed font size on contract-audit-item
    
    * Fixed issue with line height on contract audit item
    
    * Fixed issue with line height
    
    * Fixed issue with line height
    
    * Resolved conflicts
    
    Co-authored-by: sara-fs <sara.khosropour@regentmarkets.com>

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info-header.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info-header.jsx
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Icon } from '@deriv/components';
+import { Icon, Text } from '@deriv/components';
 
 const Header = ({ onClickGoBack, title }) => (
     <div className='contract-type-info__action-bar'>
         <span className='contract-type-info__icon' id='dt_contract_info_back_nav' onClick={onClickGoBack}>
             <Icon icon='IcArrowLeftBold' />
         </span>
-        <span className='contract-type-info__title'>{title}</span>
+        <Text size='s' weight='bold' color='prominent' className='contract-type-info__title'>
+            {title}
+        </Text>
     </div>
 );
 

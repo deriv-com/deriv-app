414e49db4e7264fb30fc6e79ed2b2685b1141620
matin-binary <matin@deriv.com>
2021-02-01 15:09:00 +0800

    Ignore

--- packages/dashboard/src/components/pages/about-us/about-us.tsx
+++ packages/dashboard/src/components/pages/about-us/about-us.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Text } from '@deriv/components';
-import TempButtons from 'Components/temp-buttons';
 
 const AboutUs: React.FC = () => {
     return (
@@ -8,11 +7,6 @@ const AboutUs: React.FC = () => {
             <Text as='p' size='l' color='less-prominent' align='center'>
                 About us
             </Text>
-            <div className='dw-about-us__wrapper'>
-                {Array.from(new Array(total_previews).keys()).map(key => (
-                    <Frame src={`${config_store.asset_path}/images/placeholder.png`} key={key} alt={`p${key + 1}`} />
-                ))}
-            </div>
         </div>
     );
 };

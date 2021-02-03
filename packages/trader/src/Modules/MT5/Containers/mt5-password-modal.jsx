ddd8e15a8aac22d1aa516c565c6ebf9daee9e9f5
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2021-02-03 08:09:19 +0330

    Keep the backward compatibility

--- packages/trader/src/Modules/MT5/Containers/mt5-password-modal.jsx
+++ packages/trader/src/Modules/MT5/Containers/mt5-password-modal.jsx
@@ -98,7 +98,7 @@ const MT5PasswordForm = props => (
                                 )}
                             </PasswordMeter>
                         </div>
-                        {props.is_real_financial_stp && props.is_labuan && (
+                        {props.is_real_financial_stp && !props.is_bvi && (
                             <div className='dc-modal__container_mt5-password-modal__description'>
                                 <Localize i18n_default_text='Your MT5 Financial STP account will be opened through Deriv (FX) Ltd. All trading in this account is subject to the regulations and guidelines of the Labuan Financial Service Authority (LFSA). None of your other accounts, including your Deriv account, is subject to the regulations and guidelines of the Labuan Financial Service Authority (LFSA).' />
                             </div>
@@ -243,10 +243,6 @@ const MT5PasswordModal = ({
     const [password, setPassword] = React.useState('');
     const [is_submitting, setIsSubmitting] = React.useState(false); // TODO handle this better
 
-    const is_labuan = React.useMemo(() => {
-        return landing_companies?.mt_financial_company?.financial_stp.shortcode === 'labuan';
-    }, [landing_companies]);
-
     const is_bvi = React.useMemo(() => {
         return landing_companies?.mt_financial_company?.financial_stp.shortcode === 'bvi';
     }, [landing_companies]);
@@ -338,7 +334,6 @@ const MT5PasswordModal = ({
                         />
                     ) : (
                         <MT5PasswordForm
-                            is_labuan={is_labuan}
                             is_bvi={is_bvi}
                             is_submitting={is_submitting}
                             account_title={account_title}
@@ -371,6 +366,7 @@ const MT5PasswordModal = ({
                         />
                     ) : (
                         <MT5PasswordForm
+                            is_bvi={is_bvi}
                             is_submitting={is_submitting}
                             account_title={account_title}
                             closeModal={closeModal}

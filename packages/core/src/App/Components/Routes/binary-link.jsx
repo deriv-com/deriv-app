426e837f15ddb917498a798d94e4808b83c200f8
Amir Ghadimi <amir.ghadimi@firstsource.tech>
2021-02-14 18:16:54 +0330

    fix test issues

--- packages/core/src/App/Components/Routes/binary-link.jsx
+++ packages/core/src/App/Components/Routes/binary-link.jsx
@@ -11,7 +11,8 @@ import getRoutesConfig from '../../Constants/routes-config';
 // when binary link is imported into components present in routes config
 // or into their descendants
 const BinaryLink = ({ active_class, to, children, href, has_error, setError, ...props }) => {
-    const { is_dashboard } = React.useContext(PlatformContext);
+    const platform_context = React.useContext(PlatformContext);
+    const is_dashboard = platform_context?.is_dashboard;
     const path = normalizePath(to);
     const route = findRouteByPath(path, getRoutesConfig({ is_dashboard }));
 

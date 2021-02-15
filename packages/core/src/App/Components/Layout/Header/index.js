cf37dd7cfc89f454a459865245bd30f1015a0870
bahar <bahar@firstsource.tech>
2021-02-15 08:30:25 +0330

    Revert "add_platforms_dashboard_header"
    This reverts commit 5a56b9280c0b425c076b8d56d7b9219eb339939b.

--- packages/core/src/App/Components/Layout/Header/index.js
+++ packages/core/src/App/Components/Layout/Header/index.js
@@ -1,4 +1,3 @@
 export * from './menu-links.jsx';
 export * from './account-actions.jsx';
-export ToggleNotifications from './toggle-notifications.jsx';
 export PlatformSwitcher from './platform-switcher.jsx';
--- packages/core/src/App/Containers/Layout/header/header.jsx
+++ packages/core/src/App/Containers/Layout/header/header.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { PlatformContext, getDashboardPlatformHeader } from '@deriv/shared';
-import { connect } from 'Stores/connect';
+import { PlatformContext } from '@deriv/shared';
 import DefaultHeader from './default-header.jsx';
-import DashboardPlatformHeader from './dashboard-platform-header.jsx';
 import DashboardHeader from './dashboard-header.jsx';
 
-const Header = ({ app_routing_history }) => {
+const Header = () => {
     const { is_dashboard } = React.useContext(PlatformContext);
-
-    if (is_dashboard) {
-        if (getDashboardPlatformHeader(app_routing_history)) {
-            return <DashboardPlatformHeader />;
-        }
-        return <DashboardHeader />;
-    }
-    return <DefaultHeader />;
+    return is_dashboard ? <DashboardHeader /> : <DefaultHeader />;
 };
 
-export default connect(({ common }) => ({
-    app_routing_history: common.app_routing_history,
-}))(withRouter(Header));
+export default Header;

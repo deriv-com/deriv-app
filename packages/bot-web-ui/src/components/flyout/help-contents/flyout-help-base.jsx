1d8dca0a82ee437cf477369102a4b80c021278e3
nikolai-deriv <nikolai@deriv.com>
2021-02-15 15:04:57 +0300

    added last component check

--- packages/bot-web-ui/src/components/flyout/help-contents/flyout-help-base.jsx
+++ packages/bot-web-ui/src/components/flyout/help-contents/flyout-help-base.jsx
@@ -51,7 +51,12 @@ const HelpBase = ({
                         const { text } = help_string;
                         switch (type) {
                             case help_content_types.TEXT:
-                                return <FlyoutText key={`${block_type}_${index}`} text={text[text_count++]} />;
+                                return (
+                                    <FlyoutText
+                                        key={`${block_type}_${index}`}
+                                        text={text[text_count < text.length && text_count++]}
+                                    />
+                                );
                             case help_content_types.VIDEO:
                                 return <FlyoutVideo key={`${block_type}_${index}`} url={url} />;
                             case help_content_types.IMAGE:

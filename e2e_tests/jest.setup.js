91f022bbf435a489aaff5a4116b9bf3a42ad4046
Mohammadreza Ghorbani <reza@firstsource.email>
2021-02-24 11:45:35 +0330

    Add jest-playwright-preset

--- e2e_tests/jest.setup.js
+++ e2e_tests/jest.setup.js
@@ -1,4 +1,4 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+require('core-js/stable');
+require('regenerator-runtime/runtime');
 
 jest.setTimeout(process.env.TEST_TIMEOUT || 30000);

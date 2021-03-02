f8b0672913e54620377ca3fae82e6cefd18e766d
xenia <kseniya@deriv.com>
2021-03-02 17:58:26 +0300

    add: withdrawal details to format response

--- packages/trader/src/Stores/Modules/Statement/Helpers/format-response.js
+++ packages/trader/src/Stores/Modules/Statement/Helpers/format-response.js
@@ -16,6 +16,7 @@ export const formatStatementTransaction = (transaction, currency, active_symbols
     const display_name = shortcode
         ? getSymbolDisplayName(active_symbols, getMarketInformation(shortcode).underlying)
         : '';
+    const withdrawal_details = transaction.withdrawal_details;
 
     return {
         action: localize(toTitleCase(transaction.action_type) /* localize-ignore */), // handled in static_strings_app.js: 'Buy', 'Sell', 'Deposit', 'Withdrawal'
@@ -32,5 +33,6 @@ export const formatStatementTransaction = (transaction, currency, active_symbols
         action_type: transaction.action_type,
         purchase_time: transaction.purchase_time,
         transaction_time: transaction.transaction_time,
+        ...(withdrawal_details && { withdrawal_details }),
     };
 };

180b31a69f90c65ba882c9f1cde527bb886ebafc
mustofa-binary <mustofa@binary.com>
2021-03-03 15:55:06 +0800

    feat: fix logic poi poa description enabled

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
@@ -21,6 +21,7 @@ const ProofOfIdentityContainer = ({
     refreshNotifications,
     removeNotificationMessage,
     onStateChange,
+    is_description_enabled = true,
     is_mx_mlt,
     height,
     redirect_button,
@@ -151,7 +152,7 @@ const ProofOfIdentityContainer = ({
         }
     }, [createVerificationConfig, previous_account_status, account_status]);
 
-    const { has_poa, is_unwelcome, allow_document_upload } = verification_status;
+    const { needs_poa, is_unwelcome, allow_document_upload } = verification_status;
     const is_rejected = identity_status_key === onfido_status_codes.rejected;
     const has_rejected_reasons = !!rejected_reasons_key.length && is_rejected;
 
@@ -172,9 +173,10 @@ const ProofOfIdentityContainer = ({
             documents_supported={documents_supported}
             status={status}
             onfido_service_token={onfido_service_token}
-            has_poa={has_poa}
+            needs_poa={needs_poa}
             height={height ?? null}
             handleComplete={handleComplete}
+            is_description_enabled={is_description_enabled}
             redirect_button={redirect_button}
         />
     );

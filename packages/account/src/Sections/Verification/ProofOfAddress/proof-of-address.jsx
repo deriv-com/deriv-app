ee0f3819f46c589883e91fca850ca534edace3f6
mustofa-binary <mustofa@binary.com>
2021-03-04 14:21:11 +0800

    fix: redirect poa

--- packages/account/src/Sections/Verification/ProofOfAddress/proof-of-address.jsx
+++ packages/account/src/Sections/Verification/ProofOfAddress/proof-of-address.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { getPlatformRedirect } from '@deriv/shared';
 import { connect } from 'Stores/connect';
 import DemoMessage from 'Components/demo-message';
 import ProofOfAddressContainer from './proof-of-address-container.jsx';
 
-const ProofOfAddress = ({ is_virtual, is_mx_mlt, refreshNotifications }) => {
+const ProofOfAddress = ({
+    is_virtual,
+    is_mx_mlt,
+    refreshNotifications,
+    should_allow_authentication,
+    app_routing_history,
+    routeBackInApp,
+}) => {
+    const history = useHistory();
+    React.useEffect(() => {
+        if (!should_allow_authentication) {
+            const from_platform = getPlatformRedirect(app_routing_history);
+            routeBackInApp(history, [from_platform]);
+        }
+    }, []);
+
     if (is_virtual) return <DemoMessage />;
 
     return <ProofOfAddressContainer is_mx_mlt={is_mx_mlt} refreshNotifications={refreshNotifications} />;
 };
 
-export default connect(({ client }) => ({
+export default connect(({ client, common }) => ({
+    app_routing_history: common.app_routing_history,
     is_mx_mlt: client.landing_company_shortcode === 'iom' || client.landing_company_shortcode === 'malta',
     is_virtual: client.is_virtual,
     refreshNotifications: client.refreshNotifications,
+    routeBackInApp: common.routeBackInApp,
+    should_allow_authentication: client.should_allow_authentication,
 }))(ProofOfAddress);
--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity.js
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity.js
@@ -10,17 +10,18 @@ export const onfido_status_codes = {
     suspected: 'suspected',
 };
 
-export const getIdentityStatus = (identity, needs_verification, is_mlt_mx) => {
+export const getIdentityStatus = (identity, needs_verification, is_mlt_mx, allow_document_upload) => {
     const { status } = identity;
     const onfido_unsupported = !identity.services.onfido.is_country_supported;
     const submissions_allowed = needs_verification?.includes('identity');
 
+    if (onfido_unsupported && (submissions_allowed || allow_document_upload)) return onfido_status_codes.unsupported;
     if (!submissions_allowed) {
         if (status === 'none' && is_mlt_mx) {
             return onfido_status_codes.not_required;
         }
         return onfido_status_codes[status];
     }
-    if (onfido_unsupported) return onfido_status_codes.unsupported;
+
     return onfido_status_codes.onfido;
 };

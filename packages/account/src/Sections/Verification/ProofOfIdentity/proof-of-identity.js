cb6fcfaa8edff5fc161a0fdb868428bc4511ffa4
mustofa-binary <mustofa@binary.com>
2021-03-04 13:49:25 +0800

    fix: space and unsupported

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity.js
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity.js
@@ -15,12 +15,12 @@ export const getIdentityStatus = (identity, needs_verification, is_mlt_mx) => {
     const onfido_unsupported = !identity.services.onfido.is_country_supported;
     const submissions_allowed = needs_verification?.includes('identity');
 
-    if (onfido_unsupported) return onfido_status_codes.unsupported;
     if (!submissions_allowed) {
         if (status === 'none' && is_mlt_mx) {
             return onfido_status_codes.not_required;
         }
         return onfido_status_codes[status];
     }
+    if (onfido_unsupported) return onfido_status_codes.unsupported;
     return onfido_status_codes.onfido;
 };

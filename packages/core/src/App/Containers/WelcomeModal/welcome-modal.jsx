073d178c2d8b84fd3aa49be155197b6cf18ce524
zaki-binary <zaki@binary.com>
2021-03-08 15:53:16 +0800

    feat: update welcome design and add dxtrade platform

--- packages/core/src/App/Containers/WelcomeModal/welcome-modal.jsx
+++ packages/core/src/App/Containers/WelcomeModal/welcome-modal.jsx
@@ -8,13 +8,10 @@ import { localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 
 const WelcomeColumn = ({
-    button_text,
     className,
     description,
     footer_text,
     icons,
-    is_hovered,
-    onButtonClick,
     onMouseEnter,
     onMouseLeave,
     platforms,
@@ -39,32 +36,39 @@ const WelcomeColumn = ({
                 </Text>
                 <div className='welcome-column__platforms__container'>
                     {platforms.map((platform, index) => (
-                        <div className='welcome-column__platform' key={index}>
-                            <Icon className='welcome-column__platform__icon' icon={platform.icon} size={32} />
-                            <h3 className='welcome-column__platform__title'>{platform.title}</h3>
-                            <Text
-                                as='p'
-                                color='less-prominent'
-                                size='xxs'
-                                align='left'
-                                className='welcome-column__platform__description'
+                        <React.Fragment key={index}>
+                            <div className='welcome-column__platform'>
+                                <Icon className='welcome-column__platform__icon' icon={platform.icon} size={32} />
+                                <h3 className='welcome-column__platform__title'>{platform.title}</h3>
+                                <Text
+                                    as='p'
+                                    color='less-prominent'
+                                    size='xxs'
+                                    align='left'
+                                    className='welcome-column__platform__description'
+                                >
+                                    {platform.description}
+                                </Text>
+                            </div>
+                            <div
+                                className={classNames('welcome-column__platform-footer', {
+                                    'welcome-column__platform-footer--has-separator': index === 0,
+                                })}
                             >
-                                {platform.description}
-                            </Text>
-                        </div>
+                                <Button
+                                    className='welcome-column__platform-button'
+                                    onClick={platform.onButtonClick}
+                                    secondary
+                                    large
+                                >
+                                    {platform.button_text}
+                                </Button>
+                            </div>
+                        </React.Fragment>
                     ))}
                 </div>
             </div>
             <div className='welcome-column__footer'>
-                <Button
-                    className='welcome-column__button'
-                    onClick={onButtonClick}
-                    large
-                    primary={is_hovered || isMobile()}
-                    secondary={!(is_hovered || isMobile())}
-                >
-                    {button_text}
-                </Button>
                 {footer_text && (
                     <Text as='p' size='xxs' color='less-prominent' className='welcome-column__footer__text'>
                         {footer_text}
@@ -75,15 +79,11 @@ const WelcomeColumn = ({
     );
 };
 
-const footer_text = localize(
-    'Whatever you choose, you will always be able to access Margin or Options & Multipliers at any time'
-);
+const footer_text = localize('You can switch between CFDs, digital options, and multipliers at any time.');
 
 WelcomeColumn.propTypes = {
-    button_text: PropTypes.string,
     description: PropTypes.string,
     icons: PropTypes.array,
-    onButtonClick: PropTypes.func,
     platforms: PropTypes.arrayOf(PropTypes.object),
     title: PropTypes.string,
 };
@@ -107,20 +107,27 @@ const WelcomeModal = ({ toggleWelcomeModal, history }) => {
     const Cards = [
         <WelcomeColumn
             key={0}
-            button_text={localize('Start here')}
             className='welcome-column--left'
             description={localize('Trade with leverage and low spreads for better returns on successful trades.')}
             icons={['IcPercentSolid']}
             is_hovered={hovered === 'left'}
-            onButtonClick={() => switchPlatform(routes.mt5)}
             platforms={[
                 {
                     icon: 'IcBrandDmt5',
-                    title: localize('Deriv MetaTrader 5 (DMT5)'),
-                    description: localize('All-in-one multi-asset trading platform for CFDs & margin trading.'),
+                    title: localize('Deriv MT5'),
+                    description: localize('Trade CFDs on the platform of choice for professional traders worldwide.'),
+                    onButtonClick: () => switchPlatform(routes.mt5),
+                    button_text: localize('Trade on MT5'),
+                },
+                {
+                    icon: 'IcBrandDxtrade',
+                    title: localize('Deriv DXtrade'),
+                    description: localize('Trade CFDs on a customisable, easy-to-use trading platform.'),
+                    onButtonClick: () => switchPlatform(routes.dxtrade),
+                    button_text: localize('Trade on DXtrade'),
                 },
             ]}
-            title={localize('Margin (MetaTrader 5)')}
+            title={localize('CFDs')}
             onMouseEnter={() => {
                 setHovered('left');
             }}
@@ -131,10 +138,9 @@ const WelcomeModal = ({ toggleWelcomeModal, history }) => {
         />,
         <WelcomeColumn
             key={1}
-            button_text={localize('Start here')}
             className='welcome-column--right'
             description={localize(
-                'Earn fixed payouts with options, or trade multipliers to combine the upside of margin trading with the simplicity of options.'
+                'Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.'
             )}
             icons={['IcUpDownSolid', 'IcCrossSolid']}
             is_hovered={hovered === 'right'}
@@ -143,12 +149,16 @@ const WelcomeModal = ({ toggleWelcomeModal, history }) => {
                 {
                     icon: 'IcBrandDtrader',
                     title: localize('DTrader'),
-                    description: localize('Start trading with a powerful, yet easy-to-use platform.'),
+                    description: localize('Our flagship options and multipliers trading platform.'),
+                    onButtonClick: () => switchPlatform(routes.trade),
+                    button_text: localize('Trade on DTrader'),
                 },
                 {
                     icon: 'IcBrandDbot',
                     title: localize('DBot'),
-                    description: localize('Automate your trading ideas without coding.'),
+                    description: localize('Automate your trading, no coding needed.'),
+                    onButtonClick: () => switchPlatform(routes.bot),
+                    button_text: localize('Trade on DBot'),
                 },
             ]}
             title={localize('Options & Multipliers')}

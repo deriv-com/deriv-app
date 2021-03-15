6aeb8c1558cba152a608954bd3456de176902441
Akmal Djumakhodjaev <akmal@binary.com>
2021-03-15 16:52:12 +0800

    Added notes existance check

--- packages/cashier/src/Containers/deposit.jsx
+++ packages/cashier/src/Containers/deposit.jsx
@@ -33,7 +33,7 @@ const DepositeSideNote = () => {
         */
     ];
     const side_note_title =
-        notes.length > 1 ? <Localize i18n_default_text='Notes' /> : <Localize i18n_default_text='Note' />;
+        notes?.length > 1 ? <Localize i18n_default_text='Notes' /> : <Localize i18n_default_text='Note' />;
 
     return <SideNote has_bullets notes={notes} title={side_note_title} />;
 };

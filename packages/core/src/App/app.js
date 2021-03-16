62cb751640b6a948273df95ba50e2d3c9f3e0160
mustofa-binary <mustofa@regentmarkets.com>
2021-03-16 14:01:27 +0800

    remove pushwoosh

--- packages/core/src/App/app.js
+++ packages/core/src/App/app.js
@@ -49,7 +49,7 @@ const initStore = notification_messages => {
     // OutdatedBrowser.init(root_store);!
     root_store.client.init();
     root_store.ui.init(notification_messages);
-    root_store.pushwoosh.init();
+    // root_store.pushwoosh.init();
 
     return root_store;
 };

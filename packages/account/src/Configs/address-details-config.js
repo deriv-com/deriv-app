259eff6c425aa5c92772e6f2e2d09cd583b351c4
reza-fs <53177016+reza-fs@users.noreply.github.com>
2021-03-18 08:13:45 +0330

    Fix Validation message for address_line_1 (#2782)
    Co-authored-by: Mohammadreza Ghorbani <reza@firstsource.email>
    Co-authored-by: Aaron Imming <aaim@protonmail.com>
    Co-authored-by: Mustofa Ghaleb Amami (Front End) <52783019+mustofa-binary@users.noreply.github.com>

--- packages/account/src/Configs/address-details-config.js
+++ packages/account/src/Configs/address-details-config.js
@@ -11,7 +11,7 @@ const address_details_config = ({ account_settings, is_svg }) => {
             supported_in: ['svg', 'iom', 'malta', 'maltainvest'],
             default_value: account_settings.address_line_1 ?? '',
             rules: [
-                ['req', localize('Address line 1 is required')],
+                ['req', localize('First line of address is required')],
                 ['address', localize('Address is not in a proper format')],
                 ['length', localize('This should not exceed {{max}} characters.', { max: 70 }), { max: 70 }],
                 ['po_box', getErrorMessages().po_box()],

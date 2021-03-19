1a28cb335d16b4ebea603f7e17e09ad6c3d8cf3c
Daniil <daniil@deriv.com>
2021-03-19 13:13:42 +0300

    feat: implemented to mobile, fixed alignment

--- packages/components/src/components/data-table/table-row-info.jsx
+++ packages/components/src/components/data-table/table-row-info.jsx
@@ -24,13 +24,17 @@ const TableRowInfo = ({ replace, is_footer, cells, className }) => {
 
     const onClickCopy = event => {
         event.stopPropagation();
-        navigator.clipboard.writeText(replace.message).then(() => {
+        navigator.clipboard.writeText(replace.message.match(/(([a-zA-Z0-9]{44}))/g)[0]).then(() => {
             onCopySuccess();
         });
     };
 
     const copy_to_clipboard = (
-        <Icon icon={is_copied ? 'IcCheckmarkCircle' : 'IcCopy'} onClick={event => onClickCopy(event)} />
+        <Icon
+            icon={is_copied ? 'IcCheckmarkCircle' : 'IcCopy'}
+            className='table__row--icon'
+            onClick={event => onClickCopy(event)}
+        />
     );
 
     return (
@@ -42,12 +46,10 @@ const TableRowInfo = ({ replace, is_footer, cells, className }) => {
                 <ThemedScrollbars height='80px'>
                     <div>
                         {replace?.component ?? (
-                            <div className='table__row--info'>
-                                <Text as='p' size='xs' className='statement__row--detail-text'>
-                                    {replace.message}
-                                </Text>
-                                {copy_to_clipboard}
-                            </div>
+                            <Text as='p' size='xs' className='statement__row--detail-text' align='center'>
+                                {replace.message}
+                                {replace.message.includes(': ') ? copy_to_clipboard : null}
+                            </Text>
                         )}
                     </div>
                 </ThemedScrollbars>

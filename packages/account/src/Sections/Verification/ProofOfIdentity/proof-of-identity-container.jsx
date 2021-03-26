66f5127e4e52dbfaa675a54a4aed09c3a0718882
Habib-Deriv <shuvohabib@gmail.com>
2021-03-26 16:01:49 +0600

    add filter function for checking

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
@@ -156,8 +156,8 @@ const ProofOfIdentityContainer = ({
 
     const { needs_poa, is_unwelcome, allow_document_upload } = verification_status;
 
-    const rejectedStatus = [onfido_status_codes.rejected, onfido_status_codes.suspected];
-    const is_rejected = rejectedStatus.map(status => onfido_status_codes[status]).includes(identity_status_key);
+    const rejectionStatus = [onfido_status_codes.rejected, onfido_status_codes.suspected];
+    const is_rejected = rejectionStatus.map(status => onfido_status_codes[status]).includes(identity_status_key);
 
     const has_rejected_reasons = !!rejected_reasons_key.length && is_rejected;
 

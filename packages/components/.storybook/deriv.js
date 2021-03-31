f9860f88b9dda01d3281f92451ecc97bef268d5e
zaki-binary <zaki@binary.com>
2021-03-31 16:46:26 +0800

    chore: trigger ci

--- packages/components/.storybook/deriv.js
+++ packages/components/.storybook/deriv.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming/create';
 
 export default create({
     base: 'light',
-    brandTitle: 'Deriv Style Guides',
+    brandTitle: 'Deriv Style Guide',
     brandUrl: 'https://app.deriv.com',
     brandImage: 'https://static.deriv.com/logos/deriv-horizontal-web-dark.png',
 });

f8c60408ab09226d89d7d929416deb50d3b233cf
mehrzad-fs <60640951+mehrzad-fs@users.noreply.github.com>
2021-04-02 10:53:44 +0430

    Mehrzad/ Dashboard account (#2481)
    * use PlatformContext for dashboard routing
    
    * dashboard account
    
    * update file uploader container
    
    * update dashboard account
    
    * update dashboard account tab
    
    * uadd dashboard primary button
    
    * update is_dashboard
    
    * remove iis_dashboard from components
    
    * init
    
    * init
    
    * update responsive design dashboard setting
    
    * dashboard account review update
    
    * revert icon.jsx
    
    * update file uploader container localize
    
    * update financial assessment
    
    * dashboard account vertical tab
    
    * dashboard account personal details
    
    * dashboard account personal details
    
    * dashboard account deriv password
    
    * dashboard account financial assesment
    
    * dashboard account connected app
    
    * dashboard account deriv password
    
    * dashboard  account all field required
    
    * update rows height on connected apps
    
    * dashboard account limits
    
    * dashboard account self exclusion
    
    * dashboard aaccount deriv password
    
    * dashboard account deriv password
    
    * dashboard account POA
    
    * dashboard acccount personal details
    
    * dashboard account poa
    
    * dashboard account poa
    
    * dashboard account self exclusion
    
    * dashboard account poi
    
    * dashboard account merged
    
    * dashboard account  login history
    
    * dashboard account message container issue
    
    * update demo  message
    
    * update self exclusion
    
    Co-authored-by: Mustofa Ghaleb Amami (Front End) <52783019+mustofa-binary@users.noreply.github.com>

--- packages/account/src/Components/icon-with-message/icon-with-message.jsx
+++ packages/account/src/Components/icon-with-message/icon-with-message.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 import * as React from 'react';
 import { Icon, Text } from '@deriv/components';
 
-const IconWithMessage = ({ icon, message }) => {
+const IconWithMessage = ({ icon, message, more_content }) => {
     return (
         <div className='da-icon-with-message'>
             <Icon icon={icon} size={128} />
             <Text className='da-icon-with-message__text' as='p' color='general' size='s' line_height='m' weight='bold'>
                 {message}
             </Text>
+            {more_content}
         </div>
     );
 };

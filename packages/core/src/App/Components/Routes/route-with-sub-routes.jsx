2d8411d47d9ec0c4e2ea3829b3930be14cf16c15
yashim-deriv <yashim@deriv.com>
2021-04-06 12:10:46 +0800

    fix: checking was done with wrong location prop

--- packages/core/src/App/Components/Routes/route-with-sub-routes.jsx
+++ packages/core/src/App/Components/Routes/route-with-sub-routes.jsx
@@ -28,7 +28,7 @@ const RouteWithSubRoutes = route => {
             }
             result = <Redirect to={to} />;
         } else if (is_valid_route && route.is_authenticated && !route.is_logged_in && !route.is_logging_in) {
-            const url_search_params = new URLSearchParams(location.search);
+            const url_search_params = new URLSearchParams(this.props.location.search);
             if (url_search_params.get('is_redirecting') !== 'true') {
                 redirectToLogin(route.is_logged_in, getLanguage());
             }

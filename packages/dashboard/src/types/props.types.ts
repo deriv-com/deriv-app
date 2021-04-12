fc4e0e02978f7b956751d338d12df9626fc17331
Akmal Djumakhodjaev <akmal@binary.com>
2021-04-12 19:38:12 +0800

    Akmal /  Added dashboard routes & responsive sidebar (#2599)
    * Fixed inability to change tabs from outside
    
    * Temp commit
    
    * Temp commit
    
    * Temp commit
    
    * Added dashboard routes & resposive sidebar
    
    * Remove logs
    
    * Revert payment agent list change
    
    * Fix eslint
    
    * Added new routes, applied suggestions
    
    * Added dynamic routing for dashboard
    
    * Fixed test issues
    
    * Fixed non-null assertion
    
    * Replaced labels with getTitle
    
    * Fixed cfds typo
    
    * Apply suggestions from code review
    
    Co-authored-by: sean-binary <72253841+sean-binary@users.noreply.github.com>
    
    * Fix: conflict
    
    * Fix: failed binary link logic
    
    * Remove is_deriv_crypto
    
    * Updated trade icon according to a new design
    
    Co-authored-by: sean-binary <72253841+sean-binary@users.noreply.github.com>
    Co-authored-by: Mustofa Ghaleb Amami (Front End) <52783019+mustofa-binary@users.noreply.github.com>

--- packages/dashboard/src/types/props.types.ts
+++ packages/dashboard/src/types/props.types.ts
@@ -36,10 +36,37 @@ export type TConfigProps = {
 
 export type TRoutesProps = {
     home: string;
+    my_apps: string;
     about_us: string;
     explore: string;
     resources: string;
+
+    market_commodities: string;
+    market_forex: string;
+    market_stock: string;
+    market_synthetic: string;
+    markets: string;
+
+    platform_binary_bot: string;
+    platform_dbot: string;
+    platform_dmt5: string;
+    platform_dmt5_financial: string;
+    platform_dmt5_financial_stp: string;
     platform_dmt5_synthetic: string;
+    platform_dtrader: string;
+    platform_smarttrader: string;
+    platforms: string;
+
+    trade_type_cfds: string;
+    trade_type_multipliers: string;
+    trade_type_options: string;
+    trade_types: string;
+
+    wallet_bank_wire: string;
+    wallet_cards: string;
+    wallet_crypto: string;
+    wallet_ewallet: string;
+    wallets: string;
 };
 
 export interface TUIProps {

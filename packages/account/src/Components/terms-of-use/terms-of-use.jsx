511129d50248fca57ac2571f7e41db7685ae14e9
Amir Ghadimi <amir.ghadimi@firstsource.tech>
2021-04-12 16:55:30 +0430

    fix: on mobile correct the width of the scroll container

--- packages/account/src/Components/terms-of-use/terms-of-use.jsx
+++ packages/account/src/Components/terms-of-use/terms-of-use.jsx
@@ -1,5 +1,6 @@
 import { Field, Formik } from 'formik';
 import React from 'react';
+import cn from 'classnames';
 import {
     Div100vhContainer,
     Modal,
@@ -46,42 +47,51 @@ const TermsOfUse = ({
             }}
         >
             {({ handleSubmit, values, isSubmitting }) => (
-                <AutoHeightWrapper default_height={200}>
-                    {({ setRef, height }) => (
+                <AutoHeightWrapper default_height={380} height_offset={isDesktop() ? 81 : null}>
+                    {({ setRef }) => (
                         <form ref={setRef} onSubmit={handleSubmit}>
                             <Div100vhContainer
-                                className='terms-of-use'
+                                className='details-form'
                                 height_offset={is_dashboard ? '242px' : '110px'}
                                 is_disabled={isDesktop()}
                             >
-                                <ThemedScrollbars autoHide={!(window.innerHeight < 890)} height={height - 72}>
-                                    <BrokerSpecificMessage target={real_account_signup_target} />
-                                    <Hr />
-                                    <SharedMessage />
-                                    <Field
-                                        component={CheckboxField}
-                                        className='terms-of-use__checkbox'
-                                        name='agreed_tos'
-                                        id='agreed_tos'
-                                        label={localize(
-                                            'I am not a PEP, and I have not been a PEP in the last 12 months.'
-                                        )}
-                                    />
-                                    <Hr />
-                                    <Field
-                                        component={CheckboxField}
-                                        className='terms-of-use__checkbox'
-                                        name='agreed_tnc'
-                                        id='agreed_tnc'
-                                        label={
-                                            <Localize
-                                                i18n_default_text='I agree to the <0>terms and conditions</0>.'
-                                                components={[
-                                                    <StaticUrl key={0} className='link' href='/terms-and-conditions' />,
-                                                ]}
-                                            />
-                                        }
-                                    />
+                                <ThemedScrollbars>
+                                    <div
+                                        className={cn('details-form__elements', 'terms-of-use')}
+                                        style={{ paddingBottom: isDesktop() ? 'unset' : null }}
+                                    >
+                                        <BrokerSpecificMessage target={real_account_signup_target} />
+                                        <Hr />
+                                        <SharedMessage />
+                                        <Field
+                                            component={CheckboxField}
+                                            className='terms-of-use__checkbox'
+                                            name='agreed_tos'
+                                            id='agreed_tos'
+                                            label={localize(
+                                                'I am not a PEP, and I have not been a PEP in the last 12 months.'
+                                            )}
+                                        />
+                                        <Hr />
+                                        <Field
+                                            component={CheckboxField}
+                                            className='terms-of-use__checkbox'
+                                            name='agreed_tnc'
+                                            id='agreed_tnc'
+                                            label={
+                                                <Localize
+                                                    i18n_default_text='I agree to the <0>terms and conditions</0>.'
+                                                    components={[
+                                                        <StaticUrl
+                                                            key={0}
+                                                            className='link'
+                                                            href='/terms-and-conditions'
+                                                        />,
+                                                    ]}
+                                                />
+                                            }
+                                        />
+                                    </div>
                                 </ThemedScrollbars>
                             </Div100vhContainer>
                             <Modal.Footer has_separator is_bypassed={isMobile()}>

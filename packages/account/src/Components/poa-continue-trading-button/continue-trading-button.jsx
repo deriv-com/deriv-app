b5cf3e48f985e2c509ff041fa31e0850df1a762f
Sean Ho <seanho96@yahoo.com>
2021-04-14 14:18:56 +0800

    fix: poi poa CTA button margins

--- packages/account/src/Components/poa-continue-trading-button/continue-trading-button.jsx
+++ packages/account/src/Components/poa-continue-trading-button/continue-trading-button.jsx
@@ -3,7 +3,7 @@ import { localize } from '@deriv/translations';
 import React from 'react';
 
 export const ContinueTradingButton = () => (
-    <ButtonLink to='/'>
+    <ButtonLink className='account-management__button' to='/'>
         <Text className='dc-btn__text' as='p' weight='bold'>
             {localize('Continue trading')}
         </Text>

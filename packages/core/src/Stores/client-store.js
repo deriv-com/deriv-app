8ddd0fc3628b77fd5d866f3480657c24f684d59c
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2021-04-16 15:43:33 +0200

    display checkbox only for EU residents

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1696,7 +1696,6 @@ export default class ClientStore extends BaseStore {
     onSignup({ password, residence, email_consent }, cb) {
         if (!this.verification_code.signup || !password || !residence) return;
         email_consent = email_consent ? 1 : 0;
-
         // Currently the code doesn't reach here and the console log is needed for debugging.
         // TODO: remove console log when AccountSignup component and validation are ready
         WS.newAccountVirtual(

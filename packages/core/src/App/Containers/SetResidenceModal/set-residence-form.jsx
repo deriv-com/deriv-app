9446a09154da77a1c44ae402b48f398c10ab7bb6
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-04-19 11:29:42 +0430

    input id added to auto complete component

--- packages/core/src/App/Containers/SetResidenceModal/set-residence-form.jsx
+++ packages/core/src/App/Containers/SetResidenceModal/set-residence-form.jsx
@@ -38,7 +38,7 @@ const SetResidenceForm = ({
                             <Autocomplete
                                 {...field}
                                 autoComplete='off'
-                                id='signup_residence_select'
+                                input_id='signup_residence_select'
                                 className={`${class_prefix}__residence-field`}
                                 type='text'
                                 label={localize('Choose country')}

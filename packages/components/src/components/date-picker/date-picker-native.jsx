640e90f8e721e7d9986b0465c9355731400a1528
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2021-04-20 11:48:46 +0430

    fix birth input color in dark theme

--- packages/components/src/components/date-picker/date-picker-native.jsx
+++ packages/components/src/components/date-picker/date-picker-native.jsx
@@ -51,7 +51,7 @@ const Native = ({
                 {value && (
                     <Text
                         size='xs'
-                        styles={{ color: 'inherit' }}
+                        styles={{ color: 'var(--text-prominent)' }}
                         className={classNames('dc-datepicker__display-text', {
                             'dc-datepicker__display-text--disabled': disabled,
                         })}

2b09ce76f5a8fa72395678b82c31dbd3727ecd9c
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-04-21 11:48:49 +0430

    useless import removed

--- packages/core/src/App/Containers/RealAccountSignup/add-or-manage-accounts.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/add-or-manage-accounts.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Tabs, ThemedScrollbars } from '@deriv/components';
 import { localize } from '@deriv/translations';
-import { isDesktop, isMobile, website_name } from '@deriv/shared';
+import { isDesktop, isMobile } from '@deriv/shared';
 import { WS } from 'Services';
 import { connect } from 'Stores/connect';
 import AddCryptoCurrency from './add-crypto-currency.jsx';

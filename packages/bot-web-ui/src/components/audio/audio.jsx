180e0db122387ee259205024b2344172654b3619
Farrah Mae Ochoa <farrahmaeochoa@Farrahs-MacBook-Pro.local>
2021-04-25 13:50:02 +0400

    Commit audio file

--- packages/bot-web-ui/src/components/audio/audio.jsx
+++ packages/bot-web-ui/src/components/audio/audio.jsx
@@ -6,7 +6,6 @@ const Audio = () => (
         <audio id='earned-money' src={`${__webpack_public_path__}media/coins.ogg`} autostart='false' />
         <audio id='job-done' src={`${__webpack_public_path__}media/job-done.ogg`} autostart='false' />
         <audio id='error' src={`${__webpack_public_path__}media/out-of-bounds.ogg`} autostart='false' />
-        <audio id='severe-error' src={`${__webpack_public_path__}media/i-am-being-serious.ogg`} autostart='false' />
     </>
 );
 

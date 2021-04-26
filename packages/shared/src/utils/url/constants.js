2bb6d422b15da3d6f5fef3b241ebdc82ae321b69
Habib-Deriv <shuvohabib@gmail.com>
2021-04-26 23:19:16 +0600

    add dynamic url for storage

--- packages/shared/src/utils/url/constants.js
+++ packages/shared/src/utils/url/constants.js
@@ -3,7 +3,7 @@ const isBrowser = () => typeof window !== 'undefined';
 const deriv_com_url = 'deriv.com';
 const deriv_me_url = 'deriv.me';
 
-const supported_domains = [deriv_com_url, deriv_me_url, 'localhost.binary.sx'];
+const supported_domains = [deriv_com_url, deriv_me_url];
 const domain_url =
     isBrowser() && supported_domains.includes(window.location.hostname) ? window.location.hostname : deriv_com_url;
 

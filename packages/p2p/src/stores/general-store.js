69b6cc97630605bb403eca3450f2e93d30058ff9
Carol Sachdeva <carol@binary.com>
2021-04-28 15:14:50 +0400

    fix: change priorities for fa + no fa

--- packages/p2p/src/stores/general-store.js
+++ packages/p2p/src/stores/general-store.js
@@ -203,21 +203,21 @@ export default class GeneralStore extends BaseStore {
                     is_not_fully_authenticated && hasStatuses(['financial_assessment_not_complete']);
                 const is_poi_authenticated = hasStatuses(['age_verification']);
 
-                if (
+                if (is_fully_authed_but_needs_fa) {
+                    // First priority: Send user to Financial Assessment if they have to submit it.
+                    this.setIsHighRiskFullyAuthedWithoutFa(true);
+                    this.setIsLoading(false);
+                    return;
+                } else if (
                     is_cashier_locked ||
                     is_not_fully_authenticated ||
                     is_fully_authed_but_poi_expired ||
                     is_blocked_because_qa_asked_for_it ||
                     is_poi_authenticated
                 ) {
-                    // First priority: If user is blocked, don't bother getting them to send FA.
+                    // Second priority: If user is blocked, don't bother asking them to submit FA.
                     this.setIsBlocked(true);
                     this.setIsLoading(false);
-                } else if (is_fully_authed_but_needs_fa) {
-                    // Second priority: Send user to Financial Assessment if they can submit it.
-                    this.setIsHighRiskFullyAuthedWithoutFa(true);
-                    this.setIsLoading(false);
-                    return;
                 }
             } else if (error) {
                 this.setIsHighRiskFullyAuthedWithoutFa(false);

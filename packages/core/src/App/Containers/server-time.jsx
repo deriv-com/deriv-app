e57fe9cf608ea4e0175c51f1ae32dbd44f27e6a0
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2021-05-12 14:32:30 +0430

    fix_local_time_format

--- packages/core/src/App/Containers/server-time.jsx
+++ packages/core/src/App/Containers/server-time.jsx
@@ -1,17 +1,17 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { toGMTFormat } from '@deriv/shared';
+import { toGMTFormat, toLocalFormat } from '@deriv/shared';
 import { connect } from 'Stores/connect';
 import { Popover } from '@deriv/components';
 
 const ServerTime = ({ is_mobile, server_time }) => {
     const gmt_time = toGMTFormat(server_time);
-
+    const local_time = toLocalFormat(server_time);
     return (
         <Popover
             alignment='top'
-            message={gmt_time}
+            message={local_time}
             className={classNames('server-time', {
                 'server-time--is-mobile': is_mobile,
             })}

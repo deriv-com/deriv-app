03f99bc5dbd9e7989291f3cabb654228f8091349
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-05-18 16:16:00 +0430

    comments added to setupTests

--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,6 @@
+
+// provides a set of custom jest matchers that you can use to extend jest.
+// These will make your tests more declarative, clear to read and to maintain.
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-extended';

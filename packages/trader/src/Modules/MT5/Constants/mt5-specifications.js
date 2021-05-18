ad3ac0b8de9270d2e51b145ac3fb42b9a0b9ea31
Mahdi Aryayi <mahdi.aryayi@firstsource.tech>
2021-05-18 12:25:53 +0430

    Updated other specs

--- packages/trader/src/Modules/MT5/Constants/mt5-specifications.js
+++ packages/trader/src/Modules/MT5/Constants/mt5-specifications.js
@@ -16,9 +16,9 @@ export const real_financial_specs = {
 // Australian user specs
 export const au_real_financial_specs = {
     [localize('Leverage')]: localize('Up to 1:30'),
-    [localize('Margin call')]: localize('150%'),
-    [localize('Stop out level')]: localize('75%'),
-    [localize('Number of assets')]: localize('50+'),
+    [localize('Margin call')]: localize('100%'),
+    [localize('Stop out level')]: localize('50%'),
+    [localize('Number of assets')]: localize('100+'),
 };
 
 export const eu_real_financial_specs = {

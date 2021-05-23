304f7361b21c2ac2f5d1e92d6406d77eea0615b7
nijilnirmal <nijil@deriv.com>
2021-05-23 13:29:52 +0400

    Remove unwanted prop and component from CashierContainer

--- packages/cashier/src/Components/cashier-container.jsx
+++ packages/cashier/src/Components/cashier-container.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Loading } from '@deriv/components';
 
-const CashierContainer = ({ iframe_height, iframe_url, is_loading }) => {
+const CashierContainer = ({ iframe_height, iframe_url }) => {
     return (
         <div className='cashier__wrapper'>
-            {is_loading && <Loading is_fullscreen={false} />}
             {iframe_url && (
                 <iframe
                     className='cashier__content'
@@ -22,7 +20,6 @@ const CashierContainer = ({ iframe_height, iframe_url, is_loading }) => {
 CashierContainer.propTypes = {
     iframe_height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     iframe_url: PropTypes.string,
-    is_loading: PropTypes.bool,
 };
 
 export default CashierContainer;

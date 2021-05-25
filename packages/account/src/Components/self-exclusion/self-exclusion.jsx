32db73ab5274fe3367ac0eaec9968b1f03034451
shayan <shayan@firstsource.tech>
2021-05-25 11:04:21 +0430

    cleanup code

--- packages/account/src/Components/self-exclusion/self-exclusion.jsx
+++ packages/account/src/Components/self-exclusion/self-exclusion.jsx
@@ -319,7 +319,8 @@ const SelfExclusion = ({
 
             if (isMounted()) {
                 const self_exclusion_value = { ...exclusion_data.current, ...response_to_string };
-                if (parseInt(self_exclusion_value.max_30day_turnover, 10) === NO_SPENDING_LIMIT_TURNOVER) {
+                const max_30day_turnover = parseInt(self_exclusion_value.max_30day_turnover);
+                if (max_30day_turnover === NO_SPENDING_LIMIT_TURNOVER) {
                     self_exclusion_value.max_30day_turnover = '';
                 }
                 setState({

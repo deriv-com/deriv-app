33887cf2f0ec2ecbcc80c870bea748c1ef2546dd
shayan <shayan@firstsource.tech>
2021-05-26 08:33:46 +0430

    close the chat by user logout

--- packages/core/src/Services/logout.js
+++ packages/core/src/Services/logout.js
@@ -6,19 +6,9 @@ import WS from './ws-methods';
 export const requestLogout = () => WS.logout().then(doLogout);
 
 function endChat() {
-    const customerSDK = init({
-        licenseId: livechat_license_id,
-        clientId: livechat_client_id,
-    });
-
-    customerSDK.on('connected', () => {
-        if (window.LiveChatWidget.get('chat_data')) {
-            const { chatId, threadId } = window.LiveChatWidget.get('chat_data');
-            if (threadId) {
-                customerSDK.deactivateChat({ chatId });
-            }
-        }
-    });
+    if (window.LC_API) {
+        window.LC_API.close_chat();
+    }
 }
 
 const doLogout = response => {
@@ -26,8 +16,6 @@ const doLogout = response => {
     removeCookies('affiliate_token', 'affiliate_tracking', 'onfido_token');
     SocketCache.clear();
     sessionStorage.clear();
-    if (!isTestLink()) {
-        endChat();
-    }
+    endChat();
     return response;
 };

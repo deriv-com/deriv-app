8515dc871c05a17d49b90185a26fbfe15aa7969c
zaki-binary <zaki@binary.com>
2021-05-31 19:26:07 +0800

    fix: do not show progress slider if already sold on mobile

--- packages/components/src/components/contract-card/contract-card-items/contract-card-body.jsx
+++ packages/components/src/components/contract-card/contract-card-items/contract-card-body.jsx
@@ -57,7 +57,7 @@ const MultiplierCardBody = ({
                 className={classNames({
                     'dc-contract-card-items-wrapper--mobile': is_mobile,
                     'dc-contract-card-items-wrapper': !is_mobile,
-                    'dc-contract-card-items-wrapper--has-progress-slider': has_progress_slider,
+                    'dc-contract-card-items-wrapper--has-progress-slider': has_progress_slider && !is_sold,
                 })}
             >
                 <ContractCardItem header={getCardLabels().STAKE} className='dc-contract-card__stake'>
@@ -88,9 +88,11 @@ const MultiplierCardBody = ({
                 <ContractCardItem header={getCardLabels().BUY_PRICE} className='dc-contract-card__buy-price'>
                     <Money amount={buy_price} currency={currency} />
                 </ContractCardItem>
-                {has_progress_slider && is_mobile && (
-                    <ContractCardItem className='dc-contract-card__date-expiry'>{progress_slider}</ContractCardItem>
-                )}
+                {has_progress_slider &&
+                    is_mobile &&
+                    !is_sold(
+                        <ContractCardItem className='dc-contract-card__date-expiry'>{progress_slider}</ContractCardItem>
+                    )}
                 <div className='dc-contract-card__limit-order-info'>
                     <ContractCardItem header={getCardLabels().TAKE_PROFIT} className='dc-contract-card__take-profit'>
                         {take_profit ? <Money amount={take_profit} currency={currency} /> : <strong>-</strong>}

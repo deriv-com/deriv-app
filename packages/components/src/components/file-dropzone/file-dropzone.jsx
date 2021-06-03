5a3aee81b8fa10bc85b6c764937398b61fbc4837
zaki-binary <zaki@binary.com>
2021-06-03 13:55:27 +0800

    chore: simplify

--- packages/components/src/components/file-dropzone/file-dropzone.jsx
+++ packages/components/src/components/file-dropzone/file-dropzone.jsx
@@ -49,91 +49,87 @@ const FileDropzone = ({ className, ...props }) => {
             // set maximum size limit for file, in bytes (binary)
             maxSize={props.max_size}
         >
-            {({ getRootProps, getInputProps, isDragAccept, isDragActive, isDragReject }) => {
-                return (
-                    <div
-                        {...getRootProps()}
-                        className={classNames('dc-file-dropzone', className, {
-                            'dc-file-dropzone--is-active': isDragActive,
-                            'dc-file-dropzone--has-file': isDragActive || props.value.length > 0,
-                            'dc-file-dropzone--has-error':
-                                (isDragReject || !!props.validation_error_message) && !isDragAccept,
-                        })}
-                        ref={dropzone_ref}
-                    >
-                        <input {...getInputProps()} />
-                        <div className='dc-file-dropzone__content'>
-                            <FadeInMessage
-                                // default message when not on hover or onDrag
-                                is_visible={
-                                    !isDragActive &&
-                                    !!props.message &&
-                                    props.value.length < 1 &&
-                                    !props.validation_error_message
-                                }
-                                timeout={150}
-                            >
-                                {props.message}
-                            </FadeInMessage>
-                            <FadeInMessage
-                                // message shown on hover if files are accepted onDrag
-                                is_visible={isDragActive && !isDragReject}
-                                timeout={150}
-                            >
-                                {props.hover_message}
-                            </FadeInMessage>
-                            {/* Handle cases for displaying multiple files and single filenames */}
+            {({ getRootProps, getInputProps, isDragAccept, isDragActive, isDragReject }) => (
+                <div
+                    {...getRootProps()}
+                    className={classNames('dc-file-dropzone', className, {
+                        'dc-file-dropzone--is-active': isDragActive,
+                        'dc-file-dropzone--has-file': isDragActive || props.value.length > 0,
+                        'dc-file-dropzone--has-error':
+                            (isDragReject || !!props.validation_error_message) && !isDragAccept,
+                    })}
+                    ref={dropzone_ref}
+                >
+                    <input {...getInputProps()} />
+                    <div className='dc-file-dropzone__content'>
+                        <FadeInMessage
+                            // default message when not on hover or onDrag
+                            is_visible={
+                                !isDragActive &&
+                                !!props.message &&
+                                props.value.length < 1 &&
+                                !props.validation_error_message
+                            }
+                            timeout={150}
+                        >
+                            {props.message}
+                        </FadeInMessage>
+                        <FadeInMessage
+                            // message shown on hover if files are accepted onDrag
+                            is_visible={isDragActive && !isDragReject}
+                            timeout={150}
+                        >
+                            {props.hover_message}
+                        </FadeInMessage>
+                        {/* Handle cases for displaying multiple files and single filenames */}
 
-                            {props.multiple && props.value.length > 0 && !props.validation_error_message
-                                ? props.value.map((file, idx) => (
-                                      <Text
-                                          size='xxs'
-                                          weight='bold'
-                                          align='center'
-                                          key={idx}
-                                          className='dc-file-dropzone__filename'
-                                      >
-                                          {props.filename_limit
-                                              ? truncateFileName(file, props.filename_limit)
-                                              : file.name}
-                                      </Text>
-                                  ))
-                                : props.value[0] &&
-                                  !isDragActive && (
-                                      <Text
-                                          size='xxs'
-                                          weight='bold'
-                                          align='center'
-                                          className='dc-file-dropzone__filename'
-                                          styles={{
-                                              maxWidth: `calc(${dropzone_ref.current?.offsetWidth || 365}px - 3.2rem)`,
-                                          }}
-                                      >
-                                          {props.filename_limit
-                                              ? truncateFileName(props.value[0], props.filename_limit)
-                                              : props.value[0].name}
-                                      </Text>
-                                  )}
-                            <FadeInMessage
-                                // message shown if there are errors with the dragged file
-                                is_visible={isDragReject}
-                                timeout={150}
-                                color='loss-danger'
-                            >
-                                {props.error_message}
-                            </FadeInMessage>
-                            <FadeInMessage
-                                // message shown on if there are validation errors with file uploaded
-                                is_visible={!!props.validation_error_message && !isDragActive}
-                                timeout={150}
-                                color='loss-danger'
-                            >
-                                {props.validation_error_message}
-                            </FadeInMessage>
-                        </div>
+                        {props.multiple && props.value.length > 0 && !props.validation_error_message
+                            ? props.value.map((file, idx) => (
+                                  <Text
+                                      size='xxs'
+                                      weight='bold'
+                                      align='center'
+                                      key={idx}
+                                      className='dc-file-dropzone__filename'
+                                  >
+                                      {props.filename_limit ? truncateFileName(file, props.filename_limit) : file.name}
+                                  </Text>
+                              ))
+                            : props.value[0] &&
+                              !isDragActive && (
+                                  <Text
+                                      size='xxs'
+                                      weight='bold'
+                                      align='center'
+                                      className='dc-file-dropzone__filename'
+                                      styles={{
+                                          maxWidth: `calc(${dropzone_ref.current?.offsetWidth || 365}px - 3.2rem)`,
+                                      }}
+                                  >
+                                      {props.filename_limit
+                                          ? truncateFileName(props.value[0], props.filename_limit)
+                                          : props.value[0].name}
+                                  </Text>
+                              )}
+                        <FadeInMessage
+                            // message shown if there are errors with the dragged file
+                            is_visible={isDragReject}
+                            timeout={150}
+                            color='loss-danger'
+                        >
+                            {props.error_message}
+                        </FadeInMessage>
+                        <FadeInMessage
+                            // message shown on if there are validation errors with file uploaded
+                            is_visible={!!props.validation_error_message && !isDragActive}
+                            timeout={150}
+                            color='loss-danger'
+                        >
+                            {props.validation_error_message}
+                        </FadeInMessage>
                     </div>
-                );
-            }}
+                </div>
+            )}
         </Dropzone>
     );
 };

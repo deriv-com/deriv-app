8cbaa89dc6f06bb2563fa4914b5ac76fcdf0f1e0
yashim-deriv <yashim@deriv.com>
2021-06-03 12:02:20 +0800

    refactor: class to functional components

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity.jsx
@@ -2,72 +2,82 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { AutoHeightWrapper, Button } from '@deriv/components';
 import { Localize } from '@deriv/translations';
-import { getPlatformRedirect, PlatformContext } from '@deriv/shared';
+import { getPlatformRedirect, useIsMounted } from '@deriv/shared';
 import { connect } from 'Stores/connect';
 import { WS } from 'Services/ws-methods';
 import DemoMessage from 'Components/demo-message';
 import MissingPersonalDetails from 'Components/poi-missing-personal-details';
 import ProofOfIdentityContainer from './proof-of-identity-container.jsx';
 
-class ProofOfIdentity extends React.Component {
-    static contextType = PlatformContext;
-    routeBackTo = redirect_route => {
-        this.props.routeBackInApp(this.props.history, [redirect_route]);
-    };
+const ProofOfIdentity = ({
+    account_status,
+    addNotificationByKey,
+    app_routing_history,
+    has_missing_required_field,
+    history,
+    is_mx_mlt,
+    is_virtual,
+    refreshNotifications,
+    removeNotificationByKey,
+    removeNotificationMessage,
+    routeBackInApp,
+    should_allow_authentication,
+}) => {
+    const isMounted = useIsMounted();
+    const from_platform = getPlatformRedirect(app_routing_history);
+    const should_show_redirect_btn = from_platform.name === 'P2P';
 
-    componentDidMount() {
-        WS.wait('get_account_status').then(() => {
-            if (!this.props.should_allow_authentication) {
-                const from_platform = getPlatformRedirect(this.props.app_routing_history);
-                this.props.routeBackInApp(this.props.history, [from_platform]);
-            }
-        });
-    }
+    const routeBackTo = redirect_route => routeBackInApp(history, [redirect_route]);
 
-    render() {
-        const from_platform = getPlatformRedirect(this.props.app_routing_history);
-        const should_show_redirect_btn = from_platform.name === 'P2P';
+    React.useEffect(() => {
+        if (isMounted) {
+            WS.wait('get_account_status').then(() => {
+                if (!should_allow_authentication) {
+                    routeBackInApp(history, [from_platform]);
+                }
+            });
+        }
+    }, [from_platform, history, isMounted, routeBackInApp, should_allow_authentication]);
 
-        if (this.props.is_virtual) return <DemoMessage />;
-        if (this.props.has_missing_required_field) return <MissingPersonalDetails />;
+    if (is_virtual) return <DemoMessage />;
+    if (has_missing_required_field) return <MissingPersonalDetails />;
 
-        return (
-            <AutoHeightWrapper default_height={200}>
-                {({ setRef, height }) => (
-                    <div ref={setRef} className='proof-of-identity'>
-                        <ProofOfIdentityContainer
-                            account_status={this.props.account_status}
-                            serviceToken={WS.serviceToken}
-                            notificationEvent={WS.notificationEvent}
-                            getAccountStatus={WS.authorized.getAccountStatus}
-                            addNotificationByKey={this.props.addNotificationByKey}
-                            is_mx_mlt={this.props.is_mx_mlt}
-                            removeNotificationByKey={this.props.removeNotificationByKey}
-                            removeNotificationMessage={this.props.removeNotificationMessage}
-                            refreshNotifications={this.props.refreshNotifications}
-                            height={height}
-                            redirect_button={
-                                should_show_redirect_btn && (
-                                    <Button
-                                        primary
-                                        className='proof-of-identity__redirect'
-                                        onClick={() => this.routeBackTo(from_platform.route)}
-                                    >
-                                        <Localize
-                                            i18n_default_text='Back to {{platform_name}}'
-                                            values={{ platform_name: from_platform.name }}
-                                        />
-                                    </Button>
-                                )
-                            }
-                            is_description_enabled
-                        />
-                    </div>
-                )}
-            </AutoHeightWrapper>
-        );
-    }
-}
+    return (
+        <AutoHeightWrapper default_height={200}>
+            {({ setRef, height }) => (
+                <div ref={setRef} className='proof-of-identity'>
+                    <ProofOfIdentityContainer
+                        account_status={account_status}
+                        serviceToken={WS.serviceToken}
+                        notificationEvent={WS.notificationEvent}
+                        getAccountStatus={WS.authorized.getAccountStatus}
+                        addNotificationByKey={addNotificationByKey}
+                        is_mx_mlt={is_mx_mlt}
+                        removeNotificationByKey={removeNotificationByKey}
+                        removeNotificationMessage={removeNotificationMessage}
+                        refreshNotifications={refreshNotifications}
+                        height={height}
+                        redirect_button={
+                            should_show_redirect_btn && (
+                                <Button
+                                    primary
+                                    className='proof-of-identity__redirect'
+                                    onClick={() => routeBackTo(from_platform.route)}
+                                >
+                                    <Localize
+                                        i18n_default_text='Back to {{platform_name}}'
+                                        values={{ platform_name: from_platform.name }}
+                                    />
+                                </Button>
+                            )
+                        }
+                        is_description_enabled
+                    />
+                </div>
+            )}
+        </AutoHeightWrapper>
+    );
+};
 
 export default connect(({ client, ui, common }) => ({
     account_status: client.account_status,

065d9a6f587d5ec175c1a702c14aaee5e79a1097
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-06-16 16:33:34 +0430

    import order of api token updated

--- packages/account/src/Sections/Security/ApiToken/api-token.jsx
+++ packages/account/src/Sections/Security/ApiToken/api-token.jsx
@@ -1,5 +1,5 @@
-import { connect } from 'Stores/connect';
 import { WS } from '@deriv/shared';
+import { connect } from 'Stores/connect';
 import ApiToken from 'Components/api-token/api-token.jsx';
 
 export default connect(({ client }) => ({ is_switching: client.is_switching, ws: WS }))(ApiToken);

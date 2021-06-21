f3f5ed1444ba624977beb27863a3689fea646ee4
Hojjat Jokar <hojjat@firstsource.tech>
2021-06-21 17:54:34 +0430

    add welcome to onboarding

--- packages/core/src/App/Containers/Onboarding/onboarding.jsx
+++ packages/core/src/App/Containers/Onboarding/onboarding.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Platforms from './platforms.jsx';
 import AccountSignup from './account-signup.jsx';
+import Welcome from './welcome.jsx';
 
 const Onboarding = () => {
     const [step, setStep] = React.useState('signup');
@@ -9,6 +10,7 @@ const Onboarding = () => {
         <>
             {step === 'signup' && <AccountSignup setStep={setStep} />}
             {step === 'platforms' && <Platforms />}
+            {step === 'welcome' && <Welcome />}
         </>
     );
 };

5e8c58a4ff20cbdd9b0d38ca71693738b74ee7ba
Hojjat Jokar <hojjat@firstsource.tech>
2021-06-22 12:36:19 +0430

    refactor: cleaupheader conditions

--- packages/core/src/App/Containers/Layout/header/header.jsx
+++ packages/core/src/App/Containers/Layout/header/header.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { PlatformContext } from '@deriv/shared';
 import DefaultHeader from './default-header.jsx';
 import DashboardPlatformHeader from './dashboard-platform-header.jsx';
 import DashboardHeader from './dashboard-header.jsx';
 import SignupOnboardingHeader from './signup-onboarding-header.jsx';
 
-const Header = () => {
+const Header = ({ location: { pathname } }) => {
     const { is_dashboard } = React.useContext(PlatformContext);
-    const url_query_string = window.location.search;
-    const url_params = new URLSearchParams(url_query_string);
-    const url = url_params.get('action');
 
     if (is_dashboard) {
         /**
          * The below line will implement when the new domain myapps.deriv.com added.
          */
-        if (/myapps.deriv/.test(window.location.pathname)) return <DashboardPlatformHeader />;
+        if (/myapps.deriv/.test(window.location.hostname)) return <DashboardPlatformHeader />;
         return <DashboardHeader />;
     }
 
-    if (url === 'signup') {
+    if (pathname === '/onboarding') {
         return <SignupOnboardingHeader />;
     }
 
     return <DefaultHeader />;
 };
 
-export default Header;
+export default withRouter(Header);

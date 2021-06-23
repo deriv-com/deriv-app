9246cb4bc1b4ac75198e1558e8f306ca1b793dc2
Hojjat Jokar <hojjat@firstsource.tech>
2021-06-23 18:43:37 +0430

    refactor: style up choose password signup

--- packages/core/src/App/Containers/Onboarding/account-signup-password.jsx
+++ packages/core/src/App/Containers/Onboarding/account-signup-password.jsx
@@ -25,58 +25,58 @@ const AccountSignupPassword = ({
 }) => {
     return (
         <div className='account-signup__password-selection'>
-            <Text as='p' weight='bold' className='account-signup__heading' align='center'>
-                <Localize i18n_default_text='Keep your account secure with a password' />
-            </Text>
-            <Field name='password'>
-                {({ field }) => (
-                    <PasswordMeter
-                        input={pw_input}
-                        has_error={!!(touched.password && errors.password)}
-                        custom_feedback_messages={getErrorMessages().password_warnings}
-                    >
-                        <PasswordInput
-                            {...field}
-                            className='account-signup__password-field'
-                            label={localize('Create a password')}
-                            error={touched.password && errors.password}
-                            required
-                            value={values.password}
-                            onBlur={handleBlur}
-                            onChange={e => {
-                                const input = e.target;
-                                setFieldTouched('password', true);
-                                if (input) updatePassword(input.value);
-                                handleChange(e);
-                            }}
-                            input_id='dt_core_account-signup-modal_account-signup-password-field'
-                        />
-                    </PasswordMeter>
-                )}
-            </Field>
+            <div className='account-signup__password-selection--main'>
+                <Text as='p' weight='bold' className='account-signup__heading' align='center'>
+                    <Localize i18n_default_text='Keep your account secure with a password' />
+                </Text>
+                <Field name='password'>
+                    {({ field }) => (
+                        <PasswordMeter
+                            input={pw_input}
+                            has_error={!!(touched.password && errors.password)}
+                            custom_feedback_messages={getErrorMessages().password_warnings}
+                        >
+                            <PasswordInput
+                                {...field}
+                                className='account-signup__password-field'
+                                label={localize('Create a password')}
+                                error={touched.password && errors.password}
+                                required
+                                value={values.password}
+                                onBlur={handleBlur}
+                                onChange={e => {
+                                    const input = e.target;
+                                    setFieldTouched('password', true);
+                                    if (input) updatePassword(input.value);
+                                    handleChange(e);
+                                }}
+                                input_id='dt_core_account-signup-modal_account-signup-password-field'
+                            />
+                        </PasswordMeter>
+                    )}
+                </Field>
 
-            <Text as='p' size='xxs' className='account-signup__subtext' align='center'>
-                <Localize i18n_default_text='Strong passwords contain at least 8 characters, combine uppercase and lowercase letters, numbers, and symbols.' />
-            </Text>
-            <Field name='email_consent'>
-                {({ field }) => (
-                    <Checkbox
-                        {...field}
-                        className={
-                            is_eu_resident
-                                ? 'account-signup__receive-update-checkbox'
-                                : 'account-signup__hidden-checkbox'
-                        }
-                        onChange={() => {
-                            setFieldValue('email_consent', !values.email_consent);
-                        }}
-                        value={values.email_consent}
-                        label={localize('I want to receive updates on Deriv products, services, and events.')}
-                        withTabIndex='0'
-                    />
+                <Text as='p' size='xxs' className='account-signup__subtext' align='center'>
+                    <Localize i18n_default_text='Strong passwords contain at least 8 characters, combine uppercase and lowercase letters, numbers, and symbols.' />
+                </Text>
+                {is_eu_resident && (
+                    <Field name='email_consent'>
+                        {({ field }) => (
+                            <Checkbox
+                                {...field}
+                                className='account-signup__receive-update-checkbox'
+                                onChange={() => {
+                                    setFieldValue('email_consent', !values.email_consent);
+                                }}
+                                value={values.email_consent}
+                                label={localize('I want to receive updates on Deriv products, services, and events.')}
+                                withTabIndex='0'
+                            />
+                        )}
+                    </Field>
                 )}
-            </Field>
-            <SignupSeparatorContainer />
+            </div>
+
             {api_error ? (
                 <React.Fragment>
                     <Text
@@ -104,16 +104,18 @@ const AccountSignupPassword = ({
                     </div>
                 </React.Fragment>
             ) : (
-                <Button
-                    className={classNames('account-signup__btn', {
-                        'account-signup__btn--disabled': !values.password || errors.password || isSubmitting,
-                    })}
-                    id='dt_core_account-signup-modal_submit-btn'
-                    type='submit'
-                    is_disabled={!values.password || !!errors.password || isSubmitting}
-                    text={localize('Start trading')}
-                    primary
-                />
+                <div className='account-signup__password-selection--footer'>
+                    <Button
+                        className={classNames('account-signup__btn', {
+                            'account-signup__btn--disabled': !values.password || errors.password || isSubmitting,
+                        })}
+                        id='dt_core_account-signup-modal_submit-btn'
+                        type='submit'
+                        is_disabled={!values.password || !!errors.password || isSubmitting}
+                        text={localize('Start trading')}
+                        primary
+                    />
+                </div>
             )}
         </div>
     );

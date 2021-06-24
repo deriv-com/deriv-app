7b2a39ac36a3b92d24fa312c5e7745e2de985429
Hojjat Jokar <hojjat@firstsource.tech>
2021-06-24 17:51:05 +0430

    fix: redirect to trader only when not cr

--- packages/core/src/App/Containers/Onboarding/onboarding.jsx
+++ packages/core/src/App/Containers/Onboarding/onboarding.jsx
@@ -5,16 +5,16 @@ import Platforms from './cfds-platforms.jsx';
 import AccountSignup from './account-signup.jsx';
 import Welcome from './welcome.jsx';
 
-const Onboarding = ({ should_show_cr_onboarding, is_logged_in, history }) => {
+const Onboarding = ({ should_show_cr_onboarding, is_logged_in, history, is_landing_company_loaded }) => {
     const [step, setStep] = React.useState('signup');
 
     React.useEffect(() => {
-        if (is_logged_in && !should_show_cr_onboarding) {
+        if (is_logged_in && is_landing_company_loaded && !should_show_cr_onboarding) {
             history.push({
                 pathname: routes.root,
             });
         }
-    }, [is_logged_in, should_show_cr_onboarding, history]);
+    }, [is_logged_in, should_show_cr_onboarding, is_landing_company_loaded, history]);
 
     return (
         <>
@@ -28,4 +28,5 @@ const Onboarding = ({ should_show_cr_onboarding, is_logged_in, history }) => {
 export default connect(({ client }) => ({
     should_show_cr_onboarding: client.should_show_cr_onboarding,
     is_logged_in: client.is_logged_in,
+    is_landing_company_loaded: client.is_landing_company_loaded,
 }))(Onboarding);

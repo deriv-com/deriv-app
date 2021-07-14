f0160dae08cb3ce1fa6d529d335b08abdf98c764
yashim-deriv <yashim@deriv.com>
2021-07-14 09:15:54 +0800

    fix: BE API structure

--- packages/account/src/Sections/Verification/Helpers/verification.js
+++ packages/account/src/Sections/Verification/Helpers/verification.js
@@ -1,5 +1,5 @@
 export const populateVerificationStatus = account_status => {
-    const { document, identity, needs_verification } = account_status.authentication;
+    const { attempts, document, identity, needs_verification } = account_status.authentication;
     const has_poa = !(document && document.status === 'none');
     const has_poi = !(identity && identity.status === 'none');
     const needs_poa = needs_verification.length && needs_verification.includes('document');
@@ -9,7 +9,7 @@ export const populateVerificationStatus = account_status => {
     const identity_status = identity.status;
 
     const { idv, onfido, manual } = identity.services;
-    const identity_last_attempt = identity.attempts.latest;
+    const identity_last_attempt = attempts.latest;
 
     return {
         allow_document_upload,

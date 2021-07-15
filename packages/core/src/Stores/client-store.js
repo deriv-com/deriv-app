7eb0d57df388d69ffade292f2462a92070bea71e
Mikita Masliyevich <83015762+mikita-deriv@users.noreply.github.com>
2021-07-15 05:16:05 +0300

    Mikita / fixed issue according to reboot au account (#3320)
    * fixed issue according to reboot au account
    
    * feat: fixed restrictint reboot
    
    * feat:fixed  reboot au account
    
    * feat: add new fix version and want to test it
    
    * feat:  add second fix  version and want to test it
    
    * feat: final fix for reboot au account
    
    * feat: revert empty line removals
    
    * test
    
    Co-authored-by: Mikit Masliyevich <mikitmasliyevich@Mikits-MacBook-Pro.local>
    Co-authored-by: System Administrator <root@Mikits-MacBook-Pro.local>
    Co-authored-by: Matin shafiei <70938039+matin-binary@users.noreply.github.com>

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -681,7 +681,9 @@ export default class ClientStore extends BaseStore {
 
     @computed
     get is_options_blocked() {
-        return isOptionsBlocked(this.residence);
+        return this.is_logged_in
+            ? isOptionsBlocked(this.residence)
+            : isOptionsBlocked(this.website_status.clients_country);
     }
 
     /**

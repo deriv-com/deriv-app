cffe73241e7dd7ac2391a8ace10e858d37f5569b
Bala <bala.krishna@regentmarkets.com>
2021-07-27 14:39:31 +0530

    chore: enable real derivx

--- packages/core/src/App/Containers/AccountSwitcher/account-switcher.jsx
+++ packages/core/src/App/Containers/AccountSwitcher/account-switcher.jsx
@@ -136,10 +136,9 @@ const AccountSwitcher = props => {
         redirectToMt5('real');
     };
 
-    // TODO: Uncomment when real account is launched
-    // const redirectToDXTradeReal = () => {
-    //     redirectToDXTrade('real');
-    // };
+    const redirectToDXTradeReal = () => {
+        redirectToDXTrade('real');
+    };
 
     const openMt5DemoAccount = account_type => {
         sessionStorage.setItem('open_cfd_account_type', `demo.${account_type}`);
@@ -151,11 +150,10 @@ const AccountSwitcher = props => {
         redirectToDXTradeDemo();
     };
 
-    // TODO: Uncomment when real account is launched
-    // const openDXTradeRealAccount = account_type => {
-    //     sessionStorage.setItem('open_cfd_account_type', `real.${account_type}`);
-    //     redirectToDXTradeReal();
-    // };
+    const openDXTradeRealAccount = account_type => {
+        sessionStorage.setItem('open_cfd_account_type', `real.${account_type}`);
+        redirectToDXTradeReal();
+    };
 
     const redirectToMt5Demo = () => {
         redirectToMt5('demo');
@@ -243,10 +241,9 @@ const AccountSwitcher = props => {
         return getSortedCFDList(props.mt5_login_list).filter(account => !isDemo(account));
     };
 
-    // TODO: Uncomment when real account is launched
-    // const getRealDXTrade = () => {
-    //     return getSortedCFDList(props.dxtrade_accounts_list).filter(account => !isDemo(account));
-    // };
+    const getRealDXTrade = () => {
+        return getSortedCFDList(props.dxtrade_accounts_list).filter(account => !isDemo(account));
+    };
 
     const findServerForAccount = acc => {
         const server_name = acc.error ? acc.error.details.server : acc.server;
@@ -259,10 +256,9 @@ const AccountSwitcher = props => {
         return getRemainingAccounts(getRealMT5(), CFD_PLATFORMS.MT5);
     };
 
-    // TODO: Uncomment when real account is launched
-    // const getRemainingRealDXTrade = () => {
-    //     return getRemainingAccounts(getRealDXTrade(), CFD_PLATFORMS.DXTRADE);
-    // };
+    const getRemainingRealDXTrade = () => {
+        return getRemainingAccounts(getRealDXTrade(), CFD_PLATFORMS.DXTRADE);
+    };
 
     const canOpenMulti = () => {
         if (props.available_crypto_currencies.length < 1 && !props.has_fiat) return true;
@@ -345,15 +341,14 @@ const AccountSwitcher = props => {
         return !props.has_active_real_account;
     };
 
-    // TODO: Uncomment when real account is launched
-    // const isRealDXTradeAddDisabled = sub_account_type => {
-    //     if (props.is_eu) {
-    //         const account = getAccountTypeFields({ category: 'real', type: sub_account_type });
-    //         return props.isAccountOfTypeDisabled(account?.account_type);
-    //     }
+    const isRealDXTradeAddDisabled = sub_account_type => {
+        if (props.is_eu) {
+            const account = getAccountTypeFields({ category: 'real', type: sub_account_type });
+            return props.isAccountOfTypeDisabled(account?.account_type);
+        }
 
-    //     return !props.has_active_real_account;
-    // };
+        return !props.has_active_real_account;
+    };
 
     if (!props.is_logged_in) return false;
 
@@ -678,8 +673,7 @@ const AccountSwitcher = props => {
                     </AccountWrapper>
                 </React.Fragment>
             )}
-            {/* TODO: Uncomment when real account is launched */}
-            {/* {props.is_dxtrade_allowed && (
+            {props.is_dxtrade_allowed && (
                 <React.Fragment>
                     <div className='acc-switcher__separator acc-switcher__separator--no-padding' />
                     <AccountWrapper
@@ -747,7 +741,7 @@ const AccountSwitcher = props => {
                         )}
                     </AccountWrapper>
                 </React.Fragment>
-            )} */}
+            )}
         </div>
     );
 

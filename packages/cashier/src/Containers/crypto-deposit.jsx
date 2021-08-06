10ff19311c9f737bacea26061d74a79bf64e9620
xenia <kseniya@deriv.com>
2021-08-06 15:00:40 +0300

    feat: api error layout

--- packages/cashier/src/Containers/crypto-deposit.jsx
+++ packages/cashier/src/Containers/crypto-deposit.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { ButtonLink, Clipboard, Loading, Text, Icon } from '@deriv/components';
+import { Button, ButtonLink, Clipboard, Loading, Text, Icon } from '@deriv/components';
 import { localize, Localize } from '@deriv/translations';
 import { CryptoConfig, isMobile } from '@deriv/shared';
 import QRCode from 'qrcode.react';
 import { connect } from 'Stores/connect';
 import '../Sass/deposit.scss';
 
-const CryptoDeposit = ({ currency, deposit_address, pollApiForDepositAddress, is_deposit_address_loading }) => {
+const CryptoDeposit = ({
+    api_error,
+    currency,
+    deposit_address,
+    is_deposit_address_loading,
+    pollApiForDepositAddress,
+}) => {
     React.useEffect(() => {
         return () => pollApiForDepositAddress(false);
     }, [pollApiForDepositAddress]);
@@ -41,26 +47,39 @@ const CryptoDeposit = ({ currency, deposit_address, pollApiForDepositAddress, is
                 <Text as='p' line_height='m' size={isMobile() ? 'xs' : 's'} align='center'>
                     <Localize i18n_default_text="Do not send any other currency to the following address. Otherwise, you'll lose funds." />
                 </Text>
-                <QRCode className='qrcode' value={deposit_address || ''} size={160} />
-                <div className='crypto-deposit__clipboard-wrapper'>
-                    <Text
-                        className='crypto-deposit__address-hash'
-                        line_height='m'
-                        size={isMobile() ? 'xxs' : 'xs'}
-                        weight='bold'
-                        align='center'
-                    >
-                        {deposit_address || ''}
-                    </Text>
-                    <Clipboard
-                        className='crypto-deposit__clipboard'
-                        text_copy={deposit_address || ''}
-                        info_message={isMobile() ? '' : localize('copy')}
-                        icon='IcCashierClipboard'
-                        success_message={localize('copied!')}
-                        popoverAlignment={isMobile() ? 'left' : 'bottom'}
-                    />
-                </div>
+
+                {api_error ? (
+                    <div className='crypto-api-error'>
+                        <Text as='p' align='center' size='xs' className='crypto-api-error__text'>
+                            <Icon icon='IcAlertWarning' />
+                            <Localize i18n_default_text='Our server cannot retrieve an address' />
+                        </Text>
+                        <Button text={localize('Refresh')} onClick={() => window.location.reload()} secondary small />
+                    </div>
+                ) : (
+                    <>
+                        <QRCode className='qrcode' value={deposit_address} size={160} />
+                        <div className='crypto-deposit__clipboard-wrapper'>
+                            <Text
+                                className='crypto-deposit__address-hash'
+                                line_height='m'
+                                size={isMobile() ? 'xxs' : 'xs'}
+                                weight='bold'
+                                align='center'
+                            >
+                                {deposit_address}
+                            </Text>
+                            <Clipboard
+                                className='crypto-deposit__clipboard'
+                                text_copy={deposit_address}
+                                info_message={isMobile() ? '' : localize('copy')}
+                                icon='IcCashierClipboard'
+                                success_message={localize('copied!')}
+                                popoverAlignment={isMobile() ? 'left' : 'bottom'}
+                            />
+                        </div>
+                    </>
+                )}
             </div>
             <div className='crypto-deposit__fiat-onramp-wrapper'>
                 <div className='crypto-deposit__fiat-onramp-description'>
@@ -83,15 +102,17 @@ const CryptoDeposit = ({ currency, deposit_address, pollApiForDepositAddress, is
 };
 
 CryptoDeposit.propTypes = {
+    api_error: PropTypes.string,
     currency: PropTypes.string,
     deposit_address: PropTypes.string,
-    pollApiForDepositAddress: PropTypes.func,
     is_deposit_address_loading: PropTypes.bool,
+    pollApiForDepositAddress: PropTypes.func,
 };
 
 export default connect(({ modules, client }) => ({
+    api_error: modules.cashier.onramp.api_error,
     currency: client.currency,
     deposit_address: modules.cashier.onramp.deposit_address,
-    pollApiForDepositAddress: modules.cashier.onramp.pollApiForDepositAddress,
     is_deposit_address_loading: modules.cashier.onramp.is_deposit_address_loading,
+    pollApiForDepositAddress: modules.cashier.onramp.pollApiForDepositAddress,
 }))(CryptoDeposit);

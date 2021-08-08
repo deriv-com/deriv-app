66fb5d4c6b3188bfe14102ba7e52662c6160b5d5
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-08-08 15:54:34 +0430

    temp commit to make trackjs work on development server

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -6,8 +6,10 @@ import 'event-source-polyfill';
 
 if (
     !!window?.localStorage.getItem?.('debug_service_worker') || // To enable local service worker related development
-    (!window.location.hostname.startsWith('localhost') && !/binary\.sx/.test(window.location.hostname)) ||
-    window.location.hostname === 'deriv-app.binary.sx'
+    !window.location.hostname.startsWith('localhost')
+
+    /** && !/binary\.sx/.test(window.location.hostname)) ||
+    window.location.hostname === 'deriv-app.binary.sx' */ // TODO: this one should revert to the original one.
 ) {
     registerServiceWorker();
 }

585b7d55723ca473451f7565611147ec9c7891ae
Bahar <bahar@firstsource.tech>
2021-08-09 08:52:01 +0430

    fix_isuue_with_side_note

--- packages/cashier/src/Containers/withdrawal.jsx
+++ packages/cashier/src/Containers/withdrawal.jsx
@@ -42,26 +42,27 @@ const WithdrawalSideNote = ({ has_invalid_crypto_address }) => {
 
 const Withdrawal = ({
     balance,
+    check10kLimit,
     container,
     crypto_transactions,
     currency,
+    current_currency_type,
     error,
-    check10kLimit,
     has_invalid_crypto_address,
     iframe_url,
     is_10k_withdrawal_limit_reached,
     is_cashier_locked,
     is_crypto,
+    is_system_maintenance,
     is_virtual,
     is_withdraw_confirmed,
     is_withdrawal_locked,
     setActiveTab,
     setErrorMessage,
     setSideNotes,
+    tab_index,
     verify_error,
     verification_code,
-    is_system_maintenance,
-    current_currency_type,
 }) => {
     React.useEffect(() => {
         setActiveTab(container);
@@ -75,24 +76,23 @@ const Withdrawal = ({
     }, [check10kLimit]);
 
     React.useEffect(() => {
-        if (verification_code && isDesktop()) {
+        if (isDesktop()) {
             if (isCryptocurrency(currency) && typeof setSideNotes === 'function') {
                 const side_notes = [];
                 if (crypto_transactions.length) {
                     side_notes.push(<RecentTransaction key={2} />);
-                } else {
-                    const side_note = [
-                        <WithdrawalSideNote key={0} has_invalid_crypto_address={has_invalid_crypto_address} />,
-                        ...(/^(UST)$/i.test(currency) ? [<USDTSideNote type='usdt' key={1} />] : []),
-                        ...(/^(eUSDT)$/i.test(currency) ? [<USDTSideNote type='eusdt' key={1} />] : []),
-                    ];
-                    side_notes.push(side_note);
                 }
+                const side_note = [
+                    <WithdrawalSideNote key={0} has_invalid_crypto_address={has_invalid_crypto_address} />,
+                    ...(/^(UST)$/i.test(currency) ? [<USDTSideNote type='usdt' key={1} />] : []),
+                    ...(/^(eUSDT)$/i.test(currency) ? [<USDTSideNote type='eusdt' key={1} />] : []),
+                ];
+                side_notes.push(side_note);
                 setSideNotes(side_notes);
             } else setSideNotes(null);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currency, has_invalid_crypto_address, verification_code]);
+    }, [currency, has_invalid_crypto_address, tab_index]);
 
     if (is_system_maintenance) {
         if (is_cashier_locked || (is_withdrawal_locked && current_currency_type === 'crypto')) {
@@ -144,18 +144,19 @@ Withdrawal.propTypes = {
     balance: PropTypes.string,
     container: PropTypes.string,
     crypto_transactions: PropTypes.array,
+    current_currency_type: PropTypes.bool,
     error: PropTypes.object,
     has_invalid_crypto_address: PropTypes.bool,
     iframe_url: PropTypes.string,
-    is_virtual: PropTypes.bool,
     is_cashier_locked: PropTypes.bool,
     is_crypto: PropTypes.bool,
+    is_system_maintenance: PropTypes.bool,
+    is_virtual: PropTypes.bool,
     is_withdraw_confirmed: PropTypes.bool,
     is_withdrawal_locked: PropTypes.bool,
     setActiveTab: PropTypes.func,
+    tab_index: PropTypes.number,
     verification_code: PropTypes.string,
-    is_system_maintenance: PropTypes.bool,
-    current_currency_type: PropTypes.bool,
 };
 
 export default connect(({ client, modules }) => ({
@@ -164,19 +165,20 @@ export default connect(({ client, modules }) => ({
     container: modules.cashier.config.withdraw.container,
     crypto_transactions: modules.cashier.transaction_history.crypto_transactions,
     currency: client.currency,
+    current_currency_type: client.current_currency_type,
     error: modules.cashier.config.withdraw.error,
     has_invalid_crypto_address: modules.cashier.config.withdraw.has_invalid_crypto_address,
     iframe_url: modules.cashier.config.withdraw.iframe_url,
     is_10k_withdrawal_limit_reached: modules.cashier.is_10k_withdrawal_limit_reached,
     is_cashier_locked: modules.cashier.is_cashier_locked,
     is_crypto: modules.cashier.is_crypto,
+    is_system_maintenance: modules.cashier.is_system_maintenance,
     is_virtual: client.is_virtual,
     is_withdraw_confirmed: modules.cashier.config.withdraw.is_withdraw_confirmed,
     is_withdrawal_locked: modules.cashier.is_withdrawal_locked,
-    verification_code: client.verification_code.payment_withdraw,
-    verify_error: modules.cashier.config.withdraw.verification.error,
     setActiveTab: modules.cashier.setActiveTab,
     setErrorMessage: modules.cashier.setErrorMessage,
-    is_system_maintenance: modules.cashier.is_system_maintenance,
-    current_currency_type: client.current_currency_type,
+    tab_index: modules.cashier.cashier_route_tab_index,
+    verification_code: client.verification_code.payment_withdraw,
+    verify_error: modules.cashier.config.withdraw.verification.error,
 }))(Withdrawal);

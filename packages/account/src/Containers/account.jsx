77147b651b73a4157334f919dbcabe43bf2a6745
yashim-deriv <yashim@deriv.com>
2021-08-20 15:37:24 +0800

    refactor: shorten code

--- packages/account/src/Containers/account.jsx
+++ packages/account/src/Containers/account.jsx
@@ -59,10 +59,7 @@ const Account = ({
         label: route_group.getTitle(),
         subitems: route_group.subroutes.map(sub => subroutes.indexOf(sub)),
     }));
-    let selected_content = subroutes.find(r => {
-        const route_regex = new RegExp(`${r.path}(/.*)?`);
-        return route_regex.test(location.pathname);
-    });
+    let selected_content = subroutes.find(r => new RegExp(`${r.path}(/.*)?`).test(location.pathname));
     const onClickClose = React.useCallback(() => routeBackInApp(history), [routeBackInApp, history]);
 
     React.useEffect(() => {

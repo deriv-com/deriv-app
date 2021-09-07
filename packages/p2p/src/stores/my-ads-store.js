1231c14423431a86346f54e2048eaf58aec40aab
artsemkavaleu-deriv <artsem@deriv.com>
2021-09-07 11:32:23 +0300

    'input validation improved'

--- packages/p2p/src/stores/my-ads-store.js
+++ packages/p2p/src/stores/my-ads-store.js
@@ -363,8 +363,8 @@ export default class MyAdsStore extends BaseStore {
             ],
             offer_amount: [
                 v => !!v,
-                v => (values.type === buy_sell.SELL && !isNaN(values.offer_amount) ? v <= this.available_balance : !!v),
                 v => !isNaN(v),
+                v => (values.type === buy_sell.SELL ? v <= this.available_balance : !!v),
                 v =>
                     v > 0 &&
                     decimalValidator(v) &&
@@ -418,8 +418,8 @@ export default class MyAdsStore extends BaseStore {
 
         const getOfferAmountMessages = field_name => [
             localize('{{field_name}} is required', { field_name }),
-            localize('Max available amount is {{value}}', { value: this.available_balance }),
             localize('Enter a valid amount'),
+            localize('Max available amount is {{value}}', { value: this.available_balance }),
             localize('Enter a valid amount'),
             localize('{{field_name}} should not be below Min limit', { field_name }),
             localize('{{field_name}} should not be below Max limit', { field_name }),

537dd290af7454a322a5bb2d229ba3b32aeff8ca
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-09-07 11:22:16 +0430

    jest configs improvement to get report files on test runtime

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+    coverageReporters: ['lcov'],
+    coverageDirectory: './coverage/',
     clearMocks: true,
     projects: [
         '<rootDir>/packages/account/jest.config.js',
@@ -6,18 +10,11 @@ module.exports = {
         '<rootDir>/packages/trader/jest.config.js',
         '<rootDir>/packages/core/jest.config.js',
     ],
-    setupFiles: [
-        "<rootDir>/jest.setup.js",
-    ],
-    testRegex: [
-        '__tests__',
-        '.*.spec.js',
-    ],
+    setupFiles: ['<rootDir>/jest.setup.js'],
+    testRegex: ['__tests__', '.*.spec.js'],
     transform: {
         '^.+\\.jsx?$': 'babel-jest',
         '^.+/es/^.+$': 'babel-jest',
     },
-    transformIgnorePatterns: [
-        "/node_modules/(?!react-virtualized).+\\.js$",
-    ],
+    transformIgnorePatterns: ['/node_modules/(?!react-virtualized).+\\.js$'],
 };

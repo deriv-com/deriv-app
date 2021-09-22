32d3a785e430c333e0f291fce3bdac6f82c350c3
Yashim Wong <75345074+yashim-deriv@users.noreply.github.com>
2021-09-22 16:19:37 +0800

    Add suggestions from code review
    Co-authored-by: reza-fs <53177016+reza-fs@users.noreply.github.com>

--- packages/account/src/Components/poa-button/poa-button.jsx
+++ packages/account/src/Components/poa-button/poa-button.jsx
@@ -3,7 +3,7 @@ import { routes } from '@deriv/shared';
 import { localize } from '@deriv/translations';
 import React from 'react';
 
-export const PoaButton = ({ custom_text }) => (
+export const PoaButton = ({ custom_text || localize('Proof of address')}) => (
     <ButtonLink className='account-management__button' to={routes.proof_of_address}>
         <Text className='dc-btn__text' as='p' weight='bold'>
             {custom_text || localize('Proof of address')}

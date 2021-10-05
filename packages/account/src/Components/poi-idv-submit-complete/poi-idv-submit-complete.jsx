cfae40b2a146c5a7ce48b3a2fec7f87bed2d5178
yashim-deriv <yashim@deriv.com>
2021-10-05 11:48:06 +0800

    text fix

--- packages/account/src/Components/poi-idv-submit-complete/poi-idv-submit-complete.jsx
+++ packages/account/src/Components/poi-idv-submit-complete/poi-idv-submit-complete.jsx
@@ -5,7 +5,7 @@ import PoaButton from 'Components/poa-button';
 import IdvDocumentPending from '../../Assets/ic-idv-document-pending.svg';
 
 const IdvSubmitComplete = ({ needs_poa, is_from_external }) => {
-    const poa_button = !is_from_external && <PoaButton custom_text={localize('Submit proof address')} />;
+    const poa_button = !is_from_external && <PoaButton custom_text={localize('Submit proof of address')} />;
 
     return (
         <div className='proof-of-identity__container'>

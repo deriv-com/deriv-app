4f856b217f78a0951a3446938ea47609072cc534
hubert-deriv <80688204+hubert-deriv@users.noreply.github.com>
2021-10-10 10:42:12 +0200

    Hubert / HOTFIX change account label from synthetic/options to gaming for UK-MX clients. (#4092)
    * changing synthetic/options to gaming for UK
    
    * making it only for MX UK accounts
    
    * changing more labels
    
    Co-authored-by: Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>

--- packages/core/src/App/Containers/RealAccountSignup/real-account-signup.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/real-account-signup.jsx
@@ -50,12 +50,12 @@ const WizardHeading = ({ real_account_signup_target, currency, is_isle_of_man_re
             ) {
                 return <Localize i18n_default_text='Add a real Deriv Options account' />;
             }
-            return <Localize i18n_default_text='Add a Deriv Gaming account' />;
+            return <Localize i18n_default_text='Add a Deriv Synthetic account' />;
         case 'iom':
             if (country_standpoint.is_united_kingdom) {
-                return <Localize i18n_default_text='Add a real Deriv Options account' />;
+                return <Localize i18n_default_text='Add a real Deriv Gaming account' />;
             }
-            return <Localize i18n_default_text='Add a Deriv Gaming account' />;
+            return <Localize i18n_default_text='Add a Deriv Synthetic account' />;
         case 'maltainvest':
             if (
                 country_standpoint.is_united_kingdom ||

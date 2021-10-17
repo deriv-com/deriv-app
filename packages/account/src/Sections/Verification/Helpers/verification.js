9c9628071548d971d8e1ba556f2b68c4fbbc779a
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-10-17 12:11:05 +0330

    fix: verification improvements

--- packages/account/src/Sections/Verification/Helpers/verification.js
+++ packages/account/src/Sections/Verification/Helpers/verification.js
@@ -1,19 +1,25 @@
 export const populateVerificationStatus = account_status => {
-    const { attempts, document, identity, needs_verification } = account_status.authentication;
+    const { attempts, document, identity, needs_verification } = {
+        ...account_status.authentication,
+        attempts: account_status.attempts || {},
+        needs_verification: account_status.needs_verification || {},
+    };
     const has_poa = !(document && document.status === 'none');
     const has_poi = !(identity && identity.status === 'none');
     const needs_poa = needs_verification.length && needs_verification.includes('document');
     const needs_poi = needs_verification.length && needs_verification.includes('identity');
 
-    const allow_document_upload = account_status.status.some(status => status === 'allow_document_upload');
-    const allow_poi_resubmission = account_status.status.some(status => status === 'allow_poi_resubmission');
-    const is_idv_disallowed = account_status.status.some(status => status === 'idv_disallowed');
+    const allow_document_upload = account_status?.status?.some(status => status === 'allow_document_upload');
+    const allow_poi_resubmission = account_status?.status?.some(status => status === 'allow_poi_resubmission');
+    const is_idv_disallowed = account_status?.status?.some(status => status === 'idv_disallowed');
 
-    const identity_status = identity.status;
+    const identity_status = identity?.status;
 
-    const { idv, onfido, manual } = identity.services;
-    const identity_last_attempt = attempts.latest;
-    const has_attempted_idv = !!(attempts.history.length && attempts.history.find(h => h.service === 'idv'));
+    const idv = identity?.services?.idv;
+    const onfido = identity?.services?.onfido;
+    const manual = identity?.services?.manual;
+    const identity_last_attempt = attempts?.latest;
+    const has_attempted_idv = !!(attempts?.history?.length && attempts?.history?.find(h => h.service === 'idv'));
 
     return {
         allow_document_upload,

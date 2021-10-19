4d672337d0c071391722e30aac23a311cb551cfb
Nijil Nirmal <62882794+nijil-binary@users.noreply.github.com>
2021-10-19 18:13:34 +0400

    Nijil/DBot - Add Multipliers (#3919)
    * Retrigger vercel deployment
    
    * Init dbot multiplier
    
    * Fix es-lint issues
    
    * Add contract update dialog validations
    
    * logic added to hasAllRequiredBlocks for multipliers
    
    * Fix es-lint errors
    
    * Update stop loss block description and label
    
    * Filter out multipliers in quick-strategy
    
    * Fix incorrect rendering of duration/multiplier blocks on workspace when they're dragged out from toolbox
    
    * Fix typos
    
    * Add missing proptypes and other minor refactors
    
    * Fix minor issues + logic
    
    * Fix: trade option block not changing according to the selected trade type once it is dragged to workspace first and then plugged to the trade parameters block
    
    * Fix: Disable trade option block when plugged inside trade parameters
    
    * Remove connection to next statement in trade_defintion_restartonerror block
    
    * Make multiplier_stop_loss and multiplier_take_profit independant blocks + other UX improvement
    
    Description \n1.Make multiplier_stop_loss and multiplier_take_profit input field shadow blocks.  \n2. Add check such that duplicate multiplier_take_profit and multiplier_stop_loss cannot be added to trade_definition_multiplier block. \n3.Show amount limits in trade_definition_multiplier block.
    
    * Cleanup workspace when switching between multiplier and other trade options
    
    * Remove extensice validation for multiplier_stop_loss block
    
    * Show dialog for multiplier contract on stop button click + minor cleanups
    
    * Update stop multiplier dialog content
    
    * Update stop multiplier dialog content and button actions
    
    * Minor bug fix
    
    * Fix stop multiplier dialog buttons styling
    
    * fix: multiplier contract not bought even after disabling TP/SL block inside multiiplier block
    
    * Disable stray blocks and their optional children recursively
    
    * fix: recover all pending transactions on page refresh or if client left page mid-contract
    
    * Restrict TP/SL blocks to multiplier block + add close button logic for dialog component
    
    * Fix profit not showing when it's zero
    
    * Add condition for restricted blocks to enable/disable stack option in context menu
    
    * Optimised proposal_open_contract call on mount of transaction tab
    
    * fix es-lint error
    
    Co-authored-by: Matin shafiei <70938039+matin-binary@users.noreply.github.com>

--- packages/components/src/components/contract-card/contract-card-items/contract-type-cell.jsx
+++ packages/components/src/components/contract-card/contract-card-items/contract-type-cell.jsx
@@ -21,6 +21,7 @@ const ContractTypeCell = ({ getContractTypeDisplay, is_high_low, multiplier, typ
 ContractTypeCell.propTypes = {
     getContractTypeDisplay: PropTypes.func,
     is_high_low: PropTypes.bool,
+    multiplier: PropTypes.number,
     type: PropTypes.string,
 };
 

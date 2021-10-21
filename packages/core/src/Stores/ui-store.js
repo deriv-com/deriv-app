70bf46a4adbe6d270a08e041bd5d34aca39a41d9
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-10-21 17:58:44 +0330

    fix: fix store

--- packages/core/src/Stores/ui-store.js
+++ packages/core/src/Stores/ui-store.js
@@ -416,6 +416,7 @@ export default class UIStore extends BaseStore {
         this.is_real_acc_signup_on = true;
         this.real_account_signup_target = target;
         this.is_accounts_switcher_on = false;
+        this.signup_modal_real_account_title = call_site || this.signup_modal_real_account_title;
     }
 
     @action.bound

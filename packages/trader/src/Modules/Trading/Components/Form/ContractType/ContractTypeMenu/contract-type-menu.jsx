b50c61671263a89f5de73fa60cc68ceb617496af
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2021-11-10 10:46:01 +0330

    convert ContractTypeMenu class component to functional component

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeMenu/contract-type-menu.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeMenu/contract-type-menu.jsx
@@ -8,111 +8,97 @@ import NoResultsMessage from './no-results-message.jsx';
 import { Header } from '../ContractTypeInfo';
 import { getContractCategoryLabel } from '../../../../Helpers/contract-type';
 
-class Dialog extends React.PureComponent {
-    input_ref = React.createRef();
+const Dialog = ({
+    categories,
+    item,
+    selected,
+    children,
+    is_info_dialog_open,
+    is_open,
+    onBackButtonClick,
+    onCategoryClick,
+    onChangeInput,
+}) => {
+    const input_ref = React.useRef(null);
 
-    state = {
-        input_value: '',
-    };
+    const [input_value, setInputValue] = React.useState('');
 
-    onChange = e => {
-        if (this.props.is_info_dialog_open) {
-            this.props.onBackButtonClick();
+    const contract_category = getContractCategoryLabel(categories, item);
+    const selectedItem = selected ? { label: selected } : contract_category;
+    const selected_category_contract = !categories?.find(category => category.label === selectedItem.label)
+        ?.contract_categories.length;
+
+    const onChange = e => {
+        if (is_info_dialog_open) {
+            onBackButtonClick();
         }
-        if (this.state.input_value) {
-            this.onClickClearInput();
+        if (input_value) {
+            onClickClearInput();
         }
-        if (this.props.onCategoryClick) {
-            this.props.onCategoryClick(e);
+        if (onCategoryClick) {
+            onCategoryClick(e);
         }
     };
 
-    onChangeInput = e => {
-        this.setState({
-            input_value: e.target.value,
-        });
-        this.props.onChangeInput(e.target.value);
+    const onChangeInputValue = e => {
+        setInputValue(e.target.value);
+        onChangeInput(e.target.value);
     };
 
-    onClickClearInput = () => {
-        this.input_ref.current.focus();
-        this.setState({ input_value: '' });
-        this.props.onChangeInput('');
+    const onClickClearInput = () => {
+        input_ref.current.focus();
+        setInputValue('');
+        onChangeInput('');
     };
 
-    get contract_category() {
-        const { categories, item } = this.props;
-        const label = getContractCategoryLabel(categories, item);
+    const action_bar_items = is_info_dialog_open ? (
+        <Header title={item.text} onClickGoBack={onBackButtonClick} />
+    ) : (
+        <SearchInput
+            ref={input_ref}
+            onChange={onChangeInputValue}
+            onClickClearInput={onClickClearInput}
+            value={input_value}
+        />
+    );
 
-        return {
-            label,
-        };
-    }
-
-    get selected() {
-        const { selected } = this.props;
-        return selected ? { label: selected } : this.contract_category;
-    }
-
-    get selected_category_contract() {
-        const { categories } = this.props;
-        return !categories.find(category => category.label === this.selected.label).contract_categories.length;
-    }
-
-    render() {
-        const { children, is_info_dialog_open, is_open, item, categories, onBackButtonClick } = this.props;
-
-        const action_bar_items = is_info_dialog_open ? (
-            <Header title={item.text} onClickGoBack={onBackButtonClick} />
-        ) : (
-            <SearchInput
-                ref={this.input_ref}
-                onChange={this.onChangeInput}
-                onClickClearInput={this.onClickClearInput}
-                value={this.state.input_value}
-            />
-        );
-
-        return (
-            <CSSTransition
-                in={is_open}
-                timeout={100}
-                classNames={{
-                    enter: 'contract-type-dialog--enter',
-                    enterDone: 'contract-type-dialog--enterDone',
-                    exit: 'contract-type-dialog--exit',
-                }}
-                unmountOnExit
-            >
-                <div className='contract-type-dialog'>
-                    <div className='contract-type-dialog__wrapper'>
-                        <VerticalTab.Layout>
-                            <VerticalTab.Headers
-                                header_title={localize('Trade types')}
-                                items={categories}
-                                selected={this.selected}
-                                onChange={this.onChange}
-                            />
-                            <div className='dc-vertical-tab__content'>
-                                <div className='dc-vertical-tab__action-bar'>{action_bar_items}</div>
-                                <div className='dc-vertical-tab__content-container'>
-                                    {this.selected_category_contract && (
-                                        <NoResultsMessage text={this.state.input_value} />
-                                    )}
-                                    {!is_info_dialog_open ? (
-                                        <ThemedScrollbars height='calc(100vh - 172px)'>{children}</ThemedScrollbars>
-                                    ) : (
-                                        children
-                                    )}
-                                </div>
+    return (
+        <CSSTransition
+            in={is_open}
+            timeout={100}
+            classNames={{
+                enter: 'contract-type-dialog--enter',
+                enterDone: 'contract-type-dialog--enterDone',
+                exit: 'contract-type-dialog--exit',
+            }}
+            unmountOnExit
+        >
+            <div className='contract-type-dialog' data-testid='contract_wrapper'>
+                <div className='contract-type-dialog__wrapper'>
+                    <VerticalTab.Layout>
+                        <VerticalTab.Headers
+                            header_title={localize('Trade types')}
+                            items={categories}
+                            selected={selectedItem}
+                            onChange={onChange}
+                        />
+                        <div className='dc-vertical-tab__content'>
+                            <div className='dc-vertical-tab__action-bar'>{action_bar_items}</div>
+                            <div className='dc-vertical-tab__content-container'>
+                                {selected_category_contract && <NoResultsMessage text={input_value} />}
+                                {!is_info_dialog_open ? (
+                                    <ThemedScrollbars height='calc(100vh - 172px)'>{children}</ThemedScrollbars>
+                                ) : (
+                                    children
+                                )}
                             </div>
-                        </VerticalTab.Layout>
-                    </div>
+                        </div>
+                    </VerticalTab.Layout>
                 </div>
-            </CSSTransition>
-        );
-    }
-}
+            </div>
+        </CSSTransition>
+    );
+};
 
 Dialog.propTypes = {
     is_info_dialog_open: PropTypes.bool,
--- packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-item.spec.js
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-item.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ContractTypeItem from '../contract-type-item.jsx';
 
@@ -14,5 +14,17 @@ describe('ContractTypeItem', () => {
         ];
         const wrapper = shallow(<ContractTypeItem contract_types={contracts} />);
         expect(wrapper).to.have.length(2);
+    });
+
+    it('should has --selected class when cliceked', () => {
+        const clickMock = jest.fn();
+        const contracts = [
+            { value: 'rise_fall', text: 'Rise/Fall' },
+            { value: 'high_low', text: 'Higher/Lower' },
+        ];
+        const wrapper = mount(<ContractTypeItem contract_types={contracts} handleSelect={clickMock} />);
+        const item = wrapper.find('#dt_contract_rise_fall_item');
+        item.simulate('click');
+        expect(clickMock.mock.calls.length).to.be.equal(1);
     });
 });

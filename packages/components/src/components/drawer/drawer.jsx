28d133220ceb030a8eec715a1048077b179f4ab2
Mikit Masliyevich <mikitmasliyevich@Mikits-MacBook-Pro.local>
2021-11-15 12:34:14 +0300

    feat: add setState to useStateCallback

--- packages/components/src/components/drawer/drawer.jsx
+++ packages/components/src/components/drawer/drawer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { isMobile } from '@deriv/shared';
 import Icon from '../icon';
-import {useStateCallback} from '../../hooks/use-state-callback';
+import { useStateCallback } from '../../hooks/use-state-callback';
 
 // TODO: use-from-shared - Use this icon from icons' shared package
 const IconDrawer = ({ className }) => (
@@ -28,66 +28,57 @@ const Drawer = ({
     width = 250,
     zIndex = 4,
 }) => {
-    const [will_open, set_is_open] = useStateCallback(is_open);
-
-    const update_is_open = (value) => {
-        set_is_open(value)
-    }
+    const [has_is_open, setIsOpen] = useStateCallback(is_open);
 
     const toggle_drawer = () => {
-        update_is_open(!will_open)
-            return () => {
-                if (toggleDrawer) {
-                    toggleDrawer(will_open);
-                } 
+        setIsOpen(!has_is_open, () => {
+            if (toggleDrawer) {
+                toggleDrawer(has_is_open);
             }
+        });
     };
-    
-        const is_mobile = isMobile();
+    const is_mobile = isMobile();
 
-        return (
+    return (
+        <div
+            className={classNames('dc-drawer', className, {
+                [`dc-drawer--${anchor}`]: !is_mobile,
+                'dc-drawer--open': has_is_open,
+            })}
+            style={{
+                zIndex,
+                transform:
+                    has_is_open &&
+                    !is_mobile &&
+                    (anchor === 'left'
+                        ? `translateX(calc(${width}px - 16px))`
+                        : `translateX(calc(-${width}px + 16px))`),
+            }}
+        >
             <div
-                className={classNames('dc-drawer', className, {
-                    [`dc-drawer--${anchor}`]: !is_mobile,
-                    'dc-drawer--open': will_open,
+                className={classNames('dc-drawer__toggle', {
+                    'dc-drawer__toggle--open': has_is_open,
                 })}
-                style={{
-                    zIndex,
-                    transform:
-                    will_open &&
-                        !is_mobile &&
-                        (anchor === 'left'
-                            ? `translateX(calc(${width}px - 16px))`
-                            : `translateX(calc(-${width}px + 16px))`),
-                }}
+                onClick={toggle_drawer}
             >
-                <div
-                    className={classNames('dc-drawer__toggle', {
-                        'dc-drawer__toggle--open': will_open,
-                    })}
-                    onClick={toggle_drawer}
-                >
-                    {is_mobile ? (
-                        <Icon icon='IcChevronUp' className='dc-drawer__toggle-icon' />
-                    ) : (
-                        <IconDrawer
-                            className={classNames('dc-drawer__toggle-icon', {
-                                [`dc-drawer__toggle-icon--${anchor}`]: !is_mobile,
-                            })}
-                        />
-                    )}
-                </div>
-                <div
-                    className={classNames('dc-drawer__container', { [`dc-drawer__container--${anchor}`]: !is_mobile })}
-                >
-                    {header && <div className='dc-drawer__header'>{header}</div>}
-                    <div className={classNames('dc-drawer__content', contentClassName)}>{children}</div>
-                    {footer && <div className='dc-drawer__footer'>{footer}</div>}
-                </div>
+                {is_mobile ? (
+                    <Icon icon='IcChevronUp' className='dc-drawer__toggle-icon' />
+                ) : (
+                    <IconDrawer
+                        className={classNames('dc-drawer__toggle-icon', {
+                            [`dc-drawer__toggle-icon--${anchor}`]: !is_mobile,
+                        })}
+                    />
+                )}
             </div>
-        );
-    }
-
+            <div className={classNames('dc-drawer__container', { [`dc-drawer__container--${anchor}`]: !is_mobile })}>
+                {header && <div className='dc-drawer__header'>{header}</div>}
+                <div className={classNames('dc-drawer__content', contentClassName)}>{children}</div>
+                {footer && <div className='dc-drawer__footer'>{footer}</div>}
+            </div>
+        </div>
+    );
+};
 
 Drawer.propTypes = {
     children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),

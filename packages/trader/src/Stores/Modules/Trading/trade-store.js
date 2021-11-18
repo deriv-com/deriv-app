8b9ddbc5e0df79a11b57b33fec470655fb65a955
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2021-11-18 15:12:19 +0100

    reverting test things

--- packages/trader/src/Stores/Modules/Trading/trade-store.js
+++ packages/trader/src/Stores/Modules/Trading/trade-store.js
@@ -296,8 +296,8 @@ export default class TradeStore extends BaseStore {
         const is_logged_in = this.root_store.client.is_logged_in;
         const showError = this.root_store.common.showError;
         const setError = this.root_store.common.setError;
-        const can_have_mx_account = await this.root_store.client.can_have_mx_account;
-        const can_have_mlt_account = await this.root_store.client.can_have_mlt_account;
+        const can_have_mx_account = this.root_store.client.can_have_mx_account;
+        const can_have_mlt_account = this.root_store.client.can_have_mlt_account;
         const can_have_mlt_or_mx_account = can_have_mlt_account === true || can_have_mx_account === true;
         const { active_symbols, error } = await WS.authorized.activeSymbols();
 
@@ -320,6 +320,7 @@ export default class TradeStore extends BaseStore {
                     return;
                 } else if (can_have_mx_account) {
                     showMxMltUnavailableError(showError, false, can_have_mx_account);
+                    return;
                 }
             } else if (!is_on_mf_account) {
                 if (!localStorage.getItem('hide_close_mx_mlt_account_notification')) {

e4ecf14e26ecd3a5f2c563c410078175ffb31bad
Khalid Ibrahim <FXTRyio@gmail.com>
2021-11-23 14:45:10 +0800

    Fixed cashier height (#4309)
    Co-authored-by: Matin shafiei <matin@binary.com>

--- packages/cashier/src/Stores/Cashier/cashier-store.js
+++ packages/cashier/src/Stores/Cashier/cashier-store.js
@@ -649,7 +649,6 @@ export default class CashierStore extends BaseStore {
         const current_container = this.active_container;
 
         this.setErrorMessage('');
-        this.setContainerHeight(0);
         this.setLoading(true);
 
         if (!this.config[this.active_container].is_session_timeout) {
@@ -677,6 +676,7 @@ export default class CashierStore extends BaseStore {
             return;
         }
         if (response_cashier.error) {
+            this.setContainerHeight(0);
             this.handleCashierError(response_cashier.error);
             this.setLoading(false);
             this.setSessionTimeout(true);

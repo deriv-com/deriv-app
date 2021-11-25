c4ab6f167b2c646dab40c044c51b8042411e63de
Maryia <87321516+maryia-deriv@users.noreply.github.com>
2021-11-25 09:39:44 +0300

    Maryia/Updated side notes on Transfer page (#3844)
    * Maryia/Updated side notes on Transfer page
    
    * Maryia/Last update of side notes on Transfer page
    
    * Maryia/Changing side notes order as per final decision
    
    * Refactoring of transfer_fee assignment as per suggestion
    
    Co-authored-by: Mahdi Aryayi <55829032+mahdiaryayi-fs@users.noreply.github.com>
    
    * Maryia/Latest minor change to the side notes
    
    * Maryia/Fix of transfer notes for mobile
    
    * Maryia/Added important details to side notes
    
    * Maryia/Renaming function as per naming convention
    
    * Maryia/Prettified the code
    
    * Maryia/Added notes for Deriv X
    
    * Maryia/Fix for mobile
    
    * Maryia/latest change for Cashier side notes
    
    * Maryia/corrected quotation marks
    
    Co-authored-by: Mahdi Aryayi <55829032+mahdiaryayi-fs@users.noreply.github.com>
    Co-authored-by: Matin shafiei <matin@binary.com>

--- packages/cashier/src/Stores/Cashier/cashier-store.js
+++ packages/cashier/src/Stores/Cashier/cashier-store.js
@@ -1483,7 +1483,7 @@ export default class CashierStore extends BaseStore {
             'fees',
             this.config.account_transfer.selected_to.currency,
         ]);
-        this.config.account_transfer.transfer_fee = typeof transfer_fee === 'undefined' ? 1 : +transfer_fee;
+        this.config.account_transfer.transfer_fee = Number(transfer_fee || 0);
     }
 
     @action.bound
@@ -1770,6 +1770,7 @@ export default class CashierStore extends BaseStore {
             );
         }
         this.setTransferFee();
+        this.setMinimumFee();
         this.setTransferLimit();
     }
 

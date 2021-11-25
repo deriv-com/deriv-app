bb3dc4fba8b84ccd254750aaab3d236a98eebda4
Bahar <bahar@firstsource.tech>
2021-11-25 17:57:36 +0330

    fix_qa_issue

--- packages/cashier/src/Stores/Cashier/cashier-store.js
+++ packages/cashier/src/Stores/Cashier/cashier-store.js
@@ -493,8 +493,6 @@ export default class CashierStore extends BaseStore {
                 await this.getAdvertizerError();
                 this.checkP2pStatus();
                 await this.check10kLimit();
-                await this.setPaymentAgentList();
-                await this.filterPaymentAgentList();
             }
         );
         when(
@@ -563,6 +561,12 @@ export default class CashierStore extends BaseStore {
 
             if (should_remount) {
                 this.onRemount = this.onMountCommon;
+            }
+
+            // we need to see if client's country has PA
+            // if yes, we can show the PA tab in cashier
+            if (!this.config.payment_agent.list.length) {
+                this.setPaymentAgentList().then(this.filterPaymentAgentList);
             }
 
             if (!this.config.payment_agent_transfer.is_payment_agent) {

916430d0be9fe8a210ac9bbc66781ccdf22bb656
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2021-12-01 14:23:33 +0330

    fix allow equals overlap

--- packages/components/src/components/input-field/increment-buttons.jsx
+++ packages/components/src/components/input-field/increment-buttons.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Button from '../button';
-import Icon from '../icon';
+import Button from '../button/button.jsx';
+import Icon from '../icon/icon.jsx';
 
 const IncrementButtons = ({
     decrementValue,

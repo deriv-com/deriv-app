5d17c11a932beb02053efcc64154add054140f21
hirad-rework <hirad@re-work.dev>
2021-12-06 11:33:46 +0330

    fix--making some changes to pass Circle Ci tests

--- packages/trader/src/Modules/CFD/Containers/cfd-dashboard.jsx
+++ packages/trader/src/Modules/CFD/Containers/cfd-dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import { Redirect } from 'react-router-dom';

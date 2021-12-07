3743b0f6fba4c6f7b4e4c4673a060d385d79dfa5
Bahar <bahar@firstsource.tech>
2021-12-07 10:59:05 +0330

    bahar/order_of_min_and_max_withdraw_amount_flipped (#4360)
    * order_of_min_and_max_withdraw_amount_flipped
    
    * amount_formatting_added
    
    Co-authored-by: Matin shafiei <matin@binary.com>

--- packages/cashier/src/Stores/Cashier/cashier-store.js
+++ packages/cashier/src/Stores/Cashier/cashier-store.js
@@ -2192,25 +2192,38 @@ export default class CashierStore extends BaseStore {
         const min_withdraw_amount = website_status.crypto_config[currency].minimum_withdrawal;
         const max_withdraw_amount = +this.max_withdraw_amount > +balance ? +balance : +this.max_withdraw_amount;
 
+        const format_balance = formatMoney(currency, balance, true);
+        const format_min_withdraw_amount = formatMoney(currency, min_withdraw_amount, true);
+        const format_max_withdraw_amount = formatMoney(currency, max_withdraw_amount, true);
+
         if (this.converter_from_amount) {
             const { is_ok, message } = validNumber(this.converter_from_amount, {
                 type: 'float',
                 decimals: getDecimalPlaces(currency),
             });
             if (!is_ok) error_message = message;
-
-            if (+balance < +this.converter_from_amount) error_message = localize('Insufficient funds');
-
-            if (
+            else if (+balance < +this.converter_from_amount) error_message = localize('Insufficient funds');
+            else if (+balance < +min_withdraw_amount)
+                error_message = (
+                    <Localize
+                        i18n_default_text='Your balance ({{format_balance}} {{currency}}) is less than the current minimum withdrawal allowed ({{format_min_withdraw_amount}} {{currency}}). Please top up your account to continue with your withdrawal.'
+                        values={{
+                            format_balance,
+                            currency,
+                            format_min_withdraw_amount,
+                        }}
+                    />
+                );
+            else if (
                 +this.converter_from_amount < +min_withdraw_amount ||
                 +this.converter_from_amount > +max_withdraw_amount
             ) {
                 error_message = (
                     <Localize
-                        i18n_default_text='The allowed withdraw amount is {{min_withdraw_amount}} to {{max_withdraw_amount}} {{currency}}'
+                        i18n_default_text='The current allowed withdraw amount is {{format_min_withdraw_amount}} to {{format_max_withdraw_amount}} {{currency}}'
                         values={{
-                            min_withdraw_amount,
-                            max_withdraw_amount,
+                            format_min_withdraw_amount,
+                            format_max_withdraw_amount,
                             currency,
                         }}
                     />

f055d7230823c747d5098833d6c24920d6efbdea
yashim-deriv <yashim@deriv.com>
2021-12-08 14:43:17 +0800

    fix: query strings not passed during passthrough login

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -13,6 +13,7 @@ import {
     State,
     toMoment,
     deriv_urls,
+    urlFor,
     urlForLanguage,
     CFD_PLATFORMS,
     routes,
@@ -1251,7 +1252,7 @@ export default class ClientStore extends BaseStore {
                 await BinarySocket.authorize(client.token);
             }
             if (redirect_url) {
-                window.location.replace(routes[redirect_url]);
+                window.location.replace(urlFor(routes[redirect_url], { query_string: window?.location?.search }));
             }
             runInAction(() => {
                 this.is_populating_account_list = false;

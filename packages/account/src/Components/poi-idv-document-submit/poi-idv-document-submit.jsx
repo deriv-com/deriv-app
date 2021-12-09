c96873125c0462f51331bfab741f6bdfe6b64b02
yashim-deriv <yashim@deriv.com>
2021-12-09 11:45:55 +0800

    fix: multiple fixes

--- packages/account/src/Components/poi-idv-document-submit/poi-idv-document-submit.jsx
+++ packages/account/src/Components/poi-idv-document-submit/poi-idv-document-submit.jsx
@@ -241,7 +241,11 @@ const IdvDocumentSubmit = ({ handleBack, handleViewComplete, is_from_external, s
                             </fieldset>
                         </div>
                         {document_image && (
-                            <div className='proof-of-identity__sample-container'>
+                            <div
+                                className={classNames('proof-of-identity__sample-container', {
+                                    'proof-of-identity__sample-container-external': is_from_external,
+                                })}
+                            >
                                 <Text size='xxs' weight='bold'>
                                     {localize('Sample:')}
                                 </Text>
@@ -256,7 +260,13 @@ const IdvDocumentSubmit = ({ handleBack, handleViewComplete, is_from_external, s
                         )}
                     </div>
                     {is_doc_selected && (
-                        <Text className='proof-of-identity__text btm-spacer' align='center' size='xs'>
+                        <Text
+                            className={classNames('proof-of-identity__text btm-spacer', {
+                                'top-spacer': is_from_external,
+                            })}
+                            align='center'
+                            size='xs'
+                        >
                             <Localize i18n_default_text='Please ensure all your personal details are the same as in your chosen document. If you wish to update your personal details, go to account settings.' />
                         </Text>
                     )}
@@ -266,7 +276,7 @@ const IdvDocumentSubmit = ({ handleBack, handleViewComplete, is_from_external, s
                         })}
                     >
                         <Button className='back-btn' onClick={handleBack} type='button' has_effect large secondary>
-                            <BackButtonIcon className='back-btn' /> {localize('Go Back')}
+                            <BackButtonIcon className='back-btn-icon' /> {localize('Go Back')}
                         </Button>
                         <Button
                             className='proof-of-identity__submit-button'

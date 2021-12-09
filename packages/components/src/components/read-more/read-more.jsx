a34a04899826ccf87b8df04151d7832d2df064e1
Bahar <bahar@firstsource.tech>
2021-12-09 13:27:45 +0330

    confusing_information_for_crypto_withdraw

--- packages/components/src/components/read-more/read-more.jsx
+++ packages/components/src/components/read-more/read-more.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const ReadMore = ({ expand_text, text, collapse_length, className }) => {
+const ReadMore = ({ className, collapse_length, expand_text, openDialog, text }) => {
     const [content, updateContent] = React.useState('');
     const [is_collapsed, setIsCollapsed] = React.useState(true);
 
     React.useEffect(() => {
-        updateContent(is_collapsed ? text.substring(0, collapse_length) : text);
+        const collapse_text = text.substring(0, collapse_length);
+        openDialog ? updateContent(collapse_text) : updateContent(is_collapsed ? collapse_text : text);
     }, [is_collapsed]);
 
     return (
-        <div
-            className={classNames('dc-read-more', className)}
-            onClick={is_collapsed ? undefined : () => setIsCollapsed(true)}
-        >
-            <span className='dc-read-more__content'>{content}</span>
-            {is_collapsed && (
-                <React.Fragment>
+        <React.Fragment>
+            {openDialog && (
+                <div className={classNames('dc-read-more', className)}>
+                    <span className='dc-read-more__content'>{content}</span>
                     <span>...</span>
-                    <span className='dc-read-more__toggle' onClick={() => setIsCollapsed(false)}>
+                    <span className='dc-read-more__dialog' onClick={() => openDialog()}>
                         {expand_text}
                     </span>
-                </React.Fragment>
+                </div>
             )}
-        </div>
+            {!openDialog && (
+                <div
+                    className={classNames('dc-read-more', className)}
+                    onClick={is_collapsed ? undefined : () => setIsCollapsed(true)}
+                >
+                    <span className='dc-read-more__content'>{content}</span>
+                    {is_collapsed && (
+                        <React.Fragment>
+                            <span>...</span>
+                            <span className='dc-read-more__toggle' onClick={() => setIsCollapsed(false)}>
+                                {expand_text}
+                            </span>
+                        </React.Fragment>
+                    )}
+                </div>
+            )}
+        </React.Fragment>
     );
 };
 ReadMore.propTypes = {
@@ -32,6 +46,7 @@ ReadMore.propTypes = {
     collapse_length: PropTypes.number,
     collapse_text: PropTypes.string,
     expand_text: PropTypes.string,
+    openDialog: PropTypes.func,
     text: PropTypes.string,
 };
 

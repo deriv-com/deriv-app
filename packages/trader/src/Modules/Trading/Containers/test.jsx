53ff531b64ffb4873833b6ab8d61a3c5a963e027
artsemkavaleu-deriv <artsem@deriv.com>
2021-12-14 10:51:07 +0300

    convert Test.jsx component from class to func

--- packages/trader/src/Modules/Trading/Containers/test.jsx
+++ packages/trader/src/Modules/Trading/Containers/test.jsx
@@ -3,83 +3,80 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'Stores/connect';
 
-class Test extends React.Component {
-    state = { is_visible: false, store: 'trade' };
-    setVisibility = this.stateVisibility.bind(this);
-    styles = {
-        container: {
-            fontSize: '10px',
-            lineHeight: '15px',
-            position: 'absolute',
-            zIndex: 1,
-            background: 'rgba(0, 0, 0, 0.8)',
-            color: '#ccc',
-            padding: '10px',
-            marginTop: '-10px',
-            display: 'none',
-            overflowY: 'auto',
-            height: '100%',
-            width: '100%',
-        },
-        prop_name: {
-            color: 'yellowgreen',
-        },
-        tabs: { display: 'flex', textAlign: 'center', marginBottom: '10px' },
-        tab: {
-            fontSize: '18px',
-            border: '1px solid grey',
-            width: '100%',
-            padding: '10px',
-        },
-    };
+const styles = {
+    container: {
+        fontSize: '10px',
+        lineHeight: '15px',
+        position: 'absolute',
+        zIndex: 1,
+        background: 'rgba(0, 0, 0, 0.8)',
+        color: '#ccc',
+        padding: '10px',
+        marginTop: '-10px',
+        display: 'none',
+        overflowY: 'auto',
+        height: '100%',
+        width: '100%',
+    },
+    prop_name: {
+        color: 'yellowgreen',
+    },
+    tabs: { display: 'flex', textAlign: 'center', marginBottom: '10px' },
+    tab: {
+        fontSize: '18px',
+        border: '1px solid grey',
+        width: '100%',
+        padding: '10px',
+    },
+};
 
-    componentDidMount = () => {
-        document.addEventListener('keyup', this.setVisibility, false);
-    };
+const Test = props => {
+    const [is_visible, setIsVisible] = React.useState(false);
+    const [store, setStore] = React.useState('trade');
 
-    componentWillUnmount = () => {
-        document.removeEventListener('keyup', this.setVisibility);
-    };
+    React.useEffect(() => {
+        document.addEventListener('keyup', stateVisibility, false);
+        return () => {
+            document.removeEventListener('keyup', stateVisibility);
+        };
+    });
 
-    stateVisibility(e) {
+    const stateVisibility = e => {
         // Ctrl + s
-        if (e.ctrlKey && e.keyCode === 83) this.setState({ is_visible: !this.state.is_visible });
-    }
+        if (e.keyCode === 83) setIsVisible(!is_visible);
+    };
 
-    renderStoreContent = ([k, v]) => {
+    const renderStoreContent = ([k, v]) => {
         return (
             k !== 'root_store' &&
             typeof v !== 'function' && (
                 <div key={k}>
-                    <span style={this.styles.prop_name}>{k}:</span>{' '}
+                    <span style={styles.prop_name}>{k}:</span>{' '}
                     {v && typeof v === 'object' ? JSON.stringify(toJS(v), null, 1) : v}
                 </div>
             )
         );
     };
 
-    render() {
-        const { container, tab, tabs } = this.styles;
-        const { is_visible, store: selected_store } = this.state;
+    const { container, tab, tabs } = styles;
 
-        return (
-            <code id='state_info' style={{ ...container, display: is_visible ? 'block' : 'none' }}>
-                <div style={tabs}>
-                    {Object.keys(this.props).map(store => (
-                        <p
-                            key={store}
-                            onClick={() => this.setState({ store })}
-                            style={{ ...tab, fontWeight: store === selected_store && 'bold' }}
-                        >
-                            {store}
-                        </p>
-                    ))}
-                </div>
-                {this.props[this.state.store].sort().map(this.renderStoreContent)}
-            </code>
-        );
-    }
-}
+    return (
+        <code id='state_info' style={{ ...container, display: is_visible ? 'block' : 'none' }}>
+            <div style={tabs}>
+                {Object.keys(props).map(storage => (
+                    <p
+                        key={storage}
+                        onClick={() => setStore(storage)}
+                        style={{ ...tab, fontWeight: storage === store && 'bold' }}
+                    >
+                        {storage}
+                    </p>
+                ))}
+            </div>
+            {props[store].sort().map(renderStoreContent)}
+        </code>
+    );
+};
 
 Test.propTypes = {
     entries: PropTypes.array,

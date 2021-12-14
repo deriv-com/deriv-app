ffe2796ac1bf5c6e1ebc81038f0bc3538d96e5f6
mehrzad-fs <60640951+mehrzad-fs@users.noreply.github.com>
2021-12-14 10:07:16 +0300

    Mehrzad / fix issue poi-idv-document (#4442)
    * fix issue poi-idv-document
    
    * add is_from_external to proptypes

--- packages/trader/src/App/Components/Form/RangeSlider/__tests__/tick-steps.spec.js
+++ packages/trader/src/App/Components/Form/RangeSlider/__tests__/tick-steps.spec.js
@@ -6,11 +6,8 @@ describe('TickSteps', () => {
     const min_value = 1;
     const max_value = 10;
     const arr_ticks = [...Array(max_value - min_value + 1).keys()];
-    it.each(arr_ticks)(
-        'should render 10 tick steps',
-        (item) => {
-            const wrapper = render(<TickSteps min_value={min_value} max_value={max_value}/>);
-            expect(wrapper.getAllByTestId(`tick_step_${item + min_value}`)).toHaveLength(1);
-        },
-    );
+    it.each(arr_ticks)('should render 10 tick steps', item => {
+        const wrapper = render(<TickSteps min_value={min_value} max_value={max_value} />);
+        expect(wrapper.getAllByTestId(`tick_step_${item + min_value}`)).toHaveLength(1);
+    });
 });

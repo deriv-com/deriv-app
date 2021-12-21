b185f6516b87482ccdd14581cfa3144afaafcc54
george-usynin-deriv <heorhi@deriv.com>
2021-12-21 17:05:15 +0300

    george/improved tests

--- packages/cashier/src/Components/Error/__tests__/error.spec.js
+++ packages/cashier/src/Components/Error/__tests__/error.spec.js
@@ -21,18 +21,23 @@ describe('<Error />', () => {
     });
 
     it('Should clear the error.message if onClickButton function is fired on <Error /> component', () => {
+        const handleClcik = jest.fn();
+
         const error = {
-            code: 'WrongResponse',
-            message: 'Something is wrong with your payment agent!!!',
+            code: 'InvalidToken',
             setErrorMessage(value) {
                 this.message = value;
             },
+            onClickButton: handleClcik,
         };
+
         render(<Error error={error} />);
-        expect(screen.getByText(error.message)).toBeInTheDocument();
-        expect(error.message).toBe('Something is wrong with your payment agent!!!');
+        expect(
+            screen.getByText('The verification link you used is invalid or expired. Please request for a new one.')
+        ).toBeInTheDocument();
         const error_btn = screen.getByRole('button');
         fireEvent.click(error_btn);
+        expect(handleClcik).toHaveBeenCalledTimes(1);
         expect(error.message).toBe('');
     });
 });

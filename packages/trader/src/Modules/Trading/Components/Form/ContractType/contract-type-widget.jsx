0bcdbf9d068a698fca507466002680ec2242148d
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-12-26 15:54:21 +0330

    feat: remove useless list state

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/contract-type-widget.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/contract-type-widget.jsx
@@ -5,88 +5,77 @@ import { localize } from '@deriv/translations';
 import ContractType from './contract-type.jsx';
 import { getContractTypeCategoryIcons } from '../../../Helpers/contract-type';
 
-class ContractTypeWidget extends React.PureComponent {
-    state = {
-        is_dialog_open: false,
-        is_info_dialog_open: false,
-        selected_category: null,
-        search_query: '',
-    };
+const ContractTypeWidget = ({ is_equal, name, value, list, onChange }) => {
+    const wrapper_ref = React.useRef(null);
+    const [is_dialog_open, setDialogVisibility] = React.useState(false);
+    const [is_info_dialog_open, setInfoDialogVisibility] = React.useState(false);
+    const [selected_category, setSelectedCategory] = React.useState(null);
+    const [search_query, setSearchQuery] = React.useState('');
+    const [item, setItem] = React.useState(null);
+    const [selected_item, setSelectedItem] = React.useState(null);
 
-    componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside);
-    }
+    React.useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside);
-    }
 
-    handleCategoryClick = ({ label }) => this.setState({ selected_category: label });
+    const handleCategoryClick = ({ label }) => setSelectedCategory(label);
 
-    handleSelect = (item, e) => {
+    const handleSelect = (clicked_item, e) => {
         if (e.target.id !== 'info-icon') {
-            this.setState(
-                {
-                    is_dialog_open: false,
-                    is_info_dialog_open: false,
-                    item,
-                },
-                () => {
-                    if (item.value !== this.props.value) {
-                        this.props.onChange({ target: { name: this.props.name, value: item.value } });
-                    }
-                }
-            );
+            setDialogVisibility(false);
+            setInfoDialogVisibility(false);
+            setItem(clicked_item);
+            setSelectedItem(clicked_item);
         }
     };
 
-    handleInfoClick = item => {
-        this.setState(state => ({
-            is_info_dialog_open: !state.is_info_dialog_open,
-            item,
-        }));
+    React.useEffect(() => {
+        if (selected_item && selected_item.value !== value) {
+            onChange({ target: { name, value: selected_item.value } });
+        }
+    }, [selected_item]);
+
+    const handleInfoClick = clicked_item => {
+        setInfoDialogVisibility(!is_info_dialog_open);
+
+        setItem(clicked_item);
     };
 
-    handleNavigationClick = item => {
-        this.setState({ item });
+    const handleNavigationClick = nav_clicked_item => {
+        setItem(nav_clicked_item);
     };
 
-    setWrapperRef = node => {
-        this.wrapper_ref = node;
-    };
-
-    handleClickOutside = event => {
+    const handleClickOutside = event => {
         if (isMobile()) return;
-        if (this.wrapper_ref && !this.wrapper_ref.contains(event.target)) {
-            this.setState({
-                is_dialog_open: false,
-                is_info_dialog_open: false,
-                item: { value: this.props.value },
-            });
+        if (wrapper_ref && !wrapper_ref.current?.contains(event.target)) {
+            setDialogVisibility(false);
+            setInfoDialogVisibility(false);
+            setItem({ ...item, value });
         }
     };
 
-    handleVisibility = () => {
-        this.setState(state => ({ is_dialog_open: !state.is_dialog_open }));
+    const handleVisibility = () => {
+        setDialogVisibility(!is_dialog_open);
     };
 
-    onWidgetClick = () => {
-        this.setState(state => ({
-            is_dialog_open: !state.is_dialog_open,
-            is_info_dialog_open: false,
-            item: { value: this.props.value },
-        }));
+    const onWidgetClick = () => {
+        setDialogVisibility(!is_dialog_open);
+        setInfoDialogVisibility(false);
+        setItem({ ...item, value });
     };
 
-    onBackButtonClick = () => {
-        this.setState({ is_info_dialog_open: false, item: { value: this.props.value } });
+    const onBackButtonClick = () => {
+        setInfoDialogVisibility(false);
+        setItem({ ...item, value });
     };
 
-    onChangeInput = search_query => this.setState({ search_query });
+    const onChangeInput = searchQueryItem => setSearchQuery(searchQueryItem);
 
-    get list_with_category() {
-        const { search_query } = this.state;
-        const { list } = this.props;
+    const list_with_category = () => {
         const contract_type_category_icon = getContractTypeCategoryIcons();
         const multipliers_category = list.filter(
             contract_category => contract_category.label === localize('Multipliers')
@@ -141,75 +130,70 @@ class ContractTypeWidget extends React.PureComponent {
 
             return contract_category;
         });
-    }
+    };
 
-    get selected_category_contracts() {
-        const { selected_category } = this.state;
-        const selected_list_category = this.list_with_category.find(item => item.label === selected_category);
-        return (selected_list_category || this.list_with_category[0]).contract_categories;
-    }
+    const selected_category_contracts = () => {
+        const selected_list_category = list_with_category()?.find(
+            categoryItem => categoryItem.label === selected_category
+        );
+        return (selected_list_category || list_with_category()[0]).contract_categories;
+    };
 
-    get selected_contract_index() {
-        const contract_types_arr = this.list_with_category?.flatMap(category => category.contract_types);
+    const selected_contract_index = () => {
+        const contract_types_arr = list_with_category()?.flatMap(category => category.contract_types);
         return contract_types_arr
             .filter(type => type.value !== 'rise_fall_equal')
-            .findIndex(type => type.value === this.state.item?.value);
-    }
-
-    render() {
-        const { is_equal, name, value } = this.props;
-        const { is_dialog_open, is_info_dialog_open, item } = this.state;
-        const { list } = this.props;
-
-        return (
-            <div
-                id='dt_contract_dropdown'
-                className={`contract-type-widget contract-type-widget--${value} dropdown--left`}
-                ref={this.setWrapperRef}
-                tabIndex='0'
+            .findIndex(type => type.value === item?.value);
+    };
+    return (
+        <div
+            data-testid='dt_contract_widget'
+            id='dt_contract_dropdown'
+            className={`contract-type-widget contract-type-widget--${value} dropdown--left`}
+            ref={wrapper_ref}
+            tabIndex='0'
+        >
+            <ContractType.Display
+                is_open={is_dialog_open || is_info_dialog_open}
+                list={list}
+                name={name}
+                onClick={onWidgetClick}
+                value={value}
+            />
+            <ContractType.Dialog
+                is_info_dialog_open={is_info_dialog_open}
+                is_open={is_dialog_open}
+                item={item || { value }}
+                categories={list_with_category()}
+                list={list}
+                selected={selected_category || list_with_category()[0]?.label}
+                onBackButtonClick={onBackButtonClick}
+                onClose={handleVisibility}
+                onChangeInput={onChangeInput}
+                onCategoryClick={handleCategoryClick}
             >
-                <ContractType.Display
-                    is_open={is_dialog_open || is_info_dialog_open}
-                    list={this.props.list}
-                    name={name}
-                    onClick={this.onWidgetClick}
-                    value={value}
-                />
-                <ContractType.Dialog
-                    is_info_dialog_open={is_info_dialog_open}
-                    is_open={is_dialog_open}
-                    item={item || { value }}
-                    categories={this.list_with_category}
-                    list={list}
-                    selected={this.state.selected_category || this.list_with_category[0].label}
-                    onBackButtonClick={this.onBackButtonClick}
-                    onClose={this.handleVisibility}
-                    onChangeInput={this.onChangeInput}
-                    onCategoryClick={this.handleCategoryClick}
-                >
-                    {!is_info_dialog_open ? (
-                        <ContractType.List
-                            handleInfoClick={this.handleInfoClick}
-                            handleSelect={this.handleSelect}
-                            is_equal={is_equal}
-                            list={this.selected_category_contracts}
-                            name={name}
-                            value={value}
-                        />
-                    ) : (
-                        <ContractType.Info
-                            handleNavigationClick={this.handleNavigationClick}
-                            initial_index={this.selected_contract_index}
-                            handleSelect={this.handleSelect}
-                            item={item || { value }}
-                            list={this.list_with_category}
-                        />
-                    )}
-                </ContractType.Dialog>
-            </div>
-        );
-    }
-}
+                {is_info_dialog_open ? (
+                    <ContractType.Info
+                        handleNavigationClick={handleNavigationClick}
+                        initial_index={selected_contract_index()}
+                        handleSelect={handleSelect}
+                        item={item || { value }}
+                        list={list_with_category()}
+                    />
+                ) : (
+                    <ContractType.List
+                        handleInfoClick={handleInfoClick}
+                        handleSelect={handleSelect}
+                        is_equal={is_equal}
+                        list={selected_category_contracts()}
+                        name={name}
+                        value={value}
+                    />
+                )}
+            </ContractType.Dialog>
+        </div>
+    );
+};
 
 ContractTypeWidget.propTypes = {
     is_equal: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),

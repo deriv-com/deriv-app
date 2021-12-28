cb0276c5f9d44dff27a481e043711f49b4375840
amina-deriv <amina@deriv.com>
2021-12-28 12:34:01 +0400

    fraud_detection

--- packages/cashier/src/Components/Error/cashier-locked.jsx
+++ packages/cashier/src/Components/Error/cashier-locked.jsx
@@ -13,6 +13,7 @@ const CashierLocked = ({
     is_system_maintenance,
     is_withdrawal_lock,
     loginid,
+    is_identity_verification_needed,
 }) => {
     const { cashier_validation } = account_status;
     const no_residence = cashier_validation?.includes('no_residence');
@@ -91,7 +92,16 @@ const CashierLocked = ({
                     ]}
                 />
             );
-        } else {
+        } else if(is_identity_verification_needed){
+                message = (
+                    <Localize
+                        i18n_default_text='Please submit your <0>proof of identity</0> to authenticate your account and access your Cashier.'
+                        components={[
+                            <a key={0} className='link' rel='noopener noreferrer' href={'/account/proof-of-identity'} />,
+                        ]}
+                    />
+                );
+        }else{
             message = (
                 <Localize
                     i18n_default_text='Your account has not been authenticated. Please submit your <0>proof of identity</0> and <1>proof of address</1> to authenticate your account and access your cashier.'
@@ -238,4 +248,5 @@ export default connect(({ client, modules }) => ({
     is_system_maintenance: modules.cashier.general_store.is_system_maintenance,
     is_withdrawal_lock: client.is_withdrawal_lock,
     loginid: client.loginid,
+    is_identity_verification_needed: client.is_identity_verification_needed,
 }))(CashierLocked);

7fc2d663cdbdefbeb723c5e8cc72f1e385072a00
artsemkavaleu-deriv <artsem@deriv.com>
2022-01-04 13:26:06 +0300

    remove extra variables and inputs

--- packages/cashier/src/Components/Confirm/__tests__/payment-agent-withdraw-confirm.spec.js
+++ packages/cashier/src/Components/Confirm/__tests__/payment-agent-withdraw-confirm.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import PaymentAgentWithdrawConfirm from '../payment-agent-withdraw-confirm';
-import Confirm from '../../confirm';
 
 jest.mock('Stores/connect', () => ({
     __esModule: true,
@@ -19,24 +18,15 @@ afterEach(cleanup);
 
 describe('<PaymentAgentWithdrawConfirm />', () => {
     const amount = 10;
-    const currency = 'testCurrency';
+    const currency = 'BTC';
     const description = 'testDescription';
-    const payment_agent_name = 'testName';
     const error = {
         code: 'testCode',
         message: 'testMessage',
     };
+    const header = 'Please confirm the transaction details in order to complete the withdrawal:';
     const requestPaymentAgentWithdraw = jest.fn();
     const setIsTryWithdrawSuccessful = jest.fn();
-    const header = 'Please confirm the transaction details in order to complete the withdrawal:';
-
-    const data = [
-        {
-            label: 'testText',
-            value: payment_agent_name,
-            key: 'testKey',
-        },
-    ];
 
     it('component should be rendered', () => {
         const { container } = render(<PaymentAgentWithdrawConfirm />);
@@ -45,45 +35,25 @@ describe('<PaymentAgentWithdrawConfirm />', () => {
     });
 
     it('component <Row /> should be rendered when has data', () => {
-        const { container } = render(
-            <PaymentAgentWithdrawConfirm>
-                <Confirm data={data} />
-            </PaymentAgentWithdrawConfirm>
-        );
+        const { container } = render(<PaymentAgentWithdrawConfirm />);
 
         expect(container.querySelector('.confirm__row')).toBeInTheDocument();
     });
 
     it('component <FormError /> should be rendered when has an error', () => {
-        render(
-            <PaymentAgentWithdrawConfirm error={error}>
-                <Confirm data={data} error={error} />
-            </PaymentAgentWithdrawConfirm>
-        );
+        render(<PaymentAgentWithdrawConfirm error={error} />);
 
         expect(screen.getByText('testMessage')).toBeInTheDocument();
     });
 
     it('header should be rendered', () => {
-        render(
-            <PaymentAgentWithdrawConfirm>
-                <Confirm header={header} />
-            </PaymentAgentWithdrawConfirm>
-        );
+        render(<PaymentAgentWithdrawConfirm />);
 
         expect(screen.getByText(header)).toBeInTheDocument();
     });
 
     it(`setIsTryWithdrawSuccessful func should be triggered when click on 'Back' button`, () => {
-        render(
-            <PaymentAgentWithdrawConfirm setIsTryWithdrawSuccessful={setIsTryWithdrawSuccessful}>
-                <Confirm
-                    onClickBack={() => {
-                        setIsTryWithdrawSuccessful(false);
-                    }}
-                />
-            </PaymentAgentWithdrawConfirm>
-        );
+        render(<PaymentAgentWithdrawConfirm setIsTryWithdrawSuccessful={setIsTryWithdrawSuccessful} />);
 
         const btn = screen.getByText('Back');
         fireEvent.click(btn);
@@ -97,13 +67,7 @@ describe('<PaymentAgentWithdrawConfirm />', () => {
                 currency={currency}
                 description={description}
                 requestPaymentAgentWithdraw={requestPaymentAgentWithdraw}
-            >
-                <Confirm
-                    onClickConfirm={() => {
-                        requestPaymentAgentWithdraw({ amount, currency, description, transfer_to });
-                    }}
-                />
-            </PaymentAgentWithdrawConfirm>
+            />
         );
 
         const btn = screen.getByText('Confirm');

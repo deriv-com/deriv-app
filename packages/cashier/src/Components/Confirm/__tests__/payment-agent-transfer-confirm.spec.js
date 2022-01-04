d271b3911a7dfffd3f71479cacf93ba54720ab29
artsemkavaleu-deriv <artsem@deriv.com>
2022-01-04 13:20:01 +0300

    remove extra variables and inputs

--- packages/cashier/src/Components/Confirm/__tests__/payment-agent-transfer-confirm.spec.js
+++ packages/cashier/src/Components/Confirm/__tests__/payment-agent-transfer-confirm.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import PaymentAgentTransferConfirm from '../payment-agent-transfer-confirm';
-import Confirm from '../../confirm';
 
 jest.mock('Stores/connect', () => ({
     __esModule: true,
@@ -10,9 +9,9 @@ jest.mock('Stores/connect', () => ({
 }));
 
 beforeAll(() => {
-    const portalRoot = global.document.createElement('div');
-    portalRoot.setAttribute('id', 'modal_root');
-    global.document.body.appendChild(portalRoot);
+    const portal_root = document.createElement('div');
+    portal_root.setAttribute('id', 'modal_root');
+    document.body.appendChild(portal_root);
 });
 
 afterEach(cleanup);
@@ -25,71 +24,41 @@ describe('<PaymentAgentTransferConfirm />', () => {
         code: 'testCode',
         message: 'testMessage',
     };
-    const transfer_to = 'test';
+    const header = 'Please confirm the transaction details in order to complete the transfer:';
     const requestPaymentAgentTransfer = jest.fn();
     const setIsTryTransferSuccessful = jest.fn();
-    const header = 'Please confirm the transaction details in order to complete the transfer:';
-
-    const data = [
-        {
-            label: 'testText',
-            value: transfer_to,
-            key: 'testKey',
-        },
-    ];
+    const transfer_to = 'test';
 
     it('component should be rendered', () => {
-        const { container } = render(
-            <PaymentAgentTransferConfirm transfer_to={transfer_to}>
-                <Confirm />
-            </PaymentAgentTransferConfirm>
-        );
+        const { container } = render(<PaymentAgentTransferConfirm transfer_to={transfer_to} />);
 
         expect(container.querySelector('.cashier__wrapper--confirm')).toBeInTheDocument();
     });
 
+    it('header should be rendered', () => {
+        render(<PaymentAgentTransferConfirm transfer_to={transfer_to} />);
+
+        expect(screen.getByText(header)).toBeInTheDocument();
+    });
+
     it('component <Row /> should be rendered when has data', () => {
-        const { container } = render(
-            <PaymentAgentTransferConfirm transfer_to={transfer_to}>
-                <Confirm data={data} />
-            </PaymentAgentTransferConfirm>
-        );
+        const { container } = render(<PaymentAgentTransferConfirm transfer_to={transfer_to} />);
 
         expect(container.querySelector('.confirm__row')).toBeInTheDocument();
     });
 
     it('component <FormError /> should be rendered when has an error', () => {
-        render(
-            <PaymentAgentTransferConfirm transfer_to={transfer_to} error={error}>
-                <Confirm data={data} error={error} />
-            </PaymentAgentTransferConfirm>
-        );
+        render(<PaymentAgentTransferConfirm error={error} transfer_to={transfer_to} />);
 
         expect(screen.getByText('testMessage')).toBeInTheDocument();
-    });
-
-    it('header should be rendered', () => {
-        render(
-            <PaymentAgentTransferConfirm transfer_to={transfer_to}>
-                <Confirm header={header} />
-            </PaymentAgentTransferConfirm>
-        );
-
-        expect(screen.getByText(header)).toBeInTheDocument();
     });
 
     it(`setIsTryTransferSuccessful func should be triggered when click on 'Back' button`, () => {
         render(
             <PaymentAgentTransferConfirm
-                transfer_to={transfer_to}
                 setIsTryTransferSuccessful={setIsTryTransferSuccessful}
-            >
-                <Confirm
-                    onClickBack={() => {
-                        setIsTryTransferSuccessful(false);
-                    }}
-                />
-            </PaymentAgentTransferConfirm>
+                transfer_to={transfer_to}
+            />
         );
 
         const btn = screen.getByText('Back');
@@ -103,15 +72,9 @@ describe('<PaymentAgentTransferConfirm />', () => {
                 amount={amount}
                 currency={currency}
                 description={description}
-                transfer_to={transfer_to}
                 requestPaymentAgentTransfer={requestPaymentAgentTransfer}
-            >
-                <Confirm
-                    onClickConfirm={() => {
-                        requestPaymentAgentTransfer({ amount, currency, description, transfer_to });
-                    }}
-                />
-            </PaymentAgentTransferConfirm>
+                transfer_to={transfer_to}
+            />
         );
 
         const btn = screen.getByText('Confirm');

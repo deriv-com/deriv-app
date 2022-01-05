1f4ab423f47b1cac3aeecdd5b23f662aaaa951ac
george-usynin-deriv <heorhi@deriv.com>
2022-01-05 17:31:54 +0300

    removed unused imports

--- packages/cashier/src/Components/Receipt/__tests__/crypto-withdraw-receipt.spec.js
+++ packages/cashier/src/Components/Receipt/__tests__/crypto-withdraw-receipt.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import CryptoWithdrawReceipt from '../crypto-withdraw-receipt';
-import { isMobile } from '@deriv/shared';
 
 jest.mock('Stores/connect.js', () => ({
     __esModule: true,

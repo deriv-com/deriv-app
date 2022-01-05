835948e36d63ae2caa66dd43a1e86de3ca1ebf3f
george-usynin-deriv <heorhi@deriv.com>
2022-01-05 17:25:23 +0300

    added tests on CryptoWithdrawReceipt component

--- packages/cashier/jest.config.js
+++ packages/cashier/jest.config.js
@@ -6,6 +6,8 @@ module.exports = {
         '\\.s(c|a)ss$': '<rootDir>/../../__mocks__/styleMock.js',
         '^.+\\.svg$': '<rootDir>/../../__mocks__/styleMock.js',
         '^Stores/(.*)$': '<rootDir>/src/Stores/$1',
+        '^Components/(.*)$': '<rootDir>/src/Components/$1',
+        '^_common/(.*)$': '<rootDir>/src/_common/$1',
     },
     testPathIgnorePatterns: ['/Routes/', '/Validator/'],
 };

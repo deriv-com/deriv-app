9cc1738808b92c0d22b4c906b162220221b8db8f
george-usynin-deriv <heorhi@deriv.com>
2022-01-11 11:00:23 +0300

    george/ test coverage improvements

--- packages/cashier/src/Components/Form/crypto-transactions-history/__tests__/crypto-transactions-renderer.spec.js
+++ packages/cashier/src/Components/Form/crypto-transactions-history/__tests__/crypto-transactions-renderer.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { isMobile } from '@deriv/shared';
 import CryptoTransactionsRenderer from '../crypto-transactions-renderer';
 
@@ -39,6 +39,74 @@ describe('<CryptoTransactionsRenderer />', () => {
         expect(screen.getByText('-0.00500000 BTC')).toBeInTheDocument();
         expect(screen.getByText('tb1q....ntxt')).toBeInTheDocument();
         expect(screen.getByText('In review')).toBeInTheDocument();
+    });
+
+    it('should show the popover with the proper message, "Yes" and "No" buttons if the "Cancel transaction" cross-button was clicked in Desktop mode', () => {
+        const { container } = render(
+            <CryptoTransactionsRenderer row={row} currency={'BTC'} is_transaction_clicked is_valid_to_cancel={1} />
+        );
+        const cancel_transaction_div = container.querySelector('.crypto-transactions-history__table-action');
+        fireEvent.click(cancel_transaction_div.firstChild);
+
+        expect(screen.getByText('Are you sure you want to cancel this transaction?')).toBeInTheDocument();
+        expect(screen.getByText('Yes')).toBeInTheDocument();
+        expect(screen.getByText('No')).toBeInTheDocument();
+    });
+
+    it('should close the popover when "No" button is clicked in Desktop mode', async () => {
+        const { container } = render(
+            <CryptoTransactionsRenderer row={row} currency={'BTC'} is_transaction_clicked is_valid_to_cancel={1} />
+        );
+        const cancel_transaction_div = container.querySelector('.crypto-transactions-history__table-action');
+        fireEvent.click(cancel_transaction_div.firstChild);
+        const no_btn = screen.getByText('No');
+        fireEvent.click(no_btn);
+
+        await waitFor(() => {
+            expect(screen.queryByText('Are you sure you want to cancel this transaction?')).not.toBeInTheDocument();
+            expect(screen.queryByText('Yes')).not.toBeInTheDocument();
+            expect(screen.queryByText('No')).not.toBeInTheDocument();
+        });
+    });
+
+    it('should close the popover when "Yes" button is clicked in Desktop mode', async () => {
+        const cancelCryptoTransaction = jest.fn();
+        const { container } = render(
+            <CryptoTransactionsRenderer
+                row={row}
+                currency={'BTC'}
+                is_transaction_clicked
+                is_valid_to_cancel={1}
+                cancelCryptoTransaction={cancelCryptoTransaction}
+            />
+        );
+        const cancel_transaction_div = container.querySelector('.crypto-transactions-history__table-action');
+        fireEvent.click(cancel_transaction_div.firstChild);
+        const yes_btn = screen.getByText('Yes');
+        fireEvent.click(yes_btn);
+
+        await waitFor(() => {
+            expect(screen.queryByText('Are you sure you want to cancel this transaction?')).not.toBeInTheDocument();
+            expect(screen.queryByText('Yes')).not.toBeInTheDocument();
+            expect(screen.queryByText('No')).not.toBeInTheDocument();
+        });
+    });
+
+    it('should trigger onClick callback when "crypto-transactions-history__table-status" is clicked in Mobile mode', () => {
+        isMobile.mockReturnValue(true);
+        const showCryptoTransactionsStatusModal = jest.fn();
+        const { container } = render(
+            <CryptoTransactionsRenderer
+                row={row}
+                currency={'BTC'}
+                showCryptoTransactionsStatusModal={showCryptoTransactionsStatusModal}
+            />
+        );
+
+        const table_status = container.querySelector('.crypto-transactions-history__table-status');
+        fireEvent.click(table_status);
+
+        expect(showCryptoTransactionsStatusModal).toHaveBeenCalledTimes(1);
     });
 
     it('should show the proper data in Mobile mode', () => {

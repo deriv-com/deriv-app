24177b8085e71d199483b619d842a2f819b490dd
artsemkavaleu-deriv <artsem@deriv.com>
2022-01-13 10:46:26 +0300

    return imports to prevent futher conflicts

--- packages/cashier/src/Components/Form/account-transfer-form.jsx
+++ packages/cashier/src/Components/Form/account-transfer-form.jsx
@@ -208,8 +208,6 @@ const AccountTransferForm = ({
     is_dark_mode_on,
     minimum_fee,
     mt5_login_list,
-    onChangeConverterFromAmount,
-    onChangeConverterToAmount,
     onChangeTransferFrom,
     onChangeTransferTo,
     onMount,
@@ -577,9 +575,6 @@ const AccountTransferForm = ({
                                                     ''
                                                 )
                                             }
-                                            onChangeConverterFromAmount={onChangeConverterFromAmount}
-                                            onChangeConverterToAmount={onChangeConverterToAmount}
-                                            resetConverter={resetConverter}
                                             to_currency={selected_to.currency}
                                             validateFromAmount={validateTransferFromAmount}
                                             validateToAmount={validateTransferToAmount}
@@ -649,8 +644,6 @@ AccountTransferForm.propTypes = {
     error: PropTypes.object,
     is_crypto: PropTypes.bool,
     minimum_fee: PropTypes.string,
-    onChangeConverterFromAmount: PropTypes.func,
-    onChangeConverterToAmount: PropTypes.func,
     onChangeTransferFrom: PropTypes.func,
     onChangeTransferTo: PropTypes.func,
     onMount: PropTypes.func,
@@ -684,8 +677,6 @@ export default connect(({ client, modules, ui }) => ({
     is_dxtrade_allowed: client.is_dxtrade_allowed,
     minimum_fee: modules.cashier.account_transfer.minimum_fee,
     mt5_login_list: client.mt5_login_list,
-    onChangeConverterFromAmount: modules.cashier.crypto_fiat_converter.onChangeConverterFromAmount,
-    onChangeConverterToAmount: modules.cashier.crypto_fiat_converter.onChangeConverterToAmount,
     onChangeTransferFrom: modules.cashier.account_transfer.onChangeTransferFrom,
     onChangeTransferTo: modules.cashier.account_transfer.onChangeTransferTo,
     onMount: client.getLimits,

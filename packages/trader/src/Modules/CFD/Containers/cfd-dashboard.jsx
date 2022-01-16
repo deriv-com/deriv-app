1db87c119e1ecf3b0dc86fb6944c49fdd7c6419c
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-01-16 10:50:01 +0330

    fix dependency warning in cfd-dashboard

--- packages/trader/src/Modules/CFD/Containers/cfd-dashboard.jsx
+++ packages/trader/src/Modules/CFD/Containers/cfd-dashboard.jsx
@@ -61,8 +61,10 @@ const CFDDashboard = props => {
         return () => {
             props.onUnmount();
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     React.useEffect(() => {
         updateActiveIndex();
         props.checkShouldOpenAccount();

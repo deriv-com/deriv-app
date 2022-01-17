b8580f7bf9141c3d3a11dd1bd411511aa57c0055
george-usynin-deriv <89790856+george-usynin-deriv@users.noreply.github.com>
2022-01-17 05:49:42 +0300

    george / added tests on CryptoWithdrawReceipt component (#4591)
    * added tests on CryptoWithdrawReceipt component
    
    * removed unused imports
    
    Co-authored-by: Carol Sachdeva <58209918+carol-binary@users.noreply.github.com>
    Co-authored-by: Matin shafiei <matin@binary.com>

--- packages/cashier/jest.config.js
+++ packages/cashier/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
         '\\.s(c|a)ss$': '<rootDir>/../../__mocks__/styleMock.js',
         '^.+\\.svg$': '<rootDir>/../../__mocks__/styleMock.js',
         '^Stores/(.*)$': '<rootDir>/src/Stores/$1',
+        '^_common/(.*)$': '<rootDir>/src/_common/$1',
         '^Config/(.*)$': '<rootDir>/src/Config/$1',
         '^Components/(.*)$': '<rootDir>/src/Components/$1',
     },

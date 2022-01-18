6d2696de53d7a7b6761e4f18f2fb2f29b7df7d9a
maryia-deriv <maryia@deriv.com>
2022-01-18 10:27:39 +0300

    Maryia/typified cfd-config

--- packages/trader/src/Stores/Modules/CFD/Helpers/cfd-config.ts
+++ packages/trader/src/Stores/Modules/CFD/Helpers/cfd-config.ts
@@ -1,84 +1,66 @@
 import { localize } from '@deriv/translations';
 
-type TAccountTypeConfig = {
-    account_type: string;
-    leverage: number;
-    short_title: string;
-};
-
-type TAccountDetails = {
-    leverage: number;
-    short_title: string;
-    title: string;
-};
-type TDxAccountDetails = TAccountDetails & {
-    dxtrade_account_type: string;
-};
-type TMtAccountDetails = TAccountDetails & {
-    mt5_account_type: string;
-};
-
 export type TDxCompanies = ReturnType<typeof getDxCompanies>;
 export type TMtCompanies = ReturnType<typeof getMtCompanies>;
 
 export const getDxCompanies = () => {
-    const synthetic_config: TAccountTypeConfig = {
+    const synthetic_config = {
         account_type: '',
         leverage: 500,
-        short_title: localize('Synthetic'),
+        short_title: localize('Synthetic') as string,
     };
-    const financial_config: TAccountTypeConfig = {
+    const financial_config = {
         account_type: 'financial',
         leverage: 1000,
-        short_title: localize('Financial'),
+        short_title: localize('Financial') as string,
     };
     return {
         demo: {
             synthetic: {
                 dxtrade_account_type: synthetic_config.account_type,
                 leverage: synthetic_config.leverage,
-                title: localize('Demo Synthetic'),
+                title: localize('Demo Synthetic') as string,
                 short_title: synthetic_config.short_title,
-            } as TDxAccountDetails,
+            },
             financial: {
                 dxtrade_account_type: financial_config.account_type,
                 leverage: financial_config.leverage,
-                title: localize('Demo Financial'),
+                title: localize('Demo Financial') as string,
                 short_title: financial_config.short_title,
-            } as TDxAccountDetails,
+            },
         },
         real: {
             synthetic: {
                 dxtrade_account_type: synthetic_config.account_type,
                 leverage: synthetic_config.leverage,
-                title: localize('Synthetic'),
+                title: localize('Synthetic') as string,
                 short_title: synthetic_config.short_title,
-            } as TDxAccountDetails,
+            },
             financial: {
                 dxtrade_account_type: financial_config.account_type,
                 leverage: financial_config.leverage,
-                title: localize('Financial'),
+                title: localize('Financial') as string,
                 short_title: financial_config.short_title,
-            } as TDxAccountDetails,
+            },
         },
-    } as const;
+    };
 };
 
 export const getMtCompanies = (is_eu: boolean) => {
-    const synthetic_config: TAccountTypeConfig = {
+    const synthetic_config = {
         account_type: '',
         leverage: 500,
-        short_title: localize('Synthetic'),
+        short_title: localize('Synthetic') as string,
     };
-    const financial_config: TAccountTypeConfig = {
+    const financial_config = {
         account_type: 'financial',
         leverage: 1000,
-        short_title: is_eu ? localize('CFDs') : localize('Financial'),
+        short_title: is_eu ? (localize('CFDs') as string) : (localize('Financial') as string),
     };
-    const financial_stp_config: TAccountTypeConfig = {
+    const financial_stp_config = {
         account_type: 'financial_stp',
         leverage: 100,
-        short_title: localize('Financial STP'),
+        short_title: localize('Financial STP') as string,
     };
 
     return {
@@ -86,41 +68,41 @@ export const getMtCompanies = (is_eu: boolean) => {
             synthetic: {
                 mt5_account_type: synthetic_config.account_type,
                 leverage: synthetic_config.leverage,
-                title: localize('Demo Synthetic'),
+                title: localize('Demo Synthetic') as string,
                 short_title: synthetic_config.short_title,
-            } as TMtAccountDetails,
+            },
             financial: {
                 mt5_account_type: financial_config.account_type,
                 leverage: financial_config.leverage,
-                title: is_eu ? localize('Demo CFDs') : localize('Demo Financial'),
+                title: is_eu ? (localize('Demo CFDs') as string) : (localize('Demo Financial') as string),
                 short_title: financial_config.short_title,
-            } as TMtAccountDetails,
+            },
             financial_stp: {
                 mt5_account_type: financial_stp_config.account_type,
                 leverage: financial_stp_config.leverage,
-                title: localize('Demo Financial STP'),
+                title: localize('Demo Financial STP') as string,
                 short_title: financial_stp_config.short_title,
-            } as TMtAccountDetails,
+            },
         },
         real: {
             synthetic: {
                 mt5_account_type: synthetic_config.account_type,
                 leverage: synthetic_config.leverage,
-                title: localize('Synthetic'),
+                title: localize('Synthetic') as string,
                 short_title: synthetic_config.short_title,
-            } as TMtAccountDetails,
+            },
             financial: {
                 mt5_account_type: financial_config.account_type,
                 leverage: financial_config.leverage,
-                title: is_eu ? localize('CFDs') : localize('Financial'),
+                title: is_eu ? (localize('CFDs') as string) : (localize('Financial') as string),
                 short_title: financial_config.short_title,
-            } as TMtAccountDetails,
+            },
             financial_stp: {
                 mt5_account_type: financial_stp_config.account_type,
                 leverage: financial_stp_config.leverage,
-                title: localize('Financial STP'),
+                title: localize('Financial STP') as string,
                 short_title: financial_stp_config.short_title,
-            } as TMtAccountDetails,
+            },
         },
-    } as const;
+    };
 };

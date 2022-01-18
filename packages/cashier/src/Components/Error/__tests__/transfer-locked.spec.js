773ad0961761590a8f07a097ff3ac92f2fd9cb9a
Bahar <bahar@firstsource.tech>
2022-01-18 10:04:53 +0300

    raise_the coverage_percentage

--- packages/cashier/src/Components/Error/__tests__/transfer-locked.spec.js
+++ packages/cashier/src/Components/Error/__tests__/transfer-locked.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Checklist } from '@deriv/components';
+import { routes } from '@deriv/shared';
 import TransferLocked from '../transfer-locked';
 
 jest.mock('Stores/connect', () => ({
@@ -10,16 +13,26 @@ jest.mock('Stores/connect', () => ({
 }));
 
 describe('TransferLocked', () => {
-    it('Should show the default lock content for every condition', () => {
-        render(<TransferLocked />);
+    it('Should show the default lock content if the account is not financial', () => {
+        render(
+            <TransferLocked
+                is_financial_account={false}
+                is_financial_information_incomplete
+                is_trading_experience_incomplete
+            />
+        );
         expect(screen.getByText('Transfers are locked')).toBeInTheDocument();
     });
 
-    it('Should show the financial assessment form', () => {
+    it('Should show the default lock content if the account is financial and the account information is incomplete', () => {
         render(
-            <TransferLocked is_financial_account is_financial_information_incomplete is_trading_experience_incomplete />
+            <TransferLocked
+                is_financial_account
+                is_financial_information_incomplete={false}
+                is_trading_experience_incomplete
+            />
         );
-        expect(screen.getByText('Complete the financial assessment form')).toBeInTheDocument();
+        expect(screen.getByText('Transfers are locked')).toBeInTheDocument();
     });
 
     it('Should trigger click on the checklist item', () => {
@@ -36,5 +49,23 @@ describe('TransferLocked', () => {
 
         fireEvent.click(btn);
         expect(onClick).toHaveBeenCalled();
+    });
+
+    it('Should show the financial assessment form and redirect to account/financial-assessment when "-->" button clicked', () => {
+        const history = createBrowserHistory();
+        const wrapper = render(
+            <Router history={history}>
+                <TransferLocked
+                    is_financial_account
+                    is_financial_information_incomplete
+                    is_trading_experience_incomplete
+                />
+            </Router>
+        );
+        const btn = wrapper.container.querySelector('.dc-checklist__item-status--action');
+        fireEvent.click(btn);
+
+        expect(history.location.pathname).toBe(routes.financial_assessment);
+        expect(screen.getByText('Complete the financial assessment form')).toBeInTheDocument();
     });
 });

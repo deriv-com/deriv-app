d024f4e9b1e1ae01a4c44bf9ac012465348e92e4
Bala <bala.krishna@regentmarkets.com>
2022-01-21 11:08:17 +0800

    chore: remove chart loader

--- packages/trader/src/App/Components/Elements/chart-loader.jsx
+++ packages/trader/src/App/Components/Elements/chart-loader.jsx
@@ -2,12 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Loading from '../../../templates/app/components/loading';
 
-const ChartLoader = ({ is_dark, is_visible }) =>
-    is_visible ? (
-        <div className='chart-container__loader'>
-            <Loading theme={is_dark ? 'dark' : 'light'} />
-        </div>
-    ) : null;
+const ChartLoader = ({ is_dark, is_visible }) => null;
 
 ChartLoader.propTypes = {
     is_dark: PropTypes.bool,

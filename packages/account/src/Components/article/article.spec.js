a9457e195215ef4a71fb298e0f2a9bd53ae70418
“Nikita” <nikita@deriv.com>
2022-01-21 16:15:55 +0300

    test: remove eslint conflicted test case

--- packages/account/src/Components/article/article.spec.js
+++ packages/account/src/Components/article/article.spec.js
@@ -1,33 +1,24 @@
-import React from "react";
-import { screen, render, fireEvent } from "@testing-library/react";
-import Article from "./article";
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import Article from './article';
 
-describe("<Article/>", () => {
+describe('<Article/>', () => {
     const props = {
         descriptions: [<>description</>],
-        title: "title",
+        title: 'title',
         onClickLearnMore: jest.fn(),
-    }
+    };
 
-    it("should show proper descriptions", () => {
+    it('should show proper descriptions', () => {
         const { rerender } = render(<Article {...props} />);
 
-        expect(screen.getByText("description")).toBeInTheDocument();
-        expect(screen.getByText("title")).toBeInTheDocument();
-        expect(screen.getByText("Learn more")).toBeInTheDocument();
+        expect(screen.getByText('description')).toBeInTheDocument();
+        expect(screen.getByText('title')).toBeInTheDocument();
+        expect(screen.getByText('Learn more')).toBeInTheDocument();
 
         rerender(<Article {...props} descriptions={[<>desc1</>, <>desc2</>]} />);
 
-        expect(screen.getByText("desc1")).toBeInTheDocument();
-        expect(screen.getByText("desc2")).toBeInTheDocument();
-    })
-
-    it("should trigger onClick callback on learn more", () => {
-        const { container } = render(<Article {...props} />);
-        const learn_more_div = container.querySelector('.da-article__learn-more');
-
-        fireEvent.click(learn_more_div);
-
-        expect(props.onClickLearnMore).toHaveBeenCalledTimes(1);
-    })
-})
+        expect(screen.getByText('desc1')).toBeInTheDocument();
+        expect(screen.getByText('desc2')).toBeInTheDocument();
+    });
+});

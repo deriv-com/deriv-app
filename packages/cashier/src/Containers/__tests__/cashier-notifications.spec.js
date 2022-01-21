233d0445a279e5ffc921db44fcdc1cee09beb2ca
Farrah Mae Ochoa <farrah@deriv.com>
2022-01-21 11:47:52 +0400

    refactor code

--- packages/cashier/src/Containers/__tests__/cashier-notifications.spec.js
+++ packages/cashier/src/Containers/__tests__/cashier-notifications.spec.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CashierNotifications from '../cashier-notifications';
-import { Icon, Counter } from '@deriv/components';
 
-jest.mock('@deriv/components', () => ({
-    __esModule: true,
-    Icon: jest.fn(() => 'mockedIcon'),
-    Counter: jest.fn(() => 'mockedCounter'),
-}));
+jest.mock('@deriv/components', () => {
+    const original_module = jest.requireActual('@deriv/components');
+
+    return {
+        ...original_module,
+        Icon: jest.fn(() => 'mockedIcon'),
+        Counter: jest.fn(() => 'mockedCounter'),
+    };
+});
 
 describe('<CashierNotifications />', () => {
     it('should only show the icon if there are no p2p notifications', () => {
         render(<CashierNotifications p2p_notification_count={0} />);
 
-        expect(Icon).toHaveBeenCalled();
+        expect(screen.getByText('mockedIcon')).toBeInTheDocument();
     });
 
-    it('should show the no. of notifications if there are p2p notifications', () => {
+    it('should show the icon and no. of notifications if there are p2p notifications', () => {
         render(<CashierNotifications p2p_notification_count={2} />);
 
-        expect(Icon).toHaveBeenCalled();
-        expect(Counter).toHaveBeenCalled();
+        expect(screen.getByText(/mockedIcon/i)).toBeInTheDocument();
+        expect(screen.getByText(/mockedCounter/i)).toBeInTheDocument();
     });
 });

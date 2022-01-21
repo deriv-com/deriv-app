5b3128d28ab236028d068701d45edf7951a4147f
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-01-21 14:31:40 +0100

    fixing bug where state changes when component is loaded, causing infinite loading

--- packages/trader/src/Modules/Trading/Components/Form/screen-large.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/screen-large.jsx
@@ -7,27 +7,40 @@ import ContractType from '../../Containers/contract-type.jsx';
 import Purchase from '../../Containers/purchase.jsx';
 import TradeParams from '../../Containers/trade-params.jsx';
 
-const ScreenLarge = ({ is_market_closed, is_trade_enabled }) => (
-    <div
-        className={classNames('sidebar__items', {
-            'sidebar__items--market-closed': is_market_closed,
-        })}
-    >
-        {!is_trade_enabled ? (
-            <TradeParamsLoader speed={2} />
-        ) : (
-            <React.Fragment>
-                <Fieldset className='trade-container__fieldset trade-types'>
-                    <ContractType />
-                </Fieldset>
-                <TradeParams />
-                <div className='purchase-container'>
-                    <Purchase is_market_closed={is_market_closed} />
-                </div>
-            </React.Fragment>
-        )}
-    </div>
-);
+const ScreenLarge = ({ is_market_closed, is_trade_enabled }) => {
+
+    const [is_trade_enabled_state, setTradeEnabledState] = React.useState(false);
+
+    React.useEffect(() => {
+        if (is_trade_enabled) {
+            setTradeEnabledState(true);
+        } else {
+            setTradeEnabledState(false);
+        };
+    }, [is_trade_enabled])
+
+    return (
+        <div
+            className={classNames('sidebar__items', {
+                'sidebar__items--market-closed': is_market_closed,
+            })}
+        >
+            {!is_trade_enabled_state ? (
+                <TradeParamsLoader speed={2} />
+            ) : (
+                <React.Fragment>
+                    <Fieldset className='trade-container__fieldset trade-types'>
+                        <ContractType />
+                    </Fieldset>
+                    <TradeParams />
+                    <div className='purchase-container'>
+                        <Purchase is_market_closed={is_market_closed} />
+                    </div>
+                </React.Fragment>
+            )}
+        </div>
+    )
+};
 
 ScreenLarge.propTypes = {
     is_market_closed: PropTypes.bool,

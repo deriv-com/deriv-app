b17d87eaee1a1d3769c8984e97f3ed471a62dc56
george-usynin-deriv <heorhi@deriv.com>
2022-01-21 15:14:44 +0300

    refactored declaring of properties, added rerender method

--- packages/cashier/src/Containers/__tests__/deposit.spec.js
+++ packages/cashier/src/Containers/__tests__/deposit.spec.js
@@ -22,7 +22,7 @@ jest.mock('Components/cashier-container', () => () => <div>CashierContainer</div
 jest.mock('Components/CashierDefault/cashier-default', () => () => <div>CashierDefault</div>);
 
 describe('<Deposit />', () => {
-    const mockDefaultProps = () => ({
+    const props = {
         currency: 'USD',
         current_currency_type: 'fiat',
         error: { is_ask_uk_funds_protection: false, message: '' },
@@ -41,130 +41,80 @@ describe('<Deposit />', () => {
         setIsDeposit: jest.fn(),
         setErrorMessage: jest.fn(),
         setSideNotes: jest.fn(),
-    });
+    };
+
     it('should render <Loading /> component', () => {
-        const renderWithCustomProperties = properties => {
-            const props = mockDefaultProps();
-            props.iframe_url = '';
-            for (let [prop, value] of Object.entries(properties)) {
-                props[prop] = value;
-            }
+        const { rerender } = render(<Deposit {...props} is_switching is_loading={false} iframe_url='' />);
 
-            const { unmount } = render(<Deposit {...props} />);
+        expect(screen.getByText('Loading')).toBeInTheDocument();
 
-            expect(screen.getByText('Loading')).toBeInTheDocument();
-
-            unmount();
-        };
-
-        renderWithCustomProperties({ is_switching: true, is_loading: false });
-        renderWithCustomProperties({ is_switching: false, is_loading: true });
+        rerender(<Deposit {...props} is_switching={false} is_loading iframe_url='' />);
     });
 
     it('should render <Virtual /> component', () => {
-        const props = mockDefaultProps();
-        props.is_virtual = true;
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_virtual />);
 
         expect(screen.getByText('Virtual')).toBeInTheDocument();
     });
 
     it('should render <CashierLocked /> component', () => {
-        const renderWithCustomProperties = properties => {
-            const props = mockDefaultProps();
+        const { rerender } = render(<Deposit {...props} is_system_maintenance is_cashier_locked />);
 
-            for (let [prop, value] of Object.entries(properties)) {
-                props[prop] = value;
-            }
+        expect(screen.getByText('CashierLocked')).toBeInTheDocument();
 
-            const { unmount } = render(<Deposit {...props} />);
+        rerender(<Deposit {...props} is_system_maintenance is_deposit_locked current_currency_type='crypto' />);
 
-            expect(screen.getByText('CashierLocked')).toBeInTheDocument();
+        expect(screen.getByText('CashierLocked')).toBeInTheDocument();
 
-            unmount();
-        };
+        rerender(<Deposit {...props} is_cashier_locked />);
 
-        renderWithCustomProperties({ is_system_maintenance: true, is_cashier_locked: true });
-        renderWithCustomProperties({
-            is_system_maintenance: true,
-            is_deposit_locked: true,
-            current_currency_type: 'crypto',
-        });
-        renderWithCustomProperties({ is_cashier_locked: true });
+        expect(screen.getByText('CashierLocked')).toBeInTheDocument();
     });
 
     it('should render <FundsProtection /> component', () => {
-        const props = mockDefaultProps();
-        props.error.is_ask_uk_funds_protection = true;
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} error={{ is_ask_uk_funds_protection: true }} />);
 
         expect(screen.getByText('FundsProtection')).toBeInTheDocument();
     });
 
     it('should render <DepositsLocked /> component', () => {
-        const props = mockDefaultProps();
-        props.is_deposit_locked = true;
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_deposit_locked />);
 
         expect(screen.getByText('DepositsLocked')).toBeInTheDocument();
     });
 
     it('should render <CryptoTransactionsHistory /> component', () => {
-        const props = mockDefaultProps();
-        props.is_crypto_transactions_visible = true;
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_crypto_transactions_visible />);
 
         expect(screen.getByText('CryptoTransactionsHistory')).toBeInTheDocument();
     });
 
     it('should render <Error /> component', () => {
-        const props = mockDefaultProps();
-        props.is_deposit = true;
-        props.error.message = 'error';
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_deposit error={{ message: 'error' }} />);
 
         expect(screen.getByText('Error')).toBeInTheDocument();
     });
 
     it('should render <CryptoDeposit /> component', () => {
-        const props = mockDefaultProps();
-        props.is_eu = true;
-        props.currency = 'BTC';
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_eu currency='BTC' />);
 
         expect(screen.getByText('CryptoDeposit')).toBeInTheDocument();
     });
 
     it('should render <CashierContainer /> component', () => {
-        const props = mockDefaultProps();
-        props.is_deposit = true;
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_deposit />);
 
         expect(screen.getByText('CashierContainer')).toBeInTheDocument();
     });
 
     it('should render <CashierDefault /> component', () => {
-        const props = mockDefaultProps();
-
         render(<Deposit {...props} />);
 
         expect(screen.getByText('CashierDefault')).toBeInTheDocument();
     });
 
     it('should trigger "setSideNotes" callback', () => {
-        const props = mockDefaultProps();
-        props.is_deposit = true;
-        props.crypto_transactions = [{}];
-        props.currency = 'UST';
-
-        render(<Deposit {...props} />);
+        render(<Deposit {...props} is_deposit crypto_transactions={[{}]} currency='UST' />);
 
         expect(props.setSideNotes).toHaveBeenCalled();
     });

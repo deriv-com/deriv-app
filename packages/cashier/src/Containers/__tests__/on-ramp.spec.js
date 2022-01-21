71bf82e46d3a0111a34e574253902f2ef1b4edd7
george-usynin-deriv <heorhi@deriv.com>
2022-01-21 19:01:03 +0300

    refactored code, added beforeEach function

--- packages/cashier/src/Containers/__tests__/on-ramp.spec.js
+++ packages/cashier/src/Containers/__tests__/on-ramp.spec.js
@@ -23,52 +23,46 @@ jest.mock('Components/on-ramp-provider-card', () => () => <div>OnRampProviderCar
 jest.mock('Components/on-ramp-provider-popup', () => () => <div>OnRampProviderPopup</div>);
 
 describe('<OnRamp />', () => {
-    const mockDefaultProps = () => ({
-        filtered_onramp_providers: [{ name: 'name' }],
-        is_cashier_locked: false,
-        is_deposit_locked: false,
-        is_loading: false,
-        is_switching: false,
-        should_show_dialog: false,
-        onMountOnramp: jest.fn(),
-        onUnmountOnramp: jest.fn(),
-        setIsOnRampModalOpen: jest.fn(),
-        setSideNotes: jest.fn(),
-        routeTo: jest.fn(),
+    let props;
+    beforeEach(() => {
+        props = {
+            filtered_onramp_providers: [{ name: 'name' }],
+            is_cashier_locked: false,
+            is_deposit_locked: false,
+            is_loading: false,
+            is_switching: false,
+            should_show_dialog: false,
+            onMountOnramp: jest.fn(),
+            onUnmountOnramp: jest.fn(),
+            setIsOnRampModalOpen: jest.fn(),
+            setSideNotes: jest.fn(),
+            routeTo: jest.fn(),
+        };
     });
 
     it('should render <Loading /> component', () => {
-        const renderWithCustomProperty = property => {
-            const props = mockDefaultProps();
-            props[property] = true;
-            const { unmount } = render(<OnRamp {...props} />);
+        const { rerender } = render(<OnRamp {...props} is_loading />);
 
-            expect(screen.getByText('Loading')).toBeInTheDocument();
-            unmount();
-        };
+        expect(screen.getByText('Loading')).toBeInTheDocument();
 
-        renderWithCustomProperty('is_loading');
-        renderWithCustomProperty('is_switching');
+        rerender(<OnRamp {...props} is_switching />);
+
+        expect(screen.getByText('Loading')).toBeInTheDocument();
     });
 
     it('should render <CashierLocked /> component', () => {
-        const props = mockDefaultProps();
-        props.is_cashier_locked = true;
-        render(<OnRamp {...props} />);
+        render(<OnRamp {...props} is_cashier_locked />);
 
         expect(screen.getByText('CashierLocked')).toBeInTheDocument();
     });
 
     it('should render <DepositLocked /> component', () => {
-        const props = mockDefaultProps();
-        props.is_deposit_locked = true;
-        render(<OnRamp {...props} />);
+        render(<OnRamp {...props} is_deposit_locked />);
 
         expect(screen.getByText('DepositLocked')).toBeInTheDocument();
     });
 
     it('should render <OnRampProviderCard /> component and "Select payment channel" message', () => {
-        const props = mockDefaultProps();
         render(<OnRamp {...props} />);
 
         expect(screen.getByText('Select payment channel')).toBeInTheDocument();
@@ -80,9 +74,7 @@ describe('<OnRamp />', () => {
         modal_root_el.setAttribute('id', 'modal_root');
         document.body.appendChild(modal_root_el);
 
-        const props = mockDefaultProps();
-        props.onramp_popup_modal_title = 'Title of the onramp popup modal';
-        render(<OnRamp {...props} is_onramp_modal_open />);
+        render(<OnRamp {...props} is_onramp_modal_open onramp_popup_modal_title='Title of the onramp popup modal' />);
 
         expect(screen.getByText('Title of the onramp popup modal')).toBeInTheDocument();
         expect(screen.getByText('OnRampProviderPopup')).toBeInTheDocument();
@@ -95,9 +87,7 @@ describe('<OnRamp />', () => {
         modal_root_el.setAttribute('id', 'modal_root');
         document.body.appendChild(modal_root_el);
 
-        const props = mockDefaultProps();
-        props.onramp_popup_modal_title = 'Title of the onramp popup modal';
-        render(<OnRamp {...props} is_onramp_modal_open />);
+        render(<OnRamp {...props} is_onramp_modal_open onramp_popup_modal_title='Title of the onramp popup modal' />);
 
         const close_cross_btn = screen.getByRole('button', { name: '' });
         fireEvent.click(close_cross_btn);
@@ -107,8 +97,6 @@ describe('<OnRamp />', () => {
     });
 
     it('should trigger "setSideNotes" callback in Desktop mode', () => {
-        const props = mockDefaultProps();
-
         render(<OnRamp {...props} />);
 
         expect(props.setSideNotes).toHaveBeenCalledTimes(1);
@@ -116,7 +104,6 @@ describe('<OnRamp />', () => {
 
     it('should show "What is Fiat onramp?" message and render <ReadMore /> component in Mobile mode', () => {
         isMobile.mockReturnValue(true);
-        const props = mockDefaultProps();
 
         render(<OnRamp {...props} />);
 
@@ -126,7 +113,6 @@ describe('<OnRamp />', () => {
 
     it('should have proper menu options in Mobile mode', () => {
         isMobile.mockReturnValue(true);
-        const props = mockDefaultProps();
         props.menu_options = [
             {
                 label: 'Deposit',
@@ -148,7 +134,6 @@ describe('<OnRamp />', () => {
 
     it('should trigger "routeTo" callback when the user chooses a different from "Fiat onramp" option in Mobile mode', () => {
         isMobile.mockReturnValue(true);
-        const props = mockDefaultProps();
         props.menu_options = [
             {
                 label: 'Deposit',

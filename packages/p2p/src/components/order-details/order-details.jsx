1f8dd4c027d4c370cf5ed16039961a76458b5177
Carol Sachdeva <carol@binary.com>
2022-01-26 18:08:31 +0800

    fix: incorrect price issue

--- packages/p2p/src/components/order-details/order-details.jsx
+++ packages/p2p/src/components/order-details/order-details.jsx
@@ -18,6 +18,7 @@ const OrderDetails = observer(({ onPageReturn }) => {
     const {
         account_currency,
         advert_details,
+        amount,
         amount_display,
         chat_channel_url: order_channel_url,
         contact_info,
@@ -32,7 +33,7 @@ const OrderDetails = observer(({ onPageReturn }) => {
         local_currency,
         other_user_details,
         payment_info,
-        price,
+        // price, TODO: Uncomment when price is fixed
         purchase_time,
         rate,
         should_highlight_alert,
@@ -92,7 +93,7 @@ const OrderDetails = observer(({ onPageReturn }) => {
                             )}
                             {!has_timer_expired && (is_pending_order || is_buyer_confirmed_order) && (
                                 <div className='order-details-card__header-amount'>
-                                    {getFormattedText(price, local_currency)}
+                                    {getFormattedText(amount * rate, local_currency)}
                                 </div>
                             )}
                             <div className='order-details-card__header-id'>
@@ -126,7 +127,7 @@ const OrderDetails = observer(({ onPageReturn }) => {
                             <div className='order-details-card__info--left'>
                                 <OrderInfoBlock
                                     label={labels.left_send_or_receive}
-                                    value={getFormattedText(price, local_currency)}
+                                    value={getFormattedText(amount * rate, local_currency)}
                                 />
                                 <OrderInfoBlock
                                     label={localize('Rate (1 {{ account_currency }})', { account_currency })}

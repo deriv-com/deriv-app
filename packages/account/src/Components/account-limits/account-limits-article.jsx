829b734b1159871abacc0af552d7dede69034392
Ashraf Ali <ashrafali@Ashrafs-MacBook-Pro.local>
2022-01-26 14:21:15 +0800

    test commit1

--- packages/account/src/Components/account-limits/account-limits-article.jsx
+++ packages/account/src/Components/account-limits/account-limits-article.jsx
@@ -6,7 +6,7 @@ import AccountArticle from 'Components/article/article.jsx';
 const AccountLimitsArticle = () => {
     return (
         <AccountArticle
-            title={localize('Account limits')}
+            title={localize('Account limits 123')}
             descriptions={[
                 <Localize key={0} i18n_default_text='These are default limits that we apply to your accounts.' />,
                 <Localize

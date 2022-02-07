fffd5a2f72f678c2330d2b9a60c3df3a28ede0e8
niloo-fs <niloofar.sadeghi@firstsource.tech>
2022-02-07 12:00:48 +0330

    .

--- packages/account/src/Components/form-footer/__tests__/form-footer.spec.js
+++ packages/account/src/Components/form-footer/__tests__/form-footer.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import FormFooter from '../form-footer';
+import { render, screen } from '@testing-library/react';
 
 const MockFormFooter = props => {
     return (

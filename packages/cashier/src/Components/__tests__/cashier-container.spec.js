d356886746fd83e69215dac8e18315ecdc69d63c
Likhith Kolayari <likhith@regentmarkets.com>
2022-02-09 11:17:55 +0400

    Improved code visibility

--- packages/cashier/src/Components/__tests__/cashier-container.spec.js
+++ packages/cashier/src/Components/__tests__/cashier-container.spec.js
@@ -13,7 +13,7 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('<CashierContainer/>', () => {
-    const URL = `https://doughflow-test.4x.my/cashier/login.asp?Sportsbook=test&PIN=CR90000010&Lang=en&Password=20192eef9f1c8258354030632a913916fe331c8f&Secret=11eb12e081e93780306d7c8a7483748a&Action=DEPOSIT&udef1=EN&udef2=deriv&theme=light`;
+    const url = `https://doughflow-test.4x.my/cashier/login.asp?Sportsbook=test&PIN=CR90000010&Lang=en&Password=20192eef9f1c8258354030632a913916fe331c8f&Secret=11eb12e081e93780306d7c8a7483748a&Action=DEPOSIT&udef1=EN&udef2=deriv&theme=light`;
     let clearIFrame = null;
 
     beforeAll(() => {
@@ -21,8 +21,9 @@ describe('<CashierContainer/>', () => {
     });
 
     it('should render the component with iframe when iframe_url is true', () => {
-        const { queryByTestId, container } = render(<CashierContainer iframe_url={URL} clearIframe={clearIFrame} />);
+        const { queryByTestId, container } = render(<CashierContainer iframe_url={url} clearIframe={clearIFrame} />);
         const el_loader = container.querySelector('.barspinner');
+
         expect(el_loader).toBeNull();
         expect(queryByTestId('doughflow_section')).toBeTruthy();
         expect(queryByTestId('cashier_section')).toBeNull();
@@ -31,6 +32,7 @@ describe('<CashierContainer/>', () => {
     it('should render the crypto section when is_crypto is true', () => {
         const { queryByTestId, container } = render(<CashierContainer is_crypto clearIframe={clearIFrame} />);
         const el_loader = container.querySelector('.barspinner');
+
         expect(el_loader).toBeNull();
         expect(queryByTestId('cashier_section')).toBeTruthy();
         expect(queryByTestId('doughflow_section')).toBeNull();
@@ -39,6 +41,7 @@ describe('<CashierContainer/>', () => {
     it('should render the loading when is_loading is true', () => {
         const { queryByTestId, container } = render(<CashierContainer clearIframe={clearIFrame} is_loading />);
         const el_loader = container.querySelector('.barspinner');
+
         expect(el_loader).toBeTruthy();
         expect(queryByTestId('doughflow_section')).toBeNull();
         expect(queryByTestId('cashier_section')).toBeNull();
@@ -46,9 +49,10 @@ describe('<CashierContainer/>', () => {
 
     it('will display doughflow and crypto if all props are provided', () => {
         const { queryByTestId, container } = render(
-            <CashierContainer is_crypto iframe_url={URL} clearIframe={clearIFrame} is_loading />
+            <CashierContainer is_crypto iframe_url={url} clearIframe={clearIFrame} is_loading />
         );
         const el_loader = container.querySelector('.barspinner');
+
         expect(el_loader).toBeTruthy();
         expect(queryByTestId('cashier_section')).toBeTruthy();
         expect(queryByTestId('doughflow_section')).toBeTruthy();
@@ -57,6 +61,7 @@ describe('<CashierContainer/>', () => {
     it('should navigate to Fiat onramp page when `Try our Fiat onramp` button is clicked', () => {
         const { getByRole } = render(<CashierContainer is_crypto clearIframe={clearIFrame} />);
         fireEvent.click(getByRole('button'));
+
         expect(mockHistoryPush).toHaveBeenCalledWith(routes.cashier_onramp);
     });
 });

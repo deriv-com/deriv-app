a9747a2d8a6793596cce387ffe08fbf842b830e8
yauheni-deriv <yauheni@deriv.com>
2022-02-10 10:30:55 +0300

    curly braces remove

--- packages/account/src/Components/unlink-modal/__tests__/unlink-modal.spec.js
+++ packages/account/src/Components/unlink-modal/__tests__/unlink-modal.spec.js
@@ -8,7 +8,7 @@ document.body.appendChild(modalRoot);
 
 describe('<UnlinkModal />', () => {
     it('should show the proper messages', () => {
-        render(<UnlinkModal identifier_title={'title'} is_open />);
+        render(<UnlinkModal identifier_title='title' is_open />);
 
         expect(screen.getByText('Are you sure you want to unlink from title?')).toBeInTheDocument();
         expect(screen.getByText('You will need to set a password to complete the process.')).toBeInTheDocument();

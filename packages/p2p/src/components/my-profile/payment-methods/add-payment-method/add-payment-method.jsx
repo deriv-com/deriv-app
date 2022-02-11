7f0ff8f9bc7b38b45f37bc7d4f0e512c373c825b
Khalid Ibrahim <khalid@deriv.com>
2022-02-11 16:27:17 +0800

    fix: pm issues

--- packages/p2p/src/components/my-profile/payment-methods/add-payment-method/add-payment-method.jsx
+++ packages/p2p/src/components/my-profile/payment-methods/add-payment-method/add-payment-method.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStores } from 'Stores';
+import PageReturn from 'Components/page-return/page-return.jsx';
 import { localize } from 'Components/i18next';
 import { MobileFullPageModal } from '@deriv/components';
 import AddPaymentMethodForm from './add-payment-method-form.jsx';
 import SelectPaymentMethod from './select-payment-method.jsx';
+import { isDesktop } from '@deriv/shared';
 
-const AddPaymentMethod = ({ should_show_separated_footer }) => {
+const AddPaymentMethod = ({ should_show_page_return = true, should_show_separated_footer }) => {
     const { my_profile_store } = useStores();
 
     React.useEffect(() => {
@@ -16,7 +18,21 @@ const AddPaymentMethod = ({ should_show_separated_footer }) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return (
+    return isDesktop() ? (
+        <React.Fragment>
+            {should_show_page_return && (
+                <PageReturn
+                    onClick={my_profile_store.hideAddPaymentMethodForm}
+                    page_title={localize('Add payment method')}
+                />
+            )}
+            {my_profile_store.selected_payment_method ? (
+                <AddPaymentMethodForm should_show_separated_footer={should_show_separated_footer} />
+            ) : (
+                <SelectPaymentMethod />
+            )}
+        </React.Fragment>
+    ) : (
         <MobileFullPageModal
             body_className='add-payment-method__modal'
             is_modal_open={my_profile_store.should_show_add_payment_method_form}

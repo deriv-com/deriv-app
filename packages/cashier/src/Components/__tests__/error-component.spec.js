1632b3a5a1ea863bd884b0b32858da3126a9d8c5
ashrafali-v <ashrafalifrk@gmail.com>
2022-02-15 10:22:08 +0800

    error component test case initial commit

---
+++ packages/cashier/src/Components/__tests__/error-component.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import ErrorComponent from '../error-component';
+
+// jest.mock('@deriv/components', () => ({
+//     ...jest.requireActual('@deriv/components'),
+//     // eslint-disable-next-line react/display-name
+//     PageError: () => <div>PageError</div>,
+// }));
+
+describe('<ErrorComponent/>', () => {
+    it('should show the default message when message is not passed', () => {
+        const message='';
+        render(<ErrorComponent message={message}/>);
+        expect(screen.getByText('Sorry, an error occured while processing your request.')).toBeInTheDocument();
+    });
+
+    it('should show the actual message when message is passed', () => {
+        const message='This is the error message';
+        render(<ErrorComponent message={message}/>);
+        expect(screen.getByText(message)).toBeInTheDocument();
+    });
+
+    it('should show refresh message when should_show_refresh is true', () => {
+        render(<ErrorComponent should_show_refresh={true}/>);
+        expect(screen.getByText('Please refresh this page to continue.')).toBeInTheDocument();
+    });
+
+    // it('should show refresh text when redirect_label is true ', () => {
+    //     render(<ErrorComponent />);
+    //     expect(screen.getByText('Refresh')).toBeInTheDocument();
+    // });
+});

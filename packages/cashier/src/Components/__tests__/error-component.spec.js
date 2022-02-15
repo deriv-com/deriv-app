731891ee69efb0ebfbd50ba68bfe77f1f90598bf
ashrafali-v <ashrafalifrk@gmail.com>
2022-02-15 10:53:28 +0800

    feat: add error component test

--- packages/cashier/src/Components/__tests__/error-component.spec.js
+++ packages/cashier/src/Components/__tests__/error-component.spec.js
@@ -10,24 +10,24 @@ import ErrorComponent from '../error-component';
 
 describe('<ErrorComponent/>', () => {
     it('should show the default message when message is not passed', () => {
-        const message='';
-        render(<ErrorComponent message={message}/>);
+        const message = '';
+        render(<ErrorComponent message={message} />);
         expect(screen.getByText('Sorry, an error occured while processing your request.')).toBeInTheDocument();
     });
 
     it('should show the actual message when message is passed', () => {
-        const message='This is the error message';
-        render(<ErrorComponent message={message}/>);
+        const message = 'This is the error message';
+        render(<ErrorComponent message={message} />);
         expect(screen.getByText(message)).toBeInTheDocument();
     });
 
     it('should show refresh message when should_show_refresh is true', () => {
-        render(<ErrorComponent should_show_refresh={true}/>);
+        render(<ErrorComponent should_show_refresh={true} />);
         expect(screen.getByText('Please refresh this page to continue.')).toBeInTheDocument();
     });
 
-    // it('should show refresh text when redirect_label is true ', () => {
-    //     render(<ErrorComponent />);
-    //     expect(screen.getByText('Refresh')).toBeInTheDocument();
-    // });
+    it('should show refresh text when redirect_label is true ', () => {
+        render(<ErrorComponent redirect_label={true}/>);
+        expect(screen.getByText('Refresh')).toBeInTheDocument();
+    });
 });

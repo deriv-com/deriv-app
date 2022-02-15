35aa1b7121b0a38cd158b3e3e428be606850e02e
artsemkavaleu-deriv <artsem@deriv.com>
2022-02-15 14:59:23 +0300

    fix eslint warnings

--- packages/p2p/src/components/page-return/__tests__/page-return.spec.js
+++ packages/p2p/src/components/page-return/__tests__/page-return.spec.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import PageReturn from '../page-return';
+import PageReturn from '../page-return.jsx';
 
 describe('<PageReturn />', () => {
     it('Component should be rendered', () => {
-        const { container } = render(<PageReturn />);
+        render(<PageReturn />);
 
-        expect(container.querySelector('.page-return')).toBeInTheDocument();
+        expect(screen.getByTestId('parentContainer')).toHaveClass('page-return');
     });
 
     it('Should render proper className passed from props', async () => {
-        const { container } = render(<PageReturn className={'test'}/>);
+        render(<PageReturn className={'test'}/>);
 
-        expect(await container.firstChild).toHaveClass('page-return test');
+        expect(await screen.findByTestId('parentContainer')).toHaveClass('page-return test');
     });
 
     it('function passed from props should be called', () => {
         const mockFunc = jest.fn();
 
-        const { container } = render(<PageReturn onClick={mockFunc}/>);
+        render(<PageReturn onClick={mockFunc}/>);
 
-        const btn = container.querySelector('.page-return__button');
+        const btn = screen.getByTestId('iconContainer');
         fireEvent.click(btn);
 
         expect(mockFunc).toHaveBeenCalledTimes(1);

7121805ae1b09a3b8d06983619c46d0b8bf1a499
Carol Sachdeva <carol@binary.com>
2022-02-16 13:06:40 +0800

    UI shit

--- packages/p2p/src/components/buy-sell/buy-sell-table.jsx
+++ packages/p2p/src/components/buy-sell/buy-sell-table.jsx
@@ -9,6 +9,7 @@ import Empty from 'Components/empty/empty.jsx';
 import { TableError } from 'Components/table/table-error.jsx';
 import { useStores } from 'Stores';
 import BuySellRow from './buy-sell-row.jsx';
+import CancelAddPaymentMethodModal from '../my-profile/payment-methods/add-payment-method/cancel-add-payment-method-modal.jsx';
 
 const BuySellRowRendererComponent = row_props => {
     const { buy_sell_store } = useStores();
@@ -56,71 +57,74 @@ const BuySellTable = ({ onScroll }) => {
 
     if (buy_sell_store.items.length) {
         return (
-            <Table className='buy-sell__table'>
-                <Modal
-                    name='sort'
-                    className='sort'
-                    is_open={buy_sell_store.is_sort_dropdown_open}
-                    height='10rem'
-                    width='80vw'
-                >
-                    <RadioGroup
-                        name='reason'
-                        className='sort-radiogroup'
-                        onToggle={buy_sell_store.handleChange}
-                        selected={buy_sell_store.selected_value}
-                        required
+            <>
+                <CancelAddPaymentMethodModal />
+                <Table className='buy-sell__table'>
+                    <Modal
+                        name='sort'
+                        className='sort'
+                        is_open={buy_sell_store.is_sort_dropdown_open}
+                        height='10rem'
+                        width='80vw'
                     >
-                        {buy_sell_store.sort_list.map((list_item, key) => {
-                            return (
-                                <RadioGroup.Item
-                                    key={key}
-                                    value={list_item.value}
-                                    label={
-                                        <Text color='prominent' size='s'>
-                                            {list_item.text}
-                                        </Text>
-                                    }
-                                />
-                            );
-                        })}
-                    </RadioGroup>
-                </Modal>
-                {isDesktop() && (
-                    <Table.Header>
-                        <Table.Row className='buy-sell__table-header'>
-                            <Table.Head>
-                                <Localize i18n_default_text='Advertisers' />
-                            </Table.Head>
-                            <Table.Head>
-                                <Localize i18n_default_text='Limits' />
-                            </Table.Head>
-                            <Table.Head>
-                                <Localize
-                                    i18n_default_text='Rate (1 {{currency}})'
-                                    values={{ currency: general_store.client.currency }}
-                                />
-                            </Table.Head>
-                            <Table.Head>
-                                <Localize i18n_default_text='Payment methods' />
-                            </Table.Head>
-                            <Table.Head />
-                        </Table.Row>
-                    </Table.Header>
-                )}
-                <Table.Body className='buy-sell__table-body'>
-                    <InfiniteDataList
-                        data_list_className='buy-sell__data-list'
-                        items={buy_sell_store.rendered_items}
-                        rowRenderer={props => <BuySellRowRenderer {...props} />}
-                        loadMoreRowsFn={buy_sell_store.loadMoreItems}
-                        has_filler
-                        has_more_items_to_load={buy_sell_store.has_more_items_to_load}
-                        keyMapperFn={item => item.id}
-                        onScroll={onScroll}
-                    />
-                </Table.Body>
-            </Table>
+                        <RadioGroup
+                            name='reason'
+                            className='sort-radiogroup'
+                            onToggle={buy_sell_store.handleChange}
+                            selected={buy_sell_store.selected_value}
+                            required
+                        >
+                            {buy_sell_store.sort_list.map((list_item, key) => {
+                                return (
+                                    <RadioGroup.Item
+                                        key={key}
+                                        value={list_item.value}
+                                        label={
+                                            <Text color='prominent' size='s'>
+                                                {list_item.text}
+                                            </Text>
+                                        }
+                                    />
+                                );
+                            })}
+                        </RadioGroup>
+                    </Modal>
+                    {isDesktop() && (
+                        <Table.Header>
+                            <Table.Row className='buy-sell__table-header'>
+                                <Table.Head>
+                                    <Localize i18n_default_text='Advertisers' />
+                                </Table.Head>
+                                <Table.Head>
+                                    <Localize i18n_default_text='Limits' />
+                                </Table.Head>
+                                <Table.Head>
+                                    <Localize
+                                        i18n_default_text='Rate (1 {{currency}})'
+                                        values={{ currency: general_store.client.currency }}
+                                    />
+                                </Table.Head>
+                                <Table.Head>
+                                    <Localize i18n_default_text='Payment methods' />
+                                </Table.Head>
+                                <Table.Head />
+                            </Table.Row>
+                        </Table.Header>
+                    )}
+                    <Table.Body className='buy-sell__table-body'>
+                        <InfiniteDataList
+                            data_list_className='buy-sell__data-list'
+                            items={buy_sell_store.rendered_items}
+                            rowRenderer={props => <BuySellRowRenderer {...props} />}
+                            loadMoreRowsFn={buy_sell_store.loadMoreItems}
+                            has_filler
+                            has_more_items_to_load={buy_sell_store.has_more_items_to_load}
+                            keyMapperFn={item => item.id}
+                            onScroll={onScroll}
+                        />
+                    </Table.Body>
+                </Table>
+            </>
         );
     }
 

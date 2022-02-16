b4c5d08fe66d721cf5cafc8b4a4e2ae57b8b614b
Likhith Kolayari <likhith@regentmarkets.com>
2022-02-16 12:42:41 +0400

    Increased coverage for App component

--- packages/p2p/src/components/__test__/app.spec.js
+++ packages/p2p/src/components/__test__/app.spec.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { useStores } from 'Stores';
+import { setLanguage } from '../i18next';
 import App from '../app';
 
 jest.mock('Stores', () => ({
     ...jest.requireActual('Stores'),
-    useStores: jest.fn(() => ({
+    useStores: jest.fn().mockReturnValue({
         general_store: {
             onUnmount: jest.fn(),
             setAppProps: jest.fn(),
             setWebsocketInit: jest.fn(),
             redirectTo: jest.fn(),
+            onMount: jest.fn(),
             client: {
                 local_currency_config: {
                     decimal_places: 1,
                 },
             },
+            path: {
+                my_ads: null,
+            },
         },
         order_store: {
             setOrderId: jest.fn(),
         },
-    })),
+    }),
 }));
 
 jest.mock('Utils/websocket', () => ({
@@ -28,7 +34,10 @@ jest.mock('Utils/websocket', () => ({
 
 jest.mock('Components/app-content', () => () => <div>AppContent</div>);
 
-// jest.mock('Components/i18next', () => () => jest.fn());
+jest.mock('Components/i18next', () => ({
+    ...jest.requireActual('Components/i18next'),
+    setLanguage: jest.fn(),
+}));
 
 describe('<App/>', () => {
     const props = {
@@ -44,10 +53,10 @@ describe('<App/>', () => {
     };
 
     it('should invoke useEffect methods', () => {
-        const spied_use_effect = jest.spyOn(React, 'useEffect');
+        const use_effect_spy = jest.spyOn(React, 'useEffect');
         render(<App {...props} />);
 
-        expect(spied_use_effect).toHaveBeenCalledTimes(1);
+        expect(use_effect_spy).toHaveBeenCalledTimes(1);
     });
 
     it('should render appContent component', () => {
@@ -57,13 +66,34 @@ describe('<App/>', () => {
         expect(el_app_content).toBeInTheDocument();
     });
 
-    // it('should invoke setLanguage function');
+    it('should invoke setLanguage function when component renders', () => {
+        render(<App {...props} lang='ENG' />);
 
-    // it('should redirect to orders when order_id is passed', () => {
-    //     const spied_redirectTo = jest
-    //         .spyOn(useStores().general_store, 'redirectTo')
-    //         .render(<App {...props} order_id='17' />);
+        expect(setLanguage).toHaveBeenCalledWith('ENG');
+    });
 
-    //     expect(spied_redirectTo).toHaveBeenCalled();
-    // });
+    it('should redirect to orders when order_id is passed', () => {
+        const { general_store } = useStores();
+        const spied_redirectTo = jest.spyOn(general_store, 'redirectTo');
+        render(<App {...props} order_id='17' />);
+
+        expect(spied_redirectTo).toHaveBeenCalled();
+    });
+
+    it('should remove the is_verifying_p2p status from local storage if it was previously set', () => {
+        localStorage.setItem('is_verifying_p2p', true);
+        const remove_item_spy = jest.spyOn(window.localStorage.__proto__, 'removeItem');
+
+        const mock_history = { location: { pathname: 'verification', push: jest.fn() } };
+        render(<App {...props} history={mock_history} />);
+
+        // expect(set_item_spy).toHaveBeenCalled();
+    });
+
+    it('should do a hard reload of page', () => {
+        const window_spy = jest.spyOn(window, 'onpageshow', 'set');
+        render(<App {...props} />);
+
+        expect(window_spy).toHaveBeenCalled();
+    });
 });

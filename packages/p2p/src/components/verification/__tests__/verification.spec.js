afe05e1edd3a4e0fa05c4780382d4c4270dc296b
artsemkavaleu-deriv <artsem@deriv.com>
2022-02-17 15:15:28 +0300

    <Verification /> test coverage

---
+++ packages/p2p/src/components/verification/__tests__/verification.spec.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { fireEvent, screen, render } from '@testing-library/react';
+import { useStores } from 'Stores';
+import Verification from '../verification';
+
+jest.mock('Stores', () => ({
+    ...jest.requireActual('Stores'),
+    useStores: jest.fn(),
+}));
+
+const mockProps = () => ({
+    should_wrap: false,
+});
+
+const mocked_store_values = {
+    is_advertiser: false,
+    nickname: '',
+    poi_status: '',
+    poiStatusText: jest.fn(),
+    props: {
+        history: '',
+    },
+};
+
+describe('<Verification />', () => {
+    it('Component should be rendered', () => {
+        const props = mockProps();
+        useStores.mockImplementation(() => ({
+            general_store: { ...mocked_store_values },
+        }));
+
+        render(<Verification {...props} />);
+
+        const el_component_container = screen.getByTestId('component_container');
+        expect(el_component_container).toBeInTheDocument();
+    });
+
+    it('Should show verification wrapper if should_wrap prop is true', () => {
+        const props = mockProps();
+        props.should_wrap = true;
+        useStores.mockImplementation(() => ({
+            general_store: { ...mocked_store_values },
+        }));
+
+        render(<Verification {...props} />);
+
+        const el_verification_wrapper = screen.getByTestId('verification_wrapper');
+        expect(el_verification_wrapper).toBeInTheDocument();
+    });
+
+    it('Should render Dp2pBlocked component', () => {
+        const props = mockProps();
+        useStores.mockImplementation(() => ({
+            general_store: { ...mocked_store_values, poi_status: 'verified', nickname: 'test' },
+        }));
+
+        render(<Verification {...props} />);
+
+        const el_Dp2pBlocked_component = screen.getByTestId('component_container');
+        expect(el_Dp2pBlocked_component).toBeInTheDocument();
+    });
+});

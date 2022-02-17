bd356a4d0cf122c40fb912c7ce93e7469be9b798
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-02-17 15:30:37 +0330

    fix/ replace icon with button

--- packages/account/src/Sections/Verification/ProofOfOwnership/proof-of-ownership.jsx
+++ packages/account/src/Sections/Verification/ProofOfOwnership/proof-of-ownership.jsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
-import { Icon, Text } from '@deriv/components';
+import { Button, Icon, Text } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import ExpandedCard from './ExpandedCard';
 
@@ -8,7 +8,16 @@ const ProofOfOwnership = () => {
     const [is_open, setIsOpen] = useState(false);
 
     const onClick = () => setIsOpen(!is_open);
-
+    const icon = (
+        <Icon
+            icon={'IcChevronUpBold'}
+            color='black'
+            size={16}
+            className={classNames('proof-of-ownership__card-item-icon', {
+                'proof-of-ownership__card-item-icon--invert': !is_open,
+            })}
+        />
+    );
     const Card = () => (
         <div className={classNames('proof-of-ownership__card', { 'proof-of-ownership__card-open': is_open })}>
             <div className='proof-of-ownership__card-item'>
@@ -16,10 +25,13 @@ const ProofOfOwnership = () => {
                 <Text className='proof-of-ownership__card-item-text' as='p' color='general' size='s' weight='bold'>
                     {localize('Credit/debit card')}
                 </Text>
-                <Icon
-                    icon={is_open ? 'IcChevronUpBold' : 'IcChevronDownBold'}
+                <Button
+                    id='proof-of-ownership'
+                    icon={icon}
                     className='proof-of-ownership__card-item-icon'
                     onClick={onClick}
+                    transparent
+                    data-testid={'proof-of-ownership-button'}
                 />
             </div>
             {is_open && <ExpandedCard />}

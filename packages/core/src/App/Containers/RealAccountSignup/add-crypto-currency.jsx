2daf4960e957efee176105127dd9e54769d85fc6
vijayasree garapati <vijayasree@deriv.com>
2022-02-22 16:54:59 +0400

    cashier-onboarding-links-broken-iphoneSE

--- packages/core/src/App/Containers/RealAccountSignup/add-crypto-currency.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/add-crypto-currency.jsx
@@ -75,7 +75,7 @@ const AddCryptoCurrency = ({
                     )}
                     {canAddFiat() && (
                         <React.Fragment>
-                            <ThemedScrollbars>
+                            <ThemedScrollbars className='test' height={'calc(100vh - 23vh)'}>
                                 <CurrencyRadioButtonGroup
                                     id='fiat_currency'
                                     is_fiat
@@ -129,7 +129,7 @@ const AddCryptoCurrency = ({
                     )}
                     {!should_show_fiat_only &&
                         (available_crypto_currencies.length !== 0 ? (
-                            <ThemedScrollbars>
+                            <ThemedScrollbars className={'test'} height={'calc(100vh - 18rem)'}>
                                 <CurrencyRadioButtonGroup
                                     id='crypto_currency'
                                     className='currency-selector__radio-group currency-selector__radio-group--with-margin'

cd73fe8b66a9c3f46c99c9ffcca218ec4e05bfe3
Likhith Kolayari <likhith@regentmarkets.com>
2022-02-24 15:05:51 +0400

    Improved code coverage for order-table-content

--- packages/p2p/src/components/orders/order-table/__test__/order-table-content.spec.js
+++ packages/p2p/src/components/orders/order-table/__test__/order-table-content.spec.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import { isMobile } from '@deriv/shared';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { useStores } from 'Stores';
+import React from 'react';
+import { createExtendedOrderDetails } from 'Utils/orders';
 import OrderTableContent from '../order-table-content.jsx';
 
 jest.mock('Stores', () => ({
@@ -12,9 +14,22 @@ jest.mock('@deriv/components', () => ({
     ...jest.requireActual('@deriv/components'),
     Div100vhContainer: jest.fn(() => <div>Mobile container</div>),
     Loading: jest.fn(() => <div>Loading</div>),
+    InfiniteDataList: jest.fn(() => <div>Data list</div>),
+}));
+
+jest.mock('@deriv/shared', () => ({
+    ...jest.requireActual('@deriv/shared'),
+    isMobile: jest.fn(),
 }));
 
 jest.mock('Components/orders/order-table/order-table-header.jsx', () => jest.fn(() => <div>Order table header</div>));
+
+jest.mock('Components/table/table-error.jsx', () => ({ TableError: jest.fn(() => <div>Table Error</div>) }));
+
+jest.mock('Utils/orders', () => ({
+    ...jest.requireActual('Utils/orders'),
+    createExtendedOrderDetails: jest.fn().mockReturnValue({ is_inactive_order: true }),
+}));
 
 describe('<OrderTableContent/>', () => {
     const mock_general_store = {
@@ -26,7 +41,6 @@ describe('<OrderTableContent/>', () => {
             server_time: null,
         },
         is_active_tab: false,
-        is_inactive_order: false,
         has_more_items_to_load: false,
         handleTabClick: jest.fn(),
     };
@@ -66,7 +80,48 @@ describe('<OrderTableContent/>', () => {
         const { general_store } = useStores();
         render(<OrderTableContent />);
         fireEvent.click(screen.getByRole('button'));
-        screen.debug();
-        expect(general_store.handleTabClick).toHaveBeenCalled(1);
+
+        expect(general_store.handleTabClick).toHaveBeenCalled();
+    });
+
+    it('should display is loading component when is_loading state is set', () => {
+        useStores.mockImplementation(() => ({
+            general_store: { ...mock_general_store },
+            order_store: { ...mock_order_store, is_loading: true },
+        }));
+        render(<OrderTableContent />);
+
+        expect(screen.getByText('Loading')).toBeInTheDocument();
+    });
+
+    it('should display API error message when api_error_message is set', () => {
+        useStores.mockImplementation(() => ({
+            general_store: { ...mock_general_store },
+            order_store: { ...mock_order_store, api_error_message: true },
+        }));
+        render(<OrderTableContent />);
+        expect(screen.getByText('Table Error')).toBeInTheDocument();
+    });
+
+    it('should invoke createExtendedOrderDetails when orders is set and render table', () => {
+        useStores.mockImplementation(() => ({
+            general_store: { ...mock_general_store },
+            order_store: { ...mock_order_store, orders: [{}] },
+        }));
+        render(<OrderTableContent />);
+
+        expect(createExtendedOrderDetails).toHaveBeenCalled();
+        expect(screen.getByText('Order table header')).toBeInTheDocument();
+    });
+
+    it('should render mobile view when the display detected is mobile', () => {
+        isMobile.mockReturnValue(true);
+        useStores.mockImplementation(() => ({
+            general_store: { ...mock_general_store },
+            order_store: { ...mock_order_store, orders: [{}] },
+        }));
+        render(<OrderTableContent />);
+
+        expect(screen.getByText('Mobile container')).toBeInTheDocument();
     });
 });

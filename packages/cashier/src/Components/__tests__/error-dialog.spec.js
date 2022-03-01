79221f3bc64134f37de3770bdd9936f0e42ac37a
aswathy-deriv <aswathy@deriv.com>
2022-03-01 16:20:41 +0400

    test case for error dialog box

---
+++ packages/cashier/src/Components/__tests__/error-dialog.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import ErrorDialog from '../error-dialog';
+import { __esModule } from '@babel/preset-typescript';
+
+jest.mock('Stores/connect', () => ({
+    __esModule: true,
+    default: 'mockedConnect',
+    connect: () => Component => Component,
+}));
+describe('<ErrorDialog />', () => {
+    const props = {
+        confirm_button_text: '',
+        error_message: 'This is the error message.',
+        has_no_close_icon: true,
+        reset: jest.fn(),
+        setShouldShow: jest.fn(),
+        should_show: true,
+        should_not_show_title: '',
+    };
+    it('should show the error component if error', () => {
+        render(<ErrorDialog {...props} />);
+        expect(screen.getByText('This is the error message.')).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: 'Retry' })).toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole('button', { name: 'Retry' }));
+        expect(props.setShouldShow).toBeCalled();
+    });
+    it('should invoke reset function', () => {
+        render(<ErrorDialog {...props} should_show={false} confirm_button_text='ok' should_not_show_title='test' />);
+        expect(props.reset).toHaveBeenCalled();
+    });
+});

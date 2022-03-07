53d9a9247beb3b93318108015ba3a8c94687533e
hirad-rework <hirad@re-work.dev>
2022-03-07 13:56:05 +0330

    fix--Localize bug fixed

--- packages/cfd/src/Components/Routes/binary-routes.jsx
+++ packages/cfd/src/Components/Routes/binary-routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { Localize, PlatformContext } from '@deriv/shared';
+import { PlatformContext } from '@deriv/shared';
+import { Localize } from '@deriv/translations';
 import getRoutesConfig from '../../Constants/routes-config';
 import RouteWithSubRoutes from './route-with-sub-routes.jsx';
 

818e87a64218ac0302285eefbb58f3457371fb48
amina-deriv <amina@deriv.com>
2022-03-08 17:58:55 +0400

    fix

--- packages/account/src/Components/poa-submitted/__tests__/poa-submitted.spec.js
+++ packages/account/src/Components/poa-submitted/__tests__/poa-submitted.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter } from 'react-router-dom';
 import { screen, render } from '@testing-library/react';
 import { Submitted } from '../submitted';
 
@@ -9,24 +8,22 @@ jest.mock('Components/poa-continue-trading-button/continue-trading-button.jsx', 
 }));
 
 describe('<Submitted />', () => {
-    let history;
     const renderWithRouter = component => {
-        history = createBrowserHistory();
-        return render(<Router history={history}>{component}</Router>);
+        return render(<BrowserRouter>{component}</BrowserRouter>);
     };
 
     it('should render the Submitted component', () => {
-        renderWithRouter(<Submitted needs_poi={true} />);
+        renderWithRouter(<Submitted needs_poi />);
         expect(screen.getByText('Your proof of address was submitted successfully')).toBeInTheDocument();
     });
 
     it('should show submit_poi message if needs_poi is true', () => {
-        renderWithRouter(<Submitted needs_poi={true} />);
+        renderWithRouter(<Submitted needs_poi />);
         expect(screen.getByText('You must also submit a proof of identity.')).toBeInTheDocument();
     });
 
     it('should show review message if needs_poi is false', () => {
-        renderWithRouter(<Submitted needs_poi={true} />);
+        renderWithRouter(<Submitted needs_poi />);
         expect(screen.getByText('Your document is being reviewed, please check back in 1-3 days.')).toBeInTheDocument();
     });
 

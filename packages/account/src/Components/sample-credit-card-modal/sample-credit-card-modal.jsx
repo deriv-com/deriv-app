b93d1b4e1a37784eb46f0171f223775819f711d3
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-03-09 09:57:20 +0330

    style/ update simple card styles

--- packages/account/src/Components/sample-credit-card-modal/sample-credit-card-modal.jsx
+++ packages/account/src/Components/sample-credit-card-modal/sample-credit-card-modal.jsx
@@ -6,13 +6,11 @@ import { getUrlBase } from '@deriv/shared';
 export const SampleCreditCardModal = ({ is_open, onClose }) => {
     return (
         <Modal
-            className='te'
+            className='sample_credit_card_modal'
             is_open={is_open}
             should_header_stick_body
             title={<Localize i18n_default_text='How to masking your card?' />}
             toggleModal={onClose}
-            width='536px'
-            height='494px'
         >
             <React.Fragment>
                 <Text className='sample_credit_card_modal-text' size='xs'>
@@ -24,8 +22,6 @@ export const SampleCreditCardModal = ({ is_open, onClose }) => {
                     src={getUrlBase('/public/images/common/sample-credit-card.png')}
                     alt={'creditcardsample'}
                     className='sample_credit_card_modal-img'
-                    width='536px' // width and height should be specified so it doesn't jump to the right after image loads
-                    height='334px'
                 />
             </React.Fragment>
         </Modal>

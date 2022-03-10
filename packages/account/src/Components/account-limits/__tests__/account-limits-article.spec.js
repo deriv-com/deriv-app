18314781e7f2d68856d9bf7550c0767adb8e1852
amina-deriv <amina@deriv.com>
2022-03-10 18:17:09 +0400

    account-limit-article

---
+++ packages/account/src/Components/account-limits/__tests__/account-limits-article.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {fireEvent, screen, render } from '@testing-library/react';
+import { Button } from '@deriv/components';
+import AccountLimitsArticle  from '../account-limits-article';
+
+describe('<AccountLimitsArticle/>', () => {
+
+    it('should render AccountLimitsArticle component', () => {
+        render(<AccountLimitsArticle />);
+        expect(screen.getByText('Account limits')).toBeInTheDocument();
+    });
+
+    it('should show the descriptions for the account limit', () => {
+        render(<AccountLimitsArticle />);
+        expect(screen.getByText('These are default limits that we apply to your accounts.')).toBeInTheDocument();
+    });
+
+
+    it('should go to help-centre page if the Help Centre link on the text is clicked', () => {
+       render(<AccountLimitsArticle/>);
+        expect(screen.getByText('Help Centre.').closest('a')).toHaveAttribute(
+            'href',
+            'https://deriv.com/help-centre'
+        );
+    });
+})

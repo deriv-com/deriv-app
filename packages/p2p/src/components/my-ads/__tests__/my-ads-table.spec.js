ff796cadd2b7c460c1562c3d42ca42525077daa4
artsemkavaleu-deriv <artsem@deriv.com>
2022-03-12 15:21:11 +0300

    replace modal_root_el to the upper scope

--- packages/p2p/src/components/my-ads/__tests__/my-ads-table.spec.js
+++ packages/p2p/src/components/my-ads/__tests__/my-ads-table.spec.js
@@ -37,10 +37,10 @@ const mocked_general_store = {
     is_barred: false,
     is_listed: true,
 };
+const modal_root_el = document.createElement('div');
 
 describe('<MyAdsTable />', () => {
     beforeAll(() => {
-        const modal_root_el = document.createElement('div');
         modal_root_el.setAttribute('id', 'modal_root');
         document.body.appendChild(modal_root_el);
     });

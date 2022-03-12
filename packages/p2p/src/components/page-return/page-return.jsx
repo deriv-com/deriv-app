c3afdd499349ed8868a147abaa3689f509e10ee2
artsemkavaleu-deriv <artsem@deriv.com>
2022-03-12 15:55:37 +0300

    Add <CreateAd /> test coverage

--- packages/p2p/src/components/page-return/page-return.jsx
+++ packages/p2p/src/components/page-return/page-return.jsx
@@ -7,7 +7,7 @@ import './page-return.scss';
 const PageReturn = ({ className, onClick, page_title }) => {
     return (
         <div className={classNames('page-return', className)}>
-            <div onClick={onClick} className='page-return__button'>
+            <div onClick={onClick} className='page-return__button' data-testid='dp2p-page-return_icon-button'>
                 <Icon icon='IcArrowLeftBold' size={16} />
             </div>
             <Text size='s' color='general' line_height='m' weight='bold'>

247c964c051d266348c7f7df0399ad2e1be5f861
artsemkavaleu-deriv <artsem@deriv.com>
2022-03-16 10:54:27 +0300

    Add <MyAdsDeleteModal /> test coverage

--- packages/p2p/src/components/my-ads/__tests__/my-ads-delete-modal.spec.js
+++ packages/p2p/src/components/my-ads/__tests__/my-ads-delete-modal.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { fireEvent, screen, render } from '@testing-library/react';
+import { fireEvent, screen, render, waitFor } from '@testing-library/react';
+import { requestWS } from 'Utils/websocket';
 import { useStores } from 'Stores';
-// import { useIsMounted } from '@deriv/shared';
 import MyAdsDeleteModal from '../my-ads-delete-modal.jsx';
 
 jest.mock('Stores', () => ({
@@ -9,12 +9,15 @@ jest.mock('Stores', () => ({
     useStores: jest.fn(),
 }));
 
-// jest.mock('@deriv/shared', () => ({
-//     ...jest.requireActual('@deriv/shared'),
-//     useIsMounted: jest.fn(),
-// }));
+jest.mock('Utils/websocket', () => ({
+    ...jest.requireActual('Utils/websocket'),
+    requestWS: jest.fn(),
+}));
 
 const mocked_my_ads_store = {
+    adverts: [],
+    delete_error_message: '',
+    setAdverts: jest.fn(),
     setDeleteErrorMessage: jest.fn(),
     setSelectedAdId: jest.fn(),
     setIsDeleteModalOpen: jest.fn(),
@@ -41,7 +44,7 @@ describe('<CreateAdErrorModal />', () => {
         expect(el_dp2p_my_ads_delete_modal_container).toBeInTheDocument();
     });
 
-    it('Component should be rendered', () => {
+    it('Must do no changes if click on canceling button', () => {
         useStores.mockImplementation(() => ({
             my_ads_store: { ...mocked_my_ads_store, is_delete_modal_open: true },
         }));
@@ -55,18 +58,35 @@ describe('<CreateAdErrorModal />', () => {
         expect(mocked_my_ads_store.setIsDeleteModalOpen).toBeCalledWith(false);
     });
 
-    it('Component should be rendered', () => {
+    it('Adverts must not be set if click on deletion button and have a response error', async () => {
         useStores.mockImplementation(() => ({
             my_ads_store: { ...mocked_my_ads_store, is_delete_modal_open: true },
         }));
-        // useIsMounted.mockImplementation(() => ({
-        //     isMounted: () => true,
-        // }));
+        requestWS.mockImplementation(() => Promise.resolve({ error: { message: 'test' } }));
         render(<MyAdsDeleteModal />);
 
         const el_dp2p_my_ads_delete_modal_delete_button = screen.getByText('Delete');
         fireEvent.click(el_dp2p_my_ads_delete_modal_delete_button);
 
         expect(mocked_my_ads_store.setIsDeleteModalOpen).toBeCalledWith(false);
+        await waitFor(() => {
+            expect(mocked_my_ads_store.setDeleteErrorMessage).toHaveBeenCalledWith('test');
+        });
+    });
+
+    it('Adverts must be set if click on deletion button and there is no response error', async () => {
+        useStores.mockImplementation(() => ({
+            my_ads_store: { ...mocked_my_ads_store, is_delete_modal_open: true },
+        }));
+        requestWS.mockImplementation(() => Promise.resolve({ error: null }));
+        render(<MyAdsDeleteModal />);
+
+        const el_dp2p_my_ads_delete_modal_delete_button = screen.getByText('Delete');
+        fireEvent.click(el_dp2p_my_ads_delete_modal_delete_button);
+
+        await waitFor(() => {
+            expect(mocked_my_ads_store.setAdverts).toHaveBeenCalled();
+            expect(mocked_my_ads_store.setIsDeleteModalOpen).toHaveBeenCalledWith(false);
+        });
     });
 });

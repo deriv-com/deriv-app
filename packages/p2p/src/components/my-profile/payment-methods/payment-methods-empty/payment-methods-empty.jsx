a48543d82647013a8ae376432b26c51303f5e12f
Carol Sachdeva <carol@binary.com>
2022-03-17 14:53:56 +0800

    fix: ad details

--- packages/p2p/src/components/my-profile/payment-methods/payment-methods-empty/payment-methods-empty.jsx
+++ packages/p2p/src/components/my-profile/payment-methods/payment-methods-empty/payment-methods-empty.jsx
@@ -12,12 +12,7 @@ const PaymentMethodsEmpty = () => {
         <React.Fragment>
             <DesktopWrapper>
                 <div className='payment-methods-empty'>
-                    <Icon
-                        icon='IcPaymentMethodsWallet'
-                        className='payment-methods-empty--icon'
-                        height={159}
-                        width={256}
-                    />
+                    <Icon icon='IcPaymentMethodsWallet' height={159} width={256} />
                     <Text className='payment-methods-empty--text' line_height='m' size='s' weight='bold'>
                         <Localize i18n_default_text='You haven’t added any payment methods yet' />
                     </Text>

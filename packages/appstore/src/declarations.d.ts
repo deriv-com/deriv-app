8e772db3befcf139bc8d83dd8e048c61959b892f
Bala <bala.krishna@regentmarkets.com>
2022-03-21 17:08:05 +0800

    remove deriv-components from declarations

--- packages/appstore/src/declarations.d.ts
+++ packages/appstore/src/declarations.d.ts
@@ -8,5 +8,3 @@ declare module '@deriv/shared';
 declare module '@deriv/translations';
 declare module '@deriv/trader';
 declare module '@deriv/account';
-declare module '@deriv/deriv-components';
-// TODO: remove the above line once types in @deriv/deriv-components is fixed

32b19827957e2bfa538eb8b8ba6ff3db3c99e95d
amina-deriv <amina@deriv.com>
2022-03-22 18:10:50 +0400

    poa-continue-trading-btn-spec

--- packages/account/src/Components/poa-continue-trading-button/continue-trading-button.jsx
+++ packages/account/src/Components/poa-continue-trading-button/continue-trading-button.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 export const ContinueTradingButton = () => (
     <ButtonLink className='account-management__button' to='/'>
-        <Text className='dc-btn__text' as='p' weight='bold'>
+        <Text className='dc-btn__text' as='p' weight='bold' data-testid='continue_btn_text'>
             {localize('Continue trading')}
         </Text>
-    </ButtonLink>
+    </ButtonLink >
 );

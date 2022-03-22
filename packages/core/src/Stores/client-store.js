85e14a8977702097058050590d17ca77f2163ae3
george-usynin-deriv <heorhi@deriv.com>
2022-03-22 10:42:19 +0300

    add comment

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1250,6 +1250,7 @@ export default class ClientStore extends BaseStore {
                 if (has_action) {
                     const query_string = filterUrlQuery(search, ['platform', 'code', 'action']);
                     if ([routes.cashier_withdrawal, routes.cashier_pa].includes(redirect_route)) {
+                        // Set redirect path for cashier withdrawal and payment agent withdrawal (after getting PTA redirect_url)
                         window.location.replace(`/redirect?${query_string}`);
                     }
                     window.location.replace(`${redirect_route}/redirect?${query_string}`);

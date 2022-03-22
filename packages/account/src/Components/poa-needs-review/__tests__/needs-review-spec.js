64dd3e0d39c65bc24e2b3cbea87a0a385be68e12
“yauheni-deriv” <yauheni@deriv.com>
2022-03-22 14:10:22 +0300

    yuheni/#54657 poa-needs-review test coverage

--- packages/account/src/Components/poa-needs-review/__tests__/needs-review-spec.js
+++ packages/account/src/Components/poa-needs-review/__tests__/needs-review-spec.js
@@ -3,32 +3,28 @@ import { BrowserRouter } from 'react-router-dom';
 import { screen, render } from '@testing-library/react';
 import { NeedsReview } from '../needs-review';
 
-
 jest.mock('Components/poa-continue-trading-button/continue-trading-button.jsx', () => ({
     ContinueTradingButton: jest.fn(() => <div>ContinueTradingButton</div>),
 }));
 
 describe('<NeedsReview/>', () => {
-
     it('should render NeedsReview component if it does not need poi', () => {
-
         render(
-        <BrowserRouter>
-            <NeedsReview needs_poi={false} is_description_enabled />
-        </BrowserRouter>
-        )
+            <BrowserRouter>
+                <NeedsReview needs_poi={false} is_description_enabled />
+            </BrowserRouter>
+        );
 
         expect(screen.getByText('Your proof of address was submitted successfully')).toBeInTheDocument();
         expect(screen.getByText('ContinueTradingButton')).toBeInTheDocument();
     });
 
     it('should render NeedsReview component if it needs poi', () => {
-
         render(
-        <BrowserRouter>
-            <NeedsReview needs_poi is_description_enabled />
-        </BrowserRouter>
-        )
+            <BrowserRouter>
+                <NeedsReview needs_poi is_description_enabled />
+            </BrowserRouter>
+        );
 
         expect(screen.getByText('Your proof of address was submitted successfully')).toBeInTheDocument();
         expect(screen.getByText('You must also submit a proof of identity.')).toBeInTheDocument();

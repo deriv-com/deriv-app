2fa0bf53b6a1780aac933cdf4bc6476dd357399d
Bala <bala.krishna@regentmarkets.com>
2022-03-22 10:57:00 +0800

    fix eslint error

--- packages/appstore/src/components/wallet/wallet-card.tsx
+++ packages/appstore/src/components/wallet/wallet-card.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Icon } from '@deriv/components';
-// TODO: remove ts-ignore once deriv-components tsc is fixed
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { Text } from '@deriv/deriv-components';
 import WalletIcon from 'Assets/svgs/wallet';
 

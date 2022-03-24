78969a5038ad8c4d531ca76ac2128de38c212626
“Nikita” <nikita@deriv.com>
2022-03-24 11:22:26 +0300

    feat:add tests for load error message component

--- packages/account/src/Components/load-error-message/__tests__/load-error-message.spec.js
+++ packages/account/src/Components/load-error-message/__tests__/load-error-message.spec.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LoadErrorMessage from '../load-error-message';
 
-jest.mock('Components/icon-message-content', () => () => (<div>IconMessageContent</div>));
+jest.mock('Components/icon-message-content', () => () => <div>IconMessageContent</div>);
 
 describe('LoadErrorMessage', () => {
     it('should render LoadErrorMessage component', () => {
-        render(<LoadErrorMessage message="message"/>);
+        render(<LoadErrorMessage message='message' />);
         expect(screen.getByText('IconMessageContent')).toBeInTheDocument();
     });
-})
+});

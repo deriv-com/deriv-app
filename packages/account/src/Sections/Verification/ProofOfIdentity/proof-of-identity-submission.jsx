c3e1e790cc859ce3f4834fe3ec769acad67acb6f
vijayasree garapati <vijayasree@deriv.com>
2022-03-25 14:07:54 +0400

    Country of document issuance page displayed again after the successful submission of POI

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-submission.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-submission.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import { WS } from '@deriv/shared';
 import CountrySelector from 'Components/poi-country-selector';
@@ -65,36 +66,39 @@ const POISubmission = ({
     );
 
     React.useEffect(() => {
-        if ((has_require_submission || allow_poi_resubmission) && identity_last_attempt) {
-            switch (identity_last_attempt.service) {
-                case service_code.idv: {
-                    if (Number(idv.submissions_left) > 0 || Number(onfido.submissions_left) > 0) {
-                        setSubmissionStatus(submission_status_code.selecting);
-                    } else {
-                        setSubmissionService(service_code.manual);
+        if (submission_status !== submission_status_code.complete) {
+            if ((has_require_submission || allow_poi_resubmission) && identity_last_attempt) {
+                switch (identity_last_attempt.service) {
+                    case service_code.idv: {
+                        if (Number(idv.submissions_left) > 0 || Number(onfido.submissions_left) > 0) {
+                            setSubmissionStatus(submission_status_code.selecting);
+                        } else {
+                            setSubmissionService(service_code.manual);
+                            setSubmissionStatus(submission_status_code.submitting);
+                        }
+                        break;
+                    }
+                    case service_code.onfido: {
+                        if (Number(onfido.submissions_left) > 0) {
+                            setSubmissionStatus(submission_status_code.selecting);
+                        } else {
+                            setSubmissionService(service_code.manual);
+                            setSubmissionStatus(submission_status_code.submitting);
+                        }
+                        break;
+                    }
+                    case service_code.manual: {
+                        setSelectedCountry(getCountryFromResidence(identity_last_attempt.country_code));
                         setSubmissionStatus(submission_status_code.submitting);
-                    }
-                    break;
-                }
-                case service_code.onfido: {
-                    if (Number(onfido.submissions_left) > 0) {
-                        setSubmissionStatus(submission_status_code.selecting);
-                    } else {
                         setSubmissionService(service_code.manual);
+                        break;
                     }
-                    break;
+                    default:
+                        break;
                 }
-                case service_code.manual: {
-                    setSelectedCountry(getCountryFromResidence(identity_last_attempt.country_code));
-                    setSubmissionStatus(submission_status_code.submitting);
-                    setSubmissionService(service_code.manual);
-                    break;
-                }
-                default:
-                    break;
+            } else {
+                setSubmissionStatus(submission_status_code.selecting);
             }
-        } else {
-            setSubmissionStatus(submission_status_code.selecting);
         }
     }, [
         allow_poi_resubmission,

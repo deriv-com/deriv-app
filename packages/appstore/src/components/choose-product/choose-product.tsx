5744ff607eeda7af9655a6f6cffa94029b2585de
Bala <bala.krishna@regentmarkets.com>
2022-03-30 16:21:22 +0800

    chore: add condition

--- packages/appstore/src/components/choose-product/choose-product.tsx
+++ packages/appstore/src/components/choose-product/choose-product.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
+import { localize } from '@deriv/translations';
 import ProductCard from 'Components/product-card';
 import './choose-product.scss';
 
 const ChooseProduct = () => {
     const [selected_product, setSeletedProduct] = React.useState('');
 
+    // TODO: add conditions to show/hide products
     const tradetype = [
         {
             type: 'CFDs',
             icon_title: 'icAppstoreMultipliersTradeType',
-            description: 'Trade with leverage and tight spreads for better returns on successful trades.',
+            description: localize('Trade with leverage and tight spreads for better returns on successful trades.'),
             checked: selected_product === 'CFDs',
             bg_image_title: 'appstore_cfds_trade_type_bg',
         },
         {
             type: 'Multipliers',
             icon_title: 'icAppstoreCfdsTradeType',
-            description: 'Combine the upside of CFDs with the simplicity of options.',
+            description: localize('Combine the upside of CFDs with the simplicity of options.'),
             checked: selected_product === 'Multipliers',
             bg_image_title: 'appstore_multipliers_trade_type_bg',
         },
         {
             type: 'Options',
             icon_title: 'icAppstoreOptionTradeType',
-            description: 'Earn fixed payouts by predicting an asset price movement.',
+            description: localize('Earn fixed payouts by predicting an asset price movement.'),
             checked: selected_product === 'Options',
             bg_image_title: 'appstore_options_trade_type_bg',
         },

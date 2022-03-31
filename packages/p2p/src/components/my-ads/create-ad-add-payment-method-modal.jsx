86f7f6dfd13940ae826fe0852b03435d18c6545d
Likhith Kolayari <likhith@regentmarkets.com>
2022-03-31 08:40:53 +0400

    Fixed title of add PM

--- packages/p2p/src/components/my-ads/create-ad-add-payment-method-modal.jsx
+++ packages/p2p/src/components/my-ads/create-ad-add-payment-method-modal.jsx
@@ -47,7 +47,7 @@ const CreateAdAddPaymentMethodModal = () => {
                 is_flex
                 is_modal_open={my_ads_store.should_show_add_payment_method_modal}
                 page_header_className='buy-sell__modal-header'
-                page_header_text={localize('Choose payment method')}
+                page_header_text={localize('Add payment method')}
                 pageHeaderReturnFn={() => my_ads_store.setShouldShowAddPaymentMethodModal(false)}
             >
                 <AddPaymentMethod should_show_page_return={false} should_show_separated_footer={true} />
@@ -61,7 +61,7 @@ const CreateAdAddPaymentMethodModal = () => {
             has_close_icon={false}
             height='560px'
             is_open={my_ads_store.should_show_add_payment_method_modal}
-            title={localize('Choose payment method')}
+            title={localize('Add payment method')}
         >
             <Modal.Body>
                 <AddPaymentMethod should_show_page_return={false} should_show_separated_footer={true} />

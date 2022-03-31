099110081b2cbdbe1fd833dd2e8e3d95b0910aa8
george-usynin-deriv <heorhi@deriv.com>
2022-03-31 09:19:32 +0300

    code improvement

--- packages/cashier/src/Components/CashierDefault/cashier-default-side-note.jsx
+++ packages/cashier/src/Components/CashierDefault/cashier-default-side-note.jsx
@@ -7,13 +7,7 @@ import { connect } from 'Stores/connect';
 import { getCurrencyDisplayCode, isMobile, routes } from '@deriv/shared';
 import 'Sass/cashier-default.scss';
 
-const CashierDefaultSideNote = ({
-    can_change_fiat_currency,
-    currency,
-    is_crypto,
-    openRealAccountSignup,
-    setDepositTarget,
-}) => {
+const CashierDefaultSideNote = ({ currency, is_crypto, openRealAccountSignup, setDepositTarget }) => {
     const currency_code = getCurrencyDisplayCode(currency);
 
     const getSideNoteDescription = () => {
@@ -27,18 +21,16 @@ const CashierDefaultSideNote = ({
         }
 
         return (
-            !can_change_fiat_currency && (
-                <Localize
-                    i18n_default_text="You can no longer change your account currency because you've made a deposit into your fiat account or created a real DMT5 or Deriv X account. Please contact us via <0>live chat</0> for clarification."
-                    components={[
-                        <span
-                            key={0}
-                            className='link link--orange cashier-default-side-note__text-nowrap'
-                            onClick={() => window.LC_API.open_chat_window()}
-                        />,
-                    ]}
-                />
-            )
+            <Localize
+                i18n_default_text="You can no longer change your account currency because you've made a deposit into your fiat account or created a real DMT5 or Deriv X account. Please contact us via <0>live chat</0> for clarification."
+                components={[
+                    <span
+                        key={0}
+                        className='link link--orange cashier-default-side-note__text-nowrap'
+                        onClick={() => window.LC_API.open_chat_window()}
+                    />,
+                ]}
+            />
         );
     };
 
@@ -89,7 +81,6 @@ const CashierDefaultSideNote = ({
 };
 
 CashierDefaultSideNote.propTypes = {
-    can_change_fiat_currency: PropTypes.bool,
     currency: PropTypes.string,
     mt5_login_list: PropTypes.array,
     openRealAccountSignup: PropTypes.func,
@@ -97,7 +88,6 @@ CashierDefaultSideNote.propTypes = {
 };
 
 export default connect(({ client, modules, ui }) => ({
-    can_change_fiat_currency: client.can_change_fiat_currency,
     currency: client.currency,
     mt5_login_list: client.mt5_login_list,
     openRealAccountSignup: ui.openRealAccountSignup,

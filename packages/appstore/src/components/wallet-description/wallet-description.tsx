d5b7c001895fce6ee535322863b2e2bdc0f30c73
vinu-deriv <vinu@deriv.com>
2022-04-04 10:53:32 +0400

    basic structure for wallet card added

---
+++ packages/appstore/src/components/wallet-description/wallet-description.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type TWalletDescription = {
+    title: string,
+    description: string,
+    depositsInformation: string,
+    withdrawalsInformation: string
+}
+const WalletDescription = ({
+    title,
+    description,
+    depositsInformation,
+    withdrawalsInformation 
+}: TWalletDescription) => {
+   return (<div>
+<p>{title}</p>
+<p>{description}</p>
+<p>{depositsInformation}</p>
+<p>{withdrawalsInformation}</p>
+   </div>)
+};
+
+export default WalletDescription;

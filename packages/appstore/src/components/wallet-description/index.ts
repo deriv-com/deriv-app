bdb6bb317defd877cb7f9b6aecc42a5219a35a20
vinu-deriv <vinu@deriv.com>
2022-04-04 10:54:30 +0400

    linting issue fixed

--- packages/appstore/src/components/wallet-description/index.ts
+++ packages/appstore/src/components/wallet-description/index.ts
@@ -1,4 +1,4 @@
-import WalletDescription from './wallet-description'
+import WalletDescription from './wallet-description';
 import './wallet-description.scss';
 
 export default WalletDescription;
--- packages/appstore/src/components/wallet-description/wallet-description.tsx
+++ packages/appstore/src/components/wallet-description/wallet-description.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 
 type TWalletDescription = {
-    title: string,
-    description: string,
-    depositsInformation: string,
-    withdrawalsInformation: string
-}
-const WalletDescription = ({
-    title,
-    description,
-    depositsInformation,
-    withdrawalsInformation 
-}: TWalletDescription) => {
-   return (<div>
-<p>{title}</p>
-<p>{description}</p>
-<p>{depositsInformation}</p>
-<p>{withdrawalsInformation}</p>
-   </div>)
+    title: string;
+    description: string;
+    depositsInformation: string;
+    withdrawalsInformation: string;
+};
+const WalletDescription = ({ title, description, depositsInformation, withdrawalsInformation }: TWalletDescription) => {
+    return (
+        <div>
+            <p>{title}</p>
+            <p>{description}</p>
+            <p>{depositsInformation}</p>
+            <p>{withdrawalsInformation}</p>
+        </div>
+    );
 };
 
 export default WalletDescription;

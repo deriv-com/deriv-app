75392e3245a1481bea620a5127c10a7967aeaa5b
vinu-deriv <vinu@deriv.com>
2022-04-05 11:15:09 +0400

    wallet description component in progress

--- packages/appstore/src/components/wallet-description/wallet-description.tsx
+++ packages/appstore/src/components/wallet-description/wallet-description.tsx
@@ -8,11 +8,14 @@ type TWalletDescription = {
 };
 const WalletDescription = ({ title, description, depositsInformation, withdrawalsInformation }: TWalletDescription) => {
     return (
-        <div>
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{depositsInformation}</p>
-            <p>{withdrawalsInformation}</p>
+        <div className='wallet-details'>
+            <div className='wallet-details__title'>{title}</div>
+            <div className='wallet-details__description'>{description}</div>
+            <div className='wallet-details__information'>
+                <div>{depositsInformation}</div>
+                <div className='wallet-details__horizontal-line' />
+                <div>{withdrawalsInformation}</div>
+            </div>
         </div>
     );
 };
---
+++ packages/appstore/src/constants/wallet-details.ts
@@ -0,0 +1,20 @@
+const wallet_details = {
+    p2p: {
+        title: 'Deriv P2P USD wallet',
+        description: "Deposit and withdraw in Bitcoin, the world's first cryptocurrency.",
+        depositInformation: 'Deposits are instant',
+        withdrawalsInformation: 'Withdrawals take 1 hour to 2 days',
+    },
+    eur: {
+        title: 'EUR wallet',
+        description: 'Quick, simple to use, and available anywhere, anytime. Used by millions of clients worldwide',
+        depositInformation: 'Deposits are instant',
+        withdrawalsInformation: 'Withdrawals take 1 hour to 2 days',
+    },
+    pa: {
+        title: 'Payment agent wallet',
+        description: 'Quick, simple to use, and available anywhere, anytime. Used by millions of clients worldwide',
+        depositInformation: 'Deposits are instant',
+        withdrawalsInformation: 'Withdrawals take 1 hour to 2 days',
+    },
+};

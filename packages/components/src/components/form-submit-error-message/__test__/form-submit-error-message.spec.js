820d8e7305176f1d244dcfb2a713cbcf81a4ee60
Likhith Kolayari <likhith@regentmarkets.com>
2022-04-05 10:14:17 +0400

    Removed unused import

--- packages/components/src/components/form-submit-error-message/__test__/form-submit-error-message.spec.js
+++ packages/components/src/components/form-submit-error-message/__test__/form-submit-error-message.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import FormSubmitErrorMessage from '../form-submit-error-message.jsx';
 
 describe('<FormSubmitErrorMessage/>', () => {

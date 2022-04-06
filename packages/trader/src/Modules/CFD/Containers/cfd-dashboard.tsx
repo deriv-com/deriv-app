e0db3c6f3e486be28b831e6102073de1bc44d8eb
hirad-rework <hirad@re-work.dev>
2022-04-06 19:40:24 +0430

    fix--DerivX Fund transfer bug fix

--- packages/trader/src/Modules/CFD/Containers/cfd-dashboard.tsx
+++ packages/trader/src/Modules/CFD/Containers/cfd-dashboard.tsx
@@ -251,9 +251,9 @@ const CFDDashboard = (props: TCFDDashboardProps) => {
         }
     };
 
-    const openAccountTransfer = (data: DetailsOfEachMT5Loginid, meta: { category: string; type?: string }) => {
+    const openAccountTransfer = (data: DetailsOfEachMT5Loginid & { account_id?: string}, meta: { category: string; type?: string }) => {
         if (meta.category === 'real') {
-            sessionStorage.setItem('cfd_transfer_to_login_id', data.login as string);
+            sessionStorage.setItem('cfd_transfer_to_login_id', data.account_id as string);
             props.disableCFDPasswordModal();
             props.history.push(routes.cashier_acc_transfer);
         } else {

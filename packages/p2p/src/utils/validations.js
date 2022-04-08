3191ab70c096cfe382daf35935f2ca7118860241
Adrienne Rio <adrienne@deriv.com>
2022-04-08 10:36:21 +0800

    Added validation for floating-point representation and prevent users from entering floating-point representation to buy/sell ads input amount

--- packages/p2p/src/utils/validations.js
+++ packages/p2p/src/utils/validations.js
@@ -3,3 +3,5 @@ export const decimalValidator = v => /^(\d+\.)?\d+$/.test(v);
 export const lengthValidator = v => v.length >= 1 && v.length <= 300;
 
 export const textValidator = v => /^[\p{L}\p{Nd}\s'.,:;()@#+/-]*$/u.test(v);
+
+export const floatingPointValidator = v => /[0-9]*[eE\-+]+[0-9]*/.test(v);

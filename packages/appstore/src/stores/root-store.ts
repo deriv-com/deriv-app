3e5589b165ed0e566f92fac31c1eb9ce0495bf78
Bala <bala.krishna@regentmarkets.com>
2022-04-08 14:16:54 +0800

    remove create wallet store

--- packages/appstore/src/stores/root-store.ts
+++ packages/appstore/src/stores/root-store.ts
@@ -1,19 +1,16 @@
 import ConfigStore from './config-store';
-import CreateWalletStore from './create-wallet-store';
 
 export default class RootStore {
     public config: ConfigStore;
     public ws: unknown;
     public client: any;
     public common: any;
-    public create_wallet: CreateWalletStore;
     public ui: any;
 
     public constructor(core_store: any) {
         this.config = new ConfigStore(this);
         this.client = core_store.client;
         this.common = core_store.common;
-        this.create_wallet = new CreateWalletStore(this);
         this.ui = core_store.ui;
     }
 }

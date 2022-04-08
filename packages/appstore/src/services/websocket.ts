c4956659390e1c4cd255614c7b812fd042233e8c
Khalid Ibrahim <khalid@deriv.com>
2022-04-08 13:07:45 +0800

    test linting on ts file

--- packages/appstore/src/services/websocket.ts
+++ packages/appstore/src/services/websocket.ts
@@ -1,7 +1,7 @@
 import { DerivWS } from 'Types';
 
 let ws: DerivWS = null;
-// testing
+
 export const useWs = (): DerivWS => ws;
 
 export const initWs = (ws_instance: DerivWS): void => (ws = ws_instance);

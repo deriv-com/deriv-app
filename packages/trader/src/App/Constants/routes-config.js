36f57525399cb74b8edd2c0a1c68303aee4e3c67
maryiafrantsava-binary <maryia.frantsava@binary.com>
2022-04-11 12:33:49 +0300

    MaryiaF/fix: TrackJS error - Loading chunk 2016 failed

--- packages/trader/src/App/Constants/routes-config.js
+++ packages/trader/src/App/Constants/routes-config.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { routes, makeLazyLoader } from '@deriv/shared';
+import { routes, makeLazyLoader, moduleLoader } from '@deriv/shared';
 import { Loading } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import Trade from 'Modules/Trading';
 
-const ContractDetails = React.lazy(() => import(/* webpackChunkName: "contract" */ 'Modules/Contract'));
+const ContractDetails = React.lazy(() =>
+    moduleLoader(() => import(/* webpackChunkName: "contract" */ 'Modules/Contract'))
+);
 
 // CFD Routes
-const CFD = React.lazy(() => import(/* webpackChunkName: "cfd", webpackPrefetch: true */ 'Modules/CFD'));
+const CFD = React.lazy(() =>
+    moduleLoader(() => import(/* webpackChunkName: "cfd", webpackPrefetch: true */ 'Modules/CFD'))
+);
 
 // Error Routes
-const Page404 = React.lazy(() => import(/* webpackChunkName: "404" */ 'Modules/Page404'));
+const Page404 = React.lazy(() => moduleLoader(() => import(/* webpackChunkName: "404" */ 'Modules/Page404')));
 
 const lazyLoadReportComponent = makeLazyLoader(
-    () => import(/* webpackChunkName: "reports" */ 'Modules/Reports'),
+    () => moduleLoader(() => import(/* webpackChunkName: "reports" */ 'Modules/Reports')),
     () => <Loading />
 );
 

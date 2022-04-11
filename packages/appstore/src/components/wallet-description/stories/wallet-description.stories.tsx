19d0e250da29098bb1a9271e0c098b745059782d
Bala <bala.krishna@regentmarkets.com>
2022-04-11 16:11:07 +0800

    fix wallet description story

--- packages/appstore/src/components/wallet-description/stories/wallet-description.stories.tsx
+++ packages/appstore/src/components/wallet-description/stories/wallet-description.stories.tsx
@@ -2,41 +2,13 @@ import type { Meta, Story } from '@storybook/react';
 import React from 'react';
 import WalletDescription from '../index';
 
-type WalletDescriptionProps = Parameters<typeof WalletDescription>[0];
-
 export default {
     title: 'WalletDescription',
     parameters: { controls: { sort: 'alpha' } },
-    argTypes: {
-        wallet_name: {
-            description: 'Optional. Controls the description of wallet.',
-            control: {
-                type: 'select',
-                options: [
-                    'aud',
-                    'eur',
-                    'gbp',
-                    'usd',
-                    'bitcoin',
-                    'ethereum',
-                    'litecoin',
-                    'tether',
-                    'usd_coin',
-                    'deriv_p2p',
-                    'payment_agent',
-                ],
-            },
-            table: {
-                type: { summary: ' string | undefined' },
-                defaultValue: { summary: 'eur' },
-            },
-        },
-    },
-} as Meta<WalletDescriptionProps>;
+    argTypes: {},
+} as Meta;
 
-const Template: Story<WalletDescriptionProps> = args => <WalletDescription {...args} />;
+const Template: Story = args => <WalletDescription {...args} />;
 
 export const DefaultWalletDescription = Template.bind({});
-DefaultWalletDescription.args = {
-    wallet_name: 'eur',
-};
+DefaultWalletDescription.args = {};

06895decde2b1b77faf6c95a931c52e38cce7afe
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-04-11 14:53:29 +0430

    fix trader files

--- packages/trader/src/Modules/CFD/Components/cfd-demo-account-display.tsx
+++ packages/trader/src/Modules/CFD/Components/cfd-demo-account-display.tsx
@@ -3,7 +3,7 @@ import { localize } from '@deriv/translations';
 import specifications from 'Modules/CFD/Constants/cfd-specifications';
 import { CFDAccountCard } from './cfd-account-card';
 import { general_messages } from '../Constants/cfd-shared-strings';
-import Loading from '../../../templates/_common/components/loading';
+import Loading from '_common/components/loading';
 import { DetailsOfEachMT5Loginid, LandingCompany } from '@deriv/api-types';
 import { TSpecifications } from '../Constants/cfd-specifications';
 import { TTradingPlatformAccounts } from './props.types';
--- packages/trader/src/Modules/CFD/Containers/loading-cfd-real-account-display.tsx
+++ packages/trader/src/Modules/CFD/Containers/loading-cfd-real-account-display.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Loading from '../../../templates/_common/components/loading';
+import Loading from '_common/components/loading';
 
 const LoadingCFDRealAccountDisplay = () => (
     <div className='cfd-real-accounts-display'>

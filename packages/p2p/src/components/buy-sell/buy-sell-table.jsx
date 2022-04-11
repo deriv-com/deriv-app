3a192796cfa3f73e22d2082869d504e3f92d0899
Likhith Kolayari <likhith@regentmarkets.com>
2022-04-11 15:40:44 +0400

    Added console to test

--- packages/p2p/src/components/buy-sell/buy-sell-table.jsx
+++ packages/p2p/src/components/buy-sell/buy-sell-table.jsx
@@ -29,6 +29,8 @@ const BuySellRowRenderer = observer(BuySellRowRendererComponent);
 const BuySellTable = ({ onScroll }) => {
     const { buy_sell_store, general_store, my_profile_store } = useStores();
 
+    console.log('List: ', buy_sell_store.rendered_items);
+
     React.useEffect(
         () => {
             my_profile_store.setIsCancelAddPaymentMethodModalOpen(false);

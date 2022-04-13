57812f4ce7af16878b654ea05c14ace61f7064bc
niloo-fs <niloofar.sadeghi@firstsource.tech>
2022-04-13 12:38:42 +0430

    bot-web-ui webpack configuration for TS

--- packages/bot-web-ui/.eslintrc.js
+++ packages/bot-web-ui/.eslintrc.js
@@ -11,7 +11,13 @@ module.exports = {
         gapi: false,
         __webpack_public_path__: false,
     },
-    extends: '../../.eslintrc.js',
+    extends: [
+        '../../.eslintrc.js',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
     settings: {
         'import/resolver': {
             webpack: { config: webpackConfig({}) },

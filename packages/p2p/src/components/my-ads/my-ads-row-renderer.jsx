8851da8d1687a980bc9097c299d286157adfd57f
Likhith Kolayari <likhith@regentmarkets.com>
2022-04-13 08:39:46 +0400

    Replaced string with constant

--- packages/p2p/src/components/my-ads/my-ads-row-renderer.jsx
+++ packages/p2p/src/components/my-ads/my-ads-row-renderer.jsx
@@ -37,7 +37,7 @@ const MyAdsRowRenderer = observer(({ row: advert, setAdvert }) => {
     const [is_popover_actions_visible, setIsPopoverActionsVisible] = React.useState(false);
 
     const amount_dealt = amount - remaining_amount;
-    const enable_action_point = rate_type === 'fixed' && floating_rate_store.change_ad_alert;
+    const enable_action_point = rate_type === ad_type.FIXED && floating_rate_store.change_ad_alert;
     const is_buy_advert = type === buy_sell.BUY;
 
     const onClickActivateDeactivate = () => {

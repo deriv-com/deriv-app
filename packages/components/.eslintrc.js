9e0a7f8348e64aef39b3ce304d9c23effc4eb5a9
niloo-fs <niloofar.sadeghi@firstsource.tech>
2022-04-13 15:08:08 +0430

    vercel bug fix

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -1,13 +1,14 @@
 const webpackConfig = require('./webpack.config');
 
 module.exports = {
-    extends: [
-        '../../.eslintrc.js',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    extends: '../../.eslintrc.js',
+    // extends: [
+    //     '../../.eslintrc.js',
+    //     'plugin:@typescript-eslint/recommended',
+    //     'plugin:@typescript-eslint/eslint-recommended',
+    // ],
+    // parser: '@typescript-eslint/parser',
+    // plugins: ['@typescript-eslint'],
     settings: {
         react: {
             version: '16',

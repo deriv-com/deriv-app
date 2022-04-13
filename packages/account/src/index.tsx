b88bd245503f8b7e7ba2dba80ad4446ab4028782
niloo-fs <niloofar.sadeghi@firstsource.tech>
2022-04-13 15:52:04 +0430

    vercel bug fix

--- packages/account/src/index.tsx
+++ packages/account/src/index.tsx
@@ -3,7 +3,7 @@ import { makeLazyLoader } from '@deriv/shared';
 import { Loading } from '@deriv/components';
 
 const App = makeLazyLoader(
-    () => import(/* webpackChunkName: "account-app", webpackPreload: true */ './App.jsx'),
+    () => import(/* webpackChunkName: "account-app", webpackPreload: true */ './App'),
     () => <Loading />
 )();
 

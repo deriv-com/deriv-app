7cd1e46fbe357ca978d6d403090104da5d759fa3
Khalid Ibrahim <khalid@deriv.com>
2022-04-13 13:20:16 +0800

    fix eslintrc to load babel plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -102,7 +102,17 @@ module.exports = {
         requireConfigFile: false,
         ecmaVersion: 8,
         babelOptions: {
-            presets: ['@babel/preset-react'],
+            presets: ['@babel/preset-react', '@babel/preset-typescript'],
+            plugins: [
+                ['@babel/plugin-proposal-decorators', { legacy: true }],
+                ['@babel/plugin-proposal-class-properties', { loose: true }],
+                '@babel/plugin-proposal-export-default-from',
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/plugin-proposal-export-namespace-from',
+                '@babel/plugin-syntax-dynamic-import',
+                '@babel/plugin-proposal-optional-chaining',
+                '@babel/plugin-proposal-nullish-coalescing-operator',
+            ],
         },
         ecmaFeatures: {
             jsx: true,

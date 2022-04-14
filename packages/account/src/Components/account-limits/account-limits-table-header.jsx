05b84144d4c26428baf78b7685d665da8075021a
Carol Sachdeva <58209918+carol-binary@users.noreply.github.com>
2022-04-14 16:23:41 +0800

    Revert "Amina/test coverage for accounts_limits spec (#5060)"
    This reverts commit 83fcb87e39e5a418771f01b0a8307213b66ea8f9.

--- packages/account/src/Components/account-limits/account-limits-table-header.jsx
+++ packages/account/src/Components/account-limits/account-limits-table-header.jsx
@@ -10,18 +10,9 @@ const AccountLimitsTableHeader = ({ align, children, renderExtraInfo }) => {
                 'da-account-limits__table-header--left': align !== 'right',
                 'da-account-limits__table-header--right': align === 'right',
             })}
-            data-testid='account_limit_table_header'
         >
             {children && (
-                <Text
-                    align={align}
-                    as='p'
-                    color='prominent'
-                    size='xxs'
-                    line_height='m'
-                    weight='bold'
-                    data-testid='account_limit_table_header_text'
-                >
+                <Text align={align} as='p' color='prominent' size='xxs' line_height='m' weight='bold'>
                     {children}
                 </Text>
             )}

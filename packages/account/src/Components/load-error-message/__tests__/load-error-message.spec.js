817a41733f91bb16c73d34e3de1de506dc86b8af
nikita-binary <nikita@binary.com>
2022-04-15 11:40:18 +0300

    feat: add proper description

--- packages/account/src/Components/load-error-message/__tests__/load-error-message.spec.js
+++ packages/account/src/Components/load-error-message/__tests__/load-error-message.spec.js
@@ -5,7 +5,7 @@ import LoadErrorMessage from '../load-error-message';
 jest.mock('Components/icon-message-content', () => () => <div>IconMessageContent</div>);
 
 describe('LoadErrorMessage', () => {
-    it('should render LoadErrorMessage component', () => {
+    it('should render LoadErrorMessage component with content', () => {
         render(<LoadErrorMessage message='message' />);
         expect(screen.getByText('IconMessageContent')).toBeInTheDocument();
     });

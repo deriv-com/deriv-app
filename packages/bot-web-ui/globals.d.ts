1bf547354c038b8e26154fbe3b11f8f2a7d025e5
niloo-fs <niloofar.sadeghi@firstsource.tech>
2022-04-17 13:45:18 +0430

    ts configuration for bot-skeleton and bot-web-ui

---
+++ packages/bot-web-ui/globals.d.ts
@@ -0,0 +1,10 @@
+declare module '@deriv/account';
+declare module '@deriv/bot-skeleton';
+declare module '@deriv/cashier';
+declare module '@deriv/components';
+declare module '@deriv/appstore';
+declare module '@deriv/p2p';
+declare module '@deriv/trader';
+declare module '@deriv/translations';
+declare module '@deriv/shared';
+declare module '@deriv/bot-skeleton/src/constants/messages';

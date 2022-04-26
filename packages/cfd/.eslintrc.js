a382bd73a4b9747e2e041f8f4922d6d157278743
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-04-26 18:51:00 +0430

    fix/ eslintrc improvement for cfd and account packages

--- packages/cfd/.eslintrc.js
+++ packages/cfd/.eslintrc.js
@@ -1,7 +1,13 @@
 const webpackConfig = require('./build/webpack.config-test.js');
 
 module.exports = {
-    extends: '../../.eslintrc.js',
+    extends: [
+        '../../.eslintrc.js',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
     settings: {
         'import/resolver': {
             webpack: { config: webpackConfig({}) },

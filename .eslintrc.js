ea7244b2aee6513d364855683eb0d9b6a6bc5f1f
Khalid Ibrahim <khalid@deriv.com>
2022-04-27 15:15:37 +0800

    add ts files override

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     parser: '@babel/eslint-parser',
-    plugins: ['prettier', 'testing-library'],
+    plugins: ['prettier', 'testing-library', '@typescript-eslint'],
     env: {
         es6: true,
         browser: true,
@@ -128,5 +128,60 @@ module.exports = {
             files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
             extends: ['plugin:testing-library/react'],
         },
+        {
+            files: ['*.ts', '*.tsx'],
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint'],
+            extends: [
+                'plugin:react/recommended',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:prettier/recommended',
+            ],
+            parserOptions: {
+                ecmaversion: 2018,
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                babelOptions: {
+                    presets: ['@babel/preset-react', '@babel/preset-typescript'],
+                    plugins: [
+                        ['@babel/plugin-proposal-decorators', { legacy: true }],
+                        ['@babel/plugin-proposal-class-properties', { loose: true }],
+                        '@babel/plugin-proposal-export-default-from',
+                        '@babel/plugin-proposal-object-rest-spread',
+                        '@babel/plugin-proposal-export-namespace-from',
+                        '@babel/plugin-syntax-dynamic-import',
+                        '@babel/plugin-proposal-optional-chaining',
+                        '@babel/plugin-proposal-nullish-coalescing-operator',
+                    ],
+                },
+            },
+            settings: {
+                react: {
+                    version: 'detect',
+                },
+            },
+        },
+        // {
+
+        //     files: ['*.ts', '*.tsx'],
+        //     parser: '@typescript-eslint/parser',
+        //     parserOptions: {
+        //         babelOptions: {
+        //             presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        //             plugins: [
+        //                 ['@babel/plugin-proposal-decorators', { legacy: true }],
+        //                 ['@babel/plugin-proposal-class-properties', { loose: true }],
+        //                 '@babel/plugin-proposal-export-default-from',
+        //                 '@babel/plugin-proposal-object-rest-spread',
+        //                 '@babel/plugin-proposal-export-namespace-from',
+        //                 '@babel/plugin-syntax-dynamic-import',
+        //                 '@babel/plugin-proposal-optional-chaining',
+        //                 '@babel/plugin-proposal-nullish-coalescing-operator',
+        //             ],
+        //         },
+        //     },
+        // },
     ],
 };

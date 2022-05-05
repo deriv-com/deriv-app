12a791361affcaef0846a09504717f7e1dc4e8ae
Likhith Kolayari <likhith@regentmarkets.com>
2022-05-05 17:58:18 +0400

    refactor: Renamed variables

--- packages/p2p/src/utils/format-value.js
+++ packages/p2p/src/utils/format-value.js
@@ -1,13 +1,13 @@
-export const roundOffDecimal = (number, decimal_point = 2) => {
-    // Rounds of the digit to the specified decimal point
-    return parseFloat(Math.round(number * Math.pow(10, decimal_point)) / Math.pow(10, decimal_point));
+export const roundOffDecimal = (number, decimal_place = 2) => {
+    // Rounds of the digit to the specified decimal place
+    return parseFloat(Math.round(number * Math.pow(10, decimal_place)) / Math.pow(10, decimal_place));
 };
 
-export const setDecimalPlaces = (value, expected_decimal_point) => {
+export const setDecimalPlaces = (value, expected_decimal_place) => {
     // Returns the accurate number of decimal places to prevent trailing zeros
     if (!value?.toString()) {
         return 0;
     }
-    const actual_decimal_point = value.toString().split('.')[1]?.length;
-    return actual_decimal_point > expected_decimal_point ? expected_decimal_point : actual_decimal_point;
+    const actual_decimal_place = value.toString().split('.')[1]?.length;
+    return actual_decimal_place > expected_decimal_place ? expected_decimal_place : actual_decimal_place;
 };

ab9623ad0e838cf9bf9305a05e6dd038682ea780
vinu-deriv <vinu@deriv.com>
2022-05-09 15:30:30 +0400

    refactored code for init function in general store cashier

--- packages/cashier/src/Stores/general-store.js
+++ packages/cashier/src/Stores/general-store.js
@@ -30,9 +30,14 @@ export default class GeneralStore extends BaseStore {
                 this.root_store.client.is_logged_in,
                 this.root_store.client.currency,
             ],
-            () => {
-                this.init();
+            async () => {
+                await this.init();
             }
+        );
+
+        reaction(
+            () => this.root_store.client.currency,
+            () => withdraw.setIsWithdrawConfirmed(false)
         );
     }
 
@@ -217,11 +222,13 @@ export default class GeneralStore extends BaseStore {
 
     // Initialise P2P attributes on app load without mounting the entire cashier
     @action.bound
-    init() {
+    async init() {
         if (this.root_store.modules.cashier) {
-            const { client, modules } = this.root_store;
+            const {
+                client: { is_logged_in, switched },
+                modules,
+            } = this.root_store;
             const { account_prompt_dialog, withdraw } = modules.cashier;
-            const { currency, is_logged_in, switched } = client;
 
             when(
                 () => is_logged_in,
@@ -232,24 +239,15 @@ export default class GeneralStore extends BaseStore {
                 }
             );
 
-            reaction(
-                () => [switched, is_logged_in, currency],
-                async () => {
-                    // wait for client settings to be populated in client-store
-                    await this.WS.wait('get_settings');
-
-                    if (is_logged_in) {
-                        await this.getAdvertizerError();
-                        account_prompt_dialog.resetLastLocation();
-                        if (!switched) {
-                            this.checkP2pStatus();
-                            // check if withdrawal limit is reached
-                            // if yes, this will trigger to show a notification
-                            await this.check10kLimit();
-                        }
-                    }
+            if (is_logged_in) {
+                account_prompt_dialog.resetLastLocation();
+                if (!switched) {
+                    this.checkP2pStatus();
+                    // check if withdrawal limit is reached
+                    // if yes, this will trigger to show a notification
+                    await withdraw.check10kLimit();
                 }
-            );
+            }
         }
     }
 

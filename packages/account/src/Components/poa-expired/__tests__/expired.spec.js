7348d3a53d710fd39fd860db3c928bf8af423e2c
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-05-11 12:29:10 +0300

    yauheni / test shanpshot for circkeCI check

--- packages/account/src/Components/poa-expired/__tests__/expired.spec.js
+++ packages/account/src/Components/poa-expired/__tests__/expired.spec.js
@@ -5,13 +5,20 @@ import { Expired } from '../expired';
 describe('<Expired/>', () => {
     const message = 'New proof of address is needed';
     const text = 'Your documents for proof of address is expired. Please submit again.';
+    const onClick = jest.fn();
 
     it('should render Expired component', () => {
-        render(<Expired />);
+        render(<Expired onClick={onClick} />);
 
         expect(screen.getByText(message)).toBeInTheDocument();
         expect(screen.getByText(text)).toBeInTheDocument();
         expect(screen.getByText('Resubmit')).toBeInTheDocument();
         expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    it('should render Expired component snapshot check', () => {
+        const tree = render(<Expired onClick={onClick} />);
+
+        expect(tree).toMatchSnapshot();
     });
 });

c185a84702b2b664ebc8e1f0fb3bfa03cae28fe4
George Usynin <103181646+george-usynin-binary@users.noreply.github.com>
2022-05-11 10:40:53 +0300

    george / add test coverage for general store (#5361)
    * add tests and set up jest config file
    
    * remove unreachable reaction
    
    * change description of the test case
    
    * fix spelling, remove duplication tests

--- packages/cashier/src/Stores/general-store.js
+++ packages/cashier/src/Stores/general-store.js
@@ -242,7 +242,26 @@ export default class GeneralStore extends BaseStore {
                     // if yes, this will trigger to show a notification
                     await withdraw.check10kLimit();
                 }
-            }
+            );
+
+            reaction(
+                () => [switched, is_logged_in, currency],
+                async () => {
+                    // wait for client settings to be populated in client-store
+                    await this.WS.wait('get_settings');
+
+                    if (is_logged_in) {
+                        await this.getAdvertizerError();
+                        account_prompt_dialog.resetLastLocation();
+                        if (!switched) {
+                            this.checkP2pStatus();
+                            // check if withdrawal limit is reached
+                            // if yes, this will trigger to show a notification
+                            await this.check10kLimit();
+                        }
+                    }
+                }
+            );
         }
     }
 

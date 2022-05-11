a13446959674d62215db7094962bd0473060a3e4
maryiafrantsava-binary <maryia.frantsava@binary.com>
2022-05-11 17:02:46 +0300

    MaryiaF/fix: The amount of Balance is not Properly shown in the Journal

--- packages/bot-skeleton/src/services/tradeEngine/trade/Balance.js
+++ packages/bot-skeleton/src/services/tradeEngine/trade/Balance.js
@@ -27,11 +27,6 @@ export default Engine =>
             const balance = (client && client.balance) || 0;
             let value = balance;
 
-            if (scope === 'BEFORE_PURCHASE') {
-                // Deduct trade amount in this scope for correct (🤦) value in balance-block
-                value = Number(balance) - this.tradeOptions.amount;
-            }
-
             balance_string = getFormattedText(value, client.currency, false);
             return type === 'STR' ? balance_string : balance;
         }

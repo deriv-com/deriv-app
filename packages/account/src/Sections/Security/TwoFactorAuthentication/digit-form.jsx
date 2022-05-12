0d841ac550bfb7455efdcd62d1a5ebfd60959b9b
Thisyahlen <thisyahlen@Thisyahlens-MacBook-Pro.local>
2022-05-12 14:54:47 +0800

    2falogout enable

--- packages/account/src/Sections/Security/TwoFactorAuthentication/digit-form.jsx
+++ packages/account/src/Sections/Security/TwoFactorAuthentication/digit-form.jsx
@@ -5,7 +5,7 @@ import { Input, Button } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import { getPropertyValue, WS } from '@deriv/shared';
 
-const DigitForm = ({ is_enabled, setEnabled }) => {
+const DigitForm = ({ is_enabled, setEnabled, logoutClient }) => {
     const [is_success, setSuccess] = React.useState(false);
     const button_text = is_enabled ? localize('Disable 2FA') : localize('Enable');
 
@@ -50,6 +50,7 @@ const DigitForm = ({ is_enabled, setEnabled }) => {
             setSuccess(true);
             resetForm();
             setEnabled(is_enabled_response);
+            logoutClient()
         }
     };
 

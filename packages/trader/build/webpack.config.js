289742a92dcadb7441e1dc0eae25c605c702f2bb
hirad-rework <hirad@re-work.dev>
2022-05-15 13:09:05 +0430

    Removed redundant packages of trader

--- packages/trader/build/webpack.config.js
+++ packages/trader/build/webpack.config.js
@@ -45,7 +45,6 @@ module.exports = function (env) {
                 '@deriv/components': '@deriv/components',
                 '@deriv/translations': '@deriv/translations',
                 '@deriv/deriv-charts': '@deriv/deriv-charts',
-                '@deriv/account': '@deriv/account',
             },
             /^@deriv\/shared\/.+$/,
             /^@deriv\/components\/.+$/,

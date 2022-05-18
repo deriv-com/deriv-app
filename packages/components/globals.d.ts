4c0020e5d4a0c0fcabf4a24debcaba283642c4f3
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-05-18 12:06:40 +0430

    remove extra packages from globals.d.ts

--- packages/components/globals.d.ts
+++ packages/components/globals.d.ts
@@ -1,9 +1,2 @@
-declare module '@deriv/account';
-declare module '@deriv/bot-skeleton';
-declare module '@deriv/bot-web-ui';
-declare module '@deriv/cashier';
-declare module '@deriv/appstore';
-declare module '@deriv/p2p';
-declare module '@deriv/trader';
 declare module '@deriv/translations';
 declare module '@deriv/shared';

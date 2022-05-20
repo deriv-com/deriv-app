2a75980dac7d36793b18277e1f3e6642e88b8b5c
jim-deriv <jim@deriv.com>
2022-05-20 16:54:15 +0800

    chore: remove duplicate config from cfd package

--- packages/cfd/.eslintrc.js
+++ packages/cfd/.eslintrc.js
@@ -1,13 +1,7 @@
 const webpackConfig = require('./build/webpack.config-test.js');
 
 module.exports = {
-    extends: [
-        '../../.eslintrc.js',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-    ],
-    parser: '@babel/eslint-parser',
-    plugins: ['@typescript-eslint'],
+    extends: ['../../.eslintrc.js'],
     settings: {
         'import/resolver': {
             webpack: { config: webpackConfig({}) },

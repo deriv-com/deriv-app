ad940fd8f3fdd6bf3c5f7ea53229df1aa05c98b1
hirad-rework <hirad@re-work.dev>
2022-05-22 14:38:24 +0430

    Tests for cfd-account-copy and loading-cfd-real-account-display

--- packages/cfd/src/Components/cfd-account-copy.tsx
+++ packages/cfd/src/Components/cfd-account-copy.tsx
@@ -6,7 +6,7 @@ import { TCFDAccountCopy } from './props.types';
 
 const CFDAccountCopy = ({ text, className }: TCFDAccountCopy) => {
     return (
-        <div className={className}>
+        <div className={className} data-testid="cfd-account-copy-test">
             <Clipboard
                 text_copy={text}
                 info_message={isMobile() ? '' : localize('copy')}

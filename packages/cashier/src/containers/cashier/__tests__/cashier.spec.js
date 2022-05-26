7f672d81d5620a8da726415288a4b7ecf4b615c8
george-usynin-binary <heorhi@binary.com>
2022-05-26 14:43:13 +0300

    fix test files mocks

--- packages/cashier/src/containers/cashier/__tests__/cashier.spec.js
+++ packages/cashier/src/containers/cashier/__tests__/cashier.spec.js
@@ -35,10 +35,10 @@ jest.mock('@deriv/shared', () => {
     };
 });
 
-jest.mock('../account-prompt-dialog/account-prompt-dialog', () => jest.fn(() => 'mockedAccountPromptDialog'));
-jest.mock('../error-dialog/error-dialog', () => jest.fn(() => 'mockedErrorDialog'));
-jest.mock('Pages/withdrawal/withdrawal', () => jest.fn(() => 'mockedWithdrawal'));
-jest.mock('Pages/deposit/deposit', () => jest.fn(() => 'mockedDeposit'));
+jest.mock('../account-prompt-dialog', () => jest.fn(() => 'mockedAccountPromptDialog'));
+jest.mock('../error-dialog', () => jest.fn(() => 'mockedErrorDialog'));
+jest.mock('../../withdrawal', () => jest.fn(() => 'mockedWithdrawal'));
+jest.mock('../../deposit', () => jest.fn(() => 'mockedDeposit'));
 
 describe('<Cashier />', () => {
     let history;

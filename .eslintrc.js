773bf23e518d10849908afdf7e66285723808932
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-05-30 19:31:00 +0430

    ci/ exclude dist files from eslint scan

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
         dataLayer: true,
         texts_json: false,
     },
+    ignorePatterns: ['**/dist/**/*.js'],
     rules: {
         camelcase: 0,
         // semi                                : ['error', 'always'],
@@ -86,7 +87,12 @@ module.exports = {
         // 'react/jsx-indent-props'            : ['error', 4],
         // 'react/jsx-max-props-per-line'      : ['error', { when: 'multiline' }],
         // 'react/jsx-tag-spacing'             : ['error', { closingSlash: 'never', beforeSelfClosing: 'always' }],
-        'react/prop-types': 0,
+        'react/prop-types': [
+            1,
+            {
+                skipUndeclared: true,
+            },
+        ],
         'react/self-closing-comp': 'error',
         // 'react/sort-prop-types'             : ['error', { ignoreCase: true, sortShapeProp: true }],
     },

ef27be98614199ef5a4b53ef1c379ec0f99a833a
Likhith Kolayari <likhith@regentmarkets.com>
2022-05-30 10:17:34 +0400

    fix: checking for response before executing function block

--- packages/p2p/src/stores/my-ads-store.js
+++ packages/p2p/src/stores/my-ads-store.js
@@ -154,23 +154,25 @@ export default class MyAdsStore extends BaseStore {
         const createAd = () => {
             requestWS(create_advert).then(response => {
                 // If we get an error we should let the user submit the form again else we just go back to the list of ads
-                if (response.error) {
-                    this.setApiErrorCode(response.error.code);
-                    this.setApiErrorMessage(response.error.message);
-                    setSubmitting(false);
-                } else if (should_not_show_auto_archive_message !== 'true' && this.adverts_archive_period) {
-                    this.setAdvertDetails(response.p2p_advert_create);
-                    setTimeout(() => {
-                        if (!this.is_api_error_modal_visible) {
-                            this.setIsAdCreatedModalVisible(true);
-                        }
-                    }, 200);
-                } else if (!this.is_api_error_modal_visible && !this.is_ad_created_modal_visible) {
-                    if (!response.p2p_advert_create.is_visible) {
+                if (response) {
+                    if (response.error) {
+                        this.setApiErrorCode(response.error.code);
+                        this.setApiErrorMessage(response.error.message);
+                        setSubmitting(false);
+                    } else if (should_not_show_auto_archive_message !== 'true' && this.adverts_archive_period) {
                         this.setAdvertDetails(response.p2p_advert_create);
-                        this.setIsAdExceedsDailyLimitModalOpen(true);
+                        setTimeout(() => {
+                            if (!this.is_api_error_modal_visible) {
+                                this.setIsAdCreatedModalVisible(true);
+                            }
+                        }, 200);
+                    } else if (!this.is_api_error_modal_visible && !this.is_ad_created_modal_visible) {
+                        if (!response.p2p_advert_create.is_visible) {
+                            this.setAdvertDetails(response.p2p_advert_create);
+                            this.setIsAdExceedsDailyLimitModalOpen(true);
+                        }
+                        this.setShowAdForm(false);
                     }
-                    this.setShowAdForm(false);
                 }
             });
         };
@@ -192,11 +194,13 @@ export default class MyAdsStore extends BaseStore {
     onClickActivateDeactivate(id, is_ad_active, setIsAdvertActive) {
         if (!this.root_store.general_store.is_barred) {
             requestWS({ p2p_advert_update: 1, id, is_active: is_ad_active ? 0 : 1 }).then(response => {
-                if (response.error) {
-                    this.setApiErrorCode(response.error.code);
-                    this.setActivateDeactivateErrorMessage(response.error.message);
-                } else {
-                    setIsAdvertActive(!!response.p2p_advert_update.is_active);
+                if (response) {
+                    if (response.error) {
+                        this.setApiErrorCode(response.error.code);
+                        this.setActivateDeactivateErrorMessage(response.error.message);
+                    } else {
+                        setIsAdvertActive(!!response.p2p_advert_update.is_active);
+                    }
                 }
                 this.setSelectedAdId('');
             });

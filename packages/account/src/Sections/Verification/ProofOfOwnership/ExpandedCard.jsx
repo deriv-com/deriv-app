615528efe99196df57872101db1d7d504a5ee0d6
jim-deriv <jim@deriv.com>
2022-06-01 09:51:36 +0800

    feat: add dynamic input field rendering to expanded card

--- packages/account/src/Sections/Verification/ProofOfOwnership/ExpandedCard.jsx
+++ packages/account/src/Sections/Verification/ProofOfOwnership/ExpandedCard.jsx
@@ -6,13 +6,15 @@ import SampleCreditCardModal from 'Components/sample-credit-card-modal';
 
 const ExpandedCard = ({ cardDetails, handleChange, handleBlur, identifier, values, setFieldValue, index, error }) => {
     const [is_sample_modal_open, setIsSampleModalOpen] = useState(false);
-
+    const controlsToShow = [...Array(cardDetails.documents_required).keys()];
     const handleUploadedFile = (name, file) => {
         setFieldValue(name, file);
     };
 
     const exampleLink = () =>
-        cardDetails.icon === 'IcCreditCard' ? (
+        cardDetails.icon === 'IcCreditCard' ||
+        cardDetails.icon === 'IcStockVisa' ||
+        cardDetails.icon === 'IcStockMasterCard' ? (
             <span
                 className='proof-of-ownership__card-open-desc-link'
                 key={0}
@@ -46,7 +48,7 @@ const ExpandedCard = ({ cardDetails, handleChange, handleBlur, identifier, value
                 ))}
                 <fieldset>
                     <div className='proof-of-ownership__card-open-inputs'>
-                        {cardDetails.input_label && (
+                        {cardDetails.input_label && cardDetails.icon !== 'IcCreditCard' && (
                             <div className='proof-of-ownership__card-open-inputs-field'>
                                 <Input
                                     label={cardDetails.input_label}
@@ -61,26 +63,42 @@ const ExpandedCard = ({ cardDetails, handleChange, handleBlur, identifier, value
                                 />
                             </div>
                         )}
-                        <div className='proof-of-ownership__card-open-inputs-upload'>
-                            <FileUploader
-                                handleFile={handleUploadedFile}
-                                fileName={values?.data?.[index]?.file?.name}
-                                dataTestID={`uploader-${values?.data?.[index]?.id}`}
-                                className='proof-of-ownership__card-open-inputs-photo'
-                                name={`data[${index}].file`}
-                                error={error?.file}
-                            />
-                            {cardDetails.icon === 'IcOnlineNaira' && (
-                                <FileUploader
-                                    handleFile={handleUploadedFile}
-                                    fileName={values?.data?.[index]?.file?.name}
-                                    dataTestID={`uploader-${values?.data?.[index]?.id}`}
-                                    className='proof-of-ownership__card-open-inputs-photo'
-                                    name={`data[${index}].file`}
-                                    error={error?.file}
-                                />
-                            )}
-                        </div>
+                        {controlsToShow.map(i => (
+                            <>
+                                {cardDetails.icon === 'IcCreditCard' && (
+                                    <div className='proof-of-ownership__card-open-inputs-field'>
+                                        <Input
+                                            label={cardDetails.input_label}
+                                            data-lpignore='true'
+                                            className='proof-of-ownership__card-open-inputs-cardnumber'
+                                            type='text'
+                                            onChange={handleChange}
+                                            disabled
+                                            value={formatIdentifier(identifier, cardDetails.icon)}
+                                            onBlur={handleBlur}
+                                            maxLength='19'
+                                        />
+                                    </div>
+                                )}
+                                <div
+                                    className={`proof-of-ownership__card-open-inputs-upload ${
+                                        !cardDetails.input_label ? 'expand' : ''
+                                    }
+                                        ${cardDetails.input_label !== null ? 'organise' : ''}
+                                    `}
+                                    key={i}
+                                >
+                                    <FileUploader
+                                        handleFile={handleUploadedFile}
+                                        fileName={values?.data?.[index]?.files[i]?.file?.name}
+                                        dataTestID={`uploader-${values?.data?.[index]?.files[i]?.id}`}
+                                        className='proof-of-ownership__card-open-inputs-photo'
+                                        name={`data[${index}].files[${i}].file`}
+                                        error={error?.files[i]?.file}
+                                    />
+                                </div>
+                            </>
+                        ))}
                     </div>
                 </fieldset>
             </div>

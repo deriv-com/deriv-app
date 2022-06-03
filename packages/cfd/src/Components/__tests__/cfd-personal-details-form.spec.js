ec7da09a3311c863a2b61b7a361743bf0bc4ad86
maryia-binary <maryia@binary.com>
2022-06-03 13:28:48 +0300

    added tests for more use cases

--- packages/cfd/src/Components/__tests__/cfd-personal-details-form.spec.js
+++ packages/cfd/src/Components/__tests__/cfd-personal-details-form.spec.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CFDPersonalDetailsForm from '../cfd-personal-details-form';
@@ -85,35 +85,6 @@ describe('<CFDPersonalDetailsForm />', () => {
                 text: 'Aland Islands',
                 value: 'ax',
             },
-            {
-                identity: {
-                    services: {
-                        idv: {
-                            documents_supported: {},
-                            has_visual_sample: 0,
-                            is_country_supported: 0,
-                        },
-                        onfido: {
-                            documents_supported: {
-                                driving_licence: {
-                                    display_name: 'Driving Licence',
-                                },
-                                national_identity_card: {
-                                    display_name: 'National Identity Card',
-                                },
-                                passport: {
-                                    display_name: 'Passport',
-                                },
-                            },
-                            is_country_supported: 1,
-                        },
-                    },
-                },
-                phone_idd: '355',
-                text: 'Albania',
-                tin_format: ['^[A-Ta-t0-9]\\d{8}[A-Wa-w]$'],
-                value: 'al',
-            },
         ],
         value: {
             citizen: '',
@@ -125,28 +96,23 @@ describe('<CFDPersonalDetailsForm />', () => {
 
     it('should render properly', () => {
         render(<CFDPersonalDetailsForm {...props} />);
+        const citizenship_input = screen.getByRole('textbox', { name: /citizenship/i });
+        const tax_residence = screen.getByRole('textbox', { name: /tax residence/i });
+        const tax_id_input = screen.getByRole('textbox', { name: /tax identification number/i });
+        const account_opening_reason = screen.getByText(/account opening reason/i);
+        const next_button = screen.getByRole('button', { name: /next/i });
+
         expect(
             screen.getByText(
                 /any information you provide is confidential and will be used for verification purposes only\./i
             )
         ).toBeInTheDocument();
         expect(screen.getAllByText('FormSubHeader').length).toBe(3);
-        expect(
-            screen.getByRole('textbox', {
-                name: /citizenship/i,
-            })
-        ).toBeInTheDocument();
-        expect(
-            screen.getByRole('textbox', {
-                name: /tax residence/i,
-            })
-        ).toBeInTheDocument();
-        expect(
-            screen.getByRole('textbox', {
-                name: /tax identification number/i,
-            })
-        ).toBeInTheDocument();
-        expect(screen.getByText(/account opening reason/i)).toBeInTheDocument();
+        expect(citizenship_input).toBeInTheDocument();
+        expect(tax_residence).toBeInTheDocument();
+        expect(tax_id_input).toBeInTheDocument();
+        expect(account_opening_reason).toBeInTheDocument();
+        expect(next_button).toBeEnabled();
     });
 
     it("should show that it's loading when is_loading is true", () => {
@@ -154,27 +120,101 @@ describe('<CFDPersonalDetailsForm />', () => {
         expect(container.querySelector('.initial-loader')).toBeInTheDocument();
     });
 
-    it('should show an error message and icon if there is a form error received from server', () => {
-        render(<CFDPersonalDetailsForm {...props} form_error='Request Processing Error' />);
+    it('should show an error message and icon if there is a general form error', () => {
+        render(<CFDPersonalDetailsForm {...props} form_error='Form submission failed.' />);
         expect(screen.getByTestId('form-submit-error')).toHaveClass('dc-icon');
-        expect(screen.getByText('Request Processing Error')).toBeInTheDocument();
+        expect(screen.getByText('Form submission failed.')).toBeInTheDocument();
+    });
+
+    it('should enable the Next button in case of all required fields are filled', async () => {
+        render(<CFDPersonalDetailsForm {...props} />);
+
+        const citizenship_input = screen.getByRole('textbox', { name: /citizenship/i });
+        const tax_residence = screen.getByRole('textbox', { name: /tax residence/i });
+        const tax_id_input = screen.getByRole('textbox', { name: /tax identification number/i });
+        const account_opening_reason = screen.getByText(/account opening reason/i);
+        const next_button = screen.getByRole('button', { name: /next/i });
+
+        const citizenship_error = 'Citizenship is required';
+        const tax_residence_error = 'Tax residence is required';
+        const tax_id_error = 'Tax identification number is required';
+        const account_opening_reason_error = 'Account opening reason is required';
+
+        fireEvent.change(citizenship_input, { target: { value: 'Indonesia' } });
+        fireEvent.click(next_button);
+        await waitFor(() => {
+            expect(screen.queryByText(citizenship_error)).not.toBeInTheDocument();
+            expect(screen.getByText(tax_residence_error)).toBeInTheDocument();
+            expect(screen.getByText(tax_id_error)).toBeInTheDocument();
+            expect(screen.getByText(account_opening_reason_error)).toBeInTheDocument();
+            expect(next_button).toBeDisabled();
+        });
+
+        fireEvent.change(tax_residence, { target: { value: 'Indonesia' } });
+        fireEvent.click(next_button);
+        await waitFor(() => {
+            expect(screen.queryByText(citizenship_error)).not.toBeInTheDocument();
+            expect(screen.queryByText(tax_residence_error)).not.toBeInTheDocument();
+            expect(screen.getByText(tax_id_error)).toBeInTheDocument();
+            expect(screen.getByText(account_opening_reason_error)).toBeInTheDocument();
+            expect(next_button).toBeDisabled();
+        });
+
+        fireEvent.change(tax_id_input, { target: { value: '023124224563456' } });
+        fireEvent.click(next_button);
+        await waitFor(() => {
+            expect(screen.queryByText(citizenship_error)).not.toBeInTheDocument();
+            expect(screen.queryByText(tax_residence_error)).not.toBeInTheDocument();
+            expect(screen.queryByText(tax_id_error)).not.toBeInTheDocument();
+            expect(screen.getByText(account_opening_reason_error)).toBeInTheDocument();
+            expect(next_button).toBeDisabled();
+        });
+
+        fireEvent.click(account_opening_reason);
+        const hedging = within(screen.getByRole('list')).getByText('Hedging');
+        fireEvent.click(hedging);
+        fireEvent.click(account_opening_reason);
+        await waitFor(() => {
+            expect(screen.queryByText(citizenship_error)).not.toBeInTheDocument();
+            expect(screen.queryByText(tax_residence_error)).not.toBeInTheDocument();
+            expect(screen.queryByText(tax_id_error)).not.toBeInTheDocument();
+            expect(screen.queryByText(account_opening_reason_error)).not.toBeInTheDocument();
+            expect(next_button).toBeEnabled();
+        });
+
+        fireEvent.click(next_button);
+        await waitFor(() => {
+            expect(props.onSubmit).toHaveBeenCalledTimes(1);
+        });
     });
 
     it('should disable the Next button in case of erroneous input in a required field', async () => {
         render(<CFDPersonalDetailsForm {...props} />);
-        expect(
-            screen.getByText(
-                /any information you provide is confidential and will be used for verification purposes only\./i
-            )
-        ).toBeInTheDocument();
-        const tax_id_input = screen.getByRole('textbox', {
-            name: /tax identification number/i,
-        });
-        const next_button = screen.getByRole('button', {
-            name: /next/i,
-        });
+
+        const tax_id_input = screen.getByRole('textbox', { name: /tax identification number/i });
+        const next_button = screen.getByRole('button', { name: /next/i });
+
         fireEvent.change(tax_id_input, { target: { value: 'invalid_text_id_0' } });
-        fireEvent.focusOut(tax_id_input);
-        await waitFor(() => expect(next_button).toBeDisabled());
+        fireEvent.click(next_button);
+
+        await waitFor(() => {
+            expect(next_button).toBeDisabled();
+            expect(screen.getByText('Tax identification number is not properly formatted.')).toBeInTheDocument();
+        });
+    });
+
+    it('should disable the Next button in case of an empty input in a required field', async () => {
+        render(<CFDPersonalDetailsForm {...props} />);
+
+        const citizenship_input = screen.getByRole('textbox', { name: /citizenship/i });
+        const next_button = screen.getByRole('button', { name: /next/i });
+
+        fireEvent.change(citizenship_input, { target: { value: '' } });
+        fireEvent.click(next_button);
+
+        await waitFor(() => {
+            expect(next_button).toBeDisabled();
+            expect(screen.getByText('Citizenship is required')).toBeInTheDocument();
+        });
     });
 });

00caad88af90b58db295b97705a144efbafce0d8
adrienne@deriv.com <adrienne@deriv.com>
2022-06-03 13:39:48 +0800

    Refactored overlay component as a wrapper

--- packages/p2p/src/components/advertiser-page/advertiser-page.jsx
+++ packages/p2p/src/components/advertiser-page/advertiser-page.jsx
@@ -29,7 +29,6 @@ const AdvertiserPage = () => {
         last_name,
         sell_orders_count,
     } = advertiser_page_store.advertiser_info;
-
     const joined_since = daysSince(created_time);
 
     React.useEffect(() => {
@@ -66,49 +65,52 @@ const AdvertiserPage = () => {
                 onClick={buy_sell_store.hideAdvertiserPage}
                 page_title={localize("Advertiser's page")}
             />
-            <div className='advertiser-page-details-container'>
-                <div className='advertiser-page__header-details'>
-                    <UserAvatar
-                        nickname={advertiser_page_store.advertiser_details_name}
-                        size={isMobile() ? 32 : 64}
-                        text_size={isMobile() ? 's' : 'sm'}
-                    />
-                    <div className='advertiser-page__header-name--column'>
-                        <div className='advertiser-page__header-name'>
-                            <Text color='prominent' line-height='m' size='s' weight='bold'>
-                                {advertiser_page_store.advertiser_details_name}
+            {/* TODO: Show Unblock confirmation modal when user clicks on this button */}
+            <BlockUserOverlay is_visible={is_blocked} onUnblock={() => true}>
+                <div className='advertiser-page-details-container'>
+                    <div className='advertiser-page__header-details'>
+                        <UserAvatar
+                            nickname={advertiser_page_store.advertiser_details_name}
+                            size={isMobile() ? 32 : 64}
+                            text_size={isMobile() ? 's' : 'sm'}
+                        />
+                        <div className='advertiser-page__header-name--column'>
+                            <div className='advertiser-page__header-name'>
+                                <Text color='prominent' line-height='m' size='s' weight='bold'>
+                                    {advertiser_page_store.advertiser_details_name}
+                                </Text>
+                                {first_name && last_name && (
+                                    <div className='advertiser-page__header-real-name'>
+                                        <Text color='less-prominent' line_height='xs' size='xs'>
+                                            {`(${first_name} ${last_name})`}
+                                        </Text>
+                                    </div>
+                                )}
+                            </div>
+                            <Text color='less-prominent' size={isMobile() ? 'xxxs' : 'xs'}>
+                                {joined_since > 0 ? (
+                                    <Localize
+                                        i18n_default_text='Joined {{days_since_joined}}d'
+                                        values={{ days_since_joined: joined_since }}
+                                    />
+                                ) : (
+                                    <Localize i18n_default_text='Joined today' />
+                                )}
                             </Text>
-                            {first_name && last_name && (
-                                <div className='advertiser-page__header-real-name'>
-                                    <Text color='less-prominent' line_height='xs' size='xs'>
-                                        {`(${first_name} ${last_name})`}
-                                    </Text>
-                                </div>
-                            )}
-                        </div>
-                        <Text color='less-prominent' size={isMobile() ? 'xxxs' : 'xs'}>
-                            {joined_since > 0 ? (
-                                <Localize
-                                    i18n_default_text='Joined {{days_since_joined}}d'
-                                    values={{ days_since_joined: joined_since }}
+                            <div className='my-profile-name--row'>
+                                <TradeBadge
+                                    is_poa_verified={!!full_verification}
+                                    is_poi_verified={!!basic_verification}
+                                    trade_count={Number(buy_orders_count) + Number(sell_orders_count)}
+                                    large
                                 />
-                            ) : (
-                                <Localize i18n_default_text='Joined today' />
-                            )}
-                        </Text>
-                        <div className='my-profile-name--row'>
-                            <TradeBadge
-                                is_poa_verified={!!full_verification}
-                                is_poi_verified={!!basic_verification}
-                                trade_count={Number(buy_orders_count) + Number(sell_orders_count)}
-                                large
-                            />
+                            </div>
                         </div>
                     </div>
+                    <AdvertiserPageStats />
                 </div>
-                <AdvertiserPageStats />
-            </div>
-            <AdvertiserPageAdverts />
+                <AdvertiserPageAdverts />
+            </BlockUserOverlay>
         </div>
     );
 };
--- packages/p2p/src/components/advertiser-page/block-user/block-user-overlay.jsx
+++ packages/p2p/src/components/advertiser-page/block-user/block-user-overlay.jsx
@@ -4,25 +4,43 @@ import { localize, Localize } from 'Components/i18next';
 import { useStores } from 'Stores';
 import { observer } from 'mobx-react-lite';
 import './block-user-overlay.scss';
+import PropTypes from 'prop-types';
 
-const BlockUserOverlay = () => {
+const BlockUserOverlay = ({ children, is_visible, onUnblock }) => {
     const { advertiser_page_store } = useStores();
 
-    return (
-        <div className='block-user-overlay'>
-            <Icon icon='IcBlockedUser' height={159} width={256} />
-            <Text className='block-user-overlay__text' line_height='m' size='s' weight='bold'>
-                <Localize
-                    i18n_default_text='You have blocked {{advertiser_name}}.'
-                    values={{ advertiser_name: advertiser_page_store.advertiser_details_name }}
-                />
-            </Text>
-            {/* TODO: Show Unblock confirmation modal when user clicks on this button */}
-            <Button className='block-user-overlay__button' secondary onClick={() => true} large>
-                {localize('Unblock')}
-            </Button>
-        </div>
-    );
+    if (is_visible) {
+        return (
+            <div className='block-user-overlay'>
+                <div className='block-user-overlay__wrapper'>
+                    <Icon icon='IcBlockedUser' height={159} width={256} />
+                    <Text className='block-user-overlay__wrapper-text' line_height='m' size='s' weight='bold'>
+                        <Localize
+                            i18n_default_text='You have blocked {{advertiser_name}}.'
+                            values={{ advertiser_name: advertiser_page_store.advertiser_details_name }}
+                        />
+                    </Text>
+                    <Button className='block-user-overlay__wrapper-button' secondary onClick={onUnblock} large>
+                        {localize('Unblock')}
+                    </Button>
+                </div>
+                {children}
+            </div>
+        );
+    }
+    return <>{children}</>;
+};
+
+BlockUserOverlay.propTypes = {
+    children: PropTypes.any,
+    /**
+     * Controls the visibility of the overlay
+     */
+    is_visible: PropTypes.bool.isRequired,
+    /**
+     * Function handler when Unblock button is clicked
+     */
+    onUnblock: PropTypes.func.isRequired,
 };
 
 export default observer(BlockUserOverlay);

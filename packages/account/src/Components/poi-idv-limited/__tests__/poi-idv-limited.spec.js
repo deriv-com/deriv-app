4d8a281691c03a6c3694b0f0b51ea1fe6100c345
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-06-06 10:22:58 +0300

    imports fix

--- packages/account/src/Components/poi-idv-limited/__tests__/poi-idv-limited.spec.js
+++ packages/account/src/Components/poi-idv-limited/__tests__/poi-idv-limited.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import IdvNoSubmissions from '../poi-idv-limited';
 
 jest.mock('../../../Assets/ic-idv-document-rejected.svg', () => jest.fn(() => 'IdvDocumentRejected'));

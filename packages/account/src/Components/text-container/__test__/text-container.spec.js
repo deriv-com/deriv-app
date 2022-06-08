64148cf01e20155130f9ffbcce5e1d8b40da6ee4
suisin-deriv <suisin@regentmarkets.com>
2022-06-08 13:58:05 +0800

    Test Case for Text Container

--- packages/account/src/Components/text-container/__test__/text-container.spec.js
+++ packages/account/src/Components/text-container/__test__/text-container.spec.js
@@ -1,15 +1,15 @@
-import React from "react"
+import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { TextContainer } from "../text-container"
+import { TextContainer } from '../text-container';
 
 describe('<TextContainer/>', () => {
     describe('renders the Text Container', () => {
         beforeEach(() => {
-            <TextContainer />
-        })
+            <TextContainer />;
+        });
 
         it('should show Text Container', () => {
             // expect(screen.getByTestId(/test_text_container/i)).not.toBeInTheDocument()
-        })
-    })
-})
+        });
+    });
+});

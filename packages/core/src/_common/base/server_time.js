097fd8e1188c585ba55d634388719fb69005c407
aizad-deriv <aizad@deriv.com>
2022-06-08 14:54:55 +0800

    changed 800 --> 500

--- packages/core/src/_common/base/server_time.js
+++ packages/core/src/_common/base/server_time.js
@@ -47,8 +47,8 @@ const ServerTime = (() => {
         };
         updateTime();
         pending.resolve();
-        // changed from 1000 ==> 800 to make it in-sync with binary.com
-        update_time_interval = setInterval(updateTime, 800);
+        // changed from 1000 ==> 500 to make it in-sync with binary.com
+        update_time_interval = setInterval(updateTime, 500);
     };
 
     const get = () => (server_time ? server_time.clone() : undefined);

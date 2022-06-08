77bb1c2fa52f121f789bd4e5459fa20936098d86
suisin-deriv <suisin@regentmarkets.com>
2022-06-08 16:10:33 +0800

    Update Test Case for Unverified

--- packages/account/src/Components/poi-unverified/__test__/unverified.spec.js
+++ packages/account/src/Components/poi-unverified/__test__/unverified.spec.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { Unverified } from '../unverified.jsx';
-import { PlatformContext } from '@deriv/shared';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { PlatformContext } from '@deriv/shared';
+import { Unverified } from '../unverified.jsx';
+
+jest.mock('@deriv/components', () => {
+    const original_module = jest.requireActual('@deriv/components');
+    return {
+        ...original_module,
+        Icon: jest.fn(() => <div data-testid='mockedIcon' />)
+    }
+})
 
 describe('Unverified', () => {
     const renderWithRouter = component =>
@@ -16,7 +24,7 @@ describe('Unverified', () => {
         renderWithRouter(<Unverified />);
 
         expect(screen.getByText(/We could not verify your proof of identity/i)).toBeInTheDocument();
-        expect(screen.getByTestId(/test-icon__true/)).toBeInTheDocument();
+        expect(screen.getByTestId(/mockedIcon/)).toBeInTheDocument();
     });
 
     describe('return true message from is_description_enabled', () => {
@@ -25,8 +33,8 @@ describe('Unverified', () => {
         });
 
         it('will show description message', () => {
-            expect(screen.getByTitle('description_message')).toBeInTheDocument();
-            expect(screen.getByTestId(/test-icon__true/)).toBeInTheDocument();
+            expect(screen.getByText(/As a precaution, we have disabled trading, deposits and withdrawals for this account. If you have any questions, please go to our Help Center/i)).toBeInTheDocument();
+            expect(screen.getByTestId(/mockedIcon/)).toBeInTheDocument();
         });
     });
 
@@ -43,7 +51,7 @@ describe('Unverified', () => {
 
         it('will show IcPoiError icon', () => {
             expect(screen.getByText(/We could not verify your proof of identity/i)).toBeInTheDocument();
-            expect(screen.getByTestId(/test-icon__false/)).toBeInTheDocument();
+            expect(screen.getByTestId(/mockedIcon/)).toBeInTheDocument();
         });
     });
 });
--- packages/account/src/Components/text-container/text-container.jsx
+++ packages/account/src/Components/text-container/text-container.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const TextContainer = ({ children }) => <div className='account__text_container' data-testid="test_text_container">{children}</div>;
+export const TextContainer = ({ children }) => <div className='account__text_container'>{children}</div>;

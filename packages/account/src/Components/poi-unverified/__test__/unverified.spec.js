ac5fe9a848e68aaa062a0c852c092120e68ae5fb
suisin-deriv <suisin@regentmarkets.com>
2022-06-08 13:56:32 +0800

    Update For TestContainer

--- packages/account/src/Components/poi-unverified/__test__/unverified.spec.js
+++ packages/account/src/Components/poi-unverified/__test__/unverified.spec.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import { Unverified } from './unverified.jsx'
-import {PlatformContext} from '@deriv/shared'
-import { BrowserRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Unverified } from '../unverified.jsx';
+import { PlatformContext } from '@deriv/shared';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Unverified', () => {
     const renderWithRouter = component =>
@@ -11,24 +11,24 @@ describe('Unverified', () => {
                 <BrowserRouter>{component}</BrowserRouter>
             </PlatformContext.Provider>
         );
-    
+
     it('should render unverified page', () => {
         renderWithRouter(<Unverified />);
 
         expect(screen.getByText(/We could not verify your proof of identity/i)).toBeInTheDocument();
         expect(screen.getByTestId(/test-icon__true/)).toBeInTheDocument();
-    })
+    });
 
     describe('return true message from is_description_enabled', () => {
         beforeEach(() => {
-            renderWithRouter(<Unverified is_description_enabled={true}/>)
-        })
-        
+            renderWithRouter(<Unverified is_description_enabled={true} />);
+        });
+
         it('will show description message', () => {
-            expect(screen.getByTitle("description_message")).toBeInTheDocument();
+            expect(screen.getByTitle('description_message')).toBeInTheDocument();
             expect(screen.getByTestId(/test-icon__true/)).toBeInTheDocument();
-        })
-    })
+        });
+    });
 
     describe('return false from is_appstore and true from is_description_enabled', () => {
         beforeEach(() => {
@@ -39,11 +39,11 @@ describe('Unverified', () => {
                     </BrowserRouter>
                 </PlatformContext.Provider>
             );
-        })
+        });
 
         it('will show IcPoiError icon', () => {
             expect(screen.getByText(/We could not verify your proof of identity/i)).toBeInTheDocument();
             expect(screen.getByTestId(/test-icon__false/)).toBeInTheDocument();
-        })
-    })
-})
+        });
+    });
+});
---
+++ packages/account/src/Components/text-container/__test__/text-container.spec.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { fireEvent, render, screen } from '@testing-library/react';
+import { TextContainer } from "../text-container"
+
+describe('<TextContainer/>', () => {
+    describe('renders the Text Container', () => {
+        beforeEach(() => {
+            <TextContainer />
+        })
+
+        it('should show Text Container', () => {
+            // expect(screen.getByTestId(/test_text_container/i)).not.toBeInTheDocument()
+        })
+    })
+})

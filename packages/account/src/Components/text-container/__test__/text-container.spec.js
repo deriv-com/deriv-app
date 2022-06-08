c9af1d6ef605730f286bc8796ae64e70b42a92ce
suisin-deriv <suisin@regentmarkets.com>
2022-06-08 16:47:26 +0800

    Update Test Case

--- packages/account/src/Components/text-container/__test__/text-container.spec.js
+++ packages/account/src/Components/text-container/__test__/text-container.spec.js
@@ -2,31 +2,22 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { TextContainer } from '../text-container';
 
-const MockTextContainer = props => {
-    return (
-        <TextContainer>
-            <p>{props.text}</p>
-        </TextContainer>
-    );
-};
-
 describe('<TextContainer/>', () => {
-    describe('renders the <TextContainer />', () => {
-        beforeEach(() => {
-            render(<MockTextContainer text='Lorem Ipsum' />);
-        });
-
         it('should contain "Lorem Ipsum" in the document', () => {
+            render(
+                <TextContainer>
+                    <p>Lorem Ipsum</p>
+                </TextContainer>
+            );
+
             expect(screen.getByText(/Lorem Ipsum/i)).toBeInTheDocument();
-        });
-    });
-    describe('renders the <TextContainer /> without any input', () => {
-        beforeEach(() => {
-            render(<MockTextContainer />);
         });
 
         it('should not contain "Lorem Ipsum" in the document but <TextContainer /> exist', () => {
+            render(
+                <TextContainer />
+            );
+
             expect(screen.getByTestId(/test_text_container/i)).toBeInTheDocument();
         });
-    });
 });

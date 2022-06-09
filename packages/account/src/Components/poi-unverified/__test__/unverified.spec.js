c7606aaee80bff2653a8125eb3d0c1bd40e7fc68
suisin-deriv <suisin@regentmarkets.com>
2022-06-09 10:51:13 +0800

    Updated Text Container test case

--- packages/account/src/Components/poi-unverified/__test__/unverified.spec.js
+++ packages/account/src/Components/poi-unverified/__test__/unverified.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import { PlatformContext } from '@deriv/shared';
 import { Unverified } from '../unverified.jsx';
 
@@ -8,7 +7,7 @@ jest.mock('@deriv/components', () => {
     const original_module = jest.requireActual('@deriv/components');
     return {
         ...original_module,
-        Icon: jest.fn(() => <div data-testid='mockedIcon' />),
+        Icon: jest.fn(() => <div data-testid='dt_mocked_icon' />),
     };
 });
 
@@ -16,15 +15,15 @@ describe('Unverified', () => {
     const renderWithRouter = component =>
         render(
             <PlatformContext.Provider value={{ is_appstore: true }}>
-                <BrowserRouter>{component}</BrowserRouter>
+                {component}
             </PlatformContext.Provider>
         );
 
-    it('should render unverified page', () => {
+    it('should render unverified component', () => {
         renderWithRouter(<Unverified />);
 
         expect(screen.getByText(/We could not verify your proof of identity/i)).toBeInTheDocument();
-        expect(screen.getByTestId(/mockedIcon/)).toBeInTheDocument();
+        expect(screen.getByTestId(/dt_mocked_icon/)).toBeInTheDocument();
     });
 
 
@@ -32,19 +31,17 @@ describe('Unverified', () => {
         renderWithRouter(<Unverified is_description_enabled={true} />);
         
         expect(screen.getByText(/As a precaution, we have disabled trading, deposits and withdrawals for this account. If you have any questions, please go to our Help Center/i)).toBeInTheDocument();
-        expect(screen.getByTestId(/mockedIcon/)).toBeInTheDocument();
+        expect(screen.getByTestId(/dt_mocked_icon/)).toBeInTheDocument();
     });
 
-    it('should show IcPoiError icon', () => {
+    it('should render Icon component when is_appstore is false', () => {
         render(
             <PlatformContext.Provider value={{ is_appstore: false }}>
-                <BrowserRouter>
-                    <Unverified />
-                </BrowserRouter>
+                <Unverified />
             </PlatformContext.Provider>
         );
 
         expect(screen.getByText(/We could not verify your proof of identity/i)).toBeInTheDocument();
-        expect(screen.getByTestId(/mockedIcon/)).toBeInTheDocument();
+        expect(screen.getByTestId(/dt_mocked_icon/)).toBeInTheDocument();
     });
 });

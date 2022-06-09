4e090adcc5d2ed5c7446b3e93d5927d5a5b94bf8
adrienne@deriv.com <adrienne@deriv.com>
2022-06-09 16:40:14 +0800

    Linked blocked_by_count returned by BE

--- packages/p2p/src/components/advertiser-page/block-user/block-user-count.jsx
+++ packages/p2p/src/components/advertiser-page/block-user/block-user-count.jsx
@@ -3,24 +3,31 @@ import { observer } from 'mobx-react-lite';
 import { Icon, Popover, Text } from '@deriv/components';
 import { localize } from 'Components/i18next';
 import './block-user-count.scss';
+import { useStores } from 'Stores';
 
 const BlockUserCount = () => {
-    // TODO: variable for testing, remove this later
-    const block_count = 0;
+    const { my_profile_store } = useStores();
+    const { blocked_by_count } = my_profile_store.advertiser_info;
+
+    React.useEffect(() => {
+        my_profile_store.getAdvertiserInfo();
+    }, []);
+
     return (
-        // TODO: Get blocked user count once BE card https://redmine.deriv.cloud/issues/62415#Remaining_features_for_P2P_blocking is merged
         <Popover
             className='block-user-count'
             classNameTarget='block-user-count__container'
             alignment='top'
             message={localize(
-                block_count === 0 ? 'Nobody has blocked you. Yay!' : '{{block_count}} people have blocked you.',
-                { block_count }
+                blocked_by_count === 0
+                    ? 'Nobody has blocked you. Yay!'
+                    : '{{blocked_by_count}} people have blocked you.',
+                { blocked_by_count }
             )}
         >
             <Icon className='block-user-count__container--icon' icon='IcUserBlockedOutline' />
             <Text size='xxs' line_height='m'>
-                {block_count}
+                {blocked_by_count}
             </Text>
         </Popover>
     );

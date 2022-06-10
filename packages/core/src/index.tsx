31c16f00bfd2568a3e742b37914def3c3bf99388
jim-deriv <jim@deriv.com>
2022-06-10 13:40:07 +0800

    chore: move render logic for App into index.tsx to follow single responsibility principle

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,6 +1,14 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
+import ReactDOM from 'react-dom';
+import React from 'react';
 import 'babel-polyfill';
 import 'promise-polyfill';
 import registerServiceWorker from 'Utils/pwa';
+import initStore from 'App/initStore';
+import App from 'App/app.jsx';
+import { checkAndSetEndpointFromUrl } from '@deriv/shared';
+import AppNotificationMessages from './App/Containers/app-notification-messages.jsx';
 
 if (
     !!window?.localStorage.getItem?.('debug_service_worker') || // To enable local service worker related development
@@ -9,6 +17,14 @@ if (
 ) {
     registerServiceWorker();
 }
+const has_endpoint_url = checkAndSetEndpointFromUrl();
 
-// eslint-disable-next-line
-import App from 'App/app.jsx';
+// if has endpoint url, APP will be redirected
+if (!has_endpoint_url) {
+    const root_store = initStore(AppNotificationMessages);
+
+    const wrapper = document.getElementById('deriv_app');
+    if (wrapper) {
+        ReactDOM.render(<App useSuspense={false} root_store={root_store} />, wrapper);
+    }
+}

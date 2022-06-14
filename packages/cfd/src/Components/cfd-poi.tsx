ddad9326ae939ffb5b72925b8bf5abcc4c57aa8e
Maryia <103177211+maryia-binary@users.noreply.github.com>
2022-06-14 08:14:14 +0300

    Maryia/Added tests for cfd-poi component (#5640)
    * Maryia/Added tests for cfd-poi component
    
    * feat: added more tests to cfd-poi
    
    * put props into beforeEach()
    
    * renamed testids as per docs
    
    * united test cases for identity_status check into 1
    
    * canceled changes that will be fixed in a separate pr

--- packages/cfd/src/Components/cfd-poi.tsx
+++ packages/cfd/src/Components/cfd-poi.tsx
@@ -88,7 +88,12 @@ const CFDPOI = ({ authentication_status, form_error, index, onCancel, onSubmit, 
             {({ handleSubmit }) => (
                 <AutoHeightWrapper default_height={200}>
                     {({ setRef, height }: { setRef: (instance: HTMLFormElement | null) => void; height: number }) => (
-                        <form ref={setRef} className='cfd-proof-of-identity' onSubmit={handleSubmit}>
+                        <form
+                            ref={setRef}
+                            className='cfd-proof-of-identity'
+                            onSubmit={handleSubmit}
+                            data-testid='dt_cfd_proof_of_identity'
+                        >
                             <div className='details-form'>
                                 <input type='hidden' name='poi_state' value={poi_state} readOnly />
                                 <Div100vhContainer

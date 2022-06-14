6707d13fe49a0dc78cc5ed0d172f8dcd5cf643fe
suisin-deriv <suisin@regentmarkets.com>
2022-06-14 15:34:01 +0800

    Update Version

--- packages/account/src/Components/poi-not-required/__test__/onfido-not-required.spec.js
+++ packages/account/src/Components/poi-not-required/__test__/onfido-not-required.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { OnfidoNotRequired } from '../onfido-not-required.jsx';
+import { OnfidoNotRequired } from '../onfido-not-required';
 
 jest.mock('@deriv/components', () => {
     const original_module = jest.requireActual('@deriv/components');

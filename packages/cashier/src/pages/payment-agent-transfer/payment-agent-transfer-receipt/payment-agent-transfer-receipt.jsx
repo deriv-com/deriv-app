4fb4dda41687e93a3740897662f6c723f5d260e8
vinu-deriv <vinu@deriv.com>
2022-06-15 17:35:30 +0400

    fix: responsive screen fix for payment-agent-transfer-receipt

--- packages/cashier/src/pages/payment-agent-transfer/payment-agent-transfer-receipt/payment-agent-transfer-receipt.jsx
+++ packages/cashier/src/pages/payment-agent-transfer/payment-agent-transfer-receipt/payment-agent-transfer-receipt.jsx
@@ -74,7 +74,7 @@ const PaymentAgentTransferReceipt = ({
                 />
             )}
             <Button
-                className='cashier__form-submit-button cashier__done-button'
+                className='cashier__form-submit-button'
                 has_effect
                 text={localize('Make a new transfer')}
                 onClick={resetPaymentAgentTransfer}

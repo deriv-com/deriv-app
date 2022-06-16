38fb17b23b39d38dc34ac3424bb9a3674544b381
aizad-deriv <aizad@deriv.com>
2022-06-16 14:26:51 +0800

    change styling based on design provided

--- packages/cfd/src/Containers/missing-real-account.tsx
+++ packages/cfd/src/Containers/missing-real-account.tsx
@@ -6,18 +6,22 @@ import { TMissingRealAccount } from './props.types';
 
 const MissingRealAccount = ({ onClickSignup, platform }: TMissingRealAccount) => (
     <div className='cfd-dashboard__missing-real'>
-        <h1 className='cfd-dashboard__missing-real--heading'>
-            {platform === CFD_PLATFORMS.MT5 ? (
-                <Localize i18n_default_text='You need a real account (fiat currency or cryptocurrency) in Deriv to create a real DMT5 account.' />
-            ) : (
-                <Localize i18n_default_text='To create a Deriv X real account, create a Deriv real account first.' />
-            )}
-        </h1>
-        <Button className='cfd-dashboard__missing-real--button' onClick={onClickSignup} type='button' primary>
-            <span className='btn__text'>
-                <Localize i18n_default_text='Create a Deriv account' />
-            </span>
-        </Button>
+        <div className='cfd-dashboard__missing-real-wrapper'>
+            <h1 className='cfd-dashboard__missing-real--heading'>
+                {platform === CFD_PLATFORMS.MT5 ? (
+                    <Localize i18n_default_text='You need a real account (fiat currency or cryptocurrency) in Deriv to create a real DMT5 account.' />
+                ) : (
+                    <Localize i18n_default_text='To create a Deriv X real account, create a Deriv real account first.' />
+                )}
+            </h1>
+        </div>
+        <div>
+            <Button className='cfd-dashboard__missing-real--button' onClick={onClickSignup} type='button' primary>
+                <span className='btn__text'>
+                    <Localize i18n_default_text='Create a Deriv account' />
+                </span>
+            </Button>
+        </div>
     </div>
 );
 

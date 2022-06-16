bd15a7a1fb33c6731be8f03661b0fc2329868379
Thisyahlen <thisyahlen@Thisyahlens-MacBook-Pro.local>
2022-06-16 14:10:27 +0800

    removed unused imports

--- packages/cfd/src/Containers/__tests__/cfd-change-password-confirmation.spec.js
+++ packages/cfd/src/Containers/__tests__/cfd-change-password-confirmation.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor, waitForElementToBeRemoved, act } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import ChangePasswordConfirmation from '../cfd-change-password-confirmation';
 
 jest.mock('@deriv/components', () => {

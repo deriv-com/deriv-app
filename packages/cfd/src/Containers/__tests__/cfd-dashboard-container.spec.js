ad14e22c2d54e03230aef30caac1c887d24b0b0e
Thisyahlen <thisyahlen@Thisyahlens-MacBook-Pro.local>
2022-06-17 16:44:00 +0800

    added more test cases

--- packages/cfd/src/Containers/__tests__/cfd-dashboard-container.spec.js
+++ packages/cfd/src/Containers/__tests__/cfd-dashboard-container.spec.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fire, fireEvent } from '@testing-library/react';
 import CFDDashboardContainer from '../cfd-dashboard-container';
 
 jest.mock('@deriv/components', () => {
     const original_module = jest.requireActual('@deriv/components');
-
     return {
         ...original_module,
-        Icon: jest.fn(() => 'mocked_icon'),
+        Icon: jest.fn(props => <div>{props.icon}</div>),
     };
 });
 
@@ -20,27 +19,89 @@ describe('CFDDashboardContainer', () => {
 
     it('should render <CFDDashboardContainer /> correctly', () => {
         render(<CFDDashboardContainer {...mock_props} />);
-        expect(screen.getByTestId('dt_cfd_dashboard_download_center_container')).toBeInTheDocument();
+        expect(screen.getByTestId(/dt_cfd_dashboard_download_center_container/i)).toBeInTheDocument();
     });
-
-    it('should render <CFDDashboardContainer /> correctly with dark mode on', () => {
-        render(<CFDDashboardContainer {...mock_props} is_dark_mode_on='true' />);
-        expect(screen.getByTestId('dt_cfd_dashboard_download_center_container')).toBeInTheDocument();
-    });
-    it('should render correct text according to the platform (mt5)', () => {
+    it('should render correct text according to the MT5 platform', () => {
         render(<CFDDashboardContainer {...mock_props} />);
         expect(
-            screen.getByText('Run MT5 from your browser or download the MT5 app for your devices')
+            screen.getByText(/Run MT5 from your browser or download the MT5 app for your devices/i)
         ).toBeInTheDocument();
         expect(
-            screen.getByText('The MT5 desktop app is not supported by Windows XP, Windows 2003, and Windows Vista.')
+            screen.getByText(/The MT5 desktop app is not supported by Windows XP, Windows 2003, and Windows Vista/i)
         ).toBeInTheDocument();
-        expect(screen.getByTestId('dt_mt5_text')).toBeInTheDocument();
+        expect(screen.getByTestId(/dt_mt5_text/i)).toBeInTheDocument();
     });
+    it('should show the proper icons for the MT5 platform ', () => {
+        render(<CFDDashboardContainer {...mock_props} />);
+        expect(screen.getByText(/IcMt5DeviceDesktop/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcMt5DeviceLaptop/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationWindows/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationMacos/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationLinux/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcMt5DeviceTablet/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcMt5DevicePhone/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationGoogle/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationApple/i)).toBeInTheDocument();
+    });
+
+    it('should download/redirect the correct file for MT5 platform', () => {
+        render(<CFDDashboardContainer {...mock_props} />);
+        expect(screen.getByText(/IcInstallationWindows/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://download.mql5.com/cdn/web/deriv.limited/mt5/deriv5setup.exe'
+        );
+        expect(screen.getByText(/IcInstallationMacos/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/MetaTrader5.dmg'
+        );
+        expect(screen.getByText(/IcInstallationLinux/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://www.metatrader5.com/en/terminal/help/start_advanced/install_linux'
+        );
+        expect(screen.getByText(/IcInstallationGoogle/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://download.mql5.com/cdn/mobile/mt5/android?server=Deriv-Demo,Deriv-Server'
+        );
+        expect(screen.getByText(/IcInstallationApple/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://download.mql5.com/cdn/mobile/mt5/ios?server=Deriv-Demo,Deriv-Server'
+        );
+    });
+
     it('should render correct text according to the platform (DerivX)', () => {
+        render(<CFDDashboardContainer {...mock_props} platform='dxtrade' is_dark_mode_on='true' />);
+        expect(screen.getByText(/Run Deriv X on your browser or download the mobile app/i)).toBeInTheDocument();
+        expect(screen.getByText(/Web terminal/i)).toBeInTheDocument();
+        expect(screen.getByTestId(/dt_dxtrade_desktop_download/i)).toBeInTheDocument();
+    });
+    it('should render the correct icons for the Deriv X platform', () => {
         render(<CFDDashboardContainer {...mock_props} platform='dxtrade' />);
-        expect(screen.getByText('Run Deriv X on your browser or download the mobile app')).toBeInTheDocument();
-        expect(screen.getByText('Web terminal')).toBeInTheDocument();
-        expect(screen.getByTestId('dt_dxtrade_desktop_download')).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDeviceDesktop/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDeviceLaptop/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcBrandDxtrade/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDeviceTablet/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDevicePhone/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationGoogle/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcInstallationApple/i)).toBeInTheDocument();
+    });
+    it('should render the correct icons if dark mode is on for DerivX', () => {
+        render(<CFDDashboardContainer {...mock_props} platform='dxtrade' is_dark_mode_on='true' />);
+        expect(screen.getByText(/IcDxtradeDeviceDesktopLight/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDeviceLaptopLight/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDeviceTabletLight/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeDevicePhoneLight/i)).toBeInTheDocument();
+    });
+
+    it('should download/redirect the correct file for DerivX', () => {
+        render(<CFDDashboardContainer {...mock_props} platform='dxtrade' />);
+        expect(screen.getByText(/IcBrandDxtrade/i).closest('a')).toHaveAttribute('href', 'https://dx.deriv.com');
+        expect(screen.getByText(/IcInstallationGoogle/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://play.google.com/store/apps/details?id=com.deriv.dx'
+        );
+        expect(screen.getByText(/IcInstallationApple/i).closest('a')).toHaveAttribute(
+            'href',
+            'https://apps.apple.com/us/app/deriv-x/id1563337503'
+        );
     });
 });

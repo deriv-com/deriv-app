c6018985e9733405847fa9426a853447217c2541
george-usynin-binary <heorhi@binary.com>
2022-06-18 14:24:14 +0300

    add tests for PaymentAgentCardDetails

---
+++ packages/cashier/src/pages/payment-agent/payment-agent-card/__tests__/payment-agent-card-details.spec.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import PaymentAgentCardDetails from '../paymen-agent-card-details';
+
+describe('<PaymentAgentCardDetails />', () => {
+    const mocked_payment_agent = {
+        currency: 'USD',
+        deposit_commission: '10',
+        email: 'pa@example.com',
+        max_withdrawal: '2000',
+        min_withdrawal: '10',
+        phones: '+12345678',
+        withdrawal_commission: '0',
+    };
+
+    it('should show proper payment agent deposit details', () => {
+        render(<PaymentAgentCardDetails payment_agent={mocked_payment_agent} />);
+
+        expect(screen.getByText('Phone number')).toBeInTheDocument();
+        expect(screen.getByText('+12345678')).toBeInTheDocument();
+        expect(screen.getByText('Email')).toBeInTheDocument();
+        expect(screen.getByText('pa@example.com')).toBeInTheDocument();
+        expect(screen.getByText('Minimum withdrawal')).toBeInTheDocument();
+        expect(screen.getByText('10.00 USD')).toBeInTheDocument();
+        expect(screen.getByText('Maximum withdrawal')).toBeInTheDocument();
+        expect(screen.getByText('2,000.00 USD')).toBeInTheDocument();
+        expect(screen.getByText('Commission on deposits')).toBeInTheDocument();
+        expect(screen.getByText('10%')).toBeInTheDocument();
+        expect(screen.getByText('Commission on withdrawal')).toBeInTheDocument();
+        expect(screen.getByText('0%')).toBeInTheDocument();
+    });
+
+    it('should handle/show payment agent phones array', () => {
+        render(
+            <PaymentAgentCardDetails
+                payment_agent={{
+                    ...mocked_payment_agent,
+                    phones: [{ phone_number: '+12345678' }, { phone_number: '+87654321' }],
+                }}
+            />
+        );
+
+        expect(screen.getByText('+12345678,')).toBeInTheDocument();
+        expect(screen.getByText('+87654321')).toBeInTheDocument();
+    });
+});

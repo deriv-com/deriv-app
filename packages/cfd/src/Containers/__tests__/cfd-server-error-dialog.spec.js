07e96e9d4294ac379f82a9a600353462da6ea259
Thisyahlen <thisyahlen@Thisyahlens-MacBook-Pro.local>
2022-06-20 14:29:58 +0800

    test coverage for cfd-server-error-dialog

--- packages/cfd/src/Containers/__tests__/cfd-server-error-dialog.spec.js
+++ packages/cfd/src/Containers/__tests__/cfd-server-error-dialog.spec.js
@@ -21,9 +21,6 @@ describe('<CFDServerErrorDialog /> ', () => {
 
     const mock_props = {
         clearCFDError: jest.fn(),
-        disableApp: jest.fn(),
-        enableApp: jest.fn(),
-        error_message: 'Sorry, an error occured while processing your request.',
         error_type: 'error_type',
         has_cfd_error: true,
         is_cfd_success_dialog_enabled: false,
@@ -32,6 +29,7 @@ describe('<CFDServerErrorDialog /> ', () => {
     it('should render the component properly', () => {
         const { container } = render(<CFDServerErrorDialog {...mock_props} />);
         expect(container.firstChild).toHaveClass('dc-dialog__wrapper dc-dialog__wrapper--enter');
+        screen.debug();
     });
 
     it('should render the proper text and error message', () => {
@@ -39,25 +37,22 @@ describe('<CFDServerErrorDialog /> ', () => {
         expect(screen.getByText(/Sorry, an error occured while processing your request/i)).toBeInTheDocument();
         expect(screen.getByText(/OK/i)).toBeInTheDocument();
         expect(screen.getByRole('heading')).toHaveTextContent(/Something’s not right/i);
-        screen.debug();
     });
 
     it('should not render the component if has_cfd_error is false', () => {
         render(<CFDServerErrorDialog {...mock_props} has_cfd_error={false} />);
         expect(screen.queryByText(/Sorry, an error occured while processing your request/i)).not.toBeInTheDocument();
-        screen.debug();
     });
 
     it('should not render the component if is_cfd_success_dialog_enabled', () => {
         render(<CFDServerErrorDialog {...mock_props} is_cfd_success_dialog_enabled />);
         expect(screen.queryByText(/Sorry, an error occured while processing your request/i)).not.toBeInTheDocument();
-        screen.debug();
     });
 
     it('should clear the component if OK is clicked', () => {
         render(<CFDServerErrorDialog {...mock_props} />);
-        fireEvent.click(screen.getByText(/OK/i));
+        const ok_btn = screen.getByRole('button', { name: /OK/i });
+        fireEvent.click(ok_btn);
         expect(mock_props.clearCFDError).toHaveBeenCalled();
-        screen.debug();
     });
 });

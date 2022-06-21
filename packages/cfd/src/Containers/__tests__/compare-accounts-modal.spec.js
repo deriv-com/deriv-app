f0ec2a5e8a9fceef64c29538a2a6a1d4066d086c
Thisyahlen <thisyahlen@Thisyahlens-MacBook-Pro.local>
2022-06-21 13:53:13 +0800

    test coverage for compare-accounts-modal

--- packages/cfd/src/Containers/__tests__/compare-accounts-modal.spec.js
+++ packages/cfd/src/Containers/__tests__/compare-accounts-modal.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render, infinity } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import CompareAccountsModal from '../compare-accounts-modal';
 
 jest.mock('Stores/connect.js', () => ({
@@ -7,32 +7,33 @@ jest.mock('Stores/connect.js', () => ({
     default: 'mockedDefaultExport',
     connect: () => Component => Component,
 }));
+beforeAll(() => {
+    const modal_root_el = document.createElement('div');
+    modal_root_el.setAttribute('id', 'modal_root');
+    document.body.appendChild(modal_root_el);
+});
 
 describe('should render the <CompareAccountsModal /> component', () => {
-    beforeAll(() => {
-        const modal_root_el = document.createElement('div');
-        modal_root_el.setAttribute('id', 'modal_root');
-        document.body.appendChild(modal_root_el);
-    });
+    const only_gaming_company = {
+        mt_gaming_company: {
+            financial: 'USD',
+        },
+    };
 
-    afterAll(() => {
-        let modal_root_el = document.getElementById('modal_root');
-        document.body.removeChild(modal_root_el);
-    });
+    const all_landing_companies = {
+        mt_gaming_company: {
+            financial: 'USD',
+        },
+        mt_financial_company: {
+            financial: 'USD',
+            financial_stp: 'USD',
+        },
+    };
 
     const mock_props = {
         disableApp: jest.fn(),
         enableApp: jest.fn(),
-        is_compare_accounts_visible: true,
-        landing_companies: {
-            mt_gaming_company: {
-                financial: 'USD',
-            },
-            mt_financial_company: {
-                financial: 'USD',
-                financial_stp: 'USD',
-            },
-        },
+        is_compare_accounts_visible: false,
         is_loading: false,
         is_logged_in: true,
         is_eu: false,
@@ -44,7 +45,59 @@ describe('should render the <CompareAccountsModal /> component', () => {
     };
 
     it('should render the <CompareAccountsModal /> component', () => {
-        render(<CompareAccountsModal {...mock_props} />);
-        screen.debug(undefined, 40000);
+        const { container } = render(<CompareAccountsModal {...mock_props} />);
+        expect(container.firstChild).toHaveClass('cfd-compare-accounts-modal__wrapper');
+        screen.debug(undefined, 50000);
+    });
+
+    it('expect compare-accounts-content to pop up when the compare accounts button is clicked', () => {
+        render(<CompareAccountsModal {...mock_props} landing_companies={all_landing_companies} />);
+        expect(screen.queryByText(/compare accounts/i)).toBeInTheDocument();
+        fireEvent.click(screen.getByText(/compare accounts/i));
+        expect(mock_props.toggleCompareAccounts).toHaveBeenCalled();
+        screen.debug(undefined, 50000);
+    });
+
+    it('should render the proper style for Mt5 platforms', () => {
+        render(<CompareAccountsModal {...mock_props} landing_companies={only_gaming_company} platform='mt5' />);
+        const div_element = document.getElementsByClassName('cfd-compare-accounts-modal__wrapper');
+        const style = window.getComputedStyle(div_element[0]);
+        expect(style.marginTop).toBe('2.4rem');
+        screen.debug(undefined, 50000);
+    });
+
+    it('should render the proper text if the user is from an eu country', () => {
+        render(
+            <CompareAccountsModal
+                {...mock_props}
+                landing_companies={all_landing_companies}
+                is_eu_country={true}
+                is_eu={true}
+                is_compare_accounts_visible={true}
+            />
+        );
+        screen.debug(undefined, 50000);
+        expect(screen.getAllByText('USD')[0]).toBeInTheDocument();
+    });
+
+    it('should render the proper text if the user is from an uk country', () => {
+        render(
+            <CompareAccountsModal
+                {...mock_props}
+                landing_companies={all_landing_companies}
+                is_uk={true}
+                is_compare_accounts_visible={true}
+            />
+        );
+
+        screen.debug(undefined, 50000);
+    });
+
+    it('should render the proper style for Dxtrade platforms', () => {
+        render(<CompareAccountsModal {...mock_props} landing_companies={only_gaming_company} platform='dxtrade' />);
+        const div_element = document.getElementsByClassName('cfd-compare-accounts-modal__wrapper');
+        const style = window.getComputedStyle(div_element[0]);
+        expect(style.marginTop).toBe('5rem');
+        screen.debug(undefined, 50000);
     });
 });

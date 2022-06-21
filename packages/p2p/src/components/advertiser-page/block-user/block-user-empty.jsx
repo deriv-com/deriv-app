04d60de831e657043102a8d3a1b7fa1497915ee8
Adrienne Rio <adrienne@deriv.com>
2022-06-21 17:22:31 +0800

    Show loading screen when icon is still loading

--- packages/p2p/src/components/advertiser-page/block-user/block-user-empty.jsx
+++ packages/p2p/src/components/advertiser-page/block-user/block-user-empty.jsx
@@ -1,36 +1,54 @@
 import React from 'react';
-import { DesktopWrapper, Icon, MobileFullPageModal, MobileWrapper, Text } from '@deriv/components';
+import { DesktopWrapper, Icon, Loading, MobileFullPageModal, MobileWrapper, Text } from '@deriv/components';
 import { my_profile_tabs } from 'Constants/my-profile-tabs';
 import { Localize, localize } from 'Components/i18next';
 import { useStores } from 'Stores';
+import './block-user-empty.scss';
 
 const BlockUserEmpty = () => {
     const { my_profile_store } = useStores();
+    const [loaded, setLoaded] = React.useState(false);
+
+    const iconRef = React.useCallback(node => {
+        if (node) {
+            setLoaded(true);
+        }
+    });
+
     return (
         <React.Fragment>
             <DesktopWrapper>
-                <div className='payment-methods-empty'>
-                    <Icon icon='IcEmptyBlockedAdvertisers' height={159} width={256} />
-                    <Text className='payment-methods-empty--text' line_height='m' size='s' weight='bold'>
+                <div className='block-user-empty'>
+                    {!loaded && <Loading className='block-user-empty__loading' is_fullscreen={false} />}
+                    <Icon
+                        className='block-user-empty__icon'
+                        ref={iconRef}
+                        icon='IcEmptyBlockedAdvertisers'
+                        height={128}
+                        width={128}
+                    />
+                    <Text className='block-user-empty__text' line_height='m' size='s' weight='bold'>
                         <Localize i18n_default_text='You have no blocked advertisers' />
                     </Text>
                 </div>
             </DesktopWrapper>
             <MobileWrapper>
                 <MobileFullPageModal
-                    body_className='payment-methods-empty'
+                    body_className='block-user-empty'
                     height_offset='80px'
-                    is_modal_open={true}
+                    is_modal_open
                     page_header_text={localize('Blocked advertisers')}
                     pageHeaderReturnFn={() => my_profile_store.setActiveTab(my_profile_tabs.MY_STATS)}
                 >
+                    {!loaded && <Loading className='block-user-empty__loading' is_fullscreen={false} />}
                     <Icon
                         icon='IcEmptyBlockedAdvertisers'
-                        className='payment-methods-empty--icon'
-                        height={159}
-                        width={256}
+                        ref={iconRef}
+                        className='block-user-empty__icon'
+                        height={128}
+                        width={128}
                     />
-                    <Text className='payment-methods-empty--text' line_height='m' size='s' weight='bold'>
+                    <Text className='block-user-empty__text' line_height='m' size='s' weight='bold'>
                         <Localize i18n_default_text='You have no blocked advertisers' />
                     </Text>
                 </MobileFullPageModal>

a9b0c82e798445c656aafb7bd704b250361611d9
george-usynin-binary <heorhi@binary.com>
2022-06-23 18:32:42 +0300

    resolve name import

--- packages/cashier/src/pages/payment-agent/payment-agent-card/payment-agent-card-description.jsx
+++ packages/cashier/src/pages/payment-agent/payment-agent-card/payment-agent-card-description.jsx
@@ -5,7 +5,7 @@ import { Icon, Text } from '@deriv/components';
 import { capitalizeFirstLetter } from '@deriv/shared';
 import { hasNormalizedPaymentMethods, getNormalizedPaymentMethod } from './helpers';
 import { connect } from 'Stores/connect';
-import { Detail } from './paymen-agent-card-details';
+import { Detail } from './paymen-agent-card-deposit-details';
 
 const PaymentAgentCardDescription = ({ is_dark_mode_on, payment_agent }) => {
     const payment_agent_urls = toJS(payment_agent.urls);

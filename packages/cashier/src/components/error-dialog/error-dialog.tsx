5560ecc738c782b8925dc5a084bdfabe623cbd40
vinu-deriv <vinu@deriv.com>
2022-06-28 15:21:17 +0400

    fixed Rootsore path in error dialog

--- packages/cashier/src/components/error-dialog/error-dialog.tsx
+++ packages/cashier/src/components/error-dialog/error-dialog.tsx
@@ -4,7 +4,7 @@ import { Dialog } from '@deriv/components';
 import { localize, Localize } from '@deriv/translations';
 import { routes } from '@deriv/shared';
 import { connect } from 'Stores/connect';
-import { RootStore } from 'Types/index';
+import { RootStore } from 'Types';
 
 type TErrorDialogProps = {
     disableApp: () => void;

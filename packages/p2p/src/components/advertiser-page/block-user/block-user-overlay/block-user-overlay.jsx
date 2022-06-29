d7f7c3235a510dfdd7b44336c12e28aead0f1e0e
Adrienne Rio <adrienne@deriv.com>
2022-06-29 18:08:55 +0800

    Renamed overlay props

--- packages/p2p/src/components/advertiser-page/block-user/block-user-overlay/block-user-overlay.jsx
+++ packages/p2p/src/components/advertiser-page/block-user/block-user-overlay/block-user-overlay.jsx
@@ -5,7 +5,7 @@ import { useStores } from 'Stores';
 import { observer } from 'mobx-react-lite';
 import PropTypes from 'prop-types';
 
-const BlockUserOverlay = ({ children, is_visible, onUnblock }) => {
+const BlockUserOverlay = ({ children, is_visible, onClickUnblock }) => {
     const { advertiser_page_store } = useStores();
 
     if (is_visible) {
@@ -19,7 +19,7 @@ const BlockUserOverlay = ({ children, is_visible, onUnblock }) => {
                             values={{ advertiser_name: advertiser_page_store.advertiser_details_name }}
                         />
                     </Text>
-                    <Button className='block-user-overlay__wrapper-button' large onClick={onUnblock} secondary>
+                    <Button className='block-user-overlay__wrapper-button' large onClick={onClickUnblock} secondary>
                         <Localize i18n_default_text='Unblock' />
                     </Button>
                 </div>
@@ -33,7 +33,7 @@ const BlockUserOverlay = ({ children, is_visible, onUnblock }) => {
 BlockUserOverlay.propTypes = {
     children: PropTypes.any,
     is_visible: PropTypes.bool.isRequired,
-    onUnblock: PropTypes.func.isRequired,
+    onClickUnblock: PropTypes.func,
 };
 
 export default observer(BlockUserOverlay);

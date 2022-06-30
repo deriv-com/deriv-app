16e51cc1790708e31894f465ef72e9770245d458
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-06-30 12:41:48 +0430

    fix eslint errors for appstore

--- packages/appstore/src/stores/root-store.ts
+++ packages/appstore/src/stores/root-store.ts
@@ -1,13 +1,19 @@
 import ConfigStore from './config-store';
 
+export type CoreStoreTypes = {
+    config: ConfigStore;
+    client: Record<string, unknown>;
+    common: Record<string, unknown>;
+    ui: Record<string, unknown>;
+};
 export default class RootStore {
     public config: ConfigStore;
     public ws: unknown;
-    public client: any;
-    public common: any;
-    public ui: any;
+    public client: Record<string, unknown>;
+    public common: Record<string, unknown>;
+    public ui: Record<string, unknown>;
 
-    public constructor(core_store: any) {
+    public constructor(core_store: CoreStoreTypes) {
         this.config = new ConfigStore(this);
         this.client = core_store.client;
         this.common = core_store.common;

d7c4b1e309ba4a1fbf57829df589f6aaa367d762
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-06-30 13:06:33 +0430

    Fix circleci errors

--- packages/appstore/src/stores/root-store.ts
+++ packages/appstore/src/stores/root-store.ts
@@ -2,16 +2,16 @@ import ConfigStore from './config-store';
 
 export type CoreStoreTypes = {
     config: ConfigStore;
-    client: Record<string, unknown>;
-    common: Record<string, unknown>;
-    ui: Record<string, unknown>;
+    client: Record<string, any>;
+    common: Record<string, any>;
+    ui: Record<string, any>;
 };
 export default class RootStore {
     public config: ConfigStore;
     public ws: unknown;
-    public client: Record<string, unknown>;
-    public common: Record<string, unknown>;
-    public ui: Record<string, unknown>;
+    public client: Record<string, any>;
+    public common: Record<string, any>;
+    public ui: Record<string, any>;
 
     public constructor(core_store: CoreStoreTypes) {
         this.config = new ConfigStore(this);
--- packages/appstore/src/types/stores.types.ts
+++ packages/appstore/src/types/stores.types.ts
@@ -1,7 +1,7 @@
 import ConfigStore from 'Stores/config-store';
 
 export type TRootStore = {
-    ui: Record<string, unknown>;
-    client: Record<string, unknown>;
+    ui: Record<string, any>;
+    client: Record<string, any>;
     config: ConfigStore;
 };

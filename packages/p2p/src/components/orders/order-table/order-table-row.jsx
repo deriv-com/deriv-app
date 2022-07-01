f5f4cda788d573ca87067ccd6400ccb3f202200c
ameerul <ameerul@deriv.com>
2022-07-01 10:49:33 +0800

    disabled button after 24 hours and added new prop to disable it

--- packages/p2p/src/components/orders/order-table/order-table-row.jsx
+++ packages/p2p/src/components/orders/order-table/order-table-row.jsx
@@ -1,14 +1,14 @@
-import { Table, Text, Icon } from '@deriv/components';
-import { isMobile, formatMoney } from '@deriv/shared';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { localize } from 'Components/i18next';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { secondsToTimer } from 'Utils/date-time';
 import { createExtendedOrderDetails } from 'Utils/orders';
 import ServerTime from 'Utils/server-time';
 import { useStores } from 'Stores';
+import { Table, Text, Icon } from '@deriv/components';
+import { isMobile, formatMoney } from '@deriv/shared';
+import { localize } from 'Components/i18next';
 import OrdersUserRatingButton from '../orders-user-rating-button';
 
 const Title = ({ send_amount, currency, order_purchase_datetime, order_type }) => {
@@ -89,6 +89,7 @@ const OrderRow = ({ style, row: order }) => {
     const transaction_amount = `${Number(amount * rate).toFixed(2)} ${local_currency}`;
     const is_buy_order_type_for_user = (is_buy_order && !is_my_ad) || (is_sell_order && is_my_ad);
     const order_type = is_buy_order_type_for_user ? localize('Buy') : localize('Sell');
+    const order_user_rating_button_expiry_time = Math.abs(new Date() - new Date(order_purchase_datetime)) / 36e5;
 
     if (isMobile()) {
         return (
@@ -180,7 +181,10 @@ const OrderRow = ({ style, row: order }) => {
                     {general_store.is_active_tab ? (
                         <div className='orders__table-time'>{remaining_time}</div>
                     ) : (
-                        <OrdersUserRatingButton has_full_text={false} />
+                        <OrdersUserRatingButton
+                            has_full_text={false}
+                            is_disabled={order_user_rating_button_expiry_time > 24}
+                        />
                     )}
                 </Table.Cell>
             </Table.Row>
--- packages/p2p/src/components/orders/orders-user-rating-button/orders-user-rating-button.jsx
+++ packages/p2p/src/components/orders/orders-user-rating-button/orders-user-rating-button.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Button, Icon } from '@deriv/components';
 import { Localize } from 'Components/i18next';
-import './orders-user-rating-button.scss';
 
-const OrdersUserRatingButton = ({ has_full_text }) => {
+const OrdersUserRatingButton = ({ has_full_text, is_disabled }) => {
     return (
-        <Button secondary small className='orders-user-rating-button'>
-            <Icon icon='IcFullStar' className='orders-user-rating-button-icon' />
+        <Button is_disabled={is_disabled} secondary small className='orders-user-rating-button'>
+            <Icon icon='IcFullStar' className='orders-user-rating-button-icon' color={is_disabled && 'disabled'} />
             {has_full_text ? (
                 <Localize i18n_default_text='Rate this transaction' />
             ) : (

2263d32e31ad539e39098f0ac9a2b0af79b58c77
Carol Sachdeva <carol@binary.com>
2022-07-04 12:22:16 +0800

    fix: message

--- packages/p2p/src/components/recommended-by/recommended-by.jsx
+++ packages/p2p/src/components/recommended-by/recommended-by.jsx
@@ -4,20 +4,23 @@ import { Icon, Popover, Text } from '@deriv/components';
 import { isMobile } from '@deriv/shared';
 import { localize } from 'Components/i18next';
 
-const RecommendedBy = ({ recommended_average, recommended_count }) => (
-    <Popover
-        alignment='top'
-        className='recommended-by--container'
-        message={localize('Recommended by {{recommended_count}} traders', {
-            recommended_count,
-        })}
-    >
+const RecommendedBy = ({ recommended_average, recommended_count }) => {
+    const message =
+        recommended_count === 1
+            ? localize('Recommended by {{recommended_count}} trader', {
+                  recommended_count,
+              })
+            : localize('Recommended by {{recommended_count}} traders', {
+                  recommended_count,
+              });
+
+    <Popover alignment='top' className='recommended-by--container' message={message}>
         <Icon className='recommended-by--icon' custom_color='var(--status-success)' icon='IcThumbsUp' size={14} />
         <Text color='less-prominent' line_height='s' size={isMobile() ? 'xxxs' : 'xs'}>
             {`${recommended_average}%`}
         </Text>
-    </Popover>
-);
+    </Popover>;
+};
 
 RecommendedBy.propTypes = {
     recommended_average: PropTypes.number,

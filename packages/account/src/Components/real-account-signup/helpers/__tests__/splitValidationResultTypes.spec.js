a2bb34672d7ee3f0388f011c562b31b3eb82b754
suisin-deriv <suisin@regentmarkets.com>
2022-07-04 09:23:53 +0800

    Update removed unused component

--- packages/account/src/Components/real-account-signup/helpers/__tests__/splitValidationResultTypes.spec.js
+++ packages/account/src/Components/real-account-signup/helpers/__tests__/splitValidationResultTypes.spec.js
@@ -1,21 +1,18 @@
-import { type } from 'os';
-import { object } from 'prop-types';
-import React from 'react';
-import { splitValidationResultTypes } from '../helpers/utils';
+import { splitValidationResultTypes } from '../utils';
 
 describe('splitValidationResultTypes', () => {
     it('should return object', () => {
         expect(typeof splitValidationResultTypes([[]])).toBe('object');
     });
 
-    it('should return warnings object with Warning message inside and errors with empty object', () => {
+    it('should return warnings object with warning message inside and errors with empty object', () => {
         expect(splitValidationResultTypes([['warn', 'This is a warning message']])).toStrictEqual({
             errors: {},
             warnings: { 0: 'This is a warning message' },
         });
     });
 
-    it('should return errors object with Error message inside and warnings with empty object', () => {
+    it('should return errors object with error message inside and warnings with empty object', () => {
         expect(splitValidationResultTypes([['error', 'This is an error message']])).toStrictEqual({
             errors: { 0: 'This is an error message' },
             warnings: {},

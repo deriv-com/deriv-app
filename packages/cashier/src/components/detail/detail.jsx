1c3c668cb4879e75cb40c0d818f649e0e4c72dbd
george-usynin-binary <heorhi@binary.com>
2022-07-05 13:07:44 +0300

    add detail shared component

---
+++ packages/cashier/src/components/detail/detail.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+import { Icon, Text } from '@deriv/components';
+import './detail.scss';
+
+const Detail = ({ action, children, className, has_red_color, icon, title }) => {
+    const detail = Array.isArray(children) ? children : [children];
+    return (
+        <div
+            className={classNames('detail', {
+                [className]: !!className,
+            })}
+        >
+            {icon && (
+                <div className='detail__icon-wrapper'>
+                    <Icon icon={`IcCashier${icon}`} />
+                </div>
+            )}
+            {detail.map((child, id) => (
+                <div key={id}>
+                    {title && (
+                        <Text as='p' line_height='s' size='xs'>
+                            {title}
+                        </Text>
+                    )}
+                    {action || !title ? (
+                        <Text
+                            as='a'
+                            color={has_red_color ? 'red' : 'prominent'}
+                            href={`${action ? `${action}:` : ''}${child}`}
+                            line_height='s'
+                            size={!title ? 'xxs' : 'xs'}
+                            weight='bold'
+                            className='detail__link'
+                        >
+                            {child}
+                            {id === detail.length - 1 ? '' : ', '}
+                        </Text>
+                    ) : (
+                        <Text as='p' line_height='s' size='xs' weight='bold'>
+                            {child}
+                        </Text>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+Detail.propTypes = {
+    action: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.array, PropTypes.element, PropTypes.string]),
+    className: PropTypes.string,
+    has_red_color: PropTypes.bool,
+    icon: PropTypes.string,
+    title: PropTypes.string,
+};
+
+export default Detail;

1dc229ad89da9cbe3306a391fb6ab61cb5e93e9b
Jim Daniels Wasswa <jim@deriv.com>
2022-07-05 12:22:39 +0800

    fix: fix rejected status page styles

--- packages/account/src/Components/poo-rejected/rejected.jsx
+++ packages/account/src/Components/poo-rejected/rejected.jsx
@@ -1,19 +1,17 @@
-import { Button, Icon } from '@deriv/components';
+import { Button, Text } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import React from 'react';
-import IconMessageContent from 'Components/icon-message-content';
+import PooRejectedIcon from '../../Assets/ic-poo-rejected.svg';
 
 export const POORejetced = ({ handleRequireSubmission }) => {
     return (
-        <div className='proof-of-identity__container'>
-            <IconMessageContent
-                message={localize('Proof of ownership verification failed')}
-                text={localize('We were unable to verify your proof of ownership.')}
-                icon={<Icon icon='IcPooRejected' size={128} />}
-            />
+        <div className='proof-of-ownership__container'>
+            <PooRejectedIcon className='icon' size={128} />
+            <Text weight='bold'>{localize('Proof of ownership verification failed')}</Text>
+            <Text size='xs'>{localize('We were unable to verify your proof of ownership.')}</Text>
             <Button
                 type='button'
-                className='account-management__continue'
+                className='proof-of-ownership__try-again-button'
                 onClick={handleRequireSubmission}
                 large
                 text={localize('Try again')}

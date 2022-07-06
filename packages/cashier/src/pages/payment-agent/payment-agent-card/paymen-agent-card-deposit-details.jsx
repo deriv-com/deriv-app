499233a67698558676b1038a22e4c58484a96512
george-usynin-binary <heorhi@binary.com>
2022-07-06 10:50:13 +0300

    refactor pa-deposit-details component

--- packages/cashier/src/pages/payment-agent/payment-agent-card/paymen-agent-card-deposit-details.jsx
+++ packages/cashier/src/pages/payment-agent/payment-agent-card/paymen-agent-card-deposit-details.jsx
@@ -7,62 +7,74 @@ import Detail from 'Components/detail';
 
 const PaymentAgentCardDetails = ({ payment_agent }) => {
     const payment_agent_phones = toJS(payment_agent.phones);
-    const detail_titles = {
-        email: localize('Email'),
-        phone_number: localize('Phone number'),
-        deposit_commission: localize('Commission on deposits'),
-        withdrawal_commission: localize('Commission on withdrawal'),
-        min_withdrawal: localize('Minimum withdrawal'),
-        max_withdrawal: localize('Maximum withdrawal'),
+
+    const PaymentAgentPhonesDetails = () => {
+        return (
+            <Detail action='tel' icon='Phone' title={localize('Phone number')}>
+                {payment_agent.phones.map(phone => phone.phone_number)}
+            </Detail>
+        );
+    };
+
+    const PaymentAgentEmailDetails = () => {
+        return (
+            <Detail
+                action='mailto'
+                icon='Email'
+                rel='noopener noreferrer'
+                target='_blank'
+                has_red_color
+                title={localize('Email')}
+            >
+                {payment_agent.email}
+            </Detail>
+        );
+    };
+
+    const PaymentAgentMinimumWithdrawalDetails = () => {
+        return (
+            <Detail icon='MinimumWithdrawal' title={localize('Minimum withdrawal')}>
+                <Money amount={payment_agent.min_withdrawal} currency={payment_agent.currency} show_currency />
+            </Detail>
+        );
+    };
+
+    const PaymentAgentMaximumWithdrawalDetails = () => {
+        return (
+            <Detail icon='MaximumWithdrawal' title={localize('Maximum withdrawal')}>
+                <Money amount={payment_agent.max_withdrawal} currency={payment_agent.currency} show_currency />
+            </Detail>
+        );
+    };
+
+    const PaymentAgentDepositComissionDetails = () => {
+        return (
+            <Detail icon='CommissionDeposit' className='deposit-commission' title={localize('Commission on deposits')}>
+                {`${payment_agent.deposit_commission}%`}
+            </Detail>
+        );
+    };
+
+    const PaymentAgentWithdrawalComissionDetails = () => {
+        return (
+            <Detail
+                icon='CommissionWithdrawal'
+                className='withdrawal_commission'
+                title={localize('Commission on withdrawal')}
+            >
+                {`${payment_agent.withdrawal_commission}%`}
+            </Detail>
+        );
     };
 
     return (
         <div className='payment-agent-card__deposit-details-container'>
-            {payment_agent_phones && (
-                <Detail action='tel' icon='Phone' title={detail_titles.phone_number}>
-                    {payment_agent.phones.map(phone => phone.phone_number)}
-                </Detail>
-            )}
-            {payment_agent.email && (
-                <Detail
-                    action='mailto'
-                    icon='Email'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    title={detail_titles.email}
-                    has_red_color
-                >
-                    {payment_agent.email}
-                </Detail>
-            )}
-            {payment_agent.min_withdrawal && (
-                <Detail icon='MinimumWithdrawal' title={detail_titles.min_withdrawal}>
-                    <Money amount={payment_agent.min_withdrawal} currency={payment_agent.currency} show_currency />
-                </Detail>
-            )}
-            {payment_agent.deposit_commission && (
-                <Detail
-                    icon='CommissionDeposit'
-                    className='deposit-commission'
-                    title={detail_titles.deposit_commission}
-                >
-                    {`${payment_agent.deposit_commission}%`}
-                </Detail>
-            )}
-            {payment_agent.max_withdrawal && (
-                <Detail icon='MaximumWithdrawal' title={detail_titles.max_withdrawal}>
-                    <Money amount={payment_agent.max_withdrawal} currency={payment_agent.currency} show_currency />
-                </Detail>
-            )}
-            {payment_agent.withdrawal_commission && (
-                <Detail
-                    icon='CommissionWithdrawal'
-                    className='withdrawal_commission'
-                    title={detail_titles.withdrawal_commission}
-                >
-                    {`${payment_agent.withdrawal_commission}%`}
-                </Detail>
-            )}
+            {payment_agent_phones && <PaymentAgentPhonesDetails />}
+            {payment_agent.email && <PaymentAgentEmailDetails />}
+            {payment_agent.min_withdrawal && <PaymentAgentMinimumWithdrawalDetails />}
+            {payment_agent.deposit_commission && <PaymentAgentDepositComissionDetails />}
+            {payment_agent.max_withdrawal && <PaymentAgentMaximumWithdrawalDetails />}
+            {payment_agent.withdrawal_commission && <PaymentAgentWithdrawalComissionDetails />}
         </div>
     );
 };

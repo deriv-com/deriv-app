71fd44c156fa8645e14ca03e3f216df0de7fcc53
Carol Sachdeva <carol@binary.com>
2022-07-06 11:53:25 +0800

    fix: subtasks

--- packages/p2p/src/components/recommended-by/recommended-by.jsx
+++ packages/p2p/src/components/recommended-by/recommended-by.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { Icon, Popover, Text } from '@deriv/components';
 import { isMobile } from '@deriv/shared';
 import { localize } from 'Components/i18next';
+import RecommendedModal from './recommended-modal.jsx';
 
 const RecommendedBy = ({ recommended_average, recommended_count }) => {
+    const [is_recommended_modal_open, setIsRecommendedModalOpen] = React.useState(false);
     const message =
         recommended_count === 1
             ? localize('Recommended by {{recommended_count}} trader', {
@@ -15,12 +17,24 @@ const RecommendedBy = ({ recommended_average, recommended_count }) => {
               });
 
     return (
-        <Popover alignment='top' className='recommended-by--container' message={message}>
-            <Icon className='recommended-by--icon' icon='IcThumbsUp' size={14} />
-            <Text color='less-prominent' line_height='s' size={isMobile() ? 'xxxs' : 'xs'}>
-                {`${recommended_average}%`}
-            </Text>
-        </Popover>
+        <React.Fragment>
+            <RecommendedModal
+                is_recommended_modal_open={is_recommended_modal_open}
+                message={message}
+                setIsRecommendedModalOpen={setIsRecommendedModalOpen}
+            />
+            <Popover
+                alignment='top'
+                className='recommended-by--container'
+                message={message}
+                onClick={() => setIsRecommendedModalOpen(true)}
+            >
+                <Icon className='recommended-by--icon' icon='IcThumbsUp' size={14} />
+                <Text color='less-prominent' line_height='s' size={isMobile() ? 'xxxs' : 'xs'}>
+                    {`${recommended_average}%`}
+                </Text>
+            </Popover>
+        </React.Fragment>
     );
 };
 
--- packages/p2p/src/components/recommended-by/recommended-modal.jsx
+++ packages/p2p/src/components/recommended-by/recommended-modal.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Modal, Text } from '@deriv/components';
 import { localize } from 'Components/i18next';
+import './recommended-modal.scss';
 
 const RecommendedModal = ({ is_recommended_modal_open, message, setIsRecommendedModalOpen }) => (
-    <Modal is_open={is_recommended_modal_open}>
+    <Modal height='12.6rem' is_open={is_recommended_modal_open} width='32.8rem'>
         <Modal.Body>
             <Text color='prominent' line_height='m' size='xxs'>
                 {message}
             </Text>
         </Modal.Body>
         <Modal.Footer>
-            <Button color='primary' onClick={() => setIsRecommendedModalOpen(false)} text={localize('Ok')} />
+            <Button large primary onClick={() => setIsRecommendedModalOpen(false)} text={localize('Ok')} />
         </Modal.Footer>
     </Modal>
 );

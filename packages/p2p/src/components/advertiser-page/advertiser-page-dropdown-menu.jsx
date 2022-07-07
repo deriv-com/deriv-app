567cd3506831d71341f0bc7ef5f8e6c087ce71c4
ameerul <ameerul@deriv.com>
2022-07-07 14:54:46 +0800

    removed is_blocked from dropdown

--- packages/p2p/src/components/advertiser-page/advertiser-page-dropdown-menu.jsx
+++ packages/p2p/src/components/advertiser-page/advertiser-page-dropdown-menu.jsx
@@ -8,8 +8,6 @@ import './advertiser-page.scss';
 const AdvertiserPageDropdownMenu = () => {
     const { advertiser_page_store, general_store } = useStores();
 
-    const { is_blocked } = advertiser_page_store.advertiser_info;
-
     const showDropdown = () => {
         if (!advertiser_page_store.is_counterparty_advertiser_blocked) {
             advertiser_page_store.setIsDropdownMenuVisible(!advertiser_page_store.is_dropdown_menu_visible);
@@ -21,7 +19,9 @@ const AdvertiserPageDropdownMenu = () => {
             <Icon className='advertiser-page__menu-dots-icon' icon='IcMenuDots' onClick={showDropdown} size={16} />
             {advertiser_page_store.is_dropdown_menu_visible && (
                 <div
-                    className={`advertiser-page__dropdown${is_blocked ? '--disabled' : ''}`}
+                    className={`advertiser-page__dropdown${
+                        advertiser_page_store.is_counterparty_advertiser_blocked ? '--disabled' : ''
+                    }`}
                     onClick={() => general_store.setIsBlockUserModalOpen(true)}
                 >
                     <Dropdown

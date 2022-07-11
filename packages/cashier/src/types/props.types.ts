5cba712982b236d0cff8a0907568b1ecb05ff728
Hamid Yaftian <hamid@re-work.dev>
2022-07-11 18:44:14 +0430

    Enhance tests

--- packages/cashier/src/types/props.types.ts
+++ packages/cashier/src/types/props.types.ts
@@ -3,3 +3,5 @@ export type TReactChangeEvent = React.ChangeEvent<HTMLInputElement>;
 export type TReactChildren = React.ReactNode;
 
 export type TReactMouseEvent = React.MouseEvent<HTMLElement>;
+
+export type TSideNotesProps = Array<string | JSX.Element | JSX.Element[]> | null;

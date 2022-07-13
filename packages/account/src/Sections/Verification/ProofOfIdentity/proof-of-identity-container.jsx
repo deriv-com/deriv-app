a589f1a9413233a1ff691ce9cb8906474edde631
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-07-13 17:13:07 +0300

    try submitting an id document instead message removed

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
@@ -76,7 +76,6 @@ const ProofOfIdentityContainer = ({
     const {
         idv,
         allow_poi_resubmission,
-        has_attempted_idv,
         identity_last_attempt,
         identity_status,
         is_age_verified,
@@ -100,7 +99,6 @@ const ProofOfIdentityContainer = ({
         return (
             <POISubmission
                 allow_poi_resubmission={allow_poi_resubmission}
-                has_attempted_idv={has_attempted_idv}
                 has_require_submission={has_require_submission}
                 height={height ?? null}
                 identity_last_attempt={identity_last_attempt}

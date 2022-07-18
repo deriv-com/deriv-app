33b8571930da67395acbe4432cc50795b637d187
george-usynin-binary <heorhi@binary.com>
2022-07-18 16:45:19 +0300

    add pa search box in deposit-withdraw container

--- packages/cashier/src/pages/payment-agent/payment-agent-deposit-withdraw-container/payment-agent-deposit-withdraw-container.jsx
+++ packages/cashier/src/pages/payment-agent/payment-agent-deposit-withdraw-container/payment-agent-deposit-withdraw-container.jsx
@@ -1,15 +1,32 @@
 import { PropTypes as MobxPropTypes } from 'mobx-react';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { DesktopWrapper, Dropdown, MobileWrapper, SelectNative, Text } from '@deriv/components';
+import { DesktopWrapper, Dropdown, Icon, Loading, MobileWrapper, SelectNative, Text } from '@deriv/components';
 import { localize, Localize } from '@deriv/translations';
 import { connect } from 'Stores/connect';
 import PaymentAgentCard from '../payment-agent-card';
 import PaymentAgentDisclaimer from '../payment-agent-disclaimer';
+import PaymentAgentSearchBox from '../payment-agent-search-box';
+
+const PaymentAgentSearchWarning = () => {
+    return (
+        <div className='payment-agent-list__search-warning cashier__wrapper--align-center'>
+            <Icon icon='IcCashierSearch' size={64} />
+            <Text as='p' line_height='m' size='xs' weight='bold'>
+                <Localize i18n_default_text='No payment agents found for your search' />
+            </Text>
+            <Text as='p' line_height='m' size='xs'>
+                <Localize i18n_default_text='Try changing your search criteria.' />
+            </Text>
+        </div>
+    );
+};
 
 const PaymentAgentDepositWithdrawContainer = ({
+    has_payment_agent_search_warning,
     is_dark_mode_on,
     is_deposit,
+    is_search_loading,
     onChangePaymentMethod,
     payment_agent_list,
     selected_bank,
@@ -28,9 +45,11 @@ const PaymentAgentDepositWithdrawContainer = ({
 
     return (
         <React.Fragment>
-            <MobileWrapper>
-                <PaymentAgentDisclaimer />
-            </MobileWrapper>
+            {!has_payment_agent_search_warning && (
+                <MobileWrapper>
+                    <PaymentAgentDisclaimer />
+                </MobileWrapper>
+            )}
             <div className='payment-agent-list__list-header'>
                 {is_deposit ? (
                     <Text as='p' line_height='s' size='xs'>
@@ -43,6 +62,7 @@ const PaymentAgentDepositWithdrawContainer = ({
                 )}
             </div>
             <div className='payment-agent-list__list-selector'>
+                <PaymentAgentSearchBox />
                 {supported_banks.length > 1 && (
                     <React.Fragment>
                         <DesktopWrapper>
@@ -76,23 +96,35 @@ const PaymentAgentDepositWithdrawContainer = ({
                     </React.Fragment>
                 )}
             </div>
-            {payment_agent_list.map((payment_agent, idx) => {
-                return (
-                    <PaymentAgentCard
-                        key={idx}
-                        is_dark_mode_on={is_dark_mode_on}
-                        is_deposit={is_deposit}
-                        payment_agent={payment_agent}
-                    />
-                );
-            })}
+            {is_search_loading ? (
+                <Loading is_fullscreen={false} className='payment-agent-list__search-loader' />
+            ) : (
+                <React.Fragment>
+                    {has_payment_agent_search_warning ? (
+                        <PaymentAgentSearchWarning />
+                    ) : (
+                        payment_agent_list.map((payment_agent, idx) => {
+                            return (
+                                <PaymentAgentCard
+                                    key={idx}
+                                    is_dark_mode_on={is_dark_mode_on}
+                                    is_deposit={is_deposit}
+                                    payment_agent={payment_agent}
+                                />
+                            );
+                        })
+                    )}
+                </React.Fragment>
+            )}
         </React.Fragment>
     );
 };
 
 PaymentAgentDepositWithdrawContainer.propTypes = {
+    has_payment_agent_search_warning: PropTypes.bool,
     is_dark_mode_on: PropTypes.bool,
     is_deposit: PropTypes.bool,
+    is_search_loading: PropTypes.bool,
     onChangePaymentMethod: PropTypes.func,
     payment_agent_list: PropTypes.array,
     selected_bank: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
@@ -100,7 +132,9 @@ PaymentAgentDepositWithdrawContainer.propTypes = {
 };
 
 export default connect(({ modules, ui }) => ({
+    has_payment_agent_search_warning: modules.cashier.payment_agent.has_payment_agent_search_warning,
     is_dark_mode_on: ui.is_dark_mode_on,
+    is_search_loading: modules.cashier.payment_agent.is_search_loading,
     onChangePaymentMethod: modules.cashier.payment_agent.onChangePaymentMethod,
     payment_agent_list: modules.cashier.payment_agent.filtered_list,
     selected_bank: modules.cashier.payment_agent.selected_bank,

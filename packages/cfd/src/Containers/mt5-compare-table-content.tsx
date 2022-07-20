53b92fafd5ed57c30d81f7d29c92da66a4e77d06
Maryia <103177211+maryia-binary@users.noreply.github.com>
2022-07-20 08:44:39 +0300

    Maryia/Refactor: compare account modal content + improvements (#32)
    * Maryia/refactoring compare accounts modal content
    
    * added a case for 0 financial or 0 synthetic accounts count
    
    * improved synthetic & financial header style loop

--- packages/cfd/src/Containers/mt5-compare-table-content.tsx
+++ packages/cfd/src/Containers/mt5-compare-table-content.tsx
@@ -219,8 +219,7 @@ const DMT5CompareModalContent = ({
     const getAvailableAccountsContent = (_content: TModalContentProps[]) => {
         return _content.map(row_data => {
             const available_accounts_values = Object.entries(row_data.values).reduce(
-                (acc, [key, value]) =>
-                    available_accounts_keys.includes(key) ? { ...acc, [key as keyof TValues]: value } : acc,
+                (acc, [key, value]) => (available_accounts_keys.includes(key) ? { ...acc, [key]: value } : acc),
                 {} as TValues
             );
             const content_data = { ...row_data, values: {} as TValues };
@@ -238,17 +237,15 @@ const DMT5CompareModalContent = ({
                     (acc, el) => (available_accounts_keys.includes(el) ? { ...acc, [el]: undefined } : acc),
                     {}
                 );
-                if (row_data.id === 'leverage') {
-                    if (available_accounts_keys.includes('financial_svg'))
-                        content_data.values.financial_svg = row_data.values.financial_vanuatu;
-                    if (available_accounts_keys.includes('financial_bvi'))
-                        content_data.values.financial_bvi = row_data.values.financial_vanuatu;
-                } else if (row_data.id === 'instruments') {
-                    if (available_accounts_keys.includes('synthetic_bvi'))
-                        content_data.values.synthetic_bvi = row_data.values.synthetic_svg;
-                    if (available_accounts_keys.includes('financial_bvi'))
-                        content_data.values.financial_bvi = row_data.values.financial_svg;
-                }
+                available_accounts_keys.forEach(key => {
+                    if (row_data.id === 'leverage' && (key === 'financial_svg' || key === 'financial_bvi')) {
+                        content_data.values[key] = row_data.values.financial_vanuatu;
+                    } else if (row_data.id === 'instruments' && key === 'synthetic_bvi') {
+                        content_data.values[key] = row_data.values.synthetic_svg;
+                    } else if (row_data.id === 'instruments' && key === 'financial_bvi') {
+                        content_data.values[key] = row_data.values.financial_svg;
+                    }
+                });
             }
             return { ...content_data, values: { ...content_data.values, ...available_accounts_values } };
         });
@@ -437,14 +434,16 @@ const DMT5CompareModalContent = ({
                                 }
                             >
                                 <Table.Head fixed className='cfd-real-compare-accounts__table-empty-cell' />
-                                {!show_eu_related && (
+                                {!show_eu_related && synthetic_accounts_count > 0 && (
                                     <Table.Head className='cfd-real-compare-accounts__table-header-item'>
                                         {localize('Synthetic')}
                                     </Table.Head>
                                 )}
-                                <Table.Head className='cfd-real-compare-accounts__table-header-item'>
-                                    {show_eu_related ? localize('CFDs') : localize('Financial')}
-                                </Table.Head>
+                                {financial_accounts_count > 0 && (
+                                    <Table.Head className='cfd-real-compare-accounts__table-header-item'>
+                                        {show_eu_related ? localize('CFDs') : localize('Financial')}
+                                    </Table.Head>
+                                )}
                             </Table.Row>
                         </Table.Header>
 

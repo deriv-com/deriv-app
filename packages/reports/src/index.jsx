ffe5b2216c26a45fc38076e7c67ee5f7b727f955
yashim-deriv <yashim@deriv.com>
2022-07-21 14:36:02 +0800

    chore: proper formatting

--- packages/reports/src/index.jsx
+++ packages/reports/src/index.jsx
@@ -1,8 +1,7 @@
-import 'promise-polyfill';
-
 import React from 'react';
 import { makeLazyLoader } from '@deriv/shared';
 import { Loading } from '@deriv/components';
+import 'promise-polyfill';
 
 const App = makeLazyLoader(
     () => import(/* webpackChunkName: "reports-app", webpackPreload: true */ './app.jsx'),

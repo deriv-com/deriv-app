0a94a55bb97c7b90a7a5703deab5cf2b536c67fd
Akmal Djumakhodjaev <akmal@binary.com>
2022-07-21 11:43:02 +0800

    Prioritise pending status from BO

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-container.jsx
@@ -121,7 +121,9 @@ const ProofOfIdentityContainer = ({
     } else if (
         !identity_last_attempt ||
         // Prioritise verified status from back office. How we know this is if there is mismatch between current statuses (Should be refactored)
-        (identity_status === 'verified' && identity_status !== identity_last_attempt.status)
+        (identity_status === identity_status_codes.verified && identity_status !== identity_last_attempt.status) ||
+        // Prioritise pending status from back office. We can't rely on last attempt as it doesn't get updated after manual upload (Should be refactored)
+        identity_status === identity_status_codes.pending
     ) {
         switch (identity_status) {
             case identity_status_codes.pending:

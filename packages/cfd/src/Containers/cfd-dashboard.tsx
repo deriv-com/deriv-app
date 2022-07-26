b002f9a78e6566d5c05833c3d9c7548dd6a90cdc
hirad-rework <hirad@re-work.dev>
2022-07-26 12:50:57 +0430

    Fixed the grammatical issue of MT5 server maintenance message

--- packages/cfd/src/Containers/cfd-dashboard.tsx
+++ packages/cfd/src/Containers/cfd-dashboard.tsx
@@ -580,7 +580,7 @@ const CFDDashboard = (props: TCFDDashboardProps) => {
                                         <Localize i18n_default_text='Server maintenance starts at 06:00 GMT every Sunday and may last up to 2 hours. Service may be disrupted during this time.' />
                                     )}
                                     {platform === CFD_PLATFORMS.MT5 && (
-                                        <Localize i18n_default_text='Server maintenance starting 01:00 GMT every Sunday. This process may take up to 2 hours to complete. Service may be disrupted during this time.' />
+                                        <Localize i18n_default_text='Server maintenance starts at 01:00 GMT every Sunday, and this process may take up to 2 hours to complete. Service may be disrupted during this time.' />
                                     )}
                                 </div>
                             </div>

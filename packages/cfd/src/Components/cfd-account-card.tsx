513092ebc2b7992c2318fac2cfe0dfebe9e709b4
hirad-rework <hirad@re-work.dev>
2022-07-26 18:34:50 +0430

    Fixed sonar cloud issue

--- packages/cfd/src/Components/cfd-account-card.tsx
+++ packages/cfd/src/Components/cfd-account-card.tsx
@@ -230,10 +230,7 @@ const CFDAccountCardComponent = ({
         existing_accounts_data?.map(acc => {
             if (acc.landing_company_short === 'svg') {
                 if (all_svg_acc.length) {
-                    all_svg_acc.find(svg_acc => {
-                        if (svg_acc.server !== acc.server) all_svg_acc.push(acc);
-                        return all_svg_acc;
-                    });
+                    all_svg_acc.find(svg_acc => (svg_acc.server !== acc.server ? all_svg_acc.push(acc) : all_svg_acc));
                 } else {
                     all_svg_acc.push(acc);
                 }

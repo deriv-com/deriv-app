b236437cbcf0d4ef0340b4960eb8d5cbe00892a6
Farzin Mirzaie <farzin@firstsource.tech>
2022-07-28 14:56:59 +0430

    refactor(cashier): :recycle: refactor the initial solution with the suggested one in the pr comments

--- packages/cashier/src/components/cashier-locked/cashier-locked.tsx
+++ packages/cashier/src/components/cashier-locked/cashier-locked.tsx
@@ -2,35 +2,8 @@ import React from 'react';
 import { Authorize, GetAccountStatus } from '@deriv/api-types';
 import { connect } from 'Stores/connect';
 import { RootStore } from 'Types';
-import {
-    EmptyStateAskCurrency,
-    EmptyStateAskFinancialRiskApproval,
-    EmptyStateAskSelfExclusionMaxTurnoverSet,
-    EmptyStateAskTINInformation,
-    EmptyStateAskUKFundsProtection,
-    EmptyStateCashierLockedAskAuthenticate,
-    EmptyStateCashierLockedAskFixDetails,
-    EmptyStateCashierLockedFinancialAssessmentRequired,
-    EmptyStateCashierLockedStatus,
-    EmptyStateCashierLockedSystemMaintenance,
-    EmptyStateCryptoLockedSystemMaintenance,
-    EmptyStateDepositLockedAskFixDetails,
-    EmptyStateDepositLockedSystemMaintenance,
-    EmptyStateDisabledStatus,
-    EmptyStateDocumentsExpired,
-    EmptyStateIdentityVerificationNeeded,
-    EmptyStateNoResidence,
-    EmptyStateNoWithdrawalOrTradingStatus,
-    EmptyStateOnlyPAWithdrawalsAllowedStatus,
-    EmptyStateSelfExclusion,
-    EmptyStateUnwelcomeStatus,
-    EmptyStateWithdrawalLockedAskAuthenticate,
-    EmptyStateWithdrawalLockedAskFixDetails,
-    EmptyStateWithdrawalLockedFinancialAssessmentRequired,
-    EmptyStateWithdrawalLockedStatus,
-    EmptyStateWithdrawalLockedSystemMaintenance,
-    EmptyStateWithdrawServiceUnavailableForPA,
-} from './cashier-locked-states';
+import EmptyState from 'Components/empty-state';
+import provider from './cashier-locked-provider';
 
 type TAccount = NonNullable<Authorize['account_list']>[0];
 
@@ -57,67 +30,18 @@ const CashierLocked = ({
     loginid,
     is_identity_verification_needed,
 }: TProps) => {
-    const { cashier_validation } = account_status;
-    const no_residence = cashier_validation?.includes('no_residence');
-    const unwelcome_status = cashier_validation?.includes('unwelcome_status');
-    const self_exclusion = cashier_validation?.includes('SelfExclusion');
-    const no_withdrawal_or_trading_status = cashier_validation?.includes('no_withdrawal_or_trading_status');
-    const only_pa_withdrawals_allowed_status = cashier_validation?.includes('only_pa_withdrawals_allowed_status');
-    const withdraw_service_unavailable_for_pa = cashier_validation?.includes('WithdrawServiceUnavailableForPA');
-    const withdrawal_locked_status = cashier_validation?.includes('withdrawal_locked_status');
-    const documents_expired = cashier_validation?.includes('documents_expired');
-    const cashier_locked_status = cashier_validation?.includes('cashier_locked_status');
-    const disabled_status = cashier_validation?.includes('disabled_status');
-    const financial_assessment_required = cashier_validation?.includes('FinancialAssessmentRequired');
-    const ask_currency = cashier_validation?.includes('ASK_CURRENCY');
-    const ask_authenticate = cashier_validation?.includes('ASK_AUTHENTICATE');
-    const ask_financial_risk_approval = cashier_validation?.includes('ASK_FINANCIAL_RISK_APPROVAL');
-    const ask_tin_information = cashier_validation?.includes('ASK_TIN_INFORMATION');
-    const ask_self_exclusion_max_turnover_set = cashier_validation?.includes('ASK_SELF_EXCLUSION_MAX_TURNOVER_SET');
-    const ask_fix_details = cashier_validation?.includes('ASK_FIX_DETAILS');
-    const ask_uk_funds_protection = cashier_validation?.includes('ASK_UK_FUNDS_PROTECTION');
-    const is_crypto = current_currency_type === 'crypto';
+    const state = provider.getMessage({
+        cashier_validation: account_status.cashier_validation,
+        is_crypto: current_currency_type === 'crypto',
+        is_system_maintenance,
+        is_cashier_locked,
+        is_deposit_locked,
+        is_withdrawal_locked,
+        is_identity_verification_needed,
+        excluded_until: is_deposit_locked ? accounts[loginid].excluded_until : undefined,
+    });
 
-    if (is_system_maintenance) {
-        if (is_crypto && is_withdrawal_locked) return <EmptyStateWithdrawalLockedSystemMaintenance />;
-        if (is_crypto && is_deposit_locked) return <EmptyStateDepositLockedSystemMaintenance />;
-        if (is_crypto) return <EmptyStateCryptoLockedSystemMaintenance />;
-        return <EmptyStateCashierLockedSystemMaintenance />;
-    }
-
-    if (is_cashier_locked) {
-        if (no_residence) return <EmptyStateNoResidence />;
-        if (documents_expired) return <EmptyStateDocumentsExpired />;
-        if (cashier_locked_status) return <EmptyStateCashierLockedStatus />;
-        if (disabled_status) return <EmptyStateDisabledStatus />;
-        if (ask_currency) return <EmptyStateAskCurrency />;
-        if (ask_authenticate && is_identity_verification_needed) return <EmptyStateIdentityVerificationNeeded />;
-        if (ask_authenticate) return <EmptyStateCashierLockedAskAuthenticate />;
-        if (ask_financial_risk_approval) return <EmptyStateAskFinancialRiskApproval />;
-        if (financial_assessment_required) return <EmptyStateCashierLockedFinancialAssessmentRequired />;
-        if (ask_tin_information) return <EmptyStateAskTINInformation />;
-        if (ask_uk_funds_protection) return <EmptyStateAskUKFundsProtection />;
-        if (ask_self_exclusion_max_turnover_set) return <EmptyStateAskSelfExclusionMaxTurnoverSet />;
-        if (ask_fix_details) return <EmptyStateCashierLockedAskFixDetails />;
-    }
-
-    if (is_deposit_locked) {
-        if (ask_fix_details) return <EmptyStateDepositLockedAskFixDetails />;
-        if (self_exclusion) return <EmptyStateSelfExclusion excluded_until={accounts[loginid].excluded_until} />;
-        if (unwelcome_status) return <EmptyStateUnwelcomeStatus />;
-    }
-
-    if (is_withdrawal_locked) {
-        if (financial_assessment_required) return <EmptyStateWithdrawalLockedFinancialAssessmentRequired />;
-        if (ask_authenticate) return <EmptyStateWithdrawalLockedAskAuthenticate />;
-        if (ask_fix_details) return <EmptyStateWithdrawalLockedAskFixDetails />;
-        if (withdraw_service_unavailable_for_pa) return <EmptyStateWithdrawServiceUnavailableForPA />;
-        if (no_withdrawal_or_trading_status) return <EmptyStateNoWithdrawalOrTradingStatus />;
-        if (withdrawal_locked_status) return <EmptyStateWithdrawalLockedStatus />;
-        if (only_pa_withdrawals_allowed_status) return <EmptyStateOnlyPAWithdrawalsAllowedStatus />;
-    }
-
-    return <EmptyStateCashierLockedStatus />;
+    return <EmptyState icon={state.icon} title={state.title} description={state.description} />;
 };
 
 export default connect(({ client, modules }: RootStore) => ({
--- packages/cashier/src/components/empty-state/empty-state.tsx
+++ packages/cashier/src/components/empty-state/empty-state.tsx
@@ -2,13 +2,13 @@ import { Icon, Text } from '@deriv/components';
 import React from 'react';
 import './empty-state.scss';
 
-type TProps = {
+export type TEmptyStateProps = {
     icon: string;
     title: string | React.ReactNode;
     description: string | React.ReactNode;
 };
 
-const EmptyState: React.FC<TProps> = ({ icon, title, description }) => (
+const EmptyState: React.FC<TEmptyStateProps> = ({ icon, title, description }) => (
     <div className='cashier-empty-state'>
         <Icon icon={icon} className='cashier-empty-state__icon' />
         <Text as='h2' weight='bold' align='center' className='cashier-empty-state__title'>

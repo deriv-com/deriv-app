604d99327f7eb9b5f07b5ee0ed8dc7ec12c2a2fc
Aizad Ridzo <aizad@deriv.com>
2022-07-28 18:53:12 +0800

    added stylings for responsive for mf new users signup

--- packages/account/src/Components/trading-assessment/risk-tolerance-warning-modal.jsx
+++ packages/account/src/Components/trading-assessment/risk-tolerance-warning-modal.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Button, Icon, Modal, Text } from '@deriv/components';
+import { Button, Icon, MobileDialog, MobileWrapper, Modal, Text } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
+import { isMobile } from '@deriv/shared';
 
 const RiskToleranceWarningModal = ({ show_risk_modal, setShowRiskModal, title }) => {
+    if (isMobile()) {
+        return (
+            <MobileWrapper>
+                <MobileDialog
+                    portal_element_id='modal_root'
+                    visible={show_risk_modal}
+                    title={title}
+                    wrapper_classname='risk-acceptance'
+                    has_close_icon={false}
+                >
+                    <Icon icon='IcRedWarning' size={65} />
+                    <Text as='p' size='xs' align='center' line_height='0.18rem'>
+                        <Localize
+                            i18n_default_text='CFDs and other financial instruments come with a high risk of losing money rapidly due to leverage. You should consider whether you understand how CFDs and other financial instruments work and whether you can afford to take the high risk of losing your money. <0/><0/> To continue, kindly note that you would need to wait 24 hours before you can proceed further.'
+                            components={[<br key={0} />]}
+                        />
+                    </Text>
+                    <Button type='button' large text={localize('OK')} primary onClick={() => setShowRiskModal(false)} />
+                </MobileDialog>
+            </MobileWrapper>
+        );
+    }
     return (
         <Modal
             width='44rem'
--- packages/account/src/Components/trading-assessment/test-warning-modal.jsx
+++ packages/account/src/Components/trading-assessment/test-warning-modal.jsx
@@ -1,8 +1,43 @@
-import { Button, Modal, Text } from '@deriv/components';
+import { Button, FormSubmitButton, MobileDialog, MobileWrapper, Modal, Text } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
 import React from 'react';
 
 const TestWarningModal = ({ show_risk_modal, onAccept, onDecline }) => {
+    if (isMobile()) {
+        return (
+            <MobileWrapper>
+                <MobileDialog
+                    wrapper_classname='test-warning'
+                    portal_element_id='modal_root'
+                    visible={show_risk_modal}
+                    title={localize('Appropriateness Test Warning')}
+                    has_close_icon={false}
+                >
+                    <Text as='p' size='xs'>
+                        <Localize i18n_default_text='In providing our services to you, we are required to obtain information from you in order to assess whether a given product or service is appropriate for you and whether you possess the experience and knowledge to understand the risks involved.' />
+                    </Text>
+                    <Text as='p' size='xs'>
+                        <Localize
+                            i18n_default_text='<0/>On the basis of the information provided in relation to your knowledge and experience, we consider that the investments available via this website are not appropriate for you.<0/><0/>'
+                            components={[<br key={0} />]}
+                        />
+                    </Text>
+                    <Text as='p' size='xs'>
+                        <Localize i18n_default_text='By clicking ‘Accept’ and proceeding with the account opening, you should note that you may be exposing yourself to risks. These risks, which may be significant, include the risk of losing the entire sum invested, and you may not have the knowledge and experience to properly assess or mitigate them.' />
+                    </Text>
+                    <FormSubmitButton
+                        label={localize('Accept')}
+                        onClick={onAccept}
+                        is_absolute
+                        has_cancel
+                        cancel_label={localize('Decline')}
+                        onCancel={onDecline}
+                    />
+                </MobileDialog>
+            </MobileWrapper>
+        );
+    }
+
     return (
         <Modal
             width='44rem'

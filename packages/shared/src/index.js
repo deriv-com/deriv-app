cbbcb6e5c4ce0d6f6b6a2256a95c873d82e3688f
Jim Daniels Wasswa <jim@deriv.com>
2022-07-28 11:00:54 +0800

    refactor: move class Validator into packages/shared

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -28,3 +28,4 @@ export * from './services';
 export * from './utils/helpers';
 export * from './utils/constants';
 export * from './utils/loader-handler';
+export * from './utils/validator';
---
+++ packages/shared/src/utils/validator/index.js
@@ -0,0 +1,3 @@
+import Validator from './validator';
+
+export { Validator };

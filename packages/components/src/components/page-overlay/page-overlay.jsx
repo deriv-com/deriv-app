1bbde48e48b5fdcdbcae9a8e6bc55cb2b087b312
Akmal Djumakhodjaev <akmal@binary.com>
2022-07-29 11:07:52 +0800

    Remove close icon from reports for CRA accounts

--- packages/components/src/components/page-overlay/page-overlay.jsx
+++ packages/components/src/components/page-overlay/page-overlay.jsx
@@ -6,7 +6,16 @@ import PropTypes from 'prop-types';
 import Icon from '../icon/icon.jsx';
 import { useOnClickOutside } from '../../hooks';
 
-const PageOverlay = ({ children, header, id, is_from_app = false, is_open, onClickClose, portal_id }) => {
+const PageOverlay = ({
+    children,
+    header,
+    id,
+    is_cra = false,
+    is_from_app = false,
+    is_open,
+    onClickClose,
+    portal_id,
+}) => {
     const page_overlay_ref = React.useRef();
 
     useOnClickOutside(page_overlay_ref, onClickClose, () => is_open && portal_id);
@@ -23,7 +32,7 @@ const PageOverlay = ({ children, header, id, is_from_app = false, is_open, onCli
                 <div className={classNames('dc-page-overlay__header', { 'dc-page-app__header ': is_from_app })}>
                     <div className='dc-page-overlay__header-wrapper'>
                         <div className='dc-page-overlay__header-title'>{header}</div>
-                        {!is_from_app && (
+                        {!is_from_app && !is_cra && (
                             <div
                                 data-testid='page_overlay_header_close'
                                 className='dc-page-overlay__header-close'

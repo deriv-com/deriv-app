faf9c5d21b90e53456df0469a34a35d067c89a80
Akmal Djumakhodjaev <akmal@binary.com>
2022-08-03 16:13:14 +0800

    Fix merge conflicts

--- packages/cfd/src/Components/cfd-poi.tsx
+++ packages/cfd/src/Components/cfd-poi.tsx
@@ -28,11 +28,8 @@ export type TCFDPOIProps = {
     addNotificationByKey: (key: string) => void;
     fetchResidenceList?: () => void;
     height: string;
-    is_cfd_poi_completed: boolean;
-    is_loading: boolean;
     is_switching: boolean;
     is_virtual: boolean;
-    setIsCfdPoiCompleted: (status: boolean) => void;
     onSave: (index: number, values: TFormValues) => void;
     refreshNotifications: () => void;
     removeNotificationByKey: (key: TCFDNotificationByKey) => void;
@@ -43,43 +40,10 @@ export type TCFDPOIProps = {
     jurisdiction_selected_shortcode: string;
 };
 
-const CFDPOI = ({
-    authentication_status,
-    form_error,
-    index,
-    is_cfd_poi_completed,
-    setIsCfdPoiCompleted,
-    onCancel,
-    onSave,
-    onSubmit,
-    value,
-    height,
-    ...props
-}: TCFDPOIProps) => {
-    const { identity_status } = authentication_status;
+const CFDPOI = ({ index, onSave, onSubmit, height, ...props }: TCFDPOIProps) => {
     const [poi_state, setPOIState] = React.useState<string>('none');
     const citizen = props.account_settings?.citizen || props.account_settings?.country_code;
     const citizen_data = props.residence_list?.find(item => item.value === citizen);
-
-    // we need extra variable since the state has issues with updating under some conditions
-    const is_next_btn_disabled = !(
-        ['pending'].includes(poi_state) || ['pending', 'verified'].includes(identity_status)
-    );
-    const [is_next_btn_disabled_state, setIsNextBtnDisabled] = React.useState(is_next_btn_disabled);
-
-    const validateForm = React.useCallback(() => {
-        const errors = {};
-        if (!['pending'].includes(poi_state) || !['pending', 'verified'].includes(identity_status)) {
-            (errors as TErrors).poi_state = true;
-        }
-        return errors;
-    }, [poi_state, identity_status]);
-
-    React.useEffect(() => {
-        if (is_cfd_poi_completed) {
-            setIsNextBtnDisabled(false);
-        }
-    }, [is_cfd_poi_completed]);
 
     const onStateChange = (status: string) => {
         setPOIState(status);
@@ -101,12 +65,10 @@ export default connect(({ client, common, notifications }: RootStore) => ({
     account_status: client.account_status,
     app_routing_history: common.app_routing_history,
     fetchResidenceList: client.fetchResidenceList,
-    is_cfd_poi_completed: client.is_cfd_poi_completed,
     is_switching: client.is_switching,
     is_virtual: client.is_virtual,
     refreshNotifications: notifications.refreshNotifications,
     routeBackInApp: common.routeBackInApp,
-    setIsCfdPoiCompleted: client.setIsCfdPoiCompleted,
     should_allow_authentication: client.should_allow_authentication,
     account_settings: client.account_settings,
     residence_list: client.residence_list,

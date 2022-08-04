fed0f1f77f141ef25a70b6c120920a08fd6332fb
shayan khaleghparast <iman@firstsource.tech>
2022-08-04 15:25:48 +0430

    fix: added Wallet and affiliate accounts to valid_login_ids_regex

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -567,7 +567,7 @@ export default class ClientStore extends BaseStore {
     @computed
     get is_valid_login() {
         if (!this.is_logged_in) return true;
-        const valid_login_ids_regex = new RegExp('^(MX|MF|VRTC|MLT|CR|FOG)[0-9]+$', 'i');
+        const valid_login_ids_regex = new RegExp('^(MX|MF|VRTC|VRDW|MLT|CR|FOG|AFF)[0-9]+$', 'i');
         return this.all_loginids.every(id => valid_login_ids_regex.test(id));
     }
 

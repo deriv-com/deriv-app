6c4167183aa222892c8968a4db59adbc1220dd2f
amina-deriv <amina@deriv.com>
2022-08-07 23:22:57 +0400

    css

--- packages/cfd/src/Containers/cfd-tnc-modal.tsx
+++ packages/cfd/src/Containers/cfd-tnc-modal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
 import RootStore from 'Stores/index';
 import { connect } from 'Stores/connect';
-import { DesktopWrapper, MobileDialog, MobileWrapper, Modal, UILoader, Text, Button } from '@deriv/components';
+import { Dialog, Text, Button } from '@deriv/components';
 import { TOpenAccountTransferMeta } from '../Components/props.types';
 import { localize } from '@deriv/translations';
 import { getMT5LicenceNotes } from '../Helpers/constants';
@@ -48,7 +49,7 @@ const CFDTncModal = ({
 
     const ModalContentForTncModal = () => (
         <>
-            <div className='cfd-tnc-modal-content'>
+            <div className='cfd-tnc-dialog-content'>
                 <Text as='p' align='left' size='xs' line_height='m'>
                     {localize(getMT5LicenceNotes(account_type_name, jurisdiction_selected_shortcode))}
                 </Text>
@@ -60,7 +61,7 @@ const CFDTncModal = ({
                     />
                 </div>
             </div>
-            <Modal.Footer>
+            <div className='cfd-tnc-dialog-content--footer'>
                 <Button text={localize('Back')} onClick={handleCancel} large secondary />
                 <Button
                     text={localize('Next')}
@@ -69,36 +70,22 @@ const CFDTncModal = ({
                     large
                     primary
                 />
-            </Modal.Footer>
+            </div>
         </>
     );
     return (
-        <React.Suspense fallback={<UILoader />}>
-            <DesktopWrapper>
-                <Modal
-                    className='cfd-tnc-modal'
-                    disableApp={disableApp}
-                    enableApp={enableApp}
-                    is_open={is_cfd_tnc_modal_visible}
-                    toggleModal={toggleCFDTncModal}
-                    height={jurisdiction_selected_shortcode === 'svg' ? '168px' : '224px'}
-                    width='464px'
-                    exit_classname='cfd-modal--custom-exit'
-                >
-                    <ModalContentForTncModal />
-                </Modal>
-            </DesktopWrapper>
-            <MobileWrapper>
-                <MobileDialog
-                    portal_element_id='deriv_app'
-                    wrapper_classname='cfd-tnc-modal'
-                    visible={is_cfd_tnc_modal_visible}
-                    onClose={toggleCFDTncModal}
-                >
-                    <ModalContentForTncModal />
-                </MobileDialog>
-            </MobileWrapper>
-        </React.Suspense>
+        <Dialog
+            is_visible={is_cfd_tnc_modal_visible}
+            disableApp={disableApp}
+            enableApp={enableApp}
+            is_mobile_full_width={false}
+            is_content_centered
+            className={classNames('cfd-tnc-modal', {
+                'cfd-tnc-modal--svg': jurisdiction_selected_shortcode === 'svg',
+            })}
+        >
+            <ModalContentForTncModal />
+        </Dialog>
     );
 };
 export default connect(({ ui, modules }: RootStore) => ({

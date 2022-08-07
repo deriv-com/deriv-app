9f16dfc8174449ed425819c923bad5e72921bce5
amina-deriv <amina@deriv.com>
2022-08-07 23:25:38 +0400

    remove commented code

--- packages/cfd/src/Containers/jurisdiction-modal-content.tsx
+++ packages/cfd/src/Containers/jurisdiction-modal-content.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Icon, Text, Checkbox, Popover, StaticUrl } from '@deriv/components';
+import { Icon, Text, Popover } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
-import { isMobile } from '@deriv/shared';
 import classNames from 'classnames';
 import { jurisdiction_contents } from 'Constants/jurisdiction-contents';
 import RootStore from 'Stores/index';
@@ -601,62 +600,6 @@ const JurisdictionModalContent = ({
         );
     };
 
-    // const dbvi_company_names: { [key: string]: { [key: string]: string } } = {
-    //     bvi: { name: 'Deriv (BVI) Ltd', tnc_url: 'tnc/deriv-(bvi)-ltd.pdf' },
-    //     labuan: { name: 'Deriv (FX) Ltd', tnc_url: 'tnc/deriv-(fx)-ltd.pdf' },
-    //     maltainvest: {
-    //         name: 'Deriv Investments (Europe) Limited',
-    //         tnc_url: 'tnc/deriv-investments-(europe)-limited.pdf',
-    //     },
-    //     vanuatu: { name: 'Deriv (V) Ltd', tnc_url: 'tnc/general-terms.pdf' },
-    // };
-
-    // const getCheckboxLabel = () => (
-    //     <Text as='p' align={!isMobile() ? 'center' : ''} size='xs' line_height='xs'>
-    //         <Localize
-    //             i18n_default_text="I confirm and accept {{company}} 's <0>Terms and Conditions</0>"
-    //             values={{ company: dbvi_company_names[jurisdiction_selected_shortcode].name }}
-    //             components={[
-    //                 <StaticUrl
-    //                     key={0}
-    //                     className='link'
-    //                     href={dbvi_company_names[jurisdiction_selected_shortcode].tnc_url}
-    //                 />,
-    //             ]}
-    //         />
-    //     </Text>
-    // );
-
-    // const ModalCheckbox = ({
-    //     onCheck,
-    //     is_checked,
-    // }: {
-    //     onCheck: React.Dispatch<React.SetStateAction<boolean>>;
-    //     is_checked: boolean;
-    // }) => (
-    //     <div className={`${card_classname}__jurisdiction-checkbox`}>
-    //         <Checkbox onChange={() => onCheck(!checked)} value={is_checked} label={getCheckboxLabel()} />
-    //     </div>
-    // // );
-
-    // const showCheckBox = () => {
-    //     if (jurisdiction_selected_shortcode) {
-    //         if (jurisdiction_selected_shortcode === 'svg') {
-    //             return false;
-    //         } else if (jurisdiction_selected_shortcode === 'vanuatu' && poa_verified && poi_verified_for_vanuatu) {
-    //             return true;
-    //         } else if (
-    //             (jurisdiction_selected_shortcode === 'bvi' || jurisdiction_selected_shortcode === 'labuan') &&
-    //             poa_verified &&
-    //             poi_verified_for_labuan_bvi
-    //         ) {
-    //             return true;
-    //         } else if (is_fully_authenticated && poi_poa_verified) return true;
-    //         return false;
-    //     }
-    //     return false;
-    // };
-
     return (
         <>
             <div className={`${card_classname}__wrapper`}>
@@ -805,7 +748,6 @@ const JurisdictionModalContent = ({
                 onCheck={setChecked}
                 className={`${card_classname}__jurisdiction-checkbox`}
             />
-            {/* {showCheckBox() && <ModalCheckbox is_checked={checked} onCheck={setChecked} />} */}
         </>
     );
 };

e63f0b3e4733c422f06cd5159e85a802f574f1ce
Thisyahlen <thisyahlen@regentmarkets.com>
2022-08-10 18:17:24 +0800

    added dxtrade

--- packages/cfd/src/Components/__tests__/cfd-account-card.spec.js
+++ packages/cfd/src/Components/__tests__/cfd-account-card.spec.js
@@ -27,6 +27,36 @@ const synthetic_descriptor = 'Trade CFDs on our synthetic indices that simulate 
 const financial_descriptor =
     'Trade major (standard & micro-lots) and minor forex, stocks & stock indices, commodities, basket indices, and crypto with high leverage.';
 
+const mt5_acc = {
+    account_type: 'demo',
+    descriptor: '',
+    balance: 10000,
+    platform: 'mt5',
+    display_balance: '10000.00',
+    display_login: '20103240',
+    email: 'name@domain.com',
+    group: 'demo\\p01_ts02\\financial\\svg_std_usd',
+    landing_company_short: 'svg',
+    leverage: 1000,
+    login: 'MTD20103240',
+    market_type: 'financial',
+};
+
+const derivx_acc = {
+    account_type: 'demo',
+    descriptor: '',
+    balance: 10000,
+    platform: 'dxtrade',
+    display_balance: '10000.00',
+    display_login: '20103240',
+    email: 'name@domain.com',
+    group: 'demo\\p01_ts02\\financial\\svg_std_usd',
+    landing_company_short: 'svg',
+    leverage: 1000,
+    login: 'MTD20103240',
+    market_type: 'financial',
+};
+
 describe('CFDAccountCard', () => {
     const props = {
         button_label: 'Top up',
@@ -37,22 +67,7 @@ describe('CFDAccountCard', () => {
             real: '',
         },
         is_hovered: false,
-        existing_accounts_data: [
-            {
-                account_type: 'demo',
-                descriptor: '',
-                balance: 10000,
-                platform: 'mt5',
-                display_balance: '10000.00',
-                display_login: '20103240',
-                email: 'name@domain.com',
-                group: 'demo\\p01_ts02\\financial\\svg_std_usd',
-                landing_company_short: 'svg',
-                leverage: 1000,
-                login: 'MTD20103240',
-                market_type: 'financial',
-            },
-        ],
+        existing_accounts_data: [],
         has_banner: true,
         has_cfd_account: false,
         has_cfd_account_error: false,
@@ -75,10 +90,11 @@ describe('CFDAccountCard', () => {
         should_show_trade_servers: true,
         toggleAccountsDialog: jest.fn(),
         toggleShouldShowRealAccountsList: jest.fn(),
-        isEligibleForMoreDemoMt5Svg: true,
-        isEligibleForMoreRealMt5: true,
+        isEligibleForMoreDemoMt5Svg: {},
+        isEligibleForMoreRealMt5: jest.fn(() => true),
         toggleMT5TradeModal: jest.fn(),
         setMT5TradeAccount: jest.fn(),
+        existing_data: {},
     };
 
     it('should render the component for Demo MT5 Synthetic account ', () => {
@@ -87,7 +103,15 @@ describe('CFDAccountCard', () => {
             category: 'demo',
             platform: 'mt5',
         };
-        render(<CFDAccountCard {...props} type={type} descriptor={synthetic_descriptor} title='Synthetic' />);
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={synthetic_descriptor}
+                title='Synthetic'
+                existing_accounts_data={[mt5_acc]}
+            />
+        );
         expect(screen.getByText(/most popular/i)).toBeInTheDocument();
         expect(screen.getByText(/demo/i)).toBeInTheDocument();
         expect(screen.getByText(/IcMt5CfdPlatform/i)).toBeInTheDocument();
@@ -107,7 +131,15 @@ describe('CFDAccountCard', () => {
             category: 'demo',
             platform: 'mt5',
         };
-        render(<CFDAccountCard {...props} type={type} descriptor={financial_descriptor} title='Financial' />);
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={financial_descriptor}
+                title='Financial'
+                existing_accounts_data={[mt5_acc]}
+            />
+        );
         expect(screen.getByText(/demo/i)).toBeInTheDocument();
         expect(screen.getByText(/IcMt5CfdPlatform/i)).toBeInTheDocument();
         expect(screen.getAllByText(/financial/i)[0]).toBeInTheDocument();
@@ -128,7 +160,15 @@ describe('CFDAccountCard', () => {
             category: 'real',
             platform: 'mt5',
         };
-        render(<CFDAccountCard {...props} type={type} descriptor={synthetic_descriptor} title='Synthetic' />);
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={synthetic_descriptor}
+                title='Synthetic'
+                existing_accounts_data={[mt5_acc]}
+            />
+        );
         expect(screen.getByText(/most popular/i)).toBeInTheDocument();
         expect(screen.getByText(/IcMt5CfdPlatform/i)).toBeInTheDocument();
         expect(screen.getAllByText(/synthetic/i)[0]).toBeInTheDocument();
@@ -148,7 +188,15 @@ describe('CFDAccountCard', () => {
             category: 'real',
             platform: 'mt5',
         };
-        render(<CFDAccountCard {...props} type={type} descriptor={financial_descriptor} title='Financial' />);
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={financial_descriptor}
+                title='Financial'
+                existing_accounts_data={[mt5_acc]}
+            />
+        );
         expect(screen.getByText(/mfsa/i)).toBeInTheDocument();
         expect(screen.getByText(/IcMt5CfdPlatform/i)).toBeInTheDocument();
         expect(screen.getAllByText(/financial/i)[0]).toBeInTheDocument();
@@ -169,7 +217,15 @@ describe('CFDAccountCard', () => {
             category: 'real',
             platform: 'mt5',
         };
-        render(<CFDAccountCard {...props} type={type} descriptor={financial_descriptor} title='Financial' />);
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={financial_descriptor}
+                title='Financial'
+                existing_accounts_data={[mt5_acc]}
+            />
+        );
         fireEvent.click(screen.getByRole('button', { name: /top up/i }));
         expect(props.onClickFund).toHaveBeenCalled();
     });
@@ -180,8 +236,66 @@ describe('CFDAccountCard', () => {
             category: 'real',
             platform: 'mt5',
         };
-        render(<CFDAccountCard {...props} type={type} descriptor={financial_descriptor} title='Financial' />);
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={financial_descriptor}
+                title='Financial'
+                existing_accounts_data={[mt5_acc]}
+            />
+        );
         fireEvent.click(screen.getByRole('button', { name: /trade/i }));
         expect(props.toggleMT5TradeModal).toHaveBeenCalled();
+    });
+
+    it('should render the account card for Demo DerivX Synthetic account ', () => {
+        const type = {
+            type: 'synthetic',
+            category: 'demo',
+            platform: 'dxtrade',
+        };
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={synthetic_descriptor}
+                title='Synthetic'
+                existing_accounts_data={[derivx_acc]}
+                platform='dxtrade'
+                is_eu={false}
+            />
+        );
+        expect(screen.getByText(/most popular/i)).toBeInTheDocument();
+        expect(screen.getByText(/demo/i)).toBeInTheDocument();
+        expect(screen.getByText(/IcDxtradeSyntheticPlatform/i)).toBeInTheDocument();
+        expect(screen.getAllByText(/synthetic/i)[0]).toBeInTheDocument();
+        expect(screen.getByText(/username/i)).toBeInTheDocument();
+        expect(screen.getByText(/mtd20103240/i)).toBeInTheDocument();
+        expect(screen.getByText(/password/i)).toBeInTheDocument();
+        expect(screen.getByText(/•••••••••••••••/i)).toBeInTheDocument();
+        expect(screen.getByText(/icedit/i)).toBeInTheDocument();
+        expect(screen.getByText(/trade on web terminal/i)).toBeInTheDocument();
+    });
+
+    it('should call onPasswordManager when the password change button icon is clicked', () => {
+        const type = {
+            type: 'synthetic',
+            category: 'demo',
+            platform: 'dxtrade',
+        };
+        render(
+            <CFDAccountCard
+                {...props}
+                type={type}
+                descriptor={synthetic_descriptor}
+                title='Synthetic'
+                existing_accounts_data={[derivx_acc]}
+                platform='dxtrade'
+            />
+        );
+        expect(screen.getByText(/icedit/i)).toBeInTheDocument();
+        fireEvent.click(screen.getByText(/icedit/i));
+        expect(props.onPasswordManager).toHaveBeenCalled();
     });
 });

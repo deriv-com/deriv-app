7d9a60b1d6d3d712dac36b5ecbf30a590bd0fbbb
Thisyahlen <thisyahlen@regentmarkets.com>
2022-08-10 11:31:16 +0800

    new tests with new bvi changes

--- packages/cfd/src/Containers/__tests__/compare-accounts-modal.spec.js
+++ packages/cfd/src/Containers/__tests__/compare-accounts-modal.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import CompareAccountsModal from '../compare-accounts-modal';
 
 jest.mock('Stores/connect.js', () => ({
@@ -20,27 +20,69 @@ afterAll(() => {
     document.body.removeChild(modal_root_el);
 });
 
-describe('CompareAccountsModal', () => {
-    const mock_props = {
-        disableApp: jest.fn(),
-        enableApp: jest.fn(),
-        is_compare_accounts_visible: true,
-        is_loading: false,
-        is_logged_in: true,
-        is_eu: false,
-        is_uk: false,
-        is_eu_country: false,
-        is_real_enabled: true,
-        platform: 'mt5',
-        residence: 'id',
-        is_demo_tab: 'false',
-        toggleCompareAccounts: jest.fn(),
-        openPasswordModal: jest.fn(),
-        openDerivRealAccountNeededModal: jest.fn(),
-    };
+jest.mock('../mt5-compare-table-content', () => jest.fn(() => 'MockedMt5CompareTableContent'));
 
-    it('should render the modal', () => {
+describe('CompareAccountsModal', () => {
+    let mock_props;
+    beforeEach(() => {
+        mock_props = {
+            disableApp: jest.fn(),
+            enableApp: jest.fn(),
+            is_compare_accounts_visible: true,
+            is_loading: false,
+            is_logged_in: true,
+            is_eu: false,
+            is_uk: false,
+            is_eu_country: false,
+            is_real_enabled: true,
+            platform: 'mt5',
+            residence: 'id',
+            is_demo_tab: true,
+            toggleCompareAccounts: jest.fn(),
+            openPasswordModal: jest.fn(),
+            openDerivRealAccountNeededModal: jest.fn(),
+        };
+    });
+    it('should render the modal', async () => {
         render(<CompareAccountsModal {...mock_props} />);
-        screen.debug();
+        await waitFor(() => {
+            expect(screen.getByText(/compare available accounts/i)).toBeInTheDocument();
+        });
+    });
+    it('should render the MockedMT5CompareTableContent for mt5', async () => {
+        render(<CompareAccountsModal {...mock_props} />);
+        await waitFor(() => {
+            expect(screen.getByText(/compare available accounts/i)).toBeInTheDocument();
+        });
+        expect(screen.getByText(/mockedMmt5comparetablecontent/i)).toBeInTheDocument();
+    });
+    it('should render the CompareAccountsModal if the platform is dxtrade', async () => {
+        render(<CompareAccountsModal {...mock_props} platform='dxtrade' />);
+        await waitFor(() => {
+            expect(screen.getAllByText(/compare accounts/i)[0]).toBeInTheDocument();
+        });
+        expect(screen.getAllByText(/maximum leverage/i)[0]).toBeInTheDocument();
+    });
+    it('should render the MockedMt5CompareTableContent if the user is not logged in', async () => {
+        render(<CompareAccountsModal {...mock_props} is_logged_in={false} />);
+        await waitFor(() => {
+            expect(screen.getByText(/compare available accounts/i)).toBeInTheDocument();
+        });
+        expect(screen.getByText(/mockedMmt5comparetablecontent/i)).toBeInTheDocument();
+    });
+    it('should call toggleCompareAccountsModal if the compare accounts button is clicked', async () => {
+        render(
+            <CompareAccountsModal
+                {...mock_props}
+                is_compare_accounts_visible={false}
+                is_demo_tab={false}
+                platform='dxtrade'
+            />
+        );
+        await waitFor(() => {
+            expect(screen.getAllByText(/compare accounts/i)[0]).toBeInTheDocument();
+        });
+        fireEvent.click(screen.getByText(/compare accounts/i));
+        expect(mock_props.toggleCompareAccounts).toHaveBeenCalled();
     });
 });

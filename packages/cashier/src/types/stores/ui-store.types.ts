fefda84345a0f1579505020a3e1af31eba4623c3
george-usynin-binary <heorhi@binary.com>
2022-08-15 17:52:06 +0300

    perf(cashier): add types in client, ui stores

--- packages/cashier/src/types/stores/ui-store.types.ts
+++ packages/cashier/src/types/stores/ui-store.types.ts
@@ -2,5 +2,8 @@ export type TUiStore = {
     current_focus: string | null;
     is_dark_mode_on: boolean;
     is_mobile: boolean;
+    disableApp: () => void;
+    enableApp: () => void;
     setCurrentFocus: (value: string) => void;
+    toggleAccountsDialog: () => void;
 };

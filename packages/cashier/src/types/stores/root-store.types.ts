128909c21e72d1192724b5587554578b9d83e836
george-usynin-binary <heorhi@binary.com>
2022-08-16 09:37:27 +0300

    refactor(cashier): refactor root-store declaration into type

--- packages/cashier/src/types/stores/root-store.types.ts
+++ packages/cashier/src/types/stores/root-store.types.ts
@@ -2,15 +2,9 @@ import { TClientStore } from './client-store.types';
 import { TCommonStore } from './common-store.types';
 import { TUiStore } from './ui-store.types';
 
-export class RootStore {
+export type RootStore = {
     client: TClientStore;
     common: TCommonStore;
     modules: any;
     ui: TUiStore;
-    constructor(core_store: { client: TClientStore; common: TCommonStore; modules: any; ui: TUiStore }) {
-        this.client = core_store.client;
-        this.common = core_store.common;
-        this.modules = core_store.modules;
-        this.ui = core_store.ui;
-    }
-}
+};

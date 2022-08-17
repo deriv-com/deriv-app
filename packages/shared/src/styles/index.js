409be6a9c55d8f4ae6fb39794623b5e59ef1d0da
Hamid Yaftian <hamid@re-work.dev>
2022-08-17 09:43:41 +0430

    Enhance bp mixin

--- packages/shared/src/styles/index.js
+++ packages/shared/src/styles/index.js
@@ -1,12 +1,5 @@
 const path = require('path');
 
-const resources = [
-    'constants.scss',
-    'mixins.scss',
-    'fonts.scss',
-    'inline-icons.scss',
-    'devices.scss',
-    'responsive.scss',
-];
+const resources = ['constants.scss', 'mixins.scss', 'fonts.scss', 'inline-icons.scss', 'responsive.scss'];
 
 module.exports = resources.map(file => path.resolve(__dirname, file));

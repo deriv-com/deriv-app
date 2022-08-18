315210a7b7aaa69b6025739f8232c786373b4465
Likhith Kolayari <likhith@regentmarkets.com>
2022-08-18 15:21:57 +0400

    feat: incorporated notification for mobile

--- packages/cashier/src/pages/withdrawal/disable-withdrawal-modal.jsx
+++ packages/cashier/src/pages/withdrawal/disable-withdrawal-modal.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button, Modal, Text } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
+import { isMobile } from '@deriv/shared';
 
 const DisableWithdrawalModal = ({ is_risk_client, onClick }) => {
     return (
-        <Modal is_open={is_risk_client}>
+        <Modal is_open={is_risk_client} small={isMobile()}>
             <Modal.Body>
                 <Text as='p' size='s' align='center'>
                     <Localize i18n_default_text='You can only make deposits at the moment. To enable withdrawals, please complete your financial assessment.' />

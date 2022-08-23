8da4f4a55508638af9e51382249192e7a1f1a337
Likhith Kolayari <likhith@regentmarkets.com>
2022-08-23 17:39:32 +0400

    fix: added configs

--- packages/account/.eslintrc.js
+++ packages/account/.eslintrc.js
@@ -1,7 +1,7 @@
 const webpackConfig = require('./build/webpack.config.js');
 
 module.exports = {
-    extends: ['../../.eslintrc.js', 'plugin:import/typescript'],
+    extends: ['../../.eslintrc.js'],
     settings: {
         'import/resolver': {
             webpack: { config: webpackConfig({}) },

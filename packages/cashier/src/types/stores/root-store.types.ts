b0c0356bd63bef3f0cee9ed1885f2d605804efea
george-usynin-binary <heorhi@binary.com>
2022-08-24 10:13:10 +0300

    refactor(types): refactor typename of RootStore

--- packages/cashier/src/types/stores/root-store.types.ts
+++ packages/cashier/src/types/stores/root-store.types.ts
@@ -2,7 +2,7 @@ import { TClientStore } from './client-store.types';
 import { TCommonStore } from './common-store.types';
 import { TUiStore } from './ui-store.types';
 
-export type TRootStore = {
+export type RootStore = {
     client: TClientStore;
     common: TCommonStore;
     modules: any;

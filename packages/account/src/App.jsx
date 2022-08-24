dbb190d63f31aec1a97aa5d8bc6bd39d68461551
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-08-24 15:19:10 +0300

    yauheni/74383/ converting app in account to functional component

--- packages/account/src/App.jsx
+++ packages/account/src/App.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import { setWebsocket } from '@deriv/shared';
 import Routes from 'Containers/routes.jsx';
 import ResetTradingPassword from 'Containers/reset-trading-password.jsx';
 import { MobxContentProvider } from 'Stores/connect';
 import initStore from 'Stores/init-store';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        const {
-            passthrough: { WS, root_store },
-        } = props;
-        this.root_store = initStore(root_store, WS);
-        setWebsocket(WS);
-    }
+const App = ({ passthrough }) => {
+    const { root_store, WS } = passthrough;
+    initStore(root_store, WS);
 
-    render() {
-        return (
-            <MobxContentProvider store={this.props.passthrough.root_store}>
-                <Routes />
-                <ResetTradingPassword />
-            </MobxContentProvider>
-        );
-    }
-}
+    return (
+        <MobxContentProvider store={root_store}>
+            <Routes />
+            <ResetTradingPassword />
+        </MobxContentProvider>
+    );
+};
 
 export default App;

13cb3def5c052764143b5ac89e860bcb31c61403
Adrienne Rio <adrienne@deriv.com>
2022-08-25 12:07:47 +0800

    Refactored code

--- packages/p2p/src/components/my-ads/edit-ad-form.jsx
+++ packages/p2p/src/components/my-ads/edit-ad-form.jsx
@@ -269,7 +269,8 @@ const EditAdForm = () => {
                                                                             account_currency,
                                                                         }
                                                                     )}
-                                                                    className='p2p-my-ads__form-field p2p-my-ads__form-field--focused'
+                                                                    label_className='p2p-my-ads__form-label--focused'
+                                                                    className='p2p-my-ads__form-field'
                                                                     trailing_icon={
                                                                         <Text
                                                                             color={

cbb5e787c2e56dd33350dcb8a82ca0cd73a2dea2
Likhith Kolayari <98398322+likhith-deriv@users.noreply.github.com>
2022-08-25 11:32:03 +0400

    feat: incorporated notification (#26)
    * fix: resolved issues with index
    
    * ref: refactored trade assessment code
    
    * fix: added unique key
    
    * fix: refactored response pick
    
    * feat: added check to disable financial and trade assessment for not risk clients
    
    * feat: made changes to dialog

--- packages/cashier/src/pages/withdrawal/disable-withdrawal-modal.jsx
+++ packages/cashier/src/pages/withdrawal/disable-withdrawal-modal.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Button, Modal, Text } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
-import { isMobile } from '@deriv/shared';
 
 const DisableWithdrawalModal = ({ is_risk_client, onClick }) => {
     return (
-        <Modal is_open={is_risk_client} small={isMobile()} className='center-risk-modal'>
+        <Modal is_open={is_risk_client} small className='center-risk-modal' title={localize('Financial Assessment')}>
             <Modal.Body>
                 <Text as='p' size='s' align='center'>
                     <Localize i18n_default_text='You can only make deposits at the moment. To enable withdrawals, please complete your financial assessment.' />

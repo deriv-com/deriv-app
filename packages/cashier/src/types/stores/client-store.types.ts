b292287a3dfac98711eca67f5ddd249528d01f55
Hamid Yaftian <hamid@re-work.dev>
2022-08-28 15:51:21 +0430

    Migrate Deposit to TS

--- packages/cashier/src/types/stores/client-store.types.ts
+++ packages/cashier/src/types/stores/client-store.types.ts
@@ -6,21 +6,31 @@ export type TClientStore = {
     account_status: GetAccountStatus;
     accounts: { [k: string]: TAccount };
     balance?: string;
+    can_change_fiat_currency: boolean;
     currency: string;
     current_currency_type?: string;
     current_fiat_currency?: string;
     is_deposit_lock: boolean;
+    is_eu: boolean;
+    is_financial_account: boolean;
+    is_financial_information_incomplete: boolean;
     is_identity_verification_needed: boolean;
     is_logging_in: boolean;
     is_switching: boolean;
+    is_tnc_needed: boolean;
+    is_trading_experience_incomplete: boolean;
     is_virtual: boolean;
     is_withdrawal_lock: boolean;
+    landing_company_shortcode: string;
     local_currency_config: {
         currency: string;
         decimal_places?: number;
     };
     loginid?: string;
     residence: string;
+    standpoint: {
+        iom: string;
+    };
     verification_code: {
         payment_withdraw: string;
     };

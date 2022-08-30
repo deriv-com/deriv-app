4b56385e7f9abaea58d92ff34236d46abec2064a
george-usynin-binary <heorhi@binary.com>
2022-08-30 16:55:25 +0300

    perf(cashier withdraw): convert withdrawal to TS

--- packages/cashier/src/pages/withdrawal/__tests__/withdrawal.spec.tsx
+++ packages/cashier/src/pages/withdrawal/__tests__/withdrawal.spec.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/display-name */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Withdrawal from '../withdrawal';

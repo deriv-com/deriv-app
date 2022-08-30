a09ac6779113821c0b03d1ced4d37843964aa274
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-08-30 18:21:00 +0300

    yauheni/74104/ts migration for account DemoMessage component

--- packages/account/src/Components/demo-message/demo-message.tsx
+++ packages/account/src/Components/demo-message/demo-message.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { localize } from '@deriv/translations';
 import IconWithMessage from 'Components/icon-with-message';
 
-const DemoMessage = ({ has_demo_icon, has_button }) => (
+type TDemoMessage = {
+    has_demo_icon?: boolean;
+    has_button?: boolean;
+};
+
+const DemoMessage = ({ has_demo_icon, has_button }: TDemoMessage) => (
     <IconWithMessage
         icon={has_demo_icon ? 'IcPoaLockDemo' : 'IcPoaLock'}
         message={localize('This feature is not available for demo accounts.')}
         has_button={has_button}
     />
 );
-
-DemoMessage.propTypes = {
-    has_button: PropTypes.bool,
-    has_demo_icon: PropTypes.bool,
-    full_width: PropTypes.bool,
-};
 
 export default DemoMessage;

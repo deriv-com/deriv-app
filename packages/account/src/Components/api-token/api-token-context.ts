a3bb21665a32dc50aff87ddd9b897602de9edcf5
Likhith Kolayari <likhith@regentmarkets.com>
2022-08-31 12:49:00 +0400

    ref: Migrated context

--- packages/account/src/Components/api-token/api-token-context.ts
+++ packages/account/src/Components/api-token/api-token-context.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-const ApiTokenContext = React.createContext({});
+const ApiTokenContext = React.createContext<Partial<import('Types').TApiContext>>({});
 
 export default ApiTokenContext;

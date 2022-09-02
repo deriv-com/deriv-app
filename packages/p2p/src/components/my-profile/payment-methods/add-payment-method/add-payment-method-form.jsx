ae293c6d30b3991c747efa7a587e5c9e0858d15e
Jim Daniels Wasswa <jim@deriv.com>
2022-09-02 14:18:05 +0800

    fix: fix failing test

--- packages/p2p/src/components/my-profile/payment-methods/add-payment-method/add-payment-method-form.jsx
+++ packages/p2p/src/components/my-profile/payment-methods/add-payment-method/add-payment-method-form.jsx
@@ -65,8 +65,12 @@ const AddPaymentMethodForm = ({ formik_ref, should_show_separated_footer = false
                                         />
                                     )}
                                 </Field>
-                                {my_profile_store.selected_payment_method_fields?.map((payment_method_field) => (
-                                    <Field name={payment_method_field[0]} id={payment_method_field[0]} key={payment_method_field[0]}>
+                                {my_profile_store.selected_payment_method_fields?.map(payment_method_field => (
+                                    <Field
+                                        name={payment_method_field[0]}
+                                        id={payment_method_field[0]}
+                                        key={payment_method_field[0]}
+                                    >
                                         {({ field }) => (
                                             <Input
                                                 {...field}

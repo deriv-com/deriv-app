9f91fc53074150e46660188303c8658f00d3a5ca
hirad-rework <hirad@re-work.dev>
2022-09-04 16:02:35 +0430

    Finished total-assets component

---
+++ packages/appstore/src/components/total-assets/total-assets.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Popover, Text } from '@deriv/components';
+import { localize } from '@deriv/translations';
+import { formatMoney } from '@deriv/shared';
+
+type TTotalAssets = {
+    amount: string;
+    currency: string;
+    platform: 'real' | 'demo';
+};
+
+const TotalAssets = ({ amount, currency, platform }: TTotalAssets) => {
+    return (
+        <div className='total-assets'>
+            <Text size='sm' weight='bold' color='green' className='total-assets-amount'>
+                {formatMoney(currency, amount, true)}
+            </Text>
+            <Text size='sm' weight='bold' color='green' className='total-assets-currency'>
+                {currency}
+            </Text>
+            <div>
+                <Popover
+                    alignment='left'
+                    className='total-assets-tooltip'
+                    classNameBubble='total-assets-tooltip--msg'
+                    icon='info'
+                    disable_message_icon
+                    is_bubble_hover_enabled
+                    message={localize(`Total assets in your Options, DMT5 and Deriv X ${platform} accounts`)}
+                    zIndex={9999}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default TotalAssets;
--- packages/appstore/src/modules/trading-hub/index.tsx
+++ packages/appstore/src/modules/trading-hub/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import TotalAssets from 'Components/total-assets';
 
 const TradingHub = () => {
-    return <div className='trading-hub'>Trading Hub</div>;
+    return (
+        <div className='trading-hub'>
+            <TotalAssets amount={'10000.0'} currency={'USD'} platform={'real'} />
+        </div>
+    );
 };
 
 export default TradingHub;

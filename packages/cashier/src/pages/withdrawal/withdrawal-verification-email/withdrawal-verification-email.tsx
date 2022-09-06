9513bbe571e347a86a48060fedebe1c5e8ba16ec
Farzin Mirzaie <farzin@firstsource.tech>
2022-09-06 10:49:13 +0430

    refactor(cashier): :recycle: refactor `WithdrawalVerificationEmail` component with `useVerifyEmail` hook

---
+++ packages/cashier/src/pages/withdrawal/withdrawal-verification-email/withdrawal-verification-email.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { MobileWrapper } from '@deriv/components';
+import { isCryptocurrency } from '@deriv/shared';
+import { localize, Localize } from '@deriv/translations';
+import RecentTransaction from 'Components/recent-transaction';
+import EmailVerificationEmptyState from 'Components/email-verification-empty-state';
+import EmptyState from 'Components/empty-state';
+import { useStore, useVerifyEmail } from '../../../hooks';
+
+const WithdrawalVerificationEmail = () => {
+    const verify = useVerifyEmail('payment_withdraw');
+    const { client, modules } = useStore();
+    const { transaction_history } = modules.cashier;
+
+    React.useEffect(() => {
+        transaction_history.onMount();
+    }, [transaction_history]);
+
+    if (verify.hasBeenSent) {
+        return <EmailVerificationEmptyState type={'payment_withdraw'} />;
+    }
+
+    return (
+        <>
+            <EmptyState
+                icon='IcCashierAuthenticate'
+                title={localize('Please help us verify your withdrawal request.')}
+                description={
+                    <>
+                        <Localize i18n_default_text="Hit the button below and we'll send you an email with a link. Click that link to verify your withdrawal request." />
+                        <br />
+                        <br />
+                        <Localize i18n_default_text='This is to protect your account from unauthorised withdrawals.' />
+                    </>
+                }
+                action={{
+                    label: localize('Send email'),
+                    onClick: verify.send,
+                }}
+            />
+            <MobileWrapper>
+                {isCryptocurrency(client.currency) && transaction_history.crypto_transactions?.length ? (
+                    <RecentTransaction />
+                ) : null}
+            </MobileWrapper>
+        </>
+    );
+};
+
+export default observer(WithdrawalVerificationEmail);

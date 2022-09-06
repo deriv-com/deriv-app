503e77ecc4cfc7097bbef7396d2dc08a72fdcc65
George Usynin <103181646+george-usynin-binary@users.noreply.github.com>
2022-09-06 14:11:34 +0300

    george / rm65041 / migrate p2p-cashier component to TS (#6177)
    * refactor(p2p-cashier): migrate p2p-cashier component to TS
    
    * commit
    
    * perf(types): add types for notification store
    
    * perf(types): add types for ui store
    
    * perf(types): convert p2p-cashier to TS
    
    * refactor(types): change RootStore type to TRootStore
    
    * test(p2p-cashier): test fix (change wrapper order fro p2p-cashier component)
    
    * refactor(types): refactor current_focus type
    
    * test(p2p-cashier): remove eslint disables

--- packages/cashier/src/types/stores/index.ts
+++ packages/cashier/src/types/stores/index.ts
@@ -1,4 +1,5 @@
 export * from './client-store.types';
 export * from './common-store.types';
+export * from './notification-store.types';
 export * from './root-store.types';
 export * from './ui-store.types';

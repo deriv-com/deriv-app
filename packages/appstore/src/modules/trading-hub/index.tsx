25a460b775090cd8721946fecdb26825e9adc052
hirad-rework <hirad@re-work.dev>
2022-09-06 15:24:38 +0430

    Added todo comment

--- packages/appstore/src/modules/trading-hub/index.tsx
+++ packages/appstore/src/modules/trading-hub/index.tsx
@@ -3,6 +3,7 @@ import Onboarding from 'Components/onboarding';
 import { trading_hub_contents } from 'Constants/trading-hub-content';
 
 const TradingHub = () => {
+    /*TODO: We need to show this component whenever user click on tour guide button*/
     return <Onboarding contents={trading_hub_contents} />;
 };
 

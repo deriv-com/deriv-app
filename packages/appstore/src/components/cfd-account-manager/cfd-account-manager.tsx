d8ad3d0fdd5ba1fb7bc4ea4381d64cf54ad2ed02
hirad-rework <hirad@re-work.dev>
2022-09-07 16:57:21 +0430

    Changed some of the namings to follow the naming convention of scss

--- packages/appstore/src/components/cfd-account-manager/cfd-account-manager.tsx
+++ packages/appstore/src/components/cfd-account-manager/cfd-account-manager.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Icon, Button, Text } from '@deriv/components';
 import { formatMoney } from '@deriv/shared';
+import { Localize } from '@deriv/translations';
 
 type TCFDAccountManager = {
     appname: string;
@@ -23,24 +24,24 @@ const CFDAccountManager = ({
 }: TCFDAccountManager) => {
     return (
         <div className='cfd-account-manager'>
-            <div className='cfd-account-manager-icon'>
+            <div className='cfd-account-manager__icon'>
                 {platform === 'financial' ? (
                     <Icon icon='IcAppstoreFinancial' size={64} />
                 ) : (
                     <Icon icon='IcAppstoreDerived' size={64} />
                 )}
             </div>
-            <div className='cfd-account-manager-details'>
+            <div className='cfd-account-manager__details'>
                 <Text size='xs'>{appname}</Text>
                 <Text size='xs'>{`${formatMoney(currency, amount, true)} ${currency}`}</Text>
                 <Text size='xs'>{loginid}</Text>
             </div>
             <div className='cfd-account-manager-buttons'>
-                <Button primary className='cfd-account-manager-buttons-topup' onClick={onClickTopUp}>
-                    Top-up
+                <Button primary className='cfd-account-manager__buttons-topup' onClick={onClickTopUp}>
+                    <Localize i18n_default_text='Top-up' />
                 </Button>
                 <Button secondary onClick={onClickTrade}>
-                    Trade
+                    <Localize i18n_default_text='Trade' />
                 </Button>
             </div>
         </div>

0724417666b5d42972ea76f127c593b1c4904150
Jim Daniels Wasswa <jim@deriv.com>
2022-09-09 13:06:12 +0800

    chore: chanage interface to readonly type

--- packages/components/src/components/types/icons.types.ts
+++ packages/components/src/components/types/icons.types.ts
@@ -1,21 +1,22 @@
 import { TGenericObjectType } from './common.types';
 
-export interface IIconsManifest {
+export type IIconsManifest = Readonly<{
     // If a new icon category is added, be sure to add it here as well. This is part of the fix for ts(7053)
-    readonly brand: string;
-    readonly cashier: string;
-    readonly common: string;
-    readonly contract: string;
-    readonly currency: string;
-    readonly dxtrade: string;
-    readonly flag: string;
-    readonly mt5: string;
-    readonly option: string;
-    readonly stock: string;
-    readonly tradetype: string;
-    readonly underlying: string;
-    readonly wallet: string;
-}
+    brand: string;
+    cashier: string;
+    common: string;
+    contract: string;
+    currency: string;
+    dxtrade: string;
+    flag: string;
+    mt5: string;
+    option: string;
+    stock: string;
+    tradetype: string;
+    underlying: string;
+    wallet: string;
+}>;
+
 export type TIconProps = {
     className?: string | TGenericObjectType;
     color?: string;

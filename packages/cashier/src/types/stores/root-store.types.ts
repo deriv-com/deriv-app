7ae46a8804a3d743b5113ad381507e227953b674
Hamid Yaftian <hamid@re-work.dev>
2022-09-12 15:04:04 +0430

    Rename RootStore to TRootStore

--- packages/cashier/src/types/stores/root-store.types.ts
+++ packages/cashier/src/types/stores/root-store.types.ts
@@ -2,11 +2,9 @@ import { TClientStore } from './client-store.types';
 import { TCommonStore } from './common-store.types';
 import { TUiStore } from './ui-store.types';
 
-export type RootStore = {
+export type TRootStore = {
     client: TClientStore;
     common: TCommonStore;
     modules: any;
     ui: TUiStore;
 };
-
-export type TRootStore = RootStore;

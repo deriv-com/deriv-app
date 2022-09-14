a519030843f2335f60a5ef2f9aefadf398f54eec
Sandeep <sandeep@deriv.com>
2022-09-14 17:45:14 +0800

    refactored db dashboard tab component

--- packages/bot-web-ui/src/components/dashboard/dashboard-components/dashboard.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-components/dashboard.tsx
@@ -6,12 +6,18 @@ import { Text } from '@deriv/components';
 const Dashboard = () => {
     return (
         <div className='dc-tabs__content-group'>
-            <span className='heading'>{localize('Load or build your bot')}</span>
-            <Text color='prominent' line_height='s' size='s'>
-                {localize(
-                    'Import bot from your computer or Google Drive, build it from scratch, or start with a quick strategy.'
-                )}
-            </Text>
+            <div className='dc-tabs__content-group__header'>
+                <Text color='prominent' line_height='xxl' size='sm' weight='bold'>
+                    {localize('Load or build your bot')}
+                </Text>
+            </div>
+            <div className='dc-tabs__content-group__description'>
+                <Text color='prominent' line_height='s' size='s'>
+                    {localize(
+                        'Import bot from your computer or Google Drive, build it from scratch, or start with a quick strategy.'
+                    )}
+                </Text>
+            </div>
             <Cards />
         </div>
     );

dc5133b5d366cc92c00b61b63687fcb62fdf1b15
Aizad Ridzo <aizad@deriv.com>
2022-09-14 16:58:55 +0800

    resolved: remove curly brackets and return statment and replaced with regular brackets. Remove size prop in Text

--- packages/cashier/src/pages/withdrawal/disable-withdrawal-modal.jsx
+++ packages/cashier/src/pages/withdrawal/disable-withdrawal-modal.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { Button, Modal, Text } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
 
-const DisableWithdrawalModal = ({ is_risk_client, onClick }) => {
-    return (
-        <Modal is_open={is_risk_client} small className='center-risk-modal' title={localize('Financial Assessment')}>
-            <Modal.Body>
-                <Text as='p' size='s' align='center'>
-                    <Localize i18n_default_text='You can only make deposits at the moment. To enable withdrawals, please complete your financial assessment.' />
-                </Text>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button type='button' large text={localize('Start assessment')} primary onClick={onClick} />
-            </Modal.Footer>
-        </Modal>
-    );
-};
+const DisableWithdrawalModal = ({ is_risk_client, onClick }) => (
+    <Modal is_open={is_risk_client} small className='center-risk-modal' title={localize('Financial Assessment')}>
+        <Modal.Body>
+            <Text as='p' align='center'>
+                <Localize i18n_default_text='You can only make deposits at the moment. To enable withdrawals, please complete your financial assessment.' />
+            </Text>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button type='button' large text={localize('Start assessment')} primary onClick={onClick} />
+        </Modal.Footer>
+    </Modal>
+);
 
 export default DisableWithdrawalModal;

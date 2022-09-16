881b2fe522780fe4b27887d2a0ecf689fcc6e3cf
Hamid Yaftian <hamid@re-work.dev>
2022-09-17 03:15:11 +0430

    Update AccoutSwitcher

--- packages/core/src/App/Containers/AccountSwitcher/account-switcher.jsx
+++ packages/core/src/App/Containers/AccountSwitcher/account-switcher.jsx
@@ -185,7 +185,10 @@ const AccountSwitcher = props => {
     };
 
     const openDXTradeDemoAccount = account_type => {
-        sessionStorage.setItem('open_cfd_account_type', `demo.${CFD_PLATFORMS.DXTRADE}.${account_type}`);
+        sessionStorage.setItem(
+            'open_cfd_account_type',
+            `demo.${CFD_PLATFORMS.DXTRADE}.${account_type === 'dxtrade' ? 'all' : account_type}`
+        );
         redirectToDXTradeDemo();
     };
 
@@ -220,7 +223,17 @@ const AccountSwitcher = props => {
     // * we should map them to landing_company:
     // mt_financial_company: { financial: {}, financial_stp: {}, swap_free: {} }
     // mt_gaming_company: { financial: {}, swap_free: {} }
-    const getRemainingAccounts = (existing_cfd_accounts, platform, is_eu, getIsEligibleForMoreAccounts) => {
+    const getRemainingAccounts = (existing_cfd_accounts, platform, is_eu, is_demo, getIsEligibleForMoreAccounts) => {
+        const all_config = getCFDConfig(
+            'all',
+            props.landing_companies?.dxtrade_all_company,
+            existing_cfd_accounts,
+            props.mt5_trading_servers,
+            platform,
+            is_eu,
+            props.trading_platform_available_accounts,
+            getIsEligibleForMoreAccounts
+        );
         const gaming_config = getCFDConfig(
             'gaming',
             platform === CFD_PLATFORMS.MT5
@@ -250,6 +263,11 @@ const AccountSwitcher = props => {
         if (is_eu) {
             return [...financial_config];
         }
+
+        // TODO: change this condition before real release
+        if (is_demo && platform === CFD_PLATFORMS.DXTRADE) {
+            return [...all_config];
+        }
         return [...gaming_config, ...financial_config];
     };
 
@@ -276,11 +294,17 @@ const AccountSwitcher = props => {
     };
 
     const getRemainingDemoMT5 = () => {
-        return getRemainingAccounts(getDemoMT5(), CFD_PLATFORMS.MT5, props.is_eu, props.isEligibleForMoreDemoMt5Svg);
+        return getRemainingAccounts(
+            getDemoMT5(),
+            CFD_PLATFORMS.MT5,
+            props.is_eu,
+            true,
+            props.isEligibleForMoreDemoMt5Svg
+        );
     };
 
     const getRemainingDemoDXTrade = () => {
-        return getRemainingAccounts(getDemoDXTrade(), CFD_PLATFORMS.DXTRADE, props.is_eu);
+        return getRemainingAccounts(getDemoDXTrade(), CFD_PLATFORMS.DXTRADE, props.is_eu, true);
     };
 
     const getRealMT5 = () => {
@@ -299,7 +323,13 @@ const AccountSwitcher = props => {
     };
 
     const getRemainingRealMT5 = () => {
-        return getRemainingAccounts(getRealMT5(), CFD_PLATFORMS.MT5, props.is_eu, props.isEligibleForMoreRealMt5);
+        return getRemainingAccounts(
+            getRealMT5(),
+            CFD_PLATFORMS.MT5,
+            props.is_eu,
+            false,
+            props.isEligibleForMoreRealMt5
+        );
     };
 
     const getRemainingRealDXTrade = () => {
@@ -621,6 +651,7 @@ const AccountSwitcher = props => {
                             )}
                             {getRemainingDemoDXTrade().map(account => (
                                 <div key={account.title} className='acc-switcher__new-account'>
+                                    {account.type}
                                     <Icon icon={`IcDxtrade-${account.icon}`} size={24} />
                                     <Text size='xs' color='general' className='acc-switcher__new-account-text'>
                                         {account.title}

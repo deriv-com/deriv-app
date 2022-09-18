4287a649035ffa454d35593e59da9e763d5bef9e
Hamid Yaftian <hamid@re-work.dev>
2022-09-18 18:11:09 +0430

    Add WebSocket calls types

--- packages/cashier/src/types/shared/websocket.types.ts
+++ packages/cashier/src/types/shared/websocket.types.ts
@@ -1,3 +1,5 @@
+import { Authorize, DetailsOfEachMT5Loginid, Balance } from '@deriv/api-types';
+
 export type TServerError = {
     code: string;
     message: string;
@@ -6,9 +8,29 @@ export type TServerError = {
 
 type TWebSocketCall = {
     cashier: (action: any, parameters: any) => Promise<any>;
+    getAccountStatus: () => Promise<any>;
+    transferBetweenAccounts: (
+        account_from?: string,
+        account_to?: string,
+        currency?: string,
+        amount?: number
+    ) => Promise<any>;
 };
 
 export type TWebSocket = {
-    send: (obj: any) => Promise<any>;
     authorized: TWebSocketCall;
+    balanceAll: () => Promise<Balance>;
+    mt5LoginList: () => {
+        mt5_login_list: DetailsOfEachMT5Loginid[];
+    };
+    send: (obj: any) => Promise<any>;
+    storage: {
+        mt5LoginList: () => {
+            mt5_login_list: DetailsOfEachMT5Loginid[];
+        };
+    };
+    tradingPlatformAccountsList: (platform: string) => {
+        trading_platform_accounts: DetailsOfEachMT5Loginid[];
+    };
+    wait: (value: string) => Promise<any>;
 };

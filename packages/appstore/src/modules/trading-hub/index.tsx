98c54ba1ad18826780abeeccf7cad3243f9d830c
Thisyahlen <thisyahlen@regentmarkets.com>
2022-09-20 11:57:57 +0800

    removed index changes

--- packages/appstore/src/modules/trading-hub/index.tsx
+++ packages/appstore/src/modules/trading-hub/index.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-// import Onboarding from 'Components/onboarding';
-// import { trading_hub_contents } from 'Constants/trading-hub-content';
-import AddOptionsAccount from '../../components/add-options-account/add-options-account';
+import Onboarding from 'Components/onboarding';
+import { trading_hub_contents } from 'Constants/trading-hub-content';
 
 const TradingHub = () => {
     /*TODO: We need to show this component whenever user click on tour guide button*/
-    // return (<Onboarding contents={trading_hub_contents} />;)
-    return <AddOptionsAccount />;
+    return <Onboarding contents={trading_hub_contents} />;
 };
 
 export default TradingHub;

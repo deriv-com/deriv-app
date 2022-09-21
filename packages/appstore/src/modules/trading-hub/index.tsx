dd51e7717b492ea2e132f51792e99a728600b472
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-09-21 17:35:57 +0430

    remove redundant import

--- packages/appstore/src/modules/trading-hub/index.tsx
+++ packages/appstore/src/modules/trading-hub/index.tsx
@@ -6,7 +6,6 @@ const TradingHub = () => {
         <div className='trading-hub'>
             Trading Hub
             <CFDAccounts account_type='real' />
-            {/* <AccountManager type='options' appname='options account' disabled={false} has_account={false} description={'Get a real Options account, start trading and manage your funds.'}/> */}
         </div>
     );
 };

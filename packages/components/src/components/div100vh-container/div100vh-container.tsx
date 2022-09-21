91e58192196b068c587ff8b362fa587c09b1f410
iman <iman@firstsource.tech>
2022-09-21 15:01:13 +0430

    fix: resolve pr comments

--- packages/components/src/components/div100vh-container/div100vh-container.tsx
+++ packages/components/src/components/div100vh-container/div100vh-container.tsx
@@ -1,15 +1,9 @@
 import React, { ReactNode } from 'react';
 import { use100vh } from 'react-div-100vh';
 
-/* Div100vh is workaround for getting accurate height of 100vh from browsers on mobile,
+/* use100vh() hook is workaround for getting accurate height of 100vh from browsers on mobile (it returns height as a number in browser and undefined in Node),
     because using normal css vh is not returning correct screen height */
-/* To adjust max-height using calculation when using height: auto (or no rvh units), pass style props and use rvh unit instead vh,
-        e.g - style={{ maxHeight: calc(100rvh - 100px )}}
-    */
-/* To adjust height using calculation, pass style props and use rvh unit instead vh,
-    e.g - style={{ height: calc(100rvh - 100px )}}
-*/
-/* To manually remove rvh calculation and revert to default browser calculation use is_disabled */
+/* To manually remove use100vh() calculation and revert to default browser calculation use is_disabled */
 /* To bypass usage of component altogether, use is_bypassed */
 
 type TDiv100vhContainerProps = {
@@ -33,11 +27,11 @@ const Div100vhContainer = ({
     max_autoheight_offset,
 }: TDiv100vhContainerProps) => {
     const screen_vertical_height = use100vh();
-    const height = screen_vertical_height ? `${screen_vertical_height}px` : '100rvh';
+    const height = screen_vertical_height ? `${screen_vertical_height}px` : '100vh';
     const height_rule = height_offset ? `calc(${height} - ${height_offset})` : `calc(${height})`;
 
     const height_style = max_autoheight_offset
-        ? { maxHeight: `calc(100rvh - ${max_autoheight_offset})` }
+        ? { maxHeight: `calc(100vh - ${max_autoheight_offset})` }
         : { height: height_rule };
 
     if (is_bypassed) return children;

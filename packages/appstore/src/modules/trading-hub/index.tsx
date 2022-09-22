d8ddd4612d5415c9b094de7a5a4468872017ccca
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-09-22 10:28:41 +0330

    clean the code

--- packages/appstore/src/modules/trading-hub/index.tsx
+++ packages/appstore/src/modules/trading-hub/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import CFDAccounts from 'Components/CFDs';
-// import  AccountManager from 'Components/account-manager'
+
 const TradingHub = () => {
     return (
         <div className='trading-hub'>
--- packages/core/src/Stores/cfd-account-store.js
+++ packages/core/src/Stores/cfd-account-store.js
@@ -13,14 +13,14 @@ export default class CFDAccountStore extends BaseStore {
         const list = {};
 
         this.root_store.client.mt5_login_list.forEach(account => {
-            // e.g. mt5.real.financial_stp
+            // e.g. mt5.real.financial
             list[getAccountListKey(account, CFD_PLATFORMS.MT5, account.landing_company_short)] = {
                 ...account,
             };
         });
 
         this.root_store.client.dxtrade_accounts_list.forEach(account => {
-            // e.g. dxtrade.real.financial_stp
+            // e.g. dxtrade.real.financial
             list[getAccountListKey(account, CFD_PLATFORMS.DXTRADE)] = {
                 ...account,
             };

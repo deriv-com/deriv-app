7ff4f52ca89e2be5e2df9049bec3d7a7d7bd9ba7
Likhith Kolayari <likhith@regentmarkets.com>
2022-09-23 11:27:03 +0400

    feat: :sparkles: moved employment field to personal details incase of MF clients

--- packages/account/src/Sections/Profile/PersonalDetails/personal-details.jsx
+++ packages/account/src/Sections/Profile/PersonalDetails/personal-details.jsx
@@ -9,6 +9,7 @@ import {
     FormSubmitErrorMessage,
     Input,
     DesktopWrapper,
+    Dropdown,
     Loading,
     MobileWrapper,
     SelectNative,
@@ -43,6 +44,7 @@ import FormBody from 'Components/form-body';
 import FormBodySection from 'Components/form-body-section';
 import FormSubHeader from 'Components/form-sub-header';
 import LoadErrorMessage from 'Components/load-error-message';
+import { getEmploymentStatusList } from 'Sections/Assessment/FinancialAssessment/financial-information-list.js';
 
 const validate = (errors, values) => (fn, arr, err_msg) => {
     arr.forEach(field => {
@@ -286,7 +288,7 @@ export const PersonalDetailsForm = ({
             required_fields.push('citizen');
         }
         if (is_mf) {
-            const required_tax_fields = ['tax_residence', 'tax_identification_number'];
+            const required_tax_fields = ['tax_residence', 'tax_identification_number', 'employment_status'];
             required_fields.push(...required_tax_fields);
         }
 
@@ -446,6 +448,7 @@ export const PersonalDetailsForm = ({
                 'allow_copiers',
                 !is_mf && 'tax_residence',
                 !is_mf && 'tax_identification_number',
+                !is_mf && 'employment_status',
                 'client_tnc_status',
                 'country_code',
                 'has_secret_answer',
@@ -524,6 +527,7 @@ export const PersonalDetailsForm = ({
             form_initial_values.tax_residence = '';
         }
         if (!form_initial_values.tax_identification_number) form_initial_values.tax_identification_number = '';
+        if (!form_initial_values.employment_status) form_initial_values.employment_status = '';
     }
 
     return (
@@ -932,6 +936,42 @@ export const PersonalDetailsForm = ({
                                                             disabled={!isChangeableField('tax_identification_number')}
                                                             required
                                                         />
+                                                    </fieldset>
+                                                )}
+                                                {'employment_status' in values && (
+                                                    <fieldset className='account-form__fieldset'>
+                                                        <DesktopWrapper>
+                                                            <Dropdown
+                                                                placeholder={localize('Employment status')}
+                                                                is_align_text_left
+                                                                name='employment_status'
+                                                                list={getEmploymentStatusList()}
+                                                                value={values.employment_status}
+                                                                onChange={handleChange}
+                                                                handleBlur={handleBlur}
+                                                                error={
+                                                                    touched.employment_status &&
+                                                                    errors.employment_status
+                                                                }
+                                                            />
+                                                        </DesktopWrapper>
+                                                        <MobileWrapper>
+                                                            <SelectNative
+                                                                placeholder={localize('Please select')}
+                                                                name='employment_status'
+                                                                label={localize('Employment status')}
+                                                                list_items={getEmploymentStatusList()}
+                                                                value={values.employment_status}
+                                                                error={
+                                                                    touched.employment_status &&
+                                                                    errors.employment_status
+                                                                }
+                                                                onChange={e => {
+                                                                    setFieldTouched('employment_status', true);
+                                                                    handleChange(e);
+                                                                }}
+                                                            />
+                                                        </MobileWrapper>
                                                     </fieldset>
                                                 )}
                                             </FormBodySection>

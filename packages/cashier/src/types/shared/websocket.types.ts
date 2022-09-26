42aa6db4c6818c8e390f4cc61fbf6dd787300178
Hamid <hamid@re-work.dev>
2022-09-26 12:29:53 +0330

    hamid/73309/ts-migration-account-transfer-store (#6520)
    * Migrate Account Transfer Store to TS
    
    * WIP: rename test files extension
    
    * WIP: Resolve warnings
    
    * WIP: Resolve test file errors
    
    * Add WebSocket calls types
    
    * WIP: Convert to TS
    
    * Change Number to +
    
    * Fix to_select list
    
    * Fix tests errors
    
    * WIP: Fix types
    
    * WIP: Fix types
    
    * WIP: Fix types
    
    * Fix types
    
    * Remove ignore eslint warning
    
    * Enhance types
    
    * Add types to the tests
    
    * Add types to the tests
    
    * Remove undefined
    
    * Resolve PR comments
    
    * Resolve PR comments
    
    * Trigger the test

--- packages/cashier/src/types/shared/websocket.types.ts
+++ packages/cashier/src/types/shared/websocket.types.ts
@@ -1,5 +1,46 @@
+import {
+    Balance,
+    CashierInformationRequest,
+    CashierInformationResponse,
+    AccountStatusResponse,
+    TransferBetweenAccountsResponse,
+} from '@deriv/api-types';
+import { TMT5LoginAccount } from 'Types';
+
 export type TServerError = {
     code: string;
     message: string;
     details?: { [key: string]: string };
+};
+
+type TWebSocketCall = {
+    cashier: (
+        action: CashierInformationRequest['cashier'],
+        parameters: Omit<CashierInformationRequest, 'cashier'>
+    ) => Promise<CashierInformationResponse>;
+    getAccountStatus: () => Promise<AccountStatusResponse>;
+    transferBetweenAccounts: (
+        account_from?: string,
+        account_to?: string,
+        currency?: string,
+        amount?: number
+    ) => Promise<TransferBetweenAccountsResponse & { error: TServerError }>;
+};
+
+export type TWebSocket = {
+    authorized: TWebSocketCall;
+    balanceAll: () => Promise<Balance>;
+    mt5LoginList: () => {
+        mt5_login_list: Array<TMT5LoginAccount>;
+    };
+    send: (obj: any) => Promise<any>;
+    storage: {
+        mt5LoginList: () => {
+            mt5_login_list: Array<TMT5LoginAccount>;
+        };
+    };
+    tradingPlatformAccountsList: (platform: string) => {
+        trading_platform_accounts: Array<TMT5LoginAccount>;
+    };
+    wait: (value: string) => Promise<any>;
 };

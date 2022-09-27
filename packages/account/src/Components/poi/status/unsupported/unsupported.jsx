1f5392f68274a6221ab91d118a66c8d3c43e6cf0
Likhith Kolayari <98398322+likhith-deriv@users.noreply.github.com>
2022-09-27 10:55:00 +0400

    fix: display the status for manual upload (#6093)
    * fix: display the status for manual upload
    
    * ref: reverted changes
    
    * fix: incorporated POI status change for manual update
    
    * fix: added null check
    
    * fix: after onfido submission fails multiple times, user must be sent to manual upload
    
    * trigger build
    
    * fix: incorporated review comment
    
    * fix: incorporated review comments
    
    * fix: incorporated review comments
    
    * fix: included a check regd resubmission for Manual upload
    
    * fix: added verified and expired status
    
    Co-authored-by: Carol Sachdeva <58209918+carol-binary@users.noreply.github.com>

--- packages/account/src/Components/poi/status/unsupported/unsupported.jsx
+++ packages/account/src/Components/poi/status/unsupported/unsupported.jsx
@@ -3,9 +3,14 @@ import classNames from 'classnames';
 import { localize } from '@deriv/translations';
 import { Timeline } from '@deriv/components';
 import { isMobile } from '@deriv/shared';
+import { identity_status_codes } from 'Sections/Verification/ProofOfIdentity/proof-of-identity-utils.js';
+import Verified from 'Components/poi/status/verified';
+import Limited from 'Components/poi/status/limited';
+import Expired from 'Components/poi/status/expired';
 import DetailComponent from './detail-component.jsx';
 import { Documents } from './documents.jsx';
 import { getDocumentIndex, DOCUMENT_TYPES } from './constants';
+import UploadComplete from '../upload-complete';
 
 const checkNimcStep = documents => {
     let has_nimc = false;
@@ -20,10 +25,27 @@ const checkNimcStep = documents => {
 const Unsupported = ({ country_code, ...props }) => {
     const [detail, setDetail] = React.useState(null);
     const toggleDetail = index => setDetail(index);
+
     const documents = getDocumentIndex({
         setDetail,
         country_code,
     });
+
+    if (props?.manual) {
+        if (props.manual.status === identity_status_codes.pending) return <UploadComplete />;
+        else if ([identity_status_codes.rejected, identity_status_codes.suspected].includes(props.manual.status)) {
+            if (!props?.allow_poi_resubmission) return <Limited />;
+        } else if (props.manual.status === identity_status_codes.verified) {
+            return <Verified needs_poa={props.needs_poa} redirect_button={props.redirect_button} />;
+        } else if (props.manual.status === identity_status_codes.expired) {
+            return (
+                <Expired
+                    redirect_button={props.redirect_button}
+                    handleRequireSubmission={props.handleRequireSubmission}
+                />
+            );
+        }
+    }
 
     if (detail !== null) {
         return (
--- packages/account/src/Sections/Verification/ProofOfIdentity/onfido.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/onfido.jsx
@@ -3,10 +3,18 @@ import UploadComplete from 'Components/poi/status/upload-complete';
 import Expired from 'Components/poi/status/expired';
 import Verified from 'Components/poi/status/verified';
 import RejectedReasons from 'Components/poi/status/rejected-reasons';
-import Limited from 'Components/poi/status/limited';
+import Unsupported from 'Components/poi/status/unsupported';
 import { identity_status_codes } from './proof-of-identity-utils';
 
-const Onfido = ({ handleRequireSubmission, is_from_external, needs_poa, onfido, redirect_button }) => {
+const Onfido = ({
+    handleRequireSubmission,
+    is_from_external,
+    needs_poa,
+    onfido,
+    redirect_button,
+    manual,
+    setIsCfdPoiCompleted,
+}) => {
     const { status, submissions_left, last_rejected: rejected_reasons } = onfido;
 
     switch (status) {
@@ -20,7 +28,15 @@ const Onfido = ({ handleRequireSubmission, is_from_external, needs_poa, onfido, 
             );
         case identity_status_codes.rejected:
         case identity_status_codes.suspected:
-            if (Number(submissions_left) < 1) return <Limited handleRequireSubmission={handleRequireSubmission} />;
+            if (Number(submissions_left) < 1) {
+                return (
+                    <Unsupported
+                        manual={manual}
+                        is_from_external={is_from_external}
+                        setIsCfdPoiCompleted={setIsCfdPoiCompleted}
+                    />
+                );
+            }
             return (
                 <RejectedReasons
                     rejected_reasons={rejected_reasons}

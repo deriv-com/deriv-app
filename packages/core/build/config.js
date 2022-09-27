371f624b918ae69e2dd5f2f687dbf98cb5fe5eec
thibault-deriv <104425314+thibault-deriv@users.noreply.github.com>
2022-09-27 09:50:27 +0200

    Rename 404.html to custom404.html
    Cloudflare pages don't need it and nginx will work like
    before since the filname is defined in the nginx config.

--- packages/core/build/config.js
+++ packages/core/build/config.js
@@ -87,7 +87,7 @@ const copyConfig = base => {
             to: 'assetlinks.json',
             toType: 'file',
         },
-        { from: path.resolve(__dirname, '../src/root_files/404.html'), to: '404.html', toType: 'file' },
+        { from: path.resolve(__dirname, '../src/root_files/custom404.html'), to: 'custom404.html', toType: 'file' },
         {
             from: path.resolve(__dirname, '../src/root_files/localstorage-sync.html'),
             to: 'localstorage-sync.html',

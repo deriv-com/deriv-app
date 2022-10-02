d4c1b16e1881ab301a513ceedf6432a4c52f6b06
Farzin Mirzaie <farzin@firstsource.tech>
2022-10-02 13:14:46 +0330

    feat(cashier): ✨ add `useDepositLocked` hook

--- packages/cashier/src/hooks/index.ts
+++ packages/cashier/src/hooks/index.ts
@@ -7,3 +7,4 @@ export { default as useNeedAuthentication } from './useNeedAuthentication';
 export { default as useNeedFinancialAssessment } from './useNeedFinancialAssessment';
 export { default as useRealSTPAccount } from './useRealSTPAccount';
 export { default as useNeedTNC } from './useNeedTNC';
+export { default as useDepositLocked } from './useDepositLocked';

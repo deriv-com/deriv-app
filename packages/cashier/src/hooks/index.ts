e7ea4c10bc751faf2c3e5b74cb200926ac08c4e6
Farzin Mirzaie <farzin@firstsource.tech>
2022-10-02 13:11:19 +0330

    feat(cashier): ✨ add `useNeedAuthentication` hook

--- packages/cashier/src/hooks/index.ts
+++ packages/cashier/src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useCountdown } from './useCountdown';
 export { default as useWS } from './useWS';
 export { default as useSubscription } from './useSubscription';
 export { default as useVerifyEmail } from './useVerifyEmail';
+export { default as useNeedAuthentication } from './useNeedAuthentication';

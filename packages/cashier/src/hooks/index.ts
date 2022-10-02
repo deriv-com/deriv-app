e0721f4ebb1d92a7f45733618fbbefec49756da8
Farzin Mirzaie <farzin@firstsource.tech>
2022-10-02 13:11:53 +0330

    feat(cashier): ✨ add `useNeedFinancialAssessment` hook

--- packages/cashier/src/hooks/index.ts
+++ packages/cashier/src/hooks/index.ts
@@ -4,3 +4,4 @@ export { default as useWS } from './useWS';
 export { default as useSubscription } from './useSubscription';
 export { default as useVerifyEmail } from './useVerifyEmail';
 export { default as useNeedAuthentication } from './useNeedAuthentication';
+export { default as useNeedFinancialAssessment } from './useNeedFinancialAssessment';

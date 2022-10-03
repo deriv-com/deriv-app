78d50b62c8e0839e3379ef224d7b36fd8299000d
Hamid Yaftian <hamid@re-work.dev>
2022-10-03 16:57:15 +0330

    Change trans

--- packages/cfd/src/Constants/cfd-shared-strings.js
+++ packages/cfd/src/Constants/cfd-shared-strings.js
@@ -20,9 +20,7 @@ export const general_messages = {
     },
     getFinancialAccountDescriptor: (platform, is_eu) => {
         if (platform === CFD_PLATFORMS.DXTRADE) {
-            return localize(
-                'Trade CFDs on forex, derived indices, cryptocurrencies, and commodities with high leverage.'
-            );
+            return localize('Trade forex, basket indices, commodities, and cryptocurrencies with high leverage.');
         } else if (platform === CFD_PLATFORMS.MT5) {
             if (is_eu) {
                 return localize(

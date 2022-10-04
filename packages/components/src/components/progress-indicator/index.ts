0ec35734c44e213ca2e400e0d6bba69bf6b730bd
Likhith Kolayari <likhith@regentmarkets.com>
2022-10-04 12:42:00 +0400

    refactor: :art: migrated progress indicator to TSx

--- packages/components/src/components/progress-indicator/index.ts
+++ packages/components/src/components/progress-indicator/index.ts
@@ -1,4 +1,4 @@
-import ProgressIndicator from './progress-indicator.jsx';
+import ProgressIndicator from './progress-indicator';
 import './progress-indicator.scss';
 
 export default ProgressIndicator;

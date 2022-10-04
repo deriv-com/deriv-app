88ca6df22503ef6e209f7bca54147ef8d159dcdd
mahdiyeh-fs <82078941+mahdiyeh-fs@users.noreply.github.com>
2022-10-04 18:11:06 +0330

    add CFDStore to modules store of core (#6676)

--- packages/appstore/src/stores/root-store.ts
+++ packages/appstore/src/stores/root-store.ts
@@ -8,15 +8,15 @@ export default class RootStore {
     public client: Record<string, any>;
     public common: Record<string, any>;
     public ui: Record<string, any>;
-    public cfd_account: Record<string, any>;
     public wallet_store: WalletStore;
+    public modules: Record<string, any>;
 
     public constructor(core_store: TRootStore) {
         this.config = new ConfigStore(this);
         this.client = core_store.client;
         this.common = core_store.common;
         this.ui = core_store.ui;
-        this.cfd_account = core_store.cfd_account;
         this.wallet_store = new WalletStore(this);
+        this.modules = core_store.modules;
     }
 }

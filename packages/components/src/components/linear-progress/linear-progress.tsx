83c577f3065cb5ebf715646795b5195408775f65
Likhith Kolayari <likhith@regentmarkets.com>
2022-10-04 15:44:00 +0400

    refactor: :art: removed unused prop values

--- packages/components/src/components/linear-progress/linear-progress.tsx
+++ packages/components/src/components/linear-progress/linear-progress.tsx
@@ -1,13 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 
-type TLinearProgress = {
-    progress: number;
-    className: string;
-    height: number;
-};
-
-export const LinearProgress = ({ progress }: Partial<TLinearProgress>) => (
+export const LinearProgress = ({ progress }: { progress: number }) => (
     <div className={classNames('dc-linear-progress')}>
         <div className={classNames('dc-linear-progress__bar')} style={{ width: `${progress}%` }} />
     </div>

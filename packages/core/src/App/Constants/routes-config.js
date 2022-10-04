d04603c08e0cb083626f020881d5f0391ec6824f
ashrafali-v <ashraf@deriv.com>
2022-10-04 17:21:30 +0800

    reverting un necessary files

--- packages/core/src/App/Constants/routes-config.js
+++ packages/core/src/App/Constants/routes-config.js
@@ -210,7 +210,7 @@ const getModules = (_params, is_social_signup, is_pre_appstore) => {
         {
             path: routes.appstore,
             component: AppStore,
-            is_authenticated: false,
+            is_authenticated: true,
             getTitle: () => localize('Appstore'),
             routes: [
                 {

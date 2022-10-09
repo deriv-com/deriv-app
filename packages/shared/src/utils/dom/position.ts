aaffc5880634b6ed0bf4835d26f2be592ce3c820
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-10-09 13:26:42 +0330

    refactor: convert date, digital-options, dom and files to ts

--- packages/shared/src/utils/dom/position.ts
+++ packages/shared/src/utils/dom/position.ts
@@ -1,14 +1,36 @@
-const getMaxHeightByAligningBottom = ({ parent_rect, child_height }) =>
+type RectResult = {
+    bottom: number;
+    height: number;
+    left: number;
+    right: number;
+    top: number;
+    width: number;
+};
+
+type TGetMaxHeightByAligning = {
+    parent_rect: RectResult;
+    child_height: number;
+};
+
+type TGetPosition = {
+    preferred_alignment: string;
+    child_el: HTMLElement;
+    parent_el: HTMLElement;
+    should_consider_parent_height: boolean;
+};
+
+const getMaxHeightByAligningBottom = ({ parent_rect, child_height }: TGetMaxHeightByAligning) =>
     parent_rect.top + parent_rect.height + child_height;
 
-const getMinHeightByAligningTop = ({ parent_rect, child_height }) => parent_rect.top - child_height;
+const getMinHeightByAligningTop = ({ parent_rect, child_height }: TGetMaxHeightByAligning) =>
+    parent_rect.top - child_height;
 
 export const getPosition = ({
     preferred_alignment = 'bottom',
     child_el,
     parent_el,
     should_consider_parent_height = true,
-}) => {
+}: TGetPosition) => {
     const parent_rect = parent_el.getBoundingClientRect();
     const child_height = child_el.clientHeight;
     const body_rect = document.body.getBoundingClientRect();

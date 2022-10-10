c8cb08dd1de4363214621244420f2a240b52a9bd
Likhith Kolayari <likhith@regentmarkets.com>
2022-10-10 08:51:27 +0400

    refactor: :art: added type for callback

--- packages/components/src/hooks/use-constructor.ts
+++ packages/components/src/hooks/use-constructor.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const useConstructor = (callBack = () => undefined) => {
+export const useConstructor = (callBack: () => void = () => undefined) => {
     const is_called_ref = React.useRef(false);
     if (!is_called_ref.current) {
         callBack();

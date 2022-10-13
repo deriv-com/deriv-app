d500b145fe22ee59f1129030b1e51d175a5f48f8
Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>
2022-10-13 19:18:34 +0800

    Components shared ts migration parent (#6563)
    * fix: circle/ci issue
    
    * refactor: added moduleLoader types
    
    * refactor: convert login, object, os, platform and promise to TS
    
    * refactor: convert login, object, os, platform and promise to TS
    
    * fix: review comment
    
    * fix: circle/ci issue
    
    * fix: circle/ci issue
    
    * fix: review comment
    
    * fix: review comment
    
    * refactor: ts migration is done
    
    * refactor: ts migration is done
    
    * refactor: ts migration is done
    
    * refactor: ts migration done
    
    * fix: refactor code to fix jest error  regarding the usage of Div100vh component
    
    * chore: testing codecov
    
    * fix: review comments
    
    * fix: review comments
    
    * refactor: convert route, routes, screen and shortcode to TS
    
    * added more types
    
    * add optional chain to some of the types
    
    * fix: resolve review comments
    
    * yauheni/74997/content expander ts migration
    
    * refactor: added type to remained methods
    
    * fix: fixed the returntype issue
    
    * fix: some types
    
    * SonarCloud issue
    
    * fix: resolve pr comments
    
    * fix: resolved pr comments
    
    * fix: resolved pr comments
    
    * fix: resolved pr comments
    
    * yauheni/74990/carousel ts migration
    
    * fix: resolve pr comments
    
    * yauheni/76942/uiloader ts migration
    
    * fix: resolved pr comments
    
    * refactor: convert objects to ts
    
    * jsx import fix
    
    * children types fix
    
    * import fix, props children types fix
    
    * yauheni/76938/timeline ts migration
    
    * refactor: make props optional and arrange them alphabrtically
    
    * fix: made types optiona;
    
    * fix: fix circle-ci error
    
    * fix: review comments
    
    * fix: review comments
    
    * yauheni/76902/modal ts migration
    
    * conflicts
    
    * refactor: migrate-money-component-to-ts
    
    * chore: make size optional on the text component
    
    * props duplication fix, typo usage fix
    
    * refactor: update type based on nitpick suggested
    
    * fix: resolve pr comments
    
    * size typo fix
    
    * types fix, disabled_items marked as optional
    
    * Update packages/components/src/components/horizontal-swipe/horizontal-swipe.tsx
    
    Co-authored-by: Maryia <103177211+maryia-binary@users.noreply.github.com>
    
    * fix: resolved pr comments
    
    * chore: added some changes
    
    * refactor: update type definitions
    
    * refactor: set default value for tabIndex
    
    * refactor: update type definitions
    
    * refactor(components): :recycle: migrate `ProgressBar` component to `TypeScript`
    
    * fix(components): :memo: resolve PR comments
    
    * fix: resolved PR comments
    
    * fix: resolved PR comments
    
    * chore: removed unnecessary type
    
    * fix: review comments
    
    * fix: remove extra packages from shared
    
    * refactor: update type definition
    
    * fix: resolve pr comments
    
    * fix: review comment
    
    * fix: review comments
    
    * refactor: pass in optional arguments
    
    * refactor: make prop optional and export component type
    
    * fix(components): :memo: resolve PR comments
    
    * Trigger Build
    
    * fix: circleci issue
    
    * fix: fixed building error and add type to react-div-100vh
    
    * chore: removed empty line
    
    * fix: resove pr comments and updated package-lock
    
    Co-authored-by: Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
    Co-authored-by: Shayan Khaleghparast <100833613+iman-fs@users.noreply.github.com>
    Co-authored-by: iman <iman@firstsource.tech>
    Co-authored-by: “yauheni-kryzhyk-deriv” <“yauheni@deriv.me”>
    Co-authored-by: yauheni-kryzhyk-deriv <103182683+yauheni-kryzhyk-deriv@users.noreply.github.com>
    Co-authored-by: Maryia <103177211+maryia-binary@users.noreply.github.com>
    Co-authored-by: Farzin Mirzaie <farzin@firstsource.tech>

--- packages/shared/src/utils/browser/browser_detect.js
+++
@@ -1,8 +0,0 @@
-export const isSafari = () => {
-    return (
-        /constructor/i.test(window.HTMLElement) ||
-        (function (p) {
-            return p.toString() === '[object SafariRemoteNotification]';
-        })(!window.safari || (typeof safari !== 'undefined' && window.safari.pushNotification))
-    );
-};

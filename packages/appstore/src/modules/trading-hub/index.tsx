d9eb2e51e0c8dc053b32adf50a42c6443334df5a
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-10-16 17:06:56 +0330

    fix dropdown

--- packages/appstore/src/modules/trading-hub/index.tsx
+++ packages/appstore/src/modules/trading-hub/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { observer } from 'mobx-react-lite';
 import Joyride from 'react-joyride';
 import { useHistory } from 'react-router-dom';
-import { Text, Button } from '@deriv/components';
+import { Text, Button, ButtonToggle, Dropdown, DesktopWrapper, MobileWrapper } from '@deriv/components';
 import { routes } from '@deriv/shared';
 import { Localize, localize } from '@deriv/translations';
 import ToggleAccountType from 'Components/toggle-account-type';
@@ -22,10 +22,24 @@ const TradingHub = () => {
     const history = useHistory();
     /*TODO: We need to show this component whenever user click on tour guide button*/
     const [account_type, setAccountType] = React.useState<TAccountCategory>('demo');
+    const [platform_type, setPlatformType] = React.useState<any>('cfd');
 
     const accountTypeChange = (event: any) => {
         setAccountType(event.target.value);
     };
+    const platformTypeChange = (event: any) => {
+        setPlatformType(event.target.value);
+    };
+
+    const account_toggle_options = [
+        { text: 'Real', value: 'real' },
+        { text: 'Demo', value: 'demo' },
+    ];
+
+    const platform_toggle_options = [
+        { text: 'CFD', value: 'cfd' },
+        { text: 'Options', value: 'options' },
+    ];
 
     tour_step_locale.last = (
         <Localize
@@ -64,15 +78,47 @@ const TradingHub = () => {
                         category={account_type}
                         className='trading-hub_header--account_assets'
                     />
-                    <ToggleAccountType
-                        accountTypeChange={(event: any) => accountTypeChange(event)}
-                        value={account_type}
-                    />
+                    <DesktopWrapper>
+                        <ToggleAccountType
+                            accountTypeChange={(event: any) => accountTypeChange(event)}
+                            value={account_type}
+                        />
+                    </DesktopWrapper>
+                    <MobileWrapper>
+                        <Dropdown
+                            id='platfrom_toggle_options'
+                            className='trading-hub_header--platfrom_toggle_options'
+                            is_alignment_left={false}
+                            is_nativepicker={false}
+                            list={account_toggle_options}
+                            name='multiplier'
+                            no_border={true}
+                            value={account_type}
+                            onChange={accountTypeChange}
+                        />
+                    </MobileWrapper>
                 </div>
             </div>
+
             <div className='trading-hub_body'>
-                <CFDAccounts account_type={account_type} />
-                <Divider horizontal className='trading-hub_body--divider' />
+                <DesktopWrapper>
+                    <CFDAccounts account_type={account_type} />
+                    <Divider horizontal className='trading-hub_body--divider' />
+                    <div>Options</div>
+                </DesktopWrapper>
+                <MobileWrapper>
+                    <ButtonToggle
+                        buttons_arr={platform_toggle_options}
+                        className='trading-hub_body--platform_type_toggle'
+                        has_rounded_button
+                        is_animated
+                        name='platforn_type'
+                        onChange={platformTypeChange}
+                        value={platform_type}
+                    />
+                    {platform_type === 'cfd' && <CFDAccounts account_type={account_type} />}
+                    {platform_type === 'options' && <div>Options</div>}
+                </MobileWrapper>
             </div>
             <Joyride
                 run={is_tour_open}

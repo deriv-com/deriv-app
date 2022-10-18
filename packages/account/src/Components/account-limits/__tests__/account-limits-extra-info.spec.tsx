024e6bcf94c3e97289de961b54e76c1dea134396
tay suisin <suisin@regentmarkets.com>
2022-10-18 18:21:41 +0800

    change Lorem Ipsom to Lorem ipsum

--- packages/account/src/Components/account-limits/__tests__/account-limits-extra-info.spec.tsx
+++ packages/account/src/Components/account-limits/__tests__/account-limits-extra-info.spec.tsx
@@ -10,19 +10,19 @@ jest.mock('@deriv/shared', () => ({
 
 describe('<AccountLimitsExtraInfo/>', () => {
     it('should render AccountLimitsExtraInfo component', () => {
-        render(<AccountLimitsExtraInfo message='Lorem Ipsom' />);
+        render(<AccountLimitsExtraInfo message='Lorem ipsom' />);
         expect(screen.getByText(/lorem ipsom/i)).toBeInTheDocument();
     });
 
     it('should render PopoverComponent if isMobile is false', () => {
         (isMobile as jest.Mock).mockReturnValue(false);
-        render(<AccountLimitsExtraInfo message='Lorem Ipsom' />);
+        render(<AccountLimitsExtraInfo message='Lorem ipsom' />);
         expect(screen.queryByTestId('dt_acc_limits_popover')).toHaveClass('da-account-limits__popover');
     });
 
     it('should pass props to PopoverComponent if isMobile is false', async () => {
         (isMobile as jest.Mock).mockReturnValue(false);
-        render(<AccountLimitsExtraInfo message='Lorem Ipsum' className='test_class' />);
+        render(<AccountLimitsExtraInfo message='Lorem ipsum' className='test_class' />);
         const popover = await screen.findByTestId('dt_acc_limits_popover');
         expect(popover).toHaveClass('test_class');
     });

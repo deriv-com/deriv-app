349527524d0470d97faca70b789b67800baed8b3
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-10-24 11:19:24 +0300

    children return type

--- packages/components/src/components/div100vh-container/div100vh-container.tsx
+++ packages/components/src/components/div100vh-container/div100vh-container.tsx
@@ -38,7 +38,7 @@ const Div100vhContainer = ({
         height: max_autoheight_offset ? '' : height_rule,
         maxHeight: max_autoheight_offset ? `calc(100rvh - ${max_autoheight_offset})` : '',
     };
-    if (is_bypassed) return children;
+    if (is_bypassed) return children as JSX.Element;
     return (
         <Div100vh id={id} className={className} style={is_disabled ? {} : height_style}>
             {children}

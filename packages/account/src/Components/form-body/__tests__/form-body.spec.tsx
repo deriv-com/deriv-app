1edcfaae7896fe765e60b1c7221428f308900b0f
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-10-24 12:13:36 +0300

    yauheni/74111/formbody ts migration

--- packages/account/src/Components/form-body/__tests__/form-body.spec.tsx
+++ packages/account/src/Components/form-body/__tests__/form-body.spec.tsx
@@ -16,18 +16,20 @@ describe('<FormBody />', () => {
                 <div>Test children</div>
             </FormBody>
         );
-        expect(container.querySelector('account__scrollbars_container--grid-layout')).toBeInTheDocument;
+        // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+        expect(container.querySelector('.account__scrollbars_container--grid-layout')).toBeInTheDocument();
         expect(screen.getByText('Test children')).toBeInTheDocument();
     });
 
     it('should render FormBody component with children in mobile', () => {
-        isMobile.mockReturnValue(true);
-        isDesktop.mockReturnValue(false);
+        (isMobile as jest.Mock).mockReturnValue(true);
+        (isDesktop as jest.Mock).mockReturnValue(false);
         const { container } = render(
             <FormBody>
                 <div>Test children</div>
             </FormBody>
         );
+        // eslint-disable-next-line testing-library/no-node-access
         expect(container.firstChild).toHaveClass('account__scrollbars_container--grid-layout');
         expect(screen.getByText('Test children')).toBeInTheDocument();
     });

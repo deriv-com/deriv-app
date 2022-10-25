acf08cc6fffc5aabdd0540a915061b683111088c
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-10-25 16:14:22 +0330

    remove redundant icons

--- packages/appstore/src/assets/svgs/wallet/index.tsx
+++ packages/appstore/src/assets/svgs/wallet/index.tsx
@@ -1,231 +1,47 @@
 import React from 'react';
-import AdvcashDark from 'Assets/svgs/wallet/advcash-dark.svg';
-import AdvcashLight from 'Assets/svgs/wallet/advcash-light.svg';
-import AirtmDark from 'Assets/svgs/wallet/airtm-dark.svg';
-import AirtmLight from 'Assets/svgs/wallet/airtm-light.svg';
-import AstropayDark from 'Assets/svgs/wallet/astropay-dark.svg';
-import AstropayLight from 'Assets/svgs/wallet/astropay-light.svg';
-import AudDark from 'Assets/svgs/wallet/aud-dark.svg';
-import AudLight from 'Assets/svgs/wallet/aud-light.svg';
-import BankWireDark from 'Assets/svgs/wallet/bank-wire-dark.svg';
-import BankWireLight from 'Assets/svgs/wallet/bank-wire-light.svg';
-import BanxaDark from 'Assets/svgs/wallet/banxa-dark.svg';
-import BanxaLight from 'Assets/svgs/wallet/banxa-light.svg';
-import BeyonicDark from 'Assets/svgs/wallet/beyonic-dark.svg';
-import BeyonicLight from 'Assets/svgs/wallet/beyonic-light.svg';
 import BinaryBot from 'Assets/svgs/wallet/ic-brand-binarybot.svg';
 import BinaryBotBlue from 'Assets/svgs/wallet/ic-appstore-binarybot-blue.svg';
-import BitcoinDark from 'Assets/svgs/wallet/bitcoin-dark.svg';
-import BitcoinLight from 'Assets/svgs/wallet/bitcoin-light.svg';
-import BoletoDark from 'Assets/svgs/wallet/boleto-dark.svg';
-import BoletoLight from 'Assets/svgs/wallet/boleto-light.svg';
-import ChangellyDark from 'Assets/svgs/wallet/changelly-dark.svg';
-import ChangellyLight from 'Assets/svgs/wallet/changelly-light.svg';
-import CimbniagaDark from 'Assets/svgs/wallet/cimbniaga-dark.svg';
-import CreditCardLight from 'Assets/svgs/wallet/credit-card-light.svg';
-import CreditCardsDark from 'Assets/svgs/wallet/credit-cards-dark.svg';
-import CreditCardsLight from 'Assets/svgs/wallet/credit-cards-light.svg';
 import CurrencyUSD from 'Assets/svgs/wallet/currency-usd.svg';
 import DBot from 'Assets/svgs/wallet/ic-brand-dbot.svg';
 import Demo from 'Assets/svgs/wallet/ic-brand-demo.svg';
-import DemoDark from 'Assets/svgs/wallet/demo-dark.svg';
-import DemoLight from 'Assets/svgs/wallet/demo-light.svg';
-import DemoMobile from 'Assets/svgs/wallet/account-demo-mobile.svg';
 import Derived from 'Assets/svgs/wallet/ic-appstore-derived.svg';
 import DerivGo from 'Assets/svgs/wallet/ic-brand-derivgo.svg';
 import DerivGoBlack from 'Assets/svgs/wallet/ic-appstore-derivgo-black.svg';
 import DerivLogo from 'Assets/svgs/wallet/ic-appstore-deriv-logo.svg';
-import DerivP2pDark from 'Assets/svgs/wallet/deriv-p2p-dark.svg';
-import DerivP2pLight from 'Assets/svgs/wallet/deriv-p2p-light.svg';
 import DerivX from 'Assets/svgs/wallet/ic-appstore-derivx.svg';
-import DinersClubDark from 'Assets/svgs/wallet/diners-club-dark.svg';
-import DinersClubLight from 'Assets/svgs/wallet/diners-club-light.svg';
 import DropDown from 'Assets/svgs/wallet/drop-down.svg';
 import DTrader from 'Assets/svgs/wallet/ic-brand-dtrader.svg';
-import EthereumDark from 'Assets/svgs/wallet/ethereum-dark.svg';
-import EthereumLight from 'Assets/svgs/wallet/ethereum-light.svg';
-import EurDark from 'Assets/svgs/wallet/eur-dark.svg';
-import EurLight from 'Assets/svgs/wallet/eur-light.svg';
-import FasapayDark from 'Assets/svgs/wallet/fasapay-dark.svg';
 import FasapayLight from 'Assets/svgs/wallet/fasapay-light.svg';
 import Financial from 'Assets/svgs/wallet/ic-appstore-financial.svg';
-import GbpDark from 'Assets/svgs/wallet/gbp-dark.svg';
-import GbpLight from 'Assets/svgs/wallet/gbp-light.svg';
-import Help2payDark from 'Assets/svgs/wallet/help2pay-dark.svg';
-import Help2payLight from 'Assets/svgs/wallet/help2pay-light.svg';
-import InstantBankTransferDark from 'Assets/svgs/wallet/instant-bank-transfer-dark.svg';
-import InstantBankTransferLight from 'Assets/svgs/wallet/instant-bank-transfer-light.svg';
-import JcbDark from 'Assets/svgs/wallet/jcb-dark.svg';
-import JcbLight from 'Assets/svgs/wallet/jcb-light.svg';
-import JetonDark from 'Assets/svgs/wallet/jeton-dark.svg';
-import JetonLight from 'Assets/svgs/wallet/jeton-light.svg';
-import LitecoinDark from 'Assets/svgs/wallet/litecoin-dark.svg';
-import LitecoinLight from 'Assets/svgs/wallet/litecoin-light.svg';
-import MaestroDark from 'Assets/svgs/wallet/maestro-dark.svg';
-import MaestroLight from 'Assets/svgs/wallet/maestro-light.svg';
-import MastercardDark from 'Assets/svgs/wallet/mastercard-dark.svg';
-import MastercardLight from 'Assets/svgs/wallet/mastercard-light.svg';
-import NetellerDark from 'Assets/svgs/wallet/neteller-dark.svg';
-import NetellerLight from 'Assets/svgs/wallet/neteller-light.svg';
-import NganluongDark from 'Assets/svgs/wallet/nganluong-dark.svg';
-import NganluongLight from 'Assets/svgs/wallet/nganluong-light.svg';
-import OneforyouDark from 'Assets/svgs/wallet/1foryou-dark.svg';
-import OneforyouLight from 'Assets/svgs/wallet/1foryou-light.svg';
-import OnlinenairaDark from 'Assets/svgs/wallet/onlinenaira-dark.svg';
-import OnlinenairaLight from 'Assets/svgs/wallet/onlinenaira-light.svg';
 import Options from 'Assets/svgs/wallet/ic-appstore-options.svg';
-import OxxoDark from 'Assets/svgs/wallet/oxxo-dark.svg';
-import OxxoLight from 'Assets/svgs/wallet/oxxo-light.svg';
-import PayLivreDark from 'Assets/svgs/wallet/pay-livre-dark.svg';
-import PayLivreLight from 'Assets/svgs/wallet/pay-livre-light.svg';
-import PaymentAgentDark from 'Assets/svgs/wallet/payment-agent-dark.svg';
 import PaymentAgentLight from 'Assets/svgs/wallet/payment-agent-light.svg';
-import PaysafeCardDark from 'Assets/svgs/wallet/paysafe-card-dark.svg';
-import PaysafecardLight from 'Assets/svgs/wallet/paysafe-card-light.svg';
-import Paytrust88Dark from 'Assets/svgs/wallet/paytrust88-dark.svg';
-import Paytrust88Light from 'Assets/svgs/wallet/paytrust88-light.svg';
-import PerfectMoneyDark from 'Assets/svgs/wallet/perfect-money-dark.svg';
-import PerfectMoneyLight from 'Assets/svgs/wallet/perfect-money-light.svg';
-import SkrillDark from 'Assets/svgs/wallet/skrill-dark.svg';
 import SkrillLight from 'Assets/svgs/wallet/skrill-light.svg';
 import SmartTrader from 'Assets/svgs/wallet/ic-brand-smarttrader.svg';
 import SmartTraderBlue from 'Assets/svgs/wallet/ic-appstore-smarttrader-blue.svg';
-import SpeiDark from 'Assets/svgs/wallet/spei-dark.svg';
-import SpeiLight from 'Assets/svgs/wallet/spei-light.svg';
-import SticpayDark from 'Assets/svgs/wallet/sticpay-dark.svg';
-import SticpayLight from 'Assets/svgs/wallet/sticpay-light.svg';
-import TetherDark from 'Assets/svgs/wallet/tether-dark.svg';
-import TetherLight from 'Assets/svgs/wallet/tether-light.svg';
-import TrustlyDark from 'Assets/svgs/wallet/trustly-dark.svg';
-import TrustlyLight from 'Assets/svgs/wallet/trustly-light.svg';
-import UsdCoinDark from 'Assets/svgs/wallet/usd-coin-dark.svg';
-import UsdCoinLight from 'Assets/svgs/wallet/usd-coin-light.svg';
-import UsdDark from 'Assets/svgs/wallet/usd-dark.svg';
 import UsdLight from 'Assets/svgs/wallet/usd-light.svg';
-import VisaDark from 'Assets/svgs/wallet/visa-dark.svg';
-import VisaElectronDark from 'Assets/svgs/wallet/visa-electron-dark.svg';
-import VisaElectronLight from 'Assets/svgs/wallet/visa-electron-light.svg';
-import VisaLight from 'Assets/svgs/wallet/visa-light.svg';
-import WebmoneyDark from 'Assets/svgs/wallet/webmoney-dark.svg';
-import WebmoneyLight from 'Assets/svgs/wallet/webmoney-light.svg';
-import WechatPayDark from 'Assets/svgs/wallet/wechat-pay-dark.svg';
-import WechatPayLight from 'Assets/svgs/wallet/wechat-pay-light.svg';
-import ZingpayDark from 'Assets/svgs/wallet/zingpay-dark.svg';
 import ZingpayLight from 'Assets/svgs/wallet/zingpay-light.svg';
 
 const Icons = {
-    '1foryouDark': OneforyouDark,
-    '1foryouLight': OneforyouLight,
-    AdvcashDark,
-    AdvcashLight,
-    AirtmDark,
-    AirtmLight,
-    AstropayDark,
-    AstropayLight,
-    AudDark,
-    AudLight,
-    BankWireDark,
-    BankWireLight,
-    BanxaDark,
-    BanxaLight,
-    BeyonicDark,
-    BeyonicLight,
     BinaryBot,
     BinaryBotBlue,
-    BitcoinDark,
-    BitcoinLight,
-    BoletoDark,
-    BoletoLight,
-    ChangellyDark,
-    ChangellyLight,
-    CimbniagaDark,
-    CreditCardLight,
-    CreditCardsDark,
-    CreditCardsLight,
     CurrencyUSD,
     DBot,
     Demo,
-    DemoDark,
-    DemoLight,
-    DemoMobile,
     Derived,
     DerivGo,
     DerivGoBlack,
     DerivLogo,
-    DerivP2pDark,
-    DerivP2pLight,
     DerivX,
-    DinersClubDark,
-    DinersClubLight,
     DropDown,
     DTrader,
-    EthereumDark,
-    EthereumLight,
-    EurDark,
-    EurLight,
-    FasapayDark,
     FasapayLight,
     Financial,
-    GbpDark,
-    GbpLight,
-    Help2payDark,
-    Help2payLight,
-    InstantBankTransferDark,
-    InstantBankTransferLight,
-    JcbDark,
-    JcbLight,
-    JetonDark,
-    JetonLight,
-    LitecoinDark,
-    LitecoinLight,
-    MaestroDark,
-    MaestroLight,
-    MastercardDark,
-    MastercardLight,
-    NetellerDark,
-    NetellerLight,
-    NganluongDark,
-    NganluongLight,
-    OnlinenairaDark,
-    OnlinenairaLight,
     Options,
-    OxxoDark,
-    OxxoLight,
-    PayLivreDark,
-    PayLivreLight,
-    PaymentAgentDark,
     PaymentAgentLight,
-    PaysafeCardDark,
-    PaysafecardLight,
-    Paytrust88Dark,
-    Paytrust88Light,
-    PerfectMoneyDark,
-    PerfectMoneyLight,
-    SkrillDark,
     SkrillLight,
     SmartTrader,
     SmartTraderBlue,
-    SpeiDark,
-    SpeiLight,
-    SticpayDark,
-    SticpayLight,
-    TetherDark,
-    TetherLight,
-    TrustlyDark,
-    TrustlyLight,
-    UsdCoinDark,
-    UsdCoinLight,
-    UsdDark,
     UsdLight,
-    VisaDark,
-    VisaElectronDark,
-    VisaElectronLight,
-    VisaLight,
-    WebmoneyDark,
-    WebmoneyLight,
-    WechatPayDark,
-    WechatPayLight,
-    ZingpayDark,
     ZingpayLight,
 };
 

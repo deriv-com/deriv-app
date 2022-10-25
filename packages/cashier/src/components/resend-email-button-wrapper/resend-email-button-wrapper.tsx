4446dc86409deb1c204f05cbb8ef6ebc8bab242a
Bahar <bahar@regentmarkets.com>
2022-10-25 15:38:42 +0800

    replace_connect_method_with_usestore_hook_for-_resendemailbuttonwrapper

--- packages/cashier/src/components/resend-email-button-wrapper/resend-email-button-wrapper.tsx
+++ packages/cashier/src/components/resend-email-button-wrapper/resend-email-button-wrapper.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { Button } from '@deriv/components';
 import { localize } from '@deriv/translations';
-import { connect } from 'Stores/connect';
-import { TRootStore } from 'Types';
+import { useStore } from '../../hooks';
 
 type TResetEmailButtonWrapperProps = {
     is_withdrawal: boolean;
-    resend_timeout: number;
     resendVerificationEmail: () => void;
 };
 
-const ResendEmailButtonWrapper = ({ resend_timeout, resendVerificationEmail }: TResetEmailButtonWrapperProps) => (
-    <Button
-        className='verification-email__resend-button'
-        classNameSpan='verification-email__resend-button-text'
-        is_disabled={resend_timeout < 60}
-        has_effect
-        text={
-            resend_timeout < 60
-                ? localize('Resend email in {{seconds}}s', {
-                      seconds: resend_timeout,
-                  })
-                : localize('Resend email')
-        }
-        onClick={resendVerificationEmail}
-        primary
-        large
-    />
-);
+const ResendEmailButtonWrapper = ({ is_withdrawal, resendVerificationEmail }: TResetEmailButtonWrapperProps) => {
+    const {
+        modules: {
+            cashier: { withdraw, payment_agent },
+        },
+    } = useStore();
+    const {
+        verification: { resend_timeout },
+    } = is_withdrawal ? withdraw : payment_agent;
 
-export default connect(({ modules }: TRootStore, props: TResetEmailButtonWrapperProps) => ({
-    resend_timeout: props.is_withdrawal
-        ? modules.cashier.withdraw.verification.resend_timeout
-        : modules.cashier.payment_agent.verification.resend_timeout,
-}))(ResendEmailButtonWrapper);
+    return (
+        <Button
+            className='verification-email__resend-button'
+            classNameSpan='verification-email__resend-button-text'
+            is_disabled={resend_timeout < 60}
+            has_effect
+            text={
+                resend_timeout < 60
+                    ? localize('Resend email in {{seconds}}s', {
+                          seconds: resend_timeout,
+                      })
+                    : localize('Resend email')
+            }
+            onClick={resendVerificationEmail}
+            primary
+            large
+        />
+    );
+};
+
+export default observer(ResendEmailButtonWrapper);

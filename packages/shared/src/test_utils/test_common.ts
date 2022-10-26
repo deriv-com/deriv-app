8f04de837365bdf05c6bfbe5087acc02bda5dd8e
ashrafali-v <ashraf@deriv.com>
2022-10-26 18:56:17 +0800

    refactor

--- packages/shared/src/test_utils/test_common.ts
+++ packages/shared/src/test_utils/test_common.ts
@@ -3,7 +3,9 @@ import { JSDOM } from 'jsdom';
 import { reset } from '../utils/url/url';
 
 const jsdom = new JSDOM();
+
 const setURL = (url: string) => {
+    jsdom.reconfigure({ url });
     // eslint-disable-next-line no-unused-expressions
     jsdom.window.location.href === url;
     reset();

64ca34e4cca3cd3da75e9f9721a7cbacda3a2784
vinu-deriv <vinu@deriv.com>
2022-10-27 13:04:25 +0400

    feat: added @deriv/ui in cashier

--- packages/cashier/src/containers/routes/routes.tsx
+++ packages/cashier/src/containers/routes/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
+import { useTheme } from '@deriv/ui';
 import { connect } from 'Stores/connect';
 import { TClientStore, TCommonStore, TRootStore } from 'Types';
 import BinaryRoutes from './binary-routes';
@@ -10,9 +11,16 @@ type TRoutesProps = RouteComponentProps & {
     has_error: TCommonStore['has_error'];
     is_logged_in: TClientStore['is_logged_in'];
     is_logging_in: TClientStore['is_logging_in'];
+    is_dark_mode_on: boolean;
 };
 
-const Routes = ({ error, has_error, is_logged_in, is_logging_in }: TRoutesProps) => {
+const Routes = ({ error, has_error, is_logged_in, is_logging_in, is_dark_mode_on }: TRoutesProps) => {
+    const { setColorMode } = useTheme();
+
+    React.useEffect(() => {
+        setColorMode(is_dark_mode_on ? 'dark' : 'light');
+    }, [is_dark_mode_on]);
+
     if (has_error) {
         return <ErrorComponent {...error} />;
     }
@@ -22,9 +30,10 @@ const Routes = ({ error, has_error, is_logged_in, is_logging_in }: TRoutesProps)
 
 // need to wrap withRouter around connect
 // to prevent updates on <BinaryRoutes /> from being blocked
-export default connect(({ client, common }: TRootStore) => ({
+export default connect(({ client, common, ui }: TRootStore) => ({
     is_logged_in: client.is_logged_in,
     is_logging_in: client.is_logging_in,
     error: common.error,
     has_error: common.has_error,
+    is_dark_mode_on: ui.is_dark_mode_on,
 }))(withRouter(Routes));

856a25675416fc1f9ec4634861423cf2304f9634
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2022-10-28 10:37:37 +0300

    yauheni/79513/no language redirection from derivapp

--- packages/components/src/components/static-url/static-url.jsx
+++ packages/components/src/components/static-url/static-url.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { getStaticUrl, PlatformContext } from '@deriv/shared';
+import { getStaticUrl, PlatformContext, setUrlLanguage } from '@deriv/shared';
+import { getLanguage } from '@deriv/translations';
 
 const StaticUrl = ({ href, is_document, children, ...props }) => {
     const { is_appstore } = React.useContext(PlatformContext);
+    setUrlLanguage(getLanguage());
 
     return (
         <a href={getStaticUrl(href, { is_appstore }, is_document)} rel='noopener noreferrer' target='_blank' {...props}>

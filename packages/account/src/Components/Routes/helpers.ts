1d699d4fc9ba8af0414590650297b38ee9c493a2
Likhith Kolayari <likhith@regentmarkets.com>
2022-11-04 16:29:38 +0400

    feat: :art: code formatted

--- packages/account/src/Components/Routes/helpers.ts
+++ packages/account/src/Components/Routes/helpers.ts
@@ -2,7 +2,7 @@ import { matchPath, RouteProps } from 'react-router';
 import { routes } from '@deriv/shared';
 import { TRouteConfig } from 'Types';
 
-export const normalizePath: (path: string) => string = path => (/^\//.test(path) ? path : `/${path || ''}`); // Default to '/'
+export const normalizePath = (path: string): string => (/^\//.test(path) ? path : `/${path || ''}`); // Default to '/'
 
 export const findRouteByPath = (path: string, routes_config?: TRouteConfig[]): RouteProps | undefined => {
     let result;

dee16ee29e6e28edc789cbe97c7472594923337c
ameerul hady <ameerul@deriv.com>
2022-11-07 14:08:51 +0800

    check with screen.availWidth

--- packages/core/src/App/app.jsx
+++ packages/core/src/App/app.jsx
@@ -13,7 +13,7 @@ import {
     isTablet,
     isTouchDevice,
     initFormErrorMessages,
-    mobileOSDetect,
+    // mobileOSDetect,
     setSharedCFDText,
     useOnLoadTranslation,
 } from '@deriv/shared';
@@ -66,13 +66,13 @@ const AppWithoutTranslation = ({ root_store }) => {
 
     const handleResize = React.useCallback(() => {
         if (isTouchDevice() && (isMobile() || isTablet())) {
-            const is_android_device = mobileOSDetect() === 'Android';
-            const view_width = is_android_device ? screen.availWidth : window.screen.width;
-            const view_height = is_android_device ? screen.availHeight : window.screen.height;
+            // const is_android_device = mobileOSDetect() === 'Android';
+            // const view_width = is_android_device ? screen.availWidth : window.screen.width;
+            // const view_height = is_android_device ? screen.availHeight : window.screen.height;
             const el_landscape_blocker = document.getElementById('landscape_blocker');
             const is_media_landscape = isSafari()
                 ? window.matchMedia('(orientation:portrait)').matches
-                : view_width <= view_height;
+                : screen.availWidth <= screen.availHeight;
 
             if (is_media_landscape) {
                 root_store.ui.onOrientationChange({ is_landscape_orientation: false });

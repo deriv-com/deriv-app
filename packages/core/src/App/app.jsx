fc72b9fc39fa12494b7aa7e71ab4c2fdab991dd0
ameerul hady <ameerul@deriv.com>
2022-11-07 16:29:57 +0800

    check window.width and height, screen messes all browsers but chrome

--- packages/core/src/App/app.jsx
+++ packages/core/src/App/app.jsx
@@ -77,7 +77,7 @@ const AppWithoutTranslation = ({ root_store }) => {
                 if (isSafari()) {
                     is_landscape = window.matchMedia('(orientation:portrait)').matches;
                 } else {
-                    is_landscape = screen.width <= screen.height;
+                    is_landscape = window.width <= window.height;
                 }
             } else if (mobileOSDetect() === 'Android') {
                 is_landscape = screen.availWidth <= screen.availHeight;

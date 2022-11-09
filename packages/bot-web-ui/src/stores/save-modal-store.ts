f7fd31b5578e3c04901bb32ee20b0d661d313213
rupato-deriv <rupato@deriv.com>
2022-11-09 14:26:37 +0800

    updated save modal store

--- packages/bot-web-ui/src/stores/save-modal-store.ts
+++ packages/bot-web-ui/src/stores/save-modal-store.ts
@@ -1,112 +1,102 @@
-import { action, observable, makeObservable } from 'mobx';
-import { runGroupedEvents, load, config } from '@deriv/bot-skeleton';
+import { observable, action, makeObservable } from 'mobx';
+import { localize } from '@deriv/translations';
+import { saveWorkspaceToRecent, save_types, save, updateWorkspaceName } from '@deriv/bot-skeleton';
+import { button_status } from 'Constants/button-status';
 import RootStore from './root-store';
 
-interface IToolbarStore {
-    is_animation_info_modal_open: boolean;
-    is_dialog_open: boolean;
-    file_name: { [key: string]: string };
-    has_undo_stack: boolean;
-    has_redo_stack: boolean;
-    toggleAnimationInfoModal: () => void;
-    onResetClick: () => void;
-    closeResetDialog: () => void;
-    onResetOkButtonClick: () => void;
-    onSortClick: () => void;
-    onUndoClick: (is_redo: boolean) => void;
-    onZoomInOutClick: (is_zoom_in: boolean) => void;
-    setHasUndoStack: () => void;
-    setHasRedoStack: () => void;
+interface ISaveModalStore {
+    is_save_modal_open: boolean;
+    button_status: { [key: string]: string } | number;
+    bot_name: { [key: string]: string } | string;
+    toggleSaveModal: () => void;
+    validateBotName: (values: string) => { [key: string]: string };
+    onConfirmSave: () => void;
+    updateBotName: (bot_name: { [key: string]: string } | string) => void;
+    setButtonStatus: (status: { [key: string]: string } | string | number) => void;
 }
 
-export default class ToolbarStore implements IToolbarStore {
+export default class SaveModalStore implements ISaveModalStore {
     root_store: RootStore;
 
     constructor(root_store: RootStore) {
         makeObservable(this, {
-            is_animation_info_modal_open: observable,
-            is_dialog_open: observable,
-            file_name: observable,
-            has_undo_stack: observable,
-            has_redo_stack: observable,
-            toggleAnimationInfoModal: action.bound,
-            onResetClick: action.bound,
-            closeResetDialog: action.bound,
-            onResetOkButtonClick: action.bound,
-            onUndoClick: action.bound,
-            setHasUndoStack: action.bound,
-            setHasRedoStack: action.bound,
+            is_save_modal_open: observable,
+            button_status: observable,
+            bot_name: observable,
+            toggleSaveModal: action.bound,
+            validateBotName: action.bound,
+            onConfirmSave: action.bound,
+            updateBotName: action.bound,
+            onDriveConnect: action.bound,
+            setButtonStatus: action.bound,
         });
 
         this.root_store = root_store;
     }
+    is_save_modal_open = false;
+    button_status = button_status.NORMAL;
+    bot_name = '';
 
-    is_animation_info_modal_open = false;
-    is_dialog_open = false;
-    file_name = config.default_file_name;
-    has_undo_stack = false;
-    has_redo_stack = false;
-
-    toggleAnimationInfoModal = (): void => {
-        this.is_animation_info_modal_open = !this.is_animation_info_modal_open;
-    };
-
-    onResetClick = (): void => {
-        this.is_dialog_open = true;
-    };
-
-    closeResetDialog = (): void => {
-        this.is_dialog_open = false;
-    };
-
-    onResetOkButtonClick = (): void => {
-        runGroupedEvents(
-            false,
-            () => {
-                const workspace = Blockly.derivWorkspace;
-                workspace.current_strategy_id = Blockly.utils.genUid();
-                load({
-                    block_string: workspace.cached_xml.main,
-                    file_name: config.default_file_name,
-                    workspace,
-                });
-            },
-            'reset'
-        );
-        this.is_dialog_open = false;
-
-        const { run_panel } = this.root_store;
-        if (run_panel.is_running) {
-            this.root_store.run_panel.stopBot();
+    toggleSaveModal = (): void => {
+        if (!this.is_save_modal_open) {
+            this.setButtonStatus(button_status.NORMAL);
         }
+
+        this.is_save_modal_open = !this.is_save_modal_open;
     };
 
-    onSortClick = () => {
-        Blockly.derivWorkspace.cleanUp();
+    validateBotName = (values: string): { [key: string]: string } => {
+        const errors = {};
+
+        if (values.bot_name.trim() === '') {
+            errors.bot_name = localize('Strategy name cannot be empty');
+        }
+
+        return errors;
     };
 
-    onUndoClick = (is_redo: boolean): void => {
-        Blockly.Events.setGroup('undo_clicked');
-        Blockly.derivWorkspace.undo(is_redo);
-        Blockly.svgResize(Blockly.derivWorkspace); // Called for CommentDelete event.
-        this.setHasRedoStack();
-        this.setHasUndoStack();
-        Blockly.Events.setGroup(false);
+    async onConfirmSave({ is_local, save_as_collection, bot_name }) {
+        this.setButtonStatus(button_status.LOADING);
+
+        const { saveFile } = this.root_store.google_drive;
+        const xml = Blockly.Xml.workspaceToDom(Blockly.derivWorkspace);
+
+        xml.setAttribute('is_dbot', 'true');
+        xml.setAttribute('collection', save_as_collection ? 'true' : 'false');
+
+        if (is_local) {
+            save(bot_name, save_as_collection, xml);
+        } else {
+            await saveFile({
+                name: bot_name,
+                content: Blockly.Xml.domToPrettyText(xml),
+                mimeType: 'application/xml',
+            });
+
+            this.setButtonStatus(button_status.COMPLETED);
+        }
+
+        this.updateBotName(bot_name);
+        saveWorkspaceToRecent(xml, is_local ? save_types.LOCAL : save_types.GOOGLE_DRIVE);
+        this.toggleSaveModal();
+    }
+
+    updateBotName = (bot_name: { [key: string]: string } | string): void => {
+        this.bot_name = bot_name;
+        updateWorkspaceName();
     };
 
-    onZoomInOutClick = (is_zoom_in: boolean): void => {
-        const workspace = Blockly.derivWorkspace;
-        const metrics = workspace.getMetrics();
-        const addition = is_zoom_in ? 1 : -1;
+    async onDriveConnect() {
+        const { google_drive } = this.root_store;
 
-        workspace.zoom(metrics.viewWidth / 2, metrics.viewHeight / 2, addition);
-    };
+        if (google_drive.is_authorised) {
+            google_drive.signOut();
+        } else {
+            google_drive.signIn();
+        }
+    }
 
-    setHasUndoStack = (): void => {
-        this.has_undo_stack = Blockly.derivWorkspace?.hasUndoStack();
-    };
-
-    setHasRedoStack = (): void => {
-        this.has_redo_stack = Blockly.derivWorkspace?.hasRedoStack();
+    setButtonStatus = (status: { [key: string]: string } | string | number): void => {
+        this.button_status = status;
     };
 }

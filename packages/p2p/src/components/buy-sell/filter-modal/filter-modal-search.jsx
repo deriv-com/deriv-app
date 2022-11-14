9e5c6c1945aa2dd4bcd3b7b0a10de94c3ca02efd
ameerul hady <ameerul@deriv.com>
2022-11-14 12:21:01 +0800

    wrapped filterModalResults in observer as it wasnt rerendering

--- packages/p2p/src/components/buy-sell/filter-modal/filter-modal-search.jsx
+++ packages/p2p/src/components/buy-sell/filter-modal/filter-modal-search.jsx
@@ -14,7 +14,7 @@ const FilterModalSearch = () => {
     const returnedFunction = debounce(() => {
         my_profile_store.getPaymentMethodsList();
         buy_sell_store.setIsFilterModalLoading(false);
-    }, 1000);
+    }, 400);
 
     const onSearch = ({ search }) => {
         my_profile_store.setSearchTerm(search.trim());

e6af86fd39c51e22ae4be39c375a4cd73d9422bc
Adrienne Rio <adrienne@deriv.com>
2022-11-16 15:20:50 +0800

    updated changes

--- packages/p2p/src/components/online-status/online-status-icon.jsx
+++ packages/p2p/src/components/online-status/online-status-icon.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import { observer } from 'mobx-react-lite';
 
-const OnlineStatusIcon = ({ is_online }) => {
+const OnlineStatusIcon = ({ is_online, size = '1em' }) => {
     return (
         <div
             className={classNames('online-status__icon', {
                 'online-status__icon--offline': is_online === 0,
                 'online-status__icon--online': is_online === 1,
             })}
+            style={{
+                width: size,
+                height: size,
+            }}
         />
     );
+};
+
+OnlineStatusIcon.propTypes = {
+    is_online: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]).isRequired,
+    size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
 export default observer(OnlineStatusIcon);

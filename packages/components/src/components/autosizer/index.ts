13a08644c67f78f2bbb6407204a3a7001728d7fd
iman <iman@firstsource.tech>
2022-11-20 16:05:40 +0330

    fix: changed declaration file location

--- packages/components/src/components/autosizer/index.ts
+++ packages/components/src/components/autosizer/index.ts
@@ -1,7 +1,8 @@
 /* eslint @typescript-eslint/triple-slash-reference: "off" */
-/// <reference path="../../../@types/@enykeev/react-virtualized/dist/es/AutoSizer.d.ts" />
+/// <reference path="AutoSizer.d.ts" />
 
 import AutoSizer from '@enykeev/react-virtualized/dist/es/AutoSizer';
 import './autosizer.scss';
 
 export default AutoSizer;
+//test

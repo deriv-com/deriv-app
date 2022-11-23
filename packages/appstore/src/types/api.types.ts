ecd77fc5f8ba26522240d0824e5e9515a294559a
Farrah Mae Ochoa <farrah@deriv.com>
2022-11-23 12:13:09 +0400

    Revert "Mahdiyeh/Trading hub dashboard (#6795)"
    This reverts commit 38d139a6f0be1f079450d1bb3fbdd829fea88586.

--- packages/appstore/src/types/api.types.ts
+++ packages/appstore/src/types/api.types.ts
@@ -1,6 +1,12 @@
+import { ResidenceList, Authorize } from '@deriv/api-types';
+
 export type TErrorResponse =
     | {
           message: string;
           code: string;
       }
     | undefined;
+
+export type ResidenceListItem = ResidenceList[0];
+
+export type TAccountList = Authorize['account_list'];
--- packages/cfd/src/Components/cfd-real-account-display.tsx
+++ packages/cfd/src/Components/cfd-real-account-display.tsx
@@ -7,8 +7,8 @@ import specifications, { TSpecifications } from '../Constants/cfd-specifications
 import { CFDAccountCard } from './cfd-account-card';
 import { general_messages } from '../Constants/cfd-shared-strings';
 import { DetailsOfEachMT5Loginid } from '@deriv/api-types';
-import { TTradingPlatformAccounts, TCFDPlatform } from './props.types';
-import { TObjectCFDAccount } from '../Containers/cfd-dashboard';
+import { TExistingData, TTradingPlatformAccounts, TCFDPlatform } from './props.types';
+import { TObjectCFDAccount } from 'Containers/cfd-dashboard';
 
 type TStandPoint = {
     financial_company: string;

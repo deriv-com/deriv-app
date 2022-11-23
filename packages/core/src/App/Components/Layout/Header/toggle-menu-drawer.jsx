2ee80969dbb3e4d8ed6199ed4c22cc4f5d8be480
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2022-11-23 10:59:56 +0400

    Revert "thisyahlen/ bugfix: exit trader hub not redirecting to dtrader (#6922)"
    This reverts commit 0eaaa1f3789df83343dcaf1c184d6c9712919772.

--- packages/core/src/App/Components/Layout/Header/toggle-menu-drawer.jsx
+++ packages/core/src/App/Components/Layout/Header/toggle-menu-drawer.jsx
@@ -9,7 +9,7 @@ import { BinaryLink } from 'App/Components/Routes';
 import getRoutesConfig from 'App/Constants/routes-config';
 import { changeLanguage } from 'Utils/Language';
 import LiveChat from 'App/Components/Elements/LiveChat';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import useLiveChat from 'App/Components/Elements/LiveChat/use-livechat.ts';
 
 const MenuLink = ({
@@ -311,24 +311,10 @@ const ToggleMenuDrawer = React.forwardRef(
         };
         const { pathname: route } = useLocation();
 
-        const history = useHistory();
-
         const is_trading_hub_category =
             route.startsWith(routes.trading_hub) ||
             route.startsWith(routes.cashier) ||
             route.startsWith(routes.account);
-
-        const tradingHubRedirect = () => {
-            if (is_pre_appstore) {
-                setIsPreAppStore(false);
-                toggleDrawer();
-                history.push(routes.root);
-            } else {
-                setIsPreAppStore(true);
-                toggleDrawer();
-                history.push(routes.trading_hub);
-            }
-        };
 
         return (
             <React.Fragment>
@@ -373,7 +359,10 @@ const ToggleMenuDrawer = React.forwardRef(
                                                 className={`header__menu--back-to-ui${is_dark_mode ? '--dark' : ''}`}
                                                 type='button'
                                                 large
-                                                onClick={tradingHubRedirect}
+                                                onClick={() => {
+                                                    setIsPreAppStore(false);
+                                                    toggleDrawer();
+                                                }}
                                             >
                                                 <Text
                                                     className={`header__menu--back-to-ui-text${
@@ -413,7 +402,10 @@ const ToggleMenuDrawer = React.forwardRef(
                                                     }`}
                                                     type='button'
                                                     large
-                                                    onClick={tradingHubRedirect}
+                                                    onClick={() => {
+                                                        setIsPreAppStore(false);
+                                                        toggleDrawer();
+                                                    }}
                                                 >
                                                     <Text
                                                         className={`header__menu--back-to-ui-text${
@@ -567,7 +559,10 @@ const ToggleMenuDrawer = React.forwardRef(
                                                 }`}
                                                 type='button'
                                                 large
-                                                onClick={tradingHubRedirect}
+                                                onClick={() => {
+                                                    setIsPreAppStore(true);
+                                                    toggleDrawer();
+                                                }}
                                             >
                                                 <Text className='header__menu--trading-hub-text' size='xs'>
                                                     {localize("Trader's hub beta")}

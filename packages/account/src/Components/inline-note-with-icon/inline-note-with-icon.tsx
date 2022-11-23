977ae539ab3d9542a8f047ecab88783d188568f1
sanjam chhatwal <sanjam@deriv.com>
2022-11-23 16:13:06 +0400

    TS Migration Inline Icon with Tests

--- packages/account/src/Components/inline-note-with-icon/inline-note-with-icon.tsx
+++ packages/account/src/Components/inline-note-with-icon/inline-note-with-icon.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { HTMLAttributes } from 'react';
 import { Icon, Text } from '@deriv/components';
 
-const InlineNoteWithIcon = ({ icon, message, title }) => {
+type TInlineNoteWithIconExtend = {
+    icon: string;
+    message: string;
+    title: string;
+};
+
+export type TInlineNoteWithIcon = HTMLAttributes<HTMLInputElement | HTMLLabelElement> &
+    React.PropsWithChildren<TInlineNoteWithIconExtend>;
+
+const InlineNoteWithIcon = ({ icon, message, title }: TInlineNoteWithIcon) => {
     return (
         <div className='da-inline-note-with-icon'>
             <div>
@@ -14,12 +22,6 @@ const InlineNoteWithIcon = ({ icon, message, title }) => {
             </Text>
         </div>
     );
-};
-
-InlineNoteWithIcon.propTypes = {
-    icon: PropTypes.string.isRequired,
-    message: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
 };
 
 export default InlineNoteWithIcon;

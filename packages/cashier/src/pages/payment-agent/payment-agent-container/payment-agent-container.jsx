42f2ba2a8d93d925b5acca07c9d4d5996bf059dc
Bahar <bahar@regentmarkets.com>
2022-11-23 11:45:53 +0800

    feat: pa infobox

--- packages/cashier/src/pages/payment-agent/payment-agent-container/payment-agent-container.jsx
+++ packages/cashier/src/pages/payment-agent/payment-agent-container/payment-agent-container.jsx
@@ -5,12 +5,14 @@ import { DesktopWrapper, Dropdown, Icon, Loading, MobileWrapper, SelectNative, T
 import { localize, Localize } from '@deriv/translations';
 import SideNote from 'Components/side-note';
 import { connect } from 'Stores/connect';
+import MissingPaymentMethodNote from '../missing-payment-method-note';
 import PaymentAgentCard from '../payment-agent-card';
 import PaymentAgentDisclaimer from '../payment-agent-disclaimer';
 import PaymentAgentReceipt from '../payment-agent-receipt';
 import PaymentAgentSearchBox from '../payment-agent-search-box';
 import PaymentAgentUnlistedWithdrawForm from '../payment-agent-unlisted-withdraw-form';
 import PaymentAgentWithdrawConfirm from '../payment-agent-withdraw-confirm';
+import './payment-agent-container.scss';
 
 const PaymentAgentSearchWarning = () => {
     return (
@@ -91,6 +93,9 @@ const PaymentAgentContainer = ({
                     <PaymentAgentDisclaimer />
                 </SideNote>
             )}
+            <SideNote className='payment-agent-list__side-note-second' has_title={false} is_mobile>
+                <MissingPaymentMethodNote />
+            </SideNote>
             <div className='payment-agent-list__list-header'>
                 {is_deposit ? (
                     <Text as='p' line_height='s' size='xs'>

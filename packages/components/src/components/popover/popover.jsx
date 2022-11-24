6bca951e44806725ba5e7e180b19a1505f9e2586
iman <iman@firstsource.tech>
2022-11-24 20:06:38 +0330

    fix: fixed popover position issue in DBot page

--- packages/components/src/components/popover/popover.jsx
+++ packages/components/src/components/popover/popover.jsx
@@ -67,7 +67,7 @@ const Popover = ({
                             ? is_open ?? ((is_hovered && message) || (is_bubble_hover_enabled && is_bubble_hovered))
                             : is_open ?? (is_hovered && message)
                     }
-                    position={alignment}
+                    positions={[].concat(alignment)}
                     transitionDuration={0.25}
                     padding={margin + 8}
                     containerClassName={classNames({

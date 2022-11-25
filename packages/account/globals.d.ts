3952f20ff117364ba51e5f7a0baa557244f4ad55
tay suisin <suisin@regentmarkets.com>
2022-11-25 14:48:11 +0800

    refactor: changed open_chat_window to packages/core/globals.d.ts

--- packages/account/globals.d.ts
+++ packages/account/globals.d.ts
@@ -1,11 +1,3 @@
-export {};
-declare global {
-    interface Window {
-        LC_API: {
-            open_chat_window: VoidFunction;
-        };
-    }
-}
 declare module '*.svg' {
     const content: any;
     export default content;

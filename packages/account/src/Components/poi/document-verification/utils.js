f3efa1b5e7fbac3ca112ab74c6d386e8af0aad8e
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-11-25 16:58:01 +0100

    refactor: cleaning up code

--- packages/account/src/Components/poi/document-verification/utils.js
+++ packages/account/src/Components/poi/document-verification/utils.js
@@ -1,19 +1,19 @@
-const ID_PATTERN = '0123456789';
-const STEPS = 5; // Steps start at 0
 const PATTERN_SIZE = 5;
-const pattern_array = [];
 
 export const recurringNumberRegex = document_number => document_number.match(/([0-9])\1{4,}/g);
 
 const createDocumentPatterns = () => {
-    let pattern_end = PATTERN_SIZE;
+    const ID_PATTERN = '0123456789';
+    const STEPS = 5; // Steps start at 0
+    const reverse_pattern = ID_PATTERN.split('').reverse().join('');
+    const pattern_array = [];
 
     for (let step = 0; step < STEPS; step++) {
-        pattern_end = PATTERN_SIZE + step;
+        const pattern_end = PATTERN_SIZE + step;
         pattern_array.push(ID_PATTERN.substring(step, pattern_end));
 
         // Reverse version of the pattern, example: 9876543210
-        pattern_array.push(ID_PATTERN.split('').reverse().join('').substring(step, pattern_end));
+        pattern_array.push(reverse_pattern.substring(step, pattern_end));
     }
 
     return pattern_array;

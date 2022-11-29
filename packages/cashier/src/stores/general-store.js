5d9c9430ad9dcaadf8d09f59231f12dc9b07c212
vinu-deriv <100689171+vinu-deriv@users.noreply.github.com>
2022-11-29 18:34:53 +0530

    Vinu/79411/derivez cashier transfer (#6896)
    * feat: :sparkles: derivez transfer for derivgo
    
    * added addition check to verify before removing derivez account
    
    * feat: additional design change fore derivez
    
    * fix: hide account number for derivez
    
    * fix: hide account number for derivez in transfer receipt

--- packages/cashier/src/stores/general-store.js
+++ packages/cashier/src/stores/general-store.js
@@ -331,6 +331,7 @@ export default class GeneralStore extends BaseStore {
 
     async onMountCommon(should_remount) {
         const { client, common, modules } = this.root_store;
+        const { is_from_derivgo } = common;
         const { account_transfer, onramp, payment_agent, payment_agent_transfer, transaction_history } =
             modules.cashier;
 
@@ -359,7 +360,7 @@ export default class GeneralStore extends BaseStore {
             }
 
             if (!account_transfer.accounts_list.length) {
-                account_transfer.sortAccountsTransfer();
+                account_transfer.sortAccountsTransfer(null, is_from_derivgo);
             }
 
             if (!payment_agent.is_payment_agent_visible && window.location.pathname.endsWith(routes.cashier_pa)) {

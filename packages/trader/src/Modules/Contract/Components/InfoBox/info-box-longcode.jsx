7e936aa1aad81c43138874845484973a09241228
Likhith Kolayari <likhith@regentmarkets.com>
2022-12-05 19:21:46 +0400

    feat: :sparkles: incorporated chart floating message change for vanilla trade

--- packages/trader/src/Modules/Contract/Components/InfoBox/info-box-longcode.jsx
+++ packages/trader/src/Modules/Contract/Components/InfoBox/info-box-longcode.jsx
@@ -2,13 +2,35 @@ import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Icon, Text } from '@deriv/components';
+import { localize, Localize } from '@deriv/translations';
 
-const InfoBoxLongcode = ({ contract_info }) => {
+const generateMessage = (is_vanilla, contract_info) => {
+    if (is_vanilla) {
+        return (
+            <Localize
+                i18n_default_text='<0>For {{contract_type}}:</0> Earn payout if {{index_name}} is {{strike_status}} than the strike price at the expiry time. Your payout is zero if the market is {{market_status}} than the strike price at the expiry time'
+                components={[<strong key={0} />]}
+                values={{
+                    contract_type:
+                        contract_info.contract_type === 'VANILLALONGCALL' ? localize('Call') : localize('Put'),
+                    index_name: contract_info.display_name,
+                    strike_status:
+                        contract_info.contract_type === 'VANILLALONGCALL' ? localize('higher') : localize('lower'),
+                    market_status:
+                        contract_info.contract_type === 'VANILLALONGCALL' ? localize('lower') : localize('higher'),
+                }}
+            />
+        );
+    }
+    return contract_info.longcode;
+};
+
+const InfoBoxLongcode = ({ contract_info, is_vanilla }) => {
     return (
         <div className='info-box-longcode'>
             <Icon icon='IcContractFlag' className='info-box-longcode-icon' size={24} />
             <Text size='xs' className='info-box-longcode-text'>
-                {contract_info.longcode}
+                {is_vanilla ? generateMessage(is_vanilla, contract_info) : contract_info.longcode}
             </Text>
         </div>
     );

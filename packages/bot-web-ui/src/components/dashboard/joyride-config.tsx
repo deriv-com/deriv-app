18ed5f4098f1e96a8e8197c4e66608fe15d14fac
rupato-deriv <rupato@deriv.com>
2022-12-06 17:26:22 +0800

    fix: rename--tour-button

--- packages/bot-web-ui/src/components/dashboard/joyride-config.tsx
+++ packages/bot-web-ui/src/components/dashboard/joyride-config.tsx
@@ -197,6 +197,7 @@ export const DBOT_ONBOARDING = [
             />
         ),
         disableOverlay: false,
+        locale: { last: 'Next' },
         ...joyride_props,
     },
 ];
@@ -336,6 +337,7 @@ export const BOT_BUILDER_TOUR = [
             />,
         ],
         disableOverlay: true,
+        locale: { last: 'Next' },
         ...joyride_props,
     },
 ];

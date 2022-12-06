864ffc752944787beb1e88696df5daec5fe0c340
Likhith Kolayari <likhith@regentmarkets.com>
2022-12-06 08:57:55 +0400

    feat: :art: incorporated chart long code message for vanilla

--- packages/trader/src/Modules/Contract/Components/InfoBox/info-box-longcode.jsx
+++ packages/trader/src/Modules/Contract/Components/InfoBox/info-box-longcode.jsx
@@ -1,36 +1,29 @@
 import { observer } from 'mobx-react';
-import PropTypes from 'prop-types';
+import PropTypes, { bool } from 'prop-types';
 import React from 'react';
 import { Icon, Text } from '@deriv/components';
 import { localize, Localize } from '@deriv/translations';
 
-const generateMessage = (is_vanilla, contract_info) => {
-    if (is_vanilla) {
-        return (
-            <Localize
-                i18n_default_text='<0>For {{contract_type}}:</0> Earn payout if {{index_name}} is {{strike_status}} than the strike price at the expiry time. Your payout is zero if the market is {{market_status}} than the strike price at the expiry time'
-                components={[<strong key={0} />]}
-                values={{
-                    contract_type:
-                        contract_info.contract_type === 'VANILLALONGCALL' ? localize('Call') : localize('Put'),
-                    index_name: contract_info.display_name,
-                    strike_status:
-                        contract_info.contract_type === 'VANILLALONGCALL' ? localize('higher') : localize('lower'),
-                    market_status:
-                        contract_info.contract_type === 'VANILLALONGCALL' ? localize('lower') : localize('higher'),
-                }}
-            />
-        );
-    }
-    return contract_info.longcode;
-};
+const generateMessageForVanillaTrade = contract_info => (
+    <Localize
+        i18n_default_text='<0>For {{contract_type}}:</0> Earn payout if {{index_name}} is {{strike_status}} than the strike price at the expiry time. Your payout is zero if the market is {{market_status}} than the strike price at the expiry time'
+        components={[<strong key={0} />]}
+        values={{
+            contract_type: contract_info.contract_type === 'VANILLALONGCALL' ? localize('Call') : localize('Put'),
+            index_name: contract_info.display_name,
+            strike_status: contract_info.contract_type === 'VANILLALONGCALL' ? localize('higher') : localize('lower'),
+            market_status: contract_info.contract_type === 'VANILLALONGCALL' ? localize('lower') : localize('higher'),
+        }}
+    />
+);
 
-const InfoBoxLongcode = ({ contract_info, is_vanilla }) => {
+const InfoBoxLongcode = ({ contract_info }) => {
+    const is_vanilla = ['VANILLALONGCALL', 'VANILLALONGPUT'].includes(contract_info.contract_type);
     return (
         <div className='info-box-longcode'>
             <Icon icon='IcContractFlag' className='info-box-longcode-icon' size={24} />
             <Text size='xs' className='info-box-longcode-text'>
-                {is_vanilla ? generateMessage(is_vanilla, contract_info) : contract_info.longcode}
+                {is_vanilla ? generateMessageForVanillaTrade(contract_info) : contract_info.longcode}
             </Text>
         </div>
     );
@@ -39,6 +32,7 @@ const InfoBoxLongcode = ({ contract_info, is_vanilla }) => {
 InfoBoxLongcode.propTypes = {
     contract_info: PropTypes.object,
     longcode: PropTypes.string,
+    is_vanilla: bool,
 };
 
 export default observer(InfoBoxLongcode);

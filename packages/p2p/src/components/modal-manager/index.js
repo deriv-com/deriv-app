972a60d7ca0fde50208637519f02eb79ee65666e
Adrienne Rio <adrienne@deriv.com>
2022-12-07 17:50:14 +0800

    refactor: fix import path for modal form

--- packages/p2p/src/components/modal-manager/index.js
+++ packages/p2p/src/components/modal-manager/index.js
@@ -1,4 +1,3 @@
-// import ModalManager from './modal-manager';
 import ModalForm from './modal-form';
 import ModalManagerContextProvider from './modal-manager-context-provider';
 import { useModalManagerContext } from './modal-manager-context';

ab8ddd8ce6022d44910b790d8bf229ac0bec525d
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-12-12 15:34:40 +0330

    refactor: remove chai from tests

--- packages/core/src/App/Components/Elements/NotificationMessage/__tests__/close-button.spec.jsx
+++ packages/core/src/App/Components/Elements/NotificationMessage/__tests__/close-button.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CloseButton from '../close-button.jsx';
@@ -9,10 +8,10 @@ configure({ adapter: new Adapter() });
 describe('CloseButton', () => {
     it('should render one <CloseButton /> component', () => {
         const wrapper = shallow(<CloseButton />);
-        expect(wrapper).to.have.length(1);
+        expect(wrapper).toHaveLength(1);
     });
     it('should render one <CloseButton /> component', () => {
         const wrapper = shallow(<CloseButton />);
-        expect(wrapper.prop('onClick').isRequired).to.be.true();
+        expect(wrapper.prop('onClick').isRequired).toBe(true);
     });
 });
--- packages/core/src/App/Components/Elements/NotificationMessage/__tests__/notification.spec.jsx
+++ packages/core/src/App/Components/Elements/NotificationMessage/__tests__/notification.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Notification from '../notification.jsx';
@@ -9,6 +8,6 @@ configure({ adapter: new Adapter() });
 describe('Notification', () => {
     it('should render one <Notification /> component', () => {
         const wrapper = shallow(<Notification />);
-        expect(wrapper).to.have.length(1);
+        expect(wrapper).toHaveLength(1);
     });
 });

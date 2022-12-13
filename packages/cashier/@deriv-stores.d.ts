be007ec376d244e2568106cc01bc15029f3b90f3
Farzin Mirzaie <farzin@deriv.com>
2022-12-13 13:46:24 +0800

    feat(stores): :sparkles: add example `CounterStore`

--- packages/cashier/@deriv-stores.d.ts
+++ packages/cashier/@deriv-stores.d.ts
@@ -1,8 +1,8 @@
-import type { TRootStore } from '@deriv/stores/types';
+import type { TStore } from '@deriv/stores';
 import type CashierStore from './src/stores/cashier-store';
 
 declare module '@deriv/stores' {
-    export function useStore(): TRootStore & {
+    export function useStore(): TStore & {
         modules: {
             cashier: CashierStore;
         };

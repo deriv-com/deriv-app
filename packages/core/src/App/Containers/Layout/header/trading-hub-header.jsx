a8f07005e985ddf83a8c832c483febc478bfaa94
hirad-rewok <91878582+hirad-rewok@users.noreply.github.com>
2022-12-13 13:27:44 +0330

    Hirad/80671/user keeps clicking next button of onboarding (#7105)
    * fix: fixed the issue where onboarding was shown on every visit
    
    * fix: merged set and unset functioned

--- packages/core/src/App/Containers/Layout/header/trading-hub-header.jsx
+++ packages/core/src/App/Containers/Layout/header/trading-hub-header.jsx
@@ -45,7 +45,7 @@ const RedirectToOldInterface = () => {
     );
 };
 
-const TradingHubOnboarding = ({ is_dark_mode }) => {
+const TradingHubOnboarding = ({ is_dark_mode, setIsOnboardingVisited }) => {
     const history = useHistory();
     return (
         <div className='trading-hub-header__tradinghub--onboarding'>
@@ -62,6 +62,7 @@ const TradingHubOnboarding = ({ is_dark_mode }) => {
                         size={20}
                         onClick={() => {
                             history.push(routes.onboarding);
+                            setIsOnboardingVisited(false);
                         }}
                     />
                 </Popover>
@@ -91,6 +92,7 @@ const TradingHubHeader = ({
     loginid,
     is_eu,
     is_eu_country,
+    setIsOnboardingVisited,
     header_extension,
     is_dark_mode,
     is_logged_in,
@@ -175,7 +177,7 @@ const TradingHubHeader = ({
                 <div className='trading-hub-header__menu-right'>
                     <RedirectToOldInterface />
                     <Divider />
-                    <TradingHubOnboarding is_dark_mode={is_dark_mode} />
+                    <TradingHubOnboarding is_dark_mode={is_dark_mode} setIsOnboardingVisited={setIsOnboardingVisited} />
                     <ShowNotifications
                         is_notifications_visible={is_notifications_visible}
                         notifications_count={notifications_count}
@@ -259,6 +261,7 @@ TradingHubHeader.propTypes = {
     toggleNotifications: PropTypes.func,
     is_social_signup: PropTypes.bool,
     location: PropTypes.object,
+    setIsOnboardingVisited: PropTypes.func,
     settings_extension: PropTypes.array,
     is_settings_modal_on: PropTypes.bool,
     menu_items: PropTypes.array,
@@ -295,5 +298,6 @@ export default connect(({ client, common, modules, notifications, ui, menu, trad
     menu_items: menu.extensions,
     replaceCashierMenuOnclick: modules.cashier.general_store.replaceCashierMenuOnclick,
     toggleIsTourOpen: tradinghub.toggleIsTourOpen,
+    setIsOnboardingVisited: tradinghub.setIsOnboardingVisited,
     loginid: client.loginid,
 }))(withRouter(TradingHubHeader));

591e32229f8eb86b6ee07f56bc823e0d49366721
Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>
2022-12-14 11:54:29 +0800

    feat: implement reusable container for platform app container (#45)
    * feat: implement reusable container for platform app container
    
    * chore: give variable a more meaningful name
    
    * chore: use a more descriptive variable name

--- packages/appstore/src/components/options-multipliers-listing/index.tsx
+++ packages/appstore/src/components/options-multipliers-listing/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Text, StaticUrl } from '@deriv/components';
-import { Localize } from '@deriv/translations';
+import { Localize, localize } from '@deriv/translations';
 import ListingContainer from 'Components/containers/listing-container';
 import { BrandConfig } from 'Constants/platform-config';
 import TradingAppCard from 'Components/containers/trading-app-card';
 import { useStores } from 'Stores/index';
 
 const OptionsAndMultipliersListing = () => {
-    const { tradinghub } = useStores();
+    const { tradinghub, client } = useStores();
+    const is_demo = tradinghub.selected_account_type === 'demo';
+    const has_real_account = client.has_any_real_account && tradinghub.selected_account_type === 'real';
 
     return (
         <ListingContainer
@@ -31,8 +33,17 @@ const OptionsAndMultipliersListing = () => {
             }
             is_deriv_platform
         >
-            {tradinghub.available_platforms.map((p: BrandConfig) => (
-                <TradingAppCard key={`trading_app_card_${p.name}`} {...p} />
+            {!is_demo && !has_real_account && (
+                <div className='full-row'>
+                    <TradingAppCard name={localize('Deriv account')} icon='Options' availability='All' type='get' />
+                </div>
+            )}
+            {tradinghub.available_platforms.map((available_platform: BrandConfig) => (
+                <TradingAppCard
+                    key={`trading_app_card_${available_platform.name}`}
+                    {...available_platform}
+                    type={is_demo || has_real_account ? 'trade' : 'none'}
+                />
             ))}
         </ListingContainer>
     );

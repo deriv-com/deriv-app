e5626e55033468d59ef86360cc1f13500fe84965
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2022-12-15 14:12:00 +0400

    refactor: emailLinkVerifiedModal (#7118)

--- packages/p2p/src/constants/modals.js
+++ packages/p2p/src/constants/modals.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export const modals = {
+    EmailLinkVerifiedModal: React.lazy(() =>
+        import(
+            /* webpackChunkName: "email-link-verified-modal" */ 'Components/modal-manager/modals/email-link-verified-modal'
+        )
+    ),
     MyAdsFloatingRateSwitchModal: React.lazy(() =>
         import(
             /* webpackChunkName: "my-ads-floating-rate-switch-modal" */ 'Components/modal-manager/modals/my-ads-floating-rate-switch-modal'

4da054970f8f601090fcfc1d5df31d18964106af
george-usynin-binary <heorhi@binary.com>
2022-12-16 16:05:08 +0300

    perf: replace connect with useStore, fix tests

--- packages/cashier/src/pages/payment-agent-transfer/__tests__/payment-agent-transfer.spec.js
+++ packages/cashier/src/pages/payment-agent-transfer/__tests__/payment-agent-transfer.spec.js
@@ -5,19 +5,6 @@ import { createBrowserHistory } from 'history';
 import PaymentAgentTransfer from '../payment-agent-transfer';
 import { StoreProvider } from '@deriv/stores';
 
-const mockRootStore = {
-    ui: {
-        is_dark_mode_on: false,
-        toggleAccountsDialog: jest.fn(),
-    },
-};
-
-jest.mock('Stores/connect', () => ({
-    __esModule: true,
-    default: 'mockedDefaultExport',
-    connect: () => Component => Component,
-}));
-
 jest.mock('@deriv/components', () => {
     const original_module = jest.requireActual('@deriv/components');
 
@@ -41,14 +28,7 @@ jest.mock('Pages/payment-agent-transfer/payment-agent-transfer-receipt', () =>
 );
 
 describe('<PaymentAgentTransfer />', () => {
-    const history = createBrowserHistory();
-    const props = {
-        balance: '100',
-        error: {},
-        onMount: jest.fn(),
-        onUnMount: jest.fn(),
-        setActiveTab: jest.fn(),
-    };
+    let history, mockRootStore;
 
     beforeAll(() => {
         const modal_root_el = document.createElement('div');
@@ -60,23 +40,57 @@ describe('<PaymentAgentTransfer />', () => {
         document.body.removeChild(modal_root_el);
     });
 
+    beforeEach(() => {
+        mockRootStore = {
+            client: {
+                balance: '100',
+                is_virtual: false,
+            },
+
+            modules: {
+                cashier: {
+                    payment_agent_transfer: {
+                        error: {},
+                        is_transfer_successful: false,
+                        is_try_transfer_successful: false,
+                        onMountPaymentAgentTransfer: jest.fn(),
+                        resetPaymentAgentTransfer: jest.fn(),
+                    },
+                    general_store: {
+                        is_cashier_locked: false,
+                        is_loading: false,
+                        setActiveTab: jest.fn(),
+                    },
+                },
+            },
+            ui: {
+                is_dark_mode_on: false,
+                toggleAccountsDialog: jest.fn(),
+            },
+        };
+
+        history = createBrowserHistory();
+    });
+
+    const renderPaymentAgentTransfer = () => {
+        return render(
+            <Router history={history}>
+                <StoreProvider store={mockRootStore}>
+                    <PaymentAgentTransfer />
+                </StoreProvider>
+            </Router>
+        );
+    };
+
     it('should render the component', () => {
-        render(<PaymentAgentTransfer {...props} />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedPaymentAgentTransferForm')).toBeInTheDocument();
     });
 
     it('should show the virtual component if client is using demo account', () => {
-        render(
-            <Router history={history}>
-                <PaymentAgentTransfer is_virtual {...props} />
-            </Router>,
-            {
-                wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-            }
-        );
+        mockRootStore.client.is_virtual = true;
+        renderPaymentAgentTransfer();
 
         expect(
             screen.getByText(/You need to switch to a real money account to use this feature./i)
@@ -84,53 +98,43 @@ describe('<PaymentAgentTransfer />', () => {
     });
 
     it('should show the loading component if in loading state', () => {
-        render(<PaymentAgentTransfer is_loading {...props} />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        mockRootStore.modules.cashier.general_store.is_loading = true;
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedLoading')).toBeInTheDocument();
     });
 
     it('should show the cashier locked component if cashier is locked', () => {
-        render(<PaymentAgentTransfer is_cashier_locked {...props} />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        mockRootStore.modules.cashier.general_store.is_cashier_locked = true;
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedCashierLocked')).toBeInTheDocument();
     });
 
     it('should show a popup if there is an error that needs CTA', () => {
-        const cta_error = {
-            is_show_full_page: true,
-        };
-
-        render(<PaymentAgentTransfer {...props} error={cta_error} />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        mockRootStore.modules.cashier.payment_agent_transfer.error.is_show_full_page = true;
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedError')).toBeInTheDocument();
     });
 
     it('should show the no balance component if account has no balance', () => {
-        render(<PaymentAgentTransfer {...props} balance='0' />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        mockRootStore.client.balance = '0';
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedNoBalance')).toBeInTheDocument();
     });
 
     it('should show the confirmation if validations are passed', () => {
-        render(<PaymentAgentTransfer is_try_transfer_successful {...props} />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        mockRootStore.modules.cashier.payment_agent_transfer.is_try_transfer_successful = true;
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedPaymentAgentTransferConfirm')).toBeInTheDocument();
     });
 
     it('should show the receipt if transfer is successful', () => {
-        render(<PaymentAgentTransfer is_transfer_successful {...props} />, {
-            wrapper: ({ children }) => <StoreProvider store={mockRootStore}>{children}</StoreProvider>,
-        });
+        mockRootStore.modules.cashier.payment_agent_transfer.is_transfer_successful = true;
+        renderPaymentAgentTransfer();
 
         expect(screen.getByText('mockedPaymentAgentTransferReceipt')).toBeInTheDocument();
     });

0471d40ffba81c951123bbb795d0559e012212ee
tay suisin <suisin@regentmarkets.com>
2022-12-16 16:45:11 +0800

    refactor: add types to self exclusion context

--- packages/account/src/Components/self-exclusion/self-exclusion-context.tsx
+++ packages/account/src/Components/self-exclusion/self-exclusion-context.tsx
@@ -1,5 +1,22 @@
+import { FormikValues } from 'formik';
 import * as React from 'react';
 
-const SelfExclusionContext = React.createContext(null);
+type TSelfExclusionContext = {
+    footer_ref?: React.RefObject<HTMLElement>;
+    goToConfirm?: () => void;
+    toggleArticle?: () => void;
+    is_app_settings?: boolean;
+    is_wrapper_bypassed?: boolean;
+    is_eu?: boolean;
+    is_uk?: boolean;
+    backToReview?: () => void;
+    backFromConfirmLimits?: () => void;
+    currency?: string | number;
+    currency_display?: string | number;
+    exclusion_texts?: string;
+    state?: FormikValues;
+};
+
+const SelfExclusionContext = React.createContext<TSelfExclusionContext>({});
 
 export default SelfExclusionContext;

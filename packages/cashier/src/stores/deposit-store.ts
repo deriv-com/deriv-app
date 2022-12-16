96aca506141ec79cb71269c7b1943cae2f3dee56
Hamid <hamid@re-work.dev>
2022-12-16 09:03:43 +0330

    hamid/75900/hide-sidenote-when-switch-account (#6441)
    * Empty side notes when switching
    
    * Resolve PR comments
    
    * WIP: update on-ramp.tsx
    
    * Prevent page to be called twice
    
    * Enhance tests
    
    * Remove duplicate setLoading call
    
    * Revert loading test
    
    * Check sidenote function before execute
    
    * Add loading when is_loading is true in cashier container
    
    * trigger tests
    
    * trigger tests
    
    * fix: remove infinite loading of the cashier page

--- packages/cashier/src/stores/deposit-store.ts
+++ packages/cashier/src/stores/deposit-store.ts
@@ -40,7 +40,6 @@ export default class DepositStore {
 
         this.error.setErrorMessage({ code: '', message: '' }, null, false);
         setContainerHeight(0);
-        setLoading(true);
 
         if (!is_session_timeout) {
             checkIframeLoaded();

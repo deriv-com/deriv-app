08cfd5c572e8bbe03141677d22ef3ff6a6c540d4
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-12-27 11:37:56 +0330

    refactor: improve tests

--- packages/core/src/App/Components/Layout/Footer/__tests__/toggle-settings.spec.tsx
+++ packages/core/src/App/Components/Layout/Footer/__tests__/toggle-settings.spec.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { ToggleSettings } from '../toggle-settings.jsx';
 
+jest.mock('Stores/connect', () => ({
+    __esModule: true,
+    default: 'mockedDefaultExport',
+    connect:
+        () =>
+        <T,>(Component: T) =>
+            Component,
+}));
+
 describe('ToggleSettings Component', () => {
     it('should has "ic-settings--active" class when "is_settings_visible" is true', () => {
         render(<ToggleSettings is_settings_visible />);
@@ -15,8 +24,11 @@ describe('ToggleSettings Component', () => {
         expect(icon).toBeInTheDocument();
     });
 
-    it('should open the modal when the user clicked on the link', () => {
-        render(<ToggleSettings />);
+    it('should call "toggleSettings" function when the user clicked on the link', () => {
+        const toggleSettings = jest.fn();
+        render(<ToggleSettings toggleSettings={toggleSettings} />);
         const link = screen.getByTestId('dt_toggle_settings');
+        fireEvent.click(link);
+        expect(toggleSettings).toHaveBeenCalledTimes(1);
     });
 });

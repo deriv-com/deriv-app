31691a29f179e9f8f87af0b883e0c33e409f7e1b
Sergei Baranovski <sergei@deriv.com>
2022-12-28 12:59:02 +0300

    fix: short version

--- packages/account/src/Components/account-limits/account-limits.jsx
+++ packages/account/src/Components/account-limits/account-limits.jsx
@@ -97,14 +97,14 @@ const AccountLimits = ({
 
     const { commodities, forex, indices, synthetic_index } = { ...market_specific };
     const forex_ordered = forex?.slice().sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
-    // sort by level "market/submarket"
-    const derived_ordered = synthetic_index?.slice().sort((a, b) => a.level.localeCompare(b.level));
-    // sort submarkets fields by alphabet
-    if (Array.isArray(derived_ordered)) {
-        derived_ordered.sort((a, b) =>
-            a.level !== 'submarket' || b.level !== 'submarket' ? 0 : a.name.localeCompare(b.name)
+    // sort submarkets by names alphabetically and put 'market' at the beginning
+    const derived_ordered = synthetic_index
+        ?.slice()
+        .sort((a, b) =>
+            a.level === 'submarket' && b.level === 'submarket'
+                ? a.name.localeCompare(b.name)
+                : a.level.localeCompare(b.level)
         );
-    }
 
     const context_value = {
         currency,

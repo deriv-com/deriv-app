f58a847a43fcf351fbc072c7336aaa48c2d08b64
ameerul hady <ameerul@deriv.com>
2022-12-28 16:26:47 +0800

    chore: fixed styling for mobile, added routing to create ad page

--- packages/p2p/src/components/buy-sell/no-ads/no-ads.jsx
+++ packages/p2p/src/components/buy-sell/no-ads/no-ads.jsx
@@ -5,22 +5,30 @@ import { useStores } from 'Stores';
 import './no-ads.scss';
 
 const NoAds = () => {
-    const { buy_sell_store, my_ads_store } = useStores();
+    const { buy_sell_store, general_store, my_ads_store } = useStores();
 
     return (
         <div className='no-ads'>
-            <Icon className='no-ads__icon' icon='IcCashierNoAds' size={128} />
-            <Text align='center' className='no-ads__text' color='general' line_height='m' size='s' weight='bold'>
+            <Icon icon='IcCashierNoAds' size={128} />
+            <Text align='center' className='no-ads__title' color='general' line_height='m' size='s' weight='bold'>
                 <Localize i18n_default_text='No ads here at the moment.' />
             </Text>
-            <Text align='center' color='general' line_height='m' size='s'>
+            <Text className='no-ads__message' align='center' color='general' line_height='m' size='s'>
                 {buy_sell_store.is_buy ? (
                     <Localize i18n_default_text='Looking to sell USD? Create a Sell ad for others to buy USD from you. Hit the button below to create your ad.' />
                 ) : (
                     <Localize i18n_default_text='Looking to buy USD? Create a Buy ad for others to sell USD from you. Hit the button below to create your ad.' />
                 )}
             </Text>
-            <Button className='no-ads__button' primary large onClick={() => my_ads_store.setShowAdForm(true)}>
+            <Button
+                className='no-ads__button'
+                primary
+                large
+                onClick={() => {
+                    general_store.handleTabClick(2);
+                    setTimeout(my_ads_store.setShowAdForm(true), 5000);
+                }}
+            >
                 <Localize i18n_default_text='Create ad' />
             </Button>
         </div>

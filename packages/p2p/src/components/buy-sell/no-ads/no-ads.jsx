d34bdd91f3f84bdeeebdf8c245373f039a363225
ameerul hady <ameerul@deriv.com>
2022-12-28 16:47:52 +0800

    chore: removed (hit the button) text

--- packages/p2p/src/components/buy-sell/no-ads/no-ads.jsx
+++ packages/p2p/src/components/buy-sell/no-ads/no-ads.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, DesktopWrapper, Icon, MobileWrapper, Text } from '@deriv/components';
+import { Button, Icon, Text } from '@deriv/components';
 import { Localize } from 'Components/i18next';
 import { useStores } from 'Stores';
 import './no-ads.scss';
@@ -13,24 +13,13 @@ const NoAds = () => {
             <Text align='center' className='no-ads__title' color='general' line_height='m' size='s' weight='bold'>
                 <Localize i18n_default_text='No ads here at the moment.' />
             </Text>
-            <DesktopWrapper>
-                <Text className='no-ads__message' align='center' color='general' line_height='m' size='s'>
-                    {buy_sell_store.is_buy ? (
-                        <Localize i18n_default_text='Looking to sell USD? Create a Sell ad for others to buy USD from you.' />
-                    ) : (
-                        <Localize i18n_default_text='Looking to buy USD? Create a Buy ad for others to sell USD from you.' />
-                    )}
-                </Text>
-            </DesktopWrapper>
-            <MobileWrapper>
-                <Text className='no-ads__message' align='center' color='general' line_height='m' size='s'>
-                    {buy_sell_store.is_buy ? (
-                        <Localize i18n_default_text='Looking to sell USD? Create a Sell ad for others to buy USD from you. Hit the button below to create your ad.' />
-                    ) : (
-                        <Localize i18n_default_text='Looking to buy USD? Create a Buy ad for others to sell USD from you. Hit the button below to create your ad.' />
-                    )}
-                </Text>
-            </MobileWrapper>
+            <Text className='no-ads__message' align='center' color='general' line_height='m' size='s'>
+                {buy_sell_store.is_buy ? (
+                    <Localize i18n_default_text='Looking to sell USD? Create a Sell ad for others to buy USD from you.' />
+                ) : (
+                    <Localize i18n_default_text='Looking to buy USD? Create a Buy ad for others to sell USD from you.' />
+                )}
+            </Text>
             <Button
                 className='no-ads__button'
                 primary

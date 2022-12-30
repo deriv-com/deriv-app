245d6ca24eeb36f8a23ba4694f42f1a560ad5d71
rupato-deriv <rupato@deriv.com>
2022-12-30 15:22:34 +0800

    chore: removed unsed json

--- packages/bot-web-ui/src/components/dashboard/joyride-config.tsx
+++ packages/bot-web-ui/src/components/dashboard/joyride-config.tsx
@@ -444,50 +444,6 @@ export const BOT_BUILDER_TOUR = [
     },
 ];
 
-export const trading_hub_contents = {
-    step1: {
-        component: null,
-        footer_header: localize('Get started on DBot'),
-        footer_text: localize('Hi [first name]! Hit Start for a quick tour to help you get started.'),
-        has_next_content: false,
-    },
-    step2: {
-        component: null,
-        footer_header: localize('Trading accounts'),
-        footer_text: localize(
-            'These are the trading accounts available to you. You can click on an account to find out more.'
-        ),
-        has_next_content: false,
-    },
-    step3: {
-        component: null,
-        footer_header: localize('CFDs, Options or Multipliers'),
-        eu_footer_header: localize('CFDs or Multipliers'),
-        footer_text: localize('You can choose between CFD trading accounts or Options and Multipliers accounts'),
-        eu_footer_text: localize('You can choose between CFD trading accounts and Multipliers accounts'),
-        has_next_content: false,
-    },
-    step4: {
-        component: null,
-        footer_header: localize('‘Get’ the accounts you want'),
-        footer_text: localize('Click the ‘Get’ button to create an account'),
-        has_next_content: false,
-    },
-    step5: {
-        component: null,
-        footer_header: localize('Top-up your account'),
-        footer_text: localize('Once you have an account click ‘Top-up’ to add funds '),
-        has_next_content: false,
-    },
-    step6: {
-        component: null,
-        footer_header: localize('Start trading'),
-        footer_text: localize('Click ‘Trade’ to start trading with your account'),
-        has_next_content: true,
-        next_content: localize('Start trading'),
-    },
-};
-
 export const DBOT_ONBOARDING_MOBILE = [
     {
         header: 'Build from scratch',

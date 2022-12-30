20dd25960a8c8fd5dc29f502af6dfd8fb0a04140
rupato-deriv <rupato@deriv.com>
2022-12-30 15:40:15 +0800

    chore: changed function name

--- packages/bot-web-ui/src/components/dashboard/tour-slider.tsx
+++ packages/bot-web-ui/src/components/dashboard/tour-slider.tsx
@@ -44,7 +44,7 @@ const TourSlider = ({
         setTourActive(false);
     };
 
-    const renderClose = () => {
+    const onTourClose = () => {
         if (step === 7) {
             onCloseTour();
             setHasTourEnded(true);
@@ -121,7 +121,7 @@ const TourSlider = ({
                             type='danger'
                             onClick={() => {
                                 onChange('inc');
-                                renderClose();
+                                onTourClose();
                             }}
                         >
                             {localize('Next')}

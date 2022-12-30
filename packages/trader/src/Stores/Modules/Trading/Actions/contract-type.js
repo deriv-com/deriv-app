a951cfcab8db4913a51e254e79d65aa613552178
aizad-deriv <103104395+aizad-deriv@users.noreply.github.com>
2022-12-30 14:07:02 +0800

    chore: fix bugs occur when contract type is change (#7228)

--- packages/trader/src/Stores/Modules/Trading/Actions/contract-type.js
+++ packages/trader/src/Stores/Modules/Trading/Actions/contract-type.js
@@ -1,6 +1,6 @@
 import { ContractType } from 'Stores/Modules/Trading/Helpers/contract-type';
 
 export const onChangeContractTypeList = ({ contract_types_list, contract_type }) => {
-    ContractType.getContractType(contract_types_list, contract_type);
+    return ContractType.getContractType(contract_types_list, contract_type);
 };
 export const onChangeContractType = store => ContractType.getContractValues(store);

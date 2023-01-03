194fea2b2dc01c870762f155960cb3674f085d9f
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2023-01-03 13:30:54 +0330

    refactor: contract-type-dialog tests

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-dialog.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-dialog.spec.tsx
@@ -1,6 +1,5 @@
-// TODO refactor old tests in this component
 import React from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ContractTypeDialog from '../contract-type-dialog.jsx';
 
 const list = [
@@ -8,13 +7,14 @@ const list = [
     { contract_types: [{ value: 'second-value' }], label: 'second-item' },
 ];
 
-const MockChildren = () => <div data-testid='dt_child_test_id' />;
-
 describe('ContractTypeDialog Component', () => {
     it('should render "children" when passed in', () => {
-        render(<ContractTypeDialog list={list} />);
-        // const child = screen.getByTestId('dt_child_test_id');
-        // const lala = within(child).getByTestId('dt_child_test_id');
-        // // expect(pElement).toBeInTheDocument();
+        render(
+            <ContractTypeDialog is_open categories={[]} list={list}>
+                <div data-testid='dt_child' />
+            </ContractTypeDialog>
+        );
+        const child = screen.getByTestId('dt_child');
+        expect(child).toBeInTheDocument();
     });
 });

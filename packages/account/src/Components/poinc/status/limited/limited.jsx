a7951ed6e1246d814862469e90ba1249c045a671
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-01-05 10:18:40 +0300

    refactor: limited status text

--- packages/account/src/Components/poinc/status/limited/limited.jsx
+++ packages/account/src/Components/poinc/status/limited/limited.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Icon } from '@deriv/components';
-import { localize } from '@deriv/translations';
+import { Localize, localize } from '@deriv/translations';
 import IconMessageContent from 'Components/icon-message-content';
 
 export const PoincLimited = () => (
     <IconMessageContent
-        message={localize("You've reached the limit for uploading your documents.")}
+        message={localize("You've reached the limit for uploading your documents")}
+        text={<Localize i18n_default_text='Please check your email inbox for more details.' />}
         icon={<Icon icon='IcPoincUnverified' size={128} />}
     />
 );

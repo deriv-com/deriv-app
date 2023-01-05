e32f86ee92eac1dbc30e2f35d91876111261817d
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-01-05 10:44:04 +0300

    refactor: limited status test

--- packages/account/src/Components/poinc/status/limited/__tests__/limited.spec.js
+++ packages/account/src/Components/poinc/status/limited/__tests__/limited.spec.js
@@ -5,6 +5,7 @@ import { PoincLimited } from '../limited';
 describe('<PoincLimited/>', () => {
     it('should render PoincLimited component', () => {
         render(<PoincLimited />);
-        expect(screen.getByText(/you've reached the limit for uploading your documents\./i)).toBeInTheDocument();
+        expect(screen.getByText(/you've reached the limit for uploading your documents/i)).toBeInTheDocument();
+        expect(screen.getByText(/please check your email inbox for more details/i)).toBeInTheDocument();
     });
 });

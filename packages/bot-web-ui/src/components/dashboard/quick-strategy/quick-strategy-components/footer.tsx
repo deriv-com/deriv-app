d46fb715d01a87cf10f357ca092a5c7261ff4cd3
maryia-matskevich-deriv <103181650+maryia-matskevich-deriv@users.noreply.github.com>
2023-01-06 09:17:18 +0300

    maryia/83951/feat: responsive Performance panel and Run panel (#7249)
    * feat: performance panel
    
    * feat: when the user open the app the 1 time performance panel should not be visible for mobile
    
    * feat: summary tab as per new design
    
    * feat: markets dropdown has been blocked on charts tab
    
    * chore: update packages/bot-web-ui/src/components/summary/summary.scss
    
    * refactor: update packages/bot-web-ui/src/components/dashboard/dashboard-component/index.scss
    
    * feat: improve perfomance panel
    
    * fix: code smells

--- packages/bot-web-ui/src/components/dashboard/quick-strategy/quick-strategy-components/footer.tsx
+++ packages/bot-web-ui/src/components/dashboard/quick-strategy/quick-strategy-components/footer.tsx
@@ -11,41 +11,45 @@ const QuickStrategyFooter = ({
     setFieldValue,
     submitForm,
     setActiveTab,
-}: TQuickStrategyFooter) => (
-    <div
-        className={classNames('quick-strategy__form-footer', {
-            'quick-strategy__form-footer--active-keyboard': is_onscreen_keyboard_active,
-        })}
-    >
-        <Button.Group>
-            <Button
-                type='button'
-                id='db-quick-strategy__button-edit'
-                text={localize('Create and edit')}
-                is_disabled={!is_submit_enabled}
-                secondary
-                large
-                onClick={() => {
-                    setFieldValue('button', 'edit');
-                    submitForm();
-                    setActiveTab(1);
-                }}
-            />
-            <Button
-                type='button'
-                id='db-quick-strategy__button-run'
-                text={localize('Run')}
-                is_disabled={!is_submit_enabled || is_stop_button_visible}
-                primary
-                large
-                onClick={() => {
-                    setActiveTab(1);
-                    setFieldValue('button', 'run');
-                    submitForm();
-                }}
-            />
-        </Button.Group>
-    </div>
-);
+}: TQuickStrategyFooter) => {
+    function handleCreateEdit() {
+        setFieldValue('button', 'edit');
+        submitForm();
+        setActiveTab(1);
+    }
+    function handleRun() {
+        setActiveTab(1);
+        setFieldValue('button', 'run');
+        submitForm();
+    }
+    return (
+        <div
+            className={classNames('quick-strategy__form-footer', {
+                'quick-strategy__form-footer--active-keyboard': is_onscreen_keyboard_active,
+            })}
+        >
+            <Button.Group>
+                <Button
+                    type='button'
+                    id='db-quick-strategy__button-edit'
+                    text={localize('Create and edit')}
+                    is_disabled={!is_submit_enabled}
+                    secondary
+                    large
+                    onClick={handleCreateEdit}
+                />
+                <Button
+                    type='button'
+                    id='db-quick-strategy__button-run'
+                    text={localize('Run')}
+                    is_disabled={!is_submit_enabled || is_stop_button_visible}
+                    primary
+                    large
+                    onClick={handleRun}
+                />
+            </Button.Group>
+        </div>
+    );
+};
 
 export default React.memo(QuickStrategyFooter);

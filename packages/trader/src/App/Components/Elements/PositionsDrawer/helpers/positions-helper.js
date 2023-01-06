2ff988ba8f3b0944ccf1d063f0b016fa7c538af7
Akmal Djumakhodjaev <akmal@binary.com>
2023-01-06 17:19:05 +0800

    fix: daily VO contracts to show in recent positions

--- packages/trader/src/App/Components/Elements/PositionsDrawer/helpers/positions-helper.js
+++ packages/trader/src/App/Components/Elements/PositionsDrawer/helpers/positions-helper.js
@@ -41,10 +41,11 @@ const digitTypeMap = contract_info => ({
 export const filterByContractType = ({ contract_type, shortcode }, trade_contract_type) => {
     const is_call_put = isCallPut(trade_contract_type);
     const is_high_low = isHighLow({ shortcode });
+    const is_vanilla = isVanillaContract(contract_type);
     const trade_types = is_call_put
         ? ['CALL', 'CALLE', 'PUT', 'PUTE']
         : getContractTypesConfig()[trade_contract_type]?.trade_types;
     const match = trade_types?.includes(contract_type);
     if (trade_contract_type === 'high_low') return is_high_low;
-    return match && !is_high_low;
+    return match && (is_vanilla || !is_high_low);
 };

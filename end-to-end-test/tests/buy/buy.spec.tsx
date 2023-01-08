b0df028fd3b0edd3ec53b3614cd093be49b0ca07
Ako <ali.hosseini@re-work.dev>
2023-01-08 15:41:23 +0330

    test: add dummy test to check the signup flow

--- end-to-end-test/tests/buy/buy.spec.tsx
+++ end-to-end-test/tests/buy/buy.spec.tsx
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
-import OnboardingFlow from '../onboarding/onboarding';
 
 test.describe('Change endpoint, Signup and login', () => {
     test('Should change the endpoint and sign up', async ({ page }) => {
-        const onboarding = new OnboardingFlow(page);
-        await onboarding.signUp();
+        await page.goto(process.env.APP_URL!);
+        // now we are logged in to the app and ready to test buu
+        await expect(page).toHaveTitle('Trader | Deriv');
         // create a new user account and logged in
         // session before running each test
     });

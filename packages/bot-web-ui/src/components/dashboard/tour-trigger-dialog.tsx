04459a3ef7b02dfcb538b16554e09348b37764d7
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-01-09 15:49:57 +0300

    fix: code smell of sonar cloud in tour-trigger-dialog.tsx(2)

--- packages/bot-web-ui/src/components/dashboard/tour-trigger-dialog.tsx
+++ packages/bot-web-ui/src/components/dashboard/tour-trigger-dialog.tsx
@@ -138,8 +138,10 @@ const TourTriggrerDialog = ({
     const onHandleConfirm = React.useCallback(() => {
         const status = tour_status_ended.key === 'finished';
         toggleTour(status ? false : !has_tour_ended, 'onConfirm');
-        return status ? (tour_status_ended.key = '') : null;
+        tour_status_ended.key = '';
+        return status ? tour_status_ended.key : null;
     }, [has_tour_ended]);
+
     return (
         <div>
             <Dialog

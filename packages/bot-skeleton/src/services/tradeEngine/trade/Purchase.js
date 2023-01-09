69825d33ad1aa81c393f9eef324dc000bff62937
Carol Sachdeva <58209918+carol-binary@users.noreply.github.com>
2023-01-09 18:28:41 +0800

    Revert V20230106_0 (#7334)
    * Revert "fix: summary panel flickering (#7321)"
    
    This reverts commit b134c888eecbda9424540bc05509837821d5089f.
    
    * Revert "translations: 📚 sync translations with crowdin (#7316)"
    
    This reverts commit 37266a2e0317f5d03b8d1dddd943030cf4c17e6d.
    
    * Revert "fix: dbot performance issue -- unified websocket connection and moved… (#7103)"
    
    This reverts commit 1abb7debf6a5a6ad12700e48e621bffbbf3372d1.

--- packages/bot-skeleton/src/services/tradeEngine/trade/Purchase.js
+++ packages/bot-skeleton/src/services/tradeEngine/trade/Purchase.js
@@ -3,7 +3,6 @@ import { BEFORE_PURCHASE } from './state/constants';
 import { contractStatus, info, log } from '../utils/broadcast';
 import { getUUID, recoverFromError, doUntilDone } from '../utils/helpers';
 import { log_types } from '../../../constants/messages';
-import { api_base } from '../../api/api-base';
 
 let delayIndex = 0;
 let purchase_reference;
@@ -42,7 +41,7 @@ export default Engine =>
                     buy_price: buy.buy_price,
                 });
             };
-            const action = () => api_base.api.send({ buy: id, price: askPrice });
+            const action = () => this.api.send({ buy: id, price: askPrice });
             this.isSold = false;
             contractStatus({
                 id: 'contract.purchase_sent',

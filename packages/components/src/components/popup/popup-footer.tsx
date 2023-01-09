f0c9abf30202f6d45fbfa0711d9becc607f1e3d2
tay suisin <suisin@regentmarkets.com>
2023-01-09 14:36:40 +0800

    chore: update to uuse propswithchildren

--- packages/components/src/components/popup/popup-footer.tsx
+++ packages/components/src/components/popup/popup-footer.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 type TPopupFooter = {
-    children: React.ReactNode;
     has_separator: boolean;
 };
 
-const PopupFooter = ({ children, has_separator }: TPopupFooter) => (
+const PopupFooter = ({ children, has_separator }: React.PropsWithChildren<TPopupFooter>) => (
     <div
         className={classNames('dc-popup-footer', {
             'dc-popup-footer__separator': has_separator,

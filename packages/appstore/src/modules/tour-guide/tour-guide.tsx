52b1c404703cfd34bfa0de2c2cb1c389341be684
Hirad <hirad@re-work.dev>
2023-01-10 13:15:00 +0330

    fix: added new functionality to the first step next button in tour guide

--- packages/appstore/src/modules/tour-guide/tour-guide.tsx
+++ packages/appstore/src/modules/tour-guide/tour-guide.tsx
@@ -8,15 +8,16 @@ import {
     tour_styles,
     tour_step_locale,
     tour_styles_dark_mode,
-    eu_tour_step_locale,
+    high_risk_tour_step_config,
+    high_risk_tour_step_locale,
 } from 'Constants/tour-steps-config-new';
 import { useStores } from 'Stores/index';
-import { routes } from '@deriv/shared';
+import { routes, ContentFlag } from '@deriv/shared';
 import { Button } from '@deriv/components';
 
 const TourGuide = () => {
     const { traders_hub, ui } = useStores();
-    const { is_tour_open, toggleIsTourOpen, setIsOnboardingVisited } = traders_hub;
+    const { is_tour_open, toggleIsTourOpen, setIsOnboardingVisited, content_flag, selectAccountType } = traders_hub;
     const { is_dark_mode_on } = ui;
 
     const history = useHistory();
@@ -32,7 +33,7 @@ const TourGuide = () => {
         </div>
     );
 
-    eu_tour_step_locale.last = (
+    high_risk_tour_step_locale.last = (
         <div
             onClick={() => {
                 setIsOnboardingVisited(true);
@@ -43,7 +44,29 @@ const TourGuide = () => {
         </div>
     );
 
-    if (tour_step_config.length === joyride_index + 1) {
+    if (joyride_index === 0) {
+        tour_step_locale.next = (
+            <div
+                onClick={() => {
+                    selectAccountType('real');
+                }}
+            >
+                <Localize i18n_default_text='Next' />
+            </div>
+        );
+
+        high_risk_tour_step_locale.next = (
+            <div
+                onClick={() => {
+                    selectAccountType('real');
+                }}
+            >
+                <Localize i18n_default_text='Next' />
+            </div>
+        );
+    }
+
+    if (tour_step_config.length === joyride_index + 1 || high_risk_tour_step_config.length === joyride_index + 1) {
         tour_step_locale.back = (
             <Button
                 has_effect
@@ -65,7 +88,7 @@ const TourGuide = () => {
             disableScrolling
             hideCloseButton
             disableCloseOnEsc
-            steps={tour_step_config}
+            steps={content_flag === ContentFlag.HIGH_RISK_CR ? high_risk_tour_step_config : tour_step_config}
             styles={is_dark_mode_on ? tour_styles_dark_mode : tour_styles}
             locale={tour_step_locale}
             floaterProps={{

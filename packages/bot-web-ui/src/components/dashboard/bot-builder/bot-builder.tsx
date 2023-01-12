9b773507d73ed908238ff881fec8811363290f63
Farabi <102643568+farabi-deriv@users.noreply.github.com>
2023-01-12 13:24:53 +0800

    Farabi/85641/fix  alignment of google drive popup (#7350)
    * fix: google drive pop up alignment
    
    * fix: text-alignment
    
    * fix: alignment
    
    * fix: google drive import scroll
    
    * fix: popup to appear on top of other components

--- packages/bot-web-ui/src/components/dashboard/bot-builder/bot-builder.tsx
+++ packages/bot-web-ui/src/components/dashboard/bot-builder/bot-builder.tsx
@@ -5,10 +5,11 @@ import { connect } from 'Stores/connect';
 import ReactJoyride from 'react-joyride';
 import classNames from 'classnames';
 import LoadModal from 'Components/load-modal';
+import { DesktopWrapper, MobileWrapper } from '@deriv/components';
+import SaveModal from 'Components/save-modal';
 import WorkspaceWrapper from './workspace-wrapper';
 import { BOT_BUILDER_TOUR, handleJoyrideCallback } from '../joyride-config';
 import TourSlider from '../tour-slider';
-import { DesktopWrapper, MobileWrapper } from '@deriv/components';
 
 type TBotBuilder = {
     app: AppStore;
@@ -35,62 +36,65 @@ const BotBuilder = ({
     }, []);
 
     return (
-        <div
-            className={classNames('bot-builder', {
-                'bot-builder--active': active_tab === 1 && !is_preview_on_popup,
-                'bot-builder--inactive': is_preview_on_popup,
-            })}
-        >
-            {is_preview_on_popup ? null : (
-                <div
-                    id='scratch_div'
-                    style={{
-                        width: 'calc(100vw - 3.2rem)',
-                        height: 'var(--bot-content-height)',
-                    }}
-                >
-                    <WorkspaceWrapper />
-                    {has_started_bot_builder_tour && active_tab === 1 && !has_started_onboarding_tour && (
-                        <>
-                            <MobileWrapper>
-                                <TourSlider />
-                            </MobileWrapper>
-                            <DesktopWrapper>
-                                <ReactJoyride
-                                    steps={BOT_BUILDER_TOUR}
-                                    run={is_tour_running}
-                                    continuous
-                                    showProgress
-                                    callback={handleJoyrideCallback}
-                                    locale={{ back: 'Previous' }}
-                                    styles={{
-                                        options: {
-                                            arrowColor: 'var(--general-main-2)',
-                                            backgroundColor: 'var(--general-main-2)',
-                                            primaryColor: 'var(--brand-red-coral)',
-                                            textColor: 'var(--text-general)',
-                                            spotlightShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
-                                        },
-                                        buttonBack: {
-                                            border: '0.1rem solid var(--text-less-prominent)',
-                                            marginRight: '1rem',
-                                            borderRadius: '0.4rem',
-                                            color: 'var(--text-general)',
-                                            padding: '0.6rem',
-                                        },
-                                        tooltipContent: {
-                                            padding: '0 1rem',
-                                        },
-                                    }}
-                                />
-                            </DesktopWrapper>
-                        </>
-                    )}
-                </div>
-            )}
-            {/* removed this outside from toolbar becuase it needs to loaded seperately without dependency */}
+        <>
+            <div
+                className={classNames('bot-builder', {
+                    'bot-builder--active': active_tab === 1 && !is_preview_on_popup,
+                    'bot-builder--inactive': is_preview_on_popup,
+                })}
+            >
+                {is_preview_on_popup ? null : (
+                    <div
+                        id='scratch_div'
+                        style={{
+                            width: 'calc(100vw - 3.2rem)',
+                            height: 'var(--bot-content-height)',
+                        }}
+                    >
+                        <WorkspaceWrapper />
+                        {has_started_bot_builder_tour && active_tab === 1 && !has_started_onboarding_tour && (
+                            <>
+                                <MobileWrapper>
+                                    <TourSlider />
+                                </MobileWrapper>
+                                <DesktopWrapper>
+                                    <ReactJoyride
+                                        steps={BOT_BUILDER_TOUR}
+                                        run={is_tour_running}
+                                        continuous
+                                        showProgress
+                                        callback={handleJoyrideCallback}
+                                        locale={{ back: 'Previous' }}
+                                        styles={{
+                                            options: {
+                                                arrowColor: 'var(--general-main-2)',
+                                                backgroundColor: 'var(--general-main-2)',
+                                                primaryColor: 'var(--brand-red-coral)',
+                                                textColor: 'var(--text-general)',
+                                                spotlightShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
+                                            },
+                                            buttonBack: {
+                                                border: '0.1rem solid var(--text-less-prominent)',
+                                                marginRight: '1rem',
+                                                borderRadius: '0.4rem',
+                                                color: 'var(--text-general)',
+                                                padding: '0.6rem',
+                                            },
+                                            tooltipContent: {
+                                                padding: '0 1rem',
+                                            },
+                                        }}
+                                    />
+                                </DesktopWrapper>
+                            </>
+                        )}
+                    </div>
+                )}
+                {/* removed this outside from toolbar becuase it needs to loaded seperately without dependency */}
+            </div>
             <LoadModal />
-        </div>
+            <SaveModal />
+        </>
     );
 };
 

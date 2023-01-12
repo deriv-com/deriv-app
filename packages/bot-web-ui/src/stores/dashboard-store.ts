5bc3a8ae010cfcce5e96712af964e58db7ed35da
rupato-deriv <rupato@deriv.com>
2023-01-12 13:53:14 +0800

    fix: removed uncommented code

--- packages/bot-web-ui/src/stores/dashboard-store.ts
+++ packages/bot-web-ui/src/stores/dashboard-store.ts
@@ -177,7 +177,7 @@ export default class DashboardStore implements IDashboardStore {
 
     setFileLoaded = (has_file_loaded: boolean): void => {
         this.has_file_loaded = has_file_loaded;
-        //clearInjectionDiv();
+        clearInjectionDiv();
     };
 
     onCloseDialog = (): void => {

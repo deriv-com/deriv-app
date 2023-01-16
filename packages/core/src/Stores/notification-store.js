a4356f3858ae9a55a388eac6b80b9b5c46999266
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-01-16 10:44:07 +0300

    refactor: remove odd comment

--- packages/core/src/Stores/notification-store.js
+++ packages/core/src/Stores/notification-store.js
@@ -1322,7 +1322,6 @@ export default class NotificationStore extends BaseStore {
         this.p2p_order_props = p2p_order_props;
     }
 
-    //TODO (yauheni-kryzhyk): this method is not used. leaving this for the upcoming new pop-up notifications implementation
     setShouldShowPopups(should_show_popups) {
         this.should_show_popups = should_show_popups;
     }

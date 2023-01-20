2bfd24e5e8ab20db8ca0b49330fd2ce95aeab69e
rupato-deriv <rupato@deriv.com>
2023-01-20 16:42:05 +0800

    fix: minor strategy fix

--- packages/components/stories/icon/icons.js
+++ packages/components/stories/icon/icons.js
@@ -632,6 +632,9 @@ export const icons =
         'IcCurrencyUst',
         'IcCurrencyVirtual',
     ],
+    'derivez': [
+        'IcDerivez',
+    ],
     'dxtrade': [
         'IcDxtradeDerivX',
         'IcDxtradeDerived',

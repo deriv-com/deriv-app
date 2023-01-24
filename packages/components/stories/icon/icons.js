303aa92991ca30824cf168c7582caa9c7ae69566
Ako <ali.hosseini@re-work.dev>
2023-01-24 19:36:50 +0330

    build: add alias to react-virtualized to bind it to its community types

--- packages/components/stories/icon/icons.js
+++ packages/components/stories/icon/icons.js
@@ -605,6 +605,9 @@ export const icons =
         'IcCurrencyUst',
         'IcCurrencyVirtual',
     ],
+    'derivez': [
+        'IcDerivez',
+    ],
     'dxtrade': [
         'IcDxtradeDerivX',
         'IcDxtradeDerived',

08e5b7d6dfd7857c4d1efdbd94baf259ef676ac5
rupato-deriv <97010868+rupato-deriv@users.noreply.github.com>
2023-01-25 19:18:25 +0800

    Rupato/86177/fix  tour guide general fixes (#7390)
    * fix: tour issues
    
    * fix: iphone
    
    * fix: content changes for bot builder tour
    
    * fix: new bot builder changes
    
    * fix: padding
    
    * fix: review comments
    
    * fix: minor strategy fix
    
    * Delete icons.js
    
    * Delete icons.js
    
    * fix: added iconjs file
    
    * fix: added iconjs file
    
    * fix: added iconjs file
    
    * fix: added iconjs file

--- packages/bot-web-ui/src/components/load-modal/local.tsx
+++ packages/bot-web-ui/src/components/load-modal/local.tsx
@@ -6,8 +6,11 @@ import RootStore from 'Stores/root-store';
 import React from 'react';
 import LocalFooter from './local-footer';
 import WorkspaceControl from './workspace-control';
+import classNames from 'classnames';
 
 type TLocalComponentProps = {
+    active_tab: number;
+    has_started_bot_builder_tour: boolean;
     handleFileChange: (
         e: React.MouseEvent | React.FormEvent<HTMLFormElement> | DragEvent,
         is_body?: boolean
@@ -17,7 +20,13 @@ type TLocalComponentProps = {
     setLoadedLocalFile: (loaded_local_file: boolean | null) => void;
 };
 
-const LocalComponent = ({ handleFileChange, loaded_local_file, setLoadedLocalFile }: TLocalComponentProps) => {
+const LocalComponent = ({
+    active_tab,
+    has_started_bot_builder_tour,
+    handleFileChange,
+    loaded_local_file,
+    setLoadedLocalFile,
+}: TLocalComponentProps) => {
     const file_input_ref = React.useRef(null);
     const [is_file_supported, setIsFileSupported] = React.useState(true);
     const is_mobile = isMobile();
@@ -25,7 +34,11 @@ const LocalComponent = ({ handleFileChange, loaded_local_file, setLoadedLocalFil
     if (loaded_local_file && is_file_supported) {
         return (
             <div className='load-strategy__container load-strategy__container--has-footer'>
-                <div className='load-strategy__local-preview'>
+                <div
+                    className={classNames('load-strategy__local-preview', {
+                        'load-strategy__local-preview--active': active_tab === 1 && has_started_bot_builder_tour,
+                    })}
+                >
                     <div className='load-strategy__title'>
                         <Localize i18n_default_text='Preview' />
                     </div>
@@ -93,7 +106,9 @@ const LocalComponent = ({ handleFileChange, loaded_local_file, setLoadedLocalFil
     );
 };
 
-const Local = connect(({ load_modal }: RootStore) => ({
+const Local = connect(({ load_modal, dashboard }: RootStore) => ({
+    active_tab: dashboard.active_tab,
+    has_started_bot_builder_tour: dashboard.has_started_bot_builder_tour,
     handleFileChange: load_modal.handleFileChange,
     is_open_button_loading: load_modal.is_open_button_loading,
     loaded_local_file: load_modal.loaded_local_file,

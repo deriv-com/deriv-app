83279c4f75b4751a01fc6c25318cf1caef8cd4fa
ameerul-deriv <103412909+ameerul-deriv@users.noreply.github.com>
2023-01-27 14:10:18 +0800

    chore: added comment

--- packages/p2p/src/stores/general-store.js
+++ packages/p2p/src/stores/general-store.js
@@ -397,6 +397,7 @@ export default class GeneralStore extends BaseStore {
                     const server_time = this.props.server_time.get();
                     const blocked_until_moment = toMoment(blocked_until);
 
+                    // Need isAfter instead of setTimeout as setTimeout has a max delay of 24.8 days
                     if (server_time.isAfter(blocked_until_moment)) this.setUserBlockedUntil(null);
                 }
             }

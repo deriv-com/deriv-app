c4c3c5023694237fc0eebb194c148fdeb7d90b50
Farrah Mae Ochoa <farrah@deriv.com>
2023-01-30 12:24:18 +0400

    fix: missing p2p_advertiser_info

--- packages/core/src/Stores/notification-store.js
+++ packages/core/src/Stores/notification-store.js
@@ -274,7 +274,7 @@ export default class NotificationStore extends BaseStore {
             p2p_advertiser_info,
         } = this.root_store.client;
         const { is_p2p_visible, p2p_completed_orders } = this.root_store.modules.cashier.general_store;
-        const { upgradable_daily_limits } = p2p_advertiser_info;
+        const { upgradable_daily_limits } = p2p_advertiser_info || {};
         const { max_daily_buy, max_daily_sell } = upgradable_daily_limits || {};
         const { is_10k_withdrawal_limit_reached } = this.root_store.modules.cashier.withdraw;
         const { current_language, selected_contract_type } = this.root_store.common;

4d5402c9273bcba5889b4c469b6950554f277192
Muhammad Hamza <120543468+hamza-deriv@users.noreply.github.com>
2023-02-08 15:43:52 +0800

    hamza/ feat: Barrier Line Colors depend upon the contract type (#80)
    * feat: Barrier Line Colors depend upon the contract type
    
    * chore: Reafctor Barrier Line Colors depend upon the contract type v1.2
    
    * chore: Reafctor Barrier Line Colors depend upon the contract type v1.3

--- packages/trader/src/Stores/Modules/Trading/trade-store.js
+++ packages/trader/src/Stores/Modules/Trading/trade-store.js
@@ -681,14 +681,27 @@ export default class TradeStore extends BaseStore {
 
         if (isBarrierSupported(contract_type)) {
             const color = this.root_store.ui.is_dark_mode_on ? BARRIER_COLORS.DARK_GRAY : BARRIER_COLORS.GRAY;
+            const turbos_color = this.getTurbosColor(contract_type);
+
             // create barrier only when it's available in response
             this.main_barrier = new ChartBarrierStore(barrier || high_barrier, low_barrier, this.onChartBarrierChange, {
-                color,
+                color: isTurbosContract(contract_type) ? turbos_color : color,
                 not_draggable: isTurbosContract(contract_type),
             });
             // this.main_barrier.updateBarrierShade(true, contract_type);
         } else {
             this.main_barrier = null;
+        }
+    };
+
+    // Get Turbos barrier Line Color Based on the Contract Type
+    getTurbosColor = type => {
+        switch (type) {
+            case 'TURBOSSHORT':
+                return BARRIER_COLORS.RED;
+            case 'TURBOSLONG':
+            default:
+                return BARRIER_COLORS.GRAY;
         }
     };
 

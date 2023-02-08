658c554822b2b8449abecf341682867a17ab972d
Sandeep <sandeep@deriv.com>
2023-02-08 18:36:12 +0800

    perf: improve proposal_open_contract api calls

--- packages/bot-skeleton/src/services/tradeEngine/trade/OpenContract.js
+++ packages/bot-skeleton/src/services/tradeEngine/trade/OpenContract.js
@@ -1,8 +1,6 @@
 import { getRoundedNumber } from '@deriv/shared';
 import { sell, openContractReceived } from './state/actions';
 import { contractStatus, contract as broadcastContract } from '../utils/broadcast';
-import { doUntilDone } from '../utils/helpers';
-import DBotStore from '../../../scratch/dbot-store';
 import { api_base } from '../../api/api-base';
 
 export default Engine =>
@@ -53,25 +51,6 @@ export default Engine =>
 
         subscribeToOpenContract(contract_id = this.contractId) {
             this.contractId = contract_id;
-            const request_object = {
-                proposal_open_contract: 1,
-                contract_id,
-                subscribe: 1,
-            };
-
-            doUntilDone(() => api_base.api.send(request_object))
-                .then(data => {
-                    const { populateConfig } = DBotStore.instance;
-                    populateConfig(data.proposal_open_contract);
-                    this.openContractId = data.proposal_open_contract.id;
-                })
-                .catch(error => {
-                    if (error.error.code !== 'AlreadySubscribed') {
-                        doUntilDone(() => api_base.api.send(request_object)).then(
-                            response => (this.openContractId = response.proposal_open_contract.id)
-                        );
-                    }
-                });
         }
 
         setContractFlags(contract) {

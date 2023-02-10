92ae37077611ab9bb92bdb8d6c073402e080d942
Akmal Djumakhodjaev <akmal@binary.com>
2023-02-10 16:42:24 +0800

    feat: Remove official logos for unofficial domains (#7031)
    * feat: remove official logos for unofficial domains
    
    * fix: empty commit
    
    * fix: revert merge issues on master
    
    * fix: typescript error

--- packages/shared/src/utils/brand/brand.ts
+++ packages/shared/src/utils/brand/brand.ts
@@ -12,7 +12,7 @@ type TLandingCompany = {
 
 type TPlatform = {
     name: string;
-    icon?: string;
+    icon: string;
 };
 
 type TPlatforms = {
@@ -26,6 +26,12 @@ type TPlatforms = {
     go: TPlatform;
 };
 
+const isDomainAllowed = (domain_name: string) => {
+    // This regex will match any official deriv production and testing domain names.
+    // Allowed deriv domains: binary.sx, binary.com, deriv.com, deriv.be, deriv.me and their subdomains.
+    return /^(((.*)\.)?(binary\.(sx|com)|deriv.(com|me|be)))$/.test(domain_name);
+};
+
 export const getLegalEntityName = (landing_company: keyof TLandingCompany) => {
     return config_data.legal_entities[landing_company];
 };
@@ -35,5 +41,17 @@ export const getBrandWebsiteName = () => {
 };
 
 export const getPlatformSettings = (platform_key: keyof TPlatforms): TPlatform => {
-    return config_data.platforms[platform_key];
+    const allowed_config_data = config_data.platforms[platform_key];
+
+    if (!isDomainAllowed(window.location.host)) {
+        // Remove all official platform logos if the app is hosted under unofficial domain
+        allowed_config_data.icon = '';
+    }
+
+    return allowed_config_data;
+};
+
+export const getAppstorePlatforms = () => {
+    const platform_data: Record<string, Record<string, string>> = config_data.platforms_appstore;
+    return Object.keys(platform_data).map(key => platform_data[key]);
 };

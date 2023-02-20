3fe530be6003f200b18aee6180e854a3716a732c
Aizad Ridzo <103104395+aizad-deriv@users.noreply.github.com>
2023-02-20 15:48:01 +0800

    Aizad/73978/routes ts (#50)
    * Merge branch 'Improvement/72232/Reports_TS_migration' of github.com:mahdiyeh-deriv/deriv-app into ts-migration
    
    * chore: finalising routes folder migration for Reports package 📦

--- packages/reports/src/Components/Routes/index.js
+++
@@ -1,7 +0,0 @@
-import BinaryLink from './binary-link';
-import RouteWithSubRoutes from './route-with-sub-routes';
-import BinaryRoutes from './binary-routes';
-
-export * from './helpers';
-export { BinaryLink, RouteWithSubRoutes };
-export default BinaryRoutes;

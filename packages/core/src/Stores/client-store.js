8e9219497f84cd0da65f14f2665f98f37bb2bea1
Niloofar Sadeghi <niloofar.sadeghi@re-work.dev>
2023-02-20 11:12:18 +0330

    fix: security-issues: Prototype-polluting assignment

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1239,12 +1239,13 @@ export default class ClientStore extends BaseStore {
     }
 
     responseAuthorize(response) {
-        this.accounts[this.loginid].email = response.authorize.email;
-        this.accounts[this.loginid].currency = response.authorize.currency;
-        this.accounts[this.loginid].is_virtual = +response.authorize.is_virtual;
-        this.accounts[this.loginid].session_start = parseInt(moment().utc().valueOf() / 1000);
-        this.accounts[this.loginid].landing_company_shortcode = response.authorize.landing_company_name;
-        this.accounts[this.loginid].country = response.country;
+        const new_accounts = Object.create(this.accounts);
+        new_accounts[this.loginid].email = response.authorize.email;
+        new_accounts[this.loginid].currency = response.authorize.currency;
+        new_accounts[this.loginid].is_virtual = +response.authorize.is_virtual;
+        new_accounts[this.loginid].session_start = parseInt(moment().utc().valueOf() / 1000);
+        new_accounts[this.loginid].landing_company_shortcode = response.authorize.landing_company_name;
+        new_accounts[this.loginid].country = response.country;
         this.updateAccountList(response.authorize.account_list);
         this.upgrade_info = this.getBasicUpgradeInfo();
         this.user_id = response.authorize.user_id;
@@ -1955,7 +1956,8 @@ export default class ClientStore extends BaseStore {
     }
 
     setResidence(residence) {
-        this.accounts[this.loginid].residence = residence;
+        const new_accounts = Object.create(this.accounts);
+        new_accounts[this.loginid].residence = residence;
     }
 
     setCitizen(citizen) {
@@ -1963,7 +1965,8 @@ export default class ClientStore extends BaseStore {
     }
 
     setEmail(email) {
-        this.accounts[this.loginid].email = email;
+        const new_accounts = Object.create(this.accounts);
+        new_accounts[this.loginid].email = email;
         this.email = email;
     }
 
@@ -2072,7 +2075,8 @@ export default class ClientStore extends BaseStore {
             const loginid = obj_params[`acct${i}`];
             const token = obj_params[`token${i}`];
             if (loginid && token) {
-                client_object[loginid].token = token;
+                const new_client_object = Object.create(client_object);
+                new_client_object[loginid].token = token;
             }
             i++;
         }

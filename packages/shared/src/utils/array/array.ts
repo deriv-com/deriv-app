a9c669dc442c12a94b3db19d3cc6a5ab2fa4e408
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-02-20 18:29:52 +0300

    refactor: packages shared utils array ts migration

--- packages/shared/src/utils/array/array.ts
+++ packages/shared/src/utils/array/array.ts
@@ -1,4 +1,4 @@
-export const shuffleArray = array => {
+export function shuffleArray<T>(array: T[]): T[] {
     if (Array.isArray(array)) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -6,4 +6,4 @@ export const shuffleArray = array => {
         }
     }
     return array;
-};
+}

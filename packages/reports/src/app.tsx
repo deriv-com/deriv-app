f36bd8b7dbb80df8ec97b744160cb51fe067c345
Akmal Djumakhodjaev <akmal@binary.com>
2023-02-22 14:41:57 +0800

    Akmal / chore: migrate app.jsx to typescript (#37)
    * chore: migrate app.jsx to typescript
    
    * chore: utilize TRootStore type
    
    * chore: replace type

--- packages/reports/src/app.tsx
+++ packages/reports/src/app.tsx
@@ -1,11 +1,18 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import Routes from 'Containers/routes';
 import { MobxContentProvider } from 'Stores/connect';
 import 'Sass/app.scss';
 import initStore from './init-store'; // eslint-disable-line import/extensions
+import type { TStores } from '@deriv/stores';
 
-const App = ({ passthrough }) => {
+type TAppProps = {
+    passthrough: {
+        root_store: TStores;
+        WS: Record<string, any>;
+    };
+};
+
+const App = ({ passthrough }: TAppProps) => {
     const [root_store] = React.useState(initStore(passthrough.root_store, passthrough.WS));
 
     return (
@@ -15,13 +22,6 @@ const App = ({ passthrough }) => {
             </React.Fragment>
         </MobxContentProvider>
     );
-};
-
-App.propTypes = {
-    passthrough: PropTypes.shape({
-        root_store: PropTypes.object,
-        WS: PropTypes.object,
-    }),
 };
 
 export default App;
--- packages/reports/src/index.tsx
+++ packages/reports/src/index.tsx
@@ -4,7 +4,7 @@ import { Loading } from '@deriv/components';
 import 'promise-polyfill';
 
 const App = makeLazyLoader(
-    () => import(/* webpackChunkName: "reports-app", webpackPreload: true */ './app.jsx'),
+    () => import(/* webpackChunkName: "reports-app", webpackPreload: true */ './app'),
     () => <Loading />
 )();
 

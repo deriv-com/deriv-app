25798b33391fb70de136d3352d5352644ea53070
Rostik Kayko <119863957+rostislav-deriv@users.noreply.github.com>
2023-03-01 10:46:42 +0300

    Update packages/hooks/src/useCashierLocked.ts
    Co-authored-by: Farzin Mirzaie <72082844+farzin-deriv@users.noreply.github.com>

--- packages/hooks/src/useCashierLocked.ts
+++ packages/hooks/src/useCashierLocked.ts
@@ -5,7 +5,7 @@ const useCashierLocked = () => {
     const { account_status } = client;
 
     const is_cashier_locked =
-        !!account_status?.status && account_status.status.some(status_name => status_name === 'cashier_locked');
+        account_status.status.some(status => status === 'cashier_locked');
 
     return is_cashier_locked;
 };

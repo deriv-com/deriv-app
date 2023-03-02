b9d7dd339b61134bb6db60ec7be2b01a87211e55
Likhith Kolayari <likhith@regentmarkets.com>
2023-03-02 12:20:00 +0400

    feat: incorporated review comments

--- packages/account/src/Helpers/utils.js
+++ packages/account/src/Helpers/utils.js
@@ -1,5 +1,8 @@
 import { localize } from '@deriv/translations';
-
+/**
+ * @param {string} selected_doc  - Could be one of the following: 'drivers_license', 'ssnit', 'id_card', 'passport'
+ * @returns {string} - Returns the placeholder text for the document number input
+ */
 export const generatePlaceholderText = selected_doc => {
     switch (selected_doc) {
         case 'drivers_license':

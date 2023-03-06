59d0f1cb6099e0d64eba9d139a21131e9ad43538
Shaheer <shaheer@deriv.com>
2023-03-06 10:55:05 +0400

    fix: :construction: prevents auto-logout after 2FA enable/disable

--- packages/account/src/Sections/Security/TwoFactorAuthentication/digit-form.jsx
+++ packages/account/src/Sections/Security/TwoFactorAuthentication/digit-form.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import classNames from 'classnames';
 import { Formik, Form, Field } from 'formik';
 import { Input, Button } from '@deriv/components';
-import { localize, getLanguage } from '@deriv/translations';
-import { getPropertyValue, WS, redirectToLogin } from '@deriv/shared';
+import { localize } from '@deriv/translations';
+import { getPropertyValue, WS } from '@deriv/shared';
 
-const DigitForm = ({ is_enabled, setTwoFAStatus, logoutClient }) => {
+const DigitForm = ({ is_enabled, setTwoFAStatus }) => {
     const [is_success, setSuccess] = React.useState(false);
-    const button_text = is_enabled ? localize('Disable 2FA') : localize('Enable');
+    const button_text = is_enabled ? localize('Disable') : localize('Enable');
 
     const initial_form = {
         digit_code: '',
@@ -50,7 +50,6 @@ const DigitForm = ({ is_enabled, setTwoFAStatus, logoutClient }) => {
             setSuccess(true);
             resetForm();
             setTwoFAStatus(is_enabled_response);
-            logoutClient().then(() => redirectToLogin(false, getLanguage()));
         }
     };
 
@@ -66,7 +65,7 @@ const DigitForm = ({ is_enabled, setTwoFAStatus, logoutClient }) => {
                                     data-lpignore='true'
                                     type='text'
                                     className='two-factor__input'
-                                    label={localize('6 digit code')}
+                                    label={localize('Authentication code')}
                                     value={values.digit_code}
                                     onChange={handleChange}
                                     onBlur={handleBlur}

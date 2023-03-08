634e42c03da8e10a46941f31c1d05245291b803d
ameerul hady <ameerul@deriv.com>
2023-03-08 15:45:32 +0800

    chore: fixed issues with server time

--- packages/p2p/src/stores/sendbird-store.js
+++ packages/p2p/src/stores/sendbird-store.js
@@ -1,5 +1,5 @@
 import SendBird from 'sendbird';
-import { epochToMoment } from '@deriv/shared';
+import { epochToMoment, toMoment } from '@deriv/shared';
 import { action, computed, observable, reaction, makeObservable } from 'mobx';
 import BaseStore from 'Stores/base_store';
 import ChatMessage, { convertFromChannelMessage } from 'Utils/chat-message';
@@ -187,7 +187,7 @@ export default class SendbirdStore extends BaseStore {
         const custom_type = '';
 
         this.active_chat_channel.getPreviousMessagesByTimestamp(
-            timestamp || this.root_store.general_store.server_time.get().utc().valueOf(),
+            timestamp || toMoment(this.root_store.general_store.server_time.get()).utc().valueOf(),
             is_inclusive_of_timestamp,
             result_size,
             reverse_results,
@@ -224,7 +224,7 @@ export default class SendbirdStore extends BaseStore {
                 // Refresh chat token ±1 hour before it expires (BE will refresh the token
                 // when we request within 2 hours of the token expiring)
                 const expiry_moment = epochToMoment(service_token.sendbird.expiry_time);
-                const delay_ms = expiry_moment.diff(server_time.get().clone().subtract(1, 'hour'));
+                const delay_ms = expiry_moment.diff(toMoment(server_time.get()).clone().subtract(1, 'hour'));
 
                 this.service_token_timeout = setTimeout(() => getSendbirdServiceToken(), delay_ms);
             });
@@ -385,7 +385,7 @@ export default class SendbirdStore extends BaseStore {
 
         // Add a placeholder message with a pending indicator
         const placeholder_msg_options = {
-            created_at: this.root_store.general_store.server_time.get().utc(),
+            created_at: toMoment(this.root_store.general_store.server_time.get()).utc(),
             chat_channel_url: this.active_chat_channel.url,
             message,
             id: msg_identifier,

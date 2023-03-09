996de361ab0ad3373e29b7080be0d7e22c3c4201
yashim-deriv <yashim@deriv.com>
2023-03-09 13:57:47 +0800

    fix: link with localization

--- packages/appstore/src/constants/platform-config.ts
+++ packages/appstore/src/constants/platform-config.ts
@@ -1,4 +1,4 @@
-import { getPlatformSettingsAppstore, routes, getStaticUrl } from '@deriv/shared';
+import { getUrlBinaryBot, getUrlSmartTrader, getPlatformSettingsAppstore, routes, getStaticUrl } from '@deriv/shared';
 import { localize } from '@deriv/translations';
 import { PlatformIcons } from 'Assets/svgs/trading-platform';
 import { TAccountCategory, TRegionAvailability } from 'Types';
@@ -45,13 +45,13 @@ export const getAppstorePlatforms = (): PlatformConfig[] => [
     {
         name: getPlatformSettingsAppstore('smarttrader').name,
         app_desc: localize('Our legacy options trading platform.'),
-        link_to: routes.smarttrader,
+        link_to: getUrlSmartTrader(),
         is_external: true,
     },
     {
         name: getPlatformSettingsAppstore('bbot').name,
         app_desc: localize('Our legacy automated trading platform.'),
-        link_to: routes.binarybot,
+        link_to: getUrlBinaryBot(),
         is_external: true,
     },
     {

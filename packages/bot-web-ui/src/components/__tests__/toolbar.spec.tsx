88adaee02d5b115259a59f50f708a683ff870f4f
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-03-14 14:14:01 +0300

    feat: empty test example

---
+++ packages/bot-web-ui/src/components/__tests__/toolbar.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('Toolbar component', () => {
+    it('should render Toolbar', () => {
+        expect(true).toBe(true);
+    });
+});

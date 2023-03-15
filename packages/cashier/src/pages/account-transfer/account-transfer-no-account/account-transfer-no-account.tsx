684e0481bad72a2b5783c2d1e477c5654216e820
Rostik <rostislav@deriv.com>
2023-03-15 11:42:40 +0300

    refactor: fixed cringe mistake on my part

--- packages/cashier/src/pages/account-transfer/account-transfer-no-account/account-transfer-no-account.tsx
+++ packages/cashier/src/pages/account-transfer/account-transfer-no-account/account-transfer-no-account.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Icon, Button, Text } from '@deriv/components';
-import { getPlatformSettings, routes } from '@deriv/shared';
+import { routes } from '@deriv/shared';
 import { Localize } from '@deriv/translations';
 import { useStore, observer } from '@deriv/stores';
 import './account-transfer-no-account.scss';
@@ -8,7 +8,6 @@ import { useHistory } from 'react-router-dom';
 
 const AccountTransferNoAccount = observer(() => {
     const {
-        client: { is_dxtrade_allowed },
         traders_hub: { closeModal },
     } = useStore();
 
@@ -26,14 +25,7 @@ const AccountTransferNoAccount = observer(() => {
                 <Localize i18n_default_text='You only have one account' />
             </Text>
             <Text as='p' size='xs' line_height='s' align='center' className='cashier__text'>
-                {is_dxtrade_allowed ? (
-                    <Localize i18n_default_text='Transferring funds will require you to create a second account' />
-                ) : (
-                    <Localize
-                        i18n_default_text='Please create another Deriv or {{platform_name_mt5}} account.'
-                        values={{ platform_name_mt5: getPlatformSettings('mt5').name }}
-                    />
-                )}
+                <Localize i18n_default_text='Transferring funds will require you to create a second account' />
             </Text>
             <Button
                 className='account-transfer-no-account__button'

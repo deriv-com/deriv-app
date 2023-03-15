f6937779376e9ec5221c94eb7e3cd557fca933b7
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-03-15 10:53:55 +0800

    refactor: localize component replace

--- packages/account/src/Components/poinc/file-uploader-container/file-uploader-container.jsx
+++ packages/account/src/Components/poinc/file-uploader-container/file-uploader-container.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Icon } from '@deriv/components';
+import { Icon, Text } from '@deriv/components';
 import { isDesktop, WS } from '@deriv/shared';
-import { Localize } from '@deriv/translations';
+import { localize } from '@deriv/translations';
 import FileUploader from 'Components/file-uploader';
 
 const PoincFileUploaderContainer = ({ document_type, is_description_enabled = true, getSocket, onFileDrop, onRef }) => {
@@ -16,10 +16,10 @@ const PoincFileUploaderContainer = ({ document_type, is_description_enabled = tr
     }, [onRef, ref]);
 
     const properties = [
-        { name: 'size', icon: 'IcLessThanEight', text: 'Less than 8MB' },
-        { name: 'format', icon: 'IcImage', text: 'JPEG JPG PNG PDF' },
-        { name: 'with-address', icon: 'IcUser', text: 'Issued under your name with your current address' },
-        { name: 'time', icon: 'IcClock', text: '1 - 6 months old' },
+        { name: 'size', icon: 'IcLessThanEight', text: localize('Less than 8MB') },
+        { name: 'format', icon: 'IcImage', text: localize('JPEG JPG PNG PDF') },
+        { name: 'with-address', icon: 'IcUser', text: localize('Issued under your name with your current address') },
+        { name: 'time', icon: 'IcClock', text: localize('1 - 6 months old') },
     ];
 
     return (
@@ -30,7 +30,9 @@ const PoincFileUploaderContainer = ({ document_type, is_description_enabled = tr
                         <li key={name} className='account__file-uploader-box'>
                             <Icon icon={icon} className='account__file-uploader-icon' size={24} />
                             <div className='account__file-uploader-item'>
-                                <Localize i18n_default_text={text} />
+                                <Text size='xxs' align='center'>
+                                    {text}
+                                </Text>
                             </div>
                         </li>
                     ))}

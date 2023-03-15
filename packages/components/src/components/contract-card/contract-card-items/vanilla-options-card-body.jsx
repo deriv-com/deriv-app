ed73314ad84166dd4419b24c540cb264b62de6f7
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2023-03-15 12:45:48 +0300

    Update vanilla-options-card-body.jsx

--- packages/components/src/components/contract-card/contract-card-items/vanilla-options-card-body.jsx
+++ packages/components/src/components/contract-card/contract-card-items/vanilla-options-card-body.jsx
@@ -2,7 +2,8 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { getDisplayStatus, isCryptocurrency } from '@deriv/shared';
-import { DesktopWrapper, MobileWrapper } from '@deriv/components';
+import DesktopWrapper from '../../desktop-wrapper';
+import MobileWrapper from '../../mobile-wrapper';
 import ContractCardItem from './contract-card-item.jsx';
 import Icon from '../../icon';
 import Money from '../../money';
@@ -11,30 +12,31 @@ import { ResultStatusIcon } from '../result-overlay/result-overlay.jsx';
 const VanillaOptionsCardBody = ({ contract_info, currency, getCardLabels, is_sold, progress_slider, status }) => {
     const { buy_price, bid_price, entry_spot_display_value, barrier, sell_price, profit } = contract_info;
     const contract_value = is_sold ? sell_price : bid_price;
+    const { CONTRACT_VALUE, ENTRY_SPOT, PURCHASE_PRICE, STRIKE, TOTAL_PROFIT_LOSS } = getCardLabels();
 
     return (
         <React.Fragment>
             <DesktopWrapper>
                 <div className='dc-contract-card-items-wrapper'>
-                    <ContractCardItem header={getCardLabels().PURCHASE_PRICE}>
+                    <ContractCardItem header={PURCHASE_PRICE}>
                         <Money amount={buy_price} currency={currency} />
                     </ContractCardItem>
 
-                    <ContractCardItem header={getCardLabels().CONTRACT_VALUE}>
+                    <ContractCardItem header={CONTRACT_VALUE}>
                         <Money amount={contract_value} currency={currency} />
                     </ContractCardItem>
 
-                    <ContractCardItem header={getCardLabels().ENTRY_SPOT}>
+                    <ContractCardItem header={ENTRY_SPOT}>
                         <Money amount={entry_spot_display_value} />
                     </ContractCardItem>
 
-                    <ContractCardItem header={getCardLabels().STRIKE}>
+                    <ContractCardItem header={STRIKE}>
                         <Money amount={barrier} />
                     </ContractCardItem>
                 </div>
                 <ContractCardItem
                     className='dc-contract-card-item__total-profit-loss'
-                    header={getCardLabels().TOTAL_PROFIT_LOSS}
+                    header={TOTAL_PROFIT_LOSS}
                     is_crypto={isCryptocurrency(currency)}
                     is_loss={+profit < 0}
                     is_won={+profit > 0}
@@ -53,21 +55,21 @@ const VanillaOptionsCardBody = ({ contract_info, currency, getCardLabels, is_sol
             <MobileWrapper>
                 <div className='dc-contract-card-items-wrapper--mobile'>
                     <div className='dc-contract-card-items-wrapper-group'>
-                        <ContractCardItem header={getCardLabels().PURCHASE_PRICE}>
+                        <ContractCardItem header={PURCHASE_PRICE}>
                             <Money amount={buy_price} currency={currency} />
                         </ContractCardItem>
 
-                        <ContractCardItem header={getCardLabels().ENTRY_SPOT}>
+                        <ContractCardItem header={ENTRY_SPOT}>
                             <Money amount={entry_spot_display_value} currency={currency} />
                         </ContractCardItem>
                     </div>
 
                     <div className='dc-contract-card-items-wrapper-group'>
-                        <ContractCardItem header={getCardLabels().CONTRACT_VALUE}>
+                        <ContractCardItem header={CONTRACT_VALUE}>
                             <Money amount={contract_value} currency={currency} />
                         </ContractCardItem>
 
-                        <ContractCardItem header={getCardLabels().STRIKE}>
+                        <ContractCardItem header={STRIKE}>
                             <Money amount={barrier} currency={currency} />
                         </ContractCardItem>
                     </div>
@@ -82,7 +84,7 @@ const VanillaOptionsCardBody = ({ contract_info, currency, getCardLabels, is_sol
                     )}
                     <ContractCardItem
                         className='dc-contract-card-item__total-profit-loss'
-                        header={getCardLabels().TOTAL_PROFIT_LOSS}
+                        header={TOTAL_PROFIT_LOSS}
                         is_crypto={isCryptocurrency(currency)}
                         is_loss={+profit < 0}
                         is_won={+profit > 0}

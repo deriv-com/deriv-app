121257210cc05e1fc8398545a7b81e6fbf397940
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-03-16 15:45:41 +0800

    chore: new account verification modal for purchase authentication

---
+++ packages/trader/src/App/Components/Elements/Modals/ServicesErrorModal/account-verification-required-modal.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, Modal } from '@deriv/components';
+import { localize, Localize } from '@deriv/translations';
+import { isMobile, routes } from '@deriv/shared';
+import { useHistory } from 'react-router-dom';
+
+type TAccountVerificationRequiredModalProps = {
+    is_visible: boolean;
+    onConfirm: () => void;
+};
+
+const AccountVerificationRequiredModal = ({ is_visible, onConfirm }: TAccountVerificationRequiredModalProps) => {
+    const history = useHistory();
+    return (
+        <Modal
+            is_open={is_visible}
+            is_vertical_centered={isMobile()}
+            toggleModal={onConfirm}
+            title={localize('Account verification required')}
+        >
+            <Modal.Body>
+                <Localize i18n_default_text='Please submit your proof of identity and proof of address to verify your account and continue trading.' />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    has_effect
+                    text={localize('Submit Proof')}
+                    onClick={() => history.push(routes.proof_of_identity)}
+                    primary
+                />
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default AccountVerificationRequiredModal;

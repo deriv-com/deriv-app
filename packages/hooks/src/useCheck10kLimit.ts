cf75949374722c9f9b29758ebef658657056dfc0
Hamid Yaftian <hamid@deriv.com>
2023-03-20 12:11:27 +0800

    fix: wrong withdraw locked message

--- packages/hooks/src/useCheck10kLimit.ts
+++ packages/hooks/src/useCheck10kLimit.ts
@@ -1,11 +1,13 @@
 import { useStore } from '@deriv/stores';
 import useMaxWithdrawAmount from './useMaxWithdrawAmount';
 
-const useCheck10kLimit = (): boolean => {
+const useCheck10kLimit = (): boolean | undefined => {
     const { client } = useStore();
     const { min_withdrawal } = client;
     const max_withdraw_amount = useMaxWithdrawAmount();
-    const is_10k_withdrawal_limit_reached = max_withdraw_amount < min_withdrawal;
+    const is_10k_withdrawal_limit_reached = !!(
+        typeof max_withdraw_amount !== 'undefined' && +max_withdraw_amount < min_withdrawal
+    );
 
     return is_10k_withdrawal_limit_reached;
 };
--- packages/hooks/src/useMaxWithdrawAmount.ts
+++ packages/hooks/src/useMaxWithdrawAmount.ts
@@ -3,7 +3,7 @@ import { useStore } from '@deriv/stores';
 const useMaxWithdrawAmount = () => {
     const { client } = useStore();
 
-    return client.getLimits()?.get_limits?.remainder || 0;
+    return client.getLimits()?.get_limits?.remainder;
 };
 
 export default useMaxWithdrawAmount;

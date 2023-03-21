bd6bf633e30adb53b88371b7119b97cda215695a
Rostik Kayko <rostislav@deriv.com>
2023-03-21 17:08:13 +0300

    refactor: mad webstorm suggestion (lets have it like this for now)

--- packages/cashier/src/utils/utility.ts
+++ packages/cashier/src/utils/utility.ts
@@ -15,7 +15,7 @@ class PromiseClass {
 
 // check if mlt or dxtrade for account text
 const getAccountText = (account: { is_dxtrade: boolean; is_mt: boolean; text: string }) => {
-    let account_text = '';
+    let account_text;
     if (account.is_dxtrade || account.is_mt) {
         account_text = account.text;
     } else {
@@ -27,7 +27,38 @@ const getAccountText = (account: { is_dxtrade: boolean; is_mt: boolean; text: st
 
 const getNormalizedPaymentMethod = (
     payment_method: string,
-    constants: { [s: string]: Array<string> },
+    constants: {
+        readonly Paypal: readonly [string];
+        readonly Bank: readonly [string, string, string, string, string, string, string, string, string, string];
+        readonly Permatabank: readonly [string];
+        readonly Wechatpay: readonly [string];
+        readonly Mandirisyariah: readonly [string];
+        readonly Diamondbank: readonly [string];
+        readonly Ewallet: readonly [string, string, string, string];
+        readonly LiteCoin: readonly [string, string];
+        readonly Icbc: readonly [string];
+        readonly Verve: readonly [string];
+        readonly Bankbri: readonly [string, string];
+        readonly Libertyreserve: readonly [string];
+        readonly Cimbniaga: readonly [string];
+        readonly Bca: readonly [string, string];
+        readonly Alipay: readonly [string];
+        readonly Bch: readonly [string];
+        readonly Bitcoin: readonly [string, string];
+        readonly Firstbank: readonly [string];
+        readonly PerfectMoney: readonly [string, string];
+        readonly WebMoney: readonly [string, string];
+        readonly Zenithbank: readonly [string];
+        readonly Dai: readonly [string];
+        readonly Tether: readonly [string];
+        readonly Card: readonly [string, string, string, string];
+        readonly Moneygram: readonly [string];
+        readonly Gtbank: readonly [string];
+        readonly Crypto: readonly [string, string, string, string, string];
+        readonly Mandiri: readonly [string];
+        readonly Eth: readonly [string, string, string];
+        readonly Bni: readonly [string];
+    },
     is_for_icon = false
 ) => {
     const method = is_for_icon ? payment_method.replace(/[' ',-]/g, '').toLowerCase() : payment_method;

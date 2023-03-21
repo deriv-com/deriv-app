e403526803f0e51be7fe8233242ee9504fd01b09
amina-deriv <amina@regentmarkets.com>
2023-03-21 15:03:59 +0400

    chore: merge master

--- packages/core/src/App/Containers/RealAccountSignup/account-wizard-form.js
+++ packages/core/src/App/Containers/RealAccountSignup/account-wizard-form.js
@@ -28,8 +28,15 @@ const shouldShowIdentityInformation = ({
     const citizen = account_settings.citizen || residence;
     const country = residence_list.find(item => item.value === citizen);
     const maltainvest = real_account_signup_target === 'maltainvest';
-    const is_age_verified = account_status.status.some(status => status === 'age_verification');
-    return !maltainvest && citizen && country?.identity?.services?.idv?.is_country_supported && !is_age_verified;
+    const is_age_verified = account_status?.status?.some(status => status === 'age_verification');
+    const { submissions_left: idv_submissions_left } = account_status?.authentication?.identity?.services?.idv;
+    return (
+        !maltainvest &&
+        citizen &&
+        country?.identity?.services?.idv?.is_country_supported &&
+        !is_age_verified &&
+        Number(idv_submissions_left) > 0
+    );
 };
 
 export const getItems = props => [

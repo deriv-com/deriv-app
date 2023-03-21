e6cc4d90266dbd035a716526561db4d1804e32d0
Farzin Mirzaie <farzin@deriv.com>
2023-03-21 15:23:27 +0800

    refactor(api): :fire: remove unnecessary utils file

--- packages/api/src/useFetch.ts
+++ packages/api/src/useFetch.ts
@@ -1,11 +1,43 @@
 import { useQuery } from 'react-query';
-import { getQueryKeys, typeSafeSend } from './utils';
+import { WS } from '@deriv/shared';
 import { TSocketEndpointNames, TSocketAcceptableProps, TSocketResponseData } from '../types';
+
+const send = async <T extends TSocketEndpointNames>(
+    name: T,
+    ...props: TSocketAcceptableProps<T, true>
+): Promise<TSocketResponseData<T>> => {
+    const request = props[0];
+    delete request?.options;
+    const response = await WS.send({ [name]: 1, ...(request || {}) });
+
+    if (response.error) {
+        throw response.error;
+    }
+
+    return response[name];
+};
+
+const getQueryKeys = (props: { [k: string]: unknown }, name?: string) => {
+    delete props.req_id;
+    if (name && props[name] === 1) delete props[name];
+
+    const orderedProps = Object.keys(props)
+        .sort()
+        .reduce((obj, key) => {
+            obj[key] = props[key];
+
+            return obj;
+        }, {} as { [k: string]: unknown });
+
+    const queryProps = Object.keys(orderedProps).length ? JSON.stringify(orderedProps) : undefined;
+
+    return [name, queryProps];
+};
 
 const useFetch = <T extends TSocketEndpointNames>(name: T, ...props: TSocketAcceptableProps<T, true>) =>
     useQuery<TSocketResponseData<T>, unknown>(
         getQueryKeys(props[0] || {}, name),
-        () => typeSafeSend(name, ...props),
+        () => send(name, ...props),
         props[0]?.options
     );
 

0cc74dac87caaa742a087fe4e79a482ca36136c8
rupato-deriv <rupato@deriv.com>
2023-03-22 14:07:44 +0800

    fix: updated footer checks

--- src/common/footer-checks.js
+++ src/common/footer-checks.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import api from '../botPage/view/deriv/api';
-
-import { get as getStorage } from '../common/utils/storageManager';
+import { get as getStorage, getTokenList } from '../common/utils/storageManager';
 /* eslint-disable camelcase */
 export const isEuLandingCompany = landing_company => /^(maltainvest|malta|iom)$/.test(landing_company);
 
@@ -35,39 +34,54 @@ const isEmptyObject = obj => {
     }
     return is_empty;
 };
-export const checkSwitcherType = async () => {
-    const is_eu_landing = await isEuLandingCompany();
-    const is_eu = await isEuCountry();
-    const { website_status } = await api.send({ website_status: 1 });
-    const { clients_country } = website_status;
-    const { landing_company } = await api.send({ landing_company: clients_country });
-    const { financial_company, gaming_company } = landing_company;
 
-    const client_accounts = JSON.parse(getStorage('client.accounts'));
-    if (isEmptyObject(client_accounts)) return false;
+const isLowRisk = (financial_company, gaming_company, token_list) => {
+    const upgradable_companies = [];
+    token_list.map((data) => {
+        const { loginInfo: { upgradeable_landing_companies } } = data;
+        upgradable_companies.push(upgradeable_landing_companies);
+    });
+    const financial_shortcode = financial_company ? financial_company.shortcode : false;
+    const gaming_shortcode = gaming_company ? gaming_company.shortcode : false;
 
-    const low_risk_landing_company =
-        financial_company?.shortcode === 'maltainvest' && gaming_company?.shortcode === 'svg';
-    const low_risk_account = is_eu_landing && low_risk_landing_company;
+    const low_risk_landing_company = financial_shortcode === 'maltainvest' && gaming_shortcode === 'svg';
+    return low_risk_landing_company || (upgradable_companies?.include('svg') && upgradable_companies?.include('maltainvest'));
+}
 
-    const low_risk_no_account = low_risk_account && Object.keys(client_accounts).length > 1;
-
+const isHighRisk = (financial_company, gaming_company, risk_classification) => {
     const restricted_countries =
         financial_company?.shortcode === 'svg' ||
         (gaming_company?.shortcode === 'svg' && financial_company?.shortcode !== 'maltainvest');
 
     const high_risk_landing_company = financial_company?.shortcode === 'svg' && gaming_company?.shortcode === 'svg';
-    const high_risk_account =
-        high_risk_landing_company || client_accounts?.risk_classification === 'high' || restricted_countries;
+    return high_risk_landing_company || risk_classification === 'high' || restricted_countries;
+}
 
-    const high_risk_no_account = high_risk_account && Object.keys(client_accounts).length === 1;
+export const checkSwitcherType = async () => {
+    const is_eu = await isEuCountry();
+    const { website_status } = await api.send({ website_status: 1 });
+    const { clients_country } = website_status;
+    const { landing_company } = await api.send({ landing_company: clients_country });
+    const { financial_company, gaming_company } = landing_company;
+    const account_status = await api.send({ 'get_account_status': 1 })
+    const { risk_classification } = account_status;
+    const token_list = getTokenList();
+    const is_low_risk = isLowRisk(financial_company, gaming_company, token_list);
+    const is_high_risk = isHighRisk(financial_company, gaming_company, risk_classification);
 
-    const is_high_risk_or_eu = is_eu && high_risk_account;
+    const client_accounts = JSON.parse(getStorage('client.accounts'));
+    if (isEmptyObject(client_accounts || token_list)) return false;
+
+    const low_risk_no_account = is_low_risk && Object.keys(client_accounts).length > 1;
+
+    const high_risk_no_account = is_high_risk && Object.keys(client_accounts).length > 1;
+
+    const is_high_risk_or_eu = is_eu && is_high_risk;
 
     return {
-        low_risk: low_risk_account,
+        low_risk: is_low_risk,
+        high_risk: is_high_risk,
         low_risk_without_account: low_risk_no_account,
-        high_risk: high_risk_account,
         high_risk_without_account: high_risk_no_account,
         high_risk_or_eu: is_high_risk_or_eu,
     };

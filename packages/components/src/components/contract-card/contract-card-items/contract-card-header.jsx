5f5d00069c82e6ac1408dfafb58ba6f7690490dd
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-03-22 12:40:36 +0300

    refactor: currency definition, fix type of variable

--- packages/components/src/components/contract-card/contract-card-items/contract-card-header.jsx
+++ packages/components/src/components/contract-card/contract-card-items/contract-card-header.jsx
@@ -111,6 +111,7 @@ ContractCardHeader.propTypes = {
     onClickSell: PropTypes.func,
     server_time: PropTypes.object,
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    is_sold: PropTypes.bool,
 };
 
 export default ContractCardHeader;

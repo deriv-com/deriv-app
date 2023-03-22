5a7d6f23fe8bfa91f8bda116cb5847e4b36829fc
Farzin Mirzaie <farzin@deriv.com>
2023-03-22 10:30:59 +0800

    refactor(api): :fire: improve `useSubscription` hook

--- packages/api/src/useSubscription.ts
+++ packages/api/src/useSubscription.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useWS } from '@deriv/shared';
 import { TSocketSubscribableEndpointNames, TSocketAcceptableProps, TSocketResponseData } from '../types';
 
@@ -7,36 +7,48 @@ const useSubscription = <T extends TSocketSubscribableEndpointNames>(name: T) =>
     const [is_subscribed, setSubscribed] = useState(false);
     const [error, setError] = useState<unknown>();
     const [data, setData] = useState<TSocketResponseData<T>>();
-    const [subscriber, setSubscriber] = useState<{ unsubscribe?: VoidFunction }>();
+    const subscriber = useRef<{ unsubscribe?: VoidFunction }>();
     const WS = useWS();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const onData = (response: any) => {
-        setData(response[name === 'ticks' ? 'tick' : name]);
-        setIsLoading(false);
-    };
+    const subscribe = useCallback(
+        (...props: TSocketAcceptableProps<T>) => {
+            setIsLoading(true);
+            setSubscribed(true);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const onError = (response: any) => {
-        setError(response.error);
-        setIsLoading(false);
-    };
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const onData = (response: any) => {
+                setData(response[name === 'ticks' ? 'tick' : name]);
+                setIsLoading(false);
+            };
 
-    const subscribe = (...props: TSocketAcceptableProps<T>) => {
-        setIsLoading(true);
-        setSubscribed(true);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const onError = (response: any) => {
+                setError(response.error);
+                setIsLoading(false);
+            };
 
-        try {
-            setSubscriber(WS.subscribe({ [name]: 1, subscribe: 1, ...(props[0] || {}) }).subscribe(onData, onError));
-        } catch (e) {
-            setError(e);
-        }
-    };
+            try {
+                subscriber.current = WS.subscribe({ [name]: 1, subscribe: 1, ...(props[0] || {}) }).subscribe(
+                    onData,
+                    onError
+                );
+            } catch (e) {
+                setError(e);
+            }
+        },
+        [WS, name]
+    );
 
-    const unsubscribe = () => {
-        subscriber?.unsubscribe?.();
+    const unsubscribe = useCallback(() => {
+        subscriber.current?.unsubscribe?.();
         setSubscribed(false);
-    };
+    }, []);
+
+    useEffect(() => {
+        return () => {
+            unsubscribe();
+        };
+    }, [unsubscribe]);
 
     return { subscribe, unsubscribe, is_loading, is_subscribed, error, data };
 };

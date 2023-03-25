fcfb12dc7174470b2ed866edc797603b0fddd1aa
Farzin Mirzaie <farzin@deriv.com>
2023-03-25 12:20:22 +0800

    feat(api): :sparkles: add `useInvalidateQuery` hook

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,5 @@
 export { default as APIProvider } from './APIProvider';
 export { default as useFetch } from './useFetch';
+export { default as useInvalidateQuery } from './useInvalidateQuery';
 export { default as useRequest } from './useRequest';
 export { default as useSubscription } from './useSubscription';

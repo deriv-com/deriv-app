09097c7d7d95ec5ca95d8cb7d38325759229c524
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-03-28 18:31:28 +0300

    Revert "chore: clean up the code(2)"
    This reverts commit 045e107a155c5d1c44c6fd4cb34476617c55787e.

--- packages/bot-web-ui/src/components/dashboard/quick-strategy/quick-strategy-components/footer.tsx
+++ packages/bot-web-ui/src/components/dashboard/quick-strategy/quick-strategy-components/footer.tsx
@@ -17,10 +17,10 @@ const QuickStrategyFooter = ({
         submitForm();
     }, [is_submit_enabled]);
 
-    const handleRun = React.useCallback(() => {
+    const handleRun = React.useCallback(async () => {
         if (is_running) {
-            setFieldValue('button', 'edit');
-            submitForm();
+            await setFieldValue('button', 'edit');
+            await submitForm();
             toggleStopBotDialog();
         } else {
             setFieldValue('button', 'run');

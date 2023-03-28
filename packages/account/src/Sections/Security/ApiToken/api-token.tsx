a45487d47c863e8e9db3b75a39ef56cc0186f27f
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-03-28 13:38:01 +0300

    refactor: security section api token ts migration

--- packages/account/src/Sections/Security/ApiToken/api-token.tsx
+++ packages/account/src/Sections/Security/ApiToken/api-token.tsx
@@ -1,6 +1,7 @@
 import { WS } from '@deriv/shared';
+import type { TRootStore } from '@deriv/stores/types';
 import { connect } from 'Stores/connect';
 import ApiToken from 'Components/api-token/api-token';
 import 'Components/api-token/api-token.scss';
 
-export default connect(({ client }) => ({ is_switching: client.is_switching, ws: WS }))(ApiToken);
+export default connect(({ client }: TRootStore) => ({ is_switching: client.is_switching, ws: WS }))(ApiToken);

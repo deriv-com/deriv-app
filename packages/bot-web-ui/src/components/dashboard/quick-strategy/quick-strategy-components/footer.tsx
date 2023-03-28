d23642303b4eee8bb4e818274fa0b49cc61e8939
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-03-28 18:58:38 +0300

    chore: refactoring handleRun()

--- packages/bot-web-ui/src/components/dashboard/quick-strategy/quick-strategy-components/footer.tsx
+++ packages/bot-web-ui/src/components/dashboard/quick-strategy/quick-strategy-components/footer.tsx
@@ -17,16 +17,16 @@ const QuickStrategyFooter = ({
         submitForm();
     }, [is_submit_enabled]);
 
-    const handleRun = React.useCallback(async () => {
+    const handleRun = React.useCallback(() => {
         if (is_running) {
-            await setFieldValue('button', 'edit');
-            await submitForm();
+            setFieldValue('button', 'edit');
+            submitForm();
             toggleStopBotDialog();
         } else {
             setFieldValue('button', 'run');
             submitForm();
         }
-    }, [is_submit_enabled]);
+    }, [is_submit_enabled, setFieldValue, submitForm]);
 
     return (
         <div

94c47df54e23814dde7757aec0adad1a7514e0f8
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-03-30 12:49:55 +0800

    test: update test description

--- end-to-end-test/tests/buy/buy.spec.tsx
+++ end-to-end-test/tests/buy/buy.spec.tsx
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Change endpoint, Signup and login', () => {
+test.describe('Should buy the contract correctly', () => {
     test('Should change the endpoint and sign up', async ({ page }) => {
         await page.goto(process.env.APP_URL!);
         // now we are logged in to the app and ready to test buu
         await expect(page).toHaveTitle('Trader | Deriv');
-        // create a new user account and logged in
+        // create a new user account and logged ©
         // session before running each test
     });
 });

cf52bfa46730b4ce963e5192aa919adccde25a83
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-04-03 11:16:45 +0400

    refactor: made changes in test files to fix error

--- packages/shared/src/utils/validation/__tests__/declarative-validation-rules.spec.js
+++ packages/shared/src/utils/validation/__tests__/declarative-validation-rules.spec.js
@@ -1,4 +1,3 @@
-import { render, screen } from '@testing-library/react';
 import { validLetterSymbol } from '../declarative-validation-rules';
 
 describe('validLetterSymbol', () => {
@@ -12,17 +11,11 @@ describe('validLetterSymbol', () => {
     it('should return false for invalid input', () => {
         expect(validLetterSymbol('123')).toBe(false);
         expect(validLetterSymbol('John!')).toBe(false);
-        expect(validLetterSymbol('a b c')).toBe(false);
     });
 
     it('should work with special characters', () => {
         expect(validLetterSymbol('!@#$%^&*()_+')).toBe(false);
         expect(validLetterSymbol('John Doe!@#')).toBe(false);
         expect(validLetterSymbol("D'Artagnan!")).toBe(false);
-    });
-
-    it('should render properly in React', () => {
-        const { getByTestId } = render(<div data-testid='result'>{validLetterSymbol('John Doe')}</div>);
-        expect(getByTestId('result')).toHaveTextContent('true');
     });
 });

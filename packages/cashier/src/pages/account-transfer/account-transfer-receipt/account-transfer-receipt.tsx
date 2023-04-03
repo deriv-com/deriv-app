5c0912994e466e64a8a0489266db45d692982ed3
aizad-deriv <aizad@deriv.com>
2023-04-03 14:15:47 +0800

    fix: able to show account receipt modal

--- packages/cashier/src/pages/account-transfer/account-transfer-receipt/account-transfer-receipt.tsx
+++ packages/cashier/src/pages/account-transfer/account-transfer-receipt/account-transfer-receipt.tsx
@@ -68,9 +68,6 @@ const AccountTransferReceipt = observer(({ onClose, history }: TAccountTransferR
             setSwitchTo(selected_to.is_mt ? selected_from : selected_to);
             toggleSwitchAlert();
         }
-        // close modal only when the user try to transfer money from traders-hub, not from cashier
-        // because in cashier this component is not a modal
-        if (is_from_outside_cashier) onClose?.();
     };
 
     return (

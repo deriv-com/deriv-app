5b227cfabb806996c04bc89823311fc997ed63f8
thisyahlen <104053934+thisyahlen-deriv@users.noreply.github.com>
2023-04-05 10:51:17 +0800

    thisyahlen/fix: login change language error (#7963)
    * fix: login change language error
    
    * fix: console error and loading
    
    ---------
    
    Co-authored-by: Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>
    Co-authored-by: yashim-deriv <yashim@deriv.com>

--- packages/core/src/App/Components/Layout/Header/menu-links.jsx
+++ packages/core/src/App/Components/Layout/Header/menu-links.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Text, Icon, Counter } from '@deriv/components';
 import { BinaryLink } from '../../Routes';
 import { observer, useStore } from '@deriv/stores';
@@ -85,16 +86,16 @@ const CashierTab = observer(() => {
 });
 
 const MenuLinks = observer(() => {
-    const { client, common, ui } = useStore();
+    const { i18n } = useTranslation();
+    const { client, ui } = useStore();
     const { is_logged_in } = client;
-    const { current_language } = common;
     const { is_mobile } = ui;
     const is_traders_hub = window.location.pathname === routes.traders_hub;
 
     if (!is_logged_in) return <></>;
 
     return (
-        <div key={`menu-links__${current_language}`} className='header__menu-links'>
+        <div key={`menu-links__${i18n.language}`} className='header__menu-links'>
             {!is_traders_hub && <ReportTab />}
             {!is_mobile && <CashierTab />}
         </div>

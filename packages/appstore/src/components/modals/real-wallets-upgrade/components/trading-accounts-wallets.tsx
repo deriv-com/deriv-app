74ca91decffa1ac2d6b943c3bb33ab3011bc010b
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-04-06 17:48:22 +0800

    fix: responsive

--- packages/appstore/src/components/modals/real-wallets-upgrade/components/trading-accounts-wallets.tsx
+++ packages/appstore/src/components/modals/real-wallets-upgrade/components/trading-accounts-wallets.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { Modal, Button, Text, Icon } from '@deriv/components';
+import { Modal, Button, Text, Icon, Div100vhContainer } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import '../real-wallets-upgrade.scss';
 import TradingPlatformIcon from 'Assets/wallets';
+import { isDesktop } from '@deriv/shared';
 
 type StepProps = {
     onBack?: () => void;
@@ -15,44 +16,66 @@ const TradingAccountsWallets = ({ onClose, onBack, eu_user }: StepProps) => {
     return (
         <React.Fragment>
             <Modal.Body className='introducing-wallets'>
-                <div className='introducing-wallets--content'>
-                    <TradingPlatformIcon icon='TradingAccounts' />
-                    <Text
-                        as='h1'
-                        color='prominent'
-                        weight='bold'
-                        align='center'
-                        size='l'
-                        className='introducing-wallets--title'
-                    >
-                        {localize('What happens to my trading accounts')}
-                    </Text>
-                    <Text as='p' color='prominent' size='m' align='center' className='introducing-wallets--description'>
-                        {localize("We'll link them")}
-                    </Text>
-                    <div className='introducing-wallets--bullet'>
-                        <Icon icon='IcAppstoreTick' />
-                        <Text as='p' color='prominent' align='center' className='introducing-wallets--bullet--text'>
-                            {localize(
-                                "We'll connect your existing trading accounts of the same currency to your new Wallet"
-                            )}
+                <Div100vhContainer is_disabled={isDesktop()} height_offset='15rem'>
+                    <div className='introducing-wallets--content'>
+                        <TradingPlatformIcon icon='TradingAccounts' />
+                        <Text
+                            as='h1'
+                            color='prominent'
+                            weight='bold'
+                            align='center'
+                            size='l'
+                            className='introducing-wallets--title'
+                        >
+                            {localize('What happens to my trading accounts')}
                         </Text>
-                    </div>
-                    {!eu_user && (
+                        <Text
+                            as='p'
+                            color='prominent'
+                            size='m'
+                            align='center'
+                            className='introducing-wallets--description'
+                        >
+                            {localize("We'll link them")}
+                        </Text>
                         <div className='introducing-wallets--bullet'>
                             <Icon icon='IcAppstoreTick' />
-                            <Text as='p' color='prominent' className='introducing-wallets--bullet--text'>
+                            <Text as='p' color='prominent' align='center' className='introducing-wallets--bullet--text'>
                                 {localize(
-                                    'For example, all your USD trading account(s) will be linked to your USD Wallet'
+                                    "We'll connect your existing trading accounts of the same currency to your new Wallet"
                                 )}
                             </Text>
                         </div>
-                    )}
-                </div>
+                        {!eu_user && (
+                            <div className='introducing-wallets--bullet'>
+                                <Icon icon='IcAppstoreTick' />
+                                <Text as='p' color='prominent' className='introducing-wallets--bullet--text'>
+                                    {localize(
+                                        'For example, all your USD trading account(s) will be linked to your USD Wallet'
+                                    )}
+                                </Text>
+                            </div>
+                        )}
+                    </div>
+                </Div100vhContainer>
             </Modal.Body>
-            <Modal.Footer>
-                <Button has_effect onClick={onBack} text={localize('Back')} secondary large />
-                <Button has_effect onClick={onClose} text={localize('Next')} primary large />
+            <Modal.Footer has_separator>
+                <Button
+                    className='introducing-wallets--footer--button'
+                    has_effect
+                    onClick={onBack}
+                    text={localize('Back')}
+                    secondary
+                    large
+                />
+                <Button
+                    className='introducing-wallets--footer--button'
+                    has_effect
+                    onClick={onClose}
+                    text={localize('Next')}
+                    primary
+                    large
+                />
             </Modal.Footer>
         </React.Fragment>
     );

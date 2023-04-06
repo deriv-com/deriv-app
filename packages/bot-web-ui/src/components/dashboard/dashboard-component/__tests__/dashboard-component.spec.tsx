f1ba4d67f83d552d4d4d017960b6d21fba88fa60
rupato-deriv <rupato@deriv.com>
2023-04-06 20:08:31 +0800

    fix: updated the test case

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/__tests__/dashboard-component.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/__tests__/dashboard-component.spec.tsx
@@ -2,15 +2,42 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UserGuide from '../user-guide';
+import { isMobile } from '@deriv/shared';
 import Sidebar from 'Components/dashboard/tutorial-tab/sidebar';
+import DashboardComponent from '../dashboard-component';
 
-jest.mock('Stores/connect', () => ({
+const mock_connect_props = {
+    dialog_options: {
+        title: 'string',
+        message: 'string',
+        ok_button_text: 'string',
+        cancel_button_text: 'string',
+    },
+    setStrategySaveType: jest.fn(),
+};
+
+jest.mock('Stores/connect.js', () => ({
     __esModule: true,
     default: 'mockedDefaultExport',
     connect:
         () =>
         <T,>(Component: T) =>
-            Component,
+        props =>
+            Component({ ...props, ...mock_connect_props }),
+}));
+
+jest.mock('@deriv/components', () => {
+    const original_module = jest.requireActual('@deriv/components');
+
+    return {
+        ...original_module,
+        Cards: jest.fn(() => 'Cards'),
+    };
+});
+
+jest.mock('@deriv/shared', () => ({
+    ...jest.requireActual('@deriv/shared'),
+    isMobile: jest.fn(() => false),
 }));
 
 jest.mock('@deriv/bot-skeleton', () => ({
@@ -46,11 +73,18 @@ export const mocked_props = {
 };
 
 describe('<Dashboard />', () => {
-    it('user guide button click', () => {
+    it('should render PopoverComponent if isMobile is false', () => {
+        (isMobile as jest.Mock).mockReturnValue(true);
+        render(<DashboardComponent {...mocked_props} />);
+        expect(screen.getByText(/Import a bot/i)).toBeInTheDocument();
+    });
+
+    it('user guide button click should be enabled', () => {
         render(<UserGuide {...mocked_props} />);
         const use_guide_button = screen.getByTestId('btn-use-guide');
         userEvent.click(use_guide_button);
         expect(screen.getByTestId('btn-use-guide')).toBeInTheDocument();
+        expect(use_guide_button).toBeEnabled();
     });
 
     it('on user guide button click it should render the tutorials tab', () => {
@@ -58,6 +92,6 @@ describe('<Dashboard />', () => {
         const use_guide_button = screen.getByTestId('btn-use-guide');
         userEvent.click(use_guide_button);
         render(<Sidebar {...mocked_props} />);
-        expect(screen.getByRole('textbox')).toHaveClass('dc-tabs__wrapper__group__search-input', { exact: true });
+        expect(mocked_props.setActiveTab).toHaveBeenCalledWith(3);
     });
 });
--- packages/bot-web-ui/src/components/dashboard/dashboard-component/__tests__/user-guide.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/__tests__/user-guide.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import UserGuide from '../user-guide';
 import userEvent from '@testing-library/user-event';
-import { mocked_props } from './dashboard-components.spec';
+import { mocked_props } from './dashboard-component.spec';
 
 describe('<UserGuide />', () => {
     it('renders user guide button', () => {

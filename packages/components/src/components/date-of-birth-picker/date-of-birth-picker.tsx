f0d64df06bc33fc3a0ec21e1748dd523a7e19b81
amir ali <amir.ali@regentmarkets.com>
2023-04-06 17:17:44 +0500

    refactor: move expression directly to prop

--- packages/components/src/components/date-of-birth-picker/date-of-birth-picker.tsx
+++ packages/components/src/components/date-of-birth-picker/date-of-birth-picker.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { toMoment } from '@deriv/shared';
 import DatePicker from '../date-picker';
 
-const DateOfBirthPicker = (props: Omit<React.ComponentProps<typeof DatePicker>, 'display_format' | 'max_date' | 'min_date'>) => {
-    const max_date = toMoment().subtract(18, 'years');
-    const min_date = toMoment().subtract(100, 'years');
-
+const DateOfBirthPicker = (
+    props: Omit<React.ComponentProps<typeof DatePicker>, 'display_format' | 'max_date' | 'min_date'>
+) => {
     return (
         <DatePicker
             {...props}
             calendar_view='year'
             display_format='DD-MM-YYYY'
-            max_date={max_date}
-            min_date={min_date}
+            max_date={toMoment().subtract(18, 'years')}
+            min_date={toMoment().subtract(100, 'years')}
         />
     );
 };

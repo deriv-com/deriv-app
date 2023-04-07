7e8453882a3c38254422bd51b5fd32be914298dd
Sergei Baranovski <sergei@deriv.com>
2023-04-07 18:18:24 +0300

    refactor: add cahnges after review

--- packages/core/src/App/Components/Layout/Header/toggle-notifications.jsx
+++ packages/core/src/App/Components/Layout/Header/toggle-notifications.jsx
@@ -3,66 +3,61 @@ import React from 'react';
 import { Counter, DesktopWrapper, Icon, MobileWrapper, Popover } from '@deriv/components';
 import NotificationsDialog from 'App/Containers/NotificationsDialog';
 import 'Sass/app/modules/notifications-dialog.scss';
-import { useStore } from '@deriv/stores';
-import { observer } from 'mobx-react';
+import { useStore, observer } from '@deriv/stores';
 
-const ToggleNotificationsDrawer = ({
-    count,
-    is_visible,
-    toggleDialog,
-    tooltip_message,
-    should_disable_pointer_events = false,
-}) => {
-    const {
-        client: { is_logging_in, is_switching, is_landing_company_loaded, is_account_setting_loaded },
-    } = useStore();
+const ToggleNotificationsDrawer = observer(
+    ({ count, is_visible, toggleDialog, tooltip_message, should_disable_pointer_events = false }) => {
+        const {
+            client: { is_logging_in, is_switching, is_landing_company_loaded, is_account_setting_loaded },
+        } = useStore();
 
-    const is_notifications_loading_completed =
-        !is_logging_in && !is_switching && is_landing_company_loaded && is_account_setting_loaded;
+        const is_notifications_loading_completed =
+            !is_logging_in && !is_switching && is_landing_company_loaded && is_account_setting_loaded;
 
-    const [number, setNumber] = React.useState(0);
+        const [notificationsCount, setNotificationsCount] = React.useState(0);
 
-    React.useEffect(() => {
-        if (is_notifications_loading_completed && !!count) setNumber(count);
-        else if (is_notifications_loading_completed) setNumber(0);
-    }, [is_notifications_loading_completed, count]);
+        React.useEffect(() => {
+            if (is_notifications_loading_completed && !!count) setNotificationsCount(count);
+            else if (is_notifications_loading_completed) setNotificationsCount(0);
+        }, [is_notifications_loading_completed, count]);
 
-    const notifications_toggler_el = (
-        <div
-            className={classNames('notifications-toggle__icon-wrapper', {
-                'notifications-toggle__icon-wrapper--active': is_visible,
-            })}
-            onClick={toggleDialog}
-        >
-            <Icon className='notifications-toggle__icon' icon='IcBell' />
-            {!!number && <Counter count={number} className='notifications-toggle__step' />}
-        </div>
-    );
+        const notifications_toggler_el = (
+            <div
+                className={classNames('notifications-toggle__icon-wrapper', {
+                    'notifications-toggle__icon-wrapper--active': is_visible,
+                })}
+                onClick={toggleDialog}
+            >
+                <Icon className='notifications-toggle__icon' icon='IcBell' />
+                {!!notificationsCount && <Counter count={notificationsCount} className='notifications-toggle__step' />}
+            </div>
+        );
 
-    return (
-        <div
-            className={classNames('notifications-toggle', {
-                'notifications-toggle--active': is_visible,
-            })}
-        >
-            <DesktopWrapper>
-                <Popover
-                    classNameBubble='notifications-toggle__tooltip'
-                    alignment='bottom'
-                    message={tooltip_message}
-                    should_disable_pointer_events={should_disable_pointer_events}
-                    zIndex={9999}
-                >
+        return (
+            <div
+                className={classNames('notifications-toggle', {
+                    'notifications-toggle--active': is_visible,
+                })}
+            >
+                <DesktopWrapper>
+                    <Popover
+                        classNameBubble='notifications-toggle__tooltip'
+                        alignment='bottom'
+                        message={tooltip_message}
+                        should_disable_pointer_events={should_disable_pointer_events}
+                        zIndex={9999}
+                    >
+                        {notifications_toggler_el}
+                    </Popover>
+                    <NotificationsDialog is_visible={is_visible} toggleDialog={toggleDialog} />
+                </DesktopWrapper>
+                <MobileWrapper>
                     {notifications_toggler_el}
-                </Popover>
-                <NotificationsDialog is_visible={is_visible} toggleDialog={toggleDialog} />
-            </DesktopWrapper>
-            <MobileWrapper>
-                {notifications_toggler_el}
-                <NotificationsDialog is_visible={is_visible} toggleDialog={toggleDialog} />
-            </MobileWrapper>
-        </div>
-    );
-};
+                    <NotificationsDialog is_visible={is_visible} toggleDialog={toggleDialog} />
+                </MobileWrapper>
+            </div>
+        );
+    }
+);
 
-export default observer(ToggleNotificationsDrawer);
+export default ToggleNotificationsDrawer;

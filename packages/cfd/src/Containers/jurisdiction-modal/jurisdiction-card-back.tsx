35ad7510207d220bed404778f7c742c731d15391
Shaheer <shaheer@deriv.com>
2023-04-07 15:03:39 +0400

    refactor: :art: improves code structure by removing unwanted return statements

--- packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-card-back.tsx
+++ packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-card-back.tsx
@@ -4,57 +4,51 @@ import { jurisdiction_verification_contents } from 'Constants/jurisdiction-verif
 import { TJurisdictionCardBackProps } from 'Containers/props.types';
 import React from 'react';
 
-const JurisdictionCardBack = ({ card_classname, toggleCardFlip, verification_docs }: TJurisdictionCardBackProps) => {
-    return (
-        <div
-            className={classNames(
-                `${card_classname}__card-content-container`,
-                `${card_classname}__card-flipped-container`
-            )}
-        >
-            <div>
-                <Icon
-                    onClick={toggleCardFlip}
-                    className='cfd-card-back-section-back-button'
-                    icon='IcBackButton'
-                    size={20}
-                />
-            </div>
-            <Text as='div' size='xxs'>
-                {jurisdiction_verification_contents.short_description}
-            </Text>
-            <div className={classNames('cfd-card-back-section-items-container', 'cfd-card-back-section-main')}>
-                {verification_docs?.map(verification_item => (
-                    <div key={verification_item} className='cfd-card-back-section-items-sub-container'>
-                        <div>
-                            <Icon
-                                icon={
-                                    jurisdiction_verification_contents.required_verification_docs[verification_item]
-                                        ?.icon
-                                }
-                            />
-                        </div>
-                        <Text as='span' size='xxs'>
-                            {jurisdiction_verification_contents.required_verification_docs[verification_item]?.text}
-                        </Text>
-                    </div>
-                ))}
-            </div>
-            <div className='cfd-card-section-divider' />
-            <div className='cfd-card-back-section-items-container'>
-                {jurisdiction_verification_contents.status_references.map(status_item => (
-                    <div className='cfd-card-back-section-items-sub-container' key={status_item.color}>
-                        <div>
-                            <Icon icon={status_item.icon} />
-                        </div>
-                        <Text as='span' size='xxs'>
-                            {status_item.text}
-                        </Text>
-                    </div>
-                ))}
-            </div>
+const JurisdictionCardBack = ({ card_classname, toggleCardFlip, verification_docs }: TJurisdictionCardBackProps) => (
+    <div
+        className={classNames(`${card_classname}__card-content-container`, `${card_classname}__card-flipped-container`)}
+    >
+        <div>
+            <Icon
+                onClick={toggleCardFlip}
+                className='cfd-card-back-section-back-button'
+                icon='IcBackButton'
+                size={20}
+            />
         </div>
-    );
-};
+        <Text as='div' size='xxs'>
+            {jurisdiction_verification_contents.short_description}
+        </Text>
+        <div className={classNames('cfd-card-back-section-items-container', 'cfd-card-back-section-main')}>
+            {verification_docs?.map(verification_item => (
+                <div key={verification_item} className='cfd-card-back-section-items-sub-container'>
+                    <div>
+                        <Icon
+                            icon={
+                                jurisdiction_verification_contents.required_verification_docs[verification_item]?.icon
+                            }
+                        />
+                    </div>
+                    <Text as='span' size='xxs'>
+                        {jurisdiction_verification_contents.required_verification_docs[verification_item]?.text}
+                    </Text>
+                </div>
+            ))}
+        </div>
+        <div className='cfd-card-section-divider' />
+        <div className='cfd-card-back-section-items-container'>
+            {jurisdiction_verification_contents.status_references.map(status_item => (
+                <div className='cfd-card-back-section-items-sub-container' key={status_item.color}>
+                    <div>
+                        <Icon icon={status_item.icon} />
+                    </div>
+                    <Text as='span' size='xxs'>
+                        {status_item.text}
+                    </Text>
+                </div>
+            ))}
+        </div>
+    </div>
+);
 
 export default JurisdictionCardBack;
--- packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-card-front.tsx
+++ packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-card-front.tsx
@@ -9,43 +9,41 @@ const JurisdictionCardFront = ({
     toggleCardFlip,
     card_values,
     card_data,
-}: TJurisdictionCardFrontProps) => {
-    return (
-        <div className={`${card_classname}__card-content-container`}>
-            {card_values.is_over_header_available ? (
-                <Text
-                    as='div'
-                    weight='bold'
-                    color='info-blue'
-                    align='center'
-                    size='xxs'
-                    className={`${card_classname}__card-content-over-header`}
-                >
-                    <Localize i18n_default_text={card_values.over_header} />
-                </Text>
-            ) : (
-                <div className={`${card_classname}__card-content-over-header-blank`} />
-            )}
+}: TJurisdictionCardFrontProps) => (
+    <div className={`${card_classname}__card-content-container`}>
+        {card_values.is_over_header_available ? (
             <Text
-                as='p'
+                as='div'
                 weight='bold'
-                color={'prominent'}
+                color='info-blue'
                 align='center'
-                size='xsm'
-                className={`${card_classname}__h2-header`}
+                size='xxs'
+                className={`${card_classname}__card-content-over-header`}
             >
-                <Localize i18n_default_text={card_values.header} />
+                <Localize i18n_default_text={card_values.over_header} />
             </Text>
-            <div className={`${card_classname}__card-section-container`}>
-                {card_data.map((item, index) => (
-                    <React.Fragment key={item.key}>
-                        <JurisdictionCardSection card_section_item={item} toggleCardFlip={toggleCardFlip} />
-                        {index < card_data.length - 1 && <div className='cfd-card-section-divider' />}
-                    </React.Fragment>
-                ))}
-            </div>
+        ) : (
+            <div className={`${card_classname}__card-content-over-header-blank`} />
+        )}
+        <Text
+            as='p'
+            weight='bold'
+            color={'prominent'}
+            align='center'
+            size='xsm'
+            className={`${card_classname}__h2-header`}
+        >
+            <Localize i18n_default_text={card_values.header} />
+        </Text>
+        <div className={`${card_classname}__card-section-container`}>
+            {card_data.map((item, index) => (
+                <React.Fragment key={item.key}>
+                    <JurisdictionCardSection card_section_item={item} toggleCardFlip={toggleCardFlip} />
+                    {index < card_data.length - 1 && <div className='cfd-card-section-divider' />}
+                </React.Fragment>
+            ))}
         </div>
-    );
-};
+    </div>
+);
 
 export default JurisdictionCardFront;

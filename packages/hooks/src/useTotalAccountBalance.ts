91a654b22120ff5b28933ec088c38050ad3e2b7d
Shayan Khaleghparast <iman@re-work.dev>
2023-04-10 13:18:19 +0330

    fix: fixed trader's hub page not rendered

--- packages/hooks/src/useTotalAccountBalance.ts
+++ packages/hooks/src/useTotalAccountBalance.ts
@@ -1,9 +1,8 @@
 import { useStore } from '@deriv/stores';
-import type { DetailsOfEachMT5Loginid } from '@deriv/api-types';
 
-const useTotalAccountBalance = (accounts: DetailsOfEachMT5Loginid[]) => {
+const useTotalAccountBalance = (accounts: { balance?: number; currency?: string }[]) => {
     const { exchange_rates } = useStore();
-    const currency = accounts?.[0].currency || 'USD';
+    const currency = accounts?.[0]?.currency || 'USD';
 
     const balance = accounts.reduce((total, account) => {
         const base_rate = exchange_rates.data?.rates?.[currency] || 1;

722c30f2e982d70aeaedbb16b84657926cc45814
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-04-10 11:48:58 +0800

    fix: address comments and combine 3 steps to 1 component

--- packages/appstore/src/types/index.ts
+++ packages/appstore/src/types/index.ts
@@ -4,3 +4,4 @@ export * from './params.types';
 export * from './api.types';
 export * from './ws.types';
 export * from './common.types';
+export * from './wallet-steps.types';
---
+++ packages/appstore/src/types/wallet-steps.types.ts
@@ -0,0 +1,7 @@
+export type TStepProps = {
+    title: string;
+    description: string;
+    bullets: string[];
+    eu_user?: boolean;
+    icon?: React.ReactNode;
+};

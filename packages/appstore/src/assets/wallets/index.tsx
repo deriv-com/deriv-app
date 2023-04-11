043b23d3505a38b966b80f2febb1f6684f0a517b
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-04-11 15:09:50 +0800

    fix: use walletimage

--- packages/appstore/src/assets/wallets/index.tsx
+++ packages/appstore/src/assets/wallets/index.tsx
@@ -3,18 +3,23 @@ import IntroducingWallets from 'Assets/wallets/introducing-wallets.svg';
 import IntroducingWalletsEU from 'Assets/wallets/introducing-wallets-eu.svg';
 import HowItWorks from 'Assets/wallets/how-it-works.svg';
 import TradingAccounts from 'Assets/wallets/trading-accounts.svg';
-import { IconProps } from '../svgs/icon-types';
 
-export const PlatformIcons = {
+export const WalletsImagesList = {
     IntroducingWallets,
     IntroducingWalletsEU,
     HowItWorks,
     TradingAccounts,
 };
-const TradingPlatformIcon = ({ icon, className, size, onClick }: IconProps<keyof typeof PlatformIcons>) => {
-    const PlatformIcon = PlatformIcons[icon] as React.ElementType;
 
-    return <PlatformIcon className={className} style={{ width: size, height: size }} onClick={onClick} />;
+type WalletsImageProps<T> = {
+    banner: T;
+    className?: string;
 };
 
-export default TradingPlatformIcon;
+const WalletsImage = ({ banner, className }: WalletsImageProps<keyof typeof WalletsImagesList>) => {
+    const Component = WalletsImagesList[banner] as React.ElementType;
+
+    return <Component className={className} data-testid={banner} />;
+};
+
+export default WalletsImage;

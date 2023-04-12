7761d1430619f2ad5365cf7eaabcea0cae2f9843
Farzin Mirzaie <farzin@deriv.com>
2023-04-12 14:51:16 +0800

    refactor: :construction: wip

--- packages/hooks/src/__tests__/useNeedAuthentication.spec.tsx
+++ packages/hooks/src/__tests__/useNeedAuthentication.spec.tsx
@@ -1,22 +1,14 @@
 import * as React from 'react';
-import { StoreProvider } from '@deriv/stores';
-import type { TStores } from '@deriv/stores';
+import { StoreProvider, mockStore } from '@deriv/stores';
 import { renderHook } from '@testing-library/react-hooks';
 import useNeedAuthentication from '../useNeedAuthentication';
 
 describe('useNeedAuthentication', () => {
     test('should be false if is_authentication_needed and is_low_risk_cr_eu_real both are false', async () => {
-        const mockRootStore: DeepPartial<TStores> = {
-            client: {
-                is_authentication_needed: false,
-            },
-            traders_hub: {
-                is_low_risk_cr_eu_real: false,
-            },
-        };
+        const mock = mockStore({});
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mockRootStore as TStores}>{children}</StoreProvider>
+            <StoreProvider store={mock}>{children}</StoreProvider>
         );
         const { result } = renderHook(() => useNeedAuthentication(), { wrapper });
 
@@ -24,17 +16,14 @@ describe('useNeedAuthentication', () => {
     });
 
     test('should be false if is_authentication_needed is false and is_low_risk_cr_eu_real is true', async () => {
-        const mockRootStore: DeepPartial<TStores> = {
-            client: {
-                is_authentication_needed: false,
-            },
+        const mock = mockStore({
             traders_hub: {
                 is_low_risk_cr_eu_real: true,
             },
-        };
+        });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mockRootStore as TStores}>{children}</StoreProvider>
+            <StoreProvider store={mock}>{children}</StoreProvider>
         );
         const { result } = renderHook(() => useNeedAuthentication(), { wrapper });
 
@@ -42,17 +31,14 @@ describe('useNeedAuthentication', () => {
     });
 
     test('should be false if is_authentication_needed is true and is_low_risk_cr_eu_real is false', async () => {
-        const mockRootStore: DeepPartial<TStores> = {
+        const mock = mockStore({
             client: {
                 is_authentication_needed: true,
             },
-            traders_hub: {
-                is_low_risk_cr_eu_real: false,
-            },
-        };
+        });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mockRootStore as TStores}>{children}</StoreProvider>
+            <StoreProvider store={mock}>{children}</StoreProvider>
         );
         const { result } = renderHook(() => useNeedAuthentication(), { wrapper });
 
@@ -60,17 +46,17 @@ describe('useNeedAuthentication', () => {
     });
 
     test('should be true if is_authentication_needed and is_low_risk_cr_eu_real both are true', async () => {
-        const mockRootStore: DeepPartial<TStores> = {
+        const mock = mockStore({
             client: {
                 is_authentication_needed: true,
             },
             traders_hub: {
                 is_low_risk_cr_eu_real: true,
             },
-        };
+        });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mockRootStore as TStores}>{children}</StoreProvider>
+            <StoreProvider store={mock}>{children}</StoreProvider>
         );
         const { result } = renderHook(() => useNeedAuthentication(), { wrapper });
 

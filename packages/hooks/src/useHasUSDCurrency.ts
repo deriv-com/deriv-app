7ad44264d28b84d3482ef4bb691382ac44495359
Farzin Mirzaie <farzin@deriv.com>
2023-04-12 15:46:36 +0800

    refactor: :construction: wip

--- packages/hooks/src/useHasUSDCurrency.ts
+++ packages/hooks/src/useHasUSDCurrency.ts
@@ -4,7 +4,7 @@ const useHasUSDCurrency = () => {
     const { client } = useStore();
     const { account_list } = client;
 
-    const has_usd_currency = account_list.some(account => account.title !== 'USD');
+    const has_usd_currency = account_list.some(account => account.title === 'USD');
 
     return has_usd_currency;
 };

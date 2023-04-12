723ec801e785565ab917dc9356129e235f3962a3
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-04-12 13:00:11 +0300

    feat: eslint import order config

--- packages/bot-web-ui/.eslintrc.js
+++ packages/bot-web-ui/.eslintrc.js
@@ -17,4 +17,46 @@ module.exports = {
             webpack: { config: webpackConfig({}) },
         },
     },
+    plugins: ['simple-import-sort'],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+    },
+    overrides: [
+        {
+            files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+            rules: {
+                'simple-import-sort/imports': [
+                    'error',
+                    {
+                        groups: [
+                            // `react` first, `next` second, then packages starting with a character
+                            ['^react$', '^next', '^[a-z]'],
+                            // Packages starting with `@`
+                            ['^@'],
+                            // Packages starting with `~`
+                            ['^~'],
+                            // Imports starting with `../`
+                            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                            // Imports starting with `./`
+                            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                            // Style imports
+                            ['^.+\\.s?css$'],
+                            // Side effect imports
+                            ['^\\u0000'],
+                        ],
+                    },
+                ],
+                //not sure we need it: show errors to each developer, i think we need only auto-fix
+
+                // "sort-imports": ["error", {
+                //     "ignoreCase": false,
+                //     "ignoreDeclarationSort": true,
+                //     "ignoreMemberSort": false,
+                //     "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+                //     "allowSeparatedGroups": false
+                // }]
+            },
+        },
+    ],
 };

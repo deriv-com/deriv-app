228311ed677ad9fe9aa54137dd0396c7674364d7
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-04-13 16:11:09 +0300

    fix: hide language icon while opening language menu

--- packages/core/src/App/Components/Layout/Header/toggle-menu-drawer.jsx
+++ packages/core/src/App/Components/Layout/Header/toggle-menu-drawer.jsx
@@ -432,16 +432,20 @@ const ToggleMenuDrawer = observer(({ platform_config }) => {
                         }
                     }}
                 >
-                    <Icon
-                        icon={`IcFlag${current_language.replace('_', '-')}`}
-                        data_testid='dt_icon'
-                        className='ic-settings-language__icon'
-                        type={current_language.replace(/(\s|_)/, '-').toLowerCase()}
-                        size={22}
-                    />
-                    <Text weight='bold' size='xxs'>
-                        <Localize i18n_default_text={current_language} />
-                    </Text>
+                    {!is_mobile_language_menu_open && (
+                        <React.Fragment>
+                            <Icon
+                                icon={`IcFlag${current_language.replace('_', '-')}`}
+                                data_testid='dt_icon'
+                                className='ic-settings-language__icon'
+                                type={current_language.replace(/(\s|_)/, '-').toLowerCase()}
+                                size={22}
+                            />
+                            <Text weight='bold' size='xxs'>
+                                <Localize i18n_default_text={current_language} />
+                            </Text>
+                        </React.Fragment>
+                    )}
                 </div>
             </React.Fragment>
         ),

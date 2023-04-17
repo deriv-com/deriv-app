05f0de528b04dc94b94282b1996335ab72805150
vinu-deriv <100689171+vinu-deriv@users.noreply.github.com>
2023-04-17 10:00:27 +0400

    Vinu/refactor: refactor react-joyride in dbot (#8208)
    * refactor: refactor react-joyride in dbot
    
    * fix: fix style in bot builder react-joyride
    
    * fix: fix style in react-joyride

--- packages/bot-web-ui/src/components/dashboard/bot-builder/bot-builder.tsx
+++ packages/bot-web-ui/src/components/dashboard/bot-builder/bot-builder.tsx
@@ -4,7 +4,7 @@ import { DesktopWrapper, MobileWrapper } from '@deriv/components';
 import { setColors, blocksCoordinate } from '@deriv/bot-skeleton';
 import LoadModal from 'Components/load-modal';
 import SaveModal from 'Components/save-modal';
-import ReactJoyride from 'react-joyride';
+import ReactJoyrideWrapper from '../react-joyride-wrapper';
 import AppStore from 'Stores/app-store';
 import { connect } from 'Stores/connect';
 import RootStore from 'Stores/index';
@@ -88,36 +88,7 @@ const BotBuilder = ({
                         <TourSlider />
                     </MobileWrapper>
                     <DesktopWrapper>
-                        <ReactJoyride
-                            steps={BOT_BUILDER_TOUR}
-                            run={is_tour_running}
-                            continuous
-                            showProgress
-                            callback={handleJoyrideCallback}
-                            locale={{ back: 'Previous' }}
-                            styles={{
-                                options: {
-                                    arrowColor: 'var(--general-main-2)',
-                                    backgroundColor: 'var(--general-main-2)',
-                                    primaryColor: 'var(--brand-red-coral)',
-                                    textColor: 'var(--text-general)',
-                                    spotlightShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
-                                },
-                                buttonBack: {
-                                    border: '0.1rem solid var(--text-less-prominent)',
-                                    marginRight: '1rem',
-                                    borderRadius: '0.4rem',
-                                    color: 'var(--text-general)',
-                                    padding: '0.6rem',
-                                },
-                                overlay: {
-                                    height: '100%',
-                                },
-                                tooltipContent: {
-                                    padding: '0 1rem',
-                                },
-                            }}
-                        />
+                        <ReactJoyrideWrapper steps={BOT_BUILDER_TOUR} run={is_tour_running} showProgress />
                     </DesktopWrapper>
                 </>
             )}

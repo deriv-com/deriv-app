5af9e7d446263d5150ce520d7a5cc2676385e4da
Taysuisin <suisin@regentmarkets.com>
2023-04-18 13:39:22 +0800

    chore: make language translate when user change language with footer language selection

--- packages/appstore/src/components/main-title-bar/account-type-dropdown.tsx
+++ packages/appstore/src/components/main-title-bar/account-type-dropdown.tsx
@@ -7,9 +7,10 @@ import { useStores } from 'Stores';
 import './account-type-dropdown.scss';
 
 const AccountTypeDropdown = () => {
-    const { traders_hub, client } = useStores();
+    const { traders_hub, client, common } = useStores();
     const { selected_account_type, selectAccountType } = traders_hub;
     const { setPrevAccountType } = client;
+    const { current_language } = common;
 
     return (
         <div className={classNames('account-type-dropdown--parent')}>
@@ -21,6 +22,7 @@ const AccountTypeDropdown = () => {
                     `account-type-dropdown--${selected_account_type}`
                 )}
                 list={getAccountTypes()}
+                key={`account-type-dropdown__icon--key-${current_language}`}
                 onChange={async (e: React.ChangeEvent<HTMLInputElement>) => {
                     await selectAccountType(e.target.value);
                     await setPrevAccountType(e.target.value);

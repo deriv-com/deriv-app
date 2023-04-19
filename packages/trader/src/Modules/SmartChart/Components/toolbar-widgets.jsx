2f52669e317113cb134f204297bcfc98c16d9156
Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>
2023-04-19 17:10:30 +0800

    Jim/88556/replace connect with use store in settings chart accumulators stats contract replay widget contract and toolbar widgets (#84)
    * chore: removeconnectsettings-chart/accumulators-stats/contract-replay-widget/contracttoolbar-widgets
    
    * refactor: remove connect from accumulators-stats.spec

--- packages/trader/src/Modules/SmartChart/Components/toolbar-widgets.jsx
+++ packages/trader/src/Modules/SmartChart/Components/toolbar-widgets.jsx
@@ -2,29 +2,28 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { isDesktop, isMobile } from '@deriv/shared';
 import { ChartMode, DrawTools, Share, StudyLegend, Views, ToolbarWidget } from 'Modules/SmartChart';
-import { connect } from 'Stores/connect';
+import { observer } from '@deriv/stores';
+import { useTraderStore } from 'Stores/useTraderStores';
 
-const ToolbarWidgets = ({ is_accumulator, position, updateChartType, updateGranularity }) => (
-    <ToolbarWidget position={position || isMobile() ? 'bottom' : null}>
-        {!is_accumulator && (
-            <ChartMode portalNodeId='modal_root' onChartType={updateChartType} onGranularity={updateGranularity} />
-        )}
-        {isDesktop() && <StudyLegend portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
-        {isDesktop() && <Views portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
-        {isDesktop() && <DrawTools portalNodeId='modal_root' />}
-        {isDesktop() && <Share portalNodeId='modal_root' />}
-    </ToolbarWidget>
-);
+const ToolbarWidgets = observer(({ position, updateChartType, updateGranularity }) => {
+    const { is_accumulator } = useTraderStore();
+    return (
+        <ToolbarWidget position={position || isMobile() ? 'bottom' : null}>
+            {!is_accumulator && (
+                <ChartMode portalNodeId='modal_root' onChartType={updateChartType} onGranularity={updateGranularity} />
+            )}
+            {isDesktop() && <StudyLegend portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
+            {isDesktop() && <Views portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
+            {isDesktop() && <DrawTools portalNodeId='modal_root' />}
+            {isDesktop() && <Share portalNodeId='modal_root' />}
+        </ToolbarWidget>
+    );
+});
 
 ToolbarWidgets.propTypes = {
     position: PropTypes.string,
     updateChartType: PropTypes.func,
     updateGranularity: PropTypes.func,
-    is_accumulator: PropTypes.func,
 };
 
-export default React.memo(
-    connect(({ modules }) => ({
-        is_accumulator: modules.trade.is_accumulator,
-    }))(ToolbarWidgets)
-);
+export default React.memo(ToolbarWidgets);

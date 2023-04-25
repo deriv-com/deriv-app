beaddf2df9609baf3ee84fe1a351e07a00e8c07f
Farzin Mirzaie <farzin@deriv.com>
2023-04-25 11:01:12 +0800

    feat(cashier): :sparkles: add `CashierOnboardingCryptoCard`

--- packages/cashier/src/modules/cashier-onboarding/components/index.ts
+++ packages/cashier/src/modules/cashier-onboarding/components/index.ts
@@ -1,4 +1,5 @@
 export * from './cashier-onboarding-card';
 export * from './cashier-onboarding-cash-card';
+export * from './cashier-onboarding-crypto-card';
 export * from './cashier-onboarding-icon-marquee';
 export * from './cashier-onboarding-title';

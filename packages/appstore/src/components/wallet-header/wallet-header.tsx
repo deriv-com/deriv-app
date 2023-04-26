6af236a44506d574988e84a62583d760c84aa634
Sergei Baranovski <sergei@deriv.com>
2023-04-26 10:30:29 +0300

    feat: delete uploaded icons because these icons already exists

--- packages/appstore/src/components/wallet-header/wallet-header.tsx
+++ packages/appstore/src/components/wallet-header/wallet-header.tsx
@@ -9,125 +9,135 @@ type TWalletHeaderCommon = {
 };
 
 type TWalletHeaderDemo = TWalletHeaderCommon & {
-    jurisdiction: 'virtual';
+    account_type: 'demo';
+    jurisdiction?: never;
     currency?: never;
 };
 
 type TWalletHeaderSvg = TWalletHeaderCommon & {
+    account_type?: 'real';
     jurisdiction: 'svg';
     currency: 'USD' | 'EUR' | 'AUD' | 'BTC' | 'ETH' | 'USDT' | 'eUSDT' | 'tUSDT' | 'LTC' | 'USDC';
 };
 
 type TWalletHeaderMalta = TWalletHeaderCommon & {
+    account_type?: 'real';
     jurisdiction: 'malta';
     currency: 'USD' | 'EUR';
 };
 
 type TWalletHeader = TWalletHeaderDemo | TWalletHeaderSvg | TWalletHeaderMalta;
 
-const WalletHeader = observer(({ balance = '0.00', currency = 'USD', jurisdiction = 'svg' }: TWalletHeader) => {
-    const [isOpen, setIsOpen] = React.useState(false);
+const WalletHeader = observer(
+    ({ balance = '0.00', currency = 'USD', jurisdiction = 'svg', account_type = 'real' }: TWalletHeader) => {
+        const [isOpen, setIsOpen] = React.useState(false);
+        const is_demo = account_type === 'demo';
 
-    const {
-        ui: { is_dark_mode_on },
-    } = useStore();
+        const {
+            ui: { is_dark_mode_on },
+        } = useStore();
 
-    const title_size = 'sm';
-    const badge_size = 'xxxs';
-    const badge_lh_size = 'xxxs';
-    const balance_title_size = 'xxs';
-    const balance_amount_size = 'm';
-    const button_text_size = 'xs';
+        const title_size = 'sm';
+        const badge_size = 'xxxs';
+        const badge_lh_size = 'xxxs';
+        const balance_title_size = 'xxs';
+        const balance_amount_size = 'm';
+        const button_text_size = 'xs';
 
-    const title_text = `<0>${jurisdiction === 'virtual' ? `Demo ${currency}` : currency} Wallet</0>`;
-    const badge_text = `<0>${jurisdiction.toUpperCase()}</0>`;
-    const balance_title_text = '<0>Wallet balance</0>';
-    const balance_amount_text = `<0>${balance} ${currency}</0>`;
+        const title_text = `<0>${is_demo ? `Demo ${currency}` : currency} Wallet</0>`;
+        const badge_text = `<0>${jurisdiction.toUpperCase()}</0>`;
+        const balance_title_text = '<0>Wallet balance</0>';
+        const balance_amount_text = `<0>${balance} ${currency}</0>`;
 
-    const btns_names =
-        jurisdiction === 'virtual'
+        const btn_names = is_demo
             ? ['Transfer', 'Transactions', 'Deposit']
             : ['Deposit', 'Withdraw', 'Transfer', 'Transactions'];
-    const wallet_buttons = (
-        <div className='wallet-header__description-buttons'>
-            {btns_names.map(name => (
-                <div key={name} className='wallet-header__description-buttons-item'>
-                    <Icon icon={`IcAppstoreWallet${name}`} custom_color={'var(--text-general)'} />
-                    <Localize
-                        i18n_default_text={`<0>${
-                            jurisdiction === 'virtual' && name === 'Deposit' ? 'Reset balance' : name
-                        }</0>`}
-                        components={[
-                            <Text
-                                key={0}
-                                weight='bold'
-                                size={button_text_size}
-                                className={classNames('wallet-header__description-buttons-item-text', {
-                                    'wallet-header__description-buttons-item-active': isOpen,
-                                })}
-                            />,
-                        ]}
-                    />
-                </div>
-            ))}
-        </div>
-    );
+        const icon_names = is_demo
+            ? ['IcAccountTransfer', 'IcStatement', 'IcCashierAdd']
+            : ['IcCashierAdd', 'IcCashierMinus', 'IcAccountTransfer', 'IcStatement'];
 
-    return (
-        <div className='wallet-header'>
-            <div className='wallet-header__container'>
-                <div
-                    className={`wallet-header__currency wallet-header__currency-${currency}${
-                        is_dark_mode_on ? '-dark' : ''
-                    }`}
-                >
-                    <Icon icon={`IcAppstoreWallet${currency[0] + currency.slice(1).toLowerCase()}Currency`} size={48} />
-                </div>
-                <div className='wallet-header__description'>
-                    <div className='wallet-header__description-title'>
+        const wallet_buttons = (
+            <div className='wallet-header__description-buttons'>
+                {btn_names.map((name, index) => (
+                    <div key={name} className='wallet-header__description-buttons-item'>
+                        <Icon icon={icon_names[index]} custom_color={'var(--text-general)'} />
                         <Localize
-                            i18n_default_text={title_text}
-                            components={[<Text key={0} weight='bold' size={title_size} />]}
+                            i18n_default_text={`<0>${is_demo && name === 'Deposit' ? 'Reset balance' : name}</0>`}
+                            components={[
+                                <Text
+                                    key={0}
+                                    weight='bold'
+                                    size={button_text_size}
+                                    className={classNames('wallet-header__description-buttons-item-text', {
+                                        'wallet-header__description-buttons-item-active': isOpen,
+                                    })}
+                                />,
+                            ]}
                         />
-                        {jurisdiction !== 'virtual' && (
+                    </div>
+                ))}
+            </div>
+        );
+
+        return (
+            <div className='wallet-header'>
+                <div className='wallet-header__container'>
+                    <div
+                        className={`wallet-header__currency wallet-header__currency-${currency}${
+                            is_dark_mode_on ? '-dark' : ''
+                        }`}
+                    >
+                        <Icon
+                            icon={`IcAppstoreWallet${currency[0] + currency.slice(1).toLowerCase()}Currency`}
+                            size={48}
+                        />
+                    </div>
+                    <div className='wallet-header__description'>
+                        <div className='wallet-header__description-title'>
                             <Localize
-                                i18n_default_text={badge_text}
-                                components={[
-                                    <Text
-                                        key={0}
-                                        weight='bold'
-                                        size={badge_size}
-                                        line_height={badge_lh_size}
-                                        className='wallet-header__description-badge'
-                                    />,
-                                ]}
+                                i18n_default_text={title_text}
+                                components={[<Text key={0} weight='bold' size={title_size} />]}
                             />
-                        )}
+                            {!is_demo && (
+                                <Localize
+                                    i18n_default_text={badge_text}
+                                    components={[
+                                        <Text
+                                            key={0}
+                                            weight='bold'
+                                            size={badge_size}
+                                            line_height={badge_lh_size}
+                                            className='wallet-header__description-badge'
+                                        />,
+                                    ]}
+                                />
+                            )}
+                        </div>
+                        {wallet_buttons}
                     </div>
-                    {wallet_buttons}
-                </div>
-                <div className='wallet-header__balance'>
-                    <div className='wallet-header__balance-title-amount'>
-                        <Localize
-                            i18n_default_text={balance_title_text}
-                            components={[<Text key={0} color='less-prominent' size={balance_title_size} />]}
-                        />
-                        <Localize
-                            i18n_default_text={balance_amount_text}
-                            components={[<Text key={0} weight='bold' size={balance_amount_size} />]}
+                    <div className='wallet-header__balance'>
+                        <div className='wallet-header__balance-title-amount'>
+                            <Localize
+                                i18n_default_text={balance_title_text}
+                                components={[<Text key={0} color='less-prominent' size={balance_title_size} />]}
+                            />
+                            <Localize
+                                i18n_default_text={balance_amount_text}
+                                components={[<Text key={0} weight='bold' size={balance_amount_size} />]}
+                            />
+                        </div>
+                        <Icon
+                            onClick={() => setIsOpen(!isOpen)}
+                            icon='IcChevronDownBold'
+                            className={classNames('wallet-header__balance-arrow-icon', {
+                                'wallet-header__balance-arrow-icon-active': isOpen,
+                            })}
                         />
                     </div>
-                    <Icon
-                        onClick={() => setIsOpen(!isOpen)}
-                        icon='IcChevronDownBold'
-                        className={classNames('wallet-header__balance-arrow-icon', {
-                            'wallet-header__balance-arrow-icon-active': isOpen,
-                        })}
-                    />
                 </div>
             </div>
-        </div>
-    );
-});
+        );
+    }
+);
 WalletHeader.displayName = 'WalletHeader';
 export default WalletHeader;
--- packages/appstore/src/modules/traders-hub/index.tsx
+++ packages/appstore/src/modules/traders-hub/index.tsx
@@ -85,7 +85,7 @@ const TradersHub = () => {
             >
                 {can_show_notify && <Notifications />}
                 <div id='traders-hub' className='traders-hub' ref={traders_hub_ref}>
-                    <WalletHeader jurisdiction={'virtual'} />
+                    <WalletHeader account_type='demo' />
                     <WalletHeader currency={'AUD'} jurisdiction={'svg'} />
                     <WalletHeader currency={'EUR'} jurisdiction={'svg'} />
                     <WalletHeader currency={'USD'} jurisdiction={'malta'} />

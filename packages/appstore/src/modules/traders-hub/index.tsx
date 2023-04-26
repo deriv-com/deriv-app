eba99c4e5b275f80b75d4ab3ed7b2c6dcdee7d56
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2023-04-26 14:15:15 +0330

    fix: update eu disclaimer percentage

--- packages/appstore/src/modules/traders-hub/index.tsx
+++ packages/appstore/src/modules/traders-hub/index.tsx
@@ -65,7 +65,7 @@ const TradersHub = () => {
             <div className='disclamer'>
                 <Text align='left' className='disclamer-text' size={isMobile() ? 'xxxs' : 'xs'}>
                     <Localize
-                        i18n_default_text='<0>EU statutory disclaimer</0>: CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. <0>71% of retail investor accounts lose money when trading CFDs with this provider</0>. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.'
+                        i18n_default_text='<0>EU statutory disclaimer</0>: CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. <0>73% of retail investor accounts lose money when trading CFDs with this provider</0>. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.'
                         components={[<strong key={0} />]}
                     />
                 </Text>

a4983dc17e500bd91e6bad89bacd5d2362ed792a
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-04-26 11:48:54 +0800

    refactor: suggestions resolved

--- packages/cfd/types.ts
+++ packages/cfd/types.ts
@@ -1,8 +1,3 @@
-import { DetailsOfEachMT5Loginid } from '@deriv/api-types';
 import type { TCoreStores } from '@deriv/stores/types';
-
-export type TNewDetailsOfEachMT5Loginid = Required<
-    Omit<DetailsOfEachMT5Loginid, 'market_type'> & { market_type?: 'financial' | 'synthetic' | 'all' }
->;
 
 export type TTradingPlatformAvailableAccount = TCoreStores['client']['trading_platform_available_accounts'][number];

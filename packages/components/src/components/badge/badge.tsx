e535ed86637c2dcde1ac698e242595b441850aa5
george-usynin-binary <heorhi@binary.com>
2023-04-28 12:25:10 +0300

    perf: :zap: add Discriminated unions types, fix colors, replace cheildren with label prop

--- packages/components/src/components/badge/badge.tsx
+++ packages/components/src/components/badge/badge.tsx
@@ -4,36 +4,42 @@ import classNames from 'classnames';
 type TBadgeSize = 'medium' | 'large';
 type TBadgeSpacing = 'tight' | 'loose';
 type TWeight = 'bold' | 'regular';
-type TType = 'contained' | 'bordered';
 type TBackgroundColor = 'blue' | 'orange' | 'red' | 'gray';
 type TRoundedCorners = 4 | 2;
 
-interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
-    background_color?: TBackgroundColor;
+interface Badge extends React.HTMLAttributes<HTMLSpanElement> {
     className?: string;
     color?: string;
+    label: string;
     rounded_corners?: TRoundedCorners;
     size?: TBadgeSize;
     spacing?: TBadgeSpacing;
-    type: TType;
     weight?: TWeight;
 }
 
-const Badge = ({
-    background_color,
-    children,
-    className,
-    color,
-    weight = 'bold',
-    rounded_corners = 4,
-    size = 'medium',
-    spacing = 'tight',
-    type,
-    ...props
-}: React.PropsWithChildren<BadgeProps>) => {
-    const is_contained = type === 'contained';
-    const is_bordered = type === 'bordered';
+interface BadgeContained extends Badge {
+    type: 'contained';
+    background_color: TBackgroundColor;
+}
 
+interface BadgeBordered extends Badge {
+    type: 'bordered';
+}
+
+type BadgeProps = BadgeContained | BadgeBordered;
+
+const Badge = (props: BadgeProps) => {
+    const {
+        className,
+        color,
+        label,
+        rounded_corners = 4,
+        size = 'medium',
+        spacing = 'tight',
+        type,
+        weight = 'bold',
+        ...rest
+    } = props;
     const getBadgeStyles = () => {
         return classNames(
             'dc-badge',
@@ -44,12 +50,12 @@ const Badge = ({
                 'dc-badge--loose': spacing === 'loose',
                 'dc-badge__medium': size === 'medium',
                 'dc-badge__large': size === 'large',
-                'dc-badge--contained': is_contained,
-                'dc-badge--bordered': is_bordered,
-                'dc-badge--blue': is_contained && background_color === 'blue',
-                'dc-badge--orange': is_contained && background_color === 'orange',
-                'dc-badge--red': is_contained && background_color === 'red',
-                'dc-badge--gray': is_contained && background_color === 'gray',
+                'dc-badge--contained': type === 'contained',
+                'dc-badge--bordered': type === 'bordered',
+                'dc-badge--blue': type === 'contained' && props.background_color === 'blue',
+                'dc-badge--orange': type === 'contained' && props.background_color === 'orange',
+                'dc-badge--red': type === 'contained' && props.background_color === 'red',
+                'dc-badge--gray': type === 'contained' && props.background_color === 'gray',
                 'dc-badge--full-rounded': rounded_corners === 4,
                 'dc-badge--two-rounded': rounded_corners === 2,
             },
@@ -57,11 +63,13 @@ const Badge = ({
         );
     };
 
-    props.style = { ...props.style, color: color || props.style?.color };
+    rest.style = { ...rest.style, color: color || rest.style?.color };
+
+    if (!label) return null;
 
     return (
-        <span className={getBadgeStyles()} {...props}>
-            {children}
+        <span className={getBadgeStyles()} {...rest}>
+            {label}
         </span>
     );
 };

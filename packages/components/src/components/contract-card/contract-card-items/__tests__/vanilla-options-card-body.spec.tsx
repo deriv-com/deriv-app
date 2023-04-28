c8b68871d9e7ed0b70dfb6cc375f246a4961ff74
Akmal Djumakhodjaev <akmal@binary.com>
2023-04-28 14:53:48 +0800

    chore: refactor test cases

--- packages/components/src/components/contract-card/contract-card-items/__tests__/vanilla-options-card-body.spec.tsx
+++ packages/components/src/components/contract-card/contract-card-items/__tests__/vanilla-options-card-body.spec.tsx
@@ -4,40 +4,32 @@ import VanillaOptionsCardBody from '../vanilla-options-card-body';
 import { TVanillaOptionsCardBodyProps } from '../vanilla-options-card-body';
 
 describe('VanillaOptionsCardBody', () => {
-  it('should render the correct content for a sold contract', async () => {
-    const { contract_info, currency, getCardLabels, is_sold, progress_slider, status }: TVanillaOptionsCardBodyProps = {
-      contract_info: {
-        buy_price: 100,
-        bid_price: 105,
-        entry_spot_display_value: '110',
-        barrier: '120',
-        sell_price: 95,
-        profit: -5,
-        status: 'loss',
-      },
-      currency: 'USD',
-      getCardLabels: () => ({
-        CONTRACT_VALUE: 'Contract Value',
-        ENTRY_SPOT: 'Entry Spot',
-        PURCHASE_PRICE: 'Purchase Price',
-        STRIKE: 'Strike',
-        TOTAL_PROFIT_LOSS: 'Total Profit/Loss',
-      }),
-      is_sold: true,
-      progress_slider: null,
+  const mock_props: TVanillaOptionsCardBodyProps = {
+    contract_info: {
+      buy_price: 100,
+      bid_price: 105,
+      entry_spot_display_value: '110',
+      barrier: '120',
+      sell_price: 95,
+      profit: -5,
       status: 'loss',
-    };
-
+    },
+    currency: 'USD',
+    getCardLabels: () => ({
+      CONTRACT_VALUE: 'Contract Value',
+      ENTRY_SPOT: 'Entry Spot',
+      PURCHASE_PRICE: 'Purchase Price',
+      STRIKE: 'Strike',
+      TOTAL_PROFIT_LOSS: 'Total Profit/Loss',
+    }),
+    is_sold: true,
+    progress_slider: null,
+    status: 'loss',
+  };
+  it('should render the correct content for a sold contract', async () => {
     // Render the component with the provided props
     const { getByText, getByTestId }: RenderResult = render(
-        <VanillaOptionsCardBody
-            contract_info={contract_info}
-            currency={currency}
-            getCardLabels={getCardLabels}
-            is_sold={is_sold}
-            progress_slider={progress_slider}
-            status={status}
-        />
+        <VanillaOptionsCardBody {...mock_props}/>
     );
 
     // Test that the correct elements are present in the component
@@ -51,38 +43,17 @@ describe('VanillaOptionsCardBody', () => {
   });
 
   it('should render the correct content for an unsold contract', async () => {
-    const { contract_info, currency, getCardLabels, is_sold, progress_slider, status }: TVanillaOptionsCardBodyProps = {
-      contract_info: {
-        buy_price: 100,
-        bid_price: 105,
-        entry_spot_display_value: '110',
-        barrier: '120',
-        profit: 5,
-        status: 'profit',
-      },
-      currency: 'USD',
-      getCardLabels: () => ({
-        CONTRACT_VALUE: 'Contract Value',
-        ENTRY_SPOT: 'Entry Spot',
-        PURCHASE_PRICE: 'Purchase Price',
-        STRIKE: 'Strike',
-        TOTAL_PROFIT_LOSS: 'Total Profit/Loss',
-      }),
-      is_sold: false,
-      progress_slider: <div />,
-      status: 'profit',
-    };
+    mock_props.contract_info.profit = 5;
+    mock_props.contract_info.status = 'profit';
+    mock_props.is_sold = false;
+    mock_props.progress_slider = <div />;
+    mock_props.status = 'profit';
+    delete mock_props.contract_info.sell_price;
+
 
     // Render the component with the provided props
     const { getByText, getByTestId }: RenderResult = render(
-        <VanillaOptionsCardBody
-            contract_info={contract_info}
-            currency={currency}
-            getCardLabels={getCardLabels}
-            is_sold={is_sold}
-            progress_slider={progress_slider}
-            status={status}
-        />
+        <VanillaOptionsCardBody {...mock_props}/>
     );
 
     // Test that the correct elements are present in the component

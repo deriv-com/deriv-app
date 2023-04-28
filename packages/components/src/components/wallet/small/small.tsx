dd27d69313cea56bedebc9269a61371f4891fd50
Hamid <hamid@regentmarkets.com>
2023-04-28 19:28:18 +0800

    hamid/wallet-icon-and-small (#8391)
    * feat: wallet-icon
    
    * fix: wallet-icon and wallet-small
    
    * feat: update styles
    
    * feat: update styles
    
    * feat: update styles
    
    * feat: update styles
    
    * feat: export wallet
    
    * feat: update type
    
    * chore: trigger-test

---
+++ packages/components/src/components/wallet/small/small.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+import Icon from '../../icon';
+import './small.scss';
+
+type TWalletSmallProps = {
+    icon: string;
+    className?: string;
+    bg?: string;
+    data_testid?: string;
+    icon_class?: string;
+    size?: string;
+};
+
+const WalletSmall = ({ bg, icon, className, data_testid, icon_class, size = '16px' }: TWalletSmallProps) => {
+    return (
+        <div className={classNames('wallet-small', bg, className)}>
+            <Icon data_testid={data_testid} className={classNames(icon_class)} icon={icon} size={size} />
+        </div>
+    );
+};
+
+export default WalletSmall;

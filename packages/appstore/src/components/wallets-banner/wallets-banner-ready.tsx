0b7647e74886b500b04295235ff7f240810554c1
Sergei Baranovski <sergei@deriv.com>
2023-04-28 14:20:53 +0300

    feat: add logout when click ready banner logout button

--- packages/appstore/src/components/wallets-banner/wallets-banner-ready.tsx
+++ packages/appstore/src/components/wallets-banner/wallets-banner-ready.tsx
@@ -4,6 +4,7 @@ import { Button, Icon, Text } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
 import { isMobile } from '@deriv/shared';
 import { TWalletsImagesListKeys } from 'Assets/svgs/wallets/image-types';
+import { useStore } from '@deriv/stores';
 
 // just for now for testing purpose, in the future 'is_eu' value will be taken from the store
 type TWalletsBannerReady = {
@@ -11,12 +12,20 @@ type TWalletsBannerReady = {
 };
 
 const WalletsBannerReady = ({ is_eu }: TWalletsBannerReady) => {
+    const {
+        client: { logout },
+    } = useStore();
+
     const image: TWalletsImagesListKeys = isMobile()
         ? `ready_mobile${is_eu ? '_eu' : ''}`
         : `ready_desktop${is_eu ? '_eu' : ''}`;
     const title_size = isMobile() ? 'xs' : 'sm';
     const description_size = isMobile() ? 'xxxs' : 'xs';
     const tick_size = isMobile() ? 16 : 24;
+
+    const onButtonClickHandler = async () => {
+        await logout();
+    };
 
     return (
         <div className='wallets-banner__container wallets-banner__ready-banner'>
@@ -33,7 +42,7 @@ const WalletsBannerReady = ({ is_eu }: TWalletsBannerReady) => {
                     components={[<Text key={0} size={description_size} />]}
                 />
                 <Button
-                    // Onclick props will be added here to show the upgrade wallets wizard in stitching user story
+                    onClick={onButtonClickHandler}
                     className='wallets-banner__ready-banner-button'
                     has_effect
                     text={localize('Log out')}

6bfb2d6c3d149176ba10ec6ead8611a2d8038405
kate-deriv <kate@regentmarkets.com>
2023-04-29 13:11:50 +0300

    refactor: add integr and unit tests for loader

--- packages/trader/src/_common/components/loading.tsx
+++ packages/trader/src/_common/components/loading.tsx
@@ -11,6 +11,7 @@ type LoadingProps = {
 const Loading = ({ className, is_invisible, theme, id }: LoadingProps) => (
     <div
         id={id}
+        data-testid='dt_barspinner'
         className={classNames('barspinner', `barspinner--${theme || 'dark'}`, { invisible: is_invisible }, className)}
     >
         {Array.from(new Array(5)).map((_x, inx) => (

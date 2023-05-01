bcebab8aa1553bdb590b43e6037ed311a287db1d
Shayan Khaleghparast <iman@re-work.dev>
2023-05-01 09:51:50 +0330

    feat: useTotalCurrency's test added

--- packages/hooks/src/__tests__/useTotalAccountBalance.spec.tsx
+++ packages/hooks/src/__tests__/useTotalAccountBalance.spec.tsx
@@ -5,7 +5,11 @@ import useTotalAccountBalance from '../useTotalAccountBalance';
 
 describe('useTotalAccountBalance', () => {
     test('should return zero when user has no account', async () => {
-        const mock = mockStore({});
+        const mock = mockStore({
+            client: {
+                active_accounts: [],
+            },
+        });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <StoreProvider store={mock}>{children}</StoreProvider>
@@ -17,11 +21,19 @@ describe('useTotalAccountBalance', () => {
 
     test('should return total balance correctly when user has multiple accounts', async () => {
         const mock = mockStore({
+            exchange_rates: {
+                data: {
+                    rates: {
+                        EUR: 2,
+                        AUD: 3,
+                    },
+                },
+            },
             client: {
                 active_accounts: [
                     {
-                        currency: 'USD',
-                        balance: 100,
+                        currency: 'AUD',
+                        balance: 300,
                     },
                     {
                         currency: 'EUR',
@@ -36,6 +48,6 @@ describe('useTotalAccountBalance', () => {
         );
         const { result } = renderHook(() => useTotalAccountBalance(mock.client.active_accounts), { wrapper });
 
-        expect(result.current.balance).toBe(300);
+        expect(result.current.balance).toBe(200);
     });
 });

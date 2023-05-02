2fe0ca3bedb41f6962be5f9c70e1b11110add067
Sergei Baranovski <sergei@deriv.com>
2023-05-02 23:35:15 +0300

    feat: change test file name

---
+++ packages/appstore/src/components/wallet/__tests__/wallet.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { mockStore, StoreProvider } from '@deriv/stores';
+import Wallet from '../wallet';
+
+const mocked_root_store = mockStore({});
+
+jest.mock('@deriv/account', () => ({
+    ...jest.requireActual('@deriv/account'),
+    getStatusBadgeConfig: jest.fn(() => ({ icon: '', text: '' })),
+}));
+
+describe('<WalletHeader />', () => {
+    describe('Check currency card', () => {
+        it('Should render right currency card for DEMO', () => {
+            render(
+                <StoreProvider store={mocked_root_store}>
+                    <p>123</p>
+                    {/* <Wallet
+                        account={{
+                            account_status: '',
+                            balance: '',
+                            currency: 'USD',
+                            jurisdiction: 'svg',
+                            account_type: 'demo',
+                        }}
+                    /> */}
+                </StoreProvider>
+            );
+            const currency_card = 'test';
+
+            expect(currency_card).toBe('test');
+        });
+    });
+});

2dccf62b2cb6ffdb968c0b018d14181291adf565
kate-deriv <kate@regentmarkets.com>
2023-05-02 11:06:38 +0300

    fix: delete container

--- packages/trader/src/Modules/Page404/Components/__tests__/Page404.spec.jsx
+++ packages/trader/src/Modules/Page404/Components/__tests__/Page404.spec.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Page404 from '../Page404';
 
 describe('Page404', () => {
     const browser_history = createBrowserHistory();
 
     it('should render Page404', () => {
-        const { container } = render(
+        render(
             <Router history={browser_history}>
                 <Page404 />
             </Router>
         );
 
-        expect(container).toBeInTheDocument();
+        expect(screen.getByText('We couldn’t find that page')).toBeInTheDocument();
     });
 });

57b9bb72f20add188f6668a804ff9829a1cae725
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-05-03 10:09:38 +0400

    refactor: to resolve sonarcloud issues

--- packages/core/src/App/Containers/RealAccountSignup/address-details.jsx
+++ packages/core/src/App/Containers/RealAccountSignup/address-details.jsx
@@ -1,3 +1,8 @@
 import { AddressDetails } from '@deriv/account';
+import { connect } from 'Stores/connect';
 
-export default AddressDetails;
+export default connect(({ client }) => ({
+    is_gb_residence: client.residence === 'gb',
+    fetchStatesList: client.fetchStatesList,
+    states_list: client.states_list,
+}))(AddressDetails);

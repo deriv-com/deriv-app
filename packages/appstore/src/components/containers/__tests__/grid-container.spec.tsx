766fe8e9173a461045b3d326b2dcb2c66ffc8352
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-05-03 11:52:40 +0800

    fix: variable

--- packages/appstore/src/components/containers/__tests__/grid-container.spec.tsx
+++ packages/appstore/src/components/containers/__tests__/grid-container.spec.tsx
@@ -6,15 +6,16 @@ import { StoreProvider, mockStore } from '@deriv/stores';
 describe('GridContainer', () => {
     it('should render children', () => {
         const store = mockStore({});
+        const traders_hub_label = 'Traders Hub';
         render(
             <StoreProvider store={store}>
                 <GridContainer>
                     <div>
-                        <h1>Traders hub</h1>
+                        <h1>{traders_hub_label}</h1>
                     </div>
                 </GridContainer>
             </StoreProvider>
         );
-        expect(screen.getByText('Traders hub')).toBeInTheDocument();
+        expect(screen.getByText(traders_hub_label)).toBeInTheDocument();
     });
 });

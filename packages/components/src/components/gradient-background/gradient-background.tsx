ca59b2b4c3aa171595bbc526897d076acb76ea5e
Farzin Mirzaie <farzin@deriv.com>
2023-05-05 16:53:45 +0800

    fix(components): :memo: resolve PR comments

--- packages/components/src/components/gradient-background/gradient-background.tsx
+++ packages/components/src/components/gradient-background/gradient-background.tsx
@@ -15,13 +15,13 @@ const GradientBackground: React.FC<React.PropsWithChildren<TProps>> = ({
     secondary,
     tertiary,
 }) => (
-    <div className='gradient-background'>
-        <div className='gradient-background__container' style={{ backgroundColor: color }}>
-            <div className='gradient-background__tertiary' style={{ background: tertiary }} />
-            <div className='gradient-background__secondary' style={{ background: secondary }} />
+    <div className='gradient-background' style={{ backgroundColor: color }}>
+        <div className='gradient-background__container'>
             <div className='gradient-background__primary' style={{ background: primary }} />
-            {children && <div className='gradient-background__children'>{children}</div>}
+            <div className='gradient-background__secondary' style={{ background: secondary }} />
+            <div className='gradient-background__tertiary' style={{ background: tertiary }} />
         </div>
+        {children && <div className='gradient-background__children'>{children}</div>}
     </div>
 );
 

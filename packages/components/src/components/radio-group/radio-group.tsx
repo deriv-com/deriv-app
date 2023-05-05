a0b75cf9c6c4a9ab9a806d2ae2ca9d42cc2a3e11
nada-deriv <nada.ajinas@deriv.com>
2023-05-05 15:39:19 +0400

    fix: replace map with condition to use filter

--- packages/components/src/components/radio-group/radio-group.tsx
+++ packages/components/src/components/radio-group/radio-group.tsx
@@ -51,44 +51,47 @@ const RadioGroup = ({
     return (
         <div className={classNames('dc-radio-group', className)}>
             {Array.isArray(children) &&
-                children.map(
-                    item =>
-                        !item.props.is_hidden && (
-                            <ItemWrapper key={item.props.value} should_wrap_items={should_wrap_items}>
-                                <label
-                                    className={classNames('dc-radio-group__item', className, {
-                                        'dc-radio-group__item--selected': selected_option === item.props.value,
-                                    })}
-                                >
-                                    <input
-                                        id={item.props.id}
-                                        name={name}
-                                        className='dc-radio-group__input'
-                                        type='radio'
-                                        value={item.props.value}
-                                        checked={selected_option === item.props.value}
-                                        onChange={onChange}
-                                        disabled={item.props.disabled}
-                                        required={required}
-                                    />
-                                    <span
-                                        className={classNames('dc-radio-group__circle', {
-                                            'dc-radio-group__circle--selected': selected_option === item.props.value,
-                                            'dc-radio-group__circle--disabled': item.props.disabled,
-                                        })}
-                                    />
-                                    <Text
-                                        size='xs'
-                                        className={classNames('dc-radio-group__label', {
-                                            'dc-radio-group__label--disabled': item.props.disabled,
+                children
+                    .filter(item => !item.props.is_hidden)
+                    .map(
+                        item =>
+                            !item.props.is_hidden && (
+                                <ItemWrapper key={item.props.value} should_wrap_items={should_wrap_items}>
+                                    <label
+                                        className={classNames('dc-radio-group__item', className, {
+                                            'dc-radio-group__item--selected': selected_option === item.props.value,
                                         })}
                                     >
-                                        {item.props.label}
-                                    </Text>
-                                </label>
-                            </ItemWrapper>
-                        )
-                )}
+                                        <input
+                                            id={item.props.id}
+                                            name={name}
+                                            className='dc-radio-group__input'
+                                            type='radio'
+                                            value={item.props.value}
+                                            checked={selected_option === item.props.value}
+                                            onChange={onChange}
+                                            disabled={item.props.disabled}
+                                            required={required}
+                                        />
+                                        <span
+                                            className={classNames('dc-radio-group__circle', {
+                                                'dc-radio-group__circle--selected':
+                                                    selected_option === item.props.value,
+                                                'dc-radio-group__circle--disabled': item.props.disabled,
+                                            })}
+                                        />
+                                        <Text
+                                            size='xs'
+                                            className={classNames('dc-radio-group__label', {
+                                                'dc-radio-group__label--disabled': item.props.disabled,
+                                            })}
+                                        >
+                                            {item.props.label}
+                                        </Text>
+                                    </label>
+                                </ItemWrapper>
+                            )
+                    )}
         </div>
     );
 };

50f586fadc6c0072776baeb95d4bc2c0b650a729
Farzin Mirzaie <farzin@deriv.com>
2023-05-05 17:49:47 +0800

    feat(components): :sparkles: add `Watermark`

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -107,6 +107,7 @@ export { default as Timeline } from './components/timeline';
 export { default as Tooltip } from './components/tooltip';
 export { default as UILoader } from './components/u-i-loader';
 export { default as VerticalTab } from './components/vertical-tab';
+export * from './components/watermark';
 export { default as Wizard } from './components/wizard';
 export * from './components/wallet';
 export * from './hooks';

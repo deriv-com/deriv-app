07c44e82e3272a3783b437b3389e2feb201a5612
Farzin Mirzaie <farzin@deriv.com>
2023-05-06 12:38:08 +0800

    fix(cashier): :memo: resolve conflicts

--- packages/cashier/src/pages/withdrawal/__tests__/withdrawal.spec.tsx
+++ packages/cashier/src/pages/withdrawal/__tests__/withdrawal.spec.tsx
@@ -32,8 +32,10 @@ jest.mock('@deriv/shared/src/utils/screen/responsive', () => ({
 jest.mock('@deriv/hooks', () => ({
     ...jest.requireActual('@deriv/hooks'),
     useCashierLocked: jest.fn(() => false),
+    useWithdrawLocked: jest.fn(() => false),
 }));
 const mockUseCashierLocked = useCashierLocked as jest.MockedFunction<typeof useCashierLocked>;
+const mockUseWithdrawLocked = useWithdrawLocked as jest.MockedFunction<typeof useWithdrawLocked>;
 
 const cashier_mock = {
     general_store: {
@@ -66,9 +68,9 @@ describe('<Withdrawal />', () => {
     let setSideNotes: VoidFunction;
 
     beforeEach(() => {
-        (useWithdrawLocked as jest.Mock).mockReturnValue(false);
         setSideNotes = jest.fn();
         mockUseCashierLocked.mockReturnValue(false);
+        mockUseWithdrawLocked.mockReturnValue(false);
     });
 
     const renderWithdrawal = (mock_root_store: TStores, is_rerender = false) => {
@@ -83,7 +85,7 @@ describe('<Withdrawal />', () => {
     };
 
     it('should render <CashierLocked /> component', () => {
-        (useWithdrawLocked as jest.Mock).mockReturnValue(true);
+        mockUseWithdrawLocked.mockReturnValue(true);
         const mock_root_store = mockStore({
             client: {
                 account_status: { cashier_validation: ['system_maintenance'] },
@@ -156,7 +158,7 @@ describe('<Withdrawal />', () => {
     });
 
     it('should render <WithdrawalLocked /> component', () => {
-        (useWithdrawLocked as jest.Mock).mockReturnValue(true);
+        mockUseWithdrawLocked.mockReturnValue(true);
         const mock_root_store = mockStore({
             client: {
                 balance: '1000',

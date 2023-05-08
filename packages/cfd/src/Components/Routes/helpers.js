e61174963882499a711f4294330f6c9fca7e5eb6
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-05-08 16:32:44 +0800

    chore: remove the dead code helper function v1.2

--- packages/cfd/src/Components/Routes/helpers.js
+++
@@ -1,8 +0,0 @@
-import { routes } from '@deriv/shared';
-
-export const isRouteVisible = (route, is_logged_in) => !(route && route.is_authenticated && !is_logged_in);
-
-export const getPath = (route_path, params = {}) =>
-    Object.keys(params).reduce((p, name) => p.replace(`:${name}`, params[name]), route_path);
-
-export const getContractPath = contract_id => getPath(routes.contract, { contract_id });

45ae26a24fca702564a37568049be1e8a90adeea
Farzin Mirzaie <farzin@deriv.com>
2023-05-10 15:06:03 +0800

    style(cashier): :lipstick: update the styles to match with the design

--- packages/cashier/src/modules/deposit-crypto/components/deposit-crypto-try-fiat-onramp/deposit-crypto-try-fiat-onramp.tsx
+++ packages/cashier/src/modules/deposit-crypto/components/deposit-crypto-try-fiat-onramp/deposit-crypto-try-fiat-onramp.tsx
@@ -10,19 +10,14 @@ const DepositCryptoTryFiatOnRamp: React.FC = observer(() => {
 
     return (
         <div className='deposit-crypto-try-fiat-onramp'>
-            <div className='deposit-crypto-try-fiat-onramp__description'>
-                <Text as='p' size={is_mobile ? 'xs' : 's'} align='center'>
-                    {localize('Looking for a way to buy cryptocurrency?')}
-                </Text>
-                {is_mobile ? null : <br />}
-                <Text as='p' size={is_mobile ? 'xs' : 's'} align='center'>
-                    {localize(
-                        'Use our fiat onramp services to buy and deposit cryptocurrency into your Deriv account.'
-                    )}
-                </Text>
-            </div>
+            <Text size={is_mobile ? 'xs' : 's'} align='center'>
+                {localize('Looking for a way to buy cryptocurrency?')}
+            </Text>
+            <Text size={is_mobile ? 'xs' : 's'} align='center'>
+                {localize('Use our fiat onramp services to buy and deposit cryptocurrency into your Deriv account.')}
+            </Text>
             <ButtonLink to='/cashier/on-ramp'>
-                <Text as='p' weight='bold' color='colored-background' size='xs'>
+                <Text weight='bold' color='colored-background' size='xs'>
                     {localize('Try our Fiat onramp')}
                 </Text>
             </ButtonLink>

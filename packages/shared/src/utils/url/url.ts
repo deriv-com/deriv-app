8458ddf4f9b9a7061c0d0f42000ac9deaf970b7b
adrienne-rio <adrienne@deriv.com>
2023-05-10 13:18:28 +0800

    chore: test codeql on push

--- packages/shared/src/utils/url/url.ts
+++ packages/shared/src/utils/url/url.ts
@@ -30,12 +30,7 @@ const isUrl = (str: string) => {
     return true;
 };
 
-export const legacyUrlForLanguage = (target_language: string, url: string = window.location.href) => {
-    if (isUrl(default_language)) {
-        return url.replace(new RegExp(`/EN/`, 'i'), `/${(target_language || 'EN').trim().toLowerCase()}/`);
-    }
-    return url.replace(new RegExp(`/${default_language}/`, 'i'), `/${(target_language || 'EN').trim().toLowerCase()}/`);
-};
+export const legacyUrlForLanguage = (target_language: string, url: string = window.location.href) => url;
 
 export const urlForLanguage = (lang: string, url: string = window.location.href) => {
     const current_url = new URL(url);

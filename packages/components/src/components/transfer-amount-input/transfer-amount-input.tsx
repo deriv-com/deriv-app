f914f7194099c46f93bcd351e58d111d15baeea9
Rostik Kayko <rostislav@deriv.com>
2023-05-10 14:09:58 +0300

    refactor: currency issue fixed

--- packages/components/src/components/transfer-amount-input/transfer-amount-input.tsx
+++ packages/components/src/components/transfer-amount-input/transfer-amount-input.tsx
@@ -2,31 +2,51 @@ import React, { useState } from 'react';
 import Input from '../input';
 
 type TTransferAmountInput = {
-    className?: string;
     currency: string;
+    decimalPoints?: number;
     disabled?: boolean;
     initialValue?: number;
     label?: React.ReactNode;
+    maxDigits: number;
 };
 
-const TransferAmountInput = ({ className, currency, disabled = false, initialValue, label }: TTransferAmountInput) => {
-    const [value, setValue] = useState(0);
+const TransferAmountInput = ({
+    currency,
+    decimalPoints = 2,
+    disabled = false,
+    initialValue = 0,
+    label,
+    maxDigits,
+}: TTransferAmountInput) => {
+    const [value, setValue] = useState(initialValue);
+    const [focus, setFocus] = useState(false);
+
+    const displayNumber = (number: number) => number.toLocaleString('en-US', { minimumFractionDigits: decimalPoints });
 
     return (
         <div className='transfer-amount-input-wrapper'>
             <span>{label}</span>
-            <Input
-                className='transfer-amount-input'
-                disabled={disabled}
-                onChange={e => {
-                    let input_value = e.target.value;
-                    input_value = input_value.replace(/\D/g, '');
-                    setValue(Number(input_value) / 100);
-                }}
-                type={'text'}
-                placeholder={`${value.toLocaleString('en-US', { minimumFractionDigits: 2 })} USD`}
-                value={value.toLocaleString('en-US', { minimumFractionDigits: 2 })}
-            />
+            <div className='transfer-amount-input-container'>
+                <Input
+                    className='transfer-amount-input'
+                    disabled={focus}
+                    type='text'
+                    value={`${displayNumber(value)} ${currency}`}
+                />
+                <Input
+                    className='transfer-amount-input'
+                    disabled={disabled}
+                    max_characters={displayNumber(Math.pow(10, maxDigits - 1) / decimalPoints).length}
+                    onFocus={() => setFocus(true)}
+                    onBlur={() => setFocus(false)}
+                    onChange={e => {
+                        const input_value = e.target.value.replace(/\D/g, '');
+                        if (Number(input_value) <= Math.pow(10, maxDigits)) setValue(Number(input_value) / 100);
+                    }}
+                    type='text'
+                    value={displayNumber(value)}
+                />
+            </div>
         </div>
     );
 };

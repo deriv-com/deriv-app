6c8ef6d387f88d769749845277f9ca443012e7ee
Farabi <102643568+farabi-deriv@users.noreply.github.com>
2023-05-11 17:49:51 +0800

    Farabi/94572/fix back button should not stop bot (#8571)
    * fix: back button functionality
    
    * fix: added the conditon in a variable

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/stop-bot-modal-content.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/stop-bot-modal-content.tsx
@@ -20,11 +20,14 @@ const StopBotModalContent = ({
     is_stop_bot_dialog_open,
     closeMultiplierContract,
     onOkButtonClick,
+    toggleStopBotDialog,
 }: TStopBotModalContent) => {
     const confirm_button_text = is_running && is_multiplier ? localize('Keep my contract') : localize('Stop my bot');
     const cancel_button_text = is_running && is_multiplier ? localize('Close my contract') : localize('Back');
     const title_text =
         is_running && is_multiplier ? localize('Keep your current contract?') : localize('Stop your current bot?');
+    const toggle_dialog_or_stop = is_running && is_multiplier ? closeMultiplierContract : toggleStopBotDialog;
+
     return (
         <React.Fragment>
             <Dialog
@@ -34,7 +37,7 @@ const StopBotModalContent = ({
                 confirm_button_text={confirm_button_text}
                 onConfirm={onOkButtonClick}
                 cancel_button_text={cancel_button_text}
-                onCancel={closeMultiplierContract}
+                onCancel={toggle_dialog_or_stop}
                 is_mobile_full_width={false}
                 className={'toolbar__dialog'}
                 has_close_icon

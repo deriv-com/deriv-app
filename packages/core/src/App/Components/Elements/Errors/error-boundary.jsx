bb7b835827316c56d3143c9d8860d0362651ea57
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-05-11 11:34:22 +0400

    refactor: remove connect method from error boundary component

--- packages/core/src/App/Components/Elements/Errors/error-boundary.jsx
+++ packages/core/src/App/Components/Elements/Errors/error-boundary.jsx
@@ -1,32 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { connect } from 'Stores/connect';
+import { observer, useStore } from '@deriv/stores';
 import ErrorComponent from './index';
 
-class ErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+const ErrorBoundary = observer(({ children }) => {
+    const root_store = useStore();
+    const [hasError, setHasError] = useState(false);
+    const [error, setError] = useState(null);
+    const [info, setInfo] = useState(null);
 
-    componentDidCatch = (error, info) => {
-        if (window.TrackJS) window.TrackJS.console.log(this.props.root_store);
+    const handleError = (errorMsg, infoMsg) => {
+        if (window.TrackJS) window.TrackJS.console.log(root_store);
 
-        this.setState({
-            hasError: true,
-            error,
-            info,
-        });
+        setHasError(true);
+        setError(errorMsg);
+        setInfo(infoMsg);
     };
+    useEffect(() => {
+        window.addEventListener('error', () => {
+            handleError(error, info);
+        });
+        return () =>
+            window.removeEventListener('error', () => {
+                handleError(error, info);
+            });
+    }, [handleError]);
 
-    render = () => (this.state.hasError ? <ErrorComponent should_show_refresh={true} /> : this.props.children);
-}
+    return hasError ? <ErrorComponent should_show_refresh={true} /> : children;
+});
 
 ErrorBoundary.propTypes = {
-    root_store: PropTypes.object,
     children: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
 
-export default connect(store => ({
-    root_store: store,
-}))(ErrorBoundary);
+export default ErrorBoundary;

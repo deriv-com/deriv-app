2878c678b83d66481dec9893fbff7aba6d9f49c8
Aum Bhatt <aum@deriv.com>
2023-05-14 15:06:53 +0400

    refactor: added single prop for handling wallet-card states

--- packages/components/src/components/wallet-card/wallet-card.tsx
+++ packages/components/src/components/wallet-card/wallet-card.tsx
@@ -1,118 +1,100 @@
 import React from 'react';
-import './wallet-card.scss';
+import classNames from 'classnames';
+import { localize } from '@deriv/translations';
+import Badge from '../badge';
+import Button from '../button';
 import Icon from '../icon';
 import Text from '../text';
-import Button from '../button';
-import classNames from 'classnames';
-import Badge from '../badge';
 import { GradientBackground } from '../gradient-background';
 import { WalletIcon } from '../wallet-icon';
+import './wallet-card.scss';
 
-type WalletCardProps = {
-    active?: boolean;
-    has_add_button?: boolean;
+type TWalletCardProps = {
     balance?: string;
     currency?: string;
-    dark?: boolean;
-    disabled?: boolean;
-    faded?: boolean;
     icon: JSX.Element;
-    jurisdiction: string;
+    badgeText: string;
     size?: 'small' | 'medium' | 'large';
     wallet_name?: string;
+    wallet_state?: 'active' | 'add' | 'default' | 'disabled' | 'faded';
 };
-const WalletCard = ({
-    active,
-    has_add_button,
+
+const WalletCard: React.FC<React.PropsWithChildren<TWalletCardProps>> = ({
     balance,
     currency,
-    dark,
-    disabled,
-    faded,
     icon,
-    jurisdiction,
+    badgeText,
     size,
     wallet_name,
-}: WalletCardProps) => {
-    return (
-        <div
-            className={classNames('wallet-card', {
-                'wallet-card--add-wallet': has_add_button,
-                'wallet-card--dark': dark,
-                'wallet-card--faded': faded,
-                [`wallet-card--${size}`]: size,
-                'wallet-card--disabled': disabled,
-            })}
-        >
-            {size === 'small' ? (
-                <div className='wallet-card__small-container'>
-                    <div
-                        className={classNames('wallet-card__small-container-overlay', {
-                            'wallet-card__small-container-overlay--active': active && !disabled && !faded,
-                            'wallet-card__small-container-overlay--hover': !disabled,
-                        })}
-                    />
-                    <WalletIcon icon={icon} primaryColor='#F44336' secondaryColor='#283991' />
-                </div>
-            ) : (
-                <GradientBackground
-                    color='var(--general-main-2)'
-                    primary='#F44336'
-                    secondary='#283991'
-                    tertiary='#F44336'
-                >
-                    <div
-                        className={classNames('wallet-card__content', {
-                            'wallet-card__content--active': active && !disabled && !faded,
-                            'wallet-card__content--hover': !disabled,
-                        })}
-                    >
-                        <div className='wallet-card__shine' />
-                        <div className='wallet-card__top-wrapper'>
-                            {icon}
-                            <Badge custom_color='var(--text-prominent' label={jurisdiction} type='bordered' />
-                        </div>
-                        <div className='wallet-card__bottom-wrapper'>
-                            {has_add_button && !active && !faded && !disabled ? (
-                                <Button
-                                    className='wallet-card__add-wallet-button'
-                                    classNameSpan='wallet-card__add-wallet-button-text'
-                                    icon={
-                                        <Icon
-                                            className='wallet-card__add-wallet-button-icon'
-                                            custom_color='$color-black-1'
-                                            icon='IcAddBold'
-                                            size={12}
-                                        />
-                                    }
-                                    text='Add'
-                                />
-                            ) : (
-                                <React.Fragment>
-                                    <Text color='prominent' size='xxxs'>
-                                        {wallet_name} Wallet
-                                    </Text>
-                                    <Text color='prominent' weight='bold' size='xs'>
-                                        {balance} {currency}
-                                    </Text>
-                                </React.Fragment>
-                            )}
-                        </div>
-                    </div>
-                </GradientBackground>
-            )}
-            {active && !disabled && !faded && (
-                <Icon
-                    className={classNames('wallet-card__active-icon', {
-                        [`wallet-card__active-icon--small`]: size === 'small',
+    wallet_state = 'default',
+}) => (
+    <div
+        className={classNames('wallet-card', {
+            [`wallet-card--${size}`]: size,
+            [`wallet-card--${wallet_state}`]: wallet_state,
+        })}
+    >
+        {size === 'small' ? (
+            <div className='wallet-card__small-container'>
+                <div
+                    className={classNames('wallet-card__small-container-overlay', {
+                        [`wallet-card__small-container-overlay--${wallet_state}`]: wallet_state,
                     })}
-                    color='brand'
-                    data_testid='ic-checkmark-circle'
-                    icon='IcCheckmarkCircle'
-                    size={size === 'small' ? 16 : 32}
                 />
-            )}
-        </div>
-    );
-};
+                <WalletIcon icon={icon} primaryColor='#F44336' secondaryColor='#283991' />
+            </div>
+        ) : (
+            <GradientBackground color='var(--general-main-2)' primary='#F44336' secondary='#283991' tertiary='#F44336'>
+                <div
+                    className={classNames('wallet-card__content', {
+                        [`wallet-card__content--${wallet_state}`]: wallet_state,
+                    })}
+                >
+                    <div className='wallet-card__shine' />
+                    <div className='wallet-card__top-wrapper'>
+                        {icon}
+                        <Badge custom_color='var(--text-prominent' label={badgeText} type='bordered' />
+                    </div>
+                    <div className='wallet-card__bottom-wrapper'>
+                        {wallet_state === 'add' ? (
+                            <Button
+                                className='wallet-card__add-wallet-button'
+                                classNameSpan='wallet-card__add-wallet-button-text'
+                                icon={
+                                    <Icon
+                                        className='wallet-card__add-wallet-button-icon'
+                                        custom_color='$color-black-1'
+                                        icon='IcAddBold'
+                                        size={12}
+                                    />
+                                }
+                                text='Add'
+                            />
+                        ) : (
+                            <React.Fragment>
+                                <Text color='prominent' size='xxxs'>
+                                    {wallet_name} {localize('Wallet')}
+                                </Text>
+                                <Text color='prominent' weight='bold' size='xs'>
+                                    {balance} {currency}
+                                </Text>
+                            </React.Fragment>
+                        )}
+                    </div>
+                </div>
+            </GradientBackground>
+        )}
+        {wallet_state === 'active' && (
+            <Icon
+                className={classNames('wallet-card__active-icon', {
+                    [`wallet-card__active-icon--small`]: size === 'small',
+                })}
+                color='brand'
+                data_testid='ic-checkmark-circle'
+                icon='IcCheckmarkCircle'
+                size={size === 'small' ? 16 : 32}
+            />
+        )}
+    </div>
+);
 export default WalletCard;

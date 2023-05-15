086eab9b3096626dfa04cd1a5ecab9efe720afed
Aum Bhatt <aum@deriv.com>
2023-05-15 15:34:58 +0400

    chore: added constants config for wallet-card

---
+++ packages/appstore/src/constants/wallet-card-config.ts
@@ -0,0 +1,15 @@
+export const wallet_card = {
+    sizes: {
+        SMALL: 'small',
+        MEDIUM: 'medium',
+        LARGE: 'large',
+    },
+    states: {
+        ACTIVE: 'active',
+        ADD: 'add',
+        ADDED: 'added',
+        DEFAULT: 'default',
+        DISABLED: 'disabled',
+        FADED: 'faded',
+    },
+};

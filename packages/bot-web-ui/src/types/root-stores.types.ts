e60005e9f9d3bad4793ec17c1fade5eb964ea0e5
shafin-deriv <shafin@regentmarkets.com>
2023-05-15 11:20:15 +0800

    refactor: remove connect from quick-strategy

--- packages/bot-web-ui/src/types/root-stores.types.ts
+++ packages/bot-web-ui/src/types/root-stores.types.ts
@@ -6,4 +6,7 @@ import type { TCoreStores } from '@deriv/stores/types';
 export type TRootStore = TCoreStores & {
     gtm?: Record<string, unknown>;
     portfolio?: Record<string, unknown>;
+    ui: TCoreStores['ui'] & {
+        is_onscreen_keyboard_active?: boolean;
+    };
 };

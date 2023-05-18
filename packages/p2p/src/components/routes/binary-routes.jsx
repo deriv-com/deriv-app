c63602d14830e11b990c757dfe496067965b7fd1
niloofar sadeghi <niloofar.sadeghi@re-work.dev>
2023-05-18 15:36:08 +0330

    fix: review comments

--- packages/p2p/src/components/routes/binary-routes.jsx
+++ packages/p2p/src/components/routes/binary-routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import { Localize } from '@deriv/translations';
-import getRoutesConfig from '../../constants/routes-config';
+import getRoutesConfig from 'Constants/routes-config';
 import RouteWithSubRoutes from './route-with-sub-routes';
 
 const Loading = () => (

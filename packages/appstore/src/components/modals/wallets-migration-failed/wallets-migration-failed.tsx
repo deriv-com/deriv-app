1a01315be946f8f4d5eca275b7bf861244d3ffc7
Sergei Baranovski <sergei@deriv.com>
2023-05-18 18:01:03 +0300

    feat: add styles for popup and  handlers for buttons

--- packages/appstore/src/components/modals/wallets-migration-failed/wallets-migration-failed.tsx
+++ packages/appstore/src/components/modals/wallets-migration-failed/wallets-migration-failed.tsx
@@ -1,65 +1,124 @@
 import React from 'react';
-import { DesktopWrapper, Text, MobileWrapper, useOnClickOutside, Dialog } from '@deriv/components';
+import { DesktopWrapper, Text, MobileWrapper, useOnClickOutside, Modal, Button } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import { useStore, observer } from '@deriv/stores';
 import './wallets-migration-failed.scss';
 
 const WalletsMigrationFailed = observer(() => {
-    const { traders_hub, ui } = useStore();
+    const { traders_hub } = useStore();
     const { is_wallet_migration_failed, toggleWalletsMigrationFailedPopup } = traders_hub;
-    const { is_mobile } = ui;
 
-    const wallets_upgrade_ref = React.useRef<HTMLDivElement>(null);
+    const wallets_migration_failed_ref = React.useRef<HTMLDivElement>(null);
 
     const handleClose = () => {
         toggleWalletsMigrationFailedPopup(false);
     };
 
+    const handLiveChatButtonClick = () => {
+        toggleWalletsMigrationFailedPopup(false);
+        window.LiveChatWidget?.call('maximize');
+    };
+
     const validateClickOutside = (e: MouseEvent) => {
+        // console.log('TRALALA');
+        // return is_wallet_migration_failed && !wallets_migration_failed_ref?.current?.contains(e.target as Node);
+
         // anywhere outside the modal should close the modal
-        if (wallets_upgrade_ref.current && !wallets_upgrade_ref.current.contains(e.target as Node)) {
+        if (wallets_migration_failed_ref.current && !wallets_migration_failed_ref.current.contains(e.target as Node)) {
+            // console.log('Validate FALSE');
             return false;
         }
+        // console.log('Validate TRUE');
         return true;
     };
 
-    useOnClickOutside(wallets_upgrade_ref, handleClose, validateClickOutside);
-
-    const dialog = (
-        <Dialog
-            confirm_button_text={localize('Back to Trader’s Hub')}
-            cancel_button_text={localize('Go to live chat')}
-            is_closed_on_cancel
-            is_closed_on_confirm
-            is_visible={is_wallet_migration_failed}
-            onCancel={handleClose}
-            onConfirm={handleClose}
-        >
-            <React.Fragment>
-                <Text
-                    as='h1'
-                    size={is_mobile ? 'xs' : 's'}
-                    color='prominent'
-                    weight='bold'
-                    className='wallets-migration-failed__title'
-                >
-                    {localize('Sorry for the interruption')}
-                </Text>
-                <Text size={is_mobile ? 'xxs' : 'xs'}>
-                    {localize(
-                        "We're unable to complete with the Wallet upgrade. Please try again later or contact us via live chat."
-                    )}
-                </Text>
-            </React.Fragment>
-        </Dialog>
-    );
+    // useOnClickOutside(wallets_migration_failed_ref, handleClose, validateClickOutside);
 
     return (
         <React.Fragment>
             {is_wallet_migration_failed && (
                 <React.Fragment>
-                    <DesktopWrapper>{dialog}</DesktopWrapper>
-                    <MobileWrapper>{dialog}</MobileWrapper>
+                    <DesktopWrapper>
+                        {/* <div ref={wallets_migration_failed_ref}> */}
+                        <Modal
+                            is_open={is_wallet_migration_failed}
+                            toggleModal={handleClose}
+                            // height='20rem'
+                            width='44rem'
+                            should_header_stick_body={false}
+                            has_close_icon={false}
+                        >
+                            <Modal.Body>
+                                <React.Fragment>
+                                    <Text
+                                        as='h1'
+                                        size='s'
+                                        color='prominent'
+                                        weight='bold'
+                                        className='wallets-migration-failed__title'
+                                    >
+                                        {localize('Sorry for the interruption')}
+                                    </Text>
+                                    <Text size='xs'>
+                                        {localize(
+                                            "We're unable to complete with the Wallet upgrade. Please try again later or contact us via live chat."
+                                        )}
+                                    </Text>
+                                </React.Fragment>
+                            </Modal.Body>
+                            <Modal.Footer className='wallets-migration-failed__footer'>
+                                <Button secondary large onClick={handLiveChatButtonClick}>
+                                    {localize('Go to live chat')}
+                                </Button>
+                                <Button primary large onClick={handleClose}>
+                                    {localize('Back to Trader’s Hub')}
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                        {/* </div> */}
+                    </DesktopWrapper>
+                    <MobileWrapper>
+                        <Modal
+                            is_open={is_wallet_migration_failed}
+                            toggleModal={handleClose}
+                            // height='16rem'
+                            width='32.3rem'
+                            should_header_stick_body={false}
+                            has_close_icon={false}
+                        >
+                            <Modal.Body className='wallets-migration-failed--mobile'>
+                                <React.Fragment>
+                                    <Text
+                                        as='h1'
+                                        size='xs'
+                                        color='prominent'
+                                        weight='bold'
+                                        className='wallets-migration-failed__title--mobile'
+                                    >
+                                        {localize('Sorry for the interruption')}
+                                    </Text>
+                                    <Text size='xxs'>
+                                        {localize(
+                                            "We're unable to complete with the Wallet upgrade. Please try again later or contact us via live chat."
+                                        )}
+                                    </Text>
+                                </React.Fragment>
+                            </Modal.Body>
+                            <Modal.Footer className='wallets-migration-failed__footer--mobile'>
+                                <Button secondary large onClick={handLiveChatButtonClick}>
+                                    {localize('Go to live chat')}
+                                </Button>
+                                <Button
+                                    primary
+                                    large
+                                    onClick={handleClose}
+                                    classNameSpan='wallets-migration-failed__text--mobile'
+                                >
+                                    {localize('Back to Trader’s Hub')}
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </MobileWrapper>
                 </React.Fragment>
             )}
         </React.Fragment>

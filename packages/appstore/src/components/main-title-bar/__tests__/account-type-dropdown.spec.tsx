9969182276b4a10b3866c7877c8b570dced2b91a
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-05-18 11:12:48 +0800

    fix: refactor

--- packages/appstore/src/components/main-title-bar/__tests__/account-type-dropdown.spec.tsx
+++ packages/appstore/src/components/main-title-bar/__tests__/account-type-dropdown.spec.tsx
@@ -4,36 +4,48 @@ import { render, screen } from '@testing-library/react';
 import { StoreProvider, mockStore } from '@deriv/stores';
 import userEvent from '@testing-library/user-event';
 
-let mock = mockStore({});
-
-const checkAccountTypeDropdownContainer = () => {
-    const wrapper = ({ children }: { children: JSX.Element }) => <StoreProvider store={mock}>{children}</StoreProvider>;
-
-    const { container } = render(<AccountTypeDropdown />, { wrapper });
-    expect(container).toBeInTheDocument();
-};
-
 describe('AccountTypeDropdown', () => {
-    beforeEach(() => {
-        mock = mockStore({});
-    });
     it('should render the component', () => {
-        checkAccountTypeDropdownContainer();
+        const mock = mockStore({});
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock}>{children}</StoreProvider>
+        );
+
+        const { container } = render(<AccountTypeDropdown />, { wrapper });
+        expect(container).toBeInTheDocument();
     });
 
     it('should show real account type', () => {
-        checkAccountTypeDropdownContainer();
+        const mock = mockStore({});
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock}>{children}</StoreProvider>
+        );
+
+        render(<AccountTypeDropdown />, { wrapper });
         expect(screen.getByText('Real')).toBeInTheDocument();
     });
 
     it('should show demo account type', () => {
-        mock.traders_hub.selected_account_type = 'demo';
-        checkAccountTypeDropdownContainer();
+        const mock = mockStore({
+            traders_hub: {
+                selected_account_type: 'demo',
+            },
+        });
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock}>{children}</StoreProvider>
+        );
+
+        render(<AccountTypeDropdown />, { wrapper });
         expect(screen.getByText('Demo')).toBeInTheDocument();
     });
 
     it('should change to demo account type', () => {
-        checkAccountTypeDropdownContainer();
+        const mock = mockStore({});
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock}>{children}</StoreProvider>
+        );
+
+        render(<AccountTypeDropdown />, { wrapper });
         userEvent.click(screen.getByText('Real'));
         userEvent.click(screen.getByText('Demo'));
         expect(mock.traders_hub.selectAccountType).toBeCalledTimes(1);

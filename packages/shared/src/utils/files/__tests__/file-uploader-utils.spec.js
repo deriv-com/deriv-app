271ae604679a706bf2f16cda1513926475c429f5
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-05-18 16:15:41 +0400

    Update packages/shared/src/utils/files/__tests__/file-uploader-utils.spec.js
    Co-authored-by: Niloofar Sadeghi <93518187+niloofar-deriv@users.noreply.github.com>

--- packages/shared/src/utils/files/__tests__/file-uploader-utils.spec.js
+++ packages/shared/src/utils/files/__tests__/file-uploader-utils.spec.js
@@ -5,7 +5,7 @@ describe('getFileExtension', () => {
     const expected_extension = 'png';
 
     it('returns the file extension', () => {
-        expect(getFileExtension(file)).toEqual(expectedExtension);
+        expect(getFileExtension(file)).toEqual(expected_extension);
     });
     it('returns null if the file type does not contain a slash', () => {
         expect(getFileExtension({ type: 'imagepng' })).toBeNull();

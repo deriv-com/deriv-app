aaa94776380c624e5786e09f21ced1a46fe9b1b0
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-05-18 12:10:18 +0800

    fix: comments

--- packages/appstore/src/components/get-more-accounts/__tests__/get-more-accounts.spec.tsx
+++ packages/appstore/src/components/get-more-accounts/__tests__/get-more-accounts.spec.tsx
@@ -4,20 +4,20 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('GetMoreAccounts', () => {
-    const props = {
+    const mock_props = {
         description: 'description',
         icon: 'icon',
         title: 'title',
         onClick: jest.fn(),
     };
     it('should render the component', () => {
-        const { container } = render(<GetMoreAccounts {...props} />);
+        const { container } = render(<GetMoreAccounts {...mock_props} />);
         expect(container).toBeInTheDocument();
     });
 
     it('should call the onClick prop when clicked', () => {
-        render(<GetMoreAccounts {...props} />);
+        render(<GetMoreAccounts {...mock_props} />);
         userEvent.click(screen.getByText('title'));
-        expect(props.onClick).toHaveBeenCalled();
+        expect(mock_props.onClick).toHaveBeenCalled();
     });
 });

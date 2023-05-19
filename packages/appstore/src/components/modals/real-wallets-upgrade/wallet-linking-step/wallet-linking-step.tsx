81865594d0bf987c75ed49754b6ef4d038ad28b8
Aum Bhatt <aum@deriv.com>
2023-05-19 19:00:30 +0400

    chore: minor UI fixes

--- packages/appstore/src/components/modals/real-wallets-upgrade/wallet-linking-step/wallet-linking-step.tsx
+++ packages/appstore/src/components/modals/real-wallets-upgrade/wallet-linking-step/wallet-linking-step.tsx
@@ -3,25 +3,34 @@ import WalletLinkWrapper from '../components/wallet-link/wallet-link-wrapper';
 import './wallet-linking-step.scss';
 import { Text, ThemedScrollbars } from '@deriv/components';
 
-const WalletLinkingStep = () => {
+type TWalletLinkingStep = {
+    title: string;
+};
+
+const WalletLinkingStep = ({ title }: TWalletLinkingStep) => {
     return (
         <div className='wallet-linking-step'>
-            <Text as='div' className='wallet-linking-step__title' size='m' weight='bold'>
-                Non-EU USD accounts
+            <Text as='div' className='wallet-linking-step__page-title' weight='bold'>
+                {title}
             </Text>
             <Text as='div' className='wallet-linking-step__description' size='s'>
                 This is how we link your accounts with your new Wallet.
             </Text>
-            <Text as='div' className='wallet-linking-step__description' size='xxs'>
+            <Text as='span' className='wallet-linking-step__note wallet-linking-step__title-text' size='xxs'>
                 Your existing funds will remain in your trading account(s) and can be transferred to your Wallet after
                 the upgrade.
             </Text>
-            <div className='wallet-linking-step__title-wrapper'>
-                <Text className='wallet-linking-step__title-text'>Your current trading account(s)</Text>
-                <Text className='wallet-linking-step__title-text'>Your new Wallet(s)</Text>
+            <div className='wallet-linking-step__title-small'>
+                <Text className='wallet-linking-step__title-text wallet-linking-step__title-small-text' size='xxxs'>
+                    Your current trading account(s)
+                </Text>
+                <Text className='wallet-linking-step__title-text wallet-linking-step__title-small-text' size='xxxs'>
+                    Your new Wallet(s)
+                </Text>
             </div>
             <ThemedScrollbars className='wallet-linking-step__content'>
                 <WalletLinkWrapper />
+                {/* <WalletLinkWrapper /> */}
             </ThemedScrollbars>
         </div>
     );

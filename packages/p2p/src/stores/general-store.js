93f98a08773f931d9a1fd330a6994257bafe69be
niloofar sadeghi <niloofar.sadeghi@re-work.dev>
2023-05-20 16:59:44 +0330

    fix: 404 error

--- packages/p2p/src/stores/general-store.js
+++ packages/p2p/src/stores/general-store.js
@@ -769,7 +769,7 @@ export default class GeneralStore extends BaseStore {
             const { list } = p2p_order_list;
             // it's an array of orders from p2p_order_list
             this.handleNotifications(order_store.orders, list);
-            list.forEach(order => order_store.syncOrder(order));
+            list?.forEach(order => order_store.syncOrder(order));
         } else if (p2p_order_info) {
             // it's a single order from p2p_order_info
             const idx_order_to_update = order_store.orders.findIndex(order => order.id === p2p_order_info.id);

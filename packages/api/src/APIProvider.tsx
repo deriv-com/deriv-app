a7ef047c373247c69366e7d797937870c3dcbf3c
Shayan Khaleghparast <iman@regentmarkets.com>
2023-05-22 16:01:48 +0330

    fix: excluded react-query-devtools from jest transformation

--- packages/api/src/APIProvider.tsx
+++ packages/api/src/APIProvider.tsx
@@ -1,13 +1,13 @@
 import React, { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 const APIProvider = ({ children }: PropsWithChildren<unknown>) => (
     <QueryClientProvider client={queryClient}>
         {children}
-        {/* <ReactQueryDevtools /> */}
+        <ReactQueryDevtools />
     </QueryClientProvider>
 );
 

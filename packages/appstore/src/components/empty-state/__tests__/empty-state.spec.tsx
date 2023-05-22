4e06ee218d89e6d01efbf2230c5eb063884b792e
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-05-22 15:21:53 +0800

    chore: add test coverage for empty state

---
+++ packages/appstore/src/components/empty-state/__tests__/empty-state.spec.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import EmptyState from '../empty-state';
+import { render, screen } from '@testing-library/react';
+import { StoreProvider, mockStore } from '@deriv/stores';
+
+describe('EmptyState', () => {
+    it('should render correctly', () => {
+        const mock = mockStore({});
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock}>{children}</StoreProvider>
+        );
+
+        const { container } = render(
+            <EmptyState
+                icon_name='icon_name'
+                renderMessage={() => (
+                    <>
+                        <p>Message</p>
+                        <p>Some content</p>
+                    </>
+                )}
+                renderTitle={() => <p>Title</p>}
+            />,
+            {
+                wrapper,
+            }
+        );
+        expect(container).toBeInTheDocument();
+    });
+
+    it('should render correctly with the correct balance and text', () => {
+        const mock = mockStore({});
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock}>{children}</StoreProvider>
+        );
+
+        const { container } = render(
+            <EmptyState
+                icon_name='icon_name'
+                renderMessage={() => (
+                    <>
+                        <p>Message</p>
+                        <p>Some content</p>
+                    </>
+                )}
+                renderTitle={() => <p>Title</p>}
+            />,
+            {
+                wrapper,
+            }
+        );
+
+        expect(container).toBeInTheDocument();
+        expect(screen.getByText('Message')).toBeInTheDocument();
+        expect(screen.getByText('Some content')).toBeInTheDocument();
+        expect(screen.getByText('Title')).toBeInTheDocument();
+    });
+});

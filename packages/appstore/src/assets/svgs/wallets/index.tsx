7ea8a110b1e7b9e62593751441d762575f5a09ff
Aizad Ridzo <103104395+aizad-deriv@users.noreply.github.com>
2023-05-23 18:59:40 +0800

    chore: stitch banner with popup (#8476)
    * chore: create component for consent wallet popup
    
    * fix: added info text based on clients region and risk status
    
    * fix: added test case
    
    * fix: make changes based on comments and reviews
    
    * fix: added mobile stylings
    
    * fix: small tweaks
    
    * fix: change svg added small changes to the stylings
    
    * fix: commit suggestions
    
    * fix: commit resolved messages
    
    * fix: commit changes based comments and did code improvements
    
    * fix: made changes to information list file
    
    * fix: added title border and checkbox function fix
    
    * fix: added bold text for low risk cr accounts in modal
    
    * fix: replace text for p2p
    
    * fix: modal not displaying properly on small screens
    
    * fix: issue with small screen final
    
    * chore: stitch banner with popup
    
    * fix: refactor some code
    
    * fix: refactor code
    
    * fix: test cases
    
    * chore: stitching 80% done
    
    * fix: fixing all the test cases
    
    * fix: commit suggestions
    
    * fix: fix test cases and other stuff
    
    * fix: remove unused stuff
    
    * fix: edit stylings making mobile view look better
    
    * fix: added form line height to wallet intro component
    
    * fix: refactor somemore
    
    * fix: commit suggestions
    
    * empty commit
    
    * empty commit
    
    * retrigger commit
    
    * chore: retrigger commit
    
    * fix: commit suggestions
    
    * fix: circleci error
    
    * fix: wallet banner test case
    
    * fix: tests
    
    * fix: issue with the disabled buttons and checkbox
    
    * fix: updated real-wallets-upgrade
    
    * fix: circleci error
    
    * fix: remove button outline from banner
    
    * fix: merge conflicts
    
    * fix: resolve circle ci issues
    
    * fix: added changes to show the banner
    
    * fix: circle ci issues
    
    * fix: remove additional button props
    
    ---------
    
    Co-authored-by: Thisyahlen Nair <thisyahlen@regentmarkets.com>

--- packages/appstore/src/assets/svgs/wallets/index.tsx
+++ packages/appstore/src/assets/svgs/wallets/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReadyToUpdateWalletsImage from './ready_to_update_wallets_image.svg';
+import ReadyToUpgradeWalletsImage from './ready_to_upgrade_wallets_image.svg';
 import UpgradeDesktopImage from 'Assets/svgs/wallets/wallets-upgrade-desktop.svg';
 import UpgradeMobileImage from 'Assets/svgs/wallets/wallets-upgrade-mobile.svg';
 import ReadyDesktopImage from 'Assets/svgs/wallets/wallets-ready-desktop.svg';
 import ReadyDesktopEuImage from 'Assets/svgs/wallets/wallets-ready-desktop-eu.svg';
 import ReadyMobileImage from 'Assets/svgs/wallets/wallets-ready-mobile.svg';
 import ReadyMobileEuImage from 'Assets/svgs/wallets/wallets-ready-mobile-eu.svg';
+import IntroducingWallets from 'Assets/svgs/wallets/introducing-wallets.svg';
+import IntroducingWalletsEU from 'Assets/svgs/wallets/introducing-wallets-eu.svg';
+import HowItWorks from 'Assets/svgs/wallets/how-it-works.svg';
+import TradingAccounts from 'Assets/svgs/wallets/trading-accounts.svg';
+import TradingAccountsEU from 'Assets/svgs/wallets/trading-accounts-eu.svg';
 import { TImageTestID, TWalletsImagesListKeys, WalletsImageProps } from './image-types';
 
 export const WalletsImagesList = {
@@ -19,7 +24,12 @@ export const WalletsImagesList = {
     ready_desktop_eu: ReadyDesktopEuImage,
     ready_mobile: ReadyMobileImage,
     ready_mobile_eu: ReadyMobileEuImage,
-    ready_to_update_wallets_image: ReadyToUpdateWalletsImage,
+    introducing_wallets: IntroducingWallets,
+    introducing_wallets_eu: IntroducingWalletsEU,
+    how_it_works: HowItWorks,
+    trading_accounts: TradingAccounts,
+    trading_accounts_eu: TradingAccountsEU,
+    ready_to_upgrade_wallets_image: ReadyToUpgradeWalletsImage,
 } as const;
 
 const WalletsImage = ({ image, className, width }: WalletsImageProps<TWalletsImagesListKeys>) => {

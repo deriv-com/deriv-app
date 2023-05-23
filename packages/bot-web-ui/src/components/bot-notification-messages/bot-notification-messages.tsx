83eee42e4759ab070ba8e351c99bbdd734649da9
vinu-deriv <vinu@deriv.com>
2023-05-23 16:28:29 +0400

    fix: eslint fix

--- packages/bot-web-ui/src/components/bot-notification-messages/bot-notification-messages.tsx
+++ packages/bot-web-ui/src/components/bot-notification-messages/bot-notification-messages.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
-import RootStore from 'Stores/index';
 import { connect } from 'Stores/connect';
+import RootStore from 'Stores/index';
 
 interface TBotNotificationMessagesProps {
     is_drawer_open: boolean;
@@ -14,6 +14,8 @@ const BotNotificationMessages = ({ is_drawer_open, Notifications }: TBotNotifica
             'notifications-container--is-panel-open': is_drawer_open,
         })}
     >
+        {' '}
+        <></>
         <Notifications />
     </div>
 );

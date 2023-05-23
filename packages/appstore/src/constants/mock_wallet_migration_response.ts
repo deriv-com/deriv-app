260475e59cfeb80045594ccf255b352e288a8c1b
Aum Bhatt <aum@deriv.com>
2023-05-23 16:27:13 +0400

    chore: added some mock data

--- packages/appstore/src/constants/mock_wallet_migration_response.ts
+++ packages/appstore/src/constants/mock_wallet_migration_response.ts
@@ -8,7 +8,13 @@ const mock_wallet_migration_response = [
                 landing_company_name: 'svg',
                 account_list: [
                     {
-                        balance: '7.00',
+                        balance: '1000.00',
+                        currency: 'USD',
+                        account_name: 'US Dollar',
+                        icon: 'IcCurrencyUsd',
+                    },
+                    {
+                        balance: '100.00',
                         currency: 'USD',
                         account_name: 'MT5 Derived SVG',
                         icon: 'IcRebrandingMt5Logo',
@@ -17,6 +23,42 @@ const mock_wallet_migration_response = [
                         balance: '123.00',
                         currency: 'USD',
                         account_name: 'MT5 Derived BVI',
+                        icon: 'IcRebrandingMt5Logo',
+                    },
+                    {
+                        balance: '100.00',
+                        currency: 'USD',
+                        account_name: 'MT5 Financial SVG',
+                        icon: 'IcRebrandingMt5Logo',
+                    },
+                    {
+                        balance: '20.00',
+                        currency: 'USD',
+                        account_name: 'MT5 Derived Vanuata',
+                        icon: 'IcRebrandingMt5Logo',
+                    },
+                    {
+                        balance: '150.00',
+                        currency: 'USD',
+                        account_name: 'MT5 Swap-free',
+                        icon: 'IcRebrandingMt5Logo',
+                    },
+                    {
+                        balance: '100.00',
+                        currency: 'USD',
+                        account_name: 'Deriv X',
+                        icon: 'IcRebrandingMt5Logo',
+                    },
+                    {
+                        balance: '100.00',
+                        currency: 'USD',
+                        account_name: 'Deriv EZ',
+                        icon: 'IcRebrandingMt5Logo',
+                    },
+                    {
+                        balance: '100.00',
+                        currency: 'USD',
+                        account_name: 'Deriv cTrader',
                         icon: 'IcRebrandingMt5Logo',
                     },
                 ],
@@ -32,15 +74,15 @@ const mock_wallet_migration_response = [
                 landing_company_name: 'svg',
                 account_list: [
                     {
-                        balance: '123.00',
+                        balance: '1000.00',
                         currency: 'USD',
-                        account_name: 'MT5 Financial SVG',
-                        icon: 'IcRebrandingMt5Logo',
+                        account_name: 'US Dollar',
+                        icon: 'IcCurrencyUsd',
                     },
                     {
                         balance: '234.00',
                         currency: 'USD',
-                        account_name: 'MT5 Derived Vanuata',
+                        account_name: 'MT5 CFDs',
                         icon: 'IcRebrandingMt5Logo',
                     },
                 ],
@@ -54,7 +96,7 @@ const mock_wallet_migration_response = [
                 landing_company_name: 'svg',
                 account_list: [
                     {
-                        balance: '567.00',
+                        balance: '0.00212012',
                         currency: 'BTC',
                         account_name: 'Bitcoin',
                         icon: 'IcCurrencyBtc',
@@ -65,7 +107,7 @@ const mock_wallet_migration_response = [
                 landing_company_name: 'svg',
                 account_list: [
                     {
-                        balance: '89.00',
+                        balance: '0.00212012',
                         currency: 'ETH',
                         account_name: 'Ethereum',
                         icon: 'IcCurrencyEth',

f9b5a2036910a8533e7dcf352c86f5a0fedc265d
adrienne-rio <adrienne@deriv.com>
2023-05-24 16:55:46 +0800

    chore: added checks for loginid

--- packages/stores/src/providers/WebsiteStatusProvider.tsx
+++ packages/stores/src/providers/WebsiteStatusProvider.tsx
@@ -2,27 +2,37 @@ import React from 'react';
 import { useSubscription } from '@deriv/api';
 import useStore from '../useStore';
 import merge from 'lodash.merge';
+import { observer } from 'mobx-react-lite';
+import { useWS } from '@deriv/shared';
 
 const WebsiteStatusProvider = ({ children }: React.PropsWithChildren<unknown>) => {
-    const { data, subscribe, unsubscribe } = useSubscription('website_status');
+    const { data, is_subscribed, subscribe, unsubscribe } = useSubscription('website_status');
     const {
-        client: { loginid },
+        client: { is_logged_in, is_logging_in, loginid },
         website_status: { update },
     } = useStore();
+    const WS = useWS();
 
-    React.useEffect(subscribe, [subscribe]);
+    // React.useEffect(subscribe, [subscribe])
 
     React.useEffect(() => {
-        if (data) update(prev => merge(prev, data));
+        if (data) update(data);
     }, [update, data]);
 
     // handles case where the response from website_status is different when client is logged in/out
+    // refer to socket-general.js
     React.useEffect(() => {
-        unsubscribe();
-        subscribe();
+        if (is_logged_in || is_logging_in) {
+            if (is_subscribed) unsubscribe();
+            WS.get()
+                .expectResponse('website_status')
+                .then(() => subscribe());
+        } else {
+            subscribe();
+        }
     }, [loginid]);
 
     return <React.Fragment>{children}</React.Fragment>;
 };
 
-export default WebsiteStatusProvider;
+export default observer(WebsiteStatusProvider);

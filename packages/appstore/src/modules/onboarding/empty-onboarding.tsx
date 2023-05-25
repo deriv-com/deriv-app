6d578f6eeca0a1d651abc8be5e09bab046ba97a8
Sergei Baranovski <sergei@deriv.com>
2023-05-25 14:21:42 +0300

    feat: change logo when user tries to signup

--- packages/appstore/src/modules/onboarding/empty-onboarding.tsx
+++ packages/appstore/src/modules/onboarding/empty-onboarding.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import TradigPlatformIconProps from 'Assets/svgs/trading-platform';
+import { observer, useStore } from '@deriv/stores';
+import classNames from 'classnames';
 
-const EmptyOnboarding = () => {
+const EmptyOnboarding = observer(() => {
+    const {
+        ui: { is_mobile },
+    } = useStore();
+
     return (
         <div className='empty-onboarding__wrapper'>
-            <div className='empty-onboarding__header'>
-                <TradigPlatformIconProps icon='DerivTradingLogo' />
+            <div
+                className={classNames('empty-onboarding__header', {
+                    'empty-onboarding__header--mobile': is_mobile,
+                })}
+            >
+                <TradigPlatformIconProps icon='DerivLogo' />
             </div>
         </div>
     );
-};
+});
 
 export default EmptyOnboarding;

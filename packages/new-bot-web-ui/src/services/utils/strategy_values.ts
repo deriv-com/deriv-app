46bf59d24e8a616c97baf7d3f312fca0ab659a39
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-05-25 17:11:57 +0300

    feat: update: settings .json file

--- packages/new-bot-web-ui/src/services/utils/strategy_values.ts
+++ packages/new-bot-web-ui/src/services/utils/strategy_values.ts
@@ -4,6 +4,10 @@ export const getStrategyValues = (essential_settings: { token: string | number; 
     const { settings } = config_data;
     const new_settings = {
         symbol: values['bot-builder__symbol'],
+        market: values['bot-builder__market'],
+        submarket: values['bot-builder__submarket'],
+        trade_type_category: values['bot-builder__trade_type_category'],
+        trade_type: values['bot-builder__trade_type'],
     };
     const updated_settings = { ...settings, ...new_settings, ...essential_settings };
     return { ...config_data, settings: updated_settings };

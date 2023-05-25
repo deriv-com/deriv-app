1b8a913375507f8d3071903998c8b2788eab0d69
Hamid Yaftian <hamid@deriv.com>
2023-05-25 20:25:11 +0800

    fix: update component name

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -116,5 +116,5 @@ export * from './components/watermark';
 export { default as Wizard } from './components/wizard';
 export * from './components/wallet-card';
 export * from './components/wallet-icon';
-export * from './components/app-icon';
+export * from './components/app-linked-with-wallet-icon';
 export * from './hooks';

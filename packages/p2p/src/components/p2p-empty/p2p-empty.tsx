11c1bb7e3fb3f7c9655b7bddae5e85bff155ae80
ameerul hady <ameerul@deriv.com>
2023-05-25 11:47:34 +0800

    chore: added default value for has_tabs

--- packages/p2p/src/components/p2p-empty/p2p-empty.tsx
+++ packages/p2p/src/components/p2p-empty/p2p-empty.tsx
@@ -5,12 +5,12 @@ import { Icon, Text } from '@deriv/components';
 type TP2pEmptyProps = {
     className?: string;
     children: React.ReactNode;
-    has_tabs: boolean;
+    has_tabs?: boolean;
     icon: string;
     title: string;
 };
 
-const P2pEmpty = ({ className, children, has_tabs, icon, title }: TP2pEmptyProps) => {
+const P2pEmpty = ({ className, children, has_tabs = false, icon, title }: TP2pEmptyProps) => {
     return (
         <div className={classNames(className, 'p2p-empty', { 'p2p-empty--no-tabs': !has_tabs })}>
             <Icon icon={icon} className='p2p-empty-icon' size={128} />

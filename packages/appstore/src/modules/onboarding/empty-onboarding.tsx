489dec51261d3e15700e28a6e85df69732c00dd6
Sergei Baranovski <sergei@deriv.com>
2023-05-25 23:40:15 +0300

    refactor: delete --mobile class, useStore and observer

--- packages/appstore/src/modules/onboarding/empty-onboarding.tsx
+++ packages/appstore/src/modules/onboarding/empty-onboarding.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import TradigPlatformIconProps from 'Assets/svgs/trading-platform';
-import { observer, useStore } from '@deriv/stores';
-import classNames from 'classnames';
 
-const EmptyOnboarding = observer(() => {
-    const {
-        ui: { is_mobile },
-    } = useStore();
-
+const EmptyOnboarding = () => {
     return (
         <div className='empty-onboarding__wrapper'>
-            <div
-                className={classNames('empty-onboarding__header', {
-                    'empty-onboarding__header--mobile': is_mobile,
-                })}
-            >
+            <div className='empty-onboarding__header'>
                 <TradigPlatformIconProps icon='DerivLogo' />
             </div>
         </div>
     );
-});
+};
 
 export default EmptyOnboarding;

4b2716c00ba0f1d2e4b86cfa90471162a99a0d4b
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-05-25 15:42:25 +0800

    fix: remove stores

--- packages/appstore/src/components/containers/__tests__/grid-container.spec.tsx
+++ packages/appstore/src/components/containers/__tests__/grid-container.spec.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import GridContainer from '../grid-container';
 import { render, screen } from '@testing-library/react';
-import { StoreProvider, mockStore } from '@deriv/stores';
 
 describe('GridContainer', () => {
     it('should render children', () => {
-        const store = mockStore({});
         const traders_hub_label = 'Traders Hub';
         render(
-            <StoreProvider store={store}>
-                <GridContainer>
-                    <div>
-                        <h1>{traders_hub_label}</h1>
-                    </div>
-                </GridContainer>
-            </StoreProvider>
+            <GridContainer>
+                <div>
+                    <h1>{traders_hub_label}</h1>
+                </div>
+            </GridContainer>
         );
         expect(screen.getByText(traders_hub_label)).toBeInTheDocument();
     });

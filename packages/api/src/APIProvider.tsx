38c32710942cbe684aa0376d1d9932804cd7f2e8
amir ali <amir.ali@regentmarkets.com>
2023-05-25 20:20:47 +0500

    fix: run react query dev tool without breaking codecov

--- packages/api/src/APIProvider.tsx
+++ packages/api/src/APIProvider.tsx
@@ -1,14 +1,19 @@
 import React, { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+let ReactQueryDevtools = () => null;
+
+if (process.env.NODE_ENV === 'development') {
+    //eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+    ReactQueryDevtools = require('@tanstack/react-query-devtools').ReactQueryDevtools;
+}
 
 const queryClient = new QueryClient();
 
 const APIProvider = ({ children }: PropsWithChildren<unknown>) => (
     <QueryClientProvider client={queryClient}>
         {children}
-        {/*Temporary disabled due to the problem on codecov, You can uncomment to use in development environment, DO NOT COMMIT THIS UNCOMMENTED*/}
-        {/*<ReactQueryDevtools />*/}
+        <ReactQueryDevtools />
     </QueryClientProvider>
 );
 

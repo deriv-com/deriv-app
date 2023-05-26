64d991be1232331730d98a9ee5fbd2429badf85b
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-05-26 14:50:05 +0800

    refactor: update react import

--- packages/p2p/src/components/trade-badge/trade-badge.tsx
+++ packages/p2p/src/components/trade-badge/trade-badge.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { Text } from '@deriv/components';
 import { localize, Localize } from 'Components/i18next';

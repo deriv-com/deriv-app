322d6ccac3524c3eee4d96231fcb41ec3749aa2e
Shaheer <shaheer@deriv.com>
2023-05-26 15:36:04 +0400

    feat: :sparkles: includes swap free to new design

--- packages/cfd/src/Constants/jurisdiction-contents/jurisdiction-svg-contents.ts
+++ packages/cfd/src/Constants/jurisdiction-contents/jurisdiction-svg-contents.ts
@@ -83,18 +83,36 @@ export const getJurisdictionSvgContents = (): TJurisdictionCardItems => ({
     ],
     swapfree_contents: [
         {
-            key: 'title',
-            title: localize('Selecting this will onboard you through Deriv (SVG) LLC (company no. 273 LLC 2020)'),
-        },
-        {
             key: 'assets',
-            title: localize(
-                '40+ assets: forex, synthetics, stocks, stock indices, cryptocurrencies, and ETFs swap-free.'
-            ),
+            title: localize('Assets'),
+            description: localize('Synthetics, Forex, Stocks, Stock Indices, Cryptocurrencies, and ETFs'),
+            title_indicators: {
+                type: 'displayText',
+                display_text: localize('40+'),
+                display_text_skin_color: 'red-darker',
+            },
         },
         {
             key: 'leverage',
-            title: localize('Leverage up to 1:1000'),
+            title: localize('Leverage'),
+            title_indicators: {
+                type: 'displayText',
+                display_text: localize('1:1000'),
+                display_text_skin_color: 'yellow-light',
+            },
+        },
+        {
+            key: 'verifications',
+            title: localize('Verifications'),
+            title_indicators: { type: 'displayIcons' },
+            description: localize(
+                'You will need to submit proof of identity and address once you reach certain thresholds.'
+            ),
+        },
+        {
+            key: 'regulator',
+            title: localize('Regulator/EDR'),
+            description: localize('Deriv (SVG) LLC (company no. 273 LLC 2020)'),
         },
     ],
     synthetic_verification_docs: ['not_applicable'],

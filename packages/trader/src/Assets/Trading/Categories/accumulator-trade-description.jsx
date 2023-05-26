892c0a6527ac2152d7852c37e9fe1a728de38d9d
Henry Hein <henry@regentmarkets.com>
2023-05-26 12:28:32 +0800

    refactor: refactor for better maintainability

--- packages/trader/src/Assets/Trading/Categories/accumulator-trade-description.jsx
+++ packages/trader/src/Assets/Trading/Categories/accumulator-trade-description.jsx
@@ -9,25 +9,19 @@ const AccumulatorTradeDescription = ({ onClick }) => {
             <Text as='p'>
                 <Localize
                     i18n_default_text='Accumulators allow you to express a view on the range of movement of an index and grow your stake exponentially at a fixed <0>growth rate</0>.'
-                    components={[
-                        <span className='contract-type-info__content--definition' onClick={onClick} key={0} />,
-                    ]}
+                    components={[<span className='contract-type-info__content-definition' onClick={onClick} key={0} />]}
                 />
             </Text>
             <Text as='p'>
                 <Localize
                     i18n_default_text='Your stake will continue to grow as long as the current spot price remains within a specified <0>range</0> from the <0>previous spot price</0>. Otherwise, you lose your stake and the trade is terminated.'
-                    components={[
-                        <span className='contract-type-info__content--definition' onClick={onClick} key={0} />,
-                    ]}
+                    components={[<span className='contract-type-info__content-definition' onClick={onClick} key={0} />]}
                 />
             </Text>
             <Text as='p'>
                 <Localize
                     i18n_default_text='You can close your trade anytime. However, be aware of <0>slippage risk<0/>.'
-                    components={[
-                        <span className='contract-type-info__content--definition' onClick={onClick} key={0} />,
-                    ]}
+                    components={[<span className='contract-type-info__content-definition' onClick={onClick} key={0} />]}
                 />
             </Text>
         </React.Fragment>
--- packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, ThemedScrollbars, Carousel, ButtonToggle } from '@deriv/components';
 import { isMobile } from '@deriv/shared';
 import { localize } from '@deriv/translations';
@@ -10,12 +10,26 @@ import ContractTypeGlossary from './contract-type-glossary.jsx';
 import classNames from 'classnames';
 
 const Info = ({ handleNavigationClick, handleSelect, initial_index, item, list }) => {
-    const [carousel_index, setCarouselIndex] = React.useState(initial_index);
-    const [selected_tab, setSelectedTab] = React.useState('description');
-    const width = isMobile() ? '328' : '528';
+    const [carousel_index, setCarouselIndex] = useState(initial_index);
+    const [selected_tab, setSelectedTab] = useState('description');
     const contract_types = getContractTypes(list, item).filter(i => i.value !== 'rise_fall_equal');
     const onClickGlossary = () => setSelectedTab('glossary');
+
     const has_toggle_buttons = carousel_index !== 8 && carousel_index !== 1;
+    const is_description_tab_selected = selected_tab === 'description';
+    const is_glossary_tab_selected = selected_tab === 'glossary';
+
+    const width = isMobile() ? '328' : '528';
+    const scroll_bar_height = is_description_tab_selected ? '510px' : '440px';
+
+    const handleItemSelect = active_index => {
+        setCarouselIndex(active_index);
+        handleNavigationClick(contract_types[active_index]);
+        if (has_toggle_buttons && is_glossary_tab_selected) {
+            setSelectedTab('description');
+        }
+    };
+
     const cards = contract_types.map((type, idx) => {
         return (
             <div
@@ -24,43 +38,39 @@ const Info = ({ handleNavigationClick, handleSelect, initial_index, item, list }
                     'contract-type-info__card--has-toggle-buttons': !has_toggle_buttons,
                 })}
             >
-                {selected_tab === 'description' ? (
-                    <ThemedScrollbars
-                        className={classNames('contract-type-info__scrollbars', {
-                            'contract-type-info__scrollbars-description--active': selected_tab === 'description',
+                <ThemedScrollbars
+                    className={classNames('contract-type-info__scrollbars', {
+                        'contract-type-info__scrollbars-description--active': is_description_tab_selected,
+                        'contract-type-info__scrollbars-glossary contract-type-info__scrollbars-glossary--active ':
+                            is_glossary_tab_selected,
+                    })}
+                    style={{
+                        left: `${is_description_tab_selected ? `-${width}` : width}px`,
+                        transform: `translate3d(${is_description_tab_selected ? width : `-${width}`}px, 0, 0)`,
+                    }}
+                    height={isMobile() ? '' : scroll_bar_height}
+                    autohide={false}
+                >
+                    <div
+                        className={classNames('', {
+                            'contract-type-info__gif--has-toggle-buttons': has_toggle_buttons,
+                            'contract-type-info__content': is_glossary_tab_selected,
+                            'contract-type-info__gif': is_description_tab_selected,
                         })}
-                        style={{ left: `-${width}px`, transform: `translate3d(${width}px, 0, 0)` }}
-                        height={isMobile() ? '' : '510px'}
-                        autohide={false}
                     >
-                        <div
-                            className={classNames('contract-type-info__gif', {
-                                'contract-type-info__gif--has-toggle-buttons': has_toggle_buttons,
-                            })}
-                        >
+                        {is_description_tab_selected ? (
                             <TradeCategoriesGIF category={type.value} />
-                        </div>
-                        <div className='contract-type-info__content'>
-                            <TradeCategories category={type.value} onClick={onClickGlossary} />
-                        </div>
-                    </ThemedScrollbars>
-                ) : (
-                    <ThemedScrollbars
-                        className={classNames(
-                            'contract-type-info__scrollbars contract-type-info__scrollbars-glossary',
-                            {
-                                'contract-type-info__scrollbars-glossary--active': selected_tab === 'glossary',
-                            }
-                        )}
-                        style={{ left: `${width}px`, transform: `translate3d(-${width}px, 0, 0)` }}
-                        height={isMobile() ? '' : '440px'}
-                        autohide={false}
-                    >
-                        <div className='contract-type-info__content contract-type-info__content-glossary'>
+                        ) : (
                             <ContractTypeGlossary category={type.value} />
-                        </div>
-                    </ThemedScrollbars>
-                )}
+                        )}
+                    </div>
+                    <div className='contract-type-info__content'>
+                        {is_description_tab_selected ? (
+                            <TradeCategories category={type.value} onClick={onClickGlossary} />
+                        ) : null}
+                    </div>
+                </ThemedScrollbars>
+
                 <Button
                     id={`dt_contract_info_${type.value}_btn`}
                     className='contract-type-info__button'
@@ -99,13 +109,7 @@ const Info = ({ handleNavigationClick, handleSelect, initial_index, item, list }
                 bullet_color='var(--text-disabled)'
                 active_bullet_color='var(--brand-red-coral)'
                 initial_index={carousel_index}
-                onItemSelect={active_index => {
-                    setCarouselIndex(active_index);
-                    handleNavigationClick(contract_types[active_index]);
-                    if (has_toggle_buttons && selected_tab === 'glossary') {
-                        setSelectedTab('description');
-                    }
-                }}
+                onItemSelect={handleItemSelect}
                 list={cards}
                 width={isMobile() ? 328 : 528}
             />

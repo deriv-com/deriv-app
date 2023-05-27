054910d3ab673a132fc9e84de465ae0c7614dc86
Farzin Mirzaie <farzin@deriv.com>
2023-05-27 12:14:32 +0800

    feat(hooks): :sparkles: add `useInputATMFormatter` hook

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -14,6 +14,7 @@ export { default as useHasSetCurrency } from './useHasSetCurrency';
 export { default as useHasSvgAccount } from './useHasSvgAccount';
 export { default as useHasSwapFreeAccount } from './useHasSwapFreeAccount';
 export { default as useHasUSDCurrency } from './useHasUSDCurrency';
+export { default as useInputATMFormatter } from './useInputATMFormatter';
 export { default as useInputDecimalFormatter } from './useInputDecimalFormatter';
 export { default as useIsP2PEnabled } from './useIsP2PEnabled';
 export { default as useIsRealAccountNeededForCashier } from './useIsRealAccountNeededForCashier';
---
+++ packages/hooks/src/useInputATMFormatter.ts
@@ -0,0 +1,106 @@
+import { useCallback, useEffect, useMemo, useRef } from 'react';
+import useInputDecimalFormatter from './useInputDecimalFormatter';
+
+const unFormatLocaleStringifyNumber = (input: string, locale: Intl.LocalesArgument) => {
+    const parts = (12345.6789).toLocaleString(locale).match(/(\D+)/g);
+
+    if (parts && parts.length > 1) {
+        const is_reverse = parts[parts.length - 1] !== '.';
+        const decimal = parts[parts.length - 1];
+        const thousand = parts[0];
+
+        if (is_reverse) return input.replaceAll(decimal, 'x').replaceAll(thousand, '').replaceAll('x', '.');
+
+        return input.replaceAll(thousand, '');
+    }
+
+    return input;
+};
+
+type TOptions = {
+    fraction_digits?: number;
+    locale?: Intl.LocalesArgument;
+};
+
+const useInputATMFormatter = (initial?: number, options?: TOptions) => {
+    const is_pasting = useRef(false);
+    const { value, onChange: onChangeDecimal } = useInputDecimalFormatter(undefined, options);
+    const { locale, fraction_digits = 2 } = options || {};
+
+    const formatted_value = useMemo(
+        () => `${Number(value).toLocaleString(locale, { minimumFractionDigits: fraction_digits })}`,
+        [fraction_digits, locale, value]
+    );
+
+    const onChange = useCallback(
+        (e: DeepPartial<React.ChangeEvent<HTMLInputElement>> | React.ChangeEvent<HTMLInputElement>) => {
+            const new_value = e?.target?.value || '';
+            const unformatted = unFormatLocaleStringifyNumber(new_value, locale);
+            // @ts-expect-error shouldn't cast to number because we will lose the trailing zeros.
+            const shifted = Math.fround(unformatted * 10).toFixed(fraction_digits);
+            // @ts-expect-error shouldn't cast to number because we will lose the trailing zeros.
+            const un_shifted = Math.fround(unformatted / 10).toFixed(fraction_digits);
+            const unformatted_fraction = unformatted.split('.')?.[1]?.length || fraction_digits;
+
+            // If the user is pasting, we don't need to shift the decimal point,
+            // We just need to format the value.
+            if (is_pasting.current) {
+                is_pasting.current = false;
+                return onChangeDecimal({ target: { value: unformatted } });
+            }
+
+            // The new value has one more decimal places than the fraction_digits,
+            // so we need to shift the decimal point to the left.
+            if (unformatted_fraction - 1 === fraction_digits) {
+                return onChangeDecimal({ target: { value: shifted } });
+            }
+
+            // The new value has one less decimal places than the fraction_digits,
+            // so we need to shift the decimal point to the right.
+            if (unformatted_fraction + 1 === fraction_digits) {
+                return onChangeDecimal({ target: { value: un_shifted } });
+            }
+
+            // The new value has the same number of decimal places as the fraction_digits,
+            // so we don't need to shift the decimal point.
+            if (unformatted_fraction === fraction_digits) {
+                return onChangeDecimal({ target: { value: unformatted } });
+            }
+
+            // The new value has more decimal places than the fraction_digits,
+            // so we chop the extra decimal points.
+            if (unformatted_fraction - 1 > fraction_digits) {
+                return onChangeDecimal({ target: { value: unformatted } });
+            }
+
+            // The new value has less decimal places than the fraction_digits,
+            // so we add the missing extra decimal point.
+            if (unformatted_fraction + 1 < fraction_digits) {
+                return onChangeDecimal({ target: { value: unformatted } });
+            }
+
+            return onChangeDecimal({ target: { value: unformatted } });
+        },
+        [locale, fraction_digits, onChangeDecimal]
+    );
+
+    const onPaste: React.ClipboardEventHandler<HTMLInputElement> = useCallback(
+        e => (is_pasting.current = e.type === 'paste'),
+        []
+    );
+
+    useEffect(() => {
+        if (initial) {
+            is_pasting.current = true;
+            onChange({
+                target: {
+                    value: `${Number(initial).toLocaleString(locale, { minimumFractionDigits: fraction_digits })}`,
+                },
+            });
+        }
+    }, [fraction_digits, initial, locale, onChange]);
+
+    return { value: formatted_value, onChange, onPaste };
+};
+
+export default useInputATMFormatter;

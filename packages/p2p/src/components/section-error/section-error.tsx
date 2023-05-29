fd83e98bfb0737522dc0fe35f396588d9b0f809a
nada-deriv <nada.ajinas@deriv.com>
2023-05-29 09:58:58 +0400

    refactor: review comments

--- packages/p2p/src/components/section-error/section-error.tsx
+++ packages/p2p/src/components/section-error/section-error.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text } from '@deriv/components';
 
-type TSectionErrorProps = {
+type TSectionError = {
     className?: string;
     message: string;
     size?: string;
 };
 
-const SectionError = ({ className = 'section-error__form', message, size = 'xxs' }: TSectionErrorProps) => (
+const SectionError = ({ className = 'section-error__form', message, size = 'xxs' }: TSectionError) => (
     <Text as='p' color='loss-danger' size={size} className={className}>
         {message}
     </Text>

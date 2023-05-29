e6838da18a643f3106ebe2e90e8de1cd7b72992d
aizad-deriv <aizad@deriv.com>
2023-05-29 15:19:21 +0800

    fix: rename constant to snake case

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -1645,8 +1645,8 @@ export default class ClientStore extends BaseStore {
                 this.is_populating_account_list = false;
             });
             const language = authorize_response.authorize.preferred_language;
-            const storedLanguage = LocalStore.get(LANGUAGE_KEY);
-            if (language !== 'EN' && storedLanguage && language !== storedLanguage) {
+            const stored_language = LocalStore.get(LANGUAGE_KEY);
+            if (language !== 'EN' && stored_language && language !== stored_language) {
                 window.history.replaceState({}, document.title, urlForLanguage(language));
                 await this.root_store.common.changeSelectedLanguage(language);
             }

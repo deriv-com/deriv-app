06e2102dcaa279c582a75fa7bc9e15efe6a07f84
Sergei Baranovski <sergei@deriv.com>
2023-05-30 12:42:30 +0300

    feat: implement updated WalletIcon component

--- packages/appstore/src/components/wallet-header/wallet-currency-card.tsx
+++ packages/appstore/src/components/wallet-header/wallet-currency-card.tsx
@@ -4,7 +4,6 @@ import { observer, useStore } from '@deriv/stores';
 import type { TAccountCategory, TWalletCurrency } from 'Types';
 import { getWalletCurrencyIcon } from 'Constants/utils';
 import { isCryptocurrency } from '@deriv/shared';
-import { getWalletModalBackgrounds } from 'Constants/wallet-background';
 
 type TWalletCurrencyCard = {
     account_type: TAccountCategory;
@@ -22,19 +21,15 @@ const WalletCurrencyCard = observer(({ account_type, currency }: TWalletCurrency
     // add check (currency !== 'USDT') because response from BE doesn't have USDT currency, just UST
     const is_fiat = !isCryptocurrency(currency) && currency !== 'USDT';
     const currency_icon_name = getWalletCurrencyIcon(is_demo ? 'demo' : currency, is_dark_mode_on);
-    const currency_icon_size = is_fiat && !is_demo ? 48 : 100;
-
-    const bg = getWalletModalBackgrounds(converted_currency);
 
     return (
         <div className='wallet-header__currency' data-testid={`dt_${converted_currency}`}>
             <WalletIcon
+                currency={converted_currency}
                 icon={currency_icon_name}
-                iconSize={currency_icon_size}
-                color={bg.color}
-                primaryColor={bg.primary}
-                secondaryColor={bg.secondary}
-                size='large'
+                size={'xlarge'}
+                type={is_fiat && !is_demo ? 'fiat' : 'crypto'}
+                has_bg={true}
             />
         </div>
     );

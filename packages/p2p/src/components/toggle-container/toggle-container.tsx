2af57dc882570ef362482b8c847e3c7e8ede636d
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-05-30 12:42:54 +0800

    refactor: delete the type and use propswithchildren

--- packages/p2p/src/components/toggle-container/toggle-container.tsx
+++ packages/p2p/src/components/toggle-container/toggle-container.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-type TToggleContainer = {
-    children: React.ReactNode;
-};
-
-const ToggleContainer = ({ children }: TToggleContainer) => (
+const ToggleContainer = ({ children }: React.PropsWithChildren<unknown>) => (
     <div className='toggle-container' data-testid='dt_toggle_container'>
         {children}
     </div>

f4ba8d9a4ce97e01d8ba40c1723a6637381d6e6f
george-usynin-binary <heorhi@binary.com>
2023-05-30 16:06:09 +0300

    fix: :art: apply comments

--- packages/components/src/components/transfer-account-selector/transfer-tile.tsx
+++ packages/components/src/components/transfer-account-selector/transfer-tile.tsx
@@ -31,18 +31,17 @@ const TransferTile = ({ is_mobile, label, placeholder, selected_account }: TTran
                     {is_mobile && <ChevronIcon />}
                 </div>
 
-                {!selected_account && (
-                    <Text size={is_mobile ? 'xxs' : 'xs'} weight='bold'>
-                        {placeholder}
-                    </Text>
-                )}
-                {selected_account && (
+                {selected_account ? (
                     <WalletTile
                         account={selected_account}
                         className='transfer-account-selector__value'
                         is_mobile={is_mobile}
                         is_value
                     />
+                ) : (
+                    <Text size={is_mobile ? 'xxs' : 'xs'} weight='bold'>
+                        {placeholder}
+                    </Text>
                 )}
             </div>
 

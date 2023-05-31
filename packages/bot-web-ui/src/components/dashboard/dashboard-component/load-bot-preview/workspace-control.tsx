c627d6ff537a45b342e29a7496d3473c98e6d8a1
rupato-deriv <rupato@deriv.com>
2023-05-31 10:38:25 +0800

    fix: resolved merge conflict

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/load-bot-preview/workspace-control.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/load-bot-preview/workspace-control.tsx
@@ -6,20 +6,22 @@ import { useDBotStore } from 'Stores/useDBotStore';
 const WorkspaceControl = observer(() => {
     const { dashboard } = useDBotStore();
     const { onZoomInOutClick } = dashboard;
-    <div className='load-strategy__preview-workspace-controls'>
-        <Icon
-            icon={'IcAddRounded'}
-            className='load-strategy__preview-workspace-icon'
-            custom_color='$color-black-1'
-            onClick={() => onZoomInOutClick(true)}
-        />
-        <Icon
-            icon={'IcMinusRounded'}
-            className='load-strategy__preview-workspace-icon'
-            custom_color='$color-black-1'
-            onClick={() => onZoomInOutClick(false)}
-        />
-    </div>;
+    return (
+        <div className='load-strategy__preview-workspace-controls'>
+            <Icon
+                icon={'IcAddRounded'}
+                className='load-strategy__preview-workspace-icon'
+                custom_color='$color-black-1'
+                onClick={() => onZoomInOutClick(true)}
+            />
+            <Icon
+                icon={'IcMinusRounded'}
+                className='load-strategy__preview-workspace-icon'
+                custom_color='$color-black-1'
+                onClick={() => onZoomInOutClick(false)}
+            />
+        </div>
+    );
 });
 
 export default WorkspaceControl;

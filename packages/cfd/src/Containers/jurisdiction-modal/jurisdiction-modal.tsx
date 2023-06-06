27c25ec77b842af744012209425d778be9c05212
Shaheer <shaheer@deriv.com>
2023-06-06 15:41:16 +0400

    fix: :bug: adds scrollable content support for low res screens

--- packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-modal.tsx
+++ packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-modal.tsx
@@ -37,7 +37,7 @@ const JurisdictionModal = ({
                         toggleModal={toggleJurisdictionModal}
                         type='button'
                         context={context}
-                        width={account_type.type === 'financial' ? '1300px' : '1160px'}
+                        width={account_type.type === 'financial' ? '1280px' : '1100px'}
                     >
                         <JurisdictionModalContentWrapper openPasswordModal={openPasswordModal} context={context} />
                     </Modal>

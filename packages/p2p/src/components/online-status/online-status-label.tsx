52194e99d9a92ab8bf7246c14a29503262578557
nada-deriv <nada.ajinas@deriv.com>
2023-06-06 10:20:55 +0400

    refactor: reduce code complexity to remove code smell

--- packages/p2p/src/components/online-status/online-status-label.tsx
+++ packages/p2p/src/components/online-status/online-status-label.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
+import { Duration } from 'moment';
 import { Text } from '@deriv/components';
-import { getDiffDuration, isMobile, toMoment, epochToMoment } from '@deriv/shared';
+import { epochToMoment, getDiffDuration, isMobile, toMoment } from '@deriv/shared';
 import { localize } from 'Components/i18next';
 
 type TOnlineStatusLabelProps = {
@@ -15,60 +16,69 @@ const OnlineStatusLabel = ({
     last_online_time,
     size = isMobile() ? 'xxxs' : 'xs',
 }: TOnlineStatusLabelProps) => {
+    const getDifference = (last_seen_online: number) => {
+        const start_time = epochToMoment(last_seen_online).unix();
+        const end_time = toMoment().unix();
+        const diff = getDiffDuration(start_time, end_time);
+        return diff;
+    };
+
+    const getStatusLabel = (diff: Duration) => {
+        if (diff.years()) return localize('Seen more than 6 months ago');
+
+        if (diff.months()) {
+            if (diff.months() > 6) {
+                return localize('Seen more than 6 months ago');
+            }
+            if (diff.months() === 1) {
+                return localize('Seen {{ duration }} month ago', {
+                    duration: diff.months(),
+                });
+            }
+            return localize('Seen {{ duration }} months ago', {
+                duration: diff.months(),
+            });
+        }
+        if (diff.days()) {
+            if (diff.days() === 1) {
+                return localize('Seen {{ duration }} day ago', {
+                    duration: diff.days(),
+                });
+            }
+            return localize('Seen {{ duration }} days ago', {
+                duration: diff.days(),
+            });
+        }
+        if (diff.hours()) {
+            if (diff.hours() === 1) {
+                return localize('Seen {{ duration }} hour ago', {
+                    duration: diff.hours(),
+                });
+            }
+            return localize('Seen {{ duration }} hours ago', {
+                duration: diff.hours(),
+            });
+        }
+        if (diff.minutes()) {
+            if (diff.minutes() === 1) {
+                return localize('Seen {{ duration }} minute ago', {
+                    duration: diff.minutes(),
+                });
+            }
+            return localize('Seen {{ duration }} minutes ago', {
+                duration: diff.minutes(),
+            });
+        }
+        return localize('Online');
+    };
+
     const last_online_label = () => {
         if (!is_online) {
             if (last_online_time) {
-                const start_time = epochToMoment(last_online_time).unix();
-                const end_time = toMoment().unix();
-                const diff = getDiffDuration(start_time, end_time);
-
-                if (diff.years()) return localize('Seen more than 6 months ago');
-                if (diff.months()) {
-                    if (diff.months() > 6) {
-                        return localize('Seen more than 6 months ago');
-                    }
-                    if (diff.months() === 1) {
-                        return localize('Seen {{ duration }} month ago', {
-                            duration: diff.months(),
-                        });
-                    }
-                    return localize('Seen {{ duration }} months ago', {
-                        duration: diff.months(),
-                    });
-                }
-                if (diff.days()) {
-                    if (diff.days() === 1) {
-                        return localize('Seen {{ duration }} day ago', {
-                            duration: diff.days(),
-                        });
-                    }
-                    return localize('Seen {{ duration }} days ago', {
-                        duration: diff.days(),
-                    });
-                }
-                if (diff.hours()) {
-                    if (diff.hours() === 1) {
-                        return localize('Seen {{ duration }} hour ago', {
-                            duration: diff.hours(),
-                        });
-                    }
-                    return localize('Seen {{ duration }} hours ago', {
-                        duration: diff.hours(),
-                    });
-                }
-                if (diff.minutes()) {
-                    if (diff.minutes() === 1) {
-                        return localize('Seen {{ duration }} minute ago', {
-                            duration: diff.minutes(),
-                        });
-                    }
-                    return localize('Seen {{ duration }} minutes ago', {
-                        duration: diff.minutes(),
-                    });
-                }
-            } else {
-                return localize('Seen more than 6 months ago');
+                const diff = getDifference(last_online_time);
+                return getStatusLabel(diff);
             }
+            return localize('Seen more than 6 months ago');
         }
         return localize('Online');
     };

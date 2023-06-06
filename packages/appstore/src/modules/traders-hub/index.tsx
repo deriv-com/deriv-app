68b645ae712883ebb34a78332d51073c0646715a
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-06-06 12:29:55 +0800

    fix: eu banner missing for MF only countries

--- packages/appstore/src/modules/traders-hub/index.tsx
+++ packages/appstore/src/modules/traders-hub/index.tsx
@@ -43,8 +43,6 @@ const TradersHub = () => {
 
     const eu_title = content_flag === ContentFlag.EU_DEMO || content_flag === ContentFlag.EU_REAL || is_eu_user;
 
-    const is_eu_low_risk = content_flag === ContentFlag.LOW_RISK_CR_EU;
-
     const platform_toggle_options = [
         { text: `${eu_title ? 'Multipliers' : 'Options & Multipliers'}`, value: 'options' },
         { text: 'CFDs', value: 'cfd' },
@@ -112,7 +110,7 @@ const TradersHub = () => {
                     {scrolled && <TourGuide />}
                 </div>
             </Div100vhContainer>
-            {is_eu_low_risk && <EUDisclamer />}
+            {is_eu_user && <EUDisclamer />}
         </>
     );
 };

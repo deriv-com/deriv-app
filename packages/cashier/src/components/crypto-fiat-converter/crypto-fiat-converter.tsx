ebe2d53bd0e56776010a96959a7235c423bc2cdc
Shayan Khaleghparast <iman@regentmarkets.com>
2023-06-06 11:30:01 +0330

    fix: checking test coverage issue

--- packages/cashier/src/components/crypto-fiat-converter/crypto-fiat-converter.tsx
+++ packages/cashier/src/components/crypto-fiat-converter/crypto-fiat-converter.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import { Field, FieldProps, useFormikContext } from 'formik';
-import { DesktopWrapper, Input, Icon, MobileWrapper } from '@deriv/components';
+import { DesktopWrapper, Input, Icon, MobileWrapper, Text, useInterval } from '@deriv/components';
 import { getCurrencyDisplayCode } from '@deriv/shared';
-import { localize } from '@deriv/translations';
+import { localize, Localize } from '@deriv/translations';
 import { observer } from '@deriv/stores';
 import { TReactChangeEvent } from '../../types';
 import { useCashierStore } from '../../stores/useCashierStores';
 import { useExchangeRate } from '@deriv/hooks';
 import './crypto-fiat-converter.scss';
-import Timer from './timer';
-import InputGroup from './input-group';
+
+type TTimerProps = {
+    onComplete: VoidFunction;
+};
+
+type TInputGroupProps = React.PropsWithChildren<{
+    className: string;
+}>;
+
+const Timer = ({ onComplete }: TTimerProps) => {
+    const initial_time = 60;
+    const [remaining_time, setRemainingTime] = React.useState<number>(initial_time);
+
+    useInterval(() => {
+        if (remaining_time > 0) {
+            setRemainingTime(remaining_time - 1);
+        }
+    }, 1000);
+
+    React.useEffect(() => {
+        if (remaining_time === 0) {
+            onComplete();
+            setRemainingTime(initial_time);
+        }
+    }, [onComplete, remaining_time]);
+
+    return (
+        <Text as='p' size='xs' color='less-prominent' className='timer'>
+            <Localize i18n_default_text='{{remaining_time}}s' values={{ remaining_time }} />
+        </Text>
+    );
+};
+
+const InputGroup = ({ children, className }: TInputGroupProps) => {
+    return (
+        <fieldset>
+            <div className={className}>{children}</div>
+        </fieldset>
+    );
+};
 
 type TCryptoFiatConverterProps = {
     from_currency: string;

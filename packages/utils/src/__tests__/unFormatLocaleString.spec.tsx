d743cc32bc394f0ab1948e959a77c37be4acf99d
Farzin Mirzaie <farzin@deriv.com>
2023-06-07 10:05:06 +0800

    fix(utils): :memo: resolve PR comments

--- packages/utils/src/__tests__/unFormatLocaleString.spec.tsx
+++ packages/utils/src/__tests__/unFormatLocaleString.spec.tsx
@@ -1,25 +1,25 @@
 import unFormatLocaleString from '../unFormatLocaleString';
 
 describe('unFormatLocaleString', () => {
-    test('should un format correctly when the number is less than 3 digits', () => {
+    test('should unformat correctly when the number is less than 3 digits', () => {
         const result = unFormatLocaleString('123', 'en-US');
 
         expect(result).toBe('123');
     });
 
-    test('should un format correctly when the number is more than 3 digits', () => {
+    test('should unformat correctly when the number is more than 3 digits', () => {
         const result = unFormatLocaleString('123,456', 'en-US');
 
         expect(result).toBe('123456');
     });
 
-    test('should un format correctly when the number is more than 3 digits and has decimals', () => {
+    test('should unformat correctly when the number is more than 3 digits and has decimals', () => {
         const result = unFormatLocaleString('123,456.78', 'en-US');
 
         expect(result).toBe('123456.78');
     });
 
-    test('should un format correctly when the number is more than 3 digits and has decimals and is in spanish', () => {
+    test('should unformat correctly when the number is more than 3 digits and has decimals and is in spanish', () => {
         const result = unFormatLocaleString('123.456,78', 'es-ES');
 
         expect(result).toBe('123456.78');

5746250f99871a145be64f7f944c736af9aa29da
Aum Bhatt <aum@deriv.com>
2023-06-08 13:29:42 +0400

    refactor: moved dark-mode logic from deposit-fiat-iframe to useDepositFiatAddress hook

--- packages/cashier/src/modules/deposit-fiat/components/deposit-fiat-iframe/deposit-fiat-iframe.tsx
+++ packages/cashier/src/modules/deposit-fiat/components/deposit-fiat-iframe/deposit-fiat-iframe.tsx
@@ -1,29 +1,26 @@
 import React, { useState } from 'react';
 import { Loading } from '@deriv/components';
 import { useDepositFiatAddress } from '@deriv/hooks';
-import { observer, useStore } from '@deriv/stores';
+import { observer } from '@deriv/stores';
 import './deposit-fiat-iframe.scss';
 
 const DepositFiatIframe: React.FC = observer(() => {
-    const { ui } = useStore();
-    const { is_dark_mode_on } = ui;
-    const { data: iframe_url, isSuccess, resend } = useDepositFiatAddress();
-    const [show_loader, setShowLoader] = useState(true);
+    const { data: iframe_url, isSuccess } = useDepositFiatAddress();
+    const [is_loading, setIsLoading] = useState(true);
 
     React.useEffect(() => {
-        resend();
-        setShowLoader(true);
-    }, [is_dark_mode_on]);
+        setIsLoading(true);
+    }, [iframe_url]);
 
     return (
         <React.Fragment>
-            {show_loader && <Loading className='deposit-fiat-iframe__loader' is_fullscreen={false} />}
+            {is_loading && <Loading className='deposit-fiat-iframe__loader' is_fullscreen={false} />}
             {isSuccess && (
                 <iframe
                     className='deposit-fiat-iframe__iframe'
-                    onLoad={() => setShowLoader(false)}
-                    src={`${iframe_url}&DarkMode=${is_dark_mode_on ? 'on' : 'off'}`}
-                    style={!show_loader ? { display: 'block' } : { display: 'none' }}
+                    onLoad={() => setIsLoading(false)}
+                    src={iframe_url}
+                    style={!is_loading ? { display: 'block' } : { display: 'none' }}
                     data-testid='dt_deposit_fiat_iframe_iframe'
                 />
             )}

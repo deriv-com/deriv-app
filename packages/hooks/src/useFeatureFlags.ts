80836d250a4a350d7131623cbdd31ec39d962a37
Farzin Mirzaie <farzin@deriv.com>
2023-06-11 19:28:33 +0800

    perf(hooks): :zap: use `reduce()` instead of `map().reduce()`

--- packages/hooks/src/useFeatureFlags.ts
+++ packages/hooks/src/useFeatureFlags.ts
@@ -6,11 +6,15 @@ const useFeatureFlags = () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const flags = feature_flags.data!;
 
-    const result = Object.keys(flags)
-        // @ts-expect-error flag key is always present in the object, Hence can ignore the TS error.
-        .map(flag => ({ [`is_${flag}_enabled`]: Boolean(flags[flag]) }))
-        .reduce((previous, current) => ({ ...previous, ...current }), {});
+    const result = Object.keys(flags).reduce(
+        (previous, current) => ({
+            ...previous,
+            // @ts-expect-error current key is always present in the object, Hence can ignore the TS error.
+            [`is_${current}_enabled`]: Boolean(flags[current]),
+        }),
+        {} as Record<`is_${keyof typeof flags}_enabled`, boolean>
+    );
 
-    return result as Record<`is_${keyof typeof flags}_enabled`, boolean>;
+    return result;
 };
 export default useFeatureFlags;

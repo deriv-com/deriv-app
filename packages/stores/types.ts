d0957a3977f000841fb078fc202c5ce1a8f43dae
Farzin Mirzaie <72082844+farzin-deriv@users.noreply.github.com>
2023-06-12 11:38:25 +0800

    Farzin/R&D/WALL-746/FeatureFlag (#8855)
    * feat(stores): :sparkles: add `FeatureFlagsStore`
    
    * feat(hooks): :sparkles: add `useFeatureFlags`
    
    * feat(core): :sparkles: add flag manager to the endpoint page
    
    * fix(stores): :green_heart: fix CI fail
    
    * ci: :green_heart: trigger build
    
    * perf(hooks): :zap: use `reduce()` instead of `map().reduce()`
    
    * perf(hooks): :zap: memoize the result of `useFeatureFlags`
    
    ---------
    
    Co-authored-by: Farzin Mirzaie <farzin@deriv.com>

--- packages/stores/types.ts
+++ packages/stores/types.ts
@@ -10,7 +10,7 @@ import type {
 } from '@deriv/api-types';
 import type { Moment } from 'moment';
 import type { RouteComponentProps } from 'react-router';
-import type { ExchangeRatesStore } from './src/stores';
+import type { ExchangeRatesStore, FeatureFlagsStore } from './src/stores';
 
 type TAccount = NonNullable<Authorize['account_list']>[0] & {
     balance?: number;
@@ -386,4 +386,5 @@ export type TCoreStores = {
 
 export type TStores = TCoreStores & {
     exchange_rates: ExchangeRatesStore;
+    feature_flags: FeatureFlagsStore;
 };

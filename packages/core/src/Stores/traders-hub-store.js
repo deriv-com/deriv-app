1b1c0295fdd7b8f5704c38de5f4109174d02f54c
Taysuisin <suisin@regentmarkets.com>
2023-06-14 17:26:38 +0800

    chore: change condition to check on EU_DEMO

--- packages/core/src/Stores/traders-hub-store.js
+++ packages/core/src/Stores/traders-hub-store.js
@@ -185,7 +185,7 @@ export default class TradersHubStore extends BaseStore {
         if (account_type === 'demo') {
             await switchAccount(account_list.find(acc => acc.is_virtual && !acc.is_disabled)?.loginid);
         } else if (account_type === 'real') {
-            if (!has_active_real_account && this.content_flag === ContentFlag.EU_REAL) {
+            if (!has_active_real_account && this.content_flag === ContentFlag.EU_DEMO) {
                 this.root_store.ui.openRealAccountSignup('maltainvest');
                 return;
             }

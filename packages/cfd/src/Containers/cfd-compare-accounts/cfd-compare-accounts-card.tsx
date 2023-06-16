8af19f52a7cbc7596c9b557f43c9f0ea62d97eca
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-06-16 11:59:51 +0800

    feat: added new banner to cfd cards

--- packages/cfd/src/Containers/cfd-compare-accounts/cfd-compare-accounts-card.tsx
+++ packages/cfd/src/Containers/cfd-compare-accounts/cfd-compare-accounts-card.tsx
@@ -4,6 +4,7 @@ import CFDInstrumentsLabelHighlighted from './cfd-instruments-label-highlighted'
 import CFDCompareAccountsDescription from './cfd-compare-accounts-description';
 import CFDCompareAccountsTitleIcon from './cfd-compare-accounts-title-icon';
 import CFDCompareAccountsPlatformLabel from './cfd-compare-accounts-platform-label';
+import { Text } from '@deriv/components';
 
 type TCFDCompareAccountsCardProps = {
     trading_platforms: TModifiedTradingPlatformAvailableAccount;
@@ -12,8 +13,11 @@ type TCFDCompareAccountsCardProps = {
 const CFDCompareAccountsCard = ({ trading_platforms }: TCFDCompareAccountsCardProps) => {
     return (
         <div className='compare-cfd-account-main-container'>
-            <CFDCompareAccountsPlatformLabel trading_platforms={trading_platforms} />
             <div className='compare-cfd-account-card-container'>
+                <CFDCompareAccountsPlatformLabel trading_platforms={trading_platforms} />
+                <Text className='compare-cfd-account-card-container__banner' weight='bold' size='xs'>
+                    New!
+                </Text>
                 <CFDCompareAccountsTitleIcon trading_platforms={trading_platforms} />
                 <CFDCompareAccountsDescription trading_platforms={trading_platforms} />
                 <CFDInstrumentsLabelHighlighted trading_platforms={trading_platforms} />

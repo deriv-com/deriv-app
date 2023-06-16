f0e748f1941ee18d8740b76d021a247190ab1a8a
Hamid Yaftian <hamid@deriv.com>
2023-06-16 15:37:46 +0800

    feat: update design based on the new changes

--- packages/appstore/src/components/demo-reset-balance/demo-reset-balance.tsx
+++ packages/appstore/src/components/demo-reset-balance/demo-reset-balance.tsx
@@ -38,23 +38,25 @@ const DemoResetBalance = observer(({ setActiveTabIndex }: TDemoResetBalanceProps
                     weight='bold'
                 >
                     {isResetBalanceSuccess ? (
-                        <Localize i18n_default_text='Your balance has been reset to 10,000.00 USD.' />
+                        <Localize i18n_default_text='Success' />
                     ) : (
                         <Localize i18n_default_text='Reset balance to 10,000.00 USD' />
                     )}
                 </Text>
 
-                {!isResetBalanceSuccess && (
-                    <Text
-                        as='p'
-                        className='reset-balance__text'
-                        size={is_mobile ? 'xxs' : 'xs'}
-                        line_height={is_mobile ? 'l' : 'xl'}
-                        align='center'
-                    >
+                <Text
+                    as='p'
+                    className='reset-balance__text'
+                    size={is_mobile ? 'xxs' : 'xs'}
+                    line_height={is_mobile ? 'l' : 'xl'}
+                    align='center'
+                >
+                    {isResetBalanceSuccess ? (
+                        <Localize i18n_default_text='Your balance has been reset to 10,000.00 USD.' />
+                    ) : (
                         <Localize i18n_default_text='Reset your virtual balance if it falls below 10,000.00 USD or exceeds 10,000.00 USD.' />
-                    </Text>
-                )}
+                    )}
+                </Text>
 
                 {isResetBalanceSuccess ? (
                     <Button

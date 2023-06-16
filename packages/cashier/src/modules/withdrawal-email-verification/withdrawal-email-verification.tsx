97a75463170452f4830769b87dbd17fa020d19aa
Aum Bhatt <aum@deriv.com>
2023-06-16 10:06:29 +0400

    chore: add error states

--- packages/cashier/src/modules/withdrawal-email-verification/withdrawal-email-verification.tsx
+++ packages/cashier/src/modules/withdrawal-email-verification/withdrawal-email-verification.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useVerifyEmail } from '@deriv/hooks';
+import Error from '../../components/error';
 import WithdrawalEmailVerificationEmptyState from './components/withdrawal-email-verification-empty-state/withdrawal-email-verification-empty-state';
 import WithdrawalEmailVerificationSent from './components/withdrawal-email-verification-sent/withdrawal-email-verification-sent';
+import ErrorStore from '../../stores/error-store';
 
 type TWithdrawalEmailVerificationProps = {
     withdraw_type: Parameters<typeof useVerifyEmail>[0];
@@ -10,7 +12,7 @@ type TWithdrawalEmailVerificationProps = {
 const WithdrawalEmailVerification = ({ withdraw_type }: TWithdrawalEmailVerificationProps) => {
     const { error, has_been_sent, send } = useVerifyEmail(withdraw_type);
 
-    if (error) return <div>error</div>;
+    if (error) return <Error error={error as ErrorStore} />;
 
     if (has_been_sent) return <WithdrawalEmailVerificationSent withdraw_type={withdraw_type} />;
 

2bd8559c85a2f6bbeb65812025eb54e82c5142cf
Aum Bhatt <aum@deriv.com>
2023-06-16 11:35:51 +0400

    chore: added verification code to the send callback

--- packages/hooks/src/useWithdrawalFiatAddress.ts
+++ packages/hooks/src/useWithdrawalFiatAddress.ts
@@ -1,15 +1,26 @@
 import { useCallback, useEffect } from 'react';
 import { useRequest } from '@deriv/api';
-import { useStore } from '@deriv/stores';
+import { observer, useStore } from '@deriv/stores';
 
 const useWithdrawalFiatAddress = () => {
+    const { client } = useStore();
     const { ui } = useStore();
     const { is_dark_mode_on } = ui;
     const { data, mutate, ...rest } = useRequest('cashier');
     const withdrawal_address =
         typeof data?.cashier === 'string' ? `${data?.cashier}&DarkMode=${is_dark_mode_on ? 'on' : 'off'}` : undefined;
 
-    const send = useCallback(() => mutate({ payload: { cashier: 'withdraw', provider: 'doughflow' } }), [mutate]);
+    const send = useCallback(
+        () =>
+            mutate({
+                payload: {
+                    cashier: 'withdraw',
+                    provider: 'doughflow',
+                    verification_code: client.verification_code.payment_withdraw,
+                },
+            }),
+        [mutate]
+    );
 
     useEffect(() => {
         send();

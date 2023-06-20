22e7b0d885614e3110deb7bbdafbb9fc3bd74398
Henry Hein <henry@regentmarkets.com>
2023-06-20 13:11:48 +0800

    fix: translation issue

--- packages/trader/src/Assets/Trading/Categories/accumulator-trade-description.tsx
+++ packages/trader/src/Assets/Trading/Categories/accumulator-trade-description.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Localize } from '@deriv/translations';
+import { Localize, localize } from '@deriv/translations';
 import { Text } from '@deriv/components';
 
 const AccumulatorTradeDescription = ({ onClick }: { onClick: () => void }) => {
     const content = [
-        'Accumulators allow you to express a view on the range of movement of an index and grow your stake exponentially at a fixed <0>growth rate</0>.',
-        'Your <0>payout</0> is the sum of your inital stake and profit.',
-        'Your stake will continue to grow as long as the current spot price remains within a specified <0>range</0> from the <0>previous spot price</0>. Otherwise, you lose your stake and the trade is terminated.',
-        'You can close your trade anytime. However, be aware of <0>slippage risk<0/>.',
+        localize(
+            'Accumulators allow you to express a view on the range of movement of an index and grow your stake exponentially at a fixed <0>growth rate</0>.'
+        ),
+        localize('Your <0>payout</0> is the sum of your inital stake and profit.'),
+        localize(
+            'Your stake will continue to grow as long as the current spot price remains within a specified <0>range</0> from the <0>previous spot price</0>. Otherwise, you lose your stake and the trade is terminated.'
+        ),
+        localize('You can close your trade anytime. However, be aware of <0>slippage risk<0/>.'),
     ];
 
     return (

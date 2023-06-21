9f6a436c2f86d0bc43e75cb323bcdda916351669
Sergei Baranovski <sergei@deriv.com>
2023-06-21 14:55:07 +0300

    feat: rewrite one test case for wallets component

--- packages/appstore/src/components/containers/__tests__/wallets.spec.tsx
+++ packages/appstore/src/components/containers/__tests__/wallets.spec.tsx
@@ -8,10 +8,6 @@ import { TWalletAccount } from 'Types';
 
 const mockedRootStore = mockStore({});
 
-// jest.mock('react-transition-group', () => ({
-//     CSSTransition: jest.fn(({ children }) => <div>{children}</div>),
-// }));
-
 jest.mock('@deriv/account', () => ({
     ...jest.requireActual('@deriv/account'),
     getStatusBadgeConfig: jest.fn(() => ({ icon: '', text: '' })),
@@ -59,27 +55,34 @@ describe('<Wallets />', () => {
         expect(container.childNodes[0]).toHaveClass('wallet__demo');
     });
 
-    // TODO: I'll repair this test a little later
-    // it('Check content appears after arrow button click', async () => {
-    //     mockedRootStore.client.loginid = 'CRW87654';
+    it('Check content appears after arrow button click', async () => {
+        mockedRootStore.client.loginid = 'CRW2';
+        mocked_props.is_selected = false;
 
-    //     render(
-    //         <StoreProvider store={mockedRootStore}>
-    //             <Wallet wallet_account={mocked_props} />
-    //         </StoreProvider>
-    //     );
+        render(
+            <StoreProvider store={mockedRootStore}>
+                <Wallet wallet_account={mocked_props} />
+            </StoreProvider>
+        );
 
-    //     const arrow_icon = screen.getByTestId('dt_arrow');
-    //     expect(screen.queryByText('wallet test content')).not.toBeInTheDocument();
-    //     userEvent.click(arrow_icon);
+        const arrow_icon = screen.getByTestId('dt_arrow');
+        expect(screen.queryByText('wallet test content')).not.toBeInTheDocument();
+        userEvent.click(arrow_icon);
 
-    //     await waitFor(() => {
-    //         mockedRootStore.client.loginid = 'CRW123123';
-    //     });
-    //     // console.log('mockedRootStore.client.loginid = ', mockedRootStore.client.loginid);
-    //     const content = screen.queryByText('wallet test content');
-    //     expect(content).toBeInTheDocument();
-    // });
+        expect(mockedRootStore.client.switchAccount).toBeCalledTimes(1);
+        mockedRootStore.client.loginid = 'CRW123123';
+        mocked_props.is_selected = true;
+
+        // rerender component because props are changed
+        render(
+            <StoreProvider store={mockedRootStore}>
+                <Wallet wallet_account={mocked_props} />
+            </StoreProvider>
+        );
+
+        const content = screen.queryByText('wallet test content');
+        expect(content).toBeInTheDocument();
+    });
 
     it('Check for demo wallet header', () => {
         mocked_props.is_virtual = true;

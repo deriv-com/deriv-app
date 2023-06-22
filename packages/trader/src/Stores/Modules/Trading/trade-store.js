3f65a9ff17628c0e31e5dbb15be3dc7cb824deda
henry-deriv <118344354+henry-deriv@users.noreply.github.com>
2023-06-22 15:09:47 +0800

    henry/85290/fix: accumulators enabled for real trading in DTrader (#8108)
    * fix: accumulators enabled for real trading in DTrader
    
    * fix: empty-commit
    
    * chore: remove unnecessary comment
    
    * fix: empty
    
    * empty commit
    
    * empty commit

--- packages/trader/src/Stores/Modules/Trading/trade-store.js
+++ packages/trader/src/Stores/Modules/Trading/trade-store.js
@@ -552,23 +552,10 @@ export default class TradeStore extends BaseStore {
             await Symbol.onChangeSymbolAsync(this.symbol);
             runInAction(() => {
                 const contract_categories = ContractType.getContractCategories();
-                //TODO yauheni, maryia - delete this 'if' statement when accumulators are allowed for real account, should leave 'else' box
-                if (
-                    this.is_accumulator &&
-                    !this.root_store.client.is_virtual &&
-                    contract_categories.contract_types_list.Accumulators
-                ) {
-                    delete contract_categories.contract_types_list.Accumulators;
-                    this.processNewValuesAsync({
-                        ...contract_categories,
-                        ...ContractType.getContractType(contract_categories.contract_types_list),
-                    });
-                } else {
-                    this.processNewValuesAsync({
-                        ...contract_categories,
-                        ...ContractType.getContractType(contract_categories.contract_types_list, this.contract_type),
-                    });
-                }
+                this.processNewValuesAsync({
+                    ...contract_categories,
+                    ...ContractType.getContractType(contract_categories.contract_types_list, this.contract_type),
+                });
                 this.processNewValuesAsync(ContractType.getContractValues(this));
             });
         }
@@ -1032,11 +1019,6 @@ export default class TradeStore extends BaseStore {
                 this.validateAllProperties();
             }
             this.debouncedProposal();
-        }
-
-        //TODO yauheni, maryia - delete this 'if' statement when accumulators are allowed for real account
-        if (!this.root_store.client.is_virtual) {
-            delete this.contract_types_list.Accumulators;
         }
     }
 

da114e2a607b923240083e3d9d29f9d5729c137d
Sergei Baranovski <sergei@deriv.com>
2023-06-22 15:36:00 +0300

    feat: add tests for ListingContainer component

---
+++ packages/appstore/src/components/containers/__tests__/listing-container.spec.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ListingContainer from '../listing-container';
+import { StoreProvider, mockStore } from '@deriv/stores';
+
+jest.mock('Components/pre-loader/title-card-loader', () => jest.fn(() => <span>TitleCardLoader</span>));
+jest.mock('Components/currency-switcher-card', () => jest.fn(() => <span>CurrencySwitcherCard</span>));
+jest.mock('Components/containers/grid-container', () => jest.fn(({ children }) => <>{children}</>));
+
+const Title = () => <span>Test Title</span>;
+const Description = () => <span>Test Description</span>;
+
+const mock = mockStore({
+    client: {
+        is_landing_company_loaded: true,
+    },
+});
+
+describe('ListingContainer', () => {
+    const wrapper = ({ children }: { children: JSX.Element }) => <StoreProvider store={mock}>{children}</StoreProvider>;
+
+    it('should render title and description when landing company is loaded', () => {
+        mock.client.is_landing_company_loaded = true;
+
+        render(
+            <ListingContainer title={<Title />} description={<Description />} is_deriv_platform={false}>
+                <div>Test Child</div>
+            </ListingContainer>,
+            {
+                wrapper,
+            }
+        );
+
+        const title = screen.queryByText('Test Title');
+        const description = screen.queryByText('Test Description');
+        expect(title).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+    });
+
+    it('should render title and description when landing company is not loaded and is not deriv platform', () => {
+        mock.client.is_landing_company_loaded = false;
+
+        render(
+            <ListingContainer title={<Title />} description={<Description />} is_deriv_platform={false}>
+                <div>Test Child</div>
+            </ListingContainer>,
+            {
+                wrapper,
+            }
+        );
+
+        const title = screen.queryByText('Test Title');
+        const description = screen.queryByText('Test Description');
+        expect(title).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+    });
+
+    it('should render TitleCardLoader when landing company is not loaded and is_deriv_platform is true', () => {
+        mock.client.is_landing_company_loaded = false;
+
+        render(
+            <ListingContainer title={<Title />} description={<Description />} is_deriv_platform={true}>
+                <div>Test Child</div>
+            </ListingContainer>,
+            {
+                wrapper,
+            }
+        );
+
+        const titleLoader = screen.queryByText('TitleCardLoader');
+        expect(titleLoader).toBeInTheDocument();
+    });
+
+    it('should render CurrencySwitcherCard when is_deriv_platform is true', () => {
+        render(
+            <ListingContainer title={<Title />} description={<Description />} is_deriv_platform={true}>
+                <div>Test Child</div>
+            </ListingContainer>,
+            {
+                wrapper,
+            }
+        );
+
+        const currencySwitcher = screen.queryByText('CurrencySwitcherCard');
+        expect(currencySwitcher).toBeInTheDocument();
+    });
+
+    it('should render children inside GridContainer', () => {
+        render(
+            <ListingContainer title={<Title />} description={<Description />}>
+                <div>Test Child</div>
+            </ListingContainer>,
+            {
+                wrapper,
+            }
+        );
+
+        const child = screen.queryByText('Test Child');
+        expect(child).toBeInTheDocument();
+    });
+});
--- packages/appstore/src/components/containers/listing-container.tsx
+++ packages/appstore/src/components/containers/listing-container.tsx
@@ -2,9 +2,10 @@ import React, { HTMLAttributes, ReactNode } from 'react';
 import CurrencySwitcherCard from 'Components/currency-switcher-card';
 import GridContainer from 'Components/containers/grid-container';
 import './listing-container.scss';
-import { useStores } from 'Stores/index';
-import { observer } from 'mobx-react-lite';
+// import { useStores } from 'Stores/index';
+// import { observer } from 'mobx-react-lite';
 import TitleCardLoader from 'Components/pre-loader/title-card-loader';
+import { observer, useStore } from '@deriv/stores';
 
 type ListingContainerProps = {
     title: ReactNode;
@@ -12,43 +13,45 @@ type ListingContainerProps = {
     is_deriv_platform?: boolean;
 };
 
-const ListingContainer = ({
-    title,
-    description,
-    is_deriv_platform = false,
-    children,
-}: ListingContainerProps & Omit<HTMLAttributes<HTMLDivElement>, 'title'>) => {
-    const { client } = useStores();
-    const { is_landing_company_loaded } = client;
+const ListingContainer = observer(
+    ({
+        title,
+        description,
+        is_deriv_platform = false,
+        children,
+    }: ListingContainerProps & Omit<HTMLAttributes<HTMLDivElement>, 'title'>) => {
+        const { client } = useStore();
+        const { is_landing_company_loaded } = client;
 
-    const Options = () => {
-        if (is_landing_company_loaded) {
-            return (
-                <div className='listing-container__title'>
-                    {title}
-                    {description}
-                </div>
-            );
-        } else if (!is_deriv_platform) {
-            return (
-                <div className='listing-container__title'>
-                    {title}
-                    {description}
-                </div>
-            );
-        }
-        return <TitleCardLoader />;
-    };
+        const Options = () => {
+            if (is_landing_company_loaded) {
+                return (
+                    <div className='listing-container__title'>
+                        {title}
+                        {description}
+                    </div>
+                );
+            } else if (!is_deriv_platform) {
+                return (
+                    <div className='listing-container__title'>
+                        {title}
+                        {description}
+                    </div>
+                );
+            }
+            return <TitleCardLoader />;
+        };
 
-    return (
-        <div className='listing-container'>
-            <div className='listing-container__top-container'>
-                <Options />
-                {is_deriv_platform && <CurrencySwitcherCard />}
+        return (
+            <div className='listing-container'>
+                <div className='listing-container__top-container'>
+                    <Options />
+                    {is_deriv_platform && <CurrencySwitcherCard />}
+                </div>
+                <GridContainer>{children}</GridContainer>
             </div>
-            <GridContainer>{children}</GridContainer>
-        </div>
-    );
-};
+        );
+    }
+);
 
-export default observer(ListingContainer);
+export default ListingContainer;

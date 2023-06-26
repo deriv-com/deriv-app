f07bfb732a1fc40e952879b4ba8bb60a82485c39
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-06-26 14:48:37 +0800

    chore: revert useisp2penabled logics

--- packages/hooks/src/__tests__/useIsP2PEnabled.spec.tsx
+++ packages/hooks/src/__tests__/useIsP2PEnabled.spec.tsx
@@ -11,30 +11,12 @@ jest.mock('@deriv/api', () => ({
 
 const mockUseFetch = useFetch as jest.MockedFunction<typeof useFetch<'website_status'>>;
 
-const mock_usefetch_return_value = {
-    data: {
-        website_status: {
-            p2p_config: {
-                disabled: 0,
-                supported_currencies: ['usd'],
-            },
-        },
-    },
-};
-
 describe('useIsP2PEnabled', () => {
-    beforeEach(() => {
+    test('should return false if users currency is not supported in p2p', () => {
+        const mock = mockStore({ client: { currency: 'AUD' } });
+
         // @ts-expect-error need to come up with a way to mock the return type of useFetch
-        mockUseFetch.mockReturnValue(mock_usefetch_return_value);
-    });
-    test('should return false if p2p is disabled', () => {
-        const mock = mockStore({
-            client: {
-                active_accounts: [{ currency: 'USD', is_virtual: 0 }],
-            },
-        });
-
-        mock_usefetch_return_value.data.website_status.p2p_config.disabled = 1;
+        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <APIProvider>
@@ -47,14 +29,16 @@ describe('useIsP2PEnabled', () => {
         expect(result.current.data).toBe(false);
     });
 
-    test('should return false if p2p is not disabled but is virtual', () => {
+    test('should return false if users currency is supported in p2p but is virtual', () => {
         const mock = mockStore({
             client: {
+                currency: 'USD',
                 is_virtual: true,
-                active_accounts: [{ currency: 'USD', is_virtual: 0 }],
             },
         });
-        mock_usefetch_return_value.data.website_status.p2p_config.disabled = 0;
+
+        // @ts-expect-error need to come up with a way to mock the return type of useFetch
+        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <APIProvider>
@@ -67,12 +51,15 @@ describe('useIsP2PEnabled', () => {
         expect(result.current.data).toBe(false);
     });
 
-    test('should return true if p2p is not disabled and is_low_risk_cr_eu_real is false', () => {
+    test('should return true if users currency is supported in p2p and is_low_risk_cr_eu_real is false', () => {
         const mock = mockStore({
             client: {
-                active_accounts: [{ currency: 'USD', is_virtual: 0 }],
+                currency: 'USD',
             },
         });
+
+        // @ts-expect-error need to come up with a way to mock the return type of useFetch
+        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <APIProvider>
@@ -85,36 +72,18 @@ describe('useIsP2PEnabled', () => {
         expect(result.current.data).toBe(true);
     });
 
-    test('should return false if p2p is not disabled but is_low_risk_cr_eu_real is true', () => {
+    test('should return false if users currency is supported in p2p but is_low_risk_cr_eu_real is true', () => {
         const mock = mockStore({
+            client: {
+                currency: 'USD',
+            },
             traders_hub: {
                 is_low_risk_cr_eu_real: true,
-            },
-            client: {
-                active_accounts: [{ currency: 'USD', is_virtual: 0 }],
             },
         });
 
         // @ts-expect-error need to come up with a way to mock the return type of useFetch
-        mockUseFetch.mockReturnValue(mock_usefetch_return_value);
-
-        const wrapper = ({ children }: { children: JSX.Element }) => (
-            <APIProvider>
-                <StoreProvider store={mock}>{children}</StoreProvider>
-            </APIProvider>
-        );
-
-        const { result } = renderHook(() => useIsP2PEnabled(), { wrapper });
-
-        expect(result.current.data).toBe(false);
-    });
-
-    test('should return false if p2p is not disabled but client has no supported currencies', () => {
-        const mock = mockStore({
-            client: {
-                active_accounts: [{ currency: 'EUR', is_virtual: 0 }],
-            },
-        });
+        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <APIProvider>
--- packages/hooks/src/useIsP2PEnabled.ts
+++ packages/hooks/src/useIsP2PEnabled.ts
@@ -6,21 +6,16 @@ import useHasP2PSupportedCurrencies from './useHasP2PSupportedCurrencies';
 const useIsP2PEnabled = () => {
     const { client, traders_hub } = useStore();
     // Todo: to replace it with useAuthorize hook
-    const { is_authorize, is_crypto, loginid, is_virtual } = client;
+    const { is_authorize, loginid, currency, is_virtual } = client;
     const { is_low_risk_cr_eu_real } = traders_hub;
     const invalidate = useInvalidateQuery();
-    const { data: has_p2p_supported_currencies } = useHasP2PSupportedCurrencies();
     // Todo: Replace it with new BE p2p_settings endpoint when its ready
     const { data, ...rest } = useFetch('website_status', { options: { enabled: is_authorize } });
 
-    const is_p2p_config_not_disabled = data?.website_status?.p2p_config?.disabled === 0;
-
-    const is_p2p_enabled =
-        is_p2p_config_not_disabled &&
-        has_p2p_supported_currencies &&
-        !is_crypto() &&
-        !is_virtual &&
-        !is_low_risk_cr_eu_real;
+    const is_p2p_supported_currency = Boolean(
+        data?.website_status?.p2p_config?.supported_currencies.includes(currency.toLocaleLowerCase())
+    );
+    const is_p2p_enabled = is_p2p_supported_currency && !is_virtual && !is_low_risk_cr_eu_real;
 
     useEffect(() => {
         invalidate('website_status');

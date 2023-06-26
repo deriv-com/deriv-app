c93e59a30e9f2e2cf6dc9c6ac7bfc50505853a93
Hamid Yaftian <hamid@deriv.com>
2023-06-26 13:07:24 +0800

    Revert "Yaswanth/88554/remove connect from account (#8551)"
    This reverts commit a36d71a78b1b2e80af8ec39765e09734dbbeb0fd.

--- packages/account/src/Containers/reset-trading-password.jsx
+++ packages/account/src/Containers/reset-trading-password.jsx
@@ -1,32 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
 import { CFD_PLATFORMS } from '@deriv/shared';
-import { observer, useStore } from '@deriv/stores';
+import { connect } from 'Stores/connect';
 import ResetTradingPasswordModal from '../Components/reset-trading-password-modal';
 
-const ResetTradingPassword = observer(() => {
-    const { ui, client } = useStore();
-    const { enableApp, disableApp, is_visible, is_loading, setResetTradingPasswordModalOpen } = ui;
+const ResetTradingPassword = ({
+    enableApp,
+    disableApp,
+    toggleResetTradingPasswordModal,
+    mt5_verification_code,
+    dxtrade_verification_code,
+    is_visible,
+    is_loading,
+}) => {
     const location = useLocation();
     const query_params = new URLSearchParams(location.search);
     const cfd_platform = /^trading_platform_(.*)_password_reset$/.exec(query_params.get('action') || '')?.[1];
     const [platform] = React.useState(cfd_platform);
-    const verification_code =
-        platform === CFD_PLATFORMS.MT5
-            ? client.verification_code.trading_platform_mt5_password_reset
-            : client.verification_code.trading_platform_dxtrade_password_reset;
+    const verification_code = platform === CFD_PLATFORMS.MT5 ? mt5_verification_code : dxtrade_verification_code;
 
     return (
         <ResetTradingPasswordModal
             platform={platform}
             enableApp={enableApp}
             disableApp={disableApp}
-            toggleResetTradingPasswordModal={setResetTradingPasswordModalOpen}
+            toggleResetTradingPasswordModal={toggleResetTradingPasswordModal}
             is_visible={is_visible}
             is_loading={is_loading}
             verification_code={verification_code}
         />
     );
-});
+};
 
-export default ResetTradingPassword;
+ResetTradingPassword.propTypes = {
+    disableApp: PropTypes.func,
+    enableApp: PropTypes.func,
+    is_loading: PropTypes.bool,
+    is_visible: PropTypes.bool,
+    toggleResetTradingPasswordModal: PropTypes.func,
+    mt5_verification_code: PropTypes.string,
+    dxtrade_verification_code: PropTypes.string,
+};
+
+export default connect(({ ui, client }) => ({
+    disableApp: ui.disableApp,
+    enableApp: ui.enableApp,
+    is_loading: ui.is_loading,
+    is_visible: ui.is_reset_trading_password_modal_visible,
+    toggleResetTradingPasswordModal: ui.setResetTradingPasswordModalOpen,
+    mt5_verification_code: client.verification_code.trading_platform_mt5_password_reset,
+    dxtrade_verification_code: client.verification_code.trading_platform_dxtrade_password_reset,
+}))(ResetTradingPassword);

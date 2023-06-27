68bc0f2fed5ef7af6125aed8040920bf738d047b
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-06-27 11:40:21 +0800

    fix: scroll to active wallet upon click

--- packages/appstore/src/components/containers/wallet.tsx
+++ packages/appstore/src/components/containers/wallet.tsx
@@ -12,13 +12,19 @@ type TWallet = {
 };
 
 const Wallet = observer(({ wallet_account }: TWallet) => {
+    const headerRef = React.useRef() as React.MutableRefObject<HTMLDivElement>;
+
     return (
-        <div className={classNames('wallet', { wallet__demo: wallet_account.is_demo })}>
+        <div ref={headerRef} className={classNames('wallet', { wallet__demo: wallet_account.is_demo })}>
             <WalletHeader wallet_account={wallet_account} />
             <CSSTransition
                 appear
                 in={wallet_account.is_selected}
                 timeout={240}
+                onEntered={() => {
+                    headerRef.current.style.scrollMargin = '20px';
+                    headerRef.current.scrollIntoView({ behavior: 'smooth' });
+                }}
                 classNames='wallet__content-transition'
                 unmountOnExit
             >
--- packages/appstore/src/modules/traders-hub/account-with-wallets.tsx
+++ packages/appstore/src/modules/traders-hub/account-with-wallets.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import Wallet from 'Components/containers/wallet';
 import { observer } from '@deriv/stores';
 import { useWalletsList } from '@deriv/hooks';
-import { Loading } from '@deriv/components';
 
 const AccountWithWallets = observer(() => {
     const { data } = useWalletsList();
-
-    if (!data) return <Loading is_fullscreen={false} />;
 
     return (
         <React.Fragment>

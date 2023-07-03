a72bb85375c01459883916cd5a71b36222cdfefa
Shahzaib <shahzaib@deriv.com>
2023-07-03 14:49:23 +0800

    ci: update package.json

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -2,14 +2,6 @@ const webpackConfig = require('./webpack.config');
 
 module.exports = {
     extends: '../../.eslintrc.js',
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            rules: {
-                'react/prop-types': 'off',
-            },
-        },
-    ],
     settings: {
         react: {
             version: '16',

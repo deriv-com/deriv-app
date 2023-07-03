f3597d3cb04c87186c6702cbf39829079c096e09
Farzin Mirzaie <farzin@deriv.com>
2023-07-03 16:22:16 +0800

    refactor(appstore): :fire: clean-up

--- packages/hooks/src/useWalletsList.ts
+++ packages/hooks/src/useWalletsList.ts
@@ -1,58 +1,67 @@
 import { useMemo } from 'react';
 import { useFetch } from '@deriv/api';
 import { useStore } from '@deriv/stores';
+import useCurrencyConfig from './useCurrencyConfig';
 
+/** A custom hook to get the list of wallets for the current user. */
 const useWalletsList = () => {
     const { client, ui } = useStore();
-    const { accounts, loginid, is_crypto } = client;
+    const { accounts, loginid } = client;
     const { is_dark_mode_on } = ui;
-    const { data, ...reset } = useFetch('authorize', {
-        payload: { authorize: accounts[loginid || ''].token },
-        options: { enabled: Boolean(loginid) },
-    });
+    const token = accounts[loginid || ''].token;
+    const { getConfig } = useCurrencyConfig();
+
+    const { data: authorize_data, ...reset } = useFetch('authorize', { payload: { authorize: token } });
     const { data: balance_data } = useFetch('balance', { payload: { account: 'all' } });
 
     const sortedWallets = useMemo(() => {
-        // @ts-expect-error Need to update @deriv/api-types to fix the TS error
-        // Filter out accounts which has account_category as wallet
-        const wallets = data?.authorize?.account_list?.filter(account => account.account_category === 'wallet');
+        const wallets = authorize_data?.authorize?.account_list?.filter(
+            // @ts-expect-error Need to update @deriv/api-types to fix the TS error
+            // Filter out accounts which has account_category as wallet
+            account => account.account_category === 'wallet'
+        );
 
-        // Modify the wallets to include the missing balance from the API response
-        // Should remove this once the API is fixed
-        const modified_wallets = wallets?.map(wallet => ({
-            ...wallet,
-            /** Indicating whether the wallet is the currently selected wallet. */
-            is_selected: wallet.loginid === loginid,
-            /** Indicating whether the wallet is a virtual-money wallet. */
-            is_demo: wallet.is_virtual === 1,
-            /** Wallet balance */
-            balance: balance_data?.balance?.accounts?.[wallet.loginid || '']?.balance || 0,
-            /** Landing company shortcode the account belongs to. Use this instead of landing_company_shortcode for wallets */
-            landing_company_name: wallet.landing_company_name === 'maltainvest' ? 'malta' : wallet.landing_company_name,
-            is_malta_wallet: wallet.landing_company_name === 'malta',
-            gradient_header_class: `wallet-header__${
-                wallet.is_virtual === 1 ? 'demo' : wallet.currency?.toLowerCase()
-            }-bg${is_dark_mode_on ? '--dark' : ''}`,
-            gradient_card_class: `wallet-card__${wallet.is_virtual === 1 ? 'demo' : wallet.currency?.toLowerCase()}-bg${
-                is_dark_mode_on ? '--dark' : ''
-            }`,
-        }));
+        // Modify the wallet data to include additional information.
+        const modified_wallets = wallets?.map(wallet => {
+            const currency_type = wallet.is_virtual === 1 ? 'demo' : wallet.currency?.toLowerCase();
 
-        // Sort the wallets alphabetically by fiat, crypto, then virtual
+            return {
+                ...wallet,
+                /** Indicating whether the wallet is the currently selected wallet. */
+                is_selected: wallet.loginid === loginid,
+                /** Indicating whether the wallet is a virtual-money wallet. */
+                is_demo: wallet.is_virtual === 1,
+                /** Wallet balance */
+                balance: balance_data?.balance?.accounts?.[wallet.loginid || '']?.balance || 0,
+                /** Landing company shortcode the account belongs to. */
+                landing_company_name: wallet.landing_company_name?.replace('maltainvest', 'malta'),
+                /** Indicating whether the wallet is a maltainvest wallet. */
+                is_malta_wallet: wallet.landing_company_name === 'malta',
+                /** The gradient class name for the wallet header background. */
+                gradient_header_class: `wallet-header__${currency_type}-bg${is_dark_mode_on ? '--dark' : ''}`,
+                /** The gradient class name for the wallet card background. */
+                gradient_card_class: `wallet-card__${currency_type}-bg${is_dark_mode_on ? '--dark' : ''}`,
+                /** Wallet's currency config information */
+                currency_config: wallet.currency ? getConfig(wallet.currency) : undefined,
+            } as const;
+        });
+
+        // Sort the wallets alphabetically by fiat, crypto, then virtual.
         return modified_wallets?.sort((a, b) => {
             if (a.is_virtual !== b.is_virtual) {
                 return a.is_virtual ? 1 : -1;
-            } else if (is_crypto(a.currency) !== is_crypto(b.currency)) {
-                return is_crypto(a.currency) ? 1 : -1;
+            } else if (a.currency_config?.is_crypto !== b.currency_config?.is_crypto) {
+                return a.currency_config?.is_crypto ? 1 : -1;
             }
 
             return (a.currency || 'USD').localeCompare(b.currency || 'USD');
         });
-    }, [balance_data?.balance?.accounts, data?.authorize?.account_list, is_crypto, loginid, is_dark_mode_on]);
+    }, [authorize_data?.authorize?.account_list, loginid, balance_data?.balance?.accounts, is_dark_mode_on, getConfig]);
 
     return {
-        ...reset,
+        /** List of wallets for current user. */
         data: sortedWallets,
+        ...reset,
     };
 };
 

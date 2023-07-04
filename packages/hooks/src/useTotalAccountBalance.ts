362602e5994756b912e2a9c2878965e857f1acd0
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-07-04 13:29:38 +0800

    fix: attemp #3

--- packages/hooks/src/useTotalAccountBalance.ts
+++ packages/hooks/src/useTotalAccountBalance.ts
@@ -1,14 +1,16 @@
 import useRealTotalAssetCurrency from './useTotalAssetCurrency';
 import useExchangeRate from './useExchangeRate';
-
+import { useStore } from '@deriv/stores';
+import { CFD_PLATFORMS } from '../../shared/src/utils/platform';
 /**
  * we can use this hook to get the total balance of the given accounts list.
  * it loops through the accounts list and adds the balance of each account
  * to the total balance, it also converts the balance to the currency of the
  * first account in the list
  */
-
 const useTotalAccountBalance = (accounts: { balance?: number; currency?: string }[]) => {
+    const { traders_hub } = useStore();
+    const { is_demo } = traders_hub;
     const total_assets_real_currency = useRealTotalAssetCurrency();
     const { getRate } = useExchangeRate();
 
@@ -16,7 +18,7 @@ const useTotalAccountBalance = (accounts: { balance?: number; currency?: string 
 
     const balance = accounts.reduce((total, account) => {
         const base_rate = getRate(total_assets_real_currency || '');
-        const rate = getRate(account.currency || total_assets_real_currency || '');
+        const rate = is_demo && CFD_PLATFORMS.MT5 ? getRate('USD') : getRate(total_assets_real_currency || '');
         const exchange_rate = base_rate / rate;
 
         return total + (account.balance || 0) * exchange_rate;

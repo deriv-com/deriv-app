3139f76d82f2a325277dbdddfe9370304ba9ef06
Shayan Khaleghparast <iman@regentmarkets.com>
2023-07-04 17:40:20 +0330

    chore: make the code cleaner

--- packages/utils/src/isServiceWorkerSupported.ts
+++ packages/utils/src/isServiceWorkerSupported.ts
@@ -1,7 +1,7 @@
 import semver from 'semver';
 import { UAParser } from 'ua-parser-js';
 
-const browsers_minimum_required_version = {
+const minimum_required_browsers_versions: Record<string, string> = {
     Chrome: '40.0.0',
     Edge: '17.0.0',
     Safari: '11.1.0',
@@ -9,27 +9,21 @@ const browsers_minimum_required_version = {
     Opera: '27.0.0',
     'Opera Android': '27.0.0',
     'Mobile Safari': '11.3.0',
-    // 'Samsung Internet': '4.0.0',
     'Android Browser': '40.0.0',
-} as const;
+};
 
+/** Check if the browser supports service worker */
 const isServiceWorkerSupported = () => {
     const parser = new UAParser();
+    const user_browser_name = parser.getBrowser().name || '';
+    const user_browser_version = semver.coerce(parser.getBrowser().version || '1.0.0.')?.version || '1.0.0';
+    const minimum_required_browsers_version = minimum_required_browsers_versions[user_browser_name];
 
-    const user_browser = {
-        name: parser.getBrowser().name || '',
-        version: semver.coerce(parser.getBrowser().version || '1.0.0.')?.version || '1.0.0',
-    };
+    // if the browser is in the list , check if the version is greater than the minimum required
+    if (minimum_required_browsers_version) semver.gt(user_browser_version, minimum_required_browsers_version);
 
-    return (
-        typeof browsers_minimum_required_version[
-            user_browser.name as keyof typeof browsers_minimum_required_version
-        ] !== 'undefined' &&
-        semver.gt(
-            user_browser.version,
-            browsers_minimum_required_version[user_browser.name as keyof typeof browsers_minimum_required_version]
-        )
-    );
+    // if the browser is not in the list, it is not supported
+    return false;
 };
 
 export default isServiceWorkerSupported;

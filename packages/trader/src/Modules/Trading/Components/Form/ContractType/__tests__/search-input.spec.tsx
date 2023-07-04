fba7a58f6e03da0d8f9ba3ebc81088bb69994e7a
Henry Hein <henry@regentmarkets.com>
2023-07-04 18:51:13 +0800

    fix: remove unused import

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/search-input.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/search-input.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SearchInput from '../ContractTypeMenu/search-input';
 
 jest.mock('@deriv/components', () => {

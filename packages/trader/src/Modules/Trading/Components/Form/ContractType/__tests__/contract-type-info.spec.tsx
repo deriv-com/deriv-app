52c01cf1db40f0e46a650f70a19d44222fae2958
Henry Hein <henry@regentmarkets.com>
2023-07-05 09:22:43 +0800

    fix: add test cases

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-info.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-info.spec.tsx
@@ -4,7 +4,8 @@ import Info from '../ContractTypeInfo/contract-type-info';
 import userEvent from '@testing-library/user-event';
 
 jest.mock('Assets/Trading/Categories/trade-categories-gif.jsx', () => jest.fn(() => 'TradeCategoriesGif'));
-jest.mock('Assets/Trading/Categories/trade-categories.jsx', () => jest.fn(() => 'TradeCategories'));
+jest.mock('Assets/Trading/Categories/trade-categories.jsx', () => jest.fn(() => 'TradeDescription'));
+jest.mock('../ContractTypeInfo/contract-type-glossary', () => jest.fn(() => 'TradeTypeGlossary'));
 
 const mocked_props: React.ComponentProps<typeof Info> = {
     handleNavigationClick: jest.fn(),
@@ -76,6 +77,17 @@ const mocked_props: React.ComponentProps<typeof Info> = {
                     label: 'Digits',
                     key: 'Digits',
                 },
+                {
+                    contract_types: [
+                        {
+                            text: 'Call/Put',
+                            value: 'vanilla',
+                        },
+                    ],
+                    icon: 'IcVanilla',
+                    label: 'Vanillas',
+                    key: 'Vanillas',
+                },
             ],
             contract_types: [
                 {
@@ -109,6 +121,10 @@ const mocked_props: React.ComponentProps<typeof Info> = {
                 {
                     text: 'Over/Under',
                     value: 'over_under',
+                },
+                {
+                    text: 'Call/Put',
+                    value: 'vanilla',
                 },
             ],
             icon: 'IcCatAll',
@@ -190,6 +206,17 @@ const mocked_props: React.ComponentProps<typeof Info> = {
                     label: 'Digits',
                     key: 'Digits',
                 },
+                {
+                    contract_types: [
+                        {
+                            text: 'Call/Put',
+                            value: 'vanilla',
+                        },
+                    ],
+                    icon: 'IcVanilla',
+                    label: 'Vanillas',
+                    key: 'Vanillas',
+                },
             ],
             contract_types: [
                 {
@@ -220,6 +247,10 @@ const mocked_props: React.ComponentProps<typeof Info> = {
                     text: 'Over/Under',
                     value: 'over_under',
                 },
+                {
+                    text: 'Call/Put',
+                    value: 'vanilla',
+                },
             ],
             icon: 'IcCatOptions',
             label: 'Options',
@@ -229,21 +260,34 @@ const mocked_props: React.ComponentProps<typeof Info> = {
 };
 
 describe('<Info />', () => {
-    it('Should render only one "Choose Rise/Fall" button', () => {
+    it('Should render only one "Choose Multipliers" button', () => {
         render(<Info {...mocked_props} />);
-        const rise_fall_button = screen.queryAllByText('Choose Rise/Fall');
-        expect(rise_fall_button).not.toHaveLength(2);
+        const trade_type_button = screen.queryByText('Choose Multipliers');
+        expect(trade_type_button).toBeInTheDocument();
     });
-    it('Should render 7 buttons, 7 Gifs, and 7 trade categories even though there are 8 trade types', () => {
+    it('Carousel should render 8 Gifs, and 8 trade descriptions', () => {
         render(<Info {...mocked_props} />);
-        expect(screen.getAllByText(/choose/i)).toHaveLength(7);
-        expect(screen.getAllByText(/tradecategoriesgif/i)).toHaveLength(7);
-        expect(screen.getAllByText('TradeCategories')).toHaveLength(7);
+        expect(screen.getAllByText(/tradecategoriesgif/i)).toHaveLength(8);
+        expect(screen.getAllByText('TradeDescription')).toHaveLength(8);
     });
-    it('should do something', () => {
+    it('Should call handleSelect when clicking on "Choose Multipliers" button', () => {
         render(<Info {...mocked_props} />);
-        const rise_fall_button = screen.getByText('Choose Rise/Fall');
-        userEvent.click(rise_fall_button);
+        const trade_type_button = screen.queryByText('Choose Multipliers') as HTMLButtonElement;
+        userEvent.click(trade_type_button);
+        expect(trade_type_button).toBeInTheDocument();
         expect(mocked_props.handleSelect).toHaveBeenCalled();
+    });
+    it('Should render toggle buttons if vanilla info page is open', () => {
+        render(<Info {...mocked_props} initial_index={7} />);
+        const trade_type_button = screen.getByText('Choose Call/Put');
+        expect(screen.getByText('Description')).toBeInTheDocument();
+        expect(screen.getByText(/glossary/i)).toBeInTheDocument();
+        expect(trade_type_button).toBeInTheDocument();
+    });
+    it('should render 8 glossary pages if glossary button is clicked', () => {
+        render(<Info {...mocked_props} initial_index={7} />);
+        const glossary_button = screen.getByText('Glossary');
+        userEvent.click(glossary_button);
+        expect(screen.getAllByText(/tradetypeglossary/i)).toHaveLength(8);
     });
 });

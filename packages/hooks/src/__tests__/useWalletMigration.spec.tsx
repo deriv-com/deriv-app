48b9e93d00d5777b993be072caf2ecd68c20f16f
Sergei Baranovski <sergei@deriv.com>
2023-07-05 11:53:38 +0300

    feat: modify useWalletMigration hook

--- packages/hooks/src/__tests__/useWalletMigration.spec.tsx
+++ packages/hooks/src/__tests__/useWalletMigration.spec.tsx
@@ -1,32 +1,53 @@
-import * as React from 'react';
-import { StoreProvider, mockStore } from '@deriv/stores';
+import React from 'react';
+import { APIProvider, useFetch, useRequest } from '@deriv/api';
 import { renderHook } from '@testing-library/react-hooks';
 import useWalletMigration from '../useWalletMigration';
 
-describe('useWalletMigration', () => {
-    test('should return status === eligible', () => {
-        const mock = mockStore({
-            client: { wallet_migration_status: 'eligible' },
-        });
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useFetch: jest.fn(),
+    useRequest: jest.fn(() => ({ mutate: jest.fn() })),
+}));
 
-        const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mock}>{children}</StoreProvider>
-        );
+const mockUseFetch = useFetch as jest.MockedFunction<typeof useFetch<'wallet_migration'>>;
+// @ts-expect-error need to come up with a way to mock the return type of useRequest
+const mockUseRequest = useRequest as jest.MockedFunction<typeof useRequest<'wallet_migration'>>;
+
+describe('useWalletMigration', () => {
+    test('should return wallet migration status', () => {
+        // @ts-expect-error need to come up with a way to mock the return type of useFetch
+        mockUseFetch.mockReturnValue({ data: { wallet_migration: { status: 'eligible' } } });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
+
         const { result } = renderHook(() => useWalletMigration(), { wrapper });
 
         expect(result.current.status).toBe('eligible');
     });
 
-    test('should return status === failed', () => {
-        const mock = mockStore({
-            client: { wallet_migration_status: 'failed' },
-        });
+    test('should send start wallet migration request', () => {
+        // @ts-expect-error need to come up with a way to mock the return type of useRequest
+        mockUseRequest.mockReturnValue({ mutate: jest.fn() });
 
-        const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mock}>{children}</StoreProvider>
-        );
+        const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
+
         const { result } = renderHook(() => useWalletMigration(), { wrapper });
 
-        expect(result.current.status).toBe('failed');
+        result.current.start_migration();
+
+        expect(mockUseRequest('wallet_migration').mutate).toBeCalledWith({ payload: { wallet_migration: 'start' } });
+    });
+
+    test('should send reset wallet migration request', () => {
+        // @ts-expect-error need to come up with a way to mock the return type of useRequest
+        mockUseRequest.mockReturnValue({ mutate: jest.fn() });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
+
+        const { result } = renderHook(() => useWalletMigration(), { wrapper });
+
+        result.current.reset_migration();
+
+        expect(mockUseRequest('wallet_migration').mutate).toBeCalledWith({ payload: { wallet_migration: 'reset' } });
     });
 });
--- packages/hooks/src/useWalletMigration.ts
+++ packages/hooks/src/useWalletMigration.ts
@@ -1,15 +1,44 @@
-import { useStore } from '@deriv/stores';
+import { useCallback } from 'react';
+import { useFetch, useInvalidateQuery, useRequest } from '@deriv/api';
 
+/** A custom hook to get the status of wallet_migration API and to start/reset the migration process */
 const useWalletMigration = () => {
-    // TODO: Use 'useFetch' in the future when BE functionality will be released
-    // or mockServer start to work
+    const invalidate = useInvalidateQuery();
 
-    const {
-        client: { wallet_migration_status },
-    } = useStore();
+    /** Make a request to wallet_migration API and onSuccess it will invalidate the cached data  */
+    const { mutate } = useRequest('wallet_migration', { onSuccess: () => invalidate('wallet_migration') });
+
+    /** Fetch the wallet_migration API and refetch it every second if the status is in_progress */
+    const { data } = useFetch('wallet_migration', {
+        payload: { wallet_migration: 'status' },
+        options: {
+            refetchInterval: response => (response?.wallet_migration?.status === 'in_progress' ? 500 : false),
+        },
+    });
+
+    const start_migration = useCallback(() => mutate({ payload: { wallet_migration: 'start' } }), [mutate]);
+
+    const reset_migration = useCallback(() => mutate({ payload: { wallet_migration: 'reset' } }), [mutate]);
+
+    const status = data?.wallet_migration?.status;
 
     return {
-        status: wallet_migration_status,
+        /** The status of the wallet_migration API */
+        status,
+        /** A boolean to check if the status is not_eligible */
+        is_ineligible: status === 'ineligible',
+        /** A boolean to check if the status is eligible */
+        is_eligible: status === 'eligible',
+        /** A boolean to check if the status is in_progress */
+        is_in_progress: status === 'in_progress',
+        /** A boolean to check if the status is completed */
+        is_migrated: status === 'done',
+        /** A boolean to check if the status is failed */
+        is_failed: status === 'failed',
+        /** Sends a request to wallet_migration API to start the migration process */
+        start_migration,
+        /** Sends a request to wallet_migration API to reset the migration process */
+        reset_migration,
     };
 };
 

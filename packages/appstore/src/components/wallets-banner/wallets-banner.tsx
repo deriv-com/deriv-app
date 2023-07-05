573ea048e2f1d353eead84b0cea87c9bdd086f14
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-07-05 11:01:19 +0800

    fix: refactor v2

--- packages/appstore/src/components/wallets-banner/wallets-banner.tsx
+++ packages/appstore/src/components/wallets-banner/wallets-banner.tsx
@@ -6,25 +6,20 @@ import { useWalletMigration } from '@deriv/hooks';
 import { observer, useStore } from '@deriv/stores';
 
 const WalletsBanner = observer(() => {
-    const { status } = useWalletMigration();
+    const { is_eligible, is_failed, is_in_progress, is_migrated } = useWalletMigration();
     const { traders_hub } = useStore();
     const { is_eu_user } = traders_hub;
 
-    switch (status) {
-        // the user can upgrade to the wallets
-        case 'eligible':
-        case 'failed':
-            return <WalletsBannerUpgrade />;
-        // the wallets upgrading is in progress
-        case 'in_progress':
-            return <WalletsBannerUpgrading is_eu={is_eu_user} />;
-        // the wallets upgrading completed
-        case 'migrated':
-            return <WalletsBannerReady is_eu={is_eu_user} />;
-        // the user can't upgrade to the wallets
-        default:
-            return null;
+    if (is_eligible || is_failed) {
+        return <WalletsBannerUpgrade />;
     }
+    if (is_in_progress) {
+        return <WalletsBannerUpgrading is_eu={is_eu_user} />;
+    }
+    if (is_migrated) {
+        return <WalletsBannerReady is_eu={is_eu_user} />;
+    }
+    return <WalletsBannerReady is_eu={is_eu_user} />;
 });
 
 export default WalletsBanner;

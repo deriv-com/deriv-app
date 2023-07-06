7b4b2f259de3411f9790d6b74858ef0cd1f8eaf9
hamza-deriv <hamza.muhammad@regentmarkets.com>
2023-07-06 13:29:22 +0800

    fix: renamed variable + icons

--- packages/cfd/src/Containers/cfd-compare-accounts/cfd-compare-accounts.tsx
+++ packages/cfd/src/Containers/cfd-compare-accounts/cfd-compare-accounts.tsx
@@ -7,7 +7,7 @@ import { Localize, localize } from '@deriv/translations';
 import { observer, useStore } from '@deriv/stores';
 import CFDCompareAccountsCard from './cfd-compare-accounts-card';
 import {
-    getSortedAvailableAccounts,
+    getSortedCFDAvailableAccounts,
     getEUAvailableAccounts,
     getMT5DemoData,
     getDxtradeDemoData,
@@ -22,28 +22,36 @@ const CompareCFDs = observer(() => {
     const { is_demo, is_eu_user, available_dxtrade_accounts } = traders_hub;
 
     const sorted_available_accounts = !is_eu_user
-        ? getSortedAvailableAccounts(trading_platform_available_accounts)
+        ? getSortedCFDAvailableAccounts(trading_platform_available_accounts)
         : getEUAvailableAccounts(trading_platform_available_accounts);
+
     // Check if dxtrade data is available
     const has_dxtrade_account_available = available_dxtrade_accounts.length > 0;
 
-    const sorted_available_eu_accounts =
+    const sorted_cfd_available_eu_accounts =
         is_eu_user && sorted_available_accounts.length ? [...sorted_available_accounts] : [];
 
     // Getting real accounts data
-    const all_real_sorted_available_accounts = !is_eu_user
+    const all_real_sorted_cfd_available_accounts = !is_eu_user
         ? [...sorted_available_accounts]
-        : [...sorted_available_eu_accounts];
+        : [...sorted_cfd_available_eu_accounts];
 
     // Getting demo accounts data
-    const demo_available_accounts = [
-        ...getMT5DemoData(all_real_sorted_available_accounts).concat(
-            getDxtradeDemoData(all_real_sorted_available_accounts)
+    const demo_cfd_available_accounts = [
+        ...getMT5DemoData(all_real_sorted_cfd_available_accounts).concat(
+            getDxtradeDemoData(all_real_sorted_cfd_available_accounts)
         ),
     ];
 
-    const all_available_accounts =
-        is_demo && demo_available_accounts.length > 0 ? demo_available_accounts : all_real_sorted_available_accounts;
+    const all_cfd_available_accounts =
+        is_demo && demo_cfd_available_accounts.length > 0
+            ? demo_cfd_available_accounts
+            : all_real_sorted_cfd_available_accounts;
+
+    // Calculate the card count for alignment of card in center
+    const card_count = has_dxtrade_account_available
+        ? all_cfd_available_accounts.length + 1
+        : all_cfd_available_accounts.length;
 
     const DesktopHeader = (
         <div className='compare-cfd-header'>
@@ -78,12 +86,12 @@ const CompareCFDs = observer(() => {
                     <PageOverlay header={DesktopHeader} is_from_app={routes.traders_hub} />
                     <div
                         className={classNames('compare-cfd-account-container', {
-                            'compare-cfd-account-container__eu': is_eu_user,
+                            'compare-cfd-account-container__card-count': card_count < 4,
                         })}
                     >
                         <div className='card-list'>
                             <CFDCompareAccountsCarousel>
-                                {all_available_accounts.map(item => (
+                                {all_cfd_available_accounts.map(item => (
                                     <CFDCompareAccountsCard
                                         trading_platforms={item}
                                         key={item.market_type + item.shortcode}
@@ -92,7 +100,7 @@ const CompareCFDs = observer(() => {
                                     />
                                 ))}
                                 {/* Renders Deriv X data */}
-                                {all_available_accounts.length > 0 && has_dxtrade_account_available && (
+                                {all_cfd_available_accounts.length > 0 && has_dxtrade_account_available && (
                                     <CFDCompareAccountsCard
                                         trading_platforms={dxtrade_data}
                                         is_eu_user={is_eu_user}
@@ -120,11 +128,11 @@ const CompareCFDs = observer(() => {
                 >
                     <div
                         className={classNames('compare-cfd-account-container', {
-                            'compare-cfd-account-container__eu--mobile': is_eu_user,
+                            'compare-cfd-account-container__card-count--mobile': card_count < 2,
                         })}
                     >
                         <CFDCompareAccountsCarousel>
-                            {all_available_accounts.map(item => (
+                            {all_cfd_available_accounts.map(item => (
                                 <CFDCompareAccountsCard
                                     trading_platforms={item}
                                     key={item.market_type + item.shortcode}
@@ -133,7 +141,7 @@ const CompareCFDs = observer(() => {
                                 />
                             ))}
                             {/* Renders Deriv X data */}
-                            {all_available_accounts.length > 0 && has_dxtrade_account_available && (
+                            {all_cfd_available_accounts.length > 0 && has_dxtrade_account_available && (
                                 <CFDCompareAccountsCard
                                     trading_platforms={dxtrade_data}
                                     is_eu_user={is_eu_user}

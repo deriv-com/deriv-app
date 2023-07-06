aa037236f5ca1c154e3c61fc2788c720a1881b0d
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-07-06 13:55:15 +0800

    fix: change to state

--- packages/hooks/src/useWalletMigration.ts
+++ packages/hooks/src/useWalletMigration.ts
@@ -10,9 +10,9 @@ const useWalletMigration = () => {
 
     /** Fetch the wallet_migration API and refetch it every second if the status is in_progress */
     const { data } = useFetch('wallet_migration', {
-        payload: { wallet_migration: 'status' },
+        payload: { wallet_migration: 'state' },
         options: {
-            refetchInterval: response => (response?.wallet_migration?.status === 'in_progress' ? 500 : false),
+            refetchInterval: response => (response?.wallet_migration?.state === 'in_progress' ? 500 : false),
         },
     });
 
@@ -20,21 +20,21 @@ const useWalletMigration = () => {
 
     const reset_migration = useCallback(() => mutate({ payload: { wallet_migration: 'reset' } }), [mutate]);
 
-    const status = data?.wallet_migration?.status;
+    const state = data?.wallet_migration?.state;
 
     return {
         /** The status of the wallet_migration API */
-        status,
+        state,
         /** A boolean to check if the status is not_eligible */
-        is_ineligible: status === 'ineligible',
+        is_ineligible: state === 'ineligible',
         /** A boolean to check if the status is eligible */
-        is_eligible: status === 'eligible',
+        is_eligible: state === 'eligible',
         /** A boolean to check if the status is in_progress */
-        is_in_progress: status === 'in_progress',
+        is_in_progress: state === 'in_progress',
         /** A boolean to check if the status is completed */
-        is_migrated: status === 'migrated',
+        is_migrated: state === 'migrated',
         /** A boolean to check if the status is failed */
-        is_failed: status === 'failed',
+        is_failed: state === 'failed',
         /** Sends a request to wallet_migration API to start the migration process */
         start_migration,
         /** Sends a request to wallet_migration API to reset the migration process */

a16e6ae76df2abe3139b8b3803ab57e04d548c46
Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>
2023-07-07 13:08:08 +0800

    Revert "Adrienne / integrated useSubscription hook for website status (#8702)"
    This reverts commit 4f7c07922350209626ad84a92614edd86c7cdefb.

--- packages/stores/src/stores/index.ts
+++ packages/stores/src/stores/index.ts
@@ -1,3 +1,2 @@
 export { default as ExchangeRatesStore } from './ExchangeRatesStore';
 export { default as FeatureFlagsStore } from './FeatureFlagsStore';
-export { default as WebsiteStatusStore } from './WebsiteStatusStore';

003272e09c20e78349e58ada8b26a0a8e1359f82
Aum Bhatt <aum@deriv.com>
2023-07-12 11:50:09 +0400

    chore: removed iframe-store deps from deposit-store.spec.tsx

--- packages/cashier/src/stores/__tests__/deposit-store.spec.ts
+++ packages/cashier/src/stores/__tests__/deposit-store.spec.ts
@@ -45,65 +45,32 @@ describe('DepositStore', () => {
         deposit_store = new DepositStore(WS as TWebSocket, root_store as TRootStore);
     });
 
-    it('should mount deposit properly', async () => {
-        const { checkIframeLoaded, setIframeUrl, setSessionTimeout, setTimeoutCashierUrl } =
-            deposit_store.root_store.modules.cashier.iframe;
-        const { updateAccountStatus } = deposit_store.root_store.client;
-
-        await deposit_store.onMountDeposit();
-        expect(checkIframeLoaded).toHaveBeenCalled();
-
-        deposit_store.root_store.modules.cashier.iframe.is_session_timeout = true;
-
-        await deposit_store.onMountDeposit();
-        expect(checkIframeLoaded).toHaveBeenCalled();
-        expect(setIframeUrl).toHaveBeenCalledWith('https://cashier.deriv.com');
-        expect(setSessionTimeout).toHaveBeenCalledWith(false);
-        expect(setTimeoutCashierUrl).toHaveBeenCalled();
-        expect(updateAccountStatus).toHaveBeenCalled();
-    });
-
     it('should handle the error on deposit', async () => {
-        const { setSessionTimeout, clearTimeoutCashierUrl } = deposit_store.root_store.modules.cashier.iframe;
         const error_message = 'Sorry, an error occured.';
         const spyHandleCashierError = jest.spyOn(deposit_store.error, 'handleCashierError');
 
-        deposit_store.root_store.modules.cashier.iframe.is_session_timeout = true;
         (deposit_store.WS.authorized.cashier as jest.Mock).mockResolvedValueOnce({ error: { message: error_message } });
 
         await deposit_store.onMountDeposit();
         expect(spyHandleCashierError).toHaveBeenCalledWith({ message: error_message });
-        expect(setSessionTimeout).toHaveBeenCalledWith(true);
-        expect(clearTimeoutCashierUrl).toHaveBeenCalled();
     });
 
     it('should not load the iframe if client is on virtual account', async () => {
         const { setLoading } = deposit_store.root_store.modules.cashier.general_store;
-        const { checkIframeLoaded } = deposit_store.root_store.modules.cashier.iframe;
 
-        deposit_store.root_store.modules.cashier.iframe.is_session_timeout = true;
         deposit_store.root_store.client.is_virtual = true;
 
         await deposit_store.onMountDeposit();
-        expect(checkIframeLoaded).not.toHaveBeenCalled();
         expect(setLoading).toHaveBeenCalledWith(false);
     });
     it('should call cashier deposit if the active_container is deposit and not on crypto', async () => {
-        const { checkIframeLoaded } = deposit_store.root_store.modules.cashier.iframe;
-
-        deposit_store.root_store.modules.cashier.iframe.is_session_timeout = true;
-
         await deposit_store.onMountDeposit();
-        expect(checkIframeLoaded).toHaveBeenCalled();
         expect(deposit_store.WS.authorized.cashier).toHaveBeenCalled();
     });
-    it('should not call cashier deposit if the active_container is not deposit and on crypto', async () => {
-        const { checkIframeLoaded } = deposit_store.root_store.modules.cashier.iframe;
+    fit('should not call cashier deposit if the active_container is not deposit and on crypto', async () => {
         deposit_store.root_store.modules.cashier.general_store.active_container = 'withdraw';
-        deposit_store.root_store.modules.cashier.iframe.is_session_timeout = true;
 
         await deposit_store.onMountDeposit();
-        expect(checkIframeLoaded).not.toHaveBeenCalled();
         expect(deposit_store.WS.authorized.cashier).not.toHaveBeenCalled();
     });
 });

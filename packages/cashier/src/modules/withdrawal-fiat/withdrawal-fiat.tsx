c4c15208598c2aec215a0a735974f151cdbe505d
Aum Bhatt <aum@deriv.com>
2023-07-12 13:56:22 +0400

    fix: fixed withdrawal-fiat-module width after updating master

--- packages/cashier/src/modules/withdrawal-fiat/withdrawal-fiat.tsx
+++ packages/cashier/src/modules/withdrawal-fiat/withdrawal-fiat.tsx
@@ -3,7 +3,7 @@ import { PageContainer } from '../../components/page-container';
 import { WithdrawalFiatIframe } from './components';
 
 const WithdrawalFiat: React.FC = () => (
-    <div data-testid='dt_withdrawal_fiat_iframe_module'>
+    <div style={{ width: '100%' }} data-testid='dt_withdrawal_fiat_iframe_module'>
         <PageContainer hide_breadcrumb={true}>
             <WithdrawalFiatIframe />
         </PageContainer>

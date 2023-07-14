c2c9e14837c2bb11f0259cc5c619db398195c5e7
Aum Bhatt <aum@deriv.com>
2023-07-14 15:57:00 +0400

    chore: some changes to statuses

--- packages/hooks/src/useAccountStatus.ts
+++ packages/hooks/src/useAccountStatus.ts
@@ -9,23 +9,28 @@ const useAccountStatus = () => {
 
     const statuses = {
         status: {
-            is_withdrawal_locked: get_account_status?.status?.includes('withdrawal_locked'),
+            is_cashier_locked: get_account_status?.status?.includes('cashier_locked') || false,
+            is_withdrawal_locked: get_account_status?.status?.includes('withdrawal_locked') || false,
         },
         cashier_validation: {
-            is_cashier_locked: get_account_status?.cashier_validation?.includes('cashier_locked_status'),
-            is_withdrawal_locked: get_account_status?.cashier_validation?.includes('withdrawal_locked_status'),
-            is_system_maintenance: get_account_status?.cashier_validation?.includes('system_maintenance'),
+            is_cashier_locked_status:
+                get_account_status?.cashier_validation?.includes('cashier_locked_status') || false,
+            is_withdrawal_locked_status:
+                get_account_status?.cashier_validation?.includes('withdrawal_locked_status') || false,
+            is_system_maintenance: get_account_status?.cashier_validation?.includes('system_maintenance') || false,
         },
         document: {
-            has_poi_submitted: get_account_status?.authentication?.identity?.status !== 'none',
-            has_poa_submitted: get_account_status?.authentication?.document?.status !== 'none',
+            has_poi_submitted: get_account_status?.authentication?.identity?.status !== 'none' || false,
+            has_poa_submitted: get_account_status?.authentication?.document?.status !== 'none' || false,
         },
         needs_verification: {
-            is_poi_needed: is_10K_limit && get_account_status?.authentication?.identity?.status !== 'verified',
+            is_poi_needed:
+                (is_10K_limit && get_account_status?.authentication?.identity?.status !== 'verified') || false,
             is_poa_needed:
-                is_10K_limit &&
-                (get_account_status?.authentication?.needs_verification?.includes('document') ||
-                    get_account_status?.authentication?.document?.status !== 'verified'),
+                (is_10K_limit &&
+                    (get_account_status?.authentication?.needs_verification?.includes('document') ||
+                        get_account_status?.authentication?.document?.status !== 'verified')) ||
+                false,
         },
     };
 

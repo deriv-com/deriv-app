11325394a5e0753fc2cafa20ad83d5da5bf0feea
george-usynin-binary <heorhi@binary.com>
2023-07-14 16:01:13 +0300

    test: fix tests

--- packages/components/src/components/message-list/__tests__/message-list.spec.tsx
+++ packages/components/src/components/message-list/__tests__/message-list.spec.tsx
@@ -5,9 +5,9 @@ import MessageList from '../message-list';
 describe('MessageList', () => {
     it('Should render the list of messages', () => {
         const list: React.ComponentProps<typeof MessageList>['list'] = [
-            { variant: 'base', id: '1', message: 'Success message', type: 'success' },
-            { variant: 'base', id: '2', message: 'Error message', type: 'error' },
-            { variant: 'base', id: '3', message: 'Info message', type: 'info' },
+            { variant: 'base', key: '1', message: 'Success message', type: 'success' },
+            { variant: 'base', key: '2', message: 'Error message', type: 'error' },
+            { variant: 'base', key: '3', message: 'Info message', type: 'info' },
         ];
 
         render(<MessageList list={list} />);

5b30e4e876e302a4668b855a512ff9e4497b73d6
Aum Bhatt <aum@deriv.com>
2023-07-14 11:56:39 +0400

    fix: fixed the useCheck10kLimit hook

--- packages/hooks/src/useCheck10kLimit.ts
+++ packages/hooks/src/useCheck10kLimit.ts
@@ -1,6 +1,7 @@
 import { useRequest } from '@deriv/api';
 import { getMinWithdrawal } from '../../shared/src/utils/currency';
 import { useStore } from '@deriv/stores';
+import { useEffect } from 'react';
 
 type TUseCheck10kLimit = {
     is_10k_withdrawal_limit_reached: boolean;
@@ -9,7 +10,8 @@ type TUseCheck10kLimit = {
 
 const useCheck10kLimit = (): TUseCheck10kLimit => {
     const { client } = useStore();
-    const { data: account_limit } = useRequest('get_limits');
+    const { data: account_limit, mutate } = useRequest('get_limits');
+    useEffect(() => mutate(), []);
     const remainder = account_limit?.get_limits?.remainder;
     const min_withdrawal = getMinWithdrawal(client.currency);
     const is_10k_withdrawal_limit_reached = !!(typeof remainder !== 'undefined' && +remainder < min_withdrawal);

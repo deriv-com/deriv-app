b1860d4470217c4032eece2b9beb9e7d135ebcb4
Aum Bhatt <aum@deriv.com>
2023-07-16 12:19:49 +0400

    chore: fixed test cases for withdrawal-locked

--- packages/cashier/src/modules/withdrawal-locked/__tests__/withdrawal-locked.spec.tsx
+++ packages/cashier/src/modules/withdrawal-locked/__tests__/withdrawal-locked.spec.tsx
@@ -1,48 +1,108 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { useAccountStatus, useIsAskFinancialRiskApprovalNeeded } from '@deriv/hooks';
 import WithdrawalLocked from '../withdrawal-locked';
-
-const mockUseWithdrawalLocked = {
-    is_poi_needed: true,
-    has_poi_submitted: true,
-    is_poa_needed: true,
-    has_poa_submitted: false,
-    is_ask_financial_risk_approval_needed: true,
-};
 
 jest.mock('@deriv/hooks', () => ({
     ...jest.requireActual('@deriv/hooks'),
-    useWithdrawalLocked: jest.fn(() => mockUseWithdrawalLocked),
+    useWithdrawalLocked: jest.fn(),
+    useAccountStatus: jest.fn(),
+    useIsAskFinancialRiskApprovalNeeded: jest.fn(),
 }));
+
+const mockUseAccountStatus = useAccountStatus as jest.MockedFunction<typeof useAccountStatus>;
+const mockUseIsAskFinancialRiskApprovalNeeded = useIsAskFinancialRiskApprovalNeeded as jest.MockedFunction<
+    typeof useIsAskFinancialRiskApprovalNeeded
+>;
 
 describe('<WithdrawalLocked />', () => {
     it('should render ask user to check verification status of POI documents ', () => {
-        mockUseWithdrawalLocked.is_poi_needed = true;
-        mockUseWithdrawalLocked.has_poi_submitted = true;
+        mockUseAccountStatus.mockReturnValue({
+            statuses: {
+                needs_verification: {
+                    is_poi_needed: true,
+                    is_poa_needed: true,
+                },
+                document: {
+                    has_poi_submitted: true,
+                    has_poa_submitted: false,
+                },
+            },
+        });
+
+        mockUseIsAskFinancialRiskApprovalNeeded.mockReturnValue({
+            is_ask_financial_risk_approval_needed: false,
+            isSuccess: true,
+        });
 
         render(<WithdrawalLocked />);
 
         expect(screen.getByText('Check proof of identity document verification status')).toBeInTheDocument();
     });
     it('should render ask user to upload of POI documents ', () => {
-        mockUseWithdrawalLocked.is_poi_needed = true;
-        mockUseWithdrawalLocked.has_poi_submitted = false;
+        mockUseAccountStatus.mockReturnValue({
+            statuses: {
+                needs_verification: {
+                    is_poi_needed: true,
+                    is_poa_needed: true,
+                },
+                document: {
+                    has_poi_submitted: false,
+                    has_poa_submitted: false,
+                },
+            },
+        });
+
+        mockUseIsAskFinancialRiskApprovalNeeded.mockReturnValue({
+            is_ask_financial_risk_approval_needed: false,
+            isSuccess: true,
+        });
 
         render(<WithdrawalLocked />);
 
         expect(screen.getByText('Upload a proof of identity to verify your identity')).toBeInTheDocument();
     });
     it('should render ask user to check verification status of POA documents ', () => {
-        mockUseWithdrawalLocked.is_poa_needed = true;
-        mockUseWithdrawalLocked.has_poa_submitted = true;
+        mockUseAccountStatus.mockReturnValue({
+            statuses: {
+                needs_verification: {
+                    is_poi_needed: false,
+                    is_poa_needed: true,
+                },
+                document: {
+                    has_poi_submitted: false,
+                    has_poa_submitted: true,
+                },
+            },
+        });
+
+        mockUseIsAskFinancialRiskApprovalNeeded.mockReturnValue({
+            is_ask_financial_risk_approval_needed: false,
+            isSuccess: true,
+        });
 
         render(<WithdrawalLocked />);
 
         expect(screen.getByText('Check proof of address document verification status')).toBeInTheDocument();
     });
     it('should render ask user to upload of POA documents ', () => {
-        mockUseWithdrawalLocked.is_poa_needed = true;
-        mockUseWithdrawalLocked.has_poa_submitted = false;
+        mockUseAccountStatus.mockReturnValue({
+            statuses: {
+                needs_verification: {
+                    is_poi_needed: false,
+                    is_poa_needed: true,
+                },
+                document: {
+                    has_poi_submitted: false,
+                    has_poa_submitted: false,
+                },
+            },
+        });
+
+        mockUseIsAskFinancialRiskApprovalNeeded.mockReturnValue({
+            is_ask_financial_risk_approval_needed: false,
+            isSuccess: true,
+        });
 
         render(<WithdrawalLocked />);
 

f4c6c4aec9c77dfa9706650025b7a7c714ce8b09
Shaheer <shaheer@deriv.com>
2023-07-17 17:01:41 +0400

    style: :art: styles and refactors modal as per figma design

--- packages/core/src/App/Containers/ResetPasswordModal/reset-password-modal.jsx
+++ packages/core/src/App/Containers/ResetPasswordModal/reset-password-modal.jsx
@@ -79,79 +79,74 @@ const ResetPasswordModal = ({
         >
             {({ handleBlur, errors, values, touched, isSubmitting, handleChange, status }) => (
                 <Dialog
+                    className='reset-password__spaced-container'
                     is_visible={is_visible}
                     disableApp={disableApp}
                     enableApp={enableApp}
                     is_loading={is_loading}
                     dismissable={status.error_msg}
                     onConfirm={() => toggleResetPasswordModal(false)}
+                    title={localize('Reset your password')}
                     has_close_icon
                     is_closed_on_cancel={false}
                 >
                     <div className='reset-password'>
                         <Form>
-                            <React.Fragment>
-                                {status.reset_complete ? (
-                                    <div className='reset-password__password-selection'>
-                                        <Text as='p' weight='bold' className='reset-password__heading'>
-                                            <Localize i18n_default_text='Your password has been changed' />
-                                        </Text>
-                                        <Text align='center' as='p' size='xxs' className='reset-password__subtext'>
-                                            <Localize i18n_default_text='We will now redirect you to the login page.' />
-                                        </Text>
-                                    </div>
-                                ) : (
-                                    <div className='reset-password__password-selection'>
-                                        <Text as='p' weight='bold' className='reset-password__heading'>
-                                            <Localize i18n_default_text='Choose a new password' />
-                                        </Text>
-                                        <fieldset className='reset-password__fieldset'>
-                                            <PasswordMeter
-                                                input={values.password}
-                                                has_error={
-                                                    !!((touched.password && errors.password) || status.error_msg)
-                                                }
-                                                custom_feedback_messages={getErrorMessages().password_warnings}
-                                            >
-                                                <PasswordInput
-                                                    autoComplete='new-password'
-                                                    className='reset-password__password-field'
-                                                    name='password'
-                                                    label={localize('Create a password')}
-                                                    onChange={handleChange}
-                                                    onBlur={handleBlur}
-                                                    error={(touched.password && errors.password) || status.error_msg}
-                                                    value={values.password}
-                                                    data-lpignore='true'
-                                                    required
-                                                />
-                                            </PasswordMeter>
-                                        </fieldset>
-                                        <Text align='center' as='p' size='xxs' className='reset-password__subtext'>
-                                            {status.error_msg ? (
-                                                <Localize
-                                                    i18n_default_text='{{error_msg}}'
-                                                    values={{ error_msg: status.error_msg }}
-                                                />
-                                            ) : (
-                                                <Localize i18n_default_text='Strong passwords contain at least 8 characters, combine uppercase and lowercase letters, numbers, and symbols.' />
-                                            )}
-                                        </Text>
-
-                                        <Button
-                                            className={classNames('reset-password__btn', {
-                                                'reset-password__btn--disabled':
-                                                    !values.password || errors.password || isSubmitting,
-                                            })}
-                                            type='submit'
-                                            is_disabled={!values.password || !!errors.password || isSubmitting}
-                                            primary
+                            {status.reset_complete ? (
+                                <div className='reset-password__password-selection'>
+                                    <Text as='p' weight='bold' className='reset-password__heading'>
+                                        <Localize i18n_default_text='Your password has been changed' />
+                                    </Text>
+                                    <Text align='center' as='p' size='xxs' className='reset-password__subtext'>
+                                        <Localize i18n_default_text='We will now redirect you to the login page.' />
+                                    </Text>
+                                </div>
+                            ) : (
+                                <div className='reset-password__password-selection'>
+                                    <fieldset className='reset-password__fieldset'>
+                                        <PasswordMeter
+                                            input={values.password}
+                                            has_error={!!((touched.password && errors.password) || status.error_msg)}
+                                            custom_feedback_messages={getErrorMessages().password_warnings}
                                         >
-                                            <Localize i18n_default_text='Reset my password' />
-                                        </Button>
-                                    </div>
-                                )}
-                            </React.Fragment>
+                                            <PasswordInput
+                                                autoComplete='new-password'
+                                                className='reset-password__password-field'
+                                                name='password'
+                                                label={localize('Create a password')}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                error={(touched.password && errors.password) || status.error_msg}
+                                                value={values.password}
+                                                data-lpignore='true'
+                                                required
+                                            />
+                                        </PasswordMeter>
+                                    </fieldset>
+                                    <Text align='center' as='p' size='xxs' className='reset-password__subtext'>
+                                        {status.error_msg ? (
+                                            <Localize
+                                                i18n_default_text='{{error_msg}}'
+                                                values={{ error_msg: status.error_msg }}
+                                            />
+                                        ) : (
+                                            <Localize i18n_default_text='Strong passwords contain at least 8 characters. combine uppercase and lowercase letters, numbers, and symbols.' />
+                                        )}
+                                    </Text>
+                                    <div className='reset-password__divider' />
+                                    <Button
+                                        className={classNames('reset-password__btn', {
+                                            'reset-password__btn--disabled':
+                                                !values.password || errors.password || isSubmitting,
+                                        })}
+                                        type='submit'
+                                        is_disabled={!values.password || !!errors.password || isSubmitting}
+                                        primary
+                                    >
+                                        <Localize i18n_default_text='Reset my password' />
+                                    </Button>
+                                </div>
+                            )}
                         </Form>
                     </div>
                 </Dialog>

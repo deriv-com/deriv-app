51454e2ad0c1bec932f41d8873ce1df64a0d1588
nada-deriv <nada.ajinas@deriv.com>
2023-07-19 13:10:23 +0400

    refactor: reuse same modal for error/create error

--- packages/p2p/src/components/modal-manager/modals/ad-create-edit-error-modal/ad-create-edit-error-modal.tsx
+++ packages/p2p/src/components/modal-manager/modals/ad-create-edit-error-modal/ad-create-edit-error-modal.tsx
@@ -3,13 +3,23 @@ import { Button, Modal, Text } from '@deriv/components';
 import { observer } from '@deriv/stores';
 import { localize } from 'Components/i18next';
 import { useModalManagerContext } from 'Components/modal-manager/modal-manager-context';
+import { ads } from 'Constants/ads';
+import { api_error_codes } from 'Constants/api-error-codes';
 import { useStores } from 'Stores';
 import { generateErrorDialogBody, generateErrorDialogTitle } from 'Utils/adverts';
 
-const CreateAdErrorModal = () => {
+type TAdCreateEditErrorModalProps = {
+    ad_type?: string;
+};
+
+const AdCreateEditErrorModal = ({ ad_type = ads.CREATE }: TAdCreateEditErrorModalProps) => {
     const { my_ads_store } = useStores();
     const { hideModal, is_modal_open } = useModalManagerContext();
-    const { api_error_message, error_code } = my_ads_store;
+    const { api_error_message, edit_ad_form_error, error_code } = my_ads_store;
+
+    const error_message = ad_type === ads.CREATE ? api_error_message : edit_ad_form_error;
+
+    const is_api_error = [api_error_codes.ADVERT_SAME_LIMITS, api_error_codes.DUPLICATE_ADVERT].includes(error_code);
 
     return (
         <Modal
@@ -21,13 +31,13 @@ const CreateAdErrorModal = () => {
         >
             <Modal.Body>
                 <Text as='p' color='prominent' size='xs'>
-                    {generateErrorDialogBody(error_code, api_error_message)}
+                    {generateErrorDialogBody(error_code, error_message)}
                 </Text>
             </Modal.Body>
             <Modal.Footer>
                 <Button
                     has_effect
-                    text={error_code ? localize('Update ad') : localize('Ok')}
+                    text={is_api_error ? localize('Update ad') : localize('Ok')}
                     onClick={hideModal}
                     primary
                     large
@@ -37,4 +47,4 @@ const CreateAdErrorModal = () => {
     );
 };
 
-export default observer(CreateAdErrorModal);
+export default observer(AdCreateEditErrorModal);
--- packages/p2p/src/components/modal-manager/modals/create-ad-error-modal/index.ts
+++
@@ -1,4 +0,0 @@
-import CreateAdErrorModal from './create-ad-error-modal';
-import './create-ad-error-modal.scss';
-
-export default CreateAdErrorModal;

96e08273e0f8dddb0fb5832b8792edf7a7808f46
Shaheer <shaheer@deriv.com>
2023-07-20 19:57:13 +0400

    test: :test_tube: adds test cases for traders hub

--- packages/appstore/src/modules/traders-hub/index.tsx
+++ packages/appstore/src/modules/traders-hub/index.tsx
@@ -64,6 +64,7 @@ const TradersHub = () => {
     const renderOrderedPlatformSections = (is_cfd_visible = true, is_options_and_multipliers_visible = true) => {
         return (
             <div
+                data-testid='dt_traders_hub'
                 className={classNames('traders-hub__main-container', {
                     'traders-hub__main-container-reversed': is_eu_user,
                 })}
@@ -111,7 +112,7 @@ const TradersHub = () => {
                 </div>
             </Div100vhContainer>
             {is_eu_low_risk && (
-                <div className='disclamer'>
+                <div data-testid='dt_traders_hub_disclaimer' className='disclamer'>
                     <Text align='left' className='disclamer-text' size={is_mobile ? 'xxxs' : 'xs'}>
                         <Localize
                             i18n_default_text='<0>EU statutory disclaimer</0>: CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. <0>73% of retail investor accounts lose money when trading CFDs with this provider</0>. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.'

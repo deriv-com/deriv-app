92ec65bc9bde98a86e4a7217b0648af01fa1bdfc
Likhith Kolayari <likhith@regentmarkets.com>
2023-07-20 13:22:31 +0400

    fix: :bug: display onfido error message

--- packages/account/src/Sections/Verification/ProofOfIdentity/onfido.jsx
+++ packages/account/src/Sections/Verification/ProofOfIdentity/onfido.jsx
@@ -3,19 +3,10 @@ import UploadComplete from 'Components/poi/status/upload-complete';
 import Expired from 'Components/poi/status/expired';
 import Verified from 'Components/poi/status/verified';
 import RejectedReasons from 'Components/poi/status/rejected-reasons';
-import Unsupported from 'Components/poi/status/unsupported';
 import { identity_status_codes } from './proof-of-identity-utils';
 
-const Onfido = ({
-    handleRequireSubmission,
-    is_from_external,
-    needs_poa,
-    onfido,
-    redirect_button,
-    manual,
-    setIsCfdPoiCompleted,
-}) => {
-    const { status, submissions_left, last_rejected: rejected_reasons } = onfido;
+const Onfido = ({ handleRequireSubmission, is_from_external, needs_poa, onfido, redirect_button }) => {
+    const { status, last_rejected: rejected_reasons } = onfido;
 
     switch (status) {
         case identity_status_codes.pending:
@@ -28,15 +19,6 @@ const Onfido = ({
             );
         case identity_status_codes.rejected:
         case identity_status_codes.suspected:
-            if (Number(submissions_left) < 1) {
-                return (
-                    <Unsupported
-                        manual={manual}
-                        is_from_external={is_from_external}
-                        setIsCfdPoiCompleted={setIsCfdPoiCompleted}
-                    />
-                );
-            }
             return (
                 <RejectedReasons
                     rejected_reasons={rejected_reasons}

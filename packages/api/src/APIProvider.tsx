55f18f771e70651350b3eace8f90058df62c237c
Hamid <hamid@regentmarkets.com>
2023-07-24 12:53:24 +0330

    fix: logout issue

--- packages/api/src/APIProvider.tsx
+++ packages/api/src/APIProvider.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 const APIProvider = ({ children }: PropsWithChildren<unknown>) => (
     <QueryClientProvider client={queryClient}>

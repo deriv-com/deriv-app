5a90305d9aef40b294d13496d25bb35b603baf0c
nada-deriv <nada.ajinas@deriv.com>
2023-07-24 13:39:54 +0400

    fix: lint fix

--- packages/p2p/src/stores/__tests__/my-ads-store.spec.js
+++ packages/p2p/src/stores/__tests__/my-ads-store.spec.js
@@ -11,44 +11,49 @@ jest.mock('Utils/websocket', () => ({
 }));
 
 describe('MyAdsStore', () => {
-    
     let my_ads_store;
     beforeEach(() => {
         const root_store = mockStore({
             general_store: {
                 is_barred: false,
                 showModal: jest.fn(),
-            }
-      })
-       my_ads_store = new MyAdsStore(root_store);
+            },
+        });
+        my_ads_store = new MyAdsStore(root_store);
     });
     it('should instantiate', () => {
         expect(my_ads_store).toBeTruthy();
     });
     it('should handle activate/deactivate of ads', async () => {
         my_ads_store.setAdverts(adverts);
-        requestWS.mockResolvedValue({ p2p_advert_update: {
+        requestWS.mockResolvedValue({
+            p2p_advert_update: {
                 ...adverts[0],
                 is_active: 0,
-            }
+            },
         });
         my_ads_store.onClickActivateDeactivate('53', true, mockFn);
         expect(requestWS).toBeCalledWith({ p2p_advert_update: 1, id: '53', is_active: 0 });
         await waitFor(() => expect(mockFn).toBeCalledWith(false));
-        await waitFor(() => expect(my_ads_store.adverts[0].is_active).toBe(0)); 
+        await waitFor(() => expect(my_ads_store.adverts[0].is_active).toBe(0));
     });
-    it('should show error popup when api throws error on activat/deactivate of ads', async() => {
+    it('should show error popup when api throws error on activat/deactivate of ads', async () => {
         my_ads_store.setAdverts(adverts);
         requestWS.mockResolvedValue({ error: { code: 'some_error', message: 'this is the error message' } });
         my_ads_store.onClickActivateDeactivate('53', true, mockFn);
         expect(requestWS).toBeCalledWith({ p2p_advert_update: 1, id: '53', is_active: 0 });
         await waitFor(() => expect(my_ads_store.error_code).toBe('some_error'));
         await waitFor(() => expect(my_ads_store.activate_deactivate_error_message).toBe('this is the error message'));
-        await waitFor(() => expect(my_ads_store.root_store.general_store.showModal).toBeCalledWith({ key: 'ErrorModal', props: {
-            has_close_icon: false,
-            error_message: 'this is the error message',
-            error_modal_title: "Something's not right",
-            width: '40rem'
-        } }));
+        await waitFor(() =>
+            expect(my_ads_store.root_store.general_store.showModal).toBeCalledWith({
+                key: 'ErrorModal',
+                props: {
+                    has_close_icon: false,
+                    error_message: 'this is the error message',
+                    error_modal_title: "Something's not right",
+                    width: '40rem',
+                },
+            })
+        );
     });
 });

bd52d8c4e3a94e4c248a769869f2edb55abeb9e3
Hirad <hirad@regentmarkets.com>
2023-07-24 14:44:16 +0800

    chore: migrated cfd-shared-strings to TS

--- packages/cfd/src/Constants/cfd-shared-strings.ts
+++ packages/cfd/src/Constants/cfd-shared-strings.ts
@@ -1,8 +1,9 @@
 import { CFD_PLATFORMS } from '@deriv/shared';
 import { localize } from '@deriv/translations';
+import { TCFDPlatform } from 'Components/props.types';
 
 export const general_messages = {
-    getWelcomeHeader: (is_logged_in, platform) => {
+    getWelcomeHeader: (is_logged_in: boolean, platform: TCFDPlatform) => {
         if (platform === CFD_PLATFORMS.DXTRADE) {
             if (is_logged_in) return localize('Welcome to your Deriv X dashboard');
             return localize('Welcome to Deriv X');
@@ -11,14 +12,14 @@ export const general_messages = {
         }
         return localize('');
     },
-    getDownloadHeader: platform => {
+    getDownloadHeader: (platform: TCFDPlatform) => {
         if (platform === CFD_PLATFORMS.DXTRADE)
             return localize('Run Deriv X on your browser or download the mobile app');
         else if (platform === CFD_PLATFORMS.MT5)
             return localize('Run MT5 from your browser or download the MT5 app for your devices');
         return '';
     },
-    getFinancialAccountDescriptor: (platform, show_eu_related_content) => {
+    getFinancialAccountDescriptor: (platform: TCFDPlatform, show_eu_related_content: boolean) => {
         if (platform === CFD_PLATFORMS.DXTRADE) {
             return localize('Trade forex, basket indices, commodities, and cryptocurrencies with high leverage.');
         } else if (platform === CFD_PLATFORMS.MT5) {

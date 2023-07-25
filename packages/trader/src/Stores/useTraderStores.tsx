fd8882c07154e3b5ce46c2adededd45606b3fb44
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2023-07-25 10:33:55 +0300

    Maryia/webrel-483/TS migration of ContractType function (#17)
    * chore: start ts migration
    
    * chore: continue ts migration of contract-types
    
    * chore: add more types to contract-types
    
    * chore: add type for getContractValues return value
    
    * chore: improve reduce types

--- packages/trader/src/Stores/useTraderStores.tsx
+++ packages/trader/src/Stores/useTraderStores.tsx
@@ -63,10 +63,12 @@ type TOverrideTradeStore = Omit<
     | 'market_close_times'
     | 'market_open_times'
     | 'multiplier_range_list'
+    | 'multiplier'
     | 'sessions'
     | 'setIsTradeParamsExpanded'
     | 'start_dates_list'
     | 'start_time'
+    | 'symbol'
     | 'take_profit'
     | 'proposal_info'
     | 'trade_types'
@@ -89,6 +91,7 @@ type TOverrideTradeStore = Omit<
     form_components: string[];
     market_open_times: string[];
     market_close_times: string[];
+    multiplier: number;
     multiplier_range_list: number[];
     proposal_info: {
         [key: string]: {
@@ -109,6 +112,7 @@ type TOverrideTradeStore = Omit<
     setIsTradeParamsExpanded: (value: boolean) => void;
     start_dates_list: Array<{ text: string; value: number }>;
     start_time: string | null;
+    symbol: string;
     take_profit?: string;
     ticks_history_stats: {
         ticks_stayed_in?: number[];

1a3a8ca32c159ee6bd5b288f62c7d0b223abf13e
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2023-07-25 10:32:24 +0300

    maryia/WEBREL-323/feat: ts migration of base-store (#18)
    * chore: start ts migration of base-store
    
    * chore: add more types to BaseStore
    
    * chore: added more types to base-store
    
    * chore: added more types to base-store
    
    * chore: finalize base-store ts migration
    
    * chore: reorder imports

--- packages/stores/src/mockStore.ts
+++ packages/stores/src/mockStore.ts
@@ -443,6 +443,11 @@ const mock = (): TStores & { is_mock: boolean } => {
             update: jest.fn(),
             unmount: jest.fn(),
         },
+        gtm: {},
+        pushwoosh: {},
+        contract_replay: {},
+        chart_barrier_store: {},
+        active_symbols: {},
     };
 };
 

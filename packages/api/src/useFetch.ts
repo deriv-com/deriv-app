8c1eebfca4efc308c07307cae3571cb6a659e543
george-usynin-binary <heorhi@binary.com>
2023-07-25 14:26:20 +0300

    perf: add enpoints that need authorize

--- packages/api/src/useFetch.ts
+++ packages/api/src/useFetch.ts
@@ -9,7 +9,33 @@ import type {
     TSocketResponseData,
 } from '../types';
 
-const AUTH_REQUIRED_ENDPOINTS = ['balance', 'get_settings'];
+const AUTH_REQUIRED_ENDPOINTS = [
+    'account_security',
+    'balance',
+    'get_account_status',
+    'get_account_types',
+    'get_available_accounts_to_transfer',
+    'get_financial_assessment',
+    'get_limits',
+    'get_self_exclusion',
+    'get_settings',
+    'mt5_login_list',
+    'new_account_real',
+    'new_account_virtual',
+    'new_account_wallet',
+    'p2p_order_list',
+    'payout_currencies',
+    'portfolio',
+    'proposal_open_contract',
+    'statement',
+    'topup_virtual',
+    'trading_platform_accounts',
+    'trading_platform_available_accounts',
+    'trading_servers',
+    'transaction',
+    'transfer_between_accounts',
+    'wallet_migration',
+];
 
 const useFetch = <T extends TSocketEndpointNames>(name: T, ...props: TSocketAcceptableProps<T, true>) => {
     const prop = props?.[0];

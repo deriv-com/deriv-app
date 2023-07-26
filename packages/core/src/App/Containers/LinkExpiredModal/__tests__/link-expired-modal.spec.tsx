47864fd95e614a9557a9b914d95218e4ac29cc9e
Shaheer <shaheer@deriv.com>
2023-07-26 12:52:38 +0400

    test: :test_tube: isolates test cases and removes common render

--- packages/core/src/App/Containers/LinkExpiredModal/__tests__/link-expired-modal.spec.tsx
+++ packages/core/src/App/Containers/LinkExpiredModal/__tests__/link-expired-modal.spec.tsx
@@ -15,38 +15,60 @@ jest.mock('@deriv/api', () => ({
 const mockUseRequest = useRequest as jest.MockedFunction<typeof useRequest<'verify_email'>>;
 
 describe('LinkExpiredModal', () => {
-    let mock: TStores;
-    const renderModal = (mock_store_override = {}) => {
+    it('should render the component when is_link_expired_modal_visible is true', () => {
         // @ts-expect-error ignore this until find a way to make arguments as partial
         mockUseRequest.mockReturnValue({ isLoading: false });
-        mock = mockStore(mock_store_override);
-        return render(<LinkExpiredModal />, {
+        const mock = mockStore({ ui: { is_link_expired_modal_visible: true } });
+        render(<LinkExpiredModal />, {
             wrapper: ({ children }) => (
                 <APIProvider>
                     <StoreProvider store={mock}>{children}</StoreProvider>
                 </APIProvider>
             ),
         });
-    };
-
-    it('should render the component when is_link_expired_modal_visible is true', () => {
-        renderModal({ ui: { is_link_expired_modal_visible: true } });
         expect(screen.queryByText('Link Expired')).toBeInTheDocument();
     });
 
     it('should not render the component when is_link_expired_modal_visible is false', () => {
-        renderModal();
+        // @ts-expect-error ignore this until find a way to make arguments as partial
+        mockUseRequest.mockReturnValue({ isLoading: false });
+        const mock = mockStore({});
+        render(<LinkExpiredModal />, {
+            wrapper: ({ children }) => (
+                <APIProvider>
+                    <StoreProvider store={mock}>{children}</StoreProvider>
+                </APIProvider>
+            ),
+        });
         expect(screen.queryByText('Link Expired')).not.toBeInTheDocument();
     });
 
     it('should not render the component close and resend email buttons', () => {
-        renderModal({ ui: { is_link_expired_modal_visible: true } });
+        // @ts-expect-error ignore this until find a way to make arguments as partial
+        mockUseRequest.mockReturnValue({ isLoading: false });
+        const mock = mockStore({ ui: { is_link_expired_modal_visible: true } });
+        render(<LinkExpiredModal />, {
+            wrapper: ({ children }) => (
+                <APIProvider>
+                    <StoreProvider store={mock}>{children}</StoreProvider>
+                </APIProvider>
+            ),
+        });
         expect(screen.getByRole('button', { name: /Close/ })).toBeInTheDocument();
         expect(screen.getByRole('button', { name: /Resend email/ })).toBeInTheDocument();
     });
 
     it('should keep the resend mail button disabled until a valid email is entered', () => {
-        renderModal({ ui: { is_link_expired_modal_visible: true } });
+        // @ts-expect-error ignore this until find a way to make arguments as partial
+        mockUseRequest.mockReturnValue({ isLoading: false });
+        const mock = mockStore({ ui: { is_link_expired_modal_visible: true } });
+        render(<LinkExpiredModal />, {
+            wrapper: ({ children }) => (
+                <APIProvider>
+                    <StoreProvider store={mock}>{children}</StoreProvider>
+                </APIProvider>
+            ),
+        });
         const resend_button = screen.getByRole('button', { name: /Resend email/ });
         expect(resend_button).toBeDisabled();
         fireEvent.change(screen.getByRole('textbox', { name: /Email address/i }), {
@@ -56,7 +78,16 @@ describe('LinkExpiredModal', () => {
     });
 
     it('should close the modal on clicking close button', async () => {
-        renderModal({ ui: { is_link_expired_modal_visible: true } });
+        // @ts-expect-error ignore this until find a way to make arguments as partial
+        mockUseRequest.mockReturnValue({ isLoading: false });
+        const mock = mockStore({ ui: { is_link_expired_modal_visible: true } });
+        render(<LinkExpiredModal />, {
+            wrapper: ({ children }) => (
+                <APIProvider>
+                    <StoreProvider store={mock}>{children}</StoreProvider>
+                </APIProvider>
+            ),
+        });
         const close_button = screen.getByRole('button', { name: /Close/ });
         userEvent.click(close_button);
         expect(mock.ui.toggleLinkExpiredModal).toBeCalledTimes(1);
@@ -64,7 +95,16 @@ describe('LinkExpiredModal', () => {
     });
 
     it('should call the verify email websocket API on clicking resend mail button', () => {
-        renderModal({ ui: { is_link_expired_modal_visible: true } });
+        // @ts-expect-error ignore this until find a way to make arguments as partial
+        mockUseRequest.mockReturnValue({ isLoading: false });
+        const mock = mockStore({ ui: { is_link_expired_modal_visible: true } });
+        render(<LinkExpiredModal />, {
+            wrapper: ({ children }) => (
+                <APIProvider>
+                    <StoreProvider store={mock}>{children}</StoreProvider>
+                </APIProvider>
+            ),
+        });
         const resend_button = screen.getByRole('button', { name: /Resend email/ });
         fireEvent.change(screen.getByRole('textbox', { name: /Email address/i }), {
             target: { value: 'test.mail@domain.com' },

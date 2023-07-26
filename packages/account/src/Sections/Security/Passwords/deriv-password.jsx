30dd3a39d9412f13149715712086ee7dd79348d4
“Matin-deriv” <matin@deriv.com>
2023-07-26 10:16:53 +0800

    chore: fix Oneall social unlinking from Apple accounts

--- packages/account/src/Sections/Security/Passwords/deriv-password.jsx
+++ packages/account/src/Sections/Security/Passwords/deriv-password.jsx
@@ -70,11 +70,7 @@ const DerivPassword = ({
     const [is_sent_email_modal_open, setIsSentEmailModalOpen] = React.useState(false);
 
     const onClickSendEmail = () => {
-        if (social_identity_provider === 'apple') {
-            WS.verifyEmail(email, 'request_email');
-        } else {
-            WS.verifyEmail(email, 'reset_password');
-        }
+        WS.verifyEmail(email, 'reset_password');
         setIsSentEmailModalOpen(true);
     };
 

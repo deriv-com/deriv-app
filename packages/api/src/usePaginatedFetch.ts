8d82d1671f8787a3b38c3f1d436871ff3d3bc000
Rostik Kayko <rostislav@deriv.com>
2023-07-26 16:03:24 +0300

    refactor: `keepPreviousData: !!offset`

--- packages/api/src/usePaginatedFetch.ts
+++ packages/api/src/usePaginatedFetch.ts
@@ -26,7 +26,7 @@ const usePaginatedFetch = <T extends TSocketPaginateableEndpointNames>(
     // exact type of the payload is not determined at this point.
     const { remove, ...rest } = useFetch(name, {
         payload: { ...payload, offset, limit },
-        options: { ...options, keepPreviousData: true },
+        options: { ...options, keepPreviousData: !!offset },
     });
 
     const loadMore = useCallback(() => setOffset(prev => prev + limit), [limit]);

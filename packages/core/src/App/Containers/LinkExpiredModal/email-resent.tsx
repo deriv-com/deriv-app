73b9a1fb3cac6cefefd2597dd298c90336d295a4
Shaheer <shaheer@deriv.com>
2023-07-26 16:18:50 +0400

    test: :test_tube: adds test for email resent component

--- packages/core/src/App/Containers/LinkExpiredModal/email-resent.tsx
+++ packages/core/src/App/Containers/LinkExpiredModal/email-resent.tsx
@@ -5,7 +5,7 @@ import { Localize } from '@deriv/translations';
 const EmailResent = () => {
     return (
         <div className='link-expired__spaced-container__content link-expired__spaced-container__email_sent'>
-            <Icon icon='IcEmailVerificationResent' size={128} />
+            <Icon data_testid='dt_email-resent' icon='IcEmailVerificationResent' size={128} />
             <Text as='p' size='s' align='center'>
                 <Localize i18n_default_text="<0>We've sent you an email.</0>" components={[<strong key={0} />]} />
             </Text>

363bd3372107471a8a01e7a5a1730308cc060834
ameerul hady <ameerul@deriv.com>
2023-07-31 14:57:12 +0800

    chore: refactored share-my-ads-socials

--- packages/p2p/src/components/modal-manager/modals/share-my-ads-modal/share-my-ads-socials/share-my-ads-socials.tsx
+++ packages/p2p/src/components/modal-manager/modals/share-my-ads-modal/share-my-ads-socials/share-my-ads-socials.tsx
@@ -2,60 +2,68 @@ import React from 'react';
 import { FacebookShareButton, TelegramShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
 import { Icon, Text } from '@deriv/components';
 import { Localize } from 'Components/i18next';
+import classNames from 'classnames';
 
 const ShareMyAdsIcons = () => {
-    // TODO: replace with proper url when available
+    // TODO: replace with proper url and message when available
     const advert_url = window.location.href;
+    const custom_message = 'This is my advert!';
+
+    const share_buttons = [
+        {
+            ShareButton: WhatsappShareButton,
+            icon: 'IcWhatsappFilled',
+            messagePropName: 'title',
+            size: 34,
+            text: 'WhatsApp',
+        },
+        { ShareButton: FacebookShareButton, icon: 'IcFacebook', messagePropName: 'quote', size: 34, text: 'Facebook' },
+        { ShareButton: TelegramShareButton, icon: 'IcTelegram', messagePropName: 'title', size: 34, text: 'Telegram' },
+        {
+            ShareButton: TwitterShareButton,
+            icon: 'IcStockTwitter',
+            messagePropName: 'quote',
+            size: 28,
+            small_icon: true,
+            text: 'Twitter',
+        },
+        {
+            ShareButton: 'a',
+            href: `https://mail.google.com/mail/?view=cm&fs=1&body=${advert_url}`,
+            icon: 'IcStockGoogle',
+            rel: 'noreferrer',
+            size: 28,
+            small_icon: true,
+            target: '_blank',
+            text: 'Gmail',
+        },
+    ];
 
     return (
         <div className='share-my-ads-icons'>
-            <WhatsappShareButton url={advert_url}>
-                <div className='share-my-ads-icons__circle'>
-                    <Icon className='share-my-ads-icons__circle-icon' icon='IcWhatsappFilled' size={34} />
-                </div>
-                <Text size='xxxs'>
-                    <Localize i18n_default_text='WhatsApp' />
-                </Text>
-            </WhatsappShareButton>
-            <FacebookShareButton url={advert_url}>
-                <div className='share-my-ads-icons__circle'>
-                    <Icon className='share-my-ads-icons__circle-icon' icon='IcFacebook' size={34} />
-                </div>
-                <Text size='xxxs'>
-                    <Localize i18n_default_text='Facebook' />
-                </Text>
-            </FacebookShareButton>
-            <TelegramShareButton url={advert_url}>
-                <div className='share-my-ads-icons__circle'>
-                    <Icon className='share-my-ads-icons__circle-icon' icon='IcTelegram' size={34} />
-                </div>
-                <Text size='xxxs'>
-                    <Localize i18n_default_text='Telegram' />
-                </Text>
-            </TelegramShareButton>
-            <TwitterShareButton url={advert_url}>
-                <div className='share-my-ads-icons__circle'>
-                    <Icon className='share-my-ads-icons__circle-icon--small' icon='IcStockTwitter' size={28} />
-                </div>
-                <Text size='xxxs'>
-                    <Localize i18n_default_text='Twitter' />
-                </Text>
-            </TwitterShareButton>
-            <div className='share-my-ads-icons__container'>
-                <a
-                    className='share-my-ads-icons__container-link'
-                    href={`https://mail.google.com/mail/?view=cm&fs=1&body=${advert_url}`}
-                    target='_blank'
-                    rel='noreferrer'
+            {share_buttons.map(({ ShareButton, href, icon, messagePropName, rel, size, small_icon, target, text }) => (
+                <ShareButton
+                    key={text}
+                    url={advert_url}
+                    {...(messagePropName && { [messagePropName]: custom_message })}
+                    {...(href && { href })}
+                    {...(target && { target: '_blank' })}
+                    {...(rel && { rel: 'noreferrer' })}
                 >
                     <div className='share-my-ads-icons__circle'>
-                        <Icon className='share-my-ads-icons__circle-icon--small' icon='IcStockGoogle' size={28} />
+                        <Icon
+                            className={classNames('share-my-ads-icons__circle-icon', {
+                                'share-my-ads-icons__circle-icon--small': small_icon,
+                            })}
+                            icon={icon}
+                            size={size}
+                        />
                     </div>
-                </a>
-                <Text size='xxxs'>
-                    <Localize i18n_default_text='Gmail' />
-                </Text>
-            </div>
+                    <Text size='xxxs'>
+                        <Localize i18n_default_text={text} />
+                    </Text>
+                </ShareButton>
+            ))}
         </div>
     );
 };

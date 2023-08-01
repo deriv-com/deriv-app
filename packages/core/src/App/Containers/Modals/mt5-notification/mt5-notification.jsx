a46556b6a2d9d1899f3eacccedd32cff62b4790b
Muhammad Hamza <120543468+hamza-deriv@users.noreply.github.com>
2023-08-01 23:09:40 +0800

    notifcation helper popup (#9526)
    * chore: notifcation helper popup
    
    * chore: move the component to core package
    
    * chore: remove package from appstore
    
    * chore: added back setIsOnboarding
    
    * chore: updates
    
    * chore: size of font
    
    * chore: number not visible in dark mode
    
    * chore: number not visible in safari
    
    * chore: number not visible in safari color

---
+++ packages/core/src/App/Containers/Modals/mt5-notification/mt5-notification.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { connect } from 'Stores/connect';
+import PropTypes from 'prop-types';
+import { Modal, DesktopWrapper, MobileDialog, MobileWrapper, UILoader, Button } from '@deriv/components';
+import { localize } from '@deriv/translations';
+import MT5NotificationDescription from './mt5-notification-description';
+
+const MT5Notification = ({ setMT5NotificationModal, is_mt5_notificaiton_modal_visible, disableApp, enableApp }) => {
+    const onclickFunc = () => {
+        if (is_mt5_notificaiton_modal_visible) {
+            setMT5NotificationModal(false);
+        } else {
+            setMT5NotificationModal(true);
+        }
+    };
+
+    return (
+        <React.Suspense fallback={<UILoader />}>
+            <DesktopWrapper>
+                <Modal
+                    disableApp={disableApp}
+                    enableApp={enableApp}
+                    has_close_icon={false}
+                    is_open={is_mt5_notificaiton_modal_visible}
+                    title={localize('Trouble accessing Deriv MT5 on your mobile?')}
+                    toggleModal={onclickFunc}
+                    className='mt5-notification-modal'
+                    height='455px'
+                    width='510px'
+                >
+                    <Modal.Body has_separator className='mt5-notification-modal-body'>
+                        <MT5NotificationDescription />
+                    </Modal.Body>
+                    <Modal.Footer has_separator>
+                        <Button
+                            primary
+                            onClick={() => {
+                                setMT5NotificationModal(false);
+                            }}
+                        >
+                            {localize('OK')}
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </DesktopWrapper>
+            <MobileWrapper>
+                <MobileDialog
+                    portal_element_id='modal_root'
+                    wrapper_classname='mt5-notification-modal-mobile'
+                    title={localize('Trouble accessing Deriv MT5 on your mobile?')}
+                    visible={is_mt5_notificaiton_modal_visible}
+                    onClose={() => {
+                        setMT5NotificationModal(false);
+                    }}
+                    has_full_height
+                    has_close_icon={false}
+                    footer={
+                        <div className='mt5-notification-modal-mobile--footer'>
+                            <Button
+                                className='mt5-notification-modal-mobile--button'
+                                primary
+                                onClick={() => setMT5NotificationModal(false)}
+                            >
+                                {localize('OK')}
+                            </Button>
+                        </div>
+                    }
+                >
+                    <MT5NotificationDescription />
+                </MobileDialog>
+            </MobileWrapper>
+        </React.Suspense>
+    );
+};
+
+MT5Notification.propTypes = {
+    disableApp: PropTypes.func,
+    enableApp: PropTypes.func,
+    setMT5NotificationModal: PropTypes.func,
+    is_mt5_notificaiton_modal_visible: PropTypes.bool,
+};
+
+export default connect(({ traders_hub, ui }) => ({
+    setMT5NotificationModal: traders_hub.setMT5NotificationModal,
+    is_mt5_notificaiton_modal_visible: traders_hub.is_mt5_notificaiton_modal_visible,
+    disableApp: ui.disableApp,
+    enableApp: ui.enableApp,
+}))(MT5Notification);

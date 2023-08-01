09c09087c4625f62782768724f5462696f3e8a01
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-08-01 10:27:01 +0800

    fix: remove isSuccess

--- packages/hooks/src/useExistingCFDAccounts.ts
+++ packages/hooks/src/useExistingCFDAccounts.ts
@@ -58,7 +58,6 @@ const useExistingCFDAccounts = () => {
         data,
         ...{ ...mt5_rest, ...dxtrade_rest, ...derivez_rest },
         isLoading: mt5_rest.isLoading || dxtrade_rest.isLoading || derivez_rest.isLoading,
-        isSuccess: mt5_rest.isSuccess && dxtrade_rest.isSuccess && derivez_rest.isSuccess,
     };
 };
 

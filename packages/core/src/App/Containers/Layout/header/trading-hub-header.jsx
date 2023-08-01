21dae0071806488dbb3bd5b1d10efd9e791b738e
Shaheer <shaheer@deriv.com>
2023-08-01 16:23:57 +0400

    refactor: :memo: moves nested component out to prevent recreation of component

--- packages/core/src/App/Containers/Layout/header/trading-hub-header.jsx
+++ packages/core/src/App/Containers/Layout/header/trading-hub-header.jsx
@@ -84,6 +84,55 @@ const ShowNotifications = ({ is_notifications_visible, notifications_count, togg
     );
 };
 
+const DefaultMobileLinks = ({
+    is_dark_mode,
+    toggleIsTourOpen,
+    is_mf,
+    is_eu,
+    is_eu_country,
+    setIsOnboardingVisited,
+    is_notifications_visible,
+    notifications_count,
+    toggleNotifications,
+    handleClickCashier,
+}) => (
+    <React.Fragment>
+        <div className='trading-hub-header__menu-right--items--onboarding'>
+            <TradingHubOnboarding
+                is_dark_mode={is_dark_mode}
+                toggleIsTourOpen={toggleIsTourOpen}
+                is_mf={is_mf}
+                is_eu={is_eu}
+                is_eu_country={is_eu_country}
+                setIsOnboardingVisited={setIsOnboardingVisited}
+            />
+        </div>
+        <div className='trading-hub-header__menu-right--items--notifications'>
+            <ShowNotifications
+                is_notifications_visible={is_notifications_visible}
+                notifications_count={notifications_count}
+                toggleNotifications={toggleNotifications}
+            />
+        </div>
+        <Popover
+            classNameBubble='account-settings-toggle__tooltip'
+            alignment='bottom'
+            message={<Localize i18n_default_text='Manage account settings' />}
+            should_disable_pointer_events
+            zIndex={9999}
+        >
+            <BinaryLink className='trading-hub-header__setting' to={routes.personal_details}>
+                <Icon icon='IcUserOutline' size={20} />
+            </BinaryLink>
+        </Popover>
+        <div className='trading-hub-header__cashier-button'>
+            <Button primary small onClick={handleClickCashier}>
+                <Localize i18n_default_text='Cashier' />
+            </Button>
+        </div>
+    </React.Fragment>
+);
+
 const TradingHubHeader = ({
     header_extension,
     is_app_disabled,
@@ -173,44 +222,6 @@ const TradingHubHeader = ({
         ]
     );
 
-    const DefaultMobileLinks = () => (
-        <React.Fragment>
-            <div className='trading-hub-header__menu-right--items--onboarding'>
-                <TradingHubOnboarding
-                    is_dark_mode={is_dark_mode}
-                    toggleIsTourOpen={toggleIsTourOpen}
-                    is_mf={is_mf}
-                    is_eu={is_eu}
-                    is_eu_country={is_eu_country}
-                    setIsOnboardingVisited={setIsOnboardingVisited}
-                />
-            </div>
-            <div className='trading-hub-header__menu-right--items--notifications'>
-                <ShowNotifications
-                    is_notifications_visible={is_notifications_visible}
-                    notifications_count={notifications_count}
-                    toggleNotifications={toggleNotifications}
-                />
-            </div>
-            <Popover
-                classNameBubble='account-settings-toggle__tooltip'
-                alignment='bottom'
-                message={<Localize i18n_default_text='Manage account settings' />}
-                should_disable_pointer_events
-                zIndex={9999}
-            >
-                <BinaryLink className='trading-hub-header__setting' to={routes.personal_details}>
-                    <Icon icon='IcUserOutline' size={20} />
-                </BinaryLink>
-            </Popover>
-            <div className='trading-hub-header__cashier-button'>
-                <Button primary small onClick={handleClickCashier}>
-                    <Localize i18n_default_text='Cashier' />
-                </Button>
-            </div>
-        </React.Fragment>
-    );
-
     return (
         <header
             className={classNames('trading-hub-header', {
@@ -293,7 +304,18 @@ const TradingHubHeader = ({
                                 </div>
                             </React.Fragment>
                         ) : (
-                            <DefaultMobileLinks />
+                            <DefaultMobileLinks
+                                is_dark_mode={is_dark_mode}
+                                toggleIsTourOpen={toggleIsTourOpen}
+                                is_mf={is_mf}
+                                is_eu={is_eu}
+                                is_eu_country={is_eu_country}
+                                setIsOnboardingVisited={setIsOnboardingVisited}
+                                is_notifications_visible={is_notifications_visible}
+                                notifications_count={notifications_count}
+                                toggleNotifications={toggleNotifications}
+                                handleClickCashier={handleClickCashier}
+                            />
                         )}
                     </div>
                 </div>

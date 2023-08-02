7ff2c5483ca0fa137f1e8c3b571abf4f75590e73
Aum Bhatt <aum@deriv.com>
2023-08-02 16:20:36 +0400

    fix: removed extra check for wallets list length in useHasWallet

--- packages/hooks/src/useHasWallet.ts
+++ packages/hooks/src/useHasWallet.ts
@@ -3,7 +3,7 @@ import useWalletsList from './useWalletsList';
 const useHasWallet = () => {
     const { data: wallets } = useWalletsList();
 
-    return wallets && wallets.length && wallets.length > 0;
+    return wallets && wallets.length > 0;
 };
 
 export default useHasWallet;

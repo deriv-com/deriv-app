ae0ec2818d3d8bd329452ae90c4c5735b36072b8
vinu-deriv <vinu@deriv.com>
2023-08-02 11:44:18 +0400

    fix: disable run button untill ticks_history call is received

--- packages/bot-web-ui/src/stores/run-panel-store.js
+++ packages/bot-web-ui/src/stores/run-panel-store.js
@@ -43,6 +43,7 @@ export default class RunPanelStore {
             showContractUpdateErrorDialog: action.bound,
             onBotSellEvent: action.bound,
             onBotStopEvent: action.bound,
+            onBotReadyEvent: action.bound,
             onBotTradeAgain: action.bound,
             onContractStatusEvent: action.bound,
             onClickSell: action.bound,
@@ -437,6 +438,7 @@ export default class RunPanelStore {
         observer.register('bot.running', this.onBotRunningEvent);
         observer.register('bot.sell', this.onBotSellEvent);
         observer.register('bot.stop', this.onBotStopEvent);
+        observer.register('bot.bot_ready', this.onBotReadyEvent);
         observer.register('bot.click_stop', this.onStopButtonClick);
         observer.register('bot.trade_again', this.onBotTradeAgain);
         observer.register('contract.status', this.onContractStatusEvent);
@@ -520,7 +522,6 @@ export default class RunPanelStore {
         const { ui } = this.core;
         const indicateBotStopped = () => {
             this.error_type = undefined;
-            this.setIsRunning(false);
             this.setContractStage(contract_stages.NOT_RUNNING);
             ui.setAccountSwitcherDisabledMessage(false);
             this.unregisterBotListeners();
@@ -536,17 +537,18 @@ export default class RunPanelStore {
                 this.error_type = undefined;
                 this.setContractStage(contract_stages.PURCHASE_SENT);
             } else {
+                this.setIsRunning(false);
                 indicateBotStopped();
             }
         } else if (this.error_type === error_types.UNRECOVERABLE_ERRORS) {
             // Bot should indicate it stopped in below cases:
             // - When error happens and it's an unrecoverable error
+            this.setIsRunning(false);
             indicateBotStopped();
         } else if (this.has_open_contract) {
             // Bot should indicate the contract is closed in below cases:
             // - When bot was running and an error happens
             this.error_type = undefined;
-            this.setIsRunning(false);
             this.is_sell_requested = false;
             this.setContractStage(contract_stages.CONTRACT_CLOSED);
             ui.setAccountSwitcherDisabledMessage(false);
@@ -561,6 +563,11 @@ export default class RunPanelStore {
         // listen for new version update
         const listen_new_version = new Event('ListenPWAUpdate');
         document.dispatchEvent(listen_new_version);
+    }
+
+    onBotReadyEvent() {
+        this.setIsRunning(false);
+        observer.unregisterAll('bot.bot_ready');
     }
 
     onBotTradeAgain(is_trade_again) {

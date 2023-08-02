e57a415977d132c5299a7435f22171d11dfc068c
Farabi Chowdhury <farabi@deriv.com>
2023-08-02 16:19:45 +0800

    fix: removed bot too risky modal and updated stop icon

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/run-strategy.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/run-strategy.tsx
@@ -3,7 +3,7 @@ import TradeAnimation from 'Components/trade-animation';
 
 const RunStrategy = () => (
     <div className='toolbar__section'>
-        <TradeAnimation should_show_overlay info_direction='left' />
+        <TradeAnimation className='toolbar__animation' should_show_overlay info_direction='left' />
     </div>
 );
 

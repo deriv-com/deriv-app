3101ed84435113092c8118bcc5391fa2a8135b34
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-08-03 16:01:21 +0800

    refactor: add comments, add more test cases

--- packages/hooks/src/__tests__/useHasMFAccountDeposited.spec.tsx
+++ packages/hooks/src/__tests__/useHasMFAccountDeposited.spec.tsx
@@ -9,6 +9,15 @@ jest.mock('@deriv/api', () => ({
     useFetch: jest.fn(() => ({ data: { get_account_status: { status: [] } } })),
 }));
 
+const mockUseFetch = useFetch as jest.MockedFunction<typeof useFetch<'get_account_status'>>;
+const mock_usefetch_return_value = {
+    data: {
+        get_account_status: {
+            status: ['allow_document_upload'],
+        },
+    },
+};
+
 describe('useHasMFAccountDeposited', () => {
     const wrapper = ({ children }: { children: JSX.Element }) => (
         <APIProvider>
@@ -16,17 +25,33 @@ describe('useHasMFAccountDeposited', () => {
         </APIProvider>
     );
 
+    beforeEach(() => {
+        // @ts-expect-error need to come up with a way to mock the return type of useFetch
+        mockUseFetch.mockReturnValue(mock_usefetch_return_value);
+    });
+
     test('should return false if expected status is not in account_status', () => {
         const { result } = renderHook(() => useHasMFAccountDeposited(), { wrapper });
 
         expect(result.current).toBe(false);
     });
 
-    test('should return true if expected status is in account_status', () => {
-        (useFetch as jest.Mock).mockImplementationOnce(() => ({
-            data: { get_account_status: { status: ['cashier_locked'] } },
-        }));
+    test('should return true if unwelcome status is in account_status', () => {
+        mock_usefetch_return_value.data.get_account_status.status = ['unwelcome'];
+        const { result } = renderHook(() => useHasMFAccountDeposited(), { wrapper });
 
+        expect(result.current).toBe(true);
+    });
+
+    test('should return true if withdrawal_locked status is in account_status', () => {
+        mock_usefetch_return_value.data.get_account_status.status = ['withdrawal_locked'];
+        const { result } = renderHook(() => useHasMFAccountDeposited(), { wrapper });
+
+        expect(result.current).toBe(true);
+    });
+
+    test('should return true if cashier_locked status is in account_status', () => {
+        mock_usefetch_return_value.data.get_account_status.status = ['cashier_locked'];
         const { result } = renderHook(() => useHasMFAccountDeposited(), { wrapper });
 
         expect(result.current).toBe(true);

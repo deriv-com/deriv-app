85d7b1f67f327f25ba088c2e4c4bfe96fb8e00f2
Sergei Baranovski <sergei@deriv.com>
2023-08-03 14:21:07 +0300

    fix: fix CircleCI

--- packages/appstore/src/components/wallet-content/__tests__/wallet-transfer-block.spec.tsx
+++ packages/appstore/src/components/wallet-content/__tests__/wallet-transfer-block.spec.tsx
@@ -25,15 +25,38 @@ const wallet_account: TWalletAccount = {
 
 jest.mock('./../../containers/currency-switcher-container', () => jest.fn(({ children }) => <div>{children}</div>));
 
+jest.mock('@deriv/hooks', () => ({
+    ...jest.requireActual('@deriv/hooks'),
+    useTradingAccountsList: jest.fn(() => ({
+        data: [{ account_category: 'trading', currency: 'USD', balance: 1000, loginid: 'CR1001' }],
+    })),
+    useWalletsList: jest.fn(() => ({
+        data: [
+            {
+                currency: 'USD',
+                icon: '',
+                balance: 10415.24,
+                landing_company_name: 'svg',
+                is_disabled: 0,
+                is_virtual: 0,
+                loginid: 'CRW10001',
+                is_selected: false,
+                is_demo: false,
+                wallet_currency_type: '',
+                is_malta_wallet: false,
+                gradient_header_class: 'wallet-header__test',
+                gradient_card_class: 'wallet-card__test',
+                currency_config: undefined,
+                linked_to: [{ loginid: 'CR1001', platform: 'dtrade' }],
+            },
+        ],
+    })),
+}));
+
 const mockedRootStore = mockStore({
     modules: {
         cfd: {
             toggleCompareAccountsModal: jest.fn(),
-        },
-    },
-    client: {
-        accounts: {
-            CR1001: { account_category: 'trading', currency: 'USD', balance: 1000 },
         },
     },
 });

d900225663b1f4aeea280427967c9c196db1e87c
Farzin Mirzaie <farzin@deriv.com>
2023-08-03 17:52:19 +0800

    fix(cashier): :memo: fix raised issues

--- packages/cashier/src/modules/deposit-crypto/components/deposit-crypto-currency-details/deposit-crypto-currency-details.tsx
+++ packages/cashier/src/modules/deposit-crypto/components/deposit-crypto-currency-details/deposit-crypto-currency-details.tsx
@@ -3,7 +3,6 @@ import { Text } from '@deriv/components';
 import { useCurrentCurrencyConfig } from '@deriv/hooks';
 import { observer, useStore } from '@deriv/stores';
 import { localize } from '@deriv/translations';
-import './deposit-crypto-currency-details.scss';
 
 const DepositCryptoCurrencyDetails: React.FC = observer(() => {
     const { ui } = useStore();
@@ -11,17 +10,12 @@ const DepositCryptoCurrencyDetails: React.FC = observer(() => {
     const currency_config = useCurrentCurrencyConfig();
 
     return (
-        <div className='deposit-crypto-currency-details'>
-            <Text align='center' size={is_mobile ? 'xs' : 's'} weight='bold'>
-                {localize('Send only {{currency_name}} ({{currency_code}}) to this address.', {
-                    currency_name: currency_config.name,
-                    currency_code: currency_config.display_code,
-                })}
-            </Text>
-            <Text align='center' size={is_mobile ? 'xxxs' : 'xxs'} color='less-prominent'>
-                {localize('This address can only be used once to make a deposit.')}
-            </Text>
-        </div>
+        <Text align='center' size={is_mobile ? 'xs' : 's'} weight='bold'>
+            {localize('Send only {{currency_name}} ({{currency_code}}) to this address.', {
+                currency_name: currency_config.name,
+                currency_code: currency_config.display_code,
+            })}
+        </Text>
     );
 });
 

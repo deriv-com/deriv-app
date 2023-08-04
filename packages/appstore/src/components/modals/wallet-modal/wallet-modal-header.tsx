bd1e8737e05f611c49d26123f8cd99d06c3f84bb
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2023-08-04 15:44:22 +0300

    george / wall-1177 / Demo transfer  insufficient funds error (#9211)
    * feat: add demo transfer error handlers, add useTransferBetweenAccounts hook
    
    * fix: types errors
    
    * fix: commit
    
    * feat: add transfer hooks
    
    * test: add tests
    
    * fix: commit
    
    * feat: :art: refactor code, add useActiveCFDAccounts hook
    
    * chore: remove comment
    
    * refactor: :fire: cleanup
    
    * fix: useAvaliableWallets hook
    
    * fix: typescript errors
    
    * test: add tests, apply comments
    
    * test: add test, add test accounts
    
    * test: fix tests
    
    * fix: fix ts errors
    
    * fix: apply comments
    
    * perf: small improvement
    
    * perf: small improvement
    
    * fix: resolve conflicts
    
    * refactor: improve logic, fix test
    
    * chore: remove unused import
    
    * feat: add reset balance button handler
    
    * fix: currency register
    
    * style: fix styles in dark mode
    
    * test: fix test
    
    * perf: minor improvements
    
    * refactor: :zap: transfer account data transfarmation layer
    
    * fix: minor fix
    
    * test: fix tests
    
    * fix: input error color
    
    * refactor: split accounts: trading_accounts and wallets
    
    * test: fix test
    
    * refactor: improve naming
    
    * fix: minor fix
    
    * feat: add useExistingCFDAccounts hook
    
    * test: fix tests
    
    * perf: replace useRequest with usefetch in useExistingCFDAccounts hook
    
    * test: fix test
    
    * style: fix selected tile style
    
    * refactor: apply comments

--- packages/appstore/src/components/modals/wallet-modal/wallet-modal-header.tsx
+++ packages/appstore/src/components/modals/wallet-modal/wallet-modal-header.tsx
@@ -1,51 +1,31 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Badge, Icon, Text } from '@deriv/components';
-import { formatMoney, getCurrencyDisplayCode } from '@deriv/shared';
-import { localize } from '@deriv/translations';
-import { getWalletCurrencyIcon } from '@deriv/utils';
+import { Icon, Text, WalletIcon } from '@deriv/components';
+import { formatMoney } from '@deriv/shared';
+import { getAccountName } from 'Constants/utils';
+import { WalletJurisdictionBadge } from 'Components/wallet-jurisdiction-badge';
+import type { TWalletAccount } from 'Types';
 
 type TWalletModalHeaderProps = {
-    balance?: string | number;
-    currency: string;
-    is_dark: boolean;
-    is_demo: boolean;
-    is_mobile: boolean;
-    shortcode: string;
     closeModal: VoidFunction;
+    is_dark: boolean;
+    is_mobile: boolean;
     is_wallet_name_visible: boolean;
-    gradient_class: string;
+    wallet: TWalletAccount;
 };
 
 const WalletModalHeader = ({
-    balance,
     closeModal,
-    currency,
     is_dark,
-    is_demo,
     is_mobile,
-    shortcode,
     is_wallet_name_visible,
-    gradient_class,
+    wallet,
 }: TWalletModalHeaderProps) => {
+    const { balance, currency, icon, currency_config, is_demo, gradient_header_class, landing_company_name } = wallet;
+    const is_crypto = currency_config?.is_crypto;
+    const display_currency_code = currency_config?.display_code;
+
     const header_class_name = 'modal-header';
-
-    const wallet_title = React.useMemo(() => {
-        return `${is_demo ? localize('Demo') : ''} ${getCurrencyDisplayCode(currency)} ${localize('Wallet')}`;
-    }, [currency, is_demo]);
-
-    const getBadgeLabel = React.useCallback(() => {
-        if (is_demo) return localize('Demo');
-        switch (shortcode) {
-            case 'svg':
-                return shortcode.toUpperCase();
-            case 'malta':
-            case 'maltainvest':
-                return 'malta'.toUpperCase();
-            default:
-                return '';
-        }
-    }, [is_demo, shortcode]);
 
     const getCloseIcon = React.useCallback(() => {
         if (is_demo && is_dark) return 'IcAppstoreCloseLight';
@@ -54,47 +34,31 @@ const WalletModalHeader = ({
         return 'IcAppstoreCloseLight';
     }, [is_dark, is_demo]);
 
-    const getStylesByClassName = React.useCallback(
-        (class_name: string) => {
-            return classNames(class_name, {
-                [`${class_name}-demo`]: is_demo,
-            });
-        },
-        [is_demo]
-    );
+    const getWalletIcon = React.useCallback(() => {
+        if (currency && ['USDT', 'eUSDT', 'tUSDT', 'UST'].includes(currency)) {
+            return is_dark ? 'IcWalletModalTetherDark' : 'IcWalletModalTetherLight';
+        }
+        return icon;
+    }, [currency, icon, is_dark]);
 
-    const getCurrencyIconSize = React.useCallback(() => {
-        // TODO: add p2p and payment_agent check
-        const is_square_icon =
-            ['btc', 'eth', 'ltc', 'usdt', 'eusdt', 'tusdt', 'ust', 'usdc', 'p2p', 'payment_agent'].includes(
-                currency.toLowerCase()
-            ) || is_demo;
+    const getStylesByClassName = (class_name: string) => {
+        return classNames(class_name, {
+            [`${class_name}-demo`]: is_demo,
+        });
+    };
 
-        const sizes = {
-            mobile: {
-                width: is_square_icon ? 64 : 48,
-                height: is_square_icon ? 40 : 48,
-            },
-            desktop: {
-                width: is_square_icon ? 128 : 64,
-                height: is_square_icon ? 80 : 64,
-            },
-        };
+    const getWalletIconType = (): React.ComponentProps<typeof WalletIcon>['type'] => {
+        if (is_demo) return 'demo';
+        return is_crypto ? 'crypto' : 'fiat';
+    };
 
-        const size = is_mobile ? sizes.mobile : sizes.desktop;
-
-        return size;
-    }, [currency, is_demo, is_mobile]);
-
-    const getCurrencyIconProps = React.useCallback(() => {
-        const icon = getWalletCurrencyIcon(is_demo ? 'demo' : currency, is_dark, true);
-        const size = getCurrencyIconSize();
-
-        return { icon, ...size };
-    }, [currency, getCurrencyIconSize, is_dark, is_demo]);
+    const getWalletIconSize = (): React.ComponentProps<typeof WalletIcon>['size'] => {
+        if (is_mobile) return is_demo || is_crypto ? 'large' : 'xlarge';
+        return 'xxlarge';
+    };
 
     return (
-        <div className={`header-background ${gradient_class}`}>
+        <div className={`header-background ${gradient_header_class}`}>
             <div
                 className={classNames(header_class_name, {
                     [`${header_class_name}--hidden-title`]: is_mobile && !is_wallet_name_visible,
@@ -107,13 +71,12 @@ const WalletModalHeader = ({
                             as='span'
                             className={getStylesByClassName(`${header_class_name}__title-wallet`)}
                         >
-                            {wallet_title}
+                            {getAccountName({
+                                display_currency_code: wallet.currency_config?.display_code,
+                                account_type: 'wallet',
+                            })}
                         </Text>
-                        {is_demo ? (
-                            <Badge type='contained' background_color='blue' label={getBadgeLabel()} />
-                        ) : (
-                            <Badge type='bordered' label={getBadgeLabel()} />
-                        )}
+                        <WalletJurisdictionBadge is_demo={is_demo} shortcode={landing_company_name} />
                     </div>
                     <Text
                         as='p'
@@ -121,12 +84,11 @@ const WalletModalHeader = ({
                         weight='bold'
                         className={getStylesByClassName(`${header_class_name}__title-balance`)}
                     >
-                        {formatMoney(currency, balance, true)} {getCurrencyDisplayCode(currency)}
+                        {formatMoney(currency || '', balance, true)} {display_currency_code}
                     </Text>
                 </div>
-                {/* TODO: replace Icon with WalletIcon component  */}
                 <div className={classNames(`${header_class_name}__currency-icon icon-visibility`)}>
-                    <Icon {...getCurrencyIconProps()} data_testid='dt_currency_icon' />
+                    <WalletIcon icon={getWalletIcon()} type={getWalletIconType()} size={getWalletIconSize()} />
                 </div>
                 <div className={classNames(`${header_class_name}__close-icon`)}>
                     <Icon icon={getCloseIcon()} onClick={closeModal} data_testid='dt_close_icon' />

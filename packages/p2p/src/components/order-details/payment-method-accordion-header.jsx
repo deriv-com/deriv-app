000f338d903a5a63025656c9ce7c22992276a7dc
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-08-04 18:32:24 +0800

    fix: upi icon is not match in order-details page

--- packages/p2p/src/components/order-details/payment-method-accordion-header.jsx
+++ packages/p2p/src/components/order-details/payment-method-accordion-header.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Icon, Text } from '@deriv/components';
+import { getPaymentMethodIcon } from 'Utils/payment-method.js';
 
 const PaymentMethodAccordionHeader = ({ payment_method }) => {
     const method = payment_method.display_name.replace(/\s|-/gm, '');
-
-    if (method === 'BankTransfer' || method === 'Other') {
-        return (
-            <div className='order-details-card__accordion-row'>
-                <Icon className='order-details-card__accordion-icon' icon={`IcCashier${method}`} size={16} />
-                <div className='order-details-card__accordion-title'>
-                    <Text color='prominent' size='xs'>
-                        {payment_method.display_name}
-                    </Text>
-                    {method === 'BankTransfer' && (
-                        <Text color='prominent' size='xs' weight='lighter'>
-                            {payment_method.fields.bank_name.value}
-                        </Text>
-                    )}
-                </div>
-            </div>
-        );
-    }
-
+    const icon = getPaymentMethodIcon(method);
     return (
         <div className='order-details-card__accordion-row'>
-            <Icon icon='IcCashierEwallet' size={16} />
-            <div className='order-details-card__accordion-column'>
+            <Icon
+                className={classNames({
+                    'order-details-card__accordion-icon': icon !== 'IcCashierEwallet',
+                })}
+                icon={icon}
+                size={16}
+            />
+            <div
+                className={
+                    icon === 'IcCashierEwallet'
+                        ? 'order-details-card__accordion-column'
+                        : 'order-details-card__accordion-title'
+                }
+            >
                 <Text color='prominent' size='xs'>
                     {payment_method.display_name}
                 </Text>
+                {method === 'BankTransfer' && (
+                    <Text color='prominent' size='xs' weight='lighter'>
+                        {payment_method.fields.bank_name.value}
+                    </Text>
+                )}
             </div>
         </div>
     );

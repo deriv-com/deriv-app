fd26f9bdcb04792bf4fbcb8fa6ce5c9424c1b85b
Sergei Baranovski <sergei@deriv.com>
2023-08-07 13:49:44 +0300

    feat: add check for MFW and VRW for EU

--- packages/core/src/Stores/traders-hub-store.js
+++ packages/core/src/Stores/traders-hub-store.js
@@ -146,8 +146,8 @@ export default class TradersHubStore extends BaseStore {
             () => [this.root_store.client.loginid, this.root_store.client.residence],
             () => {
                 const residence = this.root_store.client.residence;
-                const active_demo = /^VRT/.test(this.root_store.client.loginid);
-                const active_real_mf = /^MF/.test(this.root_store.client.loginid);
+                const active_demo = /^VRT|VRW/.test(this.root_store.client.loginid);
+                const active_real_mf = /^MF|MFW/.test(this.root_store.client.loginid);
 
                 const default_region = () => {
                     if (((active_demo || active_real_mf) && isEuCountry(residence)) || active_real_mf) {

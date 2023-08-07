10f241a3dd5b292e8a052fd6dfa3bcfb55c5836b
Shayan Khaleghparast <iman@regentmarkets.com>
2023-08-07 20:13:09 +0330

    chore: renamed variable name

--- packages/hooks/src/useExchangeRate.ts
+++ packages/hooks/src/useExchangeRate.ts
@@ -9,19 +9,19 @@ const useExchangeRate = () => {
     const { exchange_rates } = useStore();
     const data = exchange_rates.data;
     const rates = data?.rates;
-    const [firstLoadedRates, SetFirstLoadedRates] = useState<typeof rates>(undefined); //this is a static data which we can use for places like total asset to avoid showing user the real time value
+    const [initialRate, setInitialRate] = useState<typeof rates>(undefined); //this is a static data which we can use for places like total asset to avoid showing user the real time value
 
     useEffect(() => {
         if (rates) {
-            if (!firstLoadedRates) SetFirstLoadedRates(rates);
+            if (!initialRate) setInitialRate(rates);
         }
     }, [rates]);
 
-    // we can have another useEffect to decide when or how we want to update the firstLoadedRates (eg, every x seconds)
+    // we can have another useEffect to decide when or how we want to update the initialRate (eg, every x seconds)
 
     const getRate = useCallback((currency: string) => rates?.[currency] || 1, [rates]);
 
-    const getInitialRate = useCallback((currency: string) => firstLoadedRates?.[currency] || 1, [firstLoadedRates]);
+    const getInitialRate = useCallback((currency: string) => initialRate?.[currency] || 1, [initialRate]);
 
     return {
         getRate,

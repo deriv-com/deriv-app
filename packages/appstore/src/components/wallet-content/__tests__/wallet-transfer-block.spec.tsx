0ff671155347c2761abc8a9f205195bf906456be
Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
2023-08-08 18:08:02 +0300

    Sergei / wall 1397 / Wrong trading account number in wallet (#9510)
    * feat: change wallet loginid to trading account loginid
    
    * feat: fix wallet-transfer-block tests
    
    * feat: add check for linked trading account
    
    * feat: add is_options_and_multipliers check for demo
    
    * feat: delete trading account from accounts in client store
    
    * feat: add important comment
    
    * feat: delete trading accounts hiding code and implement some suggestions
    
    * feat: implement suggestions
    
    * feat: add balance and currency to linked_to accounts in useWalletsList
    
    * feat: create useTradingAccountsList hook
    
    * fix: fix CircleCI
    
    * feat: add one more test case for useTradingAccountsList hook
    
    * feat: add check for MFW and VRW for EU
    
    * fix: fix CircleCI issue

--- packages/appstore/src/components/wallet-content/__tests__/wallet-transfer-block.spec.tsx
+++ packages/appstore/src/components/wallet-content/__tests__/wallet-transfer-block.spec.tsx
@@ -6,18 +6,52 @@ import { TWalletAccount } from 'Types';
 import { StoreProvider, mockStore } from '@deriv/stores';
 
 const wallet_account: TWalletAccount = {
-    name: 'USD',
     currency: 'USD',
     icon: '',
     balance: 10415.24,
-    icon_type: 'fiat',
     landing_company_name: 'svg',
-    is_disabled: false,
-    is_virtual: false,
+    is_disabled: 0,
+    is_virtual: 0,
     loginid: 'CRW10001',
+    is_selected: false,
+    is_demo: false,
+    wallet_currency_type: '',
+    is_malta_wallet: false,
+    gradient_header_class: 'wallet-header__test',
+    gradient_card_class: 'wallet-card__test',
+    currency_config: undefined,
+    linked_to: [{ loginid: 'CR1001', platform: 'dtrade' }],
 };
 
 jest.mock('./../../containers/currency-switcher-container', () => jest.fn(({ children }) => <div>{children}</div>));
+
+jest.mock('@deriv/hooks', () => ({
+    ...jest.requireActual('@deriv/hooks'),
+    useTradingAccountsList: jest.fn(() => ({
+        data: [{ account_category: 'trading', currency: 'USD', balance: 1000, loginid: 'CR1001' }],
+    })),
+    useWalletsList: jest.fn(() => ({
+        data: [
+            {
+                currency: 'USD',
+                icon: '',
+                balance: 10415.24,
+                landing_company_name: 'svg',
+                is_disabled: 0,
+                is_virtual: 0,
+                loginid: 'CRW10001',
+                is_selected: false,
+                is_demo: false,
+                wallet_currency_type: '',
+                is_malta_wallet: false,
+                gradient_header_class: 'wallet-header__test',
+                gradient_card_class: 'wallet-card__test',
+                currency_config: undefined,
+                linked_to: [{ loginid: 'CR1001', platform: 'dtrade' }],
+            },
+        ],
+    })),
+}));
 
 const mockedRootStore = mockStore({
     modules: {
@@ -34,9 +68,7 @@ describe('<WalletTransferBlock />', () => {
                 <WalletTransferBlock wallet_account={wallet_account} />
             </StoreProvider>
         );
-        const { currency } = wallet_account;
-
-        const balance_title = screen.queryByText(`10,415.24 ${currency}`);
+        const balance_title = screen.queryByText('1,000.00 USD');
 
         expect(balance_title).toBeInTheDocument();
     });
@@ -47,9 +79,7 @@ describe('<WalletTransferBlock />', () => {
                 <WalletTransferBlock wallet_account={wallet_account} />
             </StoreProvider>
         );
-        const { loginid } = wallet_account;
-
-        const loginid_title = screen.queryByText(String(loginid));
+        const loginid_title = screen.queryByText('CR1001');
 
         expect(loginid_title).toBeInTheDocument();
     });
---
+++ packages/hooks/src/__tests__/useTradingAccountsList.spec.tsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import { APIProvider } from '@deriv/api';
+import { StoreProvider, mockStore } from '@deriv/stores';
+import { renderHook } from '@testing-library/react-hooks';
+import useTradingAccountsList from '../useTradingAccountsList';
+
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useFetch: jest.fn((name: string) => {
+        if (name === 'authorize') {
+            return {
+                data: {
+                    authorize: {
+                        account_list: [
+                            {
+                                account_category: 'trading',
+                                currency: 'USD',
+                                is_virtual: 1,
+                                loginid: 'CR1001',
+                            },
+                            {
+                                account_category: 'trading',
+                                currency: 'USD',
+                                is_virtual: 0,
+                                loginid: 'CR1002',
+                            },
+
+                            {
+                                account_category: 'trading',
+                                currency: 'UST',
+                                is_virtual: 0,
+                                loginid: 'CR1003',
+                            },
+                            {
+                                account_category: 'trading',
+                                currency: 'BTC',
+                                is_virtual: 0,
+                                loginid: 'CR1004',
+                            },
+                            {
+                                account_category: 'wallet',
+                                currency: 'USD',
+                                is_virtual: 0,
+                                loginid: 'CRW1001',
+                            },
+                            {
+                                account_category: 'wallet',
+                                currency: 'BTC',
+                                is_virtual: 0,
+                                loginid: 'CRW1002',
+                            },
+                        ],
+                    },
+                },
+            };
+        } else if (name === 'balance') {
+            return {
+                data: {
+                    balance: {
+                        accounts: {
+                            CR1001: {
+                                balance: 10000,
+                            },
+                            CR1002: {
+                                balance: 10,
+                            },
+                            CR1003: {
+                                balance: 179,
+                            },
+                            CR1004: {
+                                balance: 15,
+                            },
+                            CRW1001: {
+                                balance: 679,
+                            },
+                            CRW1002: {
+                                balance: 2.34,
+                            },
+                        },
+                    },
+                },
+            };
+        }
+
+        return undefined;
+    }),
+}));
+
+describe('useTradingAccountsList', () => {
+    test('should return trading accounts list for the current loginid', () => {
+        const mock = mockStore({ client: { accounts: { CR1001: { token: '12345' } }, loginid: 'CR1001' } });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <APIProvider>
+                <StoreProvider store={mock}>{children}</StoreProvider>
+            </APIProvider>
+        );
+
+        const { result } = renderHook(() => useTradingAccountsList(), { wrapper });
+
+        expect(result.current.data?.every(account => account.account_category === 'trading')).toEqual(true);
+        expect(result.current.data?.length).toEqual(4);
+        expect(result.current.data?.find(account => account.loginid === 'CR1003')?.balance).toEqual(179);
+    });
+
+    test('should return correct balance', () => {
+        const mock = mockStore({ client: { accounts: { CR1001: { token: '12345' } }, loginid: 'CR1001' } });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <APIProvider>
+                <StoreProvider store={mock}>{children}</StoreProvider>
+            </APIProvider>
+        );
+
+        const { result } = renderHook(() => useTradingAccountsList(), { wrapper });
+
+        expect(result.current.data?.find(account => account.loginid === 'CR1003')?.balance).toEqual(179);
+    });
+});

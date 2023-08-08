3d076a4fa098676ed7fb544ea34f17b1337b0269
Sergei Baranovski <sergei@deriv.com>
2023-08-08 13:59:21 +0300

    feat: refactor useAvailableWallets

--- packages/hooks/src/useWalletAccountsList.ts
+++ packages/hooks/src/useWalletAccountsList.ts
@@ -1,59 +1,20 @@
 import { useMemo } from 'react';
 import useAccountsList from './useAccountsList';
 
-const currency_to_icon_mapper: Record<string, Record<'light' | 'dark', string>> = {
-    Demo: {
-        dark: 'IcWalletDerivDemoDark',
-        light: 'IcWalletDerivDemoLight',
-    },
-    USD: {
-        dark: 'IcWalletCurrencyUsd',
-        light: 'IcWalletCurrencyUsd',
-    },
-    EUR: {
-        dark: 'IcWalletCurrencyEur',
-        light: 'IcWalletCurrencyEur',
-    },
-    AUD: {
-        dark: 'IcWalletCurrencyAud',
-        light: 'IcWalletCurrencyAud',
-    },
-    GBP: {
-        dark: 'IcWalletCurrencyGbp',
-        light: 'IcWalletCurrencyGbp',
-    },
-    BTC: {
-        dark: 'IcWalletBitcoinDark',
-        light: 'IcWalletBitcoinLight',
-    },
-    ETH: {
-        dark: 'IcWalletEthereumDark',
-        light: 'IcWalletEthereumLight',
-    },
-    USDT: {
-        dark: 'IcWalletTetherDark',
-        light: 'IcWalletTetherLight',
-    },
-    eUSDT: {
-        dark: 'IcWalletTetherDark',
-        light: 'IcWalletTetherLight',
-    },
-    tUSDT: {
-        dark: 'IcWalletTetherDark',
-        light: 'IcWalletTetherLight',
-    },
-    UST: {
-        dark: 'IcWalletTetherDark',
-        light: 'IcWalletTetherLight',
-    },
-    LTC: {
-        dark: 'IcWalletLiteCoinDark',
-        light: 'IcWalletLiteCoinLight',
-    },
-    USDC: {
-        dark: 'IcWalletUsdCoinDark',
-        light: 'IcWalletUsdCoinLight',
-    },
+const currency_to_icon_mapper: Record<string, string> = {
+    Demo: 'IcWalletDerivDemoLight',
+    USD: 'IcWalletCurrencyUsd',
+    EUR: 'IcWalletCurrencyEur',
+    AUD: 'IcWalletCurrencyAud',
+    GBP: 'IcWalletCurrencyGbp',
+    BTC: 'IcWalletBitcoinLight',
+    ETH: 'IcWalletEthereumLight',
+    USDT: 'IcWalletTetherLight',
+    eUSDT: 'IcWalletTetherLight',
+    tUSDT: 'IcWalletTetherLight',
+    UST: 'IcWalletTetherLight',
+    LTC: 'IcWalletLiteCoinLight',
+    USDC: 'IcWalletUsdCoinLight',
 };
 
 /** A custom hook that gets the list of all wallet accounts for the current user. */
@@ -70,19 +31,7 @@ const useWalletAccountsList = () => {
     const modified_accounts = useMemo(() => {
         return filtered_accounts?.map(wallet => {
             const wallet_currency_type = wallet.is_virtual ? 'Demo' : wallet.currency || '';
-
-            const gradients = {
-                /** The gradient class name for the wallet header background. */
-                header: {
-                    dark: `wallet-header__${wallet_currency_type.toLowerCase()}-bg--dark`,
-                    light: `wallet-header__${wallet_currency_type.toLowerCase()}-bg`,
-                },
-                /** The gradient class name for the wallet card background. */
-                card: {
-                    dark: `wallet-header__${wallet_currency_type.toLowerCase()}-bg--dark`,
-                    light: `wallet-header__${wallet_currency_type.toLowerCase()}-bg`,
-                },
-            };
+            const wallet_gradient_class_name = `${wallet_currency_type.toLowerCase()}-bg`;
 
             return {
                 ...wallet,
@@ -92,8 +41,10 @@ const useWalletAccountsList = () => {
                 landing_company_name: wallet.landing_company_name?.replace('maltainvest', 'malta'),
                 /** Indicating whether the wallet is a maltainvest wallet. */
                 is_malta_wallet: wallet.landing_company_name === 'malta',
-                /** The gradient class names for the wallet background. */
-                gradients,
+                /** The gradient class name for the wallet header background. */
+                gradient_header_class: `wallet-header__${wallet_gradient_class_name}`,
+                /** The gradient class name for the wallet card background. */
+                gradient_card_class: `wallet-card__${wallet_gradient_class_name}`,
                 /** Local asset name for the wallet icon. ex: `IcWalletCurrencyUsd` for `USD`  */
                 icon: currency_to_icon_mapper[wallet_currency_type],
             } as const;

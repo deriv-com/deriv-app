00735420f1431734820711fd762b37e6c6f2d1bf
Sergei Baranovski <sergei@deriv.com>
2023-08-09 19:24:24 +0300

    feat: change CardSliderSwiper and useAuthorize

--- packages/hooks/src/useAuthorize.ts
+++ packages/hooks/src/useAuthorize.ts
@@ -8,7 +8,10 @@ const useAuthorize = (token?: string) => {
     const { accounts, loginid = '' } = client;
     const current_token = accounts[loginid || '']?.token;
 
-    const { data, ...rest } = useFetch('authorize', { payload: { authorize: token || current_token } });
+    const { data, ...rest } = useFetch('authorize', {
+        payload: { authorize: token || current_token },
+        options: { keepPreviousData: true, staleTime: 10000 },
+    });
 
     // Add additional information to the authorize response.
     const modified_authorize = useMemo(() => ({ ...data?.authorize }), [data?.authorize]);

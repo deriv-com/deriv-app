ec8800e2a0a76fa55982d1f0b36d58d2c5a88cb9
Shaheer <shaheer@deriv.com>
2023-08-14 15:20:41 +0400

    refactor: :art: migrates notifications dialog to typescript

--- packages/core/src/App/Containers/NotificationsDialog/notifications-dialog.tsx
+++ packages/core/src/App/Containers/NotificationsDialog/notifications-dialog.tsx
@@ -1,42 +1,50 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { DesktopWrapper, MobileDialog, MobileWrapper, useOnClickOutside } from '@deriv/components';
 import { LocalStore } from '@deriv/shared';
+import { observer, useStore } from '@deriv/stores';
 import { localize } from '@deriv/translations';
-import { connect } from 'Stores/connect';
 import NotificationListWrapper from './notification-list-wrapper';
 
-const NotificationsDialog = ({
-    is_visible,
-    loginid,
-    notifications,
-    toggleDialog,
-    removeNotificationMessage,
-    removeNotificationMessageByKey,
-    removeNotifications,
-}) => {
-    const wrapper_ref = React.useRef();
+const NotificationsDialog = observer(() => {
+    const { client, notifications } = useStore();
+    const { loginid } = client;
+    const {
+        is_notifications_visible,
+        notifications: notifications_array,
+        removeNotifications,
+        removeNotificationMessage,
+        removeNotificationMessageByKey,
+        toggleNotificationsModal,
+    } = notifications;
 
-    const handleClickOutside = event => {
-        const notifications_toggle_btn = !event.target.classList.contains('notifications-toggle__icon-wrapper');
-        if (!wrapper_ref.current?.contains(event.target) && is_visible && notifications_toggle_btn) {
-            toggleDialog();
+    const wrapper_ref = React.useRef<HTMLDivElement>(null);
+
+    const handleClickOutside = (event: MouseEvent) => {
+        const notifications_toggle_btn = !(event?.target as Element)?.classList.contains(
+            'notifications-toggle__icon-wrapper'
+        );
+        if (
+            !wrapper_ref?.current?.contains(event.target as Node) &&
+            is_notifications_visible &&
+            notifications_toggle_btn
+        ) {
+            toggleNotificationsModal();
         }
     };
 
     const clearNotifications = () => {
         const p2p_settings = LocalStore.getObject('p2p_settings');
-        if (p2p_settings[loginid]) {
+        if (loginid && p2p_settings[loginid]) {
             p2p_settings[loginid].is_notifications_visible = false;
         }
         LocalStore.setObject('p2p_settings', p2p_settings);
 
-        notifications.forEach(item => {
-            removeNotificationMessageByKey(item.key);
+        notifications_array.forEach(item => {
+            removeNotificationMessageByKey({ key: item.key });
             removeNotificationMessage({
                 key: item.key,
-                should_show_again: item.should_show_again || false,
+                should_show_again: item?.should_show_again ?? false,
             });
             removeNotifications(true);
         });
@@ -51,15 +59,15 @@ const NotificationsDialog = ({
                     portal_element_id='modal_root'
                     title={localize('Notifications')}
                     wrapper_classname='notifications-mobile-dialog'
-                    visible={is_visible}
-                    onClose={toggleDialog}
+                    visible={is_notifications_visible}
+                    onClose={toggleNotificationsModal}
                 >
                     <NotificationListWrapper clearNotifications={clearNotifications} ref={wrapper_ref} />
                 </MobileDialog>
             </MobileWrapper>
             <DesktopWrapper>
                 <CSSTransition
-                    in={is_visible}
+                    in={is_notifications_visible}
                     classNames={{
                         enter: 'notifications-dialog--enter',
                         enterDone: 'notifications-dialog--enter-done',
@@ -73,25 +81,6 @@ const NotificationsDialog = ({
             </DesktopWrapper>
         </React.Fragment>
     );
-};
+});
 
-NotificationsDialog.propTypes = {
-    is_visible: PropTypes.bool,
-    loginid: PropTypes.string,
-    notifications: PropTypes.array,
-    toggleDialog: PropTypes.func,
-    removeNotificationMessage: PropTypes.func,
-    removeNotificationByKey: PropTypes.func,
-    removeNotificationMessageByKey: PropTypes.func,
-    removeNotifications: PropTypes.func,
-};
-
-export default connect(({ common, client, notifications }) => ({
-    app_routing_history: common.app_routing_history,
-    loginid: client.loginid,
-    notifications: notifications.notifications,
-    removeNotificationByKey: notifications.removeNotificationByKey,
-    removeNotificationMessage: notifications.removeNotificationMessage,
-    removeNotifications: notifications.removeNotifications,
-    removeNotificationMessageByKey: notifications.removeNotificationMessageByKey,
-}))(NotificationsDialog);
+export default NotificationsDialog;

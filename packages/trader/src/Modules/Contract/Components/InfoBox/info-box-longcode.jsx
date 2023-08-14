084f7e9a3fad34be6139af04d8c14b75c7c2e076
maryia-deriv <maryia@binary.com>
2023-08-14 11:07:32 +0300

    feat: redesign mobile infobox

--- packages/trader/src/Modules/Contract/Components/InfoBox/info-box-longcode.jsx
+++ packages/trader/src/Modules/Contract/Components/InfoBox/info-box-longcode.jsx
@@ -4,9 +4,10 @@ import classNames from 'classnames';
 import React from 'react';
 import { Icon, Text } from '@deriv/components';
 import { localize } from '@deriv/translations';
+import { isMobile } from '@deriv/shared';
 
 const InfoBoxLongcode = ({ contract_info }) => {
-    const max_longcode_length = 150;
+    const max_longcode_length = isMobile() ? 70 : 150;
     const [is_collapsed, setIsCollapsed] = React.useState(true);
 
     const handleToggle = () => {

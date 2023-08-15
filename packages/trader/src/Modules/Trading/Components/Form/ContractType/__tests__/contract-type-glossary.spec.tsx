c2606e0570c7ad2e432024ab52a180292288d054
Henry Hein <henry@regentmarkets.com>
2023-08-15 12:08:45 +0800

    fix: test cases

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-glossary.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-glossary.spec.tsx
@@ -14,7 +14,18 @@ describe('<ContractTypeGlossary />', () => {
         render(<ContractTypeGlossary category='vanillalongcall' />);
 
         expect(
-            screen.getByText(/Contracts will expire at exactly 23:59:59 GMT on your selected expiry date./i)
+            screen.getByText(
+                'If you select "Call", you’ll earn a payout if the final price is above the strike price at expiry. Otherwise, you won’t receive a payout.'
+            )
+        ).toBeInTheDocument();
+    });
+    it('Ensure turbos glossary is rendered properly', () => {
+        render(<ContractTypeGlossary category='turboslong' />);
+
+        expect(
+            screen.getByText(
+                'We’ll offer to buy your contract at this price should you choose to sell it before its expiry. This is based on several factors, such as the current spot price'
+            )
         ).toBeInTheDocument();
     });
     it('Ensure placeholder text is rendered if category does not exist', () => {

f73c097774a7c2ed7db47f92042d9f52859a2108
Likhith Kolayari <likhith@regentmarkets.com>
2023-08-16 15:04:01 +0400

    replaced test component

--- packages/api/src/APIProvider.tsx
+++ packages/api/src/APIProvider.tsx
@@ -1,31 +1,8 @@
-// import React, { PropsWithChildren } from 'react';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-// const queryClient = new QueryClient();
-
-// const APIProvider = ({ children }: PropsWithChildren<unknown>) => (
-//     <QueryClientProvider client={queryClient}>
-//         {children}
-//         <ReactQueryDevtools />
-//     </QueryClientProvider>
-// );
-
-// export default APIProvider;
-
 import React, { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-function getQueryClientContext(): QueryClient {
-    if (!window.ReactQueryClient) {
-        window.ReactQueryClient = new QueryClient();
-    }
-
-    return window.ReactQueryClient;
-}
-
-export const queryClient = getQueryClientContext();
+const queryClient = new QueryClient();
 
 const APIProvider = ({ children }: PropsWithChildren<unknown>) => (
     <QueryClientProvider client={queryClient}>

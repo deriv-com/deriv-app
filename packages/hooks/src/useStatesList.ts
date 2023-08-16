2c595a6c93c31e02cefaea70d2c089698457247a
Likhith Kolayari <likhith@regentmarkets.com>
2023-08-16 15:13:58 +0400

    fix: eslint issues

--- packages/hooks/src/useStatesList.ts
+++ packages/hooks/src/useStatesList.ts
@@ -5,7 +5,7 @@ import { useStore } from '@deriv/stores';
  * Custom hook to get states list for a particular country.
  * @returns an object with the states list and the options to manage API response.
  */
-const useStatesList = country => {
+const useStatesList = (country: string) => {
     const { data, ...rest } = useFetch('states_list', {
         // @ts-expect-error The `states_list` type from `@deriv/api-types` is not correct.
         // The type should be `string`, but it's an alias to string type.

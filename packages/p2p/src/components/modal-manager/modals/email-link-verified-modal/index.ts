35617deacf445b0d8c556b7ee41a436531d0df38
Nada <nada.ajinas@deriv.com>
2023-08-17 16:20:54 +0400

    refactor: email link verified modal refactoring

--- packages/p2p/src/components/modal-manager/modals/email-link-verified-modal/index.ts
+++ packages/p2p/src/components/modal-manager/modals/email-link-verified-modal/index.ts
@@ -1,4 +1,4 @@
-import EmailLinkVerifiedModal from './email-link-verified-modal.jsx';
+import EmailLinkVerifiedModal from './email-link-verified-modal';
 import './email-link-verified-modal.scss';
 
 export default EmailLinkVerifiedModal;

6aed6f7f7a19ea145364dc58cdda4f29d8dfb7da
Sergei Baranovski <sergei@deriv.com>
2023-08-17 15:37:15 +0300

    refactor: back cahnges for useDepositFiatAddress

--- packages/hooks/src/__tests__/useDepositFiatAddress.spec.tsx
+++ packages/hooks/src/__tests__/useDepositFiatAddress.spec.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { APIProvider } from '@deriv/api';
+import { mockStore, StoreProvider } from '@deriv/stores';
 import useDepositFiatAddress from '../useDepositFiatAddress';
 
 jest.mock('@deriv/api', () => ({
@@ -10,22 +11,48 @@ jest.mock('@deriv/api', () => ({
 
 describe('useDepositFiatAddress', () => {
     it('should get the iframe url when cashier API is called', () => {
-        const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
+        const mock = mockStore({ ui: { is_dark_mode_on: false } });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <APIProvider>
+                <StoreProvider store={mock}>{children}</StoreProvider>
+            </APIProvider>
+        );
 
         const { result } = renderHook(() => useDepositFiatAddress(), { wrapper });
 
         result.current.resend();
-
         expect(result.current.data).toMatch('https://example.com');
     });
 
-    it('should get the iframe url for light mode', () => {
-        const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
+    it('should get the iframe url for dark mode', () => {
+        const mock = mockStore({ ui: { is_dark_mode_on: true } });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <APIProvider>
+                <StoreProvider store={mock}>{children}</StoreProvider>
+            </APIProvider>
+        );
 
         const { result } = renderHook(() => useDepositFiatAddress(), { wrapper });
 
         result.current.resend();
 
+        expect(result.current.data).toBe('https://example.com&DarkMode=on');
+    });
+
+    it('should get the iframe url for light mode', () => {
+        const mock = mockStore({ ui: { is_dark_mode_on: false } });
+
+        const wrapper = ({ children }: { children: JSX.Element }) => (
+            <APIProvider>
+                <StoreProvider store={mock}>{children}</StoreProvider>
+            </APIProvider>
+        );
+
+        const { result } = renderHook(() => useDepositFiatAddress(), { wrapper });
+
+        result.current.resend();
         expect(result.current.data).toBe('https://example.com&DarkMode=off');
     });
 });

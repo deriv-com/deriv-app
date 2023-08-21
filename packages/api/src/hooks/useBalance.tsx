81034516464cd092f154699d0bde504ab45f3b1a
adrienne-rio <adrienne@deriv.com>
2023-08-21 18:18:35 +0800

    refactor: added interval polling to refetch balance every 30s

--- packages/api/src/hooks/useBalance.tsx
+++ packages/api/src/hooks/useBalance.tsx
@@ -1,25 +1,19 @@
 import { useFetch } from '@deriv/api';
 import { useMemo } from 'react';
 
-/** A custom hook that gets the balance for user accounts.
- * If no account argument is provided, by default the hook will return the balance of all accounts.
- *
- * @param account {string} - If set to 'all', return the balances of all accounts one by one.
- *                           If set to 'current', return the balance of current account.
- *                           If set as an account id, return the balance of that account.
- *                           Default is set to 'all'.
- */
-const useBalance = (account?: string) => {
+/** A custom hook that gets the balance for all the user accounts. */
+const useBalance = () => {
     const { data: balance_data, ...rest } = useFetch('balance', {
-        payload: { account: account ?? 'all' },
-        // TODO: Add a subscription when we have `BalanceProvider`.
+        payload: { account: 'all' },
+        options: {
+            refetchInterval: 30000,
+        },
     });
 
     // Add additional information to the balance data.
     const modified_balance = useMemo(() => ({ ...balance_data?.balance }), [balance_data?.balance]);
 
     return {
-        /** The balance response. */
         data: modified_balance,
         ...rest,
     };

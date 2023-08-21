9dda185e90257d6dbca423e40784710adbb5f007
Hirad <hirad@regentmarkets.com>
2023-08-21 11:44:24 +0800

    fix: code smells are fixed

--- packages/cfd/src/templates/app/components/loading.tsx
+++ packages/cfd/src/templates/app/components/loading.tsx
@@ -27,7 +27,7 @@ const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status,
             <div id={id} className={classNames('initial-loader__barspinner', 'barspinner', theme_class)}>
                 {Array.from(new Array(5)).map((x, inx) => (
                     <div
-                        key={inx}
+                        key={x}
                         className={`initial-loader__barspinner--rect barspinner__rect barspinner__rect--${
                             inx + 1
                         } rect${inx + 1}`}
@@ -36,7 +36,7 @@ const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status,
             </div>
             {is_slow_loading &&
                 status.map((text, inx) => (
-                    <Text as='h3' color='prominent' size='xs' align='center' key={inx}>
+                    <Text as='h3' color='prominent' size='xs' align='center' key={id}>
                         {text}
                     </Text>
                 ))}

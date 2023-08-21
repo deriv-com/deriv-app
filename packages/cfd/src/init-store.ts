bbc3728b92ddf9ef02eea266f31e7a5df0140bab
Hamza <hamza.muhammad@regentmarkets.com>
2023-08-21 17:03:21 +0800

    chore: migrating the initstore to typescript

--- packages/cfd/src/init-store.ts
+++ packages/cfd/src/init-store.ts
@@ -2,12 +2,14 @@ import { configure } from 'mobx';
 import RootStore from './Stores';
 import { setWebsocket } from '@deriv/shared';
 import ServerTime from '_common/base/server_time';
+import type { TCoreStores } from '@deriv/stores/types';
+import { TWebSocket } from '../types';
 
 configure({ enforceActions: 'observed' });
 
-let root_store;
+let root_store: RootStore;
 
-const initStore = (core_store, websocket) => {
+const initStore = (core_store: TCoreStores, websocket: TWebSocket) => {
     if (root_store) return root_store;
 
     ServerTime.init(core_store.common);
--- packages/cfd/types.ts
+++ packages/cfd/types.ts
@@ -1,6 +1,21 @@
+import { DetailsOfEachMT5Loginid } from '@deriv/api-types';
 import { Jurisdiction } from '@deriv/shared';
 import type { TCoreStores } from '@deriv/stores/types';
 
 export type TTradingPlatformAvailableAccount = TCoreStores['client']['trading_platform_available_accounts'][number];
 
 export type TJurisdiction = typeof Jurisdiction[keyof typeof Jurisdiction];
+
+export type TWebSocket = {
+    mt5LoginList: () => Promise<{
+        mt5_login_list: DetailsOfEachMT5Loginid[];
+    }>;
+    storage: {
+        mt5LoginList: () => Promise<{
+            mt5_login_list: DetailsOfEachMT5Loginid[];
+        }>;
+    };
+    tradingPlatformAccountsList: (platform: string) => Promise<{
+        trading_platform_accounts: (DetailsOfEachMT5Loginid & { account_id: string })[];
+    }>;
+};

d961b6f13514359c5633f2cfeac7e683e513fb67
Sergei Baranovski <sergei@deriv.com>
2023-08-21 10:22:29 +0300

    refactor: reverted changes back

--- packages/appstore/src/components/modals/real-wallets-upgrade/components/ready-to-upgrade-wallets/test/ready-to-upgrade-wallets.spec.tsx
+++ packages/appstore/src/components/modals/real-wallets-upgrade/components/ready-to-upgrade-wallets/test/ready-to-upgrade-wallets.spec.tsx
@@ -4,45 +4,31 @@ import ReadyToUpgradeWallets from '../ready-to-upgrade-wallets';
 import { StoreProvider, mockStore } from '@deriv/stores';
 
 const mock = mockStore({});
-const toggleCheckbox = jest.fn();
 
 describe('ReadyToUpgradeWallets', () => {
-    it('should render', () => {
+    const containerReadyToUpgradeWallets = () => {
+        const toggleCheckbox = jest.fn();
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <StoreProvider store={mock}>{children}</StoreProvider>
         );
 
-        const { container } = render(
-            <ReadyToUpgradeWallets is_eu={false} toggleCheckbox={toggleCheckbox} value={false} />,
-            {
-                wrapper,
-            }
-        );
+        return render(<ReadyToUpgradeWallets is_eu={false} toggleCheckbox={toggleCheckbox} value={false} />, {
+            wrapper,
+        });
+    };
 
+    it('should render', () => {
+        const { container } = containerReadyToUpgradeWallets();
         expect(container).toBeInTheDocument();
     });
 
     it('should render checkbox', () => {
-        const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mock}>{children}</StoreProvider>
-        );
-
-        render(<ReadyToUpgradeWallets is_eu={false} toggleCheckbox={toggleCheckbox} value={false} />, {
-            wrapper,
-        });
-
+        containerReadyToUpgradeWallets();
         expect(screen.getByRole('checkbox')).toBeInTheDocument();
     });
 
     it('should render info section based on region', () => {
-        const wrapper = ({ children }: { children: JSX.Element }) => (
-            <StoreProvider store={mock}>{children}</StoreProvider>
-        );
-
-        render(<ReadyToUpgradeWallets is_eu={false} toggleCheckbox={toggleCheckbox} value={false} />, {
-            wrapper,
-        });
-
+        containerReadyToUpgradeWallets();
         expect(
             screen.getByText(
                 'During the upgrade, deposits, withdrawals, transfers, and adding new accounts will be unavailable.'

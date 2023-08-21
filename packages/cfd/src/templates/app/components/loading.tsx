9b0f4c9c9c44eafce9b0da15197b516f6e0ea155
Hirad <hirad@regentmarkets.com>
2023-08-21 11:45:01 +0800

    fix: removed the redundant index from the map

--- packages/cfd/src/templates/app/components/loading.tsx
+++ packages/cfd/src/templates/app/components/loading.tsx
@@ -35,7 +35,7 @@ const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status,
                 ))}
             </div>
             {is_slow_loading &&
-                status.map((text, inx) => (
+                status.map(text => (
                     <Text as='h3' color='prominent' size='xs' align='center' key={id}>
                         {text}
                     </Text>

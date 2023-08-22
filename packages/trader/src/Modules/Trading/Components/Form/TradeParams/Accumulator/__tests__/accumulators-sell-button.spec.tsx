98541b628e280d57de2029d6258d4a0698b962c8
kate-deriv <kate@regentmarkets.com>
2023-08-22 14:51:00 +0300

    fix: tests

--- packages/trader/src/Modules/Trading/Components/Form/TradeParams/Accumulator/__tests__/accumulators-sell-button.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/TradeParams/Accumulator/__tests__/accumulators-sell-button.spec.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AccumulatorsSellButton from '../accumulators-sell-button';
+import { mockStore } from '@deriv/stores';
+import { TCoreStores } from '@deriv/stores/types';
+import TraderProviders from '../../../../../../../trader-providers';
 
-const mock_default_props = {
+const mock_default_props: React.ComponentProps<typeof AccumulatorsSellButton> = {
     is_disabled: false,
     onClick: jest.fn(),
+    contract_info: {
+        is_valid_to_sell: 1,
+        status: 'open',
+    },
     current_stake: 10,
     currency: 'USD',
 };
 
 describe('AccumulatorsSellButton', () => {
+    const mockAccumulatorsSellButton = (mocked_props: typeof mock_default_props) => {
+        return (
+            <TraderProviders store={mockStore({}) as TCoreStores}>
+                <AccumulatorsSellButton {...mocked_props} />
+            </TraderProviders>
+        );
+    };
+
     it('should render component', () => {
-        render(<AccumulatorsSellButton {...mock_default_props} />);
+        render(mockAccumulatorsSellButton(mock_default_props));
 
         expect(screen.getByRole('button')).toBeEnabled();
         expect(screen.getByText(/Sell/i)).toBeInTheDocument();
@@ -20,7 +35,7 @@ describe('AccumulatorsSellButton', () => {
     });
     it('should render component with disabled button and without current stake', () => {
         const new_mock_props = { ...mock_default_props, is_disabled: true, current_stake: null };
-        render(<AccumulatorsSellButton {...new_mock_props} />);
+        render(mockAccumulatorsSellButton(new_mock_props));
 
         expect(screen.getByRole('button')).toBeDisabled();
         expect(screen.getByText(/Sell/i)).toBeInTheDocument();

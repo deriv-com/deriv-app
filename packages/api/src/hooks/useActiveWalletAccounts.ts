9d871b2ace35da4bdebfae7dd91c1df654127e95
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-08-22 14:20:41 +0800

    refactor: return values of useActiveWalletAccounts

--- packages/api/src/hooks/useActiveWalletAccounts.ts
+++ packages/api/src/hooks/useActiveWalletAccounts.ts
@@ -3,11 +3,14 @@ import useWalletAccountsList from './useWalletAccountsList';
 
 /** A custom hook that returns the wallet object for the current active wallet. */
 const useActiveWalletAccount = () => {
-    const { data } = useWalletAccountsList();
+    const { data, ...rest } = useWalletAccountsList();
     const active_wallet = useMemo(() => data?.find(wallet => wallet.is_active), [data]);
 
-    /** User's current active wallet. */
-    return active_wallet;
+    return {
+        /** User's current active wallet. */
+        data: active_wallet,
+        ...rest,
+    };
 };
 
 export default useActiveWalletAccount;

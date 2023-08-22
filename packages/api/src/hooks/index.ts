0d4ae578d00ee3373569bd1a48ae7cd445a3f9f2
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-08-22 15:44:52 +0800

    chore: added useAvailableAccounts and useAccountTypes hooks into api package

--- packages/api/src/hooks/index.ts
+++ packages/api/src/hooks/index.ts
@@ -1,2 +1,4 @@
+export { default as useAccountTypes } from './useAccountTypes';
 export { default as useAuthorize } from './useAuthorize';
+export { default as useAvailableAccounts } from './useAvailableAccounts';
 export { default as useLandingCompany } from './useLandingCompany';
---
+++ packages/api/src/hooks/useAvailableAccounts.ts
@@ -0,0 +1,31 @@
+import { useMemo } from 'react';
+import useAccountTypes from './useAccountTypes';
+import useLandingCompany from './useLandingCompany';
+
+/** A custom hook to get available accounts for every landing companies */
+const useAvailableAccounts = () => {
+    const { data: landing_company_data } = useLandingCompany();
+    const { data: financial_accounts } = useAccountTypes(landing_company_data?.financial_company);
+    const { data: gaming_accounts } = useAccountTypes(landing_company_data?.gaming_company);
+    const { data: virtual_accounts } = useAccountTypes(landing_company_data?.virtual_company);
+
+    const available_accounts = useMemo(() => {
+        if (!financial_accounts && !gaming_accounts && !virtual_accounts) return;
+
+        return {
+            /** List of available financial accounts */
+            financial_accounts,
+            /** List of available gaming accounts */
+            gaming_accounts,
+            /** List of available virtual accounts */
+            virtual_accounts,
+        };
+    }, [financial_accounts, gaming_accounts, virtual_accounts]);
+
+    return {
+        /** List of available accounts */
+        data: available_accounts,
+    };
+};
+
+export default useAvailableAccounts;

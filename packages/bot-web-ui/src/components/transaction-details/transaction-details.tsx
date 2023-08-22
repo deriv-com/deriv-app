68fc8694404ad273aa124dbf216b55a3480212cc
shafin-deriv <shafin@regentmarkets.com>
2023-08-22 14:58:09 +0800

    chore: revert to original PR

--- packages/bot-web-ui/src/components/transaction-details/transaction-details.tsx
+++ packages/bot-web-ui/src/components/transaction-details/transaction-details.tsx
@@ -1,16 +1,14 @@
 import React, { Suspense } from 'react';
 import { Loading } from '@deriv/components';
 import { isMobile } from '@deriv/shared';
-import TransactionDetailsDesktop from './transaction-details-desktop';
-import TransactionDetailsMobile from './transaction-details-mobile';
 
-// const TransactionDetailsDesktop = React.lazy(
-//     () => import(/* webpackChunkName: "transaction-details-desktop" */ './transaction-details-desktop')
-// );
+const TransactionDetailsDesktop = React.lazy(
+    () => import(/* webpackChunkName: "transaction-details-desktop" */ './transaction-details-desktop')
+);
 
-// const TransactionDetailsMobile = React.lazy(
-//     () => import(/* webpackChunkName: "transaction-details-mobile" */ './transaction-details-mobile')
-// );
+const TransactionDetailsMobile = React.lazy(
+    () => import(/* webpackChunkName: "transaction-details-mobile" */ './transaction-details-mobile')
+);
 
 export default function TransactionDetails() {
     const is_mobile = isMobile();

8fe3ea4a8117b4a1a63984b286fda07824d7ac81
yashim-deriv <yashim@deriv.com>
2023-08-22 14:21:41 +0800

    chore: add deprecation notice

--- packages/translations/src/utils/localize.ts
+++ packages/translations/src/utils/localize.ts
@@ -1,6 +1,7 @@
 import { str as crc32 } from 'crc-32';
 import i18n from './i18n-instance';
 
+/** @deprecated Use the <Localize /> component instead. This doesn't follow the react lifecycle */
 export const localize = <T extends object>(string: string, values?: T) => {
     return i18n.t(crc32(string).toString(), { defaultValue: string, ...values });
 };

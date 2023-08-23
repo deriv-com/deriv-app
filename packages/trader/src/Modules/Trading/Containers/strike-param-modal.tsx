0d1a556f8e707a38b0218eb62b0ed5db4fdf71ec
maryia-deriv <maryia@binary.com>
2023-08-23 19:38:09 +0300

    fix: info tooltip state for StrikeParamModal

---
+++ packages/trader/src/Modules/Trading/Containers/strike-param-modal.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Localize, localize } from '@deriv/translations';
+import { Div100vhContainer, Modal, Popover, RadioGroup } from '@deriv/components';
+import classNames from 'classnames';
+
+type TStrikeParamModalProps = {
+    is_open: boolean;
+    toggleModal: () => void;
+    strike: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    name: string;
+    strike_price_list: { text: string; value: string }[];
+    vanilla_trade_type: string;
+};
+
+const StrikeParamModal = ({
+    is_open,
+    toggleModal,
+    strike,
+    onChange,
+    name,
+    strike_price_list,
+    vanilla_trade_type,
+}: TStrikeParamModalProps) => {
+    const [is_popover_open, setIsPopoverOpen] = React.useState(false);
+
+    const handleToggleModal = () => {
+        if (is_popover_open) setIsPopoverOpen(false);
+        toggleModal();
+    };
+
+    return (
+        <Modal
+            className='trade-params'
+            is_open={is_open}
+            should_header_stick_body={false}
+            is_title_centered
+            toggleModal={handleToggleModal}
+            height='auto'
+            width='calc(100vw - 32px)'
+            title={localize('Strike')}
+        >
+            <Div100vhContainer className='mobile-widget-dialog__wrapper' max_autoheight_offset='48px'>
+                <div className='trade-params__vanilla-ic-info-wrapper'>
+                    <Popover
+                        is_open={is_popover_open}
+                        alignment='bottom'
+                        icon='info'
+                        id='dt_vanilla-stake__tooltip'
+                        zIndex='9999'
+                        is_bubble_hover_enabled
+                        message={
+                            <Localize
+                                i18n_default_text='If you buy a "<0>{{trade_type}}</0>" option, you receive a payout at expiry if the final price is {{payout_status}} the strike price. Otherwise, your “<0>{{trade_type}}</0>” option will expire worthless.'
+                                components={[<strong key={0} />]}
+                                values={{
+                                    trade_type:
+                                        vanilla_trade_type === 'VANILLALONGCALL' ? localize('Call') : localize('Put'),
+                                    payout_status:
+                                        vanilla_trade_type === 'VANILLALONGCALL'
+                                            ? localize('above')
+                                            : localize('below'),
+                                }}
+                            />
+                        }
+                        classNameBubble='trade-params--modal-wrapper__content'
+                        onClick={() => setIsPopoverOpen(!is_popover_open)}
+                    />
+                </div>
+                <div className={classNames('trade-params__amount-keypad', 'trade-params--mobile-strike')}>
+                    <RadioGroup
+                        name={name}
+                        className='trade-params__amount--mobile'
+                        onToggle={onChange}
+                        selected={strike}
+                    >
+                        {strike_price_list.map(item => (
+                            <RadioGroup.Item key={item.value} value={String(item.value)} label={item.value} />
+                        ))}
+                    </RadioGroup>
+                </div>
+            </Div100vhContainer>
+        </Modal>
+    );
+};
+
+export default StrikeParamModal;

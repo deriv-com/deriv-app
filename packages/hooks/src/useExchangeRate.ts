e673ef49b64b67bdf68b57fc19a1a186d09ec9e0
Shayan Khaleghparast <iman@regentmarkets.com>
2023-08-23 08:46:21 +0330

    feat: calculating the delay

--- packages/hooks/src/useExchangeRate.ts
+++ packages/hooks/src/useExchangeRate.ts
@@ -8,12 +8,24 @@ import { useStore } from '@deriv/stores';
 const useExchangeRate = () => {
     const { exchange_rates } = useStore();
     const data = exchange_rates.data;
+    const last_updated = data?.date;
     const rates = data?.rates;
     const [initialRates, setInitialRates] = useState<typeof rates>(undefined); //this is a static data which we can use for places like total asset to avoid showing user the real time value
+    const [initialDate, setInitialDate] = useState<number | undefined>(undefined);
+    //this is a static data which we can use for places like total asset to avoid showing user the real time value
 
+    // console.log(last_updated, initialDate)
     useEffect(() => {
         if (rates) {
             if (!initialRates) setInitialRates(rates);
+            if (!initialDate && last_updated) setInitialDate(last_updated);
+        }
+
+        if (last_updated && initialDate) {
+            if (last_updated - initialDate > 20000) {
+                setInitialRates(rates);
+                setInitialDate(last_updated);
+            }
         }
     }, [rates]);
 

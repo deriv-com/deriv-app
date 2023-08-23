f6556e730918deb98675d5e9cc919167db59050d
thisyahlen <104053934+thisyahlen-deriv@users.noreply.github.com>
2023-08-23 15:16:08 +0800

    thisyahlen/feat: add useCurrencyConfig hook to deriv api (#9783)
    * chore: add useCurrencyConfig hook to deriv api
    
    * fix: imports
    
    * fix: empty trigger
    
    * fix: remove icon property
    
    * chore: deprecate useCurrencyConfig from hooks package

--- packages/api/src/hooks/index.ts
+++ packages/api/src/hooks/index.ts
@@ -2,4 +2,5 @@ export { default as useAccountsList } from './useAccountsList';
 export { default as useActiveWalletAccounts } from './useActiveWalletAccounts';
 export { default as useAuthorize } from './useAuthorize';
 export { default as useBalance } from './useBalance';
+export { default as useCurrencyConfig } from './useCurrencyConfig';
 export { default as useWalletAccountsList } from './useWalletAccountsList';

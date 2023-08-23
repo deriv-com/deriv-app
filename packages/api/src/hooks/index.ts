cf2b5a6744e2a7e510aa50b81f3273570af1f87d
Farhan Ahmad Nurzi <125247833+farhan-nurzi-deriv@users.noreply.github.com>
2023-08-23 15:08:09 +0800

    farhan/feat: added useWalletAccountsList and useActiveWalletAccounts to API package (#9772)
    * feat: added use-authorize hook taken from sergei pr
    
    Co-authored-by: Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
    
    * chore: sorted imports for use-authorize
    
    Co-authored-by: Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
    
    * chore: moved default empty string in use-authorize
    
    * chore: incorporated code reviews
    
    * chore: add useAccountsList to deriv/api
    
    * fix: address comments
    
    * chore: useWalletAccountsList and useActiveWalletAccountsList hooks
    
    Co-authored-by: Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
    
    * refactor: return values of useActiveWalletAccounts
    
    ---------
    
    Co-authored-by: adrienne-rio <adrienne@deriv.com>
    Co-authored-by: Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
    Co-authored-by: Thisyahlen Nair <thisyahlen@regentmarkets.com>

--- packages/api/src/hooks/index.ts
+++ packages/api/src/hooks/index.ts
@@ -1,3 +1,5 @@
 export { default as useAccountsList } from './useAccountsList';
+export { default as useActiveWalletAccounts } from './useActiveWalletAccounts';
 export { default as useAuthorize } from './useAuthorize';
 export { default as useBalance } from './useBalance';
+export { default as useWalletAccountsList } from './useWalletAccountsList';

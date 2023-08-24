d278604b7540334092792971a340e01ff27fc0d7
Sergei Baranovski <sergei@deriv.com>
2023-08-24 13:25:52 +0300

    refactor: rename all variables to snake_case

--- packages/components/src/components/gradient-background-two-point/gradient-background-two-point.tsx
+++ packages/components/src/components/gradient-background-two-point/gradient-background-two-point.tsx
@@ -3,26 +3,26 @@ import './gradient-background-two-point.scss';
 
 type TGradientBackgroundTwoPoint = {
     blur_radius?: number;
-    backgroundColor: CSSProperties['backgroundColor'];
-    primaryColor: CSSProperties['background'];
-    secondaryColor: CSSProperties['background'];
+    background_color: CSSProperties['backgroundColor'];
+    primary_color: CSSProperties['background'];
+    secondary_color: CSSProperties['background'];
 };
 
 const GradientBackgroundTwoPoint: React.FC<React.PropsWithChildren<TGradientBackgroundTwoPoint>> = ({
     children,
     blur_radius = 48,
-    backgroundColor,
-    primaryColor,
-    secondaryColor,
+    background_color,
+    primary_color,
+    secondary_color,
 }) => (
-    <div className='gradient-background-two-point' style={{ backgroundColor }}>
+    <div className='gradient-background-two-point' style={{ backgroundColor: background_color }}>
         <div
             className='gradient-background-two-point__primary'
-            style={{ filter: `blur(${blur_radius}px)`, background: primaryColor }}
+            style={{ filter: `blur(${blur_radius}px)`, background: primary_color }}
         />
         <div
             className='gradient-background-two-point__secondary'
-            style={{ filter: `blur(${blur_radius}px)`, background: secondaryColor }}
+            style={{ filter: `blur(${blur_radius}px)`, background: secondary_color }}
         />
         {children && <div className='gradient-background-two-point__children'>{children}</div>}
     </div>

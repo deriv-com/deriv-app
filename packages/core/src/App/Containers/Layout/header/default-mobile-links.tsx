480a39a0716b41f14fb3aea7db3acec879492688
Shaheer <shaheer@deriv.com>
2023-08-24 14:45:31 +0400

    refactor: :art: returns component directly without return statement

--- packages/core/src/App/Containers/Layout/header/default-mobile-links.tsx
+++ packages/core/src/App/Containers/Layout/header/default-mobile-links.tsx
@@ -10,33 +10,31 @@ type TDefaultMobileLinks = {
     handleClickCashier: () => void;
 };
 
-const DefaultMobileLinks = ({ handleClickCashier }: TDefaultMobileLinks) => {
-    return (
-        <React.Fragment>
-            <div className='trading-hub-header__menu-right--items--onboarding'>
-                <TradersHubOnboarding />
-            </div>
-            <div className='trading-hub-header__menu-right--items--notifications'>
-                <ShowNotifications />
-            </div>
-            <Popover
-                alignment='bottom'
-                classNameBubble='account-settings-toggle__tooltip'
-                message={localize('Manage account settings')}
-                should_disable_pointer_events
-                zIndex='9999'
-            >
-                <BinaryLink className='trading-hub-header__setting' to={routes.personal_details}>
-                    <Icon icon='IcUserOutline' size={20} />
-                </BinaryLink>
-            </Popover>
-            <div className='trading-hub-header__cashier-button'>
-                <Button primary small onClick={handleClickCashier}>
-                    <Localize i18n_default_text='Cashier' />
-                </Button>
-            </div>
-        </React.Fragment>
-    );
-};
+const DefaultMobileLinks = ({ handleClickCashier }: TDefaultMobileLinks) => (
+    <React.Fragment>
+        <div className='trading-hub-header__menu-right--items--onboarding'>
+            <TradersHubOnboarding />
+        </div>
+        <div className='trading-hub-header__menu-right--items--notifications'>
+            <ShowNotifications />
+        </div>
+        <Popover
+            alignment='bottom'
+            classNameBubble='account-settings-toggle__tooltip'
+            message={localize('Manage account settings')}
+            should_disable_pointer_events
+            zIndex='9999'
+        >
+            <BinaryLink className='trading-hub-header__setting' to={routes.personal_details}>
+                <Icon icon='IcUserOutline' size={20} />
+            </BinaryLink>
+        </Popover>
+        <div className='trading-hub-header__cashier-button'>
+            <Button primary small onClick={handleClickCashier}>
+                <Localize i18n_default_text='Cashier' />
+            </Button>
+        </div>
+    </React.Fragment>
+);
 
 export default DefaultMobileLinks;

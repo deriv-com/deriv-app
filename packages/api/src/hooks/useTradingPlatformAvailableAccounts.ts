f9c1381a4fba70f420dcf0d4780c65aaed755757
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-08-24 15:57:31 +0800

    chore: add useTradingPlatformAvailableAccounts to deriv api

---
+++ packages/api/src/hooks/useTradingPlatformAvailableAccounts.ts
@@ -0,0 +1,39 @@
+import { useMemo } from 'react';
+import useFetch from '../useFetch';
+
+/** @description This hook is used to get all the available MT5 accounts. */
+const useTradingPlatformAvailableAccounts = () => {
+    const { data: mt5_available_accounts, ...rest } = useFetch('trading_platform_available_accounts', {
+        payload: { platform: 'mt5' },
+    });
+
+    const modified_mt5_available_accounts = useMemo(
+        () =>
+            mt5_available_accounts?.trading_platform_available_accounts?.map(account => {
+                return {
+                    ...account,
+                };
+            }),
+        [mt5_available_accounts?.trading_platform_available_accounts]
+    );
+
+    /** This function is used to group the available MT5 accounts by market type. By `all`, `financial` and `gaming` */
+    const grouped_mt5_available_accounts = useMemo(() => {
+        return modified_mt5_available_accounts?.reduce((acc, account) => {
+            const { market_type } = account;
+            if (!acc[market_type as keyof typeof acc]) {
+                acc[market_type || 'gaming'] = [];
+            }
+            acc[market_type || 'gaming'].push(account);
+            return acc;
+        }, {} as Record<string, typeof modified_mt5_available_accounts>);
+    }, [modified_mt5_available_accounts]);
+
+    return {
+        /** The available MT5 accounts grouped by market type */
+        data: grouped_mt5_available_accounts,
+        ...rest,
+    };
+};
+
+export default useTradingPlatformAvailableAccounts;

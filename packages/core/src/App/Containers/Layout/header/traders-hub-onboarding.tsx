f4b1a16c0685a8c45d97cc58952fc95855ccf4d8
Shaheer <shaheer@deriv.com>
2023-08-25 15:05:29 +0400

    refactor: :art: removes unused classes

--- packages/core/src/App/Containers/Layout/header/traders-hub-onboarding.tsx
+++ packages/core/src/App/Containers/Layout/header/traders-hub-onboarding.tsx
@@ -12,26 +12,24 @@ const TradersHubOnboarding = observer(() => {
     const { is_dark_mode_on } = ui;
 
     return (
-        <div data-testid='dt_traders_hub_onboarding' className='trading-hub-header__tradershub--onboarding'>
-            <div className='trading-hub-header__tradinghub--onboarding--logo'>
-                <Popover
-                    classNameBubble='account-settings-toggle__tooltip'
-                    alignment='bottom'
-                    message={localize('View onboarding')}
-                    should_disable_pointer_events
-                    zIndex='9999'
-                >
-                    <Icon
-                        data_testid='dt_traders_hub_onboarding_icon'
-                        icon={is_dark_mode_on ? 'IcAppstoreTradingHubOnboardingDark' : 'IcAppstoreTradingHubOnboarding'}
-                        size={20}
-                        onClick={() => {
-                            history.push(routes.onboarding);
-                            setIsOnboardingVisited(false);
-                        }}
-                    />
-                </Popover>
-            </div>
+        <div data-testid='dt_traders_hub_onboarding'>
+            <Popover
+                classNameBubble='account-settings-toggle__tooltip'
+                alignment='bottom'
+                message={localize('View onboarding')}
+                should_disable_pointer_events
+                zIndex='9999'
+            >
+                <Icon
+                    data_testid='dt_traders_hub_onboarding_icon'
+                    icon={is_dark_mode_on ? 'IcAppstoreTradingHubOnboardingDark' : 'IcAppstoreTradingHubOnboarding'}
+                    size={20}
+                    onClick={() => {
+                        history.push(routes.onboarding);
+                        setIsOnboardingVisited(false);
+                    }}
+                />
+            </Popover>
         </div>
     );
 });

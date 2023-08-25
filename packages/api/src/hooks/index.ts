a26b7893b462b932c75338a0696a8c9cf22790d5
Farzin Mirzaie <farzin@deriv.com>
2023-08-25 14:38:46 +0800

    feat(api): :sparkles: add `useTradingAccountsList`, `useActiveTradingAccount`, and `useActiveAccount` hooks

--- packages/api/src/hooks/index.ts
+++ packages/api/src/hooks/index.ts
@@ -1,8 +1,11 @@
 export { default as useAccountsList } from './useAccountsList';
-export { default as useActiveWalletAccounts } from './useActiveWalletAccounts';
+export { default as useActiveAccount } from './useActiveAccount';
+export { default as useActiveTradingAccount } from './useActiveTradingAccount';
+export { default as useActiveWalletAccount } from './useActiveWalletAccount';
 export { default as useAuthorize } from './useAuthorize';
 export { default as useBalance } from './useBalance';
 export { default as useCurrencyConfig } from './useCurrencyConfig';
 export { default as useMT5LoginList } from './useMT5LoginList';
+export { default as useTradingAccountsList } from './useTradingAccountsList';
 export { default as useTradingPlatformAccounts } from './useTradingPlatformAccounts';
 export { default as useWalletAccountsList } from './useWalletAccountsList';

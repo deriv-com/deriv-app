ccef88f83078f30d79592221f7a1ba8c3f2f8a82
maryia-deriv <maryia@binary.com>
2023-08-25 18:19:41 +0300

    fix: mobile language switcher

--- packages/core/src/App/Components/Layout/Header/__tests__/platform-switcher.spec.tsx
+++ packages/core/src/App/Components/Layout/Header/__tests__/platform-switcher.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RouteComponentProps, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import PlatformSwitcher from '../platform-switcher';

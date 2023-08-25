f7153ff56bfe7e311a6cb3a343b6aed1e3b4b3ce
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-08-25 16:49:19 +0800

    chore: add wallet header list data for desktop

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { useFetch } from '@deriv/api';
+import WalletList from './components/WalletList';
 
 const AppContent: React.FC = () => {
-    const { data } = useFetch('time', { options: { refetchInterval: 1000 } });
-
     return (
-        <div
-            style={{
-                display: 'flex',
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                height: '100%',
-                fontSize: 30,
-            }}
-        >
-            <h1>Server Time</h1>
-            <br />
-            <br />
-            {data?.time && <h1>{new Date(data?.time * 1000).toLocaleString()}</h1>}
+        <div>
+            <WalletList />
         </div>
     );
 };

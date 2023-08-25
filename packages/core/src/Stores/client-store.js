c51ec527e77b9a7953bdfc937dce93e79f69c024
“Matin-deriv” <matin@deriv.com>
2023-08-25 15:13:39 +0800

    chore: backward compatible for MLT clients

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -799,7 +799,7 @@ export default class ClientStore extends BaseStore {
 
     get is_valid_login() {
         if (!this.is_logged_in) return true;
-        const valid_login_ids_regex = new RegExp('^(MF|VRTC|CR|FOG)[0-9]+$', 'i');
+        const valid_login_ids_regex = new RegExp('^(MF|VRTC|MLT|CR|FOG)[0-9]+$', 'i');
         return this.all_loginids.every(id => valid_login_ids_regex.test(id));
     }
 

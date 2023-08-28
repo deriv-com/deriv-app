ee24cefba178cf3aeea583aa4a88bf00e6de4083
Sergei Baranovski <sergei@deriv.com>
2023-08-28 09:04:50 +0300

    feat: resolve conflicts

--- packages/utils/src/getActiveLoginIDFromLocalStorage.ts
+++ packages/utils/src/getActiveLoginIDFromLocalStorage.ts
@@ -4,6 +4,9 @@
 const getActiveLoginIDFromLocalStorage = () => {
     const active_loginid = localStorage.getItem('active_loginid');
 
+    // If there is no active loginid, return undefined.
+    if (!active_loginid) return;
+
     return active_loginid;
 };
 

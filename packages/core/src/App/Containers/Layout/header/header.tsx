aca9eaaa4c63797ce557f5ec7d06ebe04ea59274
Shaheer <shaheer@deriv.com>
2023-08-28 17:02:05 +0400

    refactor: :pencil2: renames traders hub variable

--- packages/core/src/App/Containers/Layout/header/header.tsx
+++ packages/core/src/App/Containers/Layout/header/header.tsx
@@ -12,14 +12,14 @@ const Header = observer(() => {
     const { is_logged_in } = client;
     const { is_appstore } = React.useContext(PlatformContext);
     const { pathname } = useLocation();
-    const trading_hub_routes =
+    const traders_hub_routes =
         pathname === routes.traders_hub || pathname.startsWith(routes.account) || pathname.startsWith(routes.cashier);
 
     if (is_appstore) {
         return <DashboardHeader />;
     } else if (is_logged_in) {
         let result;
-        if (trading_hub_routes) {
+        if (traders_hub_routes) {
             result = <TradersHubHeader is_acc_switcher_disabled={false} />;
         } else if (pathname === routes.onboarding) {
             result = null;

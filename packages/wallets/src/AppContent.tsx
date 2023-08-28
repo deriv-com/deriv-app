22ec005fdf68606ea57af376c13b83dcabeb9aec
adrienne-rio <adrienne@deriv.com>
2023-08-28 16:09:38 +0800

    refactor: added assets folder

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { useFetch } from '@deriv/api';
+import IcBrandingDerivEzDashboard from './assets/ic-branding-derivez-dashboard.svg';
 
+export const getUrlBase = (path = '') => {
+    const l = window.location;
+
+    if (!/^\/(br_)/.test(l.pathname)) return path;
+
+    return `/${l.pathname.split('/')[1]}${/^\//.test(path) ? path : `/${path}`}`;
+};
 const AppContent: React.FC = () => {
     const { data } = useFetch('time', { options: { refetchInterval: 1000 } });
 
@@ -16,6 +24,7 @@ const AppContent: React.FC = () => {
                 fontSize: 30,
             }}
         >
+            <IcBrandingDerivEzDashboard width={50} height={50} />
             <h1>Server Time</h1>
             <br />
             <br />

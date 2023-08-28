dafc8fd44dcf404d07c3dbf0b446fd4e41184e5c
Sergei Baranovski <sergei@deriv.com>
2023-08-28 11:29:15 +0300

    feat: change useAuthorize to follow api package

--- packages/hooks/src/useAuthorize.ts
+++ packages/hooks/src/useAuthorize.ts
@@ -5,11 +5,10 @@ import { getActiveAuthTokenIDFromLocalStorage } from '@deriv/utils';
 /** A custom hook that authorize the user with the given token. If no token is given, it will use the current token. */
 const useAuthorize = (token?: string) => {
     const current_token = getActiveAuthTokenIDFromLocalStorage();
-    const auth_token = token || current_token || '';
 
     const { data, ...rest } = useFetch('authorize', {
-        payload: { authorize: auth_token },
-        options: { keepPreviousData: true, enabled: Boolean(auth_token) },
+        payload: { authorize: token || current_token || '' },
+        options: { keepPreviousData: true, enabled: Boolean(current_token) },
     });
 
     // Add additional information to the authorize response.

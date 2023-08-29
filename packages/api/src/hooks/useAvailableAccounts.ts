e6d8009bc4ee3015ff2d1af083b3656ee3417202
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-08-29 14:20:34 +0800

    refactor: separate accounts usememo

--- packages/api/src/hooks/useAvailableAccounts.ts
+++ packages/api/src/hooks/useAvailableAccounts.ts
@@ -5,11 +5,30 @@ import useLandingCompany from './useLandingCompany';
 /** A custom hook to get available accounts for every landing companies */
 const useAvailableAccounts = () => {
     const { data: landing_company_data } = useLandingCompany();
-    const { data: financial_accounts } = useAccountTypes(landing_company_data?.financial_company_shortcode);
-    const { data: virtual_accounts } = useAccountTypes(landing_company_data?.virtual_company_shortcode);
+    const { data: available_financial_accounts } = useAccountTypes(landing_company_data?.financial_company_shortcode);
+    const { data: available_virtual_accounts } = useAccountTypes(landing_company_data?.virtual_company_shortcode);
 
+    // memoize the available financial accounts
+    const financial_accounts = useMemo(() => {
+        if (!available_financial_accounts) return;
+
+        return {
+            ...available_financial_accounts,
+        };
+    }, [available_financial_accounts]);
+
+    // memoize the available virtual accounts
+    const virtual_accounts = useMemo(() => {
+        if (!available_virtual_accounts) return;
+
+        return {
+            ...available_virtual_accounts,
+        };
+    }, [available_virtual_accounts]);
+
+    // memoize the combined available accounts
     const available_accounts = useMemo(() => {
-        if (!financial_accounts && !virtual_accounts) return;
+        if (!available_financial_accounts && !available_virtual_accounts) return;
 
         return {
             /** List of available financial accounts */
@@ -17,7 +36,7 @@ const useAvailableAccounts = () => {
             /** List of available virtual accounts */
             virtual_accounts,
         };
-    }, [financial_accounts, virtual_accounts]);
+    }, [available_financial_accounts, available_virtual_accounts]);
 
     return {
         /** List of available accounts */

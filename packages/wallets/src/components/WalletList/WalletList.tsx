8bcc89bded14af8f2f396ccbeca3a9cb251c45f2
Hamid <hamid@regentmarkets.com>
2023-08-30 11:22:57 +0330

    fix: overwritten styles

--- packages/wallets/src/components/WalletList/WalletList.tsx
+++ packages/wallets/src/components/WalletList/WalletList.tsx
@@ -5,18 +5,18 @@ import './WalletList.scss';
 const WalletList: React.FC = () => {
     const { data } = useWalletAccountsList();
 
-    if (!data.length) return <h1>No wallets found</h1>;
+    if (!data.length) return <h1 className='wallet-list-title'>No wallets found</h1>;
 
     return (
-        <div className='account-list'>
+        <div className='wallet-list-account-list'>
             {data?.map(account => {
                 return (
-                    <div className='account-item' key={account.loginid}>
-                        <div className='currency'>{account.currency}</div>
+                    <div className='wallet-list-account-item' key={account.loginid}>
+                        <div className='wallet-list-currency'>{account.currency}</div>
                         <br />
-                        <div className='account-category'>{account.landing_company_name}</div>
+                        <div className='wallet-list-account-category'>{account.landing_company_name}</div>
                         <br />
-                        <div className='balance'>{account.balance}</div>
+                        <div className='wallet-list-balance'>{account.balance}</div>
                     </div>
                 );
             })}

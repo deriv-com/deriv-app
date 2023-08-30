abaffc885238f58fb3191f5915b978fffcddd70f
Sergei Baranovski <sergei@deriv.com>
2023-08-30 21:10:36 +0300

    feat: decline eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
                 'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
                 'plugin:react/recommended',
                 'plugin:@typescript-eslint/recommended',
-                // 'plugin:prettier/recommended',
+                'plugin:prettier/recommended',
             ],
             parserOptions: {
                 ecmaversion: 2018,

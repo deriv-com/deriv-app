acf0997201adf2dab4e9642166096abaaa2ae9fe
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-08-30 10:12:20 +0800

    chore: add more styles

--- packages/wallets/src/components/WalletHeaderList/WalletHeader.tsx
+++ packages/wallets/src/components/WalletHeaderList/WalletHeader.tsx
@@ -8,7 +8,7 @@ const WalletHeader: React.FC = () => {
     if (!data.length) return <h1>No wallets found</h1>;
 
     return (
-        <>
+        <React.Fragment>
             {data?.map(account => {
                 return (
                     <div className='wallet-list__card_container' key={account.loginid}>
@@ -17,6 +17,20 @@ const WalletHeader: React.FC = () => {
                                 <div className='wallet-list__wallet-icon'>
                                     <p> Icon (Placeholder)</p>
                                 </div>
+                                <div className='wallet-list__action-container'>
+                                    <div className='wallet-list__title'>
+                                        {account.currency_config?.display_code} Wallet
+                                        <div className='wallet-list__badge'>
+                                            <p>{account.landing_company_name?.toLocaleUpperCase()}</p>
+                                        </div>
+                                    </div>
+                                    <div className='wallet-list__actions'>
+                                        <p>Deposit</p>
+                                        <p>Withdraw</p>
+                                        <p>Transfer</p>
+                                        <p>Transactions</p>
+                                    </div>
+                                </div>
                             </div>
                             <WalletCardBalance account={account} />
                             <div className='wallet-list__toggle'>V</div>
@@ -24,7 +38,7 @@ const WalletHeader: React.FC = () => {
                     </div>
                 );
             })}
-        </>
+        </React.Fragment>
     );
 };
 

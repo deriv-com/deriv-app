1a5c7e6efe2ca88ca93aa7411b543d67e7461ce0
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-09-01 14:59:01 +0400

    refactor: refactoring spec file

--- packages/hooks/src/__tests__/useP2POrderList.spec.tsx
+++ packages/hooks/src/__tests__/useP2POrderList.spec.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { APIProvider, usePaginatedFetch } from '@deriv/api';
 import { StoreProvider, mockStore } from '@deriv/stores';
 import { renderHook } from '@testing-library/react-hooks';
-import useP2PAdvertList from '../useP2PAdvertList';
 import useP2POrderList from '../useP2POrderList';
 
 type TWrapper = {
@@ -19,7 +18,6 @@ const mockUsePaginatedFetch = usePaginatedFetch as jest.MockedFunction<typeof us
 describe('useP2POrdertList', () => {
     it('should return undefined if there is no response', () => {
         const mock_store = mockStore({});
-        // @ts-expect-error need to come up with a way to mock the return type of usePaginatedFetch
         mockUsePaginatedFetch.mockReturnValue({});
 
         const wrapper = ({ children }: TWrapper) => (

0ab3cbb1686a00ebe23e901d615f43822777d66e
Aum Bhatt <aum@deriv.com>
2023-09-01 17:18:56 +0400

    feat: logic for adding the account names to messages

--- packages/appstore/src/components/wallet-transfer/components/wallet-transfer-messages/wallet-transfer-messages.tsx
+++ packages/appstore/src/components/wallet-transfer/components/wallet-transfer-messages/wallet-transfer-messages.tsx
@@ -1,51 +1,34 @@
 import React from 'react';
 import { useTransferMessageList } from '@deriv/hooks';
 import { localize } from '@deriv/translations';
-import { AnimatedList } from '@deriv/components';
-import { AlertMessage } from '@deriv/components';
+import { AnimatedList, AlertMessage } from '@deriv/components';
 import { getAccountName } from 'Constants/utils';
 import { formatMoney } from '@deriv/shared';
 
-const message_code_to_message_mapper: any = {
-    WalletToTradingAppDailyLimit: (
-        is_first_transfer: boolean,
-        limit: string,
-        currency: string,
-        from_name: string,
-        to_name: string
-    ) =>
+const message_code_to_message_mapper = {
+    WalletToTradingAppDailyLimit: value =>
         localize(
             'The{{remaining}} daily transfer limit between your {{wallet_name}} and {{trading_account_name}} is {{limit_value}} {{currency}}.',
             {
-                remaining: !is_first_transfer ? ' remaining' : '',
-                wallet_name: from_name,
-                trading_account_name: to_name,
-                limit_value: formatMoney(currency, limit, true),
-                currency: currency,
+                remaining: !value.is_first_transfer ? ' remaining' : '',
+                wallet_name: value.from_name,
+                trading_account_name: value.to_name,
+                limit_value: formatMoney(value.currency, value.limit, true),
+                currency: value.currency,
             }
         ),
-    DemoWalletToTradingAppDailyLimit: (
-        is_first_transfer: boolean,
-        limit: string,
-        currency: string,
-        from_name: string,
-        to_name: string
-    ) =>
+    DemoWalletToTradingAppDailyLimit: value =>
         localize('Your{{remaining}} daily transfer limit for virtual funds is {{limit_value}} {{currency}}.', {
-            remaining: !is_first_transfer ? ' remaining' : '',
-            wallet_name: from_name,
-            trading_account_name: to_name,
-            limit_value: formatMoney(currency, limit, true),
-            currency: currency,
+            remaining: !value.is_first_transfer ? ' remaining' : '',
+            wallet_name: value.from_name,
+            trading_account_name: value.to_name,
+            limit_value: formatMoney(value.currency, value.limit, true),
+            currency: value.currency,
         }),
 };
 
-const WalletTransferMessages = ({ from_account, to_account, setMessageList }: any) => {
+const WalletTransferMessages = ({ from_account, to_account }: any) => {
     const { data: message_list } = useTransferMessageList(from_account, to_account);
-
-    React.useEffect(() => {
-        // setMessageList(message_list);
-    }, [message_list]);
 
     // Translation and currency_exchange happens here
 
@@ -61,19 +44,15 @@ const WalletTransferMessages = ({ from_account, to_account, setMessageList }: an
         <AnimatedList>
             {message_list &&
                 message_list.map((msg: any) => {
-                    console.log('=> message', msg);
                     return (
                         <AlertMessage
                             key={msg.code}
                             variant={msg.cta ? 'with-action-button' : 'base'}
-                            button_label='deposit'
-                            message={message_code_to_message_mapper[msg.code](
-                                msg.is_first_transfer,
-                                msg.limit,
-                                msg.currency,
-                                msg.mt5_market_type
-                            )}
-                            onClickHandler={() => {}}
+                            message={message_code_to_message_mapper[msg.code]({
+                                ...msg,
+                                from_name: getAccountName({ ...from_account }),
+                                to_name: getAccountName({ ...to_account }),
+                            })}
                             type={msg.type}
                         />
                     );

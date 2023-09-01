d5cab089716b7dc576a67b89d3f3d5c4b380f474
amina-deriv <amina@regentmarkets.com>
2023-09-01 17:31:22 +0400

    feat: back side

--- packages/appstore/src/components/modals/mt5-migration-modal/mt5-migration-modal.tsx
+++ packages/appstore/src/components/modals/mt5-migration-modal/mt5-migration-modal.tsx
@@ -7,7 +7,9 @@ import {
     UILoader,
     Text,
     Icon,
-    FormSubmitButton,
+    Button,
+    Checkbox,
+    StaticUrl,
 } from '@deriv/components';
 import { Localize, localize } from '@deriv/translations';
 import { observer, useStore } from '@deriv/stores';
@@ -61,8 +63,116 @@ const MT5MigrationModal = observer(() => {
     } = useMT5SVGEligibleToMigrate();
 
     const { disableApp, enableApp, is_mt5_migration_modal_open } = ui;
-
+    const [show_modal_front_side, setShowModalFrontSide] = React.useState(true);
+    const [is_checked, setIsChecked] = React.useState(false);
     const modal_title = <Localize i18n_default_text='Enhancing your trading experience' />;
+
+    const ShowFrontSide = () => (
+        <React.Fragment>
+            <div>
+                <div className='mt5-migration-modal__description'>
+                    <Text as='p' color='general' size='s' align='center'>
+                        <Localize
+                            i18n_default_text='We’re upgrading your SVG account(s) by moving them to the {{eligible_account_to_migrate}} jurisdiction.'
+                            values={{
+                                eligible_account_to_migrate,
+                            }}
+                        />
+                    </Text>
+                </div>
+                <div className='mt5-migration-modal__migration_content'>
+                    <div className='mt5-migration-modal__migration_content-items'>
+                        {eligible_svg_to_bvi_derived_accounts && <MigrateAccount to='bvi' type='derived' />}
+                        {eligible_svg_to_bvi_financial_accounts && <MigrateAccount to='bvi' type='financial' />}
+                    </div>
+                    <div className='mt5-migration-modal__migration_content-items'>
+                        {eligible_svg_to_vanuatu_derived_accounts && <MigrateAccount to='vanuatu' type='derived' />}
+                        {eligible_svg_to_vanuatu_financial_accounts && <MigrateAccount to='vanuatu' type='financial' />}
+                    </div>
+                </div>
+                <div>
+                    <Text as='p' align='center'>
+                        <Localize
+                            i18n_default_text='Click <0>Next</0> to start your transition.'
+                            components={[<strong key={0} />]}
+                        />
+                    </Text>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+
+    const ShowBackSide = () => (
+        <React.Fragment>
+            <div>
+                <div className='mt5-migration-modal__description'>
+                    <Text as='p' color='general' size='s' align='center' weight='bold'>
+                        <Localize i18n_default_text='What will happen to the funds in my existing account(s)?' />
+                    </Text>
+                </div>
+                <div className='mt5-migration-modal__existing-accounts'>
+                    <div className='mt5-migration-modal__existing-accounts-card'>
+                        <div className='mt5-migration-modal__existing-accounts-card-content'>
+                            <Text as='div' weight='bold'>
+                                <Localize i18n_default_text='If you have open positions' />
+                            </Text>
+
+                            <Text as='div' size='xs'>
+                                <Localize i18n_default_text='Your funds will remain in your existing MT5 account(s).' />
+                            </Text>
+
+                            <Text as='div' size='xs'>
+                                <Localize i18n_default_text='You can continue trading on your existing MT5 account(s) until you close all open positions.' />
+                            </Text>
+
+                            <Text as='div' size='xs'>
+                                <Localize i18n_default_text='New MT5 account(s) under the BVI jurisdiction will be created for future trades.' />
+                            </Text>
+                        </div>
+                    </div>
+                    <div className='mt5-migration-modal__existing-accounts-card'>
+                        <div className='mt5-migration-modal__existing-accounts-card-content'>
+                            <Text weight='bold'>
+                                <Localize i18n_default_text='If you have open positions' />
+                            </Text>
+
+                            <Text as='p' size='xs'>
+                                <Localize i18n_default_text='Your funds will remain in your existing MT5 account(s).' />
+                            </Text>
+
+                            <Text as='p' size='xs'>
+                                <Localize i18n_default_text='You can continue trading on your existing MT5 account(s) until you close all open positions.' />
+                            </Text>
+
+                            <Text as='p' size='xs'>
+                                <Localize i18n_default_text='New MT5 account(s) under the BVI jurisdiction will be created for future trades.' />
+                            </Text>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=' mt5-migration-modal__existing-accounts-card-content'>
+                        <Checkbox
+                            value={is_checked}
+                            onChange={() => setIsChecked(true)}
+                            label={
+                                <Text as='p' size={'xs'} line_height='xs'>
+                                    <Localize
+                                        i18n_default_text='I agree to move my MT5 account(s) and agree to Deriv BVI Ltd’s <0>terms and conditions</0>'
+                                        components={[
+                                            <StaticUrl key={0} className='link' href={'tnc/deriv-(bvi)-ltd.pdf'} />,
+                                        ]}
+                                    />
+                                </Text>
+                            }
+                            defaultChecked={!!is_checked}
+                        />
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+
     return (
         <div>
             <React.Suspense fallback={<UILoader />}>
@@ -74,57 +184,37 @@ const MT5MigrationModal = observer(() => {
                     is_open={true}
                     title={modal_title}
                     width='58.8rem'
-                    height={no_of_svg_accounts_to_migrate < 1 ? '54.2rem' : '44rem'}
+                    height={
+                        show_modal_front_side ? (no_of_svg_accounts_to_migrate < 1 ? '54.2rem' : '44rem') : '61.6rem'
+                    }
                 >
-                    <div>
-                        <div className='mt5-migration-modal__description'>
-                            <Text as='p' color='general' size='s' align='center'>
-                                <Localize
-                                    i18n_default_text='We’re upgrading your SVG account(s) by moving them to the {{eligible_account_to_migrate}} jurisdiction.'
-                                    values={{
-                                        eligible_account_to_migrate,
-                                    }}
-                                />
-                            </Text>
-                        </div>
-                        <div className='mt5-migration-modal__migration_content'>
-                            <div className='mt5-migration-modal__migration_content-items'>
-                                {eligible_svg_to_bvi_derived_accounts && <MigrateAccount to='bvi' type='derived' />}
-                                {eligible_svg_to_bvi_financial_accounts && <MigrateAccount to='bvi' type='financial' />}
-                            </div>
-                            <div className='mt5-migration-modal__migration_content-items'>
-                                {eligible_svg_to_vanuatu_derived_accounts && (
-                                    <MigrateAccount to='vanuatu' type='derived' />
-                                )}
-                                {eligible_svg_to_vanuatu_financial_accounts && (
-                                    <MigrateAccount to='vanuatu' type='financial' />
-                                )}
-                            </div>
-                        </div>
-                        <div>
-                            <Text as='p' align='center'>
-                                <Localize
-                                    i18n_default_text='Click <0>Next</0> to start your transition.'
-                                    components={[<strong key={0} />]}
-                                />
-                            </Text>
-                        </div>
-                    </div>
+                    {show_modal_front_side ? <ShowFrontSide /> : <ShowBackSide />}
+
                     {/* </Modal.Body> */}
-                    <Modal.Footer has_separator>
-                        <FormSubmitButton
-                            // is_disabled={isSubmitDisabled(errors)}
-                            label={localize('Next')}
-                            // is_absolute={isMobile()}
-                        />
-                    </Modal.Footer>
+
+                    {show_modal_front_side ? (
+                        <Modal.Footer has_separator>
+                            <Button type='button' large primary onClick={() => setShowModalFrontSide(false)}>
+                                Next
+                            </Button>
+                        </Modal.Footer>
+                    ) : (
+                        <Modal.Footer has_separator>
+                            <Button type='button' large secondary onClick={() => setShowModalFrontSide(true)}>
+                                Back
+                            </Button>
+
+                            <Button
+                                type='button'
+                                large
+                                primary
+                                onClick={() => setShowModalFrontSide(false)} // need to toggle
+                            >
+                                Next
+                            </Button>
+                        </Modal.Footer>
+                    )}
                 </Modal>
-                {/* </DesktopWrapper> */}
-                {/* <MobileWrapper>
-                    <MobileDialog portal_element_id='deriv_app' title={'modal_title'} visible={true}>
-                        <div>Test</div>
-                    </MobileDialog>
-                </MobileWrapper> */}
             </React.Suspense>
         </div>
     );

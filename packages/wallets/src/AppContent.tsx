a98cd2f19d9742ef4a8bad296dd43e7bdbec7a99
Farzin Mirzaie <farzin@deriv.com>
2023-09-01 16:26:48 +0800

    feat(core): :recycle: server `@deriv/wallets` in `@deriv/core`

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuthorize } from '@deriv/api';
 import WalletsCarousel from './components/WalletCarousel';
 import WalletList from './components/WalletList';
-import IcTest from './public/ic-test.svg';
+// import IcTest from './public/ic-test.svg';
 import './app-content.scss';
 
 const AppContent: React.FC = () => {
@@ -13,7 +13,7 @@ const AppContent: React.FC = () => {
     return (
         <div>
             <div className='wallets-app-content-icon' />
-            <IcTest width={25} height={25} />
+            {/* <IcTest width={25} height={25} /> */}
             <WalletList />
             <WalletsCarousel />
         </div>

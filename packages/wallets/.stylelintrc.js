bce6542967e29db05417afd63175b95ec079f60e
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-09-02 17:56:41 +0800

    fix: lint styles

--- packages/wallets/.stylelintrc.js
+++ packages/wallets/.stylelintrc.js
@@ -5,7 +5,7 @@ module.exports = {
             // selectors must be prefixed with "wallets-" to avoid name conflicts in other packages
             '^wallets-[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
             {
-                resolveNestedSelectors: true,
+                resolveNestedSelectors: false,
                 message: 'Expected selector to match BEM CSS pattern and to be prefixed with "wallets-"',
             },
         ],

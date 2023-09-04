6111f73527d90cc83f61e52714f2715dcfa84198
Hamza <hamza.muhammad@regentmarkets.com>
2023-09-04 15:03:51 +0800

    chore: useCreateOtherCFDAccount and useCreateMT5Account hooks added

---
+++ packages/api/src/hooks/useCreateOtherCFDAccount.ts
@@ -0,0 +1,24 @@
+import { useMemo } from 'react';
+import useRequest from '../useRequest';
+import useInvalidateQuery from '../useInvalidateQuery';
+
+/** A custom hook that creates the Other CFD account. */
+const useCreateOtherCFDAccount = () => {
+    const invalidate = useInvalidateQuery();
+    const { data, ...rest } = useRequest('trading_platform_new_account', {
+        onSuccess: () => {
+            invalidate('trading_platform_accounts');
+        },
+    });
+
+    // Add additional information to the create Other CFD account response.
+    const modified_data = useMemo(() => {
+        if (!data) return undefined;
+
+        return { ...data };
+    }, [data]);
+
+    return { data: modified_data, ...rest };
+};
+
+export default useCreateOtherCFDAccount;

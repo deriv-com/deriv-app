c4c70de9481af88fbe7d1dafe9f7163e69baa032
thisyahlen <104053934+thisyahlen-deriv@users.noreply.github.com>
2023-09-04 16:03:09 +0800

    thisyahlen/chore: update ui for wallet header (#9870)
    * chore: update ui for wallet header
    
    * chore: add more styles
    
    * chore: add more styles v2
    
    * fix: lint errors
    
    * chore: split components
    
    * chore: split v2
    
    * chore: split v3
    
    * chore: add svg for header button actions
    
    * fix: format balance and css styles
    
    * fix: landing company name uppercase
    
    * fix: comments
    
    * chore: split moreeeee
    
    * fix: rename css
    
    * fix: lint styles
    
    * fix: styles
    
    * fix: comments
    
    * fix: comments v3
    
    * fix: comments v4
    
    * fix: comments v5

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import WalletList from './components/WalletList';
-import WalletsCarousel from './components/WalletsCarousel';
-import IcBrandDerivGo from './public/ic-brand-derivgo.svg';
-import './app-content.scss';
+import { DesktopWalletsList } from './components';
+// import WalletsCarousel from './components/WalletCarousel';
+import './AppContent.scss';
 
 const AppContent: React.FC = () => {
     return (
-        <div>
-            <div className='wallets-app-content-icon' />
-            <IcBrandDerivGo width={25} height={25} />
-            <WalletList />
-            <WalletsCarousel />
+        <div className='wallets-app'>
+            <div className='wallets-app__content'>
+                <DesktopWalletsList />
+            </div>
+            {/* <WalletsCarousel /> */}
         </div>
     );
 };

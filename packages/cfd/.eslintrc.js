d8ccf9c90a552463068fd7be4e8c045c3e52fc2a
Hirad <hirad@regentmarkets.com>
2023-09-05 17:22:33 +0800

    chore: fixed the cfd-logic package issues

---
+++ packages/cfd/.eslintrc.js
@@ -0,0 +1,10 @@
+const webpackConfig = require('./build/webpack.config-test.js');
+
+module.exports = {
+    extends: ['../../.eslintrc.js'],
+    settings: {
+        'import/resolver': {
+            webpack: { config: webpackConfig({}) },
+        },
+    },
+};

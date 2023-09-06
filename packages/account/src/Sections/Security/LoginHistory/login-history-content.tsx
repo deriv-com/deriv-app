937886d3a5a09e38ee630e616515a49eb39ade67
Taysuisin <suisin@regentmarkets.com>
2023-09-06 18:38:07 +0800

    chore: move LoginHistoryContent to a file

---
+++ packages/account/src/Sections/Security/LoginHistory/login-history-content.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Localize } from '@deriv/translations';
+import { Table } from '@deriv/components';
+import { TData } from '@deriv/api/src/hooks/useLoginHistory';
+import LoginHistoryTableRow from './login-history-table-row';
+import LoginHistoryListRow from './login-history-list-row';
+
+type TGetFields = {
+    [key: string]: JSX.Element;
+};
+
+type TLoginHistoryContent = {
+    data: TData;
+    is_mobile: boolean;
+};
+
+const fields = {
+    date: 'Date and time',
+    action: 'Action',
+    browser: 'Browser',
+    ip: 'IP address',
+    status: 'Status',
+};
+
+const getFields = () =>
+    Object.fromEntries(
+        Object.entries(fields).map(([key, value]) => [
+            key,
+            <Localize i18n_default_text='{{value}}' values={{ value }} key={key} />,
+        ])
+    );
+
+const LoginHistoryContent = ({ data, is_mobile }: TLoginHistoryContent) => {
+    if (is_mobile) {
+        return renderList(data);
+    }
+    return renderTable(getFields(), data);
+};
+
+const renderTable = (fields: TGetFields, login_history: TData) => (
+    <Table fixed className='login-history__table'>
+        <Table.Header>
+            <Table.Row className='login-history__table__header'>
+                {Object.values(fields).map((field, index) => (
+                    <Table.Head key={index}>{field}</Table.Head>
+                ))}
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {login_history.map(item => (
+                <LoginHistoryTableRow key={item.id} {...item} />
+            ))}
+        </Table.Body>
+    </Table>
+);
+
+const renderList = (login_history: TData) => {
+    return (
+        <Table className='login-history__list'>
+            <Table.Body>
+                {login_history.map(item => {
+                    return <LoginHistoryListRow key={item.id} {...item} />;
+                })}
+            </Table.Body>
+        </Table>
+    );
+};
+
+export default LoginHistoryContent;
--- packages/account/src/Sections/Security/LoginHistory/login-history.tsx
+++ packages/account/src/Sections/Security/LoginHistory/login-history.tsx
@@ -1,73 +1,9 @@
 import React from 'react';
-import { Loading, Table, ThemedScrollbars } from '@deriv/components';
+import { Loading, ThemedScrollbars } from '@deriv/components';
 import { observer, useStore } from '@deriv/stores';
-import { Localize } from '@deriv/translations';
-import { TData } from '@deriv/api/src/hooks/useLoginHistory';
 import { useLoginHistory } from '@deriv/api';
+import LoginHistoryContent from './login-history-content';
 import LoadErrorMessage from 'Components/load-error-message';
-import LoginHistoryTableRow from './login-history-table-row';
-import LoginHistoryListRow from './login-history-list-row';
-
-export type TGetFields = {
-    [key: string]: JSX.Element;
-};
-
-type TLoginHistoryContent = {
-    data: TData;
-    is_mobile: boolean;
-};
-
-const fields = {
-    date: 'Date and time',
-    action: 'Action',
-    browser: 'Browser',
-    ip: 'IP address',
-    status: 'Status',
-};
-
-const getFields = () =>
-    Object.fromEntries(
-        Object.entries(fields).map(([key, value]) => [
-            key,
-            <Localize i18n_default_text='{{value}}' values={{ value }} key={key} />,
-        ])
-    );
-
-const LoginHistoryContent = ({ data, is_mobile }: TLoginHistoryContent) => {
-    if (is_mobile) {
-        return renderList(data);
-    }
-    return renderTable(getFields(), data);
-};
-
-const renderTable = (fields: TGetFields, login_history: TData) => (
-    <Table fixed className='login-history__table'>
-        <Table.Header>
-            <Table.Row className='login-history__table__header'>
-                {Object.values(fields).map((field, index) => (
-                    <Table.Head key={index}>{field}</Table.Head>
-                ))}
-            </Table.Row>
-        </Table.Header>
-        <Table.Body>
-            {login_history.map(item => (
-                <LoginHistoryTableRow key={item.id} {...item} />
-            ))}
-        </Table.Body>
-    </Table>
-);
-
-const renderList = (login_history: TData) => {
-    return (
-        <Table className='login-history__list'>
-            <Table.Body>
-                {login_history.map(item => {
-                    return <LoginHistoryListRow key={item.id} {...item} />;
-                })}
-            </Table.Body>
-        </Table>
-    );
-};
 
 const LoginHistory = observer(() => {
     const {

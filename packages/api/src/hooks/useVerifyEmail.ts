e871a26347a903f234cdc4e7b09fbba61ff7ccf8
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-09-07 12:38:36 +0800

    chore: add useverifyemail hook to api package

---
+++ packages/api/src/hooks/useVerifyEmail.ts
@@ -0,0 +1,20 @@
+import { useCallback } from 'react';
+import { useRequest } from '@deriv/api';
+
+type TPayload = Parameters<ReturnType<typeof useRequest<'verify_email'>>['mutate']>[0]['payload'];
+
+/** A custom hook for verifying email address */
+const useVerifyEmail = () => {
+    const { mutate: _mutate, ...rest } = useRequest('verify_email');
+
+    /**  @param payload - The payload to be sent to the server */
+    const mutate = useCallback((payload: TPayload) => _mutate({ payload }), [_mutate]);
+
+    return {
+        /** The mutation function that accepts a payload and sends it to the server */
+        mutate,
+        ...rest,
+    };
+};
+
+export default useVerifyEmail;

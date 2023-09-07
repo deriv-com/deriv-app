401cddc85a5e1fe7555ec60929f6de3a93265d53
Mahdiyeh Amirkhani <mahdiyehamirkhani@Mahdiyeh-Amirkhanis-Mac-J79XQHKXVJ.local>
2023-09-07 18:47:51 +0800

    chore: add momemt

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
                     {
                         groups: [
                             // Packages `react` related packages come first.
-                            ['^react', '^@?\\w'],
+                            ['^react', '^moment', '^@?\\w'],
                             // Internal packages.
                             ['^(@|components)(/.*|$)'],
                             // Side effect imports.

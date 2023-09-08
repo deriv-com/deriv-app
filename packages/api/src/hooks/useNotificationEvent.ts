6eb6bf589af18f8122953022b425b00984a9a43a
Likhith Kolayari <likhith@regentmarkets.com>
2023-09-08 17:42:34 +0400

    chore: :memo: Added documentation for UseNotificationEvent hook

--- packages/api/src/hooks/useNotificationEvent.ts
+++ packages/api/src/hooks/useNotificationEvent.ts
@@ -5,6 +5,11 @@ import { TSocketRequestPayload } from '../../types';
 
 type TNotificationEventPayload = TSocketRequestPayload<'notification_event'>['payload'];
 
+/**
+ * Hook to send notification event to the server
+ * @name useNotificationEvent
+ * @returns response, mutation function and other properties from useRequest hook
+ */
 const useNotificationEvent = () => {
     const invalidate = useInvalidateQuery();
     const { data, mutate, ...rest } = useRequest('notification_event', {
@@ -12,6 +17,11 @@ const useNotificationEvent = () => {
             invalidate('notification_event');
         },
     });
+
+    /**
+     * Function to send notification event to the server
+     * @param payload - notification event payload
+     */
     const send = React.useCallback((payload: TNotificationEventPayload) => mutate({ payload }), [mutate]);
 
     return {

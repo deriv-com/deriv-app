dad8f1610e7b2098943c5d6dc94abc7ba38addd0
Shahzaib <shahzaib@deriv.com>
2023-09-08 17:54:36 +0800

    chore: refactored object.freeze to as const

--- packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-utils.ts
+++ packages/account/src/Sections/Verification/ProofOfIdentity/proof-of-identity-utils.ts
@@ -1,20 +1,20 @@
-export const identity_status_codes = Object.freeze({
+export const identity_status_codes = {
     none: 'none',
     pending: 'pending',
     rejected: 'rejected',
     verified: 'verified',
     expired: 'expired',
     suspected: 'suspected',
-});
+} as const;
 
-export const submission_status_code = Object.freeze({
+export const submission_status_code = {
     selecting: 'selecting',
     submitting: 'submitting',
     complete: 'complete',
-});
+} as const;
 
-export const service_code = Object.freeze({
+export const service_code = {
     idv: 'idv',
     onfido: 'onfido',
     manual: 'manual',
-});
+} as const;

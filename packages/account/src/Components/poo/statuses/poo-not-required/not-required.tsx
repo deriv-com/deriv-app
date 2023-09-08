9607fd1ffb93e86b40eeeb48459f24e8a4f20a7a
Shaheer <shaheer@deriv.com>
2023-09-08 11:45:28 +0400

    refactor: :art: returns component without return statement

--- packages/account/src/Components/poo/statuses/poo-not-required/not-required.tsx
+++ packages/account/src/Components/poo/statuses/poo-not-required/not-required.tsx
@@ -3,18 +3,16 @@ import { Icon } from '@deriv/components';
 import { Localize } from '@deriv/translations';
 import IconMessageContent from 'Components/icon-message-content';
 
-const POONotRequired = () => {
-    return (
-        <div className='proof-of-ownership__container'>
-            <IconMessageContent
-                message={<Localize i18n_default_text="Your proof of ownership isn't required." />}
-                text={
-                    <Localize i18n_default_text='You are not required to submit proof of ownership at this time. We will inform you if proof of ownership is required in the future.' />
-                }
-                icon={<Icon icon='IcPooVerified' size={128} />}
-            />
-        </div>
-    );
-};
+const POONotRequired = () => (
+    <div className='proof-of-ownership__container'>
+        <IconMessageContent
+            message={<Localize i18n_default_text="Your proof of ownership isn't required." />}
+            text={
+                <Localize i18n_default_text='You are not required to submit proof of ownership at this time. We will inform you if proof of ownership is required in the future.' />
+            }
+            icon={<Icon icon='IcPooVerified' size={128} />}
+        />
+    </div>
+);
 
 export default POONotRequired;

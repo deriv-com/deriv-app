43d74875ec7c139c74cc59e4bcb7b7dc95a2e29e
heorhi-deriv <heorhi@binary.com>
2023-09-12 16:36:29 +0300

    refactor: :recycle: refactor wallet upgrade props

--- packages/appstore/src/components/modals/real-wallets-upgrade/components/modal-elements/mobile-real-wallets-upgrade.tsx
+++ packages/appstore/src/components/modals/real-wallets-upgrade/components/modal-elements/mobile-real-wallets-upgrade.tsx
@@ -1,53 +1,26 @@
 import React from 'react';
 import { MobileDialog, Modal } from '@deriv/components';
 import { observer, useStore } from '@deriv/stores';
-import { TRealWalletsUpgrade } from 'Types';
+import { TRealWalletsUpgradeSteps } from 'Types';
 import WalletsUpgradeContent from './wallets-upgrade-content';
 import WalletsUpgradeFooter from './wallets-upgrade-footer';
 
-const MobileRealWalletsUpgrade = observer(
-    ({
-        current_step,
-        handleBack,
-        handleClose,
-        handleNext,
-        is_disabled,
-        toggleCheckbox,
-        upgradeToWallets,
-    }: TRealWalletsUpgrade) => {
-        const { traders_hub: is_real_wallets_upgrade_on } = useStore();
-        return (
-            <MobileDialog
-                portal_element_id='modal_root'
-                visible={is_real_wallets_upgrade_on}
-                onClose={handleClose}
-                wrapper_classname='wallet-steps'
-                footer={
-                    <WalletsUpgradeFooter
-                        handleClose={handleClose}
-                        handleNext={handleNext}
-                        handleBack={handleBack}
-                        toggleCheckbox={toggleCheckbox}
-                        upgradeToWallets={upgradeToWallets}
-                        is_disabled={is_disabled}
-                        current_step={current_step}
-                    />
-                }
-            >
-                <Modal.Body className='wallet-steps'>
-                    <WalletsUpgradeContent
-                        handleClose={handleClose}
-                        handleNext={handleNext}
-                        handleBack={handleBack}
-                        toggleCheckbox={toggleCheckbox}
-                        upgradeToWallets={upgradeToWallets}
-                        is_disabled={is_disabled}
-                        current_step={current_step}
-                    />
-                </Modal.Body>
-            </MobileDialog>
-        );
-    }
-);
+const MobileRealWalletsUpgrade = observer(({ wallet_upgrade_steps }: TRealWalletsUpgradeSteps) => {
+    const { traders_hub: is_real_wallets_upgrade_on } = useStore();
+
+    return (
+        <MobileDialog
+            portal_element_id='modal_root'
+            visible={is_real_wallets_upgrade_on}
+            onClose={wallet_upgrade_steps.handleClose}
+            wrapper_classname='wallet-steps'
+            footer={<WalletsUpgradeFooter wallet_upgrade_steps={wallet_upgrade_steps} />}
+        >
+            <Modal.Body className='wallet-steps'>
+                <WalletsUpgradeContent wallet_upgrade_steps={wallet_upgrade_steps} />
+            </Modal.Body>
+        </MobileDialog>
+    );
+});
 
 export default MobileRealWalletsUpgrade;

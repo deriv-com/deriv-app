a92e7e40b1745b7bcd4b7a98e5be7c83fd6cc12f
Likhith Kolayari <likhith@regentmarkets.com>
2023-09-12 18:01:47 +0400

    revert: :beers: removed icon changes

--- packages/components/stories/icon/icons.js
+++ packages/components/stories/icon/icons.js
@@ -300,7 +300,6 @@ export const icons =
         'IcBotBuilderArrow',
         'IcBotBuilderTabIcon',
         'IcBotBuilder',
-        'IcBotStop',
         'IcBox',
         'IcButtonBack',
         'IcCalendarDatefrom',

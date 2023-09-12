4b1becf40a5a736efdc5c76d85603c9c5979d589
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-09-12 11:40:03 +0800

    chore: comments

--- packages/wallets/src/components/ProgressBar/ProgressBar.tsx
+++ packages/wallets/src/components/ProgressBar/ProgressBar.tsx
@@ -11,14 +11,14 @@ type TProps = {
 const ProgressBar: React.FC<TProps> = ({ is_transition, active_index, indexes, setActiveIndex }) => {
     return (
         <div className='wallets-progress-bar'>
-            {indexes.map(st => {
-                const is_active = st === active_index;
+            {indexes.map(idx => {
+                const is_active = idx === active_index;
 
                 const barClassName = is_active ? 'wallets-progress-bar-active' : 'wallets-progress-bar-inactive';
                 return (
                     <div
-                        key={`progress-bar__${st}`}
-                        onClick={() => setActiveIndex(st)}
+                        key={`progress-bar__${idx}`}
+                        onClick={() => setActiveIndex(idx)}
                         className={`${barClassName} ${is_transition ? 'wallets-progress-bar-transition' : ''}`}
                     />
                 );

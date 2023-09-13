66270f9f7b12082aa1d72d7e7de2e96c056302d1
Nada <nada.ajinas@deriv.com>
2023-09-13 16:49:57 +0400

    fix: udpated function

--- packages/p2p/src/utils/adverts.ts
+++ packages/p2p/src/utils/adverts.ts
@@ -8,30 +8,34 @@ import { api_error_codes } from 'Constants/api-error-codes';
  * @returns {string} The title for the error dialog
  */
 export const generateErrorDialogTitle = (error_code: string): string => {
-    if (error_code === api_error_codes.ADVERT_SAME_LIMITS) {
-        return localize('You already have an ad with this range');
-    } else if (error_code === api_error_codes.DUPLICATE_ADVERT) {
-        return localize('You already have an ad with this rate');
+    switch (error_code) {
+        case api_error_codes.ADVERT_SAME_LIMITS:
+            return localize('You already have an ad with this range');
+        case api_error_codes.DUPLICATE_ADVERT:
+            return localize('You already have an ad with this rate');
+        default:
+            return localize("Something's not right");
     }
-    return localize("Something's not right");
 };
 
 /**
- * Function that generates the error message for the error dialog based on the given error code and message.
+ * Function to generate the error message for the error dialog based on the given error code and message.
  *
  * @param {string} error_code - The error code to determine the error message for.
  * @param {string} error_message - The custom error message to be displayed, if applicable.
  * @returns {string} The error message for the dialog.
  */
 export const generateErrorDialogBody = (error_code: string, error_message?: string): string => {
-    if (error_code === api_error_codes.ADVERT_SAME_LIMITS) {
-        return localize(
-            'Please set a different minimum and/or maximum order limit. \n\nThe range of your ad should not overlap with any of your active ads.'
-        );
-    } else if (error_code === api_error_codes.DUPLICATE_ADVERT) {
-        return localize(
-            'You already have an ad with the same exchange rate for this currency pair and order type. \n\nPlease set a different rate for your ad.'
-        );
+    switch (error_code) {
+        case api_error_codes.ADVERT_SAME_LIMITS:
+            return localize(
+                'Please set a different minimum and/or maximum order limit. \n\nThe range of your ad should not overlap with any of your active ads.'
+            );
+        case api_error_codes.DUPLICATE_ADVERT:
+            return localize(
+                'You already have an ad with the same exchange rate for this currency pair and order type. \n\nPlease set a different rate for your ad.'
+            );
+        default:
+            return error_message ?? localize("Something's not right");
     }
-    return error_message ?? localize("Something's not right");
 };

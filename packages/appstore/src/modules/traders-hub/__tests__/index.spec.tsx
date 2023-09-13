010f71c2fff0369dd10df7d2d716cd7c03d3c901
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-09-13 17:20:49 +0800

    chore: updated test case

--- packages/appstore/src/modules/traders-hub/__tests__/index.spec.tsx
+++ packages/appstore/src/modules/traders-hub/__tests__/index.spec.tsx
@@ -9,7 +9,17 @@ jest.mock('Components/cfds-listing', () => jest.fn(() => 'mockedCFDsListing'));
 jest.mock('Components/options-multipliers-listing', () => jest.fn(() => 'mocked<OptionsAndMultipliersListing>'));
 
 describe('TradersHub', () => {
-    const render_container = (mock_store_override = {}) => {
+    const mock_store_override = {
+        client: { is_logged_in: true },
+        traders_hub: { is_eu_user: false, content_flag: ContentFlag.LOW_RISK_CR_EU },
+        modules: {
+            cashier: {
+                account_transfer: { is_transfer_confirm: false },
+                general_store: { is_loading: false },
+            },
+        },
+    };
+    const render_container = () => {
         const mock_store = mockStore(mock_store_override);
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <StoreProvider store={mock_store}>{children}</StoreProvider>
@@ -21,37 +31,34 @@ describe('TradersHub', () => {
     };
 
     it('should display the component', () => {
-        const { container } = render_container({ client: { is_logged_in: true } });
+        const { container } = render_container();
+        mock_store_override.client.is_logged_in = true;
         expect(container).toBeInTheDocument();
     });
 
     it('should display both CFDs and Multipliers section', () => {
-        render_container({ client: { is_logged_in: true } });
+        render_container();
         const dashboard_sections = screen.getByTestId('dt_traders_hub');
         expect(dashboard_sections?.textContent?.match(/Multipliers/)).not.toBeNull();
         expect(dashboard_sections?.textContent?.match(/CFDs/)).not.toBeNull();
     });
 
     it('should display Multipliers and CFDs section in order if the user is non eu', () => {
-        render_container({
-            client: { is_logged_in: true },
-            traders_hub: { is_eu_user: false },
-        });
+        render_container();
+        mock_store_override.traders_hub.is_eu_user = false;
         const dashboard_sections = screen.getByTestId('dt_traders_hub');
         expect(dashboard_sections).not.toHaveClass('traders-hub__main-container-reversed');
     });
 
     it('should display Multipliers and CFDs section in reverse order if the user is eu', () => {
-        render_container({ client: { is_logged_in: true }, traders_hub: { is_eu_user: true } });
+        render_container();
+        mock_store_override.traders_hub.is_eu_user = true;
         const dashboard_sections = screen.getByTestId('dt_traders_hub');
-        expect(dashboard_sections).toHaveClass('traders-hub__main-container-reversed');
+        expect(dashboard_sections).toHaveClass('traders-hub__main-container');
     });
 
     it('should display disclaimer if the user is from low risk eu country', () => {
-        render_container({
-            client: { is_logged_in: true },
-            traders_hub: { content_flag: ContentFlag.LOW_RISK_CR_EU },
-        });
+        render_container();
         const disclaimer = screen.getByTestId('dt_traders_hub_disclaimer');
         expect(disclaimer).toBeInTheDocument();
     });

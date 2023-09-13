af5c83233ffac9cf3be4272655bebb6fcba99867
ameerul hady <ameerul@deriv.com>
2023-09-13 12:34:30 +0800

    chore: annotated types

--- packages/p2p/src/utils/validations.ts
+++ packages/p2p/src/utils/validations.ts
@@ -5,7 +5,7 @@
  * @param {String} value - The string to validate as a decimal number.
  * @returns {boolean} A boolean indicating the string is a valid decimal number.
  */
-export const decimalValidator = (value: string) => /^(\d+\.)?\d+$/.test(value);
+export const decimalValidator = (value: string): boolean => /^(\d+\.)?\d+$/.test(value);
 
 /**
  * Validates the length of the text is between 1 and 300 characters.
@@ -13,14 +13,14 @@ export const decimalValidator = (value: string) => /^(\d+\.)?\d+$/.test(value);
  * @param {String} value - The string to validate length.
  * @returns {boolean} A boolean indicating the length of the text is valid.
  */
-export const lengthValidator = (value: string) => value.length >= 1 && value.length <= 300;
+export const lengthValidator = (value: string): boolean => value.length >= 1 && value.length <= 300;
 
 /**
  * Validates whether a string contains only letters, numbers, spaces, and certain punctuation marks.
  * @param {String} value - The string to validate text.
  * @returns {boolean} A boolean indicating the text is valid.
  */
-export const textValidator = (value: string) => /^[\p{L}\p{Nd}\s'.,:;()@#+/-]*$/u.test(value);
+export const textValidator = (value: string): boolean => /^[\p{L}\p{Nd}\s'.,:;()@#+/-]*$/u.test(value);
 
 /**
  * Validates if the given value falls within the set range and returns a boolean.
@@ -29,7 +29,7 @@ export const textValidator = (value: string) => /^[\p{L}\p{Nd}\s'.,:;()@#+/-]*$/
  * @param {Number} limit - The limit to validate against.
  * @returns {boolean} A boolean indicating if the value is within the set range.
  */
-export const rangeValidator = (input: number, limit: number) => input >= limit * -1 && input <= limit;
+export const rangeValidator = (input: number, limit: number): boolean => input >= limit * -1 && input <= limit;
 
 /**
  * Validates floating-point integers in input box and checks if the string contains only
@@ -38,5 +38,5 @@ export const rangeValidator = (input: number, limit: number) => input >= limit *
  * @param {String} value - The value to validate as a floating-point integer.
  * @returns {boolean} A boolean indicating if the value is a valid floating-point integer.
  */
-export const floatingPointValidator = (value: string) =>
+export const floatingPointValidator = (value: string): boolean =>
     ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', '.'].includes(value) || /^\d*\.?\d+$/.test(value);

71e4dd23c2df122cb6a12cee246d45f2734b49b1
Henry Hein <henry@regentmarkets.com>
2023-09-14 14:19:31 +0800

    fix: remove comment code

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -7,8 +7,6 @@ import './AppContent.scss';
 const AppContent: React.FC = () => {
     const { is_mobile } = useDevice();
 
-    // if (is_mobile) return <WalletsCarousel />;
-
     return (
         <div className='wallets-app'>
             <div className='wallets-app__content'>{is_mobile ? <WalletsCarousel /> : <DesktopWalletsList />}</div>\

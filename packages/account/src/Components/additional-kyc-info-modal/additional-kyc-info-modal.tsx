a91a97843f7228aec342221481e111872ba71605
Shahzaib <shahzaib@deriv.com>
2023-09-14 18:44:09 +0800

    chore: make modal title bold for mobile

--- packages/account/src/Components/additional-kyc-info-modal/additional-kyc-info-modal.tsx
+++ packages/account/src/Components/additional-kyc-info-modal/additional-kyc-info-modal.tsx
@@ -87,7 +87,7 @@ export const AdditionalKycInfoModal = observer(() => {
                         is_open
                         portal_id='deriv_app'
                         header={
-                            <Text size='xs' align='left'>
+                            <Text size='xs' align='left' weight='bold'>
                                 <ModalTitle />
                             </Text>
                         }

cf08c7e563e2399aa4ea20a91e722443bf7d6685
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-09-14 14:53:26 +0800

    fix: edge cases for the landing company in desktop and mobile

--- packages/wallets/src/components/WalletCard/WalletCard.tsx
+++ packages/wallets/src/components/WalletCard/WalletCard.tsx
@@ -10,6 +10,10 @@ type TProps = {
 
 const WalletCard: React.FC<TProps> = ({ account }) => {
     const { wallet_currency_type, landing_company_name, currency, display_balance } = account || {};
+
+    const formattedLandingCompany =
+        landing_company_name === 'virtual' ? 'Demo' : landing_company_name?.toUpperCase() || 'SVG';
+
     return (
         <div className='wallets-card' key={account.loginid}>
             <div className='wallets-card__data'>
@@ -17,7 +21,9 @@ const WalletCard: React.FC<TProps> = ({ account }) => {
                     <div className='wallets-card__data__details-container'>
                         <WalletListCardIcon type={wallet_currency_type} />
                         <div className='wallets-card__data__details-landing_company'>
-                            {landing_company_name && <WalletListCardBadge label={landing_company_name} />}
+                            {landing_company_name && (
+                                <WalletListCardBadge label={formattedLandingCompany} is_demo={account?.is_virtual} />
+                            )}
                         </div>
                     </div>
                     <div className='wallets-card__data__details-balance'>

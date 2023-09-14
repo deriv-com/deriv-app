9cad6535a079a3a318929fbf5c4364ac24035766
Henry Hein <henry@regentmarkets.com>
2023-09-14 08:51:12 +0800

    fix: subtask

--- packages/reports/src/Containers/open-positions.tsx
+++ packages/reports/src/Containers/open-positions.tsx
@@ -463,7 +463,7 @@ const OpenPositions = ({
     error,
     getPositionById,
     is_accumulator,
-    is_eu: show_accu_in_dropdown,
+    is_eu: hide_accu_in_dropdown,
     is_loading,
     is_multiplier,
     NotificationMessages,
@@ -489,7 +489,7 @@ const OpenPositions = ({
     const is_accumulator_selected = contract_type_value === contract_types[2].text;
     const is_multiplier_selected = contract_type_value === contract_types[1].text;
     const contract_types_list = contract_types
-        .filter(contract_type => contract_type.text !== localize('Accumulators') || show_accu_in_dropdown)
+        .filter(contract_type => contract_type.text !== localize('Accumulators') || !hide_accu_in_dropdown)
         .map(({ text }) => ({ text, value: text }));
     const accumulators_rates_list = accumulator_rates.map(value => ({ text: value, value }));
     const active_positions_filtered = active_positions?.filter(({ contract_info }) => {
@@ -640,7 +640,7 @@ const OpenPositions = ({
                                     onChange={e => setContractTypeValue(e.target.value)}
                                 />
                             </div>
-                            {is_accumulator_selected && show_accu_in_dropdown && (
+                            {is_accumulator_selected && !hide_accu_in_dropdown && (
                                 <div className='open-positions__accumulator-container__rates-dropdown'>
                                     <Dropdown
                                         is_align_text_left
@@ -670,7 +670,7 @@ const OpenPositions = ({
                                     setContractTypeValue(e.target.value)
                                 }
                             />
-                            {is_accumulator_selected && show_accu_in_dropdown && (
+                            {is_accumulator_selected && !hide_accu_in_dropdown && (
                                 <SelectNative
                                     className='open-positions__accumulator-container--mobile__rates-dropdown'
                                     list_items={accumulators_rates_list}

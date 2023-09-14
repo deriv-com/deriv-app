e787575bfd005eee09ab061d2014aa4c7b91d100
thisyahlen <104053934+thisyahlen-deriv@users.noreply.github.com>
2023-09-14 11:45:37 +0800

    thisyahlen/chore: add Currency Icons to each wallet (#10058)
    * chore: add walletcurrencyicons
    
    * chore: refactor
    
    * chore: add gradient to mobile
    
    * chore: comments
    
    * chore: comments

--- packages/wallets/src/components/WalletCard/WalletCard.tsx
+++ packages/wallets/src/components/WalletCard/WalletCard.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { useWalletAccountsList } from '@deriv/api';
 import { WalletListCardBadge } from '../WalletListCardBadge';
+import { WalletListCardIcon } from '../WalletListCardIcon';
 import './WalletCard.scss';
 
 type TProps = {
-    account: NonNullable<ReturnType<typeof useWalletAccountsList>['data']>[number];
+    account: ReturnType<typeof useWalletAccountsList>['data'][number];
 };
 
 const WalletCard: React.FC<TProps> = ({ account }) => {
+    const { wallet_currency_type, landing_company_name, currency, display_balance } = account || {};
     return (
         <div className='wallets-card' key={account.loginid}>
             <div className='wallets-card__data'>
                 <div className='wallets-card__data__details'>
-                    <div className='wallets-card__data__details-icon'>
-                        <p>{account.currency}</p>
+                    <div className='wallets-card__data__details-container'>
+                        <WalletListCardIcon type={wallet_currency_type} />
+                        <div className='wallets-card__data__details-landing_company'>
+                            {landing_company_name && <WalletListCardBadge label={landing_company_name} />}
+                        </div>
                     </div>
-
                     <div className='wallets-card__data__details-balance'>
-                        <p>{account.currency} Wallet</p>
+                        <p>{currency} Wallet</p>
                         <p>
-                            {account.display_balance} {account.currency}
+                            {display_balance} {currency}
                         </p>
-                    </div>
-                    <div className='wallets-card__data__details-landing_company'>
-                        {account?.landing_company_name && <WalletListCardBadge label={account?.landing_company_name} />}
                     </div>
                 </div>
             </div>

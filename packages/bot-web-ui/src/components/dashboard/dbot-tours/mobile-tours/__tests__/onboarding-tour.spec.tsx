f6e579e4180a9f6acda6a33afca59baa11a4f6b1
rupato-deriv <rupato@regentmarkets.com>
2023-09-15 16:45:43 +0800

    fix: added disable sort

--- packages/bot-web-ui/src/components/dashboard/dbot-tours/mobile-tours/__tests__/onboarding-tour.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/dbot-tours/mobile-tours/__tests__/onboarding-tour.spec.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line simple-import-sort/imports
 import React from 'react';
 import { mockStore, StoreProvider } from '@deriv/stores';
 import { render, screen, waitFor } from '@testing-library/react';
@@ -7,6 +6,7 @@ import { mock_ws } from 'Utils/mock';
 import RootStore from 'Stores/root-store';
 import { DBotStoreProvider, mockDBotStore } from 'Stores/useDBotStore';
 import OnboardingTour from '../onboarding-tour';
+// eslint-disable-next-line simple-import-sort/imports
 import '@testing-library/react/dont-cleanup-after-each';
 
 jest.mock('@deriv/bot-skeleton/src/scratch/blockly', () => jest.fn());

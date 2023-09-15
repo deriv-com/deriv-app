081013c5775a5d49a9dd7bdd012e257e6641c17e
Farzin <farzin@deriv.com>
2023-09-15 17:14:04 +0800

    feat(api): add `standalone` support

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,6 @@ import useQuery from './useQuery';
 import useMutation from './useMutation';
 
 export { default as APIProvider } from './APIProvider';
-export { default as useAPI } from './useAPI';
 export { default as useInvalidateQuery } from './useInvalidateQuery';
 export { default as usePaginatedFetch } from './usePaginatedFetch';
 export { default as useSubscription } from './useSubscription';
--- packages/api/src/useAPI.ts
+++ packages/api/src/useAPI.ts
@@ -10,16 +10,12 @@ import type {
 const useAPI = () => {
     const api = useContext(APIContext);
 
-    if (!api) {
-        throw new Error('useAPI must be used within APIProvider');
-    }
-
     const send = useCallback(
         async <T extends TSocketEndpointNames>(
             name: T,
             payload?: TSocketRequestPayload<T>
         ): Promise<TSocketResponseData<T>> => {
-            const response = await api.send({ [name]: 1, ...(payload || {}) });
+            const response = await api?.send({ [name]: 1, ...(payload || {}) });
 
             if (response.error) {
                 throw response.error;
@@ -43,7 +39,7 @@ const useAPI = () => {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 onError: (response: any) => void
             ) => { unsubscribe?: VoidFunction };
-        } => api.subscribe({ [name]: 1, subscribe: 1, ...(payload || {}) }),
+        } => api?.subscribe({ [name]: 1, subscribe: 1, ...(payload || {}) }),
         [api]
     );
 

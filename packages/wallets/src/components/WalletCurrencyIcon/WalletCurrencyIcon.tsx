cab994c6923b31b462bcf43585b4bde64df7dac3
adrienne-rio <adrienne@deriv.com>
2023-09-18 17:58:38 +0800

    chore: removed lazy loading currency icons

--- packages/wallets/src/components/WalletCurrencyIcon/WalletCurrencyIcon.tsx
+++ packages/wallets/src/components/WalletCurrencyIcon/WalletCurrencyIcon.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import currencies from './currencies';
+import AUDIcon from '../../public/images/currencies/aud.svg';
+import BTCIcon from '../../public/images/currencies/btc.svg';
+import ETHIcon from '../../public/images/currencies/eth.svg';
+import EURIcon from '../../public/images/currencies/eur.svg';
+import TetherIcon from '../../public/images/currencies/eusdt.svg';
+import GBPIcon from '../../public/images/currencies/gbp.svg';
+import LTCIcon from '../../public/images/currencies/ltc.svg';
+import USDIcon from '../../public/images/currencies/usd.svg';
+import USDCIcon from '../../public/images/currencies/usdc.svg';
+
+const currencies = {
+    aud: AUDIcon,
+    btc: BTCIcon,
+    eth: ETHIcon,
+    eur: EURIcon,
+    eusdt: TetherIcon,
+    ltc: LTCIcon,
+    usd: USDIcon,
+    gbp: GBPIcon,
+    usdc: USDCIcon,
+    tusdt: TetherIcon,
+    ust: TetherIcon,
+};
 
 type TWalletCurrencyIconProps = {
     currency: string;
 };
 
 const WalletCurrencyIcon = ({ currency }: TWalletCurrencyIconProps) => {
-    const LazyCurrencyIcon = currencies[currency as keyof typeof currencies];
-    if (LazyCurrencyIcon) {
+    const CurrencyIcon = React.useMemo(() => currencies[currency as keyof typeof currencies], [currency]);
+
+    if (CurrencyIcon) {
         return (
             <div className='wallets-currency-icon'>
-                <React.Suspense>
-                    <LazyCurrencyIcon />
-                </React.Suspense>
+                <CurrencyIcon />
             </div>
         );
     }

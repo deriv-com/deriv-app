e9f5fc46436545251d8c1eaeba6f4986f1a4301c
nada-deriv <122768621+nada-deriv@users.noreply.github.com>
2023-09-18 13:12:03 +0400

    Nada/p2 ps 234/buy sell ads message (#8273)
    * fix: change content for my-ads empty
    
    * fix: pr comments fixed
    
    * feat: refactor code
    
    * feat: added unit test, style fix
    
    * feat: reorder imports
    
    * fix: issue with sendbird mock
    
    * fix: route to buy tab only in buy sell section
    
    * fix: failing test

--- packages/p2p/src/components/buy-sell/no-ads/no-ads.jsx
+++ packages/p2p/src/components/buy-sell/no-ads/no-ads.jsx
@@ -6,53 +6,51 @@ import { Localize } from 'Components/i18next';
 import { useStores } from 'Stores';
 import { routes } from '@deriv/shared';
 import './no-ads.scss';
+import classNames from 'classnames';
 
-const NoAds = () => {
+const NoAds = ({ is_ads_page = false }) => {
     const { buy_sell_store, general_store, my_ads_store } = useStores();
+    const { handleTabClick, is_barred } = general_store;
+    const { is_buy, local_currencies, selected_local_currency, setCreateSellAdFromNoAds } = buy_sell_store;
+    const { setShowAdForm } = my_ads_store;
     const history = useHistory();
 
-    const is_default_currency = buy_sell_store.local_currencies.filter(
-        currency =>
-            currency.text.toLowerCase() === buy_sell_store.selected_local_currency.toLowerCase() && currency.is_default
+    const is_default_currency = local_currencies.filter(
+        currency => currency.text.toLowerCase() === selected_local_currency?.toLowerCase() && currency.is_default
     ).length;
 
+    const onClickButton = () => {
+        if (!is_ads_page) handleTabClick(2);
+        if (is_buy && !is_ads_page) setCreateSellAdFromNoAds(true);
+        setShowAdForm(true);
+        history.push(routes.p2p_my_ads);
+    };
+
     return (
-        <div className='no-ads'>
+        <div className={classNames('no-ads', { 'ads-page': is_ads_page })}>
             <Icon icon='IcCashierNoAds' size={128} />
-            {is_default_currency ? (
+            {is_default_currency || is_ads_page ? (
                 <React.Fragment>
-                    <Text
-                        align='center'
-                        className='no-ads__title'
-                        color='general'
-                        line_height='m'
-                        size='s'
-                        weight='bold'
-                    >
-                        <Localize i18n_default_text='No ads for this currency 😞' />
+                    <Text align='center' className='no-ads__title' weight='bold'>
+                        {is_ads_page ? (
+                            <Localize i18n_default_text='You have no ads.' />
+                        ) : (
+                            <Localize i18n_default_text='No ads for this currency 😞' />
+                        )}
                     </Text>
-                    <Text className='no-ads__message' align='center' color='general' line_height='m' size='s'>
+                    <Text className='no-ads__message' align='center'>
                         <Localize i18n_default_text='Looking to buy or sell USD? You can post your own ad for others to respond.' />
                     </Text>
-                    <Button
-                        className='no-ads__button'
-                        disabled={general_store.is_barred}
-                        primary
-                        large
-                        onClick={() => {
-                            if (!general_store.is_barred) {
-                                general_store.handleTabClick(2);
-                                if (buy_sell_store.is_buy) buy_sell_store.setCreateSellAdFromNoAds(true);
-                                my_ads_store.setShowAdForm(true);
-                                history.push(routes.p2p_my_ads);
-                            }
-                        }}
-                    >
-                        <Localize i18n_default_text='Create ad' />
+                    <Button className='no-ads__button' disabled={is_barred} primary large onClick={onClickButton}>
+                        {is_ads_page ? (
+                            <Localize i18n_default_text='Create new ad' />
+                        ) : (
+                            <Localize i18n_default_text='Create ad' />
+                        )}
                     </Button>
                 </React.Fragment>
             ) : (
-                <Text align='center' className='no-ads__title' color='general' line_height='m' size='s' weight='bold'>
+                <Text align='center' className='no-ads__title' weight='bold'>
                     <Localize i18n_default_text='No ads for this currency at the moment 😞' />
                 </Text>
             )}

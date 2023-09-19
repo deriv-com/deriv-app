81629dd5f2dc42ebb0344f5f1d489df843fecf3f
shontzu <108507236+shontzu-deriv@users.noreply.github.com>
2023-09-19 13:23:01 +0800

    refactor: naming convention for shouldShowLoader
    Co-authored-by: Matin shafiei <matin@binary.com>

--- packages/appstore/src/components/main-title-bar/asset-summary.tsx
+++ packages/appstore/src/components/main-title-bar/asset-summary.tsx
@@ -31,14 +31,14 @@ const AssetSummary = observer(() => {
     const eu_account = is_eu_user && !no_MF_account;
     const cr_account = !is_eu_user && !no_CR_account;
 
-    const shouldShowLoader =
+    const should_show_loader =
         ((is_switching || is_logging_in) && (eu_account || cr_account)) ||
         !is_landing_company_loaded ||
         !last_update ||
         is_loading ||
         is_transfer_confirm;
 
-    if (shouldShowLoader) {
+    if (should_show_loader) {
         return (
             <React.Fragment>
                 <div className='asset-summary__container content-loader'>

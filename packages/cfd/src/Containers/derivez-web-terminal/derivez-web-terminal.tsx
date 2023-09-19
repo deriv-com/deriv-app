6b2c36934610971cc56504f9e699e6e379f3f808
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-09-19 10:26:50 +0800

    chore: remove themesync code + responsive iframe

--- packages/cfd/src/Containers/derivez-web-terminal/derivez-web-terminal.tsx
+++ packages/cfd/src/Containers/derivez-web-terminal/derivez-web-terminal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { routes } from '@deriv/shared';
 import { getDerivEzWebTerminalLink } from '../../Helpers/constants';
@@ -11,40 +11,11 @@ const DerivezWebTerminal = () => {
     const routeToPrevious = () => history.push(routes.traders_hub);
     const { traders_hub, ui } = useStore();
     const { is_real } = traders_hub;
-    const { setDarkMode } = ui;
     const account_type = is_real ? 'real' : 'demo';
 
     const derivez_token = location.state.derivez_token || '';
 
     const derivezWebTraderUrl = getDerivEzWebTerminalLink(account_type, derivez_token);
-
-    useEffect(() => {
-        // listen to theme change from cloudfront(derivez) localStorage and update app.deriv theme
-        window.addEventListener('message', function (e) {
-            const key = e.data.key;
-            const value = e.data.value;
-
-            if (key === 'panda-forex__theme') {
-                setDarkMode(value === 'theme-dark-deriv');
-            }
-        });
-
-        // send theme change from app.deriv localStorage to cloudfront(derivez) localStorage
-        window.addEventListener('storage', function (e) {
-            if (e.key === 'ui_store') {
-                const uiStore = JSON.parse(e.newValue || '');
-
-                if (uiStore.ui_store.is_dark_mode_on !== localStorage.ui_store.is_dark_mode_on) {
-                    const message = {
-                        key: 'panda-forex__theme',
-                        value: uiStore.ui_store.is_dark_mode_on ? 'theme-dark-deriv' : 'theme-white-deriv',
-                    };
-
-                    window.parent.postMessage(message, derivezWebTraderUrl);
-                }
-            }
-        });
-    }, []);
 
     return (
         <div className='derivez-web-terminal'>

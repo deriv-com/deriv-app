7d6019229039c713bdeed7c90458f803e9e40490
Nada <nada.ajinas@deriv.com>
2023-09-19 17:57:16 +0400

    fix: failing test fix, reuse util function for status label

--- packages/p2p/src/utils/adverts.ts
+++ packages/p2p/src/utils/adverts.ts
@@ -1,3 +1,4 @@
+import { Duration } from 'moment';
 import { getDiffDuration, toMoment, epochToMoment } from '@deriv/shared';
 import { localize } from 'Components/i18next';
 import { api_error_codes } from 'Constants/api-error-codes';
@@ -41,60 +42,75 @@ export const generateErrorDialogBody = (error_code: string, error_message?: stri
     }
 };
 
-export const getLastOnlineLabel = (is_online, last_online_time) => {
-    if (!is_online) {
-        if (last_online_time) {
-            const start_time = epochToMoment(last_online_time).unix();
-            const end_time = toMoment().unix();
-            const diff = getDiffDuration(start_time, end_time);
+const getStatusLabel = (diff: Duration) => {
+    if (diff.years()) return localize('Seen more than 6 months ago');
 
-            if (diff.years()) return localize('Seen more than 6 months ago');
-            if (diff.months()) {
-                if (diff.months() > 6) {
-                    return localize('Seen more than 6 months ago');
-                }
-                if (diff.months() === 1) {
-                    return localize('Seen {{ duration }} month ago', {
-                        duration: diff.months(),
-                    });
-                }
-                return localize('Seen {{ duration }} months ago', {
-                    duration: diff.months(),
-                });
-            }
-            if (diff.days()) {
-                if (diff.days() === 1) {
-                    return localize('Seen {{ duration }} day ago', {
-                        duration: diff.days(),
-                    });
-                }
-                return localize('Seen {{ duration }} days ago', {
-                    duration: diff.days(),
-                });
-            }
-            if (diff.hours()) {
-                if (diff.hours() === 1) {
-                    return localize('Seen {{ duration }} hour ago', {
-                        duration: diff.hours(),
-                    });
-                }
-                return localize('Seen {{ duration }} hours ago', {
-                    duration: diff.hours(),
-                });
-            }
-            if (diff.minutes()) {
-                if (diff.minutes() === 1) {
-                    return localize('Seen {{ duration }} minute ago', {
-                        duration: diff.minutes(),
-                    });
-                }
-                return localize('Seen {{ duration }} minutes ago', {
-                    duration: diff.minutes(),
-                });
-            }
-        } else {
+    if (diff.months()) {
+        if (diff.months() > 6) {
             return localize('Seen more than 6 months ago');
         }
+        if (diff.months() === 1) {
+            return localize('Seen {{ duration }} month ago', {
+                duration: diff.months(),
+            });
+        }
+        return localize('Seen {{ duration }} months ago', {
+            duration: diff.months(),
+        });
+    }
+    if (diff.days()) {
+        if (diff.days() === 1) {
+            return localize('Seen {{ duration }} day ago', {
+                duration: diff.days(),
+            });
+        }
+        return localize('Seen {{ duration }} days ago', {
+            duration: diff.days(),
+        });
+    }
+    if (diff.hours()) {
+        if (diff.hours() === 1) {
+            return localize('Seen {{ duration }} hour ago', {
+                duration: diff.hours(),
+            });
+        }
+        return localize('Seen {{ duration }} hours ago', {
+            duration: diff.hours(),
+        });
+    }
+    if (diff.minutes()) {
+        if (diff.minutes() === 1) {
+            return localize('Seen {{ duration }} minute ago', {
+                duration: diff.minutes(),
+            });
+        }
+        return localize('Seen {{ duration }} minutes ago', {
+            duration: diff.minutes(),
+        });
+    }
+    return localize('Online');
+};
+
+const getTimeDifference = (last_seen_online: number) => {
+    const start_time = epochToMoment(last_seen_online).unix();
+    const end_time = toMoment().unix();
+    return getDiffDuration(start_time, end_time);
+};
+
+/**
+ * Function to generate the status label for the user based on the given online status and last online time.
+ *
+ * @param {0|1} is_online - The online status of the user
+ * @param {number} last_online_time - The last online time in epoch
+ * @returns {string} The status label to be shown.
+ */
+export const getLastOnlineLabel = (is_online: 0 | 1, last_online_time?: number) => {
+    if (!is_online) {
+        if (last_online_time) {
+            const diff = getTimeDifference(last_online_time);
+            return getStatusLabel(diff);
+        }
+        return localize('Seen more than 6 months ago');
     }
     return localize('Online');
 };

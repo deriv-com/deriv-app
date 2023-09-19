92a0f5db76a83124fc3fa1a1f717b42a6b403627
adrienne-rio <adrienne@deriv.com>
2023-09-19 17:29:31 +0800

    Revert "chore: refactored icon component"
    This reverts commit 954062c200c23544d0ba5f0c0e5da33e6f10b6e5.

--- packages/wallets/src/components/WalletListCardIcon/WalletListCardIcon.tsx
+++ packages/wallets/src/components/WalletListCardIcon/WalletListCardIcon.tsx
@@ -1,77 +1,49 @@
 import React from 'react';
 import useDevice from '../../hooks/useDevice';
-import AUD from '../../public/images/currencies/aud.svg';
-import BTC from '../../public/images/currencies/btc.svg';
-import Demo from '../../public/images/currencies/demo.svg';
-import ETH from '../../public/images/currencies/eth.svg';
-import EUR from '../../public/images/currencies/eur.svg';
-import Tether from '../../public/images/currencies/eusdt.svg';
-import GBP from '../../public/images/currencies/gbp.svg';
-import LTC from '../../public/images/currencies/ltc.svg';
-import USD from '../../public/images/currencies/usd.svg';
-import USDC from '../../public/images/currencies/usdc.svg';
+import Bitcion from '../../public/images/bitcion.svg';
+import Demo from '../../public/images/demo.svg';
+import ETH from '../../public/images/eth.svg';
+import EUR from '../../public/images/eur.svg';
+import Tether from '../../public/images/eusdt.svg';
+import GBP from '../../public/images/gbp.svg';
+import LTC from '../../public/images/ltc.svg';
+import USD from '../../public/images/usd.svg';
+import USDC from '../../public/images/usdc.svg';
 
 const type_to_icon_mapper = {
-    AUD,
     USD,
     EUR,
     GBP,
-    BTC,
+    BTC: Bitcion,
     USDC,
     ETH,
     LTC,
     UST: Tether,
     eUSDT: Tether,
-    tUSDT: Tether,
     Demo,
 };
 
-const crypto_icons = ['BTC', 'USDC', 'ETH', 'LTC', 'UST', 'eUSDT', 'tUSDT', 'Demo'];
-const currency_icons = ['AUD', 'USD', 'EUR', 'GBP'];
-
-type TIconSize = {
-    mobile: number;
-    desktop: number;
+const type_to_size_mapper = {
+    USD: { mobile: 32, desktop: 48 },
+    EUR: { mobile: 32, desktop: 48 },
+    GBP: { mobile: 32, desktop: 48 },
+    BTC: { mobile: 45, desktop: 90 },
+    USDC: { mobile: 45, desktop: 90 },
+    ETH: { mobile: 45, desktop: 90 },
+    LTC: { mobile: 45, desktop: 90 },
+    UST: { mobile: 45, desktop: 90 },
+    eUSDT: { mobile: 45, desktop: 90 },
+    Demo: { mobile: 45, desktop: 90 },
 };
 
 type TProps = {
     type: keyof typeof type_to_icon_mapper | Omit<string, keyof typeof type_to_icon_mapper>;
-    crypto_size?: TIconSize;
-    currency_size?: TIconSize;
 };
 
-const WalletListCardIcon: React.FC<TProps> = ({ type, crypto_size, currency_size }) => {
+const WalletListCardIcon: React.FC<TProps> = ({ type }) => {
     const { is_mobile } = useDevice();
-
-    const getSize = React.useCallback(() => {
-        const is_crypto = crypto_icons.includes(type as string);
-        const is_currency = currency_icons.includes(type as string);
-
-        if (is_crypto) {
-            return (
-                crypto_size || {
-                    mobile: 45,
-                    desktop: 90,
-                }
-            );
-        }
-        if (is_currency) {
-            return (
-                currency_size || {
-                    mobile: 32,
-                    desktop: 48,
-                }
-            );
-        }
-
-        return {
-            mobile: 32,
-            desktop: 48,
-        };
-    }, [type]);
-
-    const Icon = React.useMemo(() => type_to_icon_mapper[type as keyof typeof type_to_icon_mapper], [type]);
-    const size = getSize()[is_mobile ? 'mobile' : 'desktop'];
+    const Icon = type_to_icon_mapper[type as keyof typeof type_to_icon_mapper];
+    const size = type_to_size_mapper[type as keyof typeof type_to_icon_mapper][is_mobile ? 'mobile' : 'desktop'];
 
     if (!Icon) return null;
 

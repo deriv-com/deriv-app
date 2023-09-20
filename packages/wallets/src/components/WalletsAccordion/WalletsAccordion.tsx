31b1f18402292c20107362ae851590341237ce48
Farhan Ahmad Nurzi <125247833+farhan-nurzi-deriv@users.noreply.github.com>
2023-09-20 10:26:39 +0800

    farhan/feat: create switchAccount function (#10008)
    * feat: create switchAccount function
    
    * chore: refactor switchAccount function and implement mobile carousel account switching
    
    * refactor: account switching refactoring
    
    * chore: clean-ups
    
    * chore: commenting swithAccount temporarily and styles clean-ups
    
    * refactor: usebalance enable option
    
    * fix: is_transition default value
    
    * refactor: clean-ups
    
    * fix: comments
    
    * chore: remove unrelated changes

--- packages/wallets/src/components/WalletsAccordion/WalletsAccordion.tsx
+++ packages/wallets/src/components/WalletsAccordion/WalletsAccordion.tsx
@@ -1,37 +1,46 @@
-import React, { ReactElement } from 'react';
-import { useWalletAccountsList } from '@deriv/api';
+import React, { ReactElement, useEffect } from 'react';
+import { useAuthorize, useWalletAccountsList } from '@deriv/api';
 import IcDropdown from '../../public/images/ic-dropdown.svg';
 import './WalletsAccordion.scss';
 
 type TProps = {
-    wallet: NonNullable<ReturnType<typeof useWalletAccountsList>['data']>[number];
+    account: NonNullable<ReturnType<typeof useWalletAccountsList>['data']>[number];
     content: ReactElement;
     header: ReactElement;
 };
 
-const WalletsAccordion: React.FC<TProps> = ({ wallet, header, content }) => {
-    const switchAccount = (loginid?: string) => {
-        // if (!loginid) return;
-        // implement switch account here
-    };
+const WalletsAccordion: React.FC<TProps> = ({ account: { is_active, is_virtual, loginid }, content, header }) => {
+    const { switchAccount } = useAuthorize();
+    const accordionRef = React.useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (is_active && accordionRef?.current) {
+            accordionRef.current.style.scrollMarginTop = '24px';
+            accordionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [is_active]);
 
     return (
-        <div className={`wallets-accordion wallets-accordion${wallet?.is_virtual ? '--virtual' : ''}`}>
+        <div
+            className={`wallets-accordion wallets-accordion ${
+                is_virtual ? 'wallets-accordion wallets-accordion--virtual' : ''
+            }`}
+        >
             <div
-                className={`wallets-accordion__header wallets-accordion__header${
-                    wallet?.is_virtual ? '--virtual' : ''
+                className={`wallets-accordion__header wallets-accordion__header ${
+                    is_virtual ? 'wallets-accordion__header wallets-accordion__header--virtual' : ''
                 }`}
             >
                 {header}
                 <div
-                    className={`wallets-accordion__dropdown${wallet.is_active ? '--open' : ''}`}
-                    onClick={() => switchAccount(wallet?.loginid)}
+                    className={`wallets-accordion__dropdown ${is_active ? 'wallets-accordion__dropdown--open' : ''}`}
+                    // onClick={() => switchAccount(loginid)}
                 >
                     <IcDropdown />
                 </div>
             </div>
-            <div className={`wallets-accordion__content${wallet.is_active ? '--visible' : ''}`}>
-                {wallet.is_active && content}
+            <div className={`wallets-accordion__content ${is_active ? 'wallets-accordion__content--visible' : ''}`}>
+                {is_active && content}
             </div>
         </div>
     );

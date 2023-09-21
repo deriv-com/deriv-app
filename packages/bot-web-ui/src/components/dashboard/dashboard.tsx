59483311336e3c7b9f480e2119f6cd979e30d992
rupato-deriv <97010868+rupato-deriv@users.noreply.github.com>
2023-09-21 16:00:54 +0800

    Rupato/146/fix: stop bot if device in sleep mode (#9076)
    * fix: stop bot if device in sleep mode
    
    * fix: comment changes
    
    * fix: comment changes
    
    * fix: changes for parameter neame
    
    * fix: transactions in loading state
    
    * fix: minor code changes
    
    * fix: review fixes
    
    * fix: review fixes
    
    * fix: removed line event listener
    
    * fix: reverted handler method
    
    * fix: for active_id
    
    * fix: removed account obsevable
    
    * fix: update transaction cache
    
    * fix: removed custom function to clear array
    
    * fix: added debugger
    
    * fix: added debugger
    
    * fix: removed optional chaining on filer
    
    * fix: updated parameter name
    
    * fix: removed visibilty change and added close listener
    
    * fix: removed instance of transactions store from dbot
    
    * fix: for testcases
    
    * fix: empty trigger
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: addded is running check for bot
    
    * fix: for clearing summary tab
    
    * fix: bug fixes
    
    * fix: typo and method name
    
    * chore: trigger empty
    
    ---------
    
    Co-authored-by: Ali(Ako) Hosseini <ali.hosseini@deriv.com>

--- packages/bot-web-ui/src/components/dashboard/dashboard.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import classNames from 'classnames';
+import dbot from '@deriv/bot-skeleton/src/scratch/dbot';
 import { updateWorkspaceName } from '@deriv/bot-skeleton';
 import { initTrashCan } from '@deriv/bot-skeleton/src/scratch/hooks/trashcan';
+import { api_base } from '@deriv/bot-skeleton/src/services/api/api-base';
 import { DesktopWrapper, Dialog, MobileWrapper, Tabs } from '@deriv/components';
 import { isMobile } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
@@ -21,7 +23,7 @@ import StrategyNotification from './strategy-notification';
 import Tutorial from './tutorial-tab';
 
 const Dashboard = observer(() => {
-    const { dashboard, load_modal, run_panel, quick_strategy } = useDBotStore();
+    const { dashboard, load_modal, run_panel, quick_strategy, summary_card } = useDBotStore();
     const {
         active_tab,
         has_tour_started,
@@ -37,12 +39,14 @@ const Dashboard = observer(() => {
         setActiveTab,
         setBotBuilderTokenCheck,
         setOnBoardingTokenCheck,
+        setWebSocketState,
         onCloseTour,
     } = dashboard;
     const { onEntered, dashboard_strategies } = load_modal;
     const { is_dialog_open, is_drawer_open, dialog_options, onCancelButtonClick, onCloseDialog, onOkButtonClick } =
         run_panel;
     const { is_strategy_modal_open } = quick_strategy;
+    const { clear } = summary_card;
     const { DASHBOARD, BOT_BUILDER, CHART, TUTORIAL } = DBOT_TABS;
     const is_tour_complete = React.useRef(true);
     let bot_tour_token: string | number = '';
@@ -66,6 +70,21 @@ const Dashboard = observer(() => {
         return tab_value;
     };
     const active_hash_tab = GetHashedValue(active_tab);
+
+    const checkAndHandleConnection = () => {
+        const api_status = api_base.getConnectionStatus();
+        //added this check because after sleep mode all the store values refresh and is_running is false.
+        const is_bot_running = document.getElementById('db-animation__stop-button') !== null;
+        if (is_bot_running && (api_status === 'Closed' || api_status === 'Closing')) {
+            dbot.terminateBot();
+            clear();
+            setWebSocketState(false);
+        }
+    };
+
+    React.useEffect(() => {
+        window.addEventListener('focus', checkAndHandleConnection);
+    }, []);
 
     React.useEffect(() => {
         if (init_render.current) {

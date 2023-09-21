239c097a206e16e9c18d816ee544049e960e8286
Shafin Al Karim <129021108+shafin-deriv@users.noreply.github.com>
2023-09-21 14:13:15 +0800

    refactor: transaction store refactor to adjust trx by loginid (#9996)
    * refactor: transaction store refactor to adjust trx by loginid
    
    * chore: remove GMT + time, reduce code smells
    
    * fix: reset transactions for intial loading
    
    * chore: fix transaction not clearing issue

--- packages/bot-web-ui/src/stores/transactions-store.js
+++ packages/bot-web-ui/src/stores/transactions-store.js
@@ -34,10 +34,9 @@ export default class TransactionsStore {
         this.core = core;
         this.disposeReactionsFn = this.registerReactions();
     }
-
     TRANSACTION_CACHE = 'transaction_cache';
 
-    elements = getStoredItemsByUser(this.TRANSACTION_CACHE, this.core?.client.loginid, []);
+    elements = getStoredItemsByUser(this.TRANSACTION_CACHE, this.core?.client?.loginid, []);
     active_transaction_id = null;
     recovered_completed_transactions = [];
     recovered_transactions = [];
@@ -45,7 +44,11 @@ export default class TransactionsStore {
     is_transaction_details_modal_open = false;
 
     get transactions() {
-        return this.elements.filter(element => element.type === transaction_elements.CONTRACT);
+        return (
+            this.elements[this.core?.client?.loginid]?.filter(
+                element => element.type === transaction_elements.CONTRACT
+            ) ?? []
+        );
     }
 
     toggleTransactionDetailsModal(is_open) {
@@ -59,6 +62,7 @@ export default class TransactionsStore {
     pushTransaction(data) {
         const is_completed = isEnded(data);
         const { run_id } = this.root_store.run_panel;
+        const current_account = this.core?.client?.loginid;
         const contract = {
             barrier: data.barrier,
             buy_price: data.buy_price,
@@ -83,7 +87,14 @@ export default class TransactionsStore {
             underlying: data.underlying,
         };
 
-        const same_contract_index = this.elements.findIndex(
+        if (!this.elements[current_account]) {
+            this.elements = {
+                ...this.elements,
+                [current_account]: [],
+            };
+        }
+
+        const same_contract_index = this.elements[current_account]?.findIndex(
             c =>
                 c.type === transaction_elements.CONTRACT &&
                 c.data.transaction_ids &&
@@ -92,32 +103,32 @@ export default class TransactionsStore {
 
         if (same_contract_index === -1) {
             // Render a divider if the "run_id" for this contract is different.
-            if (this.elements.length > 0) {
+            if (this.elements[current_account]?.length > 0) {
                 const is_new_run =
-                    this.elements[0].type === transaction_elements.CONTRACT &&
-                    contract.run_id !== this.elements[0].data.run_id;
+                    this.elements[current_account]?.[0].type === transaction_elements.CONTRACT &&
+                    contract.run_id !== this.elements[current_account]?.[0].data.run_id;
 
                 if (is_new_run) {
-                    this.elements.unshift({
+                    this.elements[current_account]?.unshift({
                         type: transaction_elements.DIVIDER,
                         data: contract.run_id,
                     });
                 }
             }
 
-            this.elements.unshift({
+            this.elements[current_account]?.unshift({
                 type: transaction_elements.CONTRACT,
                 data: contract,
             });
         } else {
             // If data belongs to existing contract in memory, update it.
-            this.elements.splice(same_contract_index, 1, {
+            this.elements[current_account]?.splice(same_contract_index, 1, {
                 type: transaction_elements.CONTRACT,
                 data: contract,
             });
         }
 
-        this.elements = this.elements.slice(); // force array update
+        this.elements = { ...this.elements }; // force update
     }
 
     setActiveTransactionId(transaction_id) {
@@ -149,9 +160,9 @@ export default class TransactionsStore {
     }
 
     clear() {
-        this.elements = this.elements.slice(0, 0);
-        this.recovered_completed_transactions = this.recovered_completed_transactions.slice(0, 0);
-        this.recovered_transactions = this.recovered_transactions.slice(0, 0);
+        this.elements[this.core?.client?.loginid] = [];
+        this.recovered_completed_transactions = this.recovered_completed_transactions?.slice(0, 0);
+        this.recovered_transactions = this.recovered_transactions?.slice(0, 0);
         this.is_transaction_details_modal_open = false;
     }
 
@@ -160,10 +171,10 @@ export default class TransactionsStore {
 
         // Write transactions to session storage on each change in transaction elements.
         const disposeTransactionElementsListener = reaction(
-            () => this.elements,
+            () => this.elements[client?.loginid],
             elements => {
                 const stored_transactions = getStoredItemsByKey(this.TRANSACTION_CACHE, {});
-                stored_transactions[client.loginid] = elements.slice(0, 5000);
+                stored_transactions[client.loginid] = elements?.slice(0, 5000) ?? [];
                 setStoredItemsByKey(this.TRANSACTION_CACHE, stored_transactions);
             }
         );
@@ -176,17 +187,9 @@ export default class TransactionsStore {
             () => this.recoverPendingContracts()
         );
 
-        const disposeSwitchAcountListener = reaction(
-            () => client.loginid,
-            () => this.clear()
-        );
-
         return () => {
             disposeTransactionElementsListener();
             disposeRecoverContracts();
-            if (typeof this.disposeSwitchAcountListener === 'function') {
-                disposeSwitchAcountListener();
-            }
         };
     }
 
@@ -239,11 +242,12 @@ export default class TransactionsStore {
         }
 
         if (!this.is_called_proposal_open_contract) {
-            if (!this.elements.length) {
+            const current_account = this.core?.client?.loginid;
+            if (!this.elements[current_account]?.length) {
                 this.sortOutPositionsBeforeAction(positions);
             }
-            if (this.elements.length && !this.elements[0].data.profit) {
-                const element_id = this.elements[0].data.contract_id;
+            if (this.elements[current_account]?.length && !this.elements[current_account]?.[0]?.data?.profit) {
+                const element_id = this.elements[current_account]?.[0].data.contract_id;
                 this.sortOutPositionsBeforeAction(positions, element_id);
             }
         }

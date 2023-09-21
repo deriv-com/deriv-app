82d2762ea0506b249574237cbebf4d83b4de5242
kate-deriv <kate@regentmarkets.com>
2023-09-21 17:30:54 +0300

    refactor: add unit tests

---
+++ packages/trader/src/Modules/Trading/Components/Form/TradeParams/Duration/__tests__/duration-range-text.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import DurationRangeText from '../duration-range-text';
+
+const mocked_props = {
+    min: '1',
+    max: '1440',
+    duration_unit: 'minutes',
+};
+
+describe('<DurationRangeText />', () => {
+    it('should render component with proper text', () => {
+        render(<DurationRangeText {...mocked_props} />);
+
+        expect(screen.getByText(/Range: 1 - 1,440 minutes/i)).toBeInTheDocument();
+    });
+});

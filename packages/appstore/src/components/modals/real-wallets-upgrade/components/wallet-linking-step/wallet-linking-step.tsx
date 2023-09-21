fd6dfe33d4ed2515b5347c883c489dd3941ff685
Aum Bhatt <aum@deriv.com>
2023-09-21 13:00:37 +0400

    chore: made the styles for the wallet-account consistent with wallet-transfer-tile

--- packages/appstore/src/components/modals/real-wallets-upgrade/components/wallet-linking-step/wallet-linking-step.tsx
+++ packages/appstore/src/components/modals/real-wallets-upgrade/components/wallet-linking-step/wallet-linking-step.tsx
@@ -78,6 +78,7 @@ const WalletLinkingStep = observer(({ data }: TWalletLinkingStep) => {
                                                     currency={account.currency}
                                                     icon={account.icon}
                                                     name={account.account_name}
+                                                    is_mobile={is_mobile}
                                                 />
                                             );
                                         })}

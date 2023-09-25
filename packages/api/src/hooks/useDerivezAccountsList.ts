a5b6fb4cef59128c58946b00f3d29a959460e741
Muhammad Hamza <120543468+hamza-deriv@users.noreply.github.com>
2023-09-25 14:14:56 +0800

    Hamza/chore: added pandats service token (#10269)
    * chore: added pandats service token
    
    * chore: include hook in index file
    
    * chore: rearrange the import statements

--- packages/api/src/hooks/useDerivezAccountsList.ts
+++ packages/api/src/hooks/useDerivezAccountsList.ts
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import useDerivezServiceToken from './useDerivezServiceToken';
 import useFetch from '../useFetch';
 
 /** A custom hook that gets the list of created DerivEz accounts. */
@@ -6,6 +7,7 @@ const useDerivezAccountsList = () => {
     const { data: derivez_accounts } = useFetch('trading_platform_accounts', {
         payload: { platform: 'derivez' },
     });
+    const { data: token } = useDerivezServiceToken();
 
     /** Adding neccesary properties to DerivEz accounts */
     const modified_derivez_accounts = useMemo(
@@ -13,8 +15,9 @@ const useDerivezAccountsList = () => {
             derivez_accounts?.trading_platform_accounts?.map(account => ({
                 ...account,
                 loginid: account.account_id,
+                token,
             })),
-        [derivez_accounts?.trading_platform_accounts]
+        [derivez_accounts?.trading_platform_accounts, token]
     );
 
     return {

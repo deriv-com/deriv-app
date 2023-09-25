d89d2be635a94a20ffc6d7c162e76fe6d5b35850
Taysuisin <suisin@regentmarkets.com>
2023-09-25 16:28:49 +0800

    chore: update logic for appstore banner

--- packages/appstore/src/components/appstore-banner/appstore-banner.tsx
+++ packages/appstore/src/components/appstore-banner/appstore-banner.tsx
@@ -4,10 +4,8 @@ import { useMT5SVGEligibleToMigrate } from '@deriv/hooks';
 
 const AppstoreBanner = () => {
     const { has_svg_accounts_to_migrate } = useMT5SVGEligibleToMigrate();
-    //TODO: will add proper condition check when API is ready
-    if (has_svg_accounts_to_migrate) return <SVGMigrationBanner />;
 
-    return null;
+    return (has_svg_accounts_to_migrate && <SVGMigrationBanner />) || null;
 };
 
 export default AppstoreBanner;

5a98ad4ae2d2d9fef278ba2fb1970d9467d9b1af
Hamza <hamza.muhammad@regentmarkets.com>
2023-09-25 12:32:17 +0800

    chore: include hook in index file

--- packages/api/src/hooks/index.ts
+++ packages/api/src/hooks/index.ts
@@ -32,3 +32,4 @@ export { default as useTradingPlatformInvestorPasswordReset } from './useTrading
 export { default as useTradingPlatformPasswordChange } from './useTradingPlatformPasswordChange';
 export { default as useTransferBetweenAccounts } from './useTransferBetweenAccounts';
 export { default as useWalletAccountsList } from './useWalletAccountsList';
+export { default as useDerivezServiceToken } from './useDerivezServiceToken';

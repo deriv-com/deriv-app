8b55b5d5870e0c9d09eb466a0ba14f0bbe8d821a
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-09-26 11:39:39 +0800

    feat: add mt5 account type modal

--- packages/wallets/src/components/ExternalTradingPlatforms/GetMoreMT5Accounts/GetMoreMT5Accounts.tsx
+++ packages/wallets/src/components/ExternalTradingPlatforms/GetMoreMT5Accounts/GetMoreMT5Accounts.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import AddIcon from '../../../public/images/add-icon.svg';
+import { useModal } from '../../ModalProvider';
+import { WalletModal } from '../../WalletModal';
+import { MT5AccountType } from '../MT5AccountType';
 import './GetMoreMT5Accounts.scss';
 
 const GetMoreMT5Accounts: React.FC = () => {
+    const { show } = useModal();
     return (
-        <div className='wallets-get-more-mt5-accounts'>
+        <div
+            className='wallets-get-more-mt5-accounts'
+            onClick={() =>
+                show(
+                    <WalletModal>
+                        <MT5AccountType />
+                    </WalletModal>
+                )
+            }
+        >
             <div className='wallets-get-more-mt5-accounts-container'>
                 <div className='wallets-get-more-mt5-accounts-container-icon'>
                     <AddIcon />
---
+++ packages/wallets/src/components/ExternalTradingPlatforms/MT5AccountType/MT5AccountType.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MT5AccountType.scss';
+
+const MT5AccountType: React.FC = () => {
+    return (
+        <div className='wallets-mt5-account-type'>
+            <div className='wallets-mt5-account-type-header'>
+                <div className='wallets-mt5-account-type-header-text'>Select Deriv MT5’s account type</div>
+            </div>
+        </div>
+    );
+};
+
+export default MT5AccountType;

d711f39fd32b0707b627e0791ad8ae467299ef5e
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-09-26 11:31:15 +0800

    chore: append login after webtrader url

--- packages/cfd/src/Containers/dmt5-trade-modal.tsx
+++ packages/cfd/src/Containers/dmt5-trade-modal.tsx
@@ -212,7 +212,9 @@ const DMT5TradeModal = ({
                     <a
                         className='dc-btn cfd-trade-modal__download-center-app--option-link'
                         type='button'
-                        href={mt5_trade_account.webtrader_url}
+                        href={`${mt5_trade_account.webtrader_url}&login=${
+                            (mt5_trade_account as TTradingPlatformAccounts)?.display_login
+                        }`}
                         target='_blank'
                         rel='noopener noreferrer'
                     >

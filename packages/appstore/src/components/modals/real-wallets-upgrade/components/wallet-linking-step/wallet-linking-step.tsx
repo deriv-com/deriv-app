cdf7768edf3c1dd0ef77d2b53783875231735db6
Aum Bhatt <aum@deriv.com>
2023-09-28 11:24:37 +0400

    fix: fixed responsive design

--- packages/appstore/src/components/modals/real-wallets-upgrade/components/wallet-linking-step/wallet-linking-step.tsx
+++ packages/appstore/src/components/modals/real-wallets-upgrade/components/wallet-linking-step/wallet-linking-step.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text, ThemedScrollbars } from '@deriv/components';
-import { Localize } from '@deriv/translations';
+import { Text, ThemedScrollbars, WalletCard } from '@deriv/components';
+import { Localize, localize } from '@deriv/translations';
 import { observer, useStore } from '@deriv/stores';
 import WalletLinkWrapper, { TWalletLinkWrapper } from '../wallet-link/wallet-link-wrapper';
 import './wallet-linking-step.scss';
+import WalletAccount from '../wallet-account/wallet-account';
+import classNames from 'classnames';
 
 type TWalletLinkingStep = {
     data: { title: string; wallets: TWalletLinkWrapper[] };
@@ -30,32 +32,79 @@ const WalletLinkingStep = observer(({ data }: TWalletLinkingStep) => {
                 <Localize i18n_default_text='Your existing funds will remain in your trading account(s) and can be transferred to your Wallet after the upgrade.' />
             </Text>
             {!is_mobile && (
-                <div className='wallet-linking-step__title-small'>
-                    <Text
-                        className='wallet-linking-step__title-text wallet-linking-step__title-small-text'
-                        color='prominent'
-                        align='center'
-                        size='xxxs'
-                    >
-                        <Localize i18n_default_text='Your current trading account(s)' />
+                <div className='wallet-linking-step__title'>
+                    <Text className='wallet-linking-step__title-text' color='prominent' size='xxxs'>
+                        {localize('Your current trading account(s)')}
                     </Text>
-                    <Text
-                        className='wallet-linking-step__title-text wallet-linking-step__title-small-text'
-                        color='prominent'
-                        align='center'
-                        size='xxxs'
-                    >
-                        <Localize i18n_default_text='Your new Wallet(s)' />
+                    <Text className='wallet-linking-step__title-text' color='prominent' size='xxxs'>
+                        {localize('Your new Wallet(s)')}
                     </Text>
                 </div>
             )}
             <ThemedScrollbars className='wallet-linking-step__content'>
-                {data.wallets.map(({ wallet_details, account_list }) => {
+                {data.wallets.map(wallet => {
                     return (
                         <WalletLinkWrapper
-                            key={wallet_details.name}
-                            account_list={account_list}
-                            wallet_details={wallet_details}
+                            key={wallet.wallet_details.name}
+                            show_left_fork={wallet.account_list.length > 1}
+                            left={() => {
+                                return (
+                                    <>
+                                        {is_mobile && (
+                                            <Text
+                                                as='div'
+                                                className={classNames('wallet-linking-step__title-text', {
+                                                    'wallet-linking-step__title--mobile': is_mobile,
+                                                })}
+                                                color='prominent'
+                                                size='xxxs'
+                                                styles={
+                                                    is_mobile && {
+                                                        borderRadius: '8px 8px 0 0',
+                                                    }
+                                                }
+                                            >
+                                                {localize('Your current trading account(s)')}
+                                            </Text>
+                                        )}
+                                        {wallet.account_list.map(account => {
+                                            return (
+                                                <WalletAccount
+                                                    key={`${account.account_name}-${account.currency}}`}
+                                                    balance={account.balance}
+                                                    currency={account.currency}
+                                                    icon={account.icon}
+                                                    name={account.account_name}
+                                                />
+                                            );
+                                        })}
+                                    </>
+                                );
+                            }}
+                            right={() => {
+                                return (
+                                    <div style={{ height: '13.6rem', position: 'relative' }}>
+                                        <WalletCard wallet={wallet.wallet_details} size='large' state='default' />
+                                        {is_mobile && (
+                                            <Text
+                                                className='wallet-linking-step__title-text'
+                                                color='prominent'
+                                                size='xxxs'
+                                                styles={
+                                                    is_mobile && {
+                                                        position: 'absolute',
+                                                        width: '21.6rem',
+                                                        bottom: '0',
+                                                        borderRadius: '0 0 8px 8px',
+                                                    }
+                                                }
+                                            >
+                                                {localize('Your new Wallet')}
+                                            </Text>
+                                        )}
+                                    </div>
+                                );
+                            }}
                         />
                     );
                 })}

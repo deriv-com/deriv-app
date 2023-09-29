b487f588ee20ec8cecca7aaaddf00a14a7bd0c33
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-09-29 10:22:58 +0800

    chore: rename CFDs real to CFDs

--- packages/shared/src/utils/cfd/cfd.ts
+++ packages/shared/src/utils/cfd/cfd.ts
@@ -7,7 +7,7 @@ let CFD_text_translated: { [key: string]: () => void };
 export const CFD_text: { [key: string]: string } = {
     dxtrade: 'Deriv X',
     mt5: 'MT5',
-    mt5_cfds: 'CFDs real',
+    mt5_cfds: 'CFDs',
     cfd: 'CFDs',
     ctrader: 'cTrader',
     derivez: 'DerivEz',

045276bf299b5aa585addcfaaecee6f9c7e26fdb
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-10-02 15:29:35 +0800

    fix: ci failed

--- packages/wallets/src/components/JurisdictionModal/JurisdictionCard.tsx
+++ packages/wallets/src/components/JurisdictionModal/JurisdictionCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import JurisdictionCardRow from './JurisdictionCardRow';
 import JurisdictionCardTag from './JurisdictionCardTag';

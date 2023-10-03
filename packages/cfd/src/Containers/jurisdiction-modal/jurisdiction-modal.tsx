2d0ced18d47d8f0b6a9777f81cde3e801a858fe3
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-10-03 15:06:57 +0800

    fix: for eu_user also experience same issue

--- packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-modal.tsx
+++ packages/cfd/src/Containers/jurisdiction-modal/jurisdiction-modal.tsx
@@ -10,11 +10,12 @@ import JurisdictionModalContentWrapper from './jurisdiction-modal-content-wrappe
 import JurisdictionModalTitle from './jurisdiction-modal-title';
 
 const JurisdictionModal = observer(({ openPasswordModal }: TJurisdictionModalProps) => {
-    const { traders_hub, ui, common } = useStore();
+    const { traders_hub, ui, common, client } = useStore();
 
     const { show_eu_related_content } = traders_hub;
     const { disableApp, enableApp } = ui;
     const { platform } = common;
+    const { is_eu } = client;
 
     const { account_type, is_jurisdiction_modal_visible, toggleJurisdictionModal } = useCfdStore();
 
@@ -38,7 +39,7 @@ const JurisdictionModal = observer(({ openPasswordModal }: TJurisdictionModalPro
             })}
         >
             <JurisdictionModalContentWrapper openPasswordModal={openPasswordModal} />
-            {account_type.type === 'financial' && <DynamicLeverageModalContent />}
+            {account_type.type === 'financial' && !is_eu && <DynamicLeverageModalContent />}
         </div>
     );
 

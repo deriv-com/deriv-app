49af62cb830553d4f5c8e69fe7490f6116388218
kate-deriv <kate@regentmarkets.com>
2023-10-04 10:19:52 +0300

    refactor: constants naming

--- packages/trader/src/Stores/Modules/Trading/Constants/__tests__/validation-rules.spec.ts
+++ packages/trader/src/Stores/Modules/Trading/Constants/__tests__/validation-rules.spec.ts
@@ -5,17 +5,17 @@ import { isSessionAvailable } from '../../Helpers/start-date';
 import type { TRuleOptions } from 'Utils/Validator/validator';
 
 const mocked_store = {
+    barrier_count: 2,
     contract_start_type: 'test_spot',
+    form_components: ['barrier'],
     has_stop_loss: true,
     has_take_profit: true,
     stop_loss: false,
     start_date: 'start_date',
-    barrier_count: 2,
-    form_components: ['barrier'],
     take_profit: false,
 } as unknown as TTradeStore;
 
-const mocked_params_1: [
+const default_mocked_params: [
     TTradeStore['barrier_1'],
     Partial<TRuleOptions>,
     TTradeStore,
@@ -30,13 +30,13 @@ const mocked_params_1: [
     },
 ];
 
-const mocked_params_2: [TTradeStore['start_time'], Partial<TRuleOptions>, TTradeStore] = [
+const start_time_mocked_params: [TTradeStore['start_time'], Partial<TRuleOptions>, TTradeStore] = [
     null,
     'test' as unknown as Partial<TRuleOptions>,
     mocked_store,
 ];
 
-const mocked_params_3: [TTradeStore['expiry_time'], Partial<TRuleOptions>, TTradeStore] = [
+const expiry_time_mocked_params: [TTradeStore['expiry_time'], Partial<TRuleOptions>, TTradeStore] = [
     null,
     'test' as unknown as Partial<TRuleOptions>,
     mocked_store,
@@ -84,11 +84,11 @@ describe('getValidationRules', () => {
         expect(returned_validation_rules).toHaveProperty('barrier_1');
         expect(returned_validation_rules.barrier_1.rules[0][1].condition(mocked_store)).toBe(true);
         expect(returned_validation_rules.barrier_1.rules[1][1].condition(mocked_store)).toBe(2);
-        expect(returned_validation_rules.barrier_1.rules[2][1].func(...mocked_params_1)).toBe(true);
+        expect(returned_validation_rules.barrier_1.rules[2][1].func(...default_mocked_params)).toBe(true);
 
         mocked_store.barrier_count = 1;
-        expect(returned_validation_rules.barrier_1.rules[2][1].func(...mocked_params_1)).toBe(true);
-        expect(returned_validation_rules.barrier_1.rules[3][1].func(...mocked_params_1)).toBe(true);
+        expect(returned_validation_rules.barrier_1.rules[2][1].func(...default_mocked_params)).toBe(true);
+        expect(returned_validation_rules.barrier_1.rules[3][1].func(...default_mocked_params)).toBe(true);
         expect(
             returned_validation_rules.barrier_1.rules[3][1].func(
                 '123',
@@ -99,7 +99,7 @@ describe('getValidationRules', () => {
                     barrier_2: '123',
                 }
             )
-        ).toBeTrue;
+        ).toBe(true);
     });
 
     it('should contain rules for barrier_2', () => {
@@ -108,8 +108,8 @@ describe('getValidationRules', () => {
         expect(returned_validation_rules).toHaveProperty('barrier_2');
         expect(returned_validation_rules.barrier_2.rules[0][1].condition(mocked_store)).toBe(true);
         expect(returned_validation_rules.barrier_2.rules[1][1].condition(mocked_store)).toBe(2);
-        expect(returned_validation_rules.barrier_2.rules[2][1].func(...mocked_params_1)).toBe(false);
-        expect(returned_validation_rules.barrier_2.rules[3][1].func(...mocked_params_1)).toBe(false);
+        expect(returned_validation_rules.barrier_2.rules[2][1].func(...default_mocked_params)).toBe(false);
+        expect(returned_validation_rules.barrier_2.rules[3][1].func(...default_mocked_params)).toBe(false);
     });
 
     it('should contain rules for duration', () => {
@@ -129,35 +129,35 @@ describe('getValidationRules', () => {
 
     it('should contain rules for start_time', () => {
         expect(returned_validation_rules).toHaveProperty('start_time');
-        expect(returned_validation_rules.start_time.rules[0][1].func(...mocked_params_2)).toBe(false);
-        expect(returned_validation_rules.start_time.rules[1][1].func(...mocked_params_2)).toBe(false);
-        expect(returned_validation_rules.start_time.rules[2][1].func(...mocked_params_2)).toBe(false);
+        expect(returned_validation_rules.start_time.rules[0][1].func(...start_time_mocked_params)).toBe(false);
+        expect(returned_validation_rules.start_time.rules[1][1].func(...start_time_mocked_params)).toBe(false);
+        expect(returned_validation_rules.start_time.rules[2][1].func(...start_time_mocked_params)).toBe(false);
 
         mocked_store.contract_start_type = 'spot';
-        expect(returned_validation_rules.start_time.rules[3][1].func(...mocked_params_2)).toBe(true);
+        expect(returned_validation_rules.start_time.rules[3][1].func(...start_time_mocked_params)).toBe(true);
 
         mocked_store.contract_start_type = 'test_spot';
-        expect(returned_validation_rules.start_time.rules[3][1].func(...mocked_params_2)).toBe(false);
+        expect(returned_validation_rules.start_time.rules[3][1].func(...start_time_mocked_params)).toBe(false);
 
         (isTimeValid as jest.Mock).mockReturnValueOnce(true);
         (isSessionAvailable as jest.Mock).mockReturnValueOnce(true);
-        expect(returned_validation_rules.start_time.rules[3][1].func(...mocked_params_2)).toBe(true);
+        expect(returned_validation_rules.start_time.rules[3][1].func(...start_time_mocked_params)).toBe(true);
     });
 
     it('should contain rules for expiry_time', () => {
         expect(returned_validation_rules).toHaveProperty('expiry_time');
-        expect(returned_validation_rules.expiry_time.rules[0][1].func(...mocked_params_3)).toBe(false);
-        expect(returned_validation_rules.expiry_time.rules[1][1].func(...mocked_params_3)).toBe(false);
-        expect(returned_validation_rules.expiry_time.rules[2][1].func(...mocked_params_3)).toBe(false);
+        expect(returned_validation_rules.expiry_time.rules[0][1].func(...expiry_time_mocked_params)).toBe(false);
+        expect(returned_validation_rules.expiry_time.rules[1][1].func(...expiry_time_mocked_params)).toBe(false);
+        expect(returned_validation_rules.expiry_time.rules[2][1].func(...expiry_time_mocked_params)).toBe(false);
 
         mocked_store.contract_start_type = 'spot';
-        expect(returned_validation_rules.expiry_time.rules[3][1].func(...mocked_params_3)).toBe(true);
+        expect(returned_validation_rules.expiry_time.rules[3][1].func(...expiry_time_mocked_params)).toBe(true);
 
         mocked_store.contract_start_type = 'test_spot';
-        expect(returned_validation_rules.expiry_time.rules[3][1].func(...mocked_params_3)).toBe(false);
+        expect(returned_validation_rules.expiry_time.rules[3][1].func(...expiry_time_mocked_params)).toBe(false);
 
         (isTimeValid as jest.Mock).mockReturnValueOnce(true);
         (isSessionAvailable as jest.Mock).mockReturnValueOnce(true);
-        expect(returned_validation_rules.expiry_time.rules[3][1].func(...mocked_params_3)).toBe(true);
+        expect(returned_validation_rules.expiry_time.rules[3][1].func(...expiry_time_mocked_params)).toBe(true);
     });
 });

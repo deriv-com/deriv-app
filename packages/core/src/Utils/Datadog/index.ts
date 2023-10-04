a62af357c99b017395a6cf88bba9ff286272c6eb
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-10-04 11:55:33 +0400

    refactor: to ignore telegram token in datadog logs

--- packages/core/src/Utils/Datadog/index.ts
+++ packages/core/src/Utils/Datadog/index.ts
@@ -41,5 +41,6 @@ datadogRum.init({
     version: dataDogVersion,
     trackFrustrations: true,
     enableExperimentalFeatures: ['clickmap'],
+    excludedActivityUrls: ['TELEGRAM_ACCESS_TOKEN'],
 });
 datadogRum.startSessionReplayRecording();

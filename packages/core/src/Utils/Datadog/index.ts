0cd69965d5c8cc4b879004223ccc11136c929c3e
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-10-05 12:18:40 +0400

    refactor: to add regex in excluded resource url

--- packages/core/src/Utils/Datadog/index.ts
+++ packages/core/src/Utils/Datadog/index.ts
@@ -41,6 +41,6 @@ datadogRum.init({
     version: dataDogVersion,
     trackFrustrations: true,
     enableExperimentalFeatures: ['clickmap'],
-    excludedActivityUrls: ['/^https://api.telegram.org/'],
+    excludedActivityUrls: ['/^https://api.telegram.org.*$/'],
 });
 datadogRum.startSessionReplayRecording();

fb524d341ad57158dc0d0ebe4bb699590caa71e1
shafin-deriv <shafin@regentmarkets.com>
2023-10-06 14:13:41 +0800

    fix: separate empty disable message for journal and transactions

--- packages/bot-web-ui/src/components/download/download.tsx
+++ packages/bot-web-ui/src/components/download/download.tsx
@@ -19,13 +19,14 @@ const Download = observer(({ tab }: TDownloadProps) => {
         clickFunction = onClickDownloadTransaction;
         disabled = !transaction_list.length || is_running;
         popover_message = localize('Download your transaction history.');
+        if (!transaction_list.length) popover_message = localize('No transaction or activity yet.');
     } else if (tab === 'journal') {
         clickFunction = onClickDownloadJournal;
         popover_message = localize('Download your journal.');
         disabled = is_clear_stat_disabled;
+        if (disabled) popover_message = localize('No transaction or activity yet.');
     }
     if (is_running) popover_message = localize('Download is unavailable while your bot is running.');
-    else if (!transaction_list.length) popover_message = localize('No transaction or activity yet.');
 
     return (
         <Popover

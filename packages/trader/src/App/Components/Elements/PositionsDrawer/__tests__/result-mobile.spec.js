9b06f53aacfa83c32987f2bb27a49120b9713b4a
Henry Hein <henry@regentmarkets.com>
2023-10-09 18:58:54 +0800

    fix: failing tests

--- packages/trader/src/App/Components/Elements/PositionsDrawer/__tests__/result-mobile.spec.js
+++ packages/trader/src/App/Components/Elements/PositionsDrawer/__tests__/result-mobile.spec.js
@@ -8,13 +8,13 @@ describe('ResultMobile', () => {
         expect(wrapper.getByTestId('result_mobile')).toBeInTheDocument();
     });
 
-    it('should ResultMobile render LOST if result is won ', () => {
+    it('should ResultMobile render CLOSED if result is won ', () => {
         const wrapper = render(<ResultMobile is_visible={true} result='won' />);
-        expect(wrapper.getByText('Won')).toBeInTheDocument(true);
+        expect(wrapper.getByText('Closed')).toBeInTheDocument(true);
     });
 
-    it('should ResultMobile render LOST if result is not won ', () => {
+    it('should ResultMobile render CLOSED if result is lost', () => {
         const wrapper = render(<ResultMobile is_visible={true} result='lost' />);
-        expect(wrapper.getByText('Lost')).toBeInTheDocument(true);
+        expect(wrapper.getByText('Closed')).toBeInTheDocument(true);
     });
 });

f4f0d885a45e6fb0646cc9e44bc1e05433a6a396
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-10-09 16:07:20 +0400

    chore: refactor accounts-limit-article

--- packages/account/src/Components/account-limits/account-limits-article.tsx
+++ packages/account/src/Components/account-limits/account-limits-article.tsx
@@ -2,16 +2,12 @@ import * as React from 'react';
 import { Localize, localize } from '@deriv/translations';
 import AccountArticle from 'Components/article';
 
-const AccountLimitsArticle = () => {
-    const getDescription = () => {
-        const description = [
-            <Localize key={0} i18n_default_text='These are default limits that we apply to your accounts.' />,
-        ];
+const getDescription = () => [
+    <Localize key={0} i18n_default_text='These are default limits that we apply to your accounts.' />,
+];
 
-        return description;
-    };
-
-    return <AccountArticle title={localize('Account limits')} descriptions={getDescription()} />;
-};
+const AccountLimitsArticle = () => (
+    <AccountArticle title={localize('Account limits')} descriptions={getDescription()} />
+);
 
 export default AccountLimitsArticle;

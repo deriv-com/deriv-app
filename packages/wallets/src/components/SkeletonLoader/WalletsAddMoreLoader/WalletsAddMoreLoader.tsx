7da34fcea94ee198c2c4ba0df471acb6713d5364
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-10-10 12:07:12 +0800

    refactor: cleam=n-ups

--- packages/wallets/src/components/SkeletonLoader/WalletsAddMoreLoader/WalletsAddMoreLoader.tsx
+++ packages/wallets/src/components/SkeletonLoader/WalletsAddMoreLoader/WalletsAddMoreLoader.tsx
@@ -3,8 +3,8 @@ import './WalletsAddMoreLoader.scss';
 
 const WalletsAddMoreLoader = () => {
     return (
-        <div className=' wallets-skeleton wallets-add-more-loader'>
-            <div className=' wallets-skeleton wallets-add-more-loader__body' />
+        <div className='wallets-skeleton wallets-add-more-loader'>
+            <div className='wallets-skeleton wallets-add-more-loader__body' />
         </div>
     );
 };
--- packages/wallets/src/components/SkeletonLoader/WalletsCarouselLoader/WalletsCarouselLoader.tsx
+++ packages/wallets/src/components/SkeletonLoader/WalletsCarouselLoader/WalletsCarouselLoader.tsx
@@ -7,10 +7,10 @@ const WalletsCarouselLoader = () => {
             <div className='wallets-carousel-loader__card-container'>
                 {Array.from({ length: 3 }).map((_, idx) => (
                     <div
-                        className=' wallets-skeleton wallets-carousel-loader__card'
+                        className='wallets-skeleton wallets-carousel-loader__card'
                         key={`wallets-carousel-loader-card-${idx}`}
                     >
-                        <div className=' wallets-skeleton wallets-carousel-loader__card-body' />
+                        <div className='wallets-skeleton wallets-carousel-loader__card-body' />
                     </div>
                 ))}
             </div>
@@ -21,7 +21,7 @@ const WalletsCarouselLoader = () => {
             <div className='wallets-carousel-loader__actions'>
                 {Array.from({ length: 4 }).map((_, idx) => (
                     <div
-                        className=' wallets-skeleton wallets-carousel-loader__actions-button'
+                        className='wallets-skeleton wallets-carousel-loader__actions-button'
                         key={`wallets-carousel-loader-action-${idx}`}
                     />
                 ))}

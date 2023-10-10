95d40093a30479785f6104c964542049cbbf9ecd
maryia-matskevich-deriv <103181650+maryia-matskevich-deriv@users.noreply.github.com>
2023-10-10 16:50:10 +0300

    Maryia/Bot-681/test: add test case for flyout-img.jsx (#10533)
    * test: FlyoutImage
    
    * chore: remove unused methods
    
    * fix: .jsx to .tsx and add types
    
    * chore: resolve conflicts
    
    * chore: trigger

--- packages/bot-web-ui/src/components/flyout/help-contents/flyout-img.tsx
+++ packages/bot-web-ui/src/components/flyout/help-contents/flyout-img.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
 
-const FlyoutImage = props => {
+type TFlyoutImageProps = {
+    width: string;
+    url: string;
+};
+
+const FlyoutImage = (props: TFlyoutImageProps) => {
     const { width, url } = props;
     const style = { width };
 
     return (
-        <div className='flyout__item'>
+        <div className='flyout__item' data-testid='dt_flyout_image'>
             <img src={url} className='flyout__image' style={style} />
         </div>
     );
-};
-
-FlyoutImage.propTypes = {
-    url: PropTypes.string,
 };
 
 export default FlyoutImage;

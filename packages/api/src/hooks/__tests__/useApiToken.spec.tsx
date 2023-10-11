c4dfb83a2e11973511f97d5d7af8fe4281e1a322
Shaheer <shaheer@deriv.com>
2023-10-11 15:36:57 +0400

    test: :test_tube: fixes test case in useApiToken

--- packages/api/src/hooks/__tests__/useApiToken.spec.tsx
+++ packages/api/src/hooks/__tests__/useApiToken.spec.tsx
@@ -1,38 +1,43 @@
+import React from 'react';
+import { useWS } from '@deriv/shared';
 import { renderHook } from '@testing-library/react-hooks';
 import useApiToken from '../useApiToken';
 import APIProvider from '../../APIProvider';
-import React from 'react';
-import { WS } from '@deriv/shared';
 
 jest.mock('@deriv/shared', () => ({
-    WS: {
-        send: jest.fn().mockResolvedValueOnce({
-            msg_type: 'api_token',
-            echo_req: {},
-            api_token: {
-                tokens: [
-                    {
-                        display_name: 'Created by script',
-                        last_used: '',
-                        scopes: ['read', 'trade', 'payments', 'admin'],
-                        token: '',
-                        valid_for_ip: '',
-                    },
-                    {
-                        display_name: 'test12',
-                        last_used: '',
-                        scopes: ['read', 'payments'],
-                        token: '',
-                        valid_for_ip: '',
-                    },
-                ],
-            },
-        }),
-    },
+    ...jest.requireActual('@deriv/shared'),
+    useWS: jest.fn(),
 }));
+const mockUseWS = useWS as jest.MockedFunction<typeof useWS>;
 
 describe('useApiToken', () => {
     it('should return the token data when a get call is made', async () => {
+        mockUseWS.mockReturnValue({
+            send: jest.fn(() =>
+                Promise.resolve({
+                    msg_type: 'api_token',
+                    echo_req: {},
+                    api_token: {
+                        tokens: [
+                            {
+                                display_name: 'Created by script',
+                                last_used: '',
+                                scopes: ['read', 'trade', 'payments', 'admin'],
+                                token: '',
+                                valid_for_ip: '',
+                            },
+                            {
+                                display_name: 'test12',
+                                last_used: '',
+                                scopes: ['read', 'payments'],
+                                token: '',
+                                valid_for_ip: '',
+                            },
+                        ],
+                    },
+                })
+            ),
+        });
         const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
         const { result, waitForNextUpdate } = renderHook(() => useApiToken(), { wrapper });
 
@@ -45,7 +50,9 @@ describe('useApiToken', () => {
 
     it('should return error when error is thrown', async () => {
         const error_message = { message: 'Invalid API token' };
-        WS.send.mockResolvedValueOnce({ error: error_message });
+        mockUseWS.mockReturnValue({
+            send: jest.fn(() => Promise.resolve({ error: error_message })),
+        });
         const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
         const { result, waitForNextUpdate } = renderHook(() => useApiToken(), { wrapper });
 

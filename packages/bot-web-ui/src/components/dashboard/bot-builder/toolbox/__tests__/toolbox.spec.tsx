9abedbbc8f7869c83eac6b8571c5876751e23264
rupato-deriv <rupato@regentmarkets.com>
2023-10-12 11:08:47 +0800

    fix: code smells and added mock for Blockly

--- packages/bot-web-ui/src/components/dashboard/bot-builder/toolbox/__tests__/toolbox.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/bot-builder/toolbox/__tests__/toolbox.spec.tsx
@@ -1,55 +1,53 @@
 import React from 'react';
-import { isMobile } from '@deriv/shared';
+import { mockStore, StoreProvider, useStore } from '@deriv/stores';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { useDBotStore } from 'Stores/useDBotStore';
+import { mock_ws, mockBlockly } from 'Utils/mock';
+import { DBotStoreProvider, mockDBotStore, useDBotStore } from 'Stores/useDBotStore';
 import Toolbox from '../toolbox';
 
-const mockDbotStore = {
-    toolbox: {
-        hasSubCategory: jest.fn(),
-        is_search_loading: false,
-        onMount: jest.fn(),
-        onSearch: jest.fn(),
-        onSearchBlur: jest.fn(),
-        onSearchClear: jest.fn(),
-        onSearchKeyUp: jest.fn(),
-        onToolboxItemClick: jest.fn(),
-        onToolboxItemExpand: jest.fn(),
-        onUnmount: jest.fn(),
-        sub_category_index: [],
-    },
-    flyout: {
-        setVisibility: jest.fn(),
-    },
-    quick_strategy: {
-        loadDataStrategy: jest.fn(),
-    },
-};
-
-jest.mock('Stores/useDBotStore', () => ({
-    useDBotStore: jest.fn(() => mockDbotStore),
+jest.mock('@deriv/bot-skeleton/src/scratch/blockly', () => jest.fn());
+jest.mock('@deriv/bot-skeleton/src/scratch/dbot', () => ({
+    saveRecentWorkspace: jest.fn(),
+    unHighlightAllBlocks: jest.fn(),
 }));
+jest.mock('@deriv/bot-skeleton/src/scratch/hooks/block_svg', () => jest.fn());
+
+window.Blockly = mockBlockly({});
 
 describe('Toolbox component', () => {
+    let wrapper: ({ children }: { children: JSX.Element }) => JSX.Element;
+    const mock_store = mockStore({
+        ui: {
+            is_mobile: false,
+        },
+    });
+    const mock_DBot_store = mockDBotStore(mock_store, mock_ws);
+
+    beforeAll(() => {
+        wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock_store}>
+                <DBotStoreProvider ws={mock_ws} mock={mock_DBot_store}>
+                    {children}
+                </DBotStoreProvider>
+            </StoreProvider>
+        );
+    });
+
+    beforeEach(() => {
+        render(<Toolbox />, { wrapper });
+    });
     it('should render Toolbox with content wrapper is open', () => {
-        render(<Toolbox />);
         expect(screen.getByTestId('dashboard__toolbox')).toBeInTheDocument();
         expect(screen.getByTestId('db-toolbox__content-wrapper')).toHaveClass('db-toolbox__content-wrapper active');
     });
     it('should render Toolbox with content wrapper is open', () => {
-        const setVisibility = jest.fn();
-        (useDBotStore as jest.Mock).mockReturnValue({ ...mockDbotStore, flyout: { setVisibility } });
-        render(<Toolbox />);
         expect(screen.getByTestId('db-toolbox__title')).toBeInTheDocument();
-
         userEvent.click(screen.getByTestId('db-toolbox__title'));
         expect(screen.getByTestId('db-toolbox__content-wrapper')).not.toHaveClass('db-toolbox__content-wrapper active');
-        expect(setVisibility).toHaveBeenCalled();
     });
     it('should not render Toolbox if it is mobile version', () => {
-        render(<Toolbox />);
-        if (isMobile()) {
+        if (mock_store.ui.is_mobile) {
             expect(screen.getByRole('dashboard__toolbox')).toBeEmptyDOMElement();
         }
     });

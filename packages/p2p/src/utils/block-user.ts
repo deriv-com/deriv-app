9a6b07be710c528c8956bc3567f6c2f33c7f56fb
ameerul hady <ameerul@deriv.com>
2023-10-12 11:10:36 +0800

    chore: added test cases for new util files

--- packages/p2p/src/utils/block-user.ts
+++ packages/p2p/src/utils/block-user.ts
@@ -1,12 +1,21 @@
 import { isMobile } from '@deriv/shared';
 import { localize } from 'Components/i18next';
 
+/**
+ * Gets the error message to be displayed
+ *
+ * @param {String} default_message - Default message to be displayed
+ * @param {boolean} error_message_boolean - Boolean to check if error message is to be displayed
+ * @param {boolean} is_invalid_advertiser_id - Boolean to check if advertiser id is invalid
+ * @param {String} name - Name of the advertiser
+ * @returns {String} error message
+ */
 export const getErrorMessage = (
     default_message: string,
     error_message_boolean: boolean,
     is_invalid_advertiser_id: boolean,
     name: string
-) => {
+): string => {
     if (is_invalid_advertiser_id) {
         if (error_message_boolean) {
             return localize("Unblocking wasn't possible as {{name}} is not using Deriv P2P anymore.", {
@@ -20,7 +29,14 @@ export const getErrorMessage = (
     return default_message;
 };
 
-export const getErrorModalTitle = (is_invalid_advertiser_id: boolean, name: boolean) => {
+/**
+ * Gets the error modal title
+ *
+ * @param {boolean} is_invalid_advertiser_id - Boolean to check if advertiser id is invalid
+ * @param {String} name - Name of the advertiser
+ * @returns {String} error modal title
+ */
+export const getErrorModalTitle = (is_invalid_advertiser_id: boolean, name: string): string => {
     return is_invalid_advertiser_id
         ? localize('{{name}} is no longer on Deriv P2P', {
               name,
@@ -28,4 +44,9 @@ export const getErrorModalTitle = (is_invalid_advertiser_id: boolean, name: bool
         : localize('Unable to block advertiser');
 };
 
-export const getWidth = () => (isMobile() ? '90rem' : '40rem');
+/**
+ * Gets the width of the modal
+ *
+ * @returns {String} width of the modal
+ */
+export const getWidth = (): string => (isMobile() ? '90rem' : '40rem');

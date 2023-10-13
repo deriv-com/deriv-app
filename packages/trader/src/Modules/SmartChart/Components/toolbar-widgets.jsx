18349f704d25522319d4099aeebe2c23f57981d3
maryia-deriv <maryia@binary.com>
2023-10-13 10:32:45 +0300

    fix: cannot wrap ToolbarWidgets with observer since it is passed into smartcharts

--- packages/trader/src/Modules/SmartChart/Components/toolbar-widgets.jsx
+++ packages/trader/src/Modules/SmartChart/Components/toolbar-widgets.jsx
@@ -2,27 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { isDesktop } from '@deriv/shared';
 import { ChartMode, DrawTools, Share, StudyLegend, Views, ToolbarWidget } from 'Modules/SmartChart';
-import { observer, useStores } from '@deriv/stores';
 
-const ToolbarWidgets = observer(({ position }) => {
-    const {
-        ui: { is_mobile },
-    } = useStores();
-    return (
-        <ToolbarWidget position={position || is_mobile ? 'bottom' : null}>
-            <ChartMode portalNodeId='modal_root' />
-            {isDesktop() && <StudyLegend portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
-            {isDesktop() && <Views portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
-            {isDesktop() && <DrawTools portalNodeId='modal_root' />}
-            {isDesktop() && <Share portalNodeId='modal_root' />}
-        </ToolbarWidget>
-    );
-});
+const ToolbarWidgets = ({ is_mobile, position }) => (
+    <ToolbarWidget position={position || is_mobile ? 'bottom' : null}>
+        <ChartMode portalNodeId='modal_root' />
+        {isDesktop() && <StudyLegend portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
+        {isDesktop() && <Views portalNodeId='modal_root' searchInputClassName='data-hj-whitelist' />}
+        {isDesktop() && <DrawTools portalNodeId='modal_root' />}
+        {isDesktop() && <Share portalNodeId='modal_root' />}
+    </ToolbarWidget>
+);
 
 ToolbarWidgets.propTypes = {
     position: PropTypes.string,
-    updateChartType: PropTypes.func,
-    updateGranularity: PropTypes.func,
+    is_mobile: PropTypes.boolean,
 };
 
 export default React.memo(ToolbarWidgets);

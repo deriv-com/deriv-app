73e3311bedb9581e0fbcc62785473d3cfed8004d
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-10-16 10:51:57 +0800

    feat: change contents

--- packages/wallets/src/components/WalletNoWalletFoundState/WalletNoWalletFoundState.tsx
+++ packages/wallets/src/components/WalletNoWalletFoundState/WalletNoWalletFoundState.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import useDevice from '../../hooks/useDevice';
 import NoWalletIcon from '../../public/images/no-wallet.svg';
+import { WalletButton, WalletText } from '../Base';
 import './WalletNoWalletFoundState.scss';
 
 const WalletNoWalletFoundState = () => {
+    const { isMobile } = useDevice();
     const history = useHistory();
+
     return (
         <div className='wallets-no-wallet-found-state'>
-            <NoWalletIcon height={100} width={100} />
-            <h4 className='wallets-no-wallet-found-state__title'>You have no wallet account 🐣</h4>
-            <p className='wallets-no-wallet-found-state__description'>
-                Disable the <span onClick={() => history.push('/endpoint')}>next_wallet</span> feature flag to see
-                Trader&apos;s Hub{' '}
-            </p>
+            <NoWalletIcon height={300} width={300} />
+            <div className='wallets-no-wallet-found-state-content__container'>
+                <div className='wallets-no-wallet-found-state-content'>
+                    <WalletText size='3xl' weight='bold'>
+                        You have no wallet account 🐣
+                    </WalletText>
+                    <WalletText size={isMobile ? 'sm' : 'md'}>
+                        Disable the{' '}
+                        <span className='wallets-no-wallet-found-state-content__emphasize-text'>next_wallet</span>{' '}
+                        feature flag to see Trader&apos;s Hub.
+                    </WalletText>
+                </div>
+                <WalletButton
+                    // @ts-expect-error putting `/endpoint` here because this component is only for internal use.
+                    onClick={() => history.push('/endpoint')}
+                    size='lg'
+                >
+                    <WalletText color='white' size='sm' weight='bold'>
+                        Endpoint
+                    </WalletText>
+                </WalletButton>
+            </div>
         </div>
     );
 };
--- packages/wallets/src/routes/WalletsListingRoute/WalletsListingRoute.tsx
+++ packages/wallets/src/routes/WalletsListingRoute/WalletsListingRoute.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { useWalletAccountsList } from '@deriv/api';
-import {
-    DesktopWalletsList,
-    WalletNoWalletFoundState,
-    WalletsAddMoreCarousel,
-    WalletsCarousel,
-} from '../../components';
+import { DesktopWalletsList, WalletsAddMoreCarousel, WalletsCarousel } from '../../components';
 import useDevice from '../../hooks/useDevice';
 import './WalletsListingRoute.scss';
 
 const WalletsListingRoute: React.FC = () => {
     const { isMobile } = useDevice();
-    const { data: walletAccounts } = useWalletAccountsList();
-
-    if (!walletAccounts || !walletAccounts.length) return <WalletNoWalletFoundState />;
-
     return (
         <div className='wallets-listing-route'>
             {isMobile ? <WalletsCarousel /> : <DesktopWalletsList />}

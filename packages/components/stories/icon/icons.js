df8359e7adec66e0d78de090de8d11f8c5091bb2
adrienne-rio <adrienne@deriv.com>
2023-10-16 14:49:35 +0800

    chore: reverted icons

--- packages/components/stories/icon/icons.js
+++ packages/components/stories/icon/icons.js
@@ -57,7 +57,7 @@ export const icons =
         'IcAppstoreWalletUsdcLight',
         'IcAppstoreWalletUsdtLight',
         'IcAppstoreWalletsLink',
-        'IcAppstoreWarning'
+        'IcAppstoreWarning',
     ],
     'brand': [
         'IcBrandCtraderWordmark',
@@ -69,7 +69,7 @@ export const icons =
         'IcBrandDmt5FinancialStp',
         'IcBrandDmt5Financial',
         'IcBrandDmt5Synthetics',
-        'IcBrandDxtradeWordmark'
+        'IcBrandDxtradeWordmark',
     ],
     'cashier': [
         'IcCashierAdd',
@@ -246,7 +246,7 @@ export const icons =
         'IcCashierWyreLight',
         'IcCashierZenithbankDark',
         'IcCashierZenithbankLight',
-        'IcCashier'
+        'IcCashier',
     ],
     'common': [
         'IcAccountCross',
@@ -626,7 +626,7 @@ export const icons =
         'IcWithdrawRequestVerification',
         'IcZingpay',
         'IcZoomIn',
-        'IcZoomOut'
+        'IcZoomOut',
     ],
     'contract': [
         'IcContractBarrier',
@@ -644,7 +644,7 @@ export const icons =
         'IcContractStartTimeCircle',
         'IcContractStartTime',
         'IcContractStrike',
-        'IcContractTarget'
+        'IcContractTarget',
     ],
     'currency': [
         'IcCurrencyAud',
@@ -668,10 +668,10 @@ export const icons =
         'IcCurrencyUsdc',
         'IcCurrencyUsdk',
         'IcCurrencyUst',
-        'IcCurrencyVirtual'
+        'IcCurrencyVirtual',
     ],
     'derivez': [
-        'IcDerivez'
+        'IcDerivez',
     ],
     'dxtrade': [
         'IcDxtradeDerivX',
@@ -688,7 +688,7 @@ export const icons =
         'IcDxtradeFinancialPlatform',
         'IcDxtradeFinancial',
         'IcDxtradeOnePassword',
-        'IcDxtradeSyntheticPlatform'
+        'IcDxtradeSyntheticPlatform',
     ],
     'flag': [
         'IcFlagDe',
@@ -705,7 +705,7 @@ export const icons =
         'IcFlagUk',
         'IcFlagVi',
         'IcFlagZhCn',
-        'IcFlagZhTw'
+        'IcFlagZhTw',
     ],
     'mt5': [
         'IcMt5CfdPlatform',
@@ -730,12 +730,10 @@ export const icons =
         'IcMt5Responsive',
         'IcMt5Support',
         'IcMt5SwapFreePlatform',
-        'IcMt5SwapFree',
         'IcMt5SyntheticDashboard',
         'IcMt5SyntheticIndices',
         'IcMt5SyntheticPlatform',
-        'IcMt5Synthetic',
-        'IcMt5TradeTypes'
+        'IcMt5TradeTypes',
     ],
     'option': [
         'IcOptionAccumulators',
@@ -753,7 +751,7 @@ export const icons =
         'IcOptionOverUnder',
         'IcOptionRaiseFall',
         'IcOptionTouchNotouch',
-        'IcOptionUpDownAsian'
+        'IcOptionUpDownAsian',
     ],
     'rebranding': [
         'IcRebrandingBinaryBot',
@@ -779,7 +777,7 @@ export const icons =
         'IcRebrandingMt5Logo',
         'IcRebrandingMt5SwapFree',
         'IcRebrandingSmarttraderDashboard',
-        'IcRebrandingSmarttrader'
+        'IcRebrandingSmarttrader',
     ],
     'stock': [
         'IcStockAdidasSalomon',
@@ -829,7 +827,7 @@ export const icons =
         'IcStockVisa',
         'IcStockWallMart',
         'IcStockWaltDisney',
-        'IcStockZoom'
+        'IcStockZoom',
     ],
     'tradetype': [
         'IcTradetypeAccu',
@@ -868,7 +866,7 @@ export const icons =
         'IcTradetypeTurbosshort',
         'IcTradetypeUpordown',
         'IcTradetypeVanillaLongCall',
-        'IcTradetypeVanillaLongPut'
+        'IcTradetypeVanillaLongPut',
     ],
     'underlying': [
         'IcUnderlying1HZ100V',
@@ -1027,7 +1025,7 @@ export const icons =
         'IcUnderlyingWLDEUR',
         'IcUnderlyingWLDGBP',
         'IcUnderlyingWLDUSD',
-        'IcUnderlyingWLDXAU'
+        'IcUnderlyingWLDXAU',
     ],
     'wallet': [
         'IcWalletBitcoinDark',
@@ -1058,6 +1056,6 @@ export const icons =
         'IcWalletTetherDark',
         'IcWalletTetherLight',
         'IcWalletUsdCoinDark',
-        'IcWalletUsdCoinLight'
-    ]
+        'IcWalletUsdCoinLight',
+    ],
 }

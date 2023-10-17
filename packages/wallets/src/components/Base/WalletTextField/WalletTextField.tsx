f09da099b6733f98d5a5e7eaadaf669c062a38d6
aizad-deriv <aizad@deriv.com>
2023-10-17 19:46:51 +0800

    fix: added hover affects, helper message and character counter

--- packages/wallets/src/components/Base/WalletTextField/WalletTextField.tsx
+++ packages/wallets/src/components/Base/WalletTextField/WalletTextField.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, useState } from 'react';
+import React, { ChangeEvent, InputHTMLAttributes, ReactElement, useState } from 'react';
 import classNames from 'classnames';
 import WalletText from '../WalletText/WalletText';
 import styles from './WalletTextField.modules.css';
@@ -6,51 +6,70 @@ import styles from './WalletTextField.modules.css';
 interface WalletTextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
     defaultValue?: string;
     helperMessage?: string;
-    id: string;
+    icon?: ReactElement;
     label?: string;
     leftIcon?: React.ReactNode;
-    showHelperMessage?: boolean;
+    showMessage?: boolean;
 }
 
 const WalletTextField: React.FC<WalletTextFieldProps> = ({
     defaultValue = '',
     helperMessage,
-    id,
+    icon,
     label,
-    leftIcon,
-    showHelperMessage = false,
+    showMessage = false,
+    ...rest
 }) => {
     const [value, setValue] = useState(defaultValue);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.target.value;
+        setValue(newValue);
+
+        if (rest.onChange) {
+            rest.onChange(e);
+        }
+    };
+
+    const MessageContainer: React.FC<{ helperMessage?: string; maxLength?: number }> = ({
+        helperMessage,
+        maxLength,
+    }) => (
+        <>
+            {helperMessage && (
+                <WalletText color='less-prominent' lineHeight='sm' size='xs' style={{ float: 'left' }}>
+                    {helperMessage}
+                </WalletText>
+            )}
+            {maxLength !== undefined && (
+                <WalletText align='right' color='less-prominent' lineHeight='sm' size='xs' style={{ float: 'right' }}>
+                    {value.length} / {maxLength}
+                </WalletText>
+            )}
+        </>
+    );
+
     return (
-        <div className={styles[`wallets-textfield`]}>
-            <div className={styles[`wallets-textfield__content`]}>
-                {leftIcon && <div className={styles[`wallets-textfield__icon`]}>{leftIcon}</div>}
+        <div className={classNames(styles['wallets-textfield'])}>
+            <div className={styles['wallets-textfield__content']}>
                 <input
                     className={styles['wallets-textfield__field']}
-                    id={id}
-                    onChange={e => setValue(e.target.value)}
+                    onChange={handleChange}
                     placeholder={label}
                     type='text'
                     value={value}
+                    {...rest}
                 />
                 {label && (
-                    <label
-                        className={classNames(styles[`wallets-textfield__label`], {
-                            [styles[`wallets-textfield__label--with-icon`]]: !!leftIcon,
-                        })}
-                        htmlFor={id}
-                    >
+                    <label className={styles['wallets-textfield__label']} htmlFor={rest.id}>
                         {label}
                     </label>
                 )}
+                {icon && <div className={styles['wallets-textfield__icon']}>{icon}</div>}
             </div>
-            {showHelperMessage && (
-                <div className={styles['wallets-textfield__message']}>
-                    <WalletText color='less-prominent' size='xs'>
-                        {helperMessage}
-                    </WalletText>
-                </div>
-            )}
+            <div className={styles['wallets-textfield__message-container']}>
+                {showMessage && <MessageContainer helperMessage={helperMessage} maxLength={rest.maxLength} />}
+            </div>
         </div>
     );
 };

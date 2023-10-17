8fd7ea436951d7e9908e292baf6bacf8473079e1
amina-deriv <amina@regentmarkets.com>
2023-10-17 16:33:01 +0400

    fix: icons

--- packages/components/stories/icon/icons.js
+++ packages/components/stories/icon/icons.js
@@ -57,7 +57,7 @@ export const icons =
         'IcAppstoreWalletUsdcLight',
         'IcAppstoreWalletUsdtLight',
         'IcAppstoreWalletsLink',
-        'IcAppstoreWarning'
+        'IcAppstoreWarning',
     ],
     'brand': [
         'IcBrandCtraderWordmark',
@@ -69,7 +69,7 @@ export const icons =
         'IcBrandDmt5FinancialStp',
         'IcBrandDmt5Financial',
         'IcBrandDmt5Synthetics',
-        'IcBrandDxtradeWordmark'
+        'IcBrandDxtradeWordmark',
     ],
     'cashier': [
         'IcCashierAdd',
@@ -246,7 +246,7 @@ export const icons =
         'IcCashierWyreLight',
         'IcCashierZenithbankDark',
         'IcCashierZenithbankLight',
-        'IcCashier'
+        'IcCashier',
     ],
     'common': [
         'IcAccountCross',
@@ -629,7 +629,7 @@ export const icons =
         'IcWithdrawRequestVerification',
         'IcZingpay',
         'IcZoomIn',
-        'IcZoomOut'
+        'IcZoomOut',
     ],
     'contract': [
         'IcContractBarrier',
@@ -647,7 +647,7 @@ export const icons =
         'IcContractStartTimeCircle',
         'IcContractStartTime',
         'IcContractStrike',
-        'IcContractTarget'
+        'IcContractTarget',
     ],
     'currency': [
         'IcCurrencyAud',
@@ -743,7 +743,7 @@ export const icons =
         'IcMt5SyntheticPlatform',
         'IcMt5TradeTypes',
         'IcMt5VanuatuDerived',
-        'IcMt5VanuatuFinancial'
+        'IcMt5VanuatuFinancial',
     ],
     'option': [
         'IcOptionAccumulators',
@@ -761,7 +761,7 @@ export const icons =
         'IcOptionOverUnder',
         'IcOptionRaiseFall',
         'IcOptionTouchNotouch',
-        'IcOptionUpDownAsian'
+        'IcOptionUpDownAsian',
     ],
     'rebranding': [
         'IcRebrandingBinaryBot',
@@ -787,7 +787,7 @@ export const icons =
         'IcRebrandingMt5Logo',
         'IcRebrandingMt5SwapFree',
         'IcRebrandingSmarttraderDashboard',
-        'IcRebrandingSmarttrader'
+        'IcRebrandingSmarttrader',
     ],
     'stock': [
         'IcStockAdidasSalomon',
@@ -837,7 +837,7 @@ export const icons =
         'IcStockVisa',
         'IcStockWallMart',
         'IcStockWaltDisney',
-        'IcStockZoom'
+        'IcStockZoom',
     ],
     'tradetype': [
         'IcTradetypeAccu',
@@ -876,7 +876,7 @@ export const icons =
         'IcTradetypeTurbosshort',
         'IcTradetypeUpordown',
         'IcTradetypeVanillaLongCall',
-        'IcTradetypeVanillaLongPut'
+        'IcTradetypeVanillaLongPut',
     ],
     'underlying': [
         'IcUnderlying1HZ100V',
@@ -1035,7 +1035,7 @@ export const icons =
         'IcUnderlyingWLDEUR',
         'IcUnderlyingWLDGBP',
         'IcUnderlyingWLDUSD',
-        'IcUnderlyingWLDXAU'
+        'IcUnderlyingWLDXAU',
     ],
     'wallet': [
         'IcWalletBitcoinDark',
@@ -1066,6 +1066,6 @@ export const icons =
         'IcWalletTetherDark',
         'IcWalletTetherLight',
         'IcWalletUsdCoinDark',
-        'IcWalletUsdCoinLight'
-    ]
+        'IcWalletUsdCoinLight',
+    ],
 }

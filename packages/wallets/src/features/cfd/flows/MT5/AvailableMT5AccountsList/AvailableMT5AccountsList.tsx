046d42c131ede03576b08d69359e9a15c87273a6
aizad-deriv <aizad@deriv.com>
2023-10-17 17:02:36 +0800

    fix: remove WalletText in AvailableMT5AccountList

--- packages/wallets/src/features/cfd/flows/MT5/AvailableMT5AccountsList/AvailableMT5AccountsList.tsx
+++ packages/wallets/src/features/cfd/flows/MT5/AvailableMT5AccountsList/AvailableMT5AccountsList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSortedMT5Accounts } from '@deriv/api';
-import { TradingAccountCard, WalletButton, WalletText } from '../../../../../components';
+import { TradingAccountCard, WalletButton } from '../../../../../components';
 import { useModal } from '../../../../../components/ModalProvider';
 import DerivedMT5 from '../../../../../public/images/mt5-derived.svg';
 import FinancialMT5 from '../../../../../public/images/mt5-financial.svg';

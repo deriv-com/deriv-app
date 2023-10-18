d4bc8dded1f4c3db84d5b5d3b2d21f5bc57504c4
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-10-18 12:03:54 +0400

    fix: dark mode banner

--- packages/appstore/src/components/appstore-banner/appstore-banner-image.tsx
+++ packages/appstore/src/components/appstore-banner/appstore-banner-image.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SVGMigrateDesktopImage from 'Assets/svgs/banner/svg-migrate-desktop.svg';
+import SVGMigrateDesktopImageDark from 'Assets/svgs/banner/svg-migrate-desktop-dark.svg';
 import SVGMigrateMobileImage from 'Assets/svgs/banner/svg-migrate-mobile.svg';
+import SVGMigrateMobileImageDark from 'Assets/svgs/banner/svg-migrate-mobile-dark.svg';
 
 export type BannerImageProps<T> = {
     image: T;
@@ -13,6 +15,8 @@ type TAppstoreBannerImageList = keyof typeof AppstoreBannerImageList;
 const AppstoreBannerImageList = {
     svg_migrate_desktop: SVGMigrateDesktopImage,
     svg_migrate_mobile: SVGMigrateMobileImage,
+    svg_migrate_desktop_dark: SVGMigrateDesktopImageDark,
+    svg_migrate_mobile_dark: SVGMigrateMobileImageDark,
 };
 
 const AppstoreBannerImage = ({ image, className, width }: BannerImageProps<TAppstoreBannerImageList>) => {

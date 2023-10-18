0d5aabc0145e14475b17cbb40d9cab1a1d69905c
aizad-deriv <aizad@deriv.com>
2023-10-18 17:26:01 +0800

    fix: remove ...rest

--- packages/wallets/src/components/Base/WalletTextField/WalletTextField.tsx
+++ packages/wallets/src/components/Base/WalletTextField/WalletTextField.tsx
@@ -3,34 +3,39 @@ import classNames from 'classnames';
 import WalletText from '../WalletText/WalletText';
 import styles from './WalletTextField.modules.css';
 
-interface WalletTextFieldProps extends ComponentProps<'input'> {
+interface WalletTextFieldProps {
     defaultValue?: string;
     helperMessage?: string;
     icon?: ReactElement;
+    id?: ComponentProps<'input'>['id'];
     label?: string;
     leftIcon?: React.ReactNode;
+    maxLength?: ComponentProps<'input'>['maxLength'];
+    onChange?: ComponentProps<'input'>['onChange'];
     showMessage?: boolean;
 }
 
 type MessageContainerProps = {
     helperMessage?: WalletTextFieldProps['helperMessage'];
-    maxLength?: number;
+    maxLength?: WalletTextFieldProps['maxLength'];
 };
 
 const WalletTextField: React.FC<WalletTextFieldProps> = ({
     defaultValue = '',
     helperMessage,
     icon,
+    id = 'wallet-textfield',
     label,
+    maxLength,
+    onChange,
     showMessage = false,
-    ...rest
 }) => {
     const [value, setValue] = useState(defaultValue);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const newValue = e.target.value;
         setValue(newValue);
-        rest?.onChange?.(e);
+        onChange?.(e);
     };
 
     const MessageContainer: React.FC<MessageContainerProps> = ({ helperMessage, maxLength }) => (
@@ -40,7 +45,7 @@ const WalletTextField: React.FC<WalletTextFieldProps> = ({
                     {helperMessage}
                 </WalletText>
             )}
-            {maxLength !== undefined && (
+            {maxLength && (
                 <WalletText align='right' color='less-prominent' lineHeight='sm' size='xs' style={{ float: 'right' }}>
                     {value.length} / {maxLength}
                 </WalletText>
@@ -53,21 +58,22 @@ const WalletTextField: React.FC<WalletTextFieldProps> = ({
             <div className={styles['wallets-textfield__content']}>
                 <input
                     className={styles['wallets-textfield__field']}
+                    id={id}
+                    maxLength={maxLength}
                     onChange={handleChange}
                     placeholder={label}
                     type='text'
                     value={value}
-                    {...rest}
                 />
                 {label && (
-                    <label className={styles['wallets-textfield__label']} htmlFor={rest.id}>
+                    <label className={styles['wallets-textfield__label']} htmlFor={id}>
                         {label}
                     </label>
                 )}
                 {icon && <div className={styles['wallets-textfield__icon']}>{icon}</div>}
             </div>
             <div className={styles['wallets-textfield__message-container']}>
-                {showMessage && <MessageContainer helperMessage={helperMessage} maxLength={rest.maxLength} />}
+                {showMessage && <MessageContainer helperMessage={helperMessage} maxLength={maxLength} />}
             </div>
         </div>
     );

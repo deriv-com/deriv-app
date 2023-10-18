451c79bd6a3749861746eeafc2221133f7f6ba7e
Wojciech Brygola <wojciech@regentmarkets.com>
2023-10-18 10:12:30 +0800

    feat: removed old Text components, removed classes

--- packages/wallets/src/features/cashier/flows/WalletResetBalance/WalletResetBalance.tsx
+++ packages/wallets/src/features/cashier/flows/WalletResetBalance/WalletResetBalance.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useActiveWalletAccount, useRequest } from '@deriv/api';
-import { Icon, Text, Button } from '@deriv/components';
+import { Icon, Text } from '@deriv/components';
 import { useHistory } from 'react-router-dom';
 import useDevice from '../../../../hooks/useDevice';
 import WalletButton from '../../../../components/Base/WalletButton/WalletButton';
+import WalletText from '../../../../components/Base/WalletText/WalletText';
 
 const WalletResetBalance = () => {
     const history = useHistory();
@@ -17,24 +18,23 @@ const WalletResetBalance = () => {
     };
 
     return (
-        <div className='wallets-transactions-no-data-state'>
+        <div className='wallets-reset-balance'>
             <Icon icon={isResetBalanceSuccess ? 'IcDemoResetBalanceDone' : 'IcDemoResetBalance'} size='128' />
-            <p>Reset balance to 10,000.00 USD</p>
-            <Text
-                as='p'
-                className='reset-balance__title'
-                line_height={isMobile ? 'xl' : 'xxl'}
-                size={isMobile ? 's' : 'sm'}
-                weight='bold'
-            >
-                {isResetBalanceSuccess ? (
-                    <>Your balance has been reset to 10,000.00 USD.</>
-                ) : (
-                    <>Reset your virtual balance if it falls below 10,000.00 USD or exceeds 10,000.00 USD.</>
-                )}
-            </Text>
+            <div>
+                <WalletText>Reset balance to 10,000.00 USD</WalletText>
+            </div>
 
-            <div className='wallets-transactions-no-data-state__buttons'>
+            <div>
+                {isResetBalanceSuccess ? (
+                    <WalletText>Your balance has been reset to 10,000.00 USD.</WalletText>
+                ) : (
+                    <WalletText>
+                        Reset your virtual balance if it falls below 10,000.00 USD or exceeds 10,000.00 USD.
+                    </WalletText>
+                )}
+            </div>
+
+            <div>
                 <WalletButton
                     color='primary'
                     disabled={!canResetBalance}

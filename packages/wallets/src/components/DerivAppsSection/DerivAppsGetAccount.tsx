7bad4e54d00ca4a2129274d17b590d377bdbbfc9
Sergei Baranovski <sergei@deriv.com>
2023-10-18 15:55:06 +0300

    feat: final components

---
+++ packages/wallets/src/components/DerivAppsSection/DerivAppsGetAccount.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import DerivApps from '../../public/images/deriv-apps.svg';
+import { WalletButton, WalletText } from '../Base';
+
+type TProps = {
+    isMaltaWallet?: boolean;
+};
+
+export const DerivAppsGetAccount = ({ isMaltaWallet }: TProps) => {
+    return (
+        <div className='wallets-deriv-apps-section'>
+            <DerivApps />
+            <div className='wallets-deriv-apps-section__details'>
+                <WalletText size='sm' weight='bold'>
+                    Deriv Apps
+                </WalletText>
+                <WalletText lineHeight='2xs' size='2xs'>
+                    {isMaltaWallet
+                        ? 'Get a Deriv Apps trading account regulated by MFSA to trade multipliers on Deriv Trader.'
+                        : 'Get a Deriv Apps trading account regulated by MFSA to trade multipliers on Deriv Trader.'}
+                </WalletText>
+            </div>
+            <WalletButton color='primary-light' text='Get' />
+        </div>
+    );
+};
--- packages/wallets/src/components/DerivAppsSection/DerivAppsSection.tsx
+++ packages/wallets/src/components/DerivAppsSection/DerivAppsSection.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import { useActiveWalletAccount } from '@deriv/api';
-import DerivApps from '../../public/images/deriv-apps.svg';
-import { WalletButton, WalletText } from '../Base';
-import { WalletListCardBadge } from '../WalletListCardBadge';
+import { DerivAppsGetAccount } from './DerivAppsGetAccount';
+import { DerivAppsTradingAccount } from './DerivAppsTradingAccount';
 import './DerivAppsSection.scss';
 
 const DerivAppsSection = () => {
     const { data: activeWallet } = useActiveWalletAccount();
+    const tradingAccountLoginId = activeWallet?.linked_to?.find(account => account?.platform === 'dtrade')?.loginid;
 
-    return (
-        <div className='wallets-deriv-apps-section'>
-            <DerivApps />
-            <div className='wallets-deriv-apps-section__details'>
-                <div className='wallets-deriv-apps-section__title-and-badge'>
-                    <WalletText size='sm'>Deriv Apps</WalletText>
-                    <WalletListCardBadge isDemo={activeWallet?.is_virtual} label={activeWallet?.landing_company_name} />
-                </div>
-                <WalletText size='sm' weight='bold'>
-                    [Balance]
-                </WalletText>
-                <WalletText color='less-prominent' lineHeight='sm' size='xs' weight='bold'>
-                    [Account ID]
-                </WalletText>
-            </div>
-            <WalletButton color='white' text='Transfer' variant='outlined' />
-        </div>
+    return tradingAccountLoginId ? (
+        <DerivAppsTradingAccount
+            isDemo={activeWallet?.is_virtual}
+            label={activeWallet?.landing_company_name}
+            tradingAccountLoginId={tradingAccountLoginId}
+        />
+    ) : (
+        <DerivAppsGetAccount isMaltaWallet={activeWallet?.is_malta_wallet} />
     );
 };
 

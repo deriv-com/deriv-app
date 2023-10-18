5f1ae1a28bf81aaba2ebd354206fb0af4f6a80c4
Sergei Baranovski <sergei@deriv.com>
2023-10-18 16:02:01 +0300

    refactor: change text for svg

--- packages/wallets/src/components/DerivAppsSection/DerivAppsGetAccount.tsx
+++ packages/wallets/src/components/DerivAppsSection/DerivAppsGetAccount.tsx
@@ -17,7 +17,7 @@ export const DerivAppsGetAccount = ({ isMaltaWallet }: TProps) => {
                 <WalletText lineHeight='2xs' size='2xs'>
                     {isMaltaWallet
                         ? 'Get a Deriv Apps trading account regulated by MFSA to trade multipliers on Deriv Trader.'
-                        : 'Get a Deriv Apps trading account regulated by MFSA to trade multipliers on Deriv Trader.'}
+                        : 'Get a Deriv Apps trading account to trade options and multipliers on these apps.'}
                 </WalletText>
             </div>
             <WalletButton color='primary-light' text='Get' />

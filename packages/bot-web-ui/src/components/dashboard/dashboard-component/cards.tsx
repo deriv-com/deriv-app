f82724fba1bdee3f1319c2d0ec773ed385b11ef7
Sandeep <sandeep@regentmarkets.com>
2023-10-18 23:40:02 +0800

    refactor: quick-strategy - linked setFormVisibility with dashboard tile

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/cards.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/cards.tsx
@@ -21,7 +21,7 @@ type TCardArray = {
 };
 
 const Cards = observer(({ is_mobile, has_dashboard_strategies }: TCardProps) => {
-    const { dashboard, load_modal, quick_strategy } = useDBotStore();
+    const { dashboard, load_modal, quick_strategy_store_1 } = useDBotStore();
     const {
         onCloseDialog,
         dialog_options,
@@ -33,7 +33,7 @@ const Cards = observer(({ is_mobile, has_dashboard_strategies }: TCardProps) => 
         showVideoDialog,
     } = dashboard;
     const { handleFileChange, loadFileFromLocal } = load_modal;
-    const { loadDataStrategy } = quick_strategy;
+    const { setFormVisibility } = quick_strategy_store_1;
 
     const [is_file_supported, setIsFileSupported] = React.useState<boolean>(true);
     const file_input_ref = React.useRef<HTMLInputElement | null>(null);
@@ -69,7 +69,7 @@ const Cards = observer(({ is_mobile, has_dashboard_strategies }: TCardProps) => 
             content: localize('Quick strategy'),
             method: () => {
                 setActiveTab(DBOT_TABS.BOT_BUILDER);
-                loadDataStrategy();
+                setFormVisibility(true);
             },
         },
     ];

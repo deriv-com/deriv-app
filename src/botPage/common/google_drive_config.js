539bbbfafd7c3e7706c06edb6e4748599444aa94
Farabi <102643568+farabi-deriv@users.noreply.github.com>
2023-10-19 16:02:14 +0800

    Revert "fix: change the scope of google drive"

--- src/botPage/common/google_drive_config.js
+++ src/botPage/common/google_drive_config.js
@@ -5,6 +5,6 @@ export default {
     API_URL_IDENTITY: ['https://accounts.google.com/gsi/client'],
     API_URL_GDRIVE: ['https://apis.google.com/js/api.js'],
     AUTH_SCOPE: 'client:picker',
-    SCOPE: 'https://www.googleapis.com/auth/drive',
+    SCOPE: 'https://www.googleapis.com/auth/drive.file',
     DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
 };

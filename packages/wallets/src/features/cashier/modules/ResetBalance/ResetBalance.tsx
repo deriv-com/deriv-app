d77b52bf8a5a44faae070db0200853d65b7218d1
Aum Bhatt <125039206+aum-deriv@users.noreply.github.com>
2023-10-20 11:20:33 +0400

    aum/chore: separate cashier modules into cashier folder (#10708)
    * refactor: created cashier folder and separated deposit-fiat-module
    
    * refactor: added the deposit-crypto-module
    
    * chore: moved cashier folder inside src
    
    * refactor: created deposit-crypto folder
    
    * refactor: added transfer module
    
    * refactor: added transactions module
    
    * refactor: moved cashier module inside features/cashier
    
    * refactor: replaced components from screens with modules
    
    * refactor: moved withdrawal-email-verification to modules
    
    * chore: moved sub-components for deposit-crypto in components folder
    
    * chore: moved sub-components of transfer to components folder
    
    * chore: export transfer components as modules
    
    * chore: removed the screens folder
    
    * feat: transactions-module
    
    * feat: transfer-module
    
    * feat: withdrawal-module
    
    * chore: renamed TransactionsCryptoRow to TransactionsPendingRow
    
    * chore: renamed TransactionsCrypto to TransactionsPending
    
    * chore: updated Transactions-module with master
    
    * Merge branch 'master' of github.com:binary-com/deriv-app into cashier-folder-wallets
    
    * chore: fixed merge conflicts
    
    * chore: rename Withdrawal to WithdrawalVerification
    
    * chore: trial to remove translations for ar.json
    
    * chore: removed more unwanted commits from master
    
    * chore: removing changes from master
    
    * chore: pushing
    
    * feat: ResetBalanceModule
    
    * chore: updated transactions
    
    * chore: removed screens

--- packages/wallets/src/features/cashier/modules/ResetBalance/ResetBalance.tsx
+++ packages/wallets/src/features/cashier/modules/ResetBalance/ResetBalance.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { useActiveWalletAccount, useMutation } from '@deriv/api';
-import WalletsActionScreen from '../../../../components/WalletsActionScreen/WalletsActionScreen';
+import { useHistory } from 'react-router-dom';
 import IcResetDemoBalance from '../../../../public/images/ic-demo-reset-balance.svg';
 import IcResetDemoBalanceDone from '../../../../public/images/ic-demo-reset-balance-done.svg';
-import './ResetBalance.scss';
+import WalletsActionScreen from '../../../../components/WalletsActionScreen/WalletsActionScreen';
 
 const ResetBalance = () => {
     const history = useHistory();
@@ -18,19 +17,17 @@ const ResetBalance = () => {
     const canReset = activeWallet?.balance !== 10000;
 
     return (
-        <div className='wallets-reset-balance'>
-            <WalletsActionScreen
-                actionText={canReset ? 'Reset balance' : 'Transfer funds'}
-                description={
-                    isResetBalanceSuccess
-                        ? 'Your balance has been reset to 10,000.00 USD.'
-                        : 'Reset your virtual balance if it falls below 10,000.00 USD or exceeds 10,000.00 USD.'
-                }
-                icon={isResetBalanceSuccess ? <IcResetDemoBalanceDone /> : <IcResetDemoBalance />}
-                onAction={canReset ? resetBalance : () => history.push(`/wallets/cashier/transfer`)}
-                title={'Reset balance to 10,000.00 USD'}
-            />
-        </div>
+        <WalletsActionScreen
+            actionText={canReset ? 'Reset balance' : 'Transfer funds'}
+            description={
+                isResetBalanceSuccess
+                    ? 'Your balance has been reset to 10,000.00 USD.'
+                    : 'Reset your virtual balance if it falls below 10,000.00 USD or exceeds 10,000.00 USD.'
+            }
+            icon={isResetBalanceSuccess ? <IcResetDemoBalanceDone /> : <IcResetDemoBalance />}
+            onAction={canReset ? resetBalance : () => history.push(`/wallets/cashier/transfer`)}
+            title={'Reset balance to 10,000.00 USD'}
+        />
     );
 };
 

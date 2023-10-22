e68fd290aa2678e14552566616ebfd4a930d2578
Rupato Braganza <rupatobraganza@Rupato-Braganzas-Mac-L2R5WHGD9W.local>
2023-10-23 03:19:57 +0800

    fix: active symbols bug fixes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -30,6 +30,8 @@ const App = () => {
 
     React.useEffect(() => {
         api_base.api.expectResponse('authorize').then(() => generateActiveSymbols());
+
+        // This code listen is used to listen to active_symbols when user is logged out and load the app
         api_base.api.onMessage().subscribe(({ data }) => {
             if (data.msg_type === 'active_symbols') {
                 symbolPromise.then(() => {

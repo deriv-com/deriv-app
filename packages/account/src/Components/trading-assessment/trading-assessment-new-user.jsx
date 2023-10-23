f09298d682a66645ed019f33cc0dbd5bf5ca219e
Taysuisin <suisin@regentmarkets.com>
2023-10-23 17:59:32 +0800

    chore: use useStore straight inside the form

--- packages/account/src/Components/trading-assessment/trading-assessment-new-user.jsx
+++ packages/account/src/Components/trading-assessment/trading-assessment-new-user.jsx
@@ -1,55 +1,47 @@
 import React from 'react';
 import TradingAssessmentForm from './trading-assessment-form';
-import { useStore, observer } from '@deriv/stores';
 
-const TradingAssessmentNewUser = observer(
-    ({
-        assessment_questions,
-        disabled_items,
-        goToNextStep,
-        goToPreviousStep,
-        onSave,
-        onCancel,
-        onSubmit,
-        getCurrentStep,
-        value,
-        setSubSectionIndex,
-    }) => {
-        const {
-            traders_hub: { is_eu_user },
-        } = useStore();
-        const handleCancel = values => {
-            const current_step = getCurrentStep() - 1;
-            onSave(current_step, values);
-            onCancel(current_step, goToPreviousStep);
-        };
+const TradingAssessmentNewUser = ({
+    assessment_questions,
+    disabled_items,
+    goToNextStep,
+    goToPreviousStep,
+    onSave,
+    onCancel,
+    onSubmit,
+    getCurrentStep,
+    value,
+    setSubSectionIndex,
+}) => {
+    const handleCancel = values => {
+        const current_step = getCurrentStep() - 1;
+        onSave(current_step, values);
+        onCancel(current_step, goToPreviousStep);
+    };
 
-        const handleSubmit = (values, actions, should_override) => {
-            let process_form_values = { ...values };
-            if (should_override) {
-                // Remove the keys with no values
-                process_form_values = Object.entries(process_form_values).reduce((accumulator, [key, val]) => {
-                    if (val) {
-                        return { ...accumulator, [key]: val };
-                    }
-                    return { ...accumulator };
-                }, {});
-            }
-            onSubmit(getCurrentStep() - 1, process_form_values, null, goToNextStep, should_override);
-        };
+    const handleSubmit = (values, actions, should_override) => {
+        let process_form_values = { ...values };
+        if (should_override) {
+            // Remove the keys with no values
+            process_form_values = Object.entries(process_form_values).reduce((accumulator, [key, val]) => {
+                if (val) {
+                    return { ...accumulator, [key]: val };
+                }
+                return { ...accumulator };
+            }, {});
+        }
+        onSubmit(getCurrentStep() - 1, process_form_values, null, goToNextStep, should_override);
+    };
 
-        return (
-            <TradingAssessmentForm
-                assessment_questions={assessment_questions}
-                form_value={value}
-                is_eu_user={is_eu_user}
-                onSubmit={handleSubmit}
-                onCancel={handleCancel}
-                setSubSectionIndex={setSubSectionIndex}
-                disabled_items={disabled_items}
-            />
-        );
-    }
-);
-
+    return (
+        <TradingAssessmentForm
+            assessment_questions={assessment_questions}
+            form_value={value}
+            onSubmit={handleSubmit}
+            onCancel={handleCancel}
+            setSubSectionIndex={setSubSectionIndex}
+            disabled_items={disabled_items}
+        />
+    );
+};
 export default TradingAssessmentNewUser;

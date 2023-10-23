6fbee46089e1e62ff7752b64a6c95eb972c3d356
lubega-deriv <lubega@deriv.com>
2023-10-23 10:35:04 +0800

    fix: resolved comments

--- packages/wallets/src/components/Base/Divider/Divider.tsx
+++ packages/wallets/src/components/Base/Divider/Divider.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Divider.scss';
 
 type TProps = {
-    horizontal?: boolean;
-    margin?: string;
+    margin?: React.CSSProperties['margin'];
+    vertical?: boolean;
 };
 
-const Divider: React.FC<TProps> = ({ horizontal = true, margin }) => (
-    <div className={horizontal ? 'wallets-divider__horizontal' : 'wallets-divider__vertical'} style={{ margin }} />
+const Divider: React.FC<TProps> = ({ margin, vertical = false }) => (
+    <div className={vertical ? 'wallets-divider__vertical' : 'wallets-divider__horizontal'} style={{ margin }} />
 );
 
 export default Divider;

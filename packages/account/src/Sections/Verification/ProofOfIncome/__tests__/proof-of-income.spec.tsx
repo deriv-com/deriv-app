80021b7ba3788e7c07da2353b27a5cd340346133
“yauheni-kryzhyk-deriv” <yauheni@deriv.me>
2023-10-24 16:21:28 +0300

    chore: poin form test

--- packages/account/src/Sections/Verification/ProofOfIncome/__tests__/proof-of-income.spec.tsx
+++ packages/account/src/Sections/Verification/ProofOfIncome/__tests__/proof-of-income.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { WS } from '@deriv/shared';
 import { StoreProvider, mockStore } from '@deriv/stores';
 import ProofOfIncome from 'Sections/Verification/ProofOfIncome';
@@ -31,6 +32,7 @@ jest.mock('@deriv/shared', () => ({
                 get_account_status: { ...mocked_get_account_status },
             }),
         },
+        getSocket: jest.fn(() => Promise.resolve()),
     },
 }));
 jest.mock('react-router-dom', () => ({
@@ -70,7 +72,7 @@ describe('ProofOfIncome', () => {
         componentTestRender(mock_tore);
         expect(await screen.findByText('Redirect')).toBeInTheDocument();
     });
-    it('Should render ProofOfIncome form when it is required ', async () => {
+    it('Should render ProofOfIncome form when it is required', async () => {
         const mocked_get_account_status_for_poinc_required = {
             authentication: {
                 attempts: { latest: { service: '' }, history: [] },
@@ -108,7 +110,59 @@ describe('ProofOfIncome', () => {
         expect(screen.getByText(upload_warning)).toBeVisible();
         expect(screen.getByText(file_type_requirements)).toBeVisible();
         const button = screen.getByRole('button');
-        expect(button).toBeDisabled();
         expect(button).toHaveTextContent('Save and submit');
+        expect(button).toBeDisabled();
+    });
+    it('Should render ProofOfIncome and upload the document successfully', async () => {
+        const mocked_get_account_status_for_poinc_required = {
+            authentication: {
+                attempts: { latest: { service: '' }, history: [] },
+                document: {
+                    status: 'none',
+                },
+                identity: {
+                    status: 'none',
+                    services: { idv: {}, onfido: {}, manual: {} },
+                },
+                income: {
+                    status: 'none',
+                },
+                needs_verification: ['income'],
+            },
+            status: ['allow_document_upload', 'age_verification'],
+        };
+        (WS.authorized.getAccountStatus as jest.Mock).mockResolvedValue({
+            get_account_status: {
+                ...mocked_get_account_status_for_poinc_required,
+            },
+        });
+
+        componentTestRender(mock_tore);
+        const dropdown_input = (await screen.findByPlaceholderText('Select your document*')) as HTMLInputElement;
+        expect(dropdown_input).toBeVisible();
+        expect(dropdown_input.value).toBe('');
+        const file_input = screen.getByTestId('dt_file_upload_input') as HTMLInputElement;
+        expect(file_input.value).toBe('');
+        expect(screen.getByText(upload_warning)).toBeVisible();
+        expect(screen.getByText(file_type_requirements)).toBeVisible();
+        const button = screen.getByRole('button');
+        expect(button).toHaveTextContent('Save and submit');
+        expect(button).toBeDisabled();
+
+        userEvent.type(dropdown_input, 'Tax return');
+        await waitFor(() => {
+            expect(dropdown_input.value).toBe('Tax return');
+            expect(button).toBeDisabled();
+        });
+
+        const file = new File(['test document'], 'test_document.png', { type: 'image/png' });
+
+        fireEvent.change(file_input, { target: { files: [file] } });
+
+        await waitFor(() => {
+            expect(file_input?.files?.[0]).toBe(file);
+            expect(file_input.files).toHaveLength(1);
+            expect(button).toBeEnabled();
+        });
     });
 });

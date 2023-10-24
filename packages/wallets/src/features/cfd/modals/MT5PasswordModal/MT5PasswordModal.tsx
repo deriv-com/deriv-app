43ec46e91e85614da75ae8a6da66cd4db410ff07
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2023-10-24 10:28:19 +0800

    [Wallets] adrienne / moved types out to root (#10883)
    * chore: moved types out to root
    
    * chore: fix build

--- packages/wallets/src/features/cfd/modals/MT5PasswordModal/MT5PasswordModal.tsx
+++ packages/wallets/src/features/cfd/modals/MT5PasswordModal/MT5PasswordModal.tsx
@@ -11,7 +11,7 @@ import { ModalWrapper, WalletButton } from '../../../../components/Base';
 import { useModal } from '../../../../components/ModalProvider';
 import MT5PasswordIcon from '../../../../public/images/ic-mt5-password.svg';
 import { CreatePassword, EnterPassword, Success } from '../../screens';
-import { TMarketTypes, TPlatforms } from '../../types';
+import { TMarketTypes, TPlatforms } from '../../../../types';
 import { MarketTypeToTitleMapper, PlatformToTitleMapper } from '../../constants';
 
 type TProps = {

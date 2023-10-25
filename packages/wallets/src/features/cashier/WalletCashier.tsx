b8d7008562c6559e96b0f32a4a255425642e50a5
Aum Bhatt <aum@deriv.com>
2023-10-25 11:29:55 +0400

    feat: added animation to cashier-header onScrolling cashier-content

--- packages/wallets/src/features/cashier/WalletCashier.tsx
+++ packages/wallets/src/features/cashier/WalletCashier.tsx
@@ -5,16 +5,22 @@ import './WalletCashier.scss';
 
 const WalletCashier = () => {
     const { isLoading } = useActiveWalletAccount();
+    const [is_content_scrolled, setIsContentScrolled] = useState(false);
+
+    const onContentScroll = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const target = e.target as HTMLDivElement;
+        setIsContentScrolled(target.scrollTop > 0);
+    };
 
     if (isLoading) return <p>Loading...</p>;
 
     return (
-        <>
-            <WalletCashierHeader />
-            <div className='wallets-cashier-content'>
+        <div className='wallets-cashier'>
+            <WalletCashierHeader hide_wallet_details={is_content_scrolled} />
+            <div className='wallets-cashier-content' onScroll={onContentScroll}>
                 <WalletCashierContent />
             </div>
-        </>
+        </div>
     );
 };
 

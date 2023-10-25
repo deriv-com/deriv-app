5be1fbf6130ea574f25ba5ebcfb80418c4fbf1f0
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-10-25 16:26:01 +0800

    ci: remove console log from analytics

--- packages/analytics/src/rudderstack.ts
+++ packages/analytics/src/rudderstack.ts
@@ -210,8 +210,6 @@ export class RudderStack {
     }
 
     init() {
-        //eslint-disable-next-line
-        console.log('RudderStack.init()', process.env.NODE_ENV, process.env.REF_NAME);
         const is_production = process.env.NODE_ENV === 'production';
         const is_staging = process.env.NODE_ENV === 'staging';
 

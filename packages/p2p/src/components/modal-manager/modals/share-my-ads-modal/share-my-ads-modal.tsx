0079c6030ed9b8e239d432e1221c7e3afe01bcd0
ameerul hady <ameerul@deriv.com>
2023-10-25 14:07:54 +0800

    chore: try html2canvas

--- packages/p2p/src/components/modal-manager/modals/share-my-ads-modal/share-my-ads-modal.tsx
+++ packages/p2p/src/components/modal-manager/modals/share-my-ads-modal/share-my-ads-modal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { toPng } from 'html-to-image';
+// import { toPng } from 'html-to-image';
 import html2canvas from 'html2canvas';
 import {
     Button,
@@ -24,7 +24,7 @@ import ShareMyAdsCard from './share-my-ads-card';
 import ShareMyAdsSocials from './share-my-ads-socials';
 
 const ShareMyAdsModal = ({ advert }: TAdvert) => {
-    const [is_downloading, setIsDownloading] = React.useState(false);
+    // const [is_downloading, setIsDownloading] = React.useState(false);
     const [is_copied, copyToClipboard, setIsCopied] = useCopyToClipboard();
     const { account_currency, advertiser_details, id, local_currency, rate_display, rate_type, type } = advert;
     const { id: advertiser_id } = advertiser_details;
@@ -69,8 +69,8 @@ const ShareMyAdsModal = ({ advert }: TAdvert) => {
 
     const handleGenerateImage = async () => {
         if (divRef.current) {
-            const canvas = await html2canvas(divRef.current);
-            const screenshot = canvas.toDataURL('image/png');
+            const canvas = await html2canvas(divRef.current, { useCORS: true, allowTaint: true });
+            const screenshot = canvas.toDataURL('image/png', 1.0);
             const file_name = `${advert.type}_${advert.id}.png`;
             const link = document.createElement('a');
             link.download = file_name;
@@ -101,83 +101,76 @@ const ShareMyAdsModal = ({ advert }: TAdvert) => {
     return (
         <Modal is_open={is_modal_open} title={localize('Share this ad')} toggleModal={hideModal} width='71rem'>
             <Modal.Body className='share-my-ads-modal__body'>
-                {is_downloading ? (
-                    <Loading is_fullscreen={false} />
-                ) : (
-                    <React.Fragment>
+                <React.Fragment>
+                    <DesktopWrapper>
+                        <Text>
+                            <Localize i18n_default_text='Promote your ad by sharing the QR code and link.' />
+                        </Text>
+                    </DesktopWrapper>
+                    <div className='share-my-ads-modal__container'>
+                        <div className='share-my-ads-modal__container__card'>
+                            <ShareMyAdsCard advert={advert} advert_url={advert_url} divRef={divRef} />
+                            <Button
+                                className='share-my-ads-modal__container__card__download-button'
+                                // disabled={is_download_disabled}
+                                secondary
+                                onClick={handleGenerateImage}
+                            >
+                                <Localize i18n_default_text='Download this QR code' />
+                            </Button>
+                            <MobileWrapper>
+                                <Button.Group className='share-my-ads-modal__container__card__button-group'>
+                                    <Button icon={<Icon icon='IcShare' />} secondary onClick={handleShareLink}>
+                                        <Localize i18n_default_text='Share link' />
+                                    </Button>
+                                    <Button
+                                        icon={
+                                            is_copied ? (
+                                                <Icon icon='IcCheckmarkCircle' custom_color='var(--status-success)' />
+                                            ) : (
+                                                <Icon icon='IcShareLink' data_testid='dt_copy_link_icon' />
+                                            )
+                                        }
+                                        secondary
+                                        onClick={onCopy}
+                                    >
+                                        <Localize i18n_default_text='Copy link' />
+                                    </Button>
+                                </Button.Group>
+                            </MobileWrapper>
+                        </div>
                         <DesktopWrapper>
-                            <Text>
-                                <Localize i18n_default_text='Promote your ad by sharing the QR code and link.' />
-                            </Text>
-                        </DesktopWrapper>
-                        <div className='share-my-ads-modal__container'>
-                            <div className='share-my-ads-modal__container__card'>
-                                <ShareMyAdsCard advert={advert} advert_url={advert_url} divRef={divRef} />
-                                <Button
-                                    className='share-my-ads-modal__container__card__download-button'
-                                    // disabled={is_download_disabled}
-                                    secondary
-                                    onClick={handleGenerateImage}
-                                >
-                                    <Localize i18n_default_text='Download this QR code' />
-                                </Button>
-                                <MobileWrapper>
-                                    <Button.Group className='share-my-ads-modal__container__card__button-group'>
-                                        <Button icon={<Icon icon='IcShare' />} secondary onClick={handleShareLink}>
-                                            <Localize i18n_default_text='Share link' />
-                                        </Button>
-                                        <Button
-                                            icon={
-                                                is_copied ? (
-                                                    <Icon
-                                                        icon='IcCheckmarkCircle'
-                                                        custom_color='var(--status-success)'
-                                                    />
-                                                ) : (
-                                                    <Icon icon='IcShareLink' data_testid='dt_copy_link_icon' />
-                                                )
-                                            }
-                                            secondary
-                                            onClick={onCopy}
-                                        >
-                                            <Localize i18n_default_text='Copy link' />
-                                        </Button>
-                                    </Button.Group>
-                                </MobileWrapper>
-                            </div>
-                            <DesktopWrapper>
-                                <div className='share-my-ads-modal__share'>
-                                    <Text weight='bold'>
-                                        <Localize i18n_default_text='Share via' />
+                            <div className='share-my-ads-modal__share'>
+                                <Text weight='bold'>
+                                    <Localize i18n_default_text='Share via' />
+                                </Text>
+                                <ShareMyAdsSocials advert_url={advert_url} custom_message={custom_message} />
+                                <MyProfileSeparatorContainer.Line
+                                    className='share-my-ads-modal__line'
+                                    is_invisible={false}
+                                />
+                                <Text>
+                                    <Localize i18n_default_text='Or copy this link' />
+                                </Text>
+                                <div className='share-my-ads-modal__copy'>
+                                    <Text className='share-my-ads-modal__copy-link' color='less-prominent'>
+                                        <Localize i18n_default_text='{{- link}}' values={{ link: advert_url }} />
                                     </Text>
-                                    <ShareMyAdsSocials advert_url={advert_url} custom_message={custom_message} />
-                                    <MyProfileSeparatorContainer.Line
-                                        className='share-my-ads-modal__line'
-                                        is_invisible={false}
-                                    />
-                                    <Text>
-                                        <Localize i18n_default_text='Or copy this link' />
-                                    </Text>
-                                    <div className='share-my-ads-modal__copy'>
-                                        <Text className='share-my-ads-modal__copy-link' color='less-prominent'>
-                                            <Localize i18n_default_text='{{- link}}' values={{ link: advert_url }} />
-                                        </Text>
-                                        <div className='share-my-ads-modal__copy-clipboard'>
-                                            <Clipboard
-                                                className='share-my-ads-modal__copy-clipboard--icon'
-                                                popoverAlignment='top'
-                                                popoverClassName='share-my-ads-modal__copy-clipboard--popover'
-                                                size='18'
-                                                success_message={localize('Copied!')}
-                                                text_copy={advert_url}
-                                            />
-                                        </div>
+                                    <div className='share-my-ads-modal__copy-clipboard'>
+                                        <Clipboard
+                                            className='share-my-ads-modal__copy-clipboard--icon'
+                                            popoverAlignment='top'
+                                            popoverClassName='share-my-ads-modal__copy-clipboard--popover'
+                                            size='18'
+                                            success_message={localize('Copied!')}
+                                            text_copy={advert_url}
+                                        />
                                     </div>
                                 </div>
-                            </DesktopWrapper>
-                        </div>
-                    </React.Fragment>
-                )}
+                            </div>
+                        </DesktopWrapper>
+                    </div>
+                </React.Fragment>
             </Modal.Body>
         </Modal>
     );

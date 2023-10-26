32d9d03d3d84d047fe42df75bc95f009ae0b787a
Rostik Kayko <rostislav@deriv.com>
2023-10-26 12:53:38 +0300

    refactor: `Divider`

--- packages/wallets/src/components/Base/Divider/Divider.tsx
+++ packages/wallets/src/components/Base/Divider/Divider.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Divider.scss';
 
 type TProps = {
-    color?: React.CSSProperties['color'];
+    color?: React.CSSProperties['borderColor'];
     margin?: React.CSSProperties['margin'];
     variant?: 'horizontal' | 'vertical';
 };
 
-const Divider: React.FC<TProps> = ({ color, margin, variant = 'horizontal' }) => (
-    <div className={`wallets-divider__${variant}`} style={{ color, margin }} />
+const Divider: React.FC<TProps> = ({ color = '#f2f3f4', margin, variant = 'horizontal' }) => (
+    <div className={`wallets-divider__${variant}`} style={{ borderColor: color, margin }} />
 );
 
 export default Divider;

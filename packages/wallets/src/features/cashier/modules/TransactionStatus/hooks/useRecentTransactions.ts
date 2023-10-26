a58f79269c771456a6eaccc8fdc2817fbc7af269
Rostik Kayko <rostislav@deriv.com>
2023-10-26 12:47:33 +0300

    refactor: apply suggestions

--- packages/wallets/src/features/cashier/modules/TransactionStatus/hooks/useRecentTransactions.ts
+++ packages/wallets/src/features/cashier/modules/TransactionStatus/hooks/useRecentTransactions.ts
@@ -1,20 +1,7 @@
 import { useCallback, useEffect, useMemo } from 'react';
-import moment from 'moment';
 import { useCryptoTransactions } from '@deriv/api';
-import { getTruncatedString } from '@deriv/utils';
 import { THooks } from '../../../../../types';
-import {
-    depositConfirmationDisplayMapper,
-    depositStatusColorMapper,
-    depositStatusDescriptionMapper,
-    depositStatusNameMapper,
-    depositTransactionHashDisplayMapper,
-    withdrawalConfirmationDisplayMapper,
-    withdrawalStatusColorMapper,
-    withdrawalStatusDescriptionMapper,
-    withdrawalStatusNameMapper,
-    withdrawalTransactionHashDisplayMapper,
-} from '../utils/transactionDetailsMappers';
+import transactionDisplayMapper from '../utils/transactionDisplayMapper';
 
 // This hook is used to populate transactions from `useCryptoTransactions` with data that is needed for the UI.
 const useRecentTransactions = (transactionType?: THooks.CryptoTransactions['transaction_type']) => {
@@ -32,60 +19,7 @@ const useRecentTransactions = (transactionType?: THooks.CryptoTransactions['tran
     }, [resetData, subscribe, transactionType, unsubscribe]);
 
     const recentTransactions = useMemo(
-        () =>
-            transactions?.map(transaction => {
-                const submitDate = transaction.submit_date;
-                const confirmations = transaction.confirmations;
-                const addressHash = transaction.address_hash;
-                const addressHashObscure = addressHash
-                    ? `${getTruncatedString(addressHash, { length: 8, type: 'middle' })}`
-                    : 'Pending';
-                const transactionHash = transaction.transaction_hash;
-                const transactionHashObscure = transactionHash
-                    ? `${getTruncatedString(transactionHash, { length: 8, type: 'middle' })}`
-                    : 'Pending';
-
-                const statusColor =
-                    transaction.transaction_type === 'deposit'
-                        ? depositStatusColorMapper[transaction.status_code]
-                        : withdrawalStatusColorMapper[transaction.status_code];
-
-                const statusName =
-                    transaction.transaction_type === 'deposit'
-                        ? depositStatusNameMapper[transaction.status_code]
-                        : withdrawalStatusNameMapper[transaction.status_code];
-
-                const statusDescription =
-                    transaction.transaction_type === 'deposit'
-                        ? depositStatusDescriptionMapper[transaction.status_code]
-                        : withdrawalStatusDescriptionMapper[transaction.status_code];
-
-                const transactionHashDisplay =
-                    transaction.transaction_type === 'deposit'
-                        ? depositTransactionHashDisplayMapper(transactionHashObscure)[transaction.status_code]
-                        : withdrawalTransactionHashDisplayMapper(transactionHashObscure)[transaction.status_code];
-
-                const confirmationDisplay =
-                    transaction.transaction_type === 'deposit'
-                        ? depositConfirmationDisplayMapper(confirmations)[transaction.status_code]
-                        : withdrawalConfirmationDisplayMapper[transaction.status_code];
-
-                const submitDateDisplay = moment
-                    .unix(submitDate || 0)
-                    .utc()
-                    .format('MMM D, YYYY');
-
-                return {
-                    ...transaction,
-                    addressHashDisplay: addressHashObscure,
-                    confirmationDisplay,
-                    statusColor,
-                    statusDescription,
-                    statusName,
-                    submitDateDisplay,
-                    transactionHashDisplay,
-                };
-            }),
+        () => transactions?.map(transaction => transactionDisplayMapper(transaction)),
         [transactions]
     );
 
---
+++ packages/wallets/src/features/cashier/modules/TransactionStatus/utils/transactionDisplayMapper.ts
@@ -0,0 +1,138 @@
+import moment from 'moment';
+import { getTruncatedString } from '@deriv/utils';
+import { THooks } from '../../../../../types';
+
+// eslint-disable-next-line camelcase
+const depositDisplayMapper = (transaction: THooks.CryptoTransactions & { transaction_type: 'deposit' }) => {
+    const transactionHashObscure = transaction.transaction_hash
+        ? `${getTruncatedString(transaction.transaction_hash, { length: 8, type: 'middle' })}`
+        : 'Pending';
+
+    return {
+        CONFIRMED: {
+            confirmationDisplay: 'Confirmed',
+            description: 'Your deposit is successful.',
+            statusColor: 'successful',
+            statusName: 'Successful',
+            transactionHashDisplay: transactionHashObscure,
+        },
+        ERROR: {
+            confirmationDisplay: 'NA',
+            description:
+                'Your deposit is unsuccessful due to an error on the blockchain. Please contact your crypto wallet service provider for more info.',
+            statusColor: 'unsuccessful',
+            statusName: 'Unsuccessful',
+            transactionHashDisplay: 'NA',
+        },
+        PENDING: {
+            confirmationDisplay: transaction.confirmations ? `${transaction.confirmations}` : 'Pending',
+            description: 'We’ve received your request and are waiting for more blockchain confirmations.',
+            statusColor: 'warning',
+            statusName: 'In process',
+            transactionHashDisplay: transactionHashObscure,
+        },
+    }[transaction.status_code];
+};
+
+// eslint-disable-next-line camelcase
+const withdrawalDisplayMapper = (transaction: THooks.CryptoTransactions & { transaction_type: 'withdrawal' }) => {
+    const transactionHashObscure = transaction.transaction_hash
+        ? `${getTruncatedString(transaction.transaction_hash, { length: 8, type: 'middle' })}`
+        : 'Pending';
+
+    return {
+        CANCELLED: {
+            confirmationDisplay: '-',
+            description: 'You’ve cancelled your withdrawal request.',
+            statusColor: 'unsuccessful',
+            statusName: 'Cancelled',
+            transactionHashDisplay: 'NA',
+        },
+        ERROR: {
+            confirmationDisplay: '-',
+            description:
+                'Your withdrawal is unsuccessful due to an error on the blockchain. Please contact us via live chat for more info.',
+            statusColor: 'unsuccessful',
+            statusName: 'Unsuccessful',
+            transactionHashDisplay: 'NA',
+        },
+        LOCKED: {
+            confirmationDisplay: '-',
+            description:
+                "We're reviewing your withdrawal request. You may still cancel this transaction if you wish. Once we start processing, you won't be able to cancel.",
+            statusColor: 'warning',
+            statusName: 'In review',
+            transactionHashDisplay: transactionHashObscure,
+        },
+        PERFORMING_BLOCKCHAIN_TXN: {
+            confirmationDisplay: '-',
+            description: 'We’re sending your request to the blockchain.',
+            statusColor: 'warning',
+            statusName: 'In process',
+            transactionHashDisplay: transactionHashObscure,
+        },
+        PROCESSING: {
+            confirmationDisplay: '-',
+            description: 'We’re awaiting confirmation from the blockchain.',
+            statusColor: 'warning',
+            statusName: 'In process',
+            transactionHashDisplay: transactionHashObscure,
+        },
+        REJECTED: {
+            confirmationDisplay: '-',
+            description: "Your withdrawal is unsuccessful. We've sent you an email with more information.",
+            statusColor: 'unsuccessful',
+            statusName: 'Unsuccessful',
+            transactionHashDisplay: 'NA',
+        },
+        REVERTED: {
+            confirmationDisplay: '-',
+            description: "Your withdrawal is unsuccessful. We've sent you an email with more information.",
+            statusColor: 'unsuccessful',
+            statusName: 'Unsuccessful',
+            transactionHashDisplay: 'NA',
+        },
+        REVERTING: {
+            confirmationDisplay: '-',
+            description: "We're processing your withdrawal.",
+            statusColor: 'warning',
+            statusName: 'In process',
+            transactionHashDisplay: 'NA',
+        },
+        SENT: {
+            confirmationDisplay: '-',
+            description: 'Your withdrawal is successful.',
+            statusColor: 'successful',
+            statusName: 'Successful',
+            transactionHashDisplay: transactionHashObscure,
+        },
+        VERIFIED: {
+            confirmationDisplay: '-',
+            description: 'We’re processing your withdrawal.',
+            statusColor: 'warning',
+            statusName: 'In process',
+            transactionHashDisplay: transactionHashObscure,
+        },
+    }[transaction.status_code];
+};
+
+const transactionDisplayMapper = (transaction: THooks.CryptoTransactions) => {
+    const addressHash = transaction.address_hash;
+    const addressHashObscure = addressHash
+        ? `${getTruncatedString(addressHash, { length: 8, type: 'middle' })}`
+        : 'Pending';
+    const submitDateDisplay = moment
+        .unix(transaction.submit_date || 0)
+        .utc()
+        .format('MMM D, YYYY');
+    return {
+        ...transaction,
+        ...(transaction.transaction_type === 'deposit'
+            ? depositDisplayMapper(transaction)
+            : withdrawalDisplayMapper(transaction)),
+        addressHashDisplay: addressHashObscure,
+        submitDateDisplay,
+    };
+};
+
+export default transactionDisplayMapper;

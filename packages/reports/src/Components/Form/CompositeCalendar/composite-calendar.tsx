dbc4f44554520ab405e4ae262e4f7a09d091c21b
kate-deriv <kate@regentmarkets.com>
2023-10-26 15:19:09 +0300

    fix: make function optional and pass it in props

--- packages/reports/src/Components/Form/CompositeCalendar/composite-calendar.tsx
+++ packages/reports/src/Components/Form/CompositeCalendar/composite-calendar.tsx
@@ -176,7 +176,12 @@ const CompositeCalendar = observer((props: TCompositeCalendar) => {
                 )}
             </DesktopWrapper>
             <MobileWrapper>
-                <CompositeCalendarMobile duration_list={list} {...props} />
+                <CompositeCalendarMobile
+                    duration_list={list}
+                    {...props}
+                    setCurrentFocus={setCurrentFocus}
+                    current_focus={current_focus}
+                />
             </MobileWrapper>
         </React.Fragment>
     );

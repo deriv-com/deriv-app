e0d98345645128223ff636d137333bd4341aaa67
kate-deriv <kate@regentmarkets.com>
2023-10-27 19:06:20 +0300

    fix: language switching issue with the chart

--- packages/trader/src/App/Components/Routes/binary-routes.tsx
+++ packages/trader/src/App/Components/Routes/binary-routes.tsx
@@ -7,8 +7,11 @@ import RouteWithSubRoutes from './route-with-sub-routes';
 const BinaryRoutes = (props: TBinaryRoutesProps) => (
     <React.Suspense fallback={<div />}>
         <Switch>
-            {getRoutesConfig().map((route: TRouteConfig) => (
-                <RouteWithSubRoutes key={route.getTitle?.()} {...route} {...props} />
+            {getRoutesConfig().map((route: TRouteConfig, index) => (
+                /* Index is the only thing that can be used for the key here because the only other property
+                that can be used as a key and available in every route is a localized title returned from getTitle() which,
+                when used, causes severe bugs upon switching between languages! */
+                <RouteWithSubRoutes key={index} {...route} {...props} />
             ))}
         </Switch>
     </React.Suspense>

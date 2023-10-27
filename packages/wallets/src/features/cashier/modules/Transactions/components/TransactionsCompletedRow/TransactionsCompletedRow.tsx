22326c8efa468ec98c5fc5b8d2fb5e19ca8dcf61
Rostik Kayko <rostislav@deriv.com>
2023-10-27 17:44:04 +0300

    style: (and refactor) `TransactionsCompletedRow`

--- packages/wallets/src/features/cashier/modules/Transactions/components/TransactionsCompletedRow/TransactionsCompletedRow.tsx
+++ packages/wallets/src/features/cashier/modules/Transactions/components/TransactionsCompletedRow/TransactionsCompletedRow.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import { useActiveWalletAccount, useTransactions } from '@deriv/api';
+import { WalletText } from '../../../../../../components/Base';
 import { WalletCurrencyCard } from '../../../../../../components/WalletCurrencyCard';
 import './TransactionsCompletedRow.scss';
 
@@ -11,33 +12,29 @@ const TransactionsCompletedRow: React.FC<TProps> = ({ transaction }) => {
     const { data } = useActiveWalletAccount();
     const displayCode = useMemo(() => data?.currency_config?.display_code || 'USD', [data]);
 
+    if (!transaction.action_type || !transaction.amount) return null;
+
     return (
         <div className='wallets-transactions-completed-row'>
             <div className='wallets-transactions-completed-row__account-details'>
                 <WalletCurrencyCard currency={data?.currency || 'USD'} isDemo={data?.is_virtual} size='md' />
-                <div>
-                    <p className='wallets-transactions-completed-row__account-details__action-type'>
-                        {transaction.action_type}
-                    </p>
-                    <p className='wallets-transactions-completed-row__account-details__wallet-name'>
+                <div className='wallets-transactions-completed-row__column'>
+                    <WalletText color='primary' size='xs'>
+                        {transaction.action_type.charAt(0).toUpperCase() + transaction.action_type.slice(1)}
+                    </WalletText>
+                    <WalletText color='general' size='xs' weight='bold'>
                         {displayCode} Wallet
-                    </p>
+                    </WalletText>
                 </div>
             </div>
             <div className='wallets-transactions-completed-row__transaction-details'>
-                <p
-                    className={`wallets-transactions-completed-row__transaction-details__amount ${
-                        transaction?.amount && transaction.amount < 0
-                            ? 'wallets-transactions-completed-row__transaction-details__amount--negative'
-                            : ''
-                    }`}
-                >
-                    {transaction?.amount && transaction?.amount > 0 ? '+' : ''}
+                <WalletText color={transaction.amount > 0 ? 'success' : 'red'} size='xs' weight='bold'>
+                    {transaction.amount && transaction.amount > 0 ? '+' : ''}
                     {transaction.display_amount}
-                </p>
-                <p className='wallets-transactions-completed-row__transaction-details__balance'>
+                </WalletText>
+                <WalletText color='primary' size='2xs'>
                     Balance: {transaction.display_balance_after}
-                </p>
+                </WalletText>
             </div>
         </div>
     );

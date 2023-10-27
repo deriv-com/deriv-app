aef0f178de4758c247614d41b6d0b15feefa8cec
Aum Bhatt <aum@deriv.com>
2023-10-27 15:02:47 +0400

    feat: change of flow and content for demo reset balance

--- packages/wallets/src/features/cashier/modules/ResetBalance/ResetBalance.tsx
+++ packages/wallets/src/features/cashier/modules/ResetBalance/ResetBalance.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { useActiveWalletAccount, useMutation } from '@deriv/api';
+import { useMutation } from '@deriv/api';
 import WalletsActionScreen from '../../../../components/WalletsActionScreen/WalletsActionScreen';
 import IcResetDemoBalance from '../../../../public/images/ic-demo-reset-balance.svg';
 import IcResetDemoBalanceDone from '../../../../public/images/ic-demo-reset-balance-done.svg';
@@ -9,26 +9,22 @@ import './ResetBalance.scss';
 const ResetBalance = () => {
     const history = useHistory();
     const { isSuccess: isResetBalanceSuccess, mutate } = useMutation('topup_virtual');
-    const { data: activeWallet } = useActiveWalletAccount();
 
     const resetBalance = () => {
         mutate();
     };
-
-    const canReset = activeWallet?.balance !== 10000;
-
     return (
         <div className='wallets-reset-balance'>
             <WalletsActionScreen
-                actionText={canReset ? 'Reset balance' : 'Transfer funds'}
+                actionText={isResetBalanceSuccess ? 'Transfer funds' : 'Reset balance'}
                 description={
                     isResetBalanceSuccess
                         ? 'Your balance has been reset to 10,000.00 USD.'
-                        : 'Reset your virtual balance if it falls below 10,000.00 USD or exceeds 10,000.00 USD.'
+                        : 'Reset your virtual balance to 10,000.00 USD.'
                 }
                 icon={isResetBalanceSuccess ? <IcResetDemoBalanceDone /> : <IcResetDemoBalance />}
-                onAction={canReset ? resetBalance : () => history.push(`/wallets/cashier/transfer`)}
-                title={'Reset balance to 10,000.00 USD'}
+                onAction={isResetBalanceSuccess ? () => history.push(`/wallets/cashier/transfer`) : resetBalance}
+                title={isResetBalanceSuccess ? 'Reset balance to 10,000.00 USD' : 'Success'}
             />
         </div>
     );

16714fd048e5aa863dfedb6a118d312b4ebd67a0
Iman Khaleghparast <imankhaleghparast@Shayan-khaleghparasts-Mac-WTQ21CJ93L.local>
2023-10-30 18:11:30 +0800

    chore: removed unnecessary parts from the hook

--- packages/api/src/hooks/useExchangeRates2.ts
+++ packages/api/src/hooks/useExchangeRates2.ts
@@ -1,18 +1,11 @@
 /* eslint-disable no-console */
-import React, { useEffect, useState } from 'react';
-import type { ExchangeRatesResponse } from '@deriv/api-types';
+// import React, { useEffect, useState } from 'react';
+// import type { ExchangeRatesResponse } from '@deriv/api-types';
 
 import useSubscription from '../useSubscription';
 
 const useExchangeRates2 = () => {
     const { subscribe, unsubscribe, data, ...rest } = useSubscription('exchange_rates');
-
-    useEffect(() => {
-        return () => {
-            unsubscribe();
-            console.log('unsubscribed from exchange_rates');
-        };
-    }, [subscribe, unsubscribe]);
 
     return {
         subscribe,

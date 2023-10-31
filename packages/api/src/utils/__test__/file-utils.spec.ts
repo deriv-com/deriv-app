2ae33af7b3529c2e7be22b7147cdf5de7151db4a
yashim-deriv <yashim@regentmarkets.com>
2023-10-31 14:23:36 +0800

    fix: add more tests

--- packages/api/src/utils/__test__/file-utils.spec.ts
+++ packages/api/src/utils/__test__/file-utils.spec.ts
@@ -1,4 +1,73 @@
-import { isSupportedImageFormat } from '../file-utils';
+import { convertToBase64, isSupportedImageFormat } from '../file-utils';
+
+describe('convertToBase64()', () => {
+    const mockImageFile = new File(['image-data-contents'], 'image1.jpg', { type: 'image/jpeg' });
+
+    it('should convert a File to base64', async () => {
+        const base64Image = await convertToBase64(mockImageFile);
+
+        expect(base64Image).toEqual(
+            expect.objectContaining({
+                src: expect.any(String),
+                filename: 'image1.jpg',
+            })
+        );
+
+        expect(base64Image.src).toMatch(/^data:image\/jpeg;base64,/);
+    });
+
+    it('should handle empty files', async () => {
+        const mockFile = new File([], 'empty.txt', { type: 'text/plain' });
+        const base64Image = await convertToBase64(mockFile);
+
+        expect(base64Image).toEqual(
+            expect.objectContaining({
+                src: 'data:text/plain;base64,',
+                filename: 'empty.txt',
+            })
+        );
+    });
+
+    it('should handle files with special characters in their names', async () => {
+        const mockFile = new File(['file contents'], 'special&chars.jpg', { type: 'image/jpeg' });
+        const base64Image = await convertToBase64(mockFile);
+
+        expect(base64Image).toEqual(
+            expect.objectContaining({
+                src: expect.any(String),
+                filename: 'special&chars.jpg',
+            })
+        );
+
+        expect(base64Image.src).toMatch(/^data:image\/jpeg;base64,/);
+    });
+
+    it('should handle non-image files', async () => {
+        const mockFile = new File(['file contents'], 'document.pdf', { type: 'application/pdf' });
+        const base64Image = await convertToBase64(mockFile);
+
+        expect(base64Image).toEqual(
+            expect.objectContaining({
+                src: expect.any(String),
+                filename: 'document.pdf',
+            })
+        );
+
+        expect(base64Image.src).toMatch(/^data:application\/pdf;base64,/);
+    });
+
+    it('should handle files with no type information', async () => {
+        const mockFile = new File(['file contents'], 'unknown', { type: '' });
+        const base64Image = await convertToBase64(mockFile);
+
+        expect(base64Image).toEqual(
+            expect.objectContaining({
+                src: expect.any(String),
+                filename: 'unknown',
+            })
+        );
+    });
+});
 
 describe('isSupportedImageFormat()', () => {
     it('should return true for supported image formats', () => {

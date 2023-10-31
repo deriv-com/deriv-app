ffa81ccdeb75d369bfb55bdb5e25854c26e32429
Rostik Kayko <rostislav@deriv.com>
2023-10-31 14:57:56 +0300

    refactor: css bem

--- packages/wallets/src/features/cashier/modules/Transactions/components/TransactionsTable/TransactionsTable.tsx
+++ packages/wallets/src/features/cashier/modules/Transactions/components/TransactionsTable/TransactionsTable.tsx
@@ -27,9 +27,9 @@ const TransactionsTable = <T,>({ columns, data, fetchMore, groupBy, rowGroupRend
             onScroll={e => (fetchMore ? fetchMore(e.target as HTMLDivElement) : null)}
             ref={tableContainerRef}
         >
-            <div className='wallets-transactions-table-content'>
+            <div className='wallets-transactions-table__content'>
                 {table.getRowModel().rows.map(rowGroup => (
-                    <div className='wallets-transactions-table-row' key={rowGroup.id}>
+                    <div className='wallets-transactions-table__row' key={rowGroup.id}>
                         {rowGroupRender(rowGroup.original)}
                         {rowGroup.subRows.map(row => (
                             <div key={row.id}>{rowRender(row.original)}</div>

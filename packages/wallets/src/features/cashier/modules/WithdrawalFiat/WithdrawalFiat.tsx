bb9f7b60850aa95d52bde49ff81015c4394a777e
lubega-deriv <lubega@deriv.com>
2023-11-01 13:24:16 +0800

    fix: resolved failed test

--- packages/wallets/src/features/cashier/modules/WithdrawalFiat/WithdrawalFiat.tsx
+++ packages/wallets/src/features/cashier/modules/WithdrawalFiat/WithdrawalFiat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useCashierFiatAddress } from '@deriv/api';
 import { Loader } from '../../../../components';
 import { WithdrawalVerificationModule } from '../WithdrawalVerification';

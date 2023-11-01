05d09e4b0b9020f998cfcec04e90a803a5bb9756
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-11-01 11:00:30 +0800

    chore: add payload to fait address hook

--- packages/api/src/hooks/useCashierFiatAddress.ts
+++ packages/api/src/hooks/useCashierFiatAddress.ts
@@ -5,13 +5,19 @@ type TCashierParam = NonNullable<
     Parameters<ReturnType<typeof useMutation<'cashier'>>['mutate']>
 >[0]['payload']['cashier'];
 
+type TParams = Omit<
+    NonNullable<Parameters<ReturnType<typeof useMutation<'cashier'>>['mutate']>>[0]['payload'],
+    'cashier' | 'provider'
+>;
+
 /** A custom hook to get the deposit and withdrawal fiat address. */
 const useCashierFiatAddress = () => {
     const { data, mutate: _mutate, ...rest } = useMutation('cashier');
     const iframe_url = typeof data?.cashier === 'string' ? data?.cashier : undefined;
 
     const mutate = useCallback(
-        (cashier: TCashierParam) => _mutate({ payload: { cashier, provider: 'doughflow' } }),
+        (cashier: TCashierParam, payload?: TParams) =>
+            _mutate({ payload: { cashier, provider: 'doughflow', ...payload } }),
         [_mutate]
     );
 

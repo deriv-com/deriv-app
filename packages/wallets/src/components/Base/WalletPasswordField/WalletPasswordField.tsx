47b05a9633f113ca937d0041bcc34a7d2373bad4
aizad-deriv <aizad@deriv.com>
2023-11-02 16:04:43 +0800

    Revert "fix: added message obj and maxlenght helper message"
    This reverts commit 1bf4af95f44a1e85db2e3fe9063ae9316bc77191.

--- packages/wallets/src/components/Base/WalletPasswordField/WalletPasswordField.tsx
+++ packages/wallets/src/components/Base/WalletPasswordField/WalletPasswordField.tsx
@@ -1,27 +1,15 @@
-import React, { FC, useState } from 'react';
+import React, { useState } from 'react';
 import { zxcvbn } from '@zxcvbn-ts/core';
 import { WalletTextField } from '../WalletTextField';
 import './WalletPasswordField.scss';
 
-type StrengthMessage = Record<1 | 2 | 3 | 4, string>;
-
-interface WalletPasswordFieldProps {
-    label?: string;
-    maxLength?: number;
-    messageObj?: StrengthMessage;
-}
-
-const WalletPasswordField: FC<WalletPasswordFieldProps> = ({ label = 'Password', maxLength, messageObj }) => {
+const WalletPasswordField = ({ label = 'Password' }) => {
     const [password, setPassword] = useState<string>('');
-    const hasMessage = !!messageObj;
-    const hasMaxLength = !!maxLength;
 
     const getPasswordStrength = () => {
         const result = zxcvbn(password);
         return result.score;
     };
-
-    const strength = getPasswordStrength();
 
     const getProgressColor = () => {
         const strengthColors = [
@@ -32,24 +20,13 @@ const WalletPasswordField: FC<WalletPasswordFieldProps> = ({ label = 'Password',
             'wallets-password__meter--complete',
         ];
 
+        const strength = getPasswordStrength();
         return strengthColors[strength];
-    };
-
-    const getProgressText = (messageObj: StrengthMessage) => {
-        if (!messageObj) return '';
-        return messageObj[strength as keyof StrengthMessage];
     };
 
     return (
         <div className='wallets-password'>
-            <WalletTextField
-                helperMessage={getProgressText(messageObj as StrengthMessage)}
-                label={label}
-                maxLength={maxLength}
-                onChange={e => setPassword(e.target.value)}
-                showMessage={hasMessage || hasMaxLength}
-                type='password'
-            />
+            <WalletTextField label={label} onChange={e => setPassword(e.target.value)} type='password' />
             <div className='wallets-password__meter'>
                 <div className={getProgressColor()} />
             </div>

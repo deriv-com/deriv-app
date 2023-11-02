e491042f0ec66b079ab86bff5e56bcbce9e8ce97
kate-deriv <kate@regentmarkets.com>
2023-11-02 14:29:30 +0300

    refactor: add tests for last digit

--- packages/trader/src/Modules/Trading/Components/Form/TradeParams/__tests__/last-digit.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/TradeParams/__tests__/last-digit.spec.tsx
@@ -1,3 +1,56 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import LastDigit from '../allow-equals';
+import { mockStore } from '@deriv/stores';
+import { TCoreStores } from '@deriv/stores/types';
+import { isDesktop } from '@deriv/shared';
+import TraderProviders from '../../../../../../trader-providers';
+import LastDigit from '../last-digit';
+
+const default_mock_store = {
+    modules: {
+        trade: {
+            onChange: jest.fn(),
+            last_digit: 5,
+        },
+    },
+};
+const numberSelector = 'Number selector';
+const digitPrediction = 'Last Digit Prediction';
+const lastDigit = `Last Digit: ${default_mock_store.modules.trade.last_digit}`;
+
+jest.mock('App/Components/Form/number-selector', () => jest.fn(() => <div>{numberSelector}</div>));
+jest.mock('@deriv/shared', () => ({
+    ...jest.requireActual('@deriv/shared'),
+    isDesktop: jest.fn().mockReturnValue(true),
+}));
+
+describe('<LastDigit />', () => {
+    const mockLastDigit = (mocked_store: TCoreStores, mocked_props?: React.ComponentProps<typeof LastDigit>) => {
+        return (
+            <TraderProviders store={mocked_store}>
+                <LastDigit {...mocked_props} />
+            </TraderProviders>
+        );
+    };
+
+    it('should render only phrase about last digit if is_minimizes is true', () => {
+        render(mockLastDigit(mockStore(default_mock_store), { is_minimized: true }));
+
+        expect(screen.getByText(lastDigit)).toBeInTheDocument();
+        expect(screen.queryByText(numberSelector)).not.toBeInTheDocument();
+        expect(screen.queryByText(digitPrediction)).not.toBeInTheDocument();
+    });
+    it('should render fieldset and number selector if is_minimizes is false', () => {
+        render(mockLastDigit(mockStore(default_mock_store)));
+
+        expect(screen.getByText(numberSelector)).toBeInTheDocument();
+        expect(screen.getByText(digitPrediction)).toBeInTheDocument();
+        expect(screen.queryByText(lastDigit)).not.toBeInTheDocument();
+    });
+    it('should not render fieldset header if it is not desktop', () => {
+        (isDesktop as jest.Mock).mockReturnValueOnce(false);
+        render(mockLastDigit(mockStore(default_mock_store)));
+
+        expect(screen.queryByText(digitPrediction)).not.toBeInTheDocument();
+    });
+});

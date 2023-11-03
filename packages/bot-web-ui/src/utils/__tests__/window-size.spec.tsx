030d7cc3bfd0f479d4aa89480a9b845ef47967f8
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-11-03 11:51:05 +0300

    chore: remove unnecessary

--- packages/bot-web-ui/src/utils/__tests__/window-size.spec.tsx
+++ packages/bot-web-ui/src/utils/__tests__/window-size.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByTestId, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import {
     getMainContentHeight,

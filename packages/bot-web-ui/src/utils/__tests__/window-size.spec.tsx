e229f50e338860c52b5301a5c9fda3b330b997c0
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-11-03 11:50:15 +0300

    test: window-size

---
+++ packages/bot-web-ui/src/utils/__tests__/window-size.spec.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { getByTestId, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import {
+    getMainContentHeight,
+    getMainContentWidth,
+    setMainContentWidth,
+    setInnerHeightToVariable,
+} from '../window-size';
+
+describe('DOM utilities', () => {
+    beforeEach(() => {
+        render(<div className='bot' data-testid='mock-bot' />);
+    });
+
+    it('should get main content height', () => {
+        const bot_element = screen.getByTestId('mock-bot');
+
+        if (!bot_element) {
+            throw new Error("Element with class '.bot' was not found");
+        }
+        bot_element.style.setProperty('--bot-content-height', '100px');
+
+        expect(getMainContentHeight()).toBe('100px');
+    });
+
+    it('should get main content width', () => {
+        const bot_element = screen.getByTestId('mock-bot');
+
+        if (!bot_element) {
+            throw new Error("Element with class '.bot' was not found");
+        }
+        bot_element.style.setProperty('--bot-content-width', '200px');
+
+        expect(getMainContentWidth()).toBe('200px');
+    });
+
+    it('should set main content width based on is_run_panel_open', () => {
+        const bot_element = screen.getByTestId('mock-bot');
+
+        setMainContentWidth(true);
+        expect(bot_element).toHaveStyle('--bot-content-width: calc(100vw - 366px)');
+
+        setMainContentWidth(false);
+        expect(bot_element).toHaveStyle('--bot-content-width: calc(100vw - 16px)');
+    });
+
+    it('should set inner height to --vh variable', () => {
+        Object.defineProperty(window, 'innerHeight', { writable: true, configurable: true, value: 768 });
+        setInnerHeightToVariable();
+
+        expect(document.body).toHaveStyle('--vh: 768px');
+    });
+});

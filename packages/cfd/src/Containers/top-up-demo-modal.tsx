274f4f50cc67b0c5ad7dabb67a48b9573658643e
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-11-03 14:25:48 +0800

    chore: added extension which is causing checks fail

--- packages/cfd/src/Containers/top-up-demo-modal.tsx
+++ packages/cfd/src/Containers/top-up-demo-modal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SuccessDialog from '../Components/success-dialog';
+import SuccessDialog from '../Components/success-dialog.jsx';
 import { Icon, Modal, Button, Money, Text } from '@deriv/components';
 import { getCFDPlatformLabel } from '@deriv/shared';
 import { localize, Localize } from '@deriv/translations';

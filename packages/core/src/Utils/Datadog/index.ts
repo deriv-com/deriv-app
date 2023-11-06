f2a1fc118aaf5d8f62c9f2828da183ac055e12c6
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-11-06 11:02:24 +0400

    refactor: to redact token from datadog rum

--- packages/core/src/Utils/Datadog/index.ts
+++ packages/core/src/Utils/Datadog/index.ts
@@ -5,6 +5,15 @@ const DATADOG_CLIENT_TOKEN = process.env.DATADOG_CLIENT_TOKEN ?? '';
 const isProduction = process.env.NODE_ENV === 'production';
 const isStaging = process.env.NODE_ENV === 'staging';
 
+function getAcct1Value(url: string) {
+    const start = url.indexOf('acct1=') + 6; // 6 is the length of 'acct1='
+    const end = url.indexOf('&', start); // Find the end of the parameter value
+    if (end === -1) {
+        return url.substring(start); // If there's no '&' after acct1, get the substring from start to the end
+    }
+    return url.substring(start, end); // Get the substring between 'acct1=' and the '&'
+}
+
 let dataDogSessionSampleRate = 0;
 let dataDogSessionReplaySampleRate = 0;
 let dataDogVersion = '';
@@ -43,13 +52,22 @@ if (isProduction || isStaging) {
         trackFrustrations: true,
         enableExperimentalFeatures: ['clickmap'],
         beforeSend: event => {
-        if (event.type === 'resource') {
-            event.resource.url = event.resource.url.replace(
-                /^https:\/\/api.telegram.org.*$/,
-                'telegram token=REDACTED'
-            );
-        }
-    },
+            if (event.type === 'resource') {
+                event.resource.url = event.resource.url.replace(
+                    /^https:\/\/api.telegram.org.*$/,
+                    'telegram token=REDACTED'
+                );
+
+                if (event.resource.url.match(/^ https:\/\/eu.deriv.com\/\/ctrader-login.*$/)) {
+                    const url = event.resource.url;
+                    const accnt = getAcct1Value(url);
+                    event.resource.url = event.resource.url.replace(
+                        /^ https:\/\/eu.deriv.com\/\/ctrader-login.*$/,
+                        `https://eu.deriv.com/ctrader-login?acct1=${accnt}&token1=redacted`
+                    );
+                }
+            }
+        },
     });
 
     datadogRum.startSessionReplayRecording();

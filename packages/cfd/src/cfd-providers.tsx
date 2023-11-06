8407724245a45c21a3934407e9791f7eab734417
Shahzaib <shahzaib@deriv.com>
2023-11-06 13:24:38 +0800

    chore: revert deriv/api from cfd package.json

--- packages/cfd/src/cfd-providers.tsx
+++ packages/cfd/src/cfd-providers.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { StoreProvider } from '@deriv/stores';
 import { CFDStoreProvider } from './Stores/Modules/CFD/Helpers/useCfdStores';
 import type { TCoreStores } from '@deriv/stores/types';
-import { APIProvider } from '@deriv/api';
 
 const CFDProviders = ({ children, store }: React.PropsWithChildren<{ store: TCoreStores }>) => {
     return (
-        <APIProvider>
-            <StoreProvider store={store}>
-                <CFDStoreProvider>{children}</CFDStoreProvider>
-            </StoreProvider>
-        </APIProvider>
+        <StoreProvider store={store}>
+            <CFDStoreProvider>{children}</CFDStoreProvider>
+        </StoreProvider>
     );
 };
 

bceb89cdf6f655642d9466f5d9c410a440c5ac57
kate-deriv <kate@regentmarkets.com>
2023-11-07 11:13:55 +0300

    refactor: create a css class instead of tag selector

--- packages/trader/src/Modules/Trading/Components/Elements/mobile-widget.tsx
+++ packages/trader/src/Modules/Trading/Components/Elements/mobile-widget.tsx
@@ -80,13 +80,13 @@ const MobileWidget = observer(() => {
                 <MultiplierAmountWidget />
             ) : (
                 <div id='duration_amount_selector' className='mobile-widget duration_amount_selector'>
-                    <button className='mobile-widget__duration' onClick={() => toggleWidget(0)}>
+                    <button className='mobile-widget__duration mobile-widget__button' onClick={() => toggleWidget(0)}>
                         {getHumanReadableDuration()}
                     </button>
-                    <button className='mobile-widget__amount' onClick={() => toggleWidget(1)}>
+                    <button className='mobile-widget__amount mobile-widget__button' onClick={() => toggleWidget(1)}>
                         <Money amount={amount} currency={currency} show_currency />
                     </button>
-                    <button className='mobile-widget__type' onClick={() => toggleWidget(1)}>
+                    <button className='mobile-widget__type mobile-widget__button' onClick={() => toggleWidget(1)}>
                         {stakeOrPayout()}
                     </button>
                 </div>

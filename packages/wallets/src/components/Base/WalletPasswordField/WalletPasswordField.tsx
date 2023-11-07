38bac9a98173184f49cf683beac5cc7d5c2ae522
aizad-deriv <aizad@deriv.com>
2023-11-07 15:54:48 +0800

    fix: added icons and view state

--- packages/wallets/src/components/Base/WalletPasswordField/WalletPasswordField.tsx
+++ packages/wallets/src/components/Base/WalletPasswordField/WalletPasswordField.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
 import { WalletTextField } from '../WalletTextField';
 import { WalletTextFieldProps } from '../WalletTextField/WalletTextField';
+import PasswordShow from '../../../public/images/ic-password-show.svg';
+import PasswordHide from '../../../public/images/ic-password-hide.svg';
 import './WalletPasswordField.scss';
+import { IconButton } from '../IconButton';
 
 type WalletPasswordFieldProps = WalletTextFieldProps;
 
 const WalletPasswordField: FC<WalletPasswordFieldProps> = () => {
+    const [viewPassword, setViewPassword] = useState(false);
+    const toggleView = () => setViewPassword(!viewPassword);
+
     const strengthColors = {
         0: 'wallets-password__meter--initial',
         1: 'wallets-password__meter--weak',
@@ -16,7 +22,18 @@ const WalletPasswordField: FC<WalletPasswordFieldProps> = () => {
 
     return (
         <div className='wallets-password'>
-            <WalletTextField type='password' />
+            <WalletTextField
+                renderRightIcon={() => (
+                    <IconButton
+                        color='transparent'
+                        icon={viewPassword ? <PasswordShow /> : <PasswordHide />}
+                        isRound
+                        onClick={toggleView}
+                        size='sm'
+                    />
+                )}
+                type={viewPassword ? 'text' : 'password'}
+            />
             <div className='wallets-password__meter'>
                 <div className={strengthColors[0]} />
             </div>

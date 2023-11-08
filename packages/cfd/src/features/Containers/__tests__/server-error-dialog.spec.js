18961dc564ec7ad8738e6de1bb150570549a7e3a
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-11-08 14:34:24 +0800

    chore: rename Containers cfd-server-error-dialog to server-error-dialog

---
+++ packages/cfd/src/features/Containers/__tests__/server-error-dialog.spec.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { mockStore } from '@deriv/stores';
+import ServerErrorDialog from '../server-error-dialog';
+import CFDProviders from '../../../cfd-providers';
+
+describe('<ServerErrorDialog /> ', () => {
+    beforeAll(() => {
+        const modal_root_el = document.createElement('div');
+        modal_root_el.setAttribute('id', 'modal_root');
+        document.body.appendChild(modal_root_el);
+    });
+    let mockRootStore;
+    beforeEach(() => {
+        mockRootStore = {
+            ui: {
+                disableApp: jest.fn(),
+                enableApp: jest.fn(),
+            },
+            modules: {
+                cfd: {
+                    clearCFDError: jest.fn(),
+                    error_type: 'error_type',
+                    has_cfd_error: true,
+                    is_cfd_success_dialog_enabled: false,
+                    error_message: 'This is a sample error message',
+                },
+            },
+        };
+    });
+
+    afterAll(() => {
+        document.body.removeChild(modal_root_el);
+    });
+
+    it('should render the component properly', () => {
+        const { container } = render(<ServerErrorDialog />, {
+            wrapper: ({ children }) => <CFDProviders store={mockStore(mockRootStore)}>{children}</CFDProviders>,
+        });
+
+        expect(container.firstChild).toHaveClass('dc-dialog__wrapper dc-dialog__wrapper--enter');
+    });
+
+    it('should render the proper text and error message', () => {
+        render(<ServerErrorDialog />, {
+            wrapper: ({ children }) => <CFDProviders store={mockStore(mockRootStore)}>{children}</CFDProviders>,
+        });
+
+        expect(screen.getByText(/This is a sample error message/i)).toBeInTheDocument();
+        expect(screen.getByRole('button')).toHaveTextContent('OK');
+        expect(screen.getByRole('heading')).toHaveTextContent(/Something’s not right/i);
+    });
+
+    it('should not render the component if has_cfd_error is false', () => {
+        const new_mockRootStore = {
+            ui: {
+                ...mockRootStore.ui,
+            },
+            modules: {
+                cfd: {
+                    ...mockRootStore.modules.cfd,
+                    has_cfd_error: false,
+                },
+            },
+        };
+        render(<ServerErrorDialog />, {
+            wrapper: ({ children }) => <CFDProviders store={mockStore(new_mockRootStore)}>{children}</CFDProviders>,
+        });
+
+        expect(screen.queryByText(/This is a sample error message/i)).not.toBeInTheDocument();
+    });
+
+    it('should not render the component if is_cfd_success_dialog_enabled', () => {
+        const new_mockRootStore = {
+            ui: {
+                ...mockRootStore.ui,
+            },
+            modules: {
+                cfd: {
+                    ...mockRootStore.modules.cfd,
+                    is_cfd_success_dialog_enabled: true,
+                },
+            },
+        };
+
+        render(<ServerErrorDialog />, {
+            wrapper: ({ children }) => <CFDProviders store={mockStore(new_mockRootStore)}>{children}</CFDProviders>,
+        });
+
+        expect(screen.queryByText(/This is a sample error message/i)).not.toBeInTheDocument();
+    });
+
+    it('should clear the component if OK is clicked', () => {
+        render(<ServerErrorDialog />, {
+            wrapper: ({ children }) => <CFDProviders store={mockStore(mockRootStore)}>{children}</CFDProviders>,
+        });
+
+        const ok_btn = screen.getByRole('button', { name: /OK/i });
+        fireEvent.click(ok_btn);
+
+        expect(mockRootStore.modules.cfd.clearCFDError).toHaveBeenCalled();
+    });
+});
---
+++ packages/cfd/src/features/Containers/server-error-dialog.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Dialog, UnhandledErrorModal } from '@deriv/components';
+import { localize } from '@deriv/translations';
+import { observer, useStore } from '@deriv/stores';
+import { useCfdStore } from '../../Stores/Modules/CFD/Helpers/useCfdStores';
+
+const ServerErrorDialog = observer(() => {
+    const { ui } = useStore();
+    const { enableApp, disableApp } = ui;
+    const { clearCFDError, error_message, error_type, has_cfd_error, is_cfd_success_dialog_enabled } = useCfdStore();
+
+    const should_show_error =
+        has_cfd_error &&
+        !is_cfd_success_dialog_enabled &&
+        error_type &&
+        error_type !== 'PasswordReset' &&
+        error_type !== 'PasswordError';
+
+    if (should_show_error) {
+        return error_message ? (
+            <Dialog
+                title={localize('Something’s not right')}
+                confirm_button_text={localize('OK')}
+                onConfirm={clearCFDError}
+                disableApp={disableApp}
+                enableApp={enableApp}
+                is_visible={should_show_error}
+            >
+                {error_message}
+            </Dialog>
+        ) : (
+            <UnhandledErrorModal />
+        );
+    }
+    return null;
+});
+
+export default ServerErrorDialog;

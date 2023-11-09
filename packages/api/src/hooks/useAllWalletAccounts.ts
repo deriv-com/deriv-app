854d6672615acc86a09adca697fc103ce859b19e
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-11-09 14:04:57 +0800

    chore: create a function to sort wallets

--- packages/api/src/hooks/useAllWalletAccounts.ts
+++ packages/api/src/hooks/useAllWalletAccounts.ts
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import useCurrencyConfig from './useCurrencyConfig';
 import useWalletAccountsList from './useWalletAccountsList';
 import useAvailableWallets from './useAvailableWallets';
@@ -32,6 +32,16 @@ const useAllWalletAccounts = () => {
         return [...(modified_non_added_wallets || []), ...(modified_added_wallets || [])];
     }, [non_added_wallets, added_wallets, getConfig]);
 
+    /** Function to sort wallet based on currency */
+    const sortWalletsByCurrency = useCallback(
+        (a: typeof modified_wallets[number], b: typeof modified_wallets[number]) => {
+            if (a.is_crypto !== b.is_crypto) return a.is_crypto ? 1 : -1;
+
+            return (a.currency || 'USD').localeCompare(b.currency || 'USD');
+        },
+        []
+    );
+
     /** Sort the wallets by fiat, crypto, then virtual */
     const sorted_wallets = useMemo(() => {
         if (!modified_wallets) return;
@@ -46,21 +56,17 @@ const useAllWalletAccounts = () => {
         // Sort the added wallets alphabetically by fiat, crypto, then virtual (if any)
         modified_wallets.sort((a, b) => {
             if (!a.is_added || !b.is_added) return 0;
-            if (a.is_crypto !== b.is_crypto) return a.is_crypto ? 1 : -1;
-
-            return (a.currency || 'USD').localeCompare(b.currency || 'USD');
+            return sortWalletsByCurrency(a, b);
         });
 
         // Sort the non-added wallets alphabetically by fiat, crypto, then virtual (if any)
         modified_wallets.sort((a, b) => {
             if (a.is_added || b.is_added) return 0;
-            if (a.is_crypto !== b.is_crypto) return a.is_crypto ? 1 : -1;
-
-            return (a.currency || 'USD').localeCompare(b.currency || 'USD');
+            return sortWalletsByCurrency(a, b);
         });
 
         return [...modified_wallets];
-    }, [modified_wallets]);
+    }, [modified_wallets, sortWalletsByCurrency]);
 
     return {
         /** Sorted wallets accounts */

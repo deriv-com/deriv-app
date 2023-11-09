6fed0985a789b71393825c157fc763438c3e4ac5
Akmal Djumakhodjaev <akmal@binary.com>
2023-11-09 15:48:06 +0800

    chore: refactor test case

--- packages/trader/src/Modules/Contract/Components/InfoBox/__tests__/info-box-longcode.spec.tsx
+++ packages/trader/src/Modules/Contract/Components/InfoBox/__tests__/info-box-longcode.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { mockStore, StoreProvider } from '@deriv/stores';
+import { TCoreStores } from '@deriv/stores/types';
 import InfoBoxLongcode from '../info-box-longcode';
 
 const test_longcode_short = 'test longcode';
@@ -20,72 +21,61 @@ jest.mock('@deriv/components', () => ({
 }));
 
 describe('InfoBoxLongcode', () => {
-    const MockInfoBoxLongcode = (mocked_props: React.ComponentProps<typeof InfoBoxLongcode>) => {
-        const mocked_store = mockStore({
-            ui: {
-                is_mobile: false,
-            },
-        });
+    const mockInfoBoxLongcode = (
+        mocked_store: TCoreStores,
+        mocked_props: React.ComponentProps<typeof InfoBoxLongcode>
+    ) => {
         return (
             <StoreProvider store={mocked_store}>
                 <InfoBoxLongcode {...mocked_props} />
             </StoreProvider>
         );
     };
+
+    const default_mocked_store = mockStore({
+        ui: {
+            is_mobile: false,
+        },
+    });
+
     it('should render InfoBoxLongcode component', () => {
-        render(<MockInfoBoxLongcode {...mocked_props} />);
+        render(mockInfoBoxLongcode(default_mocked_store, mocked_props));
 
         expect(screen.getByText('MockedIcon')).toBeInTheDocument();
         expect(screen.getByText(test_longcode_short)).toBeInTheDocument();
     });
     it('should not render specific text if longcode is less then 150 symbols', () => {
-        render(<MockInfoBoxLongcode {...mocked_props} />);
+        render(mockInfoBoxLongcode(default_mocked_store, mocked_props));
 
         expect(screen.queryByText(view_more_text)).not.toBeInTheDocument();
     });
     it('should render specific text "View more" if longcode is more then 150 symbols', () => {
         mocked_props.contract_info.longcode = test_longcode_long;
-        render(<MockInfoBoxLongcode {...mocked_props} />);
+        render(mockInfoBoxLongcode(default_mocked_store, mocked_props));
 
         expect(screen.getByText(view_more_text)).toBeInTheDocument();
     });
     it('should render specific text "View less" if longcode is more then 150 symbols and user click on expand button', () => {
-        render(<MockInfoBoxLongcode {...mocked_props} />);
+        render(mockInfoBoxLongcode(default_mocked_store, mocked_props));
         userEvent.click(screen.getByText(view_more_text));
 
         expect(screen.queryByText(view_more_text)).not.toBeInTheDocument();
         expect(screen.getByText(view_less_text)).toBeInTheDocument();
     });
     it('should render specific text "View more" if longcode is more then 47 symbols for mobile', () => {
-        const mocked_store = mockStore({
-            ui: {
-                is_mobile: true,
-            },
-        });
+        default_mocked_store.ui.is_mobile = true;
         mocked_props.contract_info.longcode = test_longcode_mobile;
-        render(
-            <StoreProvider store={mocked_store}>
-                <InfoBoxLongcode {...mocked_props} />
-            </StoreProvider>
-        );
+        render(mockInfoBoxLongcode(default_mocked_store, mocked_props));
 
         expect(screen.getByText(view_more_text)).toBeInTheDocument();
     });
     it('should render modal if longcode is more then 47 symbols for mobile and user clicks on "View more" button', () => {
-        const mocked_store = mockStore({
-            ui: {
-                is_mobile: true,
-            },
-        });
+        default_mocked_store.ui.is_mobile = true;
         const modal_root_el = document.createElement('div');
         modal_root_el.setAttribute('id', 'modal_root');
         document.body.appendChild(modal_root_el);
         mocked_props.contract_info.longcode = test_longcode_mobile;
-        render(
-            <StoreProvider store={mocked_store}>
-                <InfoBoxLongcode {...mocked_props} />
-            </StoreProvider>
-        );
+        render(mockInfoBoxLongcode(default_mocked_store, mocked_props));
         userEvent.click(screen.getByText(view_more_text));
 
         expect(screen.getByText(view_more_text)).toBeInTheDocument();

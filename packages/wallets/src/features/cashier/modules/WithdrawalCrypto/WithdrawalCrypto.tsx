bca24d5924178deeb40088103185560c059137e4
Aum Bhatt <aum@deriv.com>
2023-11-09 18:01:07 +0400

    refactor: made separate component for the disclaimer

--- packages/wallets/src/features/cashier/modules/WithdrawalCrypto/WithdrawalCrypto.tsx
+++ packages/wallets/src/features/cashier/modules/WithdrawalCrypto/WithdrawalCrypto.tsx
@@ -2,28 +2,14 @@ import React from 'react';
 import { InlineMessage, WalletText } from '../../../../components';
 import './WithdrawalCrypto.scss';
 import { useActiveWalletAccount } from '@deriv/api';
-import { WithdrawalCryptoForm } from './components';
+import { WithdrawalCryptoForm, WithdrawalDisclaimer } from './components';
 import { Formik } from 'formik';
 
 const WithdrawalCrypto = () => {
     return (
         <div className='wallets-withdrawal-crypto'>
             <WalletText weight='bold'>Withdraw Bitcoin (BTC) to your wallet</WalletText>
-            <div className='wallets-withdrawal-crypto-disclaimer'>
-                <InlineMessage>
-                    <ul className='wallets-withdrawal-crypto-disclaimer__items'>
-                        <li>
-                            Do not enter an address linked to an initial coin offering (ICO) purchase or crowdsale. If
-                            you do, the initial coin offering (ICO) tokens will not be credited into your account.
-                        </li>
-                        <li>
-                            Please note that your maximum and minimum withdrawal limits aren’t fixed. They change due to
-                            the high volatility of cryptocurrency.
-                        </li>
-                    </ul>
-                </InlineMessage>
-            </div>
-
+            <WithdrawalDisclaimer />
             <WithdrawalCryptoForm />
         </div>
     );
---
+++ packages/wallets/src/features/cashier/modules/WithdrawalCrypto/components/WithdrawalDisclaimer/index.ts
@@ -0,0 +1,1 @@
+export { default as WithdrawalDisclaimer } from './WithdrawalDisclaimer';

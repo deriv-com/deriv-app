5740c0499e9e8fd14f1356f37d0d4a4db2dddb8a
Sandeep <sandeep@regentmarkets.com>
2023-11-09 12:33:44 +0800

    feat: quick-strategy added max-stake and contract-type for dalembert

--- packages/bot-web-ui/src/components/quick-strategy/config.ts
+++ packages/bot-web-ui/src/components/quick-strategy/config.ts
@@ -179,8 +179,8 @@ export const STRATEGIES: TStrategies = {
             "The D'Alembert strategy increases the stake after a losing trade and reduces the stake after a successful trade by the number of units that traders decide. One unit is equal to the amount of the initial stake. To manage risk, set the maximum stake for a single trade. The stake for the next trade will reset to the initial stake if it exceeds the maximum stake."
         ),
         fields: [
-            [SYMBOL, TRADETYPE_FULL_WIDTH, LABEL_STAKE, STAKE, DURATION_TYPE, DURATION],
-            [LABEL_PROFIT, PROFIT, LABEL_LOSS, LOSS, LABEL_DALEMBERT_UNIT, UNIT],
+            [SYMBOL, TRADETYPE, CONTRACT_TYPE, LABEL_STAKE, STAKE, DURATION_TYPE, DURATION],
+            [LABEL_PROFIT, PROFIT, LABEL_LOSS, LOSS, LABEL_DALEMBERT_UNIT, UNIT, CHECKBOX_MAX_STAKE, MAX_STAKE],
         ],
     },
     OSCARS_GRIND: {

2d915593e861f15e3bff72d75df59eb94e30d73c
maryia-deriv <maryia@binary.com>
2023-11-13 13:11:03 +0300

    test: VideoStream

--- packages/trader/src/App/Components/Elements/VideoStream/__tests__/video-stream.spec.tsx
+++ packages/trader/src/App/Components/Elements/VideoStream/__tests__/video-stream.spec.tsx
@@ -4,27 +4,48 @@ import VideoStream from '../video-stream';
 import { getDescriptionVideoId } from 'Modules/Trading/Helpers/contract-type';
 
 const mocked_props = {
-    autoplay: true,
+    autoplay: false,
     height: 270,
     preload: 'auto',
     src: getDescriptionVideoId('vanilla', false),
+    test_id: 'dt_video_stream',
 };
 
 describe('VideoStream component', () => {
-    it('should have "positions-toggle--active" class when "is_open" is "true"', () => {
+    it('should render iframe with a video with no controls & enabled picture-in-picture by default', () => {
         render(<VideoStream {...mocked_props} />);
+        const iframe = screen.getByTestId('dt_video_stream');
+        const iframe_src = iframe.getAttribute('src');
+        const picture_in_picture_param = iframe
+            .getAttribute('allow')
+            ?.split(';')
+            ?.find(param => param.includes('picture-in-picture'));
+        expect(picture_in_picture_param?.includes('*')).toBeTruthy();
+        expect(iframe_src?.includes('letterboxColor=transparent&preload=auto&controls=false')).toBeTruthy();
     });
-
-    // it('should have "positions-toggle--has-count" class when "positions_count > 0"', () => {
-    //     render(<VideoStream {...mocked_props} controls />);
-    // });
-
-    // it('should call "togglePositions" when the user clicked on the link', () => {
-    //     const mockTogglePositions = jest.fn();
-    //     render(<VideoStream {...mocked_props} disable_picture_in_picture />);
-    // });
-
-    // it('should render "IcPortfolio" icon', () => {
-    //     render(<VideoStream {...mocked_props} loop />);
-    // });
+    it('should render iframe with a video with controls', () => {
+        render(<VideoStream {...mocked_props} controls />);
+        const iframe_src = screen.getByTestId('dt_video_stream').getAttribute('src');
+        // controls are shown when controls param is skipped from query string in the iframe src:
+        expect(iframe_src?.includes('controls')).toBeFalsy();
+    });
+    it('should render iframe with a video with disabled picture-in-picture', () => {
+        render(<VideoStream {...mocked_props} disable_picture_in_picture />);
+        const picture_in_picture_param = screen
+            .getByTestId('dt_video_stream')
+            .getAttribute('allow')
+            ?.split(';')
+            ?.find(param => param.includes('picture-in-picture'));
+        expect(picture_in_picture_param?.includes('none')).toBeTruthy();
+    });
+    it('should render iframe with a looped video', () => {
+        render(<VideoStream {...mocked_props} loop />);
+        const iframe_src = screen.getByTestId('dt_video_stream').getAttribute('src');
+        expect(iframe_src?.includes('loop=true')).toBeTruthy();
+    });
+    it('should render iframe with an autoplaying video', () => {
+        render(<VideoStream {...mocked_props} autoplay />);
+        const iframe_src = screen.getByTestId('dt_video_stream').getAttribute('src');
+        expect(iframe_src?.includes('autoplay=true')).toBeTruthy();
+    });
 });

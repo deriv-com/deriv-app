79e093e8828291f6b4c29913124d940c7836f414
Wojciech Brygola <wojciech@regentmarkets.com>
2023-11-13 08:47:46 +0000

    feat: WALL-2497 moved logic to useEffect

--- packages/wallets/src/features/cashier/flows/WalletWithdrawal/WalletWithdrawal.tsx
+++ packages/wallets/src/features/cashier/flows/WalletWithdrawal/WalletWithdrawal.tsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { WithdrawalFiatModule, WithdrawalVerificationModule } from '../../modules';
 
 const WalletWithdrawal = () => {
-    const queryParams = new URLSearchParams(location.search);
-    const verificationQueryParam = queryParams.get('verification');
-
     const [verificationCode, setVerificationCode] = useState('');
 
-    if (verificationQueryParam) {
-        setVerificationCode(verificationQueryParam);
+    useEffect(() => {
+        const queryParams = new URLSearchParams(location.search);
+        const verificationQueryParam = queryParams.get('verification');
 
-        const url = new URL(window.location.href);
-        url.searchParams.delete('verification'); // Remove the 'verification_code' query parameter
-        window.history.replaceState({}, document.title, url.toString());
-    }
+        if (verificationQueryParam) {
+            setVerificationCode(verificationQueryParam);
+
+            const url = new URL(window.location.href);
+            url.searchParams.delete('verification'); // Remove the 'verification_code' query parameter
+            window.history.replaceState({}, document.title, url.toString());
+        }
+    }, []);
 
     if (verificationCode) {
         return <WithdrawalFiatModule verificationCode={verificationCode} />;

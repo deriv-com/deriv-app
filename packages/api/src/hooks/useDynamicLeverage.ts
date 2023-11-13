5af0249839b8a078fb07692ee3c1164cc0c53617
arshad-rao-deriv <muhammad.arshad@deriv.com>
2023-11-13 20:32:24 +0400

    refactor: enhance dynamic leverage hook so that additional logic can be added to the response

--- packages/api/src/hooks/useDynamicLeverage.ts
+++ packages/api/src/hooks/useDynamicLeverage.ts
@@ -1,13 +1,21 @@
+import { useMemo } from 'react';
 import useQuery from '../useQuery';
 
 /** A custom hook that gets dynamic leverage values. */
 const useDynamicLeverage = (platform: 'mt5' | 'dxtrade' | 'ctrader') => {
-    const { data: dynamic_leverages, ...rest } = useQuery('trading_platform_leverage', {
+    const { data, ...rest } = useQuery('trading_platform_leverage', {
         payload: { platform },
     });
 
+    // Add additional information to the dynamic leverage response.
+    const modified_data = useMemo(() => {
+        if (!data?.trading_platform_leverage) return;
+
+        return { ...data?.trading_platform_leverage };
+    }, [data]);
+
     return {
-        data: dynamic_leverages?.trading_platform_leverage.leverage,
+        data: modified_data?.leverage,
         ...rest,
     };
 };

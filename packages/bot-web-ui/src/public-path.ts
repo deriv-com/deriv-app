b010c9564351b55b037c8dba3e89772c11e5cc6e
Farabi Chowdhury <farabi@deriv.com>
2023-11-14 13:41:26 +0800

    fix: removed survicate code

--- packages/bot-web-ui/src/public-path.ts
+++ packages/bot-web-ui/src/public-path.ts
@@ -14,25 +14,4 @@ export function setBotPublicPath(path: string) {
 
 export const getImageLocation = (image_name: string) => getUrlBase(`/public/images/common/${image_name}`);
 
-// eslint-disable-next-line import/no-mutable-exports
-let initSurvicateCalled = false;
-const initSurvicate = () => {
-    initSurvicateCalled = true;
-    if (document.getElementById('dbot-survicate')) {
-        const survicate_box = document.getElementById('survicate-box') || undefined;
-        if (survicate_box) {
-            survicate_box.style.display = 'block';
-        }
-        return;
-    }
-
-    const script = document.createElement('script');
-    script.id = 'dbot-survicate';
-    script.async = true;
-    script.src = 'https://survey.survicate.com/workspaces/83b651f6b3eca1ab4551d95760fe5deb/web_surveys.js';
-    document.body.appendChild(script);
-};
-
-export { initSurvicate, initSurvicateCalled };
-
 setBotPublicPath(getUrlBase('/'));

7e53a414b156c8d7733be19ad5c548b852113042
maryia-deriv <maryia@binary.com>
2023-11-14 15:24:20 +0300

    revert: contract-type.tsx

--- packages/trader/src/Modules/Trading/Helpers/__tests__/contract-type.spec.tsx
+++ packages/trader/src/Modules/Trading/Helpers/__tests__/contract-type.spec.tsx
@@ -1,15 +1,9 @@
 import {
-    ACCU_MANUAL_VIDEO_ID,
-    CF_STREAM_CUSTOMER_URL,
-    DESCRIPTION_VIDEO_ID,
-    getAccuManualVideoId,
+    getContractTypeCategoryIcons,
     getAvailableContractTypes,
     getContractCategoryKey,
-    getContractTypeCategoryIcons,
     getContractTypes,
     getCategoriesSortedByKey,
-    getDescriptionDownloadUrl,
-    getDescriptionVideoId,
     ordered_trade_categories,
 } from '../contract-type';
 
@@ -118,46 +112,5 @@ describe('getCategoriesSortedByKey', () => {
     });
     it('should return an empty array if a list is not received', () => {
         expect(getCategoriesSortedByKey()).toEqual([]);
-    });
-});
-
-describe('getAccuManualVideoId', () => {
-    it('should return AccumulatorsStatsManualModal video id for desktop in light theme', () => {
-        expect(getAccuManualVideoId(false, false)).toEqual(ACCU_MANUAL_VIDEO_ID.desktop.light);
-    });
-    it('should return AccumulatorsStatsManualModal video id for desktop in dark theme', () => {
-        expect(getAccuManualVideoId(false, true)).toEqual(ACCU_MANUAL_VIDEO_ID.desktop.dark);
-    });
-    it('should return AccumulatorsStatsManualModal video id for mobile in dark theme', () => {
-        expect(getAccuManualVideoId(true, true)).toEqual(ACCU_MANUAL_VIDEO_ID.mobile.dark);
-    });
-    it('should return AccumulatorsStatsManualModal video id for mobile in light theme', () => {
-        expect(getAccuManualVideoId(true, false)).toEqual(ACCU_MANUAL_VIDEO_ID.mobile.light);
-    });
-    it('should return AccumulatorsStatsManualModal video id for desktop in light theme when arguments are empty', () => {
-        expect(getAccuManualVideoId()).toEqual(ACCU_MANUAL_VIDEO_ID.desktop.light);
-    });
-});
-
-describe('getDescriptionVideoId', () => {
-    it('should return an id for Vanillas description video in light theme', () => {
-        expect(getDescriptionVideoId('vanilla', false)).toEqual(DESCRIPTION_VIDEO_ID.vanilla.light);
-    });
-    it('should return an id for Turbos description video in dark theme', () => {
-        expect(getDescriptionVideoId('turbos', true)).toEqual(DESCRIPTION_VIDEO_ID.turbos.dark);
-    });
-    it('should return undefined when called with empty arguments', () => {
-        expect(getDescriptionVideoId()).toEqual(undefined);
-    });
-});
-
-describe('getDescriptionDownloadUrl', () => {
-    it('should return a download URL for Vanillas description video in light theme', () => {
-        expect(getDescriptionDownloadUrl('vanilla', false)).toEqual(
-            `${CF_STREAM_CUSTOMER_URL}${DESCRIPTION_VIDEO_ID.vanilla.light}/downloads/default.mp4`
-        );
-    });
-    it('should return an empty string if called with empty arguments', () => {
-        expect(getDescriptionDownloadUrl()).toEqual('');
     });
 });

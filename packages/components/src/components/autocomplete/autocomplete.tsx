1b16d42340dd750ee1cb347a737f4d671a7a3fc6
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-11-14 09:19:27 +0300

    chore: add optional type

--- packages/components/src/components/autocomplete/autocomplete.tsx
+++ packages/components/src/components/autocomplete/autocomplete.tsx
@@ -34,7 +34,7 @@ type TAutocompleteProps = {
     value: string;
     onSearch?: (value: string, items: TItem[]) => [];
     data_testid: string;
-    readOnly: boolean;
+    readOnly?: boolean;
 };
 
 const KEY_CODE = {

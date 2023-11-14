4b489b006ed58fcdb9593c4c464f0c79cdb158f1
Hirad <hirad@regentmarkets.com>
2023-11-14 14:27:26 +0800

    fix: making changes based on the comments

--- packages/cfd/src/features/hooks/useCurrentList.ts
+++ packages/cfd/src/features/hooks/useCurrentList.ts
@@ -1,6 +1,6 @@
 import { useStore } from '@deriv/stores';
 import { useMT5AccountsList, useDxtradeAccountsList, useCtraderAccountsList } from '@deriv/api';
-import { getAccountListKey } from '@deriv/shared';
+import { getAccountListKey, CFD_PLATFORMS, Jurisdiction } from '@deriv/shared';
 
 type MT5AccountsList = NonNullable<ReturnType<typeof useMT5AccountsList>['data']>[number];
 type DxtradeAccountsList = NonNullable<ReturnType<typeof useDxtradeAccountsList>['data']>[number];
@@ -24,23 +24,23 @@ const useCurrentList = () => {
     mt5_accounts
         ?.filter(acc =>
             show_eu_related_content
-                ? acc.landing_company_short === 'maltainvest'
-                : acc.landing_company_short !== 'maltainvest'
+                ? acc.landing_company_short === Jurisdiction.MALTA_INVEST
+                : acc.landing_company_short !== Jurisdiction.MALTA_INVEST
         )
         .forEach(account => {
-            current_list[getAccountListKey(account, 'mt5', account.landing_company_short)] = {
+            current_list[getAccountListKey(account, CFD_PLATFORMS.MT5, account.landing_company_short)] = {
                 ...account,
             };
         });
 
     dxtrade_accounts?.forEach(account => {
-        current_list[getAccountListKey(account, 'dxtrade', account.landing_company_short)] = {
+        current_list[getAccountListKey(account, CFD_PLATFORMS.DXTRADE, account.landing_company_short)] = {
             ...account,
         };
     });
 
     ctrader_accounts?.forEach(account => {
-        current_list[getAccountListKey(account, 'ctrader', account.landing_company_short)] = {
+        current_list[getAccountListKey(account, CFD_PLATFORMS.CTRADER, account.landing_company_short)] = {
             ...account,
         };
     });

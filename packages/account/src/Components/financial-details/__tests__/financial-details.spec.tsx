71042d6441a4bcde87ba5b0fc94ddc76028b5bef
Likhith Kolayari <likhith@regentmarkets.com>
2023-11-14 09:35:22 +0400

    fix: incorporated review comments

--- packages/account/src/Components/financial-details/__tests__/financial-details.spec.tsx
+++ packages/account/src/Components/financial-details/__tests__/financial-details.spec.tsx
@@ -12,10 +12,17 @@ jest.mock('@deriv/shared', () => ({
 }));
 
 const modal_root_el = document.createElement('div');
-modal_root_el.setAttribute('id', 'modal_root');
-document.body.appendChild(modal_root_el);
 
 describe('<FinancialDetails />', () => {
+    beforeAll(() => {
+        modal_root_el.setAttribute('id', 'modal_root');
+        document.body.appendChild(modal_root_el);
+    });
+
+    afterAll(() => {
+        document.body.removeChild(modal_root_el);
+    });
+
     const mock_props: React.ComponentProps<typeof FinancialDetails> = {
         getCurrentStep: jest.fn(),
         goToNextStep: jest.fn(),

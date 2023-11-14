99f6e7398e3e537d932768dda0732313756d0144
Mark Wylde <129744061+markwylde-deriv@users.noreply.github.com>
2023-11-14 11:41:49 +0000

    chore: move out of alpha folder

--- packages/wallets/integration-tests/menu.spec.tsx
+++ packages/wallets/integration-tests/menu.spec.tsx
@@ -1,10 +1,10 @@
 import { mock_general, mock_loggedIn, setupMocks } from '@deriv/integration';
 import { expect, test } from '@playwright/test';
-import { mock_balance } from '../mocks/mock_balance';
-import { mock_crypto_config } from '../mocks/mock_crypto_config';
-import { mock_get_account_types } from '../mocks/mock_get_account_types';
-import { mock_proposal_open_contract } from '../mocks/mock_proposal_open_contract';
-import mock_wallets_authorize, { DEFAULT_WALLET_ACCOUNTS } from '../mocks/mock_wallets_authorize';
+import { mock_balance } from './mocks/mock_balance';
+import { mock_crypto_config } from './mocks/mock_crypto_config';
+import { mock_get_account_types } from './mocks/mock_get_account_types';
+import { mock_proposal_open_contract } from './mocks/mock_proposal_open_contract';
+import mock_wallets_authorize, { DEFAULT_WALLET_ACCOUNTS } from './mocks/mock_wallets_authorize';
 
 test.describe('Wallets - Traders Hub', () => {
     test('render header', async ({ baseURL, page }) => {

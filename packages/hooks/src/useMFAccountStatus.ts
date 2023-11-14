db12a64daa88b3a0873a71dd373c550faa5ef98a
Taysuisin <suisin@regentmarkets.com>
2023-11-14 14:42:05 +0800

    chore: fix wrong modal popup displayed

--- packages/hooks/src/useMFAccountStatus.ts
+++ packages/hooks/src/useMFAccountStatus.ts
@@ -1,15 +1,17 @@
 import { AccountStatus } from '@deriv/shared';
-import useContentFlag from './useContentFlag';
 import useHasMaltaInvestAccount from './useHasMaltaInvestAccount';
 import useGetMFAccountStatus from './useGetMFAccountStatus';
+import { useStore } from '@deriv/stores';
 
 const useMFAccountStatus = () => {
-    const { is_low_risk_cr_eu, is_eu_real } = useContentFlag();
+    const {
+        client: { is_eu },
+    } = useStore();
     const has_malta_invest_account = useHasMaltaInvestAccount();
     const mf_status = useGetMFAccountStatus();
 
     const should_show_status_for_multipliers_account =
-        (is_low_risk_cr_eu || is_eu_real) &&
+        is_eu &&
         has_malta_invest_account &&
         mf_status &&
         [AccountStatus.PENDING, AccountStatus.FAILED, AccountStatus.NEEDS_VERIFICATION].includes(mf_status);

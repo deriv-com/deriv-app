93a96c08cd5b1502a07fa345f7788c91e10abf6a
Aizad Ridzo <103104395+aizad-deriv@users.noreply.github.com>
2023-11-15 14:55:46 +0800

    [wallets] Aizad/WalletPasswordField v2 (#11306)
    * chore: making changes on existing WalletPasswordField
    - remove usestate within component and added Password prop
    - change stylings to follow Figma/production
    
    * fix: rename MessageContainer component to HelperMessage for better clarity
    
    * fix: small changes
    
    * chore: added invalid and valid states of the text field and helper message
    - updated styles in WalletTextfield
    - updated styles in WalletPasswordField
    - added constants for helper message'
    
    * chore: resolve conflicts
    - apply comments
    - replace HTMLIInputProps with ComponentProps
    
    * fix: resolve conflicts pt.2
    
    * chore: added new stuff
    - replace zxcvbn in favor of regex
    - seperated functions and place inside its own file
    - remove zxcvbn from wallets package
    
    * chore: resolve conflicts pt.1
    - move patterns to utils
    - rename helperMessage to message
    - remove duplicated types
    
    * chore: resolve comments pt.2
    - change password validation handler
    - implement PasswordField inside of CreatePassword
    - change PasswordViewer handler
    
    * chore: resolve comments pt.3
    - remove ...rest from the component props and only includes the ones that are being used
    
    * fix: resolve comments pt.4
    
    * fix: resolve comments pt.5
    
    * fix: typescript error

--- packages/wallets/src/components/Base/WalletTextField/WalletTextField.tsx
+++ packages/wallets/src/components/Base/WalletTextField/WalletTextField.tsx
@@ -1,8 +1,8 @@
-import React, { ChangeEvent, CSSProperties, forwardRef, InputHTMLAttributes, useState } from 'react';
-import MessageContainer, { MessageContainerProps } from './HelperMessage';
+import React, { ChangeEvent, ComponentProps, CSSProperties, FC, useState } from 'react';
+import HelperMessage, { HelperMessageProps } from './HelperMessage';
 import './WalletTextField.scss';
 
-export interface WalletTextFieldProps extends InputHTMLAttributes<HTMLInputElement>, MessageContainerProps {
+export interface WalletTextFieldProps extends ComponentProps<'input'>, HelperMessageProps {
     defaultValue?: string;
     label?: string;
     maxWidth?: CSSProperties['maxWidth'];
@@ -10,61 +10,53 @@ export interface WalletTextFieldProps extends InputHTMLAttributes<HTMLInputEleme
     showMessage?: boolean;
 }
 
-const WalletTextField = forwardRef<HTMLInputElement, WalletTextFieldProps>(
-    (
-        {
-            defaultValue = '',
-            helperMessage,
-            label,
-            maxLength,
-            maxWidth = '33rem',
-            name = 'wallet-textfield',
-            onChange,
-            renderRightIcon,
-            showMessage = false,
-            ...rest
-        },
-        ref
-    ) => {
-        const [value, setValue] = useState(defaultValue);
+const WalletTextField: FC<WalletTextFieldProps> = ({
+    defaultValue = '',
+    label,
+    maxLength,
+    maxWidth = '33rem',
+    message,
+    name = 'wallet-textfield',
+    onChange,
+    renderRightIcon,
+    showMessage = false,
+    ...rest
+}) => {
+    const [value, setValue] = useState(defaultValue);
 
-        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-            const newValue = e.target.value;
-            setValue(newValue);
-            onChange?.(e);
-        };
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.target.value;
+        setValue(newValue);
+        onChange?.(e);
+    };
 
-        return (
-            <div className='wallets-textfield' style={{ maxWidth }}>
-                <div className='wallets-textfield__box'>
-                    <input
-                        className='wallets-textfield__field'
-                        id={name}
-                        maxLength={maxLength}
-                        onChange={handleChange}
-                        placeholder={label}
-                        value={value}
-                        {...rest}
-                        ref={ref}
-                    />
-                    {label && (
-                        <label className='wallets-textfield__label' htmlFor={name}>
-                            {label}
-                        </label>
-                    )}
-                    {typeof renderRightIcon === 'function' && (
-                        <div className='wallets-textfield__icon'>{renderRightIcon()}</div>
-                    )}
-                </div>
-                <div className='wallets-textfield__message-container'>
-                    {showMessage && (
-                        <MessageContainer helperMessage={helperMessage} inputValue={value} maxLength={maxLength} />
-                    )}
-                </div>
+    return (
+        <div className='wallets-textfield' style={{ maxWidth }}>
+            <div className='wallets-textfield__box'>
+                <input
+                    className='wallets-textfield__field'
+                    id={name}
+                    maxLength={maxLength}
+                    onChange={handleChange}
+                    placeholder={label}
+                    value={value}
+                    {...rest}
+                />
+                {label && (
+                    <label className='wallets-textfield__label' htmlFor={name}>
+                        {label}
+                    </label>
+                )}
+                {typeof renderRightIcon === 'function' && (
+                    <div className='wallets-textfield__icon'>{renderRightIcon()}</div>
+                )}
             </div>
-        );
-    }
-);
+            <div className='wallets-textfield__message-container'>
+                {showMessage && <HelperMessage inputValue={value} maxLength={maxLength} message={message} />}
+            </div>
+        </div>
+    );
+};
 
 WalletTextField.displayName = 'WalletTextField';
 export default WalletTextField;

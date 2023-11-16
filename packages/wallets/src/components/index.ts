98b57ea5c04084dff786b703b9021e40fc2a75dd
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-11-16 14:14:26 +0800

    chore: email sent content screen

--- packages/wallets/src/components/index.ts
+++ packages/wallets/src/components/index.ts
@@ -6,6 +6,7 @@ export * from './Dropzone';
 export * from './FadedAnimatedList';
 export * from './Loader';
 export * from './OptionsAndMultipliersListing';
+export * from './SentEmailContent';
 export * from './SkeletonLoader';
 export * from './ToggleSwitch';
 export * from './TradingAccountCard';

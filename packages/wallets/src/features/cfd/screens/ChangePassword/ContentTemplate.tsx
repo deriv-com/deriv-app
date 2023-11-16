9278d2a40901461c28c3656d6b8add17c19aeaf1
aizad-deriv <aizad@deriv.com>
2023-11-16 15:44:28 +0800

    fix: remove curly brackets

--- packages/wallets/src/features/cfd/screens/ChangePassword/ContentTemplate.tsx
+++ packages/wallets/src/features/cfd/screens/ChangePassword/ContentTemplate.tsx
@@ -8,19 +8,17 @@ interface ContentTemplateProps {
     icon: React.ReactNode;
 }
 
-const ContentTemplate: React.FC<ContentTemplateProps> = ({ bodyText, button, headingText, icon }) => {
-    return (
-        <>
-            <div className='change-password__content__icon'>{icon}</div>
-            <div className='wallets-change-password__content__text'>
-                <WalletText align='center' weight='bold'>
-                    {headingText}
-                </WalletText>
-                {bodyText}
-            </div>
-            <div className='wallets-change-password__content__btn'>{button}</div>
-        </>
-    );
-};
+const ContentTemplate: React.FC<ContentTemplateProps> = ({ bodyText, button, headingText, icon }) => (
+    <>
+        <div className='change-password__content__icon'>{icon}</div>
+        <div className='wallets-change-password__content__text'>
+            <WalletText align='center' weight='bold'>
+                {headingText}
+            </WalletText>
+            {bodyText}
+        </div>
+        <div className='wallets-change-password__content__btn'>{button}</div>
+    </>
+);
 
 export default ContentTemplate;

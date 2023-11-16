aaba36d9d0554ffe718e95b69416d5fede1b73ed
Farhan Ahmad Nurzi <125247833+farhan-nurzi-deriv@users.noreply.github.com>
2023-11-16 17:04:58 +0800

    [WALL] Farhan/WALL-2593/Complete your personal details page screen (#11407)
    * feat: create personal details form
    
    * refactor: 🔧 remove styles props
    
    * fix: 🛠️ dropdown scss
    
    * fix: ⚒️ missing props
    
    * fix: ⚒️ incorrect props
    
    * refactor: 🔧 remove default max-width
    
    * fix: ⚒️ typscript errors

---
+++ packages/wallets/src/features/accounts/screens/PersonalDetails/constants.tsx
@@ -0,0 +1,14 @@
+export const accountOpeningReasonList = [
+    {
+        text: 'Hedging',
+        value: 'Hedging',
+    },
+    {
+        text: 'Income Earning',
+        value: 'Income Earning',
+    },
+    {
+        text: 'Speculative',
+        value: 'Speculative',
+    },
+];

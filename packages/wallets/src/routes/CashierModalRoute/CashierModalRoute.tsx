3e0a469da1cdfa0d2352755d99b11905a5c80e6b
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-11-17 16:40:18 +0800

    chore: 💳 hide header on modal for overlay

--- packages/wallets/src/routes/CashierModalRoute/CashierModalRoute.tsx
+++ packages/wallets/src/routes/CashierModalRoute/CashierModalRoute.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { createPortal } from 'react-dom';
 import { WalletCashier } from '../../features';
+import useDevice from '../../hooks/useDevice';
 import './CashierModalRoute.scss';
 
-const CashierModalRoute: React.FC = () => (
-    <div className='wallets-cashier-modal-route'>
-        <WalletCashier />
-    </div>
-);
+const CashierModalRoute = () => {
+    const { isMobile } = useDevice();
+    const rootRef = useRef<HTMLElement>(document.getElementById('wallets_modal_root'));
+
+    if (isMobile)
+        return createPortal(
+            <div className='wallets-cashier-modal-route'>
+                <WalletCashier />
+            </div>,
+            // @ts-expect-error-next-line ref type mismatch
+            rootRef.current
+        );
+
+    return (
+        <div className='wallets-cashier-modal-route'>
+            <WalletCashier />
+        </div>
+    );
+};
 
 export default CashierModalRoute;

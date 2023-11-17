c9f49db9ac9d59504de3ea050fa55e97024ae2f2
rupato-deriv <rupato@regentmarkets.com>
2023-11-17 13:53:36 +0800

    fix: reverted localize component

--- packages/bot-web-ui/src/components/dashboard/tutorial-tab/common/common-tabs/index.tsx
+++ packages/bot-web-ui/src/components/dashboard/tutorial-tab/common/common-tabs/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Localize } from '@deriv/translations';
+import { localize } from '@deriv/translations';
 import FAQContent from '../../faq-content';
 import GuideContent from '../../guide-content';
 import NoSearchResult from '../no-search-result-found';
@@ -22,13 +22,13 @@ export const generateTutorialTabs = (sidebar_content, is_mobile, search) => {
     const { guide_tab_content, faq_tab_content, filtered_tab_list } = sidebar_content;
 
     const tutorial_tabs = [
-        { label: <Localize i18n_default_text='Guide' />, content: <GuideTab guideList={guide_tab_content} /> },
+        { label: localize('Guide'), content: <GuideTab guideList={guide_tab_content} /> },
         {
-            label: <Localize i18n_default_text='FAQ' />,
+            label: localize('FAQ'),
             content: <FAQTab faqList={faq_tab_content} isMobile={is_mobile} />,
         },
         {
-            label: <Localize i18n_default_text='Search' />,
+            label: localize('Search'),
             content: (
                 <SearchTab
                     filteredList={filtered_tab_list}

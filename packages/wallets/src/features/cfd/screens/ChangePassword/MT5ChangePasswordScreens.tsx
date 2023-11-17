03aa17650715859aed23808c90ff83a842b2c212
aizad-deriv <aizad@deriv.com>
2023-11-17 11:44:39 +0800

    chore: added index inside of ChangePasswordScreens

--- packages/wallets/src/features/cfd/screens/ChangePassword/MT5ChangePasswordScreens.tsx
+++ packages/wallets/src/features/cfd/screens/ChangePassword/MT5ChangePasswordScreens.tsx
@@ -4,20 +4,15 @@ import { useModal } from '../../../../components/ModalProvider';
 import MT5PasswordIcon from '../../../../public/images/ic-mt5-password.svg';
 
 const MT5ChangePasswordScreens = () => {
-    const [activeScreen, setActiveScreen] = useState(0);
+    type TChangePasswordScreenIndex = 'confirmationScreen' | 'emailVerification' | 'introScreen';
 
-    const handleClick = () => setActiveScreen(prev => prev + 1);
+    const [activeScreen, setActiveScreen] = useState<TChangePasswordScreenIndex>('introScreen');
+    const handleClick = (nextScreen: TChangePasswordScreenIndex) => setActiveScreen(nextScreen);
 
     const { hide } = useModal();
 
-    const ChangePasswordScreens = [
-        {
-            bodyText: 'Use this password to log in to your Deriv MT5 accounts on the desktop, web, and mobile apps.',
-            button: <WalletButton onClick={handleClick} size='lg' text='Change password' />,
-            headingText: 'Deriv MT5 password',
-            icon: <MT5PasswordIcon />,
-        },
-        {
+    const ChangePasswordScreens = {
+        confirmationScreen: {
             bodyText: (
                 <WalletText align='center' color='error' size='sm'>
                     This will change the password to all of your Deriv MT5 accounts.
@@ -25,18 +20,22 @@ const MT5ChangePasswordScreens = () => {
             ),
             button: (
                 <div className='wallets-change-password__btn'>
-                    {/* TODO: Double confirm with the designers or QA on the behavior of the Cancel button*/}
                     <WalletButton onClick={() => hide()} size='lg' text='Cancel' variant='outlined' />
-                    {/* TODO: Append logic send email for password reset to Confirm Button */}
-                    <WalletButton onClick={handleClick} size='lg' text='Confirm' />
+                    <WalletButton onClick={() => handleClick('emailVerification')} size='lg' text='Confirm' />
                 </div>
             ),
             headingText: 'Confirm to change your Deriv MT5 password',
             icon: <MT5PasswordIcon />,
         },
-    ];
+        introScreen: {
+            bodyText: 'Use this password to log in to your Deriv MT5 accounts on the desktop, web, and mobile apps.',
+            button: <WalletButton onClick={() => handleClick('confirmationScreen')} size='lg' text='Change password' />,
+            headingText: 'Deriv MT5 password',
+            icon: <MT5PasswordIcon />,
+        },
+    };
 
-    if (activeScreen === 2)
+    if (activeScreen === 'emailVerification')
         return (
             <div className='wallets-change-password__sent-email-wrapper'>
                 <SentEmailContent />

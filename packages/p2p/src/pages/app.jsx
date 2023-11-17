f6820b8ab936759d400eef9fa78c2c5486ea5e81
Shayan Khaleghparast <100833613+shayan-deriv@users.noreply.github.com>
2023-11-17 14:52:54 +0800

    Shayan/P2PS-1753/refactor exchange rates hook (#11034)
    * feat: adding new hook for exchange_rates [WIP]
    
    * chore: removed unnecessary parts from the hook
    
    * feat: refactoring asset-summary [wip]
    
    * feat: adding global provider for handling subscription
    
    * build: added ts-usehooks to hooks package
    
    * chore: removed unused code
    
    * revert: reverted changes in ExchangeRatesProvider
    
    * fix: updated the context to store rates in localstorage
    
    * refactor: refactored tradershub with the new hook
    
    * fix: fixed typescript error
    
    * chore: cleaned the code
    
    * refactor: refactored cashier transfer page
    
    * refactor: replace old exchange rate with the new one
    
    * refactor: added exchange rate provider
    
    * fix: removed extra imports
    
    * refactor: added usep2pexchangerate hook for avoiding duplications
    
    * test: added tests for usep2pexchangerate hook
    
    * fix: fixed crypto-fiat-converter.spec.tsx errors
    
    * fix: fixed test errors
    
    * refactor: refactored percentage-selector component
    
    * feat: added test for the new useExchangeRate
    
    * feat: added new test
    
    * fix: resolved pr comment
    
    * fix: resolved pr comment
    
    * fix: fixing cashier page crashing in test link
    
    * fix: trigger ci build
    
    * build: updated package-lock.json
    
    * fix: fixed test failing
    
    * fix: p2p tests
    
    * fix: conditions
    
    * fix: floating rate tests
    
    * fix: resolved pr comments
    
    * fix: fixed test failing
    
    * fix: resolved pr comments
    
    * fix: minor fixes
    
    * revert: revert my latest changes for useTotalAccountBalance
    
    * fix: bug fixed
    
    * fix: checking p2p issue
    
    * chore: removed console.log
    
    * fix: fixed crypto withdrawal issue
    
    * chore: removed asset-summary legacy code
    
    * chore: renamed some variables
    
    * chore: removed legacy useExchangeRate and useTotalAccountBalance hooks and their tests
    
    * refactor: removed legacy code for exchange_rate call
    
    * fix: fixing build error
    
    * test: removed my-ads-row-renderer test
    
    * chore: renamed the variables
    
    * chore: removed unused code
    
    * fix: added new line at the end of package.json
    
    ---------
    
    Co-authored-by: Iman Khaleghparast <imankhaleghparast@Shayan-khaleghparasts-Mac-WTQ21CJ93L.local>
    Co-authored-by: Iman Khaleghparast <imankhaleghparast@Shayan-khaleghparasts-Mac-WTQ21CJ93L-Standard.local>
    Co-authored-by: Niloofar <niloofar.sadeghi@regentmarkets.com>

--- packages/p2p/src/pages/app.jsx
+++ packages/p2p/src/pages/app.jsx
@@ -255,15 +255,13 @@ const App = () => {
     }
 
     return (
-        <>
-            <main className='p2p'>
-                <ModalManagerContextProvider>
-                    <ModalManager />
-                    <AppContent order_id={order_id} />
-                    <Routes />
-                </ModalManagerContextProvider>
-            </main>
-        </>
+        <main className='p2p'>
+            <ModalManagerContextProvider>
+                <ModalManager />
+                <AppContent order_id={order_id} />
+                <Routes />
+            </ModalManagerContextProvider>
+        </main>
     );
 };
 

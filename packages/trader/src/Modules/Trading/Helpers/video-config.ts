c8deebbfd9a490c1700d858410f1c6f93c88b02b
maryia-deriv <maryia@binary.com>
2023-11-19 22:47:06 +0300

    refactor: video-config.ts

--- packages/trader/src/Modules/Trading/Helpers/video-config.ts
+++ packages/trader/src/Modules/Trading/Helpers/video-config.ts
@@ -1,21 +1,23 @@
 import { TVideo } from '../Components/Form/ContractType/types';
 
-export const getDescriptionVideoId = (videos: TVideo[] = [], contract_type = '') => {
-    return videos.find(video => video.meta.name.includes(`dtrader_${contract_type}`))?.uid ?? '';
+export const getDescriptionVideoId = (videos: TVideo[] = [], trade_type = '') => {
+    // finds a video id for the exact trade type by the video file name
+    return videos.find(video => video.meta.name.startsWith(`dtrader_${trade_type}`))?.uid ?? '';
 };
 
-export const getDescriptionVideos = async (is_dark_mode_on = false) => {
+export const getDescriptionVideos = async (is_dark_mode_on = false): Promise<TVideo[]> => {
     const endpoint = `https://api.cloudflare.com/client/v4/accounts/${
         process.env.CLOUDFLARE_ACCOUNT_ID
-    }/stream?search=description_${is_dark_mode_on ? 'dark' : 'light'}`;
+    }/stream?search=description_${is_dark_mode_on ? 'dark' : 'light'}.mp4`;
+    // finds all videos with name containing description_light.mp4 or description_dark.mp4
     try {
         const response = await fetch(endpoint, {
             headers: {
                 Authorization: `bearer ${process.env.CLOUDFLARE_STREAM_API_TOKEN}`,
             },
         });
-        const result = await response.json();
-        return result.result;
+        const { result } = await response.json();
+        return result;
     } catch (error) {
         return [];
     }

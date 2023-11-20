c54e91dc1c91fab8acb99c94f132ea50b4cd70a3
adrienne-rio <adrienne@deriv.com>
2023-11-20 17:21:10 +0800

    chore: removed unusued changes

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -5,6 +5,7 @@ import './AppContent.scss';
 const AppContent: React.FC = () => {
     return (
         <div className='wallets-app'>
+            <div className='wallets-modal-responsive-root' id='wallets_modal_responsive_root' />
             <Router />
         </div>
     );

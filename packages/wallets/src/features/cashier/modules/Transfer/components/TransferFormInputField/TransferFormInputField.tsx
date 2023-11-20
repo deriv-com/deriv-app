e758274e30718eb63944be08211cd7c784c07765
Rostik Kayko <rostislav@deriv.com>
2023-11-20 13:30:30 +0300

    refactor: move logic to `useInputATMFormatter`

--- packages/wallets/src/features/cashier/modules/Transfer/components/TransferFormInputField/TransferFormInputField.tsx
+++ packages/wallets/src/features/cashier/modules/Transfer/components/TransferFormInputField/TransferFormInputField.tsx
@@ -15,8 +15,6 @@ type TProps = {
     value: number;
 };
 
-const separatorRegex = /[,.]/g;
-
 const WalletTransferFormInputField: React.FC<TProps> = ({
     currency,
     disabled,
@@ -28,58 +26,21 @@ const WalletTransferFormInputField: React.FC<TProps> = ({
     value,
 }) => {
     const inputRef = useRef<HTMLInputElement>(null);
-    const input = inputRef.current;
-
     const [isFocused, setIsFocused] = useState<boolean>(false);
-    const [caretNeedsRepositioning, setCaretNeedsRepositioning] = useState<boolean>(false);
-    const [caret, setCaret] = useState<number>();
 
     const {
         onChange: formatOnChange,
         onPaste: formatOnPaste,
         value: formattedValue,
-    } = useInputATMFormatter(value, {
+    } = useInputATMFormatter(inputRef, value, {
         fractionDigits,
         locale,
+        maxDigits,
     });
-    const [prevFormattedValue, setPrevFormattedValue] = useState<string>(formattedValue);
 
     useEffect(() => {
-        setPrevFormattedValue(formattedValue);
         onChange?.(Number(unFormatLocaleString(formattedValue, locale)));
-    }, [caretNeedsRepositioning, formattedValue, locale, onChange]);
-
-    // keep the caret from jumping
-    useEffect(() => {
-        if (caret && caretNeedsRepositioning && input) {
-            const indexBeforeCaret = formattedValue.length - 1 - caret;
-
-            // if before a comma or period, prefer positioning the caret to the left of it
-            const newCaretPosition = separatorRegex.test(formattedValue[indexBeforeCaret])
-                ? indexBeforeCaret - 1
-                : indexBeforeCaret;
-
-            input.setSelectionRange(newCaretPosition, newCaretPosition);
-            setCaretNeedsRepositioning(false);
-        }
-    }, [caret, formattedValue, caretNeedsRepositioning, input]);
-
-    // override some editing behavior for better UX
-    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (!input) return;
-
-        const newCaretPosition = input.value.length - (input.selectionStart ?? 0);
-        setCaret(newCaretPosition);
-        setCaretNeedsRepositioning(true);
-
-        if (maxDigits && input.value.replace(separatorRegex, '').length > maxDigits) return;
-
-        const hasNoChangeInDigits =
-            input.value.length + 1 === prevFormattedValue.length &&
-            input.value.replaceAll(separatorRegex, '') === prevFormattedValue.replaceAll(separatorRegex, '');
-        if (hasNoChangeInDigits) return;
-        formatOnChange(e);
-    };
+    }, [formattedValue, locale, onChange]);
 
     return (
         <div className='wallets-transfer-form-input-field'>
@@ -95,7 +56,7 @@ const WalletTransferFormInputField: React.FC<TProps> = ({
                         className='wallets-transfer-form-input-field__input'
                         disabled={disabled}
                         onBlur={() => setIsFocused(false)}
-                        onChange={onChangeHandler}
+                        onChange={formatOnChange}
                         onFocus={() => setIsFocused(true)}
                         onPaste={formatOnPaste}
                         ref={inputRef}
--- packages/wallets/src/hooks/useInputATMFormatter.ts
+++ packages/wallets/src/hooks/useInputATMFormatter.ts
@@ -1,24 +1,65 @@
-import { useCallback, useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { unFormatLocaleString } from '@deriv/utils';
 import useInputDecimalFormatter from './useInputDecimalFormatter';
 
 type TOptions = {
     fractionDigits?: number;
     locale?: Intl.LocalesArgument;
+    maxDigits?: number;
 };
 
-const useInputATMFormatter = (initial?: number, options?: TOptions) => {
+const separatorRegex = /[,.]/g; // locale-agnostic
+
+const useInputATMFormatter = (inputRef: React.RefObject<HTMLInputElement>, initial?: number, options?: TOptions) => {
+    const input = inputRef.current;
+
     const isPasting = useRef(false);
     const { onChange: onChangeDecimal, value } = useInputDecimalFormatter(undefined, options);
-    const { fractionDigits = 2, locale } = options || {};
+    const { fractionDigits = 2, locale, maxDigits = 14 } = options || {};
 
     const formattedValue = useMemo(
         () => `${Number(value).toLocaleString(locale, { minimumFractionDigits: fractionDigits })}`,
         [fractionDigits, locale, value]
     );
 
+    // maintain the input state to compare new values to it on every change
+    const [prevFormattedValue, setPrevFormattedValue] = useState<string>(formattedValue);
+    useEffect(() => {
+        setPrevFormattedValue(formattedValue);
+    }, [formattedValue]);
+
+    // keep the caret from jumping
+    const [caretNeedsRepositioning, setCaretNeedsRepositioning] = useState<boolean>(false);
+    const [caret, setCaret] = useState<number>();
+    useEffect(() => {
+        if (caret && caretNeedsRepositioning && input) {
+            const indexBeforeCaret = formattedValue.length - 1 - caret;
+
+            // if before a comma or period, prefer positioning the caret to the left of it
+            const newCaretPosition = separatorRegex.test(formattedValue[indexBeforeCaret])
+                ? indexBeforeCaret
+                : indexBeforeCaret + 1;
+
+            input.setSelectionRange(newCaretPosition, newCaretPosition);
+            setCaretNeedsRepositioning(false);
+        }
+    }, [caret, formattedValue, caretNeedsRepositioning, input]);
+
     const onChange = useCallback(
         (e: DeepPartial<React.ChangeEvent<HTMLInputElement>> | React.ChangeEvent<HTMLInputElement>) => {
+            if (!input) return;
+
+            const newCaretPosition = input.value.length - (input.selectionStart ?? 0);
+            setCaret(newCaretPosition);
+            setCaretNeedsRepositioning(true);
+
+            if (maxDigits && input.value.replace(separatorRegex, '').length > maxDigits) return;
+
+            const hasNoChangeInDigits =
+                input.value.length + 1 === prevFormattedValue.length &&
+                input.value.replaceAll(separatorRegex, '') === prevFormattedValue.replaceAll(separatorRegex, '');
+            if (hasNoChangeInDigits) return;
+
             const newValue = e?.target?.value || '';
             const unformatted = unFormatLocaleString(newValue, locale);
             const shifted = (Number(unformatted) * 10).toFixed(fractionDigits);
@@ -64,7 +105,7 @@ const useInputATMFormatter = (initial?: number, options?: TOptions) => {
 
             return onChangeDecimal({ target: { value: unformatted } });
         },
-        [locale, fractionDigits, onChangeDecimal]
+        [input, maxDigits, prevFormattedValue, locale, fractionDigits, onChangeDecimal]
     );
 
     const onPaste: React.ClipboardEventHandler<HTMLInputElement> = useCallback(

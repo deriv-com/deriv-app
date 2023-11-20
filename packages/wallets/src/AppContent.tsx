411c5d4d2d680135dc3689fefba3e3ecb2f5dba7
Sergei Baranovski <sergei@deriv.com>
2023-11-20 16:59:28 +0300

    feat: add viewport vh instead of svh

--- packages/wallets/src/AppContent.tsx
+++ packages/wallets/src/AppContent.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { defineViewportVh } from './utils/utils';
 import { Router } from './routes';
 import './AppContent.scss';
 
 const AppContent: React.FC = () => {
+    useEffect(() => {
+        defineViewportVh();
+        window.addEventListener('resize', defineViewportVh);
+        return () => window.removeEventListener('resize', defineViewportVh);
+    }, []);
+
     return (
         <div className='wallets-app'>
             <div className='wallets-modal-show-header-root' id='wallets_modal_show_header_root' />
--- packages/wallets/src/utils/utils.ts
+++ packages/wallets/src/utils/utils.ts
@@ -7,3 +7,8 @@ type TServerError = {
 
 export const isServerError = (error: unknown): error is TServerError =>
     typeof error === 'object' && error !== null && 'code' in error;
+
+export const defineViewportVh = () => {
+    const viewportVh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--viewport-vh', `${viewportVh}px`);
+};

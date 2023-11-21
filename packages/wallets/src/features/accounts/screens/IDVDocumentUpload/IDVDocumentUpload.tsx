8c30dc39ba0ae6f1be57be052c5518f36d7b18a6
Thisyahlen <thisyahlen@regentmarkets.com>
2023-11-21 15:41:22 +0800

    feat: add validations to account flow
    Co-authored-by: adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>

--- packages/wallets/src/features/accounts/screens/IDVDocumentUpload/IDVDocumentUpload.tsx
+++ packages/wallets/src/features/accounts/screens/IDVDocumentUpload/IDVDocumentUpload.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import { FlowTextField, WalletDropdown, WalletText } from '../../../../components';
+import React, { useMemo, useState } from 'react';
+import { FlowTextField, useFlow, WalletDropdown, WalletText } from '../../../../components';
+import { drivingLicenseValidator, passportValidator, requiredValidator, ssnitValidator } from '../../validations';
 import { IDVDocumentUploadDetails } from './components';
 import './IDVDocumentUpload.scss';
 
 const IDVDocumentUpload = () => {
+    const { setFormValues } = useFlow();
+
+    const [selectedDocument, setSelectedDocument] = useState('documentNumber');
+
+    const validationSchema = useMemo(() => {
+        switch (selectedDocument) {
+            case 'driverLicense':
+                return drivingLicenseValidator;
+            case 'passport':
+                return passportValidator;
+            case 'ssnit':
+                return ssnitValidator;
+            default:
+                return requiredValidator;
+        }
+    }, [selectedDocument]);
+
     return (
         <div className='wallets-idv-document-upload'>
             <div className='wallets-idv-document-upload__body'>
                 <div className='wallets-idv-document-upload__title'>
                     <WalletText weight='bold'>Identity verification</WalletText>
                 </div>
-                {/* TODO: Update dropdown after Formik is implemented */}
                 <WalletDropdown
                     label='Choose the document type'
-                    list={[]}
-                    name='wallets-idv-document-upload__dropdown'
-                    onSelect={() => null}
+                    list={[
+                        { text: 'Drivers License', value: 'driverLicense' },
+                        { text: 'Passport', value: 'passport' },
+                        { text: 'Social Security and National Insurance Trust (SSNIT)', value: 'ssnit' },
+                    ]}
+                    name='documentType'
+                    onSelect={selectedItem => {
+                        setSelectedDocument(selectedItem);
+                        setFormValues('documentType', selectedItem);
+                    }}
                     value={undefined}
                 />
-                <FlowTextField label='Enter your document number' name='documentNumber' />
+                <FlowTextField
+                    label='Enter your document number'
+                    name='documentNumber'
+                    validationSchema={validationSchema}
+                />
                 <div className='wallets-idv-document-upload__title'>
                     <WalletText weight='bold'>Details</WalletText>
                 </div>

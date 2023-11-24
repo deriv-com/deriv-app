b399795f95279ecb81dc7abe27384bf396c9b3e9
Shaheer <shaheer@deriv.com>
2023-11-24 16:00:22 +0400

    refactor: :art: updates conected apps data table template

--- packages/account/src/Sections/Security/ConnectedApps/data-table-template.tsx
+++ packages/account/src/Sections/Security/ConnectedApps/data-table-template.tsx
@@ -1,45 +1,47 @@
-/* eslint-disable react/display-name */
 import React from 'react';
-import { Text } from '@deriv/components';
-import { localize } from '@deriv/translations';
-import {
-    TDataTableTemplateProps,
-    getConnectedAppsAction,
-    getConnectedAppsLastLogin,
-    getConnectedAppsScopes,
-} from './template-helper';
+import classNames from 'classnames';
+import { OauthApps } from '@deriv/api-types';
+import { Button, Text } from '@deriv/components';
+import { toMoment } from '@deriv/shared';
+import { Localize } from '@deriv/translations';
+import { CONNECTED_APPS_COLUMN_NAMES, getConnectedAppsScopes } from './template-helper';
 
-type Column = {
-    title: string;
-    col_index: 'name' | 'scopes' | 'last_used' | 'app_id';
-    renderCellContent: React.FC<{ cell_value: string & number & string[] }>;
-};
+type TDataTableTemplate = { connected_apps: OauthApps; handleToggleModal: (app_id: number) => void };
 
-const DataTableTemplate = ({ handleToggleModal }: TDataTableTemplateProps): Column[] => [
-    {
-        title: localize('Name'),
-        col_index: 'name',
-        renderCellContent: ({ cell_value }) => (
-            <Text size='xs' className='name__content'>
-                {cell_value}
+const DataTableTemplate = ({ connected_apps, handleToggleModal }: TDataTableTemplate) => (
+    <div className='connected-apps__tabular--wrapper'>
+        {CONNECTED_APPS_COLUMN_NAMES.map((column_name, index) => (
+            <Text
+                size='xs'
+                weight='bold'
+                key={column_name.key}
+                className={classNames(
+                    'connected-apps__tabular--cell connected-apps__tabular--header',
+                    index === 0 ? 'connected-apps__tabular--skip-left-padding' : ''
+                )}
+            >
+                {column_name}
             </Text>
-        ),
-    },
-    {
-        title: localize('Permission'),
-        col_index: 'scopes',
-        renderCellContent: ({ cell_value }) => getConnectedAppsScopes(cell_value),
-    },
-    {
-        title: localize('Last login'),
-        col_index: 'last_used',
-        renderCellContent: ({ cell_value }) => getConnectedAppsLastLogin(cell_value),
-    },
-    {
-        title: localize('Action'),
-        col_index: 'app_id',
-        renderCellContent: ({ cell_value }) => getConnectedAppsAction(cell_value, handleToggleModal),
-    },
-];
+        ))}
+        {connected_apps.map(connected_app => (
+            <React.Fragment key={connected_app.app_id}>
+                <Text size='xs' className='connected-apps__tabular--cell connected-apps__tabular--skip-left-padding'>
+                    {connected_app.name}
+                </Text>
+                <Text size='xs' className='connected-apps__tabular--cell'>
+                    {getConnectedAppsScopes(connected_app.scopes)}
+                </Text>
+                <Text size='xs' className='connected-apps__tabular--cell'>
+                    {toMoment(connected_app.last_used).format('YYYY-MM-DD HH:mm:ss')}
+                </Text>
+                <span className='connected-apps__tabular--cell'>
+                    <Button small secondary onClick={() => handleToggleModal(connected_app.app_id)}>
+                        <Localize i18n_default_text='Revoke access' />
+                    </Button>
+                </span>
+            </React.Fragment>
+        ))}
+    </div>
+);
 
 export default DataTableTemplate;

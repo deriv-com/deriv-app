533b407d37bdddf2554f371d3e17bfa3d6aec969
maryia-deriv <maryia@binary.com>
2023-11-24 12:30:52 +0300

    refactor: address comments

--- packages/core/src/App/Containers/trade-notifications.tsx
+++ packages/core/src/App/Containers/trade-notifications.tsx
@@ -17,33 +17,34 @@ const TradeNotifications = observer(({ show_trade_notifications }: { show_trade_
                 {trade_notifications.slice(0, 3).map(notification => {
                     const { buy_price, contract_id, currency, contract_type, id, profit, symbol, status, timestamp } =
                         notification;
+                    const is_open = status === 'open';
                     return (
                         <SwipeableNotification
                             key={id}
-                            is_failure={status !== 'open' && profit < 0}
-                            is_success={status !== 'open' && profit >= 0}
+                            is_failure={!is_open && profit < 0}
+                            is_success={!is_open && profit >= 0}
                             onUnmount={() => removeTradeNotifications(id)}
                             redirect_to={getContractPath(contract_id)}
-                            timestamp={status === 'open' ? null : timestamp}
+                            timestamp={is_open ? null : timestamp}
                             visibility_duration_ms={3000}
                         >
                             <Text as='p' size='xxxs' line_height='s'>
                                 <Localize
-                                    i18n_default_text='<0>Trade {{status}}:</0> {{trade_type_name}} on {{symbol}}'
+                                    i18n_default_text='<0>{{title}}:</0> {{trade_type_name}} on {{symbol}}'
                                     components={[<strong key={0} />]}
                                     values={{
-                                        status: status === 'open' ? localize('opened') : localize('closed'),
                                         symbol,
+                                        title: is_open ? localize('Trade opened') : localize('Trade closed'),
                                         trade_type_name: contract_type,
                                     }}
                                     shouldUnescape
                                 />
                             </Text>
                             <Text as='p' size='xxxs' line_height='s'>
-                                {status === 'open' ? getCardLabels().STAKE : getCardLabels().TOTAL_PROFIT_LOSS}{' '}
+                                {is_open ? getCardLabels().STAKE : getCardLabels().TOTAL_PROFIT_LOSS}{' '}
                                 <Text size='xxxs' line_height='s' weight='bold'>
                                     <Money
-                                        amount={status === 'open' ? buy_price : profit}
+                                        amount={is_open ? buy_price : profit}
                                         currency={currency}
                                         has_sign
                                         should_format

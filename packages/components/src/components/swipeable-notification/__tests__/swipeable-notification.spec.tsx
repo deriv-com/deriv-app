d49292bdd49fbc16a2f4066c0c92e16a52f17118
maryia-deriv <maryia@binary.com>
2023-11-24 10:01:45 +0300

    test: mock useSwipeable

--- packages/components/src/components/swipeable-notification/__tests__/swipeable-notification.spec.tsx
+++ packages/components/src/components/swipeable-notification/__tests__/swipeable-notification.spec.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import SwipeableNotification from '../swipeable-notification';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import { useSwipeable } from 'react-swipeable';
+
+jest.mock('react-swipeable', () => ({
+    useSwipeable: jest.fn(() => ({
+        onMouseDown: jest.fn(),
+    })),
+}));
 
 describe('SwipeableNotification', () => {
     const content = 'Notification content';
@@ -16,9 +23,16 @@ describe('SwipeableNotification', () => {
     };
 
     it('should render the correct content', () => {
+        const onMouseDown = jest.fn();
+        (useSwipeable as jest.Mock).mockReturnValueOnce({ onMouseDown });
         renderwithRouter();
 
-        expect(screen.getByText(content)).toBeInTheDocument();
+        const notification = screen.getByText(content);
+        expect(notification).toBeInTheDocument();
+        const link = screen.getByRole('link');
+        // there is no mouseDown event in userEvent, hence using fireEvent
+        fireEvent.mouseDown(link);
+        expect(onMouseDown).toBeCalled();
     });
     it('should render 15s ago timestamp', () => {
         const fifteen_sec_ago = Math.floor(Date.now() / 1000) - 15;
@@ -38,7 +52,7 @@ describe('SwipeableNotification', () => {
             onUnmountCallback();
             return { cancel: jest.fn() };
         });
-        renderwithRouter({ visibility_duration_ms: 3000, onUnmount: onUnmountCallback });
+        renderwithRouter({ visibility_duration_ms: 3000, onUnmount: onUnmountCallback, timestamp: 1234567890 });
         expect(onUnmountCallback).toBeCalled();
     });
     it('should redirect to URL passed as redirect_to', () => {

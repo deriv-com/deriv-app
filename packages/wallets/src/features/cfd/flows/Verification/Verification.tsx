b26e461cc53cf05081fc80e359d6baa6e73fb18f
Farhan Ahmad Nurzi <125247833+farhan-nurzi-deriv@users.noreply.github.com>
2023-11-24 11:54:21 +0800

    [WALL] Farhan/WALL-2723/Add validation to manual flow (#11704)
    * chore: ✨ add validations for manual upload
    
    * refactor: 🔧 move the type out
    
    * refactor: 🧹 cleaning up function and remove unused validator

--- packages/wallets/src/features/cfd/flows/Verification/Verification.tsx
+++ packages/wallets/src/features/cfd/flows/Verification/Verification.tsx
@@ -38,6 +38,42 @@ type TVerificationProps = {
     selectedJurisdiction: THooks.AvailableMT5Accounts['shortcode'];
 };
 
+type TManualVerificationFooter = {
+    context: TFlowProviderContext<typeof screens>;
+    isNextDisabled: boolean;
+    isNextLoading: boolean;
+    nextFlowHandler: () => void;
+};
+
+const getManualVerificationFooter = ({
+    context,
+    isNextDisabled,
+    isNextLoading,
+    nextFlowHandler,
+}: TManualVerificationFooter) => {
+    if (!context.formValues.selectedManualDocument) return undefined;
+
+    const onClickBack = () => {
+        if (context.currentScreenId === 'selfieScreen') {
+            context.switchScreen('manualScreen');
+        } else context.setFormValues('selectedManualDocument', '');
+    };
+
+    // eslint-disable-next-line react/display-name
+    return () => (
+        <WalletButtonGroup isFlex>
+            <WalletButton onClick={onClickBack} size='lg' text='Back' variant='outlined' />
+            <WalletButton
+                disabled={isNextDisabled}
+                isLoading={isNextLoading}
+                onClick={nextFlowHandler}
+                size='lg'
+                text='Next'
+            />
+        </WalletButtonGroup>
+    );
+};
+
 const Verification: FC<TVerificationProps> = ({ selectedJurisdiction }) => {
     const { data: poiStatus, isSuccess: isSuccessPOIStatus } = usePOI();
     const { data: poaStatus, isSuccess: isSuccessPOAStatus } = usePOA();
@@ -100,35 +136,23 @@ const Verification: FC<TVerificationProps> = ({ selectedJurisdiction }) => {
                         !formValues.drivingLicenceNumber ||
                         !formValues.drivingLicenseExpiryDate ||
                         !formValues.drivingLicenseCardFront ||
-                        !formValues.drivingLicenseCardBack ||
-                        isManualUploadLoading
+                        !formValues.drivingLicenseCardBack
                     );
                 } else if (formValues.selectedManualDocument === 'passport') {
-                    return (
-                        !formValues.passportNumber ||
-                        !formValues.passportExpiryDate ||
-                        !formValues.passportCard ||
-                        isManualUploadLoading
-                    );
+                    return !formValues.passportNumber || !formValues.passportExpiryDate || !formValues.passportCard;
                 } else if (formValues.selectedManualDocument === 'identity-card') {
                     return (
                         !formValues.identityCardNumber ||
                         !formValues.identityCardExpiryDate ||
                         !formValues.identityCardFront ||
-                        !formValues.identityCardBack ||
-                        isManualUploadLoading
+                        !formValues.identityCardBack
                     );
                 } else if (formValues.selectedManualDocument === 'nimc-slip') {
-                    return (
-                        !formValues.nimcNumber ||
-                        !formValues.nimcCardFront ||
-                        !formValues.nimcCardBack ||
-                        isManualUploadLoading
-                    );
+                    return !formValues.nimcNumber || !formValues.nimcCardFront || !formValues.nimcCardBack;
                 }
                 return !formValues.selectedManualDocument;
             case 'selfieScreen':
-                return !formValues.selfie;
+                return !formValues.selfie || isManualUploadLoading || isUploadLoading;
             case 'onfidoScreen':
                 return !formValues.hasSubmittedOnfido;
             case 'personalDetailsScreen':
@@ -166,11 +190,13 @@ const Verification: FC<TVerificationProps> = ({ selectedJurisdiction }) => {
                         last_name: formValues.lastName,
                     });
                 } else if (currentScreenId === 'selfieScreen') {
+                    await uploadDocument(formValues);
                     await upload({
                         document_issuing_country: settings?.country_code ?? undefined,
                         document_type: 'selfie_with_id',
                         file: formValues.selfie,
                     });
+                    setFormValues('selectedManualDocument', '');
                 }
 
                 // handle screen switching
@@ -182,8 +208,6 @@ const Verification: FC<TVerificationProps> = ({ selectedJurisdiction }) => {
                     show(<MT5PasswordModal marketType={selectedMarketType} platform={platform} />);
                 }
             } else if (currentScreenId === 'manualScreen') {
-                await uploadDocument(formValues);
-                setFormValues('selectedManualDocument', '');
                 switchScreen('selfieScreen');
             } else if (currentScreenId === 'poaScreen') {
                 updateSettings({
@@ -234,39 +258,22 @@ const Verification: FC<TVerificationProps> = ({ selectedJurisdiction }) => {
                 return (
                     <ModalStepWrapper
                         renderFooter={
-                            context.currentScreenId === 'manualScreen' && !context.formValues.selectedManualDocument
-                                ? undefined
-                                : () => {
-                                      if (context.currentScreenId === 'manualScreen')
-                                          return (
-                                              <WalletButtonGroup isFlex>
-                                                  <WalletButton
-                                                      onClick={() =>
-                                                          context.setFormValues('selectedManualDocument', '')
-                                                      }
-                                                      size='lg'
-                                                      text='Back'
-                                                      variant='outlined'
-                                                  />
-                                                  <WalletButton
-                                                      disabled={isNextDisabled(context)}
-                                                      isLoading={isNextLoading(context)}
-                                                      onClick={() => nextFlowHandler(context)}
-                                                      size='lg'
-                                                      text='Next'
-                                                  />
-                                              </WalletButtonGroup>
-                                          );
-                                      return (
-                                          <WalletButton
-                                              disabled={isNextDisabled(context)}
-                                              isLoading={isNextLoading(context)}
-                                              onClick={() => nextFlowHandler(context)}
-                                              size='lg'
-                                              text='Next'
-                                          />
-                                      );
-                                  }
+                            context.currentScreenId === 'manualScreen' || context.currentScreenId === 'selfieScreen'
+                                ? getManualVerificationFooter({
+                                      context,
+                                      isNextDisabled: isNextDisabled(context),
+                                      isNextLoading: isNextLoading(context),
+                                      nextFlowHandler: () => nextFlowHandler(context),
+                                  })
+                                : () => (
+                                      <WalletButton
+                                          disabled={isNextDisabled(context)}
+                                          isLoading={isNextLoading(context)}
+                                          onClick={() => nextFlowHandler(context)}
+                                          size='lg'
+                                          text='Next'
+                                      />
+                                  )
                         }
                         title='Add a real MT5 account'
                     >

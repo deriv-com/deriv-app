6c8bc8fc4d5edd9a9c4109db373b8db222c469b1
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-11-24 14:08:59 +0800

    refactor: rename data-testid for copy-text-icon

--- packages/cfd/src/features/Components/__tests__/copy-text-icon.js
+++ packages/cfd/src/features/Components/__tests__/copy-text-icon.js
@@ -5,7 +5,7 @@ import { CopyTextIcon } from '../copy-text-icon';
 describe('<CopyTextIcon />', () => {
     it('component should be rendered', () => {
         const { getByTestId } = render(<CopyTextIcon />);
-        const mainDiv = getByTestId('cfd_account_copy_main_div');
+        const mainDiv = getByTestId('copy_text_icon_main_div');
 
         expect(mainDiv).toBeInTheDocument();
     });
--- packages/cfd/src/features/Components/copy-text-icon.tsx
+++ packages/cfd/src/features/Components/copy-text-icon.tsx
@@ -10,7 +10,7 @@ type TCopyTextIcon = {
 
 const CopyTextIcon = ({ text, className }: TCopyTextIcon) => {
     return (
-        <div className={className} data-testid='cfd_account_copy_main_div'>
+        <div className={className} data-testid='copy_text_icon_main_div'>
             <Clipboard
                 text_copy={text}
                 info_message={isMobile() ? '' : localize('copy')}

d3e05df1319878c8d34e4333ab870ee93c6f9e2c
Rostik Kayko <rostislav@deriv.com>
2023-11-24 11:14:18 +0300

    fix: mt5 tmp workaround

--- packages/wallets/src/features/cashier/modules/Transactions/components/TransactionsCompletedRow/components/TransactionsCompletedRowTransferAccountDetails/TransactionsCompletedRowTransferAccountDetails.tsx
+++ packages/wallets/src/features/cashier/modules/Transactions/components/TransactionsCompletedRow/components/TransactionsCompletedRowTransferAccountDetails/TransactionsCompletedRowTransferAccountDetails.tsx
@@ -13,7 +13,10 @@ const TransactionsCompletedRowTransferAccountDetails: React.FC<TProps> = ({ acco
     const wallet = accounts.wallets?.find(account => account.loginid === loginid);
     const dtradeAccount = accounts.dtrade?.find(account => account.loginid === loginid);
     const dxtradeAccount = accounts.dxtrade?.find(account => account.login === loginid);
-    const mt5Account = accounts.mt5?.find(account => account.login === loginid);
+    // TODO: remove the `replace` calls once backend resolves `statement` and `mt5_login_list` accounts `loginid` inconsistency
+    const mt5Account = accounts.mt5?.find(
+        account => account.login?.replace(/^\D+/g, '') === loginid.replace(/^\D+/g, '')
+    );
     const ctraderAccount = accounts.ctrader?.find(account => account.login === loginid);
 
     const transferAccount = [wallet, dtradeAccount, dxtradeAccount, mt5Account, ctraderAccount].find(Boolean);

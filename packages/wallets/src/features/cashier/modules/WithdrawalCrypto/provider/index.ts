ea0b45caa07969558a177f18a7cd5d368fdb8a1f
Aum Bhatt <aum@deriv.com>
2023-11-24 17:27:36 +0400

    refactor: creation of the WithdrawalCryptoContext

---
+++ packages/wallets/src/features/cashier/modules/WithdrawalCrypto/provider/index.ts
@@ -0,0 +1,4 @@
+import WithdrawalCryptoProvider, { TWithdrawalCrypto, useWithdrawalCryptoContext } from './WithdrawalCryptoProvider';
+
+export { useWithdrawalCryptoContext, WithdrawalCryptoProvider };
+export type { TWithdrawalCrypto };

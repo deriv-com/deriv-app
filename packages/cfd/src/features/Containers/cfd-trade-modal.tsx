2e3ce5dfbcbb1641755efa3b6c0bb2051735102a
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-11-27 15:07:46 +0800

    refactor: updated imports and removed redundant type definition

--- packages/cfd/src/features/Containers/cfd-trade-modal.tsx
+++ packages/cfd/src/features/Containers/cfd-trade-modal.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useMT5AccountsList, useDxtradeServiceToken, useCtraderServiceToken } from '@deriv/api';
+import { DetailsOfEachMT5Loginid } from '@deriv/api-types';
 import { DesktopWrapper, Div100vhContainer, Modal, MobileWrapper, PageOverlay, UILoader } from '@deriv/components';
 import { localize } from '@deriv/translations';
 import { observer, useStore } from '@deriv/stores';
+import { TCFDPasswordReset } from 'Containers/props.types';
 import OtherCFDsTradeModal from './other-cfds-trade-modal';
 import MT5TradeModal from './mt5-trade-modal';
-import { TCFDDashboardContainer, TCFDPasswordReset } from '../../Containers/props.types';
-import { DetailsOfEachMT5Loginid } from '@deriv/api-types';
-import { TCFDsPlatformType } from 'Components/props.types';
 
 type TCFDTradeModalProps = {
     is_eu_user: boolean;
@@ -23,37 +22,9 @@ type TCFDTradeModalProps = {
     is_demo: string;
 };
 
-type TMT5TradeModalProps = {
-    mt5_trade_account: DetailsOfEachMT5Loginid & {
-        webtrader_url?: string;
-    };
-    show_eu_related_content: boolean;
-    onPasswordManager: (
-        arg1: string | undefined,
-        arg2: string,
-        group: TCFDPasswordReset['account_group'],
-        arg4: string,
-        arg5: string | undefined
-    ) => void;
-    toggleModal: () => void;
-};
-
-type TOtherCFDsTradeModalProps = {
-    mt5_trade_account: Required<DetailsOfEachMT5Loginid>;
-    is_eu_user: boolean;
-    onPasswordManager: (
-        arg1: string | undefined,
-        arg2: string,
-        group: TCFDPasswordReset['account_group'],
-        arg4: string,
-        arg5: string | undefined
-    ) => void;
-    toggleModal: () => void;
-    dxtrade_tokens: TCFDDashboardContainer['dxtrade_tokens'];
-    ctrader_tokens: TCFDDashboardContainer['ctrader_tokens'];
-    is_demo: string;
-    platform: TCFDsPlatformType;
-    is_mobile?: boolean;
+type TTokens = {
+    demo: string;
+    real: string;
 };
 
 const CFDTradeModal = observer(
@@ -72,7 +43,7 @@ const CFDTradeModal = observer(
             if (platform === 'mt5') {
                 return (
                     <MT5TradeModal
-                        mt5_trade_account={mt5_trade_account as unknown as TMT5TradeModalProps['mt5_trade_account']}
+                        mt5_trade_account={mt5_trade_account as unknown as DetailsOfEachMT5Loginid}
                         show_eu_related_content={show_eu_related_content}
                         onPasswordManager={onPasswordManager}
                         toggleModal={toggleModal}
@@ -81,14 +52,14 @@ const CFDTradeModal = observer(
             }
             return (
                 <OtherCFDsTradeModal
-                    mt5_trade_account={mt5_trade_account as unknown as TOtherCFDsTradeModalProps['mt5_trade_account']}
+                    mt5_trade_account={mt5_trade_account as unknown as Required<DetailsOfEachMT5Loginid>}
                     is_eu_user={is_eu_user}
                     onPasswordManager={onPasswordManager}
                     toggleModal={toggleModal}
                     is_demo={is_demo}
                     platform={platform}
-                    ctrader_tokens={ctrader_tokens as unknown as TCFDDashboardContainer['ctrader_tokens']}
-                    dxtrade_tokens={dxtrade_tokens as unknown as TCFDDashboardContainer['dxtrade_tokens']}
+                    ctrader_tokens={ctrader_tokens as unknown as TTokens}
+                    dxtrade_tokens={dxtrade_tokens as unknown as TTokens}
                     is_mobile={is_mobile}
                 />
             );

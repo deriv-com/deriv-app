d7e5c6324cb1fee40dffdad0def780b81f72e24b
Hamza <hamza.muhammad@regentmarkets.com>
2023-11-28 10:24:55 +0800

    chore: rename the component

--- packages/api/src/hooks/useOtherCFDPlatformsDeposit.ts
+++ packages/api/src/hooks/useOtherCFDPlatformsDeposit.ts
@@ -4,7 +4,7 @@ import useMutation from '../useMutation';
 type TPayload = Parameters<ReturnType<typeof useMutation<'trading_platform_deposit'>>['mutate']>[0]['payload'];
 
 /** A custom hook for top-up of Other CFD Accounts */
-const useTradingPlatformDeposit = () => {
+const useOtherCFDPlatformsDeposit = () => {
     const { mutate: _mutate, mutateAsync: _mutateAsync, ...rest } = useMutation('trading_platform_deposit');
 
     const mutate = useCallback((payload: TPayload) => _mutate({ payload }), [_mutate]);
@@ -18,4 +18,4 @@ const useTradingPlatformDeposit = () => {
     };
 };
 
-export default useTradingPlatformDeposit;
+export default useOtherCFDPlatformsDeposit;

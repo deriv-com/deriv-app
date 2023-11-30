395544bb5e2ed75d3150130be1c08907da301b66
Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
2023-11-30 14:31:13 +0300

    [WALL] Sergei / wall 2751 / Add change investor password logic (#11879)
    * feat: intermediate result
    
    * feat: add formik
    
    * feat: separate components to different screens
    
    * refactor: create mt5ChangeInvestorPasswordScreens scss file

---
+++ packages/wallets/src/features/cfd/screens/ChangePassword/InvestorPassword/MT5ChangeInvestorPasswordSavedScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { WalletButton, WalletsActionScreen, WalletText } from '../../../../../components';
+import MT5PasswordUpdatedIcon from '../../../../../public/images/ic-mt5-password-updated.svg';
+
+type TProps = {
+    setNextScreen?: VoidFunction;
+};
+
+const MT5ChangeInvestorPasswordSavedScreen: React.FC<TProps> = ({ setNextScreen }) => {
+    return (
+        <WalletsActionScreen
+            description={
+                <WalletText align='center' size='sm'>
+                    Your investor password has been changed.
+                </WalletText>
+            }
+            descriptionSize='sm'
+            icon={<MT5PasswordUpdatedIcon />}
+            renderButtons={() => <WalletButton onClick={setNextScreen} size='lg' text='Okay' />}
+            title='Password saved'
+        />
+    );
+};
+
+export default MT5ChangeInvestorPasswordSavedScreen;

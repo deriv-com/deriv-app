c91038eff80f5c34aaade703e20a8c5f3b8e3743
Farabi Chowdhury <farabi@deriv.com>
2023-12-01 17:54:44 +0800

    chore: added new tests based on conditional rendering

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/__tests__/stop-bot- modal-content.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/__tests__/stop-bot- modal-content.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mockStore, StoreProvider } from '@deriv/stores';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { mock_ws } from 'Utils/mock';
 import RootStore from 'Stores/index';
 import { DBotStoreProvider, mockDBotStore } from 'Stores/useDBotStore';
@@ -15,10 +15,10 @@ jest.mock('@deriv/bot-skeleton/src/scratch/dbot', () => ({
 jest.mock('@deriv/bot-skeleton/src/scratch/hooks/block_svg', () => jest.fn());
 
 const mocked_props = {
-    is_running: false,
+    is_running: true,
     is_dialog_open: false,
     is_contract_dialog_open: false,
-    is_stop_bot_dialog_open: false,
+    is_stop_bot_dialog_open: true,
     is_multiplier: false,
     closeMultiplierContract: jest.fn(),
     onOkButtonClick: jest.fn(),
@@ -26,9 +26,18 @@ const mocked_props = {
 };
 
 describe('StopBotModalContent', () => {
-    let wrapper: ({ children }: { children: JSX.Element }) => JSX.Element, mock_DBot_store: RootStore | undefined;
+    let wrapper: ({ children }: { children: JSX.Element }) => JSX.Element,
+        mock_DBot_store: RootStore | undefined,
+        modal_root_el: HTMLElement;
 
     beforeAll(() => {
+        modal_root_el = document.createElement('div');
+        modal_root_el.setAttribute('id', 'modal_root');
+        document.body.appendChild(modal_root_el);
+        mock_DBot_store?.quick_strategy?.toggleStopBotDialog();
+    });
+
+    beforeEach(() => {
         const mock_store = mockStore({});
         mock_DBot_store = mockDBotStore(mock_store, mock_ws);
 
@@ -46,5 +55,19 @@ describe('StopBotModalContent', () => {
             wrapper,
         });
         expect(container).toBeInTheDocument();
+    });
+    it('should render the StopBotModalContent component with close contract message', () => {
+        const close_contract = /Close your contract now or keep it running./i;
+        render(<StopBotModalContent {...mocked_props} is_contract_dialog_open={true} is_multiplier={true} />, {
+            wrapper,
+        });
+        expect(screen.getByText(close_contract)).toBeInTheDocument();
+    });
+    it('should render the StopBotModalContent component with stop bot message', () => {
+        const stop_bot = 'Stopping the current bot will load the Quick Strategy you just created to the workspace.';
+        render(<StopBotModalContent {...mocked_props} />, {
+            wrapper,
+        });
+        expect(screen.getByText(stop_bot)).toBeInTheDocument();
     });
 });

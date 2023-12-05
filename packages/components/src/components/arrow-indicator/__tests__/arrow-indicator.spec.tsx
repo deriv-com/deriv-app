bee36d18a36e2bcd4f2d72b5b79b21ec209f8d60
maryia-deriv <maryia@binary.com>
2023-12-05 12:54:12 +0300

    feat: add logic for disappearance after 3 seconds to ArrowIndicator

--- packages/components/src/components/arrow-indicator/__tests__/arrow-indicator.spec.tsx
+++ packages/components/src/components/arrow-indicator/__tests__/arrow-indicator.spec.tsx
@@ -1,29 +1,47 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import ArrowIndicator from '../arrow-indicator';
 import Icon from '../../icon/icon';
 
 jest.mock('../../icon/icon', () => jest.fn((props: React.ComponentProps<typeof Icon>) => <div>{props.icon}</div>));
 
 describe('ArrowIndicator', () => {
-    it('should not render if has_increased is undefined', () => {
+    it('should not render if value is undefined', () => {
         render(<ArrowIndicator />);
         expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
         expect(screen.queryByText('IcLoss')).not.toBeInTheDocument();
     });
-    it('should not render if has_increased is null', () => {
-        render(<ArrowIndicator has_increased={null} />);
+    it('should not render if value is null', () => {
+        render(<ArrowIndicator />);
         expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
         expect(screen.queryByText('IcLoss')).not.toBeInTheDocument();
     });
-    it('should render IcProfit if has_increased is true', () => {
-        render(<ArrowIndicator has_increased />);
+    it('should render IcProfit if value has increased', () => {
+        const { rerender } = render(<ArrowIndicator value='123.94' />);
+        expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
+        expect(screen.queryByText('IcLoss')).not.toBeInTheDocument();
+        rerender(<ArrowIndicator value='123.95' />);
         expect(screen.getByText('IcProfit')).toBeInTheDocument();
         expect(screen.queryByText('IcLoss')).not.toBeInTheDocument();
     });
-    it('should render IcLoss if has_increased is false', () => {
-        render(<ArrowIndicator has_increased={false} />);
+    it('should render IcLoss if value has decreased', () => {
+        const { rerender } = render(<ArrowIndicator value='123.94' />);
+        expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
+        expect(screen.queryByText('IcLoss')).not.toBeInTheDocument();
+        rerender(<ArrowIndicator value='123.93' />);
         expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
         expect(screen.getByText('IcLoss')).toBeInTheDocument();
+    });
+    it('should appear when the value changes and disappear after 3 seconds if not changed again', () => {
+        jest.useFakeTimers();
+        const { rerender } = render(<ArrowIndicator value='123.94' />);
+        expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
+        rerender(<ArrowIndicator value='123.95' />);
+        expect(screen.getByText('IcProfit')).toBeInTheDocument();
+        act(() => {
+            jest.advanceTimersByTime(3000);
+        });
+        expect(screen.queryByText('IcProfit')).not.toBeInTheDocument();
+        jest.useRealTimers();
     });
 });
--- packages/components/src/components/arrow-indicator/arrow-indicator.tsx
+++ packages/components/src/components/arrow-indicator/arrow-indicator.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Icon from '../icon/icon';
+import { usePrevious } from '../../hooks';
 
 type TArrowIndicatorProps = {
     classname?: string;
-    has_increased?: boolean | null;
+    value?: number | string;
 };
 
-const ArrowIndicator = ({ classname, has_increased }: TArrowIndicatorProps) => {
-    if (typeof has_increased !== 'boolean') return null;
+const ArrowIndicator = ({ classname, value }: TArrowIndicatorProps) => {
+    const [is_hidden, setHidden] = React.useState(true);
+    React.useEffect(() => {
+        setHidden(false);
+        const timeout_id = setTimeout(() => {
+            setHidden(true);
+        }, 3000);
+        return () => clearTimeout(timeout_id);
+    }, [value]);
+    const current_value = Number(value);
+    const previous_value = Number(usePrevious(value));
+    const has_defined_values = !isNaN(previous_value) && !isNaN(current_value);
+    const has_increased = has_defined_values && previous_value < current_value;
+    const is_unchanged = previous_value === current_value;
+    if (!has_defined_values || is_hidden || is_unchanged) return null;
     return <div className={classname}>{has_increased ? <Icon icon='IcProfit' /> : <Icon icon='IcLoss' />}</div>;
 };
 
-export default ArrowIndicator;
+export default React.memo(ArrowIndicator);

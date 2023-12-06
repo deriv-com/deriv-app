ba152ef79067e36e131cbdd3be8a65ebdf757c99
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-12-06 12:41:36 +0300

    fix: test cases of bot-builder

--- packages/bot-web-ui/src/components/dashboard/bot-builder/__tests__/bot-builder.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/bot-builder/__tests__/bot-builder.spec.tsx
@@ -1,49 +1,27 @@
 import React from 'react';
+import { unmountComponentAtNode } from 'react-dom';
+import { mockStore, StoreProvider } from '@deriv/stores';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { render, screen } from '@testing-library/react';
-import BotBuilder from '..';
-import { useDBotStore } from 'Stores/useDBotStore';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { mock_ws } from 'Utils/mock';
+import RootStore from 'Stores/root-store';
+import { DBotStoreProvider, mockDBotStore } from 'Stores/useDBotStore';
+import BotBuilder from '../bot-builder';
 
-jest.mock('@deriv/bot-skeleton/src/scratch/blockly', () => jest.fn());
-
-jest.mock('@deriv/bot-skeleton/src/scratch/dbot', () => ({
-    saveRecentWorkspace: jest.fn(),
-    unHighlightAllBlocks: jest.fn(),
-    terminateBot: jest.fn(),
-    terminateConnection: jest.fn(),
-    initWorkspace: jest.fn().mockResolvedValue({ key: 0 }),
-}));
-
-jest.mock('@deriv/bot-skeleton/src/scratch/hooks/block_svg', () => jest.fn());
-jest.mock('../../dbot-tours/bot-builder-tour', () => jest.fn(() => <div>BotBuilderTourHandler</div>));
-jest.mock('../../../quick-strategy', () => jest.fn(() => <div>QuickStrategy1</div>));
+jest.mock('@deriv/bot-skeleton/src/scratch/dbot', () => jest.fn());
 jest.mock('../workspace-wrapper', () => jest.fn(() => <div>WorkspaceWrapper</div>));
-jest.mock('../../dashboard-component/load-bot-preview/save-modal', () => jest.fn(() => <div>SaveModal</div>));
-jest.mock('../../../load-modal', () => jest.fn(() => <div>LoadModal</div>));
-jest.mock('../../../bot-snackbar', () =>
-    jest.fn(({ handleClose }) => <div onClick={() => handleClose()}>BotSnackbar</div>)
-);
+jest.mock('../../../quick-strategy', () => jest.fn(() => <div>QuickStrategy</div>));
+jest.mock('../../dbot-tours/bot-builder-tour', () => jest.fn(() => <div>BotBuilderTourHandler</div>));
 
-const mockDBot = {
-    dashboard: {
-        is_preview_on_popup: false,
-        active_tab: 0,
-        active_tour: false,
+jest.mock('@deriv/bot-skeleton', () => ({
+    ...jest.requireActual('@deriv/bot-skeleton'),
+    onWorkspaceResize: jest.fn(),
+    DBot: {
+        initWorkspace: jest.fn(() => Promise.resolve()),
+        terminateBot: jest.fn(),
+        terminateConnection: jest.fn(),
     },
-    quick_strategy: {
-        is_open: true,
-    },
-    run_panel: {
-        is_running: true,
-    },
-    app: {
-        onMount: jest.fn(),
-        onUnmount: jest.fn(),
-    },
-};
-
-jest.mock('../../../../stores/useDBotStore', () => ({
-    useDBotStore: jest.fn(() => mockDBot),
 }));
 
 const mockTextToDom = jest.fn(() => {
@@ -51,7 +29,9 @@ const mockTextToDom = jest.fn(() => {
     return xml_element;
 });
 
-window.Blockly = {
+const mockRemoveChangeListener = jest.fn();
+
+const getMockBlockly = (type: string) => ({
     Xml: {
         textToDom: mockTextToDom,
     },
@@ -59,73 +39,89 @@ window.Blockly = {
         options: {},
         getGesture: jest.fn(),
         getVariableMap: jest.fn(),
-        getAllFields: jest.fn(() => []),
-        removeChangeListener: jest.fn(),
+        removeChangeListener: mockRemoveChangeListener,
         dispose: jest.fn(),
-        addChangeListener: jest.fn(),
+        addChangeListener: jest.fn(cb => cb({ type })),
+        getAllBlocks: jest.fn(() => []),
     },
     VerticalFlyout: jest.fn(() => ({
         workspace_: {
             createPotentialVariableMap: jest.fn(),
         },
     })),
-    svgResize: () => ({}),
-};
-
-Object.defineProperty(window, 'performance', {
-    value: {
-        clearMeasures: jest.fn(),
-    },
 });
 
-describe('BotBuilder component', () => {
-    it('should render BotBuilder', () => {
-        (useDBotStore as jest.Mock).mockReturnValueOnce({
-            ...mockDBot,
-            dashboard: {
-                is_preview_on_popup: true,
+describe('BotBuilder', () => {
+    let wrapper: ({ children }: { children: JSX.Element }) => JSX.Element, mock_DBot_store: RootStore | undefined;
+    beforeEach(() => {
+        const mock_store = mockStore({
+            ui: {
+                setAccountSwitcherDisabledMessage: jest.fn(),
             },
         });
-
-        render(<BotBuilder />);
-
-        expect(screen.getByTestId('bot-builder-container')).toBeInTheDocument();
+        mock_DBot_store = mockDBotStore(mock_store, mock_ws);
+        wrapper = ({ children }: { children: JSX.Element }) => (
+            <StoreProvider store={mock_store}>
+                <DBotStoreProvider ws={mock_ws} mock={mock_DBot_store}>
+                    {children}
+                </DBotStoreProvider>
+            </StoreProvider>
+        );
     });
-
-    it('should render BotBuilder with BotBuilderTourHandler when active_tab equals 1', () => {
-        (useDBotStore as jest.Mock).mockReturnValueOnce({
-            ...mockDBot,
-            dashboard: {
-                active_tab: 1,
-            },
-        });
-
-        render(<BotBuilder />);
-
+    it('should render BotBuilderTourHandler', () => {
+        window.Blockly = getMockBlockly('not-ui');
+        mock_DBot_store?.dashboard?.setActiveTab(1);
+        render(<BotBuilder />, { wrapper });
         expect(screen.getByText('BotBuilderTourHandler')).toBeInTheDocument();
     });
 
-    it('should render BotBuilder with QuickStrategy1 when is_open equals true', () => {
-        (useDBotStore as jest.Mock).mockReturnValueOnce({
-            ...mockDBot,
-            quick_strategy: {
-                is_open: true,
-            },
-        });
-
-        render(<BotBuilder />);
-
-        expect(screen.getByText('QuickStrategy1')).toBeInTheDocument();
+    it('should render QuickStrategy', () => {
+        window.Blockly = getMockBlockly('not-ui');
+        mock_DBot_store?.quick_strategy?.setFormVisibility(true);
+        render(<BotBuilder />, { wrapper });
+        expect(screen.getByText('QuickStrategy')).toBeInTheDocument();
     });
 
-    it('should not render BotBuilder with WorkspaceWrapper when is_preview_on_popup true', async () => {
-        (useDBotStore as jest.Mock).mockReturnValueOnce({
-            ...mockDBot,
-            dashboard: {
-                is_preview_on_popup: true,
-            },
-        });
-        render(<BotBuilder />);
-        expect(screen.queryByTestId('preview-container')).not.toBeInTheDocument();
+    it('should render BotBuilder with worksapce wrapper', () => {
+        window.Blockly = getMockBlockly('not-ui');
+        render(<BotBuilder />, { wrapper });
+        expect(screen.getByText('WorkspaceWrapper')).toBeInTheDocument();
+    });
+
+    it('should show warning on change of block if bot is running', () => {
+        window.Blockly = getMockBlockly('not-ui');
+        mock_DBot_store?.run_panel.setIsRunning(true);
+        mock_DBot_store?.toolbar.setResetButtonState(false);
+        render(<BotBuilder />, { wrapper });
+        expect(screen.getByText(/Changes you make will not affect your running bot./i)).toBeInTheDocument();
+    });
+
+    it('should not show warning on change of block if bot is running but reset is clicked', () => {
+        window.Blockly = getMockBlockly('not-ui');
+        mock_DBot_store?.run_panel.setIsRunning(true);
+        mock_DBot_store?.toolbar.setResetButtonState(true);
+        render(<BotBuilder />, { wrapper });
+        const el = screen.queryByText(/Changes you make will not affect your running bot./i);
+        expect(el).not.toBeInTheDocument();
+    });
+
+    it('should remove listeners on component unmount', async () => {
+        window.Blockly = getMockBlockly('ui');
+        mock_DBot_store?.run_panel.setIsRunning(true);
+        mock_DBot_store?.toolbar.setResetButtonState(false);
+        const { container } = render(<BotBuilder />, { wrapper });
+        unmountComponentAtNode(container);
+        await waitFor(() => expect(mockRemoveChangeListener).toHaveBeenCalledTimes(1));
+    });
+
+    it('should close the notification on click of notification close button', async () => {
+        window.Blockly = getMockBlockly('not-ui');
+        mock_DBot_store?.run_panel.setIsRunning(true);
+        mock_DBot_store?.toolbar.setResetButtonState(false);
+        render(<BotBuilder />, { wrapper });
+        const notification_close_button = screen.getByTestId('bot-snackbar-notification-close');
+        userEvent.click(notification_close_button);
+        const el = screen.queryByText(/Changes you make will not affect your running bot./i);
+        await waitFor(() => expect(el).not.toBeInTheDocument());
     });
 });

7a852db07f12e1c8edbcad2c355618c6d5967505
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-12-06 14:07:48 +0800

    refactor: broken return statement into smaller chunks of variables

--- packages/cfd/src/features/hooks/useIsEuRegion.ts
+++ packages/cfd/src/features/hooks/useIsEuRegion.ts
@@ -7,22 +7,25 @@ const useIsEuRegion = () => {
     const isEuRegion = useMemo(() => {
         if (!landing_company) return false;
 
-        const euShortcodeRegex = /^(maltainvest|malta|iom)$/;
-        const euExcludedRegex = /^mt$/;
+        const eu_shortcode_regex = /^(maltainvest|malta|iom)$/;
+        const eu_excluded_regex = /^mt$/;
 
         const { gaming_company, financial_company, mt_gaming_company, mt_all_company, id: residence } = landing_company;
-        const financialShortcode = financial_company?.shortcode;
-        const gamingShortcode = gaming_company?.shortcode;
-        const mtGamingShortcode = mt_gaming_company?.financial?.shortcode || mt_all_company?.swap_free?.shortcode;
-        const isCurrentMf = financial_company?.shortcode === 'maltainvest';
+        const financial_shortcode = financial_company?.shortcode;
+        const gaming_shortcode = gaming_company?.shortcode;
+        const mt_gaming_shortcode = mt_gaming_company?.financial?.shortcode || mt_all_company?.swap_free?.shortcode;
 
-        return (
-            isCurrentMf ||
-            (financialShortcode || gamingShortcode || mtGamingShortcode
-                ? (euShortcodeRegex.test(financialShortcode as unknown as string) && gamingShortcode !== 'svg') ||
-                  euShortcodeRegex.test(gamingShortcode as string)
-                : euExcludedRegex.test(residence as string))
-        );
+        const is_current_mf = financial_shortcode === 'maltainvest';
+        const shortcodes = financial_shortcode || gaming_shortcode || mt_gaming_shortcode;
+        const is_financial_eu_and_not_gaming_svg =
+            eu_shortcode_regex.test(financial_shortcode as unknown as string) && gaming_shortcode !== 'svg';
+        const is_gaming_eu = eu_shortcode_regex.test(gaming_shortcode as string);
+        const is_residence_eu = eu_excluded_regex.test(residence as string);
+
+        const is_eu_based_on_shortcodes = shortcodes && (is_financial_eu_and_not_gaming_svg || is_gaming_eu);
+        const is_eu_based_on_residence = !shortcodes && is_residence_eu;
+
+        return is_current_mf || is_eu_based_on_shortcodes || is_eu_based_on_residence;
     }, [landing_company]);
 
     return {

bafee96416ffbaae43ce8a0e153589d61786ec3a
maryia-deriv <maryia@binary.com>
2023-12-08 18:15:20 +0300

    chore: revert import

--- packages/reports/src/Containers/reports.tsx
+++ packages/reports/src/Containers/reports.tsx
@@ -13,7 +13,6 @@ import {
 import { getSelectedRoute } from '@deriv/shared';
 import { localize } from '@deriv/translations';
 import { observer, useStore } from '@deriv/stores';
-import { useFeatureFlags } from '@deriv/hooks';
 import { Analytics } from '@deriv/analytics';
 import { TRoute } from 'Types';
 import 'Sass/app/modules/reports.scss';

0117e2168afc30a03d161e52137b81e1ff6d0ce1
henry-deriv <118344354+henry-deriv@users.noreply.github.com>
2023-12-08 10:23:48 +0800

    [DTRA] henry/webrel-26/test-coverage-trading-helper-files (#11733)
    * fix: add trade-store props to mockStore file
    
    * fix: test cases
    
    * fix: failing test cases and add more tests
    
    * fix: remove commmented out imports
    
    * fix: remove comment
    
    * fix: change mocked value same as trade_store
    
    * fix: add more tests cases according to comments
    
    * fix: failin test
    
    * fix: failing test
    
    * fix: resolve comments
    
    * fix: resolve comments
    
    * fix: failing test

--- packages/trader/src/Stores/Modules/Trading/Helpers/__tests__/proposal.spec.ts
+++ packages/trader/src/Stores/Modules/Trading/Helpers/__tests__/proposal.spec.ts
@@ -1,8 +1,10 @@
+import { mockStore } from '@deriv/stores';
 import { getProposalInfo, createProposalRequests } from '../proposal';
 
 describe('Proposal', () => {
     describe('getProposalInfo function', () => {
-        const fake_store = {
+        let fake_store = mockStore({}).modules.trade;
+        fake_store = {
             currency: 'EUR',
             basis_list: [
                 { text: 'Payout', value: 'payout' },
@@ -13,11 +15,13 @@ describe('Proposal', () => {
 
         const fake_obj_prev_contract_basis = {
             text: 'payout',
-            value: 1234,
+            value: '1234',
         };
 
         it('should return 0 as profit when proposal has an error', () => {
             const fake_response = {
+                echo_req: { test: 'test' },
+                msg_type: 'proposal' as const,
                 error: { message: 'This is error' },
             };
 
@@ -47,13 +51,18 @@ describe('Proposal', () => {
 
         it('should return profit and return calculated if proposal has no error', () => {
             const fake_response = {
+                echo_req: { test: 'test' },
+                msg_type: 'proposal' as const,
                 proposal: {
                     proposal: 1,
                     ask_price: 50,
-                    display_value: 200,
+                    display_value: '200',
                     payout: 300,
                     id: 'id1',
                     longcode: 'This is a longcode',
+                    date_start: 12312313,
+                    spot: 200,
+                    spot_time: 123123,
                 },
             };
 
@@ -62,27 +71,32 @@ describe('Proposal', () => {
                 commission: undefined,
                 error_code: undefined,
                 error_field: undefined,
+                growth_rate: undefined,
                 limit_order: undefined,
                 profit: '250.00',
                 returns: '500.00%',
-                stake: 200,
+                stake: '200',
+                spot: 200,
+                spot_time: 123123,
                 payout: 300,
                 id: 'id1',
                 message: 'This is a longcode',
                 has_error: false,
                 has_error_details: false,
-                has_increased: false,
+                has_increased: true,
                 obj_contract_basis: {
                     text: 'Stake',
-                    value: 200,
+                    value: '200',
                 },
             });
         });
     });
 
     describe('createProposalRequests function', () => {
+        let fake_store = mockStore({}).modules.trade;
+
         it('should return the request containing trade type which is not already in the request', () => {
-            const fake_store = {
+            fake_store = {
                 amount: '10',
                 basis: 'payout',
                 currency: 'USD',
@@ -137,7 +151,7 @@ describe('Proposal', () => {
         });
 
         it('should return the request as before if all trade types already exist in the request', () => {
-            const fake_store = {
+            fake_store = {
                 amount: '10',
                 basis: 'payout',
                 currency: 'USD',
@@ -181,7 +195,7 @@ describe('Proposal', () => {
         });
 
         it('should return an empty object if there is no trade type', () => {
-            const fake_store = {
+            fake_store = {
                 trade_types: {},
                 proposal_requests: {},
             };

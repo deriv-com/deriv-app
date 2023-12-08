d31aa0e11f266a5f7f93cc04a1e7874338f6331a
maryia-deriv <maryia@binary.com>
2023-12-08 11:57:27 +0300

    refactor: remove unused condition

--- packages/components/src/components/arrow-indicator/arrow-indicator.tsx
+++ packages/components/src/components/arrow-indicator/arrow-indicator.tsx
@@ -22,26 +22,24 @@ const ArrowIndicator = ({ className, value }: TArrowIndicatorProps) => {
     const { icon, previous_icon, previous_value } = data;
     const has_comparable_values = !isNaN(Number(data.value)) && !isNaN(Number(value));
     const timeout_id = React.useRef<ReturnType<typeof setTimeout>>();
+
     React.useEffect(() => {
         setIsVisible(true);
+        setData(prev_data => {
+            const has_increased = Number(prev_data.value) < Number(value);
+            const icon_name = has_increased ? 'IcProfit' : 'IcLoss';
+            return {
+                icon: has_comparable_values ? icon_name : '',
+                previous_icon: prev_data.icon,
+                previous_value: prev_data.value,
+                value,
+            };
+        });
 
-        if (data.value !== Number(value)) {
-            setData(prev_data => {
-                const has_increased = Number(prev_data.value) < Number(value);
-                const icon_name = has_increased ? 'IcProfit' : 'IcLoss';
-                return {
-                    icon: has_comparable_values ? icon_name : '',
-                    previous_icon: prev_data.icon,
-                    previous_value: prev_data.value,
-                    value,
-                };
-            });
-
-            clearTimeout(timeout_id.current);
-            timeout_id.current = setTimeout(() => {
-                setIsVisible(false);
-            }, 3000);
-        }
+        clearTimeout(timeout_id.current);
+        timeout_id.current = setTimeout(() => {
+            setIsVisible(false);
+        }, 3000);
         return () => clearTimeout(timeout_id.current);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value]);

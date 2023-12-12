b35b1f76d34a1d6e0f9a50c4f8e74f63c0512080
Sergei Baranovski <120570511+sergei-deriv@users.noreply.github.com>
2023-12-12 10:20:14 +0300

    [WALL] Sergei / trying to fix package-lock and get back Step types (#11312)
    * Revert "feat: update package-lock.json"
    
    This reverts commit dba7836d023a59236fb1c89b1158e75d63d98e27.
    
    * feat: trying to fix package-lock.json
    
    * feat: add Step type instead of any

--- packages/wallets/src/components/WalletTourGuide/DesktopSteps.tsx
+++ packages/wallets/src/components/WalletTourGuide/DesktopSteps.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Step } from '@deriv/react-joyride';
 import { WalletText } from '../Base';
 import './WalletTourGuide.scss';
 
@@ -8,8 +9,7 @@ const getDesktopSteps = (
     hasDerivAppsTradingAccount: boolean,
     isAllWalletsAlreadyAdded: boolean,
     walletNumber = 1
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): any[] => [
+): Step[] => [
     // Wallet header
     {
         content: (

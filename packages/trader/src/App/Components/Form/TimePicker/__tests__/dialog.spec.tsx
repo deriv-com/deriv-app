b7e60b6aa3a8927a43373c5da9cb34510eb1af96
Henry Hein <henry@regentmarkets.com>
2023-12-12 15:33:39 +0800

    fix: change to pascal case

--- packages/trader/src/App/Components/Form/TimePicker/__tests__/dialog.spec.tsx
+++ packages/trader/src/App/Components/Form/TimePicker/__tests__/dialog.spec.tsx
@@ -16,22 +16,22 @@ describe('<Dialog />', () => {
 
     it('Should render minute and hours dialogs', () => {
         render(<Dialog {...default_props} />);
-        const hourElement = screen.getByText('23');
-        const minuteElement = screen.getByText('45');
+        const hour_element = screen.getByText('23');
+        const minute_element = screen.getByText('45');
 
         expect(screen.getByText(/hour/i)).toBeInTheDocument();
         expect(screen.getByText(/minute/i)).toBeInTheDocument();
-        expect(hourElement).toBeInTheDocument();
-        expect(minuteElement).toBeInTheDocument();
-        expect(minuteElement).toHaveClass('testPreClass__selector-list-item--selected');
-        expect(hourElement).toHaveClass('testPreClass__selector-list-item--selected');
+        expect(hour_element).toBeInTheDocument();
+        expect(minute_element).toBeInTheDocument();
+        expect(minute_element).toHaveClass('testPreClass__selector-list-item--selected');
+        expect(hour_element).toHaveClass('testPreClass__selector-list-item--selected');
     });
     it('Selecting disabled hour and minute does not call onChange function', () => {
         render(<Dialog {...default_props} />);
-        const disabledHourElement = screen.getByText('12');
-        const disabledMinuteElement = screen.getByText('45');
-        userEvent.click(disabledHourElement);
-        userEvent.click(disabledMinuteElement);
+        const disabled_hour_element = screen.getByText('12');
+        const disabled_minute_element = screen.getByText('45');
+        userEvent.click(disabled_hour_element);
+        userEvent.click(disabled_minute_element);
 
         expect(default_props.onChange).not.toHaveBeenCalled();
     });

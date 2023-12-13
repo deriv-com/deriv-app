c851087b931dd3c00237ea4822e3a3a5893755ce
Jim Daniels Wasswa <jim@deriv.com>
2023-12-13 14:48:07 +0800

    chore: transform other fields based on code review comments

--- packages/api/src/hooks/p2p/useAdvertInfo.ts
+++ packages/api/src/hooks/p2p/useAdvertInfo.ts
@@ -5,10 +5,13 @@ import useQuery from '../../useQuery';
  * This custom hook returns the advert info for a specific advert by calling 'p2p_advert_info' endpoint
  */
 const useAdvertInfo = (
-    id?: string,
+    payload: NonNullable<Parameters<typeof useQuery<'p2p_advert_info'>>[1]>['payload'],
     options?: NonNullable<Parameters<typeof useQuery<'p2p_advert_info'>>[1]>['options']
 ) => {
-    const { data, ...rest } = useQuery('p2p_advert_info', { payload: { id }, options });
+    const { data, ...rest } = useQuery('p2p_advert_info', {
+        payload: { id: payload?.id, use_client_limits: payload?.use_client_limits },
+        options,
+    });
 
     const modified_data = useMemo(() => {
         const p2p_advert_info = data?.p2p_advert_info;
@@ -21,6 +24,15 @@ const useAdvertInfo = (
             is_buy: p2p_advert_info?.type === 'buy',
             /** Determines whether the advert is a sell advert or not. */
             is_sell: p2p_advert_info?.type === 'sell',
+            is_block_trade: Boolean(p2p_advert_info?.block_trade),
+            is_deleted: Boolean(p2p_advert_info?.deleted),
+            is_active: Boolean(p2p_advert_info?.is_active),
+            is_visible: Boolean(p2p_advert_info?.is_visible),
+            /**
+             * @deprecated This property was deprecated on back-end
+             * @see https://api.deriv.com/api-explorer#p2p_advert_info
+             * **/
+            payment_method: p2p_advert_info?.payment_method,
         };
     }, [data?.p2p_advert_info]);
 

96072f970a5249e4a2cc6eba2092d67a7c85c8e5
adrienne-rio <adrienne@deriv.com>
2023-12-13 15:35:47 +0800

    chore: refactored code based on reviews

--- packages/api/src/hooks/p2p/useAdvertDelete.ts
+++ packages/api/src/hooks/p2p/useAdvertDelete.ts
@@ -38,18 +38,20 @@ const useAdvertDelete = () => {
     );
 
     const modified_data = useMemo(() => {
-        if (!data?.p2p_advert_update) return undefined;
+        const p2p_advert_update = data?.p2p_advert_update;
+
+        if (!p2p_advert_update) return undefined;
 
         return {
-            ...data?.p2p_advert_update,
+            ...p2p_advert_update,
             /** Indicates if this is block trade advert or not. */
-            is_block_trade: Boolean(data?.p2p_advert_update?.block_trade),
+            is_block_trade: Boolean(p2p_advert_update?.block_trade),
             /** The activation status of the advert. */
-            is_active: Boolean(data?.p2p_advert_update?.is_active),
+            is_active: Boolean(p2p_advert_update?.is_active),
             /** Indicates that this advert will appear on the main advert list. */
-            is_visible: Boolean(data?.p2p_advert_update?.is_visible),
+            is_visible: Boolean(p2p_advert_update?.is_visible),
             /** Indicates that the advert has been deleted. */
-            is_deleted: Boolean(data?.p2p_advert_update?.deleted),
+            is_deleted: Boolean(p2p_advert_update?.deleted),
         };
     }, [data?.p2p_advert_update]);
 

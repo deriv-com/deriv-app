3b7d907e40d30b6b240d6a7f00a551a2ead89fac
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-12-13 13:13:34 +0800

    build: add stylelint conf

--- packages/p2p-v2/.stylelintrc.js
+++ packages/p2p-v2/.stylelintrc.js
@@ -5,10 +5,10 @@ module.exports = {
         'declaration-block-no-redundant-longhand-properties': true,
         'declaration-no-important': true,
         'selector-class-pattern': [
-            // selectors must be prefixed with "wallets-" to avoid name conflicts in other packages
-            '^wallets-[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+            // selectors must be prefixed with "p2p-v2-" to avoid name conflicts in other packages
+            '^p2p-v2-[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
             {
-                message: 'Expected selector to match BEM CSS pattern and to be prefixed with "wallets-"',
+                message: 'Expected selector to match BEM CSS pattern and to be prefixed with "p2p-v2-"',
             },
         ],
     },

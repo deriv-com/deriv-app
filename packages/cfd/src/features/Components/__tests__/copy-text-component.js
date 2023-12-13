53225141618fdd38426ec899038ad88d24833405
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-12-13 11:58:17 +0800

    refactor: naming convention of data-testid

--- packages/cfd/src/features/Components/__tests__/copy-text-component.js
+++ packages/cfd/src/features/Components/__tests__/copy-text-component.js
@@ -5,7 +5,7 @@ import { CopyTextComponent } from '../copy-text-component';
 describe('<CopyTextComponent />', () => {
     it('component should be rendered', () => {
         const { getByTestId } = render(<CopyTextComponent />);
-        const mainDiv = getByTestId('copy_text_component_main_div');
+        const mainDiv = getByTestId('dt_copy_text_component_main_div');
 
         expect(mainDiv).toBeInTheDocument();
     });

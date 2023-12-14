4925d7d97bac98b94ded6e5bdb44b62da0d21071
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-12-14 14:11:23 +0800

    chore: add test for fetchnextpage

--- packages/api/src/hooks/p2p/__tests__/useAdvertiserAdverts.spec.tsx
+++ packages/api/src/hooks/p2p/__tests__/useAdvertiserAdverts.spec.tsx
@@ -8,6 +8,8 @@ jest.mock('../../../useInfiniteQuery', () => jest.fn());
 
 const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
 
+const mockUseInfiniteQuery = useInfiniteQuery as jest.MockedFunction<typeof useInfiniteQuery<'p2p_advertiser_adverts'>>;
+
 describe('useAdvertiserAdverts', () => {
     it('should return undefined when there is no data', () => {
         // @ts-expect-error need to come up with a way to mock the return type of useInfiinteQuery
@@ -17,13 +19,13 @@ describe('useAdvertiserAdverts', () => {
     });
 
     it('should return adverts list with the correct details', () => {
-        (useInfiniteQuery as jest.MockedFunction<typeof useInfiniteQuery>).mockReturnValue({
-            // @ts-expect-error need to come up with a way to mock the return type of useQuery
+        mockUseInfiniteQuery.mockReturnValue({
             data: {
                 pages: [
                     {
                         p2p_advertiser_adverts: {
                             list: {
+                                // @ts-expect-error need to come up with a way to mock the return type of useQuery
                                 advertiser_details: {
                                     completed_orders_count: 0,
                                     id: '1',
@@ -73,5 +75,46 @@ describe('useAdvertiserAdverts', () => {
         expect(advertiser_adverts?.[0].advertiser_details?.total_completion_rate).toBe(0);
         expect(advertiser_adverts?.[0].advertiser_details?.is_recommended).toBe(false);
         expect(advertiser_adverts?.[0].advertiser_details?.has_not_been_recommended).toBe(true);
+    });
+
+    it('should call fetchNextPage when loadMoreAdverts is called', () => {
+        mockUseInfiniteQuery.mockReturnValue({
+            data: {
+                pages: [
+                    {
+                        p2p_advertiser_adverts: {
+                            list: {
+                                // @ts-expect-error need to come up with a way to mock the return type of useQuery
+                                advertiser_details: {
+                                    completed_orders_count: 0,
+                                    id: '1',
+                                    is_online: 1,
+                                    is_recommended: null,
+                                    last_online_time: 111,
+                                    loginid: '111',
+                                    name: 'test',
+                                    rating_average: 0,
+                                    rating_count: 0,
+                                    recommended_average: 0,
+                                    recommended_count: 0,
+                                    total_completion_rate: 0,
+                                },
+                                amount: 50,
+                                id: '101',
+                                price: 13500,
+                                type: 'buy',
+                                rate_type: 'float',
+                            },
+                        },
+                    },
+                ],
+            },
+            fetchNextPage: jest.fn(),
+        });
+
+        const { result } = renderHook(() => useAdvertiserAdverts(), { wrapper });
+        result.current.loadMoreAdverts();
+
+        expect(mockUseInfiniteQuery('p2p_advertiser_adverts').fetchNextPage).toBeCalled();
     });
 });

1f0242b5135eb6b0068dc55029cd2817f1835e21
Thisyahlen <thisyahlen@regentmarkets.com>
2023-12-14 10:01:53 +0800

    chore: rename rest of it

--- packages/tradershub/src/index.tsx
+++ packages/tradershub/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeLazyLoader, moduleLoader } from './utils/loader';
 
 const LazyApp = makeLazyLoader(
-    () => moduleLoader(() => import(/* webpackChunkName: "wallets-app", webpackPreload: true */ './App')),
+    () => moduleLoader(() => import(/* webpackChunkName: "tradershub", webpackPreload: true */ './App')),
     () => <div />
 )();
 

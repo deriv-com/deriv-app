175eeff36b015dc9c8f86b2db9ab4525c41f2a8b
aizad-deriv <aizad@deriv.com>
2023-12-14 13:56:34 +0800

    chore: updated styles - remove scss styles - remove index.js for scss inside of webpack - added traders hub mainpage template

--- packages/tradershub/src/AppContent.tsx
+++ packages/tradershub/src/AppContent.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { Router } from './routes';
-import './AppContent.scss';
 
-const AppContent = () => {
-    return (
-        <div className='traders-hub-app'>
-            <Router />
-        </div>
-    );
-};
+const AppContent = () => <Router />;
 
 export default AppContent;

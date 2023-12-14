2701ba7a99698151958f4e8fb83402f92ef8a863
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-12-14 12:40:26 +0800

    feat: remove p2p-v2 from cashier

--- packages/cashier/src/containers/routes/binary-routes.tsx
+++ packages/cashier/src/containers/routes/binary-routes.tsx
@@ -4,7 +4,6 @@ import { Localize, localize } from '@deriv/translations';
 import getRoutesConfig from 'Constants/routes-config';
 import RouteWithSubRoutes from './route-with-sub-routes';
 import { useFeatureFlags } from '@deriv/hooks';
-import P2P_V2 from '@deriv/p2p-v2';
 import { routes } from '@deriv/shared';
 
 type TBinaryRoutesProps = {
@@ -28,7 +27,6 @@ const BinaryRoutes = (props: TBinaryRoutesProps) => {
             const routes_replicate = routesConfig;
             routes_replicate[0].routes?.push({
                 path: routes.cashier_p2p_v2,
-                component: P2P_V2,
                 getTitle: () => localize('Deriv P2P-V2'),
                 icon_component: 'IcDp2p',
             });

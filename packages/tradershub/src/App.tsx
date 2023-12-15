c3c7d6731f7b2aeaf9d801dbf2c5e3f66f968af7
maryia-deriv <maryia@binary.com>
2023-12-15 15:55:30 +0300

    Revert "thisyahlen/fix: tradershub build (#12301)"
    This reverts commit c7ab0379dd6d26e74dd10c9e4792fbd7679e5594.

--- packages/tradershub/src/App.tsx
+++ packages/tradershub/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { APIProvider } from '@deriv/api';
 import AppContent from './AppContent';
 import './index.scss';
 
 const App: React.FC = () => {
-    return <AppContent />;
+    return (
+        <APIProvider standalone>
+            <AppContent />
+        </APIProvider>
+    );
 };
 
 export default App;
--- packages/tradershub/src/routes/index.ts
+++ packages/tradershub/src/routes/index.ts
@@ -1,2 +1,1 @@
 export { default as Router } from './Router';
-export * from './TradersHubRoute';

5462024f7b93e1e11c29bdd52e6bdb5ca5a04fad
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-12-15 16:55:03 +0400

    refactor: added useUpdateAdvertiser hook

---
+++ packages/api/src/hooks/p2p/useUpdateAdvertiser.ts
@@ -0,0 +1,63 @@
+import { useCallback, useMemo } from 'react';
+import useMutation from '../../useMutation';
+import useInvalidateQuery from '../../useInvalidateQuery';
+
+type TPayload = Parameters<ReturnType<typeof useMutation<'p2p_advertiser_update'>>['mutate']>[0];
+
+/** A custom hook that updates a P2P advert. This can only be used by an approved P2P advertiser.
+ *
+ * To update an advert, specify the payload arguments that should be updated, for instance:
+ * @example
+ *  mutate({
+        "id": 1234, // required
+        "is_active": 0 // optional
+    });
+ *
+*/
+const useUpdateAdvertiser = () => {
+    const invalidate = useInvalidateQuery();
+    const {
+        data,
+        mutate: _mutate,
+        ...rest
+    } = useMutation('p2p_advertiser_update', {
+        onSuccess: () => {
+            invalidate('p2p_advertiser_list');
+        },
+    });
+
+    const mutate = useCallback((payload: TPayload) => _mutate({ payload }), [_mutate]);
+
+    const modified_data = useMemo(() => {
+        const p2p_advertiser_update = data?.p2p_advertiser_update;
+        if (!p2p_advertiser_update) return undefined;
+
+        const { basic_verification, full_verification, is_approved, is_listed, is_online, show_name, created_time } =
+            p2p_advertiser_update;
+
+        return {
+            ...p2p_advertiser_update,
+            basic_verification: Boolean(basic_verification),
+            /** Indicating whether the advertiser's address has been verified. */
+            full_verification: Boolean(full_verification),
+            /** The approval status of the advertiser. */
+            is_approved: Boolean(is_approved),
+            /** Indicates if the advertiser's active adverts are listed. When false, adverts won't be listed regardless if they are active or not. */
+            is_listed: Boolean(is_listed),
+            /** Indicates if the advertiser is currently online. */
+            is_online: Boolean(is_online),
+            /** When true, the advertiser's real name will be displayed on to other users on adverts and orders. */
+            show_name: Boolean(show_name),
+            /** The epoch time that the client became an advertiser. */
+            created_time: created_time ? new Date(created_time) : undefined,
+        };
+    }, [data?.p2p_advertiser_update]);
+
+    return {
+        data: modified_data,
+        mutate,
+        ...rest,
+    };
+};
+
+export default useUpdateAdvertiser;

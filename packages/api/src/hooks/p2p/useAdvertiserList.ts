e1b97b1fa4d8d383a30641739a11752ca17fd2f6
shayan khaleghparast <iman@regentmarkets.com>
2023-12-15 16:49:48 +0800

    feat: added useAdvertiserList hook

---
+++ packages/api/src/hooks/p2p/useAdvertiserList.ts
@@ -0,0 +1,63 @@
+import React from 'react';
+import useInfiniteQuery from '../../useInfiniteQuery';
+import useAuthorize from '../useAuthorize';
+
+/**
+ * This custom hook returns available advertisers who has/had trade with the current advertiser.
+ */
+const useAdvertiserList = (
+    payload?: NonNullable<Parameters<typeof useInfiniteQuery<'p2p_advertiser_list'>>[1]>['payload']
+) => {
+    const { isSuccess } = useAuthorize();
+    const { data, fetchNextPage, ...rest } = useInfiniteQuery('p2p_advertiser_list', {
+        payload: { ...payload, offset: payload?.offset, limit: payload?.limit },
+        options: {
+            getNextPageParam: (lastPage, pages) => {
+                if (!lastPage?.p2p_advertiser_list?.list) return;
+
+                return pages.length;
+            },
+            enabled: isSuccess,
+        },
+    });
+
+    // Flatten the data array.
+    const flatten_data = React.useMemo(() => {
+        if (!data?.pages?.length) return;
+
+        return data?.pages?.flatMap(page => page?.p2p_advertiser_list?.list);
+    }, [data?.pages]);
+
+    // Add additional information to the 'p2p_advertiser_list' data
+    const modified_data = React.useMemo(() => {
+        if (!flatten_data?.length) return undefined;
+
+        return flatten_data.map(advertiser => ({
+            ...advertiser,
+            /** Indicating whether the advertiser's identify has been verified. */
+            basic_verification: Boolean(advertiser?.basic_verification),
+            /** Indicating whether the advertiser's address has been verified. */
+            full_verification: Boolean(advertiser?.full_verification),
+            /** The approval status of the advertiser. */
+            is_approved: Boolean(advertiser?.is_approved),
+            /** Indicates that the advertiser is blocked. */
+            is_blocked: Boolean(advertiser?.is_blocked),
+            /** Indicates that the advertiser is a favourite. */
+            is_favourite: Boolean(advertiser?.is_favourite),
+            /** Indicates if the advertiser's active adverts are listed. When false, adverts won't be listed regardless if they are active or not. */
+            is_listed: Boolean(advertiser?.is_listed),
+            /** Indicates if the advertiser is currently online. */
+            is_online: Boolean(advertiser?.is_online),
+            /** Indicates that the advertiser was recommended in the most recent review by the current user. */
+            is_recommended: Boolean(advertiser?.is_recommended),
+        }));
+    }, [flatten_data]);
+
+    return {
+        data: modified_data,
+        loadMoreAdvertisers: fetchNextPage,
+        ...rest,
+    };
+};
+
+export default useAdvertiserList;

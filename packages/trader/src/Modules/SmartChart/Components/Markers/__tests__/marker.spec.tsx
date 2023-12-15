30b974c6e70d793e5468d66dc408d2faa24db215
maryia-deriv <maryia@binary.com>
2023-12-15 15:51:52 +0300

    test: marker

--- packages/trader/src/Modules/SmartChart/Components/Markers/__tests__/marker.spec.tsx
+++ packages/trader/src/Modules/SmartChart/Components/Markers/__tests__/marker.spec.tsx
@@ -24,32 +24,37 @@ jest.mock('Modules/SmartChart', () => ({
 
 describe('ChartMarker', () => {
     const ContentComponent = (props: TMarkerContentConfig) => <div>{JSON.stringify(props)}</div>;
-    const test_marker_config_1 = {
-        ContentComponent,
-        className: 'chart-marker-line',
-        x: 1702561946,
-        y: null,
-    };
-    const test_marker_config_2 = {
-        ContentComponent,
-        x: 1702561947,
-        y: '1966.66',
-    };
-    const marker_content_props = {
-        line_style: 'solid',
-        label: 'Start Time',
-    };
-    const props: React.ComponentProps<typeof ChartMarker> = {
-        marker_config: test_marker_config_1,
-        marker_content_props,
-        is_bottom_widget_visible: false,
-    };
     it('should render without y coordinate', async () => {
-        render(<ChartMarker {...props} />);
-        expect(screen.getByText('{"line_style":"solid","label":"Start Time"}')).toBeInTheDocument();
+        render(
+            <ChartMarker
+                marker_config={{
+                    ContentComponent,
+                    className: 'chart-marker-line',
+                    x: 1702561946,
+                    y: null,
+                }}
+                marker_content_props={{
+                    line_style: 'solid',
+                    label: 'Start Time',
+                    marker_config: {},
+                }}
+            />
+        );
+        expect(screen.getByText('{"line_style":"solid","label":"Start Time","marker_config":{}}')).toBeInTheDocument();
     });
     it('should render with y coordinate', async () => {
-        render(<ChartMarker {...props} marker_config={test_marker_config_2} />);
-        expect(screen.getByText('{"line_style":"solid","label":"Start Time"}')).toBeInTheDocument();
+        render(
+            <ChartMarker
+                marker_config={{
+                    ContentComponent,
+                    x: 1702561947,
+                    y: '1966.66',
+                }}
+                marker_content_props={{
+                    className: 'chart-spot__entry',
+                }}
+            />
+        );
+        expect(screen.getByText('{"className":"chart-spot__entry"}')).toBeInTheDocument();
     });
 });

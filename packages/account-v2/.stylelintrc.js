6de5cf8275a4a06726a91a27f490f56d9cb28132
Likhith Kolayari <likhith@regentmarkets.com>
2023-12-18 17:20:30 +0400

    chore: added stylelint file

--- packages/account-v2/.stylelintrc.js
+++ packages/account-v2/.stylelintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    extends: ['../../.stylelintrc.js', 'stylelint-config-standard-scss'],
+    extends: ['../../.stylelintrc.js'],
+    rules: {
+        'declaration-block-no-duplicate-properties': true,
+        'declaration-block-no-redundant-longhand-properties': true,
+        'declaration-no-important': true,
+        'selector-class-pattern': [
+            '^[A-Za-z]+((-){0,2}(_){0,2}[A-Za-z]+)*[A-Za-z]$',
+            {
+                message: 'Expected selector to match BEM CSS pattern',
+            },
+        ],
+    },
 };

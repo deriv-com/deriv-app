451cd92d7099a9253495729f2a725d39c0705d24
Thisyahlen <thisyahlen@regentmarkets.com>
2023-12-18 15:30:00 +0800

    chore: add components to tradershub package

--- packages/tradershub/src/App.tsx
+++ packages/tradershub/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { APIProvider } from '@deriv/api';
 import AppContent from './AppContent';
+import { ModalProvider } from './components';
 import './index.scss';
 
 const App: React.FC = () => (
     <APIProvider standalone>
-        <AppContent />
+        <ModalProvider>
+            <AppContent />
+        </ModalProvider>
     </APIProvider>
 );
 

731fa4d6bcf74eb315485e38e95319fea048fb24
Farabi Chowdhury <farabi@deriv.com>
2023-12-19 10:15:58 +0800

    fix: increase coverage

--- packages/bot-web-ui/src/components/dashboard/dashboard-component/load-bot-preview/__tests__/local.spec.tsx
+++ packages/bot-web-ui/src/components/dashboard/dashboard-component/load-bot-preview/__tests__/local.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mockStore, StoreProvider } from '@deriv/stores';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import userEvent from '@testing-library/user-event';
 import { DBOT_TABS } from 'Constants/bot-contents';
@@ -9,6 +9,11 @@ import { mock_ws } from 'Utils/mock';
 import RootStore from 'Stores/index';
 import { DBotStoreProvider, mockDBotStore } from 'Stores/useDBotStore';
 import Local from '../local';
+
+window.Blockly = {
+    derivWorkspace: { asyncClear: () => ({}) },
+    Xml: { domToWorkspace: () => ({}), textToDom: () => ({}) },
+};
 
 jest.mock('@deriv/bot-skeleton/src/scratch/blockly', () => jest.fn());
 jest.mock('@deriv/bot-skeleton/src/scratch/dbot', () => ({
@@ -23,7 +28,6 @@ describe('local', () => {
 
     beforeEach(() => {
         mock_DBot_store = mockDBotStore(mock_store, mock_ws);
-
         wrapper = ({ children }: { children: JSX.Element }) => (
             <StoreProvider store={mock_store}>
                 <DBotStoreProvider ws={mock_ws} mock={mock_DBot_store}>
@@ -51,24 +55,8 @@ describe('local', () => {
     it('should render the open button to open bot builder tab', async () => {
         render(<Local />, { wrapper });
 
-        const open = screen.getByRole('button', { name: 'Open' });
-        await waitFor(() => {
-            async () => {
-                await userEvent.click(open);
-                expect(mock_DBot_store?.dashboard.setActiveTab(DBOT_TABS.BOT_BUILDER));
-                expect(mock_DBot_store?.dashboard.setPreviewOnDialog).toBeCalledWith(false);
-                // expect(mock_DBot_store?.dashboard.setPreviewOnDialog).toBeFalsy();
-                expect(mock_DBot_store?.load_modal.loadFileFromRecent).toBeCalled();
-            };
-        });
-    });
-
-    it('should close the preview dialog when clicking the close button', () => {
-        mock_store.ui.is_mobile = true;
-        render(<Local />, { wrapper });
-
-        const cancel_button = screen.getByRole('button', { name: '' });
-        userEvent.click(cancel_button);
-        expect(mock_DBot_store?.dashboard.has_mobile_preview_loaded).toBeFalsy();
+        const open_button = screen.getByRole('button', { name: 'Open' });
+        userEvent.click(open_button);
+        expect(mock_DBot_store?.dashboard.setActiveTab(DBOT_TABS.BOT_BUILDER));
     });
 });

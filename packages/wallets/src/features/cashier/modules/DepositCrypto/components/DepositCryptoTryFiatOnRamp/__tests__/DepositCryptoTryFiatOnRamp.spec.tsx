4fec1c20550b0bdb2f09eb0718100052fa212ffe
lubega-deriv <142860499+lubega-deriv@users.noreply.github.com>
2023-12-19 17:34:49 +0800

    chore: deposit crypto try fiat onramp unit test (#12359)

---
+++ packages/wallets/src/features/cashier/modules/DepositCrypto/components/DepositCryptoTryFiatOnRamp/__tests__/DepositCryptoTryFiatOnRamp.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import DepositCryptoTryFiatOnRamp from '../DepositCryptoTryFiatOnRamp';
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: jest.fn(),
+}));
+
+describe('DepositCryptoTryFiatOnRamp', () => {
+    it('should render component correctly', () => {
+        const mockPush = jest.fn();
+        (useHistory as jest.Mock).mockReturnValue({ push: mockPush });
+
+        render(<DepositCryptoTryFiatOnRamp />);
+        expect(screen.getByText(/Looking for a way to buy cryptocurrencies?/)).toBeInTheDocument();
+        expect(screen.getByText('Try Fiat onramp')).toBeInTheDocument();
+    });
+
+    it('should navigate to /wallets/cashier/on-ramp when the link is clicked', () => {
+        const mockPush = jest.fn();
+        (useHistory as jest.Mock).mockReturnValue({ push: mockPush });
+
+        render(<DepositCryptoTryFiatOnRamp />);
+
+        fireEvent.click(screen.getByText('Try Fiat onramp'));
+        expect(mockPush).toHaveBeenCalledWith('/wallets/cashier/on-ramp');
+    });
+});

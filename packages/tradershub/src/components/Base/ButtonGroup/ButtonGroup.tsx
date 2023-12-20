4c02f4ebb56aca6a5aada785385c20b31afce964
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-12-20 15:24:38 +0800

    refactor: added documentation and replace conditional classnames with qtMerge

--- packages/tradershub/src/components/Base/ButtonGroup/ButtonGroup.tsx
+++ packages/tradershub/src/components/Base/ButtonGroup/ButtonGroup.tsx
@@ -1,24 +1,21 @@
 import React, { FC, PropsWithChildren } from 'react';
-import classNames from 'classnames';
+import { qtMerge } from '@deriv/quill-design';
 
-type TButtonGroupProps = {
-    isFlex?: boolean;
-    isFullWidth?: boolean;
-    isVertical?: boolean;
-};
+type TButtonGroupProps = { className?: string };
 
-const ButtonGroup: FC<PropsWithChildren<TButtonGroupProps>> = ({ children, isFlex, isFullWidth, isVertical }) => {
-    return (
-        <div
-            className={classNames('grid gap-3 grid-cols-[minmax(0,1fr)] grid-flow-col', {
-                'flex flex-col': isVertical,
-                'grid-cols-': isFlex,
-                'w-full': isFullWidth,
-            })}
-        >
-            {children}
-        </div>
-    );
-};
+/**
+ * `ButtonGroup` is a functional component that displays its children in a flex container.
+ * It uses the `qtMerge` function from `@deriv/quill-design` to combine predefined styles with any additional styles passed in through the `className` prop.
+ *
+ * @param {PropsWithChildren<TButtonGroupProps>} props - The properties that define the `ButtonGroup` component.
+ * @param {React.ReactNode} props.children - The child elements to be displayed within the `ButtonGroup`.
+ * @param {string} [props.className] - Additional CSS classes to apply to the `ButtonGroup`.
+ *
+ * @returns {React.ReactElement} A `div` element with the `ButtonGroup`'s children and the combined styles.
+ */
+
+const ButtonGroup: FC<PropsWithChildren<TButtonGroupProps>> = ({ children, className }) => (
+    <div className={qtMerge('flex flex-col align-center gap-400 lg:flex-row ', className)}> {children}</div>
+);
 
 export default ButtonGroup;

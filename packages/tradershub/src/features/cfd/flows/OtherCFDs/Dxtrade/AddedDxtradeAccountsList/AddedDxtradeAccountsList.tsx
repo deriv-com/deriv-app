30844d621108fe8b7cf2666ee0d632263b1ef6d0
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-12-20 10:22:59 +0800

    refactor: dxtrade trading accounts card

---
+++ packages/tradershub/src/features/cfd/flows/OtherCFDs/Dxtrade/AddedDxtradeAccountsList/AddedDxtradeAccountsList.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useDxtradeAccountsList } from '@deriv/api';
+import { Button, Text } from '@deriv/quill-design';
+import { TradingAccountCard } from '../../../../../../components';
+import { getStaticUrl } from '../../../../../../helpers/urls';
+import DerivX from '../../../../../../public/images/cfd/derivx.svg';
+import { PlatformDetails } from '../../../../constants';
+
+const AddedDxtradeAccountsList: React.FC = () => {
+    const { data } = useDxtradeAccountsList();
+
+    const leadingComponent = () => (
+        <div
+            className='cursor-pointer'
+            onClick={() => {
+                window.open(getStaticUrl('/derivx'));
+            }}
+            // Fix sonarcloud issue
+            onKeyDown={event => {
+                if (event.key === 'Enter') {
+                    window.open(getStaticUrl('/derivx'));
+                }
+            }}
+        >
+            <DerivX />
+        </div>
+    );
+
+    const trailingComponent = () => (
+        <div className='flex flex-col space-y-1'>
+            <Button
+                // todo: open transfer modal
+                variant='outlined'
+            >
+                Transfer
+            </Button>
+            <Button /* todo:  show <MT5TradeModal/> */>Open</Button>
+        </div>
+    );
+
+    return (
+        <TradingAccountCard leading={leadingComponent} trailing={trailingComponent}>
+            <div className='flex flex-col fles-grow'>
+                {data?.map(account => (
+                    <React.Fragment key={account?.account_id}>
+                        <Text size='sm'>{PlatformDetails.dxtrade.title}</Text>
+                        <Text size='sm' weight='bold'>
+                            {account?.display_balance}
+                        </Text>
+                        <Text color='primary' size='xs' weight='bold'>
+                            {account?.login}
+                        </Text>
+                    </React.Fragment>
+                ))}
+            </div>
+        </TradingAccountCard>
+    );
+};
+
+export default AddedDxtradeAccountsList;
---
+++ packages/tradershub/src/features/cfd/flows/OtherCFDs/Dxtrade/AvailableDxtradeAccountsList/AvailableDxtradeAccountsList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, Text } from '@deriv/quill-design';
+import { TradingAccountCard } from '../../../../../../components';
+import { getStaticUrl } from '../../../../../../helpers/urls';
+import DerivX from '../../../../../../public/images/cfd/derivx.svg';
+
+const AvailableDxtradeAccountsList: React.FC = () => {
+    return (
+        <TradingAccountCard
+            leading={() => (
+                <div
+                    className='cursor-pointer'
+                    onClick={() => {
+                        window.open(getStaticUrl('/derivx'));
+                    }}
+                >
+                    <DerivX />
+                </div>
+            )}
+            trailing={() => <Button color='primary-light' /* todo: open <DxtradeEnterPasswordModal /> */>Get</Button>}
+        >
+            <div className='flex-grow'>
+                <p>
+                    <Text size='sm' weight='bold'>
+                        Deriv X
+                    </Text>
+                </p>
+                <Text size='xs'>This account offers CFDs on a highly customisable CFD trading platform.</Text>
+            </div>
+        </TradingAccountCard>
+    );
+};
+
+export default AvailableDxtradeAccountsList;

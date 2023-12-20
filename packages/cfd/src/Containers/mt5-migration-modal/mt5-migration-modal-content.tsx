5c7270aa4c4362931ad00e807617b76bdd786059
Taysuisin <suisin@regentmarkets.com>
2023-12-20 11:20:36 +0800

    chore: remove commented code

--- packages/cfd/src/Containers/mt5-migration-modal/mt5-migration-modal-content.tsx
+++ packages/cfd/src/Containers/mt5-migration-modal/mt5-migration-modal-content.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { Div100vhContainer } from '@deriv/components';
 import { observer, useStore } from '@deriv/stores';
 import MT5MigrationFrontSideContent from './mt5-migration-front-side-content';
-/* Temperory commented out these components because the new design is not ready yet */
-// import MT5MigrationBackSideContent from './mt5-migration-back-side-content';
-// import { useMT5MigrationModalContext } from './mt5-migration-modal-context';
 
 const MT5MigrationModalContent = observer(() => {
     const { ui } = useStore();
     const { is_mobile } = ui;
-    // const { show_modal_front_side } = useMT5MigrationModalContext();
     return (
         <Div100vhContainer
             className='mt5-migration-modal__mobile-container'
@@ -17,7 +13,6 @@ const MT5MigrationModalContent = observer(() => {
             is_bypassed={!is_mobile}
         >
             <MT5MigrationFrontSideContent />
-            {/* {show_modal_front_side ? <MT5MigrationFrontSideContent /> : <MT5MigrationBackSideContent />} */}
         </Div100vhContainer>
     );
 });

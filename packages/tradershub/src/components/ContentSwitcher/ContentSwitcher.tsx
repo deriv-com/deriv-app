cdaf0b895465ceecda9e913fe28d89913a1e2d27
aizad-deriv <aizad@deriv.com>
2023-12-20 18:53:58 +0800

    fix: updated TSDocs with examples

--- packages/tradershub/src/components/ContentSwitcher/ContentSwitcher.tsx
+++ packages/tradershub/src/components/ContentSwitcher/ContentSwitcher.tsx
@@ -50,6 +50,24 @@ export const useTabsContext = () => {
  * Component for switching between different content panels.
  * @param {TContentSwitcher} props The props for the component.
  * @returns {JSX.Element} The rendered component.
+ *
+ * @example
+ * ```tsx
+ * <ContentSwitcher>
+ *     <ContentSwitcher.HeaderList list={['Tab 1', 'Tab 2', 'Tab 3']} />
+ *     <ContentSwitcher.PanelContainer>
+ *         <ContentSwitcher.Panel>
+ *          <Heading.H3>Tab 1</Heading.H3>
+ *         </ContentSwitcher.Panel>
+ *         <ContentSwitcher.Panel>
+ *          <Heading.H3>Tab 2</Heading.H3>
+ *         </ContentSwitcher.Panel>
+ *         <ContentSwitcher.Panel>
+ *          <Heading.H3>Tab 3</Heading.H3>
+ *         </ContentSwitcher.Panel>
+ *     </ContentSwitcher.PanelContainer>
+ * </ContentSwitcher>
+ * ```
  */
 const ContentSwitcher = ({ children, className }: TContentSwitcher) => {
     const [activeTabIndex, setActiveTabIndex] = useState(0);
@@ -104,11 +122,7 @@ const ContentHeaderList: FC<TContentHeaderList> = ({ className, list }) => {
         >
             {list.map((tab, i) => (
                 <Button
-                    className={qtJoin(
-                        'rounded-200',
-                        i !== activeTabIndex &&
-                            'bg-transparent hover:bg-transparent hover:bg-transparent enabled:bg-transparent'
-                    )}
+                    className={qtJoin('rounded-200', i !== activeTabIndex && 'bg-transparent')}
                     colorStyle='white'
                     fullWidth
                     key={i}

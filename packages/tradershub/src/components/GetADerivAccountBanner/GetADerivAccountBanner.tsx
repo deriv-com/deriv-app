fc4286f998bfa8214690231d3acb78d21a00319f
Thisyahlen <thisyahlen@regentmarkets.com>
2023-12-21 14:58:56 +0800

    fix: added tsdoc

--- packages/tradershub/src/components/GetADerivAccountBanner/GetADerivAccountBanner.tsx
+++ packages/tradershub/src/components/GetADerivAccountBanner/GetADerivAccountBanner.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react';
 import { Button, Text } from '@deriv/quill-design';
 
-const GetADerivAccountBanner: FC = () => {
-    return (
-        <div className='flex items-center self-stretch justify-center p-800 gap-800 rounded-200 bg-system-light-secondary-background'>
-            <Text bold>You need a Deriv account to create a CFD account.</Text>
-            <Button className='rounded-200'>Get a Deriv account</Button>
-        </div>
-    );
-};
+/**
+ * `GetADerivAccountBanner` is a functional component that displays a banner message
+ * informing the user that they need a Deriv account to create a CFD account.
+ * It includes a button that leads the user to a modal where they can create a Deriv account.
+ *
+ * @returns {React.ReactElement} A `div` element containing the banner message and the button.
+ */
+const GetADerivAccountBanner: FC = () => (
+    <div className='flex items-center self-stretch justify-center p-800 gap-800 rounded-200 bg-system-light-secondary-background'>
+        <Text bold>You need a Deriv account to create a CFD account.</Text>
+        <Button className='rounded-200'>Get a Deriv account</Button>
+    </div>
+);
 
 export default GetADerivAccountBanner;

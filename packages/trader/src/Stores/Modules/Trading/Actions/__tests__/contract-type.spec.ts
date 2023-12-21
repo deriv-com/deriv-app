18b2ca117f5114bd5aafcd2502f74ade21cac3d8
maryia-deriv <maryia@binary.com>
2023-12-21 14:56:29 +0300

    test: init contract-type.spec

---
+++ packages/trader/src/Stores/Modules/Trading/Actions/__tests__/contract-type.spec.ts
@@ -0,0 +1,19 @@
+// import { onChangeContractTypeList } from "../contract-type";
+
+describe('onChangeContractTypeList', () => {
+    // it('should return', () => {
+    //     expect(onChangeContractTypeList()).toHaveProperty('stop_loss');
+    // });
+    // it('should return', () => {
+    //     expect(onChangeContractTypeList()).toHaveProperty('take_profit');
+    // });
+});
+
+describe('onChangeContractType', () => {
+    // it('should return', () => {
+    //     expect(onChangeContractType()).toHaveProperty('stop_loss');
+    // });
+    // it('should return', () => {
+    //     expect(onChangeContractType()).toHaveProperty('take_profit');
+    // });
+});

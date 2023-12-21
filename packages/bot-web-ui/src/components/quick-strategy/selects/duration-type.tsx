843daca1453ece8f8e74836bbb542fb2560e2048
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-12-21 10:03:57 +0300

    fix: User should not be able to delete the duration and trade type or purchase condition values

--- packages/bot-web-ui/src/components/quick-strategy/selects/duration-type.tsx
+++ packages/bot-web-ui/src/components/quick-strategy/selects/duration-type.tsx
@@ -81,6 +81,7 @@ const DurationUnit: React.FC<TDurationUnit> = ({ attached }) => {
                     return (
                         <Autocomplete
                             {...field}
+                            readOnly
                             inputMode='none'
                             data-testid='qs_autocomplete_durationtype'
                             autoComplete='off'
--- packages/bot-web-ui/src/components/quick-strategy/selects/trade-type.tsx
+++ packages/bot-web-ui/src/components/quick-strategy/selects/trade-type.tsx
@@ -83,6 +83,7 @@ const TradeTypeSelect: React.FC = () => {
                     return (
                         <Autocomplete
                             {...field}
+                            readOnly
                             inputMode='none'
                             data-testid='qs_autocomplete_tradetype'
                             autoComplete='off'

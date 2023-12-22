f340e451d7b5f2874c8e5e1060f11f33b0ad5c7c
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-12-22 12:05:04 +0800

    chore: added TSDocs

--- packages/tradershub/src/components/Base/WalletClipboard/WalletClipboard.tsx
+++ packages/tradershub/src/components/Base/WalletClipboard/WalletClipboard.tsx
@@ -5,17 +5,16 @@ import Clipboard from '../../../public/images/clipboard.svg';
 import CheckmarkCircle from '../../../public/images/ic-checkmark-circle.svg';
 import { Tooltip } from '../Tooltip';
 
-type TProps = {
-    infoMessage?: string;
-    popoverAlignment: 'bottom' | 'left' | 'right' | 'top';
-    successMessage: string;
-    textCopy: string;
-};
-
-const WalletClipboard = ({
-    //  info_message, popoverAlignment, success_message,
-    textCopy,
-}: TProps) => {
+/**
+ * Props for the WalletClipboard component.
+ * @property {string} textCopy - The text to be copied to the clipboard.
+ *
+ * @example
+ * ```jsx
+ * <WalletClipboard textCopy="Text to be copied" />
+ * ```
+ */
+const WalletClipboard = (textCopy: string) => {
     const [, copy] = useCopyToClipboard();
     const { isMobile } = useBreakpoint();
     const [isCopied, setIsCopied] = useState(false);
@@ -23,7 +22,11 @@ const WalletClipboard = ({
     const isHovered = useHover(hoverRef);
     let timeoutClipboard: ReturnType<typeof setTimeout>;
 
-    const onClick = (event: { stopPropagation: () => void }) => {
+    /**
+     * Handle click event to copy text to clipboard.
+     * @param {React.MouseEvent<HTMLButtonElement, MouseEvent>} event - The click event.
+     */
+    const onClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         setIsCopied(true);
         copy(textCopy);
         timeoutClipboard = setTimeout(() => {

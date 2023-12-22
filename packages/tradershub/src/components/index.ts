6c4f87a500d012675b99a57fec053d8bd423dbbc
Thisyahlen <thisyahlen@regentmarkets.com>
2023-12-22 11:40:31 +0800

    chore: add options and multipliers section for tradershub v2

--- packages/tradershub/src/components/index.ts
+++ packages/tradershub/src/components/index.ts
@@ -3,4 +3,5 @@ export * from './GetADerivAccountBanner';
 export * from './ModalProvider';
 export * from './ModalStepWrapper';
 export * from './ModalWrapper';
+export * from './OptionsAndMultiplersSection';
 export * from './TradingAccountCard';

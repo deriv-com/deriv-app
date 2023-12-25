ea9c0cb7fc85f40cafaf26f097949bb7f95b15dc
Aum Bhatt <aum@deriv.com>
2023-12-25 11:47:52 +0400

    chore: added more tests for the percentage-message

--- packages/wallets/src/features/cashier/modules/WithdrawalCrypto/components/WithdrawalCryptoForm/components/WithdrawalCryptoPercentageSelector/__tests__/WithdrawalCryptoPercentageSelector.spec.tsx
+++ packages/wallets/src/features/cashier/modules/WithdrawalCrypto/components/WithdrawalCryptoForm/components/WithdrawalCryptoPercentageSelector/__tests__/WithdrawalCryptoPercentageSelector.spec.tsx
@@ -4,17 +4,16 @@ import { render, screen } from '@testing-library/react';
 import { useWithdrawalCryptoContext } from '../../../../../provider';
 import WithdrawalCryptoPercentageSelector from '../WithdrawalCryptoPercentageSelector';
 
-const mockUseFormikContext = jest.spyOn(formik, 'useFormikContext') as jest.Mock;
-
 jest.mock('../../../../../provider', () => ({
     ...jest.requireActual('../../../../../provider'),
     useWithdrawalCryptoContext: jest.fn(),
 }));
 
+const mockUseFormikContext = jest.spyOn(formik, 'useFormikContext') as jest.Mock;
 const mockUseWithdrawalCryptoContext = useWithdrawalCryptoContext as jest.Mock;
 
 describe('<WithdrawalCryptoPercentageSelector />', () => {
-    it('should show the percentage message when the value is greater than minimum withdrawal balance and less than the maximum withdrawal limit', () => {
+    it('should show the percentage message when the input amount is between min withdrawal limit and max withdrawal limit', () => {
         mockUseFormikContext.mockReturnValue({
             values: {
                 cryptoAmount: '9.00000000',
@@ -29,6 +28,24 @@ describe('<WithdrawalCryptoPercentageSelector />', () => {
         });
 
         render(<WithdrawalCryptoPercentageSelector />);
-        screen.getByText('90% of available balance (10.00000000 BTC)');
+        expect(screen.getByText('90% of available balance (10.00000000 BTC)')).toBeInTheDocument();
+    });
+
+    it('should hide the percentage message when the input amount is between min withdrawal limit and max withdrawal limit', () => {
+        mockUseFormikContext.mockReturnValue({
+            values: {
+                cryptoAmount: '11.00000000',
+            },
+        });
+        mockUseWithdrawalCryptoContext.mockReturnValue({
+            activeWallet: {
+                balance: 10,
+                currency: 'BTC',
+                display_balance: '10.00000000 BTC',
+            },
+        });
+
+        render(<WithdrawalCryptoPercentageSelector />);
+        expect(screen.queryByText('90% of available balance (10.00000000 BTC)')).not.toBeInTheDocument();
     });
 });

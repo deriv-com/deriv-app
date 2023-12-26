53a357c559cfcf742453a5da6b681907c9bc6757
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-12-26 09:43:14 +0300

    refactor: components

--- packages/bot-web-ui/src/components/quick-strategy/form-wrappers/accordion-strategy-group.tsx
+++ packages/bot-web-ui/src/components/quick-strategy/form-wrappers/accordion-strategy-group.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { Text } from '@deriv/components';
 import { observer, useStore } from '@deriv/stores';
-import { useDBotStore } from 'Stores/useDBotStore';
-import { STRATEGIES } from '../config';
 import { TDescriptionItem, TDescription, TDataGroupedObjectsByTitle } from '../types';
 import Accordion from '../../dashboard/dbot-tours/common/accordion';
 
@@ -14,13 +12,12 @@ type TDescriptionContent = {
 
 type TAccordionStrategyGroupProps = {
     tutorial_selected_strategy?: string;
+    grouped_objects_by_title: TDescription[];
+    expanded_subtitles_storage: { [key: string]: boolean };
+    setExpandedSubtitlesStorage: (value: { [key: string]: boolean }) => void;
 };
 
-type TExpandedSubtitlesStorageDefault = {
-    [key: string]: boolean;
-};
-
-const DescriptionItem = ({ item, font_size }: TDescriptionItem) => {
+const DescriptionItem = ({ item, font_size }: TDescriptionItem): React.ReactNode => {
     const class_name = item?.className ?? '';
     switch (item.type) {
         case 'text': {
@@ -50,81 +47,52 @@ const DescriptionItem = ({ item, font_size }: TDescriptionItem) => {
     }
 };
 
-const DescriptionContent = ({ data, font_size }: TDescriptionContent): React.ReactElement[] => {
+const DescriptionContent = ({ data, font_size }: TDescriptionContent): React.ReactNode => {
     const content_data = Array.isArray(data) ? data : (data as unknown as TDescriptionItem[]).slice(1);
     return content_data.map(item => (
-        <DescriptionItem item={item} font_size={font_size} key={item?.content && item?.content[0]} />
+        <React.Fragment key={item?.content && item?.content[0]}>
+            <DescriptionItem item={item} font_size={font_size} />
+        </React.Fragment>
     ));
 };
 
-const AccordionStrategyGroup = observer(({ tutorial_selected_strategy }: TAccordionStrategyGroupProps) => {
-    const { ui } = useStore();
-    const { is_mobile } = ui;
-    const desktop_font_size = tutorial_selected_strategy ? 's' : 'xs';
-    const font_size: string = React.useMemo<string>(() => (is_mobile ? 'xs' : desktop_font_size), [is_mobile]);
+const AccordionStrategyGroup = observer(
+    ({
+        tutorial_selected_strategy,
+        grouped_objects_by_title,
+        expanded_subtitles_storage,
+        setExpandedSubtitlesStorage,
+    }: TAccordionStrategyGroupProps) => {
+        const { ui } = useStore();
+        const { is_mobile } = ui;
+        const desktop_font_size = tutorial_selected_strategy ? 's' : 'xs';
+        const font_size: string = React.useMemo<string>(() => (is_mobile ? 'xs' : desktop_font_size), [is_mobile]);
 
-    const { quick_strategy } = useDBotStore();
-    const { selected_strategy } = quick_strategy;
-
-    const expanded_subtitles_storage_default: TExpandedSubtitlesStorageDefault = {};
-    const [expanded_subtitles_storage, setExpandedSubtitlesStorage] = useState(expanded_subtitles_storage_default);
-
-    const strategy = STRATEGIES[tutorial_selected_strategy || (selected_strategy as keyof typeof STRATEGIES)];
-
-    const makeGroupedObjectsByTitle = () => {
-        return strategy?.description?.reduce((acc: TDescriptionItem[][], obj: TDescriptionItem) => {
-            const is_subtitle = obj.type === 'subtitle_italic' || obj.type === 'subtitle';
-            if (is_subtitle) {
-                acc.push([]);
-
-                const generateStorageKey = (obj: TDataGroupedObjectsByTitle, selected_strategy: string): string => {
-                    return `${obj.content[0]}__${selected_strategy}`.split(' ').join('_').toLowerCase();
-                };
-
-                expanded_subtitles_storage_default[
-                    generateStorageKey(obj as TDataGroupedObjectsByTitle, selected_strategy)
-                ] = obj?.expanded ?? false;
-            }
-            //If long description available, show content intro paragraph under heading, skip short description.
-            const shouldShowLongDescriptionIntro = () => {
-                return acc.length - 1 === 0 && obj.type === 'text' && obj.content?.length === 2;
-            };
-            if (shouldShowLongDescriptionIntro()) {
-                obj.content?.shift();
-            }
-            acc[acc.length - 1].push(obj);
-            return acc;
-        }, []);
-    };
-
-    const grouped_objects_by_title = Array.isArray(strategy?.description)
-        ? makeGroupedObjectsByTitle()
-        : [{ type: 'text', content: [strategy?.description] }];
-
-    return (
-        <>
-            {Array.isArray(grouped_objects_by_title) &&
-                grouped_objects_by_title.map((data: TDescription, idx: number) => {
-                    const subtitle_value = (data as TDescriptionItem[])?.[0]?.content?.[0] ?? '';
-                    return (
-                        <Accordion
-                            key={`accordion-${subtitle_value}-${idx}`}
-                            content_data={{
-                                header: subtitle_value,
-                                content: <DescriptionContent data={data} font_size={font_size} />,
-                            }}
-                            expanded={!!(data as TDescriptionItem[])[0]?.expanded}
-                            is_cursive={false}
-                            no_collapsible={(data as TDescriptionItem[])[0]?.no_collapsible}
-                            has_subtitle={!!subtitle_value}
-                            expanded_subtitles_storage={expanded_subtitles_storage}
-                            setExpandedSubtitlesStorage={setExpandedSubtitlesStorage}
-                            font_size={font_size}
-                        />
-                    );
-                })}
-        </>
-    );
-});
+        return (
+            <>
+                {Array.isArray(grouped_objects_by_title) &&
+                    grouped_objects_by_title.map((data: TDescription, idx: number) => {
+                        const subtitle_value = (data as TDescriptionItem[])?.[0]?.content?.[0] ?? '';
+                        return (
+                            <Accordion
+                                key={`accordion-${subtitle_value}-${idx}`}
+                                content_data={{
+                                    header: subtitle_value,
+                                    content: <DescriptionContent data={data} font_size={font_size} />,
+                                }}
+                                expanded={!!(data as TDescriptionItem[])[0]?.expanded}
+                                is_cursive={false}
+                                no_collapsible={(data as TDescriptionItem[])[0]?.no_collapsible}
+                                has_subtitle={!!subtitle_value}
+                                expanded_subtitles_storage={expanded_subtitles_storage}
+                                setExpandedSubtitlesStorage={setExpandedSubtitlesStorage}
+                                font_size={font_size}
+                            />
+                        );
+                    })}
+            </>
+        );
+    }
+);
 
 export default AccordionStrategyGroup;
--- packages/bot-web-ui/src/components/quick-strategy/form-wrappers/strategy-description.tsx
+++ packages/bot-web-ui/src/components/quick-strategy/form-wrappers/strategy-description.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { observer } from '@deriv/stores';
+import { useDBotStore } from 'Stores/useDBotStore';
+import { STRATEGIES } from '../config';
+import { TDescriptionItem, TDataGroupedObjectsByTitle } from '../types';
 import AccordionStrategyGroup from './accordion-strategy-group';
 import './strategy-description.scss';
 
@@ -8,22 +12,68 @@ type TStrategyDescription = Partial<{
     tutorial_selected_strategy: string;
 }>;
 
-const StrategyDescription: React.FC<TStrategyDescription> = ({
-    formfields,
-    active_tab,
-    tutorial_selected_strategy,
-}) => (
-    <>
-        {active_tab === 'TRADE_PARAMETERS' ? (
-            <div className='qs__body__content__form'>{formfields}</div>
-        ) : (
-            <div className='qs__body__content__description'>
-                <div>
-                    <AccordionStrategyGroup tutorial_selected_strategy={tutorial_selected_strategy} />
-                </div>
-            </div>
-        )}
-    </>
+type TExpandedSubtitlesStorageDefault = {
+    [key: string]: boolean;
+};
+
+const StrategyDescription: React.FC<TStrategyDescription> = observer(
+    ({ formfields, active_tab, tutorial_selected_strategy }) => {
+        const { quick_strategy } = useDBotStore();
+        const { selected_strategy } = quick_strategy;
+
+        const expanded_subtitles_storage_default: TExpandedSubtitlesStorageDefault = {};
+        const [expanded_subtitles_storage, setExpandedSubtitlesStorage] = useState(expanded_subtitles_storage_default);
+
+        const strategy = STRATEGIES[tutorial_selected_strategy || (selected_strategy as keyof typeof STRATEGIES)];
+
+        const makeGroupedObjectsByTitle = () => {
+            return strategy?.description?.reduce((acc: TDescriptionItem[][], obj: TDescriptionItem) => {
+                const is_subtitle = obj.type === 'subtitle_italic' || obj.type === 'subtitle';
+                if (is_subtitle) {
+                    acc.push([]);
+
+                    const generateStorageKey = (obj: TDataGroupedObjectsByTitle, selected_strategy: string): string => {
+                        return `${obj.content[0]}__${selected_strategy}`.split(' ').join('_').toLowerCase();
+                    };
+
+                    expanded_subtitles_storage_default[
+                        generateStorageKey(obj as TDataGroupedObjectsByTitle, selected_strategy)
+                    ] = obj?.expanded ?? false;
+                }
+                //If long description available, show content intro paragraph under heading, skip short description.
+                const shouldShowLongDescriptionIntro = () => {
+                    return acc.length - 1 === 0 && obj.type === 'text' && obj.content?.length === 2;
+                };
+                if (shouldShowLongDescriptionIntro()) {
+                    obj.content?.shift();
+                }
+                acc[acc.length - 1].push(obj);
+                return acc;
+            }, []);
+        };
+
+        const grouped_objects_by_title = Array.isArray(strategy?.description)
+            ? makeGroupedObjectsByTitle()
+            : [{ type: 'text', content: [strategy?.description] }];
+        return (
+            <>
+                {active_tab === 'TRADE_PARAMETERS' ? (
+                    <div className='qs__body__content__form'>{formfields}</div>
+                ) : (
+                    <div className='qs__body__content__description'>
+                        <div>
+                            <AccordionStrategyGroup
+                                tutorial_selected_strategy={tutorial_selected_strategy}
+                                grouped_objects_by_title={grouped_objects_by_title}
+                                expanded_subtitles_storage={expanded_subtitles_storage}
+                                setExpandedSubtitlesStorage={setExpandedSubtitlesStorage}
+                            />
+                        </div>
+                    </div>
+                )}
+            </>
+        );
+    }
 );
 
 export default StrategyDescription;

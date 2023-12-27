99742693cc6ee25517c2a55feecd1a762ca74ded
Hamza <hamza.muhammad@regentmarkets.com>
2023-12-27 12:42:54 +0800

    chore: move variable to  constant file

--- packages/tradershub/src/features/cfd/screens/Jurisdiction/JurisdictionCard/JurisdictionCardBack.tsx
+++ packages/tradershub/src/features/cfd/screens/Jurisdiction/JurisdictionCard/JurisdictionCardBack.tsx
@@ -1,31 +1,12 @@
 import React, { Dispatch, FC, SetStateAction } from 'react';
 import { Text } from '@deriv/quill-design';
 import BackArrowIcon from '../../../../../public/images/ic-back-arrow.svg';
-import DocumentsIcon from '../../../../../public/images/ic-documents.svg';
-import IdCardIcon from '../../../../../public/images/ic-id-card.svg';
-import NotApplicableIcon from '../../../../../public/images/ic-not-applicable.svg';
-import SelfieIcon from '../../../../../public/images/ic-selfie.svg';
-import VerificationFailedStatusIcon from '../../../../../public/images/ic-verification-failed-status.svg';
-import VerificationPendingStatusIcon from '../../../../../public/images/ic-verification-pending-status.svg';
-import VerificationSuccessStatusIcon from '../../../../../public/images/ic-verification-success-status.svg';
+import { verificationIconsMapper, verificationStatusIconsMapper } from '../constants';
 import { jurisdictionVerificationContents } from '../jurisdiction-contents/jurisdiction-verification-contents';
 import {
     TJurisdictionCardItemVerification,
     TJurisdictionCardItemVerificationItem,
 } from '../jurisdiction-contents/props.types';
-
-const verificationIconsMapper: Record<string, JSX.Element> = {
-    documentNumber: <IdCardIcon />,
-    nameAndAddress: <DocumentsIcon />,
-    notApplicable: <NotApplicableIcon />,
-    selfie: <SelfieIcon />,
-};
-
-const verificationStatusIconsMapper: Record<string, JSX.Element> = {
-    verificationFailedStatusIcon: <VerificationFailedStatusIcon />,
-    verificationPendingStatusIcon: <VerificationPendingStatusIcon />,
-    verificationSuccessStatusIcon: <VerificationSuccessStatusIcon />,
-};
 
 type TJurisdictionCardBackProps = {
     setIsFlipped: Dispatch<SetStateAction<boolean>>;

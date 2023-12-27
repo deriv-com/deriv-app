d23c78f91bc4739bca2aef833bd4408d14d699a7
adrienne-rio <adrienne@deriv.com>
2023-12-27 16:50:05 +0800

    chore: fix eslint issues

--- packages/utils/src/files.ts
+++ packages/utils/src/files.ts
@@ -1,11 +1,16 @@
+declare global {
+    interface Blob {
+        lastModifiedDate: number;
+        name: string;
+    }
+}
+
 /**
  * The function renames the files by removing any non ISO-8859-1 code point from filename and returns a new blob object with the updated file name.
  * @returns {Blob}
  */
-
 export const renameFile = (file: File) => {
     const new_file = new Blob([file], { type: file.type });
-    // @ts-expect-error Blob has a global declaration file in shared package only with name as attribute
     // eslint-disable-next-line no-control-regex
     new_file.name = file.name.replace(/[^\x00-\x7F]+/g, '');
     return new_file;

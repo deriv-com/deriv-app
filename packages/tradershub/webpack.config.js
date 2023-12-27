8055e88d963aca8e7e7149a62ede3081014ecdaf
yashim-deriv <yashim@regentmarkets.com>
2023-12-27 16:59:06 +0800

    fix: file extension tsx -> ts

--- packages/tradershub/webpack.config.js
+++ packages/tradershub/webpack.config.js
@@ -180,7 +180,6 @@ module.exports = function (env) {
         externals: [
             {
                 '@deriv/api': true,
-                '@deriv/library': true,
                 classnames: true,
                 react: true,
                 'react-dom': true,

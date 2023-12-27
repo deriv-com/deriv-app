aa35f1d0122dc1b2f7840dddca3f6eef88ae04f7
shontzu <108507236+shontzu-deriv@users.noreply.github.com>
2023-12-27 09:49:43 +0800

    refactor: replace rem with px
    Co-authored-by: Muhammad Hamza <120543468+hamza-deriv@users.noreply.github.com>

--- packages/tradershub/src/components/Base/Tooltip/Tooltip.tsx
+++ packages/tradershub/src/components/Base/Tooltip/Tooltip.tsx
@@ -37,7 +37,7 @@ const Tooltip: FC<TProps> = ({ alignment = 'left', children, className, isVisibl
             {isVisible && (
                 <div className={(qtMerge(`absolute z-10 flex items-center transform-${alignment}`), className)}>
                     <div className={`bg-system-light-active-background w-200 h-400 clip-path-${alignment}`} />
-                    <div className='w-max max-w-[22rem] p-200 rounded-md leading-[1.8rem] whitespace-pre-wrap bg-system-light-active-background'>
+                    <div className='w-max max-w-[220px] p-200 rounded-md leading-[18px] whitespace-pre-wrap bg-system-light-active-background'>
                         <Text size='sm'>{message}</Text>
                     </div>
                 </div>

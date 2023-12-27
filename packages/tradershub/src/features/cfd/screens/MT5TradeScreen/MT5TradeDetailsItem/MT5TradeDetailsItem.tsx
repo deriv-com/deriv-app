614aaa730bfe0d8c17aa9904295efb98b88d0c83
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-12-27 13:10:08 +0800

    refactor: MT5TradeDetailsItem

---
+++ packages/tradershub/src/features/cfd/screens/MT5TradeScreen/MT5TradeDetailsItem/MT5TradeDetailsItem.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useRef } from 'react';
+import { useHover } from 'usehooks-ts';
+import { qtMerge, Text, useBreakpoint } from '@deriv/quill-design';
+import { Tooltip, WalletClipboard } from '../../../../../components/Base';
+import { useModal } from '../../../../../components/ModalProvider';
+import EditIcon from '../../../../../public/images/ic-edit.svg';
+
+type TMT5TradeDetailsItemProps = {
+    className?: string;
+    label?: string;
+    value: string;
+    variant?: 'clipboard' | 'info' | 'password';
+};
+
+const MT5TradeDetailsItem: FC<TMT5TradeDetailsItemProps> = ({ className, label, value, variant = 'clipboard' }) => {
+    const { isDesktop } = useBreakpoint();
+    const hoverRef = useRef(null);
+    const isHovered = useHover(hoverRef);
+    const { show } = useModal();
+    return (
+        <div
+            className={qtMerge(
+                'flex items-center h-1600 justify-between bg-system-light-secondary-background p-250 pl-400',
+                className
+            )}
+        >
+            {variant !== 'info' && (
+                <React.Fragment>
+                    <Text color='less-prominent' size='sm'>
+                        {label}
+                    </Text>
+                    <div className='flex items-center space-x-400 pr-400'>
+                        <Text size='sm' weight='bold'>
+                            {value}
+                        </Text>
+                        {variant === 'clipboard' && (
+                            <WalletClipboard popoverAlignment='right' successMessage='' textCopy={value} />
+                        )}
+                        {variant === 'password' && (
+                            <Tooltip alignment='left' isVisible={isHovered && isDesktop} message='Change password'>
+                                <div ref={hoverRef}>
+                                    <EditIcon className='cursor-pointer' />
+                                </div>
+                            </Tooltip>
+                        )}
+                    </div>
+                </React.Fragment>
+            )}
+            {variant === 'info' && (
+                <Text color='less-prominent' size={isDesktop ? 'sm' : 'md'}>
+                    {value}
+                </Text>
+            )}
+        </div>
+    );
+};
+
+export default MT5TradeDetailsItem;

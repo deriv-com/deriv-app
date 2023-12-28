f77202bb1172fafa35f76ab7838bb5af0d677e04
yauheni-deriv <yauheni@regentmarkets.com>
2023-12-28 16:01:42 +0300

    chore: test for useIsPasskeySupported hook

---
+++ packages/hooks/src/__tests__/useIsPasskeySupported.spec.tsx
@@ -0,0 +1,32 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { platformAuthenticatorIsAvailable } from '@simplewebauthn/browser';
+import useIsPasskeySupported from '../useIsPasskeySupported';
+
+jest.mock('@simplewebauthn/browser', () => ({
+    platformAuthenticatorIsAvailable: jest.fn(),
+}));
+
+describe('useIsPasskeySupported', () => {
+    it('sets state correctly when passkey is supported', async () => {
+        (platformAuthenticatorIsAvailable as jest.Mock).mockResolvedValue(true);
+
+        const { result, waitForNextUpdate } = renderHook(() => useIsPasskeySupported());
+
+        expect(result.current.is_loading).toBe(true);
+        await waitForNextUpdate();
+        expect(result.current.is_loading).toBe(false);
+        expect(result.current.is_passkey_supported).toBe(true);
+    });
+
+    it('sets state correctly when passkey is not supported', async () => {
+        // Mocking the external function to return false
+        (platformAuthenticatorIsAvailable as jest.Mock).mockResolvedValue(false);
+
+        const { result, waitForNextUpdate } = renderHook(() => useIsPasskeySupported());
+
+        expect(result.current.is_loading).toBe(true);
+        await waitForNextUpdate();
+        expect(result.current.is_loading).toBe(false);
+        expect(result.current.is_passkey_supported).toBe(false);
+    });
+});

929995c3de13ecb584e7a382d4e7081a8a5ad179
yauheni-deriv <yauheni@regentmarkets.com>
2023-12-28 18:17:13 +0300

    chore: restore jest config

--- packages/hooks/src/__tests__/useIsPasskeySupported.spec.tsx
+++ packages/hooks/src/__tests__/useIsPasskeySupported.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import { platformAuthenticatorIsAvailable } from '@simplewebauthn/browser';
 import useIsPasskeySupported from '../useIsPasskeySupported';
 

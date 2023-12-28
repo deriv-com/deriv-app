e0f4f620a47a14a4e27bd1a087b2c8c164f25bd0
Hamza <hamza.muhammad@regentmarkets.com>
2023-12-28 12:52:07 +0800

    chore: remove the empty fragment

--- packages/tradershub/src/features/cfd/screens/Jurisdiction/JurisdictionCard/JurisdictionCard.tsx
+++ packages/tradershub/src/features/cfd/screens/Jurisdiction/JurisdictionCard/JurisdictionCard.tsx
@@ -109,93 +109,91 @@ const JurisdictionCard: React.FC<TJurisdictionCardProps> = ({ isAdded, isSelecte
             }}
             tabIndex={0}
         >
-            <>
-                <div className='flex flex-col justify-center w-full h-full transition-transform duration-300 px-800 pt-1000 pb-1800 backface-hidden transform-gpu'>
-                    {isOverHeaderAvailable && <JurisdictionCardTag tag={overHeader || ''} />}
-                    <div className='mt-[25px] mb-[15px] text-center'>
-                        <Text bold className='text-system-light-primary-background' size='lg'>
-                            {header}
+            <div className='flex flex-col justify-center w-full h-full transition-transform duration-300 px-800 pt-1000 pb-1800 backface-hidden transform-gpu'>
+                {isOverHeaderAvailable && <JurisdictionCardTag tag={overHeader || ''} />}
+                <div className='mt-[25px] mb-[15px] text-center'>
+                    <Text bold className='text-system-light-primary-background' size='lg'>
+                        {header}
+                    </Text>
+                </div>
+                <div className='border-sm border-system-light-secondary-background'>
+                    {rows.map(row => {
+                        return (
+                            <JurisdictionCardRow
+                                description={parseDescription(row)}
+                                key={`wallets-jurisdiction-card--${row?.title}`}
+                                renderTag={() => {
+                                    if (!row?.titleIndicators) return;
+
+                                    if (
+                                        row.titleIndicators?.type === 'displayIcons' &&
+                                        verificationDocs &&
+                                        marketType &&
+                                        marketType !== 'all'
+                                    ) {
+                                        return (
+                                            <div className='flex gap-300'>
+                                                {!(marketType in verificationDocs)
+                                                    ? verificationDocumentsMapper.notApplicable.icon
+                                                    : verificationDocs[marketType]?.map(doc => (
+                                                          <JurisdictionCardVerificationTag
+                                                              category={verificationDocumentsMapper[doc].category}
+                                                              icon={verificationDocumentsMapper[doc].icon}
+                                                              key={`verification-doc-${doc}`}
+                                                          />
+                                                      ))}
+                                            </div>
+                                        );
+                                    }
+
+                                    if (row?.titleIndicators?.displayText) {
+                                        return (
+                                            <div
+                                                className={qtMerge(
+                                                    'rounded-200 bg-brand-red-darker text-system-light-primary-background px-500 py-[5px]',
+                                                    row.titleIndicators?.displayTextSkinColor === 'red-darker' &&
+                                                        'bg-brand-red-darker',
+                                                    row.titleIndicators?.displayTextSkinColor === 'red-dark' &&
+                                                        'bg-brand-red-dark',
+                                                    row.titleIndicators?.displayTextSkinColor === 'red-light' &&
+                                                        'bg-brand-red-light',
+                                                    row.titleIndicators?.displayTextSkinColor === 'yellow-dark' &&
+                                                        'bg-brand-yellow-dark',
+                                                    row.titleIndicators?.displayTextSkinColor === 'yellow-light' &&
+                                                        'bg-brand-yellow-light',
+                                                    row.titleIndicators?.displayTextSkinColor === 'voilet-dark' &&
+                                                        'bg-brand-voilet-dark',
+                                                    row.titleIndicators?.displayTextSkinColor === 'brown-dark' &&
+                                                        'bg-brand-brown-dark'
+                                                )}
+                                            >
+                                                <Text
+                                                    bold
+                                                    className='leading-[1] text-system-light-primary-background'
+                                                    size='sm'
+                                                >
+                                                    {row.titleIndicators.displayText}
+                                                </Text>
+                                            </div>
+                                        );
+                                    }
+                                }}
+                                title={row.title}
+                            />
+                        );
+                    })}
+                </div>
+                {isAdded && (
+                    <div className='absolute w-full text-center rounded-b-[13px] rounded-t-50 bottom-50 left-50 p-400 bg-random-blue'>
+                        <Text bold className='text-system-light-primary-background' size='sm'>
+                            Added
                         </Text>
                     </div>
-                    <div className='border-sm border-system-light-secondary-background'>
-                        {rows.map(row => {
-                            return (
-                                <JurisdictionCardRow
-                                    description={parseDescription(row)}
-                                    key={`wallets-jurisdiction-card--${row?.title}`}
-                                    renderTag={() => {
-                                        if (!row?.titleIndicators) return;
-
-                                        if (
-                                            row.titleIndicators?.type === 'displayIcons' &&
-                                            verificationDocs &&
-                                            marketType &&
-                                            marketType !== 'all'
-                                        ) {
-                                            return (
-                                                <div className='flex gap-300'>
-                                                    {!(marketType in verificationDocs)
-                                                        ? verificationDocumentsMapper.notApplicable.icon
-                                                        : verificationDocs[marketType]?.map(doc => (
-                                                              <JurisdictionCardVerificationTag
-                                                                  category={verificationDocumentsMapper[doc].category}
-                                                                  icon={verificationDocumentsMapper[doc].icon}
-                                                                  key={`verification-doc-${doc}`}
-                                                              />
-                                                          ))}
-                                                </div>
-                                            );
-                                        }
-
-                                        if (row?.titleIndicators?.displayText) {
-                                            return (
-                                                <div
-                                                    className={qtMerge(
-                                                        'rounded-200 bg-brand-red-darker text-system-light-primary-background px-500 py-[5px]',
-                                                        row.titleIndicators?.displayTextSkinColor === 'red-darker' &&
-                                                            'bg-brand-red-darker',
-                                                        row.titleIndicators?.displayTextSkinColor === 'red-dark' &&
-                                                            'bg-brand-red-dark',
-                                                        row.titleIndicators?.displayTextSkinColor === 'red-light' &&
-                                                            'bg-brand-red-light',
-                                                        row.titleIndicators?.displayTextSkinColor === 'yellow-dark' &&
-                                                            'bg-brand-yellow-dark',
-                                                        row.titleIndicators?.displayTextSkinColor === 'yellow-light' &&
-                                                            'bg-brand-yellow-light',
-                                                        row.titleIndicators?.displayTextSkinColor === 'voilet-dark' &&
-                                                            'bg-brand-voilet-dark',
-                                                        row.titleIndicators?.displayTextSkinColor === 'brown-dark' &&
-                                                            'bg-brand-brown-dark'
-                                                    )}
-                                                >
-                                                    <Text
-                                                        bold
-                                                        className='leading-[1] text-system-light-primary-background'
-                                                        size='sm'
-                                                    >
-                                                        {row.titleIndicators.displayText}
-                                                    </Text>
-                                                </div>
-                                            );
-                                        }
-                                    }}
-                                    title={row.title}
-                                />
-                            );
-                        })}
-                    </div>
-                    {isAdded && (
-                        <div className='absolute w-full text-center rounded-b-[13px] rounded-t-50 bottom-50 left-50 p-400 bg-random-blue'>
-                            <Text bold className='text-system-light-primary-background' size='sm'>
-                                Added
-                            </Text>
-                        </div>
-                    )}
-                </div>
-                {marketType && marketType !== 'all' && verificationDocs && (
-                    <JurisdictionCardBack setIsFlipped={setIsFlipped} verificationDocs={verificationDocs[marketType]} />
                 )}
-            </>
+            </div>
+            {marketType && marketType !== 'all' && verificationDocs && (
+                <JurisdictionCardBack setIsFlipped={setIsFlipped} verificationDocs={verificationDocs[marketType]} />
+            )}
         </div>
     );
 };

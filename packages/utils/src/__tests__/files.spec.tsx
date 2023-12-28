0519591ec7f136b7e59eb93598775ea58ae836a3
adrienne-rio <adrienne@deriv.com>
2023-12-28 10:52:25 +0800

    chore: added test files for new utils

---
+++ packages/utils/src/__tests__/files.spec.tsx
@@ -0,0 +1,23 @@
+import { renameFile } from '../files'; // Replace 'yourFileName' with the actual file name
+
+describe('renameFile', () => {
+    it('should remove non-ASCII characters from the file name', () => {
+        const originalFileName = 'test_file_日本語.txt';
+        const fakeFile = new File([], originalFileName);
+
+        const renamedFile = renameFile(fakeFile);
+
+        // eslint-disable-next-line no-control-regex
+        expect(renamedFile.name).not.toMatch(/[^\x00-\x7F]+/g);
+    });
+
+    it('should preserve the file type and content', () => {
+        const originalFileName = 'test_file.txt';
+        const originalFileContent = 'This is a test file content.';
+        const fakeFile = new File([originalFileContent], originalFileName, {
+            type: 'text/plain',
+        });
+        const renamedFile = renameFile(fakeFile);
+        expect(renamedFile.type).toBe(fakeFile.type);
+    });
+});

892f6689160d171cf103daf1a66afad12e2ab427
shontzu <108507236+shontzu-deriv@users.noreply.github.com>
2023-12-28 13:54:22 +0800

    [TRAH] shontzu/TRAH-2338/show-ctrader-in-tradershub-route (#12566)
    * refactor: add cTrader to tradershub route
    
    * chore: import react component beginning of file

--- packages/tradershub/src/features/cfd/flows/CTrader/AddedCTraderAccountsList/AddedCTraderAccountsList.tsx
+++ packages/tradershub/src/features/cfd/flows/CTrader/AddedCTraderAccountsList/AddedCTraderAccountsList.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useCtraderAccountsList } from '@deriv/api';
+import React, { FC, Fragment } from 'react';
+import { useActiveTradingAccount, useCtraderAccountsList } from '@deriv/api';
 import { Button, Text } from '@deriv/quill-design';
 import { TradingAccountCard } from '../../../../../components';
 import { getStaticUrl } from '../../../../../helpers/urls';
 import CTrader from '../../../../../public/images/cfd/ctrader.svg';
 import { PlatformDetails } from '../../../constants';
 
-const AddedCTraderAccountsList: React.FC = () => {
+const AddedCTraderAccountsList: FC = () => {
     const { data: cTraderAccounts } = useCtraderAccountsList();
+    const { data: activeTrading } = useActiveTradingAccount();
 
     const leading = () => (
         <div
@@ -27,10 +28,10 @@ const AddedCTraderAccountsList: React.FC = () => {
     );
 
     const trailing = () => (
-        <div className='flex flex-col space-y-1'>
+        <div className='flex flex-col gap-y-200'>
             <Button
                 // todo: open transfer modal
-                variant='outlined'
+                variant='secondary'
             >
                 Transfer
             </Button>
@@ -42,17 +43,19 @@ const AddedCTraderAccountsList: React.FC = () => {
         <div>
             <TradingAccountCard leading={leading} trailing={trailing}>
                 <div className='flex flex-col flex-grow'>
-                    {cTraderAccounts?.map(account => (
-                        <React.Fragment key={`added-ctrader-${account.login}`}>
-                            <Text size='sm'>{PlatformDetails.ctrader.title}</Text>
-                            <Text size='sm' weight='bold'>
-                                {account?.formatted_balance}
-                            </Text>
-                            <Text color='primary' size='sm' weight='bold'>
-                                {account.login}
-                            </Text>
-                        </React.Fragment>
-                    ))}
+                    {cTraderAccounts
+                        ?.filter(account => account.is_virtual === activeTrading?.is_virtual)
+                        .map(account => (
+                            <Fragment key={`added-ctrader-${account.login}`}>
+                                <Text size='sm'>{PlatformDetails.ctrader.title}</Text>
+                                <Text bold size='sm'>
+                                    {account?.formatted_balance}
+                                </Text>
+                                <Text bold color='primary' size='sm'>
+                                    {account.login}
+                                </Text>
+                            </Fragment>
+                        ))}
                 </div>
             </TradingAccountCard>
         </div>

c03ff5e45599b9157b2428502b338b0e49da3707
aizad-deriv <aizad@deriv.com>
2023-12-29 18:22:42 +0800

    fix: added first_real_accounts and demo_loginid inside useTradingAccountList

--- packages/tradershub/src/components/DemoRealSwitcher/DemoRealSwitcher.tsx
+++ packages/tradershub/src/components/DemoRealSwitcher/DemoRealSwitcher.tsx
@@ -4,8 +4,8 @@ import { LabelPairedChevronDownSmRegularIcon } from '@deriv/quill-icons';
 import { Listbox, Transition } from '@headlessui/react';
 
 const currencyState = [
-    { color: 'status-light-information', label: 'Demo', value: 'demo' },
-    { color: 'status-light-success', label: 'Real', value: 'real' },
+    { color: 'information', label: 'Demo', value: 'demo' },
+    { color: 'success', label: 'Real', value: 'real' },
 ];
 
 const DemoRealSwitcher = () => {
@@ -18,12 +18,12 @@ const DemoRealSwitcher = () => {
                 <div className='relative mt-1'>
                     <Listbox.Button
                         as='button'
-                        className={`relative w-full cursor-default py-[3px] px-400 border-75 rounded-200 border-${color} flex gap-x-400 items-center`}
+                        className={`cursor-pointer relative w-full py-[3px] px-400 border-75 rounded-200 border-status-light-${color} flex gap-x-400 items-center`}
                     >
-                        <Text bold className={`text-${color}`} size='sm'>
+                        <Text bold className={`text-status-light-${color}`} size='sm'>
                             {label}
                         </Text>
-                        <LabelPairedChevronDownSmRegularIcon className={`fill-${color}`} />
+                        <LabelPairedChevronDownSmRegularIcon className={`fill-status-light-${color}`} />
                     </Listbox.Button>
                     <Transition leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
                         <Listbox.Options
@@ -33,9 +33,9 @@ const DemoRealSwitcher = () => {
                             {currencyState?.map(currency => (
                                 <Listbox.Option
                                     as='div'
-                                    className='cursor-default bg-system-light-primary-background hover:bg-system-light-hover-background'
+                                    className='cursor-pointer bg-system-light-primary-background hover:bg-system-light-hover-background'
                                     key={currency.label}
-                                    value={currency.value}
+                                    value={currency}
                                 >
                                     {({ selected }) => (
                                         <Text

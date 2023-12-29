6bda48682873c654cd5b739a190f8e16fc7b894f
aizad-deriv <aizad@deriv.com>
2023-12-29 16:58:45 +0800

    feat: create real demo switch account on tradershub

--- packages/tradershub/src/components/index.ts
+++ packages/tradershub/src/components/index.ts
@@ -2,6 +2,7 @@ export * from './Base';
 export * from './ActionScreen';
 export * from './ContentSwitcher';
 export * from './CurrencySwitcher';
+export * from './DemoRealSwitcher';
 export * from './Dialog';
 export * from './GetADerivAccountBanner';
 export * from './Modal';

f000931ff3b1ffb4245095739bc41ef966e8812b
Aum Bhatt <aum@deriv.com>
2024-01-02 16:07:52 +0400

    chore: refactor validator tests

--- packages/wallets/src/features/cashier/modules/WithdrawalCrypto/utils/__tests__/withdrawalCryptoValidators.spec.ts
+++ packages/wallets/src/features/cashier/modules/WithdrawalCrypto/utils/__tests__/withdrawalCryptoValidators.spec.ts
@@ -20,9 +20,13 @@ describe('withdrawalCryptoValidator', () => {
     };
     const mockRemainder = 9;
 
-    it('should check if no errors are returned when valid inputs are provided for crypto address, crypto amount and fiat amount', () => {
+    it('should check if no errors are returned when valid inputs are provided for crypto address', () => {
         const cryptoAddressMessages = validateCryptoAddress(mockCryptoAddress);
 
+        expect(cryptoAddressMessages).toEqual(undefined);
+    });
+
+    it('should check if no errors are returned when valid inputs are provided for crypto amount', () => {
         const cryptoAmountMessages = validateCryptoInput(
             mockActiveWallet,
             mockFractionalDigits,
@@ -31,28 +35,31 @@ describe('withdrawalCryptoValidator', () => {
             mockValue
         );
 
+        expect(cryptoAmountMessages).toEqual(undefined);
+    });
+
+    it('should check if no errors are returned when valid inputs are provided for fiat amount', () => {
         const fiatAmountMessages = validateFiatInput(mockFractionalDigits, mockValue);
 
-        expect(cryptoAddressMessages).toEqual(undefined);
-        expect(cryptoAmountMessages).toEqual(undefined);
         expect(fiatAmountMessages).toEqual(undefined);
     });
 
-    it('should return error for invalid crypto address', () => {
+    it('should return error for invalid crypto address when length is lesser than 25 characters', () => {
         mockCryptoAddress = 'wrfie0493n0';
-        let cryptoAddressMessages = validateCryptoAddress(mockCryptoAddress);
-
-        expect(cryptoAddressMessages).toEqual('Your wallet address should have 25 to 64 characters.');
-
-        mockCryptoAddress = 'moie0420349irn039fn09krf0n9r0f9n0r9fkn093nkf09k3n0f9n309fn09rkf09r0f9n93n0';
-        cryptoAddressMessages = validateCryptoAddress(mockCryptoAddress);
+        const cryptoAddressMessages = validateCryptoAddress(mockCryptoAddress);
 
         expect(cryptoAddressMessages).toEqual('Your wallet address should have 25 to 64 characters.');
     });
 
-    it('should return "Should be a valid number." error for invalid crypto amount and fiat amount', () => {
-        mockValue = 'aad';
+    it('should return error for invalid crypto address when length is greater than 64 characters', () => {
+        mockCryptoAddress = 'moie0420349irn039fn09krf0n9r0f9n0r9fkn093nkf09k3n0f9n309fn09rkf09r0f9n93n0';
+        const cryptoAddressMessages = validateCryptoAddress(mockCryptoAddress);
 
+        expect(cryptoAddressMessages).toEqual('Your wallet address should have 25 to 64 characters.');
+    });
+
+    it('should return "Should be a valid number." error for invalid crypto amount', () => {
+        mockValue = 'aad';
         let cryptoAmountMessages = validateCryptoInput(
             mockActiveWallet,
             mockFractionalDigits,
@@ -61,54 +68,61 @@ describe('withdrawalCryptoValidator', () => {
             mockValue
         );
 
-        let fiatAmountMessages = validateFiatInput(mockFractionalDigits, mockValue);
+        expect(cryptoAmountMessages).toEqual('Should be a valid number.');
+
+        mockValue = '5.';
+        cryptoAmountMessages = validateCryptoInput(
+            mockActiveWallet,
+            mockFractionalDigits,
+            mockIsClientVerified,
+            mockRemainder,
+            mockValue
+        );
 
         expect(cryptoAmountMessages).toEqual('Should be a valid number.');
+
+        mockValue = '.';
+        cryptoAmountMessages = validateCryptoInput(
+            mockActiveWallet,
+            mockFractionalDigits,
+            mockIsClientVerified,
+            mockRemainder,
+            mockValue
+        );
+
+        expect(cryptoAmountMessages).toEqual('Should be a valid number.');
+
+        mockValue = '5.03abcd';
+        cryptoAmountMessages = validateCryptoInput(
+            mockActiveWallet,
+            mockFractionalDigits,
+            mockIsClientVerified,
+            mockRemainder,
+            mockValue
+        );
+
+        expect(cryptoAmountMessages).toEqual('Should be a valid number.');
+    });
+
+    it('should return "Should be a valid number." error for invalid fiat amount', () => {
+        mockValue = 'aad';
+        let fiatAmountMessages = validateFiatInput(mockFractionalDigits, mockValue);
+
         expect(fiatAmountMessages).toEqual('Should be a valid number.');
 
         mockValue = '5.';
-
-        cryptoAmountMessages = validateCryptoInput(
-            mockActiveWallet,
-            mockFractionalDigits,
-            mockIsClientVerified,
-            mockRemainder,
-            mockValue
-        );
-
         fiatAmountMessages = validateFiatInput(mockFractionalDigits, mockValue);
 
-        expect(cryptoAmountMessages).toEqual('Should be a valid number.');
         expect(fiatAmountMessages).toEqual('Should be a valid number.');
 
         mockValue = '.';
-
-        cryptoAmountMessages = validateCryptoInput(
-            mockActiveWallet,
-            mockFractionalDigits,
-            mockIsClientVerified,
-            mockRemainder,
-            mockValue
-        );
-
         fiatAmountMessages = validateFiatInput(mockFractionalDigits, mockValue);
 
-        expect(cryptoAmountMessages).toEqual('Should be a valid number.');
         expect(fiatAmountMessages).toEqual('Should be a valid number.');
 
         mockValue = '5.03abcd';
-
-        cryptoAmountMessages = validateCryptoInput(
-            mockActiveWallet,
-            mockFractionalDigits,
-            mockIsClientVerified,
-            mockRemainder,
-            mockValue
-        );
-
         fiatAmountMessages = validateFiatInput(mockFractionalDigits, mockValue);
 
-        expect(cryptoAmountMessages).toEqual('Should be a valid number.');
         expect(fiatAmountMessages).toEqual('Should be a valid number.');
     });
 
@@ -126,11 +140,10 @@ describe('withdrawalCryptoValidator', () => {
         expect(cryptoAmountMessages).toEqual('Insufficient funds');
     });
 
-    it('should return error if amount > max withdrawal limit OR amount < min withdrawal limit', () => {
-        // for verified user
+    it('should return error if amount > max withdrawal limit OR amount < min withdrawal limit for a verified user', () => {
         mockValue = '11.0000000';
 
-        let cryptoAmountMessages = validateCryptoInput(
+        const cryptoAmountMessages = validateCryptoInput(
             mockActiveWallet,
             mockFractionalDigits,
             mockIsClientVerified,
@@ -139,12 +152,13 @@ describe('withdrawalCryptoValidator', () => {
         );
 
         expect(cryptoAmountMessages).toEqual('Insufficient funds');
+    });
 
-        // for unverified user show 10k limit
-
+    it('should return error if amount > max withdrawal limit OR amount < min withdrawal limit for an unverified user', () => {
+        mockValue = '11.0000000';
         mockIsClientVerified = false;
 
-        cryptoAmountMessages = validateCryptoInput(
+        const cryptoAmountMessages = validateCryptoInput(
             mockActiveWallet,
             mockFractionalDigits,
             mockIsClientVerified,

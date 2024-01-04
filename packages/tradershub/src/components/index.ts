e18efb987512486cb8ea2868f565dde585f5989e
aizad-deriv <aizad@deriv.com>
2024-01-04 16:33:54 +0800

    fix: update component directory

--- packages/tradershub/src/components/index.ts
+++ packages/tradershub/src/components/index.ts
@@ -1,7 +1,7 @@
 export * from './ActionScreen';
-export * from './Base';
 export * from './ButtonGroup';
 export * from './ButtonGroup';
+export * from './Clipboard';
 export * from './ContentSwitcher';
 export * from './CurrencySwitcher';
 export * from './Dialog';
@@ -10,6 +10,7 @@ export * from './Modal';
 export * from './ModalStepWrapper';
 export * from './OptionsAndMultipliersSection';
 export * from './StaticLink';
+export * from './Tooltip';
 export * from './TotalAssets';
 export * from './TradingAccountCard';
 export * from './TradingAccountsList';

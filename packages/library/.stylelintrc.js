af983ff52f12096baa811731085fef990359daca
adrienne-rio <adrienne@deriv.com>
2024-01-04 12:17:03 +0800

    feat: added build step for tailwindcss in library package

---
+++ packages/library/.stylelintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    rules: {},
+    ignoreFiles: ['packages/library/**/main*.css'],
+};
--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,4 +1,4 @@
-import './index.scss';
+import './main.css';
 
 export * as Base from './base';
 export * as Provider from './providers';

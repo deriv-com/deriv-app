253a64a37a6bffc60fc217870bf532e96bdc9a5c
amina-deriv <amina@regentmarkets.com>
2024-01-05 15:01:23 +0400

    fix: test

--- packages/appstore/src/components/traders-hub-banner/migration-banner.tsx
+++ packages/appstore/src/components/traders-hub-banner/migration-banner.tsx
@@ -4,8 +4,16 @@ import { Button, Text } from '@deriv/components';
 import { Localize } from '@deriv/translations';
 import { observer, useStore } from '@deriv/stores';
 import { useMT5SVGEligibleToMigrate } from '@deriv/hooks';
-import { getMT5AccountTitle, JURISDICTION_MARKET_TYPES, Jurisdiction } from '@deriv/shared';
+import {
+    getMT5AccountTitle,
+    JURISDICTION_MARKET_TYPES,
+    Jurisdiction,
+    getFormattedJurisdictionMarketTypes,
+    getCFDPlatformNames,
+    CFD_PLATFORMS,
+} from '@deriv/shared';
 import TradersHubBannerImage from './traders-hub-banner-image';
+import { platform } from 'os';
 
 const MigrationBanner = observer(() => {
     const {
@@ -18,7 +26,7 @@ const MigrationBanner = observer(() => {
     const { has_derived_mt5_to_migrate, has_derived_and_financial_mt5 } = useMT5SVGEligibleToMigrate();
 
     const image = is_mobile ? 'migrate_mobile' : 'migrate_desktop';
-    const size = is_mobile ? 'xs' : 'm';
+    const size = 'xs';
 
     const openMT5MigrationModal = () => {
         setMT5MigrationError('');
@@ -35,16 +43,11 @@ const MigrationBanner = observer(() => {
                     {has_derived_and_financial_mt5 ? (
                         <Text size={size}>
                             <Localize
-                                i18n_default_text='We’re upgrading your <0>{{account_1}}</0> and <0>{{account_2}} </0> accounts.'
+                                i18n_default_text='Upgrade your <0>{{account_1}}</0> and <0>{{account_2}} {{platform}} </0> accounts.'
                                 values={{
-                                    account_1: getMT5AccountTitle({
-                                        account_type: JURISDICTION_MARKET_TYPES.DERIVED,
-                                        jurisdiction: Jurisdiction.SVG,
-                                    }),
-                                    account_2: getMT5AccountTitle({
-                                        account_type: JURISDICTION_MARKET_TYPES.FINANCIAL,
-                                        jurisdiction: Jurisdiction.SVG,
-                                    }),
+                                    account_1: getFormattedJurisdictionMarketTypes(JURISDICTION_MARKET_TYPES.DERIVED),
+                                    account_2: getFormattedJurisdictionMarketTypes(JURISDICTION_MARKET_TYPES.FINANCIAL),
+                                    platform: getCFDPlatformNames(CFD_PLATFORMS.MT5),
                                 }}
                                 components={[<strong key={0} />]}
                             />
@@ -52,27 +55,22 @@ const MigrationBanner = observer(() => {
                     ) : (
                         <Text size={size}>
                             <Localize
-                                i18n_default_text='We’re upgrading your <0>{{account_title}}</0> account.'
+                                i18n_default_text='We’re upgrading your <0>{{account_title}} {{platform}} </0> account.'
                                 values={{
-                                    account_title: getMT5AccountTitle({
-                                        account_type: has_derived_mt5_to_migrate
+                                    account_title: getFormattedJurisdictionMarketTypes(
+                                        has_derived_mt5_to_migrate
                                             ? JURISDICTION_MARKET_TYPES.DERIVED
-                                            : JURISDICTION_MARKET_TYPES.FINANCIAL,
-                                        jurisdiction: Jurisdiction.SVG,
-                                    }),
+                                            : JURISDICTION_MARKET_TYPES.FINANCIAL
+                                    ),
+                                    platform: getCFDPlatformNames(CFD_PLATFORMS.MT5),
                                 }}
                                 components={[<strong key={0} />]}
                             />
                         </Text>
                     )}
                 </div>
-                <Button
-                    className='traders-hub-banner__migrate-banner-button'
-                    primary
-                    large
-                    onClick={openMT5MigrationModal}
-                >
-                    <Localize i18n_default_text='Upgrade now' />
+                <Button primary_light onClick={openMT5MigrationModal}>
+                    <Localize i18n_default_text='Upgrade' />
                 </Button>
             </div>
             <TradersHubBannerImage image={image} class_name='traders-hub-banner__image' />
--- packages/appstore/src/components/traders-hub-banner/traders-hub-banner-image.tsx
+++ packages/appstore/src/components/traders-hub-banner/traders-hub-banner-image.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import MigrateDesktopImage from 'Assets/svgs/banner/migrate-desktop.svg';
 import MigrateMobileImage from 'Assets/svgs/banner/migrate-mobile.svg';
+import MigrateCard from 'Assets/svgs/banner/migrate-card.svg';
 
 export type TBannerImageProps<TTradersHubBannerImageListProps> = {
-    image: TTradersHubBannerImageListProps;
+    image?: TTradersHubBannerImageListProps;
     class_name?: string;
     width?: React.CSSProperties['width'];
 };
@@ -15,11 +16,10 @@ const TradersHubBannerImageList = {
     migrate_mobile: MigrateMobileImage,
 };
 
-const TradersHubBannerImage = ({ image, class_name, width }: TBannerImageProps<TTradersHubBannerImageListProps>) => {
-    const Component = TradersHubBannerImageList[image];
-    const data_testid = `dt_${image}`;
+const TradersHubBannerImage = ({ class_name, width }: TBannerImageProps<TTradersHubBannerImageListProps>) => {
+    const Component = MigrateCard;
 
-    return <Component className={class_name} style={{ width }} data-testid={data_testid} />;
+    return <Component className={class_name} style={{ width }} data-testid='dt_migrate_card' />;
 };
 
 export default TradersHubBannerImage;

6c943f50752a9858573a0ef928ff82709c81856a
adrienne-rio <adrienne@deriv.com>
2024-01-08 12:15:39 +0800

    chore: removed fc types p2p-v2

--- packages/p2p-v2/src/App.tsx
+++ packages/p2p-v2/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { APIProvider } from '@deriv/api';
 import { Router } from './routes';
 
-const App: React.FC = () => (
+const App = () => (
     <APIProvider standalone>
         <Router />
     </APIProvider>

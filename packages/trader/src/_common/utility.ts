ae0e76c77373db1108d8335d99d9edc580280a83
kate-deriv <kate@regentmarkets.com>
2024-01-08 09:24:37 +0300

    chore: remove unused function

--- packages/trader/src/_common/utility.ts
+++ packages/trader/src/_common/utility.ts
@@ -1,10 +1,3 @@
-let static_hash: string;
-export const getStaticHash = () => {
-    static_hash =
-        static_hash || (document.querySelector('script[src*="main"]')?.getAttribute('src') || '').split('.')[1];
-    return static_hash;
-};
-
 export class PromiseClass {
     promise: Promise<unknown>;
     reject?: (reason?: unknown) => void;

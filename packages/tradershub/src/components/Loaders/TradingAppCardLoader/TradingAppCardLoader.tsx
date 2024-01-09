ba3d68c66f9011a07bf3fb117aaa5021e8d5d576
Hirad <hirad@regentmarkets.com>
2024-01-09 14:19:52 +0800

    chore: added px-50 back to the stylings

--- packages/tradershub/src/components/Loaders/TradingAppCardLoader/TradingAppCardLoader.tsx
+++ packages/tradershub/src/components/Loaders/TradingAppCardLoader/TradingAppCardLoader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TradingAppCardLoader = () => (
-    <div className='flex flex-col gap-1200 py-75'>
+    <div className='flex flex-col gap-1200 py-75 px-50'>
         <div className='flex flex-row gap-700 align-middle'>
             <div className='animate-pulse bg-solid-slate-100 w-2500 h-2500 rounded-500' />
             <div className='flex flex-col gap-500'>

fd4a526b10f0d45ee8c180883f1c8b902eb1eaf6
kate-deriv <kate@regentmarkets.com>
2024-01-09 16:46:39 +0300

    feat: started adding functinality for numpud

--- packages/trader/src/Modules/Trading/Components/Playground/new-trade-param-popup.tsx
+++ packages/trader/src/Modules/Trading/Components/Playground/new-trade-param-popup.tsx
@@ -12,17 +12,33 @@ const NewTradeParamPopup = ({ onClick }: { onClick: () => void }) => {
             <div className='trade-param_popup_container' onClick={onClickHandler}>
                 <div className='trade-param_popup_top'>
                     <div className='footer-new_bottom-sheet_separator' />
-                    <div>Duration</div>
-                    <div>Minutes</div>
-                    <input
-                        type='number'
-                        style={{ border: '1px solid black' }}
-                        min='0'
-                        inputMode='numeric'
-                        pattern='[0-9]*'
-                        title='Non-negative integral number'
-                    />
-                    <div>Acceptable range: 1 - 1,440</div>
+                    <div className='trade-param_popup_title'>
+                        Duration <span>i</span>
+                    </div>
+                    <div className='trade-param_popup_duration'>
+                        <span>Ticks</span>
+                        <span>Seconds</span>
+                        <span
+                            style={{
+                                backgroundColor: ' var(--core-color-opacity-black-800, rgba(0, 0, 0, 0.88))',
+                                color: 'var(--core-color-solid-slate-50, #FFF)',
+                            }}
+                        >
+                            Minutes
+                        </span>
+                    </div>
+                    <div>
+                        <input
+                            type='number'
+                            min='0'
+                            inputMode='numeric'
+                            pattern='[0-9]*'
+                            title='Non-negative integral number'
+                            className='trade-param_popup_input'
+                            value={1}
+                        />
+                        <div className='trade-param_popup_input_text'>Acceptable range: 1 - 1,440 minutes</div>
+                    </div>
                 </div>
                 <div className='trade-param_popup_bottom'>
                     <button className='footer-new_bottom-sheet_button'>Save</button>

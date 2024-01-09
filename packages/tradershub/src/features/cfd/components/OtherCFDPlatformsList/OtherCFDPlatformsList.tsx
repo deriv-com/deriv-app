fe1c3b03519bd046a95033389fb711968d001ea6
Hirad <hirad@regentmarkets.com>
2024-01-09 11:06:10 +0800

    fix: fixed some of the stylings

--- packages/tradershub/src/features/cfd/components/OtherCFDPlatformsList/OtherCFDPlatformsList.tsx
+++ packages/tradershub/src/features/cfd/components/OtherCFDPlatformsList/OtherCFDPlatformsList.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useActiveTradingAccount, useAuthorize, useDxtradeAccountsList, useInvalidateQuery } from '@deriv/api';
 import { Text } from '@deriv/quill-design';
+import { THooks } from '../../../../types';
 import { AddedDxtradeAccountsList, AvailableDxtradeAccountsList } from '../../flows/OtherCFDs/Dxtrade';
+import { TradingAppCardLoader } from '../TradingAppCardLoader';
 
 const OtherCFDPlatformsList = () => {
     const { isFetching } = useAuthorize();
@@ -9,7 +11,9 @@ const OtherCFDPlatformsList = () => {
     const { data: activeTradingAccount } = useActiveTradingAccount();
     const invalidate = useInvalidateQuery();
 
-    const hasDxtradeAccount = dxTradeAccounts?.some(account => account.is_virtual === activeTradingAccount?.is_virtual);
+    const hasDxtradeAccount = dxTradeAccounts?.some(
+        (account: THooks.DxtradeAccountsList) => account.is_virtual === activeTradingAccount?.is_virtual
+    );
 
     useEffect(() => {
         if (!isFetching) {
@@ -21,6 +25,7 @@ const OtherCFDPlatformsList = () => {
         <div className='pb-1200'>
             <Text bold>Other CFD Platforms</Text>
             <div className='grid grid-cols-3 gap-x-800 gap-y-2400 lg:grid-cols-1 lg:grid-rows-1'>
+                {!isFetchedAfterMount && <TradingAppCardLoader />}
                 {isFetchedAfterMount &&
                     (hasDxtradeAccount ? <AddedDxtradeAccountsList /> : <AvailableDxtradeAccountsList />)}
             </div>

3fbfffc25175f0b4eafe6479ce565eedfa4ebc62
heorhi-deriv <heorhi@binary.com>
2024-01-11 15:35:08 +0300

    refactor: :sparkles: migrate webpack config to ts

---
+++ packages/cashier-v2/build/build-loaders.ts
@@ -0,0 +1,76 @@
+import { RuleSetRule } from 'webpack';
+import { TBuildOptions } from './types/build-types';
+
+export const buildLoaders = (options: TBuildOptions): RuleSetRule[] => {
+    const babelLoader = {
+        exclude: /node_modules/,
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: [
+            {
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    rootMode: 'upward',
+                },
+            },
+        ],
+    };
+
+    const styleLoader = {
+        test: /\.(sc|sa|c)ss$/,
+        use: [
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    url: true,
+                },
+            },
+            {
+                loader: 'postcss-loader',
+                options: {
+                    postcssOptions: {
+                        config: options.paths.root,
+                    },
+                },
+            },
+            {
+                loader: 'resolve-url-loader',
+                options: {
+                    keepQuery: true,
+                    sourceMap: true,
+                },
+            },
+            'sass-loader',
+            {
+                loader: 'sass-resources-loader',
+                options: {
+                    // Provide path to the file with resources
+                    resources: [
+                        // eslint-disable-next-line global-require, import/no-dynamic-require
+                        ...require('../../shared/src/styles/index.js'),
+                    ],
+                },
+            },
+        ],
+    };
+
+    const sourceMapLoader = {
+        loader: 'source-map-loader',
+        test: (input: string) => options.isRelease && /\.js$/.test(input),
+    };
+
+    return [
+        {
+            // https://github.com/webpack/webpack/issues/11467
+            include: /node_modules/,
+            resolve: {
+                fullySpecified: false,
+            },
+            test: /\.m?js/,
+        },
+        babelLoader,
+        sourceMapLoader,
+        styleLoader,
+    ];
+};

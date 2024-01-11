a9d31d53a5c7de4d9a1b36fe1084bbed6aaf675d
aizad-deriv <aizad@deriv.com>
2024-01-11 12:18:22 +0800

    chore: remove commented code

--- packages/tradershub/src/routes/TradersHubRoute/TradersHubRoute.tsx
+++ packages/tradershub/src/routes/TradersHubRoute/TradersHubRoute.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Button, Heading, Text, useBreakpoint } from '@deriv/quill-design';
+import { Heading, useBreakpoint } from '@deriv/quill-design';
 import {
     CFDSection,
     ContentSwitcher,
     DemoRealSwitcher,
     OptionsAndMultipliersSection,
     RegulationSwitcherMobile,
-    StaticLink,
     TotalAssets,
 } from '../../components';
-import { CTraderList } from '../../features/cfd/components/CTraderList';
-import { MT5PlatformsList } from '../../features/cfd/components/MT5PlatformsList';
-import { OtherCFDPlatformsList } from '../../features/cfd/components/OtherCFDPlatformsList';
 
 const TradersHubRoute = () => {
     const { isMobile } = useBreakpoint();
@@ -38,9 +34,6 @@ const TradersHubRoute = () => {
                         </ContentSwitcher.Panel>
                         <ContentSwitcher.Panel>
                             <CFDSection />
-                            {/* <MT5PlatformsList />
-                            <CTraderList />
-                            <OtherCFDPlatformsList /> */}
                         </ContentSwitcher.Panel>
                     </ContentSwitcher.PanelContainer>
                 </ContentSwitcher>
@@ -58,32 +51,6 @@ const TradersHubRoute = () => {
             </div>
             <OptionsAndMultipliersSection />
             <CFDSection />
-
-            {/* <div className='border-solid p-1200 rounded-1200 border-xs border-opacity-black-100'>
-                <div className='pb-1200'>
-                    <div className='flex items-center gap-200'>
-                        <Heading.H4 className='font-sans'>CFDs</Heading.H4>
-                        <Button className='no-underline' colorStyle='coral' size='sm' variant='tertiary'>
-                            Compare Accounts
-                        </Button>
-                    </div>
-                    <Text size='sm'>
-                        Trade with leverage and tight spreads for better returns on trades.
-                        <StaticLink size='md' staticUrl='/trade-types/cfds/'>
-                            Learn more
-                        </StaticLink>
-                    </Text>
-                </div>
-                <div className='space-y-1200'>
-                    <MT5PlatformsList />
-                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-1200'>
-                        <CTraderList />
-                    </div>
-                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-1200'>
-                        <OtherCFDPlatformsList />
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 };

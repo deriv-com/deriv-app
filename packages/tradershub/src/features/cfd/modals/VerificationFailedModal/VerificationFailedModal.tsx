cc14dbb0721937b30198b45d6584f40149058f21
Sergei Baranovski <sergei@deriv.com>
2024-01-11 13:15:28 +0300

    feat: delete FC

--- packages/tradershub/src/features/cfd/modals/VerificationFailedModal/VerificationFailedModal.tsx
+++ packages/tradershub/src/features/cfd/modals/VerificationFailedModal/VerificationFailedModal.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Modal } from '../../../../components';
 import { VerificationFailed } from '../../screens';
 
-const VerificationFailedModal: FC = () => {
+const VerificationFailedModal = () => {
     return (
         <Modal>
             <Modal.Content>

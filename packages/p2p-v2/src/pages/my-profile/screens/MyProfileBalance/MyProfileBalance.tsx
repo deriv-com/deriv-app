67034fb8329cff76afb51873e94e39d4a9da9595
adrienne-rio <adrienne@deriv.com>
2024-01-11 16:51:55 +0800

    chore: backup

--- packages/p2p-v2/src/pages/my-profile/screens/MyProfileBalance/MyProfileBalance.tsx
+++ packages/p2p-v2/src/pages/my-profile/screens/MyProfileBalance/MyProfileBalance.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import { useAdvertiserInfo } from '@deriv/api';
+import './MyProfileBalance.scss';
 
 const MyProfileBalance = () => {
     const { data: advertiserInfo, isLoading } = useAdvertiserInfo();
 
     if (isLoading) return <h1>Loading...</h1>;
 
+    const balanceText = new Intl.NumberFormat('en-US', {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+        style: 'decimal',
+    }).format(advertiserInfo?.balance_available || 0);
+
     return (
-        <div>
-            <div>{advertiserInfo?.balance_available}</div>
-            <h1>Buy</h1>
-            <div>
-                <div>
-                    <h2>Daily limit</h2>
-                    <span>{advertiserInfo?.daily_buy_limit}</span>
-                </div>
-                <div>
-                    <h2>Available</h2>
-                    <span>{advertiserInfo?.balance_available}</span>
+        <div className='p2p-v2-my-profile-balance'>
+            <div className='p2p-v2-my-profile-balance__amount'>
+                <h1>Available Deriv P2P Balance</h1>
+                <span>{balanceText} USD</span>
+            </div>
+            <div className='p2p-v2-my-profile-balance__item'>
+                <h1>Buy</h1>
+                <div className='p2p-v2-my-profile-balance__item-limits'>
+                    <div>
+                        <h1>Daily limit</h1>
+                        <span>{advertiserInfo?.daily_buy_limit} USD</span>
+                    </div>
+                    <div>
+                        <h1>Available</h1>
+                        <span>{advertiserInfo?.balance_available} USD</span>
+                    </div>
                 </div>
             </div>
-            <h1>Sell</h1>
-            <div>
-                <div>
-                    <h2>Daily limit</h2>
-                    <span>{advertiserInfo?.daily_sell_limit}</span>
+            <div className='p2p-v2-my-profile-balance__item'>
+                <h1>Sell</h1>
+                <div className='p2p-v2-my-profile-balance__item-limits'>
+                    <div>
+                        <h1>Daily limit</h1>
+                        <span>{advertiserInfo?.daily_sell_limit} USD</span>
+                    </div>
+                    <div>
+                        <h1>Available</h1>
+                        <span>{advertiserInfo?.balance_available} USD</span>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h2>Available</h2>
-                <span>{advertiserInfo?.balance_available}</span>
             </div>
         </div>
     );

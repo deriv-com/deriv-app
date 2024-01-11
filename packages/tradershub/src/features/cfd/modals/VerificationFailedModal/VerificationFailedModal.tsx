5401ae506d00dbb31a4ea49033bc6494ab6c08a1
Sergei Baranovski <sergei@deriv.com>
2024-01-11 13:37:28 +0300

    feat: delete curly braces

--- packages/tradershub/src/features/cfd/modals/VerificationFailedModal/VerificationFailedModal.tsx
+++ packages/tradershub/src/features/cfd/modals/VerificationFailedModal/VerificationFailedModal.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Modal } from '../../../../components';
 import { VerificationFailed } from '../../screens';
 
-const VerificationFailedModal = () => {
-    return (
-        <Modal>
-            <Modal.Content>
-                <VerificationFailed />
-            </Modal.Content>
-        </Modal>
-    );
-};
+const VerificationFailedModal = () => (
+    <Modal>
+        <Modal.Content>
+            <VerificationFailed />
+        </Modal.Content>
+    </Modal>
+);
 
 export default VerificationFailedModal;

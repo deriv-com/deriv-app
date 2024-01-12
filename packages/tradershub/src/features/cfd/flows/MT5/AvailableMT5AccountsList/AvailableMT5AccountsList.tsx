7f2082055889914675e9b32862e5744095f38359
arshad-rao-deriv <muhammad.arshad@deriv.com>
2024-01-12 10:32:01 +0400

    fix: Added Jurisdiction Modal flow to MT5Account Card

--- packages/tradershub/src/features/cfd/flows/MT5/AvailableMT5AccountsList/AvailableMT5AccountsList.tsx
+++ packages/tradershub/src/features/cfd/flows/MT5/AvailableMT5AccountsList/AvailableMT5AccountsList.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useAuthorize } from '@deriv/api';
+import { Provider } from '@deriv/library';
 import {
     TradingAccountCard,
     TradingAccountCardContent,
@@ -6,14 +8,23 @@ import {
 } from '../../../../../components';
 import { THooks } from '../../../../../types';
 import { MarketTypeDetails } from '../../../constants';
+import { JurisdictionModal } from '../../../modals/JurisdictionModal';
 import { MT5AccountIcon } from '../MT5AccountIcon';
 
 const AvailableMT5AccountsList = ({ account }: { account: THooks.MT5AccountsList }) => {
     const { description, title } = MarketTypeDetails[account.market_type || 'all'];
+    const { data: activeAccount } = useAuthorize();
+    const { setCfdState } = Provider.useCFDContext();
+    const { show } = Provider.useModal();
+
+    const trailingButtonClick = () => {
+        setCfdState('marketType', account.market_type);
+        !activeAccount?.is_virtual && show(<JurisdictionModal />); /* show MT5PasswordModal for demo */
+    };
 
     const LeadingIcon = () => <MT5AccountIcon account={account} />;
 
-    const TrailingButton = () => <TradingAccountCardLightButton />;
+    const TrailingButton = () => <TradingAccountCardLightButton onSubmit={trailingButtonClick} />;
 
     return (
         <TradingAccountCard leading={LeadingIcon} trailing={TrailingButton}>

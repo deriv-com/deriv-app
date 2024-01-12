a9d557ce9db5d2cd9af06a565e0202d153548a4e
Thisyahlen <thisyahlen@regentmarkets.com>
2024-01-12 09:42:50 +0800

    chore: swap the order of cfd and deriv platform section and hide ctrader and derivx for eu

--- packages/tradershub/src/components/CFDSection/CFDContent/CFDContent.tsx
+++ packages/tradershub/src/components/CFDSection/CFDContent/CFDContent.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useIsEuRegion } from '@deriv/api';
 import { CTraderList, MT5PlatformsList, OtherCFDPlatformsList } from '../../../features/cfd/components';
 
-const CFDContent = () => (
-    <div className='space-y-800 pt-800 lg:space-y-1200 lg:pt-1200'>
-        <MT5PlatformsList />
-        <CTraderList />
-        <OtherCFDPlatformsList />
-    </div>
-);
+const CFDContent = () => {
+    const { isEU } = useIsEuRegion();
+    return (
+        <div className='space-y-800 pt-800 lg:space-y-1200 lg:pt-1200'>
+            <MT5PlatformsList />
+            {!isEU && <CTraderList />}
+            {!isEU && <OtherCFDPlatformsList />}
+        </div>
+    );
+};
 
 export default CFDContent;

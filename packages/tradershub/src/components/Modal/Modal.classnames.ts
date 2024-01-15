fb8aa1aa6335e8bd5773a10a1ea1f12a0dbb3fc7
arshad-rao-deriv <muhammad.arshad@deriv.com>
2024-01-15 12:45:38 +0400

    fix: updated modal footer styles

--- packages/tradershub/src/components/Modal/Modal.classnames.ts
+++ packages/tradershub/src/components/Modal/Modal.classnames.ts
@@ -2,7 +2,7 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { ExcludeAllNull } from '@deriv/quill-design';
 
 export const ModalFooterClass = cva(
-    'grid grid-cols-1 gap-400 p-800 border border-solid border-t-100 border-system-light-secondary-background bottom-0 lg:flex lg:items-center lg:px-1200 lg:py-800',
+    'grid gap-400 p-800 border border-solid border-t-100 border-system-light-secondary-background bottom-0 lg:items-center lg:px-1200 lg:py-800',
     {
         variants: {
             align: {

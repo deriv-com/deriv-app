63acb8eb71801a7a33007522dd6b4dcc829a7c25
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2024-01-16 10:57:29 +0800

    [URGENT] P2P-V2 remove utils temporarily (#12962)
    * fix: issue with package-lock caching
    
    * fix: remove utils temporarily from p2p-v2
    
    * fix: remove utils temporarily from p2p-v2

--- packages/p2p-v2/src/hooks/useSendbird.ts
+++ packages/p2p-v2/src/hooks/useSendbird.ts
@@ -1,9 +1,21 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useAdvertiserInfo, useChatCreate, useOrderInfo, useSendbirdServiceToken, useServerTime } from '@deriv/api';
-import { renameFile } from '@deriv/utils';
 import SendbirdChat, { BaseChannel, User } from '@sendbird/chat';
 import { GroupChannel, GroupChannelHandler, GroupChannelModule } from '@sendbird/chat/groupChannel';
 import { BaseMessage, MessageType, MessageTypeFilter } from '@sendbird/chat/message';
+
+/**
+ * The function renames the files by removing any non ISO-8859-1 code point from filename and returns a new blob object with the updated file name.
+ * @returns {Blob}
+ */
+export const renameFile = (file: File) => {
+    const newFile = new Blob([file], { type: file.type });
+    newFile.name = file.name
+        .split('')
+        .filter(char => char.charCodeAt(0) >= 32 && char.charCodeAt(0) <= 126)
+        .join('');
+    return newFile;
+};
 
 const ChatMessageStatus = {
     ERRORED: 'ERRORED',

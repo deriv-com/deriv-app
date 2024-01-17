18678105ba0083153ea100a1b546c320ffa72d42
Aum Bhatt <aum@deriv.com>
2024-01-17 16:19:06 +0400

    fix: :ambulance: set preflight as false in tailwind config

--- packages/cashier-v2/src/routes/Home/index.tsx
+++ packages/cashier-v2/src/routes/Home/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Heading } from '@deriv/quill-design';
 
 const Home: React.FC<{ path: string }> = ({ path }) => {
     return (
-        <div className='flex justify-center items-center pt-2500'>
-            <Heading.H2> Welcome to Cashier Version 2 from {path} Page 💰</Heading.H2>
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: '3rem',
+                height: '100%',
+            }}
+        >
+            Welcome to Cashier Version 2 from {path} Page 💰
         </div>
     );
 };
--- packages/cashier-v2/tailwind.config.ts
+++ packages/cashier-v2/tailwind.config.ts
@@ -2,6 +2,9 @@ import type { Config } from 'tailwindcss';
 import QuillTailwindConfig from '@deriv/quill-design/quill-tailwind/tailwind.config.cjs';
 
 export default {
+    corePlugins: {
+        preflight: false,
+    },
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     presets: [QuillTailwindConfig],
 } satisfies Config;

c3f3f77a1d754700b771370a01cdd1372a1a6a03
Sergei Baranovski <sergei@deriv.com>
2024-01-17 15:17:09 +0300

    feat: finalize modal

--- packages/tradershub/src/features/cfd/modals/TopUpModal/TopUpModal.tsx
+++ packages/tradershub/src/features/cfd/modals/TopUpModal/TopUpModal.tsx
@@ -1,17 +1,9 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { useIsEuRegion } from '@deriv/api';
 import { Button, Heading, Text, useBreakpoint } from '@deriv/quill-design';
 import { Modal } from '../../../../components/Modal';
 import { THooks, TPlatforms } from '../../../../types';
-import {
-    CFDPlatforms,
-    getCTraderCompanies,
-    getDxCompanies,
-    getMT5Companies,
-    MarketType,
-    MarketTypeDetails,
-    PlatformDetails,
-} from '../../constants';
+import { CFDPlatforms, MarketType, MarketTypeDetails, PlatformDetails } from '../../constants';
 
 type TTradeModalProps = {
     account: THooks.CtraderAccountsList | THooks.DxtradeAccountsList | THooks.MT5AccountsList;
@@ -22,56 +14,18 @@ const TopUpModal = ({ account, platform }: TTradeModalProps) => {
     const { isDesktop } = useBreakpoint();
     const { data: isEuRegion } = useIsEuRegion();
 
-    const ctraderCompanies = getCTraderCompanies();
-    const dxtradeCompanies = getDxCompanies();
-    const mt5Companies = getMT5Companies(isEuRegion);
-
     const platformTitle = PlatformDetails[platform].title;
-
     const marketTypeDetails = MarketTypeDetails(isEuRegion)[account.market_type ?? MarketType.ALL];
-
     const marketTypeTitle = marketTypeDetails?.title ?? '';
-
     const title = platform === CFDPlatforms.MT5 ? `${platformTitle} ${marketTypeTitle}` : platformTitle;
 
     const HeadingTag = isDesktop ? Heading.H3 : Heading.H2;
-
-    const getAccountTitle = useCallback(() => {
-        let title = '';
-        if ((!mt5Companies && !dxtradeCompanies && !ctraderCompanies) || !account) return '';
-
-        switch (platform) {
-            case CFDPlatforms.MT5:
-                title =
-                    mt5Companies[account.is_virtual ? 'demo' : 'real'][
-                        account.market_type as keyof typeof mt5Companies['demo' | 'real']
-                    ].title;
-                break;
-            case CFDPlatforms.CTRADER:
-                title =
-                    ctraderCompanies[account.is_virtual ? 'demo' : 'real'][
-                        account.market_type as keyof typeof ctraderCompanies['demo' | 'real']
-                    ].title;
-                break;
-            case CFDPlatforms.DXTRADE:
-                title =
-                    dxtradeCompanies[account.is_virtual ? 'demo' : 'real'][
-                        account.market_type as keyof typeof dxtradeCompanies['demo' | 'real']
-                    ].title;
-                break;
-            default:
-                break;
-        }
-
-        return title;
-    }, [mt5Companies, dxtradeCompanies, account, ctraderCompanies, platform]);
 
     return (
         <Modal className='max-w-[330px] md:max-w-[440px]'>
             <Modal.Header title='Fund top up' />
             <Modal.Content className='flex flex-col items-center justify-center space-y-1200 p-1200'>
                 <Text bold>{title} Demo account</Text>
-                <Text bold>{getAccountTitle()} Demo account</Text>
                 <div className='text-center'>
                     <Text bold size='sm'>
                         Balance

3f73045812592030706d37f6871d79eee1f49452
ameerul hady <ameerul@deriv.com>
2024-01-18 15:08:01 +0800

    chore: updated deriv/ui library

--- packages/p2p-v2/src/routes/Home/index.tsx
+++ packages/p2p-v2/src/routes/Home/index.tsx
@@ -3,7 +3,6 @@ import { useActiveAccount } from '@deriv/api';
 import { MobileCloseHeader } from '../../components';
 import { useDevice } from '../../hooks';
 import './index.scss';
-import { NicknameModal } from '../../components/Modals/NicknameModal';
 
 const Home: React.FC<{ path: string }> = ({ path }) => {
     const { data: activeAccountData, isLoading } = useActiveAccount();
@@ -17,7 +16,6 @@ const Home: React.FC<{ path: string }> = ({ path }) => {
 
     return (
         <>
-            <NicknameModal />
             {isMobile && <MobileCloseHeader />}
             <div className='p2p-v2-home-style'>
                 <div> Welcome to P2P Version 2 from {path} Page ;) </div>

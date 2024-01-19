b4a5a87afb2425f4591a3deae5baafe558888884
Thisyahlen <thisyahlen@regentmarkets.com>
2024-01-19 09:18:14 +0800

    fix: undefined category in mt5 financial jurisdiction modal

--- packages/tradershub/src/features/cfd/screens/Jurisdiction/JurisdictionCard/JurisdictionCard.tsx
+++ packages/tradershub/src/features/cfd/screens/Jurisdiction/JurisdictionCard/JurisdictionCard.tsx
@@ -146,13 +146,17 @@ const JurisdictionCard = ({ isAdded = false, isSelected = false, jurisdiction, o
                                         <div className='flex gap-300'>
                                             {!(marketType in verificationDocs)
                                                 ? verificationDocumentsMapper.notApplicable.icon
-                                                : verificationDocs[marketType]?.map(doc => (
-                                                      <JurisdictionCardVerificationTag
-                                                          category={verificationDocumentsMapper[doc].category}
-                                                          icon={verificationDocumentsMapper[doc].icon}
-                                                          key={`verification-doc-${doc}`}
-                                                      />
-                                                  ))}
+                                                : verificationDocs[marketType]?.map(doc => {
+                                                      if (doc in verificationDocumentsMapper) {
+                                                          return (
+                                                              <JurisdictionCardVerificationTag
+                                                                  category={verificationDocumentsMapper[doc].category}
+                                                                  icon={verificationDocumentsMapper[doc].icon}
+                                                                  key={`verification-doc-${doc}`}
+                                                              />
+                                                          );
+                                                      }
+                                                  })}
                                         </div>
                                     );
                                 }

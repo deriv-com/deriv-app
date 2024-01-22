9ae6fdf6165420c515c7acdc7ab48b884be6a4d8
Sergei Baranovski <sergei@deriv.com>
2024-01-22 12:20:54 +0300

    feat: complete with responsive mode

--- packages/tradershub/src/features/cfd/modals/RegulationModal/Row.tsx
+++ packages/tradershub/src/features/cfd/modals/RegulationModal/Row.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { qtMerge } from '@deriv/quill-design';
-import { Text } from '@deriv-com/ui/dist/components/Text';
+import { Text, TextProps } from '@deriv-com/ui/dist/components/Text';
 import { TRegulatorsContentProps, TRowItem } from '../../../../constants/regulators-modal-content';
 
 const Row = ({ attribute, content, id, idx }: TRegulatorsContentProps) => (
-    <tr className={qtMerge('min-h-2000', idx === 0 && 'bg-brand-red-light bg-opacity-200')} key={id}>
-        <td className='sticky z-10 border-solid inset-y-50 start-50 py-500 px-400 border-75 border-system-light-active-background'>
+    <tr className={qtMerge('min-h-2000', idx === 0 && 'bg-system-light-red-light-background ')} key={id}>
+        <td
+            className={`sticky z-10 align-middle border-solid start-50 py-500 px-400 border-system-light-active-background border-x-75 border-b-75 ${
+                idx === 0 ? 'bg-[#ffe3e3]' : 'bg-system-light-primary-background'
+            }
+            `}
+        >
             <Text align='left' as='p' color='general' size='xs' weight='bold'>
                 {attribute}
             </Text>
         </td>
         {Object.keys(content).map(rowKey => (
             <td
-                className='flex-col justify-center border-solid whitespace-nowrap py-500 px-400 border-75 border-system-light-active-background'
+                className='flex-col justify-center align-middle border-solid whitespace-nowrap py-500 px-400 border-b-75 border-r-75 border-system-light-active-background'
                 key={rowKey}
             >
                 {Array.isArray(content[rowKey]) ? (
                     (content[rowKey] as TRowItem[])?.map(item => (
                         <Text
-                            align={item?.options?.align ?? 'center'}
+                            align={(item?.options?.align as TextProps['align']) ?? 'center'}
                             as='p'
-                            color={item?.options?.color ?? 'prominent'}
+                            color={(item?.options?.color as TextProps['color']) ?? 'prominent'}
                             key={`${id}_${rowKey}_${item?.text}`}
                             size='2xs'
-                            weight={item?.options?.weight}
+                            weight={item?.options?.weight as TextProps['weight']}
                         >
                             {item?.text}
                             {item?.options?.should_show_asterick_at_end && <Text color='error'>*</Text>}
@@ -31,11 +36,11 @@ const Row = ({ attribute, content, id, idx }: TRegulatorsContentProps) => (
                     ))
                 ) : (
                     <Text
-                        align={(content[rowKey] as TRowItem)?.options?.align ?? 'center'}
+                        align={((content[rowKey] as TRowItem)?.options?.align as TextProps['align']) ?? 'center'}
                         as='p'
-                        color={(content[rowKey] as TRowItem)?.options?.color ?? 'prominent'}
+                        color={((content[rowKey] as TRowItem)?.options?.color as TextProps['color']) ?? 'prominent'}
                         size='2xs'
-                        weight={(content[rowKey] as TRowItem)?.options?.weight}
+                        weight={(content[rowKey] as TRowItem)?.options?.weight as TextProps['weight']}
                     >
                         {(content[rowKey] as TRowItem)?.text}
                     </Text>

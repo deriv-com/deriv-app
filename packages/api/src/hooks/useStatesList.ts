f0a3e1f4fcf877aaebfc75dadbcf2ccb58829a90
arshad-rao-deriv <muhammad.arshad@deriv.com>
2024-01-22 13:38:19 +0400

    fix: Make the options parameter optional in useStatesList

--- packages/api/src/hooks/useStatesList.ts
+++ packages/api/src/hooks/useStatesList.ts
@@ -6,7 +6,7 @@ import { TSocketRequestQueryOptions } from '../../types';
 /** Custom hook to get states list for a particular country. */
 type TStatesList = Exclude<NonNullable<ReturnType<typeof useSettings>['data']['residence' | 'country']>, undefined>;
 
-const useStatesList = (country: TStatesList, options: TSocketRequestQueryOptions<'states_list'>) => {
+const useStatesList = (country: TStatesList, options?: TSocketRequestQueryOptions<'states_list'>) => {
     const { data, ...rest } = useQuery('states_list', {
         // @ts-expect-error The `states_list` type from `@deriv/api-types` is not correct.
         // The type should be `string`, but it's an alias to string type.

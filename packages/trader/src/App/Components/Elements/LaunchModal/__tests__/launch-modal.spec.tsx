c7eb068a80165bfa020001ff2de3a0f055af60e6
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-01-23 07:08:29 +0300

    [DTRA] / Kate / DTRA-680 / Add popup for "Turbos" launch (#12761)
    * refactor: launch modal for turbos
    
    * refactor: remove unused images
    
    * feat: add functionality for opening description
    
    * refactor: test cases
    
    * chore: remove unused icon
    
    * fix: add check for popup
    
    * fix: add one more setter

--- packages/trader/src/App/Components/Elements/LaunchModal/__tests__/launch-modal.spec.tsx
+++ packages/trader/src/App/Components/Elements/LaunchModal/__tests__/launch-modal.spec.tsx
@@ -7,8 +7,12 @@ import LaunchModal from '../launch-modal';
 
 const mocked_default_props = {
     handleChange: jest.fn(() => LocalStore.set('launchModalShown', JSON.stringify(true))),
+    setShowDescription: jest.fn(),
     open: true,
 };
+const launch_modal_id = 'launch-modal';
+
+jest.mock('Assets/SvgComponents/trade_explanations/img-turbos.svg', () => jest.fn(() => <div>Turbos Image</div>));
 
 describe('<LaunchModal />', () => {
     beforeAll(() => {
@@ -28,22 +32,23 @@ describe('<LaunchModal />', () => {
     it('should render launch modal ', () => {
         render(<LaunchModal {...mocked_default_props} />);
 
-        expect(screen.getByTestId('launch-modal')).toBeInTheDocument();
+        expect(screen.getByTestId(launch_modal_id)).toBeInTheDocument();
     });
 
-    it('should set the localStorage key launchModalShown to true on clicking the continue button', async () => {
+    it('should set the localStorage key launchModalShown to true on clicking the Ok button', async () => {
         render(<LaunchModal {...mocked_default_props} />);
-        expect(screen.getByTestId('launch-modal')).toBeInTheDocument();
+        expect(screen.getByTestId(launch_modal_id)).toBeInTheDocument();
 
-        const continue_btn = screen.getByRole('button', { name: 'Continue' });
+        const continue_btn = screen.getByRole('button', { name: 'Ok' });
         userEvent.click(continue_btn);
         const value = JSON.parse(LocalStore.get('launchModalShown') ?? 'false');
 
         expect(value).toBe(true);
     });
+
     it('should not display launch modal if open is equal to false', () => {
         render(<LaunchModal {...mocked_default_props} open={false} />);
 
-        expect(screen.queryByTestId('launch-modal')).not.toBeInTheDocument();
+        expect(screen.queryByTestId(launch_modal_id)).not.toBeInTheDocument();
     });
 });

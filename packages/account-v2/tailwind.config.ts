f938c0d92c1fa8930a2c6cd5891bd3c364579eff
Shahzaib <shahzaib@deriv.com>
2024-01-24 16:08:27 +0800

    chore: reset tailwind config

--- packages/account-v2/tailwind.config.ts
+++ packages/account-v2/tailwind.config.ts
@@ -1,24 +1,8 @@
 import type { Config } from 'tailwindcss';
 import QuillTailwindConfig from '@deriv/quill-design/quill-tailwind/tailwind.config.cjs';
-const plugin = require('tailwindcss/plugin');
 
 export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    plugins: [
-        plugin(({ addUtilities }) => {
-            addUtilities({
-                '.outline': {
-                    'outline-style': 'solid',
-                },
-                '.outline-1': {
-                    'outline-width': '1px',
-                },
-                '.pl-800': {
-                    'padding-left': '16px',
-                },
-            });
-        }),
-    ],
     presets: [QuillTailwindConfig],
     theme: {
         extend: {

c97a498d8c5ca204bcc482487ded6ae4c87d418e
kate-deriv <kate@regentmarkets.com>
2024-01-24 09:18:58 +0300

    refactor: revert changes

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info-header.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/ContractTypeInfo/contract-type-info-header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Localize } from '@deriv/translations';
 import { Icon, Text } from '@deriv/components';
 import { clickAndKeyEventHandler } from '@deriv/shared';
 
@@ -30,13 +29,9 @@ const Header = ({
                 <Icon icon='IcArrowLeftBold' />
             </span>
         )}
-        <Localize
-            i18n_default_text='<0>{{title}}</0>'
-            components={[
-                <Text key={0} size={text_size} weight='bold' color='prominent' className='contract-type-info__title' />,
-            ]}
-            values={{ title }}
-        />
+        <Text size={text_size} weight='bold' color='prominent' className='contract-type-info__title'>
+            {title}
+        </Text>
         {should_render_close && (
             <span
                 className='contract-type-info__icon-cross'

5c0731a831684cc69d2a82c6079d10254b2b2996
adrienne-rio <adrienne@deriv.com>
2024-01-25 14:31:35 +0800

    feat: added tabs for desktop view

--- packages/p2p-v2/.stylelintrc.js
+++ packages/p2p-v2/.stylelintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         'declaration-no-important': true,
         'selector-class-pattern': [
             // selectors must be prefixed with "p2p-v2-" to avoid name conflicts in other packages
-            '^p2p-v2-[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+            '^(p2p-v2|derivs)-[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
             {
                 message: 'Expected selector to match BEM CSS pattern and to be prefixed with "p2p-v2-"',
             },

fb18b6f48cd5661b4c7f6c992b9444a6389a6a6e
Nada <nada.ajinas@deriv.com>
2024-01-25 10:59:50 +0400

    fix: style fixes

--- packages/p2p-v2/src/pages/my-profile/screens/MyProfileCounterparties/MyProfileCounterparties.tsx
+++ packages/p2p-v2/src/pages/my-profile/screens/MyProfileCounterparties/MyProfileCounterparties.tsx
@@ -1,24 +1,32 @@
 import React, { PropsWithChildren, useState } from 'react';
 import { Text } from '@deriv-com/ui/dist/components/Text';
 import { p2p } from '@deriv/api';
+import { useDevice } from '../../../../hooks';
+import { BlockUnblockUserFilterModal } from '../../../../components/Modals/BlockUnblockUserFilterModal';
+import { FullPageMobileWrapper } from '../../../../components';
 import MyProfileCounterpartiesEmpty from './MyProfileCounterpartiesEmpty';
 import MyProfileCounterpartiesHeader from './MyProfileCounterpartiesHeader';
 import MyProfileCounterpartiesTable from './MyProfileCounterpartiesTable';
 import MyProfileCounterpartiesTableRow from './MyProfileCounterpartiesTableRow';
 import './MyProfileCounterparties.scss';
-import { BlockUnblockUserFilterModal } from '../../../../components/Modals/BlockUnblockUserFilterModal';
-import { useDevice } from '../../../../hooks';
-import { FullPageMobileWrapper } from '../../../../components';
 
 const MyProfileCounterpartiesDisplayWrapper = ({ children }: PropsWithChildren<unknown>) => {
     const { isMobile } = useDevice();
     if (isMobile) {
-        return (<FullPageMobileWrapper renderHeader={() => <Text weight='bold' size='md' className='p2p-v2-my-profile-counterparties__header'>My counterparties</Text>}>
-            {children}
-        </FullPageMobileWrapper>)
+        return (
+            <FullPageMobileWrapper
+                renderHeader={() => (
+                    <Text className='p2p-v2-my-profile-counterparties__header' size='md' weight='bold'>
+                        My counterparties
+                    </Text>
+                )}
+            >
+                {children}
+            </FullPageMobileWrapper>
+        );
     }
     return children;
-}
+};
 
 const MyProfileCounterparties = () => {
     const [searchValue, setSearchValue] = useState('');
@@ -46,19 +54,19 @@ const MyProfileCounterparties = () => {
     const onToggle = (value: string) => {
         setDropdownValue(value);
         setIsFilterModalOpen(false);
-    }
+    };
 
     return (
         <MyProfileCounterpartiesDisplayWrapper>
             <div className='p2p-v2-my-profile-counterparties'>
-                <Text as='p'>
-                    {`When you block someone, you won’t see their ads and they won’t be able to place orders on your ads`}
+                <Text as='p' size='sm'>
+                    {`When you block someone, you won’t see their ads, and they can’t see yours. Your ads will be hidden from their search results, too.`}
                 </Text>
                 <MyProfileCounterpartiesHeader
                     dropdownValue={dropdownValue}
+                    onClickFilter={onClickFilter}
                     setDropdownValue={setDropdownValue}
                     setSearchValue={setSearchValue}
-                    onClickFilter={onClickFilter}
                 />
                 <div className='p2p-v2-my-profile-counterparties__content'>
                     <MyProfileCounterpartiesTable
@@ -68,11 +76,20 @@ const MyProfileCounterparties = () => {
                         isLoading={isLoading}
                         loadMoreAdvertisers={loadMoreAdvertisers}
                         rowRender={item => (
-                            <MyProfileCounterpartiesTableRow id={item.id!} isBlocked={item.is_blocked} nickname={item.name!} />
+                            <MyProfileCounterpartiesTableRow
+                                id={item.id!}
+                                isBlocked={item.is_blocked}
+                                nickname={item.name!}
+                            />
                         )}
                     />
                 </div>
-                <BlockUnblockUserFilterModal isModalOpen={isFilterModalOpen} onRequestClose={() => setIsFilterModalOpen(false)} selected={dropdownValue}  onToggle={onToggle}/>
+                <BlockUnblockUserFilterModal
+                    isModalOpen={isFilterModalOpen}
+                    onRequestClose={() => setIsFilterModalOpen(false)}
+                    onToggle={onToggle}
+                    selected={dropdownValue}
+                />
             </div>
         </MyProfileCounterpartiesDisplayWrapper>
     );
--- packages/p2p-v2/src/pages/my-profile/screens/MyProfileCounterparties/MyProfileCounterpartiesTableRow.tsx
+++ packages/p2p-v2/src/pages/my-profile/screens/MyProfileCounterparties/MyProfileCounterpartiesTableRow.tsx
@@ -3,6 +3,7 @@ import { Text } from '@deriv-com/ui/dist/components/Text';
 import { Button } from '@deriv-com/ui/dist/components/Button';
 import { UserAvatar } from '../../../../components';
 import { BlockUnblockUserModal } from '../../../../components/Modals/BlockUnblockUserModal';
+import { useDevice } from '../../../../hooks';
 import './MyProfileCounterpartiesTableRow.scss';
 
 type TMyProfileCounterpartiesTableRowProps = {
@@ -13,15 +14,14 @@ type TMyProfileCounterpartiesTableRowProps = {
 
 const MyProfileCounterpartiesTableRow = ({ id, isBlocked, nickname }: TMyProfileCounterpartiesTableRowProps) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const { isMobile } = useDevice();
 
     return (
         <>
             <div className='p2p-v2-my-profile-counterparties-table-row'>
                 <div className='p2p-v2-my-profile-counterparties-table-row__nickname-wrapper'>
                     <UserAvatar nickname={nickname} size={32} textSize='md' />
-                    <Text lineHeight='md' size='sm'>
-                        {nickname}
-                    </Text>
+                    <Text size={isMobile ? 'md' : 'sm'}>{nickname}</Text>
                 </div>
                 {/* TODO: variant to be replaced after available in @deriv-com/ui */}
                 <Button onClick={() => setIsModalOpen(true)} variant={isBlocked ? 'outlined' : 'ghost'}>

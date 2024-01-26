6780eef2781e1e6e345e62cbb5a1d220ef98852f
Shahzaib <shahzaib@deriv.com>
2024-01-26 15:33:48 +0800

    Account v2 currency selector styles & selection (#13094)
    * chore: component docs
    
    * chore: currency selection with formik and the context
    
    * chore: currency selector type decription
    
    * chore: reset context upon wizard closing
    
    * chore: reset tailwind config
    
    * chore: removed unused imports
    
    * chore: fiat currency description

--- packages/account-v2/src/pages/CurrencySelector/Currencies.tsx
+++ packages/account-v2/src/pages/CurrencySelector/Currencies.tsx
@@ -1,28 +1,47 @@
 import React, { useMemo } from 'react';
-import { Heading, qtMerge, Text } from '@deriv/quill-design';
+import clsx from 'clsx';
+import { useFormikContext } from 'formik';
+import { Heading, Text } from '@deriv/quill-design';
 import { CURRENCY_TYPES, getCurrencyConfig } from '../../constants/currencyConfig';
+import CurrencyCard from './CurrencyCard';
 
 type TCurrencies = {
     type: keyof typeof CURRENCY_TYPES;
 };
 
+/**
+ * @name Currencies
+ * @description The Currencies component is used to display the currencies in the currency selector screen.
+ * @param {string} type - The type of the currency.
+ * @returns {React.ReactNode}
+ * @example <Currencies type={type} />
+ * @example <Currencies type={CURRENCY_TYPES.FIAT} />
+ */
 const Currencies = ({ type }: TCurrencies) => {
     const currencies = useMemo(() => getCurrencyConfig(type), [type]);
+    const { values } = useFormikContext<{ currency: string }>();
+
+    const isFiatCurrencySelected = currencies.find(currency => currency.id === values.currency);
+
     return (
         <div className='text-center'>
             <Heading.H5 className='mb-300'>
                 {type === CURRENCY_TYPES.CRYPTO ? 'Cryptocurrencies' : 'Fiat Currencies'}
             </Heading.H5>
-            <div className={qtMerge(`flex flex-wrap ${currencies.length < 4 ? 'justify-center' : ''}`)}>
-                {currencies.map(({ icon: Icon, key, title }) => (
-                    <div className='w-1/4 box-border my-1000 cursor-pointer' key={key}>
-                        <Icon />
-                        <Text className='my-200' size='sm'>
-                            {title}
-                        </Text>
-                        <Text size='sm'>({key})</Text>
-                    </div>
+            <div
+                className={clsx('flex flex-wrap', {
+                    'justify-center': currencies.length < 4,
+                })}
+            >
+                {currencies.map(currency => (
+                    <CurrencyCard {...currency} key={currency.id} />
                 ))}
+                {isFiatCurrencySelected && type === CURRENCY_TYPES.FIAT && (
+                    <Text className='mt-200' size='sm'>
+                        You are limited to one fiat account. You won’t be able to change your account currency if you
+                        have already made your first deposit or created a real Deriv MT5 or Deriv X account.
+                    </Text>
+                )}
             </div>
         </div>
     );

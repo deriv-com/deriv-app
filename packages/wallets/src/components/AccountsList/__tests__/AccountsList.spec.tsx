cb9194703b6aaaae6b53ed08b862005a298baff5
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-01-29 12:38:32 +0800

    chore: ✨ unit tests for accoutslist component

---
+++ packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
@@ -0,0 +1,40 @@
+import React, { PropsWithChildren } from 'react';
+import { APIProvider } from '@deriv/api';
+import { render, screen } from '@testing-library/react';
+import useDevice from '../../../hooks/useDevice';
+import { ModalProvider } from '../../ModalProvider';
+import AccountsList from '../AccountsList';
+
+jest.mock('../../../hooks/useDevice');
+const mockUseDevice = useDevice as jest.MockedFunction<typeof useDevice>;
+
+const wrapper = ({ children }: PropsWithChildren) => (
+    <APIProvider>
+        <ModalProvider>{children}</ModalProvider>
+    </APIProvider>
+);
+
+describe('AccountsList', () => {
+    it('should render account list in mobile view', () => {
+        mockUseDevice.mockReturnValue({
+            isDesktop: false,
+            isMobile: true,
+            isTablet: false,
+        });
+        render(<AccountsList isWalletSettled={true} />, { wrapper });
+        expect(screen.getByTestId('dt_tabs')).toBeInTheDocument();
+        expect(screen.getByText('CFDs')).toBeInTheDocument();
+        expect(screen.getByText('Options & multipliers')).toBeInTheDocument();
+    });
+
+    it('should render account list in desktop view', () => {
+        mockUseDevice.mockReturnValue({
+            isDesktop: true,
+            isMobile: false,
+            isTablet: false,
+        });
+        render(<AccountsList isWalletSettled={true} />, { wrapper });
+
+        expect(screen.getByTestId('dt_desktop_accounts_list')).toBeInTheDocument();
+    });
+});

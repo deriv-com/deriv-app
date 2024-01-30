e7aef1410bc19491da58360db6fd2016f7b6bdec
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-01-30 10:59:39 +0800

    chore: 🔧 refactor tests

--- packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
+++ packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
@@ -23,6 +23,8 @@ describe('AccountsList', () => {
         });
         render(<AccountsList isWalletSettled={true} />, { wrapper });
         expect(screen.getByTestId('dt_tabs')).toBeInTheDocument();
+        expect(screen.getByTestId('dt_tab_list')).toBeInTheDocument();
+        expect(screen.getByTestId('dt_tab_panels')).toBeInTheDocument();
         expect(screen.getByText('CFDs')).toBeInTheDocument();
         expect(screen.getByText('Options & multipliers')).toBeInTheDocument();
     });
@@ -36,5 +38,7 @@ describe('AccountsList', () => {
         render(<AccountsList isWalletSettled={true} />, { wrapper });
 
         expect(screen.getByTestId('dt_desktop_accounts_list')).toBeInTheDocument();
+        expect(screen.getByText('CFDs')).toBeInTheDocument();
+        expect(screen.getByText('Options & Multipliers')).toBeInTheDocument();
     });
 });

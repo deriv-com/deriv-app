b6fd682c26f076b87a1046675f8ad078bd2617ff
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-01-30 16:01:44 +0800

    chore: ✨ add tests

--- packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
+++ packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
@@ -1,12 +1,23 @@
-import React, { PropsWithChildren } from 'react';
+import React, { ComponentProps, PropsWithChildren } from 'react';
 import { APIProvider } from '@deriv/api';
 import { render, screen } from '@testing-library/react';
 import useDevice from '../../../hooks/useDevice';
 import { ModalProvider } from '../../ModalProvider';
+import WalletMobileTourGuide from '../../WalletTourGuide/WalletMobileTourGuide';
 import AccountsList from '../AccountsList';
 
 jest.mock('../../../hooks/useDevice');
 const mockUseDevice = useDevice as jest.MockedFunction<typeof useDevice>;
+
+const mockWalletMobileTourGuide = jest.fn();
+jest.mock(
+    '../../WalletTourGuide/WalletMobileTourGuide',
+    // eslint-disable-next-line react/display-name
+    () => (props: ComponentProps<typeof WalletMobileTourGuide>) => {
+        mockWalletMobileTourGuide(props);
+        return <p>mock wallet tour guide</p>;
+    }
+);
 
 const wrapper = ({ children }: PropsWithChildren) => (
     <APIProvider>
@@ -40,5 +51,25 @@ describe('AccountsList', () => {
         expect(screen.getByTestId('dt_desktop_accounts_list')).toBeInTheDocument();
         expect(screen.getByText('CFDs')).toBeInTheDocument();
         expect(screen.getByText('Options & Multipliers')).toBeInTheDocument();
+    });
+
+    it('should render wallet tour guide in mobile view with isWalletSettled set to false', () => {
+        mockUseDevice.mockReturnValue({
+            isDesktop: false,
+            isMobile: true,
+            isTablet: false,
+        });
+        render(<AccountsList isWalletSettled={false} />, { wrapper });
+        expect(mockWalletMobileTourGuide).toBeCalledWith(expect.objectContaining({ isWalletSettled: false }));
+    });
+
+    it('should render wallet tour guide in mobile view with isWalletSettled set to true', () => {
+        mockUseDevice.mockReturnValue({
+            isDesktop: false,
+            isMobile: true,
+            isTablet: false,
+        });
+        render(<AccountsList isWalletSettled={true} />, { wrapper });
+        expect(mockWalletMobileTourGuide).toBeCalledWith(expect.objectContaining({ isWalletSettled: true }));
     });
 });

bdd7b6561243c01ddcd0bc4377a27f833a0b4182
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-01-30 13:04:47 +0800

    fix: 🔧 checking active loginid from localstorage instead of authorize hook to prevent extra calls

--- packages/api/src/hooks/useWalletMigration.ts
+++ packages/api/src/hooks/useWalletMigration.ts
@@ -1,8 +1,8 @@
 import { useCallback } from 'react';
-import useAuthorize from './useAuthorize';
 import useInvalidateQuery from '../useInvalidateQuery';
 import useMutation from '../useMutation';
 import useQuery from '../useQuery';
+import { getActiveLoginIDFromLocalStorage } from '@deriv/utils';
 
 /** A custom hook to get the status of wallet_migration API and to start/reset the migration process */
 const useWalletMigration = () => {
@@ -11,14 +11,14 @@ const useWalletMigration = () => {
     /** Make a request to wallet_migration API and onSuccess it will invalidate the cached data  */
     const { mutate } = useMutation('wallet_migration', { onSuccess: () => invalidate('wallet_migration') });
 
-    const { isSuccess } = useAuthorize();
+    const activeLoginID = getActiveLoginIDFromLocalStorage();
 
     /** Fetch the wallet_migration API and refetch it every second if the status is in_progress */
     const { data } = useQuery('wallet_migration', {
         payload: { wallet_migration: 'state' },
         options: {
             refetchInterval: response => (response?.wallet_migration?.state === 'in_progress' ? 500 : false),
-            enabled: isSuccess,
+            enabled: !!activeLoginID,
         },
     });
 

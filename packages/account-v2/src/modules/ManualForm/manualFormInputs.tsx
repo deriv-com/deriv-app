d6d67a7632e3e2c1b0587ce0a92ed3ad1a71076c
Shaheer <shaheer@deriv.com>
2024-01-31 20:13:28 +0400

    feat: checks country specific expiry date field

--- packages/account-v2/src/modules/ManualForm/manualFormInputs.tsx
+++ packages/account-v2/src/modules/ManualForm/manualFormInputs.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Field, FieldProps } from 'formik';
+import { useSettings } from '@deriv/api';
 import { WalletDatePicker } from '../../components/base/WalletDatePicker';
 import { WalletText } from '../../components/base/WalletText';
 import { WalletTextField } from '../../components/base/WalletTextField';
@@ -9,7 +10,9 @@ import { getFieldsData, getTitleForFormInputs } from '../../helpers/manualFormHe
 type TManualFormInputsProps = { selectedDocument: TManualDocumentTypes };
 
 export const ManualFormInputs = ({ selectedDocument }: TManualFormInputsProps) => {
+    const { data: settings } = useSettings();
     const fieldsData = getFieldsData(selectedDocument);
+    const isExpiryDateRequired = settings?.country !== 'ng';
 
     return (
         <Fragment>
@@ -25,17 +28,19 @@ export const ManualFormInputs = ({ selectedDocument }: TManualFormInputsProps) =
                         />
                     )}
                 </Field>
-                <Field name='document_expiry'>
-                    {({ field, meta }: FieldProps) => (
-                        <WalletDatePicker
-                            {...field}
-                            errorMessage={meta.error}
-                            isInvalid={meta.touched && Boolean(meta.error)}
-                            label={fieldsData.documentExpiry.label}
-                            onDateChange={field.onChange}
-                        />
-                    )}
-                </Field>
+                {isExpiryDateRequired && (
+                    <Field name='document_expiry'>
+                        {({ field, meta }: FieldProps) => (
+                            <WalletDatePicker
+                                {...field}
+                                errorMessage={meta.error}
+                                isInvalid={meta.touched && Boolean(meta.error)}
+                                label={fieldsData.documentExpiry.label}
+                                onDateChange={field.onChange}
+                            />
+                        )}
+                    </Field>
+                )}
             </div>
         </Fragment>
     );

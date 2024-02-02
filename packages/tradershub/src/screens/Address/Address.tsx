88b8fb319d0e65347595c7c899bd7ad8fdaa1f98
Thisyahlen <thisyahlen@regentmarkets.com>
2024-02-02 11:59:24 +0800

    chore: poa screen in real account signup

--- packages/tradershub/src/screens/Address/Address.tsx
+++ packages/tradershub/src/screens/Address/Address.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Form, Formik, FormikValues } from 'formik';
+import { Input, Text, Dropdown } from '@deriv-com/ui';
 import Actions from '../../flows/RealAccountSIgnup/SignupWizard/Actions';
 import WizardScreenWrapper from '../../flows/RealAccountSIgnup/SignupWizard/WizardScreenWrapper';
 import { ACTION_TYPES, useSignupWizardContext } from '../../providers/SignupWizardProvider/SignupWizardContext';
+import { useSettings, useStatesList } from '@deriv/api';
+import { LabelPairedChevronDownMdRegularIcon } from '@deriv/quill-icons';
 
 /**
  * @name Address
@@ -11,6 +14,9 @@ import { ACTION_TYPES, useSignupWizardContext } from '../../providers/SignupWiza
  * @returns {React.ReactNode}
  */
 const Address = () => {
+    const { data: getSettings } = useSettings();
+    const country = getSettings?.country_code ?? '';
+    const { data: statesList } = useStatesList(country);
     const { dispatch } = useSignupWizardContext();
 
     const handleSubmit = (values: FormikValues) => {
@@ -27,13 +33,31 @@ const Address = () => {
                 {() => (
                     <Form className='flex flex-col flex-grow w-full overflow-y-auto'>
                         <div className='flex-1 overflow-y-auto p-1200'>
-                            <p className='font-bold text-50'>
-                                Only use an address for which you have proof of residence -
-                            </p>
-                            <p className='text-50'>
-                                a recent utility bill (e.g. electricity, water, gas, landline or internet), bank
-                                statement, or government-issued letter with your name and this address.
-                            </p>
+                            <div className='flex flex-col'>
+                                <Text weight='bold' size='sm'>
+                                    Only use an address for which you have proof of residence -
+                                </Text>
+                                <Text size='sm'>
+                                    a recent utility bill (e.g. electricity, water, gas, landline or internet), bank
+                                    statement, or government-issued letter with your name and this address.
+                                </Text>
+                            </div>
+                            <div className='flex flex-col items-center self-stretch gap-2000 mt-1500'>
+                                <Input label='First line of address*' name='firstName' />
+                                <Input label='Second line of address' name='lastName' />
+                                <Input label='Town/City*' name='townCity' />
+                                <div>
+                                    <Dropdown
+                                        variant='comboBox'
+                                        label='State/Province'
+                                        list={statesList}
+                                        name='stateProvince'
+                                        onSelect={() => {}}
+                                        dropdownIcon={<LabelPairedChevronDownMdRegularIcon />}
+                                    />
+                                </div>
+                                <Input className='text-body-sm' label='Postal/ZIP Code' name='zipCode' />
+                            </div>
                         </div>
                         <Actions />
                     </Form>

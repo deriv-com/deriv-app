c0c5b15fae04fc357ea378757f7aa1f50ce7d33a
Nada <nada.ajinas@deriv.com>
2024-02-02 16:33:24 +0400

    fix: coveralls

--- packages/p2p-v2/src/pages/my-ads/components/AdStatus/__tests__/AdStatus.spec.tsx
+++ packages/p2p-v2/src/pages/my-ads/components/AdStatus/__tests__/AdStatus.spec.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { APIProvider } from '@deriv/api';
 import { render, screen } from '@testing-library/react';
 import AdStatus from '../AdStatus';
 
+const wrapper = ({ children }: { children: JSX.Element }) => <APIProvider>{children}</APIProvider>;
+
 describe('AdStatus', () => {
     it('should render the component as expected with Inactive as default', () => {
-        render(<AdStatus />);
+        render(<AdStatus />, { wrapper });
         expect(screen.getByText('Inactive')).toBeInTheDocument();
     });
     it('should render active when isActive is true', () => {
-        render(<AdStatus isActive={true} />);
+        render(<AdStatus isActive={true} />, { wrapper });
         expect(screen.getByText('Active')).toBeInTheDocument();
     });
 });

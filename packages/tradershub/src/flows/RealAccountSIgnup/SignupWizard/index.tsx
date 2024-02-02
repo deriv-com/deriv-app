44e16c7a525d4036fe77f96b2a2cd0f276aae7fe
Sergei Baranovski <sergei@deriv.com>
2024-02-02 11:25:54 +0300

    feat: delete as='p' from Text component because it's default from now

--- packages/tradershub/src/flows/RealAccountSIgnup/SignupWizard/index.tsx
+++ packages/tradershub/src/flows/RealAccountSIgnup/SignupWizard/index.tsx
@@ -42,9 +42,7 @@ const SignupWizard = () => {
             <div className='bg-background-primary-base md:max-h-[717px] md:max-w-[1040px] h-screen w-screen md:rounded-800 flex overflow-hidden'>
                 {!isMobile && (
                     <div className='min-w-[256px] bg-system-light-secondary-background p-1200'>
-                        <Text as='p' className='font-bold pt-1600 pb-1200 text-300'>
-                            Add a Deriv Account
-                        </Text>
+                        <Text className='font-bold pt-1600 pb-1200 text-300'>Add a Deriv Account</Text>
                         <ProgressBar activeStep={currentStep} steps={FORM_PROGRESS_STEPS} />
                     </div>
                 )}

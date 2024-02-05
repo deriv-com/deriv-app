76de2d3c27782f505b66b26ba6529af077737bd2
arshad-rao-deriv <muhammad.arshad@deriv.com>
2024-02-05 12:42:17 +0400

    test: Added unit tests for POAForm

---
+++ packages/account-v2/src/components/DemoMessage/__tests__/DemoMessage.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { DemoMessage } from '../DemoMessage';
+import { useAuthorize } from '@deriv/api';
+
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useAuthorize: jest.fn(() => ({
+        data: {
+            account_list: [
+                {
+                    is_virtual: 0,
+                },
+            ],
+        },
+    })),
+}));
+
+describe('DemoMessage', () => {
+    it('renders the demo message button if any real account', () => {
+        render(<DemoMessage />);
+        expect(screen.getByText('Switch to real account')).toBeInTheDocument();
+    });
+
+    it('renders the demo message button if no real account', () => {
+        (useAuthorize as jest.Mock).mockImplementation(() => ({
+            data: {
+                account_list: [
+                    {
+                        is_virtual: 1,
+                    },
+                ],
+            },
+        }));
+        render(<DemoMessage />);
+        expect(screen.getByText('Add a real account')).toBeInTheDocument();
+    });
+});
---
+++ packages/account-v2/src/containers/POAForm/__tests__/AddressDetailsForm.spec.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import { APIProvider, useSettings } from '@deriv/api';
+import { useBreakpoint } from '@deriv/quill-design';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { AddressDetailsForm } from '../AddressDetailsForm';
+
+jest.mock('@deriv/quill-design', () => ({
+    useBreakpoint: jest.fn(() => ({ isMobile: false })),
+}));
+
+jest.mock('../DocumentSubmission', () => {
+    const DocumentSubmission = () => <div>DocumentSubmission</div>;
+    DocumentSubmission.displayName = 'DocumentSubmission';
+    return DocumentSubmission;
+});
+
+const updateSettings = jest.fn();
+const mockUploadDocument = jest.fn();
+const mockDefaultSettings = {
+    data: {
+        country_code: 'id',
+    },
+    error: undefined,
+    mutation: {
+        error: undefined,
+        isLoading: false,
+        mutateAsync: updateSettings,
+    },
+};
+
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useDocumentUpload: jest.fn(() => ({
+        error: undefined,
+        isLoading: false,
+        upload: mockUploadDocument,
+    })),
+    useInvalidateQuery: jest.fn(() => jest.fn()),
+    useSettings: jest.fn(() => mockDefaultSettings),
+}));
+
+describe('AddressDetailsForm', () => {
+    it('should render the form with initial values', () => {
+        (useSettings as jest.Mock).mockImplementation(() => ({
+            ...mockDefaultSettings,
+            data: {
+                address_city: 'New York',
+                address_line_1: '125 Main St',
+                address_line_2: 'Apt 4B',
+                address_postcode: '10001',
+                address_state: 'NY',
+            },
+        }));
+        render(
+            <APIProvider>
+                <AddressDetailsForm />
+            </APIProvider>
+        );
+
+        expect(screen.getByLabelText('Town/City*')).toHaveValue('New York');
+        expect(screen.getByLabelText('First line of address*')).toHaveValue('125 Main St');
+        expect(screen.getByLabelText('Second line of address')).toHaveValue('Apt 4B');
+        expect(screen.getByLabelText('Postal/ZIP Code')).toHaveValue('10001');
+        expect(screen.getByLabelText('State/Province')).toHaveValue('NY');
+    });
+
+    it('should submit the form with updated values when "Save and Submit" button is clicked', async () => {
+        render(
+            <APIProvider>
+                <AddressDetailsForm />
+            </APIProvider>
+        );
+
+        // Simulate user input
+        const city = screen.getByLabelText('Town/City*');
+        userEvent.clear(city);
+        userEvent.type(city, 'London');
+        const address1 = screen.getByLabelText('First line of address*');
+        userEvent.clear(address1);
+        userEvent.type(address1, '121 Main St');
+        const address2 = screen.getByLabelText('Second line of address');
+        userEvent.clear(address2);
+        userEvent.type(address2, 'Apt 4AC');
+        const zipCode = screen.getByLabelText('Postal/ZIP Code');
+        userEvent.clear(zipCode);
+        userEvent.type(zipCode, '1011');
+        const state = screen.getByLabelText('State/Province');
+        userEvent.clear(state);
+        userEvent.type(state, 'GB');
+
+        const submitButton = screen.getByText('Save and Submit');
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(updateSettings).toHaveBeenCalledWith({
+                payload: {
+                    address_city: 'London',
+                    address_line_1: '121 Main St',
+                    address_line_2: 'Apt 4AC',
+                    address_postcode: '1011',
+                    address_state: 'GB',
+                },
+            });
+            expect(mockUploadDocument).toHaveBeenCalled();
+        });
+    });
+
+    it('should show Proof of address on Mobile', () => {
+        (useBreakpoint as jest.Mock).mockImplementation(() => ({ isMobile: true }));
+
+        render(
+            <APIProvider>
+                <AddressDetailsForm />
+            </APIProvider>
+        );
+
+        expect(screen.getByText('Proof of address')).toBeInTheDocument();
+    });
+
+    it('should show resubmitting message', () => {
+        render(
+            <APIProvider>
+                <AddressDetailsForm resubmitting />
+            </APIProvider>
+        );
+
+        expect(screen.getByText(/We were unable to verify your address/)).toBeInTheDocument();
+    });
+
+    it('should show error component if get api returns error', () => {
+        (useSettings as jest.Mock).mockImplementation(() => ({
+            ...mockDefaultSettings,
+            error: { error: { message: 'Settings not available' } },
+        }));
+
+        render(
+            <APIProvider>
+                <AddressDetailsForm resubmitting />
+            </APIProvider>
+        );
+
+        expect(screen.getByText(/Settings not available/)).toBeInTheDocument();
+    });
+
+    it('should show error if update api returns error', () => {
+        (useSettings as jest.Mock).mockImplementation(() => ({
+            ...mockDefaultSettings,
+            mutation: { error: { error: { message: 'Settings not updated' } } },
+        }));
+
+        render(
+            <APIProvider>
+                <AddressDetailsForm resubmitting />
+            </APIProvider>
+        );
+
+        expect(screen.getByText(/Settings not updated/)).toBeInTheDocument();
+    });
+});

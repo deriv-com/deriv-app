a664847c06592b037ea7149f2dc45e273ade8e96
Sergei Baranovski <sergei@deriv.com>
2024-02-05 14:00:36 +0300

    feat: write tests for ActionScreen component

--- packages/tradershub/src/components/ActionScreen/__tests__/ActionScreen.spec.tsx
+++ packages/tradershub/src/components/ActionScreen/__tests__/ActionScreen.spec.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ActionScreen from '../ActionScreen';
 
-// jest.mock('@deriv/quill-design', () => ({
-//     ...jest.requireActual('@deriv/quill-design'),
-//     // useFlow: jest.fn(() => mockUseFlow),
-// }));
+jest.mock('@deriv/quill-design', () => {
+    return {
+        qtMerge: jest.fn(),
+    };
+});
+jest.mock('@deriv-com/ui', () => {
+    return {
+        Text: jest.fn(({ children }) => <span>{children}</span>),
+    };
+});
 
 describe('ActionScreen component', () => {
     it('renders with title', () => {
@@ -14,46 +20,38 @@ describe('ActionScreen component', () => {
         expect(titleElement).toBeInTheDocument();
     });
 
-    // it('renders with description', () => {
-    //     render(<ActionScreen description='Description' />);
-    //     const descriptionElement = screen.getByText('Description');
-    //     expect(descriptionElement).toBeInTheDocument();
-    // });
+    it('renders with description', () => {
+        render(<ActionScreen description='Description' />);
+        const descriptionElement = screen.getByText('Description');
+        expect(descriptionElement).toBeInTheDocument();
+    });
 
-    // it('renders with custom icon', () => {
-    //     const CustomIcon = () => <div>Custom Icon</div>;
-    //     render(<ActionScreen icon={<CustomIcon />} />);
-    //     const iconElement = screen.getByText('Custom Icon');
-    //     expect(iconElement).toBeInTheDocument();
-    // });
+    it('renders if description is valid ReactNode', () => {
+        render(<ActionScreen description={<span>Valid React node</span>} />);
+        const descriptionElement = screen.getByText('Valid React node');
+        expect(descriptionElement).toBeInTheDocument();
+    });
 
-    // it('calls renderButtons function', () => {
-    //     const renderButtons = jest.fn();
-    //     render(<ActionScreen renderButtons={renderButtons} />);
-    //     expect(renderButtons).toHaveBeenCalled();
-    // });
+    it('renders with custom icon', () => {
+        const CustomIcon = () => <div>Custom Icon</div>;
+        render(<ActionScreen description='Description' icon={<CustomIcon />} />);
+        const iconElement = screen.getByText('Custom Icon');
+        expect(iconElement).toBeInTheDocument();
+    });
 
-    // it('renders children', () => {
-    //     render(
-    //         <ActionScreen>
-    //             <div>Child</div>
-    //         </ActionScreen>
-    //     );
-    //     const childElement = screen.getByText('Child');
-    //     expect(childElement).toBeInTheDocument();
-    // });
+    it('calls renderButtons function', () => {
+        const renderButtons = jest.fn();
+        render(<ActionScreen description='Description' renderButtons={renderButtons} />);
+        expect(renderButtons).toHaveBeenCalled();
+    });
 
-    // it('applies custom className', () => {
-    //     render(<ActionScreen className='custom-class' />);
-    //     const component = screen.getByTestId('action-screen');
-    //     expect(component).toHaveClass('custom-class');
-    // });
-
-    // it('renders with default title and description size', () => {
-    //     render(<ActionScreen description='Description' title='Title' />);
-    //     const titleElement = screen.getByTestId('title');
-    //     const descriptionElement = screen.getByTestId('description');
-    //     expect(titleElement).toHaveStyle('font-size: 16px');
-    //     expect(descriptionElement).toHaveStyle('font-size: 14px');
-    // });
+    it('renders children', () => {
+        render(
+            <ActionScreen description='Description'>
+                <div>Child</div>
+            </ActionScreen>
+        );
+        const childElement = screen.getByText('Child');
+        expect(childElement).toBeInTheDocument();
+    });
 });

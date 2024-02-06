5ab5c48e114e3fd8a234059487f08eb957afb32a
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-02-06 16:57:29 +0800

    style: remove unneccessary css

--- packages/tradershub/src/features/cfd/screens/EnterPassword/EnterPassword.tsx
+++ packages/tradershub/src/features/cfd/screens/EnterPassword/EnterPassword.tsx
@@ -57,15 +57,13 @@ const EnterPassword = ({
         platform === PlatformDetails.dxtrade.platform ? accountType : marketTypeDetails[marketType]?.title;
 
     return (
-        <div className='flex ps-800 w-full lg:inline-flex lg:w-[400px] flex-col justify-center items-start rounded-400 border-sm bg-system-light-primary-background'>
-            <div className='flex flex-col w-full'>
-                <Text weight='bold'>Enter your {title} password</Text>
-                <div className='flex flex-col gap-800 lg:gap-400 lg:py-1200'>
-                    <Text size='sm'>
-                        Enter your {title} password to add a {title} {marketTypeTitle} account.
-                    </Text>
-                    <PasswordInput label={`${title} password`} onChange={onPasswordChange} value={password} />
-                </div>
+        <div className='flex w-full lg:inline-flex lg:w-[400px] flex-col justify-center items-start rounded-400 border-sm bg-system-light-primary-background'>
+            <Text weight='bold'>Enter your {title} password</Text>
+            <div className='flex flex-col gap-800 lg:gap-400 lg:py-1200'>
+                <Text size='sm'>
+                    Enter your {title} password to add a {title} {marketTypeTitle} account.
+                </Text>
+                <PasswordInput label={`${title} password`} onChange={onPasswordChange} value={password} />
             </div>
             {isDesktop && (
                 <div className='flex items-center justify-end w-full gap-400 pt-1200'>

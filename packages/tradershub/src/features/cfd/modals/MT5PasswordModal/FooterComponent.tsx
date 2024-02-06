3861b30ebca39dc30c73c0b8d2cf90a70a562290
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-02-06 11:46:38 +0800

    chore: remove passing prop to hook, get from CFDProvider

--- packages/tradershub/src/features/cfd/modals/MT5PasswordModal/FooterComponent.tsx
+++ packages/tradershub/src/features/cfd/modals/MT5PasswordModal/FooterComponent.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useMT5AccountsList } from '@deriv/api';
-import { Provider } from '@deriv/library';
 import useMT5AccountHandler from '../../../../hooks/useMT5AccountHandler';
-import { MarketType, TTM5FilterLandingCompany } from '../../constants';
 import AddAccountButtonsGroup from './ButtonGroups/AddAccountButtonsGroup';
 import CreateAccountButton from './ButtonGroups/CreateAccountButton';
 import SuccessButtonGroup from './ButtonGroups/SuccessButtonGroup';
@@ -13,13 +11,7 @@ type TFooterComponentProps = {
 
 const FooterComponent = ({ password }: TFooterComponentProps) => {
     const { data: mt5Accounts } = useMT5AccountsList();
-    const { getCFDState } = Provider.useCFDContext();
-    const marketType = getCFDState('marketType') ?? MarketType.ALL;
-    const selectedJurisdiction = getCFDState('selectedJurisdiction') as TTM5FilterLandingCompany;
-    const { isCreateMT5AccountSuccess } = useMT5AccountHandler({
-        marketType,
-        selectedJurisdiction,
-    });
+    const { isCreateMT5AccountSuccess } = useMT5AccountHandler();
     const hasMT5Account = mt5Accounts?.find(account => account.login);
 
     if (isCreateMT5AccountSuccess) return <SuccessButtonGroup />;

a5cabf6d681db97194354db5a195f9f7b170fbca
Shaheer <122449658+shaheer-deriv@users.noreply.github.com>
2024-02-07 14:40:17 +0400

    [Account-v2] Shaheer / COJ-576 / Document upload screen (#13267)
    * feat: adds manual upload screen
    
    * feat: checks country specific expiry date field
    
    * test: adds test cases
    
    * test: adds test cases
    
    * feat: adds required asterisk
    
    * refactor: refactors util
    
    * feat: adds dropzone
    
    * refactor: cleans yup validation
    
    * feat: moves logic to hook
    
    * refactor: refactors border color
    
    * feat: refactors border color
    
    * feat: uses input from deriv ui
    
    * refactor: refactors input field
    
    * feat: footer responsive
    
    * feat: input fields responsive
    
    * feat: manual form responsive
    
    * docs: adds comment
    
    * fix: fixes formik value
    
    * test: mocks useDevice
    
    * feat: adds aria label
    
    * feat: adds onsubmit handler
    
    * test: resolves test
    
    * fix: removes class usage
    
    * fix: fixes type issues
    
    * fix: adds todo comment
    
    * fix: adds missing test id
    
    * fix: fixes circleci issues
    
    * fix: fixes sonarcloud issue
    
    * fix: ignore style lint
    
    * fix: fixes class usage
    
    * feat: adds dark class
    
    * fix: resolves review comments
    
    * fix: removes tailwind config
    
    * fix: removes class usage
    
    * fix: removes dark class
    
    * fix: adds width class
    
    * fix: fixes deriv ui import

--- packages/account-v2/src/App.tsx
+++ packages/account-v2/src/App.tsx
@@ -7,6 +7,7 @@ import { BreakpointProvider } from '@deriv/quill-design';
 import { DOCUMENT_LIST, INITIAL_VALUES, SELECTED_COUNTRY } from './mocks/idv-form.mock';
 import { IDVForm } from './modules/IDVForm';
 import { getIDVFormValidationSchema } from './modules/IDVForm/utils';
+import { ManualForm } from './modules/ManualForm';
 import RouteLinks from './router/components/route-links/route-links';
 import './index.scss';
 
@@ -18,6 +19,8 @@ const App: React.FC = () => {
         <APIProvider standalone>
             <BreakpointProvider>
                 <div className=' text-solid-slate-500 text-heading-h1'>Account V2</div>
+                {/* TODO - Remove this ManualForm later, only rendered here for testing purposes. */}
+                <ManualForm onSubmit={() => {}} selectedDocument='driving_licence' />
                 {/* [TODO]:Mock - Remove Mock values */}
                 <Formik initialValues={INITIAL_VALUES} onSubmit={() => {}} validationSchema={getValidationSchema}>
                     <IDVForm selectedCountry={SELECTED_COUNTRY} />

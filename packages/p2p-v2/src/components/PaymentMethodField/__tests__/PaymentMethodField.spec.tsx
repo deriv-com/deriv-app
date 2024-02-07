63f22bfdf7260df3133f8389af4f8758e9644bf9
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-02-07 11:14:39 +0800

    chore: update selector

--- packages/p2p-v2/src/components/PaymentMethodField/__tests__/PaymentMethodField.spec.tsx
+++ packages/p2p-v2/src/components/PaymentMethodField/__tests__/PaymentMethodField.spec.tsx
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react';
 import PaymentMethodField from '../PaymentMethodField';
 
 jest.mock('react-hook-form', () => ({
+    ...jest.requireActual('react-hook-form'),
     Controller: ({ control, defaultValue, name, render }) =>
         render({
             field: { control, name, onBlur: jest.fn(), onChange: jest.fn(), value: defaultValue },
@@ -19,11 +20,11 @@ const mockUseForm = useForm as jest.MockedFunction<typeof useForm>;
 describe('PaymentMethodField', () => {
     const { control } = mockUseForm();
     it('should render a textarea when the field prop is set to instructions', () => {
-        render(<PaymentMethodField control={control} defaultValue='' displayName='test' field='instructions' />);
-        expect(screen.getByText('test')).toBeInTheDocument();
+        render(<PaymentMethodField control={control} defaultValue='' displayName='textarea' field='instructions' />);
+        expect(screen.getByText('textarea')).toBeInTheDocument();
     });
     it('should render an input when the field prop is set to text', () => {
         render(<PaymentMethodField control={control} defaultValue='' displayName='input' field='text' required />);
-        expect(screen.getByText('input')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('input')).toBeInTheDocument();
     });
 });

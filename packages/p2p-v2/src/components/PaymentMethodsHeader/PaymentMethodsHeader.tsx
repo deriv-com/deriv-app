56cd18c56d934acdb3156b841886ecf92cf3c2ca
ameerul hady <ameerul@deriv.com>
2024-02-07 09:54:44 +0800

    fix: failing test cases

--- packages/p2p-v2/src/components/PaymentMethodsHeader/PaymentMethodsHeader.tsx
+++ packages/p2p-v2/src/components/PaymentMethodsHeader/PaymentMethodsHeader.tsx
@@ -18,7 +18,12 @@ type TPaymentMethodsHeaderProps = {
 const PaymentMethodsHeader = ({ onGoBack, title }: TPaymentMethodsHeaderProps) => (
     <div className='p2p-v2-payment-method-form__header'>
         <div className='p2p-v2-payment-method-form__back-button'>
-            {onGoBack && <LabelPairedArrowLeftLgBoldIcon onClick={onGoBack} />}
+            {onGoBack && (
+                <LabelPairedArrowLeftLgBoldIcon
+                    data-testid='dt_p2p_v2_payment_methods_header_left_arrow_icon'
+                    onClick={onGoBack}
+                />
+            )}
         </div>
         {/* TODO: Remember to translate this */}
         <Text size='sm' weight='bold'>

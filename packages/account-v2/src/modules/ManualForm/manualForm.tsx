0c88d35b74bb284ff8ec78733fcc92765ba527fa
Shaheer <shaheer@deriv.com>
2024-02-07 14:03:36 +0400

    fix: fixes deriv ui import

--- packages/account-v2/src/modules/ManualForm/manualForm.tsx
+++ packages/account-v2/src/modules/ManualForm/manualForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Formik } from 'formik';
-import { Button } from '@deriv/ui';
+import { Button } from '@deriv-com/ui';
 import { MANUAL_FORM_INITIAL_VALUES, TManualDocumentTypes } from '../../constants/manualFormConstants';
 import { getManualFormValidationSchema } from '../../utils/manualFormUtils';
 import { ManualFormDocumentUpload } from './manualFormDocumentUpload';
@@ -26,7 +26,7 @@ export const ManualForm = ({ onSubmit, selectedDocument }: TManualFormProps) => 
                         <ManualFormDocumentUpload selectedDocument={selectedDocument} />
                         <ManualFormFooter />
                         <div className='flex justify-end gap-800 bg-vp px-400 py-800 border-t-solid-grey-2 border-solid border-t-100'>
-                            <Button color='secondary' type='button'>
+                            <Button type='button' variant='outlined'>
                                 Back
                             </Button>
                             <Button disabled={!isValid}>Next</Button>

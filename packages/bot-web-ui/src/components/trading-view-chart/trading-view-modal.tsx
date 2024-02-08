8fc90ed05a22bb79e88aedb5812c5c3e64689ae0
shafin-deriv <shafin@regentmarkets.com>
2024-02-08 16:48:01 +0800

    fix: Trading view chart drag issue

--- packages/bot-web-ui/src/components/trading-view-chart/trading-view-modal.tsx
+++ packages/bot-web-ui/src/components/trading-view-chart/trading-view-modal.tsx
@@ -36,7 +36,7 @@ const TradingViewModal = observer(() => {
 
     return (
         <Draggable
-            bounds='.dashboard__main'
+            bounds='.bot-dashboard'
             xaxis={xaxis}
             yaxis={yaxis}
             is_visible={is_trading_view_modal_visible}

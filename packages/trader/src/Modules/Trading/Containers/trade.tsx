4d14a9bbb9941a0b2de3d9a9c8badaab64a108db
kate-deriv <kate@regentmarkets.com>
2024-02-08 13:52:19 +0300

    fix: remove sonarcloud code smell

--- packages/trader/src/Modules/Trading/Containers/trade.tsx
+++ packages/trader/src/Modules/Trading/Containers/trade.tsx
@@ -147,83 +147,81 @@ const Trade = observer(() => {
     // TODO: Uncomment and update this when DTrader 2.0 development starts:
     // if (useFeatureFlags().is_dtrader_v2_enabled) return <Text size='xl'>Hello! I am DTrader 2.0.</Text>;
     return (
-        <React.Fragment>
-            <div
-                className={classNames('trade-container', {
-                    [`trade-container--${is_accumulator ? 'accumulators' : 'turbos'}`]: is_accumulator || is_turbos,
-                })}
-                id='trade_container'
-            >
-                <DesktopWrapper>
-                    <PositionsDrawer />
-                </DesktopWrapper>
-                {/* Div100vhContainer is workaround for browsers on devices
+        <div
+            className={classNames('trade-container', {
+                [`trade-container--${is_accumulator ? 'accumulators' : 'turbos'}`]: is_accumulator || is_turbos,
+            })}
+            id='trade_container'
+        >
+            <DesktopWrapper>
+                <PositionsDrawer />
+            </DesktopWrapper>
+            {/* Div100vhContainer is workaround for browsers on devices
                     with toolbars covering screen height,
                     using css vh is not returning correct screen height */}
-                <Div100vhContainer
-                    className='chart-container'
-                    height_offset={chart_height_offset}
-                    id='chart_container'
-                    is_disabled={isDesktop()}
+            <Div100vhContainer
+                className='chart-container'
+                height_offset={chart_height_offset}
+                id='chart_container'
+                is_disabled={isDesktop()}
+            >
+                <NotificationMessages show_trade_notifications={is_mobile} />
+                <React.Suspense
+                    fallback={<ChartLoader is_dark={is_dark_theme} is_visible={!symbol || !!is_chart_loading} />}
                 >
-                    <NotificationMessages show_trade_notifications={is_mobile} />
-                    <React.Suspense
-                        fallback={<ChartLoader is_dark={is_dark_theme} is_visible={!symbol || !!is_chart_loading} />}
-                    >
-                        <DesktopWrapper>
-                            <div
-                                className={classNames('chart-container__wrapper', {
-                                    'vanilla-trade-chart': is_vanilla,
-                                })}
-                            >
-                                <ChartLoader is_visible={is_chart_loading || should_show_active_symbols_loading} />
-                                <TradeChart topWidgets={topWidgets} is_accumulator={is_accumulator} />
-                            </div>
-                        </DesktopWrapper>
-                        <MobileWrapper>
+                    <DesktopWrapper>
+                        <div
+                            className={classNames('chart-container__wrapper', {
+                                'vanilla-trade-chart': is_vanilla,
+                            })}
+                        >
                             <ChartLoader is_visible={is_chart_loading || should_show_active_symbols_loading} />
-                            <SwipeableWrapper
-                                className={classNames({ 'vanilla-trade-chart': is_vanilla })}
-                                is_disabled={
-                                    !show_digits_stats ||
-                                    !is_trade_enabled ||
-                                    !form_components.length ||
-                                    is_chart_loading ||
-                                    should_show_active_symbols_loading
-                                }
-                                is_swipe_disabled={swipe_index === 1}
-                                onChange={onChangeSwipeableIndex}
-                            >
-                                {show_digits_stats && <DigitsWidget digits={digits} tick={tick} />}
-                                <TradeChart
-                                    bottomWidgets={show_digits_stats ? bottomWidgets : undefined}
-                                    has_barrier={has_barrier}
-                                    is_accumulator={is_accumulator}
-                                    topWidgets={topWidgets}
-                                />
-                            </SwipeableWrapper>
-                        </MobileWrapper>
-                    </React.Suspense>
-                </Div100vhContainer>
-                <div className={form_wrapper_class}>
-                    {is_market_closed && !is_market_unavailable_visible && (
-                        <MarketIsClosedOverlay
-                            is_eu={is_eu}
-                            is_synthetics_trading_market_available={is_synthetics_trading_market_available}
-                            onClick={onTryOtherMarkets}
-                            onMarketOpen={prepareTradeStore}
-                            symbol={symbol}
-                        />
-                    )}
-                    <FormLayout
-                        is_market_closed={is_market_closed}
-                        is_trade_enabled={
-                            is_trade_enabled && form_components.length > 0 && network_status.class === 'online'
-                        }
+                            <TradeChart topWidgets={topWidgets} is_accumulator={is_accumulator} />
+                        </div>
+                    </DesktopWrapper>
+                    <MobileWrapper>
+                        <ChartLoader is_visible={is_chart_loading || should_show_active_symbols_loading} />
+                        <SwipeableWrapper
+                            className={classNames({ 'vanilla-trade-chart': is_vanilla })}
+                            is_disabled={
+                                !show_digits_stats ||
+                                !is_trade_enabled ||
+                                !form_components.length ||
+                                is_chart_loading ||
+                                should_show_active_symbols_loading
+                            }
+                            is_swipe_disabled={swipe_index === 1}
+                            onChange={onChangeSwipeableIndex}
+                        >
+                            {show_digits_stats && <DigitsWidget digits={digits} tick={tick} />}
+                            <TradeChart
+                                bottomWidgets={show_digits_stats ? bottomWidgets : undefined}
+                                has_barrier={has_barrier}
+                                is_accumulator={is_accumulator}
+                                topWidgets={topWidgets}
+                            />
+                        </SwipeableWrapper>
+                    </MobileWrapper>
+                </React.Suspense>
+            </Div100vhContainer>
+            <div className={form_wrapper_class}>
+                {is_market_closed && !is_market_unavailable_visible && (
+                    <MarketIsClosedOverlay
+                        is_eu={is_eu}
+                        is_synthetics_trading_market_available={is_synthetics_trading_market_available}
+                        onClick={onTryOtherMarkets}
+                        onMarketOpen={prepareTradeStore}
+                        symbol={symbol}
                     />
-                </div>
+                )}
+                <FormLayout
+                    is_market_closed={is_market_closed}
+                    is_trade_enabled={
+                        is_trade_enabled && form_components.length > 0 && network_status.class === 'online'
+                    }
+                />
             </div>
-        </React.Fragment>
+        </div>
     );
 });
 

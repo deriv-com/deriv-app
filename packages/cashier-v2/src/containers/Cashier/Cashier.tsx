c26453f138d93d2c25a0c99ea22492a1c0096afc
Aum Bhatt <aum@deriv.com>
2024-02-13 12:20:28 +0400

    chore: basic setup to display withdrawal verification page

--- packages/cashier-v2/src/containers/Cashier/Cashier.tsx
+++ packages/cashier-v2/src/containers/Cashier/Cashier.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { PageLayout } from '@deriv-com/ui';
+import { useAuthorize } from '@deriv/api';
+import { Loader, PageLayout } from '@deriv-com/ui';
 import { TRouteTypes } from '../../types';
 import Menu from './Menu';
 import styles from './Cashier.module.scss';
 
 const Cashier = ({ routes }: TRouteTypes.TRouteComponent) => {
+    const { isLoading } = useAuthorize();
+
     return (
         <div className={styles.container}>
             <PageLayout sidebar={<Menu routes={routes} />}>
-                <Switch>
-                    {routes?.map(route => {
-                        const { path, title } = route;
-                        return (
-                            <Route exact key={path} path={path}>
-                                <route.component path={path} title={title} />
-                            </Route>
-                        );
-                    })}
-                </Switch>
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    <Switch>
+                        {routes?.map(route => {
+                            const { path, title } = route;
+                            return (
+                                <Route exact key={path} path={path}>
+                                    <route.component path={path} title={title} />
+                                </Route>
+                            );
+                        })}
+                    </Switch>
+                )}
             </PageLayout>
         </div>
     );

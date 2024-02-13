946d95e866b3abe0c046fcb65f732e0a269dcb6f
rupato-deriv <rupato@deriv.com>
2024-02-13 12:57:32 +0800

    fix: authorize call on mobile

--- packages/hooks/src/usePaymentAgentTransferVisible.ts
+++ packages/hooks/src/usePaymentAgentTransferVisible.ts
@@ -1,28 +1,31 @@
 import { useFetch } from '@deriv/api';
-import { WS } from '@deriv/shared';
-import React from 'react';
+import { WS, isBot } from '@deriv/shared';
+import { useStore } from '@deriv/stores';
+import { useEffect, useState } from 'react';
 
 const usePaymentAgentTransferVisible = () => {
-    const [is_authorized, setIsAuthorized] = React.useState(false);
-    const { data, ...rest } = useFetch('get_settings', { options: { enabled: Boolean(is_authorized) } });
+    const { client } = useStore();
+    const { is_authorize } = client;
+    const [is_authorized, setIsAuthorized] = useState(false);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const checkAuthorize = async () => {
             try {
                 const authorized = await WS.wait('authorize');
-                setIsAuthorized(authorized);
+                if (authorized) setIsAuthorized(is_authorize);
             } catch (error) {
-                /* eslint-disable no-console */
-                console.error('Error', error);
+                console.error('Error while authorizing:', error); // eslint-disable-line
             }
         };
-        checkAuthorize();
-    }, []);
 
-    const is_payment_agent_transfer_visible = Boolean(data?.get_settings?.is_authenticated_payment_agent);
+        checkAuthorize();
+    }, [is_authorize]);
+
+    const { data, ...rest } = useFetch('get_settings', { options: { enabled: Boolean(is_authorized) } });
+    const isPaymentAgentTransferVisible = Boolean(data?.get_settings?.is_authenticated_payment_agent);
 
     return {
-        data: is_payment_agent_transfer_visible,
+        data: isPaymentAgentTransferVisible,
         ...rest,
     };
 };

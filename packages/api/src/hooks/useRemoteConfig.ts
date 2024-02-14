11ab0b7bee7d37db2f32ecf6b47b70d66a3d402a
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-02-14 12:55:25 +0800

    feat: use url from env vars

--- packages/api/src/hooks/useRemoteConfig.ts
+++ packages/api/src/hooks/useRemoteConfig.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import initData from '../features.json';
 
 const remoteConfigQuery = async function () {
-    const response = await fetch('https://deriv-static.firebaseio.com/features.json');
+    const response = await fetch(process.env.REMOTE_CONFIG_URL);
     if (!response.ok) {
         throw new Error('Remote Config Server is out of reach!');
     }

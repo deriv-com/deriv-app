3ff8e5e584da822ce5a843baafc1a0f7bdd18152
Nada <nada.ajinas@deriv.com>
2024-02-14 10:24:50 +0400

    fix: rewrite the styles

--- packages/p2p-v2/src/pages/my-ads/screens/MyAds/MyAds.tsx
+++ packages/p2p-v2/src/pages/my-ads/screens/MyAds/MyAds.tsx
@@ -4,9 +4,9 @@ import { useDevice } from '@/hooks';
 import { MyAdsTable } from './MyAdsTable';
 
 const MyAds = () => {
-    const { isDesktop, isMobile } = useDevice();
+    const { isMobile } = useDevice();
     return (
-        <div className={clsx('flex flex-col', { 'h-[calc(100vh-12rem)]': isMobile }, { 'h-full': isDesktop })}>
+        <div className={clsx('flex flex-col', isMobile ? 'h-[calc(100vh-12rem)]' : 'h-full')}>
             <MyAdsTable />
         </div>
     );

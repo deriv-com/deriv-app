960102428656cb957dd1da750f7e3eb1eac34220
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2024-02-14 09:34:28 +0300

    [DTRA] Maryia/DTRA-683/fix: jumpToCurrentMonth logic in TwoMonthPicker + TS errors (#13465)
    * fix: jump to current month + TS errors
    
    * test: TwoMonthPicker
    
    * refactor: remove empty callback

--- packages/components/src/components/calendar/views/types.ts
+++ packages/components/src/components/calendar/views/types.ts
@@ -1,6 +1,6 @@
 export type CommonPropTypes = {
     calendar_date: moment.MomentInput;
-    isPeriodDisabled: (date: moment.MomentInput, unit: moment.unitOfTime.StartOf) => boolean;
+    isPeriodDisabled: (date: moment.Moment, unit: moment.unitOfTime.StartOf) => boolean;
     selected_date: moment.MomentInput;
     updateSelected: (e: React.MouseEvent<HTMLSpanElement>, type: moment.unitOfTime.StartOf) => void;
 };

8fbaddc8e5359a34e704817940ff7523da04cc16
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2024-02-14 10:38:13 +0300

    [Cashier-V2] george / FEQ-1708 / Integrate the VerticalTab component in cashier v2 (#13531)
    * feat(cashier-v2): :sparkles: add CashierLayout with VerticalTab+Content+SideNotes
    
    * perf(cashier-v2): :recycle: naming improvement

--- packages/cashier-v2/src/containers/Cashier/Cashier.tsx
+++ packages/cashier-v2/src/containers/Cashier/Cashier.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { PageLayout } from '@deriv-com/ui';
-import { TRouteTypes } from '../../types';
-import Menu from './Menu';
+import React, { useState } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useAuthorize } from '@deriv/api';
+import { Loader, PageLayout, VerticalTab, VerticalTabItems } from '@deriv-com/ui';
+import { CashierLayout } from '../../components';
+import type { TRouteTypes, TSideNotes } from '../../types';
 import styles from './Cashier.module.scss';
 
-const Cashier = ({ routes }: TRouteTypes.TRouteComponent) => {
+const Cashier: React.FC<TRouteTypes.TRouteComponent> = ({ routes }) => {
+    const [sideNotes, setSideNotes] = useState<TSideNotes>({
+        notes: [],
+        position: undefined,
+    });
+    const routesWithID = routes?.map(route => ({ ...route, id: route.path })) ?? [];
+    const { isLoading } = useAuthorize();
+    const history = useHistory();
+
+    const onSelectItemHandler = (path: TRouteTypes.TRoutes) => history.push(path);
+
+    if (isLoading) return <Loader />;
+
     return (
         <div className={styles.container}>
-            <PageLayout sidebar={<Menu routes={routes} />}>
-                <Switch>
-                    {routes?.map(route => {
-                        const { path, title } = route;
-                        return (
-                            <Route exact key={path} path={path}>
-                                <route.component path={path} title={title} />
-                            </Route>
-                        );
-                    })}
-                </Switch>
+            <PageLayout
+                sidebar={
+                    <VerticalTab>
+                        <VerticalTabItems
+                            activeTab={history.location.pathname}
+                            items={routesWithID}
+                            //@ts-expect-error VerticalTabItems should have generic type for "id" prop and not limited to the type of string
+                            onSelectItem={onSelectItemHandler}
+                            should_have_panel={false}
+                            wrapperClassName={styles['vertical-tab-wrapper']}
+                        />
+                    </VerticalTab>
+                }
+            >
+                <CashierLayout sideNotes={sideNotes}>
+                    <Switch>
+                        {routes?.map(route => {
+                            const { path, title } = route;
+                            return (
+                                <Route exact key={path} path={path}>
+                                    <route.component path={path} setSideNotes={setSideNotes} title={title} />
+                                </Route>
+                            );
+                        })}
+                    </Switch>
+                </CashierLayout>
             </PageLayout>
         </div>
     );

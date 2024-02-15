8eac03798097c314d4e1ed0b801af0d7774c5f58
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-02-15 12:34:25 +0800

    feat: change the json file structure

--- packages/api/src/hooks/useRemoteConfig.ts
+++ packages/api/src/hooks/useRemoteConfig.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import initData from '../features.json';
+import initData from '../remote_config.json';
 
 const remoteConfigQuery = async function () {
     if (!process.env.REMOTE_CONFIG_URL) {

21154e41e0a94d60a09463643c7813fa9ba7f335
Aum Bhatt <aum@deriv.com>
2024-02-19 09:16:55 +0400

    feat: added WithdrawalCryptoModule

---
+++ packages/cashier-v2/src/lib/WithdrawalCrypto/WithdrawalCrypto.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Text } from '@deriv-com/ui';
+import WithdrawalCryptoProvider, { useWithdrawalCryptoContext } from './provider/WithdrawalCryptoProvider';
+import { WithdrawalCryptoDisclaimer, WithdrawalCryptoForm, WithdrawalCryptoReceipt } from './components';
+import styles from './WithdrawalCrypto.module.scss';
+
+type TWithdrawalCryptoProps = {
+    onClose: () => void;
+    verificationCode: string;
+};
+
+const WithdrawalCrypto: React.FC = () => {
+    const { activeWallet, getCurrencyConfig, isWithdrawalSuccess, onClose, withdrawalReceipt } =
+        useWithdrawalCryptoContext();
+
+    if (isWithdrawalSuccess) return <WithdrawalCryptoReceipt onClose={onClose} withdrawalReceipt={withdrawalReceipt} />;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles['side-pane']} />
+            <div className={styles.content}>
+                <Text className={styles.title} weight='bold'>
+                    Withdraw {activeWallet?.currency ? getCurrencyConfig(activeWallet?.currency)?.name : ''} (
+                    {activeWallet?.currency}) to your wallet
+                </Text>
+                <WithdrawalCryptoDisclaimer />
+                <WithdrawalCryptoForm />
+            </div>
+            <div className={styles['side-pane']} />
+        </div>
+    );
+};
+
+const WithdrawalCryptoModule: React.FC<TWithdrawalCryptoProps> = ({ onClose, verificationCode }) => {
+    return (
+        <WithdrawalCryptoProvider onClose={onClose} verificationCode={verificationCode}>
+            <WithdrawalCrypto />
+        </WithdrawalCryptoProvider>
+    );
+};
+
+export default WithdrawalCryptoModule;

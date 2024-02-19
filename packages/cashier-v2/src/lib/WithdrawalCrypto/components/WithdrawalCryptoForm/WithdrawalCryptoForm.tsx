8499b68c5c0dec03418d4f4f5998575d720d0bdd
Aum Bhatt <aum@deriv.com>
2024-02-19 10:28:57 +0400

    feat: added WithdrawalCryptoForm Component

---
+++ packages/cashier-v2/src/lib/WithdrawalCrypto/components/WithdrawalCryptoForm/WithdrawalCryptoForm.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Field, FieldProps, Formik } from 'formik';
+import { Button, Input } from '@deriv-com/ui';
+import { useWithdrawalCryptoContext } from '../../provider/WithdrawalCryptoProvider';
+import { validateCryptoAddress } from '../../utils';
+import { WithdrawalCryptoAmountConverter } from './components/WithdrawalCryptoAmountConverter';
+import { WithdrawalCryptoPercentageSelector } from './components/WithdrawalCryptoPercentageSelector';
+import styles from './WithdrawalCryptoForm.module.scss';
+
+const WithdrawalCryptoForm: React.FC = () => {
+    const { activeWallet, fractionalDigits, requestCryptoWithdrawal } = useWithdrawalCryptoContext();
+
+    return (
+        <Formik
+            initialValues={{
+                cryptoAddress: '',
+                cryptoAmount: '',
+                fiatAmount: '',
+            }}
+            onSubmit={values =>
+                requestCryptoWithdrawal({
+                    address: values.cryptoAddress,
+                    amount: parseFloat(parseFloat(values.cryptoAmount).toFixed(fractionalDigits.crypto)),
+                })
+            }
+        >
+            {({ errors, handleSubmit, isSubmitting, values }) => {
+                return (
+                    <form autoComplete='off' className={styles.container} onSubmit={handleSubmit}>
+                        <div className={styles['crypto-address']}>
+                            <Field name='cryptoAddress' validate={validateCryptoAddress}>
+                                {({ field }: FieldProps<string>) => (
+                                    <Input
+                                        {...field}
+                                        error={Boolean(errors?.cryptoAddress)}
+                                        label={`Your ${activeWallet?.currency_config?.name} cryptocurrency wallet address`}
+                                        message={errors.cryptoAddress}
+                                    />
+                                )}
+                            </Field>
+                        </div>
+                        <WithdrawalCryptoPercentageSelector />
+                        <WithdrawalCryptoAmountConverter />
+                        <div className={styles.submit}>
+                            <Button
+                                disabled={Object.keys(errors).length !== 0 || !values.cryptoAmount || isSubmitting}
+                                isLoading={isSubmitting}
+                                size='lg'
+                                type='submit'
+                            >
+                                Withdraw
+                            </Button>
+                        </div>
+                    </form>
+                );
+            }}
+        </Formik>
+    );
+};
+
+export default WithdrawalCryptoForm;

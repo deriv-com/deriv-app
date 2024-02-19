c0e3727628b6e44f219a8563f803c774c54379ef
amina-deriv <amina@regentmarkets.com>
2024-02-19 11:26:59 +0400

    fix: remove console

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -2302,7 +2302,6 @@ export default class ClientStore extends BaseStore {
     }
 
     onSignup({ citizenship, password, residence }, cb) {
-        console.log(password);
         if (!this.verification_code.signup || !password || !residence || !citizenship) return;
         WS.newAccountVirtual(this.verification_code.signup, password, residence, this.getSignupParams())
             .then(async response => {

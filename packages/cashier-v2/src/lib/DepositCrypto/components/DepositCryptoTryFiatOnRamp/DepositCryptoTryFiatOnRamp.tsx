3d5ed4e55760e76b7879f43e6b77ca13c5a755f6
heorhi-deriv <heorhi@binary.com>
2024-02-19 16:25:58 +0300

    fix: :ambulance: fix sonarlint issues

--- packages/cashier-v2/src/lib/DepositCrypto/components/DepositCryptoTryFiatOnRamp/DepositCryptoTryFiatOnRamp.tsx
+++ packages/cashier-v2/src/lib/DepositCrypto/components/DepositCryptoTryFiatOnRamp/DepositCryptoTryFiatOnRamp.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Text } from '@deriv-com/ui';
 import styles from './DepositCryptoTryFiatOnRamp.module.scss';
 
 const DepositCryptoTryFiatOnRamp = () => {
-    const history = useHistory();
-
     return (
         <div className={styles.container}>
             <Text align='center' size='xs'>
                 Looking for a way to buy cryptocurrencies?&nbsp;
-                <a className={styles.link} onClick={() => history.push('/cashier-v2/on-ramp')}>
-                    Try Fiat onramp
-                </a>
+                <Link className={styles.link} to='/cashier-v2/on-ramp'>
+                    {' '}
+                    Try Fiat onramp{' '}
+                </Link>{' '}
                 .
             </Text>
         </div>

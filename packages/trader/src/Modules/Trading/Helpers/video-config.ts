c8e664aee2daf3234795c177f23914511b97012b
kate-deriv <kate@regentmarkets.com>
2024-02-20 14:54:09 +0300

    feat: add new videos

--- packages/trader/src/Modules/Trading/Helpers/video-config.ts
+++ packages/trader/src/Modules/Trading/Helpers/video-config.ts
@@ -11,8 +11,20 @@ type TVideoVariants = {
 If considered necessary later, the current approach can be replaced with HTTP-request to fetch videos by their file names. */
 export const DESCRIPTION_VIDEO_ID: TDtraderVideoUrl = {
     accumulator: {
-        light: '3a8f185f22a3e2b9da59cea6e70e706e',
-        dark: '0602069e9de7f4206929402606928853',
+        light: '29503b759673ded4474e35673d01f9b4',
+        dark: 'b12da36650ec8f51ff1ab7d92a9b434a',
+    },
+    high_low: {
+        light: 'ce561710371cfed69ff611a354e3fa71',
+        dark: 'bf5a9e4f8294ca18f1c67db864a88b6f',
+    },
+    rise_fall: {
+        light: '8f6fb30a657da5ce6dcd01771e6b4c45',
+        dark: '4e8c7f882beadf32ff6eb19b71946ca9',
+    },
+    touch: {
+        light: '2413d2a82b1a15266fcb2387d217d586',
+        dark: 'b34b30d90f6012a7b384c6be89fd117f',
     },
     turbos: {
         light: '4d03df21b56b33101806394f9bdffb43',

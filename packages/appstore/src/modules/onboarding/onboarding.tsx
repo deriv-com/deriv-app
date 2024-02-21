2eab1b7f236b623aa21d95e327141d5de2d33e69
Mahdiyeh Amirkhani <mahdiyehamirkhani@Mahdiyeh-Amirkhanis-Mac-J79XQHKXVJ-Standard.local>
2024-02-21 14:10:53 +0800

    fix: hide tour for EU after password setting

--- packages/appstore/src/modules/onboarding/onboarding.tsx
+++ packages/appstore/src/modules/onboarding/onboarding.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { routes } from '@deriv/shared';
+import { routes, ContentFlag } from '@deriv/shared';
 import { useStore, observer } from '@deriv/stores';
 import OnboardingSkeleton from '../../components/loader';
 import TradingPlatformIcon from 'Assets/svgs/trading-platform';
@@ -11,7 +11,7 @@ const Onboarding = observer(() => {
 
     const { traders_hub, client, ui } = useStore();
     const { is_landing_company_loaded, is_logged_in, setPrevAccountType } = client;
-    const { is_demo_low_risk, selectAccountType, toggleIsTourOpen } = traders_hub;
+    const { content_flag, is_demo_low_risk, selectAccountType, toggleIsTourOpen } = traders_hub;
     const { is_from_signup_account } = ui;
 
     if (is_logged_in) {
@@ -19,9 +19,10 @@ const Onboarding = observer(() => {
             return <OnboardingSkeleton />;
         }
         history.push(routes.traders_hub);
-        if (!is_from_signup_account) {
+        if (is_from_signup_account && content_flag !== ContentFlag.EU_DEMO) {
             toggleIsTourOpen(true);
         }
+
         if (is_demo_low_risk) {
             selectAccountType('real');
             setPrevAccountType('demo');

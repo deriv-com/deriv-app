f1bba18c44b66e9e6260cc895493c56cf91bffc3
kate-deriv <kate@regentmarkets.com>
2024-02-21 15:53:34 +0300

    refactor: update ids

--- packages/trader/src/Modules/Trading/Helpers/video-config.ts
+++ packages/trader/src/Modules/Trading/Helpers/video-config.ts
@@ -7,32 +7,37 @@ type TVideoVariants = {
     light: string;
 };
 
-/* The video upload feature is not available yet. The following video uids are taken from CF Stream account.
+/* The video upload feature is not available yet. The following video ids are taken from CF Stream account.
 If considered necessary later, the current approach can be replaced with HTTP-request to fetch videos by their file names. */
 export const DESCRIPTION_VIDEO_ID: TDtraderVideoUrl = {
     accumulator: {
-        light: '29503b759673ded4474e35673d01f9b4',
-        dark: 'b12da36650ec8f51ff1ab7d92a9b434a',
+        light: '02a41af73b2728ec46cc9843fe478fcc',
+        dark: '44d37db7346ffeb7ebf1626ab7fdf04c',
     },
+
     high_low: {
-        light: 'ce561710371cfed69ff611a354e3fa71',
-        dark: 'bf5a9e4f8294ca18f1c67db864a88b6f',
+        light: 'f528c662c377601908a741ae2aedaec6',
+        dark: '4c05445e1bd28f3f9f95b0ab566c4f9b',
     },
+
     rise_fall: {
-        light: '8f6fb30a657da5ce6dcd01771e6b4c45',
-        dark: '4e8c7f882beadf32ff6eb19b71946ca9',
+        light: '7719c7e5436f58e59ab47510445108ba',
+        dark: 'b72ef7ec914afe3dbb2a1601cad3a76f',
     },
+
     touch: {
-        light: '2413d2a82b1a15266fcb2387d217d586',
-        dark: 'b34b30d90f6012a7b384c6be89fd117f',
+        light: 'c781a6844f37a3308fe8774b4450dfc9',
+        dark: 'c91f2b9859dc95ce8eecf7df327aaf00',
     },
+
     turbos: {
-        light: '4d03df21b56b33101806394f9bdffb43',
-        dark: '8d71878b0f2ba06cf83b51919e50d77b',
+        light: 'd37ccbb8db358970b3628ee5e2b46ec3',
+        dark: 'aa5dfdd1f7d28c03d18a10c1d689389c',
     },
+
     vanilla: {
-        light: 'bed2bd4d874b15999590265e25b7778b',
-        dark: '0d22d76a321833f1c6b732cf055057ae',
+        light: '9b8b6ae67d3a720e4aee2665889d17fb',
+        dark: '8f04e9d768b8e08a1d87830c95c6b9c8',
     },
 };
 

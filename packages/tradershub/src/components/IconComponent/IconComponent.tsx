55d5ee742bfa152316bffd4f40935f1e97391faa
thisyahlen <104053934+thisyahlen-deriv@users.noreply.github.com>
2024-02-22 12:02:12 +0800

    thisyahlen/TRAH-2990/chore: add currency to IconComponent (#13742)
    * chore: add currency to IconComponent
    
    * chore: change to numbers

--- packages/tradershub/src/components/IconComponent/IconComponent.tsx
+++ packages/tradershub/src/components/IconComponent/IconComponent.tsx
@@ -5,6 +5,16 @@ import {
     AccountsDmt5DerivedIcon,
     AccountsDmt5FinancialIcon,
     AccountsDmt5SwfIcon,
+    CurrencyAudIcon,
+    CurrencyBtcIcon,
+    CurrencyDemoIcon,
+    CurrencyEthIcon,
+    CurrencyEurIcon,
+    CurrencyGbpIcon,
+    CurrencyLtcIcon,
+    CurrencyUsdcIcon,
+    CurrencyUsdIcon,
+    CurrencyUsdtIcon,
     DerivProductDerivBotBrandLightLogoHorizontalIcon,
     DerivProductDerivGoBrandLightLogoHorizontalIcon,
     DerivProductDerivTraderBrandLightLogoHorizontalIcon,
@@ -22,7 +32,7 @@ interface IconProps<T> {
     width?: CSSProperties['width'];
 }
 
-export const PlatformIcons: Record<string, ElementType> = {
+export const Icons: Record<string, ElementType> = {
     BinaryBot: PartnersProductBinaryBotBrandLightLogoHorizontalIcon,
     CFDs: AccountsDmt5CfdsIcon,
     CTrader: PartnersProductDerivCtraderBrandLightLogoHorizontalIcon,
@@ -35,14 +45,24 @@ export const PlatformIcons: Record<string, ElementType> = {
     Financial: AccountsDmt5FinancialIcon,
     SmartTrader: PartnersProductSmarttraderBrandLightLogoIcon,
     SwapFree: AccountsDmt5SwfIcon,
+    AUD: CurrencyAudIcon,
+    BTC: CurrencyBtcIcon,
+    ETH: CurrencyEthIcon,
+    EUR: CurrencyEurIcon,
+    eUSDT: CurrencyUsdtIcon,
+    GBP: CurrencyGbpIcon,
+    LTC: CurrencyLtcIcon,
+    tUSDT: CurrencyUsdtIcon,
+    USD: CurrencyUsdIcon,
+    USDC: CurrencyUsdcIcon,
+    UST: CurrencyUsdtIcon,
+    virtual: CurrencyDemoIcon,
 };
 
-const PlatformIcon = ({ className, height = 48, icon, onClick, width = 48 }: IconProps<keyof typeof PlatformIcons>) => {
-    const TradingPlatformIcon = PlatformIcons[icon] as ElementType;
+const IconComponent = ({ className, height = 48, icon, onClick, width = 48 }: IconProps<keyof typeof Icons>) => {
+    const Icon = Icons[icon] as ElementType;
 
-    return TradingPlatformIcon ? (
-        <TradingPlatformIcon className={className} height={height} onClick={onClick} width={width} />
-    ) : null;
+    return Icon ? <Icon className={className} height={height} onClick={onClick} width={width} /> : null;
 };
 
-export default PlatformIcon;
+export default IconComponent;

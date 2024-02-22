0541676f752fa2311d498b1a0297235c02bc42ac
Aum Bhatt <aum@deriv.com>
2024-02-22 12:33:52 +0400

    fix: fix styling for WithdrawalCryptoDisclaimer

--- packages/cashier-v2/src/lib/WithdrawalCrypto/components/WithdrawalCryptoDisclaimer/WithdrawalCryptoDisclaimer.tsx
+++ packages/cashier-v2/src/lib/WithdrawalCrypto/components/WithdrawalCryptoDisclaimer/WithdrawalCryptoDisclaimer.tsx
@@ -4,13 +4,13 @@ import styles from './WithdrawalCryptoDisclaimer.module.scss';
 
 const WithdrawalDisclaimer = () => (
     <div className={styles.container}>
-        <InlineMessage>
+        <InlineMessage iconPosition='top' variant='warning'>
             <ul className={styles.list}>
-                <Text as='li'>
+                <Text as='li' size='2xs'>
                     Do not enter an address linked to an initial coin offering (ICO) purchase or crowdsale. If you do,
                     the initial coin offering (ICO) tokens will not be credited into your account.
                 </Text>
-                <Text as='li'>
+                <Text as='li' size='2xs'>
                     Please note that your maximum and minimum withdrawal limits aren’t fixed. They change due to the
                     high volatility of cryptocurrency.
                 </Text>

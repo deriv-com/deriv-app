61c353b106a480f134138c5b12d69e0776bcfb17
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-02-23 11:11:04 +0300

    DTRA / Kate / FEQ-1586 / Reports - Cleanup unimported js files (#13560)
    * refactor: remove unused files
    
    * fix: add deleted data testid in order to fix test

--- packages/reports/src/_common/components/loading.tsx
+++ packages/reports/src/_common/components/loading.tsx
@@ -6,11 +6,13 @@ type TLoading = {
     is_invisible?: boolean;
     theme?: string;
     id?: string;
+    data_testid?: string;
 };
 
-const Loading = ({ className, is_invisible, theme, id }: TLoading) => (
+const Loading = ({ className, is_invisible, theme, id, data_testid }: TLoading) => (
     <div
         id={id}
+        data-testid={data_testid}
         className={classNames('barspinner', `barspinner--${theme || 'dark'}`, { invisible: is_invisible }, className)}
     >
         {Array.from(new Array(5)).map((x, inx) => (

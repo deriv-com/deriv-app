5aeab0057f7e7f6fb900afb4314a7de22ef0f0e5
Taysuisin <suisin@regentmarkets.com>
2024-02-23 12:02:19 +0800

    chore: add country selector into account-v2 package

---
+++ packages/account-v2/src/components/CountrySelector/CountrySelector.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useResidenceList } from '@deriv/api';
+import { LabelPairedChevronDownMdRegularIcon } from '@deriv/quill-icons';
+import { Dropdown } from '@deriv-com/ui';
+
+type TCountrySelector = {
+    errorMessage?: string;
+    label: string;
+};
+
+const CountrySelector = ({ errorMessage, label }: TCountrySelector) => {
+    const { data: residenceList } = useResidenceList();
+
+    return (
+        <Dropdown
+            dropdownIcon={<LabelPairedChevronDownMdRegularIcon />}
+            errorMessage={errorMessage}
+            label={label}
+            list={residenceList}
+            name='country'
+            /*eslint-disable @typescript-eslint/no-empty-function */
+            onSelect={() => {}}
+            variant='prompt'
+        />
+    );
+};
+
+export default CountrySelector;
---
+++ packages/account-v2/src/components/CountrySelector/__test__/CountrySelector.spec.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { APIProvider, useResidenceList } from '@deriv/api';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CountrySelector from '../CountrySelector';
+
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useResidenceList: jest.fn(),
+}));
+
+const mockUseResidenceList = useResidenceList as jest.MockedFunction<typeof useResidenceList>;
+
+beforeEach(() => {
+    mockUseResidenceList.mockReturnValue(
+        // @ts-expect-error need a way to mock useQuery data
+        {
+            data: [
+                {
+                    identity: {
+                        services: {
+                            idv: {
+                                is_country_supported: 0,
+                            },
+                        },
+                    },
+                    text: 'Indonesia',
+                    value: 'id',
+                },
+            ],
+        }
+    );
+});
+
+describe('CountrySelector', () => {
+    it('should render CountrySelector component', () => {
+        render(
+            <APIProvider>
+                <CountrySelector label='Country' />
+            </APIProvider>
+        );
+        const inputField = screen.getByRole('combobox', { name: /Country/i });
+        expect(inputField).toBeInTheDocument();
+    });
+
+    it('should show residence list when user click on country selector', () => {
+        render(
+            <APIProvider>
+                <CountrySelector label='Country' />
+            </APIProvider>
+        );
+        const inputField = screen.getByRole('combobox', { name: /Country/i });
+        userEvent.click(inputField);
+        expect(screen.getByText('Indonesia')).toBeInTheDocument();
+    });
+
+    it('should change the value of combobox to be Indonesia', () => {
+        render(
+            <APIProvider>
+                <CountrySelector label='Country' />
+            </APIProvider>
+        );
+        const inputField = screen.getByRole('combobox', { name: /Country/i });
+        userEvent.click(inputField);
+        const dropDown = screen.getByText('Indonesia');
+        userEvent.click(dropDown);
+        expect(inputField).toHaveValue('Indonesia');
+    });
+});

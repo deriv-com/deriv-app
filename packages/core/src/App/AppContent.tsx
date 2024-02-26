297479b390514f80b729a104e73356332d2e882d
amir ali <amir.ali@regentmarkets.com>
2024-02-26 16:02:11 +0800

    feat: use remote config hook for growthbook

--- packages/core/src/App/AppContent.tsx
+++ packages/core/src/App/AppContent.tsx
@@ -13,10 +13,38 @@ import PlatformContainer from './Containers/PlatformContainer/PlatformContainer.
 import Routes from './Containers/Routes/routes.jsx';
 import AppToastMessages from './Containers/app-toast-messages.jsx';
 import Devtools from './Devtools';
+import Cookies from 'js-cookie';
+import { useRemoteConfig } from '@deriv/api';
+import { LocalStore, getAppId } from '@deriv/shared';
+import { Analytics } from '@deriv-com/analytics';
+import { getLanguage } from '@deriv/translations';
 
 const AppContent: React.FC<{ passthrough: unknown }> = observer(({ passthrough }) => {
     const { is_next_wallet_enabled } = useFeatureFlags();
     const store = useStore();
+
+    const { data } = useRemoteConfig();
+
+    React.useEffect(() => {
+        if (process.env.RUDDERSTACK_KEY) {
+            const config = {
+                growthbookKey: data.marketing_growthbook ? process.env.GROWTHBOOK_CLIENT_KEY : undefined,
+                growthbookDecryptionKey: data.marketing_growthbook ? process.env.GROWTHBOOK_DECRYPTION_KEY : undefined,
+                rudderstackKey: process.env.RUDDERSTACK_KEY,
+            };
+            Analytics.initialise(config);
+            Analytics.setAttributes({
+                account_type: LocalStore?.get('active_loginid')?.substring(0, 2) ?? 'unlogged',
+                app_id: String(getAppId()),
+                device_type: store?.ui?.is_mobile ? 'mobile' : 'desktop',
+                device_language: navigator?.language || 'en-EN',
+                user_language: getLanguage().toLowerCase(),
+                country: Cookies.get('clients_country') || Cookies?.getJSON('website_status'),
+            });
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data]);
 
     return (
         <PlatformContainer>

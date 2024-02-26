5726cb6422372bf4c48a6c80c95764b20760645c
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2024-02-26 12:26:51 +0800

    Update StarRating.spec.tsx

--- packages/p2p-v2/src/components/StarRating/__tests__/StarRating.spec.tsx
+++ packages/p2p-v2/src/components/StarRating/__tests__/StarRating.spec.tsx
@@ -3,7 +3,7 @@ import StarRating from '../StarRating';
 import { render, screen } from '@testing-library/react';
 
 describe('StarRating', () => {
-    it('should render the correct stars', () => {
+    it('should render the passed filled/empty star icons', () => {
         render(<StarRating initialValue={3} ratingValue={3.3} />);
         expect(screen.queryAllByTestId('dt_p2p_v2_star_rating_empty_star')).toHaveLength(5);
         expect(screen.queryAllByTestId('dt_p2p_v2_star_rating_full_star')).toHaveLength(5);

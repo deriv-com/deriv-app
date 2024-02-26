8a4e7cba9ea1cbd48469fcebb1fa3ffba430887f
Mahdiyeh Amirkhani <mahdiyehamirkhani@Mahdiyeh-Amirkhanis-Mac-J79XQHKXVJ-Standard.local>
2024-02-26 14:55:42 +0800

    chore: change information content for fiat in Add account

--- packages/account/src/Components/currency-selector/radio-button-group.tsx
+++ packages/account/src/Components/currency-selector/radio-button-group.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
-import { localize } from '@deriv/translations';
-import { Text } from '@deriv/components';
 
 export type TRadioButtonGroup = {
     className: string;
@@ -10,7 +8,6 @@ export type TRadioButtonGroup = {
     item_count: number;
     label: string;
     description: React.ReactNode;
-    has_fiat?: boolean;
 };
 
 /**
@@ -22,7 +19,6 @@ export type TRadioButtonGroup = {
  * @param {number} item_count - number of items
  * @param {string} label - label for the radio button
  * @param {React.ReactNode} description - description for the radio button
- * @param {boolean} has_fiat - has fiat currency
  * @returns {React.ReactNode} - returns a React node
  */
 const RadioButtonGroup = ({
@@ -33,9 +29,8 @@ const RadioButtonGroup = ({
     is_fiat,
     item_count,
     description,
-    has_fiat,
 }: React.PropsWithChildren<TRadioButtonGroup>) => {
-    const [is_currency_selected, setIsCurrencySelected] = useState(false);
+    const [, setIsCurrencySelected] = useState(false);
 
     const onCurrencyClicked = () => {
         setIsCurrencySelected(true);
@@ -51,11 +46,7 @@ const RadioButtonGroup = ({
                     {label}
                 </h2>
             )}
-            {is_fiat && has_fiat && (
-                <Text size='xxs' className='currency-selector__subheading'>
-                    {localize('You are limited to one fiat currency only')}
-                </Text>
-            )}
+            {is_fiat && <div className='currency-selector__description'>{description}</div>}
             <div
                 className={classNames('currency-list__items', {
                     'currency-list__items__center': item_count < 4,
@@ -66,7 +57,6 @@ const RadioButtonGroup = ({
             >
                 {children}
             </div>
-            {is_fiat && is_currency_selected && <p className='currency-selector__description'>{description}</p>}
         </div>
     );
 };

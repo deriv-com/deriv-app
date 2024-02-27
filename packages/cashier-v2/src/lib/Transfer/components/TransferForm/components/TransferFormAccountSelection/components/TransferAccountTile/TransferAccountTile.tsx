67713bf79557bcb4cbb041293f6030e34501613b
Aum Bhatt <aum@deriv.com>
2024-02-27 16:56:10 +0400

    feat: added TransferAccountTile component

---
+++ packages/cashier-v2/src/lib/Transfer/components/TransferForm/components/TransferFormAccountSelection/components/TransferAccountTile/TransferAccountTile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text } from '@deriv-com/ui';
+import { AccountIcon } from '../../../../../../../../components';
+import styles from './TransferAccountTile.module.scss';
+
+const TransferAccountTile = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.account}>
+                <AccountIcon accountName='Dmt5Cfds' size='sm' />
+                <div className={styles['account-info']}>
+                    <Text size='sm'>Account Name</Text>
+                    <Text color='less-prominent' size='sm'>
+                        CR1234
+                    </Text>
+                </div>
+            </div>
+            <Text size='sm'>10,000 USD</Text>
+        </div>
+    );
+};
+
+export default TransferAccountTile;

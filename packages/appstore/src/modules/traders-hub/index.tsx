6833e60cc9d4971eba4c41abba2ab1f59ae6e6db
Sergei Baranovski <sergei@deriv.com>
2024-02-28 15:19:57 +0300

    feat: add styles for eu users to scroll disclaimer

--- packages/appstore/src/modules/traders-hub/index.tsx
+++ packages/appstore/src/modules/traders-hub/index.tsx
@@ -129,15 +129,16 @@ const TradersHub = observer(() => {
 
     return (
         <>
-            <Div100vhContainer
-                className={classNames('traders-hub--mobile', {
-                    'traders-hub--mobile--eu-user': is_eu_user,
-                })}
-                height_offset='50px'
-                is_disabled={isDesktop()}
-            >
+            <Div100vhContainer className='traders-hub--mobile' height_offset='50px' is_disabled={isDesktop()}>
                 {can_show_notify && <Notifications />}
-                <div id='traders-hub' className='traders-hub' ref={traders_hub_ref}>
+                <div
+                    id='traders-hub'
+                    className={classNames('traders-hub', {
+                        'traders-hub--eu-user': is_eu_user && is_mt5_allowed,
+                        'traders-hub--eu-user-without-mt5': is_eu_user && !is_mt5_allowed,
+                    })}
+                    ref={traders_hub_ref}
+                >
                     <MainTitleBar />
                     <DesktopWrapper>{getOrderedPlatformSections(true)}</DesktopWrapper>
                     <MobileWrapper>

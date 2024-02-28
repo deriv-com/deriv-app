ac483be28de9d834cdb01f5729658e6b650057e6
Taysuisin <suisin@regentmarkets.com>
2024-02-28 10:06:55 +0800

    chore: replace rounded-pill with rounded-full

--- packages/account-v2/src/components/IconButton/icon-button.classnames.ts
+++ packages/account-v2/src/components/IconButton/icon-button.classnames.ts
@@ -5,13 +5,13 @@ const iconButtonBaseClass = 'grid place-content-center border-none hover:cursor-
 export const iconButtonVariants = cva(iconButtonBaseClass, {
     variants: {
         isRound: {
-            true: 'rounded-pill',
+            true: 'rounded-full',
             false: 'rounded-4',
         },
         size: {
             sm: 'p-4 h-24 rounded-4',
             md: 'p-8 h-32 rounded-64',
-            lg: 'p-12 h-40 rounded-pill',
+            lg: 'p-12 h-40 rounded-full',
         },
         color: {
             primary: 'bg-typography-link hover:bg-solid-red-5',

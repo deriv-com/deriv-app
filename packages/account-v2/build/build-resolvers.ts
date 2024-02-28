b9fa4523c543a7513871bdd572eeff7873c50e36
adrienne-rio <adrienne@deriv.com>
2024-02-28 17:25:08 +0800

    chore: test out a theory

--- packages/account-v2/build/build-resolvers.ts
+++ packages/account-v2/build/build-resolvers.ts
@@ -1,5 +1,9 @@
 import { ResolveOptions } from 'webpack';
+import path from 'path';
 
 export const buildResolvers = (): ResolveOptions => ({
+    alias: {
+        '@deriv/api': path.resolve(__dirname, '../node_modules/@deriv/api'),
+    },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
 });

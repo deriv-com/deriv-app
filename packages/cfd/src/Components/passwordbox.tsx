3b4368d16c50feb1306030ad3c6c24f8d3995471
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-02-28 11:05:13 +0800

    fix: 'forgot password' is not underlined in safari

--- packages/cfd/src/Components/passwordbox.tsx
+++ packages/cfd/src/Components/passwordbox.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Button } from '@deriv/components';
 import { TPasswordBoxProps } from './props.types';
-import { localize } from '@deriv/translations';
+import { Localize } from '@deriv/translations';
 
 const PasswordBox = ({ onClick }: TPasswordBoxProps) => (
     <div className='cfd-trade-modal__password-box'>
         <Button className='cfd-trade-modal__password-action' transparent onClick={onClick}>
-            {localize('Forgot password?')}
+            <Localize i18n_default_text='Forgot password?' />
         </Button>
     </div>
 );

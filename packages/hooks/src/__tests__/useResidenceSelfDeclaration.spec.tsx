b0da7d3cc1b76f1cd300c34270969bad5b425162
Shaheer <shaheer@deriv.com>
2024-02-29 21:18:44 +0400

    refactor: refactors hook for account_opening_signup_declaration_required

--- packages/hooks/src/__tests__/useResidenceSelfDeclaration.spec.tsx
+++ packages/hooks/src/__tests__/useResidenceSelfDeclaration.spec.tsx
@@ -3,6 +3,22 @@ import { mockStore, StoreProvider } from '@deriv/stores';
 import { renderHook } from '@testing-library/react-hooks';
 import useResidenceSelfDeclaration from '../useResidenceSelfDeclaration';
 
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useResidenceList: jest.fn(() => ({
+        data: [
+            {
+                value: 'es',
+                account_opening_signup_declaration_required: true,
+            },
+            {
+                value: 'id',
+                account_opening_signup_declaration_required: false,
+            },
+        ],
+    })),
+}));
+
 describe('useResidenceSelfDeclaration', () => {
     test('should return true if client residence is spain', async () => {
         const mock = mockStore({
@@ -17,7 +33,8 @@ describe('useResidenceSelfDeclaration', () => {
         const { result } = renderHook(() => useResidenceSelfDeclaration(), { wrapper });
         expect(result.current.is_residence_self_declaration_required).toBe(true);
     });
-    test('should return false if client residence is spain', async () => {
+
+    test('should return false if client residence is not spain', async () => {
         const mock = mockStore({
             client: {
                 residence: 'id',
--- packages/hooks/src/useResidenceSelfDeclaration.ts
+++ packages/hooks/src/useResidenceSelfDeclaration.ts
@@ -1,11 +1,16 @@
+import { useResidenceList } from '@deriv/api';
 import { useStore } from '@deriv/stores';
 
 /** A custom hook used for enabling residenceSelfDeclaration checkbox in terms of use section in real account signup */
 const useResidenceSelfDeclaration = () => {
     const { client } = useStore();
+    const { data: residence_list } = useResidenceList();
     const { residence } = client;
-    //if residence is spain, we need to set is_residence_self_declaration_required  as true
-    return { is_residence_self_declaration_required: residence === 'es' };
+    const is_residence_self_declaration_required = residence_list?.find(
+        residence_item => residence_item?.value === residence
+    )?.account_opening_signup_declaration_required;
+
+    return { is_residence_self_declaration_required };
 };
 
 export default useResidenceSelfDeclaration;

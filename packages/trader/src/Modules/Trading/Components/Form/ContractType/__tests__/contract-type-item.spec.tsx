318d556bb60bc801ce014a198b1ac20eea3a083c
maryia-deriv <maryia@binary.com>
2024-02-29 22:53:31 +0300

    style: old misspellings

--- packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-item.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/ContractType/__tests__/contract-type-item.spec.tsx
@@ -23,12 +23,12 @@ describe('ContractTypeItem component', () => {
         const divElement = screen.getAllByTestId('dt_contract_item');
         expect(divElement).toHaveLength(2);
     });
-    it('should has "contract-type-item--selected" class when type.value array includes value', () => {
+    it('should have "contract-type-item--selected" class when type.value array includes value', () => {
         render(<ContractTypeItem {...mockProps} value={TRADE_TYPES.RISE_FALL_EQUAL} />);
         const divElement = screen.getAllByTestId('dt_contract_item');
         expect(divElement[0]).toHaveClass('contract-type-item--selected');
     });
-    it('should has "contract-type-item--selected" class when type.value is a string equal to value', () => {
+    it('should have "contract-type-item--selected" class when type.value is a string equal to value', () => {
         render(<ContractTypeItem {...mockProps} value={TRADE_TYPES.HIGH_LOW} />);
         const divElement = screen.getAllByTestId('dt_contract_item');
         expect(divElement[1]).toHaveClass('contract-type-item--selected');

926c6f3367f7185de0e6a529da02b53f5ceeb8d5
Shahzaib <shahzaib@deriv.com>
2024-02-29 13:48:58 +0800

    fix: update zip code regex to test leading spaces

--- packages/shared/src/utils/validation/regex-validation.ts
+++ packages/shared/src/utils/validation/regex-validation.ts
@@ -3,7 +3,7 @@ export const regex_checks = {
         address_city: /^\p{L}[\p{L}\s'.-]{0,99}$/u,
         address_line_1: /^[\p{L}\p{Nd}\s'.,:;()\u00b0@#/-]{1,70}$/u,
         address_line_2: /^[\p{L}\p{Nd}\s'.,:;()\u00b0@#/-]{0,70}$/u,
-        address_postcode: /^[a-zA-Z0-9\s-]{0,20}$/,
+        address_postcode: /^(?! )[a-zA-Z0-9\s-]{0,20}$/,
         address_state: /^[\w\s\W'.;,-]{0,99}$/,
         non_jersey_postcode: /^(?!\s*je.*)[a-zA-Z0-9\s-]*/i,
     },

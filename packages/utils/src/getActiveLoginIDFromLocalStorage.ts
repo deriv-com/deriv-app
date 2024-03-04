3cd816993fd34ce4bc15794d5b7155ff61aab929
Rostik Kayko <rostislav@deriv.com>
2024-03-04 18:07:12 +0300

    fix: a non-ideal way

--- packages/utils/src/getActiveLoginIDFromLocalStorage.ts
+++ packages/utils/src/getActiveLoginIDFromLocalStorage.ts
@@ -2,7 +2,8 @@
  * Gets the active `loginid` for the current user from the `localStorage`.
  */
 const getActiveLoginIDFromLocalStorage = (loginIDKey?: string) => {
-    const active_loginid = localStorage.getItem(loginIDKey ?? 'active_loginid');
+    const active_loginid =
+        localStorage.getItem(loginIDKey ?? 'active_loginid') || localStorage.getItem('active_loginid');
 
     // If there is no active loginid, return undefined.
     if (!active_loginid) return;

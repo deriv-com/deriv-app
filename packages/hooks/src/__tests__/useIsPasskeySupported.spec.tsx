b4e0b7e50d60c3782af209849e55aa53353fa248
utkarsha-deriv <125863995+utkarsha-deriv@users.noreply.github.com>
2024-03-04 13:22:34 +0400

    chore: add feature flag for passkeys using growthbook (#41)
    * chore: add feature flag for passkeys using growthbook
    
    * feat: add growthbook feature flag handling
    
    * test: modify testcases for useIsPasskeySupported hook
    
    * feat: add a condition to check if growthbook feature i loaded and then trigger checkPaaskeySupport
    
    * fix: remove comment
    
    * fix: remove typecasting and use git add . instead

--- packages/hooks/src/__tests__/useIsPasskeySupported.spec.tsx
+++ packages/hooks/src/__tests__/useIsPasskeySupported.spec.tsx
@@ -1,14 +1,19 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { platformAuthenticatorIsAvailable } from '@simplewebauthn/browser';
+import useGrowthbookFeatureFlag from '../useGrowthbookFeatureFlag';
 import useIsPasskeySupported from '../useIsPasskeySupported';
 
 jest.mock('@simplewebauthn/browser', () => ({
     platformAuthenticatorIsAvailable: jest.fn(),
 }));
+jest.mock('../useGrowthbookFeatureFlag', () => jest.fn());
+
+const mockUseGrowthbookFeatureFlag = useGrowthbookFeatureFlag as jest.MockedFunction<typeof useGrowthbookFeatureFlag>;
 
 describe('useIsPasskeySupported', () => {
     it('sets state correctly when passkey is supported', async () => {
         (platformAuthenticatorIsAvailable as jest.Mock).mockResolvedValue(true);
+        mockUseGrowthbookFeatureFlag.mockReturnValue(true);
 
         const { result, waitForNextUpdate } = renderHook(() => useIsPasskeySupported());
 
@@ -17,8 +22,31 @@ describe('useIsPasskeySupported', () => {
         expect(result.current.is_passkey_support_checking).toBe(false);
         expect(result.current.is_passkey_supported).toBe(true);
     });
-    it('sets state correctly when passkey is not supported', async () => {
+    it('does not support passkey when growthbook feature flag is true and authenticator is not supported', async () => {
         (platformAuthenticatorIsAvailable as jest.Mock).mockResolvedValue(false);
+        mockUseGrowthbookFeatureFlag.mockReturnValue(true);
+
+        const { result, waitForNextUpdate } = renderHook(() => useIsPasskeySupported());
+
+        expect(result.current.is_passkey_support_checking).toBe(true);
+        await waitForNextUpdate();
+        expect(result.current.is_passkey_support_checking).toBe(false);
+        expect(result.current.is_passkey_supported).toBe(false);
+    });
+    it('does not support passkey when growthbook feature flag is false and authenticator is not supported', async () => {
+        (platformAuthenticatorIsAvailable as jest.Mock).mockResolvedValue(false);
+        mockUseGrowthbookFeatureFlag.mockReturnValue(false);
+
+        const { result, waitForNextUpdate } = renderHook(() => useIsPasskeySupported());
+
+        expect(result.current.is_passkey_support_checking).toBe(true);
+        await waitForNextUpdate();
+        expect(result.current.is_passkey_support_checking).toBe(false);
+        expect(result.current.is_passkey_supported).toBe(false);
+    });
+    it('does not support passkey when growthbook feature flag is false and authenticator is supported', async () => {
+        (platformAuthenticatorIsAvailable as jest.Mock).mockResolvedValue(false);
+        mockUseGrowthbookFeatureFlag.mockReturnValue(false);
 
         const { result, waitForNextUpdate } = renderHook(() => useIsPasskeySupported());
 
--- packages/hooks/src/useIsPasskeySupported.ts
+++ packages/hooks/src/useIsPasskeySupported.ts
@@ -1,17 +1,20 @@
 import React from 'react';
 import { platformAuthenticatorIsAvailable } from '@simplewebauthn/browser';
+import useGrowthbookFeatureFlag from './useGrowthbookFeatureFlag';
 
 const useIsPasskeySupported = () => {
     const [is_passkey_supported, setIsPasskeySupported] = React.useState(false);
     const [is_passkey_support_checking, setIsPasskeySupportChecking] = React.useState(true);
+    const is_passkeys_enabled = useGrowthbookFeatureFlag({
+        featureFlag: 'web_passkeys',
+        defaultValue: 'unintialised',
+    });
 
     React.useEffect(() => {
         const checkPasskeySupport = async () => {
             try {
                 const result = await platformAuthenticatorIsAvailable();
-                //TODO: add feature flag with growthbook instead of is_passkeys_enabled here
-                const is_passkeys_enabled = true;
-                setIsPasskeySupported(result && is_passkeys_enabled);
+                setIsPasskeySupported(result && !!is_passkeys_enabled);
             } catch (error) {
                 /* eslint-disable no-console */
                 console.error('Error checking passkey support:', error);
@@ -19,9 +22,8 @@ const useIsPasskeySupported = () => {
                 setIsPasskeySupportChecking(false);
             }
         };
-
-        checkPasskeySupport();
-    }, []);
+        if (is_passkeys_enabled !== 'unintialised') checkPasskeySupport();
+    }, [is_passkeys_enabled]);
 
     return { is_passkey_supported, is_passkey_support_checking };
 };

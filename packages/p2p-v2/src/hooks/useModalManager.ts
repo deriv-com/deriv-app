8865feb13d8cebe5831c634021c451161db4c19e
adrienne-rio <adrienne@deriv.com>
2024-03-05 15:07:31 +0800

    feat: added modal manager hook and test cases

---
+++ packages/p2p-v2/src/hooks/useModalManager.ts
@@ -0,0 +1,113 @@
+import { useEffect } from 'react';
+import { useMap } from 'usehooks-ts';
+import useQueryString from './useQueryString';
+
+type TUseModalManagerConfig = {
+    shouldReinitializeModals?: boolean;
+};
+
+/**
+ * Hook to manage states for showing/hiding multiple modals
+ * Use this hook when you are managing more than 1 modal to show/hide
+ *
+ * @example
+ * ```
+ * const {isModalOpenFor, showModal} = useModalManager()
+ *
+ * return (
+ *      <>
+ *          <ModalA isOpen={isModalOpenFor('ModalA')} />
+ *          <ModalB isOpen={isModalOpenFor('ModalB')} />
+ *          <button onClick={() => showModal('ModalA')}>...</button>
+ *      </>
+ * )
+ * ```
+ */
+export default function useModalManager(config?: TUseModalManagerConfig) {
+    const { deleteQueryString, queryString, setQueryString } = useQueryString();
+
+    const [isModalOpenScopes, actions] = useMap();
+
+    useEffect(() => {
+        if (config?.shouldReinitializeModals !== undefined && config.shouldReinitializeModals === false) {
+            deleteQueryString('modal');
+        } else {
+            // sync modal query string in the URL with the initial modal open scopes
+            const modalHash = queryString.get('modal');
+            if (modalHash) {
+                const modalKeys = modalHash.split('+');
+                const currentModal = modalKeys.slice(-1)[0];
+                modalKeys.forEach(modalKey => {
+                    actions.set(modalKey, false);
+                });
+                actions.set(currentModal, true);
+            }
+        }
+    }, []);
+
+    const hideModal = () => {
+        const modalHash = queryString.get('modal');
+
+        if (modalHash) {
+            const modalIds = modalHash.split('+');
+            const currentModalId = modalIds.pop();
+            const previousModalId = modalIds.slice(-1)[0];
+            if (previousModalId) {
+                actions.set(currentModalId, false);
+                actions.set(previousModalId, true);
+            } else {
+                actions.set(currentModalId, false);
+            }
+            if (modalIds.length === 0) {
+                deleteQueryString('modal');
+            } else {
+                setQueryString({
+                    modal: modalIds.join('+'),
+                });
+            }
+        }
+    };
+
+    /**
+     * Keep the previous modal ids in the URL query strings separated by '+'
+     * This way, when there is a new modal to be shown, we can track the previous modals from the query string based on the last 2 segments
+     *
+     * Example:
+     * - ModalA is shown, URL becomes /...?modal=ModalA (current modal is ModalA, there is no previous modal)
+     * - ModalB is shown next, URL becomes /...?modal=ModalA+ModalB (current modal is ModalB, previous modal is ModalA)
+     * - ModalC is shown next, URL becomes /...?modal=ModalA+ModalB+ModalC (current modal is ModalC, previous modal is ModalB)
+     * - ModalC is closed, URL becomes becomes /...?modal=modalA+ModalB (current modal is ModalB, previous modal is ModalA)
+     */
+    const showModal = (modalId: string) => {
+        const modalHash = queryString.get('modal');
+
+        if (modalHash) {
+            const modalIds = modalHash.split('+');
+            const currentModalId = modalIds.slice(-1)[0];
+            // set the previous modal open state to false
+            // set the new modal open state to true
+            actions.set(currentModalId, false);
+            actions.set(modalId, true);
+            // push the state of the new modal to the hash
+            modalIds.push(modalId);
+            setQueryString({
+                modal: modalIds.join('+'),
+            });
+        } else {
+            actions.set(modalId, true);
+            setQueryString({
+                modal: modalId,
+            });
+        }
+    };
+
+    const isModalOpenFor = (modalKey: string) => {
+        return isModalOpenScopes.get(modalKey) || false;
+    };
+
+    return {
+        hideModal,
+        isModalOpenFor,
+        showModal,
+    };
+}

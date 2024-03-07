da35e41b20547023220e55bc7105f0bee2009f55
yaswanth-deriv <yaswanth@regentmarkets.com>
2024-03-07 12:32:00 +0400

    refactor: made changes in regex

--- packages/core/src/Utils/Datadog/index.ts
+++ packages/core/src/Utils/Datadog/index.ts
@@ -83,11 +83,11 @@ const initDatadog = (is_datadog_enabled: boolean) => {
                         'telegram token=REDACTED'
                     );
 
-                    if (event.resource.url.match(/^https:\/\/eu\.deriv\.com\/ctrader-login*/)) {
+                    if (event.resource.url.match(/^https:\/\/eu\.deriv\.com\/ctrader-login[a-zA-Z0-9&?&=]*$/)) {
                         const url = event.resource.url;
                         const accnt = getAcct1Value(url);
                         event.resource.url = event.resource.url.replace(
-                            /^https:\/\/eu\.deriv\.com\/ctrader-login*/,
+                            /^https:\/\/eu\.deriv\.com\/ctrader-login[a-zA-Z0-9&?&=]*$/,
                             `https://eu.deriv.com/ctrader-login?acct1=${accnt}&token1=redacted`
                         );
                     }

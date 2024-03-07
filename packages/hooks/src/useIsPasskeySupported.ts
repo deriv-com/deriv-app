bc91e942324f11966dd2fc55112b5ad5e68622fc
yauheni-deriv <yauheni@regentmarkets.com>
2024-03-07 15:24:31 +0300

    chore: null case for passkey feature flag

--- packages/hooks/src/useIsPasskeySupported.ts
+++ packages/hooks/src/useIsPasskeySupported.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import { platformAuthenticatorIsAvailable } from '@simplewebauthn/browser';
 import { Analytics } from '@deriv-com/analytics';
+import { isProduction } from '@deriv/shared';
 
 const useIsPasskeySupported = () => {
     const [is_passkey_supported, setIsPasskeySupported] = React.useState(false);
@@ -8,23 +9,23 @@ const useIsPasskeySupported = () => {
     const is_passkeys_enabled = Analytics?.getFeatureValue('web_passkeys', undefined);
 
     React.useEffect(() => {
-        if (is_passkeys_enabled === undefined) return;
+        if (is_passkeys_enabled === undefined || is_passkeys_enabled === null) return;
 
         const checkPasskeySupport = async () => {
             try {
                 const result = await platformAuthenticatorIsAvailable();
                 setIsPasskeySupported(result && !!is_passkeys_enabled);
             } catch (error) {
-                /* eslint-disable no-console */
-                console.error('Error checking passkey support:', error);
+                if (!isProduction()) {
+                    /* eslint-disable no-console */
+                    console.error('Error checking passkey support:', error);
+                }
             } finally {
                 setIsPasskeySupportChecking(false);
             }
         };
         checkPasskeySupport();
     }, [is_passkeys_enabled]);
-
-    console.log('is_passkey_supported', is_passkey_supported);
 
     return { is_passkey_supported, is_passkey_support_checking };
 };

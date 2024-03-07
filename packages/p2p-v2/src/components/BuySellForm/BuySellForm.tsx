c2fbd3f08016fb9c0c3da1305b71cc0d9ebe676e
Nada <nada.ajinas@deriv.com>
2024-03-07 09:35:38 +0400

    fix: removed curly braces

--- packages/p2p-v2/src/components/BuySellForm/BuySellForm.tsx
+++ packages/p2p-v2/src/components/BuySellForm/BuySellForm.tsx
@@ -157,9 +157,8 @@ const BuySellForm = ({
                     <div className='px-[2.4rem] mt-[2.4rem]'>
                         <InlineMessage variant='info'>
                             <Text size={isMobile ? 'xs' : '2xs'}>
-                                {
-                                    'If the market rate changes from the rate shown here, we won’t be able to process your order.'
-                                }
+                                If the market rate changes from the rate shown here, we won’t be able to process your
+                                order.
                             </Text>
                         </InlineMessage>
                     </div>

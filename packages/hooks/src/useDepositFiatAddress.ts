d7621a1593b73675c30530673a99c2c0e20d1c78
Taysuisin <suisin@regentmarkets.com>
2024-03-07 10:29:25 +0800

    chore: change cashier url string to include language tag

--- packages/hooks/src/useDepositFiatAddress.ts
+++ packages/hooks/src/useDepositFiatAddress.ts
@@ -7,7 +7,12 @@ const useDepositFiatAddress = () => {
     const { is_dark_mode_on } = ui;
     const { data, mutate, ...rest } = useRequest('cashier');
     const dark_mode = is_dark_mode_on ? 'on' : 'off';
-    const deposit_iframe_url = typeof data?.cashier === 'string' ? `${data?.cashier}&DarkMode=${dark_mode}` : undefined;
+    const url = window.location.href;
+    const urlObject = new URL(url);
+    const lang = urlObject.searchParams.get('lang')?.toLowerCase();
+    const updated_cashier_url =
+        typeof data?.cashier === 'string' ? data?.cashier?.replace(/Lang=en/, `Lang=${lang}`) : undefined;
+    const deposit_iframe_url = updated_cashier_url && `${updated_cashier_url}&DarkMode=${dark_mode}`;
 
     const send = useCallback(() => mutate({ payload: { cashier: 'deposit', provider: 'doughflow' } }), [mutate]);
 

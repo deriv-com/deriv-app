a9cdf67d330c476a420a189aca210666b9743dbc
Thisyahlen <thisyahlen@regentmarkets.com>
2024-03-08 15:17:15 +0800

    chore: sentemailcontent modal title fix

--- packages/tradershub/src/modals/SentEmailContentModal/SentEmailContentModal.tsx
+++ packages/tradershub/src/modals/SentEmailContentModal/SentEmailContentModal.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SentEmailContent } from '@/components';
 import { useQueryParams } from '@/hooks';
-import { Modal } from '@deriv-com/ui';
+import { Modal, Text } from '@deriv-com/ui';
 
 const SentEmailContentModal = () => {
     const { isModalOpen, closeModal } = useQueryParams();
     return (
         <Modal isOpen={isModalOpen('SentEmailContentModal')} onRequestClose={closeModal}>
-            <Modal.Header title="We've sent you an email" />
+            <Modal.Header>
+                <Text weight='bold'>We&apos;ve sent you an email</Text>
+            </Modal.Header>
             <Modal.Body>
                 <SentEmailContent />
             </Modal.Body>

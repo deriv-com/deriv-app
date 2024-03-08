ede68ec40c9011a281e57c99fa094bad603b6011
Nada <nada.ajinas@deriv.com>
2024-03-08 12:43:27 +0400

    fix: chat

---
+++ packages/p2p-v2/src/pages/my-ads/screens/OrdersChatSection/OrdersChatSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrdersChatSection = () => {
+    return (
+        <div>
+            <h1>OrdersChatSection</h1>
+        </div>
+    );
+};
+
+export default OrdersChatSection;

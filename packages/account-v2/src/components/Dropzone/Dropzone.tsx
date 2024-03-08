2d44cf2ed4773f08a37b71cd44e9274ce5cebe41
Taysuisin <suisin@regentmarkets.com>
2024-03-08 10:39:56 +0800

    chore: update data-testid to snake_case

--- packages/account-v2/src/components/Dropzone/Dropzone.tsx
+++ packages/account-v2/src/components/Dropzone/Dropzone.tsx
@@ -145,14 +145,14 @@ const Dropzone: React.FC<TProps> = ({
                                 className={classNames('account-dropzone__thumb', {
                                     'account-dropzone__thumb--has-frame': hasFrame,
                                 })}
-                                data-testid='dt_remove-button'
+                                data-testid='dt_remove_button'
                                 style={{ backgroundImage: `url(${file.preview})` }}
                             >
                                 {hasFrame && <DropzoneFrame />}
                                 <IconButton
                                     className='absolute top-400 right-400 rounded-pill'
                                     color='black'
-                                    data-testid='dt_remove-button-icon'
+                                    data-testid='dt_remove_button_icon'
                                     icon={<StandaloneXmarkBoldIcon className='fill-solid-slate-50' iconSize='sm' />}
                                     isRound
                                     onClick={removeFile}

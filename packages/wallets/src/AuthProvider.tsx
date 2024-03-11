acbd50fef3908f5b0e40087078c43192a6bbecb7
Rostik Kayko <rostislav@deriv.com>
2024-03-11 16:43:40 +0300

    fix: `active_wallets_loginid -> active_wallet_loginid`

--- packages/wallets/src/AuthProvider.tsx
+++ packages/wallets/src/AuthProvider.tsx
@@ -1,7 +1,7 @@
 import React, { ComponentProps } from 'react';
 import { AuthProvider } from '@deriv/api-v2';
 
-const WALLETS_LOGINID_LOCALSTORAGE_KEY = 'active_wallets_loginid';
+const WALLETS_LOGINID_LOCALSTORAGE_KEY = 'active_wallet_loginid';
 
 const WalletsAuthProvider = ({ children, ...rest }: Omit<ComponentProps<typeof AuthProvider>, 'loginIDKey'>) => (
     <AuthProvider {...rest} loginIDKey={WALLETS_LOGINID_LOCALSTORAGE_KEY}>

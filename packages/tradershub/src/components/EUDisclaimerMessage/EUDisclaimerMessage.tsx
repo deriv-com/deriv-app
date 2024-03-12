3b6b701eb094e6db18956563b60e2900c26bc476
mitra <mitra@regentmarkets.com>
2024-03-12 11:56:59 +0800

    fix: :lipstick: address comment

--- packages/tradershub/src/components/EUDisclaimerMessage/EUDisclaimerMessage.tsx
+++ packages/tradershub/src/components/EUDisclaimerMessage/EUDisclaimerMessage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from '@deriv-com/ui';
 
 const EUDisclaimerMessage = () => (
-    <div className='fixed bottom-[36px] w-full bg-system-light-secondary-background'>
+    <div className='fixed bottom-36 w-full bg-system-light-secondary-background'>
         <div className='max-w-[1440px] mx-auto py-8 px-16 lg:px-24'>
             <Text className='w-full text-xs lg:text-default leading-[1.5] '>
                 <span className='font-bold'>EU statutory disclaimer</span>: CFDs are complex instruments and come with a

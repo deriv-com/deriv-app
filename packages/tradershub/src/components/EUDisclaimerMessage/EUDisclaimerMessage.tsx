ad77f6e17b72882791c65cb5c66d2ede442692c7
mitra <mitra@regentmarkets.com>
2024-03-12 11:34:42 +0800

    fix: :art: fix the style per design

--- packages/tradershub/src/components/EUDisclaimerMessage/EUDisclaimerMessage.tsx
+++ packages/tradershub/src/components/EUDisclaimerMessage/EUDisclaimerMessage.tsx
@@ -3,8 +3,8 @@ import { Text } from '@deriv-com/ui';
 
 const EUDisclaimerMessage = () => (
     <div className='fixed bottom-[36px] w-full bg-system-light-secondary-background'>
-        <div className='max-w-[1390px] mx-auto py-8 sm:px-16'>
-            <Text className='w-full text-default sm:text-xs leading-[1.5]'>
+        <div className='max-w-[1440px] mx-auto py-8 px-16 lg:px-24'>
+            <Text className='w-full text-xs lg:text-default leading-[1.5] '>
                 <span className='font-bold'>EU statutory disclaimer</span>: CFDs are complex instruments and come with a
                 high risk of losing money rapidly due to leverage.
                 <span className='font-bold'>

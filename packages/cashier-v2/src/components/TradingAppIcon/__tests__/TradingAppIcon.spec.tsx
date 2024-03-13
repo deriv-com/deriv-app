d7f3a9c4df5528c8e0c7ebfec98774cc302053fe
Aum Bhatt <aum@deriv.com>
2024-03-13 17:28:12 +0400

    perf: added unit tests for CurrencyIcon

--- packages/cashier-v2/src/components/TradingAppIcon/__tests__/TradingAppIcon.spec.tsx
+++ packages/cashier-v2/src/components/TradingAppIcon/__tests__/TradingAppIcon.spec.tsx
@@ -5,35 +5,12 @@ import TradingAppIcon from '../TradingAppIcon';
 jest.mock('@deriv/quill-icons', () => ({
     ...jest.requireActual('@deriv/quill-icons'),
     AccountsDerivXIcon: jest.fn(({ iconSize }) => <div>AccountsDerivXIcon-{iconSize}</div>),
-    AccountsDmt5DerivedIcon: jest.fn(({ iconSize }) => <div>AccountsDmt5DerivedIcon-{iconSize}</div>),
-    AccountsDmt5FinancialIcon: jest.fn(({ iconSize }) => <div>AccountsDmt5FinancialIcon-{iconSize}</div>),
-    AccountsDmt5SwfIcon: jest.fn(({ iconSize }) => <div>AccountsDmt5SwfIcon-{iconSize}</div>),
 }));
 
 describe('<TradingAppIcon />', () => {
-    it('should render the correct icon for Deriv X account', () => {
+    it('should render the correct trading app icon', () => {
         render(<TradingAppIcon name='DERIVX' size='sm' />);
 
         expect(screen.getByText('AccountsDerivXIcon-sm')).toBeInTheDocument();
-    });
-    it('should render the correct icon for MT5 Derived account', () => {
-        render(<TradingAppIcon name='DMT5_DERIVED' size='sm' />);
-
-        expect(screen.getByText('AccountsDmt5DerivedIcon-sm')).toBeInTheDocument();
-    });
-    it('should render the correct icon for MT5 Financial account', () => {
-        render(<TradingAppIcon name='DMT5_FINANCIAL' size='sm' />);
-
-        expect(screen.getByText('AccountsDmt5FinancialIcon-sm')).toBeInTheDocument();
-    });
-    it('should render the correct icon for MT5 Synthetic account', () => {
-        render(<TradingAppIcon name='DMT5_SYNTHETIC' size='sm' />);
-
-        expect(screen.getByText('AccountsDmt5FinancialIcon-sm')).toBeInTheDocument();
-    });
-    it('should render the correct icon for MT5 Swap-Free account', () => {
-        render(<TradingAppIcon name='DMT5_ALL' size='sm' />);
-
-        expect(screen.getByText('AccountsDmt5SwfIcon-sm')).toBeInTheDocument();
     });
 });

a348b8bc475ee306e9b8f6b80e348038c1804e75
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-03-14 19:16:46 +0800

    build: update tailwind config

--- packages/account-v2/src/modules/tailwind.config.ts
+++ packages/account-v2/src/modules/tailwind.config.ts
@@ -2,10 +2,9 @@ import type { Config } from 'tailwindcss';
 
 export default {
     presets: [require('../../tailwind.config.ts')],
-    content: ['../src/**/*.{js,jsx,ts,tsx}'],
+    content: ['./dist/js/*.js', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {},
     },
-    purge: ['./src/**/*.{js,jsx,ts,tsx}'],
     plugins: [],
 } satisfies Config;

53a660fcd02ee5b94e20ab97917e10209cd33756
Wojciech Brygola <wojciech@regentmarkets.com>
2024-03-17 23:43:39 +0700

    feat: fixed default behavriour

--- packages/wallets/src/AuthProvider.tsx
+++ packages/wallets/src/AuthProvider.tsx
@@ -9,13 +9,15 @@ const WalletsAuthProvider = ({ children, ...rest }: Omit<ComponentProps<typeof A
         loginIDKey={WALLETS_LOGINID_LOCALSTORAGE_KEY}
         selectDefaultAccount={(accountsObject: any) => {
             const loginIds = Object.keys(accountsObject);
-            return loginIds.filter((loginId: string) => {
+            const result = loginIds.filter((loginId: string) => {
                 const { account_category, account_type, currency } = accountsObject[loginId];
                 const isWallet = account_category == 'wallet';
                 const isVirtual = account_type == 'virtual';
                 const isUsd = currency == 'USD';
                 return isWallet && !isVirtual && isUsd;
             })[0];
+
+            return result;
         }}
     >
         {children}

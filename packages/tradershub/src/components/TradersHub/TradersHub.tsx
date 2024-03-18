7229fa12fc489513280ede05c3f9096eb0f53060
mitra <mitra@regentmarkets.com>
2024-03-18 11:37:12 +0800

    fix: :bug: put Fragment

--- packages/tradershub/src/components/TradersHub/TradersHub.tsx
+++ packages/tradershub/src/components/TradersHub/TradersHub.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { AppContainer, EUDisclaimerMessage } from '@/components';
 import { useRegulationFlags } from '@/hooks';
 import { TradersHubRoute } from '@/routes';
@@ -7,12 +7,12 @@ const TradersHub = () => {
     const { isEU } = useRegulationFlags();
 
     return (
-        <>
+        <Fragment>
             <AppContainer>
                 <TradersHubRoute />
             </AppContainer>
             {isEU && <EUDisclaimerMessage />}
-        </>
+        </Fragment>
     );
 };
 

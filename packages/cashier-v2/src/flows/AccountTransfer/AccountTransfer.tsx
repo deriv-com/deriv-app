1c703f330312dc0f7095f3e7dadf6e5d720d8ebf
Aum Bhatt <aum@deriv.com>
2024-03-20 15:51:34 +0400

    chore: basic setup for TransferModule

---
+++ packages/cashier-v2/src/flows/AccountTransfer/AccountTransfer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PageContainer } from '../../components';
+import { Transfer } from '../../lib';
+
+const AccountTransfer = () => {
+    return (
+        <PageContainer>
+            <Transfer />
+        </PageContainer>
+    );
+};
+
+export default AccountTransfer;
---
+++ packages/cashier-v2/src/lib/Transfer/Transfer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Transfer = () => {
+    return <div>TransferModule</div>;
+};
+
+export default Transfer;

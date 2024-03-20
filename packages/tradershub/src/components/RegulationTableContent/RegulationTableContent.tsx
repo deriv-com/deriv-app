772588439657057efd9a2160b7016d6e159392e3
Thisyahlen <thisyahlen@regentmarkets.com>
2024-03-20 12:01:44 +0800

    chore: fix console error in regulation modal

--- packages/tradershub/src/components/RegulationTableContent/RegulationTableContent.tsx
+++ packages/tradershub/src/components/RegulationTableContent/RegulationTableContent.tsx
@@ -5,30 +5,41 @@ import Row from './Row';
 
 const RegulationTitle = ['Non-EU regulation', 'EU regulation'] as const;
 
+const TableHeader = () => (
+    <thead>
+        <tr>
+            <th className='sticky z-10 border-solid border-r-1 border-b-1 border-system-light-active-background start-0 min-h-40 bg-system-light-primary-background' />
+            {RegulationTitle.map(title => (
+                <th
+                    className='p-10 text-center border-solid min-h-40 border-r-1 border-y-1 border-system-light-active-background'
+                    key={title}
+                >
+                    <Text size='sm' weight='bold'>
+                        {title}
+                    </Text>
+                </th>
+            ))}
+        </tr>
+    </thead>
+);
+
+const TableBody = () => {
+    const combinedContents = [...getCFDContents, ...getOptionsContents];
+
+    return (
+        <tbody>
+            {combinedContents.map((row, idx) => (
+                <Row key={row.id} {...row} idx={idx} />
+            ))}
+        </tbody>
+    );
+};
+
 const RegulationTableContent = () => (
     <div className='overflow-x-scroll'>
         <table className='border-separate table-auto min-w-[457px] bg-system-light-primary-background'>
-            <thead>
-                <th className='sticky z-10 border-solid border-r-1 border-b-1 border-system-light-active-background start-0 min-h-40 bg-system-light-primary-background' />
-                {RegulationTitle.map(title => (
-                    <th
-                        className='p-10 text-center border-solid min-h-40 border-r-1 border-y-1 border-system-light-active-background'
-                        key={title}
-                    >
-                        <Text size='sm' weight='bold'>
-                            {title}
-                        </Text>
-                    </th>
-                ))}
-            </thead>
-            <tbody>
-                {getCFDContents.map((row, idx) => (
-                    <Row key={row.id} {...row} idx={idx} />
-                ))}
-                {getOptionsContents.map((row, idx) => (
-                    <Row key={row.id} {...row} idx={idx} />
-                ))}
-            </tbody>
+            <TableHeader />
+            <TableBody />
         </table>
     </div>
 );

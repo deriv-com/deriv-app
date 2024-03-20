8b135f5f791896db5986b9aeb5b0cbc1208e8c23
adrienne-rio <adrienne@deriv.com>
2024-03-20 15:37:51 +0800

    chore: added remove eslint rule in file

--- packages/p2p-v2/types.ts
+++ packages/p2p-v2/types.ts
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/no-namespace */
 import { useAdvertiserStats } from '@/hooks';
 import { p2p, useServerTime } from '@deriv/api-v2';
 
@@ -50,13 +51,10 @@ const prefix = '/cashier/p2p-v2';
 
 export type TRoutes = `${typeof prefix}/cashier/p2p-v2` | `${typeof prefix}`;
 
-// eslint-disable-next-line  @typescript-eslint/no-namespace
 export namespace THooks {
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace AdvertiserAdverts {
         export type Get = NonNullable<ReturnType<typeof p2p.advertiserAdverts.useGet>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace Advert {
         export type Get = NonNullable<ReturnType<typeof p2p.advert.useGet>['data']>;
         export type GetList = NonNullable<ReturnType<typeof p2p.advert.useGetList>['data']>;
@@ -64,24 +62,20 @@ export namespace THooks {
         export type Update = NonNullable<ReturnType<typeof p2p.advert.useUpdate>['data']>;
         export type Delete = NonNullable<ReturnType<typeof p2p.advert.useDelete>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace Advertiser {
         export type GetInfo = NonNullable<ReturnType<typeof p2p.advertiser.useGetInfo>['data']>;
         export type GetList = NonNullable<ReturnType<typeof p2p.advertiser.useGetList>['data']>;
         export type Create = NonNullable<ReturnType<typeof p2p.advertiser.useCreate>['data']>;
         export type Update = NonNullable<ReturnType<typeof p2p.advertiser.useUpdate>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace Counterparty {
         export type Get = NonNullable<ReturnType<typeof p2p.counterparty.useGet>['data']>;
         export type Block = NonNullable<ReturnType<typeof p2p.counterparty.useBlock>['data']>;
         export type Unblock = NonNullable<ReturnType<typeof p2p.counterparty.useUnblock>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace OrderDispute {
         export type Dispute = NonNullable<ReturnType<typeof p2p.orderDispute.useDispute>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace Order {
         export type Cancel = NonNullable<ReturnType<typeof p2p.order.useCancel>['data']>;
         export type Confirm = NonNullable<ReturnType<typeof p2p.order.useConfirm>['data']>;
@@ -89,18 +83,15 @@ export namespace THooks {
         export type Get = NonNullable<ReturnType<typeof p2p.order.useGet>['data']>;
         export type GetList = NonNullable<ReturnType<typeof p2p.order.useGetList>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace PaymentMethods {
         export type Get = NonNullable<ReturnType<typeof p2p.paymentMethods.useGet>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace AdvertiserPaymentMethods {
         export type Get = NonNullable<ReturnType<typeof p2p.advertiserPaymentMethods.useGet>['data']>;
         export type Create = NonNullable<ReturnType<typeof p2p.advertiserPaymentMethods.useCreate>['data']>;
         export type Update = NonNullable<ReturnType<typeof p2p.advertiserPaymentMethods.useUpdate>['data']>;
         export type Delete = NonNullable<ReturnType<typeof p2p.advertiserPaymentMethods.useDelete>['data']>;
     }
-    // eslint-disable-next-line  @typescript-eslint/no-namespace
     export namespace Settings {
         export type Get = NonNullable<ReturnType<typeof p2p.settings.useGetSettings>['data']>;
     }

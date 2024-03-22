0dbc1ecbc80b6c5080466a578f438c21ea120814
Taysuisin <suisin@regentmarkets.com>
2024-03-22 14:30:53 +0800

    chore: update regex to follow @deriv-com/utils regex patterns

--- packages/account-v2/src/utils/personalDetailsUtils.ts
+++ packages/account-v2/src/utils/personalDetailsUtils.ts
@@ -1,32 +1,32 @@
 import * as Yup from 'yup';
 import { GetSettings } from '@deriv/api-types';
+import { ValidationConstants } from '@deriv-com/utils';
 
 export const getPersonalDetailsBaseValidationSchema = () => {
     const characterLengthMessage = 'You should enter 2-50 characters.';
     const addressLengthMessage = 'Should be less than 70 characters.';
     const phoneNumberLengthMessage = 'You should enter 9-35 numbers.';
 
+    const regexPattern = ValidationConstants.patterns;
+
     return Yup.object({
         accountOpeningReason: Yup.string().required('Account opening reason is required.'),
         addressCity: Yup.string()
             .required('Town/City is required.')
             .max(70, addressLengthMessage)
-            .matches(
-                /^[A-Za-z]+(?:[.' -]*[A-Za-z]+){1,70}$/,
-                'Only letters, space, hyphen, period, and apostrophe are allowed.'
-            ),
+            .matches(regexPattern.addressCity, 'Only letters, space, hyphen, period, and apostrophe are allowed.'),
         addressLine1: Yup.string()
             .trim()
             .required('First line of address is required.')
             .max(70, addressLengthMessage)
-            .matches(/^[\p{L}\p{Nd}\s'.,:;()\u00b0@#/-]{0,70}$/u, 'Use only the following special characters: 70'),
+            .matches(regexPattern.address, 'Use only the following special characters: 70'),
         addressLine2: Yup.string()
             .trim()
             .max(70, addressLengthMessage)
-            .matches(/^[\p{L}\p{Nd}\s'.,:;()\u00b0@#/-]{0,70}$/u, 'Use only the following special characters: 70'),
+            .matches(regexPattern.address, 'Use only the following special characters: 70'),
         addressPostcode: Yup.string()
             .max(20, 'Please enter a Postal/ZIP code under 20 chatacters.')
-            .matches(/^[A-Za-z0-9][A-Za-z0-9\s-]*$/, 'Only letters, numbers, space, and hyphen are allowed.'),
+            .matches(regexPattern.postalCode, 'Only letters, numbers, space, and hyphen are allowed.'),
         citizenship: Yup.string().required('Citizenship is required.'),
         countryOfResidence: Yup.string().required('Country of residence is required.'),
         dateOfBirth: Yup.date().typeError('Please enter a valid date.').required('Date of birth is required.'),
@@ -35,16 +35,16 @@ export const getPersonalDetailsBaseValidationSchema = () => {
             .required('First name is required.')
             .min(2, characterLengthMessage)
             .max(50, characterLengthMessage)
-            .matches(/^[a-zA-Z\s\-.']+$/, 'Letters, spaces, periods, hyphens, apostrophes only.'),
+            .matches(regexPattern.name, 'Letters, spaces, periods, hyphens, apostrophes only.'),
         lastName: Yup.string()
             .required('Last Name is required.')
             .min(2, characterLengthMessage)
             .max(50, characterLengthMessage)
-            .matches(/^[a-zA-Z\s\-.']+$/, 'Letters, spaces, periods, hyphens, apostrophes only.'),
+            .matches(regexPattern.name, 'Letters, spaces, periods, hyphens, apostrophes only.'),
         nameDOBConfirmation: Yup.boolean().required(),
         phoneNumber: Yup.string()
             .required('Phone number is required.')
-            .matches(/^\+?([0-9-]+\s)*[0-9-]+$/, 'Please enter a valid phone number.')
+            .matches(regexPattern.phoneNumber, 'Please enter a valid phone number.')
             .min(9, phoneNumberLengthMessage)
             .max(35, phoneNumberLengthMessage),
         placeOfBirth: Yup.string().required('Place of birth is required.'),

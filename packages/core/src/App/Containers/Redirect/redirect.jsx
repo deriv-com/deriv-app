7f687ae826adf244701f4101089388710aa98dd6
Rostik Kayko <rostislav@deriv.com>
2024-03-25 15:02:41 +0300

    refactor: comment

--- packages/core/src/App/Containers/Redirect/redirect.jsx
+++ packages/core/src/App/Containers/Redirect/redirect.jsx
@@ -141,11 +141,13 @@ const Redirect = observer(() => {
         }
         case 'payment_withdraw': {
             if (is_next_wallet) {
-                // 1. pass verification_code through query param as we do not want to use localstorage/session storage
-                //    though can't use "verification_code" as name param
-                //    as there is general logic within client-store
-                //    which removes anything which resembles code=XYZ
-                // 2. pass loginid as a query param so that the withdrawal component knows what account is being withdrawn from
+                /**
+                 * 1. pass verification_code through query param as we do not want to use localstorage/session storage
+                 *    though can't use "verification_code" as name param
+                 *    as there is general logic within client-store
+                 *    which removes anything which resembles code=XYZ
+                 * 2. pass loginid as a query param so that the withdrawal component knows what account is being withdrawn from
+                 */
                 history.push(
                     `${routes.wallets_withdrawal}?verification=${verification_code.payment_withdraw}${
                         client.loginid ? `&loginid=${client.loginid}` : ''

36406117654ddfcd24cdc52650ab98b7d2b6563d
Dhruv-deriv <dhruv@regentmarkets.com>
2024-03-26 12:05:35 +0400

    fix: added neccesary changes in code in new branch

--- packages/api-v2/src/hooks/index.ts
+++ packages/api-v2/src/hooks/index.ts
@@ -80,3 +80,4 @@ export { default as useExchangeRates } from './useExchangeRates';
 export { default as useIsDIELEnabled } from './useIsDIELEnabled';
 export { default as useKycAuthStatus } from './useKycAuthStatus';
 export { default as useClientCountry } from './useClientCountry';
+export { default as useLoginHistory } from './useLoginHistory';

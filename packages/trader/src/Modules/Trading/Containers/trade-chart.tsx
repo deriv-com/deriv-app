eb875babd7896d76fcd01912d055936861bd730b
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-03-26 16:08:14 +0800

    fix: layout

--- packages/trader/src/Modules/Trading/Containers/trade-chart.tsx
+++ packages/trader/src/Modules/Trading/Containers/trade-chart.tsx
@@ -9,6 +9,7 @@ import ToolbarWidgets from '../../SmartChart/Components/toolbar-widgets';
 import type { TBottomWidgetsParams } from './trade';
 import { SmartChart } from 'Modules/SmartChart';
 import { ChartBarrierStore } from 'Stores/Modules/SmartChart/chart-barrier-store';
+import { isMobile as isMobileShared } from '@deriv/shared';
 
 type TTradeChartProps = {
     bottomWidgets?: (props: TBottomWidgetsParams) => React.ReactElement;
@@ -118,7 +119,7 @@ const TradeChart = observer((props: TTradeChartProps) => {
             enabledNavigationWidget={!isMobile}
             enabledChartFooter={false}
             id='trade'
-            isMobile={isMobile}
+            isMobile={isMobileShared()}
             maxTick={isMobile ? max_ticks : undefined}
             granularity={show_digits_stats || is_accumulator ? 0 : granularity}
             requestAPI={wsSendRequest}

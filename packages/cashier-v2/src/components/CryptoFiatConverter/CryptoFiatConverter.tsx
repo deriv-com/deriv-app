a964a51cdca008dc6173b056181e665a93d28681
Aum Bhatt <aum@deriv.com>
2024-03-27 13:05:11 +0400

    refactor: added PercentageSelector to CryptoFiatConverter

--- packages/cashier-v2/src/components/CryptoFiatConverter/CryptoFiatConverter.tsx
+++ packages/cashier-v2/src/components/CryptoFiatConverter/CryptoFiatConverter.tsx
@@ -3,9 +3,12 @@ import clsx from 'clsx';
 import { Field, FieldProps, useFormikContext } from 'formik';
 import { InferType } from 'yup';
 import { StandaloneArrowDownBoldIcon } from '@deriv/quill-icons';
-import { Input, useDevice } from '@deriv-com/ui';
+import { Input, Text, useDevice } from '@deriv-com/ui';
+import { PercentageSelector } from '../PercentageSelector';
 import { getCryptoFiatConverterValidationSchema, TGetCryptoFiatConverterValidationSchema } from './utils';
 import styles from './CryptoFiatConverter.module.scss';
+
+type TContext = InferType<ReturnType<typeof getCryptoFiatConverterValidationSchema>>;
 
 type TGetConvertedAmountParams =
     | TGetCryptoFiatConverterValidationSchema['fromAccount']
@@ -27,13 +30,24 @@ const getConvertedAmount = (amount: string, source: TGetConvertedAmountParams, t
     return convertedValue;
 };
 
-type TContext = InferType<ReturnType<typeof getCryptoFiatConverterValidationSchema>>;
-
 const CryptoFiatConverter: React.FC<TGetCryptoFiatConverterValidationSchema> = ({ fromAccount, toAccount }) => {
     const { isMobile } = useDevice();
     const [isFromInputActive, setIsFromInputActive] = useState(true);
+    const [areAccountCurrenciesDifferent, setAreAccountCurrenciesDifferent] = useState<boolean>(true);
+    const [percentage, setPercentage] = useState<number>(0);
 
-    const { errors, setFieldValue, setValues } = useFormikContext<TContext>();
+    const { errors, setFieldValue, setValues, values } = useFormikContext<TContext>();
+
+    useEffect(() => {
+        setAreAccountCurrenciesDifferent(fromAccount.currency !== toAccount.currency);
+    }, [fromAccount, toAccount]);
+
+    useEffect(() => {
+        if (values.fromAmount && !errors.fromAmount && !errors.toAmount) {
+            const value = Number(values.fromAmount);
+            setPercentage(Math.round((value * 100) / fromAccount.balance));
+        }
+    }, [errors.fromAmount, errors.toAmount, fromAccount.balance, values.fromAmount]);
 
     useEffect(() => {
         if (errors.toAmount && !isFromInputActive) {
@@ -46,6 +60,19 @@ const CryptoFiatConverter: React.FC<TGetCryptoFiatConverterValidationSchema> = (
             setFieldValue('toAmount', '');
         }
     }, [errors.fromAmount, isFromInputActive, setFieldValue]);
+
+    const onChangePercentage = (per: number) => {
+        const computedAmount = ((Number(fromAccount.balance) * per) / 100).toFixed(fromAccount.fractionalDigits) ?? 0;
+        const convertedAmount = getConvertedAmount(computedAmount, fromAccount, toAccount);
+
+        setPercentage(per);
+
+        setValues(currentValues => ({
+            ...currentValues,
+            fromAmount: computedAmount,
+            toAmount: convertedAmount,
+        }));
+    };
 
     const handleFromAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const convertedValue = getConvertedAmount(e.target.value, fromAccount, toAccount);
@@ -69,52 +96,68 @@ const CryptoFiatConverter: React.FC<TGetCryptoFiatConverterValidationSchema> = (
 
     return (
         <div className={styles.container}>
-            <Field name='fromAmount'>
-                {({ field }: FieldProps) => (
-                    <Input
-                        {...field}
-                        autoComplete='off'
-                        data-testid='dt_crypto_fiat_converter_from_amount_field'
-                        error={Boolean(errors.fromAmount)}
-                        isFullWidth={fromAccount.currency !== toAccount.currency}
-                        label={`Amount (${fromAccount.currency})`}
-                        message={errors.fromAmount}
-                        onChange={handleFromAmountChange}
-                        onFocus={() => {
-                            setIsFromInputActive(true);
-                        }}
-                        type='text'
+            {areAccountCurrenciesDifferent && (
+                <div className={styles['percentage-selector-container']}>
+                    <PercentageSelector
+                        amount={Number(values.fromAmount) ?? 0}
+                        balance={Number(fromAccount.balance) ?? 0}
+                        onChangePercentage={onChangePercentage}
                     />
-                )}
-            </Field>
-            {fromAccount.currency !== toAccount.currency && (
-                <>
-                    <div className={styles['arrow-container']}>
-                        <StandaloneArrowDownBoldIcon
-                            className={clsx(styles['arrow-icon'], { [styles['arrow-icon--rtl']]: isFromInputActive })}
-                            iconSize={isMobile ? 'sm' : 'md'}
-                        />
-                    </div>
-                    <Field name='toAmount'>
-                        {({ field }: FieldProps) => (
-                            <Input
-                                {...field}
-                                autoComplete='off'
-                                data-testid='dt_crypto_fiat_converter_to_amount_field'
-                                error={Boolean(errors.toAmount)}
-                                isFullWidth
-                                label={`Amount (${toAccount.currency})`}
-                                message={errors.toAmount}
-                                onChange={handleToAmountChange}
-                                onFocus={() => {
-                                    setIsFromInputActive(false);
-                                }}
-                                type='text'
-                            />
-                        )}
-                    </Field>
-                </>
+                    <Text color='less-prominent' size='xs'>
+                        {percentage}% of available balance ({fromAccount.balance})
+                    </Text>
+                </div>
             )}
+            <div className={styles['input-container']}>
+                <Field name='fromAmount'>
+                    {({ field }: FieldProps) => (
+                        <Input
+                            {...field}
+                            autoComplete='off'
+                            data-testid='dt_crypto_fiat_converter_from_amount_field'
+                            error={Boolean(errors.fromAmount)}
+                            isFullWidth={fromAccount.currency !== toAccount.currency}
+                            label={`Amount (${fromAccount.currency})`}
+                            message={errors.fromAmount}
+                            onChange={handleFromAmountChange}
+                            onFocus={() => {
+                                setIsFromInputActive(true);
+                            }}
+                            type='text'
+                        />
+                    )}
+                </Field>
+                {areAccountCurrenciesDifferent && (
+                    <>
+                        <div className={styles['arrow-container']}>
+                            <StandaloneArrowDownBoldIcon
+                                className={clsx(styles['arrow-icon'], {
+                                    [styles['arrow-icon--rtl']]: isFromInputActive,
+                                })}
+                                iconSize={isMobile ? 'sm' : 'md'}
+                            />
+                        </div>
+                        <Field name='toAmount'>
+                            {({ field }: FieldProps) => (
+                                <Input
+                                    {...field}
+                                    autoComplete='off'
+                                    data-testid='dt_crypto_fiat_converter_to_amount_field'
+                                    error={Boolean(errors.toAmount)}
+                                    isFullWidth
+                                    label={`Amount (${toAccount.currency})`}
+                                    message={errors.toAmount}
+                                    onChange={handleToAmountChange}
+                                    onFocus={() => {
+                                        setIsFromInputActive(false);
+                                    }}
+                                    type='text'
+                                />
+                            )}
+                        </Field>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
--- packages/cashier-v2/src/lib/Transfer/components/TransferForm/components/index.ts
+++ packages/cashier-v2/src/lib/Transfer/components/TransferForm/components/index.ts
@@ -1,2 +1,1 @@
-export * from './TransferAmountPercentageSelector';
 export * from './TransferCryptoFiatAmountConverter';

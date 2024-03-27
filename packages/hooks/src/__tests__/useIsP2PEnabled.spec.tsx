048e5c236e17109983b206676ce8478a1d83a7cb
ameerul-deriv <103412909+ameerul-deriv@users.noreply.github.com>
2024-03-27 13:50:17 +0800

    [P2PS] / Ameerul / P2PS-2143 Replace website_status/p2p_config API call with p2p_settings (#14157)
    * feat: added p2p settings provider
    
    * chore: replaced p2p_config checks from order and my-ads
    
    * chore: added p2p_settings in hooks and in socket
    
    * chore: fixed failing test cases
    
    * fix: cashier taking too long to load on tab switch
    
    * chore: moved cookie to isP2PEnabled hook
    
    * fix: localhost not showing cookies
    
    * fix: failing build
    
    * chore: moved subscribe to routes, set is_p2p_enabled in store from hook
    
    * fix: added setIsP2PEnabled inside mockStore
    
    * chore: added suggestions
    
    * chore: remove computed is_p2p_enabled
    
    * fix: subtasks
    
    * fix: sonar cloud issues
    
    * chore: empty commit
    
    * chore: empty commit
    
    * chore: empty commit
    
    * fix: remove dropdown stylelint issue
    
    * chore: skip failing test case
    
    * fix: subtasks
    
    * fix: withdrawal crypto not showing, review_period not updating
    
    * fix: hide modal for either Cancel or Ill do this later
    
    * chore: replaced is_authorise with isSuccess, fix deposit issue
    
    * chore: return null if currency_config is undefined
    
    * chore: show empty string if currency_config is undefined
    
    * chore: added ? check for currency_config
    
    * chore: test check if issue is from deposit-crypto
    
    * chore: uncomment uncommented code

--- packages/hooks/src/__tests__/useIsP2PEnabled.spec.tsx
+++ packages/hooks/src/__tests__/useIsP2PEnabled.spec.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import { useFetch } from '@deriv/api';
 import { mockStore } from '@deriv/stores';
 import { renderHook } from '@testing-library/react-hooks';
-import useIsP2PEnabled from '../useIsP2PEnabled';
 import { withMockAPIProvider } from '../mocks';
+import useIsP2PEnabled from '../useIsP2PEnabled';
+import useP2PSettings from '../useP2PSettings';
 
-jest.mock('@deriv/api', () => ({
-    ...jest.requireActual('@deriv/api'),
-    useFetch: jest.fn(),
-}));
+jest.mock('../useP2PSettings');
 
-const mockUseFetch = useFetch as jest.MockedFunction<typeof useFetch<'website_status'>>;
+const mockUseP2PSettings = useP2PSettings as jest.MockedFunction<typeof useP2PSettings>;
 
 describe('useIsP2PEnabled', () => {
+    beforeEach(() => {
+        mockUseP2PSettings.mockReturnValue({
+            // @ts-expect-error need to come up with a way to mock the return type
+            p2p_settings: {
+                supported_currencies: ['usd'],
+            },
+            // @ts-expect-error need to come up with a way to mock the return type
+            rest: {
+                isLoading: false,
+                isSubscribed: true,
+                isIdle: false,
+                error: undefined,
+            },
+        });
+    });
     test('should return false if users currency is not supported in p2p', () => {
         const mock = mockStore({ client: { currency: 'AUD' } });
 
-        // @ts-expect-error need to come up with a way to mock the return type of useFetch
-        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
-
-        const wrapper = withMockAPIProvider(mock);
+        const wrapper = withMockAPIProvider(mock, true);
 
         const { result } = renderHook(() => useIsP2PEnabled(), { wrapper });
 
-        expect(result.current.data).toBe(false);
+        expect(result.current.is_p2p_enabled).toBe(false);
     });
 
     test('should return false if users currency is supported in p2p but is virtual', () => {
@@ -34,14 +42,11 @@ describe('useIsP2PEnabled', () => {
             },
         });
 
-        // @ts-expect-error need to come up with a way to mock the return type of useFetch
-        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
-
-        const wrapper = withMockAPIProvider(mock);
+        const wrapper = withMockAPIProvider(mock, true);
 
         const { result } = renderHook(() => useIsP2PEnabled(), { wrapper });
 
-        expect(result.current.data).toBe(false);
+        expect(result.current.is_p2p_enabled).toBe(false);
     });
 
     test('should return true if users currency is supported in p2p and is_low_risk_cr_eu_real is false', () => {
@@ -51,14 +56,11 @@ describe('useIsP2PEnabled', () => {
             },
         });
 
-        // @ts-expect-error need to come up with a way to mock the return type of useFetch
-        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
-
-        const wrapper = withMockAPIProvider(mock);
+        const wrapper = withMockAPIProvider(mock, true);
 
         const { result } = renderHook(() => useIsP2PEnabled(), { wrapper });
 
-        expect(result.current.data).toBe(true);
+        expect(result.current.is_p2p_enabled).toBe(true);
     });
 
     test('should return false if users currency is supported in p2p but is_low_risk_cr_eu_real is true', () => {
@@ -71,13 +73,10 @@ describe('useIsP2PEnabled', () => {
             },
         });
 
-        // @ts-expect-error need to come up with a way to mock the return type of useFetch
-        mockUseFetch.mockReturnValue({ data: { website_status: { p2p_config: { supported_currencies: ['usd'] } } } });
-
-        const wrapper = withMockAPIProvider(mock);
+        const wrapper = withMockAPIProvider(mock, true);
 
         const { result } = renderHook(() => useIsP2PEnabled(), { wrapper });
 
-        expect(result.current.data).toBe(false);
+        expect(result.current.is_p2p_enabled).toBe(false);
     });
 });

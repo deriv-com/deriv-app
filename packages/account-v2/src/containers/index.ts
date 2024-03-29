79f9630c4ba1197ab7ca639c3bcb93ceb4d83fa3
Dhruv-deriv <dhruv@regentmarkets.com>
2024-03-29 12:28:30 +0400

    fix: removed imports and files

--- packages/account-v2/src/containers/index.ts
+++ packages/account-v2/src/containers/index.ts
@@ -1,5 +1,6 @@
 export { AccountClosureForm } from './AccountClosureForm/AccountClosureForm';
 export { AccountClosureSteps } from './AccountClosureSteps/AccountClosureSteps';
+export { LoginHistoryMobileCard } from './LoginHistoryTable/LoginHistoryMobileCard';
 export { LoginHistoryTable } from './LoginHistoryTable/LoginHistoryTable';
 export { OnfidoView } from './Onfido/OnfidoView';
 export { PersonalDetailsFormWithExample } from './PersonalDetailsFormWithExample/PersonalDetailsFormWithExample';

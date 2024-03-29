318213fc7d62d23f90d0ed749570e5f98dc17168
Dhruv-deriv <dhruv@regentmarkets.com>
2024-03-29 15:09:16 +0400

    fix: updated the header array for table headers

--- packages/account-v2/src/containers/LoginHistoryTable/LoginHistoryTable.tsx
+++ packages/account-v2/src/containers/LoginHistoryTable/LoginHistoryTable.tsx
@@ -7,29 +7,36 @@ type TLoginHistoryProps = {
     loginHistory: LoginHistory;
 };
 
-const header = {
-    action: 'Action',
-    browser: 'Browser',
-    datetime: 'Date and Time',
-    ipAddress: 'IP Address',
-    status: 'Status',
-} as const;
-
-const columnOrder = ['datetime', 'action', 'browser', 'ipAddress', 'status'] as const;
+const header = [
+    {
+        header: 'Date and Time',
+    },
+    {
+        header: 'Action',
+    },
+    {
+        header: 'Browser',
+    },
+    {
+        header: 'IP Address',
+    },
+    {
+        header: 'Status',
+    },
+];
 
 export const LoginHistoryTable = ({ loginHistory }: TLoginHistoryProps) => {
     const formattedLoginHistory = formattedLoginHistoryData(loginHistory);
-    const columns = columnOrder.map(key => ({ header: header[key] }));
-
     return (
         <div className='flex flex-col'>
             <Table
-                columns={columns}
+                columns={header}
                 data={formattedLoginHistory}
                 isFetching={false}
-                //[TODO]:
                 // eslint-disable-next-line @typescript-eslint/no-empty-function
-                loadMoreFunction={() => {}}
+                loadMoreFunction={() => {
+                    //[TODO]:
+                }}
                 renderHeader={header => <span>{header}</span>}
                 rowRender={data => (
                     <div className='grid grid-flow-col text-default'>

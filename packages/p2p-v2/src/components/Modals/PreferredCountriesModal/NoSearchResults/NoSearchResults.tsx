9d2a150586e2dc22deb4756b930ce626609db31a
Nada <nada.ajinas@deriv.com>
2024-04-01 12:45:32 +0400

    fix: text size fix for no seach results

--- packages/p2p-v2/src/components/Modals/PreferredCountriesModal/NoSearchResults/NoSearchResults.tsx
+++ packages/p2p-v2/src/components/Modals/PreferredCountriesModal/NoSearchResults/NoSearchResults.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Text } from '@deriv-com/ui';
+import { Text, useDevice } from '@deriv-com/ui';
 
 type TNoSearchResultsProps = {
     value: string;
 };
 const NoSearchResults = ({ value }: TNoSearchResultsProps) => {
+    const { isMobile } = useDevice();
     return (
         <div className='flex flex-col items-center justify-center mt-64 break-all'>
-            <Text align='center' weight='bold'>
+            <Text align='center' size={isMobile ? 'lg' : 'md'} weight='bold'>
                 {`No results for “${value}”.`}
             </Text>
-            <Text align='center'>Check your spelling or use a different term.</Text>
+            <Text align='center' size={isMobile ? 'md' : 'sm'}>
+                Check your spelling or use a different term.
+            </Text>
         </div>
     );
 };

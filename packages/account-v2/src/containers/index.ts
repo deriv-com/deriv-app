e8a6d0b39916b79b604cb5fb3eced6c05b67f0cf
Dhruv Neb <158162395+Dhruv-deriv@users.noreply.github.com>
2024-04-01 10:30:24 +0400

    Dhruv/coj 653/integrate the login history component accountsv2 (#14353)
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: added neccesary changes in code in new branch
    
    * fix: resolve comments
    
    * fix: resolve imports
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: resolve imports and rearranged imports
    
    * fix: resolved imports and added TODO
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: resolved importsof dayjs  and added TODO
    
    * fix:added TODO
    
    * fix: removed files
    
    * fix: removed files
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: removed extra file
    
    * fix: removed extra files and names updated
    
    * fix: removed extra file and names updated
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: files updated
    
    * fix: removed extra file
    
    * fix: removed extra code
    
    * fix: removed imports
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: removed imports and files
    
    * fix: removed imports and files and renamed header
    
    * fix: removed useMemo
    
    * fix: removed useMemo and renamed function
    
    * fix: removed useMemo and renamed function and header is declared as const
    
    * fix: updated the header array for table headers
    
    * fix: fixed container import
    
    * feat: Created a hook for fetching login history data
    
    * feat: Created static for Login History Section in accounts-v2
    
    * feat: added content to table
    
    * fix: index
    
    * fix: renamed files and updated dayjs
    
    * fix: snonarcloud issues
    
    ---------
    
    Co-authored-by: Likhith Kolayari <likhith@regentmarkets.com>

--- packages/account-v2/src/containers/index.ts
+++ packages/account-v2/src/containers/index.ts
@@ -1,5 +1,6 @@
 export { AccountClosureForm } from './AccountClosureForm/AccountClosureForm';
 export { AccountClosureSteps } from './AccountClosureSteps/AccountClosureSteps';
+export { LoginHistoryTable } from './LoginHistoryTable/LoginHistoryTable';
 export { OnfidoView } from './Onfido/OnfidoView';
 export { PersonalDetailsFormWithExample } from './PersonalDetailsFormWithExample/PersonalDetailsFormWithExample';
 export { POIFlowContainer } from './POIFlowContainer/POIFlowContainer';

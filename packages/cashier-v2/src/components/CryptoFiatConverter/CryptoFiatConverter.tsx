8d59adc94e7b12f5c96bc7335d8dc2e5631d9b47
Aum Bhatt <aum@deriv.com>
2024-04-01 09:06:27 +0400

    chore: remove unused imports

--- packages/cashier-v2/src/components/CryptoFiatConverter/CryptoFiatConverter.tsx
+++ packages/cashier-v2/src/components/CryptoFiatConverter/CryptoFiatConverter.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { Field, FieldProps, useFormikContext } from 'formik';
 import { InferType } from 'yup';

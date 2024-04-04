e03b99f0be67803b16a42b4472282650395798c5
yauheni-deriv <103182683+yauheni-deriv@users.noreply.github.com>
2024-04-04 12:28:18 +0300

    fix: [WEBREL-2641]/evgeniy/account settings flickering, not showing the error in passkeys page (#14477)
    * fix: [WEBREL-2641]/evgeniy/account settings flickering, not showing the error in passkeys page
    
    * fix: redirect from passkeys if FF off
    
    * fix: test
    
    * fix: test

--- packages/hooks/src/useGetPasskeysList.ts
+++ packages/hooks/src/useGetPasskeysList.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@deriv/api';
 import { useStore } from '@deriv/stores';
 
 const useGetPasskeysList = () => {
-    const { client, ui } = useStore();
+    const { client } = useStore();
     const { is_passkey_supported, is_logged_in } = client;
 
     const { data, error, isLoading, refetch, ...rest } = useQuery('passkeys_list', {
@@ -13,7 +13,7 @@ const useGetPasskeysList = () => {
 
     return {
         passkeys_list: data?.passkeys_list,
-        passkeys_list_error: error?.error ?? null,
+        passkeys_list_error: error ?? null,
         reloadPasskeysList: refetch,
         is_passkeys_list_loading: isLoading,
         ...rest,

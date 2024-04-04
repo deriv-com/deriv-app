e6a5db6a1b3cf94ec73562eab10da4faa3c49643
arshad-rao-deriv <muhammad.arshad@deriv.com>
2024-04-04 14:00:29 +0400

    refactor: removed unnecessary changes from cashier

--- packages/cashier-v2/build/build-resolvers.ts
+++ packages/cashier-v2/build/build-resolvers.ts
@@ -1,9 +1,5 @@
-import path from 'path';
 import { ResolveOptions } from 'webpack';
 
 export const buildResolvers = (): ResolveOptions => ({
-    alias: {
-        '@deriv-lib/account-v2-lib': path.resolve(__dirname, '../../account-v2/src/modules'),
-    },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
 });

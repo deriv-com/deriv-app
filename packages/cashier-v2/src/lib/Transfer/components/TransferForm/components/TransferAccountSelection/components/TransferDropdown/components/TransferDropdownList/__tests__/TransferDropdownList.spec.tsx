5bb5baaec23e5acc6391d8617a05f04cf6ae9bc5
Aum Bhatt <aum@deriv.com>
2024-04-05 09:46:40 +0400

    fix: unit tests for TransferDropdownList

--- packages/cashier-v2/src/lib/Transfer/components/TransferForm/components/TransferAccountSelection/components/TransferDropdown/components/TransferDropdownList/__tests__/TransferDropdownList.spec.tsx
+++ packages/cashier-v2/src/lib/Transfer/components/TransferForm/components/TransferAccountSelection/components/TransferDropdown/components/TransferDropdownList/__tests__/TransferDropdownList.spec.tsx
@@ -1,55 +1,77 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import TransferDropdownList from '../TransferDropdownList';
+import { TTransferableAccounts } from 'src/lib/Transfer/types';
 
 jest.mock('../../TransferAccountTile', () => ({
     ...jest.requireActual('../../TransferAccountTile'),
     TransferAccountTile: jest.fn(({ account, isActive }) => (
-        <span>
-            TransferAccountTile-{account.loginid}-{String(isActive)}
-        </span>
+        <span>{`TransferAccountTile-${account.loginid}-${String(isActive)}`}</span>
     )),
 }));
 
-const mockAccounts = [
-    {
-        loginid: 'CR1',
-    },
-    {
-        loginid: 'CR2',
-    },
-];
-
-const mockSelectedAccount = {
-    loginid: 'CR2',
+const mockAccounts = {
+    'Deriv MT5 accounts': [{ account_type: 'mt5', loginid: 'CR1' }],
+    'Deriv cTrader accounts': [{ account_type: 'ctrader', loginid: 'CR2' }],
+    'Deriv X accounts': [],
 };
+
+const mockSelectedAccount = mockAccounts['Deriv cTrader accounts'][0];
+
+const mockOnSelect = jest.fn(account => {});
 
 describe('<TransferDropdownList />', () => {
     it('should test if accounts are rendered properly under the correct header', () => {
-        //@ts-expect-error since this is a mock, we only need partial properties of accounts data
-        render(<TransferDropdownList accounts={mockAccounts} header='Deriv Mt5 accounts' />);
-        expect(screen.getByText('Deriv Mt5 accounts')).toBeInTheDocument();
-        expect(screen.getByText('TransferAccountTile-CR1-false')).toBeInTheDocument();
-    });
-
-    it('should test if onSelect is triggered upon clicking the account', async () => {
-        const mockOnSelect = jest.fn(account => {});
-        //@ts-expect-error since this is a mock, we only need partial properties of accounts data
-        render(<TransferDropdownList accounts={mockAccounts} header='Deriv Mt5 accounts' onSelect={mockOnSelect} />);
-        const firstAccount = screen.getByText('TransferAccountTile-CR1-false');
-
-        await fireEvent.click(firstAccount);
-
-        expect(mockOnSelect).toBeCalledWith(mockAccounts[0]);
-    });
-
-    it('should test if selected account is passed with isActive prop as true', async () => {
-        const mockOnSelect = jest.fn(account => {});
         render(
             <TransferDropdownList
                 //@ts-expect-error since this is a mock, we only need partial properties of accounts data
                 accounts={mockAccounts}
-                header='Deriv Mt5 accounts'
+                onSelect={mockOnSelect}
+                //@ts-expect-error since this is a mock, we only need partial properties of accounts data
+                value={mockAccounts['Deriv cTrader accounts']}
+            />
+        );
+
+        expect(screen.getByText('Deriv MT5 accounts')).toBeInTheDocument();
+        expect(screen.getByText('TransferAccountTile-CR1-false')).toBeInTheDocument();
+    });
+
+    it('should test if there are no accounts for a particular account_type then the header is also not present for that type', () => {
+        render(
+            <TransferDropdownList
+                //@ts-expect-error since this is a mock, we only need partial properties of accounts data
+                accounts={mockAccounts}
+                onSelect={mockOnSelect}
+                //@ts-expect-error since this is a mock, we only need partial properties of accounts data
+                value={mockAccounts['Deriv cTrader accounts']}
+            />
+        );
+
+        expect(screen.queryByText('Deriv X accounts')).not.toBeInTheDocument();
+    });
+
+    it('should test if onSelect is triggered upon clicking the account', async () => {
+        render(
+            <TransferDropdownList
+                //@ts-expect-error since this is a mock, we only need partial properties of accounts data
+                accounts={mockAccounts}
+                onSelect={mockOnSelect}
+                //@ts-expect-error since this is a mock, we only need partial properties of accounts data
+                value={mockAccounts['Deriv cTrader accounts']}
+            />
+        );
+        const firstAccount = screen.getByText('TransferAccountTile-CR1-false');
+
+        await fireEvent.click(firstAccount);
+
+        expect(mockOnSelect).toBeCalledWith(mockAccounts['Deriv MT5 accounts'][0]);
+    });
+
+    it('should test if selected account is passed with isActive prop as true', async () => {
+        render(
+            <TransferDropdownList
+                //@ts-expect-error since this is a mock, we only need partial properties of accounts data
+                accounts={mockAccounts}
                 onSelect={mockOnSelect}
                 //@ts-expect-error since this is a mock, we only need partial properties of accounts data
                 value={mockSelectedAccount}

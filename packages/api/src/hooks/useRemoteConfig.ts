750abcb661e0f6776996ca0b286988c7c2d01ca5
shayan khaleghparast <iman@regentmarkets.com>
2024-04-08 15:23:12 +0800

    fix: fixed remote config fetch request fail

--- packages/api/src/hooks/useRemoteConfig.ts
+++ packages/api/src/hooks/useRemoteConfig.ts
@@ -1,24 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { QueryFunction, useQuery } from '@tanstack/react-query';
 import initData from '../remote_config.json';
 
-const remoteConfigQuery = async function () {
+const remoteConfigQuery: QueryFunction<any, string[], any> = async function ({ signal }) {
     const isProductionOrStaging = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
     const REMOTE_CONFIG_URL = process.env.REMOTE_CONFIG_URL || '';
     if (isProductionOrStaging && REMOTE_CONFIG_URL === '') {
         throw new Error('Remote Config URL is not set!');
     }
-    const response = await fetch(REMOTE_CONFIG_URL);
-    if (!response.ok) {
+    try {
+        const response = await fetch(REMOTE_CONFIG_URL, {
+            signal,
+        });
+        return response.json();
+    } catch (error) {
         throw new Error('Remote Config Server is out of reach!');
     }
-    return response.json();
 };
 
-function useRemoteConfig() {
+function useRemoteConfig(enabled = false) {
     return useQuery({
         queryKey: ['remoteConfig'],
         queryFn: remoteConfigQuery,
         initialData: initData,
+        enabled,
     });
 }
 

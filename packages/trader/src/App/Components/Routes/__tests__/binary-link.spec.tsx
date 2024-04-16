b16ebde35003dc0d97e1bfb4483d3a431ab36962
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-04-16 09:25:51 +0800

    chore: test case fix

--- packages/trader/src/App/Components/Routes/__tests__/binary-link.spec.tsx
+++ packages/trader/src/App/Components/Routes/__tests__/binary-link.spec.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { cleanup, render, screen } from '@testing-library/react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { BinaryLink } from '../index';
+import userEvent from '@testing-library/user-event';
 
 type TMockBinaryLink = {
     to?: string;
@@ -16,23 +17,33 @@ const MockBinaryLink = ({ to }: TMockBinaryLink) => (
 );
 
 describe('BinaryLink component', () => {
+    beforeEach(() => cleanup());
     it('should render "children" when passed in', () => {
-        render(<MockBinaryLink />);
+        render(<MockBinaryLink to='/dtrader' />);
         expect(screen.getByTestId('dt_child')).toBeInTheDocument();
     });
 
     it('should have "active_class" when passed in', () => {
-        render(<MockBinaryLink to='/' />);
-        expect(screen.getByTestId('dt_binary_link')).toHaveClass('active_class');
+        render(<MockBinaryLink to='/dtrader' />);
+        userEvent.click(screen.getByTestId('dt_binary_link'));
+        const link = screen.getByTestId('dt_binary_link');
+        expect(link).toHaveClass('active_class');
     });
 
-    it('should render "NavLink" when "to" property is passed', () => {
-        render(<MockBinaryLink to='/' />);
+    it('should render "NavLink" when valid "to" property is passed', () => {
+        render(<MockBinaryLink to='/dtrader' />);
         expect(screen.getByTestId('dt_binary_link')).toBeInTheDocument();
     });
 
-    it('should render "a" element whe property "to" is not passed', () => {
-        render(<MockBinaryLink />);
-        expect(screen.getByTestId('dt_binary_link')).toBeInTheDocument();
+    it('throws an error for an invalid route', () => {
+        const viewComponent = () => render(<MockBinaryLink to='/invalid' />);
+        expect(viewComponent).toThrowError('Route not found: /invalid');
     });
+
+    // There is no active path right now and there will be when tradershub become the default page.
+    // That's when we can run this test case
+    // it('should render "a" element when property "to" is not passed', () => {
+    //     render(<MockBinaryLink />);
+    //     expect(screen.getByTestId('dt_binary_link')).toBeInTheDocument();
+    // });
 });

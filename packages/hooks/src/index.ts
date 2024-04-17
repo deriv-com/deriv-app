7ac6b7312360eb2859c829919fa0122851757487
“Matin-deriv” <matin@deriv.com>
2024-04-17 13:48:54 +0800

    chore: removal of useAccountTransferVisible

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,4 +1,3 @@
-export { default as useAccountTransferVisible } from './useAccountTransferVisible';
 export { default as useActiveWallet } from './useActiveWallet';
 export { default as useAuthorize } from './useAuthorize';
 export { default as useAvailableWallets } from './useAvailableWallets';

07e9be1014ad9dd3c5a4589830d7ca57cf925e3a
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-04-17 16:59:50 +0800

    chore: remove forwardref

--- packages/account-v2/src/components/InputWithButton/InputWithButton.tsx
+++ packages/account-v2/src/components/InputWithButton/InputWithButton.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps, forwardRef } from 'react';
+import React, { ComponentProps } from 'react';
 import { Button } from '@deriv-com/ui';
 import { FormInputField } from '../FormFields';
 
@@ -9,29 +9,36 @@ type InputWithButtonProps = ComponentProps<typeof FormInputField> & {
     onClick?: () => void;
 };
 
-export const InputWithButton = forwardRef<HTMLInputElement, InputWithButtonProps>(
-    ({ buttonText, isDisabled, isLoading, label, name, onClick, validationSchema, ...rest }: InputWithButtonProps) => {
-        return (
-            <div className='flex w-[400px] sm:max-w-[400px] sm:w-full mt-[27px] mx-auto mb-0'>
-                <FormInputField
-                    {...rest}
-                    className='rounded-ee-none rounded-se-none border-r-0 h-40'
-                    label={label}
-                    name={name}
-                    validationSchema={validationSchema}
-                    wrapperClassName='w-full'
-                />
-                <Button
-                    className='rounded-es-none rounded-ss-none h-40 min-w-64'
-                    disabled={isDisabled}
-                    isLoading={isLoading}
-                    onClick={onClick}
-                >
-                    {buttonText}
-                </Button>
-            </div>
-        );
-    }
-);
+export const InputWithButton = ({
+    buttonText,
+    isDisabled,
+    isLoading,
+    label,
+    name,
+    onClick,
+    validationSchema,
+    ...rest
+}: InputWithButtonProps) => {
+    return (
+        <div className='flex w-[400px] sm:max-w-[400px] sm:w-full mt-[27px] mx-auto mb-0'>
+            <FormInputField
+                {...rest}
+                className='rounded-ee-none rounded-se-none border-r-0 h-40'
+                label={label}
+                name={name}
+                validationSchema={validationSchema}
+                wrapperClassName='w-full'
+            />
+            <Button
+                className='rounded-es-none rounded-ss-none h-40 min-w-64'
+                disabled={isDisabled}
+                isLoading={isLoading}
+                onClick={onClick}
+            >
+                {buttonText}
+            </Button>
+        </div>
+    );
+};
 
 InputWithButton.displayName = 'InputWithButton';

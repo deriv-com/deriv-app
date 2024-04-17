4a04b7218d8adcc70e35633e650cb3a64bcb37af
hasan-deriv <abuhasan@deriv.com>
2024-04-17 10:56:28 +0800

    fix: sonarcloud issues

--- packages/shared/src/utils/os/os_detect.ts
+++ packages/shared/src/utils/os/os_detect.ts
@@ -8,15 +8,15 @@ declare global {
         };
     }
     interface Navigator {
-        userAgentData?: NavigatorUAData; // Your custom property
+        userAgentData?: NavigatorUAData;
     }
 }
 
-interface NavigatorUAData {
+type NavigatorUAData = {
     brands: Array<{ brand: string; version: string }>;
     mobile: boolean;
     getHighEntropyValues(hints: string[]): Promise<HighEntropyValues>;
-}
+};
 
 type HighEntropyValues = {
     platform?: string;
@@ -107,7 +107,7 @@ export const mobileOSDetect = () => {
 };
 
 export const mobileOSDetectAsync = async () => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera || '';
+    const userAgent = navigator.userAgent ?? window.opera ?? '';
     const huaweiDevicesRegex =
         /\b(ALP-|AMN-|ANA-|ANE-|ANG-|AQM-|ARS-|ART-|ATU-|BAC-|BLA-|BRQ-|CAG-|CAM-|CAN-|CAZ-|CDL-|CDY-|CLT-|CRO-|CUN-|DIG-|DRA-|DUA-|DUB-|DVC-|ELE-|ELS-|EML-|EVA-|EVR-|FIG-|FLA-|FRL-|GLK-|HMA-|HW-|HWI-|INE-|JAT-|JEF-|JER-|JKM-|JNY-|JSC-|LDN-|LIO-|LON-|LUA-|LYA-|LYO-|MAR-|MED-|MHA-|MLA-|MRD-|MYA-|NCE-|NEO-|NOH-|NOP-|OCE-|PAR-|PIC-|POT-|PPA-|PRA-|RNE-|SEA-|SLA-|SNE-|SPN-|STK-|TAH-|TAS-|TET-|TRT-|VCE-|VIE-|VKY-|VNS-|VOG-|VTR-|WAS-|WKG-|WLZ-|JAD-|MLD-|RTE-|NAM-|NEN-|BAL-|JLN-|YAL|MGA-|FGD-|XYAO-|BON-|ALN-|ALT-|BRA-|DBY2-|STG-|MAO-|LEM-|GOA-|FOA-|MNA-|LNA-)\b/i;
 

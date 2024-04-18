2baa50464dd79be125be818ce73a7c890dde70f0
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2024-04-18 16:12:55 +0300

    fix: Enabling & disabling the max stake in the mid of running bot behaves differently on test link

--- packages/bot-web-ui/src/pages/bot-builder/quick-strategy/form-wrappers/desktop-form-wrapper.tsx
+++ packages/bot-web-ui/src/pages/bot-builder/quick-strategy/form-wrappers/desktop-form-wrapper.tsx
@@ -23,7 +23,7 @@ const FormWrapper: React.FC<TDesktopFormWrapper> = observer(({ children, onClick
     const [activeTab, setActiveTab] = React.useState('TRADE_PARAMETERS');
     const { submitForm, isValid, setFieldValue, validateForm } = useFormikContext<TFormValues>();
     const { quick_strategy } = useDBotStore();
-    const { selected_strategy, setSelectedStrategy, onSubmit } = quick_strategy;
+    const { selected_strategy, setSelectedStrategy, onSubmit, is_stop_bot_dialog_open } = quick_strategy;
     const strategy = STRATEGIES[selected_strategy as keyof typeof STRATEGIES];
     const { handleSubmit } = useQsSubmitHandler();
 
@@ -55,88 +55,90 @@ const FormWrapper: React.FC<TDesktopFormWrapper> = observer(({ children, onClick
     };
 
     return (
-        <div className='qs'>
-            <div className='qs__head'>
-                <div className='qs__head__title'>
-                    <Text weight='bold'>{localize('Quick Strategy')}</Text>
-                </div>
-                <div className='qs__head__action'>
-                    <span
-                        data-testid='qs-desktop-close-button'
-                        onClick={onClickClose}
-                        tabIndex={0}
-                        onKeyDown={(e: React.KeyboardEvent) => {
-                            if (e.key === 'Enter') {
-                                onClickClose();
-                            }
-                        }}
-                    >
-                        <Icon icon='IcCross' />
-                    </span>
-                </div>
-            </div>
-            <div className='qs__body'>
-                <div className='qs__body__sidebar'>
-                    <div className='qs__body__sidebar__subtitle'>
-                        <Text size='xs'>{localize('Choose a template below and set your trade parameters.')}</Text>
+        !is_stop_bot_dialog_open && (
+            <div className='qs'>
+                <div className='qs__head'>
+                    <div className='qs__head__title'>
+                        <Text weight='bold'>{localize('Quick Strategy')}</Text>
                     </div>
-                    <div className='qs__body__sidebar__items'>
-                        <ul>
-                            {(Object.keys(STRATEGIES) as (keyof typeof STRATEGIES)[]).map(key => {
-                                const str = STRATEGIES[key];
-                                const active = key === selected_strategy;
-                                return (
-                                    <li
-                                        className={classNames({ active })}
-                                        key={key}
-                                        onClick={() => onChangeStrategy(String(key))}
-                                    >
-                                        <Text size='xs' weight={active ? 'bold' : 'normal'}>
-                                            {str.label}
-                                        </Text>
-                                    </li>
-                                );
+                    <div className='qs__head__action'>
+                        <span
+                            data-testid='qs-desktop-close-button'
+                            onClick={onClickClose}
+                            tabIndex={0}
+                            onKeyDown={(e: React.KeyboardEvent) => {
+                                if (e.key === 'Enter') {
+                                    onClickClose();
+                                }
+                            }}
+                        >
+                            <Icon icon='IcCross' />
+                        </span>
+                    </div>
+                </div>
+                <div className='qs__body'>
+                    <div className='qs__body__sidebar'>
+                        <div className='qs__body__sidebar__subtitle'>
+                            <Text size='xs'>{localize('Choose a template below and set your trade parameters.')}</Text>
+                        </div>
+                        <div className='qs__body__sidebar__items'>
+                            <ul>
+                                {(Object.keys(STRATEGIES) as (keyof typeof STRATEGIES)[]).map(key => {
+                                    const str = STRATEGIES[key];
+                                    const active = key === selected_strategy;
+                                    return (
+                                        <li
+                                            className={classNames({ active })}
+                                            key={key}
+                                            onClick={() => onChangeStrategy(String(key))}
+                                        >
+                                            <Text size='xs' weight={active ? 'bold' : 'normal'}>
+                                                {str.label}
+                                            </Text>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='qs__body__content'>
+                        <ThemedScrollbars
+                            className={classNames('qs__form__container', {
+                                'qs__form__container--no-footer': activeTab !== 'TRADE_PARAMETERS',
                             })}
-                        </ul>
+                            autohide={false}
+                        >
+                            <div ref={active_tab_ref}>
+                                <FormTabs
+                                    active_tab={activeTab}
+                                    onChange={handleTabChange}
+                                    description={strategy?.description}
+                                />
+                            </div>
+                            <StrategyTabContent formfields={children} active_tab={activeTab} />
+                        </ThemedScrollbars>
+                        {activeTab === 'TRADE_PARAMETERS' && (
+                            <div className='qs__body__content__footer'>
+                                <Button secondary disabled={!isValid} onClick={onEdit}>
+                                    {localize('Edit')}
+                                </Button>
+                                <Button
+                                    data-testid='qs-run-button'
+                                    primary
+                                    onClick={e => {
+                                        e.preventDefault();
+                                        onRun();
+                                    }}
+                                    disabled={!isValid}
+                                >
+                                    {localize('Run')}
+                                </Button>
+                            </div>
+                        )}
                     </div>
                 </div>
-                <div className='qs__body__content'>
-                    <ThemedScrollbars
-                        className={classNames('qs__form__container', {
-                            'qs__form__container--no-footer': activeTab !== 'TRADE_PARAMETERS',
-                        })}
-                        autohide={false}
-                    >
-                        <div ref={active_tab_ref}>
-                            <FormTabs
-                                active_tab={activeTab}
-                                onChange={handleTabChange}
-                                description={strategy?.description}
-                            />
-                        </div>
-                        <StrategyTabContent formfields={children} active_tab={activeTab} />
-                    </ThemedScrollbars>
-                    {activeTab === 'TRADE_PARAMETERS' && (
-                        <div className='qs__body__content__footer'>
-                            <Button secondary disabled={!isValid} onClick={onEdit}>
-                                {localize('Edit')}
-                            </Button>
-                            <Button
-                                data-testid='qs-run-button'
-                                primary
-                                onClick={e => {
-                                    e.preventDefault();
-                                    onRun();
-                                }}
-                                disabled={!isValid}
-                            >
-                                {localize('Run')}
-                            </Button>
-                        </div>
-                    )}
-                </div>
             </div>
-        </div>
+        )
     );
 });
 

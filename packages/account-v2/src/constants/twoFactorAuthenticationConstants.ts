1ed5caf2e10029e9d1586466affddd8279d98654
Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>
2024-04-19 18:05:28 +0800

    Jim/COJ-523/Add two factor section (#14596)
    * chore: implement initial two factor authentication
    
    * chore: implement two factor authentication and add test cases
    
    * chore: add form component
    
    * chore: remove unnecessary comment
    
    * chore: undo auto fix on dropdown styles
    
    * chore: update sidenote title and remove unused code
    
    * chore: add tsdoc and fix type error
    
    * fix: fix failing test
    
    * chore: remove unnecessary onclick handler
    
    * chore: remove unnecessary import
    
    * chore: clean up
    
    * fix: fix failing test
    
    * chore: clean up
    
    * chore: resolve comments
    
    * chore: resolve review comments
    
    * chore: address review comments
    
    * chore: disable grid cols for mobile
    
    * chore: address review comments
    
    * chore: resolve review comments
    
    * chore: remove forwardref

---
+++ packages/account-v2/src/constants/twoFactorAuthenticationConstants.ts
@@ -0,0 +1,4 @@
+export const TWO_FACTOR_AUTHENTICATION_URLS = {
+    authy: 'https://authy.com/',
+    googleAuthenticator: 'https://github.com/google/google-authenticator/wiki#implementations',
+};

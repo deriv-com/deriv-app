70f13a299a85d7a649a28403fb4b0c6e25ad18aa
Dhruv-deriv <dhruv@deriv.com>
2024-04-22 09:11:59 +0400

    fix: added map function and removed index

--- packages/account-v2/src/containers/LoginHistoryTable/LoginHistoryTableCard.tsx
+++ packages/account-v2/src/containers/LoginHistoryTable/LoginHistoryTableCard.tsx
@@ -4,47 +4,39 @@ import { Text } from '@deriv-com/ui';
 import { formattedLoginHistoryData } from '../../utils';
 
 type TLoginHistoryProps = { loginHistory: LoginHistory };
+
 export const LoginHistoryTableCard = ({ loginHistory }: TLoginHistoryProps) => {
     const formattedLoginHistory = formattedLoginHistoryData(loginHistory);
+
     return (
         <div className='inline-flex flex-col items-start gap-16 w-full'>
-            {formattedLoginHistory.map((item, index) => (
+            {formattedLoginHistory.map(item => (
                 <div
-                    className='shadow-md p-16 rounded-lg background: var(--system-light-8-primary-background, #FFF) w-full'
-                    key={index}
+                    className='shadow-md p-16 rounded-lg bg-[var(--system-light-8-primary-background,#FFF)] w-full'
+                    key={item.ip}
                 >
-                    <div className='flex gap-100'>
+                    <div className='flex gap-40'>
                         <div className='justify-left flex flex-col items-start gap-8 row-span-3'>
-                            <div className='grid grid-row-2 gap-4'>
-                                <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='bold'>
-                                    Date and Time
-                                </Text>
-                                <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='normal'>
-                                    {item.date}
-                                </Text>
-                            </div>
-                            <div className='grid grid-row-2 gap-4'>
-                                <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='bold'>
-                                    Browser
-                                </Text>
-                                <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='normal'>
-                                    {item.browser}
-                                </Text>
-                            </div>
-                            <div className='grid grid-row-2 gap-4'>
-                                <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='bold'>
-                                    IP Address
-                                </Text>
-                                <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='normal'>
-                                    {item.ip}
-                                </Text>
-                            </div>
+                            {[
+                                { label: 'Date and Time', value: item.date },
+                                { label: 'Browser', value: item.browser },
+                                { label: 'IP Address', value: item.ip },
+                            ].map((gridItem, gridIndex) => (
+                                <div className='grid grid-row-2 gap-4' key={gridIndex}>
+                                    <Text align='left' color='general' size='lg' weight='bold'>
+                                        {gridItem.label}
+                                    </Text>
+                                    <Text align='left' color='general' size='lg' weight='normal'>
+                                        {gridItem.value}
+                                    </Text>
+                                </div>
+                            ))}
                         </div>
-                        <div className=' justify-center  items-right col-span-1 flex flex-col  gap-4 '>
-                            <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='bold'>
+                        <div className='justify-center items-right col-span-1 flex flex-col gap-4'>
+                            <Text align='left' color='general' size='lg' weight='bold'>
                                 Action
                             </Text>
-                            <Text align='left' color='general' fontStyle='IBM Plex Sans' size='lg' weight='normal'>
+                            <Text align='left' color='general' size='lg' weight='normal'>
                                 {item.action}
                             </Text>
                         </div>

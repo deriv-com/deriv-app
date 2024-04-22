bbf6cb3881a2fee8523c69f1e65e804cd069dab4
Dhruv-deriv <dhruv@deriv.com>
2024-04-22 11:37:44 +0400

    fix: removed unwanted values

--- packages/account-v2/src/containers/LoginHistoryTable/LoginHistoryTableCard.tsx
+++ packages/account-v2/src/containers/LoginHistoryTable/LoginHistoryTableCard.tsx
@@ -12,7 +12,7 @@ export const LoginHistoryTableCard = ({ loginHistory }: TLoginHistoryProps) => {
         <div className='inline-flex flex-col items-start gap-16 w-full'>
             {formattedLoginHistory.map(item => (
                 <div
-                    className='shadow-md p-16 rounded-lg bg-[var(--system-light-8-primary-background,#FFF)] w-full'
+                    className='shadow-md p-16 rounded-lg bg-[var(system-light-primary-background,#FFF)] w-full'
                     key={item.ip}
                 >
                     <div className='flex gap-40'>
@@ -23,20 +23,20 @@ export const LoginHistoryTableCard = ({ loginHistory }: TLoginHistoryProps) => {
                                 { label: 'IP Address', value: item.ip },
                             ].map((gridItem, gridIndex) => (
                                 <div className='grid grid-row-2 gap-4' key={gridIndex}>
-                                    <Text align='left' color='general' size='lg' weight='bold'>
+                                    <Text size='lg' weight='bold'>
                                         {gridItem.label}
                                     </Text>
-                                    <Text align='left' color='general' size='lg' weight='normal'>
+                                    <Text size='lg' weight='normal'>
                                         {gridItem.value}
                                     </Text>
                                 </div>
                             ))}
                         </div>
                         <div className='justify-center items-right col-span-1 flex flex-col gap-4'>
-                            <Text align='left' color='general' size='lg' weight='bold'>
+                            <Text size='lg' weight='bold'>
                                 Action
                             </Text>
-                            <Text align='left' color='general' size='lg' weight='normal'>
+                            <Text size='lg' weight='normal'>
                                 {item.action}
                             </Text>
                         </div>

0f44605424bfc0cc6fde73b52c3feee017637e76
adrienne-rio <adrienne@deriv.com>
2024-04-23 11:39:57 +0800

    chore: removed documentation for p2p-country-list hook

--- packages/api-v2/src/hooks/p2p/entity/country/p2p-country-list/useCountryList.ts
+++ packages/api-v2/src/hooks/p2p/entity/country/p2p-country-list/useCountryList.ts
@@ -1,14 +1,6 @@
 import useQuery from '../../../../../useQuery';
 import useAuthorize from '../../../../useAuthorize';
 
-/**
- * A custom hook that returns an object containing the list of countries available for P2P trading.
- *
- * For returning details of a specific country, the country code can be passed in the payload.
- * @example: useCountryList({ country: 'id' })
- *
- */
-
 const useCountryList = (payload?: NonNullable<Parameters<typeof useQuery<'p2p_country_list'>>[1]>['payload']) => {
     const { isSuccess } = useAuthorize();
     const { data, ...rest } = useQuery('p2p_country_list', {

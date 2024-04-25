4a71f6e0f90512d7b0de6d66859c36218f48e56d
amina-deriv <84661147+amina-deriv@users.noreply.github.com>
2024-04-25 17:18:53 +0400

    Amina/account limits section for accounts v2 (#14747)
    * feat: draft
    
    * feat: draft
    
    * fix: table layout
    
    * fix: subcategory
    
    * fix: table value
    
    * fix: category
    
    * fix: side note
    
    * fix: content
    
    * fix: content
    
    * fix: content
    
    * fix: type
    
    * fix: resolve conflict
    
    * fix: resolve type
    
    * fix: fill color
    
    * fix: resolve commits
    
    * fix: remove eslint formating
    
    * fix: remove eslint formating
    
    * fix: resolve conflicts
    
    * fix: resolve commits
    
    * fix: conflict

--- packages/account-v2/src/components/IconWithMessage/IconWithMessage.tsx
+++ packages/account-v2/src/components/IconWithMessage/IconWithMessage.tsx
@@ -1,15 +1,17 @@
 import React, { ReactNode } from 'react';
 import { Text } from '@deriv-com/ui';
+import { twMerge } from 'tailwind-merge';
 
 type TIconWithMessage = {
     actionButton?: ReactNode;
     children?: ReactNode;
+    className?: string;
     icon: JSX.Element;
     title: string;
 };
 
-export const IconWithMessage = ({ actionButton, children, icon, title }: TIconWithMessage) => (
-    <div className='flex flex-col w-full'>
+export const IconWithMessage = ({ actionButton, children, className, icon, title }: TIconWithMessage) => (
+    <div className={twMerge('flex flex-col w-full', className)}>
         {icon}
         <div className='grid justify-center gap-16 mt-24 mb-32'>
             <Text align='center' size='md' weight='bold'>

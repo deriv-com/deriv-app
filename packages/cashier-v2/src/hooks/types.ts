e441e2a4837d344a9d805b8fb46481f4369a57cd
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2024-04-25 17:26:28 +0300

    Update types.ts

--- packages/cashier-v2/src/hooks/types.ts
+++ packages/cashier-v2/src/hooks/types.ts
@@ -15,7 +15,7 @@ import {
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace THooks {
     export type Accounts = NonNullable<ReturnType<typeof useAllAccountsList>['data']>;
-    export type AccountLimits = Omit<NonNullable<ReturnType<typeof useAccountLimits>['data']>, 'daily_transfers'>;
+    export type AccountLimits = NonNullable<ReturnType<typeof useAccountLimits>['data']>;
     export type ActiveAccount = NonNullable<ReturnType<typeof useActiveAccount>['data']>;
     export type CryptoWithdrawal = NonNullable<ReturnType<typeof useCryptoWithdrawal>['mutateAsync']>;
     export type CurrencyConfig = NonNullable<ReturnType<typeof useCurrencyConfig>['data']>[string];

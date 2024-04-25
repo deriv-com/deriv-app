d4769733486ff681e0785aec7247668bcdd4d73d
hasan-deriv <abuhasan@deriv.com>
2024-04-25 16:11:28 +0800

    chore: updated mt5 success modal content

--- packages/cfd/src/Containers/cfd-password-modal.tsx
+++ packages/cfd/src/Containers/cfd-password-modal.tsx
@@ -163,7 +163,9 @@ const ReviewMessageForMT5 = ({
     } else if (jurisdiction_selected_shortcode === JURISDICTION.LABUAN) {
         return <Localize i18n_default_text='We’re reviewing your documents. This should take about 1 to 3 days.' />;
     } else if (jurisdiction_selected_shortcode === JURISDICTION.MALTA_INVEST) {
-        return <Localize i18n_default_text='Enable trading with your first transfer.' />;
+        return (
+            <Localize i18n_default_text='To start trading, transfer funds from your Deriv account into this account.' />
+        );
     }
     return null;
 };

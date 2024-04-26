97c01c7f89bfad8a75234dc53f9bf75962bbed3a
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-04-26 10:19:38 +0400

    fix: remove email

--- packages/account/src/Sections/Profile/PersonalDetails/validation.ts
+++ packages/account/src/Sections/Profile/PersonalDetails/validation.ts
@@ -55,7 +55,7 @@ const getBaseSchema = () =>
                 localize('Only letters, space, hyphen, period, and apostrophe are allowed.')
             ),
         address_postcode: Yup.string()
-            .max(20, localize('Please enter a Postal/ZIP code under 20 chatacters.'))
+            .max(20, localize('Please enter a Postal/ZIP code under 20 characters.'))
             .matches(/^[A-Za-z0-9][A-Za-z0-9\s-]*$/, localize('Only letters, numbers, space, and hyphen are allowed.')),
     });
 
@@ -118,7 +118,6 @@ export const makeSettingsRequest = (
     const request = settings;
 
     if (request.residence) delete request.residence;
-    if (request.email) delete request.email;
     if (request.first_name) {
         request.first_name = request.first_name.trim();
     }

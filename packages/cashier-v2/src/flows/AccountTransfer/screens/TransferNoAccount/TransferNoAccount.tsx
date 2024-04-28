af7102f9fa08466a391d46cc1e313a3b8a76590d
Aum Bhatt <aum@deriv.com>
2024-04-28 16:49:01 +0400

    perf: added unit tests for TransferNoAccount

--- packages/cashier-v2/src/flows/AccountTransfer/screens/TransferNoAccount/TransferNoAccount.tsx
+++ packages/cashier-v2/src/flows/AccountTransfer/screens/TransferNoAccount/TransferNoAccount.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { THooks } from 'src/hooks/types';
 import { DerivLightCashierNoBalanceIcon } from '@deriv/quill-icons';
 import { ActionScreen, Button } from '@deriv-com/ui';
+import { THooks } from '../../../../hooks/types';
 
 type TTransferNotAccountProps = {
     accounts: THooks.TransferAccounts;
@@ -11,6 +11,7 @@ const TransferNoAccount: React.FC<React.PropsWithChildren<TTransferNotAccountPro
     if (accounts.length < 2)
         return (
             <ActionScreen
+                // TODO: Add onClick prop for opening account creation flow + unit tests
                 actionButtons={<Button size='lg'>Create Account</Button>}
                 description='Please create another Deriv, Deriv MT5, or Deriv X account.'
                 icon={<DerivLightCashierNoBalanceIcon height='128' width='128' />}

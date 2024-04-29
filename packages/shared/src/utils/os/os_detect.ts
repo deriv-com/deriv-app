124c252ef25caf67b35bbdf1ebcec232437b7db0
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-04-29 15:15:11 +0800

    refactor: comments

--- packages/shared/src/utils/os/os_detect.ts
+++ packages/shared/src/utils/os/os_detect.ts
@@ -42,9 +42,9 @@ export const isDesktopOs = () => {
 export const isMobileOs = () =>
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-export const isTabletOs = /ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(
-    navigator.userAgent.toLowerCase()
-);
+export const isTabletOs =
+    /ipad|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(navigator.userAgent.toLowerCase()) ||
+    (/android/i.test(navigator.userAgent) && !/mobile/i.test(navigator.userAgent));
 
 export const OSDetect = () => {
     // For testing purposes or more compatibility, if we set 'config.os'

6a1e7c661427a44d4b1d6a83dc0a75c4f536ebeb
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-04-30 12:20:04 +0800

    chore: sendEmail handler

--- packages/tradershub/src/features/cfd/modals/ButtonGroups/AddAccountButtonsGroup.tsx
+++ packages/tradershub/src/features/cfd/modals/ButtonGroups/AddAccountButtonsGroup.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { ButtonGroup } from '@/components';
 import { useQueryParams } from '@/hooks';
 import { useCFDContext } from '@/providers';
+import { platformPasswordResetRedirectLink } from '@/utils';
+import { useActiveTradingAccount, useSettings, useVerifyEmail } from '@deriv/api-v2';
 import { Button } from '@deriv-com/ui';
-import { PlatformDetails } from '../../constants';
+import { CFDPlatforms, PlatformDetails } from '../../constants';
 import DxtradeCreateAccountButton from '../DxtradePasswordModal/DxtradeCreateAccountButton';
 import MT5CreateAccountButton from '../MT5PasswordModal/MT5CreateAccountButton';
 
@@ -13,12 +15,31 @@ type TAddAccountButtonsGroupProps = {
 
 const AddAccountButtonsGroup = ({ password }: TAddAccountButtonsGroupProps) => {
     const { cfdState } = useCFDContext();
-    const { platform } = cfdState;
+    const { data } = useSettings();
+    const { mutate } = useVerifyEmail();
+    const { data: activeTrading } = useActiveTradingAccount();
+    const { platform = CFDPlatforms.MT5 } = cfdState;
     const { openModal } = useQueryParams();
+
+    const handleSendEmail = async () => {
+        if (data.email) {
+            await mutate({
+                type:
+                    platform === PlatformDetails.dxtrade.platform
+                        ? 'trading_platform_dxtrade_password_reset'
+                        : 'trading_platform_mt5_password_reset',
+                url_parameters: {
+                    redirect_to: platformPasswordResetRedirectLink(platform, activeTrading?.is_virtual),
+                },
+                verify_email: data.email,
+            });
+        }
+        openModal('SentEmailContentModal');
+    };
 
     return (
         <ButtonGroup className='justify-end w-full'>
-            <Button color='black' onClick={() => openModal('SentEmailContentModal')} variant='outlined'>
+            <Button color='black' onClick={handleSendEmail} variant='outlined'>
                 Forgot password?
             </Button>
             {platform === PlatformDetails.dxtrade.platform && (

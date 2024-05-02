717545e82984423c32ae703d27cf32de3e1c0595
yauheni-deriv <yauheni@regentmarkets.com>
2024-05-02 14:51:09 +0300

    chore: [UPM-982]/evgeniy/redirect to error page while clicking contact us link

---
+++ packages/components/src/components/open-livechat-link/index.ts
@@ -0,0 +1,4 @@
+import OpenLiveChatLink from './open-livechat-link';
+import './open-livechat-link.scss';
+
+export default OpenLiveChatLink;
---
+++ packages/components/src/components/open-livechat-link/open-livechat-link.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Localize } from '@deriv/translations';
+import Text from '../text';
+
+type TOpenLiveChatLink = {
+    text_size?: React.ComponentProps<typeof Text>['size'];
+};
+
+const OpenLiveChatLink = ({ children, text_size }: React.PropsWithChildren<TOpenLiveChatLink>) => {
+    return (
+        <button type='button' className='livechat__link' onClick={() => window.LC_API.open_chat_window()}>
+            <Text size={text_size || 'xs'} weight='bold' color='brand-red-coral'>
+                {children || <Localize i18n_default_text='live chat' />}
+            </Text>
+        </button>
+    );
+};
+
+export default OpenLiveChatLink;

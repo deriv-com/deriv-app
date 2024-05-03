c93ea2313f86d834b8ba22acf05f5053d73f1844
maryia-deriv <maryia@binary.com>
2024-05-03 17:20:15 +0300

    refactor: test for routes-config

--- packages/reports/src/Constants/__tests__/routes-config.spec.tsx
+++ packages/reports/src/Constants/__tests__/routes-config.spec.tsx
@@ -1,5 +1,5 @@
-import React, { ComponentProps } from 'react';
-import { Router } from 'react-router';
+import React from 'react';
+import { RouteComponentProps, Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 import { mockStore } from '@deriv/stores';
 import { routes } from '@deriv/shared';
@@ -8,64 +8,34 @@ import BinaryRoutes from '../../Components/Routes';
 import ReportsProviders from '../../reports-providers';
 import getRoutesConfig from '../routes-config';
 
-jest.mock('Modules/Page404', () => {
-    return {
-        __esModule: true,
-        default: () => <div>Error 404</div>,
-    };
-});
+jest.mock('Modules/Page404', () => ({
+    __esModule: true,
+    default: () => <div>Error 404</div>,
+}));
 
-jest.mock('../../Containers', () => {
-    return {
-        __esModule: true,
-        default: {
-            ...jest.requireActual('../../Containers').default,
-            OpenPositions: () => <div>Open positions</div>,
-            ProfitTable: () => <div>Trade table</div>,
-            Statement: () => <div>Statement</div>,
-        },
-    };
-});
+jest.mock('../../Containers', () => ({
+    __esModule: true,
+    default: {
+        ...jest.requireActual('../../Containers').default,
+        OpenPositions: () => <div>Open positions</div>,
+        ProfitTable: () => <div>Trade table</div>,
+        Statement: () => <div>Statement</div>,
+    },
+}));
 
 describe('Routes Config', () => {
-    const history = createMemoryHistory();
-
     // we need to render BinaryRoutes component that uses routes-config to test that lazy-loaded components are loaded
-    const MockBinaryRoutes = ({ is_logged_in, is_logging_in }: ComponentProps<typeof BinaryRoutes>) => {
-        return (
-            <ReportsProviders
-                store={mockStore({
-                    client: { is_logged_in, is_logging_in },
-                    ui: { is_reports_visible: true },
-                })}
-            >
-                <Router history={history}>
-                    <BinaryRoutes is_logged_in={is_logged_in} is_logging_in={is_logging_in} />
-                </Router>
-            </ReportsProviders>
-        );
-    };
+    const MockBinaryRoutes = ({ history }: Partial<RouteComponentProps>) => (
+        <ReportsProviders store={mockStore({ client: { is_logged_in: true }, ui: { is_reports_visible: true } })}>
+            <Router history={history}>
+                <BinaryRoutes is_logged_in />
+            </Router>
+        </ReportsProviders>
+    );
 
     it('should return default routes config', () => {
         const routesConfig = getRoutesConfig();
         expect(routesConfig).toHaveLength(2);
-    });
-    it('should not render Reports / Open positions when a user is not logged in', async () => {
-        history.push(routes.positions);
-        render(<MockBinaryRoutes is_logged_in={false} is_logging_in={false} />);
-        await waitFor(() => {
-            expect(screen.queryByText('Reports')).not.toBeInTheDocument();
-            expect(screen.queryByText('Open positions')).not.toBeInTheDocument();
-        });
-    });
-    it('should render Loading component while a user is logging in', async () => {
-        history.push(routes.positions);
-        render(<MockBinaryRoutes is_logged_in={false} is_logging_in />);
-        await waitFor(() => {
-            expect(screen.queryByText('Reports')).not.toBeInTheDocument();
-            expect(screen.queryByText('Open positions')).not.toBeInTheDocument();
-            expect(screen.getByTestId('dt_initial_loader')).toBeInTheDocument();
-        });
     });
     it('should return routes with Reports / Open positions route', async () => {
         const routesConfig = getRoutesConfig();
@@ -73,8 +43,9 @@ describe('Routes Config', () => {
         expect(routesConfig?.[0]?.getTitle?.()).toBe('Reports');
         expect(routesConfig?.[0]?.is_authenticated).toBe(true);
         expect(routesConfig?.[0].routes[0].path).toBe(routes.positions);
+        const history = createMemoryHistory();
         history.push(routes.positions);
-        render(<MockBinaryRoutes is_logged_in is_logging_in />);
+        render(<MockBinaryRoutes history={history} />);
         await waitFor(() => {
             expect(screen.getByText('Reports')).toBeInTheDocument();
             expect(screen.getByText('Open positions')).toBeInTheDocument();
@@ -84,8 +55,9 @@ describe('Routes Config', () => {
         const routesConfig = getRoutesConfig();
         expect(routesConfig?.[0].routes[1].path).toBe(routes.profit);
         expect(routesConfig?.[0].routes[1].getTitle()).toBe('Trade table');
+        const history = createMemoryHistory();
         history.push(routes.profit);
-        render(<MockBinaryRoutes is_logged_in is_logging_in />);
+        render(<MockBinaryRoutes history={history} />);
         await waitFor(() => {
             expect(screen.getByText('Reports')).toBeInTheDocument();
             expect(screen.getByText('Trade table')).toBeInTheDocument();
@@ -95,18 +67,20 @@ describe('Routes Config', () => {
         const routesConfig = getRoutesConfig();
         expect(routesConfig?.[0].routes[2].path).toBe(routes.statement);
         expect(routesConfig?.[0].routes[2].getTitle()).toBe('Statement');
+        const history = createMemoryHistory();
         history.push(routes.statement);
-        render(<MockBinaryRoutes is_logged_in is_logging_in />);
+        render(<MockBinaryRoutes history={history} />);
         await waitFor(() => {
             expect(screen.getByText('Reports')).toBeInTheDocument();
             expect(screen.getByText('Statement')).toBeInTheDocument();
         });
     });
-    it('should return routes with Page 404 and render it when the path does not exist', async () => {
+    it('should return routes with route for Page 404 which loads when the path does not exist', async () => {
         const routesConfig = getRoutesConfig();
         expect(routesConfig?.[1]?.getTitle?.()).toBe('Error 404');
+        const history = createMemoryHistory();
         history.push('/non-existent-path');
-        render(<MockBinaryRoutes is_logged_in is_logging_in={false} />);
+        render(<MockBinaryRoutes history={history} />);
         await waitFor(() => {
             expect(screen.getByText('Error 404')).toBeInTheDocument();
         });

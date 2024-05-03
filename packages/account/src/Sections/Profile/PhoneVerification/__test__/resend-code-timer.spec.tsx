867afaf3b7a83a8d6d93ca77593597aa686faf75
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-05-03 15:56:10 +0400

    chore: edit testcases

--- packages/account/src/Sections/Profile/PhoneVerification/__test__/resend-code-timer.spec.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/__test__/resend-code-timer.spec.tsx
@@ -4,12 +4,24 @@ import userEvent from '@testing-library/user-event';
 import ResendCodeTimer from '../resend-code-timer';
 
 describe('ConfirmPhoneNumber', () => {
-    it('should disable button when Resend code is clicked', () => {
-        render(<ResendCodeTimer />);
+    it('should disable button after its clicked', () => {
+        render(<ResendCodeTimer verification_location={'email'} />);
         const resend_button = screen.getByRole('button', { name: 'Resend code' });
 
         userEvent.click(resend_button);
 
         expect(resend_button).toBeDisabled();
+    });
+
+    it('should display Resend code if user requests for new code inside email verification section', () => {
+        render(<ResendCodeTimer verification_location={'email'} />);
+        const resend_button = screen.getByRole('button', { name: 'Resend code' });
+        expect(resend_button).toBeInTheDocument();
+    });
+
+    it('should display Didn’t get the code? if user requests for new code inside phone number verification section', () => {
+        render(<ResendCodeTimer verification_location={'phone'} />);
+        const resend_button = screen.getByRole('button', { name: 'Didn’t get the code?' });
+        expect(resend_button).toBeInTheDocument();
     });
 });

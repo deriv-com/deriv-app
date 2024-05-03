acb80bef4c0747982c462e6ef8dd8b2aec119664
sergei-deriv <sergei@deriv.com>
2024-05-04 01:12:10 +0300

    fix: remove exclamation mark

--- packages/appstore/src/modules/traders-hub-logged-out/traders-hub-logged-out.tsx
+++ packages/appstore/src/modules/traders-hub-logged-out/traders-hub-logged-out.tsx
@@ -19,7 +19,7 @@ const TradersHubLoggedOut = observer(() => {
     React.useEffect(() => {
         if (clients_country) {
             getLandingCompany(clients_country);
-            if (!isEuCountry(clients_country)) {
+            if (isEuCountry(clients_country)) {
                 setTogglePlatformType('cfd');
                 selectRegion('EU');
             } else {

f0940667ed4bf573dee176484abc7950a4574dd0
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-05-03 15:48:14 +0400

    fix: change text for email and phone, add TODOs

--- packages/account/src/Sections/Profile/PhoneVerification/resend-code-timer.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/resend-code-timer.tsx
@@ -2,25 +2,34 @@ import React, { useRef } from 'react';
 import { Button, CaptionText } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
 
-const ResendCodeTimer = () => {
+type TResendCodeTimer = {
+    verification_location: string;
+};
+const ResendCodeTimer = ({ verification_location }: TResendCodeTimer) => {
+    // TODO: Use dynamic value for setting initial value for counter once mockApi for timestamp is finalised
     const [timer, setTimer] = React.useState(60);
-    const [startTimer, setStartTimer] = React.useState(false);
-    const resendCodeText = useRef('Resend code');
+    const [start_timer, setStartTimer] = React.useState(false);
+    const resend_code_text = verification_location === 'email' ? 'Resend code' : 'Didn’t get the code?';
+    const request_code = useRef(resend_code_text);
 
     React.useEffect(() => {
         let countdown: NodeJS.Timeout;
-        if (startTimer && timer > 0) {
+        if (start_timer && timer > 0) {
             countdown = setInterval(() => {
                 setTimer(prevTime => prevTime - 1);
-                resendCodeText.current = `Resend code in ${timer - 1}s`;
+                // TODO: change seconds to mins based on interval calculated using timestamp from mock api
+                request_code.current =
+                    verification_location === 'email'
+                        ? `Resend code in ${timer - 1}s`
+                        : `Didn’t get the code?(${timer - 1})`;
             }, 1000);
         } else {
             setStartTimer(false);
-            resendCodeText.current = 'Resend code';
+            request_code.current = 'Resend code';
         }
 
         return () => clearInterval(countdown);
-    }, [timer, startTimer]);
+    }, [timer, start_timer]);
 
     const resendCode = () => {
         setTimer(60);
@@ -28,9 +37,9 @@ const ResendCodeTimer = () => {
     };
 
     return (
-        <Button variant='tertiary' onClick={resendCode} disabled={startTimer} color='black'>
+        <Button variant='tertiary' onClick={resendCode} disabled={start_timer} color='black'>
             <CaptionText bold underlined>
-                <Localize i18n_default_text='{{resendCode}}' values={{ resendCode: resendCodeText.current }} />
+                <Localize i18n_default_text='{{resendCode}}' values={{ resendCode: request_code.current }} />
             </CaptionText>
         </Button>
     );

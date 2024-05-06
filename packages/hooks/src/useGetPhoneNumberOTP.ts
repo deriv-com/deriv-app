cdd1f2fe018a9edcaae2cbfbd9f334766d6b3598
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-05-06 17:00:14 +0400

    chore: add comments

--- packages/hooks/src/useGetPhoneNumberOTP.ts
+++ packages/hooks/src/useGetPhoneNumberOTP.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@deriv/api';
 import { VERIFICATION_SERVICES } from '@deriv/shared';
 
+/** A hook for requesting OTP which is sent on whatsapp or sms platforms */
 const useGetPhoneNumberOTP = () => {
     const { data, mutate, ...rest } = useMutation('phone_number_challenge');
 

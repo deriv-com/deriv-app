baf0e532cf495c268f9fe1363ceebdd12889d2bb
heorhi-deriv <heorhi@binary.com>
2024-05-07 16:44:57 +0300

    test(wallets): :test_tube: refactor tests

--- packages/utils/src/__tests__/getAccountListWithAuthToken.spec.ts
+++ packages/utils/src/__tests__/getAccountListWithAuthToken.spec.ts
@@ -1,26 +1,56 @@
 import getAccountListWithAuthToken from '../getAccountListWithAuthToken';
 import { AuthorizeResponse } from '@deriv/api-types';
 
+type TAccount = NonNullable<NonNullable<AuthorizeResponse['authorize']>['account_list']>[number];
+
 describe('getAccountListWithAuthToken', () => {
-    let accounts: NonNullable<AuthorizeResponse['authorize']>['account_list'];
+    let accounts: TAccount[];
+
+    const usdAccount: TAccount = {
+        account_category: 'wallet',
+        account_type: 'doughflow',
+        broker: 'CRW',
+        created_at: 1714662434,
+        currency: 'USD',
+        currency_type: 'fiat',
+        is_disabled: 0,
+        is_virtual: 0,
+        landing_company_name: 'svg',
+        linked_to: [],
+    };
+
+    const btcAccount: TAccount = {
+        account_category: 'wallet',
+        account_type: 'crypto',
+        broker: 'CRW',
+        created_at: 1714662788,
+        currency: 'BTC',
+        currency_type: 'crypto',
+        is_disabled: 0,
+        is_virtual: 0,
+        landing_company_name: 'svg',
+        linked_to: [],
+    };
+
+    const demoAccount: TAccount = {
+        account_category: 'wallet',
+        account_type: 'virtual',
+        broker: 'VRW',
+        created_at: 1714662434,
+        currency: 'USD',
+        currency_type: 'fiat',
+        is_disabled: 0,
+        is_virtual: 1,
+        landing_company_name: 'virtual',
+        linked_to: [],
+    };
 
     beforeEach(() => {
-        localStorage.clear();
-
         localStorage.setItem(
             'client.accounts',
             JSON.stringify({
                 CRW1121: {
-                    account_category: 'wallet',
-                    account_type: 'doughflow',
-                    broker: 'CRW',
-                    created_at: 1714662434,
-                    currency: 'USD',
-                    currency_type: 'fiat',
-                    is_disabled: 0,
-                    is_virtual: 0,
-                    landing_company_shortcode: 'svg',
-                    linked_to: [],
+                    ...usdAccount,
                     token: 'xob1xob1xob1xob1xob1xob1xob1',
                     email: 'ge4@binary.com',
                     session_start: 1714715763,
@@ -31,32 +61,14 @@ describe('getAccountListWithAuthToken', () => {
                     residence: 'aq',
                 },
                 CRW1122: {
-                    account_category: 'wallet',
-                    account_type: 'crypto',
-                    broker: 'CRW',
-                    created_at: 1714662788,
-                    currency: 'BTC',
-                    currency_type: 'crypto',
-                    is_disabled: 0,
-                    is_virtual: 0,
-                    landing_company_shortcode: 'svg',
-                    linked_to: [],
+                    ...btcAccount,
                     token: 'xob1xob1xob1xob1xob1xob1xob1',
                     excluded_until: '',
                     landing_company_name: 'svg',
                     balance: 0,
                 },
                 VRW1069: {
-                    account_category: 'wallet',
-                    account_type: 'virtual',
-                    broker: 'VRW',
-                    created_at: 1714662434,
-                    currency: 'USD',
-                    currency_type: 'fiat',
-                    is_disabled: 0,
-                    is_virtual: 1,
-                    landing_company_shortcode: 'virtual',
-                    linked_to: [],
+                    ...demoAccount,
                     token: 'xob1xob1xob1xob1xob1xob1xob1',
                     excluded_until: '',
                     landing_company_name: 'virtual',
@@ -66,65 +78,39 @@ describe('getAccountListWithAuthToken', () => {
         );
 
         accounts = [
+            { ...usdAccount, loginid: 'CRW1121' },
             {
-                account_category: 'wallet',
-                account_type: 'doughflow',
-                broker: 'CRW',
-                created_at: 1714662434,
-                currency: 'USD',
-                currency_type: 'fiat',
-                is_disabled: 0,
-                is_virtual: 0,
-                landing_company_name: 'svg',
-                linked_to: [],
-                loginid: 'CRW1121',
-            },
-            {
-                account_category: 'wallet',
-                account_type: 'crypto',
-                broker: 'CRW',
-                created_at: 1714662788,
-                currency: 'BTC',
-                currency_type: 'crypto',
-                is_disabled: 0,
-                is_virtual: 0,
-                landing_company_name: 'svg',
-                linked_to: [],
+                ...btcAccount,
                 loginid: 'CRW1122',
             },
             {
-                account_category: 'wallet',
-                account_type: 'virtual',
-                broker: 'VRW',
-                created_at: 1714662434,
-                currency: 'USD',
-                currency_type: 'fiat',
-                is_disabled: 0,
-                is_virtual: 1,
-                landing_company_name: 'virtual',
-                linked_to: [],
+                ...demoAccount,
                 loginid: 'VRW1069',
             },
         ];
     });
 
+    afterEach(() => {
+        localStorage.clear();
+    });
+
     it('should return all accounts that have a token', () => {
         const accountsWithToken = getAccountListWithAuthToken(accounts);
 
-        expect(accountsWithToken?.length).toBe(accounts?.length);
+        expect(accountsWithToken).toHaveLength(Number(accounts?.length));
     });
 
     it('should return undefined if no accounts provided', () => {
         const accountsWithToken = getAccountListWithAuthToken();
 
-        expect(accountsWithToken).toBe(undefined);
+        expect(accountsWithToken).not.toBeDefined();
     });
 
     it('should return and empty array if it was provided as accounts', () => {
         const accountsWithToken = getAccountListWithAuthToken([]);
 
         expect(accountsWithToken).toBeInstanceOf(Array);
-        expect(accountsWithToken?.length).toBe(0);
+        expect(accountsWithToken).toHaveLength(0);
     });
 
     it('should return proper list of accounts with token when we are adding new account', () => {
@@ -144,7 +130,7 @@ describe('getAccountListWithAuthToken', () => {
 
         const accountsWithToken = getAccountListWithAuthToken(accounts);
 
-        expect(accounts?.length).toBe(4);
-        expect(accountsWithToken?.length).toBe(3);
+        expect(accounts).toHaveLength(4);
+        expect(accountsWithToken).toHaveLength(3);
     });
 });

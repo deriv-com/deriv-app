29b8a4c7363c533fd4a2a7174620b8c942621ece
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2024-05-07 17:27:20 +0300

    [WALL] george / WALL-3284 / Blank Page with Invalid token when accessing wallets in desktop (for wallets created using a different device/browser) (#14946)
    * feat(wallets): :fire: remove accounts without token from `account_list` and `authorize` responses
    
    * test(wallets): :test_tube: add test for getAccountListWithAuthToken util
    
    * style(wallets): :lipstick: fix tab padding
    
    * test(wallets): :test_tube: refactor tests

--- packages/api-v2/src/hooks/useAuthorize.ts
+++ packages/api-v2/src/hooks/useAuthorize.ts
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
 import { useAuthContext } from '../AuthProvider';
+import { getAccountListWithAuthToken } from '@deriv/utils';
 
 /** A custom hook that authorize the user with the given token. If no token is given,
  * it will use the current token from localStorage.
@@ -9,7 +10,7 @@ const useAuthorize = () => {
     const { data, switchAccount, isLoading, isSuccess, isFetching, isError, refetch, error, loginid } = ctx;
 
     const modifiedData = useMemo(() => {
-        return { ...data?.authorize };
+        return { ...data?.authorize, account_list: getAccountListWithAuthToken(data?.authorize?.account_list) };
     }, [data]);
 
     const value = useMemo(() => {

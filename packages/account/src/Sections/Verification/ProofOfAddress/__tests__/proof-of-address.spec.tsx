843b0ab32cba3a355e4e2bfb9817c80896793580
Likhith Kolayari <likhith@regentmarkets.com>
2024-05-07 11:35:12 +0400

    chore: added testcases for ProofOfAddress

---
+++ packages/account/src/Sections/Verification/ProofOfAddress/__tests__/proof-of-address.spec.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import ProofOfAddress from '../proof-of-address';
+import { StoreProvider, mockStore } from '@deriv/stores';
+import { useKycAuthStatus } from '../../../../hooks';
+import { BrowserRouter } from 'react-router-dom';
+
+type TKYCAuthStatusResponse = Partial<ReturnType<typeof useKycAuthStatus>>;
+
+const mock_kyc_auth_status_response: TKYCAuthStatusResponse = {
+    kyc_auth_status: {
+        address: { status: 'pending' },
+        identity: { status: 'none' },
+    },
+    isLoading: false,
+    isSuccess: false,
+    reFetchKycAuthStatus: jest.fn(),
+};
+
+jest.mock('../../../../hooks', () => ({
+    useKycAuthStatus: jest.fn(() => mock_kyc_auth_status_response),
+}));
+
+// jest.mock('@deriv/hooks', () => ({
+//     ...jest.requireActual('@deriv/hooks'),
+//     useFileUploader: jest.fn(() => ({
+//         upload: jest.fn(),
+//     })),
+// }));
+
+describe('ProofOfAddress', () => {
+    const store = mockStore({});
+
+    const renderComponent = (store_config = store) =>
+        render(
+            <BrowserRouter>
+                <StoreProvider store={store_config}>
+                    <ProofOfAddress />
+                </StoreProvider>
+            </BrowserRouter>
+        );
+
+    it('should render message on POA when client is on virtual account', () => {
+        const store_config = mockStore({
+            client: {
+                is_virtual: true,
+            },
+        });
+
+        renderComponent(store_config);
+        expect(screen.getByText('This feature is not available for demo accounts.')).toBeInTheDocument();
+    });
+
+    it('should render ProofofAddressContainer when client is on real account', () => {
+        renderComponent();
+
+        expect(screen.getByText(/Your proof of address was submitted successfully/i)).toBeInTheDocument();
+        expect(screen.getByText(/You must also submit a proof of identity/i)).toBeInTheDocument();
+    });
+});

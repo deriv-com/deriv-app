1735790eb6d5ceafe95f9ed36c9cc8cddf2ceb88
Aum Bhatt <aum@deriv.com>
2024-05-07 11:29:43 +0400

    feat: added balance subscription for wallet header balance

--- packages/wallets/src/components/WalletListCardBalance/WalletListCardBalance.tsx
+++ packages/wallets/src/components/WalletListCardBalance/WalletListCardBalance.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { useActiveWalletAccount, useBalance } from '@deriv/api-v2';
+import React, { useEffect } from 'react';
+import { useActiveWalletAccount, useBalanceSubscription } from '@deriv/api-v2';
+import { displayMoney } from '@deriv/api-v2/src/utils';
 import { WalletText } from '../Base';
 import './WalletListCardBalance.scss';
 
 const WalletListCardBalance = () => {
-    const { isLoading } = useBalance();
+    const { data: balanceData, isLoading, subscribe, unsubscribe } = useBalanceSubscription();
     const { data: activeWallet, isLoading: isActiveWalletLoading } = useActiveWalletAccount();
 
-    const balance = activeWallet?.display_balance;
+    useEffect(() => {
+        subscribe({ loginid: activeWallet?.loginid });
+        return () => unsubscribe();
+    }, [activeWallet?.loginid, subscribe, unsubscribe]);
 
     return (
         <div className='wallets-balance__container'>
@@ -18,7 +22,9 @@ const WalletListCardBalance = () => {
                 />
             ) : (
                 <WalletText align='right' size='xl' weight='bold'>
-                    {balance}
+                    {displayMoney?.(balanceData?.balance ?? 0, activeWallet?.currency ?? '', {
+                        fractional_digits: activeWallet?.currency_config?.fractional_digits,
+                    })}
                 </WalletText>
             )}
         </div>

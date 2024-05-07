f7b5b73e3efba39c0d34ae661f0b63626b94cf94
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-05-07 16:23:19 +0400

    chore: use states instead of useRef and update testcase

--- packages/account/src/Sections/Profile/PhoneVerification/__test__/resend-code-timer.spec.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/__test__/resend-code-timer.spec.tsx
@@ -5,23 +5,23 @@ import ResendCodeTimer from '../resend-code-timer';
 
 describe('ConfirmPhoneNumber', () => {
     it('should disable button after its clicked', () => {
-        render(<ResendCodeTimer resend_code_text={'Resend code'} />);
-        const resend_button = screen.getByRole('button', { name: 'Resend code' });
+        render(<ResendCodeTimer resend_code_text='Resend code' count_from={60} />);
+        const resend_button = screen.getByRole('button', { name: 'Resend code in 60s' });
 
         userEvent.click(resend_button);
 
         expect(resend_button).toBeDisabled();
     });
 
-    it('should display Resend code if user requests for new code inside email verification section', () => {
-        render(<ResendCodeTimer resend_code_text={'Resend code'} />);
-        const resend_button = screen.getByRole('button', { name: 'Resend code' });
+    it('should display correct title if value of resend_code_text is Resend code', () => {
+        render(<ResendCodeTimer resend_code_text='Resend code' count_from={60} />);
+        const resend_button = screen.getByRole('button', { name: 'Resend code in 60s' });
         expect(resend_button).toBeInTheDocument();
     });
 
-    it('should display Didn’t get the code? if user requests for new code inside phone number verification section', () => {
-        render(<ResendCodeTimer resend_code_text={'Didn’t get the code?'} />);
-        const resend_button = screen.getByRole('button', { name: 'Didn’t get the code?' });
+    it('should display correct title if value of resend_code_text is Didn’t get the code?', () => {
+        render(<ResendCodeTimer resend_code_text='Didn’t get the code?' count_from={60} />);
+        const resend_button = screen.getByRole('button', { name: 'Didn’t get the code?(60s)' });
         expect(resend_button).toBeInTheDocument();
     });
 });
--- packages/account/src/Sections/Profile/PhoneVerification/resend-code-timer.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/resend-code-timer.tsx
@@ -4,41 +4,46 @@ import { Localize } from '@deriv/translations';
 
 type TResendCodeTimer = {
     resend_code_text: string;
+    count_from: number;
 };
-const ResendCodeTimer = ({ resend_code_text }: TResendCodeTimer) => {
+const ResendCodeTimer = ({ count_from = 60, resend_code_text }: TResendCodeTimer) => {
     // TODO: Use dynamic value for setting initial value for counter once mockApi for timestamp is finalised
-    const [timer, setTimer] = React.useState(60);
-    const [start_timer, setStartTimer] = React.useState(false);
-    const request_code = useRef(resend_code_text);
+    const [timer, setTimer] = React.useState(count_from);
+    const [start_timer, setStartTimer] = React.useState(true);
+    // TODO: change seconds to mins based on interval calculated using timestamp from mock api
+    const initial_timer_title =
+        resend_code_text === 'Resend code' ? `Resend code in ${timer}s` : `Didn’t get the code?(${timer}s)`;
+    const [timer_title, setTimerTitle] = React.useState(initial_timer_title);
+
+    const setTitle = (timer: number, text: string) => {
+        const title = text === 'Resend code' ? `Resend code in ${timer}s` : `Didn’t get the code?(${timer}s)`;
+        setTimerTitle(title);
+    };
 
     React.useEffect(() => {
         let countdown: ReturnType<typeof setInterval>;
         if (start_timer && timer > 0) {
             countdown = setInterval(() => {
                 setTimer(prevTime => prevTime - 1);
-                // TODO: change seconds to mins based on interval calculated using timestamp from mock api
-                request_code.current =
-                    resend_code_text === 'Resend code'
-                        ? `Resend code in ${timer - 1}s`
-                        : `Didn’t get the code?(${timer - 1}s)`;
+                setTitle(timer, resend_code_text);
             }, 1000);
         } else {
             setStartTimer(false);
-            request_code.current = resend_code_text;
+            setTimerTitle(resend_code_text);
         }
 
         return () => clearInterval(countdown);
     }, [timer, start_timer]);
 
     const resendCode = () => {
-        setTimer(60);
+        setTimer(count_from);
         setStartTimer(true);
     };
 
     return (
         <Button variant='tertiary' onClick={resendCode} disabled={start_timer} color='black'>
             <CaptionText bold underlined>
-                <Localize i18n_default_text='{{resendCode}}' values={{ resendCode: request_code.current }} />
+                <Localize i18n_default_text='{{resendCode}}' values={{ resendCode: timer_title }} />
             </CaptionText>
         </Button>
     );

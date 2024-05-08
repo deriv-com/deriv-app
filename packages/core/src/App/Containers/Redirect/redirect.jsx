e2e19bfd140880d1dc43bb8c425c2aebe5a29af2
hasan-deriv <abuhasan@deriv.com>
2024-05-08 14:48:00 +0800

    fix: password modal re-trigger

--- packages/core/src/App/Containers/Redirect/redirect.jsx
+++ packages/core/src/App/Containers/Redirect/redirect.jsx
@@ -139,7 +139,10 @@ const Redirect = observer(() => {
                 }
             }
 
-            setResetTradingPasswordModalOpen(true);
+            // The if statement is needed because the modal from core is triggered by the stores.
+            // Whereas the one in wallet is triggered by the hooks.
+            // This is to prevent the modal from being triggered twice.
+            if (!routes.wallets) setResetTradingPasswordModalOpen(true);
             break;
         }
         case 'payment_withdraw': {

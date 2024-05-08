df2a7c007fb2dd8ecbf577956fe9852932182379
aizad-deriv <aizad@deriv.com>
2024-05-08 11:17:54 +0800

    chore: update password scoring system according to production and expected outcomes

--- packages/wallets/src/utils/password-validation.ts
+++ packages/wallets/src/utils/password-validation.ts
@@ -30,33 +30,17 @@ const isPasswordValid = (password: string) => {
     return passwordRegex.isPasswordValid.test(password) && passwordRegex.isLengthValid.test(password);
 };
 
-const isPasswordModerate = (password: string) => {
+export const calculateScoreCFD = (password: string) => {
+    let score = 0;
     const hasMoreThanOneSymbol = (password.match(/\W/g) ?? []).length > 1;
-    return (
-        isPasswordValid(password) &&
-        hasMoreThanOneSymbol &&
-        password.length >= passwordValues.minLength &&
-        password.length < passwordValues.longPassword &&
-        passwordRegex.isLengthValid
-    );
-};
-
-const isPasswordStrong = (password: string) => {
-    const hasMoreThanOneSymbol = (password.match(/\W/g) ?? []).length > 1;
-    return (
-        isPasswordValid(password) &&
-        hasMoreThanOneSymbol &&
-        password.length >= passwordValues.longPassword &&
-        passwordRegex.isLengthValid
-    );
-};
-
-export const calculateScore = (password: string) => {
-    if (password.length === 0) return 0;
-    if (!isPasswordValid(password)) return 1;
-    if (!isPasswordStrong(password) && isPasswordValid(password) && !isPasswordModerate(password)) return 2;
-    if (!isPasswordStrong(password) && isPasswordValid(password) && isPasswordModerate(password)) return 3;
-    if (isPasswordStrong(password)) return 4;
+    if (password.length === 0) return score;
+    if (!isPasswordValid(password)) score += 1;
+    if (isPasswordValid(password)) score += 1;
+    if (isPasswordValid(password) && password.length > passwordValues.minLength) score += 1;
+    if (isPasswordValid(password) && password.length > passwordValues.longPassword) score += 1;
+    if ((isPasswordValid(password) && password.length > passwordValues.maxLengthMT5) || hasMoreThanOneSymbol)
+        score += 1;
+    return score;
 };
 
 // MT5 Password Validation

a97e03858aec74c06c052627fb2c8b212f3e52fc
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2024-05-09 04:50:34 +0300

    Maryia/WEBREL-2578/test: coverage for routes & routes-config in Reports (#14973)
    * test: routes.tsx in reports
    
    * test: routes-config
    
    * refactor: test for routes-config
    
    * fix: remove extra async expect
    
    * refactor: packages/reports/src/Constants/__tests__/routes-config.spec.tsx

--- packages/reports/src/Containers/routes.tsx
+++ packages/reports/src/Containers/routes.tsx
@@ -1,11 +1,11 @@
 import { observer, useStore } from '@deriv/stores';
-import { withRouter } from 'react-router';
+import { RouteComponentProps, withRouter } from 'react-router';
 import React from 'react';
 import BinaryRoutes from 'Components/Routes';
 import ErrorComponent from 'Components/Errors';
 import { TRoutes } from 'Types';
 
-const Routes = observer(({ passthrough }: TRoutes) => {
+const Routes = observer(({ passthrough }: TRoutes & RouteComponentProps) => {
     const { client, common } = useStore();
     const { is_logged_in, is_logging_in } = client;
     const { error, has_error } = common;

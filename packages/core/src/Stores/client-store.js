e06512f6dee68ec33cc2ae5f3575e279cd387843
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2024-05-13 06:39:28 +0300

    revert: remove should_show_eu_content SmartCharts prop + stop tracking MARKET_INFO_REDIRECT (#14971)

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -298,7 +298,6 @@ export default class ClientStore extends BaseStore {
             has_wallet: computed,
             should_restrict_bvi_account_creation: computed,
             should_restrict_vanuatu_account_creation: computed,
-            should_show_eu_content: computed,
             should_show_eu_error: computed,
             is_virtual: computed,
             is_eu: computed,
@@ -859,11 +858,6 @@ export default class ClientStore extends BaseStore {
         return !!this.mt5_login_list.filter(
             item => item?.landing_company_short === 'vanuatu' && item?.status === 'poa_failed'
         ).length;
-    }
-
-    get should_show_eu_content() {
-        const is_current_mf = this.landing_company_shortcode === 'maltainvest';
-        return (!this.is_logged_in && this.is_eu_country) || this.is_eu || is_current_mf;
     }
 
     get should_show_eu_error() {
--- packages/trader/src/Modules/Trading/Containers/trade-chart.tsx
+++ packages/trader/src/Modules/Trading/Containers/trade-chart.tsx
@@ -20,7 +20,7 @@ type TTradeChartProps = {
 
 const TradeChart = observer((props: TTradeChartProps) => {
     const { has_barrier, is_accumulator, topWidgets } = props;
-    const { client, ui, common, contract_trade, portfolio } = useStore();
+    const { ui, common, contract_trade, portfolio } = useStore();
     const {
         accumulator_barriers_data,
         accumulator_contract_barriers_data,
@@ -36,7 +36,6 @@ const TradeChart = observer((props: TTradeChartProps) => {
     const { is_chart_countdown_visible, is_chart_layout_default, is_dark_mode_on, is_mobile, is_positions_drawer_on } =
         ui;
     const { current_language, is_socket_opened } = common;
-    const { should_show_eu_content } = client;
     const {
         active_symbols,
         barriers_flattened: extra_barriers,
@@ -137,7 +136,6 @@ const TradeChart = observer((props: TTradeChartProps) => {
             requestForgetStream={wsForgetStream}
             requestSubscribe={wsSubscribe}
             settings={settings}
-            should_show_eu_content={should_show_eu_content}
             allowTickChartTypeOnly={show_digits_stats || is_accumulator}
             stateChangeListener={chartStateChange}
             symbol={symbol}

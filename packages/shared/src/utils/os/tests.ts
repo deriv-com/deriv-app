89928a9b2e26988962a0116eb7d343f309d786f2
kate-deriv <kate@regentmarkets.com>
2024-05-15 17:17:33 +0300

    refactor: device detection

--- packages/shared/src/utils/os/tests.ts
+++ packages/shared/src/utils/os/tests.ts
@@ -1,80 +1,29 @@
-const device: Record<string, () => boolean> = {};
-
-// The client user agent string.
-// Lowercase, so we can use the more efficient indexOf(), instead of Regex
 const userAgent = window.navigator.userAgent.toLowerCase();
 
-// Main functions
-
-device.ios = function () {
-    return device.iphone() || device.ipod() || device.ipad();
-};
-
-device.iphone = function () {
-    return !device.windows() && find('iphone');
-};
-
-device.ipod = function () {
-    return find('ipod');
-};
-
-device.ipad = function () {
-    const iPadOS13Up = navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1;
-    return find('ipad') || iPadOS13Up;
-};
-
-device.android = function () {
-    return !device.windows() && find('android');
-};
-
-device.androidTablet = function () {
-    return device.android() && !find('mobile');
-};
-
-device.blackberry = function () {
-    return find('blackberry') || find('bb10');
-};
-
-device.blackberryTablet = function () {
-    return device.blackberry() && find('tablet');
-};
-
-device.windows = function () {
-    return find('windows');
-};
-
-device.windowsPhone = function () {
-    return device.windows() && find('phone');
-};
-
-device.windowsTablet = function () {
-    return device.windows() && find('touch') && !device.windowsPhone();
-};
-
-device.fxos = function () {
-    return (find('(mobile') || find('(tablet')) && find(' rv:');
-};
-
-device.fxosTablet = function () {
-    return device.fxos() && find('tablet');
-};
-
-device.tablet = function () {
-    return (
+const device = {
+    ipad: () => {
+        const iPadOS13Up = navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1;
+        return find('ipad') || iPadOS13Up;
+    },
+    android: () => !device.windows() && find('android'),
+    androidTablet: () => device.android() && !find('mobile'),
+    blackberry: () => find('blackberry') || find('bb10'),
+    blackberryTablet: () => device.blackberry() && find('tablet'),
+    windows: () => find('windows'),
+    windowsPhone: () => device.windows() && find('phone'),
+    windowsTablet: () => device.windows() && find('touch') && !device.windowsPhone(),
+    fxos: () => (find('(mobile') || find('(tablet')) && find(' rv:'),
+    fxosTablet: () => device.fxos() && find('tablet'),
+    tablet: () =>
         device.ipad() ||
         device.androidTablet() ||
         device.blackberryTablet() ||
         device.windowsTablet() ||
-        device.fxosTablet()
-    );
+        device.fxosTablet(),
 };
 
-function includes(haystack: string, needle: string) {
-    return haystack.indexOf(needle) !== -1;
-}
+const includes = (haystack: string, needle: string) => haystack.indexOf(needle) !== -1;
 
-function find(needle: string) {
-    return includes(userAgent, needle);
-}
+const find = (needle: string) => includes(userAgent, needle);
 
 export default device;

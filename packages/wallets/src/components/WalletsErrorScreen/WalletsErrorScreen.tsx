0834d72494a0b077151e56e86d1ff634352a0d6b
lubega-deriv <142860499+lubega-deriv@users.noreply.github.com>
2024-05-16 23:04:40 +0800

    chore: transfer api error (#15203)

--- packages/wallets/src/components/WalletsErrorScreen/WalletsErrorScreen.tsx
+++ packages/wallets/src/components/WalletsErrorScreen/WalletsErrorScreen.tsx
@@ -9,14 +9,16 @@ type TProps = {
     buttonVariant?: ComponentProps<typeof WalletButton>['variant'];
     message?: string;
     onClick?: () => void;
+    showIcon?: boolean;
     title?: string;
 };
 
 const WalletsErrorScreen: React.FC<TProps> = ({
-    buttonText = 'Try again',
+    buttonText,
     buttonVariant = 'ghost',
-    message = 'Sorry an error occurred. Please try accessing our cashier page again.',
+    message = 'Sorry an error occurred. Please try accessing our cashier again.',
     onClick,
+    showIcon = true,
     title = 'Oops, something went wrong!',
 }) => {
     const { isMobile } = useDevice();
@@ -28,12 +30,14 @@ const WalletsErrorScreen: React.FC<TProps> = ({
     return (
         <WalletsActionScreen
             description={message}
-            icon={<DerivLightErrorIconIcon {...imageSize} data-testid='dt_error_icon' />}
-            renderButtons={() => (
-                <WalletButton onClick={onClick} size='lg' variant={buttonVariant}>
-                    {buttonText}
-                </WalletButton>
-            )}
+            icon={showIcon && <DerivLightErrorIconIcon {...imageSize} data-testid='dt_error_icon' />}
+            renderButtons={() =>
+                buttonText ? (
+                    <WalletButton onClick={onClick} size='lg' variant={buttonVariant}>
+                        {buttonText}
+                    </WalletButton>
+                ) : null
+            }
             title={title}
         />
     );

15dafbce1a8ab0299498cc59506d4981d60d4a6e
Hirad <hirad@regentmarkets.com>
2024-05-16 18:11:29 +0800

    fix: added mt5 standard icon to the project

--- packages/components/src/components/icon/icons.js
+++ packages/components/src/components/icon/icons.js
@@ -762,6 +762,7 @@ import './mt5/ic-mt5-one-password.svg';
 import './mt5/ic-mt5-open-markets.svg';
 import './mt5/ic-mt5-password-updated.svg';
 import './mt5/ic-mt5-responsive.svg';
+import './mt5/ic-mt5-standard.svg';
 import './mt5/ic-mt5-support.svg';
 import './mt5/ic-mt5-svg-derived.svg';
 import './mt5/ic-mt5-svg-financial.svg';

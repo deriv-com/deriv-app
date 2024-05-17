130af8f3ea1fc734f6cd1baac7ea463c5bf88731
utkarsha-deriv <utkarsha@regentmarkets.com>
2024-05-17 10:09:23 +0400

    style: initial style changes

--- packages/account/src/Sections/Profile/LanguageSettings/language-settings.tsx
+++ packages/account/src/Sections/Profile/LanguageSettings/language-settings.tsx
@@ -6,14 +6,16 @@ import { routes } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import FormSubHeader from 'Components/form-sub-header';
 import LanguageRadioButton from 'Components/language-settings';
+import { useDevice } from '@deriv-com/ui';
 
 const LanguageSettings = observer(() => {
     const { common, ui } = useStore();
     const { is_mobile } = ui;
     const { changeSelectedLanguage, current_language } = common;
     const { has_wallet } = useStoreWalletAccountsList();
+    const { isDesktop } = useDevice();
 
-    if (is_mobile || has_wallet) {
+    if (!isDesktop || has_wallet) {
         return <Redirect to={routes.traders_hub} />;
     }
 

86a37780753265727e6df9a6ae87b467265531af
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-20 19:43:27 +0800

    feat: make the requests faster for GB

--- packages/hooks/src/useGrowthbookFeatureFlag.ts
+++ packages/hooks/src/useGrowthbookFeatureFlag.ts
@@ -11,11 +11,12 @@ const useGrowthbookFeatureFlag = ({ featureFlag }: UseGrowthbookFeatureFlagArgs)
     const { data } = useRemoteConfig();
 
     useEffect(() => {
+        let analytics_interval: NodeJS.Timeout;
         if (data.marketing_growthbook) {
             let timeout = 0;
-            const analytics_interval = setInterval(() => {
-                // Check if the analytics instance is available for ten seconds before setting the feature flag value
-                if (timeout > 10) {
+            analytics_interval = setInterval(() => {
+                // Check if the analytics instance is available for 10 seconds before setting the feature flag value
+                if (timeout > 20) {
                     // If the analytics instance is not available after 10 seconds, clear the interval
                     clearInterval(analytics_interval);
                     return;
@@ -33,7 +34,7 @@ const useGrowthbookFeatureFlag = ({ featureFlag }: UseGrowthbookFeatureFlagArgs)
                     });
                     clearInterval(analytics_interval);
                 }
-            }, 1000);
+            }, 500);
         }
         return () => {
             clearInterval(analytics_interval);

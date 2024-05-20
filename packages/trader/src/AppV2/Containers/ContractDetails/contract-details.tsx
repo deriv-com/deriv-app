5c56436a2df2719cbd5b3876a022744476f2818f
Henry Hein <henry@regentmarkets.com>
2024-05-20 15:52:47 +0800

    fix: earn payout bottom modal

--- packages/trader/src/AppV2/Containers/ContractDetails/contract-details.tsx
+++ packages/trader/src/AppV2/Containers/ContractDetails/contract-details.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Text } from '@deriv-com/quill-ui';
+import InfoSheet from 'AppV2/Components/InfoModal';
+import { localize } from '@deriv/translations';
+import { LabelPairedCircleInfoSmRegularIcon } from '@deriv/quill-icons';
 
 const ContractDetails = () => {
     return (
@@ -7,12 +10,23 @@ const ContractDetails = () => {
             <div className='placeholder'>
                 <Text size='sm'>Contract Details</Text>
             </div>
-            <div className='placeholder'>
-                <Text size='sm'>How do I earn a payout?</Text>
-            </div>
+            <InfoSheet
+                header_content={localize('How do I earn a payout?')}
+                body_content={localize(
+                    'After the entry spot tick, your stake will grow continuously by 1% for every tick that the spot price remains within the ± 0.06444% from the previous spot price.'
+                )}
+            />
             <div className='placeholder'>
                 <Text size='sm'>Contract card</Text>
             </div>
+            Take Profit
+            <InfoSheet
+                icon={<LabelPairedCircleInfoSmRegularIcon />}
+                header_content={localize('Take profit')}
+                body_content={localize(
+                    'When your profit reaches or exceeds the set amount, your trade will be closed automatically.'
+                )}
+            />
             <div className='placeholder'>
                 <Text size='sm'>Chart placeholder</Text>
             </div>

7dca783bbe0b5fe76944675bc43a7ec5206e272d
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-20 19:29:05 +0800

    feat: make sure that GB instance is available and then return the requested value

--- packages/hooks/src/useGrowthbookFeatureFlag.ts
+++ packages/hooks/src/useGrowthbookFeatureFlag.ts
@@ -1,28 +1,44 @@
 import { useState, useEffect } from 'react';
 import { Analytics } from '@deriv-com/analytics';
+import { useRemoteConfig } from '@deriv/api';
 
-interface UseGrowthbookFeatureFlagArgs<T> {
+interface UseGrowthbookFeatureFlagArgs {
     featureFlag: string;
-    defaultValue: T;
 }
 
-const useGrowthbookFeatureFlag = <T extends string | boolean>({
-    featureFlag,
-    defaultValue,
-}: UseGrowthbookFeatureFlagArgs<T>) => {
-    const [featureFlagValue, setFeatureFlagValue] = useState<T>(defaultValue);
+const useGrowthbookFeatureFlag = ({ featureFlag }: UseGrowthbookFeatureFlagArgs) => {
+    const [featureFlagValue, setFeatureFlagValue] = useState(Analytics?.getFeatureValue(featureFlag));
+    const { data } = useRemoteConfig();
 
     useEffect(() => {
-        const value = (Analytics?.getFeatureValue(featureFlag, defaultValue) || defaultValue) as T;
-        setFeatureFlagValue(value);
-
-        // Set the renderer for GrowthBook to update the value when the feature flag changes
-        Analytics.getInstances()?.ab?.GrowthBook?.setRenderer(() => {
-            const value = (Analytics?.getFeatureValue(featureFlag, defaultValue) || defaultValue) as T;
-
-            setFeatureFlagValue(value);
-        });
-    }, [featureFlag, defaultValue]);
+        if (data.marketing_growthbook) {
+            let timeout = 0;
+            const analytics_interval = setInterval(() => {
+                // Check if the analytics instance is available for ten seconds before setting the feature flag value
+                if (timeout > 10) {
+                    // If the analytics instance is not available after 10 seconds, clear the interval
+                    clearInterval(analytics_interval);
+                    return;
+                }
+                timeout += 1;
+                if (Analytics.getInstances().ab) {
+                    const setFeatureValue = () => {
+                        const value = Analytics?.getFeatureValue(featureFlag);
+                        setFeatureFlagValue(value);
+                    };
+                    setFeatureValue();
+                    Analytics.getInstances()?.ab?.GrowthBook?.setRenderer(() => {
+                        // this will be called whenever the feature flag value changes and acts as a event listener
+                        setFeatureValue();
+                    });
+                    clearInterval(analytics_interval);
+                }
+            }, 1000);
+        }
+        return () => {
+            clearInterval(analytics_interval);
+        };
+    }, []);
 
     return featureFlagValue;
 };

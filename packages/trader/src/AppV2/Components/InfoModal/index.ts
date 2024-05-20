52ba11e9f3bf265dd83df387b184a0c05c14ae23
Henry Hein <henry@regentmarkets.com>
2024-05-20 16:12:27 +0800

    fix: make it single responsibility component

--- packages/trader/src/AppV2/Components/InfoModal/index.ts
+++
@@ -1,4 +0,0 @@
-import InfoModal from './info-modal';
-import './info-modal.scss';
-
-export default InfoModal;
---
+++ packages/trader/src/AppV2/Components/PayoutInfoModal/payout-info-modal.tsx
@@ -0,0 +1,33 @@
+import { CaptionText, Modal } from '@deriv-com/quill-ui';
+import { Localize } from '@deriv/translations';
+import React from 'react';
+
+const PayoutInfoModal = () => {
+    const [isVisible, setIsVisible] = React.useState(false);
+    return (
+        <>
+            <button onClick={() => setIsVisible(!isVisible)}>
+                <CaptionText size='sm' bold underlined className='payout-info-text'>
+                    <Localize i18n_default_text='How do I earn a payout?' />
+                </CaptionText>
+            </button>
+            <Modal
+                isOpened={isVisible}
+                toggleModal={setIsVisible}
+                primaryButtonLabel={<Localize i18n_default_text='Got it' />}
+                shouldCloseOnPrimaryButtonClick
+                isMobile
+                showHandleBar
+            >
+                <Modal.Header title={<Localize i18n_default_text='How do I earn a payout?' />} />
+                <Modal.Body>
+                    {
+                        <Localize i18n_default_text='After the entry spot tick, your stake will grow continuously by 1% for every tick that the spot price remains within the ± 0.06444% from the previous spot price.' />
+                    }
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+};
+
+export default PayoutInfoModal;

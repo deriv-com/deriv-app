2b4ecaba379fecae70ef4c0fe5a8788f4c835692
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2024-05-20 14:45:22 +0300

    fix(wallets): :ambulance: fix deposit iframe loading (#15252)

--- packages/wallets/src/features/cashier/modules/DepositFiat/__tests__/DepositFiat.spec.tsx
+++ packages/wallets/src/features/cashier/modules/DepositFiat/__tests__/DepositFiat.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useCashierFiatAddress } from '@deriv/api-v2';
 import { fireEvent, render, screen } from '@testing-library/react';
 import DepositFiat from '../DepositFiat';
 
@@ -13,21 +12,13 @@ describe('DepositFiat', () => {
     });
 
     it('should render loader initially', () => {
-        (useCashierFiatAddress as jest.Mock).mockReturnValueOnce({
-            data: null,
-        });
-
         render(<DepositFiat />);
         expect(screen.getByTestId('dt_wallets_loader')).toBeInTheDocument();
         expect(screen.queryByTestId('dt_deposit-fiat-iframe')).not.toBeInTheDocument();
     });
 
     it('should display iframe correctly after onLoad event', () => {
-        (useCashierFiatAddress as jest.Mock).mockReturnValue({
-            data: 'https://iframe_url',
-        });
-
-        render(<DepositFiat />);
+        render(<DepositFiat iframeUrl='https://iframe_url' />);
 
         const iframe = screen.getByTestId('dt_deposit-fiat-iframe');
         expect(iframe).toHaveAttribute('src', 'https://iframe_url');

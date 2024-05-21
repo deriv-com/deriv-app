cca47a51b015593f6176615a4d86c8f3d1353ccb
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-21 18:54:42 +0800

    test: update the test cases to mock the remote conf hook

--- packages/hooks/src/__tests__/useGrowthbookFeatureFlag.spec.tsx
+++ packages/hooks/src/__tests__/useGrowthbookFeatureFlag.spec.tsx
@@ -1,30 +1,46 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useGrowthbookFeatureFlag from '../useGrowthbookFeatureFlag';
 import { Analytics } from '@deriv-com/analytics';
+import { useRemoteConfig } from '@deriv/api';
+
+jest.mock('@deriv/api');
+jest.mock('@tanstack/react-query');
+jest.mock('@deriv-com/analytics');
 
 describe('useGrowthbookFeatureFlag', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    const mockData = { marketing_growthbook: true };
+
     test('Should call getFeatureValue from the package', async () => {
+        useRemoteConfig.mockImplementation(() => ({
+            data: mockData,
+        }));
         const { result } = renderHook(() =>
             useGrowthbookFeatureFlag({
                 defaultValue: false,
                 featureFlag: 'dummy-feature-flag',
             })
         );
+        const [featureFlagValue] = result.current;
 
-        expect(result.current).toBe(false);
+        expect(featureFlagValue).toBe(false);
         expect(Analytics.getFeatureValue).toHaveBeenCalled();
         expect(Analytics.getFeatureValue).toHaveBeenCalledWith('dummy-feature-flag', false);
     });
 
     test('The default value for the feature flag must be sent correctly to the package', async () => {
-        const { result } = renderHook(() =>
+        useRemoteConfig.mockImplementation(() => ({
+            data: mockData,
+        }));
+        renderHook(() =>
             useGrowthbookFeatureFlag({
                 defaultValue: true,
                 featureFlag: 'dummy-feature-flag-1',
             })
         );
 
-        expect(result.current).toBe(true);
         expect(Analytics.getFeatureValue).toHaveBeenCalled();
         expect(Analytics.getFeatureValue).toHaveBeenCalledWith('dummy-feature-flag-1', true);
     });

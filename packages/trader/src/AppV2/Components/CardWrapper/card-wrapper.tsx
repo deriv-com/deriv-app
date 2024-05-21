7efac63f5a0101874c073d9345df4a46c79e927e
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-05-21 16:54:04 +0800

    chore: card wrapper

---
+++ packages/trader/src/AppV2/Components/CardWrapper/card-wrapper.tsx
@@ -0,0 +1,17 @@
+import { Text } from '@deriv-com/quill-ui';
+import React from 'react';
+
+const CardWrapper = ({ title = 'Entry & exit details', children }: { title: string; children: React.ReactNode }) => {
+    return (
+        <div className='card-wrapper'>
+            {title !== '' && (
+                <Text size='sm' bold className='title'>
+                    {title}
+                </Text>
+            )}
+            {children}
+        </div>
+    );
+};
+
+export default CardWrapper;

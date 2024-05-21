2674dedd30f87d9e3afafabc37d21f0774b405d4
hasan-deriv <abuhasan@deriv.com>
2024-05-21 13:00:17 +0800

    fix: add test cases and comment to calculateTotalBalance utility

--- packages/wallets/src/utils/ctrader.ts
+++ packages/wallets/src/utils/ctrader.ts
@@ -1,8 +1,28 @@
-type CTraderAccount = {
+/**
+ * Calculates the total balance from a list of accounts.
+ *
+ * @template T - A type extending the CTraderAccount type.
+ * @param {T[]} accountsList - An array of accounts, each potentially containing a `display_balance` property.
+ * @returns {string} The total balance of all accounts in the list, formatted as a string with two decimal places.
+ */
+export type CTraderAccount = {
     // eslint-disable-next-line camelcase
     display_balance?: number | string | null;
 };
 
 export function calculateTotalBalance<T extends CTraderAccount>(accountsList: T[]): string {
-    return accountsList.reduce((acc, cur) => acc + +(cur?.display_balance || 0), 0).toFixed(2);
+    return accountsList
+        .reduce((acc, cur) => {
+            let balance = 0;
+            if (typeof cur?.display_balance === 'number') {
+                balance = cur.display_balance;
+            } else if (typeof cur?.display_balance === 'string') {
+                const parsedBalance = parseFloat(cur.display_balance);
+                if (!isNaN(parsedBalance)) {
+                    balance = parsedBalance;
+                }
+            }
+            return acc + balance;
+        }, 0)
+        .toFixed(2);
 }

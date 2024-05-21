226c89ed3dd6ed81b29d4a8ffd747880ed04b151
Tay Sui Sin <suisin@regentmarkets.com>
2024-05-21 18:09:01 +0800

    chore: update language-radio-button size for account section

--- packages/account/src/Components/language-settings/language-radio-button.tsx
+++ packages/account/src/Components/language-settings/language-radio-button.tsx
@@ -33,7 +33,7 @@ const LanguageRadioButton = ({ is_current_language, id, language_code, name, onC
             <label htmlFor={id} className='settings-language__language--center-label'>
                 <div>
                     {TranslationFlag[language_code] ? (
-                        TranslationFlag[language_code]()
+                        TranslationFlag[language_code](36)
                     ) : (
                         <Icon icon={`IcFlag${id}`} className='settings-language__language-flag' />
                     )}

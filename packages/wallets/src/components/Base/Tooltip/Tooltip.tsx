746e3afc22e1c09459ffc1e02d1a19f0db668e12
rostislav-deriv <rostislav@deriv.com>
2024-05-22 14:14:21 +0300

    test: add unit tests for tooltip

--- packages/wallets/src/components/Base/Tooltip/Tooltip.tsx
+++ packages/wallets/src/components/Base/Tooltip/Tooltip.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Tooltip.scss';
 
 type TProps = {
@@ -16,11 +17,18 @@ const Tooltip: React.FC<React.PropsWithChildren<TProps>> = ({
     message,
 }) => {
     return (
-        <div className={`wallets-tooltip`}>
+        <div className='wallets-tooltip'>
             {children}
             {isVisible && (
-                <div className={`wallets-tooltip__content wallets-tooltip__content--${alignment} ${className ?? ''}`}>
-                    <div className={`wallets-tooltip__arrow wallets-tooltip__arrow--${alignment}`} />
+                <div
+                    className={classNames(
+                        'wallets-tooltip__conten',
+                        `wallets-tooltip__content--${alignment}`,
+                        className
+                    )}
+                    data-testid='dt_tooltip_content'
+                >
+                    <div className={classNames('wallets-tooltip__arrow', `wallets-tooltip__arrow--${alignment}`)} />
                     <div className='wallets-tooltip__message'>{message}</div>
                 </div>
             )}

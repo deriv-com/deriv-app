9bdd05621834107d9fbbee97e095e5e839ee7a67
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-05-23 15:04:42 +0800

    chore: migrate profit helpers to typescript and add test cases

--- packages/reports/src/Stores/Modules/Profit/Helpers/format-request.ts
+++ packages/reports/src/Stores/Modules/Profit/Helpers/format-request.ts
@@ -1,32 +1,48 @@
 import { epochToMoment, toMoment } from '@deriv/shared';
 
-const getDateTo = (partial_fetch_time, date_to) => {
+type TDateToOrFrom = number | null;
+type TPartialFetchTime = boolean | number;
+
+const getDateTo = (partial_fetch_time: TPartialFetchTime, date_to: TDateToOrFrom) => {
     const today = toMoment().startOf('day').unix();
     if (date_to && today > date_to) {
         return date_to;
-    } else if (partial_fetch_time) {
-        return epochToMoment(today).add(1, 'd').subtract(1, 's').unix();
     }
     return epochToMoment(today).add(1, 'd').subtract(1, 's').unix();
 };
 
-const getDateFrom = (should_load_partially, partial_fetch_time, date_from, date_to) => {
+const getDateFrom = (
+    should_load_partially: boolean,
+    partial_fetch_time: TPartialFetchTime,
+    date_from: TDateToOrFrom,
+    date_to: TDateToOrFrom
+) => {
     const today = toMoment().startOf('day').unix();
-    if (today > date_to) {
+    if (date_to && today > date_to) {
         return date_from;
     }
     return should_load_partially && partial_fetch_time ? partial_fetch_time : date_from;
 };
 
-const shouldSendDateFrom = (date_from, should_load_partially, partial_fetch_time, date_to) => {
+const shouldSendDateFrom = (
+    date_from: TDateToOrFrom,
+    should_load_partially: boolean,
+    partial_fetch_time: TPartialFetchTime,
+    date_to: TDateToOrFrom
+) => {
     const today = toMoment().startOf('day').unix();
-    if (today > date_to) {
+    if (date_to && today > date_to) {
         return !!date_from || should_load_partially;
     }
     return should_load_partially ? partial_fetch_time || date_from : !!date_from || false;
 };
 
-const getDateBoundaries = (date_from, date_to, partial_fetch_time, should_load_partially = false) => ({
+const getDateBoundaries = (
+    date_from: TDateToOrFrom,
+    date_to: TDateToOrFrom,
+    partial_fetch_time: TPartialFetchTime,
+    should_load_partially = false
+) => ({
     // eslint-disable-next-line max-len
     ...(shouldSendDateFrom(date_from, should_load_partially, partial_fetch_time, date_to) && {
         date_from: getDateFrom(should_load_partially, partial_fetch_time, date_from, date_to),

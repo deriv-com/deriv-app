e9b8a6b799b340fd7520202f0a251c12eedf3435
mitra-deriv <64970259+mitra-deriv@users.noreply.github.com>
2024-05-23 14:37:47 +0800

    [WEBREL]/Mitra/[WEBREL-2753] remove crypto subheader (#15316)
    * fix: :bug: remove crypto subhead
    
    * fix: :test_tube: fix failing test

--- packages/account/src/Components/currency-selector/currency-selector.tsx
+++ packages/account/src/Components/currency-selector/currency-selector.tsx
@@ -217,8 +217,8 @@ const CurrencySelector = observer(
                                             ?.length && (
                                             <React.Fragment>
                                                 <RadioButtonGroup
+                                                    is_title_enabled={false}
                                                     className='currency-selector__radio-group currency-selector__radio-group--with-margin'
-                                                    label={localize('Cryptocurrencies')}
                                                     item_count={
                                                         reorderCurrencies(
                                                             crypto as keyof typeof reorderCurrencies,

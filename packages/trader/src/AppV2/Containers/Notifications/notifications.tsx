ce2fa81f1bc1a0156573f9a0d4266dc55c7b254f
Henry Hein <henry@regentmarkets.com>
2024-05-24 15:01:52 +0800

    fix: remove usedevice

--- packages/trader/src/AppV2/Containers/Notifications/notifications.tsx
+++ packages/trader/src/AppV2/Containers/Notifications/notifications.tsx
@@ -1,14 +1,12 @@
 import { useNotifications, NotificationBanners } from '@deriv-com/quill-ui';
 import React from 'react';
-import { useDevice } from '@deriv/components';
 
 const Notifications = () => {
     const { banners, removeBanner } = useNotifications();
-    const { is_mobile } = useDevice();
 
     return (
         <>
-            <NotificationBanners banners={banners} isMobile={is_mobile} onClose={removeBanner} />
+            <NotificationBanners banners={banners} onClose={removeBanner} />
         </>
     );
 };

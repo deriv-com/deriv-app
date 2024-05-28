da529298a74d72952eaf66339754007bee388cea
hasan-deriv <abuhasan@deriv.com>
2024-05-28 13:43:11 +0800

    chore: added comment calculate total utility function

--- packages/wallets/src/utils/calculate-total-by-key.ts
+++ packages/wallets/src/utils/calculate-total-by-key.ts
@@ -1,3 +1,15 @@
+/**
+ * Calculates the total of a specified key in an array of objects.
+ *
+ * The function handles numeric values, numeric strings, and ignores non-numeric strings.
+ * It returns the total as a string formatted to two decimal places.
+ *
+ * @template T - The type of the objects in the array.
+ * @param {T[]} items - The array of objects to process.
+ * @param {keyof T} key - The key whose values will be summed.
+ * @returns {string} - The total of the specified key formatted to two decimal places.
+ */
+
 export function calculateTotalByKey<T>(items: T[], key: keyof T): string {
     return items
         .reduce((acc, cur) => {

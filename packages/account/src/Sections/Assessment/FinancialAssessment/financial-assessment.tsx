aad8587540c3ddeda62e7688e4f0d9065d008040
Muhammad Fasih Ali Naqvi <muhammadfasih@Muhammad-Fasihs-Mac-X7KDC4NKQ0-Standard.local>
2024-05-29 12:47:23 +0800

    fix: removed unwanted code

--- packages/account/src/Sections/Assessment/FinancialAssessment/financial-assessment.tsx
+++ packages/account/src/Sections/Assessment/FinancialAssessment/financial-assessment.tsx
@@ -14,7 +14,7 @@ import {
     SelectNative,
     Text,
 } from '@deriv/components';
-import { routes, platforms, WS, shouldHideOccupationField, isTablet } from '@deriv/shared';
+import { routes, platforms, WS, shouldHideOccupationField } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import { localize, Localize } from '@deriv/translations';
 import LeaveConfirm from 'Components/leave-confirm';

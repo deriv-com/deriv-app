53c77d0e17c6b7a095bfcea615bfb5597615c3e0
Likhith Kolayari <likhith@regentmarkets.com>
2024-05-30 10:16:04 +0400

    fix: webpack chunk render issue

--- packages/account/build/webpack.config.js
+++ packages/account/build/webpack.config.js
@@ -32,45 +32,10 @@ module.exports = function (env) {
             extensions: ['.js', '.jsx', '.ts', '.tsx'],
         },
         optimization: {
+            chunkIds: 'named',
+            moduleIds: 'named',
             minimize: IS_RELEASE,
             minimizer: MINIMIZERS,
-            splitChunks: {
-                automaticNameDelimiter: '~',
-                cacheGroups: {
-                    default: {
-                        minChunks: 2,
-                        priority: -20,
-                        reuseExistingChunk: true,
-                    },
-                    defaultVendors: {
-                        idHint: 'vendors',
-                        priority: -10,
-                        test: /[\\/]node_modules[\\/]/,
-                    },
-                    assets: {
-                        chunks: 'all',
-                        name: 'assets',
-                        test: /[\\/]Assets[\\/]/,
-                        reuseExistingChunk: true,
-                        idHint: 'assets',
-                    },
-                    common: {
-                        name: 'common',
-                        test: /[\\/]Constants[\\/]/,
-                        reuseExistingChunk: true,
-                        priority: -5,
-                        idHint: 'common',
-                        enforce: true,
-                        minChunks: 2,
-                    },
-                },
-                chunks: 'all',
-                maxAsyncRequests: 30,
-                maxInitialRequests: 3,
-                minChunks: 1,
-                minSize: 102400,
-                minSizeReduction: 102400,
-            },
         },
         output: {
             filename: 'account/js/[name].js',

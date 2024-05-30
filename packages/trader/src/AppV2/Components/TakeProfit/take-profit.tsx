a4e709c701a1e766ab5d29da7934c807af4f712e
Henry Hein <henry@regentmarkets.com>
2024-05-30 17:28:12 +0800

    fix: handle all cases of risk management items display

---
+++ packages/trader/src/AppV2/Components/TakeProfit/take-profit.tsx
@@ -0,0 +1,25 @@
+import { Localize } from '@deriv/translations';
+import React from 'react';
+import { getContractDetailsConfig } from 'AppV2/Utils/contract-details-config';
+import { observer } from '@deriv/stores';
+import useContractDetails from 'AppV2/Hooks/useContractDetails';
+import RiskManagementItem from '../RiskManagementItem';
+
+const TakeProfit = observer(() => {
+    const { contract_info } = useContractDetails();
+    const { is_take_profit_visible } = getContractDetailsConfig(contract_info.contract_type ?? '');
+
+    return (
+        is_take_profit_visible && (
+            <RiskManagementItem
+                label={<Localize i18n_default_text='Take profit' />}
+                modal_body_content={
+                    <Localize i18n_default_text='When your profit reaches or exceeds the set amount, your trade will be closed automatically.' />
+                }
+                validation_message='hello'
+            />
+        )
+    );
+});
+
+export default TakeProfit;
--- packages/trader/src/AppV2/Containers/ContractDetails/contract-details.tsx
+++ packages/trader/src/AppV2/Containers/ContractDetails/contract-details.tsx
@@ -4,22 +4,20 @@ import EntryExitDetails from 'AppV2/Components/EntryExitDetails';
 import TakeProfitHistory from 'AppV2/Components/TakeProfitHistory';
 import PayoutInfo from 'AppV2/Components/PayoutInfo';
 import ChartPlaceholder from '../Chart';
-import { Localize } from '@deriv/translations';
-import RiskManagementItem from 'AppV2/Components/RiskManagementItem';
 import CardWrapper from 'AppV2/Components/CardWrapper';
 import { observer } from '@deriv/stores';
 import useContractDetails from 'AppV2/Hooks/useContractDetails';
-import { isValidToCancel } from '@deriv/shared';
 import OrderDetails from 'AppV2/Components/OrderDetails';
 import { getContractDetailsConfig } from 'AppV2/Utils/contract-details-config';
+import TakeProfit from 'AppV2/Components/TakeProfit/take-profit';
+import StopLoss from 'AppV2/Components/StopLoss/stop-loss';
+import DealCancellation from 'AppV2/Components/DealCancellation/deal-cancellation';
+import { isOpen } from '@deriv/shared';
 
 const ContractDetails = observer(() => {
     const { contract_info, is_loading } = useContractDetails();
     if (is_loading) return <></>;
-    const is_valid_to_cancel = isValidToCancel(contract_info);
-    const { is_deal_cancellation_visible, is_stop_loss_visible, is_take_profit_visible, is_tp_history_visible } =
-        getContractDetailsConfig(contract_info.contract_type ?? '');
-
+    const { is_tp_history_visible } = getContractDetailsConfig(contract_info.contract_type ?? '');
     const historyData = [
         { date: '01 Jan 2024', time: '12:00:00 GMT', action: 'Take profit', amount: '5.00 USD' },
         { date: '02 Jan 2024', time: '13:00:00 GMT', action: 'Take profit', amount: '10.00 USD' },
@@ -56,32 +54,13 @@ const ContractDetails = observer(() => {
             <div className='placeholder'>
                 <ChartPlaceholder />
             </div>
-            {is_valid_to_cancel && is_deal_cancellation_visible && (
+            <DealCancellation />
+            {isOpen(contract_info) && (
                 <CardWrapper>
-                    <RiskManagementItem
-                        label={<Localize i18n_default_text='Deal cancellation' />}
-                        modal_body_content={<Localize i18n_default_text='Whatever you desire' />}
-                        validation_message='hello'
-                        is_deal_cancellation
-                    />
+                    <TakeProfit />
+                    <StopLoss />
                 </CardWrapper>
             )}
-            <CardWrapper>
-                {is_take_profit_visible && (
-                    <RiskManagementItem
-                        label={<Localize i18n_default_text='Take profit' />}
-                        modal_body_content={<Localize i18n_default_text='Whatever you desire' />}
-                        validation_message='hello'
-                    />
-                )}
-                {is_stop_loss_visible && (
-                    <RiskManagementItem
-                        label={<Localize i18n_default_text='Stop Loss' />}
-                        modal_body_content={<Localize i18n_default_text='Whatever you desire' />}
-                        validation_message='hello'
-                    />
-                )}
-            </CardWrapper>
             <div className='placeholder'>
                 <OrderDetails contract_info={contract_info} />
             </div>

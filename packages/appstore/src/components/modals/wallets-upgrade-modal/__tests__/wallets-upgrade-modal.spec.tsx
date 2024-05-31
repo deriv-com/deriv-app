505ab9437432d14eac820b343a21bb397fb7c7d8
Shahzaib <shahzaib@deriv.com>
2024-05-31 14:42:41 +0800

    chore(test): fix react import in test case

--- packages/appstore/src/components/modals/wallets-upgrade-modal/__tests__/wallets-upgrade-modal.spec.tsx
+++ packages/appstore/src/components/modals/wallets-upgrade-modal/__tests__/wallets-upgrade-modal.spec.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { useWalletMigration } from '@deriv/hooks';

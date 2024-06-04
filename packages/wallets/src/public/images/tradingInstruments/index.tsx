3a9be8f5a436c205a580bafda01b60661685f87a
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-06-04 11:00:56 +0800

    fix: PR complains

--- packages/wallets/src/public/images/tradingInstruments/index.tsx
+++ packages/wallets/src/public/images/tradingInstruments/index.tsx
@@ -1,9 +1,9 @@
 import Baskets from './ic-instrument-baskets.svg';
 import Commodities from './ic-instrument-commodities.svg';
 import Cryptocurrencies from './ic-instrument-cryptocurrencies.svg';
-import StandardFX from './ic-instrument-standard-fx.svg';
 import ETF from './ic-instrument-etf.svg';
 import Forex from './ic-instrument-forex.svg';
+import StandardFX from './ic-instrument-standard-fx.svg';
 import StockIndices from './ic-instrument-stock-indices.svg';
 import Stocks from './ic-instrument-stocks.svg';
 import Synthetics from './ic-instrument-synthetics.svg';
@@ -12,9 +12,9 @@ const InstrumentsIcons = {
     Baskets,
     Commodities,
     Cryptocurrencies,
-    StandardFX,
     ETF,
     Forex,
+    StandardFX,
     StockIndices,
     Stocks,
     Synthetics,

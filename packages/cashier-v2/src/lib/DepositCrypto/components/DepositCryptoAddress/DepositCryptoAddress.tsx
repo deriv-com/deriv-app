f89c34486256b9eb11a9a86bf3a4713fe2a877c8
Wojciech Brygola <wojciech@regentmarkets.com>
2024-06-06 08:40:43 +0100

    feat: fixed eslint

--- packages/cashier-v2/src/lib/DepositCrypto/components/DepositCryptoAddress/DepositCryptoAddress.tsx
+++ packages/cashier-v2/src/lib/DepositCrypto/components/DepositCryptoAddress/DepositCryptoAddress.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import QRCode from 'qrcode.react';
-import { useAuthorize, useDepositCryptoAddress } from '@deriv/api-v2';
+import { useDepositCryptoAddress } from '@deriv/api-v2';
 import { Text, useDevice } from '@deriv-com/ui';
 import { Clipboard } from '../../../../components';
 import { DepositCryptoAddressLoader } from '../DepositCryptoAddressLoader';

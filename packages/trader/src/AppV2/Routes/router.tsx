5d66774a98b7ca439d2225d11c37d6fcfb00a371
balakrishna-binary <bala.krishna@regentmarkets.com>
2024-06-10 15:51:25 +0800

    fix: unit testing

--- packages/trader/src/AppV2/Routes/router.tsx
+++ packages/trader/src/AppV2/Routes/router.tsx
@@ -1,16 +1,18 @@
 import React, { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { UILoader } from '@deriv/components';
 import traderRoutes from './routes';
 
 const Router: React.FC = () => {
     return (
         <Suspense fallback={<UILoader />}>
-            <Switch>
-                {traderRoutes.map((route, index) => (
-                    <Route key={index} path={route.path} exact={route.exact} component={route.component} />
-                ))}
-            </Switch>
+            <BrowserRouter>
+                <Switch>
+                    {traderRoutes.map((route, index) => (
+                        <Route key={index} path={route.path} exact={route.exact} component={route.component} />
+                    ))}
+                </Switch>
+            </BrowserRouter>
         </Suspense>
     );
 };

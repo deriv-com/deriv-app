bb3c84a6cad1c6fc41ec0cabc25ce5b240507fa4
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2024-06-10 11:02:56 +0800

    Revert "[TRAH-3530] shahzaib / notification popups display removal & content …"
    This reverts commit c16a99ce6e3c1429e4e10c3db0e974c806dd6204.

--- packages/wallets/src/constants/constants.tsx
+++ packages/wallets/src/constants/constants.tsx
@@ -41,7 +41,7 @@ export const optionsAndMultipliersContent = [
         title: i18n.t('Binary Bot'),
     },
     {
-        description: i18n.t('The mobile app for trading multipliers and accumulators.'),
+        description: i18n.t('The mobile trading app for multipliers.'),
         icon: <DerivProductDerivGoBrandLightLogoHorizontalIcon height='48' width='48' />,
         isExternal: true,
         redirect: getStaticUrl('/deriv-go'),

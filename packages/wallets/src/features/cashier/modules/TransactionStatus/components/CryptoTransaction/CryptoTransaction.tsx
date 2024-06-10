b5913eff02804082608328d37499b8399520bb8f
vinu-deriv <vinu@deriv.com>
2024-06-10 18:20:15 +0400

    fix: added todo to remove transfer fee from transaction type in TCryptoTransaction

--- packages/wallets/src/features/cashier/modules/TransactionStatus/components/CryptoTransaction/CryptoTransaction.tsx
+++ packages/wallets/src/features/cashier/modules/TransactionStatus/components/CryptoTransaction/CryptoTransaction.tsx
@@ -13,6 +13,7 @@ import './CryptoTransaction.scss';
 type TCryptoTransaction = {
     currencyDisplayCode: THooks.CurrencyConfig['code'];
     currencyDisplayFraction?: THooks.CurrencyConfig['fractional_digits'];
+    // TODO: Remove transaction_fee from transaction type once API is updated
     /* eslint-disable camelcase */
     transaction: THooks.CryptoTransactions & { transaction_fee?: number };
 };

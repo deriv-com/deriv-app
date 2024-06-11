f0ec370304d5f7b21103230f53d072a2f50be73d
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2024-06-11 08:12:27 +0300

    [DTRA] Maryia/DTRA-1276/fix: update limit order barriers language without reload + hide them for closed contracts (#15436)
    * fix: limit order barriers translation on language change
    
    * chore: keep setLimitOrderBarriers & ChartBarrierStore in trader consistent with those in core
    
    * fix: types
    
    * fix: hide limit order barriers for ended contract
    
    * refactor: place barriers.ts, chart-barrier-store, limit-orders in shared as they are reused
    
    * test: ChartBarrierStore + address sonarcloud
    
    * fix: update/show/hide relevant limit order barriers without page reload
    
    * test: fix failing test

--- packages/shared/src/utils/helpers/chart-barrier-store.ts
+++ packages/shared/src/utils/helpers/chart-barrier-store.ts
@@ -1,19 +1,21 @@
 import { action, computed, observable, makeObservable } from 'mobx';
-import { BARRIER_LINE_STYLES, CONTRACT_SHADES, DEFAULT_SHADES } from '@deriv/shared';
-import { barriersToString } from './Helpers/barriers';
+import { BARRIER_LINE_STYLES, CONTRACT_SHADES, DEFAULT_SHADES } from '../constants';
+import { barriersToString } from './barriers';
 
 type TOnChartBarrierChange = null | ((barrier_1: string, barrier_2?: string) => void);
-type TOnChangeParams = { high: string | number; low?: string | number };
+type TOnChangeParams = { high: string | number; low?: string | number; title?: string; hidePriceLines?: boolean };
 type TChartBarrierStoreOptions =
     | {
-          color: string;
+          color?: string;
+          hideBarrierLine?: boolean;
           line_style?: string;
           not_draggable?: boolean;
+          shade?: string;
       }
     | Record<string, never>;
 
 export class ChartBarrierStore {
-    color: string;
+    color?: string;
     lineStyle: string;
     shade?: string;
     shadeColor?: string;
@@ -32,7 +34,7 @@ export class ChartBarrierStore {
         high_barrier?: string | number,
         low_barrier?: string | number,
         onChartBarrierChange: TOnChartBarrierChange = null,
-        { color, line_style, not_draggable }: TChartBarrierStoreOptions = {}
+        { color, hideBarrierLine, line_style, not_draggable, shade }: TChartBarrierStoreOptions = {}
     ) {
         makeObservable(this, {
             color: observable,
@@ -55,6 +57,7 @@ export class ChartBarrierStore {
         });
 
         this.color = color;
+        this.hideBarrierLine = hideBarrierLine;
         this.lineStyle = line_style || BARRIER_LINE_STYLES.SOLID;
         this.onChange = this.onBarrierChange;
 
@@ -67,7 +70,7 @@ export class ChartBarrierStore {
             this.low = low_barrier;
         }
 
-        this.shade = this.default_shade;
+        this.shade = shade ?? this.default_shade;
 
         const has_barrier = !!high_barrier;
         this.relative = !has_barrier || /^[+-]/.test(high_barrier.toString());
@@ -75,12 +78,20 @@ export class ChartBarrierStore {
         this.hidePriceLines = !has_barrier;
     }
 
-    updateBarriers(high: string | number, low?: string | number, isFromChart = false) {
+    updateBarriers(
+        high: string | number,
+        low?: string | number,
+        title?: string,
+        hidePriceLines?: boolean,
+        isFromChart = false
+    ) {
         if (!isFromChart) {
             this.relative = /^[+-]/.test(high.toString());
         }
         this.high = high || undefined;
         this.low = low || undefined;
+        this.title = title ?? this.title;
+        this.hidePriceLines = hidePriceLines ?? this.hidePriceLines;
     }
 
     updateBarrierShade(should_display: boolean, contract_type: string) {
@@ -88,8 +99,8 @@ export class ChartBarrierStore {
             (should_display && CONTRACT_SHADES[contract_type as keyof typeof CONTRACT_SHADES]) || this.default_shade;
     }
 
-    onBarrierChange({ high, low }: TOnChangeParams) {
-        this.updateBarriers(high, low, true);
+    onBarrierChange({ high, low, title, hidePriceLines }: TOnChangeParams) {
+        this.updateBarriers(high, low, title, hidePriceLines, true);
         this.onChartBarrierChange?.(...(barriersToString(this.relative, high, low) as [string, string | undefined]));
     }
 

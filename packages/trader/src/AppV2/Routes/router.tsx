5cf2c19e0fb79a8c41e35cd1be6df77a97ff14ac
balakrishna-binary <bala.krishna@regentmarkets.com>
2024-06-11 11:12:52 +0800

    chore: check needs authentication

--- packages/trader/src/AppV2/Routes/router.tsx
+++ packages/trader/src/AppV2/Routes/router.tsx
@@ -1,15 +1,30 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { UILoader } from '@deriv/components';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { UILoader, RouteWithSubroutes } from '@deriv/components';
 import traderRoutes from './routes';
+import { observer, useStore } from '@deriv/stores';
+import Page404 from 'Modules/Page404';
 
 const Router: React.FC = () => {
+    const { client, common } = useStore();
+    const { is_logged_in } = client;
+    const { current_language } = common;
+
     return (
         <Suspense fallback={<UILoader />}>
             <BrowserRouter>
                 <Switch>
                     {traderRoutes.map((route, index) => (
-                        <Route key={index} path={route.path} exact={route.exact} component={route.component} />
+                        <RouteWithSubroutes
+                            key={index}
+                            is_logged_in={is_logged_in}
+                            language={current_language}
+                            Component404={Page404}
+                            should_redirect_login={true}
+                            routes={traderRoutes}
+                            to={''}
+                            {...route}
+                        />
                     ))}
                 </Switch>
             </BrowserRouter>
@@ -17,4 +32,4 @@ const Router: React.FC = () => {
     );
 };
 
-export default Router;
+export default observer(Router);
--- packages/trader/src/AppV2/Routes/routes.tsx
+++ packages/trader/src/AppV2/Routes/routes.tsx
@@ -1,32 +1,45 @@
 import { lazy } from 'react';
-import { RouteProps } from 'react-router';
 import { routes } from '@deriv/shared';
 import Trade from 'AppV2/Containers/Trade';
 import Markets from 'AppV2/Containers/Markets';
 import Positions from 'AppV2/Containers/Positions';
 import Menu from 'AppV2/Containers/Menu';
+import { TRouteConfig } from 'Types';
 
-const traderRoutes: RouteProps[] = [
+type TRouteConfigExtended = Omit<TRouteConfig, 'routes'> & {
+    path: string;
+    component: React.ComponentType;
+    default: boolean;
+};
+
+const traderRoutes: TRouteConfigExtended[] = [
     {
         path: routes.trade,
         component: Trade,
         exact: true,
+        default: false,
     },
     {
         path: routes.markets,
         component: Markets,
+        default: false,
     },
     {
         path: routes.trader_positions,
         component: Positions,
+        is_authenticated: true,
+        default: false,
     },
     {
         path: routes.trader_menu,
         component: Menu,
+        default: false,
     },
     {
         // default route
+        path: '/*',
         component: lazy(() => import('Modules/Page404')),
+        default: false,
     },
 ];
 

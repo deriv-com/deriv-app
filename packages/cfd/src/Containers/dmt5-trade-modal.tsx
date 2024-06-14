1cc3b7fba290ff28f5d3a672825b667ce003f84f
Hirad <hirad@regentmarkets.com>
2024-06-14 11:22:08 +0800

    chore: removing the redundant div

--- packages/cfd/src/Containers/dmt5-trade-modal.tsx
+++ packages/cfd/src/Containers/dmt5-trade-modal.tsx
@@ -117,14 +117,12 @@ const DMT5TradeModal = observer(
                         )}
                         {is_demo && (
                             <React.Fragment>
-                                <div>
-                                    <Text size='xs' line_height='l' className='cfd-trade-modal__desc-heading'>
-                                        {getAccountTitle()}
-                                    </Text>
-                                    <Text size='xs' line_height='l' className='cfd-trade-modal--tag'>
-                                        Demo
-                                    </Text>
-                                </div>
+                                <Text size='xs' line_height='l' className='cfd-trade-modal__desc-heading'>
+                                    {getAccountTitle()}
+                                </Text>
+                                <Text size='xs' line_height='l' className='cfd-trade-modal--tag'>
+                                    Demo
+                                </Text>
                             </React.Fragment>
                         )}
                         {(mt5_trade_account as TTradingPlatformAccounts)?.display_login && (

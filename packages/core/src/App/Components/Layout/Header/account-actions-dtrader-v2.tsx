97ceabb5986d7cf449d57d2f5cc5aa87c0262726
kate-deriv <kate@regentmarkets.com>
2024-06-14 12:30:33 +0300

    refactor: create copy of account info file

--- packages/core/src/App/Components/Layout/Header/account-actions-dtrader-v2.tsx
+++ packages/core/src/App/Components/Layout/Header/account-actions-dtrader-v2.tsx
@@ -4,17 +4,9 @@ import { StandaloneBellRegularIcon } from '@deriv/quill-icons';
 import { formatMoney, moduleLoader } from '@deriv/shared';
 import { LoginButton } from './login-button.jsx';
 import { SignupButton } from './signup-button.jsx';
-import 'Sass/app/_common/components/account-switcher.scss';
 import { Badge } from '@deriv-com/quill-ui';
-
-const AccountInfo = React.lazy(() =>
-    moduleLoader(
-        () =>
-            import(
-                /* webpackChunkName: "account-info", webpackPreload: true */ 'App/Components/Layout/Header/account-info.jsx'
-            )
-    )
-);
+import 'Sass/app/_common/components/account-switcher.scss';
+import AccountInfoDTraderV2 from './account-info-dtrader-v2';
 
 type TAccountActionsDTraderV2 = {
     acc_switcher_disabled_message?: string;
@@ -28,9 +20,30 @@ type TAccountActionsDTraderV2 = {
     is_acc_switcher_on?: boolean;
     is_logged_in?: boolean;
     is_virtual?: boolean;
-    notifications_count: number;
+    notifications_count?: number;
     toggleAccountsDialog: (value?: boolean | undefined) => void;
 };
+
+export type TAccountInfoDTraderV2 = Omit<
+    TAccountActionsDTraderV2,
+    'toggleAccountsDialog' | 'is_acc_switcher_disabled' | 'is_acc_switcher_on'
+> & {
+    is_dialog_on?: boolean;
+    toggleDialog: (value?: boolean | undefined) => void;
+    is_disabled?: boolean;
+    is_mobile?: boolean;
+};
+// const AccountInfoDTraderV2 = React.lazy(
+//     () =>
+//         moduleLoader(
+//             () =>
+//                 import(
+//                     /* webpackChunkName: "account-info-dtrader-v2", webpackPreload: true */ 'App/Components/Layout/Header/account-info-dtrader-v2'
+//                 )
+//         ) as Promise<{
+//             default: React.ComponentType<TAccountInfoDTraderV2>;
+//         }>
+// );
 
 const AccountActionsDTraderV2 = React.memo(
     ({
@@ -52,7 +65,7 @@ const AccountActionsDTraderV2 = React.memo(
             return (
                 <React.Fragment>
                     <React.Suspense fallback={<div />}>
-                        <AccountInfo
+                        <AccountInfoDTraderV2
                             acc_switcher_disabled_message={acc_switcher_disabled_message}
                             account_type={account_type}
                             balance={typeof balance === 'undefined' ? balance : formatMoney(currency, balance, true)}
@@ -87,6 +100,7 @@ const AccountActionsDTraderV2 = React.memo(
                 </React.Fragment>
             );
         }
+        // TODO: remove Login/out after
         return (
             <React.Fragment>
                 <LoginButton className='acc-info__button' />
---
+++ packages/core/src/App/Components/Layout/Header/account-info-dtrader-v2.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import classNames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
+import { DesktopWrapper, Icon, MobileWrapper, Text } from '@deriv/components';
+import { Localize } from '@deriv/translations';
+import { getCurrencyDisplayCode } from '@deriv/shared';
+import AccountSwitcher from 'App/Containers/AccountSwitcher';
+import AccountSwitcherMobile from 'App/Containers/AccountSwitcher/account-switcher-mobile';
+import AccountInfoWrapper from './account-info-wrapper';
+import AccountInfoIcon from './account-info-icon';
+import DisplayAccountType from './display-account-type';
+import { TAccountInfoDTraderV2 } from './account-actions-dtrader-v2';
+
+const AccountInfoDTraderV2 = ({
+    acc_switcher_disabled_message,
+    account_type = '',
+    balance,
+    currency,
+    disableApp,
+    enableApp,
+    is_dialog_on,
+    is_eu,
+    is_virtual,
+    toggleDialog,
+    is_disabled,
+    is_mobile,
+}: TAccountInfoDTraderV2) => {
+    const currency_lower = currency?.toLowerCase();
+
+    return (
+        <div className='acc-info__wrapper'>
+            <div className='acc-info__separator' />
+            <AccountInfoWrapper
+                is_disabled={is_disabled}
+                disabled_message={acc_switcher_disabled_message}
+                is_mobile={is_mobile}
+            >
+                <div
+                    data-testid='dt_acc_info'
+                    id='dt_core_account-info_acc-info'
+                    className={classNames('acc-info', {
+                        'acc-info--show': is_dialog_on,
+                        'acc-info--is-virtual': is_virtual,
+                        'acc-info--is-disabled': is_disabled,
+                    })}
+                    onClick={is_disabled ? undefined : () => toggleDialog()}
+                >
+                    <span className='acc-info__id'>
+                        <DesktopWrapper>
+                            <AccountInfoIcon is_virtual={is_virtual} currency={currency_lower} />
+                        </DesktopWrapper>
+                        <MobileWrapper>
+                            {(is_virtual || currency) && (
+                                <AccountInfoIcon is_virtual={is_virtual} currency={currency_lower} />
+                            )}
+                        </MobileWrapper>
+                    </span>
+                    {(typeof balance !== 'undefined' || !currency) && (
+                        <div className='acc-info__account-type-and-balance'>
+                            <p
+                                data-testid='dt_balance'
+                                className={classNames('acc-info__balance', {
+                                    'acc-info__balance--no-currency': !currency && !is_virtual,
+                                })}
+                            >
+                                {!currency ? (
+                                    <Localize i18n_default_text='No currency assigned' />
+                                ) : (
+                                    `${balance} ${getCurrencyDisplayCode(currency)}`
+                                )}
+                            </p>
+                            <Text size='xxxs' line_height='s'>
+                                <DisplayAccountType account_type={account_type} is_eu={is_eu} />
+                            </Text>
+                        </div>
+                    )}
+                    {is_disabled ? (
+                        <Icon data_testid='dt_lock_icon' icon='IcLock' />
+                    ) : (
+                        <Icon
+                            data_testid='dt_select_arrow'
+                            icon='IcChevronDownBold'
+                            className='acc-info__select-arrow'
+                        />
+                    )}
+                </div>
+            </AccountInfoWrapper>
+            <MobileWrapper>
+                <AccountSwitcherMobile
+                    is_visible={is_dialog_on}
+                    disableApp={disableApp}
+                    enableApp={enableApp}
+                    toggle={toggleDialog}
+                />
+            </MobileWrapper>
+            <DesktopWrapper>
+                <CSSTransition
+                    in={is_dialog_on}
+                    timeout={200}
+                    classNames={{
+                        enter: 'acc-switcher__wrapper--enter',
+                        enterDone: 'acc-switcher__wrapper--enter-done',
+                        exit: 'acc-switcher__wrapper--exit',
+                    }}
+                    unmountOnExit
+                >
+                    <div className='acc-switcher__wrapper'>
+                        <AccountSwitcher is_visible={is_dialog_on} toggle={toggleDialog} />
+                    </div>
+                </CSSTransition>
+            </DesktopWrapper>
+        </div>
+    );
+};
+
+export default AccountInfoDTraderV2;

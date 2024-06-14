9599c8bc1b4c7e90d70ed2a2ee44e44cd493a4f0
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-06-14 15:15:43 +0800

    chore:fix

--- packages/trader/src/AppV2/Components/DealCancellationRemainingTime/deal-cancellation-remaining-time.tsx
+++ packages/trader/src/AppV2/Components/DealCancellationRemainingTime/deal-cancellation-remaining-time.tsx
@@ -1,6 +1,7 @@
 import { Tag } from '@deriv-com/quill-ui';
+import { RemainingTime } from '@deriv/components';
 import { LabelPairedStopwatchCaptionRegularIcon } from '@deriv/quill-icons';
-import { formatDuration, getDiffDuration } from '@deriv/shared';
+import { formatDuration, getCardLabels, getDiffDuration } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import useContractDetails from 'AppV2/Hooks/useContractDetails';
 import React from 'react';
@@ -19,18 +20,24 @@ const DealCancellationRemainingTime = observer(({ format = 'mm:ss' }: DealCancel
         return <React.Fragment>{''}</React.Fragment>;
     }
 
-    const { timestamp } = formatDuration(getDiffDuration(start_time.unix(), end_time), format);
-
     return (
-        <Tag
-            className='deal-cancellation-badge'
-            color='custom'
-            data-testid='deal-cancellation-badge'
-            icon={<LabelPairedStopwatchCaptionRegularIcon />}
-            label={timestamp}
-            size='sm'
-            variant='custom'
-        />
+        <React.Fragment>
+            <Tag
+                className='deal-cancellation-badge'
+                color='custom'
+                data-testid='deal-cancellation-badge'
+                label={
+                    <RemainingTime
+                        end_time={end_time}
+                        format='mm:ss'
+                        getCardLabels={getCardLabels}
+                        start_time={start_time}
+                    />
+                }
+                size='sm'
+                variant='custom'
+            />
+        </React.Fragment>
     );
 });
 

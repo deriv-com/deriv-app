159c64c1cc8ce099c1567dc565df6f04293f4318
kate-deriv <kate@regentmarkets.com>
2024-06-14 17:40:43 +0300

    feat: add new account switcher in header

--- packages/core/src/App/Components/Layout/Header/account-info-dtrader-v2.tsx
+++ packages/core/src/App/Components/Layout/Header/account-info-dtrader-v2.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Icon, Text } from '@deriv/components';
 import { Localize } from '@deriv/translations';
 import { getCurrencyDisplayCode } from '@deriv/shared';
 import AccountSwitcherMobile from 'App/Containers/AccountSwitcher/account-switcher-mobile';
-import DisplayAccountType from './display-account-type';
 import { TAccountInfoDTraderV2 } from './account-actions-dtrader-v2';
 import AccountInfoWrapper from './account-info-wrapper';
 import {
@@ -28,17 +26,20 @@ import {
     CurrencyTusdIcon,
     CurrencyUsdkIcon,
     CurrencyXrpIcon,
+    LabelPairedChevronDownCaptionRegularIcon,
+    LabelPairedLockCaptionRegularIcon,
 } from '@deriv/quill-icons';
+import { CaptionText, Heading } from '@deriv-com/quill-ui';
 
 const AccountInfoDTraderV2 = ({
     acc_switcher_disabled_message,
-    account_type = '',
+    // account_type = '',
     balance,
     currency,
     disableApp,
     enableApp,
     is_dialog_on,
-    is_eu,
+    // is_eu,
     is_virtual,
     toggleDialog,
     is_disabled,
@@ -75,7 +76,6 @@ const AccountInfoDTraderV2 = ({
         return config[key as keyof typeof config] ?? <CurrencyPlaceholderIcon iconSize='md' />;
     };
 
-    // TODO: remove unused css
     // TODO: disabling logic?
     return (
         <React.Fragment>
@@ -87,36 +87,35 @@ const AccountInfoDTraderV2 = ({
             >
                 <div className='header-v2__acc-info__wrapper' onClick={is_disabled ? undefined : () => toggleDialog()}>
                     {getAccountIcon(currency, !!is_virtual)}
-                    {(typeof balance !== 'undefined' || !currency) && (
-                        <div className='acc-info__account-type-and-balance'>
-                            <p
-                                data-testid='dt_balance'
-                                className={classNames('acc-info__balance', {
-                                    'acc-info__balance--no-currency': !currency && !is_virtual,
-                                })}
-                            >
+                    <div className='header-v2__acc-info'>
+                        <div className='header-v2__acc-info__name'>
+                            <CaptionText color='quill-typography__color--default'>
+                                <Localize i18n_default_text='Placeholder' />
+                            </CaptionText>
+                            {is_disabled ? (
+                                <LabelPairedLockCaptionRegularIcon />
+                            ) : (
+                                <LabelPairedChevronDownCaptionRegularIcon
+                                    className={classNames('header-v2__acc-info__select-arrow', {
+                                        'header-v2__acc-info__select-arrow--up': is_dialog_on,
+                                    })}
+                                />
+                            )}
+                        </div>
+                        {(balance ?? !currency) && (
+                            <Heading.H5 color='quill-typography__color--default'>
+                                {/* TODO: case without currency is taken from current production. No design */}
                                 {!currency ? (
                                     <Localize i18n_default_text='No currency assigned' />
                                 ) : (
                                     `${balance} ${getCurrencyDisplayCode(currency)}`
                                 )}
-                            </p>
-                            <Text size='xxxs' line_height='s'>
-                                <DisplayAccountType account_type={account_type} is_eu={!!is_eu} />
-                            </Text>
-                        </div>
-                    )}
-                    {is_disabled ? (
-                        <Icon data_testid='dt_lock_icon' icon='IcLock' />
-                    ) : (
-                        <Icon
-                            data_testid='dt_select_arrow'
-                            icon='IcChevronDownBold'
-                            className={classNames('header-v2__acc-info__select-arrow', {
-                                'header-v2__acc-info__select-arrow--up': is_dialog_on,
-                            })}
-                        />
-                    )}
+                                {/* <Text size='xxxs' line_height='s'>
+                                    <DisplayAccountType account_type={account_type} is_eu={!!is_eu} />
+                                </Text> */}
+                            </Heading.H5>
+                        )}
+                    </div>
                 </div>
             </AccountInfoWrapper>
             <AccountSwitcherMobile

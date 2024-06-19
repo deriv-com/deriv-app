77696f10b001ffd5526bfee3e22941c5437b1025
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-06-19 13:36:07 +0800

    fixes header

--- packages/trader/src/AppV2/Hooks/useContractDetails.ts
+++ packages/trader/src/AppV2/Hooks/useContractDetails.ts
@@ -1,15 +1,24 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router';
 import { useStore } from '@deriv/stores';
+
+const usePrevious = value => {
+    const ref = React.useRef();
+    React.useEffect(() => {
+        ref.current = value;
+    });
+
+    return ref.current;
+};
 
 const useContractDetails = () => {
     const store = useStore();
     const { contract_replay, contract_trade } = store;
     const { getContractById } = contract_trade;
-    const { contract_store, onMount } = contract_replay;
+    const { contract_store, onMount, onUnmount } = contract_replay;
+
     const { contract_info } = contract_store;
     const contract = getContractById(contract_info.contract_id);
-
     const location = useLocation();
 
     useEffect(() => {
@@ -17,7 +26,8 @@ const useContractDetails = () => {
             const urlContractId = location.pathname.split('/').pop();
             onMount(parseInt(urlContractId));
         }
-    }, [location.pathname, onMount, contract_info.contract_id]);
+        return () => onUnmount();
+    }, [location.pathname, onMount, onUnmount, contract_info.contract_id]);
 
     return {
         contract_info,

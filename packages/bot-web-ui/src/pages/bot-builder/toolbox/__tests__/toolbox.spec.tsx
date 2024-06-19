ae06185860a975680877a0e821ca66985531948b
Farabi <farabi@deriv.com>
2024-06-19 12:32:49 +0800

    fix: block menu active click case

--- packages/bot-web-ui/src/pages/bot-builder/toolbox/__tests__/toolbox.spec.tsx
+++ packages/bot-web-ui/src/pages/bot-builder/toolbox/__tests__/toolbox.spec.tsx
@@ -87,10 +87,11 @@ describe('Toolbox', () => {
 
     it('should render setVisibility to be false on blocks menu click', () => {
         render(<Toolbox />, { wrapper });
-
         const blocks_menu = screen.getByTestId('db-toolbox__title');
+        expect(screen.getByTestId('db-toolbox__content-wrapper')).toHaveClass('active');
+
         userEvent.click(blocks_menu);
-        expect(mock_DBot_store?.flyout.is_visible).toBeFalsy();
+        expect(screen.getByTestId('db-toolbox__content-wrapper')).not.toHaveClass('active');
     });
 
     it('should render setFormVisibility to be true on quick strategy button is clicked', () => {
@@ -101,7 +102,7 @@ describe('Toolbox', () => {
         expect(mock_DBot_store?.quick_strategy.is_open).toBeTruthy();
     });
 
-    it('should render flyout to be true when block menu category is clicked', () => {
+    it('should render flyout when block menu category is clicked', () => {
         render(<Toolbox />, { wrapper });
         const block_menu_category = screen.getByText('Trade parameters');
 
@@ -109,7 +110,7 @@ describe('Toolbox', () => {
         expect(mock_DBot_store?.flyout.is_visible).toBeTruthy();
     });
 
-    it('should render flyout to be false when block menu subcategory is clicked', () => {
+    it('should not render flyout when block menu subcategory is clicked', () => {
         render(<Toolbox />, { wrapper });
         const block_menu_subcategory = screen.getByText('Utility');
 
@@ -117,7 +118,7 @@ describe('Toolbox', () => {
         expect(mock_DBot_store?.flyout.is_visible).toBeFalsy();
     });
 
-    it('should render flyout to be true when block menu subcategory item is clicked', () => {
+    it('should render flyout when block menu subcategory item is clicked', () => {
         render(<Toolbox />, { wrapper });
         const block_menu_subcategory = screen.getByText('Utility');
 

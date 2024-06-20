eeec1ad6f0218532a5414a0218dd127d9ba33eba
kate-deriv <kate@regentmarkets.com>
2024-06-20 15:31:27 +0300

    refactor: add tests for account actions file

---
+++ packages/core/src/App/Components/Layout/Header/dtrader-v2/__tests__/header-account-actions-dtrader-v2.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { StoreProvider, mockStore } from '@deriv/stores';
+import HeaderAccountActionsDTraderV2 from '../header-account-actions-dtrader-v2';
+
+const defaultMockStore = mockStore({});
+jest.mock('../account-actions-dtrader-v2', () => jest.fn(() => 'AccountActionsDTraderV2'));
+
+describe('HeaderAccountActionsDTraderV2', () => {
+    it('should render component', () => {
+        render(
+            <StoreProvider store={defaultMockStore}>
+                <HeaderAccountActionsDTraderV2 />
+            </StoreProvider>
+        );
+
+        expect(screen.getByText('AccountActionsDTraderV2')).toBeInTheDocument();
+    });
+});

f0ab7fc0841ebe9beaf0af7533333135a94572dd
Tay Sui Sin <suisin@regentmarkets.com>
2024-06-21 14:31:34 +0800

    chore: update translation-flag constant based on comment

--- packages/shared/src/utils/constants/translation-flag.tsx
+++ packages/shared/src/utils/constants/translation-flag.tsx
@@ -21,29 +21,36 @@ import {
     FlagVietnamIcon,
 } from '@deriv/quill-icons';
 
+const flagComponents = {
+    AR: FlagArabLeagueIcon,
+    EN: FlagUnitedKingdomIcon,
+    ES: FlagSpainIcon,
+    BN: FlagBangladeshIcon,
+    DE: FlagGermanyIcon,
+    FR: FlagFranceIcon,
+    ID: FlagIndonesiaIcon,
+    IT: FlagItalyIcon,
+    // KO: ,
+    MN: FlagMongoliaIcon,
+    PL: FlagPolandIcon,
+    PT: FlagPortugalIcon,
+    RU: FlagRussiaIcon,
+    SI: FlagSriLankaIcon,
+    SW: FlagTanzaniaIcon,
+    TR: FlagTurkeyIcon,
+    VI: FlagVietnamIcon,
+    ZH_CN: FlagChinaSimplifiedIcon,
+    ZH_TW: FlagChinaTraditionalIcon,
+    TH: FlagThailandIcon,
+};
+
 type TTranslationFlag = {
     [key: string]: (width?: number, height?: number) => React.ReactNode;
 };
 
-export const TranslationFlag: TTranslationFlag = {
-    AR: (width, height) => <FlagArabLeagueIcon width={width} height={height} />,
-    EN: (width, height) => <FlagUnitedKingdomIcon width={width} height={height} />,
-    ES: (width, height) => <FlagSpainIcon width={width} height={height} />,
-    BN: (width, height) => <FlagBangladeshIcon width={width} height={height} />,
-    DE: (width, height) => <FlagGermanyIcon width={width} height={height} />,
-    FR: (width, height) => <FlagFranceIcon width={width} height={height} />,
-    ID: (width, height) => <FlagIndonesiaIcon width={width} height={height} />,
-    IT: (width, height) => <FlagItalyIcon width={width} height={height} />,
-    // KO: ,
-    MN: (width, height) => <FlagMongoliaIcon width={width} height={height} />,
-    PL: (width, height) => <FlagPolandIcon width={width} height={height} />,
-    PT: (width, height) => <FlagPortugalIcon width={width} height={height} />,
-    RU: (width, height) => <FlagRussiaIcon width={width} height={height} />,
-    SI: (width, height) => <FlagSriLankaIcon width={width} height={height} />,
-    SW: (width, height) => <FlagTanzaniaIcon width={width} height={height} />,
-    TR: (width, height) => <FlagTurkeyIcon width={width} height={height} />,
-    VI: (width, height) => <FlagVietnamIcon width={width} height={height} />,
-    ZH_CN: (width, height) => <FlagChinaSimplifiedIcon width={width} height={height} />,
-    ZH_TW: (width, height) => <FlagChinaTraditionalIcon width={width} height={height} />,
-    TH: (width, height) => <FlagThailandIcon width={width} height={height} />,
-};
+export const TranslationFlag: TTranslationFlag = Object.fromEntries(
+    Object.entries(flagComponents).map(([key, FlagComponent]) => [
+        key,
+        (width, height) => <FlagComponent width={width} height={height} />,
+    ])
+);

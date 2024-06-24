8955a774dfebe0973b1c2b7986edf86b31cbe2e5
kate-deriv <kate@regentmarkets.com>
2024-06-24 09:51:19 +0300

    refactor: apply suggections for banners icon

--- packages/core/src/App/Components/Layout/Header/dtrader-v2/real-signup-banner-dtrader-v2.tsx
+++ packages/core/src/App/Components/Layout/Header/dtrader-v2/real-signup-banner-dtrader-v2.tsx
@@ -15,33 +15,33 @@ type TRealSignupBannerDTraderV2 = {
     openRealAccount: () => void;
     is_eu?: boolean;
 };
-const RealSignupBannerDTraderV2 = ({ openRealAccount, is_eu }: TRealSignupBannerDTraderV2) => (
-    <button className='banner__container' onClick={openRealAccount}>
-        <div className='banner'>
-            <div
-                className={classNames('banner__currency', {
-                    'banner__currency--is-eu': is_eu,
-                })}
-            >
-                {is_eu ? (
-                    <React.Fragment>
-                        <CurrencyEurIcon iconSize='md' className='icon-1' />
-                        <CurrencyUsdIcon iconSize='md' className='icon-2' />
-                    </React.Fragment>
-                ) : (
-                    <React.Fragment>
-                        <CurrencyBtcIcon iconSize='md' className='icon-1' />
-                        <CurrencyEthIcon iconSize='md' className='icon-2' />
-                        <CurrencyUsdIcon iconSize='md' className='icon-3' />
-                    </React.Fragment>
-                )}
+const RealSignupBannerDTraderV2 = ({ openRealAccount, is_eu }: TRealSignupBannerDTraderV2) => {
+    const banner_icons = {
+        eu: [CurrencyEurIcon, CurrencyUsdIcon],
+        row: [CurrencyBtcIcon, CurrencyEthIcon, CurrencyUsdIcon],
+    };
+
+    return (
+        <button className='banner__container' onClick={openRealAccount}>
+            <div className='banner'>
+                <div
+                    className={classNames('banner__currency', {
+                        'banner__currency--is-eu': is_eu,
+                    })}
+                >
+                    {is_eu
+                        ? banner_icons.eu.map((Icon, idx) => <Icon key={idx} iconSize='md' className='banner__icon' />)
+                        : banner_icons.row.map((Icon, idx) => (
+                              <Icon key={idx} iconSize='md' className='banner__icon' />
+                          ))}
+                </div>
+                <Text size='md' className='banner__text'>
+                    <Localize i18n_default_text='Open a real account now' />
+                </Text>
+                <LabelPairedChevronRightMdRegularIcon className='banner__arrow' />
             </div>
-            <Text size='md' className='banner__text'>
-                <Localize i18n_default_text='Open a real account now' />
-            </Text>
-            <LabelPairedChevronRightMdRegularIcon className='banner__arrow' />
-        </div>
-    </button>
-);
+        </button>
+    );
+};
 
 export default RealSignupBannerDTraderV2;

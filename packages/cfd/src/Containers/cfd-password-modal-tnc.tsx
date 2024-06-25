5de4bd446ce24fa5fae3c3daea59e1d4bf8565cf
hasan-deriv <abuhasan@deriv.com>
2024-06-25 13:17:08 +0800

    fix: tnc to bottom

--- packages/cfd/src/Containers/cfd-password-modal-tnc.tsx
+++ packages/cfd/src/Containers/cfd-password-modal-tnc.tsx
@@ -4,20 +4,22 @@ import { observer, useStore } from '@deriv/stores';
 import CfdPasswordModalInfo from './cfd-password-modal-info';
 import JurisdictionCheckBox from './jurisdiction-modal/jurisdiction-modal-checkbox';
 import { useCfdStore } from '../Stores/Modules/CFD/Helpers/useCfdStores';
+import classNames from 'classnames';
 
 type CfdPasswordModalTncProps = {
     platform: typeof CFD_PLATFORMS[keyof typeof CFD_PLATFORMS];
     checked: boolean;
     onCheck: () => void;
+    className?: string;
 };
 
-const CfdPasswordModalTnc = observer(({ platform, checked, onCheck }: CfdPasswordModalTncProps) => {
+const CfdPasswordModalTnc = observer(({ platform, checked, onCheck, className }: CfdPasswordModalTncProps) => {
     const { client } = useStore();
     const { should_restrict_vanuatu_account_creation, should_restrict_bvi_account_creation } = client;
     const { jurisdiction_selected_shortcode } = useCfdStore();
 
     return (
-        <React.Fragment>
+        <div className={classNames('cfd-password-modal-tnc', className)}>
             <CfdPasswordModalInfo
                 jurisdiction_selected_shortcode={jurisdiction_selected_shortcode}
                 platform={getCFDPlatformLabel(platform)}
@@ -31,7 +33,7 @@ const CfdPasswordModalTnc = observer(({ platform, checked, onCheck }: CfdPasswor
                 should_restrict_bvi_account_creation={should_restrict_bvi_account_creation}
                 should_restrict_vanuatu_account_creation={should_restrict_vanuatu_account_creation}
             />
-        </React.Fragment>
+        </div>
     );
 });
 

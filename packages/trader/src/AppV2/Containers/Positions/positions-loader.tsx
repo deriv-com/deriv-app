37d02bbd2a4cd18e01b8ae2d039c8bbec2de0382
maryia-deriv <maryia@binary.com>
2024-06-25 18:11:09 +0300

    refactor: isolate ContractCardLoader

--- packages/trader/src/AppV2/Containers/Positions/positions-loader.tsx
+++ packages/trader/src/AppV2/Containers/Positions/positions-loader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Skeleton, VARIANT } from 'AppV2/Components/Skeleton';
+import { Skeleton } from 'AppV2/Components/Skeleton';
+import { ContractCardLoader } from 'AppV2/Components/ContractCard';
 
 type TPositionsLoaderProps = {
     isClosedTab?: boolean;
@@ -13,30 +14,15 @@ const PositionsLoader = ({ isClosedTab }: TPositionsLoaderProps) => {
                     return <Skeleton key={idx} width='144px' height='32px' />;
                 })}
             </div>
-            <div className='skeleton-pnl-or-date'>
+            <div className='skeleton-total-pnl-or-date'>
                 {Array.from(new Array(isClosedTab ? 1 : 2)).map((_, idx) => {
                     return <Skeleton key={idx} width={idx === 0 ? '112px' : '68px'} height='22px' />;
                 })}
             </div>
             <div className='skeleton-contract-card-list'>
-                {Array.from(new Array(7)).map((_, idx) => {
-                    return (
-                        <div key={idx} className='skeleton-contract-card'>
-                            <div className='details'>
-                                <Skeleton variant={VARIANT.ICON} />
-                                <div className='title'>
-                                    <Skeleton width='88px' height='18px' />
-                                    <Skeleton width='128px' height='18px' />
-                                </div>
-                                <Skeleton className='stake' width='56px' height='18px' />
-                            </div>
-                            <div className='status-and-profit'>
-                                <Skeleton width='96px' height='24px' />
-                                <Skeleton width='72px' height='22px' />
-                            </div>
-                        </div>
-                    );
-                })}
+                {Array.from(new Array(7)).map((_, idx) => (
+                    <ContractCardLoader key={idx} />
+                ))}
             </div>
         </div>
     );

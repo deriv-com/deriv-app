c1f2e6e78dfe6e979ce7141ffa01023652998058
sergei-deriv <sergei@deriv.com>
2024-06-25 10:58:57 +0300

    feat: move ubuntu font import to app.scss

--- packages/appstore/src/components/modals/deriv-go-modal/deriv-go-modal.tsx
+++ packages/appstore/src/components/modals/deriv-go-modal/deriv-go-modal.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { observer, useStore } from '@deriv/stores';
 import { DerivGoModalResponsive } from './deriv-go-modal-responsive/deriv-go-modal-responsive';
 import { DerivGoModalDesktop } from './deriv-go-modal-desktop/deriv-go-modal-desktop';
-import './deriv-go-modal.scss';
 
 const DerivGoModal = observer(() => {
     const { ui } = useStore();

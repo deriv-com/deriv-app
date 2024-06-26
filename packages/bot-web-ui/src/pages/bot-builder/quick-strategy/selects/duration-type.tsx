f4446c503c35dbc4b02923a069fb4543a8917082
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2024-06-26 10:58:00 +0300

    fix: issue with changing duration type

--- packages/bot-web-ui/src/pages/bot-builder/quick-strategy/selects/duration-type.tsx
+++ packages/bot-web-ui/src/pages/bot-builder/quick-strategy/selects/duration-type.tsx
@@ -13,6 +13,7 @@ type TDurationUnit = {
 
 const DurationUnit: React.FC<TDurationUnit> = ({ attached }: TDurationUnit) => {
     const [list, setList] = React.useState<TDurationUnitItem[]>([]);
+    const [prevSymbol, setPrevSymbol] = React.useState('');
     const { quick_strategy } = useDBotStore();
     const { setValue, setCurrentDurationMinMax } = quick_strategy;
     const { setFieldValue, validateForm, values } = useFormikContext<TFormData>();
@@ -32,18 +33,16 @@ const DurationUnit: React.FC<TDurationUnit> = ({ attached }: TDurationUnit) => {
                 setList(duration_units);
                 const selected = values?.durationtype;
                 const has_selected = duration_units?.some(duration => duration.value === selected);
-                if (!has_selected) {
-                    setFieldValue?.('durationtype', durations?.[0]?.unit);
-                    setFieldValue?.('duration', durations?.[0]?.min);
-                    setValue('durationtype', durations?.[0]?.unit ?? '');
+                if (!has_selected || prevSymbol !== symbol) {
                     setCurrentDurationMinMax(durations?.[0]?.min, durations?.[0]?.max);
+                    setFieldValue?.('durationtype', durations?.[0]?.unit, true);
+                    setFieldValue?.('duration', durations?.[0]?.min, true);
+                    setValue('durationtype', durations?.[0]?.unit ?? '');
                 } else {
                     const duration = duration_units?.find((duration: TDurationUnitItem) => duration.value === selected);
-                    // Keep these lines to ensure the duration resets to default (e.g., for changing from GOLD/USD to GBP/NZD).
-                    setFieldValue?.('duration', duration?.min);
-                    setValue('duration', duration?.min ?? 0);
                     setCurrentDurationMinMax(duration?.min, duration?.max);
                 }
+                setPrevSymbol(symbol as string);
             };
             getDurationUnits();
         }

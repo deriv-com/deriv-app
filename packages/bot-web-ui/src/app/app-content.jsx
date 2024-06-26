9d119d2a67598f29a919256e4f513c4db3e9e7ec
shafin-deriv <shafin@regentmarkets.com>
2024-06-26 19:41:57 +0800

    chore: try to regenerate the issue in test link

--- packages/bot-web-ui/src/app/app-content.jsx
+++ packages/bot-web-ui/src/app/app-content.jsx
@@ -52,6 +52,7 @@ const AppContent = observer(() => {
 
     function checkIfApiInitialized() {
         init_api_interval.current = setInterval(() => {
+            // console.log('test api', api_base?.api);
             if (api_base?.api) {
                 clearInterval(init_api_interval.current);
                 // Listen for proposal open contract messages to check

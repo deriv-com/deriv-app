c5dca39b0fe408e34265f253de74bfaa3aa92442
maryia-deriv <maryia@binary.com>
2024-06-27 12:37:55 +0300

    feat: add more blocks to ContractDetailsLoader

--- packages/trader/src/AppV2/Components/Skeleton/skeleton.tsx
+++ packages/trader/src/AppV2/Components/Skeleton/skeleton.tsx
@@ -5,6 +5,11 @@ type TSkeletonProps = {
     animated?: boolean;
     className?: string;
     height?: string;
+    paragraph?: {
+        gap?: string;
+        rows: number;
+        width?: string;
+    };
     variant?: 'icon';
     width?: string;
 };
@@ -13,7 +18,7 @@ export const VARIANT = {
     ICON: 'icon',
 } as const;
 
-const Skeleton = ({ animated = true, className, height, width, variant }: TSkeletonProps) => {
+const Skeleton = ({ animated = true, className, height, paragraph, width, variant }: TSkeletonProps) => {
     const getDefaultSize = () => (variant === VARIANT.ICON ? '32px' : '100%');
 
     const style: React.CSSProperties = {
@@ -21,6 +26,24 @@ const Skeleton = ({ animated = true, className, height, width, variant }: TSkele
         height: height ?? getDefaultSize(),
     };
 
+    if (paragraph?.rows) {
+        return (
+            <div className='skeleton-paragraph' style={{ gap: paragraph.gap ?? '8px' }}>
+                {Array.from(new Array(paragraph.rows)).map((_, idx) => {
+                    return (
+                        <div
+                            key={idx}
+                            className={clsx(className, 'skeleton', animated && 'animated')}
+                            style={{
+                                width: idx === paragraph.rows - 1 ? '96px' : paragraph.width ?? style.width,
+                                height: style.height,
+                            }}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
     return <div className={clsx(className, 'skeleton', animated && 'animated')} style={style} />;
 };
 
--- packages/trader/src/AppV2/Containers/ContractDetails/contract-details-loader.tsx
+++ packages/trader/src/AppV2/Containers/ContractDetails/contract-details-loader.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { ContractCardLoader } from 'AppV2/Components/ContractCard';
+import { Skeleton } from 'AppV2/Components/Skeleton';
 
 const ContractDetailsLoader = () => {
     return (
         <div className='contract-details-loader'>
             <ContractCardLoader />
+            <div className='skeleton-box__chart'>
+                <Skeleton height='344px' />
+            </div>
+            <div className='skeleton-box__details'>
+                <Skeleton width='136px' height='24px' />
+                <div>
+                    {Array.from(new Array(4)).map((_, idx) => {
+                        return (
+                            <div key={idx} className='skeleton-box__details-row'>
+                                <Skeleton width='88px' height='22px' />
+                                <Skeleton width='128px' height='22px' />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className='skeleton-box__details'>
+                <Skeleton width='136px' height='24px' />
+                <Skeleton paragraph={{ rows: 2 }} height='18px' />
+            </div>
+            <div className='skeleton-box__details'>
+                <Skeleton width='136px' height='24px' />
+                <Skeleton paragraph={{ rows: 2 }} height='18px' />
+            </div>
         </div>
     );
 };

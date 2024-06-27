a216c8c1da964ca9d45ea288d45726862526886f
sergei-deriv <sergei@deriv.com>
2024-06-27 14:25:00 +0300

    feat: add check for wallets and dont hide landsacpe blocker for this kind of accounts

--- packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
+++ packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { isDisabledLandscapeBlockerRoute, isTabletOs, routes } from '@deriv/shared';
+import { observer } from '@deriv/stores';
+import { useStoreWalletAccountsList } from '@deriv/hooks';
 import LandscapeBlockerSvg from 'Assets/SvgComponents/settings/landscape-blocker.svg';
 import './landscape-blocker.scss';
 
-const LandscapeBlocker = () => {
+const LandscapeBlocker = observer(() => {
+    // need to check for wallet account and don't hide landscape blocker for users migrated to wallets
+    const { has_wallet } = useStoreWalletAccountsList();
     const location = useLocation();
     const pathname = location?.pathname;
     const is_hidden_landscape_blocker = isDisabledLandscapeBlockerRoute(pathname);
     const shouldShowDtraderTabletView = pathname === routes.trade && isTabletOs;
 
-    if (is_hidden_landscape_blocker || shouldShowDtraderTabletView) return null;
+    if (!has_wallet && (is_hidden_landscape_blocker || shouldShowDtraderTabletView)) return null;
 
     return (
         <div id='landscape_blocker' className='landscape-blocker'>
@@ -27,6 +31,6 @@ const LandscapeBlocker = () => {
             </div>
         </div>
     );
-};
+});
 
 export default LandscapeBlocker;

8caec5e27b5f1e285e52c79bb5ac4fb7737ce2fa
Likhith Kolayari <likhith@regentmarkets.com>
2024-06-27 09:25:14 +0400

    fix: prettier issue

--- packages/core/src/App/Components/Layout/Footer/__tests__/toggle-language-settings.spec.tsx
+++ packages/core/src/App/Components/Layout/Footer/__tests__/toggle-language-settings.spec.tsx
@@ -6,18 +6,16 @@ import { ToggleLanguageSettings } from '../toggle-language-settings';
 
 jest.mock('../../../../Containers/SettingsModal/settings-language', () => jest.fn(() => <div>LanguageSettings</div>));
 
-jest.mock('@deriv-com/translations',()=>({
+jest.mock('@deriv-com/translations', () => ({
     useTranslations: jest.fn().mockReturnValue({
         currentLang: 'EN',
         localize: jest.fn().mockImplementation(key => key),
         switchLanguage: jest.fn(),
     }),
-    Localize: jest.fn().mockImplementation(({i18n_default_text})=>i18n_default_text),
-
+    Localize: jest.fn().mockImplementation(({ i18n_default_text }) => i18n_default_text),
 }));
 
 describe('ToggleLanguageSettings Component', () => {
-
     it('should has "ic-settings-active" class when "is_settings_visible" is true', () => {
         const mockRootStore = mockStore({ ui: { is_language_settings_modal_on: true } });
 

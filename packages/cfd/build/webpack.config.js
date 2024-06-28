4e065a1b46345ad04ea6a764fd6e6c3f611cd659
hasan-deriv <abuhasan@deriv.com>
2024-06-28 16:13:50 +0800

    fix: added classnames to external

--- packages/cfd/build/webpack.config.js
+++ packages/cfd/build/webpack.config.js
@@ -69,6 +69,7 @@ module.exports = function (env) {
                 '@deriv/stores': '@deriv/stores',
                 formik: 'formik',
                 'react-transition-group': 'react-transition-group',
+                classnames: 'classnames',
             },
             /^@deriv\/shared\/.+$/,
             /^@deriv\/components\/.+$/,

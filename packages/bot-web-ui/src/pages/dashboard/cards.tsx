2f8084942f3319fd7bbb982f2b10ebad8f0e3c36
shafin-deriv <shafin@regentmarkets.com>
2024-07-01 13:30:04 +0800

    chore: fix tour issue

--- packages/bot-web-ui/src/pages/dashboard/cards.tsx
+++ packages/bot-web-ui/src/pages/dashboard/cards.tsx
@@ -28,24 +28,32 @@ const Cards = observer(({ is_mobile, has_dashboard_strategies }: TCardProps) => 
     const { toggleLoadModal, setActiveTabIndex } = load_modal;
     const { setFormVisibility } = quick_strategy;
 
-    const openGoogleDriveDialog = () => {
-        setActiveTab(DBOT_TABS.BOT_BUILDER);
-        // Added the setTimeout to maintain sequence as tab change was contradicting with load modal open
-        const timerId = setTimeout(() => {
-            toggleLoadModal();
-            setActiveTabIndex(is_mobile ? 1 : 2);
-            clearTimeout(timerId);
+    const switchTabOnLoadModalOpen = () => {
+        // Added the setInterval to maintain sequence as tab change was contradicting with load modal open
+        const timerId = setInterval(() => {
+            const load_modal_element = document.getElementsByClassName('load-strategy__wrapper');
+            if (load_modal_element.length > 0 && load_modal_element[0]) {
+                const element = load_modal_element[0];
+                const style = window.getComputedStyle(element);
+                // Only switch tab when the load modal is fully visible
+                if (style?.opacity === '1') {
+                    setActiveTab(DBOT_TABS.BOT_BUILDER);
+                    clearInterval(timerId);
+                }
+            }
         }, 0);
     };
 
+    const openGoogleDriveDialog = () => {
+        toggleLoadModal();
+        setActiveTabIndex(is_mobile ? 1 : 2);
+        switchTabOnLoadModalOpen();
+    };
+
     const openFileLoader = () => {
-        setActiveTab(DBOT_TABS.BOT_BUILDER);
-        // Added the setTimeout to maintain sequence as tab change was contradicting with load modal open
-        const timerId = setTimeout(() => {
-            toggleLoadModal();
-            setActiveTabIndex(is_mobile ? 0 : 1);
-            clearTimeout(timerId);
-        }, 0);
+        toggleLoadModal();
+        setActiveTabIndex(is_mobile ? 0 : 1);
+        switchTabOnLoadModalOpen();
     };
 
     const actions: TCardArray[] = [

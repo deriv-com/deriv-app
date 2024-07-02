431f8e551a2b542f850a23f2caad115e6b1ac8c3
kate-deriv <kate@regentmarkets.com>
2024-07-02 16:36:56 +0300

    fix: bug with closing modal during dragging video

--- packages/trader/src/AppV2/Components/Guide/Description/video-preview.tsx
+++ packages/trader/src/AppV2/Components/Guide/Description/video-preview.tsx
@@ -3,33 +3,15 @@ import { Stream } from '@cloudflare/stream-react';
 import { CaptionText } from '@deriv-com/quill-ui';
 import { LabelPairedPlayMdFillIcon } from '@deriv/quill-icons';
 import { Localize, localize } from '@deriv/translations';
-import { observer, useStore } from '@deriv/stores';
-import { clickAndKeyEventHandler } from '@deriv/shared';
-import { CONTRACT_LIST, getDescriptionVideoId } from 'AppV2/Utils/trade-types-utils';
-import VideoPlayer from 'App/Components/Elements/VideoPlayer';
+import { CONTRACT_LIST } from 'AppV2/Utils/trade-types-utils';
 
 type TVideoPreview = {
     contract_type: string;
+    toggleVideoPlayer: () => void;
+    video_src: string;
 };
 
-const VideoPreview = observer(({ contract_type }: TVideoPreview) => {
-    const [is_video_player_opened, setIsVideoPlayerOpened] = React.useState(false);
-    const modal_ref = React.useRef<HTMLDialogElement>(null);
-
-    const { ui } = useStore();
-    const { is_dark_mode_on } = ui;
-
-    const video_src = getDescriptionVideoId(contract_type, is_dark_mode_on);
-
-    const toggleVideoPlayer = (e?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => {
-        clickAndKeyEventHandler(() => setIsVideoPlayerOpened(!is_video_player_opened), e);
-    };
-
-    React.useEffect(() => {
-        if (modal_ref.current) is_video_player_opened ? modal_ref.current.showModal() : modal_ref.current.close();
-    }, [is_video_player_opened]);
-
-    // TODO: temporary we have no video for Turbos, so returning null until it will be ready
+const VideoPreview = ({ contract_type, toggleVideoPlayer, video_src }: TVideoPreview) => {
     if (contract_type === CONTRACT_LIST.TURBOS) return null;
 
     return (
@@ -59,26 +41,8 @@ const VideoPreview = observer(({ contract_type }: TVideoPreview) => {
                     <Localize i18n_default_text='Watch this video to learn about this trade type.' />
                 </CaptionText>
             </div>
-            {is_video_player_opened && (
-                <dialog
-                    ref={modal_ref}
-                    onClick={toggleVideoPlayer}
-                    onKeyDown={toggleVideoPlayer}
-                    className='modal-player'
-                >
-                    <div onClick={e => e.stopPropagation()} onKeyDown={e => e.stopPropagation()}>
-                        <VideoPlayer
-                            className='modal-player__wrapper'
-                            data_testid='dt_video_player'
-                            height='206px'
-                            is_mobile
-                            src={video_src}
-                        />
-                    </div>
-                </dialog>
-            )}
         </div>
     );
-});
+};
 
 export default VideoPreview;
--- packages/trader/src/AppV2/Components/Guide/decription-modal.tsx
+++ packages/trader/src/AppV2/Components/Guide/decription-modal.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { ActionSheet, Heading, Chip, Text } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
-import { AVAILABLE_CONTRACTS, CONTRACT_LIST } from 'AppV2/Utils/trade-types-utils';
+import { observer, useStore } from '@deriv/stores';
+import { clickAndKeyEventHandler } from '@deriv/shared';
+import { AVAILABLE_CONTRACTS, CONTRACT_LIST, getDescriptionVideoId } from 'AppV2/Utils/trade-types-utils';
+import VideoPlayer from 'App/Components/Elements/VideoPlayer';
 import TradeDescription from './Description/trade-description';
 import VideoPreview from './Description/video-preview';
 
@@ -13,65 +16,100 @@ type TDescriptionModal = {
     selected_contract_type: string;
 };
 
-const DescriptionModal = ({
-    is_open,
-    onClose,
-    onChipSelect,
-    onTermClick,
-    selected_contract_type,
-}: TDescriptionModal) => {
-    //TODO: temporary, until we'll have ordered list, coming from contract type selection
-    const order = [
-        CONTRACT_LIST['RISE/FALL'],
-        CONTRACT_LIST.ACCUMULATORS,
-        CONTRACT_LIST.MULTIPLIERS,
-        CONTRACT_LIST.VANILLAS,
-        CONTRACT_LIST.TURBOS,
-        CONTRACT_LIST['HIGHER/LOWER'],
-        CONTRACT_LIST['TOUCH/NO TOUCH'],
-        CONTRACT_LIST['MATCHES/DIFFERS'],
-        CONTRACT_LIST['EVEN/ODD'],
-        CONTRACT_LIST['OVER/UNDER'],
-    ];
+const DescriptionModal = observer(
+    ({ is_open, onClose, onChipSelect, onTermClick, selected_contract_type }: TDescriptionModal) => {
+        const [is_video_player_opened, setIsVideoPlayerOpened] = React.useState(false);
+        const modal_ref = React.useRef<HTMLDialogElement>(null);
 
-    const ordered_contract_list = [...AVAILABLE_CONTRACTS].sort(
-        (a, b) => order.findIndex(item => item === a.id) - order.findIndex(item => item === b.id)
-    );
+        const { ui } = useStore();
+        const { is_dark_mode_on } = ui;
 
-    return (
-        <ActionSheet.Root isOpen={is_open} onClose={onClose} position='left'>
-            <ActionSheet.Portal shouldCloseOnDrag>
-                <ActionSheet.Content className='guide__wrapper'>
-                    <Heading.H4 className='guide__title'>
-                        <Localize i18n_default_text='Trade types' />
-                    </Heading.H4>
-                    <div className='guide__menu'>
-                        {ordered_contract_list.map(({ tradeType, id }) => (
-                            <Chip.Selectable
-                                key={id}
-                                onChipSelect={onChipSelect}
-                                selected={id === selected_contract_type}
-                            >
-                                <Text size='sm'>{tradeType}</Text>
-                            </Chip.Selectable>
-                        ))}
-                    </div>
-                    <div className='guide__contract-description' key={selected_contract_type}>
-                        <TradeDescription contract_type={selected_contract_type} onTermClick={onTermClick} />
-                        <VideoPreview contract_type={selected_contract_type} />
-                    </div>
-                </ActionSheet.Content>
-                <ActionSheet.Footer
-                    alignment='vertical'
-                    primaryAction={{
-                        content: <Localize i18n_default_text='Got it' />,
-                        onAction: onClose,
-                    }}
-                    className='guide__button'
-                />
-            </ActionSheet.Portal>
-        </ActionSheet.Root>
-    );
-};
+        const video_src = getDescriptionVideoId(selected_contract_type, is_dark_mode_on);
+        //TODO: temporary, until we'll have ordered list, coming from contract type selection
+        const order = [
+            CONTRACT_LIST['RISE/FALL'],
+            CONTRACT_LIST.ACCUMULATORS,
+            CONTRACT_LIST.MULTIPLIERS,
+            CONTRACT_LIST.VANILLAS,
+            CONTRACT_LIST.TURBOS,
+            CONTRACT_LIST['HIGHER/LOWER'],
+            CONTRACT_LIST['TOUCH/NO TOUCH'],
+            CONTRACT_LIST['MATCHES/DIFFERS'],
+            CONTRACT_LIST['EVEN/ODD'],
+            CONTRACT_LIST['OVER/UNDER'],
+        ];
+
+        const toggleVideoPlayer = (e?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => {
+            clickAndKeyEventHandler(() => setIsVideoPlayerOpened(!is_video_player_opened), e);
+        };
+
+        const ordered_contract_list = [...AVAILABLE_CONTRACTS].sort(
+            (a, b) => order.findIndex(item => item === a.id) - order.findIndex(item => item === b.id)
+        );
+
+        React.useEffect(() => {
+            if (modal_ref.current) is_video_player_opened ? modal_ref.current.showModal() : modal_ref.current.close();
+        }, [is_video_player_opened]);
+
+        return (
+            <React.Fragment>
+                <ActionSheet.Root isOpen={is_open} onClose={onClose} position='left'>
+                    <ActionSheet.Portal shouldCloseOnDrag>
+                        <ActionSheet.Content className='guide__wrapper'>
+                            <Heading.H4 className='guide__title'>
+                                <Localize i18n_default_text='Trade types' />
+                            </Heading.H4>
+                            <div className='guide__menu'>
+                                {ordered_contract_list.map(({ tradeType, id }) => (
+                                    <Chip.Selectable
+                                        key={id}
+                                        onChipSelect={onChipSelect}
+                                        selected={id === selected_contract_type}
+                                    >
+                                        <Text size='sm'>{tradeType}</Text>
+                                    </Chip.Selectable>
+                                ))}
+                            </div>
+                            <div className='guide__contract-description' key={selected_contract_type}>
+                                <TradeDescription contract_type={selected_contract_type} onTermClick={onTermClick} />
+                                <VideoPreview
+                                    contract_type={selected_contract_type}
+                                    toggleVideoPlayer={toggleVideoPlayer}
+                                    video_src={video_src}
+                                />
+                            </div>
+                        </ActionSheet.Content>
+                        <ActionSheet.Footer
+                            alignment='vertical'
+                            primaryAction={{
+                                content: <Localize i18n_default_text='Got it' />,
+                                onAction: onClose,
+                            }}
+                            className='guide__button'
+                        />
+                    </ActionSheet.Portal>
+                </ActionSheet.Root>
+                {is_video_player_opened && (
+                    <dialog
+                        ref={modal_ref}
+                        onClick={toggleVideoPlayer}
+                        onKeyDown={toggleVideoPlayer}
+                        className='modal-player'
+                    >
+                        <div onClick={e => e.stopPropagation()} onKeyDown={e => e.stopPropagation()}>
+                            <VideoPlayer
+                                className='modal-player__wrapper'
+                                data_testid='dt_video_player'
+                                height='206px'
+                                is_mobile
+                                src={video_src}
+                            />
+                        </div>
+                    </dialog>
+                )}
+            </React.Fragment>
+        );
+    }
+);
 
 export default DescriptionModal;

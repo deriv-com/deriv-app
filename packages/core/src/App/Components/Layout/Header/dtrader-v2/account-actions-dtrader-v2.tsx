b20b70dd351e06e8ef0c3417d8a1668fbbf45aa3
maryia-deriv <maryia@binary.com>
2024-07-04 17:23:27 +0300

    chore: update notifications icon and bottom loader in closed tab

--- packages/core/src/App/Components/Layout/Header/dtrader-v2/account-actions-dtrader-v2.tsx
+++ packages/core/src/App/Components/Layout/Header/dtrader-v2/account-actions-dtrader-v2.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import 'Sass/app/_common/components/account-switcher-dtrader-v2.scss';
-import { LabelPairedBellLgRegularIcon, StandaloneBellRegularIcon } from '@deriv/quill-icons';
 import { formatMoney, moduleLoader } from '@deriv/shared';
 import { Badge } from '@deriv-com/quill-ui';
-import { Skeleton, VARIANT } from '@deriv/components';
+import NotificationsIconDTraderV2 from './notifications-icon-dtrader-v2';
 
 type TAccountActionsDTraderV2 = {
     acc_switcher_disabled_message?: string;
@@ -53,47 +52,39 @@ const AccountActionsDTraderV2 = ({
     is_virtual,
     notifications_count,
     toggleAccountsDialog,
-}: TAccountActionsDTraderV2) => {
-    const NotificationsMissingOrLoading = () =>
-        account_switcher_title ? (
-            <StandaloneBellRegularIcon iconSize='md' className='notifications__icon' />
-        ) : (
-            <Skeleton variant={VARIANT.ICON} />
-        );
-    return (
-        <React.Suspense fallback={<div />}>
-            <AccountInfoDTraderV2
-                acc_switcher_disabled_message={acc_switcher_disabled_message}
-                account_switcher_title={account_switcher_title}
-                account_type={account_type}
-                balance={typeof balance === 'undefined' ? balance : formatMoney(currency, balance, true)}
-                currency={currency}
-                is_disabled={is_acc_switcher_disabled}
-                is_eu={is_eu}
-                is_virtual={is_virtual}
-                is_dialog_on={is_acc_switcher_on}
-                toggleDialog={toggleAccountsDialog}
-            />
-            {has_notifications_icon && (
-                <div className='notifications__wrapper'>
-                    {notifications_count ? (
-                        <Badge
-                            color='danger'
-                            contentSize='sm'
-                            label={notifications_count.toString()}
-                            position='top-right'
-                            size='sm'
-                            variant='notification'
-                        >
-                            <StandaloneBellRegularIcon iconSize='md' className='notifications__icon' />
-                        </Badge>
-                    ) : (
-                        <NotificationsMissingOrLoading />
-                    )}
-                </div>
-            )}
-        </React.Suspense>
-    );
-};
+}: TAccountActionsDTraderV2) => (
+    <React.Suspense fallback={<div />}>
+        <AccountInfoDTraderV2
+            acc_switcher_disabled_message={acc_switcher_disabled_message}
+            account_switcher_title={account_switcher_title}
+            account_type={account_type}
+            balance={typeof balance === 'undefined' ? balance : formatMoney(currency, balance, true)}
+            currency={currency}
+            is_disabled={is_acc_switcher_disabled}
+            is_eu={is_eu}
+            is_virtual={is_virtual}
+            is_dialog_on={is_acc_switcher_on}
+            toggleDialog={toggleAccountsDialog}
+        />
+        {has_notifications_icon && (
+            <div className='notifications__wrapper'>
+                {notifications_count ? (
+                    <Badge
+                        color='danger'
+                        contentSize='sm'
+                        label={notifications_count.toString()}
+                        position='top-right'
+                        size='sm'
+                        variant='notification'
+                    >
+                        <NotificationsIconDTraderV2 />
+                    </Badge>
+                ) : (
+                    <NotificationsIconDTraderV2 is_loading={!account_switcher_title} />
+                )}
+            </div>
+        )}
+    </React.Suspense>
+);
 
 export default AccountActionsDTraderV2;

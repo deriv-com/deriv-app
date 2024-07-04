e8f28fed5836216043873ae55a7668b0fbdd0de5
sergei-deriv <sergei@deriv.com>
2024-07-04 18:49:56 +0300

    feat: need to think about updating data flow

--- packages/core/src/App/Containers/OneTimeDepositModal/one-time-deposit-modal.tsx
+++ packages/core/src/App/Containers/OneTimeDepositModal/one-time-deposit-modal.tsx
@@ -1,39 +1,108 @@
 import React from 'react';
 import { useDevice } from '@deriv-com/ui';
+import { useInvalidateQuery, useTransactions } from '@deriv/api';
 import { MobileFullPageModal, Modal, Text } from '@deriv/components';
 import { useHasMFAccountDeposited, useCryptoTransactions, useCurrentCurrencyConfig } from '@deriv/hooks';
 import { observer, useStore } from '@deriv/stores';
 import { Localize } from '@deriv/translations';
 import DepositFiatIframe from '@deriv/cashier/src/modules/deposit-fiat/components/deposit-fiat-iframe/deposit-fiat-iframe';
 import useLiveChat from 'App/Components/Elements/LiveChat/use-livechat';
+import DepositNowOrLaterModal from '../Modals/deposit-now-or-later-modal';
 
 const OneTimeDepositModal = observer(() => {
     const { isDesktop } = useDevice();
     const { client, ui } = useStore();
-    const { loginid, is_cr_account, is_mf_account, is_logged_in, currency } = client;
-    const { should_show_one_time_deposit_modal, setShouldShowOneTimeDepositModal, toggleAccountSuccessModal } = ui;
+    const { loginid, is_cr_account, is_mf_account, is_logged_in } = client;
+    const {
+        should_show_one_time_deposit_modal,
+        setShouldShowOneTimeDepositModal,
+        toggleAccountSuccessModal,
+        setShouldShowDepositNowOrLaterModal,
+    } = ui;
     const liveChat = useLiveChat(false, loginid);
     const currency_config = useCurrentCurrencyConfig();
     // const { has_mf_account_deposited } = useHasMFAccountDeposited();
-    const has_mf_account_deposited = false;
-    const { data: crypto_transactions } = useCryptoTransactions(
+    // const has_mf_account_deposited = false;
+    const { data: crypto_transactions, has_transactions } = useCryptoTransactions(
         is_logged_in && is_cr_account && currency_config?.is_crypto
     );
+    // const { has_deposit, updateStatement } = useStatement();
 
-    React.useEffect(() => {
-        if (has_mf_account_deposited) {
-            onCloseModal();
-        }
-    }, [has_mf_account_deposited]);
+    const { data, dataUpdatedAt, fetchStatus, status, isFetching, isLoading } = useTransactions('deposit');
+    const [is_account_deposited, setIsAccountDeposited] = React.useState(false);
+    const [is_trying_to_close, setIsTryingToClose] = React.useState(false);
+    const [data_updated, setDataUpdated] = React.useState(0);
+
+    const invalidate = useInvalidateQuery();
+
+    // console.log(
+    //     // 'dataUpdatedAt = ',
+    //     // dataUpdatedAt,
+    //     // ', data_updated = ',
+    //     // data_updated,
+    //     ', fetchStatus = ',
+    //     fetchStatus,
+    //     ', status = ',
+    //     status,
+    //     ', isFetching = ',
+    //     isFetching,
+    //     ', isLoading = ',
+    //     isLoading
+    // );
+
+    // React.useEffect(() => {
+    //     if (has_mf_account_deposited) {
+    //         onCloseModal();
+    //     }
+    // }, [has_mf_account_deposited]);
+
+    // console.log('data = ', data);
 
     const onLiveChatClick = () => {
         liveChat.widget?.call('maximize');
     };
 
-    const onCloseModal = () => {
+    const onCloseModal = async () => {
+        // setShouldShowOneTimeDepositModal(false);
+        // const res = await updateStatement();
+        // console.log('res = ', res);
+        // setDataUpdated(dataUpdatedAt);
+        invalidate('statement');
+
+        setIsTryingToClose(true);
+        setShouldShowDepositNowOrLaterModal(true);
+
+        return;
+
+        if (!is_account_deposited) {
+            // show confirm modal
+
+            return;
+        }
+
         setShouldShowOneTimeDepositModal(false);
-        toggleAccountSuccessModal();
+        if (is_mf_account) toggleAccountSuccessModal();
     };
+
+    // check the user already made a deposit
+    React.useEffect(() => {
+        // check statement
+        if (data?.length) {
+            setIsAccountDeposited(true);
+            return;
+        }
+
+        // check crypto transactions
+        if (is_cr_account && crypto_transactions && has_transactions) {
+            if (crypto_transactions?.some(tx => tx.is_deposit)) setIsAccountDeposited(true);
+        }
+    }, [crypto_transactions, has_transactions, setIsAccountDeposited]);
+
+    // check the flow when the user try to close the modal
+    // React.useEffect(() => {
+    //     if (is_trying_to_close) {
+    //     }
+    // }, [is_trying_to_close]);
 
     const getModalContent = () => (
         <div className='one-time-deposit-modal__content'>
@@ -87,6 +156,7 @@ const OneTimeDepositModal = observer(() => {
                     {getModalContent()}
                 </MobileFullPageModal>
             )}
+            <DepositNowOrLaterModal />
         </React.Fragment>
     );
 });

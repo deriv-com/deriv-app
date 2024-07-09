f4b5c30d6b42c9470894018f1eb42862efe3a8d6
Akmal Djumakhodjaev <akmal@binary.com>
2024-07-09 18:07:56 +0800

    fix: prettier

--- packages/trader/src/AppV2/Components/DraggableList/draggable-list-item.tsx
+++ packages/trader/src/AppV2/Components/DraggableList/draggable-list-item.tsx
@@ -22,12 +22,20 @@ const DraggableListItem: React.FC<DraggableListItemProps> = ({
     return (
         <div className='draggable-list-item'>
             <div className='draggable-list-item__left'>
-                <div className='draggable-list-item__left-icon' data-testid='dt_draggable_list_item_left_icon'  onClick={onLeftIconClick}>
+                <div
+                    className='draggable-list-item__left-icon'
+                    data-testid='dt_draggable_list_item_left_icon'
+                    onClick={onLeftIconClick}
+                >
                     {leftIcon || default_left_icon}
                 </div>
                 <div className='draggable-list-item__title'>{title}</div>
             </div>
-            <div className='draggable-list-item__icon' data-testid='dt_draggable_list_item_icon' onClick={onRightIconClick}>
+            <div
+                className='draggable-list-item__icon'
+                data-testid='dt_draggable_list_item_icon'
+                onClick={onRightIconClick}
+            >
                 {rightIcon || default_right_icon}
             </div>
         </div>

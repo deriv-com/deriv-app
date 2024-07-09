9b12e43dccdafd14ff3a7341a289eb60caccda45
Likhith Kolayari <likhith@regentmarkets.com>
2024-07-09 16:33:58 +0400

    chore: added testcases to validation schema

--- packages/account/src/Configs/__test__/user-profile-validation-config.spec.ts
+++ packages/account/src/Configs/__test__/user-profile-validation-config.spec.ts
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 import {
     getEmploymentAndTaxValidationSchema,
     getAddressDetailValidationSchema,
+    getPersonalDetailsBaseValidationSchema,
 } from '../user-profile-validation-config';
 import { ValidationConstants } from '@deriv-com/utils';
 
@@ -211,6 +212,70 @@ describe('getAddressDetailValidationSchema', () => {
         await expect(address_city.validate('a'.repeat(102))).rejects.toThrow('Only 99 characters, please.');
         await expect(address_city.validate('%_ASD')).rejects.toThrow(
             'Only letters, periods, hyphens, apostrophes, and spaces, please.'
+        );
+    });
+});
+
+describe('getPersonalDetailsBaseValidationSchema', () => {
+    let validation_schema: ReturnType<typeof getPersonalDetailsBaseValidationSchema>;
+
+    it('should validate a valid set of personal details for non-eu clients', async () => {
+        validation_schema = getPersonalDetailsBaseValidationSchema('svg');
+        const valid_values = {
+            first_name: 'John',
+            last_name: 'Doe',
+            phone: '+1234567890',
+            account_opening_reason: 'Investment',
+            date_of_birth: '1990-01-01',
+            place_of_birth: 'Germany',
+        };
+
+        expect(validation_schema.isValidSync(valid_values)).toBeTruthy();
+    });
+
+    it('should validate a valid set of personal details for eu clients', async () => {
+        validation_schema = getPersonalDetailsBaseValidationSchema('maltainvest');
+        const valid_values = {
+            salutation: 'Mr',
+            first_name: 'John',
+            last_name: 'Doe',
+            phone: '+1234567890',
+            account_opening_reason: 'Investment',
+            date_of_birth: '1990-01-01',
+            place_of_birth: 'Germany',
+            citizen: 'Germany',
+        };
+
+        expect(validation_schema.isValidSync(valid_values)).toBeTruthy();
+    });
+
+    it('should throw an error if salutation is not provided for maltainvest clients', () => {
+        validation_schema = getPersonalDetailsBaseValidationSchema('maltainvest');
+        const invalid_values = {
+            first_name: 'John',
+            last_name: 'Doe',
+            phone: '+1234567890',
+            account_opening_reason: 'Investment',
+            date_of_birth: '1990-01-01',
+            place_of_birth: 'Germany',
+            citizen: 'Germany',
+        };
+
+        try {
+            validation_schema.validateSync(invalid_values);
+        } catch (error: unknown) {
+            expect((error as Yup.ValidationError).message).toBe('Salutation is required.');
+        }
+    });
+
+    it('should throw an error if first name is not provided', async () => {
+        const { first_name } = getPersonalDetailsBaseValidationSchema('svg').fields;
+
+        expect(first_name.validate('John')).resolves.toBe('John');
+        expect(first_name.validate('')).rejects.toThrow('First name is required.');
+        // expect(first_name.validate('A')).rejects.toThrow('Letters, spaces, periods, hyphens, apostrophes only.');
+        expect(first_name.validate('ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKL')).rejects.toThrow(
+            'Enter no more than 50 characters.'
         );
     });
 });

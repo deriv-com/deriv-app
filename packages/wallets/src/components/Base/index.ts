5189992b85e4ee5acbb3bb81ba204689278bb61b
aizad-deriv <aizad@deriv.com>
2024-07-09 13:51:43 +0800

    chore: Remove tooltip component from Wallets package

--- packages/wallets/src/components/Base/index.ts
+++ packages/wallets/src/components/Base/index.ts
@@ -7,7 +7,6 @@ export * from './ModalStepWrapper';
 export * from './ModalWrapper';
 export * from './ProgressBar';
 export * from './Tabs';
-export * from './Tooltip';
 export * from './WalletAlertMessage';
 export * from './WalletButton';
 export * from './WalletButtonGroup';

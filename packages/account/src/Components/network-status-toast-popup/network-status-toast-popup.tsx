271e67062f4f896adc2c3e96976222c8148e2e6e
yauheni-deriv <yauheni@regentmarkets.com>
2024-07-10 11:57:35 +0300

    chore: move settimeout to useeffect

--- packages/account/src/Components/network-status-toast-popup/network-status-toast-popup.tsx
+++ packages/account/src/Components/network-status-toast-popup/network-status-toast-popup.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { observer, useStore } from '@deriv/stores';
 import { useDevice } from '@deriv-com/ui';
@@ -12,26 +12,27 @@ type TNetworkStatusToast = {
 };
 
 const NetworkStatusToast = ({ status, portal_id, message }: TNetworkStatusToast) => {
-    const [is_open, setIsOpen] = useState(false);
+    const [is_visible, setIsVisible] = useState(false);
     const { isDesktop } = useDevice();
     const new_portal_id = document.getElementById(portal_id);
 
-    if (!new_portal_id || !message) return null;
+    useEffect(() => {
+        if (!new_portal_id || !message || isDesktop) return;
+        if (!is_visible && status !== 'online') {
+            setIsVisible(true);
+        } else if (is_visible && status === 'online') {
+            setTimeout(() => {
+                setIsVisible(false);
+            }, 1500);
+        }
+    }, [isDesktop, is_visible, message, new_portal_id, status]);
 
-    if (!is_open && status !== 'online') {
-        setIsOpen(true);
-    } else if (is_open && status === 'online') {
-        setTimeout(() => {
-            setIsOpen(false);
-        }, 1500);
-    }
+    if (!new_portal_id || !message || isDesktop) return null;
 
     return createPortal(
-        !isDesktop && (
-            <div className='network-status__container'>
-                <Snackbar hasCloseButton={false} message={message} isVisible={is_open} actionText='' />
-            </div>
-        ),
+        <div className='network-status__container'>
+            <Snackbar hasCloseButton={false} message={message} isVisible={is_visible} actionText='' />
+        </div>,
         new_portal_id
     );
 };

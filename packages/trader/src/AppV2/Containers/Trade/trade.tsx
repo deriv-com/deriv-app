6f9274de2ad3b0a9667be1ac06fab555b1fa6cb8
maryia-deriv <maryia@binary.com>
2024-07-10 14:41:25 +0300

    chore: init branch

--- packages/trader/src/AppV2/Containers/Trade/trade.tsx
+++ packages/trader/src/AppV2/Containers/Trade/trade.tsx
@@ -3,6 +3,7 @@ import { Text } from '@deriv-com/quill-ui';
 import BottomNav from 'AppV2/Components/BottomNav';
 
 const Trade = () => {
+    // TODO: Implement trade page
     return (
         <BottomNav>
             <Text size='sm'>Trade</Text>

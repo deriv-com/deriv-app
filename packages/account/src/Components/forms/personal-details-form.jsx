9a1584bd94574fe839301a28e811c3c27f3404bf
fasihali-deriv <muhammad.fasih@regentmarkets.com>
2024-07-11 19:10:49 +0800

    chore: removed popup

--- packages/account/src/Components/forms/personal-details-form.jsx
+++ packages/account/src/Components/forms/personal-details-form.jsx
@@ -751,14 +751,7 @@ const TaxResidenceField = ({
     );
 };
 
-const TaxIdentificationNumberField = ({
-    is_tin_popover_open,
-    setIsTinPopoverOpen,
-    setIsTaxResidencePopoverOpen,
-    disabled,
-    required = false,
-}) => {
-    const { isDesktop } = useDevice();
+const TaxIdentificationNumberField = ({ disabled, required = false }) => {
     return (
         <div className='details-form__tax'>
             <FormInputField
@@ -769,38 +762,6 @@ const TaxIdentificationNumberField = ({
                 disabled={disabled}
                 required={required}
             />
-            <div
-                data-testid='tax_identification_number_pop_over'
-                onClick={e => {
-                    setIsTaxResidencePopoverOpen(false);
-                    setIsTinPopoverOpen(true);
-                    if (e.target.tagName !== 'A') e.stopPropagation();
-                }}
-            >
-                <Popover
-                    alignment={isDesktop ? 'right' : 'left'}
-                    icon='info'
-                    is_open={is_tin_popover_open}
-                    message={
-                        <Localize
-                            i18n_default_text={
-                                "Don't know your tax identification number? Click <0>here</0> to learn more."
-                            }
-                            components={[
-                                <a
-                                    key={0}
-                                    className='link link--red'
-                                    rel='noopener noreferrer'
-                                    target='_blank'
-                                    href='https://www.oecd.org/tax/automatic-exchange/crs-implementation-and-assistance/tax-identification-numbers/'
-                                />,
-                            ]}
-                        />
-                    }
-                    zIndex={9998}
-                    disable_message_icon
-                />
-            </div>
         </div>
     );
 };

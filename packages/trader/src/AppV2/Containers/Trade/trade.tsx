b9983eb4e3519dee1c855007cf5ba3989cfb04c7
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2024-07-11 06:10:37 +0300

    [DTRA] Maryia/DTRA-1351/feat: Skeleton loader for DTrader V2 (#15854)
    * feat: init Skeleton
    
    * feat: add icon variant to skeleton
    
    * feat: PositionsLoader
    
    * refactor: isolate ContractCardLoader
    
    * feat: remove ContractCardLoader
    
    * feat: init contract-details-loader
    
    * chore: temporary - replace core and trader loader with specific dtrader_v2 loaders placeholders
    
    * feat: add more blocks to ContractDetailsLoader
    
    * feat: init TradeLoader
    
    * refactor: use numeric width, height and gap
    
    * refactor: move loaders for v2 to components/
    
    * fix: remove unused import
    
    * feat: add draft tab skeletons & refactoring
    
    * fix: PositionsContent test
    
    * fix: contract-details test + remove unused skeleton
    
    * Merge branch master into maryia/DTRA-1351/skeleton-loader-dtrader-v2
    
    * chore: update styles of page loaders
    
    * feat: add skeleton to header
    
    * refactor: skeleton and revert to having all skeletons with border-radius
    
    * feat: HeaderLoader
    
    * refactor: and distinguish between open/closed tabs
    
    * chore: update notifications icon and bottom loader in closed tab
    
    * revert: extra files after merge from master
    
    * chore: apply tokens, fix test, improve load-more-spinner
    
    * test: cover new components with tests
    
    * chore: remove scrollbar from Trade page loader
    
    * test: fix AccountActionsDTraderV2 tests
    
    * test: fix utils tests
    
    * test: isDTraderV2
    
    * refactor: added comments about dtrader features temporary outside trader, moved HeaderLoader to core
    
    * refactor: replace Array.from with spread operator

--- packages/trader/src/AppV2/Containers/Trade/trade.tsx
+++ packages/trader/src/AppV2/Containers/Trade/trade.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Text } from '@deriv-com/quill-ui';
 import BottomNav from 'AppV2/Components/BottomNav';
+import { observer } from 'mobx-react';
+import { useTraderStore } from 'Stores/useTraderStores';
+import { Loading } from '@deriv/components';
 
-const Trade = () => {
-    return (
-        <BottomNav>
-            <Text size='sm'>Trade</Text>
-        </BottomNav>
-    );
-};
+const Trade = observer(() => {
+    const { active_symbols, onMount, onUnmount } = useTraderStore();
+
+    React.useEffect(() => {
+        onMount();
+        return onUnmount;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <BottomNav>{active_symbols.length ? <Text size='sm'>Trade</Text> : <Loading.DTraderV2 />}</BottomNav>;
+});
 
 export default Trade;

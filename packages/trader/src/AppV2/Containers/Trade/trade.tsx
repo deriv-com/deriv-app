09c888fcc0186772e8ca898683d4638d803cc086
Maryia <103177211+maryia-deriv@users.noreply.github.com>
2024-07-11 11:57:12 +0300

    Maryia/DTRA-1421/feat: add temporary Trade types selector (chips) & Assets dropdown (#137)
    * feat: add trade types selector (chips)
    
    * feat: add assets (dropdown)
    
    * chore: use Dropdown for assets

--- packages/trader/src/AppV2/Containers/Trade/trade.tsx
+++ packages/trader/src/AppV2/Containers/Trade/trade.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import { Text } from '@deriv-com/quill-ui';
+import { Dropdown } from '@deriv/components';
+import { Chip, Text } from '@deriv-com/quill-ui';
 import BottomNav from 'AppV2/Components/BottomNav';
+import { useTraderStore } from 'Stores/useTraderStores';
+import { getAvailableContractTypes } from 'Modules/Trading/Helpers/contract-type';
+import { TRADE_TYPES, unsupported_contract_types_list } from '@deriv/shared';
+import { observer } from 'mobx-react';
 
-const Trade = () => {
-    // TODO: Implement trade page
+const Trade = observer(() => {
+    const { active_symbols, contract_type, contract_types_list, onMount, onChange, onUnmount, symbol } =
+        useTraderStore();
+    const filtered_contract_types = getAvailableContractTypes(
+        contract_types_list as unknown as Parameters<typeof getAvailableContractTypes>[0],
+        unsupported_contract_types_list
+    );
+    const symbols = active_symbols.map(({ display_name, symbol: underlying }) => ({
+        text: display_name,
+        value: underlying,
+    }));
+    const trade_types = Object.values(filtered_contract_types)
+        .map(({ contract_types }) => contract_types)
+        .flat()
+        .filter(
+            ({ value }) =>
+                ![TRADE_TYPES.VANILLA.PUT, TRADE_TYPES.TURBOS.SHORT, TRADE_TYPES.RISE_FALL_EQUAL].includes(value)
+        );
+
+    React.useEffect(() => {
+        onMount();
+        return onUnmount;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const onSymbolSelect = (value: string) => {
+        onChange({
+            target: {
+                name: 'symbol',
+                value,
+            },
+        });
+    };
+    const onTradeTypeSelect = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const value = trade_types.find(({ text }) => text === (e.target as HTMLButtonElement).textContent)?.value;
+        onChange({
+            target: {
+                name: 'contract_type',
+                value,
+            },
+        });
+    };
+
     return (
         <BottomNav>
-            <Text size='sm'>Trade</Text>
+            <div className='trade__trade-types'>
+                {trade_types.map(({ text, value }) => (
+                    <Chip.Selectable key={value} onChipSelect={onTradeTypeSelect} selected={value === contract_type}>
+                        <Text size='sm'>{text}</Text>
+                    </Chip.Selectable>
+                ))}
+            </div>
+            <div className='trade__assets'>
+                <Dropdown list={symbols} name='symbol' onChange={onChange} value={symbol} />
+            </div>
         </BottomNav>
     );
-};
+});
 
 export default Trade;

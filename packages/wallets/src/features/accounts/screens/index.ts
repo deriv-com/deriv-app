d55c6aed623e6a5ad36205f427132f4dcf080124
Aum Bhatt <aum@deriv.com>
2024-07-12 15:15:44 +0400

    refactor: converted PoiPoaDocsSubmitted to ResubmissionSuccessMessage

--- packages/wallets/src/features/accounts/screens/index.ts
+++ packages/wallets/src/features/accounts/screens/index.ts
@@ -1,6 +1,4 @@
 export * from './ManualDocumentUpload';
 export * from './PoaScreen';
-export * from './PoiPoaDocsSubmitted';
 export * from './PoiUploadError';
 export * from './SelfieDocumentUpload';
-export * from './VerifyDocumentDetails';

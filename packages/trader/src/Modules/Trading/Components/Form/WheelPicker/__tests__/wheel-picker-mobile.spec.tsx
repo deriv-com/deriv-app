d6b38df6f3c4d245dbb7d71ac184010c67afc9ed
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-07-15 15:31:23 +0800

    fix: test case

--- packages/trader/src/Modules/Trading/Components/Form/WheelPicker/__tests__/wheel-picker-mobile.spec.tsx
+++ packages/trader/src/Modules/Trading/Components/Form/WheelPicker/__tests__/wheel-picker-mobile.spec.tsx
@@ -12,12 +12,16 @@ describe('WheelPickerMobile', () => {
     });
 
     it('renders correctly with the initial state', () => {
-        render(<WheelPickerMobile options={options} onChange={mockOnChange} defaultValue={options[3]} />);
+        render(
+            <WheelPickerMobile currency='usd' options={options} onChange={mockOnChange} defaultValue={options[3]} />
+        );
         expect(screen.getByText('0.34')).toBeInTheDocument();
     });
 
     it('calls onChange when the value changes via swipe', () => {
-        render(<WheelPickerMobile options={options} onChange={mockOnChange} defaultValue={options[3]} />);
+        render(
+            <WheelPickerMobile currency='usd' options={options} onChange={mockOnChange} defaultValue={options[3]} />
+        );
         // eslint-disable-next-line testing-library/no-node-access
         const pickerContainer = screen.getByText('0.34').closest('.picker-viewport') as HTMLElement;
 
@@ -37,19 +41,25 @@ describe('WheelPickerMobile', () => {
     it('sets default value from props correctly', () => {
         const defaultValue = '0.09';
 
-        render(<WheelPickerMobile options={options} onChange={mockOnChange} defaultValue={defaultValue} />);
+        render(
+            <WheelPickerMobile currency='usd' options={options} onChange={mockOnChange} defaultValue={defaultValue} />
+        );
         expect(screen.getByText('0.09')).toBeInTheDocument();
     });
 
     it('updates the selected index when an option is clicked', () => {
-        render(<WheelPickerMobile options={options} onChange={mockOnChange} defaultValue={options[3]} />);
+        render(
+            <WheelPickerMobile currency='usd' options={options} onChange={mockOnChange} defaultValue={options[3]} />
+        );
 
         userEvent.click(screen.getByText('0.76'));
         expect(mockOnChange).toHaveBeenCalledWith('0.76');
     });
 
     it('calls onChange when the value changes via click', () => {
-        render(<WheelPickerMobile options={options} onChange={mockOnChange} defaultValue={options[3]} />);
+        render(
+            <WheelPickerMobile currency='usd' options={options} onChange={mockOnChange} defaultValue={options[3]} />
+        );
 
         userEvent.click(screen.getByText('0.38'));
         expect(mockOnChange).toHaveBeenCalledWith('0.38');

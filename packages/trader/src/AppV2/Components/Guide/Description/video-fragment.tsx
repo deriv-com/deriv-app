b23530a7eea42401587527cc0334099c06f8ab5f
kate-deriv <kate@regentmarkets.com>
2024-07-15 18:10:38 +0300

    feat: import new package for lottie and add video for acc

--- packages/trader/src/AppV2/Components/Guide/Description/video-fragment.tsx
+++ packages/trader/src/AppV2/Components/Guide/Description/video-fragment.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Localize } from '@deriv/translations';
 import { Loading } from '@deriv/components';
-import { Text } from '@deriv-com/quill-ui';
 import { useDevice } from '@deriv-com/ui';
 import { getUrlBase } from '@deriv/shared';
 import { CONTRACT_LIST } from 'AppV2/Utils/trade-types-utils';
-import Lottie from 'react-lottie';
-// import * as animationData from './lottie/accumulators_lottie_mobile.lottie';
-import * as animationData from './lottie/accumulators_lottie_mobile.json';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 type TVideoFragment = {
     contract_type: string;
 };
+
 const VideoFragment = ({ contract_type }: TVideoFragment) => {
     const [is_loading, setIsLoading] = React.useState(true);
+    const [dotLottie, setDotLottie] = React.useState<EventTarget | null>(null);
 
     const { isMobile } = useDevice();
 
@@ -24,49 +22,27 @@ const VideoFragment = ({ contract_type }: TVideoFragment) => {
             getUrlBase(`/public/videos/${contract_type.toLowerCase()}_${isMobile ? 'mobile' : 'desktop'}.${extension}`),
         [contract_type, isMobile]
     );
-    const mp4_src = React.useMemo(() => getVideoSource('mp4'), [getVideoSource]);
+    const lottie_src = React.useMemo(() => getVideoSource('lottie'), [getVideoSource]);
 
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice',
-        },
+    React.useEffect(() => {
+        const onLoad = () => {
+            setIsLoading(false);
+        };
+
+        if (dotLottie) {
+            dotLottie.addEventListener('load', onLoad);
+        }
+
+        return () => {
+            if (dotLottie) {
+                dotLottie.removeEventListener('load', onLoad);
+            }
+        };
+    }, [dotLottie]);
+
+    const dotLottieRefCallback = (dotLottie: EventTarget | null) => {
+        setDotLottie(dotLottie);
     };
-    const events = {
-        DATA_READY: 'data_ready',
-        LOOP_COMPLETE: 'loopComplete',
-        ENTER_FRAME: 'enterFrame',
-        SEGMENT_START: 'segmentStart',
-        CONFIG_LOADED: 'config_ready',
-    } as const;
-
-    const eventListeners = [
-        {
-            eventName: events.DATA_READY,
-            callback: () => {
-                console.log('data is ready');
-                setIsLoading(false);
-            },
-        },
-        {
-            eventName: events.LOOP_COMPLETE,
-            callback: () => console.log('the animation completed'),
-        },
-        {
-            eventName: events.ENTER_FRAME,
-            callback: () => console.log('enter frame'),
-        },
-        {
-            eventName: events.SEGMENT_START,
-            callback: () => console.log('segmentStart'),
-        },
-        {
-            eventName: events.CONFIG_LOADED,
-            callback: () => console.log('config_ready'),
-        },
-    ];
 
     return (
         <div
@@ -76,7 +52,14 @@ const VideoFragment = ({ contract_type }: TVideoFragment) => {
             })}
         >
             {is_loading && <Loading is_fullscreen={false} />}
-            <Lottie options={defaultOptions} height={161} width={248} eventListeners={eventListeners} />
+            <DotLottieReact
+                src={lottie_src}
+                loop
+                autoplay
+                dotLottieRefCallback={
+                    dotLottieRefCallback as React.ComponentProps<typeof DotLottieReact>['dotLottieRefCallback']
+                }
+            />
         </div>
     );
 };

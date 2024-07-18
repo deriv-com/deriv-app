5284e7288dfe11c6a6cbfdc1e279e951ca60b4f9
Likhith Kolayari <likhith@regentmarkets.com>
2024-07-18 08:43:43 +0400

    fix: removed unused ts-ignore

--- packages/account/src/Sections/Profile/PhoneVerification/otp-verification.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/otp-verification.tsx
@@ -140,8 +140,6 @@ const OTPVerification = observer(({ phone_verification_type, setOtpVerification 
                     value={otp}
                     type='number'
                     maxLength={6}
-                    /* eslint-disable @typescript-eslint/ban-ts-comment */
-                    /* @ts-expect-error [TODO] fix type error */
                     buttonDisabled={otp.length < 6}
                 />
                 <ResendCodeTimer

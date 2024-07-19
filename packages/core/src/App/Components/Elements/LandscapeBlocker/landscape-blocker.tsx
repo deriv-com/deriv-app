054e265615947f031cab82dbeae6f361b54ed53b
Henry Hein <henry@regentmarkets.com>
2024-07-19 11:20:23 +0800

    fix: test

--- packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
+++ packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { isDisabledLandscapeBlockerRoute, isTabletOs, routes } from '@deriv/shared';
+import { isDisabledLandscapeBlockerRoute, isMobileOs, isTabletOs, routes } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import LandscapeBlockerSvg from 'Assets/SvgComponents/settings/landscape-blocker.svg';
 import './landscape-blocker.scss';
+import { useDevice } from '@deriv-com/ui';
 
 const LandscapeBlocker = observer(() => {
     // need to check for wallet account and don't hide landscape blocker for users migrated to wallets
     const {
         client: { has_wallet },
     } = useStore();
+    const { isMobile, isTablet } = useDevice();
+    console.log(isMobile, 'isMobile', isTablet, 'isTablet', isTabletOs, 'isTabletOS', isMobileOs(), 'isMobileOS')
     const location = useLocation();
     const pathname = location?.pathname;
     const is_hidden_landscape_blocker = isDisabledLandscapeBlockerRoute(pathname);

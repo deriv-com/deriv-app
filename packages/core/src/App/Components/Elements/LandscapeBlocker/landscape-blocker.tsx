24b2a2004cdd782508dee7db1876498f017ba2ae
Henry Hein <henry@regentmarkets.com>
2024-07-19 16:02:56 +0800

    fix: remove logic that was blocking on tablet

--- packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
+++ packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
@@ -17,7 +17,7 @@ const LandscapeBlocker = observer(() => {
     const pathname = location?.pathname;
     const is_hidden_landscape_blocker = isDisabledLandscapeBlockerRoute(pathname);
     const shouldShowDtraderTabletView = pathname === routes.trade && isTabletOs;
-    const showBlockerDtraderMobileLandscapeView = isTablet && isMobileOs() && pathname === routes.trade;
+    const showBlockerDtraderMobileLandscapeView = !isMobile && isMobileOs() && pathname === routes.trade;
 
     if (
         !has_wallet &&

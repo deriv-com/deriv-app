b64ce38fead186a31d1dae4d439c2286da13f467
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-07-22 17:49:26 +0300

    DTRA-1354/ Kate / Tests for isDigitContractWinning function  (#160)
    * refactor: add tests for util function
    
    * fix: test case

--- packages/trader/src/AppV2/Utils/__tests__/trade-param-utils.spec.tsx
+++ packages/trader/src/AppV2/Utils/__tests__/trade-param-utils.spec.tsx
@@ -1,5 +1,5 @@
-import { TRADE_TYPES } from '@deriv/shared';
-import { getTradeParams } from '../trade-params-utils';
+import { CONTRACT_TYPES, TRADE_TYPES } from '@deriv/shared';
+import { getTradeParams, isDigitContractWinning } from '../trade-params-utils';
 
 describe('getTradeParams', () => {
     it('should return correct array with keys for Rise/Fall', () => {
@@ -23,5 +23,55 @@ describe('getTradeParams', () => {
             'expiration',
             'mult_info_display',
         ]);
+    });
+});
+
+describe('isDigitContractWinning', () => {
+    it('should return false if contract_type is not defined', () => {
+        expect(isDigitContractWinning(undefined, null, null)).toBeFalsy();
+    });
+
+    it('should return false if contract_type is not digit type', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.TURBOS.LONG, null, null)).toBeFalsy();
+    });
+
+    it('should return true for Matches if current_digit === selected_digit and false if they are not equal', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.MATCH_DIFF.MATCH, 1, 1)).toBeTruthy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.MATCH_DIFF.MATCH, 1, 2)).toBeFalsy();
+    });
+
+    it('should return true for Differs if current_digit !== selected_digit and false if they are equal', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.MATCH_DIFF.DIFF, 1, 1)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.MATCH_DIFF.DIFF, 1, 2)).toBeTruthy();
+    });
+
+    it('should return true for Over if current_digit and selected_digit are not null and current_digit > selected_digit. In the rest cases should return false', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.OVER, 1, 2)).toBeTruthy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.OVER, null, null)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.OVER, 0, 0)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.OVER, 2, 1)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.OVER, 2, 2)).toBeFalsy();
+    });
+
+    it('should return true for Under if current_digit and selected_digit are not null and current_digit < selected_digit. In the rest cases should return false', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.UNDER, 2, 1)).toBeTruthy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.UNDER, null, null)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.UNDER, 0, 0)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.UNDER, 1, 2)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.OVER_UNDER.UNDER, 2, 2)).toBeFalsy();
+    });
+
+    it('should return true for Odd if current_digit is not null and it has an odd value', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.ODD, null, 1)).toBeTruthy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.ODD, null, null)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.ODD, null, 2)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.ODD, null, 0)).toBeFalsy();
+    });
+
+    it('should return true for Even if current_digit is not null and it has an even value or 0', () => {
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.EVEN, null, 2)).toBeTruthy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.EVEN, null, 0)).toBeTruthy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.EVEN, null, null)).toBeFalsy();
+        expect(isDigitContractWinning(CONTRACT_TYPES.EVEN_ODD.EVEN, null, 1)).toBeFalsy();
     });
 });

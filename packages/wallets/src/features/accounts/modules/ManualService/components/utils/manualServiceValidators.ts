cf31ee9ee8e5b67ff75521870426910d757248eb
Aum Bhatt <aum@deriv.com>
2024-07-22 09:40:08 +0400

    refactor: expiryDateValidator and remove validations file

--- packages/wallets/src/features/accounts/modules/ManualService/components/utils/manualServiceValidators.ts
+++ packages/wallets/src/features/accounts/modules/ManualService/components/utils/manualServiceValidators.ts
@@ -1,9 +1,28 @@
+import moment from 'moment';
 import * as Yup from 'yup';
 
 export const documentRequiredValidator = (documentType: string) => Yup.string().required(`${documentType} is required`);
 
-export const expiryDateValidator = Yup.date()
-    .min(new Date(), 'Expiry date cannot be today date or in the past')
-    .required('Expiry date is required');
+export const expiryDateValidator = Yup.string()
+    .nullable()
+    .required('Expiry date is required.')
+    .test({
+        name: 'test-expiry-date-is-null',
+        test: (value, context) => {
+            if (value === null) {
+                return context.createError({ message: 'Expiry date is required.' });
+            }
+            return true;
+        },
+    })
+    .test({
+        name: 'test-min-expiry-date',
+        test: (value, context) => {
+            if (moment(value).isBefore(new Date())) {
+                return context.createError({ message: 'Expiry date cannot be today date or in the past' });
+            }
+            return true;
+        },
+    });
 
 export const fileValidator = Yup.mixed().required();

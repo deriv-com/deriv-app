08ad58e13063fded8e8212ace04939d08eef930c
kate-deriv <kate@regentmarkets.com>
2024-07-23 09:15:59 +0300

    chore: apply suggestions

--- packages/trader/src/AppV2/Components/TradeParameters/trade-parameters-container.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/trade-parameters-container.tsx
@@ -13,34 +13,31 @@ const TradeParametersContainer = ({
     children,
     is_minimized,
     is_minimized_visible,
-}: React.PropsWithChildren<TTradeParametersContainer>) => (
-    <React.Fragment>
-        {is_minimized ? (
-            <CSSTransition
-                in={is_minimized_visible}
-                timeout={0}
-                classNames={{
-                    appear: 'trade-params__options__wrapper--minimized--enter',
-                    enter: 'trade-params__options__wrapper--minimized--enter',
-                    enterDone: 'trade-params__options__wrapper--minimized--enter-done',
-                    exit: 'trade-params__options__wrapper--minimized--exit',
-                }}
-                unmountOnExit
-            >
-                {children}
-            </CSSTransition>
-        ) : (
-            <section className='trade-params'>
-                <div className='trade-params__title'>
-                    <Text>
-                        <Localize i18n_default_text='Set your trade' />
-                    </Text>
-                    <Guide has_label />
-                </div>
-                {children}
-            </section>
-        )}
-    </React.Fragment>
-);
+}: React.PropsWithChildren<TTradeParametersContainer>) =>
+    is_minimized ? (
+        <CSSTransition
+            in={is_minimized_visible}
+            timeout={0}
+            classNames={{
+                appear: 'trade-params__options__wrapper--minimized--enter',
+                enter: 'trade-params__options__wrapper--minimized--enter',
+                enterDone: 'trade-params__options__wrapper--minimized--enter-done',
+                exit: 'trade-params__options__wrapper--minimized--exit',
+            }}
+            unmountOnExit
+        >
+            {children}
+        </CSSTransition>
+    ) : (
+        <section className='trade-params'>
+            <div className='trade-params__title'>
+                <Text>
+                    <Localize i18n_default_text='Set your trade' />
+                </Text>
+                <Guide has_label />
+            </div>
+            {children}
+        </section>
+    );
 
 export default React.memo(TradeParametersContainer);

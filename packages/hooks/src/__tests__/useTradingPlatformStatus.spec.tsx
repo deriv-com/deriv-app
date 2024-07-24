ca3ccad853eeaf16b515a7c6ad52373997efdcca
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-07-24 18:23:44 +0800

    fix: mockUseQuery

--- packages/hooks/src/__tests__/useTradingPlatformStatus.spec.tsx
+++ packages/hooks/src/__tests__/useTradingPlatformStatus.spec.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
-import { APIProvider } from '@deriv/api';
+import { APIProvider, useQuery } from '@deriv/api';
 import { mockStore, StoreProvider } from '@deriv/stores';
 import { renderHook } from '@testing-library/react-hooks';
 import useTradingPlatformStatus from '../useTradingPlatformStatus';
 
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    useQuery: jest.fn(),
+}));
+
+const mockUseQuery = useQuery as jest.MockedFunction<typeof useQuery<'trading_platform_status'>>;
+
 describe('useTradingPlatformStatus', () => {
     test('should return trading_platform_status', async () => {
-        const mock = mockStore({ client: { accounts: { CRW909900: { token: '12345' } }, loginid: 'CRW909900' } });
+        const mock = mockStore({});
+
+        // @ts-expect-error need to come up with a way to mock the return type of useFetch
+        mockUseQuery.mockReturnValue({
+            data: {
+                trading_platform_status: [
+                    {
+                        platform: 'mt5',
+                        status: 'active',
+                    },
+                ],
+            },
+            isSuccess: true,
+        });
 
         const wrapper = ({ children }: { children: JSX.Element }) => (
             <APIProvider>
@@ -16,10 +36,11 @@ describe('useTradingPlatformStatus', () => {
 
         const { result } = renderHook(() => useTradingPlatformStatus(), { wrapper });
 
-        expect(result.current?.data).toEqual([
-            { platform: 'mt5', status: 'active' },
-            { platform: 'ctrader', status: 'unavailable' },
-            { platform: 'dxtrade', status: 'maintenance' },
+        expect(result.current.data).toEqual([
+            {
+                platform: 'mt5',
+                status: 'active',
+            },
         ]);
     });
 });

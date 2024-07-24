21153d67e54a4f6f83eae52ef913f4872348033b
aizad-deriv <aizad@deriv.com>
2024-07-24 10:56:08 +0800

    chore: remove landscape blocker for Wallets

--- packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
+++ packages/core/src/App/Components/Elements/LandscapeBlocker/landscape-blocker.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { isDisabledLandscapeBlockerRoute, isTabletOs, routes } from '@deriv/shared';
-import { observer, useStore } from '@deriv/stores';
+import { observer } from '@deriv/stores';
 import LandscapeBlockerSvg from 'Assets/SvgComponents/settings/landscape-blocker.svg';
 import './landscape-blocker.scss';
 
 const LandscapeBlocker = observer(() => {
-    // need to check for wallet account and don't hide landscape blocker for users migrated to wallets
-    const {
-        client: { has_wallet },
-    } = useStore();
     const location = useLocation();
     const pathname = location?.pathname;
     const is_hidden_landscape_blocker = isDisabledLandscapeBlockerRoute(pathname);
     const shouldShowDtraderTabletView = pathname === routes.trade && isTabletOs;
 
-    if (!has_wallet && (is_hidden_landscape_blocker || shouldShowDtraderTabletView)) return null;
+    if (is_hidden_landscape_blocker || shouldShowDtraderTabletView) return null;
 
     return (
         <div id='landscape_blocker' className='landscape-blocker'>

8b9a0a426c4243d1fa4e206b995c7abc123fdb45
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-07-25 13:09:58 +0800

    chore: hiding long code in contract details

--- packages/trader/src/Modules/Contract/Containers/replay-chart.tsx
+++ packages/trader/src/Modules/Contract/Containers/replay-chart.tsx
@@ -14,6 +14,7 @@ import ResetContractChartElements from 'Modules/SmartChart/Components/Markers/re
 import { SmartChart } from 'Modules/SmartChart';
 import ChartMarker from 'Modules/SmartChart/Components/Markers/marker';
 import { useDevice } from '@deriv-com/ui';
+import { useFeatureFlags } from '@deriv/hooks';
 
 const ReplayChart = observer(
     ({
@@ -80,6 +81,8 @@ const ReplayChart = observer(
 
         const has_ended = !!getEndTime(contract_info);
 
+        const { is_dtrader_v2_enabled } = useFeatureFlags();
+
         return (
             <SmartChart
                 id='replay'
@@ -105,7 +108,7 @@ const ReplayChart = observer(
                 stateChangeListener={chartStateChange}
                 symbol={symbol}
                 allTicks={all_ticks}
-                topWidgets={ChartTopWidgets}
+                topWidgets={is_dtrader_v2_enabled && isMobile ? <></> : ChartTopWidgets}
                 isConnectionOpened={is_socket_opened}
                 isStaticChart={
                     // forcing chart reload when start_epoch changes to an earlier epoch for ACCU closed contract:

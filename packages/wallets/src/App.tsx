8e0dc33b1c151bf84cf024e17cb2514523e84836
lubega-deriv <lubega@deriv.com>
2024-07-29 12:26:22 +0800

    fix: clean up code

--- packages/wallets/src/App.tsx
+++ packages/wallets/src/App.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import { APIProvider } from '@deriv/api-v2';
-import { initializeI18n, TranslationProvider } from '@deriv-com/translations';
 import { ModalProvider } from './components/ModalProvider';
 import AppContent from './AppContent';
 import WalletsAuthProvider from './AuthProvider';
 import './styles/fonts.scss';
 import './index.scss';
+import './translations/i18n';
 
-const App: React.FC = () => {
-    const i18nInstance = initializeI18n({
-        cdnUrl: `${process.env.CROWDIN_URL}/${process.env.WALLETS_TRANSLATION_PATH}`, // 'https://translations.deriv.com/deriv-app-wallets/staging'
-        useSuspense: false,
-    });
-
-    return (
-        <APIProvider standalone>
-            <WalletsAuthProvider>
-                <ModalProvider>
-                    <TranslationProvider defaultLang='EN' i18nInstance={i18nInstance}>
-                        <AppContent />
-                    </TranslationProvider>
-                </ModalProvider>
-            </WalletsAuthProvider>
-        </APIProvider>
-    );
-};
+const App: React.FC = () => (
+    <APIProvider standalone>
+        <WalletsAuthProvider>
+            <ModalProvider>
+                <AppContent />
+            </ModalProvider>
+        </WalletsAuthProvider>
+    </APIProvider>
+);
 
 export default App;

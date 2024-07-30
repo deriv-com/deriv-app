96d3765e075800c67efaa0053b686f76a5c68a7c
Farabi <farabi@deriv.com>
2024-07-30 12:47:50 +0800

    chore: add test coverage for all dbot tour files in tutorials folder

--- packages/bot-web-ui/src/pages/tutorials/dbot-tours/bot-builder-tour/__tests__/bot-builder-tour-mobile.spec.tsx
+++ packages/bot-web-ui/src/pages/tutorials/dbot-tours/bot-builder-tour/__tests__/bot-builder-tour-mobile.spec.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { mockStore, StoreProvider } from '@deriv/stores';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DBOT_TABS } from 'Constants/bot-contents';
 import { mock_ws } from 'Utils/mock';
 import { DBotStoreProvider, mockDBotStore } from 'Stores/useDBotStore';
-import OnboardingTourMobile from '../onboarding-tour/onboarding-tour-mobile';
-import { DBOT_ONBOARDING_MOBILE } from '../tour-content';
+import BotBuilderTourMobile from '../bot-builder-tour-mobile';
 
 jest.mock('@deriv/bot-skeleton/src/scratch/blockly', () => jest.fn());
 jest.mock('@deriv/bot-skeleton/src/scratch/dbot', () => jest.fn());
@@ -13,12 +14,12 @@ jest.mock('@deriv/deriv-charts', () => ({
     setSmartChartsPublicPath: jest.fn(),
 }));
 
-describe('Tour Content', () => {
+describe('BotBuilder Tour Mobile', () => {
     let wrapper: ({ children }: { children: JSX.Element }) => JSX.Element;
     const mock_store = mockStore({});
     const mock_DBot_store = mockDBotStore(mock_store, mock_ws);
 
-    beforeEach(() => {
+    beforeAll(() => {
         wrapper = ({ children }: { children: JSX.Element }) => (
             <StoreProvider store={mock_store}>
                 <DBotStoreProvider ws={mock_ws} mock={mock_DBot_store}>
@@ -27,11 +28,27 @@ describe('Tour Content', () => {
             </StoreProvider>
         );
     });
-    it('DBOT_ONBOARDING_MOBILE is called', () => {
-        render(<OnboardingTourMobile />, {
+    it('should render BotBuilderTourMobile component', () => {
+        mock_DBot_store.dashboard.setTourDialogVisibility(true);
+        mock_DBot_store.dashboard.setActiveTab(DBOT_TABS.BOT_BUILDER);
+
+        render(<BotBuilderTourMobile />, {
             wrapper,
         });
-        const first_step = DBOT_ONBOARDING_MOBILE[0];
-        expect(screen.getByText(first_step.header)).toBeInTheDocument();
+        expect(screen.getByText('Bot Builder guide')).toBeInTheDocument();
+    });
+
+    it('should render BotBuilderTourMobile when active tour is set to bot builder and tour dialog is false', () => {
+        mock_DBot_store.dashboard.active_tour = 'bot_builder';
+        mock_DBot_store.dashboard.setShowMobileTourDialog(false);
+
+        render(<BotBuilderTourMobile />, {
+            wrapper,
+        });
+
+        const start_button = screen.getByRole('button', { name: 'Start' });
+        userEvent.click(start_button);
+
+        expect(screen.getByTestId('botbuilder-tour-mobile')).toBeInTheDocument();
     });
 });

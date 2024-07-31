88c8e9e44105291b33b060ffd05960110240ff88
kate-deriv <kate@regentmarkets.com>
2024-07-31 17:07:35 +0300

    refactor: add setter for callback into a mockstore

--- packages/stores/src/mockStore.ts
+++ packages/stores/src/mockStore.ts
@@ -604,6 +604,7 @@ const mock = (): TStores & { is_mock: boolean } => {
             positions: [],
             removePositionById: jest.fn(),
             setContractType: jest.fn(),
+            setAddNotificationBannerCallback: jest.fn(),
         },
         contract_trade: {
             accountSwitchListener: jest.fn(),

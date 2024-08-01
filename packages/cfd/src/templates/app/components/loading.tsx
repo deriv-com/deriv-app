0e178ba1972682fb7bed784c093207ded6fa052d
hasan-deriv <abuhasan@deriv.com>
2024-08-01 18:47:41 +0800

    fix: added test cases

--- packages/cfd/src/templates/app/components/loading.tsx
+++ packages/cfd/src/templates/app/components/loading.tsx
@@ -3,18 +3,19 @@ import classNames from 'classnames';
 import { Text } from '@deriv/components';
 
 type TLoadingProps = {
-    className: string;
-    id: string;
-    is_fullscreen: boolean;
-    is_slow_loading: boolean;
-    status: string[];
-    theme: string;
+    className?: string;
+    id?: string;
+    is_fullscreen?: boolean;
+    is_slow_loading?: boolean;
+    status?: string[];
+    theme?: string;
 };
 
 const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status, theme }: TLoadingProps) => {
     const theme_class = theme ? `barspinner-${theme}` : 'barspinner-light';
     return (
         <div
+            data-testid='cfd_loading_main_div'
             className={classNames(
                 'initial-loader',
                 {
@@ -23,7 +24,11 @@ const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status,
                 className
             )}
         >
-            <div id={id} className={classNames('initial-loader__barspinner', 'barspinner', theme_class)}>
+            <div
+                id={id}
+                data-testid='cfd_loading_spinner'
+                className={classNames('initial-loader__barspinner', 'barspinner', theme_class)}
+            >
                 {Array.from(new Array(5)).map((x, inx) => (
                     <div
                         key={inx}
@@ -34,7 +39,7 @@ const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status,
                 ))}
             </div>
             {is_slow_loading &&
-                status.map(text => (
+                status?.map(text => (
                     <Text as='h3' color='prominent' size='xs' align='center' key={text}>
                         {text}
                     </Text>

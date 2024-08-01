f3e7bfe60f393cfa88904d9694ed085f63556e35
kate-deriv <kate@regentmarkets.com>
2024-08-01 15:08:40 +0300

    refactor: remobe purchase notification for other pages

--- packages/trader/src/AppV2/Containers/Notifications/notifications.tsx
+++ packages/trader/src/AppV2/Containers/Notifications/notifications.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { observer, useStore } from '@deriv/stores';
 import { useNotifications, NotificationBanners } from '@deriv-com/quill-ui';
 import { StandaloneFlagCheckeredFillIcon } from '@deriv/quill-icons';
+import { routes } from '@deriv/shared';
+import { useLocation } from 'react-router-dom';
 
 const Notifications = observer(() => {
     const { addBanner, banners, removeBanner } = useNotifications();
     const { portfolio } = useStore();
     const { setAddNotificationBannerCallback } = portfolio;
+    const { pathname } = useLocation();
+
+    React.useEffect(() => {
+        if (pathname === routes.trade) return;
+        banners.forEach(({ type, id }) => {
+            if (!type) {
+                // Sell notifications have type and Purchase ones do not.
+                removeBanner(id);
+            }
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [banners, pathname]);
 
     React.useEffect(() => {
         const addNotificationBannerCallback = (params: Parameters<typeof addBanner>[0], result: string) =>

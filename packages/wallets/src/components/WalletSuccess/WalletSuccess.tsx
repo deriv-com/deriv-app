f86f2386c7cf4dd4508e92f65c091c85ab8ac379
lubega-deriv <lubega@deriv.com>
2024-08-01 13:38:11 +0800

    fix: clean up code

--- packages/wallets/src/components/WalletSuccess/WalletSuccess.tsx
+++ packages/wallets/src/components/WalletSuccess/WalletSuccess.tsx
@@ -3,10 +3,10 @@ import { WalletsActionScreen } from '../WalletsActionScreen';
 import './WalletSuccess.scss';
 
 type TSuccessProps = {
-    description: React.ReactNode;
+    description: ReactNode;
     renderButtons?: ComponentProps<typeof WalletsActionScreen>['renderButtons'];
     renderIcon: () => ReactNode;
-    title: React.ReactNode;
+    title: ReactNode;
 };
 
 const WalletSuccess: React.FC<TSuccessProps> = ({ description, renderButtons, renderIcon, title }) => {

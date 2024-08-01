5f6fce1272b799bde707a096da88a679e015bfc1
hasan-deriv <abuhasan@deriv.com>
2024-08-01 13:11:53 +0800

    feat: added mockclear to failed tests

--- packages/cfd/src/Components/Errors/__tests__/error-component.spec.tsx
+++ packages/cfd/src/Components/Errors/__tests__/error-component.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { routes } from '@deriv/shared';
 import ErrorComponent from '../error-component';
 
@@ -14,27 +13,13 @@ const mockErrorData = {
 };
 
 describe('ErrorComponent', () => {
+    afterEach(() => {
+        cleanup();
+        jest.clearAllMocks();
+    });
     it('should render as a dialog when is_dialog is true', () => {
         render(<ErrorComponent is_dialog={true} />);
         expect(screen.getByRole('dialog')).toBeInTheDocument();
-    });
-
-    it('should render as a dialog with provided data', async () => {
-        mockErrorData.header = '';
-        mockErrorData.redirect_label = '';
-        render(<ErrorComponent is_dialog={true} {...mockErrorData} />);
-        expect(
-            screen.getByRole('heading', {
-                level: 1,
-                name: /there was an error/i,
-            })
-        ).toBeInTheDocument();
-        const buttonEl = screen.getByRole('button', {
-            name: /ok/i,
-        });
-        expect(buttonEl).toBeInTheDocument();
-        userEvent.click(buttonEl);
-        expect(mockErrorData.redirectOnClick).toHaveBeenCalledTimes(1);
     });
 
     it('should render as a standard message when is_dialog is false', () => {
@@ -48,13 +33,36 @@ describe('ErrorComponent', () => {
                 <ErrorComponent {...mockErrorData} />
             </MemoryRouter>
         );
-        expect(screen.getByText(mockErrorData.header)).toBeInTheDocument();
+        expect(
+            screen.getByRole('heading', {
+                level: 3,
+                name: mockErrorData.header,
+            })
+        ).toBeInTheDocument();
         expect(screen.getByText(mockErrorData.message)).toBeInTheDocument();
         expect(screen.getByText(/refresh/i)).toBeInTheDocument();
         const linkEl = screen.getByRole('link', { name: mockErrorData.redirect_label });
         expect(linkEl).toBeInTheDocument();
         expect(linkEl).toHaveAttribute('href', routes.trade);
         fireEvent.click(linkEl);
+        expect(mockErrorData.redirectOnClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('should render as a dialog with provided data', () => {
+        mockErrorData.header = '';
+        mockErrorData.redirect_label = '';
+        render(<ErrorComponent is_dialog={true} {...mockErrorData} />);
+        expect(
+            screen.getByRole('heading', {
+                level: 1,
+                name: /there was an error/i,
+            })
+        ).toBeInTheDocument();
+        const buttonEl = screen.getByRole('button', {
+            name: /ok/i,
+        });
+        expect(buttonEl).toBeInTheDocument();
+        fireEvent.click(buttonEl);
         expect(mockErrorData.redirectOnClick).toHaveBeenCalledTimes(1);
     });
 });

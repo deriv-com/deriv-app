f2a33e478f1b51c56e35581c6e9e76cc655af65c
henry-deriv <118344354+henry-deriv@users.noreply.github.com>
2024-08-01 16:03:40 +0800

    [DTRA] Henry/dtra 1443/market closed message v2 (#16196)
    * fix: stop scrolling to center on symbol selection for all category
    
    * fix: flex-start tabs list
    
    * fix: market is closed message
    
    * fix: type error
    
    * fix: add tests
    
    * fix: add tests
    
    * fix: add a mock for closedmarketmessage
    
    * fix: remove unused import
    
    * fix: comment

---
+++ packages/trader/src/AppV2/Components/MarketCountdownTimer/__tests__/market-countdown-timer.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // for the `toBeInTheDocument` matcher
+import MarketCountdownTimer from '..';
+
+describe('MarketCountDownTimer', () => {
+    it('renders correctly with given time left', () => {
+        render(<MarketCountdownTimer time_left={{ days: 1, hours: 2, minutes: 3, seconds: 4 }} />);
+
+        expect(screen.getByText('26:03:04')).toBeInTheDocument();
+    });
+    it('renders correctly when there is no time left', () => {
+        render(<MarketCountdownTimer time_left={{ days: 0, hours: 0, minutes: 0, seconds: 0 }} />);
+
+        expect(screen.getByText('00:00:00')).toBeInTheDocument();
+    });
+    it('renders correctly with edge case values', () => {
+        render(<MarketCountdownTimer time_left={{ days: 0, hours: 1, minutes: 1, seconds: 1 }} />);
+
+        expect(screen.getByText('01:01:01')).toBeInTheDocument();
+    });
+});

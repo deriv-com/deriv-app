46ef13930c2e0d4124d130c72d27b87a9bcc03a9
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-08-02 11:48:28 +0800

    chore: remove the store value

--- packages/stores/src/stores/FeatureFlagsStore.ts
+++ packages/stores/src/stores/FeatureFlagsStore.ts
@@ -3,7 +3,6 @@ import BaseStore from './BaseStore';
 const FLAGS = {
     dtrader_v2: false,
     next_cashier: false,
-    p2p_v2: false,
     sharkfin: false,
     wallet: false,
 } satisfies Record<string, boolean>;

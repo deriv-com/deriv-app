dc29949f60938cb3b79b953b562ce834a8bb970b
hasan-deriv <abuhasan@deriv.com>
2024-08-02 17:16:07 +0800

    fix: removed unused helpers functions

--- packages/cfd/src/Components/Routes/index.js
+++ packages/cfd/src/Components/Routes/index.js
@@ -2,5 +2,4 @@ import BinaryRoutes from './binary-routes.jsx';
 import RouteWithSubRoutes from './route-with-sub-routes.jsx';
 
 export default BinaryRoutes;
-export * from './helpers';
 export { RouteWithSubRoutes };

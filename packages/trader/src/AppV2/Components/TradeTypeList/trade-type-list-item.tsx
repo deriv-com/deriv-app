1be5bea7ad9ab7d1a6b6f7ba9e958492ff4ab882
Akmal Djumakhodjaev <akmal@binary.com>
2024-08-02 15:37:11 +0800

    feat: modify trade type list item

--- packages/trader/src/AppV2/Components/TradeTypeList/trade-type-list-item.tsx
+++ packages/trader/src/AppV2/Components/TradeTypeList/trade-type-list-item.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import { StandaloneCirclePlusFillIcon } from '@deriv/quill-icons';
+import clsx from 'clsx';
 
 type TTradeTypeListItemProps = {
-    title: string;
+    title: React.ReactNode;
+    selected: boolean;
     leftIcon?: React.ReactNode;
     rightIcon?: React.ReactNode;
     onLeftIconClick?: () => void;
     onRightIconClick?: () => void;
+    onTradeTypeClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
 const TradeTypeListItem: React.FC<TTradeTypeListItemProps> = ({
     title,
+    selected,
     leftIcon,
     rightIcon,
     onLeftIconClick,
     onRightIconClick,
+    onTradeTypeClick,
 }) => {
     const default_icon = <StandaloneCirclePlusFillIcon fill='var(--core-color-solid-green-700)' iconSize='sm' />;
 
     return (
-        <div className='trade-type-list-item'>
+        <button
+            className={clsx('trade-type-list-item', { 'trade-type-list-item--selected': selected })}
+            onClick={onTradeTypeClick}
+        >
             {leftIcon && (
                 <button className='trade-type-list-item__left-icon' onClick={onLeftIconClick}>
                     {leftIcon}
                 </button>
             )}
             <div className='trade-type-list-item__title'>{title}</div>
-            <button className='trade-type-list-item__icon' onClick={onRightIconClick}>
-                {rightIcon || default_icon}
-            </button>
-        </div>
+            {onRightIconClick && (
+                <button className='trade-type-list-item__icon' onClick={onRightIconClick}>
+                    {rightIcon || default_icon}
+                </button>
+            )}
+        </button>
     );
 };
 

fe85b055ca261728c9916def5e8faa9d6d98f6af
Henry Hein <henry@regentmarkets.com>
2024-08-02 13:27:16 +0800

    fix: correctly sort market categories

--- packages/trader/src/AppV2/Components/MarketCategories/market-categories.tsx
+++ packages/trader/src/AppV2/Components/MarketCategories/market-categories.tsx
@@ -4,6 +4,7 @@ import useActiveSymbols from 'AppV2/Hooks/useActiveSymbols';
 import { categorizeSymbols } from 'AppV2/Utils/symbol-categories-utils';
 import MarketCategory from '../MarketCategory';
 import MarketCategoryTab from '../MarketCategoryTab/market-category-tab';
+import { observer } from '@deriv/stores';
 
 type TMarketCategories = {
     selectedSymbol: string;
@@ -12,32 +13,35 @@ type TMarketCategories = {
     isOpen: boolean;
 };
 
-const MarketCategories = forwardRef(
-    ({ selectedSymbol, setSelectedSymbol, setIsOpen, isOpen }: TMarketCategories, ref: Ref<HTMLDivElement>) => {
-        const { activeSymbols } = useActiveSymbols();
-        const categorizedSymbols = categorizeSymbols(activeSymbols);
-        return (
-            <React.Fragment>
-                <Tab.List>
-                    {Object.values(categorizedSymbols).map(category => (
-                        <MarketCategoryTab key={category.market} category={category} />
-                    ))}
-                </Tab.List>
-                <Tab.Content className='market-categories__list' ref={ref}>
-                    {Object.values(categorizedSymbols).map(category => (
-                        <MarketCategory
-                            key={category.market}
-                            category={category}
-                            selectedSymbol={selectedSymbol}
-                            setSelectedSymbol={setSelectedSymbol}
-                            setIsOpen={setIsOpen}
-                            isOpen={isOpen}
-                        />
-                    ))}
-                </Tab.Content>
-            </React.Fragment>
-        );
-    }
+const MarketCategories = observer(
+    // eslint-disable-next-line react/display-name
+    forwardRef(
+        ({ selectedSymbol, setSelectedSymbol, setIsOpen, isOpen }: TMarketCategories, ref: Ref<HTMLDivElement>) => {
+            const { activeSymbols } = useActiveSymbols();
+            const categorizedSymbols = categorizeSymbols(activeSymbols);
+            return (
+                <React.Fragment>
+                    <Tab.List>
+                        {Object.values(categorizedSymbols).map(category => (
+                            <MarketCategoryTab key={category.market} category={category} />
+                        ))}
+                    </Tab.List>
+                    <Tab.Content className='market-categories__list' ref={ref}>
+                        {Object.values(categorizedSymbols).map(category => (
+                            <MarketCategory
+                                key={category.market}
+                                category={category}
+                                selectedSymbol={selectedSymbol}
+                                setSelectedSymbol={setSelectedSymbol}
+                                setIsOpen={setIsOpen}
+                                isOpen={isOpen}
+                            />
+                        ))}
+                    </Tab.Content>
+                </React.Fragment>
+            );
+        }
+    )
 );
 
 MarketCategories.displayName = 'MarketCategories';

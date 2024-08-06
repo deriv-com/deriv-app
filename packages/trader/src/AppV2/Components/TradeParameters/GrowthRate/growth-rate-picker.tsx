00cab93bdb2a28a80d4331bb6d1e2efde34f0ec7
maryia-deriv <maryia@binary.com>
2024-08-06 17:43:53 +0300

    feat: make proposal request when the wheel stops turning

--- packages/trader/src/AppV2/Components/TradeParameters/GrowthRate/growth-rate-picker.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/GrowthRate/growth-rate-picker.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import clsx from 'clsx';
+import debounce from 'lodash.debounce';
 import { ActionSheet, Text, WheelPickerContainer } from '@deriv-com/quill-ui';
 import { localize, Localize } from '@deriv/translations';
 import { getGrowthRatePercentage } from '@deriv/shared';
+import { Skeleton } from '@deriv/components';
 
 type TGrowthRatePickerProps = {
     accumulator_range_list?: number[];
     growth_rate: number;
-    is_small_screen?: boolean;
     maximum_ticks: number;
     onSave: () => void;
     setGrowthRate: (growth_rate: number) => void;
+    should_show_details?: boolean;
     tick_size_barrier_percentage: string;
 };
+
+const debouncedSetGrowthRate = debounce((setGrowthRate, growth_rate) => {
+    setGrowthRate(growth_rate);
+}, 200);
 
 const GrowthRatePicker = ({
     accumulator_range_list = [],
     growth_rate,
-    is_small_screen,
     maximum_ticks,
     onSave,
     setGrowthRate,
+    should_show_details,
     tick_size_barrier_percentage,
 }: TGrowthRatePickerProps) => {
+    const initial_growth_rate = React.useRef<number>();
+    const selected_growth_rate = React.useRef<number>(growth_rate);
     const data = accumulator_range_list.map(rate => ({ value: `${getGrowthRatePercentage(rate)}%` }));
     const details_content = [
         {
@@ -35,24 +42,46 @@ const GrowthRatePicker = ({
         },
     ];
 
+    React.useEffect(() => {
+        if (!initial_growth_rate.current && growth_rate) {
+            initial_growth_rate.current = growth_rate;
+        }
+        return () => {
+            debouncedSetGrowthRate.cancel();
+            if (initial_growth_rate.current && initial_growth_rate.current !== selected_growth_rate.current) {
+                setGrowthRate(initial_growth_rate.current);
+            }
+        };
+    }, []);
+
+    const handleSave = () => {
+        initial_growth_rate.current = selected_growth_rate.current;
+        onSave();
+    };
+
+    const handlePickerValuesChange = (_idx: number, value: string | number) => {
+        const new_value = Number((value as string).slice(0, -1)) / 100;
+        if (new_value === selected_growth_rate.current) return;
+        debouncedSetGrowthRate(setGrowthRate, new_value);
+        selected_growth_rate.current = new_value;
+    };
+
     if (!accumulator_range_list.length) return null;
     return (
         <React.Fragment>
-            <ActionSheet.Content
-                className={clsx('growth-rate__content', is_small_screen && 'growth-rate__content--small')}
-            >
-                <div className='growth-rate__picker'>
+            <ActionSheet.Content className='growth-rate__picker'>
+                <div className='growth-rate__wheel-picker'>
                     <WheelPickerContainer
                         data={[data]}
                         inputValues={[`${getGrowthRatePercentage(growth_rate)}%`]}
-                        setInputValues={(_idx, value) => setGrowthRate(Number((value as string).slice(0, -1)) / 100)}
+                        setInputValues={handlePickerValuesChange}
                     />
                 </div>
                 <div className='growth-rate__details'>
                     {details_content.map(({ label, value }) => (
                         <span key={value} className='growth-rate__details-item'>
                             <Text size='sm'>{label}</Text>
-                            <Text size='sm'>{value}</Text>
+                            {should_show_details ? <Text size='sm'>{value}</Text> : <Skeleton height={22} width={75} />}
                         </span>
                     ))}
                 </div>
@@ -61,7 +90,7 @@ const GrowthRatePicker = ({
                 alignment='vertical'
                 primaryAction={{
                     content: <Localize i18n_default_text='Save' />,
-                    onAction: onSave,
+                    onAction: handleSave,
                 }}
                 shouldCloseOnPrimaryButtonClick={false}
             />
--- packages/trader/src/AppV2/Components/TradeParameters/GrowthRate/growth-rate.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/GrowthRate/growth-rate.tsx
@@ -4,7 +4,7 @@ import { observer } from 'mobx-react';
 import { ActionSheet, TextField } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
 import { useTraderStore } from 'Stores/useTraderStores';
-import { getGrowthRatePercentage } from '@deriv/shared';
+import { CONTRACT_TYPES, getGrowthRatePercentage, isEmptyObject } from '@deriv/shared';
 import Carousel from 'AppV2/Components/Carousel';
 import CarouselHeader from 'AppV2/Components/Carousel/carousel-header';
 import TradeParamDefinition from 'AppV2/Components/TradeParamDefinition';
@@ -18,15 +18,21 @@ const GrowthRate = observer(({ is_minimized }: TGrowthRateProps) => {
     const {
         accumulator_range_list,
         growth_rate,
+        is_purchase_enabled,
+        is_trade_enabled,
         has_open_accu_contract,
         maximum_ticks,
         onChange,
+        proposal_info,
         tick_size_barrier_percentage,
     } = useTraderStore();
 
     const [is_open, setIsOpen] = React.useState(false);
     const [selected_growth_rate, setSelectedGrowthRate] = React.useState(growth_rate);
     const is_small_screen = window.innerHeight <= 640;
+    const info = proposal_info?.[CONTRACT_TYPES.ACCUMULATOR] || {};
+    const is_proposal_data_available =
+        is_trade_enabled && !isEmptyObject(proposal_info) && !!info.id && is_purchase_enabled;
 
     React.useEffect(() => {
         setSelectedGrowthRate(growth_rate);
@@ -50,11 +56,11 @@ const GrowthRate = observer(({ is_minimized }: TGrowthRateProps) => {
             component: (
                 <GrowthRatePicker
                     accumulator_range_list={accumulator_range_list}
-                    is_small_screen={is_small_screen}
                     maximum_ticks={maximum_ticks}
                     onSave={onSaveButtonClick}
                     growth_rate={selected_growth_rate}
-                    setGrowthRate={setSelectedGrowthRate}
+                    setGrowthRate={handleGrowthRateChange}
+                    should_show_details={is_proposal_data_available}
                     tick_size_barrier_percentage={tick_size_barrier_percentage}
                 />
             ),
@@ -93,7 +99,7 @@ const GrowthRate = observer(({ is_minimized }: TGrowthRateProps) => {
             <ActionSheet.Root isOpen={is_open} onClose={onActionSheetClose} position='left' expandable={false}>
                 <ActionSheet.Portal shouldCloseOnDrag fullHeightOnOpen={is_small_screen}>
                     <Carousel
-                        classname='growth-rate__carousel'
+                        classname={clsx('growth-rate__carousel', is_small_screen && 'growth-rate__carousel--small')}
                         header={CarouselHeader}
                         pages={action_sheet_content}
                         title={<Localize i18n_default_text='Growth rate' />}

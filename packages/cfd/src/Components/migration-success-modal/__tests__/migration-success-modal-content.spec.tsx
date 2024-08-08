cd6418c4e82f2cb2ccf7fdc3e57d1c0d47390070
fasihali-deriv <121229483+fasihali-deriv@users.noreply.github.com>
2024-08-08 17:24:05 +0800

    Fasih/COJ-1146/ Updated modal content and design (#16267)
    * chore: updated modal content and design
    
    * chore: removed unused code
    
    * chore: fixed multiple markets name issue
    
    * chore: fixed test
    
    * chore: fixed popup closing issue and textual issue
    
    * chore: updated test according to text
    
    * chore: fixed mobile botton issue

--- packages/cfd/src/Components/migration-success-modal/__tests__/migration-success-modal-content.spec.tsx
+++ packages/cfd/src/Components/migration-success-modal/__tests__/migration-success-modal-content.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { StoreProvider, mockStore } from '@deriv/stores';
 import { CFDStoreProvider } from '../../../Stores/Modules/CFD/Helpers/useCfdStores';
 import MigrationSuccessModalContent from '../migration-success-modal-content';
@@ -14,53 +14,39 @@ const wrapper = ({ children }) => (
 );
 describe('<MigrationSuccessModal />', () => {
     const props: React.ComponentProps<typeof MigrationSuccessModalContent> = {
-        has_open_positions: false,
         icon: 'icon',
         eligible_account_to_migrate: 'BVI',
-        directToCashier: jest.fn(),
+        closePopupModal: jest.fn(),
+        jurisdiction_market_name: ['Financial'],
     };
     it('component should be rendered', () => {
         render(<MigrationSuccessModalContent {...props} />, { wrapper });
 
         expect(
             screen.getByRole('heading', {
-                name: /success!/i,
+                name: /Upgrade complete/i,
             })
         ).toBeInTheDocument();
 
-        expect(screen.getByText(/your new account\(s\) are ready for trading\./i)).toBeInTheDocument();
+        const account_text = screen.getByText(/Start trading with your new/i);
+        const strong_text = within(account_text).getByText(/MT5 Financial BVI/i);
+
+        expect(account_text).toBeInTheDocument();
+        expect(strong_text.tagName).toBe('STRONG');
+
+        expect(screen.getByText(/Important: Your account./i)).toBeInTheDocument();
+
         expect(
-            screen.getByText(/for new trades, please transfer your funds into the new account\(s\)\./i)
+            screen.getByText(/You can manage your existing positions, but you can't start a new trade./i)
         ).toBeInTheDocument();
-    });
 
-    it('should not render open position banner if has_open_positions is false', () => {
-        render(<MigrationSuccessModalContent {...props} />, { wrapper });
-
-        expect(
-            screen.queryByText(/you can continue with the open positions on your current account\(s\)\./i)
-        ).not.toBeInTheDocument();
-
-        const button = screen.getByRole('button', {
-            name: /transfer now/i,
-        });
-
-        userEvent.click(button);
-        expect(props.directToCashier).toHaveBeenCalled();
-    });
-
-    it('should render open position banner if has_open_positions is true', () => {
-        render(<MigrationSuccessModalContent {...props} has_open_positions />, { wrapper });
-
-        expect(
-            screen.getByText(/you can continue with the open positions on your current account\(s\)\./i)
-        ).toBeInTheDocument();
+        expect(screen.getByText(/We'll close accounts with no open positions after 60 days./i)).toBeInTheDocument();
 
         const button = screen.getByRole('button', {
             name: /ok/i,
         });
 
         userEvent.click(button);
-        expect(props.directToCashier).toHaveBeenCalled();
+        expect(props.closePopupModal).toHaveBeenCalled();
     });
 });

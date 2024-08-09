ebbefdd3300c9f7174272d36f9e5414ce1ec8bea
kate-deriv <kate@regentmarkets.com>
2024-08-09 08:52:52 +0300

    refactor: move validation to be

--- packages/trader/src/AppV2/Components/TradeParameters/TakeProfit/take-profit-input.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/TakeProfit/take-profit-input.tsx
@@ -5,14 +5,20 @@ import { Localize, localize } from '@deriv/translations';
 type TTakeProfitInputProps = {
     currency?: string;
     decimals?: number;
-    error_message?: React.ReactNode;
     is_enabled?: boolean;
     is_accumulator?: boolean;
     message?: React.ReactNode;
     onToggleSwitch: (new_value: boolean) => void;
-    onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    onSave: () => void;
+    onChangeMultiple: (prop: { has_take_profit?: boolean; has_cancellation?: false }) => Promise<void>;
+    onChange: (e: {
+        target: {
+            name: string;
+            value: unknown;
+        };
+    }) => Promise<void>;
+    onActionSheetClose: () => void;
     take_profit_value?: string | number;
+    validation_error?: string;
 };
 
 const TakeProfitInput = React.forwardRef(
@@ -20,17 +26,68 @@ const TakeProfitInput = React.forwardRef(
         {
             currency,
             decimals,
-            error_message,
             is_enabled,
             is_accumulator,
             message,
             onToggleSwitch,
-            onInputChange,
-            onSave,
+            onChangeMultiple,
+            onChange,
+            onActionSheetClose,
             take_profit_value,
+            validation_error,
         }: TTakeProfitInputProps,
         ref: React.ForwardedRef<HTMLInputElement>
     ) => {
+        const initial_value_ref = React.useRef<string | number>();
+        const selected_value_ref = React.useRef<string | number | undefined>(take_profit_value);
+
+        const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            //TODO: check if we will need all this logic with latest Quill update. Add disabling "-" icon when value is < 1
+            let value: string | number = e.target.value;
+            value = String(value).trim().replace(',', '.');
+
+            if (value !== '' && Number(value) <= 0) value = '0';
+            if (value === selected_value_ref.current) return;
+            selected_value_ref.current = value;
+
+            onChange({
+                target: {
+                    name: 'take_profit',
+                    value,
+                },
+            });
+        };
+        //TODO: togglechange and onChangeMultiple
+        const onSave = () => {
+            if (validation_error && is_enabled) {
+                return;
+            }
+
+            if (selected_value_ref.current !== initial_value_ref.current) {
+                initial_value_ref.current = selected_value_ref.current;
+            }
+
+            onChangeMultiple({
+                has_take_profit: is_enabled,
+                ...(is_enabled ? { has_cancellation: false } : {}),
+            });
+
+            onActionSheetClose();
+        };
+
+        React.useEffect(() => {
+            if (!initial_value_ref.current && take_profit_value) {
+                initial_value_ref.current = take_profit_value;
+            }
+
+            return () => {
+                if (initial_value_ref.current !== selected_value_ref.current) {
+                    onChange({ target: { name: 'take_profit', value: initial_value_ref.current } });
+                }
+            };
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, []);
+
         return (
             <React.Fragment>
                 <ActionSheet.Content className='take-profit__wrapper'>
@@ -45,12 +102,12 @@ const TakeProfitInput = React.forwardRef(
                         disabled={!is_enabled}
                         decimals={decimals}
                         inputMode='decimal'
-                        message={message}
+                        message={validation_error || message}
                         name='take_profit'
                         onChange={onInputChange}
                         placeholder={localize('Amount')}
                         ref={ref}
-                        status={error_message ? 'error' : 'neutral'}
+                        status={validation_error ? 'error' : 'neutral'}
                         textAlignment='center'
                         unitLeft={currency}
                         variant='fill'
--- packages/trader/src/AppV2/Components/TradeParameters/TakeProfit/take-profit.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/TakeProfit/take-profit.tsx
@@ -25,12 +25,10 @@ const TakeProfit = observer(({ is_minimized }: TTakeProfitProps) => {
         onChangeMultiple,
         onChange,
         validation_params,
+        validation_errors,
     } = useTraderStore();
-
     const [is_open, setIsOpen] = React.useState(false);
     const [is_enabled, setIsEnabled] = React.useState(has_take_profit);
-    const [take_profit_value, setTakeProfitValue] = React.useState<string | number | undefined>(take_profit);
-    const [error_message, setErrorMessage] = React.useState<React.ReactNode>();
 
     const input_ref = React.useRef<HTMLInputElement>(null);
     const focused_input_ref = React.useRef<HTMLInputElement>(null);
@@ -39,83 +37,32 @@ const TakeProfit = observer(({ is_minimized }: TTakeProfitProps) => {
     const min_take_profit = validation_params?.take_profit?.min;
     const max_take_profit = validation_params?.take_profit?.max;
     const decimals = getDecimalPlaces(currency);
+    const validation_error = validation_errors.take_profit[0];
 
     const getInputMessage = () =>
-        is_enabled
-            ? error_message || (
-                  <Localize
-                      i18n_default_text='Acceptable range: {{min_take_profit}} to {{max_take_profit}} {{currency}}'
-                      values={{ currency, min_take_profit, max_take_profit }}
-                  />
-              )
-            : '';
-
-    const isTakeProfitOutOfRange = (value = take_profit_value) => {
-        if (!value) {
-            setErrorMessage(<Localize i18n_default_text='Please enter a take profit amount.' />);
-            return true;
-        }
-        if (Number(value) < Number(min_take_profit) || Number(value) > Number(max_take_profit)) {
-            setErrorMessage(
-                <Localize
-                    i18n_default_text='Acceptable range: {{min_take_profit}} to {{max_take_profit}} {{currency}}'
-                    values={{ currency, min_take_profit, max_take_profit }}
-                />
-            );
-            return true;
-        }
-        setErrorMessage('');
-        return false;
-    };
+        is_enabled && min_take_profit && max_take_profit && !validation_error ? (
+            <Localize
+                i18n_default_text='Acceptable range: {{min_take_profit}} to {{max_take_profit}} {{currency}}'
+                values={{ currency, min_take_profit, max_take_profit }}
+            />
+        ) : (
+            ''
+        );
 
     const onToggleSwitch = (new_value: boolean) => {
         setIsEnabled(new_value);
 
         if (new_value) {
-            if (take_profit_value !== '' && take_profit_value !== undefined) {
-                isTakeProfitOutOfRange();
-            }
-
             clearTimeout(focus_timeout.current);
             focus_timeout.current = focusAndOpenKeyboard(focused_input_ref.current, input_ref.current);
         } else {
             input_ref.current?.blur();
-            setErrorMessage('');
         }
-    };
-
-    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        //TODO: check if we will need all this logic with latest Quill update. Add disabling "-" icon when value is < 1
-        let value: string | number = e.target.value;
-        value = String(value).trim().replace(',', '.');
-
-        if (value !== '' && Number(value) <= 0) value = '0';
-
-        setTakeProfitValue(value);
-        isTakeProfitOutOfRange(value);
-    };
-
-    const onSave = () => {
-        if (isTakeProfitOutOfRange() && is_enabled) return;
-
-        onChangeMultiple({
-            has_take_profit: is_enabled,
-            ...(is_enabled ? { has_cancellation: false } : {}),
-        });
-        onChange({
-            target: {
-                name: 'take_profit',
-                value: take_profit_value,
-            },
-        });
-        onActionSheetClose();
     };
 
     const onActionSheetClose = () => {
         setIsOpen(false);
         setIsEnabled(has_take_profit);
-        setTakeProfitValue(take_profit);
-        setErrorMessage('');
     };
 
     const action_sheet_content = [
@@ -125,15 +72,16 @@ const TakeProfit = observer(({ is_minimized }: TTakeProfitProps) => {
                 <TakeProfitInput
                     currency={currency}
                     decimals={decimals}
-                    error_message={error_message}
                     is_enabled={is_enabled}
                     is_accumulator={is_accumulator}
                     message={getInputMessage()}
                     onToggleSwitch={onToggleSwitch}
-                    onInputChange={onInputChange}
-                    onSave={onSave}
+                    onChangeMultiple={onChangeMultiple}
+                    onChange={onChange}
+                    onActionSheetClose={onActionSheetClose}
                     ref={input_ref}
-                    take_profit_value={take_profit_value}
+                    take_profit_value={take_profit}
+                    validation_error={validation_error}
                 />
             ),
         },
@@ -144,11 +92,8 @@ const TakeProfit = observer(({ is_minimized }: TTakeProfitProps) => {
     ];
 
     React.useEffect(() => {
-        setIsEnabled(has_take_profit);
-        setTakeProfitValue(take_profit);
-
         return () => clearTimeout(focus_timeout.current);
-    }, [has_take_profit, take_profit]);
+    }, []);
 
     return (
         <React.Fragment>

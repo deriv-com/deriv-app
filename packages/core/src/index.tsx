2e037546d51c1ccaf29b79b2075c534eb1c46b20
aswathy-deriv <aswathy@regentmarkets.com>
2024-08-09 15:07:12 +0400

    fix: async call is added in the analytics initializer

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -12,23 +12,25 @@ import { checkAndSetEndpointFromUrl } from '@deriv/shared';
 import AppNotificationMessages from './App/Containers/app-notification-messages.jsx';
 import { AnalyticsInitializer } from 'Utils/Analytics';
 
-AnalyticsInitializer();
-if (
-    !!window?.localStorage.getItem?.('debug_service_worker') || // To enable local service worker related development
-    (!window.location.hostname.startsWith('localhost') && !/binary\.sx/.test(window.location.hostname)) ||
-    window.location.hostname === 'deriv-app.binary.sx'
-) {
-    registerServiceWorker();
-}
-
-const has_endpoint_url = checkAndSetEndpointFromUrl();
-
-// if has endpoint url, APP will be redirected
-if (!has_endpoint_url) {
-    const root_store = initStore(AppNotificationMessages);
-
-    const wrapper = document.getElementById('deriv_app');
-    if (wrapper) {
-        ReactDOM.render(<App useSuspense={false} root_store={root_store} />, wrapper);
+(async () => {
+    await AnalyticsInitializer();
+    if (
+        !!window?.localStorage.getItem?.('debug_service_worker') || // To enable local service worker related development
+        (!window.location.hostname.startsWith('localhost') && !/binary\.sx/.test(window.location.hostname)) ||
+        window.location.hostname === 'deriv-app.binary.sx'
+    ) {
+        registerServiceWorker();
     }
-}
+
+    const has_endpoint_url = checkAndSetEndpointFromUrl();
+
+    // if has endpoint url, APP will be redirected
+    if (!has_endpoint_url) {
+        const root_store = initStore(AppNotificationMessages);
+
+        const wrapper = document.getElementById('deriv_app');
+        if (wrapper) {
+            ReactDOM.render(<App useSuspense={false} root_store={root_store} />, wrapper);
+        }
+    }
+})();

a1f06d4996667b8aa7343e7d694711384539b31e
kate-deriv <kate@regentmarkets.com>
2024-08-12 19:50:17 +0300

    feat: add content dependency

--- packages/trader/src/AppV2/Components/TradeParameters/RiskManagement/risk-management.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/RiskManagement/risk-management.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
-import { ActionSheet, Text, TextField } from '@deriv-com/quill-ui';
+import { observer } from 'mobx-react';
+import { useTraderStore } from 'Stores/useTraderStores';
+import { ActionSheet, Text, TextField, SectionMessage } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
 import Carousel from 'AppV2/Components/Carousel';
 import CarouselHeader from 'AppV2/Components/Carousel/carousel-header';
@@ -11,9 +13,11 @@ type TRiskManagementProps = {
     is_minimized?: boolean;
 };
 
-const RiskManagement = ({ is_minimized }: TRiskManagementProps) => {
+const RiskManagement = observer(({ is_minimized }: TRiskManagementProps) => {
     const [is_open, setIsOpen] = React.useState(false);
+    const { cancellation_range_list } = useTraderStore();
 
+    const should_show_deal_cancellation = cancellation_range_list?.length > 0;
     const classname = clsx('trade-params__option', is_minimized && 'trade-params__option--minimized');
     const action_sheet_content = [
         {
@@ -38,12 +42,23 @@ const RiskManagement = ({ is_minimized }: TRiskManagementProps) => {
                             <Text className='risk-management__description__definition'>
                                 <Localize i18n_default_text='When your loss reaches or exceeds this amount, your trade will be closed automatically.' />
                             </Text>
-                            <Text bold color='quill-typography__color--prominent'>
-                                <Localize i18n_default_text='Deal cancellation' />
-                            </Text>
-                            <Text className='risk-management__description__definition'>
-                                <Localize i18n_default_text='When this is active, you can cancel your trade within the chosen time frame. Your stake will be returned without loss.' />
-                            </Text>
+                            {should_show_deal_cancellation && (
+                                <React.Fragment>
+                                    <Text bold color='quill-typography__color--prominent'>
+                                        <Localize i18n_default_text='Deal cancellation' />
+                                    </Text>
+                                    <Text className='risk-management__description__definition'>
+                                        <Localize i18n_default_text='When this is active, you can cancel your trade within the chosen time frame. Your stake will be returned without loss.' />
+                                    </Text>
+                                    <SectionMessage
+                                        message={
+                                            <Localize i18n_default_text='Take profit and/or stop loss are not available while deal cancellation is active.' />
+                                        }
+                                        size='sm'
+                                        status='info'
+                                    />
+                                </React.Fragment>
+                            )}
                         </React.Fragment>
                     }
                     classname='risk-management__description'
@@ -79,6 +94,6 @@ const RiskManagement = ({ is_minimized }: TRiskManagementProps) => {
             </ActionSheet.Root>
         </>
     );
-};
+});
 
 export default RiskManagement;

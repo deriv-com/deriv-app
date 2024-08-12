5c9a7173b9c9f4b65d4a000c88e8c70a96c89300
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2024-08-12 11:06:56 +0300

    chore: use global colors

--- packages/bot-web-ui/src/pages/dashboard/announcements/announcement-components.tsx
+++ packages/bot-web-ui/src/pages/dashboard/announcements/announcement-components.tsx
@@ -14,7 +14,7 @@ export const TitleAnnounce = ({ title, announce }: { title: string; announce: bo
         size='xs'
         line_height='l'
         weight={announce ? 'bold' : 'normal'}
-        styles={!announce ? { color: '#000000B8' } : {}}
+        styles={!announce ? { color: 'var(--text-announce-message-disabled)' } : {}}
     >
         {title}
     </Text>
@@ -26,11 +26,11 @@ export const MessageAnnounce = ({ message, date, announce }: { message: string; 
             size='xs'
             line_height='l'
             weight={announce ? 'normal' : 'lighter'}
-            styles={!announce ? { color: '#000000B8' } : {}}
+            styles={!announce ? { color: 'var(--text-announce-message-disabled)' } : {}}
         >
             {message}
         </Text>
-        <Text size='xxs' styles={{ color: '#0000007A' }}>
+        <Text size='xxs' styles={{ color: 'var(--text-date)' }}>
             {date}
         </Text>
     </>

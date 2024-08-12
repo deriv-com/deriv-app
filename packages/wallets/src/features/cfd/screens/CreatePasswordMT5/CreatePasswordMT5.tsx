73935438fc8b40e4c4db34eac7e74415d9ca43ea
hasan-deriv <abuhasan@deriv.com>
2024-08-12 15:01:50 +0800

    fix: fixed mt5 create password screen

--- packages/wallets/src/features/cfd/screens/CreatePasswordMT5/CreatePasswordMT5.tsx
+++ packages/wallets/src/features/cfd/screens/CreatePasswordMT5/CreatePasswordMT5.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { DerivLightDmt5PasswordIcon, DerivLightIcDxtradePasswordIcon } from '@deriv/quill-icons';
+import { DerivLightDmt5PasswordIcon } from '@deriv/quill-icons';
 import { Checkbox, InlineMessage } from '@deriv-com/ui';
 import { WalletButton, WalletPasswordFieldLazy, WalletText } from '../../../../components/Base';
 import useDevice from '../../../../hooks/useDevice';
 import { TPlatforms } from '../../../../types';
 import { validPassword, validPasswordMT5 } from '../../../../utils/password-validation';
 import { CFD_PLATFORMS, PlatformDetails } from '../../constants';
-import './CreatePassword.scss';
+import './CreatePasswordMT5.scss';
 
 type TProps = {
     isLoading?: boolean;
@@ -16,12 +16,7 @@ type TProps = {
     platform: TPlatforms.All;
 };
 
-const CreatePasswordIcon = {
-    dxtrade: <DerivLightIcDxtradePasswordIcon height={120} width={120} />,
-    mt5: <DerivLightDmt5PasswordIcon height={120} width={120} />,
-} as const;
-
-const CreatePassword: React.FC<TProps> = ({ isLoading, onPasswordChange, onPrimaryClick, password, platform }) => {
+const CreatePasswordMT5: React.FC<TProps> = ({ isLoading, onPasswordChange, onPrimaryClick, password, platform }) => {
     const { isDesktop } = useDevice();
     const { title } = PlatformDetails[platform as keyof typeof PlatformDetails];
     const isMT5 = platform === CFD_PLATFORMS.MT5;
@@ -38,7 +33,7 @@ const CreatePassword: React.FC<TProps> = ({ isLoading, onPasswordChange, onPrima
                 </div>
             )}
             <div className='wallets-create-password__body'>
-                {CreatePasswordIcon[platform as keyof typeof CreatePasswordIcon]}
+                <DerivLightDmt5PasswordIcon height={120} width={120} />
                 <WalletText size={isDesktop ? 'sm' : 'md'}>
                     Note: You can use this password for all your {title} accounts.
                 </WalletText>
@@ -84,4 +79,4 @@ const CreatePassword: React.FC<TProps> = ({ isLoading, onPasswordChange, onPrima
     );
 };
 
-export default CreatePassword;
+export default CreatePasswordMT5;

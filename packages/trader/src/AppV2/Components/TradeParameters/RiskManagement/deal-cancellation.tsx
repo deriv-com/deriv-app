ee4b1b3e9cd22613455c2b51eb65bd640531ea0b
kate-deriv <kate@regentmarkets.com>
2024-08-14 12:08:19 +0300

    feat: add snackbar for dc save

--- packages/trader/src/AppV2/Components/TradeParameters/RiskManagement/deal-cancellation.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/RiskManagement/deal-cancellation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { useTraderStore } from 'Stores/useTraderStores';
-import { Button, Text, ToggleSwitch, WheelPicker } from '@deriv-com/quill-ui';
+import { Button, Text, ToggleSwitch, WheelPicker, useSnackbar } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
 import { addUnit } from 'AppV2/Utils/trade-params-utils';
 
@@ -10,17 +10,55 @@ type TDealCancellationProps = {
 };
 
 const DealCancellation = observer(({ closeActionSheet }: TDealCancellationProps) => {
-    const { has_cancellation, cancellation_range_list, cancellation_duration, onChangeMultiple } = useTraderStore();
+    const {
+        has_cancellation,
+        has_take_profit,
+        has_stop_loss,
+        cancellation_range_list,
+        cancellation_duration,
+        onChangeMultiple,
+    } = useTraderStore();
+    const { addSnackbar } = useSnackbar();
 
     const [is_enabled, setIsEnabled] = React.useState(has_cancellation);
     const [selected_value, setSelectedValue] = React.useState<string>(addUnit(cancellation_duration));
 
+    const getSnackBarText = ({
+        has_cancellation,
+        has_take_profit,
+        has_stop_loss,
+    }: {
+        has_cancellation?: boolean;
+        has_take_profit?: boolean;
+        has_stop_loss?: boolean;
+    }) => {
+        if (has_cancellation) {
+            if (has_take_profit && has_stop_loss)
+                return <Localize i18n_default_text='TP and SL have been turned off.' />;
+            if (has_take_profit) return <Localize i18n_default_text='TP has been turned off.' />;
+            if (has_stop_loss) return <Localize i18n_default_text='SL has been turned off.' />;
+        }
+        if ((has_take_profit || has_stop_loss) && has_cancellation)
+            return <Localize i18n_default_text='DC has been turned off.' />;
+    };
+
     const onSave = () => {
+        const new_cancellation_duration = addUnit(selected_value, 'm', false);
+
+        if (has_cancellation === is_enabled && new_cancellation_duration === cancellation_duration) return;
+
+        if (has_take_profit || has_stop_loss) {
+            addSnackbar({
+                message: getSnackBarText({ has_cancellation: is_enabled, has_stop_loss, has_take_profit }),
+                hasCloseButton: true,
+                delay: 100,
+            });
+        }
         onChangeMultiple({
             has_cancellation: is_enabled,
             ...(is_enabled ? { has_take_profit: false } : {}),
             ...(is_enabled ? { has_stop_loss: false } : {}),
-            cancellation_duration: addUnit(selected_value, 'm', false),
+            cancellation_duration: new_cancellation_duration,
         });
         closeActionSheet();
     };

949f5e87934583e426189623ab1824a5d4025fb3
hasan-deriv <abuhasan@deriv.com>
2024-08-15 15:49:39 +0800

    fix: fixed typescript error in test cases

--- packages/cfd/src/Components/success-dialog/__tests__/success-dialog.spec.tsx
+++ packages/cfd/src/Components/success-dialog/__tests__/success-dialog.spec.tsx
@@ -13,6 +13,12 @@ jest.mock('@deriv/components', () => ({
 
 describe('<SuccessDialog />', () => {
     let modal_root_el: HTMLElement;
+    const mock_props = {
+        toggleModal: jest.fn(),
+        icon: <Icon icon='IcCashierWallet' />,
+        has_close_icon: true,
+        message: '',
+    };
 
     beforeAll(() => {
         modal_root_el = document.createElement('div');
@@ -25,7 +31,7 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should render SuccessDialog when is_open is true', () => {
-        render(<SuccessDialog is_open={true} />);
+        render(<SuccessDialog {...mock_props} is_open={true} />);
         expect(
             screen.getByRole('heading', {
                 name: /success!/i,
@@ -34,7 +40,7 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should render heading when heading prop is passed', () => {
-        render(<SuccessDialog is_open={true} heading={<h2>Heading from props</h2>} />);
+        render(<SuccessDialog {...mock_props} is_open={true} heading={<h2>Heading from props</h2>} />);
         expect(
             screen.getByRole('heading', {
                 level: 2,
@@ -45,21 +51,28 @@ describe('<SuccessDialog />', () => {
 
     it('should render message when message prop is a React element', () => {
         const customMessage = <span>Custom React Element Message</span>;
-        render(<SuccessDialog is_open={true} message={customMessage} />);
+        render(<SuccessDialog {...mock_props} is_open={true} message={customMessage} />);
         const messageElement = screen.getByText('Custom React Element Message');
         expect(messageElement.tagName.toLowerCase()).toBe('span');
     });
 
     it('should render message within a paragraph when message prop is plain text', () => {
         const plainTextMessage = 'Plain text message';
-        render(<SuccessDialog is_open={true} message={plainTextMessage} classNameMessage='message-class-name' />);
+        render(
+            <SuccessDialog
+                {...mock_props}
+                is_open={true}
+                message={plainTextMessage}
+                classNameMessage='message-class-name'
+            />
+        );
         const messageElement = screen.getByText(plainTextMessage);
         expect(messageElement.tagName.toLowerCase()).toBe('p');
         expect(messageElement).toHaveClass('message-class-name');
     });
 
     it('should not render cancel button when `has_cancel` is true but `onCancel` is not provided', () => {
-        render(<SuccessDialog is_open={true} has_cancel />);
+        render(<SuccessDialog {...mock_props} is_open={true} has_cancel />);
         expect(
             screen.queryByRole('button', {
                 name: 'Maybe later',
@@ -68,7 +81,7 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should not render cancel button when `onCancel` is provided but `has_cancel` is false', () => {
-        render(<SuccessDialog is_open={true} onCancel={mockCancelFn} />);
+        render(<SuccessDialog {...mock_props} is_open={true} onCancel={mockCancelFn} />);
         expect(
             screen.queryByRole('button', {
                 name: 'Maybe later',
@@ -77,7 +90,7 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should render cancel button when `has_cancel` is true and `onCancel` is provided', () => {
-        render(<SuccessDialog is_open={true} has_cancel onCancel={mockCancelFn} />);
+        render(<SuccessDialog {...mock_props} is_open={true} has_cancel onCancel={mockCancelFn} />);
 
         screen
             .getByRole('button', {
@@ -89,7 +102,9 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should render cancel button text when `text_cancel` prop is provided', () => {
-        render(<SuccessDialog is_open={true} has_cancel onCancel={mockCancelFn} text_cancel='Cancel' />);
+        render(
+            <SuccessDialog {...mock_props} is_open={true} has_cancel onCancel={mockCancelFn} text_cancel='Cancel' />
+        );
         expect(
             screen.getByRole('button', {
                 name: 'Cancel',
@@ -98,7 +113,9 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should render submit button when `has_submit` is true and `onSubmit` is provided', () => {
-        render(<SuccessDialog is_open={true} has_submit onSubmit={mockSubmitFn} text_submit='Submit' />);
+        render(
+            <SuccessDialog {...mock_props} is_open={true} has_submit onSubmit={mockSubmitFn} text_submit='Submit' />
+        );
 
         screen
             .getByRole('button', {
@@ -112,6 +129,7 @@ describe('<SuccessDialog />', () => {
     it('should have medium className for buttons when `is_medium_button` prop is true', () => {
         render(
             <SuccessDialog
+                {...mock_props}
                 is_open={true}
                 has_cancel
                 onCancel={mockCancelFn}
@@ -130,7 +148,7 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should not render submit button when `has_submit` is true but `onSubmit` is not provided', () => {
-        render(<SuccessDialog is_open={true} has_submit text_submit='Submit' />);
+        render(<SuccessDialog {...mock_props} is_open={true} has_submit text_submit='Submit' />);
         expect(
             screen.queryByRole('button', {
                 name: 'Submit',
@@ -139,19 +157,19 @@ describe('<SuccessDialog />', () => {
     });
 
     it('should have xlarge icon class based on icon_size prop is xlarge', () => {
-        render(<SuccessDialog is_open={true} icon_size='xlarge' />);
+        render(<SuccessDialog {...mock_props} is_open={true} icon_size='xlarge' />);
         expect(screen.getByTestId('dt_cfd_success_modal_icon_wrapper')).toHaveClass(
             'success-change__icon-area--xlarge'
         );
     });
 
     it('should have large icon class based on icon_size prop is large', () => {
-        render(<SuccessDialog is_open={true} icon_size='large' />);
+        render(<SuccessDialog {...mock_props} is_open={true} icon_size='large' />);
         expect(screen.getByTestId('dt_cfd_success_modal_icon_wrapper')).toHaveClass('success-change__icon-area--large');
     });
 
     it('should render the first SVG element correctly', () => {
-        render(<SuccessDialog is_open={true} icon={<Icon icon='IcCashierWallet' />} />);
+        render(<SuccessDialog {...mock_props} is_open={true} icon={<Icon icon='IcCashierWallet' />} />);
         expect(screen.getByText('IcCashierWallet')).toBeInTheDocument();
     });
 });

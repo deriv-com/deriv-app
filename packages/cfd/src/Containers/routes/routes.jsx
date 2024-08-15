ee33013c459b427ad571ec38e5252c491a87accf
hasan-deriv <abuhasan@deriv.com>
2024-08-15 16:57:42 +0800

    fix: removed unnecessary props

--- packages/cfd/src/Containers/routes/routes.jsx
+++ packages/cfd/src/Containers/routes/routes.jsx
@@ -1,11 +1,10 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import { withRouter } from 'react-router';
-import BinaryRoutes from '../Components/Routes';
-import ErrorComponent from '../Components/Errors/error-component';
+import BinaryRoutes from '../../Components/Routes';
+import ErrorComponent from '../../Components/Errors';
 import { observer, useStore } from '@deriv/stores';
 
-const Routes = observer(({ passthrough }) => {
+const Routes = observer(() => {
     const { client, common } = useStore();
 
     const { is_logged_in, is_logging_in } = client;
@@ -15,12 +14,8 @@ const Routes = observer(({ passthrough }) => {
         return <ErrorComponent {...error} />;
     }
 
-    return <BinaryRoutes is_logged_in={is_logged_in} is_logging_in={is_logging_in} passthrough={passthrough} />;
+    return <BinaryRoutes is_logged_in={is_logged_in} is_logging_in={is_logging_in} />;
 });
-
-Routes.propTypes = {
-    passthrough: PropTypes.object,
-};
 
 // need to wrap withRouter around connect
 // to prevent updates on <BinaryRoutes /> from being blocked

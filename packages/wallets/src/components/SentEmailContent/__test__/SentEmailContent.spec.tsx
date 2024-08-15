d1f3be9ef55f6eeeeca50e0710dd5215a318de2d
aizad-deriv <aizad@deriv.com>
2024-08-15 14:36:04 +0800

    chore: added test case for SentEmailContent

---
+++ packages/wallets/src/components/SentEmailContent/__test__/SentEmailContent.spec.tsx
@@ -0,0 +1,92 @@
+import React, { PropsWithChildren } from 'react';
+import { APIProvider, AuthProvider } from '@deriv/api-v2';
+import { useDevice } from '@deriv-com/ui';
+import { fireEvent, render, screen } from '@testing-library/react';
+import useSendPasswordResetEmail from '../../../hooks/useSendPasswordResetEmail';
+import { ModalProvider } from '../../ModalProvider';
+import SentEmailContent from '../SentEmailContent';
+
+jest.mock('../../../hooks/useSendPasswordResetEmail', () => ({
+    __esModule: true,
+    default: jest.fn(),
+}));
+jest.mock('@deriv-com/ui', () => ({
+    ...jest.requireActual('@deriv-com/ui'),
+    useDevice: jest.fn(() => ({})),
+}));
+
+const wrapper = ({ children }: PropsWithChildren) => (
+    <APIProvider>
+        <AuthProvider>
+            <ModalProvider>{children}</ModalProvider>
+        </AuthProvider>
+    </APIProvider>
+);
+
+describe('SentEmailContent', () => {
+    beforeEach(() => {
+        (useDevice as jest.Mock).mockReturnValue({ isDesktop: true });
+        (useSendPasswordResetEmail as jest.Mock).mockReturnValue({
+            error: null,
+            sendEmail: jest.fn(),
+        });
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
+    it('renders the component and handles resend email flow', () => {
+        render(<SentEmailContent platform='mt5' />, { wrapper });
+
+        expect(screen.getByText("We've sent you an email")).toBeInTheDocument();
+        expect(
+            screen.getByText('Please click on the link in the email to change your Deriv MT5 password.')
+        ).toBeInTheDocument();
+        expect(screen.getByText("Didn't receive the email?")).toBeInTheDocument();
+
+        fireEvent.click(screen.getByText("Didn't receive the email?"));
+
+        expect(
+            screen.getByText('The email is in your spam folder (Sometimes things get lost there).')
+        ).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                'You accidentally gave us another email address (Usually a work or a personal one instead of the one you meant).'
+            )
+        ).toBeInTheDocument();
+        expect(
+            screen.getByText('The email address you entered had a mistake or typo (happens to the best of us).')
+        ).toBeInTheDocument();
+        expect(
+            screen.getByText("We can't deliver the email to this address (Usually because of firewalls or filtering).")
+        ).toBeInTheDocument();
+
+        fireEvent.click(screen.getByText('Resend email'));
+
+        expect(screen.getByText(/Resend email in \d+ seconds/)).toBeInTheDocument();
+    });
+
+    it('executes the resend email function when the button is clicked', () => {
+        render(<SentEmailContent platform='mt5' />, { wrapper });
+
+        fireEvent.click(screen.getByText("Didn't receive the email?"));
+        fireEvent.click(screen.getByText('Resend email'));
+        expect(useSendPasswordResetEmail).toBeCalled();
+    });
+
+    it('returns WalletError when it throws error', () => {
+        (useSendPasswordResetEmail as jest.Mock).mockReturnValue({
+            error: {
+                error: {
+                    code: 'error code',
+                    message: 'error message',
+                },
+            },
+        });
+
+        render(<SentEmailContent platform='mt5' />, { wrapper });
+        expect(screen.getByText('error code')).toBeInTheDocument();
+        expect(screen.getByText('error message')).toBeInTheDocument();
+    });
+});

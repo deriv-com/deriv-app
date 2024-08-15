9e425cfcee4d916c616a6aa7b583aed91d250814
hasan-deriv <abuhasan@deriv.com>
2024-08-15 17:33:36 +0800

    fix: missing imports

--- packages/cfd/src/Components/Routes/index.ts
+++ packages/cfd/src/Components/Routes/index.ts
@@ -1,4 +1,4 @@
-import BinaryRoutes from './binary-routes.jsx';
+import BinaryRoutes from './binary-routes';
 import RouteWithSubRoutes from './route-with-sub-routes.jsx';
 
 export default BinaryRoutes;

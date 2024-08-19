644c45dbdacce8ce8849a0796e50fbcac678b578
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-08-19 13:17:27 +0800

    test: add test cases for desktop wallets list component

---
+++ packages/wallets/src/components/DesktopWalletsList/__tests__/DesktopWalletsList.spec.tsx
@@ -0,0 +1,47 @@
+import React, { PropsWithChildren } from 'react';
+import { APIProvider, useActiveWalletAccount } from '@deriv/api-v2';
+import WalletsAuthProvider from '../../../AuthProvider';
+import { ModalProvider } from '../../ModalProvider';
+import { render, screen } from '@testing-library/react';
+import DesktopWalletsList from '../DesktopWalletsList';
+
+jest.mock('@deriv/api-v2', () => ({
+    ...jest.requireActual('@deriv/api-v2'),
+    useActiveWalletAccount: jest.fn(),
+}));
+
+const wrapper = ({ children }: PropsWithChildren) => {
+    return (
+        <APIProvider>
+            <WalletsAuthProvider>
+                <ModalProvider>{children}</ModalProvider>
+            </WalletsAuthProvider>
+        </APIProvider>
+    );
+};
+
+describe('DesktopWalletsList', () => {
+    it('renders the component when the activeWalletAccount is not initializing', () => {
+        const mockUseActiveWalletAccount = {
+            data: {
+                is_active: true,
+                is_virtual: false,
+                loginid: 'CRW1',
+            },
+            isInitializing: false,
+        };
+        (useActiveWalletAccount as jest.Mock).mockReturnValue(mockUseActiveWalletAccount);
+
+        render(<DesktopWalletsList />, { wrapper });
+        expect(screen.getByTestId('dt_wallets_container')).toBeInTheDocument();
+    });
+    it('renders the component when the activeWalletAccount is initializing', () => {
+        const mockUseActiveWalletAccount = {
+            isInitializing: true,
+        };
+        (useActiveWalletAccount as jest.Mock).mockReturnValue(mockUseActiveWalletAccount);
+
+        render(<DesktopWalletsList />, { wrapper });
+        expect(screen.getByTestId('dt_wallets_card_loader')).toBeInTheDocument();
+    });
+});

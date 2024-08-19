e436f8aaf3b325740203cc8c2aa32a30576c4ef7
Sandeep <sandeep@regentmarkets.com>
2024-08-19 13:46:26 +0800

    chore: :zap: added input limits back

--- packages/bot-web-ui/src/pages/server-side-bot/add-bot/inputs/add-input.tsx
+++ packages/bot-web-ui/src/pages/server-side-bot/add-bot/inputs/add-input.tsx
@@ -158,6 +158,8 @@ const QSInput: React.FC<TQSInput> = observer(
                                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleOnChange(e)}
                                         placeholder={is_exclusive_field ? '0' : ''}
                                         bottom_label={is_exclusive_field ? currency : ''}
+                                        max_characters={2}
+                                        maxLength={2}
                                         ref={input_ref}
                                     />
                                 </Popover>

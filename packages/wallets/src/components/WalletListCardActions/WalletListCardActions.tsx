1866396fe396b6b68d51d1a1133bd2a67b15f51d
aizad-deriv <aizad@deriv.com>
2024-08-20 13:29:44 +0800

    chore: update Text size for wallet action button

--- packages/wallets/src/components/WalletListCardActions/WalletListCardActions.tsx
+++ packages/wallets/src/components/WalletListCardActions/WalletListCardActions.tsx
@@ -94,7 +94,11 @@ const WalletListCardActions: React.FC<TProps> = ({ accountsActiveTabIndex }) => 
                             }}
                             size='lg'
                         />
-                        <Text size='sm' weight={button.text === localize('Deposit') ? 'bold' : 'normal'}>
+                        <Text
+                            align='center'
+                            size='2xs'
+                            weight={button.text === localize('Deposit') ? 'bold' : 'normal'}
+                        >
                             {button.text}
                         </Text>
                     </div>

1894376dea17512418802fc7496655645aae0097
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-08-21 13:27:35 +0800

    feat: signup feature flag

--- packages/core/src/App/Components/Layout/Header/signup-button.jsx
+++ packages/core/src/App/Components/Layout/Header/signup-button.jsx
@@ -1,19 +1,44 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from '@deriv/components';
 import { redirectToSignUp } from '@deriv/shared';
 import { localize } from '@deriv/translations';
+import { useGrowthbookGetFeatureValue } from '@deriv/hooks';
 
-const SignupButton = ({ className }) => (
-    <Button
-        id='dt_signup_button'
-        className={className}
-        has_effect
-        text={localize('Sign up')}
-        onClick={() => redirectToSignUp()}
-        primary
-    />
-);
+const SignupButton = ({ className }) => {
+    const [trigger_os_signup] = useGrowthbookGetFeatureValue({
+        featureFlag: 'trigger_os_signup',
+        defaultValue: false,
+    });
+
+    const handleOutSystemsRedirection = () => {
+        switch (process.env.NODE_ENV) {
+            case 'production':
+                return 'https://hub.deriv.com/tradershub/signup';
+            case 'staging':
+                return 'https://staging-hub.deriv.com/tradershub/signup';
+            default:
+                return 'https://dev-hub.deriv.com/tradershub/signup';
+        }
+    };
+
+    const handleSignup = () => {
+        if (trigger_os_signup) {
+            window.open(handleOutSystemsRedirection());
+        } else redirectToSignUp();
+    };
+
+    return (
+        <Button
+            id='dt_signup_button'
+            className={className}
+            has_effect
+            text={localize('Sign up')}
+            onClick={handleSignup}
+            primary
+        />
+    );
+};
 
 SignupButton.propTypes = {
     className: PropTypes.string,

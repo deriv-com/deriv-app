0d91ceacd949d7c101e133020c6857317ae76b5b
Likhith Kolayari <likhith@regentmarkets.com>
2024-08-23 15:13:44 +0400

    fix: reset testcases

--- packages/account/src/Configs/__test__/user-profile-validation.spec.ts
+++ packages/account/src/Configs/__test__/user-profile-validation.spec.ts
@@ -58,7 +58,7 @@ describe('getPersonalDetailsBaseValidationSchema', () => {
 
         await expect(first_name.validate('John')).resolves.toBe('John');
         await expect(first_name.validate('')).rejects.toThrow('First name is required.');
-        await expect(first_name.validate('A')).rejects.toThrow('Letters, spaces, periods, hyphens, apostrophes only.');
+        await expect(first_name.validate('A1')).rejects.toThrow('Letters, spaces, periods, hyphens, apostrophes only.');
         await expect(
             first_name.validate('ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKL')
         ).rejects.toThrow('Enter no more than 50 characters.');
@@ -69,9 +69,49 @@ describe('getPersonalDetailsBaseValidationSchema', () => {
 
         await expect(last_name.validate('Doe')).resolves.toBe('Doe');
         await expect(last_name.validate('')).rejects.toThrow('Last name is required.');
-        await expect(last_name.validate('A')).rejects.toThrow('Letters, spaces, periods, hyphens, apostrophes only.');
+        await expect(last_name.validate('A2')).rejects.toThrow('Letters, spaces, periods, hyphens, apostrophes only.');
         await expect(
             last_name.validate('ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKL')
         ).rejects.toThrow('Enter no more than 50 characters.');
+    });
+
+    it('should throw an error if date of birth is invalid', async () => {
+        const { date_of_birth } = getPersonalDetailsBaseValidationSchema('svg').fields;
+
+        await expect(date_of_birth.validate('1990-01-01')).resolves.toBe('1990-01-01');
+        await expect(date_of_birth.validate('')).rejects.toThrow('Date of birth is required.');
+        await expect(date_of_birth.validate('2021-01-01')).rejects.toThrow('You must be 18 years old and above.');
+    });
+
+    it('should throw an error if phone number is invalid', async () => {
+        const { phone } = getPersonalDetailsBaseValidationSchema('svg').fields;
+
+        await expect(phone.validate('+1234567890')).resolves.toBe('+1234567890');
+        await expect(phone.validate('')).rejects.toThrow('Phone is required.');
+        await expect(phone.validate('1234567890')).rejects.toThrow(
+            'Please enter a valid phone number (e.g. +15417541234).'
+        );
+        await expect(phone.validate('1234567890'.repeat(4))).rejects.toThrow('You should enter 9-35 characters.');
+    });
+
+    it('should throw an error if place of birth is invalid', async () => {
+        const { place_of_birth } = getPersonalDetailsBaseValidationSchema('svg').fields;
+
+        await expect(place_of_birth.validate('Germany')).resolves.toBe('Germany');
+        await expect(place_of_birth.validate('')).rejects.toThrow('Place of birth is required.');
+    });
+
+    it('should throw an error if account opening reason is invalid', async () => {
+        const { account_opening_reason } = getPersonalDetailsBaseValidationSchema('svg').fields;
+
+        await expect(account_opening_reason.validate('Investment')).resolves.toBe('Investment');
+        await expect(account_opening_reason.validate('')).rejects.toThrow('Intended use of account is required.');
+    });
+
+    it('should throw an error if citizen is invalid', async () => {
+        const { citizen } = getPersonalDetailsBaseValidationSchema('maltainvest').fields;
+
+        await expect(citizen.validate('Germany')).resolves.toBe('Germany');
+        await expect(citizen.validate('')).rejects.toThrow('Citizenship is required.');
     });
 });

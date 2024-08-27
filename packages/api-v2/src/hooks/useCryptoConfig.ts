daa22bd370d440daeb3062f29d9478f69d14d129
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2024-08-27 12:06:34 +0300

    [WALL] DONT'T MERGE george / WALL-4406 / Adding XRP Crypto to Wallets (#16135)
    * [WALL] george / WALL-4519 / Support XRP Deposits and Withdrawals in Wallets (#42)
    
    * refactor(wallets): :recycle: refactor wallets deposit/withdraw visibility
    
    * fix(wallets): :ambulance: fix playwright tests
    
    * feat(wallets): :sparkles: add UI elements for XRP integration (#43)
    
    * feat(wallets): :sparkles: add UI elements for XRP integration
    
    * style: :lipstick: remove dark background
    
    * fix: :ambulance: prevent crypto_config call for doughflow providers
    
    * fix: hardcode min withdrawal value for xrp
    
    * fix: withdrawalCryptoValidator min value
    
    * fix: withdrawalCryptoValidators min withdrawal value
    
    * fix: get min value from fractional digits
    
    ---------
    
    Co-authored-by: lubega-deriv <lubega@deriv.com>

--- packages/api-v2/src/hooks/useCryptoConfig.ts
+++ packages/api-v2/src/hooks/useCryptoConfig.ts
@@ -3,10 +3,10 @@ import useAuthorize from './useAuthorize';
 import useAuthorizedQuery from '../useAuthorizedQuery';
 
 /** A custom hook to get the cryptocurrencies config information from `crypto_config` endpoint. */
-const useCryptoConfig = () => {
+const useCryptoConfig = (options?: Parameters<typeof useAuthorizedQuery<'crypto_config'>>[2]) => {
     const { data: authorizeData, isLoading: isAuthorizeLoading } = useAuthorize();
 
-    const { data, isLoading: isCryptConfigLoading, ...rest } = useAuthorizedQuery('crypto_config');
+    const { data, isLoading: isCryptConfigLoading, ...rest } = useAuthorizedQuery('crypto_config', {}, { ...options });
 
     const cryptoConfig = authorizeData?.currency
         ? data?.crypto_config?.currencies_config[authorizeData?.currency]

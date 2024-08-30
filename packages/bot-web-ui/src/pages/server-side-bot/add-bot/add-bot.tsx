6fd49debad444adba4f9031f48c5e0aa44573c40
shafin-deriv <shafin@regentmarkets.com>
2024-08-30 11:21:19 +0800

    fix: remove extra validation

--- packages/bot-web-ui/src/pages/server-side-bot/add-bot/add-bot.tsx
+++ packages/bot-web-ui/src/pages/server-side-bot/add-bot/add-bot.tsx
@@ -162,11 +162,7 @@ export const FormikWrapper: React.FC<TFormikWrapper> = observer(({ children, set
                         }
                         sub_schema[field.name] = schema;
                     } else if (field.validation.includes('required') && field?.type === 'text') {
-                        const schema = Yup.string()
-                            .required(localize('Field cannot be empty'))
-                            .test('not-only-spaces', localize('Field cannot be empty'), (value: string | undefined) => {
-                                return Boolean(value && value.trim().length > 0);
-                            });
+                        const schema = Yup.string().required(localize('Field cannot be empty'));
                         sub_schema[field.name] = schema;
                     }
                 }

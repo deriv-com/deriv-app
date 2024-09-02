f2be22b78da005d76a0a19fd67f150f2c6345ddc
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-09-02 13:14:48 +0800

    chore: checking

--- packages/trader/src/AppV2/Components/AccumulatorStats/accumulator-stats-description.tsx
+++ packages/trader/src/AppV2/Components/AccumulatorStats/accumulator-stats-description.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { ActionSheet, Text } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
+import VideoPlayer from '@deriv/components/src/components/video-player/video-player';
 
 const AccumulatorStatsDescription = ({ onActionSheetClose }: { onActionSheetClose: () => void }) => {
     return (
         <ActionSheet.Portal showHandlebar={false}>
             <div className='stats-description'>
-                <div className='stats-description__placeholder' />
+                <VideoPlayer
+                    className='stats-description__player'
+                    data_testid='dt_video_player'
+                    is_mobile
+                    src='https://customer-hhvo3ceuqt00w8g8.cloudflarestream.com/cfd0bd25af245c1c987106a247cbb81a/watch'
+                />
                 <div className='stats-description__content'>
                     <div className='stats-description__content__title'>
                         <Text size='lg' bold>

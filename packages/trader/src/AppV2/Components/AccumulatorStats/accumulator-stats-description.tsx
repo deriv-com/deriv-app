f29f24d905c4d619eb0fff8cea0681a4da0fff02
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-09-02 18:30:36 +0800

    fix: fix

--- packages/trader/src/AppV2/Components/AccumulatorStats/accumulator-stats-description.tsx
+++ packages/trader/src/AppV2/Components/AccumulatorStats/accumulator-stats-description.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { ActionSheet, Text } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
-import VideoPlayer from '@deriv/components/src/components/video-player/video-player';
 
 const AccumulatorStatsDescription = ({ onActionSheetClose }: { onActionSheetClose: () => void }) => {
+    const src = 'cfd0bd25af245c1c987106a247cbb81a';
+
     return (
         <ActionSheet.Portal showHandlebar={false}>
             <div className='stats-description'>
-                <VideoPlayer
+                <iframe
+                    allowFullScreen={false}
                     className='stats-description__player'
-                    data_testid='dt_video_player'
-                    is_mobile
-                    src='https://customer-hhvo3ceuqt00w8g8.cloudflarestream.com/cfd0bd25af245c1c987106a247cbb81a/watch'
+                    src={`https://iframe.cloudflarestream.com/${src}?muted=true&controls=false&autoplay=true&loop=true&preload=auto`}
                 />
+
                 <div className='stats-description__content'>
                     <div className='stats-description__content__title'>
                         <Text size='lg' bold>

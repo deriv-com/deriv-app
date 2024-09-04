fc46cb56d1a2a4d3e538bd6b6f786cb368039454
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-09-04 07:22:07 +0300

    DTRA / Kate / DTRA-1722 / Show error message on purchase button if index was turned off from BO (#16705)
    * refactor: add bo disabling text on purchase button
    
    * refactor: add conditional rendering and loader for acc info and barroer info and added tests
    
    * refactor: purchase button

--- packages/trader/src/AppV2/Components/TradeParameters/AccumulatorsInformation/__tests__/accumulators-information.spec.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/AccumulatorsInformation/__tests__/accumulators-information.spec.tsx
@@ -35,6 +35,26 @@ describe('AccumulatorsInformation', () => {
         expect(container).toBeEmptyDOMElement();
     });
 
+    it('should not render if there is API error ', () => {
+        default_mock_store.modules.trade.proposal_info = {
+            ACCU: {
+                has_error: true,
+            },
+        };
+        const { container } = mockAccumulatorsInformation();
+
+        expect(container).toBeEmptyDOMElement();
+    });
+
+    it('should render loader if maximum_payout is falsy but there is no API error', () => {
+        default_mock_store.modules.trade.maximum_payout = 0;
+        mockAccumulatorsInformation();
+
+        expect(screen.getByText('Max. payout')).toBeInTheDocument();
+        expect(screen.getByTestId('dt_skeleton')).toBeInTheDocument();
+        expect(screen.queryByText('4,000.00 USD')).not.toBeInTheDocument();
+    });
+
     it('should render description that is provided', () => {
         mockAccumulatorsInformation();
 
--- packages/trader/src/AppV2/Components/TradeParameters/BarrierInfo/barrier-info.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/BarrierInfo/barrier-info.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { observer } from '@deriv/stores';
+import { Skeleton } from '@deriv/components';
 import { useTraderStore } from 'Stores/useTraderStores';
 import { Text } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
 
 const BarrierInfo = observer(() => {
-    const { barrier_1 } = useTraderStore();
+    const { barrier_1, contract_type, proposal_info } = useTraderStore();
+    const contract_key = contract_type.toUpperCase();
+    const has_error = proposal_info[contract_key]?.has_error;
 
+    if (has_error) return null;
     return (
         <div className='barrier-info__container'>
             <Text size='sm'>
                 <Localize i18n_default_text='Barrier' />
             </Text>
-            <Text size='sm' bold>
-                {barrier_1}
-            </Text>
+            {barrier_1 ? (
+                <Text size='sm' bold>
+                    {barrier_1}
+                </Text>
+            ) : (
+                <Skeleton width={50} height={14} />
+            )}
         </div>
     );
 });

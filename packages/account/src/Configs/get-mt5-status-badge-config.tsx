ed8a12dd1d78a4ed89d0ae5dd5883846ee324727
Dhruv Neb <dhruv@regentmarkets.com>
2024-09-09 09:49:53 +0400

    fix: addes styling to icon

--- packages/account/src/Configs/get-mt5-status-badge-config.tsx
+++ packages/account/src/Configs/get-mt5-status-badge-config.tsx
@@ -10,80 +10,54 @@ const getMT5StatusBadgeConfig = (mt5_account_status: TMT5AccountStatus) => {
     switch (mt5_account_status) {
         case MT5_ACCOUNT_STATUS.PENDING:
             return {
-                text: (
-                    <Localize
-                        i18n_default_text='<1>In review</1>'
-                        components={[
-                            <Text
-                                key={0}
-                                weight='bold'
-                                size='xxxs'
-                                color='var(--status-warning)'
-                                className='link-pending'
-                            />,
-                        ]}
-                    />
-                ),
+                text: <Localize i18n_default_text='In review' className='link-pending' />,
                 icon: 'IcMt5Pending',
+                icon_size: '19',
             };
         case MT5_ACCOUNT_STATUS.FAILED:
             return {
                 text: (
                     <Localize
-                        i18n_default_text='<1>Failed</1>'
-                        components={[
-                            <Text key={0} weight='bold' size='xxxs' color='var(--status-danger)' />,
-                            <Text
-                                key={1}
-                                className='link-verification-failed'
-                                onClick={() => {
-                                    //TODO: default jurisdiction;
-                                }}
-                            />,
-                        ]}
+                        i18n_default_text='Failed'
+                        className='link-verification-failed'
+                        onClick={() => {
+                            //TODO: default jurisdiction;
+                        }}
                     />
                 ),
                 icon: 'IcMt5Failed',
+                icon_size: 19,
             };
         case MT5_ACCOUNT_STATUS.NEEDS_VERIFICATION: {
             return {
-                text: (
-                    <Localize
-                        i18n_default_text='<1>Needs Verification</1>'
-                        components={[
-                            <Text key={0} weight='bold' size='xxxs' color='var(--status-info)' />,
-                            <Text
-                                key={1}
-                                className='link-need-verification'
-                                onClick={() => {
-                                    //TODO: default jurisdiction;
-                                }}
-                            />,
-                        ]}
-                    />
-                ),
+                text: <Localize i18n_default_text='Needs Verification' />,
                 icon: 'IcMt5Verification',
+                icon_size: '19',
             };
         }
         case MT5_ACCOUNT_STATUS.MIGRATED_WITH_POSITION:
             return {
                 text: <Localize i18n_default_text='<0>No new positions</0>' components={[BadgeTextComponent]} />,
                 icon: 'IcAlertWarning',
+                icon_size: '19',
             };
         case MT5_ACCOUNT_STATUS.MIGRATED_WITHOUT_POSITION:
             return {
                 text: <Localize i18n_default_text='<0>Account closed</0>' components={[BadgeTextComponent]} />,
                 icon: 'IcAlertWarning',
+                icon_size: '19',
             };
         case MT5_ACCOUNT_STATUS.UNDER_MAINTENANCE:
             return {
                 text: <Localize i18n_default_text='<0>Server maintenance</0>' components={[BadgeTextComponent]} />,
                 icon: 'IcAlertWarning',
+                icon_size: '19',
             };
         case TRADING_PLATFORM_STATUS.UNAVAILABLE:
             return {
                 text: <Localize i18n_default_text='<0>Unavailable</0>' components={[BadgeTextComponent]} />,
                 icon: 'IcAlertWarning',
+                icon_size: '2',
             };
         default:
             return {

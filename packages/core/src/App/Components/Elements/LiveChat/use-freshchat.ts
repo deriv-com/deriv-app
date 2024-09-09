960da813b2d2c3c42e520160492f5eb14738503f
sergei-deriv <sergei@deriv.com>
2024-09-09 11:35:53 +0300

    feat: change script url

--- packages/core/src/App/Components/Elements/LiveChat/use-freshchat.ts
+++ packages/core/src/App/Components/Elements/LiveChat/use-freshchat.ts
@@ -1,12 +1,13 @@
 import { useStore } from '@deriv/stores';
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useScript } from 'usehooks-ts';
 
 const useFreshChat = () => {
-    useScript('//uae.fw-cdn.com/40116340/63296.js');
+    useScript('https://fw-cdn.com/11706964/4344125.js');
+
     const [isReady, setIsReady] = useState(false);
     const { client } = useStore();
-    const { getFreshworksToken } = client;
+    const { getFreshworksToken, is_logged_in } = client;
 
     const setDefaultSettings = () => {
         window.fcWidgetMessengerConfig = {
@@ -21,14 +22,16 @@ const useFreshChat = () => {
             onInit() {
                 window.fcWidget.on('widget:loaded', async () => {
                     setIsReady(true);
-                    const token = await getFreshworksToken();
-                    window.fcWidget.user.setProperties({ cf_user_jwt: token });
+                    if (is_logged_in) {
+                        const token = await getFreshworksToken();
+                        window.fcWidget.user.setProperties({ cf_user_jwt: token });
+                    }
                 });
             },
         };
     };
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         setDefaultSettings();
     }, []);
 

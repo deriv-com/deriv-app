b3f8d08a95bef4a8ab84e4586019784e7c199454
balakrishna-binary <bala.krishna@regentmarkets.com>
2024-09-09 16:36:37 +0800

    fix: sonar cloud

--- packages/trader/src/AppV2/Hooks/__tests__/useContractsForCompany.spec.tsx
+++ packages/trader/src/AppV2/Hooks/__tests__/useContractsForCompany.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import { mockStore } from '@deriv/stores';
 import TraderProviders from '../../../trader-providers';
 import { WS, getContractCategoriesConfig, getContractTypesConfig, cloneObject } from '@deriv/shared';

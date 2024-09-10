f4f9bf9573e4361c237baa2c99e04c036004f08f
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-09-10 15:49:34 +0300

    DTRA-1815 / Kate / [DTrader-V2]: Add Onboarding video for Draggable component (Trade types selection) (#16759)
    * refactor: prepare folder structure
    
    * refactor: change structure of flags on clocalstorage
    
    * chore: remove code smells
    
    * refactor: create reusable stream iframe component
    
    * refactor: add tests
    
    * chore: remove coed smalls
    
    * refactor: add id for teh video
    
    * refactor: apply suggestions
    
    * chore: remove extra waitfor in tests
    
    * chore: fix test case

--- packages/trader/src/AppV2/Containers/Trade/trade.tsx
+++ packages/trader/src/AppV2/Containers/Trade/trade.tsx
@@ -17,7 +17,7 @@ import TradeTypes from './trade-types';
 import MarketSelector from 'AppV2/Components/MarketSelector';
 import useContractsForCompany, { TContractTypesList } from 'AppV2/Hooks/useContractsForCompany';
 import AccumulatorStats from 'AppV2/Components/AccumulatorStats';
-import OnboardingGuide from 'AppV2/Components/OnboardingGuide';
+import OnboardingGuide from 'AppV2/Components/OnboardingGuide/GuideForPages';
 
 const Trade = observer(() => {
     const [is_minimized_params_visible, setIsMinimizedParamsVisible] = React.useState(false);
@@ -28,8 +28,11 @@ const Trade = observer(() => {
     const { active_symbols, contract_type, has_cancellation, symbol, is_accumulator, onMount, onChange, onUnmount } =
         useTraderStore();
     const { contract_types_list } = useContractsForCompany();
-    const [guide_dtrader_v2] = useLocalStorageData<boolean>('guide_dtrader_v2_trade_page', false);
-
+    const [guide_dtrader_v2] = useLocalStorageData<Record<string, boolean>>('guide_dtrader_v2', {
+        trade_types_selection: false,
+        trade_page: false,
+        positions_page: false,
+    });
     const trade_types = React.useMemo(() => {
         return Array.isArray(contract_types_list) && contract_types_list.length === 0
             ? []
@@ -107,7 +110,7 @@ const Trade = observer(() => {
                         </TradeParametersContainer>
                         <PurchaseButton />
                     </div>
-                    {!guide_dtrader_v2 && is_logged_in && <OnboardingGuide type='trade_page' />}
+                    {!guide_dtrader_v2?.trade_page && is_logged_in && <OnboardingGuide type='trade_page' />}
                 </React.Fragment>
             ) : (
                 <Loading.DTraderV2 />

f843e6a3433846f43dc29fb238c89f3abdecbf00
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-09-11 16:12:52 +0800

    chore: disable real demo switcher for disabled wallets

--- packages/wallets/src/components/WalletListHeader/WalletListHeader.tsx
+++ packages/wallets/src/components/WalletListHeader/WalletListHeader.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 import { useActiveWalletAccount, useWalletAccountsList } from '@deriv/api-v2';
 import { Localize } from '@deriv-com/translations';
 import { Text } from '@deriv-com/ui';
@@ -13,8 +14,9 @@ const WalletListHeader: React.FC = () => {
     const switchWalletAccount = useWalletAccountSwitcher();
 
     const demoAccount = wallets?.find(wallet => wallet.is_virtual)?.loginid;
-    const firstRealAccount = wallets?.find(wallet => !wallet.is_virtual)?.loginid;
-    const shouldShowSwitcher = demoAccount && firstRealAccount;
+    const firstRealAccount = wallets?.find(wallet => !wallet.is_virtual && !wallet.is_disabled)?.loginid;
+    const hasAnyActiveRealWallets = wallets?.some(wallet => !wallet.is_virtual && !wallet.is_disabled);
+    const shouldShowSwitcher = (demoAccount && firstRealAccount) || !hasAnyActiveRealWallets;
     const isDemo = activeWallet?.is_virtual;
     const [isChecked, setIsChecked] = useState(!isDemo);
 
@@ -46,17 +48,27 @@ const WalletListHeader: React.FC = () => {
                                 <Localize i18n_default_text='Demo' />
                             </Text>
                         </div>
-                        <div className='wallets-list-header__label-item'>
+                        <div
+                            className={classNames('wallets-list-header__label-item', {
+                                'wallets-list-header__label-item--disabled': !hasAnyActiveRealWallets,
+                            })}
+                        >
                             <Text size='sm'>
                                 <Localize i18n_default_text='Real' />
                             </Text>
                         </div>
                     </div>
-                    <label className='wallets-list-header__switcher' htmlFor='wallets-list-header__switcher'>
+                    <label
+                        className={classNames('wallets-list-header__switcher', {
+                            'wallets-list-header__switcher--disabled': !hasAnyActiveRealWallets,
+                        })}
+                        htmlFor='wallets-list-header__switcher'
+                    >
                         <input
                             checked={isChecked}
                             className='wallets-list-header__switcher-input'
                             data-testid='wallets_list_header__switcher_input'
+                            disabled={!hasAnyActiveRealWallets}
                             id='wallets-list-header__switcher'
                             onChange={handleToggle}
                             type='checkbox'

742b99171efb6d56632022534be33d8c64ba76d4
sergei-deriv <sergei@deriv.com>
2024-09-12 14:24:03 +0300

    feat: update link for freshchat

--- packages/hooks/src/useFreshChat.ts
+++ packages/hooks/src/useFreshChat.ts
@@ -3,7 +3,8 @@ import { useLayoutEffect } from 'react';
 import { useScript } from 'usehooks-ts';
 
 const useFreshChat = () => {
-    useScript('https://fw-cdn.com/11706964/4344125.js');
+    // useScript('https://fw-cdn.com/11706964/4344125.js');
+    useScript('https://uae.fw-cdn.com/40116340/63296.js');
 
     const { client } = useStore();
     const { getFreshworksToken, is_logged_in, loginid, email, account_settings, currency, residence, user_id } = client;
@@ -21,17 +22,17 @@ const useFreshChat = () => {
             onInit() {
                 window.fcWidget.on('widget:loaded', async () => {
                     if (is_logged_in && loginid) {
-                        let uuid = '';
-                        try {
-                            const res = await window.fcWidget.user.getUUID();
-                            uuid = res.data.uuid;
-                        } catch (error) {
-                            // eslint-disable-next-line no-console
-                            console.log('something wrong with UUID. Error = ', error);
-                        }
+                        // let uuid = '';
+                        // try {
+                        //     const res = await window.fcWidget.user.getUUID();
+                        //     uuid = res.data.uuid;
+                        // } catch (error) {
+                        //     // eslint-disable-next-line no-console
+                        //     console.log('something wrong with UUID. Error = ', error);
+                        // }
 
                         const token = await getFreshworksToken({
-                            freshchat_uuid: uuid,
+                            // freshchat_uuid: uuid,
                             user_id,
                             email,
                             first_name: account_settings.first_name ?? '',

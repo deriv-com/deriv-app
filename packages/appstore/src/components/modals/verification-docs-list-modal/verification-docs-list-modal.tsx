e9d59eee29b143923c844e1f18e4661634ca3843
Dhruv Neb <dhruv@regentmarkets.com>
2024-09-12 12:04:47 +0400

    fix: fixed icons

--- packages/appstore/src/components/modals/verification-docs-list-modal/verification-docs-list-modal.tsx
+++ packages/appstore/src/components/modals/verification-docs-list-modal/verification-docs-list-modal.tsx
@@ -31,7 +31,7 @@ const MT5BadgeStatus = (status: TAuthStatusCodes) => {
                 text: <Localize i18n_default_text='In review' />,
                 icon: 'IcMt5Pending',
             };
-        case AUTH_STATUS_CODES.REJECTED && AUTH_STATUS_CODES.SUSPENDED:
+        case AUTH_STATUS_CODES.REJECTED || AUTH_STATUS_CODES.SUSPENDED:
             return {
                 text: <Localize i18n_default_text='Failed' />,
                 icon: 'IcMt5Failed',

bd0b7ae3c19cf32c6b0fad85c6b7e1f862a502f8
kate-deriv <kate@regentmarkets.com>
2024-09-13 18:19:00 +0300

    fix: add class fr html tag in ui store and import quill

--- packages/components/src/components/loading/dtrader-v2-loaders/trade-loader.tsx
+++ packages/components/src/components/loading/dtrader-v2-loaders/trade-loader.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Skeleton } from '../../skeleton';
+import { Skeleton } from '@deriv-com/quill-ui';
 
 const TradeLoader = () => {
     return (
         <div className='loading-dtrader-v2__trade' data-testid='dt_trade_loader'>
             <div className='skeleton-box__trade-types'>
                 {[...new Array(6)].map((_, idx) => (
-                    <Skeleton key={idx} width={88} height={32} />
+                    <Skeleton.Square key={idx} width={88} height={32} />
                 ))}
             </div>
             <div className='skeleton-box__market'>
-                <Skeleton width={200} height={42} />
+                <Skeleton.Square width={200} height={42} />
             </div>
             <div className='skeleton-box__trade-params'>
                 <div className='skeleton-box__trade-params-row'>
-                    <Skeleton width={96} height={24} />
-                    <Skeleton width={96} height={32} />
+                    <Skeleton.Square width={96} height={24} />
+                    <Skeleton.Square width={96} height={32} />
                 </div>
                 <div className='skeleton-box__trade-params-column'>
                     {[...new Array(3)].map((_, idx) => (
-                        <Skeleton key={idx} height={56} />
+                        <Skeleton.Square key={idx} height={56} />
                     ))}
                 </div>
             </div>
             <div className='skeleton-box__chart'>
-                <Skeleton />
+                <Skeleton.Square />
             </div>
         </div>
     );
--- packages/core/src/Stores/ui-store.js
+++ packages/core/src/Stores/ui-store.js
@@ -26,7 +26,9 @@ export default class UIStore extends BaseStore {
     settings_extension = undefined;
     notification_messages_ui = undefined;
 
-    is_dark_mode_on = window?.matchMedia?.('(prefers-color-scheme: dark)').matches && isMobile();
+    is_dark_mode_on =
+        (window?.matchMedia?.('(prefers-color-scheme: dark)').matches && isMobile()) ||
+        JSON.parse(localStorage.getItem('ui_store'))?.is_dark_mode_on;
     is_settings_modal_on = false;
     is_language_settings_modal_on = false;
     is_mobile_language_menu_open = false;
@@ -438,12 +440,17 @@ export default class UIStore extends BaseStore {
         });
     }
     changeTheme = () => {
+        const html = document.querySelector('html');
         if (this.is_dark_mode_on) {
             document.body.classList.remove('theme--light');
+            html.classList.remove('light');
             document.body.classList.add('theme--dark');
+            html.classList.add('dark');
         } else {
             document.body.classList.remove('theme--dark');
+            html.classList.remove('dark');
             document.body.classList.add('theme--light');
+            html.classList.add('light');
         }
     };
 

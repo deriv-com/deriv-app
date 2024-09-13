baae0c0a22c61da51d6ddf9fe414a5fefda8aa79
kate-deriv <kate@regentmarkets.com>
2024-09-13 18:30:58 +0300

    refactor: remove quill

--- packages/components/src/components/loading/dtrader-v2-loaders/trade-loader.tsx
+++ packages/components/src/components/loading/dtrader-v2-loaders/trade-loader.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Skeleton } from '@deriv-com/quill-ui';
+import { Skeleton } from '../../skeleton';
 
 const TradeLoader = () => {
     return (
         <div className='loading-dtrader-v2__trade' data-testid='dt_trade_loader'>
             <div className='skeleton-box__trade-types'>
                 {[...new Array(6)].map((_, idx) => (
-                    <Skeleton.Square key={idx} width={88} height={32} />
+                    <Skeleton key={idx} width={88} height={32} />
                 ))}
             </div>
             <div className='skeleton-box__market'>
-                <Skeleton.Square width={200} height={42} />
+                <Skeleton width={200} height={42} />
             </div>
             <div className='skeleton-box__trade-params'>
                 <div className='skeleton-box__trade-params-row'>
-                    <Skeleton.Square width={96} height={24} />
-                    <Skeleton.Square width={96} height={32} />
+                    <Skeleton width={96} height={24} />
+                    <Skeleton width={96} height={32} />
                 </div>
                 <div className='skeleton-box__trade-params-column'>
                     {[...new Array(3)].map((_, idx) => (
-                        <Skeleton.Square key={idx} height={56} />
+                        <Skeleton key={idx} height={56} />
                     ))}
                 </div>
             </div>
             <div className='skeleton-box__chart'>
-                <Skeleton.Square />
+                <Skeleton />
             </div>
         </div>
     );

a5e6cfb7f4cc047dceecff5be475eeb96f511bcc
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-09-19 16:13:38 +0800

    chore: delete integration package

--- packages/integration/src/utils/asserts/asserts.ts
+++
@@ -1,6 +0,0 @@
-import { Page, expect } from '@playwright/test';
-
-export const assertField = async (page: Page, label: string, value: string) => {
-    const field = await page.getByLabel(label).first();
-    expect(await field.inputValue()).toBe(value);
-};

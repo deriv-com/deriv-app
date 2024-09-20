8e60571f5b3630266a25afcce1ae033de1409524
lubega-deriv <lubega@deriv.com>
2024-09-20 16:16:00 +0800

    fix: update api-v2 jest config

--- packages/api-v2/jest.config.js
+++ packages/api-v2/jest.config.js
@@ -2,4 +2,7 @@ const baseConfigForPackages = require('../../jest.config.base');
 
 module.exports = {
     ...baseConfigForPackages,
+    moduleNameMapper: {
+        '@deriv-com/translations': '<rootDir>/../../__mocks__/translation.mock.js',
+    },
 };

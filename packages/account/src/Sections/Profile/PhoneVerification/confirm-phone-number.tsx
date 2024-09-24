c813eca3232985a0add320c646e2253b4039ab3e
Tay Sui Sin <suisin@regentmarkets.com>
2024-09-24 16:42:30 +0800

    chore: replace regex in confirm your phone number screen

--- packages/account/src/Sections/Profile/PhoneVerification/confirm-phone-number.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/confirm-phone-number.tsx
@@ -49,7 +49,7 @@ const ConfirmPhoneNumber = observer(({ show_confirm_phone_number, setOtpVerifica
     }, [show_confirm_phone_number, trackPhoneVerificationEvents]);
 
     useEffect(() => {
-        setPhoneNumber(account_settings?.phone?.replace(/[^0-9]/g, '') || '');
+        setPhoneNumber(account_settings?.phone?.replace(/\D/g, '') || '');
     }, [account_settings?.phone]);
 
     useEffect(() => {

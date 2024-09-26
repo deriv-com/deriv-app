683e726920ce6aa8c52cad903633329e6d4fa8d9
Nada <nada.ajinas@deriv.com>
2024-09-26 10:24:16 +0400

    fix: deriv

--- packages/shared/src/utils/config/platform-config.ts
+++ packages/shared/src/utils/config/platform-config.ts
@@ -33,7 +33,7 @@ export const platforms: TPlatforms = {
     p2p_v2: {
         icon_text: undefined,
         is_hard_redirect: true,
-        platform_name: 'Deirv P2P',
+        platform_name: 'Deriv P2P',
         route_to_path: '',
         url: process.env.NODE_ENV === 'production' ? 'https://p2p.deriv.com' : 'https://staging-p2p.deriv.com',
     },

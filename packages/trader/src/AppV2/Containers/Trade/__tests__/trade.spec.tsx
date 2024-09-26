f7c0f1bc6e4d89eb2a8f963a44825e7321b383d2
Henry Hein <henry@regentmarkets.com>
2024-09-26 16:46:38 +0800

    fix: try again

--- packages/trader/src/AppV2/Containers/Trade/__tests__/trade.spec.tsx
+++ packages/trader/src/AppV2/Containers/Trade/__tests__/trade.spec.tsx
@@ -154,7 +154,7 @@ describe('Trade', () => {
 
         expect(screen.getByText('Trade Types Selection')).toBeInTheDocument();
         expect(screen.getByText('MarketSelector')).toBeInTheDocument();
-        expect(screen.getByText('Trade Parameters')).toBeInTheDocument();
+        expect(screen.getAllByText('Trade Parameters')).toHaveLength(2);
         expect(screen.getByText('Chart')).toBeInTheDocument();
         expect(screen.getByText('Purchase Button')).toBeInTheDocument();
         expect(screen.getByText('OnboardingGuide')).toBeInTheDocument();

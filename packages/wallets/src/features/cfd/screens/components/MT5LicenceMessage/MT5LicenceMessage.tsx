b5ee6fb615224ae67c1fe8401e516f0e2dee237f
Aum Bhatt <aum@deriv.com>
2024-09-26 15:05:51 +0400

    fix: temporary hardcoding of compnay number for non-regulated in MT5LicenceMessage

--- packages/wallets/src/features/cfd/screens/components/MT5LicenceMessage/MT5LicenceMessage.tsx
+++ packages/wallets/src/features/cfd/screens/components/MT5LicenceMessage/MT5LicenceMessage.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { LabelPairedTriangleExclamationMdBoldIcon } from '@deriv/quill-icons';
 import { Localize } from '@deriv-com/translations';
 import { InlineMessage, Text, useDevice } from '@deriv-com/ui';
 import { THooks } from '../../../../../types';
-import { getMarketTypeDetails, MARKET_TYPE, PlatformDetails } from '../../../constants';
+import { getMarketTypeDetails, JURISDICTION, MARKET_TYPE, PlatformDetails } from '../../../constants';
 import './MT5LicenceMessage.scss';
 
 type TMT5LicenseMessageProps = {
@@ -12,26 +11,35 @@ type TMT5LicenseMessageProps = {
 
 const MT5LicenseMessage: React.FC<TMT5LicenseMessageProps> = ({ account }) => {
     const { isDesktop } = useDevice();
+    const isSvg = account.shortcode === JURISDICTION.SVG;
 
     return (
-        <InlineMessage
-            className='wallets-mt5-licence-message'
-            icon={<LabelPairedTriangleExclamationMdBoldIcon />}
-            iconPosition='top'
-            variant='info'
-        >
+        <InlineMessage className='wallets-mt5-licence-message' iconPosition='top' variant='info'>
             <Text align='start' size={isDesktop ? '2xs' : 'xs'}>
-                <Localize
-                    i18n_default_text='You are adding your {{accountTitle}} {{accountName}} account under {{companyName}}, regulated by the {{regulatoryAuthority}}{{licenceNumber}}.'
-                    values={{
-                        accountName: getMarketTypeDetails(account.product)[account.market_type || MARKET_TYPE.ALL]
-                            .title,
-                        accountTitle: PlatformDetails.mt5.title,
-                        companyName: account.name,
-                        licenceNumber: account.licence_number ? ` (licence no. ${account.licence_number})` : '',
-                        regulatoryAuthority: account.regulatory_authority,
-                    }}
-                />
+                {isSvg ? (
+                    // TODO: remove this hardcoded logic for the company number for SVG once BE provides company_number key for non-regulated accounts
+                    <Localize
+                        i18n_default_text='You are adding your {{accountTitle}} {{accountName}} account under {{companyName}} (company no. 273 LLC 2020).'
+                        values={{
+                            accountName: getMarketTypeDetails(account.product)[account.market_type || MARKET_TYPE.ALL]
+                                .title,
+                            accountTitle: PlatformDetails.mt5.title,
+                            companyName: account.name,
+                        }}
+                    />
+                ) : (
+                    <Localize
+                        i18n_default_text='You are adding your {{accountTitle}} {{accountName}} account under {{companyName}}, regulated by the {{regulatoryAuthority}}{{licenceNumber}}.'
+                        values={{
+                            accountName: getMarketTypeDetails(account.product)[account.market_type || MARKET_TYPE.ALL]
+                                .title,
+                            accountTitle: PlatformDetails.mt5.title,
+                            companyName: account.name,
+                            licenceNumber: account.licence_number ? ` (licence no. ${account.licence_number})` : '',
+                            regulatoryAuthority: account.regulatory_authority,
+                        }}
+                    />
+                )}
             </Text>
         </InlineMessage>
     );

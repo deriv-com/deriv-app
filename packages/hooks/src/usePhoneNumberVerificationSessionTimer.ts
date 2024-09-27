0364a95904d1d48f1c60b03931dcb92fda7981a4
Sui Sin <103026762+suisin-deriv@users.noreply.github.com>
2024-09-27 18:53:20 +0800

    [PNV] UPM1635 Suisin/chore: fix growthbook FF off pnv routing issue (#16999)
    * chore: fix growthbook FF off pnv routing issue
    
    * chore: fix console log issue

--- packages/hooks/src/usePhoneNumberVerificationSessionTimer.ts
+++ packages/hooks/src/usePhoneNumberVerificationSessionTimer.ts
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { WS } from '@deriv/shared';
+import { useIsMounted, WS } from '@deriv/shared';
 import { useStore } from '@deriv/stores';
 import dayjs from 'dayjs';
 
@@ -10,6 +10,7 @@ const usePhoneNumberVerificationSessionTimer = () => {
     const { client } = useStore();
     const { account_settings } = client;
     const { phone_number_verification } = account_settings;
+    const isMounted = useIsMounted();
 
     const formatTime = useCallback((totalSeconds: number) => {
         if (totalSeconds <= 0) {
@@ -34,10 +35,12 @@ const usePhoneNumberVerificationSessionTimer = () => {
                 // next_request is to compare request_in_miliseconds with server's response time
                 const next_request = Math.round(request_in_milliseconds.diff(response.time * 1000) / 1000);
 
-                if (next_request > 0) {
-                    setSessionTimer(next_request);
-                } else {
-                    setSessionTimer(0);
+                if (isMounted()) {
+                    if (next_request > 0) {
+                        setSessionTimer(next_request);
+                    } else {
+                        setSessionTimer(0);
+                    }
                 }
             }
         });

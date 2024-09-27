f17c21cf55303d5204087569a6bd4bba88a60ef4
Aum Bhatt <aum@deriv.com>
2024-09-27 09:06:05 +0400

    fix: logic for getClientVerification util

--- packages/wallets/src/features/cfd/utils/utils.ts
+++ packages/wallets/src/features/cfd/utils/utils.ts
@@ -1,13 +1,13 @@
 import { THooks } from '../../../types';
 
 export const getClientVerification = (account: THooks.SortedMT5Accounts) => {
-    if (!('client_kyc_status' in account)) {
-        return false;
-    }
+    const hasClientKycStatus = 'client_kyc_status' in account;
 
-    const isPoiRequired = 'poi_status' in account.client_kyc_status ? account.poi_status !== 'verified' : false;
-    const isPoaRequired = 'poa_status' in account.client_kyc_status && account.poa_status !== 'verified';
-    const isValidTin = 'valid_tin' in account.client_kyc_status && !!account.valid_tin;
+    const isPoiRequired =
+        hasClientKycStatus && 'poi_status' in account.client_kyc_status ? account.poi_status !== 'verified' : false;
+    const isPoaRequired =
+        hasClientKycStatus && 'poa_status' in account.client_kyc_status && account.poa_status !== 'verified';
+    const isValidTin = hasClientKycStatus && 'valid_tin' in account.client_kyc_status && !!account.valid_tin;
 
     return {
         isPoaRequired,

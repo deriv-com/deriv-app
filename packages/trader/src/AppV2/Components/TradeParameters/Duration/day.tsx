6a060dcdfe3eefe8bd692b2081a8f032609a1322
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-09-30 15:48:04 +0800

    chore: setting end time when gmt time changes

--- packages/trader/src/AppV2/Components/TradeParameters/Duration/day.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Duration/day.tsx
@@ -5,6 +5,16 @@ import React, { useEffect } from 'react';
 
 import DaysDatepicker from './datepicker';
 import EndTimePicker from './timepicker';
+import { useStore } from '@deriv/stores';
+import { useTraderStore } from 'Stores/useTraderStores';
+import { setTime, toMoment } from '@deriv/shared';
+import { getBoundaries } from 'Stores/Modules/Trading/Helpers/end-time';
+import { getClosestTimeToCurrentGMT } from 'AppV2/Utils/trade-params-utils';
+
+const timeToMinutes = (time: string) => {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+};
 
 const DayInput = ({
     setEndTime,
@@ -17,8 +27,22 @@ const DayInput = ({
     end_date: Date;
     end_time: string;
 }) => {
+    const [current_gmt_time, setCurrentGmtTime] = React.useState<string>('');
     const [open, setOpen] = React.useState(false);
     const [open_timepicker, setOpenTimePicker] = React.useState(false);
+    const { common } = useStore();
+    const { server_time } = common;
+    const { expiry_date, market_open_times, market_close_times } = useTraderStore();
+
+    const moment_expiry_date = toMoment(expiry_date);
+    const market_open_datetimes = market_open_times.map(open_time => setTime(moment_expiry_date.clone(), open_time));
+    const market_close_datetimes = market_close_times.map(close_time =>
+        setTime(moment_expiry_date.clone(), close_time)
+    );
+    const server_datetime = toMoment(server_time);
+    const boundaries = getBoundaries(server_datetime.clone(), market_open_datetimes, market_close_datetimes);
+    const adjusted_start_time =
+        boundaries.start[0]?.clone().add(5, 'minutes').format('HH:mm') || getClosestTimeToCurrentGMT(5);
 
     const formatted_date = end_date.toLocaleDateString('en-GB', {
         day: 'numeric',
@@ -31,12 +55,24 @@ const DayInput = ({
         year: 'numeric',
     });
 
+    React.useEffect(() => {
+        const updateCurrentGmtTime = () => {
+            const now = new Date();
+            const gmt_time = now.toLocaleTimeString('en-GB', { timeZone: 'GMT', hour12: false });
+            setCurrentGmtTime(gmt_time);
+        };
+        updateCurrentGmtTime();
+        const interval = setInterval(updateCurrentGmtTime, 1000);
+        if (end_time !== '' && Math.abs(timeToMinutes(adjusted_start_time) - timeToMinutes(end_time)) === 5) {
+            setEndTime(adjusted_start_time);
+        }
+        return () => clearInterval(interval);
+    }, [end_time, adjusted_start_time]);
+
     useEffect(() => {
         if (formatted_date === formatted_current_date && !end_time) {
-            // the last time of the timepicker
-            setEndTime('23:55');
+            setEndTime(adjusted_start_time);
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [end_date]);
 
     return (
@@ -93,7 +129,14 @@ const DayInput = ({
                         }
                     />
                     {open && <DaysDatepicker end_date={end_date} setEndDate={setEndDate} />}
-                    {open_timepicker && <EndTimePicker setEndTime={setEndTime} end_time={end_time} />}
+                    {open_timepicker && (
+                        <EndTimePicker
+                            setEndTime={setEndTime}
+                            end_time={end_time}
+                            current_gmt_time={current_gmt_time}
+                            adjusted_start_time={adjusted_start_time}
+                        />
+                    )}
                     <ActionSheet.Footer
                         alignment='vertical'
                         shouldCloseOnPrimaryButtonClick={false}
--- packages/trader/src/AppV2/Components/TradeParameters/Duration/timepicker.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Duration/timepicker.tsx
@@ -1,40 +1,18 @@
 import { CaptionText, Text, TimeWheelPickerContainer } from '@deriv-com/quill-ui';
-import { setTime, toMoment } from '@deriv/shared';
-import { useStore } from '@deriv/stores';
 import { Localize } from '@deriv/translations';
-import { getClosestTimeToCurrentGMT } from 'AppV2/Utils/trade-params-utils';
 import React from 'react';
-import { getBoundaries } from 'Stores/Modules/Trading/Helpers/end-time';
-import { useTraderStore } from 'Stores/useTraderStores';
 
-const EndTimePicker = ({ end_time, setEndTime }: { end_time: string; setEndTime: (arg: string) => void }) => {
-    const [current_gmt_time, setCurrentGmtTime] = React.useState<string>('');
-    const { common } = useStore();
-    const { server_time } = common;
-    const { expiry_date, market_open_times, market_close_times } = useTraderStore();
-
-    const moment_expiry_date = toMoment(expiry_date);
-    const market_open_datetimes = market_open_times.map(open_time => setTime(moment_expiry_date.clone(), open_time));
-    const market_close_datetimes = market_close_times.map(close_time =>
-        setTime(moment_expiry_date.clone(), close_time)
-    );
-    const server_datetime = toMoment(server_time);
-    const boundaries = getBoundaries(server_datetime.clone(), market_open_datetimes, market_close_datetimes);
-    const adjusted_start_time =
-        boundaries.start[0]?.clone().add(5, 'minutes').format('HH:mm') || getClosestTimeToCurrentGMT(5);
-
-    React.useEffect(() => {
-        const updateCurrentGmtTime = () => {
-            const now = new Date();
-            const gmt_time = now.toLocaleTimeString('en-GB', { timeZone: 'GMT', hour12: false });
-            setCurrentGmtTime(gmt_time);
-        };
-        updateCurrentGmtTime();
-        const interval = setInterval(updateCurrentGmtTime, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
+const EndTimePicker = ({
+    end_time,
+    setEndTime,
+    current_gmt_time,
+    adjusted_start_time,
+}: {
+    end_time: string;
+    setEndTime: (arg: string) => void;
+    current_gmt_time: string;
+    adjusted_start_time: string;
+}) => {
     return (
         <div className='duration-container__time-picker'>
             <TimeWheelPickerContainer

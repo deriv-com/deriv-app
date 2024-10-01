3b56298867487ca6da92c0a14d732e12c9400624
Sui Sin <103026762+suisin-deriv@users.noreply.github.com>
2024-10-01 15:49:40 +0800

    Suisin/webrel3204/translation issue pnv flow (#17032)
    * chore: make const localize before passing it to useState value to test translation fix
    
    * chore: revert unrelated files
    
    * chore: empty commit
    
    * Update bn.json
    
    * Update pt.json
    
    * Update bn.json
    
    * chore: update bn and pt json file

--- packages/hooks/src/useSendOTPVerificationCode.tsx
+++ packages/hooks/src/useSendOTPVerificationCode.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useMutation } from '@deriv/api';
 import { TSocketError } from '@deriv/api/types';
-import { localize, Localize } from '@deriv/translations';
+import { Localize } from '@deriv/translations';
 import useRequestPhoneNumberOTP from './useRequestPhoneNumberOTP';
 import { useStore } from '@deriv/stores';
 import useSettings from './useSettings';
@@ -30,13 +30,27 @@ const useSendOTPVerificationCode = () => {
         requestOnWhatsApp,
     } = useRequestPhoneNumberOTP();
 
+    const phone_code_expired_message = <Localize i18n_default_text='Code expired. Get a new one.' />;
+    const email_code_expired_message = <Localize i18n_default_text='Code expired. Get a new code.' />;
+    const invalid_phone_code_message = <Localize i18n_default_text='Invalid code. Try again.' />;
+    const invalid_email_code_message = <Localize i18n_default_text='Invalid code. Try again or get a new code.' />;
+    const request_other_carrier_message = (
+        <Localize
+            i18n_default_text="We're unable to send codes via {{ current_carrier }} right now. Get your code by {{other_carriers}}."
+            values={{
+                current_carrier: getCurrentCarrier(),
+                other_carriers: getOtherCarrier(),
+            }}
+        />
+    );
+
     const formatPhoneOtpError = (error: TSocketError<'phone_number_verify'>['error']) => {
         switch (error.code) {
             case 'PhoneCodeExpired':
-                setPhoneOtpErrorMessage(localize('Code expired. Get a new one.'));
+                setPhoneOtpErrorMessage(phone_code_expired_message);
                 break;
             case 'InvalidOTP':
-                setPhoneOtpErrorMessage(localize('Invalid code. Try again.'));
+                setPhoneOtpErrorMessage(invalid_phone_code_message);
                 break;
             case 'NoAttemptsLeft':
                 refetch();
@@ -52,10 +66,10 @@ const useSendOTPVerificationCode = () => {
     const formatEmailOtpError = (error: TSocketError<'phone_number_challenge'>['error']) => {
         switch (error.code) {
             case 'EmailCodeExpired':
-                setPhoneOtpErrorMessage(localize('Code expired. Get a new code.'));
+                setPhoneOtpErrorMessage(email_code_expired_message);
                 break;
             case 'InvalidToken':
-                setPhoneOtpErrorMessage(localize('Invalid code. Try again or get a new code.'));
+                setPhoneOtpErrorMessage(invalid_email_code_message);
                 break;
             case 'NoAttemptsLeft':
                 refetch();
@@ -63,15 +77,7 @@ const useSendOTPVerificationCode = () => {
                 setShowCoolDownPeriodModal(true);
                 break;
             case 'PhoneNumberVerificationSuspended':
-                setPhoneOtpErrorMessage(
-                    <Localize
-                        i18n_default_text="We're unable to send codes via {{ current_carrier }} right now. Get your code by {{other_carriers}}."
-                        values={{
-                            current_carrier: getCurrentCarrier(),
-                            other_carriers: getOtherCarrier(),
-                        }}
-                    />
-                );
+                setPhoneOtpErrorMessage(request_other_carrier_message);
                 break;
             default:
                 setPhoneOtpErrorMessage(error.message);

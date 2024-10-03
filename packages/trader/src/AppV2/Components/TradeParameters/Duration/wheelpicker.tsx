6686a275cdd43a5c3aad689a6475fbd25cf6c362
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-10-03 17:15:06 +0800

    chore: setting sheet height when only one unit

--- packages/trader/src/AppV2/Components/TradeParameters/Duration/wheelpicker.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Duration/wheelpicker.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-lonely-if */
 import { WheelPickerContainer } from '@deriv-com/quill-ui';
 import { observer } from '@deriv/stores';
 import { getOptionPerUnit } from 'AppV2/Utils/trade-params-utils';
@@ -21,36 +22,41 @@ const DurationWheelPicker = observer(
         is24_hour_selected: boolean;
         setIs24HourSelected: (arg: boolean) => void;
     }) => {
-        const { duration_min_max } = useTraderStore();
-
+        const { duration_min_max, duration_units_list } = useTraderStore();
         const options = React.useMemo(() => getOptionPerUnit(unit, duration_min_max), [unit, duration_min_max]);
 
+        const handleContainerHeight = () => {
+            if (unit === 'd') {
+                return '228px';
+            }
+            return duration_units_list.length === 1 ? '230px' : '268px';
+        };
+
         return (
-            <div className={clsx('duration-container__wheel-picker-container')}>
-                <div
-                    className={clsx({
-                        'duration-container__wheel-picker-container__loading': false,
-                    })}
-                >
-                    <WheelPickerContainer
-                        data={!is24_hour_selected ? options : [options[0], [{ ...options[1][0] }]]}
-                        defaultValue={[String(selected_time)]}
-                        containerHeight={unit == 'd' ? '228px' : '268px'}
-                        inputValues={unit == 'h' ? selected_hour : selected_time}
-                        setInputValues={(index, val) => {
-                            if (unit == 'h') {
-                                if (index == 0 && val === 24) {
-                                    setIs24HourSelected(true);
-                                } else if (index == 0 && val !== 24) {
-                                    setIs24HourSelected(false);
-                                }
-                            } else {
+            <div
+                className={clsx('duration-container__wheel-picker-container', {
+                    'duration-container__wheel-picker-container__single':
+                        duration_units_list.length == 1 && unit !== 'd',
+                })}
+            >
+                <WheelPickerContainer
+                    data={!is24_hour_selected ? options : [options[0], [{ ...options[1][0] }]]}
+                    defaultValue={[String(selected_time)]}
+                    containerHeight={handleContainerHeight()}
+                    inputValues={unit == 'h' ? selected_hour : selected_time}
+                    setInputValues={(index, val) => {
+                        if (unit == 'h') {
+                            if (index == 0 && val === 24) {
+                                setIs24HourSelected(true);
+                            } else if (index == 0 && val !== 24) {
                                 setIs24HourSelected(false);
                             }
-                            setWheelPickerValue(index, val);
-                        }}
-                    />
-                </div>
+                        } else {
+                            setIs24HourSelected(false);
+                        }
+                        setWheelPickerValue(index, val);
+                    }}
+                />
             </div>
         );
     }

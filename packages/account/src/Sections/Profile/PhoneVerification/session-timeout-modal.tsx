79a07632a354b61f793c1dc69169a043c71870f4
Tay Sui Sin <suisin@regentmarkets.com>
2024-10-04 15:17:58 +0800

    chore: update session timeout tracking event

--- packages/account/src/Sections/Profile/PhoneVerification/session-timeout-modal.tsx
+++ packages/account/src/Sections/Profile/PhoneVerification/session-timeout-modal.tsx
@@ -7,21 +7,33 @@ import { useDevice } from '@deriv-com/ui';
 import { observer, useStore } from '@deriv/stores';
 import { useEffect } from 'react';
 
-const SessionTimeoutModal = observer(() => {
+const SessionTimeoutModal = observer(({ is_at_otp_verification }: { is_at_otp_verification: boolean }) => {
     const { isMobile } = useDevice();
     const history = useHistory();
     const { localize } = useTranslations();
     const { should_show_session_timeout_modal } = usePhoneNumberVerificationSessionTimer();
     const { ui } = useStore();
-    const { is_phone_verification_completed, setShouldShowPhoneNumberOTP, setIsForcedToExitPnv } = ui;
+    const {
+        is_phone_verification_completed,
+        setShouldShowPhoneNumberOTP,
+        setIsForcedToExitPnv,
+        should_show_phone_number_otp,
+    } = ui;
     const { trackPhoneVerificationEvents } = usePhoneVerificationAnalytics();
+
+    const getSubformName = () => {
+        if (is_at_otp_verification) {
+            return should_show_phone_number_otp ? 'verify_phone_otp_screen' : 'verify_email_screen';
+        }
+        return 'verify_phone_screen';
+    };
 
     useEffect(() => {
         if (should_show_session_timeout_modal) {
             setIsForcedToExitPnv(true);
             trackPhoneVerificationEvents({
-                action: 'open',
-                subform_name: 'session_timeout_popup',
+                action: 'open_session_expired_popup',
+                subform_name: getSubformName(),
             });
         }
     }, [should_show_session_timeout_modal, trackPhoneVerificationEvents]);

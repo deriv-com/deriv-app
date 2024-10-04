fe21edb578538560c2d87d921b6d93d14554e266
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-10-04 15:20:25 +0800

    build: update root config and packages

--- types/global.d.ts
+++ types/global.d.ts
@@ -4,7 +4,9 @@ declare global {
         LiveChatWidget: {
             init: () => void;
             on: (key: string, callback: VoidFunction) => void;
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             get: (key: string) => any;
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             call: (key: string, value?: any) => void;
         };
         LC_API: {
@@ -14,9 +16,11 @@ declare global {
         TrackJS: { console: { log: (arg0: unknown[]) => void }; track: (arg0: object) => void };
         Blockly;
         Onfido: {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             init: (args: any) => any;
         };
         DD_RUM: object | undefined;
+        dataLayer: object[];
     }
 }
 

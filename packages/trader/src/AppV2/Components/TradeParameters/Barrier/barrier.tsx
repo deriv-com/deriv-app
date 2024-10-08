4c5402aa38f66e8c33c00bc220be26e05a9fe186
Akmal Djumakhodjaev <akmal@binary.com>
2024-10-08 11:52:07 +0800

    feat: add snackbar on trade page for barrier

--- packages/trader/src/AppV2/Components/TradeParameters/Barrier/barrier.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Barrier/barrier.tsx
@@ -8,16 +8,20 @@ import Carousel from 'AppV2/Components/Carousel';
 import BarrierDescription from './barrier-description';
 import BarrierInput from './barrier-input';
 import CarouselHeader from 'AppV2/Components/Carousel/carousel-header';
+import { useSnackbar } from '@deriv-com/quill-ui';
 
 type TDurationProps = {
     is_minimized?: boolean;
 };
 
 const Barrier = observer(({ is_minimized }: TDurationProps) => {
-    const { barrier_1, onChange, duration_unit, setV2ParamsInitialValues, v2_params_initial_values } = useTraderStore();
+    const { barrier_1, onChange, duration_unit, setV2ParamsInitialValues, v2_params_initial_values, validation_errors, proposal_info, trade_type_tab } = useTraderStore();
     const [is_open, setIsOpen] = React.useState(false);
     const [initialBarrierValue, setInitialBarrierValue] = React.useState('');
     const isDays = duration_unit == 'd';
+    const has_error = validation_errors.barrier_1.length > 0 || proposal_info?.[trade_type_tab]?.has_error;
+    const { addSnackbar } = useSnackbar();
+    const [barrier_error_shown, setBarrierErrorShown] = React.useState(false);
 
     const onClose = (is_saved = false) => {
         if (is_open) {
@@ -28,6 +32,21 @@ const Barrier = observer(({ is_minimized }: TDurationProps) => {
             setIsOpen(false);
         }
     };
+
+    React.useEffect(() => {
+        const has_error = proposal_info?.[trade_type_tab]?.has_error;
+        const error_field = proposal_info?.[trade_type_tab]?.error_field;
+        const message = proposal_info?.[trade_type_tab]?.message;
+
+        if (has_error && error_field === 'barrier' && !barrier_error_shown && !is_open && !is_minimized) {
+            addSnackbar({
+                message: message,
+                hasCloseButton: true,
+                status: 'fail',
+            });
+            setBarrierErrorShown(true);
+        }
+    }, [proposal_info]);
 
     const barrier_carousel_pages = [
         {
@@ -51,6 +70,7 @@ const Barrier = observer(({ is_minimized }: TDurationProps) => {
                 value={v2_params_initial_values.barrier_1 || barrier_1}
                 onClick={() => setIsOpen(true)}
                 className={clsx('trade-params__option', is_minimized && 'trade-params__option--minimized')}
+                status={has_error && !is_open ? 'error' : undefined}
             />
             <ActionSheet.Root isOpen={is_open} onClose={() => onClose(false)} position='left' expandable={false}>
                 <ActionSheet.Portal shouldCloseOnDrag>

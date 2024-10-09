19fe36f38ffbdac4ce46be7aefaec409d68de32d
Rupato <rupato@deriv.com>
2024-10-09 13:45:15 +0800

    fix: dbot console errors

--- packages/bot-web-ui/src/pages/tutorials/dbot-tours/onboarding-tour/onboarding-tour-mobile.tsx
+++ packages/bot-web-ui/src/pages/tutorials/dbot-tours/onboarding-tour/onboarding-tour-mobile.tsx
@@ -32,14 +32,8 @@ const OnboardingTourMobile = observer(() => {
     const hide_prev_button = [1, 2, 8];
     const is_tour_active = active_tour === 'onboarding';
     
-    const getItemFromLocalStorage = (key: string): Promise<string | null> => {
-        return new Promise(resolve => {
-            const value = getSetting(key);
-            resolve(value);
-        });
-    };
-    const checkTokenForTour = async () => {
-        const token = await getItemFromLocalStorage('onboard_tour_token');
+    const checkTokenForTour = () => {
+        const token = getSetting('onboard_tour_token');
         if (!token && active_tab === 0) {
             setActiveTour('onboarding');
         }

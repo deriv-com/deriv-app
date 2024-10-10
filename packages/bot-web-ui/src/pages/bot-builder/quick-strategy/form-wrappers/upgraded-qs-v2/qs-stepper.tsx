33140e3d9ca199068ba64b7494e549bdc978fa56
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2024-10-10 11:48:26 +0300

    add Stepper component from Quill, install quill libraries, update the version

--- packages/bot-web-ui/src/pages/bot-builder/quick-strategy/form-wrappers/upgraded-qs-v2/qs-stepper.tsx
+++ packages/bot-web-ui/src/pages/bot-builder/quick-strategy/form-wrappers/upgraded-qs-v2/qs-stepper.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Stepper } from '@deriv-com/quill-ui';
 import { LinearProgressBar } from '@deriv-com/ui';
 import { QsSteps } from './trade-constants';
-import { localize } from '@deriv/translations';
 
 type TQSStepper = {
     current_step: QsSteps;
@@ -9,22 +9,16 @@ type TQSStepper = {
 };
 
 const QSStepper = ({ current_step, is_mobile = false }: TQSStepper) => {
-    const items = [
-        { name: 'strategy_template', label: 'Strategy template' },
-        { name: 'trade_parameters', label: 'Trade parameters' },
-    ];
-
     const percentage = current_step === QsSteps.StrategyCompleted ? 100 : 50;
-
     return is_mobile ? (
         <LinearProgressBar percentage={percentage} label='' danger_limit={101} is_loading={false} warning_limit={0} />
     ) : (
-        <React.Fragment>
-            {items.map(item => (
-                <label key={item.name}>{localize(item.label)}</label>
-            ))}
-            <div>{`current_step: ${current_step}`}</div>
-        </React.Fragment>
+        <div className='qs-stepper'>
+            <Stepper.Vertical
+                currentStep={current_step}
+                labels={['Default', 'Strategy template', 'Trade parameters']}
+            />
+        </div>
     );
 };
 

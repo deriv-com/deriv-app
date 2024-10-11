e288681ad290fa4073b0a748cffcfb8b9511e82c
vinu-deriv <vinu@regentmarkets.com>
2024-10-11 10:23:24 +0400

    feat: added growthbook feature flag for dtrader-v2

--- packages/trader/src/index.tsx
+++ packages/trader/src/index.tsx
@@ -1,27 +1,53 @@
 import React from 'react';
 import { getPositionsV2TabIndexFromURL, isDTraderV2, makeLazyLoader, moduleLoader, routes } from '@deriv/shared';
 import { Loading } from '@deriv/components';
+import { useGrowthbookGetFeatureValue } from '@deriv/hooks';
+import { TCoreStores } from '@deriv/stores/types';
+import { TWebSocket } from 'Types';
+import { Analytics } from '@deriv-com/analytics';
 
-const App = makeLazyLoader(
-    () =>
-        moduleLoader(() => {
-            if (isDTraderV2()) {
-                return import(/* webpackChunkName: "trader-app-v2", webpackPreload: true */ './AppV2/index');
-            }
-            return import(/* webpackChunkName: "trader-app", webpackPreload: true */ './App/index');
-        }),
-    () => {
-        if (isDTraderV2())
-            return (
-                <Loading.DTraderV2
-                    initial_app_loading
-                    is_contract_details={window.location.pathname.startsWith('/contract/')}
-                    is_positions={window.location.pathname === routes.trader_positions}
-                    is_closed_tab={getPositionsV2TabIndexFromURL() === 1}
-                />
-            );
-        return <Loading />;
+type Apptypes = {
+    passthrough: {
+        root_store: TCoreStores;
+        WS: TWebSocket;
+    };
+};
+
+const AppLoader = makeLazyLoader(
+    () => moduleLoader(() => import(/* webpackChunkName: "trader-app", webpackPreload: true */ './App/index')),
+    () => <Loading />
+)() as React.ComponentType<Apptypes>;
+
+const AppV2Loader = makeLazyLoader(
+    () => moduleLoader(() => import(/* webpackChunkName: "trader-app-v2", webpackPreload: true */ './AppV2/index')),
+    () => (
+        <Loading.DTraderV2
+            initial_app_loading
+            is_contract_details={window.location.pathname.startsWith('/contract/')}
+            is_positions={window.location.pathname === routes.trader_positions}
+            is_closed_tab={getPositionsV2TabIndexFromURL() === 1}
+        />
+    )
+)() as React.ComponentType<Apptypes>;
+
+const App = ({ passthrough }: Apptypes) => {
+    const [dtrader_v2_enabled] = useGrowthbookGetFeatureValue({
+        featureFlag: 'dtrader_v2_enabled',
+    });
+    const [dtrader_v2_enabled_gb, setDTraderV2EnabledGb] = React.useState(false);
+
+    React.useEffect(() => {
+        setDTraderV2EnabledGb(Boolean(dtrader_v2_enabled));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dtrader_v2_enabled, Analytics?.getInstances?.().ab?.GrowthBook.getFeatures()]);
+
+    if (Analytics?.getInstances?.().ab?.GrowthBook.getFeatures()) {
+        return isDTraderV2() || (dtrader_v2_enabled_gb && window.innerWidth < 600) ? (
+            <AppV2Loader passthrough={passthrough} />
+        ) : (
+            <AppLoader passthrough={passthrough} />
+        );
     }
-)();
-
+    return <Loading />;
+};
 export default App;

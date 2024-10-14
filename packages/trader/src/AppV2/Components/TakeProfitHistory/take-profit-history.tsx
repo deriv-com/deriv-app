d3410b65dba8b4fc6060129a611fbb1ae2cb33e1
kate-deriv <kate@regentmarkets.com>
2024-10-14 16:26:58 +0300

    feat: add swipe handlers for carousel

--- packages/trader/src/AppV2/Components/TakeProfitHistory/take-profit-history.tsx
+++ packages/trader/src/AppV2/Components/TakeProfitHistory/take-profit-history.tsx
@@ -1,13 +1,16 @@
 import { Text, CaptionText, Pagination } from '@deriv-com/quill-ui';
 import { formatDate, formatMoney, formatTime, TContractStore } from '@deriv/shared';
 import CardWrapper from '../CardWrapper';
+import Carousel from 'AppV2/Components/Carousel';
 import React, { useState } from 'react';
 import clsx from 'classnames';
 import { localize, Localize } from '@deriv/translations';
 
+type THistory = TContractStore['contract_update_history'];
+
 type TContractHistory = {
     currency?: string;
-    history?: [] | TContractStore['contract_update_history'];
+    history?: [] | THistory;
 };
 
 type TPagination = {
@@ -16,20 +19,15 @@ type TPagination = {
 };
 
 const TakeProfitHistory = ({ history = [], currency }: TContractHistory) => {
-    const [currentPage, setCurrentPage] = useState(0);
-    const itemsPerPage = 4;
-    const totalPages = Math.ceil(history.length / itemsPerPage);
+    const [current_page, setCurrentPage] = useState(0);
+    const items_per_page = 4;
+    const total_pages = Math.ceil(history.length / items_per_page);
+    const has_tp = history.some(item => item.order_type === 'take_profit' || item.display_name === 'Take profit');
+    const has_sl = history.some(item => item.order_type === 'stop_loss' || item.display_name === 'Stop loss');
 
-    const config_with_pages = history.reduce((result, _item, index) => {
-        if (!(index % 4)) {
-            result.push(history.slice(index, index + itemsPerPage));
-        }
-        return result;
-    }, [] as TContractStore['contract_update_history'][]);
-
-    const currentItems = history.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
-    const has_tp = currentItems.some(item => item.order_type === 'take_profit' || item.display_name === 'Take profit');
-    const has_sl = currentItems.some(item => item.order_type === 'stop_loss' || item.display_name === 'Stop loss');
+    const handlePageChange = React.useCallback((pagination: TPagination) => {
+        setCurrentPage(pagination.currentPage - 1);
+    }, []);
 
     const getHistoryTitle = () => {
         if (has_tp && has_sl) return <Localize i18n_default_text='TP & SL history' />;
@@ -37,19 +35,20 @@ const TakeProfitHistory = ({ history = [], currency }: TContractHistory) => {
         if (has_sl) return <Localize i18n_default_text='SL history' />;
     };
 
-    const handlePageChange = (pagination: TPagination) => {
-        // console.log('pagination.currentPage - 1', pagination.currentPage - 1);
-        setCurrentPage(pagination.currentPage - 1);
-    };
+    if (!history.length) return null;
 
-    return (
-        <CardWrapper title={getHistoryTitle()} className='take-profit-history'>
-            <div
-                className={clsx('take-profit-history__table', {
-                    'take-profit-history__table--fixed-height': history.length > itemsPerPage,
-                })}
-            >
-                {currentItems.map((item, index) => (
+    const pages_config = (history as THistory).reduce((result: THistory[], _item: typeof history[0], index: number) => {
+        if (!(index % items_per_page)) {
+            result.push(history.slice(index, index + items_per_page));
+        }
+        return result;
+    }, []);
+
+    const pages = pages_config.map((array, index) => ({
+        id: index,
+        component: (
+            <React.Fragment>
+                {array.map((item, index) => (
                     <div key={`take-profit-history-${index}`} className='take-profit-history__table-row'>
                         <div
                             className={clsx('take-profit-history__table-cell', 'take-profit-history__table-cell--left')}
@@ -73,11 +72,29 @@ const TakeProfitHistory = ({ history = [], currency }: TContractHistory) => {
                         </div>
                     </div>
                 ))}
+            </React.Fragment>
+        ),
+    }));
+
+    return (
+        <CardWrapper title={getHistoryTitle()} className='take-profit-history'>
+            <div
+                className={clsx('take-profit-history__table', {
+                    'take-profit-history__table--fixed-height': history.length > items_per_page,
+                })}
+            >
+                <Carousel
+                    classname='take-profit-history__carousel'
+                    current_index={current_page}
+                    is_swipeable
+                    pages={pages}
+                    setCurrentIndex={setCurrentPage}
+                />
             </div>
-            {totalPages > 1 && (
+            {total_pages > 1 && (
                 <Pagination
                     contentLength={history.length}
-                    contentPerPage={itemsPerPage}
+                    contentPerPage={items_per_page}
                     hideChevron
                     onClickPagination={handlePageChange}
                     variant='bullet'

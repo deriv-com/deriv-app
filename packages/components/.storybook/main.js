e7be3088689d0a9a020b9eb39ffa2b4f50d49676
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-10-15 16:55:51 +0800

    build: update packages in components

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,78 +1,91 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin-v6');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-webpack5',
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+
+  framework: '@storybook/react',
+
+  core: {
+    builder: {
+      name: '@storybook/builder-webpack5',
+      options: {},
     },
-    webpackFinal: async config => {
-        config?.module?.rules?.push({
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-                {
-                    loader: 'sass-resources-loader',
-                    options: {
-                        // Provide path to the file with resources
-                        resources: require('@deriv/shared/src/styles/index.js'),
-                    },
-                },
-            ],
-        });
+  },
 
-        config.resolve.alias = {
-            ...config.resolve?.alias,
-            Components: path.resolve(__dirname, '../src/components'),
-            Stories: path.resolve(__dirname, '../stories'),
-            Shared: path.resolve(__dirname, './shared'),
-        };
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+        {
+          loader: 'sass-resources-loader',
+          options: {
+            resources: require('@deriv/shared/src/styles/index.js'),
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
 
-        config?.module?.rules?.push({
-            resolve: {
-                alias: {
-                    Components: path.resolve(__dirname, '../src/components'),
-                    Stories: path.resolve(__dirname, '../stories'),
-                },
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        });
-        config?.module?.rules?.push({
-            test: /\.js[x]?$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: [
-                        ['@babel/plugin-proposal-decorators', { legacy: true }],
-                        ['@babel/plugin-proposal-class-properties', { loose: true }],
-                        '@babel/plugin-proposal-export-default-from',
-                        '@babel/plugin-proposal-object-rest-spread',
-                        '@babel/plugin-proposal-export-namespace-from',
-                        '@babel/plugin-syntax-dynamic-import',
-                        '@babel/plugin-proposal-optional-chaining',
-                        '@babel/plugin-proposal-nullish-coalescing-operator',
-                        ['@babel/plugin-transform-private-methods', { loose: true }],
-                        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-                    ],
-                },
-            },
-        });
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      Components: path.resolve(__dirname, '../src/components'),
+      Stories: path.resolve(__dirname, '../stories'),
+      Shared: path.resolve(__dirname, './shared'),
+    };
 
-        config?.plugins?.push(
-            new CopyPlugin({
-                patterns: [
-                    { from: path.resolve(__dirname, '../lib/icon/sprites'), to: 'public/sprites', toType: 'dir' },
-                ],
-            })
-        );
-        // Return the altered config
-        return config;
-    },
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+          plugins: [
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            '@babel/plugin-proposal-export-default-from',
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-proposal-optional-chaining',
+            '@babel/plugin-proposal-nullish-coalescing-operator',
+            ['@babel/plugin-transform-private-methods', { loose: true }],
+            ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+          ],
+        },
+      },
+    });
+
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, '../lib/icon/sprites'),
+            to: 'public/sprites',
+            noErrorOnMissing: true,
+          },
+        ],
+      })
+    );
+
+    config.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx');
+    return config;
+  },
 };

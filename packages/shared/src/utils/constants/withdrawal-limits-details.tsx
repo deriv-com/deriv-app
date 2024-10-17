bc6342b568531dc014d1cea156c0bee845eb05b6
Tay Sui Sin <suisin@regentmarkets.com>
2024-10-17 13:50:38 +0800

    chore: update to use localize function instead of Localize component

--- packages/shared/src/utils/constants/withdrawal-limits-details.tsx
+++ packages/shared/src/utils/constants/withdrawal-limits-details.tsx
@@ -1,16 +1,12 @@
-import { Localize, localize } from '@deriv/translations';
+import { localize } from '@deriv/translations';
 
 export const getWithdrawalTitle = (withdrawal_type: string, num_of_days?: number) => {
     const titles = {
-        lifetime_limit: <Localize i18n_default_text='Total withdrawal allowed (Lifetime)' />,
-        num_of_days_limit: (
-            <Localize i18n_default_text='Total withdrawal allowed ({{num_of_days}} days).' values={{ num_of_days }} />
-        ),
-        withdrawal_since_inception_monetary: <Localize i18n_default_text='Total withdrawn (Lifetime)' />,
-        withdrawal_for_x_days_monetary: (
-            <Localize i18n_default_text='Total withdrawn ({{num_of_days}} days)' values={{ num_of_days }} />
-        ),
-        remainder: <Localize i18n_default_text='Maximum withdrawal remaining' />,
+        lifetime_limit: localize('Total withdrawal allowed (Lifetime)'),
+        num_of_days_limit: localize('Total withdrawal allowed ({{num_of_days}} days).', { num_of_days }),
+        withdrawal_since_inception_monetary: localize('Total withdrawn (Lifetime)'),
+        withdrawal_for_x_days_monetary: localize('Total withdrawn ({{num_of_days}} days)', { num_of_days }),
+        remainder: localize('Maximum withdrawal remaining'),
     };
     return titles[withdrawal_type as keyof typeof titles] || null;
 };

9011bbade41808420c8bd7e5e3b77be67e8d3e48
Nada <nada.ajinas@deriv.com>
2024-10-18 16:40:28 +0400

    fix: removed console

--- packages/p2p/src/pages/app.jsx
+++ packages/p2p/src/pages/app.jsx
@@ -50,14 +50,10 @@ const App = () => {
         if (isGBLoaded) {
             if (is_p2p_standalone_enabled) {
                 const current_url = window.location.href;
-                /* eslint-disable no-console */
                 const split_url = current_url.split('/p2p')[1] || '';
-                console.log('split_url', split_url);
                 const target_url = is_production ? URLConstants.derivP2pProduction : URLConstants.derivP2pStaging;
                 const redirection_url = target_url + split_url;
-                // window.location.href = targetUrl + splitUrl;
-                /* eslint-disable no-console */
-                console.log('targetUrl', redirection_url);
+                window.location.href = redirection_url;
             }
         }
     }, [isGBLoaded, is_p2p_standalone_enabled, is_production]);

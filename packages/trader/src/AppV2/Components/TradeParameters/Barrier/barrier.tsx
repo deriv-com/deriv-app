d7c74630423a69854812410e47fd61aaef339233
Akmal Djumakhodjaev <akmal@binary.com>
2024-10-22 17:09:10 +0800

    Akmal /  feat: barrier field improvements (#17088)
    * chore: previous fixes
    
    * feat: add decimal places
    
    * chore: previous stake fixes
    
    * feat: add snackbar on trade page for barrier
    
    * feat: fix formatting
    
    * fix: do not save value when switching tabs
    
    * fix: change label color
    
    * feat: reset snackbar after resetting the barrier
    
    * fix: declare in one line
    
    * fix: apply shorthand
    
    * feat: apply new improvements
    
    * fix: fixes batch 1
    
    * fix: fixes for barrier batch 2
    
    * fix: update test cases
    
    * fix: ensure initial barrier gets updated whenever barrier changes

--- packages/trader/src/AppV2/Components/TradeParameters/Barrier/barrier.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Barrier/barrier.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import { observer } from 'mobx-react';
-import { ActionSheet, TextField } from '@deriv-com/quill-ui';
+import { ActionSheet, TextField, useSnackbar } from '@deriv-com/quill-ui';
 import { Localize } from '@deriv/translations';
 import { useTraderStore } from 'Stores/useTraderStores';
 import Carousel from 'AppV2/Components/Carousel';
@@ -14,10 +14,24 @@ type TDurationProps = {
 };
 
 const Barrier = observer(({ is_minimized }: TDurationProps) => {
-    const { barrier_1, onChange, duration_unit, setV2ParamsInitialValues, v2_params_initial_values } = useTraderStore();
+    const {
+        barrier_1,
+        onChange,
+        duration_unit,
+        setV2ParamsInitialValues,
+        v2_params_initial_values,
+        validation_errors,
+        proposal_info,
+        trade_type_tab,
+    } = useTraderStore();
     const [is_open, setIsOpen] = React.useState(false);
     const [initialBarrierValue, setInitialBarrierValue] = React.useState('');
     const isDays = duration_unit == 'd';
+    const has_error =
+        validation_errors.barrier_1.length > 0 ||
+        (proposal_info?.[trade_type_tab]?.has_error && proposal_info?.[trade_type_tab]?.error_field === 'barrier');
+    const { addSnackbar } = useSnackbar();
+    const [barrier_error_shown, setBarrierErrorShown] = React.useState(false);
 
     const onClose = React.useCallback(
         (is_saved = false) => {
@@ -32,6 +46,34 @@ const Barrier = observer(({ is_minimized }: TDurationProps) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [initialBarrierValue, is_open]
     );
+
+    React.useEffect(() => {
+        if (v2_params_initial_values.barrier_1?.toString() !== barrier_1) {
+            setV2ParamsInitialValues({ value: barrier_1, name: 'barrier_1' });
+        }
+    }, [barrier_1]);
+
+    React.useEffect(() => {
+        const has_error = proposal_info?.[trade_type_tab]?.has_error;
+        const error_field = proposal_info?.[trade_type_tab]?.error_field;
+        const message = proposal_info?.[trade_type_tab]?.message;
+
+        if (has_error && error_field === 'barrier' && !barrier_error_shown && !is_open && !is_minimized) {
+            addSnackbar({
+                message,
+                hasCloseButton: true,
+                status: 'fail',
+                style: { marginBottom: '48px' },
+            });
+            setBarrierErrorShown(true);
+        }
+    }, [proposal_info]);
+
+    React.useEffect(() => {
+        if (is_open) {
+            setBarrierErrorShown(false);
+        }
+    }, [is_open]);
 
     const barrier_carousel_pages = [
         {
@@ -51,10 +93,12 @@ const Barrier = observer(({ is_minimized }: TDurationProps) => {
             <TextField
                 variant='fill'
                 readOnly
+                noStatusIcon
                 label={<Localize i18n_default_text='Barrier' key={`barrier${is_minimized ? '-minimized' : ''}`} />}
                 value={v2_params_initial_values.barrier_1 || barrier_1}
                 onClick={() => setIsOpen(true)}
                 className={clsx('trade-params__option', is_minimized && 'trade-params__option--minimized')}
+                status={has_error && !is_open ? 'error' : undefined}
             />
             <ActionSheet.Root
                 isOpen={is_open}

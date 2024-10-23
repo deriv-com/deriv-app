927209d8abc265f51361db9e41fdb85fd558034d
Tay Sui Sin <suisin@regentmarkets.com>
2024-10-23 14:47:42 +0800

    chore: add code dropdown in legacy settings page

--- packages/hooks/src/useGetPhoneNumberList.ts
+++ packages/hooks/src/useGetPhoneNumberList.ts
@@ -60,11 +60,18 @@ const useGetPhoneNumberList = () => {
         carriers: country.carriers,
     }));
 
+    const formatted_countries_list_for_core = countries.map(country => ({
+        text: `${country.display_name} (${country.calling_country_code})`,
+        value: country.calling_country_code,
+        disabled: false,
+    }));
+
     const selected_phone_code = getSelectedPhoneCode();
 
     const selected_country_list = getSelectedCountryList();
 
     return {
+        formatted_countries_list_for_core,
         formatted_countries_list,
         short_code_selected: clients_country,
         selected_phone_code,

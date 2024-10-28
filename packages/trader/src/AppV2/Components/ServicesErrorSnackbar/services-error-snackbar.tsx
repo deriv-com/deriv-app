eff27888683130278508f5f780318788c9728691
kate-deriv <kate@regentmarkets.com>
2024-10-28 16:40:27 +0300

    feat: add condition for action in the snackbar

--- packages/trader/src/AppV2/Components/ServicesErrorSnackbar/services-error-snackbar.tsx
+++ packages/trader/src/AppV2/Components/ServicesErrorSnackbar/services-error-snackbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { localize } from '@deriv/translations';
 import { useLocation } from 'react-router-dom';
 import { observer, useStore } from '@deriv/stores';
 import { useTraderStore } from 'Stores/useTraderStores';
 import { useSnackbar, SnackbarController } from '@deriv-com/quill-ui';
-import { isEmptyObject, isValidToCancel, routes } from '@deriv/shared';
+import { getStaticUrl, isEmptyObject, isValidToCancel, routes } from '@deriv/shared';
 import useContractDetails from 'AppV2/Hooks/useContractDetails';
-import { checkIsServiceModalError } from 'AppV2/Utils/layout-utils';
+import { checkIsServiceModalError, SERVICE_ERROR } from 'AppV2/Utils/layout-utils';
 
 const ServicesErrorSnackbar = observer(() => {
     const {
@@ -18,7 +19,7 @@ const ServicesErrorSnackbar = observer(() => {
     const { addSnackbar } = useSnackbar();
     const { pathname } = useLocation();
 
-    const { message } = services_error || {};
+    const { code, message } = services_error || {};
     const has_services_error = !isEmptyObject(services_error);
     const is_modal_error = checkIsServiceModalError({ services_error, is_mf_verification_pending_modal_visible });
 
@@ -29,11 +30,19 @@ const ServicesErrorSnackbar = observer(() => {
             return has_services_error;
         return false;
     };
+
     const should_show_error_snackbar = checkShouldShowErrorSnackBar();
+    const should_contain_action = should_show_error_snackbar && code === SERVICE_ERROR.COMPANY_WIDE_LIMIT_EXCEEDED;
     const bottom_position =
         location.pathname.startsWith('/contract/') && is_multiplier && isValidToCancel(contract_info)
             ? '104px'
             : '48px';
+    const action_props = {
+        actionText: localize('View'),
+        onActionClick: () => {
+            window.open(getStaticUrl('tnc/trading-terms.pdf', true));
+        },
+    };
 
     React.useEffect(() => {
         if (should_show_error_snackbar) {
@@ -47,6 +56,7 @@ const ServicesErrorSnackbar = observer(() => {
                     marginBottom: is_logged_in ? bottom_position : '-8px',
                     width: 'calc(100% - var(--core-spacing-800)',
                 },
+                ...(should_contain_action ? { action_props } : {}),
             });
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps

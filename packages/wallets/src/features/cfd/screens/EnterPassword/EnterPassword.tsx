ffc8e45c319c5451981545e4e130836dc6d26d84
heorhi-deriv <heorhi@binary.com>
2024-10-31 14:55:00 +0300

    chore: change weight

--- packages/wallets/src/features/cfd/screens/EnterPassword/EnterPassword.tsx
+++ packages/wallets/src/features/cfd/screens/EnterPassword/EnterPassword.tsx
@@ -70,7 +70,7 @@ const EnterPassword: React.FC<TProps> = ({
     return (
         <div className='wallets-enter-password'>
             {isDesktop && (
-                <Text align='start' lineHeight='xl' weight='bold'>
+                <Text align='start' lineHeight='xl'>
                     {modalTitle}
                 </Text>
             )}

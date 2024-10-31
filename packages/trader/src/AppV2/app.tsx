98e09b6de4677dc0863d23a84b20c62c995a3c27
kate-deriv <kate@regentmarkets.com>
2024-10-31 15:46:36 +0300

    fix: make dtrader style links to be always the last

--- packages/trader/src/AppV2/app.tsx
+++ packages/trader/src/AppV2/app.tsx
@@ -25,20 +25,36 @@ const App = ({ passthrough }: Apptypes) => {
         return () => root_store.ui.setPromptHandler(false);
     }, [root_store]);
 
+    React.useLayoutEffect(() => {
+        const head = document.head;
+        const links = head.querySelectorAll('link[rel="stylesheet"]');
+        const is_last_dtrader = (links[links.length - 1] as HTMLLinkElement)?.href?.includes('/trader');
+        const dtrader_links = [...links].filter(link => (link as HTMLLinkElement)?.href?.includes('/trader'));
+
+        if (is_last_dtrader) return;
+
+        const dtrader_links_clone = dtrader_links?.map(link => link?.cloneNode(true));
+        dtrader_links_clone.forEach(link => head.appendChild(link));
+
+        return () => dtrader_links_clone?.forEach(link => head.removeChild(link));
+    }, []);
+
     return (
-        <TraderProviders store={root_store}>
-            <ReportsStoreProvider>
-                <ModulesProvider store={root_store}>
-                    <NotificationsProvider>
-                        <SnackbarProvider>
-                            <Notifications />
-                            <Router />
-                            <ServicesErrorSnackbar />
-                        </SnackbarProvider>
-                    </NotificationsProvider>
-                </ModulesProvider>
-            </ReportsStoreProvider>
-        </TraderProviders>
+        <React.Fragment>
+            <TraderProviders store={root_store}>
+                <ReportsStoreProvider>
+                    <ModulesProvider store={root_store}>
+                        <NotificationsProvider>
+                            <SnackbarProvider>
+                                <Notifications />
+                                <Router />
+                                <ServicesErrorSnackbar />
+                            </SnackbarProvider>
+                        </NotificationsProvider>
+                    </ModulesProvider>
+                </ReportsStoreProvider>
+            </TraderProviders>
+        </React.Fragment>
     );
 };
 

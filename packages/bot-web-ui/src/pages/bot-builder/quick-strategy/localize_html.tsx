31c46b2ba77bd5204045b7bbb73c954e23b9f19d
rupato-deriv <rupato@deriv.com>
2024-11-13 13:21:46 +0800

    fix: tick count and take profit descriptions

--- packages/bot-web-ui/src/pages/bot-builder/quick-strategy/localize_html.tsx
+++ packages/bot-web-ui/src/pages/bot-builder/quick-strategy/localize_html.tsx
@@ -13,13 +13,13 @@ export const LocalizeHTMLForSellConditions = additional_data => {
                 <span>
                     <strong>{localize('Take Profit: ')}</strong>
                 </span>
-                <span>{localize('The position closes after the profit and loss crosses the take profit amount.')}</span>
+                <span>{localize('The position closes once its profit exceeds the take-profit amount')}</span>
             </div>
             <div className='sell_conditions__tick_count'>
                 <span>
                     <strong>{localize('Tick Count: ')}</strong>
                 </span>
-                <span>{localize('Counting the number of ticks before selling the position.')}</span>
+                <span>{localize('The holding period measured in ticks before the position is sold')}</span>
             </div>
             <div>
                 <Localize

eafe59256ddf6756b66bfb397afc292a44996f90
kate-deriv <kate@regentmarkets.com>
2024-11-14 11:49:28 +0300

    chore: add brackets

--- packages/trader/src/AppV2/Components/Filter/contract-type-filter.tsx
+++ packages/trader/src/AppV2/Components/Filter/contract-type-filter.tsx
@@ -40,7 +40,7 @@ const ContractTypeFilter = ({ contractTypeFilter, onApplyContractTypeFilter }: T
                 <Text size='sm'>
                     <Localize i18n_default_text='Trade types' />
                 </Text>
-                {!!changedOptions.length && <Text>{changedOptions.length}</Text>}
+                {!!changedOptions.length && <Text>({changedOptions.length})</Text>}
             </Chip.Standard>
             <ActionSheet.Root isOpen={isDropdownOpen} onClose={onActionSheetClose} position='left' expandable={false}>
                 <ActionSheet.Portal shouldCloseOnDrag>

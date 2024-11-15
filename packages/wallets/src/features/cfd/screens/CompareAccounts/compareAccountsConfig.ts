9d347e7e43feac7d0374e101ba17f7cd15bea6cc
Akmal Djumakhodjaev <akmal@binary.com>
2024-11-15 11:57:05 +0800

    Revert "[WALL] george / WALL-5105 / Remove the jurisdiction information on co…" (#17511)
    This reverts commit 0c0815a8e40662b4325e508b98e283caca0c7ad2.

--- packages/wallets/src/features/cfd/screens/CompareAccounts/compareAccountsConfig.ts
+++ packages/wallets/src/features/cfd/screens/CompareAccounts/compareAccountsConfig.ts
@@ -156,8 +156,15 @@ const getPlatformType = (platform: TPlatforms.All) => {
 };
 
 const cfdConfig = (localize: ReturnType<typeof useTranslations>['localize']) => ({
+    counterparty_company: localize('Deriv (SVG) LLC'),
+    counterparty_company_description: localize('Counterparty company'),
+    jurisdiction: localize('St. Vincent & Grenadines'),
+    jurisdiction_description: localize('Jurisdiction'),
     leverage: localize('Up to 1:1000'),
     leverage_description: localize('Maximum leverage'),
+    regulator: localize('Financial Commission'),
+    regulator_description: localize('Regulator/External dispute resolution'),
+    regulator_license: '',
     spread: localize('0.5 pips'),
     spread_description: localize('Spreads from'),
 });
@@ -166,24 +173,54 @@ const getJurisdictionDescription = (localize: ReturnType<typeof useTranslations>
     switch (shortcode) {
         case MARKET_TYPE_SHORTCODE.SYNTHETIC_BVI:
         case MARKET_TYPE_SHORTCODE.FINANCIAL_BVI:
-            return { ...cfdConfig(localize) };
+            return {
+                ...cfdConfig(localize),
+                counterparty_company: localize('Deriv (BVI) Ltd'),
+                jurisdiction: localize('British Virgin Islands'),
+                regulator: localize('British Virgin Islands Financial Services Commission'),
+                regulator_description: localize('Regulator/External dispute resolution'),
+                regulator_license: localize('(License no. SIBA/L/18/1114)'),
+            };
         case MARKET_TYPE_SHORTCODE.SYNTHETIC_VANUATU:
         case MARKET_TYPE_SHORTCODE.FINANCIAL_VANUATU:
-            return { ...cfdConfig(localize) };
+            return {
+                ...cfdConfig(localize),
+                counterparty_company: localize('Deriv (V) Ltd'),
+                jurisdiction: localize('Vanuatu'),
+                regulator: localize('Vanuatu Financial Services Commission'),
+                regulator_description: localize('Regulator/External dispute resolution'),
+                regulator_license: '',
+            };
         case MARKET_TYPE_SHORTCODE.FINANCIAL_LABUAN:
             return {
                 ...cfdConfig(localize),
+                counterparty_company: localize('Deriv (FX) Ltd'),
+                jurisdiction: localize('Labuan'),
                 leverage: localize('Up to 1:100'),
+                regulator: localize('Labuan Financial Services Authority'),
+                regulator_description: localize('Regulator/External dispute resolution'),
+                regulator_license: localize('(License no. MB/18/0024)'),
                 spread: localize('0.6 pips'),
             };
         case MARKET_TYPE_SHORTCODE.FINANCIAL_MALTAINVEST:
             return {
                 ...cfdConfig(localize),
+                counterparty_company: localize('Deriv Investments (Europe) Limited'),
+                jurisdiction: localize('Malta'),
                 leverage: localize('Up to 1:30'),
+                regulator: localize('Financial Commission'),
+                regulator_description: '',
+                regulator_license: localize(
+                    'Regulated by the Malta Financial Services Authority (MFSA) (licence no. IS/70156)'
+                ),
             };
         case MARKET_TYPE_SHORTCODE.ALL_ZERO_SPREAD_BVI:
             return {
                 ...cfdConfig(localize),
+                counterparty_company: localize('Deriv (BVI) Ltd'),
+                jurisdiction: localize('British Virgin Islands'),
+                regulator: localize('British Virgin Islands Financial Services Commission'),
+                regulator_license: localize('(License no. SIBA/L/18/1114)'),
                 spread: localize('0.0 pips'),
             };
         case MARKET_TYPE_SHORTCODE.ALL_DXTRADE:
@@ -195,4 +232,120 @@ const getJurisdictionDescription = (localize: ReturnType<typeof useTranslations>
     }
 };
 
-export { getHighlightedIconLabel, getJurisdictionDescription, getPlatformType };
+const acknowledgedStatus = ['pending', 'verified'];
+
+const getPoiAcknowledgedForMaltainvest = (authenticationInfo?: THooks.Authentication) => {
+    const services = authenticationInfo?.identity?.services ?? {};
+    const { manual: { status: manualStatus } = {}, onfido: { status: onfidoStatus } = {} } = services;
+
+    return [onfidoStatus, manualStatus].some(status => acknowledgedStatus.includes(status ?? ''));
+};
+
+const getPoiAcknowledgedForBviLabuanVanuatu = (authenticationInfo?: THooks.Authentication) => {
+    const services = authenticationInfo?.identity?.services ?? {};
+    const riskClassification = authenticationInfo?.risk_classification ?? '';
+    const {
+        idv: { status: idvStatus } = {},
+        manual: { status: manualStatus } = {},
+        onfido: { status: onfidoStatus } = {},
+    } = services;
+
+    if (riskClassification === 'high') {
+        return Boolean(onfidoStatus && acknowledgedStatus.includes(onfidoStatus));
+    }
+    return [idvStatus, onfidoStatus, manualStatus].some(status => acknowledgedStatus.includes(status ?? ''));
+};
+
+const shouldRestrictBviAccountCreation = (mt5Accounts: THooks.MT5AccountsList[]) =>
+    !!mt5Accounts.filter(item => item?.landing_company_short === 'bvi' && item?.status === 'poa_failed').length;
+
+const shouldRestrictVanuatuAccountCreation = (mt5Accounts: THooks.MT5AccountsList[]) =>
+    !!mt5Accounts.filter(item => item?.landing_company_short === 'vanuatu' && item?.status === 'poa_failed').length;
+
+const getAccountVerificationStatus = (
+    shortCode: THooks.AvailableMT5Accounts['shortcode'],
+    shouldRestrictBviAccountCreation: boolean,
+    shouldRestrictVanuatuAccountCreation: boolean,
+    hasSubmittedPersonalDetails: boolean,
+    authenticationInfo?: THooks.Authentication,
+    isDemo?: boolean
+) => {
+    const {
+        has_poa_been_attempted: hasPoaBeenAttempted,
+        has_poi_been_attempted: hasPoiBeenAttempted,
+        poa_status: poaStatus,
+    } = authenticationInfo || {};
+    const poiOrPoaNotSubmitted = !hasPoaBeenAttempted || !hasPoiBeenAttempted;
+    const poaAcknowledged = acknowledgedStatus.includes(poaStatus ?? '');
+
+    const poiAcknowledgedForMaltainvest = getPoiAcknowledgedForMaltainvest(authenticationInfo);
+    const poiAcknowledgedForBviLabuanVanuatu = getPoiAcknowledgedForBviLabuanVanuatu(authenticationInfo);
+
+    if (shortCode === JURISDICTION.SVG) {
+        return true;
+    }
+    if (shortCode === JURISDICTION.BVI) {
+        return (
+            poiAcknowledgedForBviLabuanVanuatu &&
+            !poiOrPoaNotSubmitted &&
+            !shouldRestrictBviAccountCreation &&
+            hasSubmittedPersonalDetails &&
+            poaAcknowledged
+        );
+    }
+    if (shortCode === JURISDICTION.VANUATU) {
+        return (
+            poiAcknowledgedForBviLabuanVanuatu &&
+            !poiOrPoaNotSubmitted &&
+            !shouldRestrictVanuatuAccountCreation &&
+            hasSubmittedPersonalDetails &&
+            poaAcknowledged
+        );
+    }
+    if (shortCode === JURISDICTION.LABUAN) {
+        return poiAcknowledgedForBviLabuanVanuatu && poaAcknowledged && hasSubmittedPersonalDetails;
+    }
+    if (shortCode === JURISDICTION.MALTAINVEST) {
+        return (poiAcknowledgedForMaltainvest && poaAcknowledged) || isDemo;
+    }
+};
+
+const isMt5AccountAdded = (
+    list: THooks.MT5AccountsList[],
+    marketType: TMarketTypes,
+    companyShortCode: string,
+    isDemo?: boolean
+) =>
+    list.some(item => {
+        const currentAccountType = isDemo ? 'demo' : 'real';
+        return (
+            item.account_type === currentAccountType &&
+            item.market_type === marketType &&
+            item.landing_company_short === companyShortCode &&
+            item.platform === CFD_PLATFORMS.MT5
+        );
+    });
+
+const isDxtradeAccountAdded = (list: THooks.DxtradeAccountsList[], isDemo?: boolean) =>
+    list.some(item => {
+        const currentAccountType = isDemo ? 'demo' : 'real';
+        return item.account_type === currentAccountType && item.platform === CFD_PLATFORMS.DXTRADE;
+    });
+
+const isCTraderAccountAdded = (list: THooks.CtraderAccountsList[], isDemo?: boolean) =>
+    list.some(item => {
+        const currentAccountType = isDemo ? 'demo' : 'real';
+        return item.account_type === currentAccountType && item.platform === CFD_PLATFORMS.CTRADER;
+    });
+
+export {
+    getAccountVerificationStatus,
+    getHighlightedIconLabel,
+    getJurisdictionDescription,
+    getPlatformType,
+    isCTraderAccountAdded,
+    isDxtradeAccountAdded,
+    isMt5AccountAdded,
+    shouldRestrictBviAccountCreation,
+    shouldRestrictVanuatuAccountCreation,
+};

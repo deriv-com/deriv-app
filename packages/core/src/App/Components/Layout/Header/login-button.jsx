680dfcb7477d773c028f221bd0e37b1a4685e30b
Thisyahlen <thisyahlen@deriv.com>
2024-11-18 11:48:23 +0800

    chore: oidc implementation

--- packages/core/src/App/Components/Layout/Header/login-button.jsx
+++ packages/core/src/App/Components/Layout/Header/login-button.jsx
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Button } from '@deriv/components';
 import { redirectToLogin } from '@deriv/shared';
 import { getLanguage, localize } from '@deriv/translations';

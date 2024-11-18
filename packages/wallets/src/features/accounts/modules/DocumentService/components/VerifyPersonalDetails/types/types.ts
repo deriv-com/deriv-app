1ee51a4c459457b91106024bed957c5904da0463
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2024-11-18 12:47:00 +0300

    [WALL] george / WALL-4908 / Default Service - Removal of unused POI/POA/TaxInfo components and files (#17485)
    * refactor(wallets): :fire: remove accounts folder, icons, related components
    
    * trigger build
    
    * Update AvailableMT5AcountsList.spec.tsx
    
    * refactor(wallets): :fire: remove unused icons

--- packages/wallets/src/features/accounts/modules/DocumentService/components/VerifyPersonalDetails/types/types.ts
+++
@@ -1,9 +0,0 @@
-import { FormatUtils } from '@deriv-com/utils';
-import { THooks } from '../../../../../../../types';
-
-export type TVerifyPersonalDetailsValues = {
-    arePersonalDetailsVerified?: boolean;
-    dateOfBirth?: ReturnType<typeof FormatUtils.getFormattedDateString>;
-    firstName: THooks.AccountSettings['first_name'];
-    lastName: THooks.AccountSettings['last_name'];
-};
--- packages/wallets/src/features/accounts/modules/ManualService/components/SelfieUpload/types/types.ts
+++
@@ -1,3 +0,0 @@
-export type TSelfieUploadValues = {
-    selfieFile?: File;
-};

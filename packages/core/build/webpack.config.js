fb3167e84749e9e1c4e36e57d3a377ad16be33d2
Tay Sui Sin <suisin@regentmarkets.com>
2024-11-19 14:02:49 +0800

    chore: revert webpack core changes

--- packages/core/build/webpack.config.js
+++ packages/core/build/webpack.config.js
@@ -74,9 +74,6 @@ module.exports = function (env) {
             publicPath: base,
             path: path.resolve(__dirname, '../dist'),
         },
-        externals: {
-            '@deriv-com/quill-ui': '@deriv-com/quill-ui',
-        },
         plugins: plugins({
             base,
             is_test_env: false,

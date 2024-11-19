2201a093c4260f110acc6f0b429f7a0c23f65c53
Prince <prince@deriv.com>
2024-11-19 11:00:38 +0400

    chore: revert remaining unnecessary changes

--- packages/p2p/src/components/dp2p-blocked/__tests__/dp2p-blocked-description.spec.tsx
+++ packages/p2p/src/components/dp2p-blocked/__tests__/dp2p-blocked-description.spec.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import { useStores } from 'Stores/index';
-
 import Dp2pBlockedDescription from '../dp2p-blocked-description';
 
 jest.mock('Stores', () => ({

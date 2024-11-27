20ebe71c3f672dc5747d9e6a0e8e6fb0a00a6b85
yauheni-deriv <yauheni@regentmarkets.com>
2024-11-27 09:04:25 +0300

    feat: init integration of endpoints

--- packages/account/src/Sections/Security/Passkeys/components/passkey-remove-with-email.tsx
+++ packages/account/src/Sections/Security/Passkeys/components/passkey-remove-with-email.tsx
@@ -6,22 +6,67 @@ import { InputGroupButton } from '@deriv-com/quill-ui';
 import { observer, useStore } from '@deriv/stores';
 import { PasskeysStatusLayout, TPasskeysButtonOnClicks } from './passkeys-status-layout';
 import { ResendCodeButton } from './resend-code-button';
+import { useReadLocalStorage } from 'usehooks-ts';
+import { makeCancellablePromise } from '@deriv/shared/src/utils/promise/make-cancellable-promise';
+
+type TTOTPResponse = { message: string; resend_timer: number };
+type TTOTPResponseError = { message: string; code: string };
 
 export const PasskeyRemoveWithEmail = observer(
     ({ onBackButtonClick, onPrimaryButtonClick }: TPasskeysButtonOnClicks) => {
         const { client } = useStore();
-        const { email_address } = client;
+        const { email_address, loginid } = client;
         const { localize } = useTranslations();
         const [code, setCode] = useState('');
         const [error, setError] = useState<string>('');
+        const [timer, setTimer] = useState(0);
+        const [isResendDisabled, setIsResendDisabled] = useState(true);
 
-        const getVerificationCodeToEmail = () => {
-            // API call for email verification code
+        // pass token to totp service to verify the account
+        const accounts = useReadLocalStorage<Record<string, { token: string }>>('client.accounts');
+        const login_token = accounts?.[loginid || '']?.token || '';
+        const test_token = process.env.TOTP_TEST_TOKEN || '';
+        const encoded_token = btoa(test_token || login_token);
+        const anon_key = process.env.ANON_KEY || '';
+        const totp_service_url = 'https://oadhrgilmqjitobovfpf.supabase.co/functions/v1/totp';
+
+        const getRemainingSeconds = (timestamp: number): number => {
+            const now = Date.now();
+            const difference = timestamp - now;
+            const secondsRemaining = Math.floor(difference / 1000);
+            return secondsRemaining;
+        };
+
+        const getVerificationCodeToEmail = async (endpoint: 'create' | 'resend') => {
+            try {
+                const resp = await fetch(`${totp_service_url}/${endpoint}`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${anon_key}`,
+                        UserAuthorization: `${encoded_token}`,
+                        'Content-Type': 'application/json',
+                    },
+                });
+                return resp.json();
+            } catch (e) {
+                setError((e as TTOTPResponseError).message);
+            }
         };
 
         useEffect(() => {
-            getVerificationCodeToEmail();
+            const fetchData = async () => {
+                const create_res = await getVerificationCodeToEmail('create');
+                setTimer(getRemainingSeconds(create_res?.resend_timer));
+                setIsResendDisabled(false);
+            };
+            fetchData();
         }, []);
+
+        const resendCode = async () => {
+            const resend_res = await getVerificationCodeToEmail('resend');
+            setTimer(getRemainingSeconds(resend_res?.resend_timer));
+            setIsResendDisabled(false);
+        };
 
         const onSubmitValues = () => {
             onPrimaryButtonClick?.(code);
@@ -74,7 +119,7 @@ export const PasskeyRemoveWithEmail = observer(
                         status={error ? 'error' : undefined}
                         message={error}
                     />
-                    <ResendCodeButton onResend={getVerificationCodeToEmail} initialDelay={10} />
+                    <ResendCodeButton onResend={resendCode} initialDelay={timer} isDisabled={isResendDisabled} />
                     <button onClick={onSetDummyError}>set error</button>
                     <button onClick={onClearError}>clear error</button>
                 </PasskeysStatusLayout>
--- packages/account/src/Sections/Security/Passkeys/components/resend-code-button.tsx
+++ packages/account/src/Sections/Security/Passkeys/components/resend-code-button.tsx
@@ -6,9 +6,10 @@ import { Localize } from '@deriv-com/translations';
 type TResendCodeButton = {
     initialDelay: number;
     onResend: () => void;
+    isDisabled: boolean;
 };
 
-export const ResendCodeButton = ({ initialDelay, onResend }: TResendCodeButton) => {
+export const ResendCodeButton = ({ initialDelay, onResend, isDisabled }: TResendCodeButton) => {
     const { isTimeout, timeRemaining } = useTimeout(initialDelay);
 
     const handleClick = useCallback(() => {
@@ -21,11 +22,11 @@ export const ResendCodeButton = ({ initialDelay, onResend }: TResendCodeButton) 
             className='passkeys-status__description-resend-code-button'
             variant='tertiary'
             onClick={handleClick}
-            disabled={!isTimeout}
+            disabled={!isTimeout || isDisabled}
             type='button'
             color='black'
             label={
-                isTimeout ? (
+                isTimeout && !isDisabled ? (
                     <Localize i18n_default_text='Resend Code' />
                 ) : (
                     <Localize i18n_default_text='Resend Code in {{timeRemaining}}s' values={{ timeRemaining }} />

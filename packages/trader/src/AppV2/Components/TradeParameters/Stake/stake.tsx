9b054071beee84ccb9d6fafa481ded54f8ad648f
kate-deriv <121025168+kate-deriv@users.noreply.github.com>
2024-12-04 13:15:20 +0300

    DTRA-2131 / Kate / Virtual keyboard hook (#17559)
    * refactor: add hook for detecting keyboard
    
    * chore: replace logs
    
    * chore: add one more check
    
    * refactor: add scrolling for rm
    
    * refactor: add tests
    
    * refactor: apply suggestions

--- packages/trader/src/AppV2/Components/TradeParameters/Stake/stake.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/Stake/stake.tsx
@@ -10,6 +10,7 @@ import { getDisplayedContractTypes } from 'AppV2/Utils/trade-types-utils';
 import StakeDetails from './stake-details';
 import useContractsForCompany from 'AppV2/Hooks/useContractsForCompany';
 import { TTradeParametersProps } from '../trade-parameters';
+import useIsVirtualKeyboardOpen from 'AppV2/Hooks/useIsVirtualKeyboardOpen';
 
 const Stake = observer(({ is_minimized }: TTradeParametersProps) => {
     const {
@@ -131,6 +132,14 @@ const Stake = observer(({ is_minimized }: TTradeParametersProps) => {
         min_stake,
         second_contract_payout,
     });
+
+    // scroll the page when a virtual keyboard pop up
+    const input_id = 'stake_input';
+    const { is_key_board_visible: should_scroll } = useIsVirtualKeyboardOpen(input_id);
+
+    React.useEffect(() => {
+        if (should_scroll) window?.scrollTo({ top: 225, behavior: 'smooth' });
+    }, [should_scroll]);
 
     React.useEffect(() => {
         if (stake_error && !is_minimized && !is_open) {
@@ -297,6 +306,7 @@ const Stake = observer(({ is_minimized }: TTradeParametersProps) => {
                             unitLeft={getCurrencyDisplayCode(currency)}
                             value={amount}
                             variant='fill'
+                            id={input_id}
                         />
                         <StakeDetails
                             commission={commission}

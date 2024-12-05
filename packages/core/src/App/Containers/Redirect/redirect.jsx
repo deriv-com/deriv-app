679002ad2d4408f1912e99e6e4bdf3253f748fab
hirad-deriv <hirad@regentmarkets.com>
2024-12-05 19:04:02 +0800

    chore: added trade_type

--- packages/core/src/App/Containers/Redirect/redirect.jsx
+++ packages/core/src/App/Containers/Redirect/redirect.jsx
@@ -307,8 +307,10 @@ const Redirect = observer(() => {
     }
     useEffect(() => {
         if (!redirected_to_route && history.location.pathname !== routes.traders_hub) {
+            const trade_type = /trade_type/i.test(url_query_string) ? url_params.get('trade_type') : '';
+
             const route_mappings = [
-                { pattern: /dtrader/i, route: routes.trade },
+                { pattern: /dtrader/i, route: `/dtrader?trade_type=${trade_type}` },
                 { pattern: /proof-of-address/i, route: routes.proof_of_address },
                 { pattern: /proof-of-identity/i, route: routes.proof_of_identity },
                 { pattern: /dbot/i, route: routes.bot },

65d789f48311621d8342c94536bb3ef22eed8911
Henry Hein <henry@regentmarkets.com>
2024-12-05 16:02:58 +0800

    fix: init guide component fix

--- packages/trader/src/AppV2/Components/MarketCategory/market-category.tsx
+++ packages/trader/src/AppV2/Components/MarketCategory/market-category.tsx
@@ -5,6 +5,7 @@ import MarketCategoryItem from '../MarketCategoryItem';
 import { ActiveSymbols } from '@deriv/api-types';
 import FavoriteSymbols from '../FavoriteSymbols';
 import { usePrevious } from '@deriv/components';
+import { useLocalStorageData } from '@deriv/hooks';
 
 type TMarketCategory = {
     category: MarketGroup;
@@ -17,15 +18,29 @@ type TMarketCategory = {
 const MarketCategory = ({ category, selectedSymbol, setSelectedSymbol, setIsOpen, isOpen }: TMarketCategory) => {
     const itemRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
     const prevSymbol = usePrevious(selectedSymbol);
+    const [guide_dtrader_v2] = useLocalStorageData<Record<string, boolean>>('guide_dtrader_v2', {
+        trade_types_selection: false,
+        trade_page: false,
+        positions_page: false,
+        market_selector: false,
+    });
 
     useEffect(() => {
-        if (isOpen && category.market === 'all' && selectedSymbol && itemRefs.current[selectedSymbol] && !prevSymbol) {
+        if (
+            isOpen &&
+            category.market === 'all' &&
+            selectedSymbol &&
+            itemRefs.current[selectedSymbol] &&
+            !prevSymbol &&
+            guide_dtrader_v2?.market_selector
+        ) {
             itemRefs.current[selectedSymbol]?.scrollIntoView({ block: 'center' });
         }
     }, [isOpen, category.market, selectedSymbol, prevSymbol]);
 
     return (
         <Tab.Panel key={category.market_display_name}>
+            {!guide_dtrader_v2?.market_selector && <div className='joyride-element' />}
             {category.market !== 'favorites' ? (
                 Object.entries(category.subgroups).map(([subgroupKey, subgroup]) => (
                     <div key={subgroupKey} className='market-category-content__container'>

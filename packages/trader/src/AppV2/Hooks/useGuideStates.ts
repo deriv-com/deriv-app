a8928717d74b640faba007e589558241b7c2031a
Henry Hein <henry@regentmarkets.com>
2024-12-06 13:15:45 +0800

    fix: make state inside hook rerender only when necessary

--- packages/trader/src/AppV2/Hooks/useGuideStates.ts
+++ packages/trader/src/AppV2/Hooks/useGuideStates.ts
@@ -7,19 +7,19 @@ type GuideFlags =
     | 'should_run_market_selector_guide';
 
 export const useGuideStates = () => {
-    const [guideStates, setGuideStates] = React.useState<Record<GuideFlags, boolean>>({
+    const [guideStates, setGuideStates] = React.useState({
         should_run_trade_types_selection_guide: false,
         should_run_trade_page_guide: false,
         should_run_positions_page_guide: false,
         should_run_market_selector_guide: false,
     });
 
-    const setGuideState = (flag: GuideFlags, value: boolean) => {
+    const setGuideState = React.useCallback((flag: GuideFlags, value: boolean) => {
         setGuideStates(prev => ({
             ...prev,
             [flag]: value,
         }));
-    };
+    }, []);
 
     return { guideStates, setGuideState };
 };

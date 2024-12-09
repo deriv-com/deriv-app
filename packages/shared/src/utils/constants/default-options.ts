5156b1b92b9d290055bcf35cb447b81182024243
lubega-deriv <lubega@deriv.com>
2024-12-09 18:23:46 +0800

    fix: remove wallets unsupported languages

--- packages/shared/src/utils/constants/default-options.ts
+++ packages/shared/src/utils/constants/default-options.ts
@@ -4,8 +4,3 @@ import { isProduction } from '../config/config';
  * Returns List of unsupported languages based on the environment.
  */
 export const UNSUPPORTED_LANGUAGES = isProduction() ? ['ID', 'MN'] : ['MN'];
-
-/**
- * Returns List of unsupported languages for Wallets.
- */
-export const WALLETS_UNSUPPORTED_LANGUAGES = [...UNSUPPORTED_LANGUAGES, 'ID'];

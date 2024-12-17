009bfc6bf9f92ec42f61f8565791a3a493b29384
Thisyahlen <thisyahlen@deriv.com>
2024-12-17 11:00:41 +0800

    chore: add loader to login button if the logged state cookie is true

--- packages/core/src/App/Components/Layout/Header/login-button.jsx
+++ packages/core/src/App/Components/Layout/Header/login-button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cookies from 'js-cookie';
 import PropTypes from 'prop-types';
 
 import { Button } from '@deriv/components';
@@ -7,8 +8,18 @@ import { redirectToLogin } from '@deriv/shared';
 import { getLanguage, localize } from '@deriv/translations';
 import { requestOidcAuthentication } from '@deriv-com/auth-client';
 
+import './login-button.scss';
+
+const PulsatingSkeleton = () => <div className='skeleton-loader' />;
+
 const LoginButton = ({ className }) => {
     const { isOAuth2Enabled } = useOauth2({});
+    const isUserLoggedState = Cookies.get('logged_state') === 'true';
+
+    if (isUserLoggedState) {
+        return <PulsatingSkeleton className={className} />;
+    }
+
     return (
         <Button
             id='dt_login_button'

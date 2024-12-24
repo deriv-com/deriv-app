3a191bb70fab1dd4d45c0c12ebea238f80fe6053
Nuzhy-Deriv <nuzhy@deriv.com>
2024-12-24 11:06:51 +0800

    fix: trackjs error

--- packages/core/src/App/Components/Elements/LiveChat/live-chat.tsx
+++ packages/core/src/App/Components/Elements/LiveChat/live-chat.tsx
@@ -23,11 +23,7 @@ const LiveChat = observer(({ showPopover }: { showPopover?: boolean }) => {
     // We will add a refactor after this
     setInterval(() => {
         if (fcAvailable || icAvailable) {
-            if (
-                window.LiveChatWidget &&
-                typeof window.LiveChatWidget.call === 'function' &&
-                typeof window.LiveChatWidget.call('destroy') === 'function'
-            ) {
+            if (window.LiveChatWidget && typeof window.LiveChatWidget.call === 'function') {
                 window.LiveChatWidget.call('destroy');
             }
         }

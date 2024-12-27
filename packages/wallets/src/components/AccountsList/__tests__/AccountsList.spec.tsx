99d35601a540e3d99b5d0e55c87de4e07c0df73e
lubega-deriv <lubega@deriv.com>
2024-12-27 11:14:54 +0800

    fix: unit test

--- packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
+++ packages/wallets/src/components/AccountsList/__tests__/AccountsList.spec.tsx
@@ -57,7 +57,7 @@ describe('AccountsList', () => {
             data: false,
             isLoading: false,
         });
-        (useGrowthbookIsOn as jest.Mock).mockReturnValue([true]);
+        (useGrowthbookIsOn as jest.Mock).mockReturnValue([true, true]);
     });
 
     afterAll(() => {
@@ -163,7 +163,7 @@ describe('AccountsList', () => {
     });
 
     it('does not render P2P redirection banner if growthbook is not loaded', () => {
-        (useGrowthbookIsOn as jest.Mock).mockReturnValue([false]);
+        (useGrowthbookIsOn as jest.Mock).mockReturnValue([false, true]);
         render(<AccountsList />, { wrapper });
 
         expect(screen.queryByText('Easily exchange USD with local currency using Deriv P2P.')).not.toBeInTheDocument();

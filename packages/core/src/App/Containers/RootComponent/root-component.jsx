7601b824e7f1c20eff5737c4155935c1a7d9bd8e
Tay Sui Sin <suisin@regentmarkets.com>
2024-12-31 13:31:50 +0800

    chore: display blank screen in wallets for hub enabled countries

--- packages/core/src/App/Containers/RootComponent/root-component.jsx
+++ packages/core/src/App/Containers/RootComponent/root-component.jsx
@@ -55,6 +55,7 @@ const RootComponent = observer(props => {
             }}
             notificationMessagesUi={notification_messages_ui}
             onWalletsOnboardingTourGuideCloseHandler={onWalletsOnboardingTourGuideCloseHandler}
+            isHubRedirectionEnabled={isHubRedirectionEnabled && !!trading_hub}
         />
     ) : (
         <AppStore {...props} />

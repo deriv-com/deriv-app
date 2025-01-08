73d86dc7ca9c1e93ca29a085b7a547bdc00d8585
Akmal Djumakhodjaev <akmal@binary.com>
2025-01-08 11:46:41 +0800

    Update types.ts

--- packages/stores/types.ts
+++ packages/stores/types.ts
@@ -27,7 +27,7 @@ import type {
     WebsiteStatus,
 } from '@deriv/api-types';
 
-import { TContractInfo } from '@deriv/shared/src/utils/contract/contract';
+import { TContractInfo } from '../shared/src/utils/contract';
 
 import type { FeatureFlagsStore } from './src/stores';
 

28943c81fd2515e515e26e4dccf4892328a2eb15
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2025-01-14 13:56:17 +0800

    Adrienne / Merge UAT OIDC changes to staging Deriv.app (#17969)
    * Adrienne / Enable OIDC back for SSO/SLO (#17926)
    
    * chore: enable back oidc sso
    
    * chore: attempt to move hook further up
    
    * fix: failing test pr to master (#17925) (#17927)
    
    Co-authored-by: nada-deriv <122768621+nada-deriv@users.noreply.github.com>
    
    * fix: failing test pr to master (#17925) (#17930)
    
    Co-authored-by: nada-deriv <122768621+nada-deriv@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: amam-deriv <mustofa@deriv.com>
    Co-authored-by: nada-deriv <122768621+nada-deriv@users.noreply.github.com>

--- packages/core/src/App/AppContent.tsx
+++ packages/core/src/App/AppContent.tsx
@@ -58,7 +58,7 @@ const AppContent: React.FC<{ passthrough: unknown }> = observer(({ passthrough }
     const { isMobile } = useDevice();
     const { switchLanguage } = useTranslations();
 
-    const { isOAuth2Enabled } = useOauth2({
+    const { isOAuth2Enabled, oAuthLogout } = useOauth2({
         handleLogout: async () => {
             await logout();
         },
@@ -67,6 +67,12 @@ const AppContent: React.FC<{ passthrough: unknown }> = observer(({ passthrough }
 
     const is_app_id_set = localStorage.getItem('config.app_id');
     const is_change_login_app_id_set = localStorage.getItem('change_login_app_id');
+
+    useSilentLoginAndLogout({
+        is_client_store_initialized,
+        isOAuth2Enabled,
+        oAuthLogout,
+    });
 
     const [isWebPasskeysFFEnabled, isGBLoaded] = useGrowthbookIsOn({
         featureFlag: 'web_passkeys',
@@ -80,16 +86,6 @@ const AppContent: React.FC<{ passthrough: unknown }> = observer(({ passthrough }
     const [isCountryCodeDropdownEnabled, isCountryCodeDropdownGBLoaded] = useGrowthbookGetFeatureValue({
         featureFlag: 'enable_country_code_dropdown',
     });
-
-    // NOTE: Commented this out for now due to single logout causing Deriv.app to be logged out continously
-    // There is a case where if logged_state is false coming from other platforms, Deriv app will SLO the user out
-    // TODO: Revert this once OIDC is enabled back for Deriv.app
-    // useSilentLoginAndLogout({
-    //     is_client_store_initialized,
-    //     isOAuth2Enabled,
-    //     oAuthLogout,
-    //     isGBLoaded,
-    // });
 
     const { data } = useRemoteConfig(true);
     const { tracking_datadog } = data;

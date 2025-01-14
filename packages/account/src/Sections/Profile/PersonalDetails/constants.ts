dc32bc30d80b436df506d2840a4cc57179853391
amina-deriv <84661147+amina-deriv@users.noreply.github.com>
2025-01-14 16:55:52 +0400

    fix: add addditional dropdown options for account opening reason

--- packages/account/src/Sections/Profile/PersonalDetails/constants.ts
+++ packages/account/src/Sections/Profile/PersonalDetails/constants.ts
@@ -4,4 +4,6 @@ export const account_opening_reason_list = [
     { text: localize('Speculative'), value: 'Speculative' },
     { text: localize('Income Earning'), value: 'Income Earning' },
     { text: localize('Hedging'), value: 'Hedging' },
+    { text: localize('Additional revenue'), value: 'Additional revenue' },
+    { text: localize('Savings'), value: 'Savings' },
 ];

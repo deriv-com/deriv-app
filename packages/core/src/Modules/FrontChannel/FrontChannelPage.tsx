80ee92083c3feb3b897323add18a0cd784760ea0
Adrienne Rio <adrienne@deriv.com>
2025-01-14 13:21:39 +0800

    chore: backup

--- packages/core/src/Modules/FrontChannel/FrontChannelPage.tsx
+++ packages/core/src/Modules/FrontChannel/FrontChannelPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { useStore } from '@deriv/stores';
 
 const FrontChannelPage = () => {
+    const store = useStore();
+    const { logout, is_client_store_initialized } = store.client;
+
     React.useEffect(() => {
-        localStorage.removeItem('client.accounts');
-    }, []);
+        if (is_client_store_initialized) logout();
+    }, [is_client_store_initialized]);
     return <></>;
 };
 

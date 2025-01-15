9c3bab4fc4f3381558613f008a3fddbc3d32f37c
Tay Sui Sin <suisin@regentmarkets.com>
2025-01-15 19:31:51 +0800

    chore: fix to not check citizen and check residence instead

--- packages/api-v2/src/hooks/useIsHubRedirectionEnabled.ts
+++ packages/api-v2/src/hooks/useIsHubRedirectionEnabled.ts
@@ -1,6 +1,5 @@
 import useClientCountry from './useClientCountry';
 import useGrowthbookGetFeatureValue from './useGrowthbookGetFeatureValue';
-import useSettings from './useSettings';
 
 type THubEnabledCountryList = {
     hub_enabled_country_list: string[];
@@ -11,15 +10,13 @@ const useIsHubRedirectionEnabled = () => {
         featureFlag: 'hub_enabled_country_list',
     });
     const { data: clientCountry } = useClientCountry();
-    const { data: accountSettings } = useSettings();
-    const { citizen } = accountSettings;
 
     const isHubRedirectionEnabled =
         typeof hubEnabledCountryList === 'object' &&
         hubEnabledCountryList !== null &&
         Array.isArray((hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list) &&
-        citizen &&
-        (hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list.includes(citizen);
+        clientCountry &&
+        (hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list.includes(clientCountry);
 
     const isChangingToHubAppId =
         typeof hubEnabledCountryList === 'object' &&
--- packages/hooks/src/useIsHubRedirectionEnabled.ts
+++ packages/hooks/src/useIsHubRedirectionEnabled.ts
@@ -11,14 +11,14 @@ const useIsHubRedirectionEnabled = () => {
         featureFlag: 'hub_enabled_country_list',
     });
     const { client } = useStore();
-    const { account_settings, clients_country } = client;
+    const { clients_country } = client;
 
     const isHubRedirectionEnabled =
         typeof hubEnabledCountryList === 'object' &&
         hubEnabledCountryList !== null &&
         Array.isArray((hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list) &&
-        account_settings.citizen &&
-        (hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list.includes(account_settings.citizen);
+        clients_country &&
+        (hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list.includes(clients_country);
 
     const isChangingToHubAppId =
         typeof hubEnabledCountryList === 'object' &&

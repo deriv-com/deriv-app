e3f78e23e209ddf32e95a43d2f3ff701afb314fc
Sui Sin <103026762+suisin-deriv@users.noreply.github.com>
2025-01-17 15:06:35 +0800

    [TRAH5305] Suisin/chore: add alpha generation guide to mt5 except gold (#17982)
    * chore: add alpha generation guide to mt5 except gold
    
    * chore: update to check windows only
    
    * chore: fix test case
    
    * fix: dmt5 test case fail issue

--- packages/cfd/src/Containers/dmt5-trade-modal.tsx
+++ packages/cfd/src/Containers/dmt5-trade-modal.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { TAdditionalDetailsOfEachMT5Loginid } from '@deriv/stores/types';
 import { useDevice } from '@deriv-com/ui';
-import { Text, Icon, Money, StatusBadge } from '@deriv/components';
+import { Text, Icon, Money, StatusBadge, InformationBanner } from '@deriv/components';
 import getMT5StatusBadgeConfig from '@deriv/account/src/Configs/get-mt5-status-badge-config';
-import { getCFDAccountKey, MT5_ACCOUNT_STATUS, PRODUCT, Jurisdiction } from '@deriv/shared';
+import { getCFDAccountKey, MT5_ACCOUNT_STATUS, PRODUCT, Jurisdiction, OSDetect } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import { Localize, localize } from '@deriv/translations';
 import { CFD_PLATFORMS, MARKET_TYPE } from '../Helpers/cfd-config';
@@ -15,6 +15,7 @@ import PasswordBox from '../Components/passwordbox';
 import SpecBox from '../Components/specbox';
 import { TCFDPasswordReset } from './props.types';
 import { TProducts, TTradingPlatformAccounts } from '../Components/props.types';
+import { StandaloneChartAreaRegularIcon, StandaloneArrowUpRightRegularIcon } from '@deriv/quill-icons';
 
 type TMT5TradeModalProps = {
     mt5_trade_account: TAdditionalDetailsOfEachMT5Loginid;
@@ -97,7 +98,7 @@ const DMT5TradeModal = observer(
                     return null;
             }
         };
-
+        const is_window = OSDetect() === 'windows';
         return (
             <div className='cfd-trade-modal-container'>
                 <div className='cfd-trade-modal'>
@@ -207,6 +208,17 @@ const DMT5TradeModal = observer(
                             <Localize i18n_default_text='Server maintenance starts at 01:00 GMT every Sunday, and this process may take up to 2 hours to complete. Service may be disrupted during this time.' />
                         </div>
                     </div>
+                    {isDesktop && is_window && mt5_trade_account.product !== PRODUCT.GOLD && (
+                        <InformationBanner
+                            information_icon={<StandaloneChartAreaRegularIcon fill='#095A66' iconSize='sm' />}
+                            title={<Localize i18n_default_text='Alpha Generation guide' />}
+                            description={
+                                <Localize i18n_default_text='Tailor your indicators with expert-driven trend analysis.' />
+                            }
+                            redirect_icon={<StandaloneArrowUpRightRegularIcon fill='#000000' iconSize='sm' />}
+                            link='https://docs.deriv.com/misc/alpha_generation_guide.pdf'
+                        />
+                    )}
                 </div>
                 {is_eligible_to_migrate && <MigrationBanner is_trade_modal />}
 

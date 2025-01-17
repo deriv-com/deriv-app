c6846e99f7e1fc58cdd13c9a9a25e09cec562211
Adrienne Rio <adrienne@deriv.com>
2025-01-17 11:23:54 +0800

    chore: remove old changes for oidc social logic

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -2226,6 +2226,18 @@ export default class ClientStore extends BaseStore {
                 if (is_account_param) {
                     obj_params[key] = value;
                     is_social_signup_provider = true;
+                    // NOTE: Remove this logic once social signup is intergated with OIDC
+                    const loggedState = Cookies.get('logged_state');
+                    console.log('setting logged state to false if cookies are false');
+                    if (loggedState === 'false') {
+                        const currentDomain = window.location.hostname.split('.').slice(-2).join('.');
+                        Cookies.set('logged_state', 'true', {
+                            expires: 30,
+                            path: '/',
+                            domain: currentDomain,
+                            secure: true,
+                        });
+                    }
                 }
             });
 

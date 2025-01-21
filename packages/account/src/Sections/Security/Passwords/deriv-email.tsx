d97376f811c4459447caa67e113f7fdadf59952f
amina-deriv <84661147+amina-deriv@users.noreply.github.com>
2025-01-21 11:35:55 +0400

    fix: email_pop

--- packages/account/src/Sections/Security/Passwords/deriv-email.tsx
+++ packages/account/src/Sections/Security/Passwords/deriv-email.tsx
@@ -1,11 +1,14 @@
-import { Fragment, useState } from 'react';
-import { Text } from '@deriv/components';
+import { Fragment, useEffect, useState } from 'react';
+
 import { useVerifyEmail } from '@deriv/api';
+import { Button, Modal, Text } from '@deriv/components';
 import { toTitleCase } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import { Localize, useTranslations } from '@deriv-com/translations';
+
 import SentEmailModal from '../../../Components/sent-email-modal';
 import UnlinkAccountModal from '../../../Components/unlink-account-modal';
+
 import EmailPasswordSection from './email-password-section';
 
 type TVerifyEmailPayload = Parameters<ReturnType<typeof useVerifyEmail>['mutate']>[0];
@@ -20,19 +23,29 @@ const DerivEmail = observer(() => {
         common: { is_from_derivgo },
         client: { social_identity_provider, is_social_signup, email },
     } = useStore();
-    const { mutate } = useVerifyEmail();
+
+    const { mutate, isError, error, isSuccess } = useVerifyEmail();
     const { localize } = useTranslations();
     const [is_unlink_account_modal_open, setIsUnlinkAccountModalOpen] = useState(false);
     const [is_send_email_modal_open, setIsSendEmailModalOpen] = useState(false);
+    const [is_error_modal_open, setIsErrorModalOpen] = useState(false);
 
     const payload: TVerifyEmailPayload = { verify_email: email, type: 'request_email' };
+
+    useEffect(() => {
+        if (isError) {
+            setIsErrorModalOpen(true);
+            setIsSendEmailModalOpen(false);
+        } else if (isSuccess) {
+            setIsSendEmailModalOpen(true);
+        }
+    }, [isError, error, isSuccess]);
 
     const onClickChangeEmail = () => {
         if (is_social_signup) {
             setIsUnlinkAccountModalOpen(true);
         } else {
             mutate(payload);
-            setIsSendEmailModalOpen(true);
         }
     };
 
@@ -40,6 +53,10 @@ const DerivEmail = observer(() => {
         mutate(payload);
         setIsUnlinkAccountModalOpen(false);
         setIsSendEmailModalOpen(true);
+    };
+
+    const onClose = () => {
+        setIsErrorModalOpen(false);
     };
 
     return (
@@ -74,6 +91,31 @@ const DerivEmail = observer(() => {
                     is_modal_when_mobile={true}
                 />
             </div>
+            {is_error_modal_open && (
+                <Modal
+                    is_open={is_error_modal_open}
+                    has_close_icon
+                    should_header_stick_body
+                    title={localize('Email change currently unavailable')}
+                    toggleModal={onClose}
+                    width='440px'
+                    height='200px'
+                >
+                    <div className='unhandled-error'>
+                        <Text className='account__email-error_text' as='p' line_height='xxl'>
+                            {error?.code === 'EmailChangeFailP2PActive' ? (
+                                <Localize i18n_default_text='Complete P2P orders and deactivate ads to proceed.' />
+                            ) : (
+                                <Localize i18n_default_text={error?.message} />
+                            )}
+                        </Text>
+
+                        <Button onClick={onClose} has_effect primary large>
+                            <Localize i18n_default_text='OK' />
+                        </Button>
+                    </div>
+                </Modal>
+            )}
         </Fragment>
     );
 });

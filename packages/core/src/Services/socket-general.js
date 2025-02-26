a90082dd3cfcf9e3d5be72aa7171770039dab55f
Adrienne Rio <adrienne@deriv.com>
2025-02-26 12:49:34 +0800

    chore: fix self exclusion not logging out

--- packages/core/src/Services/socket-general.js
+++ packages/core/src/Services/socket-general.js
@@ -13,7 +13,7 @@ import { localize } from '@deriv/translations';
 import ServerTime from '_common/base/server_time';
 import BinarySocket from '_common/base/socket_base';
 import WS from './ws-methods';
-import { OAuth2Logout } from '@deriv-com/analytics';
+import { OAuth2Logout } from '@deriv-com/auth-client';
 
 let client_store, common_store, gtm_store;
 let reconnectionCounter = 1;
@@ -142,14 +142,13 @@ const BinarySocketGeneral = (() => {
             const current_session_duration = session_duration_limit ? ServerTime.get() - moment(session_start_time) : 0;
             const remaining_session_time = duration * 60 * 1000 - current_session_duration;
             clearTimeout(session_timeout);
-            session_timeout = setTimeout(async () => {
-                await OAuth2Logout({
+            session_timeout = setTimeout(() => {
+                OAuth2Logout({
                     WSLogoutAndRedirect: () => {
                         client_store.logout();
                         sessionStorage.removeItem('session_start_time');
                     },
                     redirectCallbackUri: `${window.location.origin}/callback`,
-                    postLogoutRedirectUri: `${window.location.origin}/`,
                 });
             }, remaining_session_time);
         } else if (!duration) {

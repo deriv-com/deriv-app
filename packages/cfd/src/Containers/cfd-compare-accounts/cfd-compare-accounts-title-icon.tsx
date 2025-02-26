ccce2a4021ee9fb4338ae60de6be212e28afd5cc
shontzu <108507236+shontzu-deriv@users.noreply.github.com>
2025-02-26 09:16:46 +0800

    [TRAH] shontzu/TRAH-5356/fe-implementation-api-group-cleanup (#18070)
    * chore: Backwards compatibility for API updates
    
    * chore: logout screen
    
    * fix: logged out version shouldnt break logged in config
    
    * chore: empty commit

--- packages/cfd/src/Containers/cfd-compare-accounts/cfd-compare-accounts-title-icon.tsx
+++ packages/cfd/src/Containers/cfd-compare-accounts/cfd-compare-accounts-title-icon.tsx
@@ -17,7 +17,10 @@ import {
 
 const CFDCompareAccountsTitleIcon = ({ trading_platforms, is_eu_user, is_demo }: TCompareAccountsCard) => {
     const { isDesktop } = useDevice();
-    const market_type = !is_eu_user ? getMarketType(trading_platforms) : 'CFDs';
+    const market_type =
+        is_eu_user && trading_platforms.market_type === 'financial'
+            ? 'CFDs' // this is for backwards compatibility with BE
+            : getMarketType(trading_platforms);
 
     const market_type_shortcode = generateMarketTypeShortcode(trading_platforms, market_type);
 

cf3a9ade7b35177f17e0d8346a0e2e40d21f853c
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2025-03-13 13:43:16 +0800

    Adrienne / Fix token undefined during account switching session storage handling (#18137)
    * chore: sync callback storage with session storage
    
    * chore: sync active_loginid with local storage

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -2249,7 +2249,7 @@ export default class ClientStore extends BaseStore {
 
         // if didn't find any login ID that matched the above condition
         // or the selected one doesn't have a token, set the first one
-        if (!active_loginid || !client_object[active_loginid].token) {
+        if (!active_loginid || !client_object[active_loginid]?.token) {
             active_loginid = obj_params.acct1;
         }
 

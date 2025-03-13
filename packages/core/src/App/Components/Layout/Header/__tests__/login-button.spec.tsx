d5ae02ae1369635fb07a35be7585f087675dcfce
Adrienne Rio <adrienne@deriv.com>
2025-03-13 17:12:04 +0800

    chore: fix test cases

--- packages/core/src/App/Components/Layout/Header/__tests__/login-button.spec.tsx
+++ packages/core/src/App/Components/Layout/Header/__tests__/login-button.spec.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { LoginButton } from '../login-button.jsx';
+import { mockStore, StoreProvider } from '@deriv/stores';
 
 describe('LoginButton', () => {
+    const mockStoreData = mockStore({});
     it('should have the right className base on the property', () => {
-        render(<LoginButton className='acc-info__button' />);
+        render(
+            <StoreProvider store={mockStoreData}>
+                <LoginButton className='acc-info__button' />
+            </StoreProvider>
+        );
         const button = screen.getByRole('button');
         expect(button).toHaveClass('acc-info__button');
     });

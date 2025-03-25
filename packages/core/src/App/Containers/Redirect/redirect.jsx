ea495783aeb4f240cb3c516f5f669fca154edddd
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2025-03-25 15:50:14 +0800

    Adrienne / Catch promise errors for OIDC (#18195)
    * chore: remove eslint for console errors
    
    * chore: catch promise errors for oidc
    
    * chore: revert unrelated changes

--- packages/core/src/App/Containers/Redirect/redirect.jsx
+++ packages/core/src/App/Containers/Redirect/redirect.jsx
@@ -351,6 +351,9 @@ const Redirect = observer(() => {
                     try {
                         requestOidcAuthentication({
                             redirectCallbackUri: `${window.location.origin}/callback`,
+                        }).catch(err => {
+                            // eslint-disable-next-line no-console
+                            console.error(err);
                         });
                     } catch (err) {
                         // eslint-disable-next-line no-console

7bf7d6f2f090f7d938d6eef4c8120d339124395e
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2025-03-27 13:56:59 +0800

    Adrienne / Fix safari redirection (#18205)
    * feat: store redirect metadata for traders hub from os-redirect
    
    * Merge branch 'master' of github.com:deriv-com/deriv-app
    
    * Merge branch 'master' of github.com:deriv-com/deriv-app
    
    * chore: remove redirection to oauth for authenticated pages in accounts
    
    * chore: remove redirection logic to auth in reports
    
    * chore: added cookie checks for redirection
    
    * chore: add cookie js package to accounts
    
    * chore: use oidc authentication for redirecting to login for auth routes
    
    * chore: use client store to lock concurrent oidc requests
    
    * chore: remove redirection logic to auth for accounts and reports
    
    * chore: fix test cases, remove unrelated changes
    
    * chore: prevent sso when redirection is kicking in for auth routes
    
    * chore: set sso prevention to false when oauth fails

--- packages/core/src/Stores/client-store.js
+++ packages/core/src/Stores/client-store.js
@@ -84,6 +84,7 @@ export default class ClientStore extends BaseStore {
     device_data = {};
     is_authorize = false;
     is_logging_in = false;
+    is_single_logging_in = false;
     is_client_store_initialized = false;
     has_logged_out = false;
     is_landing_company_loaded = false;
@@ -214,6 +215,7 @@ export default class ClientStore extends BaseStore {
             device_data: observable,
             is_authorize: observable,
             is_logging_in: observable,
+            is_single_logging_in: observable,
             is_client_store_initialized: observable,
             has_logged_out: observable,
             is_landing_company_loaded: observable,
@@ -368,6 +370,7 @@ export default class ClientStore extends BaseStore {
             setUrlParams: action.bound,
             setIsAuthorize: action.bound,
             setIsLoggingIn: action.bound,
+            setIsSingleLoggingIn: action.bound,
             setPreSwitchAccount: action.bound,
             broadcastAccountChange: action.bound,
             switchAccountHandler: action.bound,
@@ -1925,6 +1928,10 @@ export default class ClientStore extends BaseStore {
 
     setIsLoggingIn(bool) {
         this.is_logging_in = bool;
+    }
+
+    setIsSingleLoggingIn(bool) {
+        this.is_single_logging_in = bool;
     }
 
     setPreSwitchAccount(is_pre_switch) {

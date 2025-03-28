347bd7df90024c87983d50e935ba795570eec4de
vinu-deriv <vinu@regentmarkets.com>
2025-03-28 09:37:53 +0400

    fix: fix for the account syncing in dtrader

--- packages/core/src/Modules/Callback/CallbackPage.tsx
+++ packages/core/src/Modules/Callback/CallbackPage.tsx
@@ -10,57 +10,57 @@ const CallbackPage = () => {
     return (
         <Callback
             onSignInSuccess={(tokens: Record<string, string>) => {
-                localStorage.setItem('config.tokens', JSON.stringify(tokens));
-                localStorage.setItem('config.account1', tokens.token1);
-                localStorage.setItem('active_loginid', tokens.acct1);
-                if (!sessionStorage.getItem('active_loginid')) sessionStorage.setItem('active_loginid', tokens.acct1);
-
+                let matchingLoginId: string | undefined, matchingToken: string | undefined;
                 const redirectTo = sessionStorage.getItem('tradershub_redirect_to');
-                if (redirectTo) {
-                    const params = new URLSearchParams(redirectTo);
-                    const queryAccount = sessionStorage.getItem('account')
-                        ? sessionStorage.getItem('account')
-                        : params.get('account');
-
-                    let matchingLoginId: string | undefined, matchingToken: string | undefined;
-                    if (queryAccount?.toLowerCase() !== 'demo') {
-                        Object.keys(tokens).find(key => {
-                            if (key.startsWith('cur') && tokens[key] === queryAccount) {
-                                const sequence = key.replace('cur', '');
-                                const isNotCRWallet =
-                                    tokens[`acct${sequence}`]?.startsWith('CR') &&
-                                    !tokens[`acct${sequence}`]?.startsWith('CRW');
-                                const isNotMFWallet =
-                                    tokens[`acct${sequence}`]?.startsWith('MF') &&
-                                    !tokens[`acct${sequence}`]?.startsWith('MFW');
-                                if (isNotCRWallet || isNotMFWallet) {
-                                    if (!matchingLoginId && !matchingToken) {
-                                        matchingLoginId = tokens[`acct${sequence}`];
-                                        matchingToken = tokens[`token${sequence}`];
-                                    }
-                                }
-                            }
-                        });
-                    } else {
-                        Object.keys(tokens).find(key => {
-                            if (key.startsWith('cur') && tokens[key] === queryAccount) {
-                                // get currency sequence number, e.g. cur1=1, cur2=2
-                                const sequence = key.replace('cur', '');
-                                const isDemo = tokens[`acct${sequence}`]?.startsWith('VRTC');
-
-                                if (isDemo) {
+                const params = new URLSearchParams(redirectTo || window.location.search);
+                localStorage.setItem('config.tokens', JSON.stringify(tokens));
+                const queryAccount = sessionStorage.getItem('account')
+                    ? sessionStorage.getItem('account')
+                    : params.get('account');
+                if (queryAccount?.toLowerCase() !== 'demo') {
+                    Object.keys(tokens).find(key => {
+                        if (key.startsWith('cur') && tokens[key] === queryAccount) {
+                            const sequence = key.replace('cur', '');
+                            const isNotCRWallet =
+                                tokens[`acct${sequence}`]?.startsWith('CR') &&
+                                !tokens[`acct${sequence}`]?.startsWith('CRW');
+                            const isNotMFWallet =
+                                tokens[`acct${sequence}`]?.startsWith('MF') &&
+                                !tokens[`acct${sequence}`]?.startsWith('MFW');
+                            if (isNotCRWallet || isNotMFWallet) {
+                                if (!matchingLoginId && !matchingToken) {
                                     matchingLoginId = tokens[`acct${sequence}`];
                                     matchingToken = tokens[`token${sequence}`];
                                 }
                             }
-                        });
-                    }
-                    if (matchingLoginId && matchingToken) {
-                        sessionStorage.setItem('active_loginid', matchingLoginId);
-                        localStorage.setItem('config.account1', matchingToken);
-                        localStorage.setItem('active_loginid', matchingLoginId);
-                    }
+                        }
+                    });
+                } else {
+                    Object.keys(tokens).find(key => {
+                        if (key.startsWith('cur') && tokens[key] === queryAccount) {
+                            // get currency sequence number, e.g. cur1=1, cur2=2
+                            const sequence = key.replace('cur', '');
+                            const isDemo = tokens[`acct${sequence}`]?.startsWith('VRTC');
 
+                            if (isDemo) {
+                                matchingLoginId = tokens[`acct${sequence}`];
+                                matchingToken = tokens[`token${sequence}`];
+                            }
+                        }
+                    });
+                }
+                if (matchingLoginId && matchingToken) {
+                    sessionStorage.setItem('active_loginid', matchingLoginId);
+                    localStorage.setItem('active_loginid', matchingLoginId);
+                    localStorage.setItem('config.account1', matchingToken);
+                } else {
+                    localStorage.setItem('config.account1', tokens.token1);
+                    localStorage.setItem('active_loginid', tokens.acct1);
+                    if (!sessionStorage.getItem('active_loginid'))
+                        sessionStorage.setItem('active_loginid', tokens.acct1);
+                }
+
+                if (redirectTo) {
                     sessionStorage.removeItem('tradershub_redirect_to');
                     window.location.href = redirectTo;
                 } else {

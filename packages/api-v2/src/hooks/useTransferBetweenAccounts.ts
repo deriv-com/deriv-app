fe0a08c070e031ecf2505abb8bd3a47903774c3a
George Usynin <103181646+heorhi-deriv@users.noreply.github.com>
2025-04-09 17:19:38 +0300

    refactor: enhance useTransferBetweenAccounts hook (#18273)

--- packages/api-v2/src/hooks/useTransferBetweenAccounts.ts
+++ packages/api-v2/src/hooks/useTransferBetweenAccounts.ts
@@ -9,21 +9,38 @@ type TPayload = NonNullable<
 
 /** A custom hook used to transfer money between client accounts */
 const useTransferBetweenAccounts = () => {
-    const { data, ...rest } = useQuery('transfer_between_accounts', {
+    const {
+        data,
+        refetch: refetchQuery,
+        ...rest
+    } = useQuery('transfer_between_accounts', {
         payload: { accounts: 'all' },
     });
 
-    const { mutate: _mutate, mutateAsync: _mutateAsync } = useMutation('transfer_between_accounts');
+    const {
+        mutate: _mutate,
+        mutateAsync: _mutateAsync,
+        error: mutateError,
+        isLoading: mutateIsLoading,
+        reset,
+    } = useMutation('transfer_between_accounts');
 
     const mutate = useCallback((payload: TPayload) => _mutate({ payload }), [_mutate]);
     const mutateAsync = useCallback((payload: TPayload) => _mutateAsync({ payload }), [_mutateAsync]);
+    const refetch = useCallback(async () => {
+        reset();
+        await refetchQuery();
+    }, [refetchQuery, reset]);
 
     return {
+        ...rest,
         data,
         /** The mutation function that accepts a payload and sends it to the server */
+        error: rest.error || mutateError,
+        isLoading: rest.isLoading || mutateIsLoading,
         mutate,
         mutateAsync,
-        ...rest,
+        refetch,
     };
 };
 

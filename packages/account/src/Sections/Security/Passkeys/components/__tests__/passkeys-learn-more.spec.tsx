eb1007e20ae70d224df48b176a31cb6e7ffcad92
nada-deriv <122768621+nada-deriv@users.noreply.github.com>
2025-04-17 16:12:27 +0400

    Nada/trah 6061/change passkey to biometrics (#18329)
    * fix: replaced passkeys with biometrics
    
    * fix: failing tests
    
    * fix: updated video and illustration
    
    * fix: failing test
    
    * fix: udpated test id
    
    * fix: replace notification content

--- packages/account/src/Sections/Security/Passkeys/components/__tests__/passkeys-learn-more.spec.tsx
+++ packages/account/src/Sections/Security/Passkeys/components/__tests__/passkeys-learn-more.spec.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
+
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
 import { PasskeysLearnMore } from '../passkeys-learn-more';
 
-jest.mock('../tips-block', () => ({
-    TipsBlock: jest.fn(() => <div>TipsBlock</div>),
-}));
-jest.mock('../description-container', () => ({
-    DescriptionContainer: jest.fn(() => <div>DescriptionContainer</div>),
-}));
-
 describe('PasskeysLearnMore', () => {
-    const mockOnPrimaryButtonClick = jest.fn();
     const mockOnSecondaryButtonClick = jest.fn();
 
-    it('renders the tips correctly', () => {
-        render(
-            <PasskeysLearnMore
-                onPrimaryButtonClick={mockOnPrimaryButtonClick}
-                onSecondaryButtonClick={mockOnSecondaryButtonClick}
-            />
-        );
+    it('renders the tips correctly', async () => {
+        render(<PasskeysLearnMore onSecondaryButtonClick={mockOnSecondaryButtonClick} />);
 
-        expect(screen.getByText('Effortless login with passkeys')).toBeInTheDocument();
-        expect(screen.getByText('DescriptionContainer')).toBeInTheDocument();
-        expect(screen.getByText('TipsBlock')).toBeInTheDocument();
-        userEvent.click(screen.getByRole('button', { name: /create passkey/i }));
-        expect(mockOnPrimaryButtonClick).toHaveBeenCalled();
-        userEvent.click(screen.getByTestId('dt_learn_more_back_button'));
+        await userEvent.click(screen.getByTestId('dt_learn_more_back_button'));
         expect(mockOnSecondaryButtonClick).toHaveBeenCalled();
     });
 });

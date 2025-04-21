ddc78ebd2426c4c4729eb2be958ada26d0fe0b0e
mayuran-deriv <129507167+mayuran-deriv@users.noreply.github.com>
2025-04-21 11:58:23 +0400

    fix: loader (#18294)

--- packages/components/src/components/video-player/video-player.tsx
+++ packages/components/src/components/video-player/video-player.tsx
@@ -17,6 +17,7 @@ type TVideoPlayerProps = {
     increased_drag_area?: boolean;
     muted?: boolean;
     src: string;
+    show_loading?: boolean;
     onModalClose?: () => void;
 };
 type TSupportedEvent = React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement> | TouchEvent | MouseEvent;
@@ -35,6 +36,7 @@ const VideoPlayer = ({
     increased_drag_area,
     muted = false,
     src,
+    show_loading = false,
     onModalClose,
 }: TVideoPlayerProps) => {
     const is_rtl = useIsRtl();
@@ -45,6 +47,7 @@ const VideoPlayer = ({
     const [current_time, setCurrentTime] = React.useState<number>();
     const [has_enlarged_dot, setHasEnlargedDot] = React.useState(false);
     const [is_animated, setIsAnimated] = React.useState(true);
+    const [is_loading, setIsLoading] = React.useState(true);
     const [is_playing, setIsPlaying] = React.useState(false);
     const [is_muted, setIsMuted] = React.useState(muted);
     const [playback_rate, setPlaybackRate] = React.useState(1);
@@ -188,6 +191,7 @@ const VideoPlayer = ({
 
         setVideoDuration(video_ref.current.duration);
         setIsPlaying(should_autoplay);
+        setIsLoading(false);
 
         if (should_autoplay) animation_ref.current = requestAnimationFrame(repeat);
     };
@@ -351,6 +355,11 @@ const VideoPlayer = ({
                 playbackRate={playback_rate}
                 volume={volume}
             />
+            {is_loading && show_loading && (
+                <div className='player__loader' style={{ height: height ?? (is_mobile ? '184.5px' : '270px') }}>
+                    <div className='player__loader-circle' />
+                </div>
+            )}
             <VideoOverlay
                 onClick={() => setShowControls(!show_controls)}
                 togglePlay={togglePlay}

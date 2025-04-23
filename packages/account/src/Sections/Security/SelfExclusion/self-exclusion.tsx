723880c538e0f39e22cadfb2d3ce2cf9ee182c4a
amam-deriv <mustofa@deriv.com>
2025-04-23 11:46:16 +0800

    fix: self exclusion logout

--- packages/account/src/Sections/Security/SelfExclusion/self-exclusion.tsx
+++ packages/account/src/Sections/Security/SelfExclusion/self-exclusion.tsx
@@ -10,6 +10,7 @@ import {
     useIsMounted,
     WS,
 } from '@deriv/shared';
+import { useOauth2 } from '@deriv/hooks';
 import { useTranslations } from '@deriv-com/translations';
 import DemoMessage from '../../../Components/demo-message';
 import '../../../Components/self-exclusion/self-exclusion.scss';
@@ -149,6 +150,11 @@ const SelfExclusion = observer(({ is_app_settings, overlay_ref, setIsOverlayShow
         },
         initial_state
     );
+    const { oAuthLogout } = useOauth2({
+        handleLogout: async () => {
+            await logout();
+        },
+    });
 
     useEffect(() => {
         if (is_virtual) {
@@ -319,7 +325,7 @@ const SelfExclusion = observer(({ is_app_settings, overlay_ref, setIsOverlayShow
                         submit_error_message: response.error.message,
                     });
                 } else {
-                    logout();
+                    oAuthLogout();
                 }
             } else {
                 setState({ show_confirm: true });

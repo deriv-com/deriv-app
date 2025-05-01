790116954a80e39b0e60cd8ba997c70a8883c854
Likhith Kolayari <98398322+likhith-deriv@users.noreply.github.com>
2025-05-01 10:21:42 +0400

    Revert "Nada/TRAH-6061/fix: replaced passkeys with biometrics (#18297)" (#18398)
    This reverts commit c71b1a7d5f2f989708a9082d9573d6a6cbf1a33c.

--- packages/account/src/Sections/Security/Passkeys/components/__tests__/no-passkeys.spec.tsx
+++ packages/account/src/Sections/Security/Passkeys/components/__tests__/no-passkeys.spec.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import { NoPasskeys } from '../no-passkeys';
 
 describe('NoPasskeys', () => {
     const mockOnPrimaryButtonClick = jest.fn();
     const mockOnSecondaryButtonClick = jest.fn();
 
-    it('renders NoPasskeys component correctly', async () => {
+    it('renders NoPasskeys component correctly', () => {
         render(
             <NoPasskeys
                 onPrimaryButtonClick={mockOnPrimaryButtonClick}
@@ -17,14 +15,10 @@ describe('NoPasskeys', () => {
             />
         );
 
-        expect(
-            screen.getByText(
-                'Use fingerprint, face recognition, or other biometric data to log in easily and securely.'
-            )
-        ).toBeInTheDocument();
-        expect(screen.getByText('Your key to safer logins')).toBeInTheDocument();
-        await userEvent.click(screen.getByRole('button', { name: /enable biometrics/i }));
-        await userEvent.click(screen.getByRole('button', { name: /learn more/i }));
+        expect(screen.getByText('Enhanced security is just a tap away.')).toBeInTheDocument();
+        expect(screen.getByText('Experience safer logins')).toBeInTheDocument();
+        userEvent.click(screen.getByRole('button', { name: /create passkey/i }));
+        userEvent.click(screen.getByRole('button', { name: /learn more/i }));
         expect(mockOnPrimaryButtonClick).toHaveBeenCalled();
         expect(mockOnSecondaryButtonClick).toHaveBeenCalled();
     });
--- packages/core/src/App/Constants/routes-config.js
+++ packages/core/src/App/Constants/routes-config.js
@@ -9,8 +9,8 @@ import Redirect from 'App/Containers/Redirect';
 import RootComponent from 'App/Containers/RootComponent';
 import Endpoint from 'Modules/Endpoint';
 
-import CallbackPage from '../../Modules/Callback/CallbackPage.tsx';
 import OSRedirect from '../Containers/OSRedirect';
+import CallbackPage from '../../Modules/Callback/CallbackPage.tsx';
 
 const CFDCompareAccounts = React.lazy(
     () => import(/* webpackChunkName: "cfd-compare-accounts" */ '@deriv/cfd/src/Containers/cfd-compare-accounts')
@@ -200,7 +200,12 @@ const getModules = () => {
                         {
                             path: routes.passkeys,
                             component: Account,
-                            getTitle: () => localize('Biometrics'),
+                            getTitle: () => (
+                                <>
+                                    {localize('Passkeys')}
+                                    <span className='dc-vertical-tab__header--new'>{localize('NEW')}!</span>
+                                </>
+                            ),
                         },
                         {
                             path: routes.self_exclusion,

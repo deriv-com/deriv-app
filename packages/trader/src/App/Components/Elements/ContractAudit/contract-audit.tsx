402fb7120961802101e3622c8e0d6e5440b4f232
mayuran-deriv <129507167+mayuran-deriv@users.noreply.github.com>
2025-05-07 10:47:48 +0400

    fix: hide accumulator tab (#18395)
    * fix: hide accumulator tab
    
    * fix: Empty commit
    
    * fix: Empty commit
    
    * fix: Empty commit
    
    * fix: comment
    
    * fix: tab remove

--- packages/trader/src/App/Components/Elements/ContractAudit/contract-audit.tsx
+++ packages/trader/src/App/Components/Elements/ContractAudit/contract-audit.tsx
@@ -76,20 +76,18 @@ const ContractAudit = ({
     }
     return (
         <div className='contract-audit__wrapper'>
-            <Tabs top className='contract-audit__tabs' onTabItemClick={onTabItemClick}>
-                <div label={localize('Details')}>
-                    <ContractDetails {...props} />
-                </div>
-                {is_multiplier ? (
-                    <div label={localize('TP & SL History')}>
+            {is_accumulator ? (
+                <ContractDetails {...props} />
+            ) : (
+                <Tabs top className='contract-audit__tabs' onTabItemClick={onTabItemClick}>
+                    <div label={localize('Details')}>
+                        <ContractDetails {...props} />
+                    </div>
+                    <div label={localize(is_multiplier ? 'TP & SL History' : 'TP History')}>
                         <ContractHistory currency={currency} history={update_history} />
                     </div>
-                ) : (
-                    <div label={localize('TP History')}>
-                        <ContractHistory currency={currency} history={update_history} />
-                    </div>
-                )}
-            </Tabs>
+                </Tabs>
+            )}
         </div>
     );
 };

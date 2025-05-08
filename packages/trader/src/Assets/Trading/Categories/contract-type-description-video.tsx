8ce50d1f313889a020915cfef3e77b56e0264185
moiz-deriv <moiz@regentmarkets.com>
2025-05-08 15:29:17 +0800

    Moiz - GRWT-5659 - [DTrader] Show the Guide video's toolbar at the beginning (#18383)
    * fix: added logic for playback controls to be visible for 5 seconds by default
    
    * fix: updated is_tutorial to should_show_controls

--- packages/trader/src/Assets/Trading/Categories/contract-type-description-video.tsx
+++ packages/trader/src/Assets/Trading/Categories/contract-type-description-video.tsx
@@ -20,6 +20,7 @@ const ContractTypeDescriptionVideo = ({ data_testid, selected_contract_type }: T
                 src={getDescriptionVideoId(selected_contract_type, is_dark_theme)}
                 is_mobile={is_mobile}
                 data_testid={data_testid}
+                should_show_controls={true}
             />
         </div>
     );

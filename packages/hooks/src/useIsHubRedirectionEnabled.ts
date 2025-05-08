aa26d8d18957ce87e1e35f090923869e6e98931e
Shayan Khaleghparast <100833613+shayan-deriv@users.noreply.github.com>
2025-05-08 19:50:03 +0800

    fix: fixed redirection issue from dtrader to hub (#18419)

--- packages/hooks/src/useIsHubRedirectionEnabled.ts
+++ packages/hooks/src/useIsHubRedirectionEnabled.ts
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie';
+
 import { useStore } from '@deriv/stores';
 
 import useGrowthbookGetFeatureValue from './useGrowthbookGetFeatureValue';
@@ -13,14 +15,16 @@ const useIsHubRedirectionEnabled = () => {
     const { client } = useStore();
     const { account_settings, clients_country } = client;
 
+    const cookieCountry = JSON.parse(Cookies.get('client_information') || '{}')?.residence;
+
+    const userCountry = cookieCountry || account_settings.country_code;
+
     const isHubRedirectionEnabled =
         typeof hubEnabledCountryList === 'object' &&
         hubEnabledCountryList !== null &&
         Array.isArray((hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list) &&
-        account_settings.country_code &&
-        (hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list.includes(
-            account_settings.country_code
-        );
+        userCountry &&
+        (hubEnabledCountryList as THubEnabledCountryList).hub_enabled_country_list.includes(userCountry);
 
     const isChangingToHubAppId =
         typeof hubEnabledCountryList === 'object' &&

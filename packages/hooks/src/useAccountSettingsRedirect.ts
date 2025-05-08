b45b293161fe4edceed78bd408fa321fc282f15f
Sui Sin <103026762+suisin-deriv@users.noreply.github.com>
2025-05-08 16:45:14 +0800

    Suisin/chore: fix mobile redirection issue (#18414)
    * chore: fix mobile redirection issue
    
    * chore: change from personal-details to home

--- packages/hooks/src/useAccountSettingsRedirect.ts
+++ packages/hooks/src/useAccountSettingsRedirect.ts
@@ -10,7 +10,7 @@ export const useAccountSettingsRedirect = () => {
     const search_params = new URLSearchParams(window.location.search);
     const account_type = search_params.get('account');
 
-    let redirect_url;
+    let redirect_url, mobile_redirect_url;
 
     // Determine if we should use the new hub endpoints
     const should_use_hub = has_wallet && isHubRedirectionEnabled;
@@ -20,11 +20,13 @@ export const useAccountSettingsRedirect = () => {
             process.env.NODE_ENV === 'production' ? 'https://hub.deriv.com' : 'https://staging-hub.deriv.com';
 
         redirect_url = `${base_url}/accounts/redirect?action=redirect_to&redirect_to=home&account=${account_type}`;
+        mobile_redirect_url = `${base_url}/accounts/redirect?action=redirect_to&redirect_to=home&account=${account_type}`;
     } else {
         redirect_url = routes.personal_details;
+        mobile_redirect_url = routes.account;
     }
 
-    return { redirect_url };
+    return { redirect_url, mobile_redirect_url };
 };
 
 export default useAccountSettingsRedirect;

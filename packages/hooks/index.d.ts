ff12fc377214346e0508d2b64b43c6f363170668
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2025-05-09 15:27:58 +0800

    chore: fix package issue

--- packages/hooks/index.d.ts
+++ packages/hooks/index.d.ts
@@ -2,7 +2,7 @@ declare module '@binary-com/binary-document-uploader' {
     import { DocumentUploadRequest } from '@deriv/api-types';
 
     class DocumentUploader {
-        constructor(settings: { connection: any });
+        constructor(settings: { connection: WebSocket | XMLHttpRequest });
         upload(file: {
             documentType?: DocumentUploadRequest['document_type'];
             documentFormat?: string | Omit<string, DocumentUploadRequest['document_format']>;
@@ -11,7 +11,7 @@ declare module '@binary-com/binary-document-uploader' {
             lifetimeValid?: DocumentUploadRequest['lifetime_valid'];
             pageType?: DocumentUploadRequest['page_type'];
             [key: string]: unknown;
-        }): Promise<{ message?: string; warning?: string; [key: string]: any }>;
+        }): Promise<{ message?: string; warning?: string; [key: string]: unknown }>;
     }
 
     export default DocumentUploader; // Export the class as default

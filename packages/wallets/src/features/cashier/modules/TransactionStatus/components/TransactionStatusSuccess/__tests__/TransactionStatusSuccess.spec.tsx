16f1bd514de0785511b66d04023912158ff9bb7e
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2025-05-09 18:25:26 +0800

    chore: fix unit test cases

--- packages/wallets/src/features/cashier/modules/TransactionStatus/components/TransactionStatusSuccess/__tests__/TransactionStatusSuccess.spec.tsx
+++ packages/wallets/src/features/cashier/modules/TransactionStatus/components/TransactionStatusSuccess/__tests__/TransactionStatusSuccess.spec.tsx
@@ -25,7 +25,9 @@ jest.mock('@deriv/api-v2', () => ({
 }));
 jest.mock('react-router-dom', () => ({
     useHistory: jest.fn(),
+    withRouter: (Component: React.ComponentType<any>) => Component,
 }));
+
 const mockUseHistory = useHistory as jest.Mock;
 
 const mockTransactions = [

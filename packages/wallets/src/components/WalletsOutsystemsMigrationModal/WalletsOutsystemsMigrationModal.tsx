d5396ca68180d58dda836a5dbae225a2996f6850
lubega-deriv <lubega@deriv.com>
2025-05-19 16:31:59 +0800

    fix: button onclick redirection

--- packages/wallets/src/components/WalletsOutsystemsMigrationModal/WalletsOutsystemsMigrationModal.tsx
+++ packages/wallets/src/components/WalletsOutsystemsMigrationModal/WalletsOutsystemsMigrationModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Cookies from 'js-cookie';
 import { Localize } from '@deriv-com/translations';
 import { Button, Text, useDevice } from '@deriv-com/ui';
@@ -9,13 +9,6 @@ import './WalletsOutsystemsMigrationModal.scss';
 
 const WalletsOustystemsMigrationModal = () => {
     const { isMobile } = useDevice();
-
-    useEffect(() => {
-        const domain = /deriv\.(com|me|be)/.test(window.location.hostname)
-            ? derivUrls.DERIV_HOST_NAME
-            : window.location.hostname;
-        Cookies.set('wallet_account', 'true', { domain });
-    }, []);
 
     const redirectToOutsystems = () => {
         const redirectUrl =

060a6f0df321e448a75d508ac3ec7868215836ce
amam-deriv <mustofa@deriv.com>
2025-05-20 10:47:24 +0800

    fix: use session storage first before url params (#18459)

--- packages/trader/src/AppV2/Components/ServiceErrorSheet/service-error-sheet.tsx
+++ packages/trader/src/AppV2/Components/ServiceErrorSheet/service-error-sheet.tsx
@@ -60,7 +60,7 @@ const ServiceErrorSheet = observer(() => {
                                 const url_query_string = window.location.search;
                                 const url_params = new URLSearchParams(url_query_string);
                                 const account_currency =
-                                    url_params.get('account') || window.sessionStorage.getItem('account');
+                                    window.sessionStorage.getItem('account') || url_params.get('account');
 
                                 window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
                             } else {

1ba7eb1a737d3df0fbe5ff4273a32abd83e74ebc
Tay Sui Sin <suisin@regentmarkets.com>
2025-05-21 12:06:57 +0800

    chore: fix demo account displaying issue

--- packages/core/src/Modules/Callback/CallbackPage.tsx
+++ packages/core/src/Modules/Callback/CallbackPage.tsx
@@ -40,11 +40,11 @@ const CallbackPage = () => {
                 //added a check for postLoginRedirectUri to basically sync account when user created a new currency from Tradershub and redirected back to DTrader
                 if (redirectTo || (postLoginRedirectUri && !!containsAccount)) {
                     const params = new URLSearchParams(redirectTo || postLoginRedirectUri);
-                    const queryAccount = sessionStorage.getItem('account')
-                        ? sessionStorage.getItem('account')
-                        : params.get('account');
+                    const queryAccount = params.get('account');
 
-                    let matchingLoginId: string | undefined, matchingToken: string | undefined;
+                    let matchingLoginId: string | undefined,
+                        matchingToken: string | undefined,
+                        matchingWalletLoginId: string | undefined;
                     if (queryAccount?.toLowerCase() !== 'demo') {
                         Object.keys(tokens).find(key => {
                             if (key.startsWith('cur') && tokens[key] === queryAccount) {
@@ -61,6 +61,10 @@ const CallbackPage = () => {
                                         matchingToken = tokens[`token${sequence}`];
                                     }
                                 }
+                                if (!isNotCRWallet && !isNotMFWallet) {
+                                    matchingWalletLoginId = tokens[`acct${sequence}`];
+                                    matchingToken = tokens[`token${sequence}`];
+                                }
                             }
                         });
                     } else {
@@ -69,9 +73,14 @@ const CallbackPage = () => {
                                 // get currency sequence number, e.g. cur1=1, cur2=2
                                 const sequence = key.replace('cur', '');
                                 const isDemo = tokens[`acct${sequence}`]?.startsWith('VRTC');
+                                const isWalletDemo = tokens[`acct${sequence}`]?.startsWith('VRW');
 
                                 if (isDemo) {
                                     matchingLoginId = tokens[`acct${sequence}`];
+                                    matchingToken = tokens[`token${sequence}`];
+                                }
+                                if (isWalletDemo) {
+                                    matchingWalletLoginId = tokens[`acct${sequence}`];
                                     matchingToken = tokens[`token${sequence}`];
                                 }
                             }
@@ -81,6 +90,9 @@ const CallbackPage = () => {
                         sessionStorage.setItem('active_loginid', matchingLoginId);
                         localStorage.setItem('config.account1', matchingToken);
                         localStorage.setItem('active_loginid', matchingLoginId);
+                    }
+                    if (matchingWalletLoginId && matchingToken) {
+                        sessionStorage.setItem('active_wallet_loginid', matchingWalletLoginId);
                     }
 
                     sessionStorage.removeItem('tradershub_redirect_to');

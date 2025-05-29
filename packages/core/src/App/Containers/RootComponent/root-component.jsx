69591563416ecf74cbb09e546b2ef57c755a4285
lubega-deriv <lubega@deriv.com>
2025-05-29 14:57:12 +0800

    fix: apply comment

--- packages/core/src/App/Containers/RootComponent/root-component.jsx
+++ packages/core/src/App/Containers/RootComponent/root-component.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import Cookies from 'js-cookie';
-
 import { useIsHubRedirectionEnabled, useOauth2, useSettings } from '@deriv/hooks';
 import { deriv_urls, moduleLoader } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';

db7baae8a4dcf7427708208f4f03ada600af3170
shontzu <108507236+shontzu-deriv@users.noreply.github.com>
2025-06-03 11:04:28 +0800

    chore: because Aizad says so
    Co-authored-by: Aizad Ridzo <103104395+aizad-deriv@users.noreply.github.com>

--- packages/core/src/Stores/traders-hub-store.js
+++ packages/core/src/Stores/traders-hub-store.js
@@ -627,7 +627,7 @@ export default class TradersHubStore extends BaseStore {
             this.CFDs_restricted_countries ||
             this.financial_restricted_countries ||
             (this.root_store.client.is_logged_in &&
-                JSON.parse(JSON.stringify(this.root_store.client.landing_companies.ctrader.all.standard)) === 'none')
+                this.root_store.client.landing_companies.ctrader.all.standard === 'none'
         ) {
             this.available_ctrader_accounts = [];
             return;

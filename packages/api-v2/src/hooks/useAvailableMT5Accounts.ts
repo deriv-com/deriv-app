33414aa29de43f2ff3c8fd92628fc9a77125313f
Tay Sui Sin <suisin@regentmarkets.com>
2025-06-03 17:29:45 +0800

    chore: update Content for compare account table

--- packages/api-v2/src/hooks/useAvailableMT5Accounts.ts
+++ packages/api-v2/src/hooks/useAvailableMT5Accounts.ts
@@ -1,5 +1,7 @@
 import { useMemo } from 'react';
+
 import useQuery from '../useQuery';
+
 import useAuthorize from './useAuthorize';
 
 const market_type_to_leverage_mapper: Record<string, number> = {
@@ -19,12 +21,21 @@ const useAvailableMT5Accounts = () => {
     const modified_mt5_available_accounts = useMemo(
         () =>
             mt5_available_accounts?.trading_platform_available_accounts?.map(account => {
+                const shouldUpdateForex =
+                    account.product === 'financial' &&
+                    ['svg', 'bvi', 'vanuatu', 'dml'].includes(account.shortcode ?? '');
                 return {
                     ...account,
                     /** The market type for the account */
                     market_type: account.market_type === 'gaming' ? 'synthetic' : account.market_type,
                     /** The platform for the account */
                     platform: 'mt5',
+                    /** Updated instruments array */
+                    instruments: shouldUpdateForex
+                        ? account.instruments?.map((instrument: string) =>
+                              instrument === 'Forex: standard/micro' ? 'Forex' : instrument
+                          )
+                        : account.instruments,
                     /** Leverage for the account */
                     leverage:
                         market_type_to_leverage_mapper[

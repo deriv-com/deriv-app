b0f85208daa43ca976f0d049bef412b4b2ff5da3
Tay Sui Sin <suisin@regentmarkets.com>
2025-06-06 15:56:52 +0800

    chore: fix test case

--- __mocks__/utils.mock.js
+++ __mocks__/utils.mock.js
@@ -2,6 +2,11 @@ module.exports = {
     getTmbToken: jest.fn(() => Promise.resolve('mock_token')),
     getActiveAuthTokenIDFromLocalStorage: jest.fn(() => 'mock_auth_token'),
     getActiveLoginIDFromLocalStorage: jest.fn(() => 'mock_login_id'),
+    Chat: {
+        open: jest.fn(),
+        close: jest.fn(),
+        clear: jest.fn(),
+    },
     // Add other utils exports as needed
     formatMoney: jest.fn((amount, currency) => `${amount} ${currency}`),
     getCurrencyDisplayCode: jest.fn(currency => currency),

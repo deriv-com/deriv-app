5aecb2b063fbdfb8ec40a1618b57bffced1e12df
Tay Sui Sin <suisin@regentmarkets.com>
2025-06-06 15:44:07 +0800

    chore: fix test case fail issue

--- __mocks__/utils.mock.js
+++ __mocks__/utils.mock.js
@@ -1,8 +1,11 @@
 module.exports = {
     getTmbToken: jest.fn(() => Promise.resolve('mock_token')),
+    getActiveAuthTokenIDFromLocalStorage: jest.fn(() => 'mock_auth_token'),
+    getActiveLoginIDFromLocalStorage: jest.fn(() => 'mock_login_id'),
     // Add other utils exports as needed
-    formatMoney: jest.fn(),
-    getCurrencyDisplayCode: jest.fn(),
-    getDecimalPlaces: jest.fn(),
+    formatMoney: jest.fn((amount, currency) => `${amount} ${currency}`),
+    getCurrencyDisplayCode: jest.fn(currency => currency),
+    getDecimalPlaces: jest.fn(() => 2),
+    addComma: jest.fn(num => num?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')),
     // Add any other exports from utils that your tests might need
 };

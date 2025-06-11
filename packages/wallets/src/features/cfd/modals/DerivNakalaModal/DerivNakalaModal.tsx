18751bb69f90e1440a878c607ddb6eb3806118a4
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2025-06-11 17:22:33 +0800

    chore: fix content issue

--- packages/wallets/src/features/cfd/modals/DerivNakalaModal/DerivNakalaModal.tsx
+++ packages/wallets/src/features/cfd/modals/DerivNakalaModal/DerivNakalaModal.tsx
@@ -166,17 +166,10 @@ export const CFDDerivNakalaLinkAccount = (props: CFDDerivNakalaAccountModalProps
 
                 <div className='wallets-cfd-nakala-modal__description'>
                     <Text align='center' as='p' size='md'>
-                        {isSuccess ? (
-                            <Localize
-                                components={[<strong key={0} />]}
-                                i18n_default_text='Link your MT5 Standard account to the <0>Deriv Nakala mobile app</0> using these details:'
-                            />
-                        ) : (
-                            <Localize
-                                components={[<strong key={0} />]}
-                                i18n_default_text='<0>Deriv Nakala mobile app</0> should be linked to your MT5 Standard Account, using these details:'
-                            />
-                        )}
+                        <Localize
+                            components={[<strong key={0} />]}
+                            i18n_default_text='Link your MT5 Standard account to the <0>Deriv Nakala mobile app</0> using these details:'
+                        />
                     </Text>
                 </div>
 

b5dae541061fcc8be561432740cd9a07e9918e70
Tay Sui Sin <suisin@regentmarkets.com>
2025-06-17 16:40:21 +0800

    chore: disable tmb for derivgo users

--- packages/utils/src/isTmbEnabled.ts
+++ packages/utils/src/isTmbEnabled.ts
@@ -1,4 +1,11 @@
 const isTmbEnabled = async () => {
+    // add deriv and impersonation check
+    const isFromBo = document.referrer.includes('https://backoffice.binary.com');
+    if (isFromBo) {
+        sessionStorage.setItem('is_from_bo', 'true');
+    }
+
+    const sessionFromBo = sessionStorage.getItem('is_from_bo') === 'true';
     const storedValue = localStorage.getItem('is_tmb_enabled');
     try {
         const url =
@@ -7,7 +14,7 @@ const isTmbEnabled = async () => {
                 : 'https://app-config-staging.firebaseio.com/remote_config/oauth/is_tmb_enabled.json';
         const response = await fetch(url);
         const result = await response.json();
-        return storedValue !== null ? storedValue === 'true' : !!result.app;
+        return storedValue !== null ? storedValue === 'true' : !sessionFromBo && !!result.app;
     } catch (e) {
         // eslint-disable-next-line no-console
         console.error(e);

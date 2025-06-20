27556aeefd9e16172259976ad5d018e05c7e8384
shayan khaleghparast <iman@regentmarkets.com>
2025-06-20 13:11:07 +0800

    fix: secure regex patterns in traders-hub-store

--- packages/core/src/Stores/Helpers/client.js
+++ packages/core/src/Stores/Helpers/client.js
@@ -5,7 +5,7 @@ export const getClientAccountType = loginid => {
     let account_type;
     if (/^VR/.test(loginid)) account_type = 'virtual';
     else if (/^MF/.test(loginid)) account_type = 'financial';
-    else if (/^MLT|MX/.test(loginid)) account_type = 'gaming';
+    else if (/^(MLT|MX)/.test(loginid)) account_type = 'gaming';
     return account_type;
 };
 

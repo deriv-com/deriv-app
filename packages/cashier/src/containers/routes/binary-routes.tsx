bf556e15fb9cfdb65c98c66b5d810bea88d61e1e
lubega-deriv <lubega@deriv.com>
2025-07-07 11:59:27 +0800

    fix: update redirection logic

--- packages/cashier/src/containers/routes/binary-routes.tsx
+++ packages/cashier/src/containers/routes/binary-routes.tsx
@@ -20,62 +20,25 @@ type TBinaryRoutesProps = {
 const BinaryRoutes = (props: TBinaryRoutesProps) => {
     const { client } = useStore();
     const { isHubRedirectionEnabled, isHubRedirectionLoaded } = useIsHubRedirectionEnabled();
-    const {
-        has_wallet,
-        is_logged_in,
-        is_logging_out,
-        prevent_redirect_to_hub,
-        is_client_store_initialized,
-        prevent_single_login,
-        setPreventSingleLogin,
-    } = client;
+    const { has_wallet } = client;
 
     const PRODUCTION_REDIRECT_URL = 'https://hub.deriv.com/tradershub';
     const STAGING_REDIRECT_URL = 'https://staging-hub.deriv.com/tradershub';
 
-    useEffect(() => {
-        if (
-            isHubRedirectionEnabled &&
-            has_wallet &&
-            !is_logging_out &&
-            is_logged_in &&
-            !prevent_redirect_to_hub &&
-            is_client_store_initialized
-        ) {
-            const redirectUrl = process.env.NODE_ENV === 'production' ? PRODUCTION_REDIRECT_URL : STAGING_REDIRECT_URL;
+    if (has_wallet && isHubRedirectionEnabled && isHubRedirectionLoaded) {
+        const redirectUrl = process.env.NODE_ENV === 'production' ? PRODUCTION_REDIRECT_URL : STAGING_REDIRECT_URL;
 
-            const domain = /deriv\.(com|me|be)/.test(window.location.hostname)
-                ? deriv_urls.DERIV_HOST_NAME
-                : window.location.hostname;
-            Cookies.set('wallet_account', true, { domain });
+        const domain = /deriv\.(com|me|be)/.test(window.location.hostname)
+            ? deriv_urls.DERIV_HOST_NAME
+            : window.location.hostname;
+        Cookies.set('wallet_account', true, { domain });
 
-            const url_query_string = window.location.search;
-            const url_params = new URLSearchParams(url_query_string);
-            const account_currency = window.sessionStorage.getItem('account') || url_params.get('account');
-            localStorage.setItem('wallet_redirect_done', true);
-            window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
-        }
-
-        const shouldStayInDerivApp = !isHubRedirectionEnabled || !has_wallet || prevent_redirect_to_hub;
-        if (prevent_single_login && isHubRedirectionLoaded && is_client_store_initialized && shouldStayInDerivApp) {
-            setPreventSingleLogin(false);
-        }
-    }, [
-        isHubRedirectionLoaded,
-        isHubRedirectionEnabled,
-        has_wallet,
-        is_logging_out,
-        is_logged_in,
-        prevent_redirect_to_hub,
-        prevent_single_login,
-        is_client_store_initialized,
-    ]);
-
-    if (isHubRedirectionEnabled) {
-        return <Loading is_fullscreen />;
+        const url_query_string = window.location.search;
+        const url_params = new URLSearchParams(url_query_string);
+        const account_currency = window.sessionStorage.getItem('account') || url_params.get('account');
+        localStorage.setItem('wallet_redirect_done', true);
+        window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
     }
-
-    if (has_wallet) return <Page404 />;
 
     return (
         <React.Suspense fallback={<Loading className='cashier__loader' is_fullscreen={false} />}>

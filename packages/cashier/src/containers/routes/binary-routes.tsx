0279c9ee67208d0096a09dc803b1c2887092c1fc
lubega-deriv <142860499+lubega-deriv@users.noreply.github.com>
2025-07-07 14:13:50 +0800

    [TRAH] Lubega / Cashier redirect to outsystems checks on staging (#18686)
    * fix: update redirection logic
    
    * fix: added console logs

--- packages/cashier/src/containers/routes/binary-routes.tsx
+++ packages/cashier/src/containers/routes/binary-routes.tsx
@@ -1,10 +1,9 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import Cookies from 'js-cookie';
 
 import { Loading } from '@deriv/components';
 import { useIsHubRedirectionEnabled } from '@deriv/hooks';
-import { deriv_urls } from '@deriv/shared';
 import { useStore } from '@deriv/stores';
 
 import getRoutesConfig from 'Constants/routes-config';
@@ -20,23 +19,24 @@ const BinaryRoutes = (props: TBinaryRoutesProps) => {
     const { client } = useStore();
     const { isHubRedirectionEnabled, isHubRedirectionLoaded } = useIsHubRedirectionEnabled();
     const { has_wallet } = client;
-
     const PRODUCTION_REDIRECT_URL = 'https://hub.deriv.com/tradershub';
     const STAGING_REDIRECT_URL = 'https://staging-hub.deriv.com/tradershub';
 
-    if (has_wallet && isHubRedirectionEnabled && isHubRedirectionLoaded) {
+    if (has_wallet) {
         const redirectUrl = process.env.NODE_ENV === 'production' ? PRODUCTION_REDIRECT_URL : STAGING_REDIRECT_URL;
 
-        const domain = /deriv\.(com|me|be)/.test(window.location.hostname)
-            ? deriv_urls.DERIV_HOST_NAME
-            : window.location.hostname;
-        Cookies.set('wallet_account', true, { domain });
-
+        console.log('hub redirection 1', isHubRedirectionEnabled);
         const url_query_string = window.location.search;
         const url_params = new URLSearchParams(url_query_string);
         const account_currency = window.sessionStorage.getItem('account') || url_params.get('account');
+        console.log('hub redirection 2', isHubRedirectionEnabled);
         window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
+        console.log('hub redirection 3', isHubRedirectionEnabled);
     }
+
+    console.log('has_wallet', has_wallet);
+    console.log('hub redirection 4', isHubRedirectionEnabled);
+    console.log('hub redirection loaded', isHubRedirectionLoaded);
 
     return (
         <React.Suspense fallback={<Loading className='cashier__loader' is_fullscreen={false} />}>

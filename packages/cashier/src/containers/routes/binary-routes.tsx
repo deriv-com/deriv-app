5764b0721d53898a48869049f3104fd9e0c19e0e
lubega-deriv <lubega@deriv.com>
2025-07-07 15:22:30 +0800

    fix: remove console logs

--- packages/cashier/src/containers/routes/binary-routes.tsx
+++ packages/cashier/src/containers/routes/binary-routes.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
@@ -17,26 +16,19 @@ type TBinaryRoutesProps = {
 
 const BinaryRoutes = (props: TBinaryRoutesProps) => {
     const { client } = useStore();
-    const { isHubRedirectionEnabled, isHubRedirectionLoaded } = useIsHubRedirectionEnabled();
+    const { isHubRedirectionEnabled } = useIsHubRedirectionEnabled();
     const { has_wallet } = client;
     const PRODUCTION_REDIRECT_URL = 'https://hub.deriv.com/tradershub';
     const STAGING_REDIRECT_URL = 'https://staging-hub.deriv.com/tradershub';
 
-    if (has_wallet) {
+    if (has_wallet && isHubRedirectionEnabled) {
         const redirectUrl = process.env.NODE_ENV === 'production' ? PRODUCTION_REDIRECT_URL : STAGING_REDIRECT_URL;
 
-        console.log('hub redirection 1', isHubRedirectionEnabled);
         const url_query_string = window.location.search;
         const url_params = new URLSearchParams(url_query_string);
         const account_currency = window.sessionStorage.getItem('account') || url_params.get('account');
-        console.log('hub redirection 2', isHubRedirectionEnabled);
         window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
-        console.log('hub redirection 3', isHubRedirectionEnabled);
     }
-
-    console.log('has_wallet', has_wallet);
-    console.log('hub redirection 4', isHubRedirectionEnabled);
-    console.log('hub redirection loaded', isHubRedirectionLoaded);
 
     return (
         <React.Suspense fallback={<Loading className='cashier__loader' is_fullscreen={false} />}>

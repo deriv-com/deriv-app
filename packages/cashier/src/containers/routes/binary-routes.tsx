abf34673346949c50d147f87a4833a3052bea9df
lubega-deriv <lubega@deriv.com>
2025-07-07 16:26:33 +0800

    fix: added use effect logic

--- packages/cashier/src/containers/routes/binary-routes.tsx
+++ packages/cashier/src/containers/routes/binary-routes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 
 import { Loading } from '@deriv/components';
@@ -17,18 +17,22 @@ type TBinaryRoutesProps = {
 const BinaryRoutes = (props: TBinaryRoutesProps) => {
     const { client } = useStore();
     const { isHubRedirectionEnabled } = useIsHubRedirectionEnabled();
-    const { has_wallet } = client;
+    const { has_wallet, is_logged_in, is_logging_out, is_client_store_initialized } = client;
     const PRODUCTION_REDIRECT_URL = 'https://hub.deriv.com/tradershub';
     const STAGING_REDIRECT_URL = 'https://staging-hub.deriv.com/tradershub';
 
-    if (has_wallet && isHubRedirectionEnabled) {
-        const redirectUrl = process.env.NODE_ENV === 'production' ? PRODUCTION_REDIRECT_URL : STAGING_REDIRECT_URL;
+    useEffect(() => {
+        if (isHubRedirectionEnabled && has_wallet && !is_logging_out && is_logged_in && is_client_store_initialized) {
+            const redirectUrl = process.env.NODE_ENV === 'production' ? PRODUCTION_REDIRECT_URL : STAGING_REDIRECT_URL;
 
-        const url_query_string = window.location.search;
-        const url_params = new URLSearchParams(url_query_string);
-        const account_currency = window.sessionStorage.getItem('account') || url_params.get('account');
-        window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
-    }
+            const url_query_string = window.location.search;
+            const url_params = new URLSearchParams(url_query_string);
+            const account_currency = window.sessionStorage.getItem('account') || url_params.get('account');
+            window.location.href = `${redirectUrl}/redirect?action=redirect_to&redirect_to=wallet${account_currency ? `&account=${account_currency}` : ''}`;
+        }
+    }, [isHubRedirectionEnabled, has_wallet, is_logging_out, is_logged_in, is_client_store_initialized]);
+
+    if (has_wallet) return <Loading is_fullscreen />;
 
     return (
         <React.Suspense fallback={<Loading className='cashier__loader' is_fullscreen={false} />}>

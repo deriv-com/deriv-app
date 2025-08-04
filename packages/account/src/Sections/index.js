913cf0cad6eb3d51a123c8522ccd24ac0cd0a23e
habib-deriv <habib@deriv.com>
2025-08-04 21:15:59 +0800

    chore: add console message to update hash

--- packages/account/src/Sections/index.js
+++ packages/account/src/Sections/index.js
@@ -1,11 +1,15 @@
-import PersonalDetails from 'Sections/Profile/PersonalDetails';
-import { ProofOfIdentityContainer, ProofOfIdentity, ProofOfIdentityFlow } from 'Sections/Verification/ProofOfIdentity';
-import { ProofOfAddress, ProofOfAddressFlow } from 'Sections/Verification/ProofOfAddress';
-import ProofOfOwnership from 'Sections/Verification/ProofOfOwnership';
-import ProofOfIncome from 'Sections/Verification/ProofOfIncome';
 import Account from 'Containers/Account/account';
-import EmploymentTaxInfo from '../Containers/employment-tax-info/employment-tax-info';
+import PersonalDetails from 'Sections/Profile/PersonalDetails';
 import DeactivateAccount from 'Sections/Security/DeactivateAccount'; // TODO: Remove once mobile team has changed this link
+import { ProofOfAddress, ProofOfAddressFlow } from 'Sections/Verification/ProofOfAddress';
+import { ProofOfIdentity, ProofOfIdentityContainer, ProofOfIdentityFlow } from 'Sections/Verification/ProofOfIdentity';
+import ProofOfIncome from 'Sections/Verification/ProofOfIncome';
+import ProofOfOwnership from 'Sections/Verification/ProofOfOwnership';
+
+import EmploymentTaxInfo from '../Containers/employment-tax-info/employment-tax-info';
+
+// eslint-disable-next-line no-console
+console.log(`File Upload Engine hash update: ${Math.random()}`);
 
 export {
     PersonalDetails,

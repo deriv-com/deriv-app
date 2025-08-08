ca7a6ae8f876b808b30c521b34e0f84e5dbd8b12
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2025-08-08 14:44:40 +0800

    chore: add null check to resolve crash issue on cashier

--- packages/hooks/src/useIsEnabledNakala.ts
+++ packages/hooks/src/useIsEnabledNakala.ts
@@ -19,7 +19,9 @@ const useIsEnabledNakala = (accounts: any[]) => {
         const account = priorityOrder.reduce((found, company) => {
             if (found) return found;
             return accounts.find(
-                acc => acc.landing_company_name?.toLowerCase() === company || acc.landing_company_short === company
+                acc =>
+                    acc &&
+                    (acc.landing_company_name?.toLowerCase() === company || acc.landing_company_short === company)
             );
         }, null);
         if (account) return account;

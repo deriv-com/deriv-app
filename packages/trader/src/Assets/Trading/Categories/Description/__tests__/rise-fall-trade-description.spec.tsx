db0f047bf940639b4ef7ad9f4edf1b5a17a6d945
mayuran-deriv <balamayuran@regentmarkets.com>
2025-08-21 11:29:46 +0400

    fix: test case

--- packages/trader/src/Assets/Trading/Categories/Description/__tests__/rise-fall-trade-description.spec.tsx
+++ packages/trader/src/Assets/Trading/Categories/Description/__tests__/rise-fall-trade-description.spec.tsx
@@ -6,6 +6,8 @@ describe('<RiseFallTradeDescription />', () => {
     it('a proper text of description should be rendered', () => {
         render(<RiseFallTradeDescription />);
 
-        expect(screen.getByText(/If you select "Rise", you win the payout if the exit/i)).toBeInTheDocument();
+        expect(
+            screen.getByText(/Rise\/Fall lets you predict if the market price will end higher/i)
+        ).toBeInTheDocument();
     });
 });

56a7fd08434057efc1431d7b0dc5529fb7489378
Nijil Nirmal <62882794+nijil-deriv@users.noreply.github.com>
2025-11-07 11:48:20 +0400

    Nijil / GRWT-6013 / Fix Dtrader v2 bottom navbar padding (#18916)
    * fix: dtrader v2 bottom navbar padding
    
    * fix: bottom nav badge style
    
    * fix: bottom navbar icon colors

--- packages/trader/src/AppV2/Components/BottomNav/bottom-nav.tsx
+++ packages/trader/src/AppV2/Components/BottomNav/bottom-nav.tsx
@@ -10,7 +10,7 @@ import {
     StandaloneClockThreeFillIcon,
     StandaloneClockThreeRegularIcon,
 } from '@deriv/quill-icons';
-import { Badge, Navigation } from '@deriv-com/quill-ui';
+import { Navigation, Text } from '@deriv-com/quill-ui';
 import { useStore } from '@deriv/stores';
 import { useHistory, useLocation } from 'react-router';
 
@@ -29,44 +29,19 @@ const BottomNav = observer(({ children, className, onScroll }: BottomNavProps) =
 
     const bottomNavItems = [
         {
-            icon: <StandaloneChartAreaRegularIcon iconSize='sm' />,
-            activeIcon: <StandaloneChartAreaFillIcon iconSize='sm' />,
+            icon: <StandaloneChartAreaRegularIcon iconSize='sm' fill='var(--component-navigationBar-icon-default)' />,
+            activeIcon: (
+                <StandaloneChartAreaFillIcon iconSize='sm' fill='var(--component-navigationBar-icon-selected)' />
+            ),
             label: <Localize i18n_default_text='Trade' />,
             path: routes.trade,
         },
         {
-            icon:
-                active_positions_count > 0 ? (
-                    <Badge
-                        variant='notification'
-                        position='top-right'
-                        label={active_positions_count.toString()}
-                        color='danger'
-                        size='sm'
-                        contentSize='sm'
-                        className='bottom-nav-item__position-badge'
-                    >
-                        <StandaloneClockThreeRegularIcon iconSize='sm' />
-                    </Badge>
-                ) : (
-                    <StandaloneClockThreeRegularIcon iconSize='sm' />
-                ),
-            activeIcon:
-                active_positions_count > 0 ? (
-                    <Badge
-                        variant='notification'
-                        position='top-right'
-                        label={active_positions_count.toString()}
-                        color='danger'
-                        size='sm'
-                        contentSize='sm'
-                        className='bottom-nav-item__position-badge'
-                    >
-                        <StandaloneClockThreeFillIcon iconSize='sm' />
-                    </Badge>
-                ) : (
-                    <StandaloneClockThreeFillIcon iconSize='sm' />
-                ),
+            icon: <StandaloneClockThreeRegularIcon iconSize='sm' fill='var(--component-navigationBar-icon-default)' />,
+            activeIcon: (
+                <StandaloneClockThreeFillIcon iconSize='sm' fill='var(--component-navigationBar-icon-selected)' />
+            ),
+            badge: active_positions_count > 0 ? active_positions_count.toString() : undefined,
             label: (
                 <React.Fragment>
                     <span className='user-guide__anchor' />
@@ -91,19 +66,19 @@ const BottomNav = observer(({ children, className, onScroll }: BottomNavProps) =
                 {children}
             </div>
             {is_logged_in ? (
-                <Navigation.Bottom className='bottom-nav-container' onChange={(_, index) => handleSelect(index)}>
+                <Navigation.Bottom onChange={(_, index) => handleSelect(index)} value={selectedIndex}>
                     {bottomNavItems.map((item, index) => (
                         <Navigation.BottomAction
                             key={index}
                             index={index}
-                            activeIcon={<></>}
-                            icon={index === selectedIndex ? item.activeIcon : item.icon}
+                            badge={item.badge}
+                            activeIcon={item.activeIcon}
+                            icon={item.icon}
                             label={item.label}
                             selected={index === selectedIndex}
                             showLabel
                             className={clsx(
                                 'bottom-nav-item',
-                                index === selectedIndex && 'bottom-nav-item--active',
                                 item.path === routes.trader_positions && 'bottom-nav-item--positions'
                             )}
                         />

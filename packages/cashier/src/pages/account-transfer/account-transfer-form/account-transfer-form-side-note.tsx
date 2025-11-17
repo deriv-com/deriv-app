ec8309c5865c548c26aba3736dfbf9b0f1dc4474
lubega-deriv <lubega@deriv.com>
2025-11-17 18:16:26 +0800

    feat: remove transfer fee

--- packages/cashier/src/pages/account-transfer/account-transfer-form/account-transfer-form-side-note.tsx
+++ packages/cashier/src/pages/account-transfer/account-transfer-form/account-transfer-form-side-note.tsx
@@ -3,7 +3,7 @@ import React, { useCallback } from 'react';
 import { SideNote, Text } from '@deriv/components';
 import { getCurrencyDisplayCode, getPlatformSettings } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
-import { Localize, localize } from '@deriv/translations';
+import { Localize } from '@deriv/translations';
 
 import { useCashierStore } from '../../../stores/useCashierStores';
 
@@ -20,7 +20,7 @@ const AccountTransferFormSideNote = observer(() => {
     const platform_name_ctrader = getPlatformSettings('ctrader').name;
 
     const { client } = useStore();
-    const { account_limits, is_dxtrade_allowed, is_ctrader_allowed } = client;
+    const { account_limits, is_ctrader_allowed } = client;
 
     const { account_transfer } = useCashierStore();
 
@@ -45,104 +45,28 @@ const AccountTransferFormSideNote = observer(() => {
     const is_dxtrade_transfer = selected_to.is_dxtrade || selected_from.is_dxtrade;
     const is_mt_transfer = selected_to.is_mt || selected_from.is_mt;
 
-    const getZeroTransferFeeString = (is_dxtrade_allowed: boolean, hasDxTradeAccount: boolean): string => {
-        if (is_dxtrade_allowed) {
-            if (hasDxTradeAccount) {
-                return localize(
-                    'We do not charge a transfer fee for transfers in the same currency between your Deriv fiat and {{platform_name_mt5}} accounts, between your Deriv fiat and {{platform_name_ctrader}} accounts, and between your Deriv fiat and {{platform_name_dxtrade}} accounts.',
-                    {
-                        platform_name_mt5,
-                        platform_name_ctrader,
-                        platform_name_dxtrade,
-                    }
-                );
-            }
-            return localize(
-                'We do not charge a transfer fee for transfers in the same currency between your Deriv fiat and {{platform_name_mt5}} accounts and between your Deriv fiat and {{platform_name_ctrader}} accounts.',
-                {
-                    platform_name_mt5,
-                    platform_name_ctrader,
-                }
-            );
-        }
-        return localize(
-            "You'll not be charged a transfer fee for transfers in the same currency between your Deriv fiat and {{platform_name_mt5}} accounts.",
-            {
-                platform_name_mt5,
-            }
-        );
-    };
-
     const getTransferFeeNote = useCallback(() => {
-        if (transfer_fee === 0) {
-            return getZeroTransferFeeString(is_dxtrade_allowed, hasDxTradeAccount);
-        } else if (transfer_fee === 1) {
-            return is_dxtrade_allowed && hasDxTradeAccount ? (
-                <Localize
-                    i18n_default_text='We’ll charge a 1% transfer fee for transfers in different currencies between your Deriv fiat and {{platform_name_mt5}} accounts and between your Deriv fiat and {{platform_name_dxtrade}} accounts.'
-                    values={{ platform_name_dxtrade, platform_name_mt5 }}
-                />
-            ) : (
-                <Localize
-                    i18n_default_text='We’ll charge a 1% transfer fee for transfers in different currencies between your Deriv fiat and {{platform_name_mt5}} accounts.'
-                    values={{ platform_name_mt5 }}
-                />
-            );
-        } else if (transfer_fee === 2 && is_crypto_to_crypto_transfer) {
+        if (transfer_fee && is_crypto_to_crypto_transfer) {
             return (
                 <Localize
-                    i18n_default_text='We’ll charge a 2% transfer fee or {{minimum_fee}} {{currency}}, whichever is higher, for transfers between your Deriv cryptocurrency accounts.'
+                    i18n_default_text='When you move funds between your Deriv cryptocurrency accounts, an adjustment might be applied to reflect the bid–ask value in the market. Some transfers might not be possible.'
                     values={{
                         minimum_fee,
                         currency: getCurrencyDisplayCode(currency),
                     }}
                 />
             );
-        } else if (transfer_fee === 2 && (is_mt_transfer || is_dxtrade_transfer)) {
-            return is_dxtrade_allowed && hasDxTradeAccount ? (
-                <Localize
-                    i18n_default_text='We’ll charge a 2% transfer fee or {{minimum_fee}} {{currency}}, whichever is higher, for transfers between your Deriv cryptocurrency and Deriv MT5 accounts and between your Deriv cryptocurrency and {{platform_name_dxtrade}} accounts.'
-                    values={{
-                        minimum_fee,
-                        currency: getCurrencyDisplayCode(currency),
-                        platform_name_dxtrade,
-                    }}
-                />
-            ) : (
-                <Localize
-                    i18n_default_text='We’ll charge a 2% transfer fee or {{minimum_fee}} {{currency}}, whichever is higher, for transfers between your Deriv cryptocurrency and Deriv MT5 accounts.'
-                    values={{
-                        minimum_fee,
-                        currency: getCurrencyDisplayCode(currency),
-                    }}
-                />
-            );
-        } else if (transfer_fee === 2 && !is_mt_transfer && !is_dxtrade_transfer) {
+        } else if (transfer_fee && (is_mt_transfer || is_dxtrade_transfer)) {
             return (
-                <Localize
-                    i18n_default_text='We’ll charge a 2% transfer fee or {{minimum_fee}} {{currency}}, whichever is higher, for transfers between your Deriv fiat and Deriv cryptocurrency accounts.'
-                    values={{
-                        minimum_fee,
-                        currency: getCurrencyDisplayCode(currency),
-                    }}
-                />
+                <Localize i18n_default_text='When you move funds between your Deriv cryptocurrency and Deriv MT5 accounts, an adjustment might be applied to reflect the bid–ask value in the market. Some transfers might not be possible.' />
+            );
+        } else if (transfer_fee && !is_mt_transfer && !is_dxtrade_transfer) {
+            return (
+                <Localize i18n_default_text='When you move funds between your Deriv cryptocurrency and Deriv fiat accounts, an adjustment might be applied to reflect the bid–ask value in the market. Some transfers might not be possible.' />
             );
         }
         return null;
-    }, [
-        currency,
-        is_crypto_to_crypto_transfer,
-        is_dxtrade_allowed,
-        is_dxtrade_transfer,
-        is_mt_transfer,
-        minimum_fee,
-        platform_name_dxtrade,
-        platform_name_mt5,
-        platform_name_ctrader,
-        transfer_fee,
-        selected_from.value,
-        selected_to.value,
-    ]);
+    }, [currency, is_crypto_to_crypto_transfer, is_dxtrade_transfer, is_mt_transfer, minimum_fee, transfer_fee]);
 
     const getDxtradeAllowedNotes = useCallback(() => {
         if (hasDxTradeAccount) {
@@ -220,13 +144,10 @@ const AccountTransferFormSideNote = observer(() => {
         allowed_transfers_count?.internal,
         allowed_transfers_count?.ctrader,
         allowed_transfers_count?.mt5,
-        is_dxtrade_allowed,
         is_ctrader_allowed,
         platform_name_dxtrade,
         platform_name_mt5,
         platform_name_ctrader,
-        selected_from.value,
-        selected_to.value,
     ]);
 
     return (
@@ -235,10 +156,9 @@ const AccountTransferFormSideNote = observer(() => {
             <AccountTransferFormSideNoteBullet>
                 <Localize i18n_default_text='Transfer limits may vary depending on the exchange rates.' />
             </AccountTransferFormSideNoteBullet>
-            <AccountTransferFormSideNoteBullet>
-                {getTransferFeeNote()}{' '}
-                <Localize i18n_default_text='Please bear in mind that some transfers may not be possible.' />
-            </AccountTransferFormSideNoteBullet>
+            {transfer_fee && (
+                <AccountTransferFormSideNoteBullet>{getTransferFeeNote()}</AccountTransferFormSideNoteBullet>
+            )}
             <AccountTransferFormSideNoteBullet>
                 <Localize i18n_default_text='Transfers may be unavailable due to high volatility or technical issues and when the exchange markets are closed.' />
             </AccountTransferFormSideNoteBullet>

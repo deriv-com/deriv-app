1bfdb63fd9e5ce756edb81f3642b892aec5bd3a3
amina-deriv <amina@regentmarkets.com>
2025-11-28 15:27:43 +0400

    fix: banner

--- packages/appstore/src/components/banners/traders-hub-banners/traders-hub-banners.tsx
+++ packages/appstore/src/components/banners/traders-hub-banners/traders-hub-banners.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Loading } from '@deriv/components';
+import { Icon, Loading, Text } from '@deriv/components';
 import { useGrowthbookGetFeatureValue, useStoreHasAccountDeposited, useWalletMigration } from '@deriv/hooks';
 import { makeLazyLoader, moduleLoader } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
+import { Localize } from '@deriv/translations';
 
 import BookBanner from 'Components/banners/book-banner';
 import WalletsBanner from 'Components/banners/wallets-banner';
+
+import './traders-hub-banner.scss';
 
 const RealAccountCreationBanner = makeLazyLoader(
     () =>
@@ -48,11 +51,7 @@ const TradersHubBanners = observer(() => {
         defaultValue: false,
     });
 
-    const is_wallet_banner_visible =
-        is_in_progress ||
-        is_migrating ||
-        is_failed ||
-        (!is_eu && is_eligible);
+    const is_wallet_banner_visible = is_in_progress || is_migrating || is_failed || (!is_eu && is_eligible);
 
     const should_add_empty_div_for_get_started_trading_banner_clever_tap = has_any_real_account;
     const should_show_real_account_creation_banner =
@@ -66,8 +65,26 @@ const TradersHubBanners = observer(() => {
         !hasTransferred &&
         !is_wallet_banner_visible;
 
+    const [showMarketMaintenanceBanner, isMarketMaintenanceFFLoaded] = useGrowthbookGetFeatureValue({
+        featureFlag: 'show-market-maintenance-banner',
+    });
+
     return (
         <React.Fragment key={current_language}>
+            {isMarketMaintenanceFFLoaded && showMarketMaintenanceBanner && (
+                <div className='da-inline-full-width-note-with-icon-banner'>
+                    <div>
+                        <Icon icon='IcAppstoreNewWarning' size='24' />
+                    </div>
+
+                    <Text as='p' size='xs' line_height='s'>
+                        <Localize
+                            i18n_default_text='Due to a market issue, your trading experience may be affected on symbols such as US stock indices, commodities, and metals.
+Pricing may be unavailable or delayed, and orders may be interrupted or rejected. Trading is suspended for selected instruments until liquidity is restored.'
+                        />
+                    </Text>
+                </div>
+            )}
             {should_add_empty_div_for_get_started_trading_banner_clever_tap && (
                 <div className='get-started-trading-banner-ct' />
             )}
